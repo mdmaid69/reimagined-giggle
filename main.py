60ee23bc015241959cc7444030d5d8badc9c20dd
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:12:16 +0000

    as mix slow range clever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+x = 10
+y = 20
+print("Sum:", x + y)
