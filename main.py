d409997723352955d59a2c33d5efef1fa561ab75
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:21:28 +0000

    gas 571enough rate wheel 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_area(radius):
+        return 3.14 * radius * radius
