365f2668031b278254d67ced5f6064cf38ac7b69
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:42:08 +0000

    goback(to) period dish high rub

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+from collections import Counter
+print(Counter("hello world"))
