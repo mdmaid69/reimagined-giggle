be49b50aa889a809578543fc8124b37dd0039fb6
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:53:58 +0000

    square below single protest recent

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
