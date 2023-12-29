16ce15d6fc6b8de25b9dfbfe44ba89be84446294
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:59:59 +0000

    considering please bother repeat turnon

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+for i in range(5):
+        print(i)
