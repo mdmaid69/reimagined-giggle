2f2a5d19854faa3bfe5fa471af324616e407e5a3
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:49:09 +0000

    breath window how information doubt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def add_numbers(x, y):
-        return x + y
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
