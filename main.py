60a648643e7604969c4799f2c3b3a61eecb7ea09
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:19:28 +0000

    frame justice who benefit responsible

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
