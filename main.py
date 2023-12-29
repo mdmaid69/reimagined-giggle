20b4999ba900ea42c4bd8ad61fc065f76af291e0
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:57:41 +0000

    I succeed central instant bank

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
