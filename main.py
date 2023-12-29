fe26af758847953c5adb8f6fcf36704794b5a032
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:10:06 +0000

    believe divide authority fresh invite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
