fe1d12d9322ef1ad1d2e4b6d3037a076c4ccaebd
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:27:26 +0000

    distance sore factory slight force

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def find_union(list1, list2):
+        return set(list1) | set(list2)
