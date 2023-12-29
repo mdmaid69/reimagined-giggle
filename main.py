611231c913f6badfdfb276135fe1025124b583f8
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:09:58 +0000

    avoid Oo strange crowd open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def extend_array(array, iterable):
+        array.extend(iterable)
