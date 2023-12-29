12f601c8f39ebaa7c2c3b84f549dc3348ee050f4
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:40:11 +0000

    correct flat eager slowly attempt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def find_min(numbers):
+        return min(numbers)
