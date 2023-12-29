4609bd2b285f0454446d5c11f573fea7d4d5f8ee
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:20:52 +0000

    art Qq club offend burst

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
