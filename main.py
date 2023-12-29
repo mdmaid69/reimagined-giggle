58d31f5ef4f48ef07dcdef2400e647c10bc10f4f
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:59:09 +0000

    want you proper wipe yes

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
