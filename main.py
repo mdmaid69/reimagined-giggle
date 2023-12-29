e9a8d188a1562e450670259e65d17bb79e29d63a
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:49:44 +0000

    authority train march cheque leave

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
