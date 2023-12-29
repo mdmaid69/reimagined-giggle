3544d236eb6e24bb3de909d648fae9e47257790f
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:04:13 +0000

    last inside drive secret seriously

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import logging
-def log_message(message):
-        logging.info(message)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def reverse_list(lst):
+        return lst[::-1]
