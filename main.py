d09b9945afafffe7aef47b75fab7c6f17fd0a8b3
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:39:54 +0000

    flood sense promise comfortable finger

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+print(sum(range(10)))
