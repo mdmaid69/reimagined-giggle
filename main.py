781dc33a6e9b563ddc1b2c9eea91d24dd01dbfbf
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:32:54 +0000

    substance old pollution thank interrupt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
