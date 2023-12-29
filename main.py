576508ec0d9ddf973c63f9623fa70a0016237f15
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:43:46 +0000

    book listenfor sign table worried

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_length(array):
+        return len(array)
+import array
+def append_to_array(array, item):
+        array.append(item)
