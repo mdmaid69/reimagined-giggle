1d2e7f704639cdf9ba168b3825221eee84126d61
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:01:04 +0000

    steep ashamed pleasant describe loss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
