e9b8f469b4a4ce35fa05dae58261fed0301ffa25
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:05:51 +0000

    practical prevent frequent related pleasant

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_stack():
+        return collections.deque()
