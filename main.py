ac470ecd9ff358f8ff720df47dc8f71137c282f6
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:16:30 +0000

    goon eye adjective fortunately main

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def convert_to_binary(n):
+        return bin(n)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
