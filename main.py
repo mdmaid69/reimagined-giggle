1689e32115d0f23ed1267300a4df6e1ef12d9678
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:26:31 +0000

    attract often job present load

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  def sort_list(lst):
+        return sorted(lst)
