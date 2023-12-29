841b1eef5147202ae95f8d9c5c028f11a90c1318
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:22:00 +0000

    see move goodbye arm exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def reverse_array(array):
+        array.reverse()
