928e31eea939a2e2852c87e27a96b60a8c6edd65
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:27:31 +0000

    people offer region zero start

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_max(lst):
+        return max(lst)
