63c229a320c73f215bea63678b5b78e5f2aaa382
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:27:35 +0000

    direct dress accident crowded top

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
