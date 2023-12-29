661930c0423e7f3d277cc5e7400660d34128c6e1
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:48:25 +0000

    exam dress bin on vanish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import random
+print(random.randint(0, 100))
