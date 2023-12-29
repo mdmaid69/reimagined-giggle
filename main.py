d488de07e09398558b98e4ce44a198ffb948448e
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:06:05 +0000

    its Tt difference ill number

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def get_array_as_set(array):
+        return set(array)
