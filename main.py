c9ec528b28a53c333b70c8587b1c524590e0baa5
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:53:16 +0000

    ordinary real experience fault opponent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
