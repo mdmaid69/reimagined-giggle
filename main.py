2df1085089c1f2f0af991f2116136b1438b00825
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:12:01 +0000

    heavy tense seem obtain major

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
