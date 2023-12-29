aaadff1160abbd85358be01690e186ebf6e56462
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:03:12 +0000

    pool lucky fun appearance wrong

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def subtract_numbers(x, y):
-        return x - y
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
