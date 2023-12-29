8f351ba3ac4987be0d1a5778e10cf44f9f059c9e
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:22:48 +0000

    plenty inside fortunately round indeed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
