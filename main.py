2ef6bbaa06710a1f9df783f25e1fd9eeaba15b59
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:02:43 +0000

    himself along secret yourself sail

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def subtract_numbers(x, y):
-        return x - y
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+print(math.pi)
