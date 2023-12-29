017a5130527cc1fc13d289cb13e98e5607b68534
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:30:52 +0000

    spill suck course minute without

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def multiply_numbers(x, y):
+        return x * y
