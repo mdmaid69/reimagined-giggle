8f4ccfd652ab14656e7fa102fb657a23782d4744
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:37:51 +0000

    several decide spend book uponon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
