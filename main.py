de3f5c2a84cbd1edc391c130eb71c9e201956bce
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:28:45 +0000

    ride wander soak mess tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
