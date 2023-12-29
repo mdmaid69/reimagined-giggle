92843c9342e613d2b1b52dc0600ed865aa901e24
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:47:44 +0000

    born fall case standard then

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_directory_name(path):
+        return os.path.dirname(path)
