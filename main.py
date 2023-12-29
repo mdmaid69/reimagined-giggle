38794cd091dd3cb808c6f3aeb1e613789c31fed6
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:38:31 +0000

    live service risk immediately certain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def count_words(sentence):
+        return len(sentence.split())
