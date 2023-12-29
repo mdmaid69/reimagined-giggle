e6691b1213177aa0694a63be8347fdbe3993dc50
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:29:48 +0000

    creature remain to only impressive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_list_from_array(array):
+        return array.tolist()
