5cb83b64f3ea794708c1633b50c838978ca7e13d
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:32:21 +0000

    Rr exaggerate firm concerned further

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
