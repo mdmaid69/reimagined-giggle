6270d6afa898743f88052b355ac9058bee188599
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:05:24 +0000

    ago whichever colour treatment crazy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
