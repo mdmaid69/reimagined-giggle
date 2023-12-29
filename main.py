ff8630653de97a4a7c7bfcab7b18f4687054fb0c
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:56:21 +0000

    schedule act lesson run tight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
