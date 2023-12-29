14566cca4e902906fad893db68604a80857929e0
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:18:46 +0000

    accommodation 179bite accidental takeback fast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_perpetuity(payment, rate):
+        return payment / rate
