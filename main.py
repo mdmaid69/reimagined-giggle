39deb2ee863804d993f43b10438287971a2cf673
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:01:47 +0000

    Vv after promise produce shine

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
