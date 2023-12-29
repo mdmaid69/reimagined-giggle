2f9953de0b727cadb5aa2ea4c6ea74abbb05a5bf
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:25:53 +0000

    mass dick the arrive window

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  def convert_to_hex(n):
-        return hex(n)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
