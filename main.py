e543bacfc1403a3fb583ef2591c79190b1c6c1ef
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:36:07 +0000

    advanced unless team happy star

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
