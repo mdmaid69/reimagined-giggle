126c241f0d8546f98fd8983340de4f3df0b3eb00
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:58:53 +0000

    about mystery negative major contact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
