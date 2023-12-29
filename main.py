79f0e03972ea50aac1da333fa3802b1e877727ea
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:40:06 +0000

    nowhere skirt warning beneath unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import datetime
+def get_today_date():
+        return datetime.date.today()
