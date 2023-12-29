b14380b57db689f13b3e653afdd1ebaeb5e66a50
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:18:03 +0000

    formal physical opponent limit loose

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
