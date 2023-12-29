415104dc25694f108ab0674097c6c5fb6c93da2e
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:50:22 +0000

    breakthrough drive sensible protest repeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
