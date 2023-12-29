8651bedb4ff4086fad90d5849528c56d91c25964
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:21:36 +0000

    possibly join angry impressed pass

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import time
+print(time.time())
