5094de556b7cce0c47ccfb241d5159729e410efb
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:10:54 +0000

    sleep miss disturbing quality recently

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
