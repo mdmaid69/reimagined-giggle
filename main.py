46975504747f49925f7bef0b6b656c6014a82264
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:15:06 +0000

    sensible build effect beginning extremely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import array
-def get_array_as_set(array):
-        return set(array)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
