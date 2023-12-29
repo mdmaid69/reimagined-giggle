39d8bfcc701e32293439f9187710aa45e2169fa8
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:16:59 +0000

    naturally reduce prefer relationship name

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-print(sys.version)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
