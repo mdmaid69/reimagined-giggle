7300bd58a688acf468d5970b8298eb064fea7abd
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:40:44 +0000

    spring night juice pity I

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_work(force, distance):
+        return force * distance
+def find_max(numbers):
+        return max(numbers)
