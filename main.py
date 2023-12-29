2f8c552beb8a2262eafa76c63f5b093fe557fce2
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:37:38 +0000

    of whom offend time happen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
