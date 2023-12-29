406a36fb8e6d15317e7f0dbe52b9c358117bbcea
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:49:22 +0000

    calmly meaning hope most mainly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
