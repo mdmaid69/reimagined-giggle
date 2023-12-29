a27482bdbb1051916d89c0cb28055ff19e4b75b5
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:05:51 +0000

    chief peace everything during combination

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
