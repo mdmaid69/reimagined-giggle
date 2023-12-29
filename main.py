7451f24ef9f92d2b891d4529d64b86c0023e92b8
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:40:33 +0000

    interested anxious consider apparently contact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
