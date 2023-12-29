f21e01d5f992851bd8cd4206d960292267c867a4
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:24:54 +0000

    hospital number stamp crash wall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def sort_numbers(numbers):
+        return sorted(numbers)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
