a5ff560fbcb21b8a4d95fade114c82ae816e1a15
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:54:00 +0000

    comeback Itself stare terrible wear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
