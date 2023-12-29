6a9ff87d64301ba2cdcdaaeeceb7b18c12cc4fbf
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:47:04 +0000

    escape tooth night dinner interview

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
