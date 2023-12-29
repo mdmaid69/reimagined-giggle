14c763dbccd2c62d31d3a30dbf26da7faa591abf
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:20:29 +0000

    actual distant next brave figure

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def find_min(lst):
+        return min(lst)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
