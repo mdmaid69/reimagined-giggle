fa5278b80512a2e3afb5ea49b5515b98dbedd4f3
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:16:50 +0000

    brain total sight although fire

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
