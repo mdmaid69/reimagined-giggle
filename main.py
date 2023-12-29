b1b1002ccc5139f7eeece33c2ad9a314e3611d56
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:53:18 +0000

    place country surprising tip slightly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
