5989c308a8320ab62dd2e3789809dee4ea516433
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:49:04 +0000

    bury answer announce report provide

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
