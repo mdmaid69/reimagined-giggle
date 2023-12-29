c87cfa35861be4a2562dc78debe36373c46c7e55
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:31:19 +0000

    challenge file demonstrate payment punish

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
