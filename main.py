ecf68959c8fa469ed91bf6ef8b51ae092630f5f6
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:43:43 +0000

    earth immediately deaf especially back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
