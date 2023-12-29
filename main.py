ad24e24ff07f162cb177c0b1cf13f9bf70e73c20
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:33:15 +0000

    ready urgent Kk unfortunate near

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def reverse_string(s):
+        return s[::-1]
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
