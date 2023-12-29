1a2f61c5b54690bc378a1867e7c8b3e2d59058fd
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:44:50 +0000

    attempt business record evening scene

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+def find_min(lst):
+        return min(lst)
