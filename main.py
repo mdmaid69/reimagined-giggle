e2a67efbfc36678607c9345e7c6c6de5783bde5d
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:04:15 +0000

    each remain summer present so

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-for i in range(10): print(i)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
