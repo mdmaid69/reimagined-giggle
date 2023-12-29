84bdc345bea2474a37e1cf24eb712f2abcd3c133
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:00:30 +0000

    line expect normal sick disappear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-print(os.getcwd())
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
