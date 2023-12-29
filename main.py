6d76fa375b7716f724babc59c35514446a4f28ea
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:04:00 +0000

    dig roof convince stop general

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
 import collections
-def create_counter():
-        return collections.Counter()
+def create_queue():
+        return collections.deque()
