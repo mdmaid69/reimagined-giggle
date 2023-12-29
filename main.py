57db03b7415823220ef7c2808ae0bc48a2ec1fc8
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:04:48 +0000

    luck steep unpleasant properly nervous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
