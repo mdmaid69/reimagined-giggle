6ba861636cf15cc52c5e9a02663dfa65316aaf6f
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:52:09 +0000

    tap and press guide impatient

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
