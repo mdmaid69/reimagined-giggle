15404126287c81a5f247d545e676c2ad9b9910da
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:58:59 +0000

    uncomfortable teach feed leave responsible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
