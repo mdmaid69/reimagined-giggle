29791fa6c5bd774e06ffb2dcea8c2c6d863644b5
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:47:42 +0000

    weather thought listen high warning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
