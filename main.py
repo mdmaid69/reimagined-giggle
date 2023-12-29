0199ad8db7142ffd283c06a017b5e988a9e782c7
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:00:35 +0000

    achievement sticky part win still

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
