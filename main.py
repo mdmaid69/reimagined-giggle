be7e2f4c50906df26fc400950792a5687009a6ec
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:40:14 +0000

    walk react among press concerning

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def calculate_work(force, distance):
-        return force * distance
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
