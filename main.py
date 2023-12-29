b987417f329c81958e75e0bd4a12c28bf5c13a81
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:53:59 +0000

    grade talkoutof shut autumn paper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
   import numpy as np
   def calculate_variance(arr):
         return np.var(arr)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
