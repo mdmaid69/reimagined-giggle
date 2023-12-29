fec0903e09eedd32681795fefce45c08e4c9c182
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:11:19 +0000

    extraordinary total guess nigga principle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import collections
-def create_priority_queue():
-        return collections.deque()
+import time
+print(time.time())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
