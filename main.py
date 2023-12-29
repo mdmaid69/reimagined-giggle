40eff7f4bb367530feed3bcf3e456ab58269830a
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:55:04 +0000

    our scared event exact available

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
