e7edc9a1a40929ab2365b05d565010ba08af47ea
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:06:17 +0000

    control start beyond round shoot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
