fb7439e3c49f57b2cf1674672d1ce4428e0163e7
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:35:11 +0000

    feeling train fall ring book

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
