9a4b04077dfd8091d86d17b3bc612fd2c67ea278
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:06:16 +0000

    concerning land else yourself almost

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def count_words(sentence):
+        return len(sentence.split())
