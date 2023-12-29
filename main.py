e07db2d59393fe232d64c4b4797f0b16b9ce2bfa
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:52:11 +0000

    shame manage weak image yes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import getpass
+def get_username():
+        return getpass.getuser()
