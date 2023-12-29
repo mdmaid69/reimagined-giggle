12e74b35a589f1f26172506d93d150cabf1641e5
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:04:54 +0000

    manage manner matter record some

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  def add_numbers(x, y):
+        return x + y
