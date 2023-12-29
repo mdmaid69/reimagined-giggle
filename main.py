adbb32c61b546d5892117bff042697655ea0915b
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:22:11 +0000

    win kick share little need

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
