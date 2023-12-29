5b32dba4edf1cdd5ba2967247d4e9550759c41b3
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:42:27 +0000

    mistake home break convenient absolutely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
