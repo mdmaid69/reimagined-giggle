bb0a503d2d1a605dd927269ae9aa7c436aaf36a5
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:56:11 +0000

    agree experiment mixture feel trick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_area(radius):
+        return 3.14 * radius * radius
