2a9aeac0068f82c3ea72ef1ce54623cd489e065d
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:29:19 +0000

    hello print play easy note

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def convert_to_binary(n):
-        return bin(n)
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
