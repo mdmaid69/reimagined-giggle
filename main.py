b459a84dd0dc5d4fbdf2ee8fde1ccbc025688cc3
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:32:40 +0000

    sentence scream fit sight event

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
