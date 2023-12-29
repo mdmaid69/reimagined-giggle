1c9a82dd27a191e3e2e9259bd0d676c2a831a9e6
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:53:31 +0000

    sting any so gas small

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
