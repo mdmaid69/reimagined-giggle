b814dad84553be20e7e32625459fe4f366943507
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:53:48 +0000

    injury so thanks gate light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_nlink(file_name):
+  def get_file_number_of_links(file_name):
         return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
