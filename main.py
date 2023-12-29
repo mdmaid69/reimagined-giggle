16015e0889bd80ecb31280787ba2fd85d7c35e1c
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:28:29 +0000

    tell slide building actual art

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
