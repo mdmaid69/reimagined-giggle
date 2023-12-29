464ac1328eb097549ccfca1b6190a270616de01b
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:31:01 +0000

    professional guess summer regularly holdon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
