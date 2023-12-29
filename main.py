536fee69635aae2ff72c7730c1aec688f2c5a810
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:37:37 +0000

    news Vv lay promise honest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def square_number(x):
+        return x**2
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
