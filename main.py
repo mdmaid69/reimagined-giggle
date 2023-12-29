24b39d6e878cae4248dbd68aa92d54cd3a6a5782
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:06:41 +0000

    help judge catch combination congratulations

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def find_min(lst):
+        return min(lst)
