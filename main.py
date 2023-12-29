2ae15e9f7bb5ec55d87b04e19482e32ba17fe572
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:11:11 +0000

    store turnon result draw corner

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
