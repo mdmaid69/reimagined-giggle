f27fb8aa8290a1bd85cd122e58066732aae46e88
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:55:05 +0000

    shelter urgent yesterday radio once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
