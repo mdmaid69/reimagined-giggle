9063bf6c27328f1e4d817666076d11cb95014598
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:38:21 +0000

    direct Mm song attach completely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
