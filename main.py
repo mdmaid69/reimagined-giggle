a09b5a6aadc6dcb52e0c7de76184f6df9ed05a16
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:03:05 +0000

    recommend street unless combine forever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
