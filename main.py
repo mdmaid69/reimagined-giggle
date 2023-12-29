06cc802409f30167231a4baf1d5c01b9a230195b
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:55:04 +0000

    save sharp number lonely joke

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
