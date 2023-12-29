cf88aeab53962e42b3c9718416cd3ec8e1a233ab
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:41:34 +0000

    burn big stand tape defend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def cube_number(x):
+        return x**3
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
