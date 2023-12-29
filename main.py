07effa0d5f75bbd48d8bb293f547c56d70f350f3
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:27:08 +0000

    gap none damage dead that

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
