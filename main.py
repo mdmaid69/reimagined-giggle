ac13996863978dab080ef5f79039c3bb1857e551
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:29:43 +0000

    before drive be attract exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def get_list_from_array(array):
+        return array.tolist()
