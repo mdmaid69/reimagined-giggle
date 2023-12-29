249db3f0e177a37f8278eef3eb442dd83dfd1c8e
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:40:42 +0000

    backwards determined at shelter die

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
