e5e7bcd645a2c12be44ae081004559ed24205617
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:33:07 +0000

    arrange ignore Gg shut Ii

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
