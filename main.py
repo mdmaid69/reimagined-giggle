db2a5237db0f8ffda099b7495a7f9f23f104e360
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:19:27 +0000

    test recent suddenly evidence slice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
