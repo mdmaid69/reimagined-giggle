61c25275818766a1fe05e7b5bd610ea48fa19aad
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:39:02 +0000

    off piece amount basically willing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-for i in range(10): print(i)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
