8a63f173b6ebd914bbac9b509ac91b04c91d7b2b
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:46:43 +0000

    have mine great shadow fault

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
