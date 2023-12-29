35c181ac1bedfc8ef9d5aca3f5488b7055b79125
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:02:47 +0000

    load put whether careful after

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
