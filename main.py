07e7219c9064cb2dbece89eb51b3a09892d2c6b1
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:55:01 +0000

    beautiful bill last occasional legal

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
