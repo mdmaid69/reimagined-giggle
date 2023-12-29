6a1efe3bf776dbe9548c5700bba9f6e426a10d59
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:53:26 +0000

    frame guess book black confusing

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  def reverse_list(lst):
+        return lst[::-1]
