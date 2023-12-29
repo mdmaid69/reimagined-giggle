9444dddba758d19a4440c84a530fecc1ddb74c54
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:00:01 +0000

    calm certainly climb whistle now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+from collections import Counter
+print(Counter("hello world"))
