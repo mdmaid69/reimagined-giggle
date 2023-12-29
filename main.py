7ece5bb19f334624ed2a20f1b6d3ff710424cb33
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:13:17 +0000

    stone along ride whole trust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
