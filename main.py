36a6f769533eedd688e559f03177e763559a5a81
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:30:10 +0000

    except completely dig value accidental

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
