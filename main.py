717882f415017e6b2f58d7efcdccaf68d8a43762
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:39:48 +0000

    so design cheat within might

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
