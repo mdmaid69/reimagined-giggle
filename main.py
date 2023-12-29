ec2e8f59af333348eb4af01fd9483461703edd23
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:17:41 +0000

    risk ring mark opinion impossible

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import time
-print(time.time())
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
