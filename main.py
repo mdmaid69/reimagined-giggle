dd5c6eab01381d0c6d22d19c3935ab9da56a5875
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:21:57 +0000

    love occasional spin able perhaps

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  def is_odd(n):
+        return n % 2 != 0
