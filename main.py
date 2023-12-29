9c9585aacb7bd34bf8041c85a4dfd7c00c0f52bc
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:01:55 +0000

    rely walk swim correction slice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
