cc5e6f6bf5b3a36f5e59214f0b0b3df21996c241
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:42:26 +0000

    concentration other positive work study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
