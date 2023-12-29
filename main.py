f0b758b817b6122d43480aafb3faa9577e22e883
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:24:45 +0000

    spin uponon wise centre hot

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_area_circle(r):
+        return 3.14 * r**2
