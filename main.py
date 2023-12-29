bc4f139387c11789a6e449add4f348a7e56ed7a4
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:03:35 +0000

    question set ashamed innocent past

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
