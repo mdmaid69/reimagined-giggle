7605f287b4a369f4a4efa372f42bb7082d485c19
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:08:03 +0000

    familiar community admire plenty somewhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import time
+def get_current_time():
+        return time.time()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
