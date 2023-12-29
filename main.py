4e019020883c006e753d97adf3c779f506403583
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:27:14 +0000

    welcome Itself this between read

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def convert_array_to_list(array):
+def get_list_from_array(array):
         return array.tolist()
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
