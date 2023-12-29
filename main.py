d6988d9e8b49dd50b4ddbf66fa66e131dbeb51ef
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:24:49 +0000

    taste fall build deserve record

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+print("Hello, world!")
