5406bb0ff27ceb047b2f073f2304b6cea76958e9
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:45:43 +0000

    getback underneath say poison slice

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-print([x**2 for x in range(10)])
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
