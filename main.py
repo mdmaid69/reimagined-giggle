d7adad2c71bcebd6259d8beef1be9bfd57f4b9a6
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:02:21 +0000

    visit mainly port for the

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def square_number(x):
-        return x**2
+import random
+print(random.randint(0, 100))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
