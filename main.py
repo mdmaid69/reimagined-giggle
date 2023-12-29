78a729c9d911ae97673ba56287133c931eb2be08
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:45:43 +0000

    floor wind whisper pronunciation accommodation

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
