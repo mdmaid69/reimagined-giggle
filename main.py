e5a51e3db76dfffd7c5d486da4b8dd9d8e3c63a0
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:37:27 +0000

    provide into advantage hope check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
