0326158af63958aa33fdf0ef06664cde585cf0c9
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:06:19 +0000

    clue apply late punishment dress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def is_palindrome(s):
+        return s == s[::-1]
