e4ce4f1ac57524903a316d84ac029dbedda2a0b9
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:01:45 +0000

    Itself basis board ticket by

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def reverse_list(lst):
+        return lst[::-1]
