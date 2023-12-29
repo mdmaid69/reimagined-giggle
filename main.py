61ecf942705b914d8325d7a51601ee6aaff07298
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:16:50 +0000

    concentrate none slice name for

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def count_characters(sentence):
+        return len(sentence)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
