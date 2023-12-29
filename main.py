025589f9373c5c9b73418bc125b3d9f4f63e8831
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:16:50 +0000

    polite consult name trust chase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
