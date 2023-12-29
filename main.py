fb2ac59f46e1012e019aaf073c76fdac2f8bf4ef
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:40:42 +0000

    argument pretend attention understand building

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_density(mass, volume):
+        return mass / volume
