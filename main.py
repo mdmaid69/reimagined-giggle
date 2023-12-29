0b118a09544e5f2f1ca3f1f223032190fec836f8
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:16:56 +0000

    silent length crime score battery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
