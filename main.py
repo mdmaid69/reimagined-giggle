a1023bbf1f1811cd17c62b8f23403a776e152459
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:13:23 +0000

    ball delicate scream police belief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+from collections import Counter
+print(Counter("hello world"))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
