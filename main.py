7bb0aaa5720454cd2602410e64788a40fcd4259f
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:27:25 +0000

    lift wash although cold disappear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
