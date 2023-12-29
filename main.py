1499f48266b6c76a898a7c86fc1c89659b8913a4
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:13:11 +0000

    lonely transparent recover inform tense

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
