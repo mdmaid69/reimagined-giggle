2a226b6e9e4d6331052ce84925f2a802f6ea224c
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:49:25 +0000

    picture favour mix thinkof wind

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sys
-def print_python_version():
-        print(sys.version)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def convert_to_octal(n):
+        return oct(n)
