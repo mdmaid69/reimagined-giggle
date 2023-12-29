e17e35448937e57301365d0316572fdb61002b4f
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:24:40 +0000

    proud outside than stop one

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+text = "Hello, world!"
+print("Characters:", len(text))
