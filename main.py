a60def6a68a351d49aad40acfae29b86cae16fda
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:37:41 +0000

    thing why bringforward command dish

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import collections
-def create_counter():
-        return collections.Counter()
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
