de16b33c0edd23978718d4c7732e688b918b4d43
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:07:09 +0000

    need small cap contact deep

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def calculate_factorial(n):
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_factorial(n):
         return 1 if n == 0 else n * calculate_factorial(n-1)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
