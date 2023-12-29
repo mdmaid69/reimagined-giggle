e1e0af7ad53ced99b7cb062ba9f50a5d09bd071d
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:38:34 +0000

    stretch sick carry damp loose

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
