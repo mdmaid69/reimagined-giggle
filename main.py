e862c938ad37673e8b5445f35071cd5504f9f25f
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:04:15 +0000

    broken Ee male on past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
