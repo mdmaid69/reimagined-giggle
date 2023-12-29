fa4ec24f8f43fa7646c32ca24d9b303eb6b16ffe
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:07:36 +0000

    hope worst along store hole

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
 import os
-def change_working_directory(path):
-        os.chdir(path)
+def remove_directory(path):
+        os.rmdir(path)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
