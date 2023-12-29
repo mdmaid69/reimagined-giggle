01b08e8d1ba25bdf4fd8076fb1b72e8cd53598b8
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:59:47 +0000

    decision unless treatment wise listen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def find_min(lst):
-        return min(lst)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
