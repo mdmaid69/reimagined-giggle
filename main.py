d1347bd1cfd717813cad1460f80420e19ce62b8b
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:06:25 +0000

    ground talkoutof avoid perfect born

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
