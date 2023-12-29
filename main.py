ed9963c93aba385f54bec76ff9ee4de2bf7a5994
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:42:33 +0000

    create because somebody first cover

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def greet(name):
+        print(f"Hello, {name}!")
 import multiprocessing
 def get_cpu_count():
         return multiprocessing.cpu_count()
