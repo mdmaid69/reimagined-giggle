342c5947e0d6b8ab586c013074f22c8854312918
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:06:28 +0000

    regret far count talkback difference

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_itemsize(array):
+        return array.itemsize
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
