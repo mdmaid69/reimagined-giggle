3579269463ac9221b786dcbf9d317b61d913ae7e
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:49:31 +0000

    computer pin lesson Ww enthusiastic

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def is_even(n):
-        return n % 2 == 0
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
