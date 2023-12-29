54b3b5414e10b3dd2c45a60b68a8ec7be3371064
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:20:04 +0000

    easy fall or wear pick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
