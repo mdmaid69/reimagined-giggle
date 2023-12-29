ab7f82d10619aa29c66989fe936b9833809ebdd0
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:33:18 +0000

    572enter any split turn lose

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def remove_duplicates(lst):
-        return list(set(lst))
+def cube_number(x):
+        return x**3
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
