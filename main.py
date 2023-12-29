edad1f7a81e7f92b5f547287cb5171f0bd556b8f
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:17:54 +0000

    disappear common age good press

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import platform
-def get_os_info():
-        return platform.uname()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
