dbbf7794cf508e42512a5a5db28669ab797cfa50
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:54:09 +0000

    among second bin will image

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+print("Hello, world!")
+def calculate_work(force, distance):
+        return force * distance
