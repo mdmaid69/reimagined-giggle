f6705ea6245633c3a7ab238675731f9db308da28
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:54:59 +0000

    each beat pay badly schedule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
