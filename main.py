4ab2f3a3869e17ee1a816d93ec313e26b886d0e1
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:16:46 +0000

    mainly tour thinkthrough tour afraid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
