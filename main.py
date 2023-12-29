1bfad3138e28ff71a665708135d110885791ff12
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:08:11 +0000

    rescue gift bin tear warning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def get_list_from_array(array):
+        return array.tolist()
