329cc35b4c47c6577d75b389cb875f08fc3620bb
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:38:09 +0000

    stretch flood very scare sea

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
