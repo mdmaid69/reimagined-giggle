8927db0ab13b2354d64f982e5d42b70dca0e27b0
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:36:08 +0000

    down clean underneath wall early

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def divide_numbers(x, y):
-        return x / y
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
