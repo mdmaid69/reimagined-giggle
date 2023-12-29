4335a955b0caa5e51a21d642a466f1dbc1bf864e
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:58:33 +0000

    bringback floor warning callat relation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
