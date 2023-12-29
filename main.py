55329831ca01e755f2526271c807efdc19f8dfca
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:40:35 +0000

    section beside them recommend show

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
