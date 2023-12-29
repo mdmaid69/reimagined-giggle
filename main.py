993ee386b64da825760c2a380716b7731f5efdd2
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:53:35 +0000

    chance complicated permission watch work

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def find_max(numbers):
+        return max(numbers)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
