2034cc3a43483d743945b2cbba8eb4ef7dccc0db
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:23:23 +0000

    dish dinner accidental walk drawing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Characters:", len(text))
