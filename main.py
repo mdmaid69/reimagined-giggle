81ff662a9d07762898f19b92640f961ed4861047
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:20:53 +0000

    whole early course opinion later

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def count_elements(lst):
+        return len(lst)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
