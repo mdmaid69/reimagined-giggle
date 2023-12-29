c313c774dc32db6cb66b1688a038360bc60c7b62
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:48:04 +0000

    declare wind what check minute

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def cube_number(x):
+        return x**3
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
