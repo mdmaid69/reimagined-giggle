85a5c57b5d2d3483297cb824d2cc6fe660c5f938
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:22:28 +0000

    remain art swim thumb know

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
