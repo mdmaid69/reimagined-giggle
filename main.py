5a32c7a3ca02769816cba4cbc1a7a628defbec7a
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:46:20 +0000

    arrange play friend hardly ignore

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
