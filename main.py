8a940210942a0072de18b91e7c9b18aa40f497e3
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:12:19 +0000

    catch study exchange 571enough rain

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
