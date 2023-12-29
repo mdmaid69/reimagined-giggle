241f632099d49ed01a3d2dc8481517276a39db56
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:28:33 +0000

    wash consult makeup aim lock

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
