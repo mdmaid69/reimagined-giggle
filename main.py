9fdb8d955010553f7035d8da9a0dff3b444be9c3
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:34:50 +0000

    except strong suck pleased clean

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
