55839d930768ef93962e4a60cd892eb19ec04e83
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:41:40 +0000

    house bringforward contact across that

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
