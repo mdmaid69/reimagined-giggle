54b3a4dc88c1dc455673d314c2cb02ff5b6909a6
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:35:41 +0000

    morning sweep himself shine machine

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+for i in range(10): print(i)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
