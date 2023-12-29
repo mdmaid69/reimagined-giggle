c63df769e690fd4b22637aa80f4be40e277bdd14
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:29:59 +0000

    below refer guide identify hope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-for i in range(5):
-        print(i)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
