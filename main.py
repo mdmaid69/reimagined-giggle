22618bff04b8d6d1ad7f7f4bcef4a68e0aada80f
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:01:17 +0000

    thankyou himself style band cover

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import sys
+print(sys.version)
+def convert_to_binary(n):
+        return bin(n)
