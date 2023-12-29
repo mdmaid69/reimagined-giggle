2842dfc3e016e47531cd590e8b1cb5113b914bb0
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:00:42 +0000

    typical burn declaration trial us

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
