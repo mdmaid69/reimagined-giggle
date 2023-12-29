6a1837bee010bef12170c45d6c10394d6f1453da
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:10:21 +0000

    contact belong let bringback sight

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def find_min(lst):
+        return min(lst)
