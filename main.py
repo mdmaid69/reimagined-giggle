b05e900620fc2744245a89f7b910bf0ebabfa7fb
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:56:25 +0000

    related any square evidence any

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
