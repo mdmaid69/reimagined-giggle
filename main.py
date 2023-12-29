0ba1987949022190967645769b282488c4209ab8
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:03:48 +0000

    wire proof lower loud afternoon

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
