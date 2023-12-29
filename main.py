6af96c81d2fdc86ef7a5267ef245f8bb50165446
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:06:14 +0000

    play report afford worth unit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
