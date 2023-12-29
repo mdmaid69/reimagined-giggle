d55a5ce5e16840342e497496da23ba13d8d7468f
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:56:56 +0000

    official week last bread happy

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_max(numbers):
-        return max(numbers)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import array
+def get_list_from_array(array):
+        return array.tolist()
