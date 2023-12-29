b703be100f2aa25f5064908cc9459295b0276762
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:02:54 +0000

    argument care gentle couple among

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
