28ef4dc411fb6e96ed4839cc79563f622843af09
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:07:14 +0000

    ahead married insist certain easy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
