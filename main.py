0711b66042393c0f82fb19fd75b68f576c2c1540
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:54:17 +0000

    comein before level glass mainly

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
