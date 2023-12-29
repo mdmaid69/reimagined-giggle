6a36309697fd63d35797a1254856f5914b9404b3
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:44:06 +0000

    punishment whenever worry appear race

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
