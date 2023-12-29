2955433fe7f84ffcfcfded74fa1512056e74bf2f
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:29:42 +0000

    safe either keepfrom rely reject

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_density(mass, volume):
+        return mass / volume
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
