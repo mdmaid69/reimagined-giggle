59259083ef0be45267fe64dc3c000bcdc38bce80
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:12:10 +0000

    spring excited rest strict schedule

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
