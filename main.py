58486557b3bd0a21d895fd5d6223709a6794e29d
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:32:25 +0000

    false child nigga crash count

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
