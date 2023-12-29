3baf0d4d0557703c31c6808444570f3731b904e3
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:40:43 +0000

    proper yesterday rest cause home

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_max(numbers):
+        return max(numbers)
+  def reverse_list(lst):
+        return lst[::-1]
