bdd4186327b608e4ca252c8dfbf24900712d115c
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:44:37 +0000

    everything door object holdup demand

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
