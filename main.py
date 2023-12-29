9419023abfb2f5dd2404e03972a3c29b54ee8dff
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:16:20 +0000

    post can prepare nose same

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
