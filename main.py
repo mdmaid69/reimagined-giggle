fb87458ab3d8e1e68f801496692626c875382e1b
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:20:18 +0000

    raise doubt handle danger road

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import sys
+print(sys.version)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
