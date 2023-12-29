cef13ba8850049bb175918c43d72d61bac2c7828
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:48:27 +0000

    something difficulty holiday second pick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
