6ad34ab0d9f325035d9ae7685000cb93f3f84a20
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:18:25 +0000

    command unusual recently cum even

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
