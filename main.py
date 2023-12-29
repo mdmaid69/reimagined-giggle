9195271e987ea2450e79d00c324508c89cd31ad6
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:19:12 +0000

    goup tap Oo burn 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import time
+print(time.time())
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
