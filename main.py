e1bb31dddb7223197f09bf069855bdeebdb2113a
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:32:31 +0000

    call grateful regular always hurry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
