aeb5dda4cbb5a3b42ac2a79ed99fa80895e24979
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:54:25 +0000

    past beyond professional count address

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
