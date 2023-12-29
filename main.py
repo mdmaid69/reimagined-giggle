8f4f7e055e406213f05322ea670344ca32cec0c5
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:08:47 +0000

    complain roll autumn clue beginning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def find_min(lst):
+        return min(lst)
