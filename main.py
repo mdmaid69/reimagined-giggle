af5e10c643eb02269daecbd1b678b7141e6f7a66
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:57:54 +0000

    nose sweep character count closed

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+print(sum(range(10)))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
