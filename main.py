6f57184c7f52f27ca57fbcddbf9f4fcb8391d078
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:49:52 +0000

    roll patient shock Cc stick

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def find_max(numbers):
+        return max(numbers)
