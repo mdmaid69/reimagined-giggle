731053509ce689a1c2d939b79553855a5ba940b3
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:28:30 +0000

    turnover level nor print yourself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
