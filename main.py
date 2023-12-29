2eb8ff643b1618b811b54e185a8986c2430048f7
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:53:55 +0000

    rule giveup false score agreement

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
