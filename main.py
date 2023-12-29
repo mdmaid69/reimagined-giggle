ff116a4cc3b7b494a5c3f46781d777960cbbaf67
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:35:04 +0000

    roof gooff proper willing goodbye

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def divide_numbers(x, y):
+        return x / y
