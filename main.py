1143423781b1d3e78c3e4bb0b6d3d548b90571cb
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:04:56 +0000

    expect understand practice nowhere warn

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def divide_numbers(x, y):
-        return x / y
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
