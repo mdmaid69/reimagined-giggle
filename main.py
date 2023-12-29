c9451372ae6d772980e24c86d9c609b65c308dfe
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:34:14 +0000

    Bb circle should throw by

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
