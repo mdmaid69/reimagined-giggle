eeb2f3d07734dd693bda2496e5e6627d17cfbb6b
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:42:19 +0000

    send warning need deep crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
