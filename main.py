2f6b83445ba99b75b891df11a70e550b1397122b
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:09:31 +0000

    pride birth keepoff score stare

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import sys
-def exit_program():
-        sys.exit()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
