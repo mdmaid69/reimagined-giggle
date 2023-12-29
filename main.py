975b5d731e3b4d29318155bce9dedc7114e6dbb2
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:50:40 +0000

    circumstance payment great would past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def is_odd(n):
+        return n % 2 != 0
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_factorial(n):
+        return math.factorial(n)
