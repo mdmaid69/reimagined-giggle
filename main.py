b3afa13234679b55eb30284245db633f32a71f6b
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:22:53 +0000

    turnback Qq rub measure broad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_bool(array):
+        return bool(array)
