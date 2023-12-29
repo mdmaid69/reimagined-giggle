86ddcefd54c618c2060ec1392270fab37188aa11
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:22:36 +0000

    hardly official rubbish crowded petrol

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
