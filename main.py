077d5f416599019cc4f9ca7e24acb9de6fed2ff3
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:01:27 +0000

    season year light game usual

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
