e3f76f363b48ae4793439d670519086d6157eb38
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:47:57 +0000

    Pp space die call giveout

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
