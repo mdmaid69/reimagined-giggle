4494b7229112149f119220bc94a4a28a89b8903d
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:07:47 +0000

    upwards offer band nonsense typical

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_volume(length, width, height):
+        return length * width * height
