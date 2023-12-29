2e39593cbeb485f3a9876207b098d35114efc8ed
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:52:17 +0000

    argument soft bad off considering

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
