87580d2b835fb86bfde38615a48c7b1db45bd6eb
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:03:44 +0000

    choice safe criticism pipe Ii

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
