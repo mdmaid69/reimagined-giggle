d5b5c7d353059b3639e5e42d41ff346984f7780f
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:25:48 +0000

    instead convenient natural cruelty brain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
