d1e453ae6415ebc67d2435388f8fb2b59c0337a4
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:55:06 +0000

    pot narrow fence scale face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
