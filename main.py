c6088d63943fbc6f4a5b60106afd2fca39b92128
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:11:40 +0000

    turnon dig instruction rush born

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
