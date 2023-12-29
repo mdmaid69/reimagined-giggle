8438fbbdcb7e6ca3f5d246386ae80ffdf0269fd6
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:14:35 +0000

    callout begin organization within bend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_base_name(path):
+        return os.path.basename(path)
