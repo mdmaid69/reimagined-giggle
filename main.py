ff5bbfd5d4d691c64aac0236d76ec21d8e0f2843
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:35:11 +0000

    become break record catch press

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
