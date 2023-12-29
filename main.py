1cb3b1ee1aaa3bb975b245a0110c33b783e6c0c2
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:42:19 +0000

    once plan destroy visit connection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  def cube_number(x):
-        return x**3
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
