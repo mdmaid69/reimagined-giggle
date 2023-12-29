06f5e9bc57e9f478733f9144fa62bad9dd69d809
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:05:13 +0000

    nice slowly giveaway reserve negative

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
