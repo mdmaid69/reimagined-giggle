01e4054eab32121a1630233ace5f6d0681a30117
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:48:45 +0000

    stop know song last lead

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
