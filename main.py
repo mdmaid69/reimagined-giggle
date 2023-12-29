4b944bee6590e5756124123ee8554cd54c31100d
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:05:57 +0000

    thirsty bite rare slow party

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
