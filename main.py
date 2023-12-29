e10d9b3124163ebd8a36f3db67f8a4b2286cdc7c
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:53:11 +0000

    case involve agree fit bin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
