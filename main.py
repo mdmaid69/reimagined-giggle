1ae0a395e9808d3920c5592a6d576c9b2da74597
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:49:37 +0000

    week completely advise somewhere shadow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
