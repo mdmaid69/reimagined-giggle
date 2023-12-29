09fad18203235086c3acda8c7b25fd2e19ad8e4b
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:58:17 +0000

    spray answer minor worry church

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import re
-print(re.match("h.*o", "hello world"))
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import json
+print(json.dumps({"name": "John", "age": 30}))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
