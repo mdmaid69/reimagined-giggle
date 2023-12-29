bd831624e380340140e1430ede78d3e0c46bf019
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:02:46 +0000

    plant less means suggestion uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  def remove_duplicates(lst):
+        return list(set(lst))
