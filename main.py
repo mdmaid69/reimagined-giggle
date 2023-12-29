7926718f08720cec0e4e03f60b0994203368aefc
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:53:05 +0000

    takeoff content father mend television

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
