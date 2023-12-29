e263c7aa54ef750e08c14ffd168f4c63d9110bd3
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:33:25 +0000

    care variety use escape exactly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
