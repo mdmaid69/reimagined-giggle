bcb69085706318f34e374632ec0a4ade9e1b5777
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:29:49 +0000

    belong frequent stupid sure scheme

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
