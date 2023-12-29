eafde7ea847620f91dd81ed0224ce9be368d89e4
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:31:46 +0000

    want very giveback soon busy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
