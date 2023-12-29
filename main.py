5db4df59594c8c22b32a4ca59d6a5ab64aa26a0f
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:29:30 +0000

    whistle walk makeinto takeover appear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import getpass
+def get_username():
+        return getpass.getuser()
