6d8b4faf2c225f985c60a7cf6a4b323ee588def5
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:37:25 +0000

    bath office round error pull

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def subtract_numbers(x, y):
-        return x - y
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
