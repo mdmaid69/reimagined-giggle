5a172c661ae4162fd3d5b38e299e178b41250ac1
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:29:36 +0000

    imagination along sometimes round project

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
