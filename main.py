76427e2736ba7a3b2a311910b8961013d4f485ce
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:57:59 +0000

    back shrink our emergency reasonable

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import platform
+def get_python_version():
+        return platform.python_version()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
