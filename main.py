adf4dfb64cfcf17f9a623192ce025c76880d4ddb
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:46:37 +0000

    estimate pull waste main man

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import platform
-def get_os_info():
-        return platform.uname()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import sys
+def print_python_version():
+        return sys.version
