013001adb671e83aa24372ac878847ed66b7abc5
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:36:26 +0000

    Dd male talk usual collection

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-print(math.pi)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
