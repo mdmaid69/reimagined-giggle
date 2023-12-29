fba5adc5a86f31407ff8a612d5d60595fe001b69
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:04:22 +0000

    pretty cruel protection neither piece

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
