3895ce701a58783fa65a7b559428cca330c22a7c
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:27:13 +0000

    fair live first lose bored

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
