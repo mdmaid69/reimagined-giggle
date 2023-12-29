5eee40c68ebb6f8018e34fa8d893108710d4f5aa
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:08:15 +0000

    steep loose bringup may argument

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
