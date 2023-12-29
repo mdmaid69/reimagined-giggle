618cd3d56d747e64857e581ea3fa99a9ed66668e
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:20:00 +0000

    person drink floor famous price

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import re
-print(re.match("h.*o", "hello world"))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_set(array):
+        return set(array)
