29087435e7daac967a8b0ef31c822b5f8d6c6673
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:31:25 +0000

    shopping fight talkinto guess dull

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def convert_to_hex(n):
+        return hex(n)
