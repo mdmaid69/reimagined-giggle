fe20931fc0a6a835ae6dcf81fb305ce559686076
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:27:55 +0000

    time 570enjoyable music plenty pure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
