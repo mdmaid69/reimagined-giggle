8c965bc741a06993ada6ad7e7874df05c63b640a
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:41:29 +0000

    wet back nowhere slowly future

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
