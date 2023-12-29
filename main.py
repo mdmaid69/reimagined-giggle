f2c59404c128b8ada7790486247749b2bf476386
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:03:57 +0000

    rub medium pin turnover show

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_power(work, time):
-        return work / time
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
