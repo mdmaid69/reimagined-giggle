4b7ff6c414e181515694d07adc43d1f389d0e277
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:03:53 +0000

    healthy share among anyone similar

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
