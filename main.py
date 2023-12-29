3ae7ad62eea7dfefe697ed3b3917e571289efaec
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:24:51 +0000

    increase common pity failure spite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
