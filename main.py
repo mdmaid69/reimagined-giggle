0d981bd4dc16cbed00b04515433383aba6cd6e45
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:56:09 +0000

    Gg person 564encourage part criticism

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
