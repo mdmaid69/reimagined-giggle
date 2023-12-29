78bcdf074364a7c6146cb1d245794d61f6b9cf03
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:07:25 +0000

    second peaceful tool return eye

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
