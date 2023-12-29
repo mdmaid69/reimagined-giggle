e3ec47b0706cf40440f49f6c6788a83fdbf04722
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:48:07 +0000

    press different makeinto ring escape

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def get_list_from_array(array):
+        return array.tolist()
