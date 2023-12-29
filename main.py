6377c4ca66d0c12b6f1e2a78213541d2b53fec49
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:15:58 +0000

    Jj hit sound would collection

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
