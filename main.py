ca1394d0003249e32cca87620b6d1936e7194fc8
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:28:16 +0000

    on below plenty example boring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
