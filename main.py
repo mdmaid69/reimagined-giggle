d8d83671963b63add36b2b59fc269c1f31736a79
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:27:58 +0000

    drop smart later giveback dress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
