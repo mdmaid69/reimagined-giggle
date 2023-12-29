67ca363217b56582906c72fb2836b3810573cf38
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:43:51 +0000

    fire care workupto press work

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  def reverse_list(lst):
+        return lst[::-1]
