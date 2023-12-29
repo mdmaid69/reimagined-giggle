a11816579ee0cf018ca80022e5607e93acb2cc13
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:54:22 +0000

    nerve extremely lesson in gather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
