22a79e642b221853342cc4ad0f4987eca649ca5f
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:07:52 +0000

    drop black skill sign praise

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
