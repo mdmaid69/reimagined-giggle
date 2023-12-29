c05a770d9c3f1399774d7a93c31d00c3abe2ee54
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:02:08 +0000

    drop comeback else underneath tool

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
