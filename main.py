50cbaa663be9e89e054292637b9fe1f76b222309
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:01:37 +0000

    mean house trust between ilovedicks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
