309b20fb730f634bcac27086b94299ba5231590f
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:10:20 +0000

    certainly introduce burst divide note

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def count_characters(sentence):
-        return len(sentence)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
