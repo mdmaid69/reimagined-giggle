ab7855c80b76716424730f6081ca97e757daed41
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:07:15 +0000

    come comefrom clean stamp comfort

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
