3ec8a49da453caaf9370084ca391a882a8bd1168
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:50:28 +0000

    skirt probably perhaps comedown side

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
