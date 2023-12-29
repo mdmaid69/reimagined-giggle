b4a654eccfa257620f38b284587e64570c0bb8d5
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:11:04 +0000

    on accidentally escape drink escape

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
