b9d473dbb66ed3eccbadc173327a417d1d84fa4c
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:17:08 +0000

    someone confused pin dress flood

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
