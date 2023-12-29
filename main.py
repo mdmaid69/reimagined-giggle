add37fe6a1222c45f2783573c18d2370c4644a3e
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:57:27 +0000

    spill jump exam mix far

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
