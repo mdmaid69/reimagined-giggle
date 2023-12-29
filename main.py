5057f4c605c01ebac37b7a3ae8a6480aea58cad6
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:17:50 +0000

    stiff bored authority apply stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
