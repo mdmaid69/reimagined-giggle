f7094d6388670e70c62891dc3174ee7e078dc5d7
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:47:42 +0000

    continue genuine patience difficult business

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def find_max(numbers):
-        return max(numbers)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import os
+def get_current_working_directory():
+        return os.getcwd()
