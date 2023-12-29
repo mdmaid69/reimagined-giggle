b9a7d9bb3da684f4cea88f09dcc48d6f382c81e9
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:42:50 +0000

    least break tired float understand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import os
+def remove_directory(path):
+        os.rmdir(path)
