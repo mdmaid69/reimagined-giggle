45ba68e76dfcd410e025a21a774582daf95f8aad
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:23:06 +0000

    rule fortunate operate conduct drive

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
