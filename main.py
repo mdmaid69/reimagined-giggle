451224274a960d47eabcc6a38c0abc569a4001b6
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:33:40 +0000

    onto whisper violent flat mean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
