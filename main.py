8b0930c121ab1cfddd5922089f0b1fce6482165f
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:46:23 +0000

    show hit bunch polite learn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
