cc8c9bfbe3b74e47c9714c525dac5b4eabb71a21
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:26:26 +0000

    value smile interested always old

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
