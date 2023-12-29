226fb4ba5f87429d108bc8596f0910a1e9590e49
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:41:21 +0000

    cut glass strike good occasion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
