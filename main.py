2ad66f138686135eae46d23068dc5bfb259252e2
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:35:46 +0000

    switch bill remove defend edge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
