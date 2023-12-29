0ebd92e0a922c6eaa4b12adb2cffccdda62f924c
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:51:39 +0000

    wish start build think acceptable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
