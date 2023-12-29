4f25042b7798b00dc4eebb3c42e9cb49dcbc9da7
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:41:05 +0000

    call march tap smell hard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def append_to_array(array, item):
+        array.append(item)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
