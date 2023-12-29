df63ef1d85361eaeda69a17c199e29626520932c
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:57:40 +0000

    politics call comeback roll boil

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import json
+def convert_to_json(data):
+        return json.dumps(data)
