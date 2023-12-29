f510b63e8458393913c2d2704da634d93168c4c4
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:43:34 +0000

    Qq show later contrast breakout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
