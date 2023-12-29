5b4388681235a2850715802439371022f675a71c
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:14:25 +0000

    urgent through ordinary custom before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import sys
+def exit_program():
+        sys.exit()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
