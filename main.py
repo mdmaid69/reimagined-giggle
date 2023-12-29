63000c1ebe39850bc430124b7c2e6074194e6e8d
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:50:33 +0000

    Hh friend breathe sign bet

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
