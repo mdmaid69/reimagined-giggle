0e18b419fd3bbeaf771504f8eb90317dd31bef8f
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:34:08 +0000

    ourselves claim bringup adjective 566end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
