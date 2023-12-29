d151e94c0cdc9a38dfaf669dfab343f3e3a3372e
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:05:32 +0000

    especially probably urgent fear honest

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import math
-print(math.pi)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
