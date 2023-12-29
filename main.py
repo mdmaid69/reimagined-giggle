3a995959f5715a9a266dec50868c9088dedfbc7b
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:49:04 +0000

    private comeover introduction each careful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
