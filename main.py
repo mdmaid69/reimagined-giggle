f2429d5cbc9e3b26d0f307ab1261786a185bb09c
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:06:08 +0000

    even note couple generally top

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
