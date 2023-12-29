ef15cac0923511151e669f8f6fe91890cff5da7f
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:53:06 +0000

    account keep please hole description

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_typecode(array):
-        return array.typecode
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
