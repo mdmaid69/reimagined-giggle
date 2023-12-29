b37653387bb05f6658b010254d01eefeba9a3da8
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:25:15 +0000

    stare contents turn yesterday however

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def find_max(lst):
-        return max(lst)
+def convert_to_octal(n):
+        return oct(n)
+import collections
+def create_counter():
+        return collections.Counter()
