74486fea519dda4984affe21f0bdec93c10e0c51
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:46:25 +0000

    original temperature safety into tragedy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-print(sys.version)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
