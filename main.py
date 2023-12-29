9d69e1cef847794d9b3bee7df9376fdd3473e39d
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:13:47 +0000

    giveback run limit character smell

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def split_path(path):
+        return os.path.split(path)
