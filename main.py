ad9485e203800aeeeec4eea0c6763e09b3dca8f5
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:28:26 +0000

    argue turndown on Mm tongue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
