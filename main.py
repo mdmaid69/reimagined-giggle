ee3019a39a9169b7401cc52f597e5a5f3ef215f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:47:18 +0000

    pain get practise cough someone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def count_elements(lst):
-        return len(lst)
+def find_max(numbers):
+        return max(numbers)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
