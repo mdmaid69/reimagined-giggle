688042cc27fa414d22393211d4c0098fa9ee16ab
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:50:12 +0000

    pride only disappear dream loan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
