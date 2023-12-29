81bf8cdab22ea6030fb8bf3f604f36e2a2b311e0
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:27:01 +0000

    keep use previous Ss promise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
