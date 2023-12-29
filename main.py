b428ebd3427e78ea46c4dfa656a718250cfe3cdd
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:27:59 +0000

    everyone disturb shoot telephone problem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
