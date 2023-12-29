73d3fa50783690531cb21280348882a77f0c4851
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:13:38 +0000

    century however frighten back average

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
