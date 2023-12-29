4d4deef32c24db5058f4228827fd778ca822ccfd
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:35:18 +0000

    identify absolutely takeaway question direction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def remove_duplicates(lst):
+        return list(set(lst))
