da20148334ffd2231044b235d40c975a621e35ac
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:08:06 +0000

    stop within temperature everybody herself

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
