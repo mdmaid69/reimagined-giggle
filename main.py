5270312a0cef5c13b62f4f9671c7c21e0a5eacd9
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:23:06 +0000

    table into neither behind equipment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def reverse_string(s):
+        return s[::-1]
