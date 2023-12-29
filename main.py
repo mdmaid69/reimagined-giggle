3cb7fce501b12229333cc289fc63b10d1f139a51
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:23:47 +0000

    draw apply church equal then

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def is_odd(n):
-        return n % 2 != 0
+def calculate_power(work, time):
+        return work / time
+  def is_even(n):
+        return n % 2 == 0
