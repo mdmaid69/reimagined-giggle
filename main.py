3f82498be8c3e040d0069a6267cd15644ec6ae4b
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:42:17 +0000

    mixture turndown sit talkoutof winter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
