84808d414c801df5b1fad4f75e80a733169d1833
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:21:54 +0000

    mark sink wonder cry workon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
