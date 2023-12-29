10583e635bd902806f04f8c12bd00d2cbe95fceb
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:44:55 +0000

    tongue his game service giveback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
