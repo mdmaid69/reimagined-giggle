0efe147081faefcd3f37f82f725e7ee84934f943
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:20:47 +0000

    young tie talkoutof rather activity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
