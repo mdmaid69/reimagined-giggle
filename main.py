fa849124705e820fba54ce9d98a8d8c7a7853293
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:03:17 +0000

    would satisfactory size succeed strict

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_list_from_array(array):
+        return array.tolist()
