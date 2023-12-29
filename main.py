bddcd64fda51716245480b0b09bedca082cd261d
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:11:06 +0000

    art clear explain complete mess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
