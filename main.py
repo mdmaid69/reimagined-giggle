356c9bc0b12e0f65209778b3c25914ad1720aa86
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:14:48 +0000

    material promise collection consult imagine

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import datetime
+def get_current_date():
+        return datetime.date.today()
