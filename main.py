d6ad564eaa419eecb209095aa4c9795ab179feb8
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:43:30 +0000

    threaten pride tense real soil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def square_number(x):
+        return x**2
