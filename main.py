a63ca07e95bc5c0e64a0168379b95d796ccdcc7f
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:45:55 +0000

    fact grateful week perform closed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def remove_from_array(array, item):
+        array.remove(item)
