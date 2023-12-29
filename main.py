9f605910c23ecf708ec0046966e8b2abd09eb496
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:53:57 +0000

    disagree land limit pile window

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
