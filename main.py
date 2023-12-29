d82125db8325fcf9493d2e7daa0fc71bde573e1e
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:08:59 +0000

    equipment somehow match continue block

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import os
+def remove_directory(path):
+        os.rmdir(path)
