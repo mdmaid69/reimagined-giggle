d3278f256a3019d45d832890cdd9eb827e65b496
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:43:08 +0000

    fall runover bang truth noone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+for i in range(5):
+        print(i)
