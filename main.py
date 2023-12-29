85d559d609eb3f650aec39941ffcdc518a4bf869
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:18:09 +0000

    familiar drink interfere blue road

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
