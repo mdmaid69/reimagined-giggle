2321f88f093e48b34bf0cbb7e624ca98a238e32c
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:17:34 +0000

    for shout answer whole division

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-for i in range(5):
-        print(i)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
