3f584ea0b9252c62bb6fd16ae0180a3b20918c57
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:07:08 +0000

    hope just bottom consist upstairs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
