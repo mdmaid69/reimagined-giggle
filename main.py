ee76468046578a8c7d9603329090b8ce39492a48
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:30:09 +0000

    straight dance true insist at

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
