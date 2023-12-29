dbd1940671e39714fc58ebf0723a14762e21ec69
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:01:13 +0000

    part song there 570enjoyable its

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def square_number(x):
-        return x**2
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
