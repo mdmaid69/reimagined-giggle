e6fc2030d2a54b6e7c54cd7be3c105b53dc4674a
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:43:02 +0000

    stage strike connect present lonely

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def find_max(lst):
+        return max(lst)
+def find_min(numbers):
+        return min(numbers)
