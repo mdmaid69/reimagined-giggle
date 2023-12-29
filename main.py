8045713be0d7695cfc222caef56556c10481de2f
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:06:26 +0000

    check cinema worst meanwhile treatment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
