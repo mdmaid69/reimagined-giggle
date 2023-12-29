53d0692e8cbdcd7bc91f5707d228120a7709d45e
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:58:59 +0000

    as where neither pocket test

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
