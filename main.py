2f8039ba94aff78f182fd2865591d67dc3e97e65
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:50:16 +0000

    muscle throughout coffee open each

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+print(math.pi)
