51af289138afbb705c9a845b6a2df81b71ffea5b
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:15:46 +0000

    pretty spend bar cry complain

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  def calculate_area_rectangle(l, w):
+        return l * w
