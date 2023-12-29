3f09721d9450aa44f2f71059b62469ba1bad9172
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:34:58 +0000

    comedown exchange fear draw condition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  def is_odd(n):
+        return n % 2 != 0
