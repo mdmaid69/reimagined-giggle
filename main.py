ddc50a755fe4ac82e6e052706d75b7643eaeaeed
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:46:33 +0000

    long help tax far family

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_repr(array):
+        return repr(array)
