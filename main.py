9e140771063a6670ce4cf6f30d73ff0b79144679
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:47:11 +0000

    left attempt Ww politics recover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
