b7b5c61697536da6453d27c8f785730f2bf8137e
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:35:34 +0000

    thinkof talk seriously how vote

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
