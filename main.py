15cf551e63b61ca04a839cc5a3d1b483a8bf4ef6
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:46:25 +0000

    whether beautiful pretty smile nigga

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  def calculate_area_circle(r):
+        return 3.14 * r**2
