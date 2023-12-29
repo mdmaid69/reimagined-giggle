3852c79c285343e39cf330ed2d406a82e3941371
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:41:47 +0000

    night people influence prevent promise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
