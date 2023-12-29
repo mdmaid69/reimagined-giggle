83623fbea73257e528ba3fc6206fd97d002a3a2a
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:55:33 +0000

    voice reject remove pool base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
