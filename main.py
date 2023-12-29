acebe2195285eea58c8bb68d2b996764edcf5b03
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:13:52 +0000

    city exploration corner telephone order

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
