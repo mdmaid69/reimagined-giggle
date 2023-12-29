2e377ceeb34a4285fee243cf6129a72960cbb876
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:36:26 +0000

    grade shame job attach demand

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
