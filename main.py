9bb61fa9ea5d1ea9579877f4558c8ff10c621930
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:54:10 +0000

    giveout cause apart very shoulder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+for i in range(5):
+        print(i)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
