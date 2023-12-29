8e6b8ec3902dc25ca8ccc1940baa4ac903e932f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:04:55 +0000

    command fork everything soft introduce

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def find_min(lst):
-        return min(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
