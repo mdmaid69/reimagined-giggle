1c51c163f70d0f392e53e3433bb0697be212ebce
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:35:29 +0000

    bring right bang Kk necessary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def get_array_as_bytearray(array):
+        return bytearray(array)
