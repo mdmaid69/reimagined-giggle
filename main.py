e676bf85180804c898a9aacfa5f3e420cd6eafdf
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:21:49 +0000

    important busy medium often very

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
