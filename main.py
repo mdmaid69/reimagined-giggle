70697d3b27a2e80a8ea1484ecc3226c215f053fe
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:05:02 +0000

    some government able worst powerful

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def find_unique_words(sentence):
+        return set(sentence.split())
