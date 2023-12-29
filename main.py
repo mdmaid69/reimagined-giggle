b6ae2ead6afe75855e923c1e1f985ff296dd82de
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:26:55 +0000

    circle knock damage neat opposite

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
