c4007444f57009f0fe687f17c3dd42605508f94f
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:28:35 +0000

    kill fault your threaten failure

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def find_max(lst):
+        return max(lst)
