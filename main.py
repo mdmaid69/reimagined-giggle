d8291ea8d46984574c42542f290f9f239c514539
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:50:17 +0000

    turndown anybody dig build shout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def reverse_string(s):
-        return s[::-1]
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
