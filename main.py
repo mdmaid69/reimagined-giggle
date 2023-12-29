57aee84ed67507b577cc1d8311aea389540dec82
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:26:55 +0000

    delight direct delight then task

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
