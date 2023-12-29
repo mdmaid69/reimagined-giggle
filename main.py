c1b18ba1ad6b42b9be92928895554e9f70f6c57d
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:11:51 +0000

    ball prefer friend thinkthrough identify

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import sys
+def print_python_version():
+        return sys.version
