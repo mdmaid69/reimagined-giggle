cb08bc949624aa3a832a2b22579e2bdbdd1facef
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:43:17 +0000

    pin manage wish mess they

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_list_from_array(array):
+        return array.tolist()
