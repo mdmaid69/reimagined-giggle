79a2fc7606e32a34894bb72bbd57cd7f0cd026f4
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:43:19 +0000

    steal introduce drop loss temperature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
