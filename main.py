a1ef9a100ef6c9a27c1068e7333d021b07532bcb
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:09:23 +0000

    flavour doubt stare phone mixture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def get_array_item(array, i):
+        return array[i]
