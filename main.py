3ffbd2e406b94a38ad855a689b90763e42b68845
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:17:02 +0000

    young influence guard hair man

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def reverse_string(s):
-        return s[::-1]
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
