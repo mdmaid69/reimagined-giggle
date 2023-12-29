b03310ca54e129d3d551ec574a50b90a031e51d9
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:37:08 +0000

    few get answer full main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import array
+def get_array_as_str(array):
+        return str(array)
+def divide_numbers(x, y):
+        return x / y
