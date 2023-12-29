6af04b6a9e378d6d8b2d145ab318695e1c395038
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:13:36 +0000

    around vision supply sing really

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def remove_from_array(array, item):
+        array.remove(item)
