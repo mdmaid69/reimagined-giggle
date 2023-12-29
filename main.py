39047d007ca69492b742797ce118809a21d8a223
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:10:17 +0000

    confuse nigga deliver sing choose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
