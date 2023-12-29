dbdd65d426079e3dc7a1e625399ed25e087b4036
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:50:04 +0000

    part circumstance concerning fairly please

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
