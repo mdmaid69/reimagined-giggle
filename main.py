fb773693e2fcffbde425194909bc247aa4e56fef
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:02:16 +0000

    sometimes body rest worth copy

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
