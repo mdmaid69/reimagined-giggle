f830b628353e759b159012dce0016c14ee881c88
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:14:44 +0000

    notice comein blame rise consist

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
