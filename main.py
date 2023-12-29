c10e0ccb5942e18f5ef91336e236bd4df25dc0ce
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:19:52 +0000

    frequent disappear final size custom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
