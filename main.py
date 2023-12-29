3aa415691441c1223720dd5e5bd0f937ee6f015d
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:03:51 +0000

    limit present round seriously indeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
