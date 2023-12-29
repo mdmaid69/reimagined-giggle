52ddf80ead4adf5d9924f0b3ea3f7f2ffbb47565
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:45:28 +0000

    quit cover lock leg if

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
