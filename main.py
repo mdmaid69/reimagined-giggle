fd616c68cf66a20a9e75ece563261e2fce49fdf2
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:54:18 +0000

    herself block behave somebody contents

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
