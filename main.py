cbf5839811b96e88720f4b794d16d178eebaf836
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:22:07 +0000

    rescue uniform sky perform past

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import math
-print(math.pi)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
