e9c733675e57b7059f5ac6b8b689a70ec4cd1994
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:04:29 +0000

    getup calm voice real theirs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
