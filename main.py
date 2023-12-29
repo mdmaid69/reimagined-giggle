3152e50fab9f3a74be7e6d58eeb595b3f010a627
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:50:40 +0000

    hope pocket rescue question doctor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
