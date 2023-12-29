13287692362b6751978f34953f724566a6741747
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:51:00 +0000

    chief certainly issue zero reason

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
