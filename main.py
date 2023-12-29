c5cc27da14a599957e297aee1f782adf9f98add5
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:19:59 +0000

    thinkof cover mean knock special

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def get_array_as_list(array):
+        return list(array)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
