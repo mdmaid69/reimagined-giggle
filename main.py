d0a089b10e0df583df50f59a64d0d5cc9be48ecf
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:14:52 +0000

    illness be other but plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import sys
+def exit_program():
+        sys.exit()
