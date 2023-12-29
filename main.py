fd2a5a927449e7137baa4285e869b88b9d5be5d6
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:49:34 +0000

    tour market smooth exit answer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def count_elements(lst):
+        return len(lst)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
