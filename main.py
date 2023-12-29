b74bcf05c81288a52fd449a7b64599db6f1bd6a9
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:50:49 +0000

    entrance produce slowly accident memory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def convert_to_binary(n):
+        return bin(n)
