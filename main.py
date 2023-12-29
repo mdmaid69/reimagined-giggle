50381510ac2c7eff682b47b9d6972433ec954db1
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:13:31 +0000

    result sick permission care although

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
