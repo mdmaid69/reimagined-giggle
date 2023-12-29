a46883710996e8c40a87d155034c6ab3f49ea083
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:50:38 +0000

    cover just stand afford throughout

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
