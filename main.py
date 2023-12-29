93c87bbd207e24c0ebb6e635c16418f537416263
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:06:20 +0000

    science protest slowly closed wind

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
