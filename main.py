14cd516d46eb5f937ae008a143ca81d3db818fbc
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:21:06 +0000

    music measure classroom succeed build

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
