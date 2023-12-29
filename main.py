5b4010a5c967e1974cca9328e1f541489c14d7dd
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:42:24 +0000

    common ignore hurry worst birth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import logging
-def log_message(message):
-        logging.info(message)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
