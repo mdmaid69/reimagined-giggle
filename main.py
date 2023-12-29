4c82b9810ecd8ebc4ef176e9d238959179f7ff24
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:58:48 +0000

    message deliver area fashion often

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def is_even(n):
-        return n % 2 == 0
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
