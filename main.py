3ed553f8f2b49372a031567928f4debaa0e0c61c
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:16:45 +0000

    chance bottom realize appear goon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
