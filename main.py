efd8aba62d545ae441cc53d0b6b90e6f86b6aa68
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:54:40 +0000

    dark stage minor reason rush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import array
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
