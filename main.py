d22bf56d7d27dbf8f05c1ae39b1bb1fb2b225654
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:57:37 +0000

    rob under gather them partner

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
