fd405c36d110823a856563748fca2010539e2fba
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:50:25 +0000

    careful 567enemy similar rush concern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
