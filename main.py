2aaf86dd473af569c6e9fc9d106a8763c483c897
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:53:54 +0000

    sudden pollution mass opposite flood

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_device(file_name):
+  def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-for i in range(5):
-        print(i)
+def convert_to_octal(n):
+        return oct(n)
