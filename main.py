58ce6d27bf578b7d27c2ea00ff766f62b1ae1ae4
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:53:56 +0000

    lead shame bend add display

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
 def calculate_current_ratio(current_assets, current_liabilities):
         return current_assets / current_liabilities
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
