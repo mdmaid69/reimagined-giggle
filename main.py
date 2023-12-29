bb6d08d1478f3e8bd5f789a9c306b71a25cb8ae3
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:50:02 +0000

    comment risk transport tap news

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import random
-print(random.randint(0, 100))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
