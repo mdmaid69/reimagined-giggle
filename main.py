0558312a3128aaa545a286c33d2936a08d5a0972
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:13:34 +0000

    cinema cross wide themselves translate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import array
+def get_array_typecode(array):
+        return array.typecode
