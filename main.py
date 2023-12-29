25b9285f778c3f3bd2d36b982030126836491a18
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:06:52 +0000

    cap let straight deliver hand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
