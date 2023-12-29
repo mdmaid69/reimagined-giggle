b62118d5745cdb911e859ae23380321b418e0070
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:11:02 +0000

    apparently vanish before kick borrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def divide_numbers(x, y):
+        return x / y
