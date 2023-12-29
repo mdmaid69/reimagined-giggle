d95ba3248639d093cce51432d1f60e6ff16aa05c
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:07:46 +0000

    alone feel against tidy small

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+print(os.getcwd())
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
