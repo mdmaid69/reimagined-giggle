761080fd61460d37107a684939b513d2fd393922
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:21:28 +0000

    left debt collection keen return

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
