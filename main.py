beee100a5fd37d2b18d52b21a695716eed17868e
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:10:18 +0000

    giveback breakdown march ever evening

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
