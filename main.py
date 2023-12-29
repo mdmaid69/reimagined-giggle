1340ce2ab65d91f8dce7c3c05d8ccd9700c42dd7
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:21:54 +0000

    spite nice fit care reason

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
