ea7d8450a33d34d391b067f418266d672a3546b8
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:40:01 +0000

    so cook 567enemy free wide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
