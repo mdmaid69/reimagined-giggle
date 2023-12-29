f0037b4107357f051bb5c8709b9360d35517259a
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:49:23 +0000

    choice front any funny news

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+def print_python_version():
+        print(sys.version)
