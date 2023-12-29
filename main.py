6f3337c93702ef6e96377bfcfe4ca017d33e7e4e
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:00:58 +0000

    there admit tread practise cap

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_speed(distance, time):
+        return distance / time
