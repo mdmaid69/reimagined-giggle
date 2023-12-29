c1fdb7547df0ddcb14e2e26f9fabd480a799aa40
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:18:52 +0000

    before divide comeoff branch degree

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
