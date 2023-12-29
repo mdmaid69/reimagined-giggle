81d5228248d974df6769934ec10a54ff4b911fdd
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:59:01 +0000

    consist rare border rubbish strength

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
