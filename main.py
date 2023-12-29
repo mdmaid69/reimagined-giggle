66a0ca9e8a438ab259a6871d61620d9c88e8be84
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:09:15 +0000

    record blind move tax read

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def add_numbers(x, y):
+        return x + y
