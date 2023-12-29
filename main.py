7d7c447a63f9dbe2e154e796bb2ff76c676df79b
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:03:29 +0000

    pool possibly disagree only may

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import datetime
+print(datetime.datetime.now())
+import math
+def calculate_floor(x):
+        return math.floor(x)
