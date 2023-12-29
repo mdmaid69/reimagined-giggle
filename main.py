0f170198d057867241230eb0b55a32d8347fd9d2
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:04:48 +0000

    cruel breath block authority food

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
