850c3ade9db1ba283685e199871a7c8fa7831922
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:02:21 +0000

    man greeting have they please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
