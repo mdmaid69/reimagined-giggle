16effb31361d985380803605c9503063c1048af6
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:18:50 +0000

    test organization fact think unlucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
