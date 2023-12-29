e91acf28424d3fbd53b6de40975eef7b55549c56
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:55:26 +0000

    spill tune unusual mine workupto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
