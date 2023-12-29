404294ede886d3435514c5d4b8168792efd65013
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:33:56 +0000

    switch before run test dull

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import os
+def change_working_directory(path):
+        os.chdir(path)
