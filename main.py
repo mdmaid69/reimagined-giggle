811ad2d0c6a1117503d9f36a4f440a97f053f820
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:29:18 +0000

    old shallow warm human ticket

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
