f005a656cf0db99725a75a4723e09f25ed5e8abb
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:26:20 +0000

    connected while them urgent tell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
