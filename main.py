9fbb4708d9bfaa8902ce47c4095d11494e93b98a
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:36:43 +0000

    today plant stamp earn stare

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
