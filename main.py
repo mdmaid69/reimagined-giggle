c93e3ff29121bbd99a790a4df6517bb0e101097e
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:44:08 +0000

    subject independent say firm swim

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def convert_to_hex(n):
+        return hex(n)
+import json
+print(json.dumps({"name": "John", "age": 30}))
