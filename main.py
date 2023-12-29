1dbd83ee7d6e27077823acc7afc73ddfc083c1bf
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:49:35 +0000

    block head treat at should

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import collections
+def create_user_list():
+        return collections.UserList()
+def calculate_area_circle(r):
+        return 3.14 * r**2
