d40c746c4b8728a10179c150e8bb175dbc916f47
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:18:23 +0000

    uponon high temperature necessary available

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def calculate_area(radius):
+        return 3.14 * radius * radius
