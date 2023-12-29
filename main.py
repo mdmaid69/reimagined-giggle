a6abef4e533b4923e8e4268ad9550d6354b22099
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:00:08 +0000

    mine Ss 573entertainment live ought

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def convert_to_hex(n):
+        return hex(n)
