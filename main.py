6d04dcc58848e183a8673ab92906e2ecc614b60c
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:56:50 +0000

    building afford bear cruelty same

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
