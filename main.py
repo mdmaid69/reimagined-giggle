4705c855fd2e836b4810b1fd6db8a3fc205b6ed2
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:38:06 +0000

    identify stir closely down considering

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
