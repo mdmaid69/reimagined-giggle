27e81b4a80c8b42f40649102fe943a00cbaf3496
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:10:59 +0000

    furniture present drink catch lonely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
