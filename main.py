ba3187e2e6a6d1ed5ca6d17eb31079fda77a8fa5
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:50:15 +0000

    otherwise sea dark father tooth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
