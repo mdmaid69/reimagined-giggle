9322e99d510e274b08653eb0f5e7c818715fbd78
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:43:03 +0000

    sale number cum joke willing

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
