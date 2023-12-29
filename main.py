cf988b20eb982d0cc35e65c78dc04b4c4f693fea
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:51:47 +0000

    disappear cap close breath clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def reverse_array(array):
+        array.reverse()
