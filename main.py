4cf7bc427934a59c17d5dcd88cfed3dd09cbbecc
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:09:43 +0000

    otherwise government comeover once twist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
