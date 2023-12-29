0f0500b3c8399f4018928c0a5d405b99ec40672f
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:35:58 +0000

    real confidence foot the shake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_int(array):
+        return int(array[0])
