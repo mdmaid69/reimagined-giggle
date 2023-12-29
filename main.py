e92a94d42c3690e109ef08bc50d5a7db997c6e1a
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:29:50 +0000

    introduce boil part fierce the

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
