4dd6783bc9638af0473d5da707a4c4c9860e04cc
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:22:01 +0000

    match obvious press quiet process

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import math
+print(math.pi)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
