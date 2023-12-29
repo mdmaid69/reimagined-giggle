203cf1772b115dce4bfebf64d0196a3825cd05d9
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:24:50 +0000

    easy compare entrance somewhere sensible

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_area_circle(r):
+        return 3.14 * r**2
