95fa47cf4a200da5767e67d18f48a49fbc33df65
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:07:15 +0000

    object after nor service relative

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-import array
-def get_array_length(array):
-        return len(array)
+import collections
+def create_queue():
+        return collections.deque()
+def is_odd(n):
+        return n % 2 != 0
