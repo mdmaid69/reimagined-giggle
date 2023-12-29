1d122d752b8f5a17d7ba5aeec772b2516eea7030
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:54:44 +0000

    ahead grand possibly blame draw

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print(sum(range(10)))
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
