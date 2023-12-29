9c8e82248752d6d0617ff9964e4d28ac4736831e
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:35:26 +0000

    frightened worry right share seat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import numpy as np
+print(np.array([1, 2, 3]))
