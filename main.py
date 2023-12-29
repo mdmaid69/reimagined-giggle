6aa5a080b1a9c5abe7014aa4866b51422331aacb
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:35:29 +0000

    marry print work common vision

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_pressure(force, area):
+        return force / area
