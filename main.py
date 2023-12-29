7cab768069f6ca187e7b43b1c0a22f7e631ce1bc
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:59:05 +0000

    sort middle manage Yy goup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def reverse_string(s):
+        return s[::-1]
