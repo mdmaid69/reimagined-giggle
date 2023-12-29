41f21d9a60a328c02c0cfc218b62a84edd07aa46
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:03:50 +0000

    conversation successful as hope tend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_as_int(array):
+        return int(array[0])
