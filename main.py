8c257ac78cc8bbc315662df976d979cd815e4604
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:01:26 +0000

    breakup poor by lead guess

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
