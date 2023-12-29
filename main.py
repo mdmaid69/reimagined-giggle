3bb98e3c486afa7f4a129b405b49a7291610f285
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:00:44 +0000

    rush negative keepup friend expect

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
