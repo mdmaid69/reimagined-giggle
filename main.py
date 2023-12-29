d0048ffa83f01e86232f38a5c076be236c21001b
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:17:42 +0000

    shoulder able bill community embarrassed

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
