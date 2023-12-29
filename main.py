1a9882eabd2853af5329b1f0570d1447056bd87e
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:20:04 +0000

    habit speed touch breakdown wish

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+name = "Python"
+print("Hello,", name)
