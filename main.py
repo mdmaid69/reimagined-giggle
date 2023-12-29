e22c2fe5ae0391900bb51017198585a73e3ccb8d
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:29:46 +0000

    operate coat office turn course

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
