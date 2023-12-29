f4c64d539aba2b56b7587e484ea630ded929e62f
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:44:55 +0000

    negative actual later fill healthy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
