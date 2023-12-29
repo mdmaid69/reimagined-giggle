4832f438aef9461fc2e5f579f90d309d58abd4d5
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:25:18 +0000

    enthusiastic copy sudden limit damage

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
