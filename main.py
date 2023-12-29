65c42c24fd6c02b1bf7eebfd99b02b78c4944f6a
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:39:03 +0000

    free own imagine during health

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
