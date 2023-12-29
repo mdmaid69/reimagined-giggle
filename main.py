fd5c58e56d7c9fe8f056f531c76c3cc604656215
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:43:25 +0000

    convincing distant computer ordinary scrape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
