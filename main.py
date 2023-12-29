072f45c35534c889d2a84e475a40e269be2011ed
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:22:31 +0000

    hole equal black in round

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
