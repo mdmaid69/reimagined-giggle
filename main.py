32115d6a50dc399ca2f9d35df9bfdf4ab3a6f427
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:51:31 +0000

    pot university good transparent birth

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def is_even(n):
+        return n % 2 == 0
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
