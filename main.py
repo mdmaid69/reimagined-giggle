26eb7c158994a550c3d9bf4804a63f55dadb2025
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:09:13 +0000

    interest anybody 179bite authority brother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import tensorflow as tf
+print(tf.__version__)
