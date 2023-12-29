628bd026ab0c9edafd8874130362a658a41ad11c
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:55:29 +0000

    target crash correct round please

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
