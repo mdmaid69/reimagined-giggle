3b36808c0ed3a75964ee7b45e71ee424e77a3c81
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:11:24 +0000

    react remove towards issue tour

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
