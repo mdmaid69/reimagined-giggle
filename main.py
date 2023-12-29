1fb7a04dbff87b088f53ae344ade0f395a3780a3
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:00:09 +0000

    sale lot list low funny

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def sort_numbers(numbers):
+        return sorted(numbers)
