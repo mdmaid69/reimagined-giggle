0c6889f01e535d13747850b8dc98a3afb1706722
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:49:09 +0000

    drink search advantage spare accidentally

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
