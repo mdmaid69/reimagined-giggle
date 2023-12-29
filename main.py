b1ec3009dd13c2c71b685500792a898a7ba73ab8
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:11:34 +0000

    further else politics content poison

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_as_bool(array):
+        return bool(array)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
