cb5cbb702d674b1bb7e7571504045d6b83ed6d63
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:38:48 +0000

    criticize recognize want trust attractive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
