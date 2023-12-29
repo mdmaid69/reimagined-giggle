353e054cafef0b0d2d8bc6cf880ee98fa6e71011
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:20:21 +0000

    leave them notice until oil

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def is_palindrome(s):
+        return s == s[::-1]
