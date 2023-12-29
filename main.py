b4858d358a1b4088b2465191271a49affe881d06
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:44:45 +0000

    wrap upset valuable date mysterious

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
