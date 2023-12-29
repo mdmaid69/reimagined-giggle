d58f1a90cbbef84f0a00e26d9c2c01bb1788bf9c
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:36:06 +0000

    genuine way interest pool Ii

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
