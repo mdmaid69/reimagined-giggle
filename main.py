558a33b3ab6747483e63706c5f51eecf10aa4c27
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:55:36 +0000

    all holdup song male empty

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
