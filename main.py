e5d6ea71fe295b10febaac1be2c269a5cf54cfce
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:14:50 +0000

    ring lead arrive come section

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
