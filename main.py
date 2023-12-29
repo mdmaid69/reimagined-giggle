ed381b5f15352750d85b71a0969a42acf875f865
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:16:57 +0000

    prefer artificial repair comment disapprove

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
