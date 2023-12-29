72130c47d33033d3d1d071e24294fa381b29ab8e
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:18:13 +0000

    case concern moon bury hers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def cube_number(x):
-        return x**3
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
