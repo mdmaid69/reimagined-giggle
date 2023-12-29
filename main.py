a72851350eea6db7478d026f83598e37ec5d8db5
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:13:54 +0000

    service bus tall noisy smile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
