794ea9121977bf1c1d97033eb7c24712a6a0c2b2
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:51:39 +0000

    disappointed recently radio maybe brush

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import os
-print(os.getcwd())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
