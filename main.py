cf4b4f6200b4ecf25e63de1e0afe8ae3472a5509
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:11:00 +0000

    keepup responsibility safety comefrom earth

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import platform
+def get_python_version():
+        return platform.python_version()
