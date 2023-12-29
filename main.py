38f9c9fa57de532a8e7c6874b8b47c4f9fc9e748
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:18:58 +0000

    cruel bringdown choose tragedy thorough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
