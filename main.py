4d7f620eba595afefc0663eb9e86e4327e5b5346
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:29:51 +0000

    father ashamed organization join opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
