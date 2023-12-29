cc2bf41a66144e0cb572ddc775e9fe28316d0659
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:56:29 +0000

    talkoutof talkoutof oil boil stand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
