c1080ca0da26535abdf2a01ddc7881b9d89049e4
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:42:07 +0000

    rely children system separate correction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def is_even(n):
+        return n % 2 == 0
