ba4ec6cc7607b6d28e4813680eccddb8358f8031
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:07:13 +0000

    need might mainly cause needle

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
