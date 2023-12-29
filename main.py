564de3902af038c2679b16aa002ce617f70718f8
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:31:36 +0000

    dress describe under listen smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
