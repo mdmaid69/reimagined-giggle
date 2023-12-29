7bc79a7d68eb93a4bb02ae7b699cfe501ef4cddb
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:52:08 +0000

    petrol responsibility pupil avoid improve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def count_characters(sentence):
+        return len(sentence)
