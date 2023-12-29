4d21356932d2920f2ab1cfa4e5805f4b3c63439e
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:26:42 +0000

    permission dry fat cruelty separate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
