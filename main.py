0e6a4fcbd085df054b77b5b19f5163828e736d5b
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:08:12 +0000

    hurt realize interest crash stone

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
