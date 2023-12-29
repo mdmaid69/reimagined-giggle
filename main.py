30b05fb6e0530bc548461449d73b849d4ee29ab5
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:14:35 +0000

    operate should prevent ring friendly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_acceleration(speed, time):
-        return speed / time
+import collections
+def create_user_list():
+        return collections.UserList()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
