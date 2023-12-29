eb8e5f252ed386b49cd370e3acbacfe3ac5d8381
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:21:38 +0000

    room question scream doubt Qq

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
