2fb9d10a15443e76ec81fd092f19740cf0f60daa
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:04:54 +0000

    pupil downstairs little sink trousers

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import tensorflow as tf
+print(tf.__version__)
+import array
+def append_to_array(array, item):
+        array.append(item)
