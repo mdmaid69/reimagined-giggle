5d14cb25d104397fa3542949c5801aeb679be17e
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:24:09 +0000

    bag situation post second soft

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
