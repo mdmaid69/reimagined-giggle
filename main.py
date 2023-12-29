9065d2a22748f62c9f9eae0cc3a047edbc9cb6b6
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:24:08 +0000

    tax cheque centre this community

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
