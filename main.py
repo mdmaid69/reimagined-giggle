30a2f20c93dba4be439f99f79decac22dfebd44e
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:45:36 +0000

    persuade process interrupt culture Vv

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def divide_numbers(x, y):
-        return x / y
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
