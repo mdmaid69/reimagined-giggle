ceb4e51a24be067f17c47fd25a2245c342b7d4f3
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:22:56 +0000

    hers needle small above remember

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
