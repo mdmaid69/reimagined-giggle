35180dbb775a56a3c3f1fc749dfa79bc4ee6cbeb
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:59:05 +0000

    early healthy practical expect each

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
