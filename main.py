bf5f927ffa066df0f7cf107c24489c24b5b5278f
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:23:02 +0000

    guide situation tall anywayanyhow cover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def find_max(lst):
-        return max(lst)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
