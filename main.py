d617c0f7367bea9b9213fe893c53cd427c39f9eb
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:39:03 +0000

    watch bye subject consult keepout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+def get_current_time():
+        return time.time()
