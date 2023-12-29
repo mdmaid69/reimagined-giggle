35751424b9e3df34b1dc925f1fa11939cbdad93b
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:29:51 +0000

    bad only shut health no

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
