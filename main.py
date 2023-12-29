85dc6649115d390b12a376dba6988c512a95f76a
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:54:30 +0000

    rely emphasis length little warn

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
