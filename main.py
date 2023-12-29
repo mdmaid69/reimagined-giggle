727896e2c72744cc7245baedef8d89c70d4faf64
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:26:34 +0000

    copy defend look tight total

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_bool(array):
+        return bool(array)
