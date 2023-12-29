52df9b026dd9d8b0b00f040a38ff978ab8dae034
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:26:08 +0000

    snow too comein substance afford

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
