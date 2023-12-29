8a25f29130535a476511d85fa443f62e0dd7af84
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:56:33 +0000

    swim equipment release possibly bin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def get_array_as_list(array):
+        return list(array)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
