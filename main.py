5d78ab8532e4836778e4a81efe352b965e27f5df
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:48:53 +0000

    waste earth impatient extraordinary programme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_work(force, distance):
+        return force * distance
