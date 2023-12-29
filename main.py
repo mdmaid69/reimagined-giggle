cf3c6c79db38c91dd464623895c5cd0d8237e817
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:22:41 +0000

    drive surface sure by match

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def count_elements(lst):
-        return len(lst)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
