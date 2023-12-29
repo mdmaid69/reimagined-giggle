096bc10cdc7c20cd1d337607d38f74fbce4ff611
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:42:54 +0000

    burn freedom broad mainly burst

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def count_words(sentence):
+        return len(sentence.split())
