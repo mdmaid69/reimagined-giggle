20f8552544fe5970226f5603d0e99628d985da87
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:16:53 +0000

    valuable watch positive act absolutely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def set_array_item(array, i, item):
+        array[i] = item
