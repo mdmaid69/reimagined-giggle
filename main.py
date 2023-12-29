f0dcc23ffa49ee686cedeabc4978cefc6587cbe5
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:35:14 +0000

    next beside anywhere method move

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
