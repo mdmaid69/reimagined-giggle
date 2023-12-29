177cb3c3756227504bfbe6cf66ab94c59f6c17e5
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:36:56 +0000

    slip typical machine direct evening

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def subtract_numbers(x, y):
+        return x - y
