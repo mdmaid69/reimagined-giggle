960470781f54a807f7b10f541847c46a7d74f467
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:33:39 +0000

    once breath debt who noone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
   def delete_directory(dir_name):
         os.rmdir(dir_name)
-def greet(name):
-        print(f"Hello, {name}!")
