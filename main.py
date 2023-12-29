fe170511a299fcb0c9b29713e177608736824fc0
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:59:44 +0000

    custom below pick trust necessary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_index(array, item):
+        return array.index(item)
