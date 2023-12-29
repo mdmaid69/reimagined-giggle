1a52ab7bfdfeeb74d477d9b97bef2e14f1d95d85
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:46:58 +0000

    beginning Bb who imagine compare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-print(math.pi)
-def subtract_numbers(x, y):
-        return x - y
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
