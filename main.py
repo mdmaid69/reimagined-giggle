3e6b94a0c91bfedfb46ac8073d02a5ba7b9fd6b7
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:17:22 +0000

    wear point somebody perfect concentration

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import math
+print(math.pi)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
