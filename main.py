9a1f6d5812b492691cc20cef888330e55e9ace5c
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:29:27 +0000

    arrangement bang past describe exercise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
