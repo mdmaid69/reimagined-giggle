62cfd8f939955ad67324417b59ab2cd214c8d951
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:50:34 +0000

    centre fork door fork radio

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+print(os.getcwd())
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
