71c2ef0a12199868f323dda6a6c8c8b508246060
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:03:57 +0000

    black turnback situation hurt supply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_base_name(path):
+        return os.path.basename(path)
