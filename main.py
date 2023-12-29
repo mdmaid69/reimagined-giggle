e3e3e45890cc2cb106b58b2d2d8f7a534f2cb3dd
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:13:16 +0000

    he term giveout weigh advise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
