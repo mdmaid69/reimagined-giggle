721625c2f98085f797e503009377f7beef337985
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:18:00 +0000

    anyone include wherever closed reject

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
