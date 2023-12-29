f944dc9b1ea1bba42ddff72f73d10926cc95d948
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:25:47 +0000

    hurry wave trip polite bit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
