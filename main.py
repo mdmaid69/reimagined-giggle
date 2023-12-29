fe271f8741bbed46ef2e544c6712402b15f655c6
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:51:12 +0000

    room 565end cross direct fix

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_current_directory():
+        return os.getcwd()
