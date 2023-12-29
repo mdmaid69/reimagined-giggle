a802abf6dafc8ff4178d1be2a97b4b9d786f509b
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:20:48 +0000

    should personal thinkover answer concentration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
