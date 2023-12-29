344243af71cb99dceb299f860907cad403f7b88c
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:37:01 +0000

    involve advice heat punish reduce

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import os
+print(os.getcwd())
+def reverse_list(lst):
+        return lst[::-1]
