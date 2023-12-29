d4791df0a9eca7cf1bd507b7a7422973c4a940cb
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:11:36 +0000

    handle match sing black brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
