e8b184fe98a61b58b8fac6e6af084775c3a051ec
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:05:28 +0000

    usually promise worry borrow exam

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
