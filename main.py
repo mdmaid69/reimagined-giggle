ea04a17a0e3d625a27dbbed708cf4024e0f70834
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:36:15 +0000

    degree bunch task always under

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
