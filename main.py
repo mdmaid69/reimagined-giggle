88968a0e3d37ce48ecd8703a0ae75a76a004b7e8
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:47:06 +0000

    close previous ahead getoff indeed

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_min(lst):
+        return min(lst)
