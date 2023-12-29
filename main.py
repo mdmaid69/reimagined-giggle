2366119c90568b588ff63ffb0d86b8110aa64b21
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:22:00 +0000

    blue debt office fairly burst

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def convert_to_hex(n):
+        return hex(n)
+def count_words(sentence):
+        return len(sentence.split())
