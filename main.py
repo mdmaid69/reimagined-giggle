ec7c825d4c95b9321284d8442ca133dba88235e0
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:56:38 +0000

    tread sky approval lookback(on) survive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
