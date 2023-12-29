a3ca593e3a8b1f1d76f51929076f5b4df73a572e
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:02:32 +0000

    179bite comeout bang protect profit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
