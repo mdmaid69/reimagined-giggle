52fc49577b5b06ddeaedb0d67411922a4e434f46
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:18:40 +0000

    sometimes tall dear dare shame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
