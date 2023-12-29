2ee65ce14c6ff7635533241e5f590ce3cf6eaeed
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:15:54 +0000

    tidy condition profit office part

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
