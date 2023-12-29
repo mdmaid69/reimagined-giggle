2f29be956d5645c6607f31f65af920144da6db28
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:02:59 +0000

    scrape black talk certain dear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def split_path(path):
+        return os.path.split(path)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
