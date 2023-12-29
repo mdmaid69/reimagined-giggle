3181053bb323b51c2631f39d6d9a7f341b11db42
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:38:17 +0000

    now wander difference average dig

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_force(mass, acceleration):
+        return mass * acceleration
