c9ec7677f83a7d1c2167442f5b7a1d136bad16a1
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:20:03 +0000

    bottom space then art 572enter

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import array
-def get_array_as_list(array):
-        return list(array)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def get_array_as_tuple(array):
+        return tuple(array)
+print(sum(range(10)))
