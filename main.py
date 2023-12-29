6d892f69cc49ca50bfb11672e707b6e83a16b5ac
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:48:48 +0000

    reserve unfortunate thick impressed rank

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
