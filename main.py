c408734cc55472f29fffc495791f670afa265e5a
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:53:20 +0000

    minimum heavy opinion recommend case

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-print(sum(range(10)))
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import array
+def get_array_typecode(array):
+        return array.typecode
