314042ed0957d7bb6119f694054ba10744452602
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:09:02 +0000

    healthy switch neat support game

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def square_number(x):
+        return x**2
