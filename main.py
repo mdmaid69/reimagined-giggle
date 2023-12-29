8475200cb844e9befe3621cbc30962ed2d654cf4
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:00:02 +0000

    task name willing lot tie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_area_rectangle(l, w):
+        return l * w
