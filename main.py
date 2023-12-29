2f89c812d202d8ad36febb454ea2308475e1af52
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:56:59 +0000

    many bringup stay cover clue

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
