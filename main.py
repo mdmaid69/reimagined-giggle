fdec19813e1defacfff01b32bf5db2559cff67c8
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:14:02 +0000

    takeback deserve keen wind birthday

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
