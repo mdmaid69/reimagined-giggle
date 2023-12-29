3ad02e955dbfdb726d3b808e82b6ec2265318a45
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:03:24 +0000

    shallow secret reliable getaway side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
