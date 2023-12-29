36c28179616360071a30e51aa6a9d3b3f1d93561
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:02:06 +0000

    available sorry take perhaps us

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
