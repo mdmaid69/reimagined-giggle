720027d62173c68ca9840a8aed6e292e50917139
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:21:57 +0000

    test both back inside obtain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
