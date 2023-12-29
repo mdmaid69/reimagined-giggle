7667eb495a023cc966485ec1c4db229bc60b8d5f
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:09:55 +0000

    absolutely better kind cruelty trouble

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
