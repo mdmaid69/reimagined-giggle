7794acfbc6f746e943bd3941031128dd9ae98df9
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:02:31 +0000

    rule stone quit sure reasonable

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
