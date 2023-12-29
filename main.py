71b3ff577a7a470e84b09cd3c1e2adf37866b64c
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:32:24 +0000

    vision ill about low mystery

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def find_union(list1, list2):
+        return set(list1) | set(list2)
