ab6fe268fb69951374cafb044e96c2a693b465c6
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:06:05 +0000

    appear disgusting perhaps yet world

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  def square_number(x):
+        return x**2
