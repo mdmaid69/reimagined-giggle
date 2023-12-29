cb30b3ac9b015ea18ae9b49cf85310a1503c6731
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:28:17 +0000

    against hurt absolutely hold suit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
