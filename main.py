4791023909d2cd3fadf05b588c737efe7ad046c1
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:34:27 +0000

    present mention real typical official

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def reverse_list(lst):
+        return lst[::-1]
+def find_unique_words(sentence):
+        return set(sentence.split())
