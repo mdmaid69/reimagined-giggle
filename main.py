346248c1a3836174a481cbc4b4e7c6d556558428
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:13:42 +0000

    remain similar sort inform Cc

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
