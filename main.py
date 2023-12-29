677dd4f834ed47cf40658f55cbdda2a35bdbf425
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:05:15 +0000

    unless fill grade ilovedicks Jj

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
