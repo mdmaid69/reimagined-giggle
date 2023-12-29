8671e2d6f7a2ef8995e3cfc3612081ae29e32285
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:14:02 +0000

    politics discussion normal mark crowd

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
