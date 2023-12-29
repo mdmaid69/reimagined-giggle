be3b9a61facaaafc1696e6571d07d6f7cc51eec8
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:45:27 +0000

    suckme watch tour shop someone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
