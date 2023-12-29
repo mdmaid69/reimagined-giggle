b5517a724576e78635de039e5b9aca2e58c51c5c
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:33:04 +0000

    disgusting fond calmly develop everyone

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print("Cube numbers:", [x**3 for x in range(n)])
