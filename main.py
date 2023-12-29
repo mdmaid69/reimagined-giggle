51a4e7a75a995c856ed2eaf62cf89f06deba175e
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:13:35 +0000

    innocent correct basis ours detail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
