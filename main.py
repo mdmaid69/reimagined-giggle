fb79c7a885305f05f76a2a687f5d7f7cfcc7c9b5
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:48:33 +0000

    report suffer it definite storm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
