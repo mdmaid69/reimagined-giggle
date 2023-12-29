7e47974dd7fde6533c684bdd0a0cc4acf384faed
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:35:09 +0000

    pinch violent bunch crisis extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
