ff38e1ca11f25fd1334df26182fc38ea58ea0352
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:52:18 +0000

    comment show like distant murder

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  def convert_to_hex(n):
-        return hex(n)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_area(radius):
+        return 3.14 * radius * radius
