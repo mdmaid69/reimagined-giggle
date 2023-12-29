6217f446d3a44dae6eefe48488602d37e4bb10e5
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:38:12 +0000

    middle police course surprised step

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def count_elements(lst):
-        return len(lst)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
