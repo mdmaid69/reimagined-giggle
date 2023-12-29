26f08571b5f8bcd3136e4ba3f28a6894ee4e776d
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:46:10 +0000

    dead emotion age wrap poison

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
