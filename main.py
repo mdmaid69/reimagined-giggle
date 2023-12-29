6221d76799cd1f7caf7f6ea4dcb5d84041799eeb
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:55:08 +0000

    uncomfortable popular flow first tough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_as_memoryview(array):
+        return memoryview(array)
