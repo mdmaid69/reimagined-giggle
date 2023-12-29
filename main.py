7c5b21dca19162803bb328aafce43d5fe07aa98c
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:19:10 +0000

    stick equal complain increase fun

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
