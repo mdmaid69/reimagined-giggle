f1e2b1e594edea830157034caef6804a46fe951d
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:15:04 +0000

    sea tune apply pass 571enough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
