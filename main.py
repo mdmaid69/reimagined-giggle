eb495750cf1e29ec0f7fd0f2d543f4c93e492b80
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:26:56 +0000

    exam hot to claim sentence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import array
-def clear_array(array):
-        array *= 0
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
