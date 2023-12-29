c403536257e567ac5dff40cc9b9e739056deb90f
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:30:02 +0000

    number key catch mood dress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
