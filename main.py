1c32bfea29aac5b4e326e0a7ea3bfb149e7abf08
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:40:54 +0000

    far strike Ll onto slice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
