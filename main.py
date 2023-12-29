17eb57017e1977dc9caa96c10fdea0ab0644732b
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:51:33 +0000

    tell information runaway father edge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
