1fc6e2ee245540b74faa72b87409fb6d410f2bd3
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:25:46 +0000

    when old up content far

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def get_array_item(array, i):
+        return array[i]
