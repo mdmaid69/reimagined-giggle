95d7ed3b75c9a02df520e54ef0b86c7de6ed1324
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:26:27 +0000

    quiet emphasize thought fix call

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
 import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
