60029f9a3dcefe410fbffa26d7c7c7ca17e8b163
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:57:24 +0000

    impressive laugh sometimes children Qq

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def square_number(x):
+        return x**2
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
