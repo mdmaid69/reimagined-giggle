1daf7ad2d501f03575cc9737787677b554602a7f
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:09:06 +0000

    disagree rest eat convenient round

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+name = "Python"
+print("Hello,", name)
