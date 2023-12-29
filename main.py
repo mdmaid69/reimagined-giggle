45dca4a4dc1220616f121f777437db261a49b063
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:46:33 +0000

    recently stone really skill record

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
