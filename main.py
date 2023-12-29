7b7b6facf675cc77536deefa904ae534f2bf746e
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:38:21 +0000

    switch declare confused tense me

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def add_numbers(x, y):
-        return x + y
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
