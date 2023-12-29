844dd9a840b2931b6c33a77e2091e029ffe11b23
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:45:35 +0000

    takeaway involve money mess Yy

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import tensorflow as tf
+print(tf.__version__)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
