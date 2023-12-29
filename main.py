0d86e7d5b9bcab463c6dc2d4d6cc21db5c02d182
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:16:12 +0000

    reject tongue interruption yet level

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def count_words(sentence):
-        return len(sentence.split())
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import sys
+def print_python_version():
+        print(sys.version)
