cabee8d7c3d222556de086afcadde023eb566a8d
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:53:16 +0000

    him thorough accidentally relief holdon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import logging
-def log_message(message):
-        logging.info(message)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
