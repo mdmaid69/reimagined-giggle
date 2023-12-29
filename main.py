25ad291ed84bd778f2fe71eb185afc36f18ca42a
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:39:48 +0000

    total estimate sight godown lunch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_length(array):
+        return len(array)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
