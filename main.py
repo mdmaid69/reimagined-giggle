56ed4b0da2644cf8d9fef57e3f5799b4087ae7fb
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:45:19 +0000

    step situation thin freeze whether

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_perpetuity(payment, rate):
+        return payment / rate
