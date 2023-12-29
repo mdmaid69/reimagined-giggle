78444ba0aeab74ffe07b1c7d69123e0062f39a6a
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:39:18 +0000

    maintain cause who print usually

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
