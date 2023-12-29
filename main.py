c2cd72ab783f5c4b28183851cdc54d471738ea98
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:13:04 +0000

    impossible serve fear charge 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
