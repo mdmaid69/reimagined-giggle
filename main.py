e8a59b1fcbfdfb09ef432ec41074d856f0098e2f
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:40:32 +0000

    natural quarter himself broad speak

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+for i in range(10): print(i)
+import array
+def get_array_as_int(array):
+        return int(array[0])
