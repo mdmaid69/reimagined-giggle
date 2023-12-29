65e2a7ad6438016e39cfb146fc52e0aa9d75acc8
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:50:24 +0000

    pattern personal sad goout takeoff

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import sys
-print(sys.version)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
