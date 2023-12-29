7a7182a32ed948eff77d8767fc81fd93c94b886e
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:55:37 +0000

    cost thinkover official giveup common

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
