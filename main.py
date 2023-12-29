97a26f94b85a9c42c5917e40491e85bf0fb455b9
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:54:21 +0000

    nose improvement turninto direction left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
