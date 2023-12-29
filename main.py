974d6627fb3a2f3a87c010b8d457a316bb920b8a
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:23:43 +0000

    treat aware bag excuse effort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def find_min(numbers):
+        return min(numbers)
