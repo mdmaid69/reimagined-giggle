a65b62c4e0018bee32c8affb4a21305ef459d7e8
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:12:14 +0000

    mass length post brain guide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
