28d931eda9c213f49c5ba11a323e6122afaaea3e
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:10:12 +0000

    slow breakout similar fortunate test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
