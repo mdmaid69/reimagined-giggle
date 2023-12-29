cbe34ddd8e2fc9bd79ebb6622be5ac0c4d3acb84
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:55:07 +0000

    both about sure strange exciting

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_hex(n):
-        return hex(n)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_acceleration(speed, time):
+        return speed / time
