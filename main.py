1584eb3cab8269d213c74c79bd3cfc6ffd55de64
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:52:41 +0000

    thinkof wire march basic comeoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
