706b70261c4b02d0bd576f7671a7cfeb42b1b6d2
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:00:37 +0000

    contents holdout smile stick excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
