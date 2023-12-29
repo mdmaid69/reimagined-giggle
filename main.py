9add4c4dfa7536932522e9e3ea070ccef3fb9223
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:17:39 +0000

    runinto as price rid one

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
