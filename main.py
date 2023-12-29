656e8cca2039b9df7b9067549bdddee0254f1084
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:55:05 +0000

    rain none anywhere push famous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
