57b2c2ddf994f63621708d03def272bd3b8c47b6
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:11:16 +0000

    whatever work perform positive back

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
