fc36ab62fe59663f6eff8adf698243694740f9fe
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:44:29 +0000

    neat cause middle type important

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
