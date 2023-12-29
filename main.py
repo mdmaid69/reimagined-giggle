9b8f8d3f2b824bf1f656dba414add4909117ee92
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:41:05 +0000

    only comeover by opposite convinced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
