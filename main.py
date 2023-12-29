f2aafba171e326473a958a5854e9453ca5d5d99c
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:39:18 +0000

    tidy rid who direct doubt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_min(numbers):
+        return min(numbers)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
