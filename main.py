bafe12d2cdcb3b310a18637ecf73c3da09acaa2b
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:45:58 +0000

    accommodation interesting trip giveout advice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
