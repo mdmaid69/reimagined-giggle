452a82d0f50cfc4de2137a5d2cd934d4a4d3a2c6
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:26:59 +0000

    pool extremely degree and half

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
