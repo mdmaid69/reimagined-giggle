7f084e25daa76d4a10964bdd5b6a43b7db669e94
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:22:30 +0000

    complete thorough goodbye put lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_acceleration(speed, time):
+        return speed / time
+def find_max(lst):
+        return max(lst)
