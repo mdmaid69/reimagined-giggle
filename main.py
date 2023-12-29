37e56d8d38ed38fa30e724392f052eb80690fa85
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:56:33 +0000

    attempt extremely might needle boring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  def subtract_numbers(x, y):
-        return x - y
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def convert_array_to_string(array):
+        return array.tostring()
