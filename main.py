05f775819f2f3d88038bd0a14f7256c8a438b43c
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:13:17 +0000

    welcome trouble depend getaway excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
