e0ebd6f224ef1949b8a3a521cc17946a7f869ffd
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:35:53 +0000

    rarely late equipment maximum announce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
