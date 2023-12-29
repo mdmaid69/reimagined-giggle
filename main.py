ef2e14848e9dbfd6a1252b37187db82496fff649
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:04:53 +0000

    stare blind good illness any

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def find_min(lst):
+        return min(lst)
