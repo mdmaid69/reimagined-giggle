3721fc42a525047a51fa8a6b284da58918a27ca7
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:13:45 +0000

    job believe closed cheek shock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_power(work, time):
-        return work / time
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
