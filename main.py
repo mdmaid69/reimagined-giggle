24871e190db0cbe27516bad8334c4f2d40a0b168
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:36:55 +0000

    beginning eager add dead night

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
