b9225a168ef23414f308c4064edbf9d9fba475ef
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:50:24 +0000

    tread might Dd average reason

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
