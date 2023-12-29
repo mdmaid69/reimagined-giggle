46d4cd662b5d3a168546ab3e54feb31193ff8d39
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:12:29 +0000

    somehow somehow fight somewhere score

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def subtract_numbers(x, y):
-        return x - y
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def convert_to_octal(n):
+        return oct(n)
