96dc7a1d1b8cf4800ace5f6075ee58389f3c4686
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:36:12 +0000

    female try Nn should forward

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_max(lst):
-        return max(lst)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
