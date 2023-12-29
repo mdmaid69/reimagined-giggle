782e2993959a4456af398a11f9346f479808172d
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:44:23 +0000

    beyond exactly pipe worth discover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_exponential(x):
+        return math.exp(x)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
