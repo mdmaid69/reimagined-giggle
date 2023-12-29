ff73e37558b333c4ac91ba1ea7f7c08898a2e953
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:48:20 +0000

    Hh bill strong again work

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
