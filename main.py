f1349b9cb3989469209ae1de3959a670938d587c
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:46:12 +0000

    relief unfair getback late upwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
