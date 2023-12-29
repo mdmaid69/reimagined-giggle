88e18cf2aa1a68396e33b27e039a69f15f13f18e
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:29:50 +0000

    since university course battery star

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_set(array):
+        return set(array)
