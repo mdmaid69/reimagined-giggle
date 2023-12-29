b442a90b3b9014ce5d8674eadd10a71a2f1b6e52
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:47:01 +0000

    draw experience home flavour broad

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_area_rectangle(l, w):
+        return l * w
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
