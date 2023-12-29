95ba104a06a8b92ae1172b21c89b2c25bca8a590
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:18:35 +0000

    doubt roof expect protect safety

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
