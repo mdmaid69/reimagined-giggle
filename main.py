3ab7475371b962724703a294694af869a5c14935
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:55:44 +0000

    nervous innocent fork equal same

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def square_number(x):
+        return x**2
