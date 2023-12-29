cb122d4ed0ac8ab8892c417f0a55f343b9f027fe
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:07:28 +0000

    unite smoke gate trip slightly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
