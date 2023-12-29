dd4a0067a56dad9fe70a6b7c158b84ea19d147fa
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:00:27 +0000

    whoever serious peaceful aim deal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
