b16b299889e0fc5f63a8c3035d4354469cc21cd7
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:22:25 +0000

    later sky keepaway big good

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def split_path(path):
+        return os.path.split(path)
