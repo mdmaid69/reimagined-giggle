59eb49223650dad6966f990a3228ecfbdf74c3b1
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:43:27 +0000

    practical mainly thumb later spot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
