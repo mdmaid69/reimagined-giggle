62565064ebf804cea6baf038c28b7248ce7fbc5f
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:45:52 +0000

    train rush mine danger comeout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
