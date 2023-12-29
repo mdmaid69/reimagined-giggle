ddb4a9c15cb10418cab53dbf72b337f33d7ea212
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:52:05 +0000

    pump cruel fond beyond meal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
