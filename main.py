4c7f3b2bccd17683ded3f0aa52abdd7ba893e0ae
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:38:55 +0000

    afford wave present prefer score

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
