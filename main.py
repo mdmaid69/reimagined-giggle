1f892057a8e68ac4dc39be052fc75fe48385c5a5
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:14:53 +0000

    fortunate backwards really cool but

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
