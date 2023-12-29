53b81c2dae473ba0916eb9b20229615dca610ea9
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:05:44 +0000

    slice harm unless door birthday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
