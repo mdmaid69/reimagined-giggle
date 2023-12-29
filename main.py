a05e4dcdb25070a7fb43fb77082e2379779a957f
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:43:16 +0000

    independent lesson sorry dick adjective

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def subtract_numbers(x, y):
+        return x - y
+def add_numbers(a, b):
+        return a + b
