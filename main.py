5c8df06164b4b9c44465fd3fd256de8a0034638f
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:40:51 +0000

    apologizeapologise shake opposite target unfair

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-for i in range(10): print(i)
+def calculate_power(work, time):
+        return work / time
+def calculate_area_circle(r):
+        return 3.14 * r**2
