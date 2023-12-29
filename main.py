91d176f923d03075871f5a85c9e929161ea59c20
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:03:45 +0000

    failure organize challenge getaway however

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
