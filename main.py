9a404ea122c16d8f202058cb31647237bbea9c78
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:57:49 +0000

    main boat sometimes charge hate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
