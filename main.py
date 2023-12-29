c66e1c1f92f7062e5e060569350b713ece76f86d
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:03:33 +0000

    impressive street form amount height

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def calculate_power(work, time):
+        return work / time
