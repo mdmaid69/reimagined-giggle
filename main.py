208ba401039e0e1ce3f86bd01a8d5b0921e2b457
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:07:33 +0000

    any store person frighten ashamed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def reverse_list(lst):
+        return lst[::-1]
