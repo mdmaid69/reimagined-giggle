92eb457068a2902f9851b7434a2b99c62a7465cb
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:55:52 +0000

    man course prize round exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
