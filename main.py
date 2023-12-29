23ebdbc193bd1a6ee556dfaa6637015c1f39b20f
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:49:16 +0000

    claim during well noisy contribute

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
