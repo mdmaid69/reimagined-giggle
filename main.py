e50e5ee1b9b440462a5cee7416820c5388e348df
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:34:35 +0000

    food label neither ring record

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
