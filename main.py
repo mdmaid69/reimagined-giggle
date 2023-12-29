71f7d1b632e4bce94db6bf5fab700a1fd68c5f28
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:21:53 +0000

    use thinkover comefrom now border

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
