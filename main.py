b4713b21c53180d6d33adb46a26c71e32ddbae38
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:06:16 +0000

    return warm careful name television

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  def convert_to_binary(n):
-        return bin(n)
+import tensorflow as tf
+print(tf.__version__)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
