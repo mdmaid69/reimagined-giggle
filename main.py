13b1c2f72c19d80d4b34513e3cfaee0e074b6125
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:32:44 +0000

    replace smooth protect trick regional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
