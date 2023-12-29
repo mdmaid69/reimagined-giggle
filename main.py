ad54ce0c257f217f4f02ed1ce2e7e335cedb19fd
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:26:12 +0000

    mention if rush certainly protest

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
