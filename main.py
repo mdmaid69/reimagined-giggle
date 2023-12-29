27ff048890648b52066bf9b768b3f95da4f672a3
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:27:49 +0000

    share window straight in fetch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
