e2bcbdae9ad9b6a1f42c862d582520921108d398
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:32:05 +0000

    objection below prefer funny from

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
