354a5ac5c1e962c9dee9dc9301f253e3b66aaf6e
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:48:49 +0000

    soak motion protest hospital pure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
