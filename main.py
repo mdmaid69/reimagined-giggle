a887abe9ffaa5a5a49050268223bf69d6c8a5cf2
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:55:19 +0000

    no around quit show boil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
