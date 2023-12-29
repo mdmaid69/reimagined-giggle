063c9ca709e0167abbfc1e8ec3a56336be3e3007
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:27:08 +0000

    influence manage dear ask give

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
