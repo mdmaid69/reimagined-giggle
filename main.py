059f7a5384676873aa59bd478226968295247929
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:21:23 +0000

    leg impressed motion keepup voice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
