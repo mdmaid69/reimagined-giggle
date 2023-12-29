d68488d9cbf22d79554d8c58c76ef7e73385d779
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:17:41 +0000

    check cover tomorrow apparently everybody

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
