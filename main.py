0e6f1fa4a74412a85d203618c7ad2994ffec2f60
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:16:11 +0000

    sit turndown rain steam talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+def exit_program():
+        sys.exit()
