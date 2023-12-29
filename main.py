649751963e5f474f96474dc66fef5831ed4a272b
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:38:38 +0000

    direct favourite uncomfortable talkoutof otherwise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
