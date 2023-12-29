545e9be7f5d1b7ae4edcf6b7497dcc1bf9e7d59f
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:05:51 +0000

    poison lucky share wild matter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import getpass
+def get_username():
+        return getpass.getuser()
