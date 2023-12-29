bb67df6eca0c4a3e40f09a428ed20043cdb6e359
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:30:31 +0000

    confused general aware move record

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def sort_list(lst):
+        return sorted(lst)
