887487ddc0d9b2c46c3f100be8cc4f4f5deb47a5
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:06:36 +0000

    awake middle deep drive water

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
