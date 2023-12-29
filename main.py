37ed1b8e37923e5062b7c2cc66e2130c3f2cf81b
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:11:34 +0000

    who supply build where form

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
