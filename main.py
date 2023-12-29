1860b7538deae08316f8da055f5963db9297cf6e
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:23:11 +0000

    argument married apparently relax who

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
