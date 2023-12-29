ab53f060f31a27884698a5ee855cadb7c4eaf858
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:25:51 +0000

    once lie contact gooff 566end

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  def convert_to_hex(n):
+        return hex(n)
