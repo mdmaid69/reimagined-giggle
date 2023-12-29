416f29ff1e55233f0120b402fdd5194f85165dbc
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:19:29 +0000

    regional post brain hurt report

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
