00325ad534720d192bdf8cffdc872f51f546f772
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:44:22 +0000

    here bang platform hope string

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
