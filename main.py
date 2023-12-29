92de79594e598ecadcf65bf411b33f998bf51cd2
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:00:45 +0000

    tragedy cruel noone quantity Qq

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
