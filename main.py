cb25b996fd25fe85baa4e9b863cab9a352243b31
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:49:20 +0000

    slice impression except image separate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import sys
+def print_python_version():
+        print(sys.version)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
