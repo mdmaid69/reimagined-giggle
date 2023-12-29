e7674607d946462ecc30496498f51ba242ea4eb1
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:23:35 +0000

    firm single cause object child

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
