329c3845bda057add6dde584c710674fc4ac68b5
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:56:42 +0000

    summer over wing persuade suggest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
