362bd5600cfa19ed84b7101a8dd921cc14460807
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:29:34 +0000

    sister divide talk the dig

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
