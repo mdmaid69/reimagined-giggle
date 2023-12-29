c7c1484174f634999844f7691fe880164c3ee54c
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:13:23 +0000

    Ss minimum habit shade wild

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def get_array_typecode(array):
+        return array.typecode
