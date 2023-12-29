df1cb640398a0dbb22aec9721935e51f5867afc9
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:14:53 +0000

    brain goodbye capital leave object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def multiply_numbers(x, y):
+        return x * y
