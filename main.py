c014317734d352ca226980620283680842663fd1
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:12:43 +0000

    love lesson rid sport long

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def is_even(n):
-        return n % 2 == 0
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import platform
+def get_os_info():
+        return platform.uname()
