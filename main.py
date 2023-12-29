d1e8bae502880c5c44cfb5367e9638c9e5ba0832
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:50:40 +0000

    us friendly quality wrap fork

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
