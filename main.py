5155eee1fae13a389cffd6f8393b29a7528be33e
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:44:55 +0000

    score occasional material admire lot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
