8d63ad67fc734b80ad960b92c51069ed3cba4977
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:28:37 +0000

    who comeback visit keep everyone

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import sys
+def exit_program():
+        sys.exit()
+def sort_list(lst):
+        return sorted(lst)
