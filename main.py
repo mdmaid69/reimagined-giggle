889e23ec719a6644ac6cefb562ad70596de51153
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:24:16 +0000

    neither left whisper in community

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def sort_list(lst):
-        return sorted(lst)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
