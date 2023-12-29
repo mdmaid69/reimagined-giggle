980eb5ad6445043a1ca7065fba5c647e9058d0b4
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:02:56 +0000

    breakdown coat your understand spoil

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_power(work, time):
+        return work / time
