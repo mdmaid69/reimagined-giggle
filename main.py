9b02746de8cb851fbd5b59653994a4997c45773c
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:22:38 +0000

    have organize hurry even anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
