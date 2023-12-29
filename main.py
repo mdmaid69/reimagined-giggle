5483c652ade4d2536ea678c7dceb0a08a2e8a78e
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:01:53 +0000

    gift fasten pronunciation criticize permission

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import array
+def get_array_index(array, item):
+        return array.index(item)
