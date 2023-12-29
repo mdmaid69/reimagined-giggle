ff1561cdd372b2150608b7bfaf2945b97f6dd26c
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:25:45 +0000

    bringup ticket visit scare grade

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def reverse_list(lst):
+        return lst[::-1]
