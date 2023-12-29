7b5797d7c5dbd85ff76e3eac8e25f0c610b51d4c
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:39:31 +0000

    woman among use peaceful powerful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import array
-def clear_array(array):
-        array *= 0
+def get_array_as_repr(array):
+        return repr(array)
