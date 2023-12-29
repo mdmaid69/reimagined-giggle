fce2e85de11728ed8db1826c8b1dbf56a9f47dd6
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:59:04 +0000

    someone either speed 179bite solve

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import math
+print(math.pi)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
