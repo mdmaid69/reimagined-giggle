07b0cf801d0322801b4ac3836cc3661b8986aac5
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:53:11 +0000

    stamp combine level wrap keepout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def convert_to_binary(n):
-        return bin(n)
+  def subtract_numbers(x, y):
+        return x - y
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
