75a783f97be771bf80e75fd2e57531a88498d5dc
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:50:57 +0000

    prefer situation drop drive attractive

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
