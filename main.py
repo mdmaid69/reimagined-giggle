49481a4c6dad16cd8b9c4f4eef71dcdaf61b8073
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:52:43 +0000

    blind break both escape violence

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import platform
+def get_os_info():
+        return platform.uname()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
