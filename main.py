0e8402dee80e5e446a75f8530b8d36b8392e416c
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:56:41 +0000

    treat area friend moment impressive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
