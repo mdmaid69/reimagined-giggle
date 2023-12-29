20ad76b174b358415bc50dd78481925992d2f702
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:22:15 +0000

    correct yesterday miss intelligent each

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
