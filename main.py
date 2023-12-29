d1044bfd93dc4cd466093c14da7251de18c51f99
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:35:39 +0000

    waste dream crazy without react

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
