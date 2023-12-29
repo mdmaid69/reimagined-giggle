78ebc82c6749994fed7ab8bfd2cf211894a2fd20
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:05:20 +0000

    urgent loan invitation mind guess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def is_odd(n):
+        return n % 2 != 0
