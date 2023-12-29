d45b389a299ef7b9219b83c52f80e7c0a4e7ba55
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:44:36 +0000

    flow talk her it today

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_item(array, i):
+        return array[i]
