93e1dd2da065dba099702afe63fb4edfbf2eb6ba
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:33:35 +0000

    voice instead splash carry dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
