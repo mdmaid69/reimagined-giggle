8a8c1e9f53e5709156ca12efa9c5d49a25f7b7d3
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:24:26 +0000

    trick lead loan dish interruption

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+print(os.getcwd())
