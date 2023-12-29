19c18d31e97d08fff8ef744d6c90a9b58589cc64
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:59:38 +0000

    major doubt chance activity manage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_max(lst):
-        return max(lst)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
