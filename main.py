5858d9c0b91e72053750457b2c2d6e8df0aeb006
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:32:15 +0000

    greeting worry whether possibly spill

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import math
 def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+        return math.factorial(n)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
