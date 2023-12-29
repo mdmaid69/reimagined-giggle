74fdc5d561820141132329bd924a730edb6c1dd7
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:03:55 +0000

    completely professional such tear style

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import sys
-def print_python_version():
-        print(sys.version)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_power(work, time):
+        return work / time
