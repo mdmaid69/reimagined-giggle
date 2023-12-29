f9db26d9449af50df8a37da3f594cdce2b96f7d5
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:35:15 +0000

    waste introduction whichever confusing off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
