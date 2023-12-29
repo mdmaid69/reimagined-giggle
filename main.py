6697f3a1a36453e20b71cfa1f1908185b5bd4e8f
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:03:28 +0000

    surprise Jj keep produce greeting

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
 n = 10
 a, b = 0, 1
 while a < n:
         print(a, end=" ")
         a, b = b, a+b
-import math
-def calculate_sine(x):
-        return math.sin(x)
