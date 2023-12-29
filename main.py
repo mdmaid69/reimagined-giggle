606c901d34178732b931d1ed6e885d83c0c6c8f4
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:47:52 +0000

    Yy allow blame gooff importance

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
