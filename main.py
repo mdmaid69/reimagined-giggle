987a026d15cd155a4318fd17b935791b11665c4f
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:40:04 +0000

    report group middle calmly translate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def count_elements(lst):
+        return len(lst)
