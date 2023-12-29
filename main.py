77bf8a3d0a0349208a94f369ea04768892d23861
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:54:12 +0000

    lock artificial mess moral react

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def convert_array_to_string(array):
+        return array.tostring()
