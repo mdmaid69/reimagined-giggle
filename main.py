fa54e51f59a3c61fe1e0929a29c5a9b8c9ad820d
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:09:49 +0000

    hand closely line trap car

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
