af93b92f0546172526802244b4c6d3cf77cab921
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:56:08 +0000

    giveaway risk act performance sure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
