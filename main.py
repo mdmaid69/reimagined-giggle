e7618c55777d5fe6276fa51c568f02110e0de99b
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:28:45 +0000

    row rest fence range hurt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
