64a2603a3aeefeca449cc222660224b1e148732b
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:53:39 +0000

    at however praise bound repair

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def add_numbers(x, y):
+        return x + y
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
