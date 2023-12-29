da9d4d80c7659fbb197fa373512f303f755c53e0
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:06:14 +0000

    foot train used bit exaggerate

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+print(math.pi)
