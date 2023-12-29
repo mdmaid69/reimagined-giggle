825ac79330dd954607d005d8fa2f9128108eb76f
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:04:23 +0000

    ignore contrast cost innocent turninto

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+print(sum(range(10)))
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
