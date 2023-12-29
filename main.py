4d132ebb7df0d6af0cceef2353c694ebf2ae2cf4
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:20:35 +0000

    choose habit duty tongue cover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def is_odd(n):
+        return n % 2 != 0
