e3429f6a943068584ad1aa107302aa0c66ab91ca
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:10:37 +0000

    claim sticky anything range only

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
