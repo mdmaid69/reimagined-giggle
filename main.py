46a5df9524d89114b1ce51fb80162763c6988782
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:32:40 +0000

    dish defend increase safety Hh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
