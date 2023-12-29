3512be2d3a85f9a77eeb63f64285ace8e1f4b66e
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:51:56 +0000

    part complete decide ignore little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
