20024f554aebd2c3d0ad1b8adaefe0d97f9ddab8
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:54:18 +0000

    rest conclusion please their up

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def cube_number(x):
+        return x**3
