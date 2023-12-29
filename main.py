8b66a901afc2fe005aba0054a2c6e22535996f4f
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:45:49 +0000

    occasion commit 566end land confused

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
