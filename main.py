3b4232e7cb81520f534692840dc1540b0baddec3
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:24:32 +0000

    alone authority who tall Ii

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_item(array, i):
+        return array[i]
+import collections
+def create_user_dict():
+        return collections.UserDict()
