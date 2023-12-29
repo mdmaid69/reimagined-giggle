bdea961da42eba518a25e9fc09a503044b9cec8a
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:20:21 +0000

    official breathe easy interest father

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
