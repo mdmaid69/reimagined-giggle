ba1d74c18fe16764633e5ce2ec06b33f66d435c7
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:29:45 +0000

    slide giveback fail godown intelligent

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def convert_to_hex(n):
+        return hex(n)
+def calculate_power(work, time):
+        return work / time
