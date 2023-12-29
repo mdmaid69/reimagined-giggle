10ab781828b83e0a2b6d4d9813e948601d79bada
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:27:19 +0000

    trap certainly neat prove bank

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
