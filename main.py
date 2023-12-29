22934b53493f3489805047b04cc5f2839172d9a7
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:05:53 +0000

    maybe expression include payment area

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
