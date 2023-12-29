a01b2897f59f853db1fcbd474ee56f88dc838d78
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:06:52 +0000

    sight mark bringdown comeround finger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
