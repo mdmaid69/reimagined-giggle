32968162d2cbae583778f1aed309f69eb1dfb9a2
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:16:55 +0000

    first accidentally swim warning Pp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
