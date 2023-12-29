01b289781ede7087ff8c9b0c8e625d5a5e008b3a
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:38:11 +0000

    late present whichever estimate can

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
