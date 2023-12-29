6fc44a1a669f6acc50aab616e821534660f85c0a
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:47:19 +0000

    estimate leg matter trade work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_queue():
-        return collections.deque()
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
