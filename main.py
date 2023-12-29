3ee54c5ec8db3d8b3b88fd2ffbb3c3322ae20489
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:11:17 +0000

    hold press at trade whisper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
