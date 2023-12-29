bf8647f322732c437f02fa813c5dcbf7703683a2
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:57:43 +0000

    by cook giveback recovery mail

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def convert_to_octal(n):
+        return oct(n)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
