1817fc8a0e73f87b005ab187ed8e5dc63e839687
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:05:24 +0000

    common complete think sad turn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
