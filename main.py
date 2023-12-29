36adecd57367116678fbe29da91c6d716a6170ce
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:01:21 +0000

    phone container confused unable sweep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def change_working_directory(path):
+        os.chdir(path)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
