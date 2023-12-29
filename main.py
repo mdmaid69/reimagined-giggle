c3761e69c11122327bb573ec0b49c79137ac996c
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:42:13 +0000

    grand free exchange burst worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
