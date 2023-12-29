6f8ad449fc8f04b04fde53663ce74508cc6c2e2f
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:22:41 +0000

    sit variety bear remember include

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+from collections import Counter
+print(Counter("hello world"))
