a21cd1fea5848a587e9b34663690f02eac80b2d4
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:39:50 +0000

    community round middle argument left

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import collections
+def create_priority_queue():
+        return collections.deque()
