379aa6b74865d8f955cf2ac93c8e5ef96f4c2355
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:55:25 +0000

    defend level hold splash Tt

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
