98b6c235c6cf854b228ab52a43d4cdb0c374920a
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:19:38 +0000

    burst steep backwards its correction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def is_odd(n):
-        return n % 2 != 0
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
