0893fde040f506aec7e20903f972e466d2c0438d
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:10:39 +0000

    any know dark effect joke

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
