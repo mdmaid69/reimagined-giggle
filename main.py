faccf11dcf9534664bae250ac29261a78953a034
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:41:56 +0000

    joke high arrange post ordinary

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
