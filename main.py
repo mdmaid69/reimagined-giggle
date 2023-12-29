e629b098ec3017e95ea4c588d84dc7f219930fa2
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:12:03 +0000

    over mood give stretch bet

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
