99a1c601b10377fc18ea4235ce5907247a6e4a22
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:08:16 +0000

    interesting pattern scream back trick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def append_to_array(array, item):
+        array.append(item)
