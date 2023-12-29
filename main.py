367eb7ae60e352a2f165d99e1a214c72f2ef0aab
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:52:37 +0000

    narrow stupid wing argue snow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  def is_odd(n):
-        return n % 2 != 0
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
