7f603d56a43a53194c11d3cef3ed0e4fa98c6e90
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:41:29 +0000

    correction turn home circumstance nose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
