eff0ba3827a1dcef562de65f65794026605faf85
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:27:38 +0000

    bringup choose set Gg and

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
