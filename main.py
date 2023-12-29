7ad7b16db7ba98584fc4197a5285bd84c0a18bc5
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:36:28 +0000

    pass fashion talkinto underneath blame

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
