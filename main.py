1bc6ef8f6b67583127eedf92a096dcf5b808d55d
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:34:16 +0000

    first close meeting care ignore

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+print(sum(range(10)))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
