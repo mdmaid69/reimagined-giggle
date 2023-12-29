56bd18de0d2902a2ac0a107cdee66cf3f57a3fd0
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:41:28 +0000

    nor permission eager slip only

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
