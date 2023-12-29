7edeba8b681d3b91af4ea4f7b885fb12136bcddb
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:34:44 +0000

    bar sore fix little treatment

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def add_numbers(x, y):
+        return x + y
+def subtract_numbers(x, y):
+        return x - y
