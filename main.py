044ac5223d9b47657c23f34af45776ecf2757e7a
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:33:37 +0000

    formal sometimes telephone uponon field

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  def sort_list(lst):
-        return sorted(lst)
+import sys
+def exit_program():
+        sys.exit()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
