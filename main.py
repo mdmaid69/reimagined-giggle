20053249e96050686908e43adc3af53ed8b6816e
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:58:27 +0000

    ball cough reaction high finger

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def add_numbers(a, b):
-        return a + b
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_work(force, distance):
+        return force * distance
