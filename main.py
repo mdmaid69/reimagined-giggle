d88edec13628731c72fb0de40ef47cc5d2af5297
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:28:18 +0000

    broad fair switch finish winter

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
