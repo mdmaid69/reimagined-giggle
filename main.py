e46ce6d0d9933e6a151a6c2eff9669abfd9a5c00
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:27:36 +0000

    each exploration relief hard minority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_square_root(x):
+        return math.sqrt(x)
