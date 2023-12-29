bd12956d708d3cefc3ee80d79dd57569dd9ce8b9
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:15:48 +0000

    gentle handle evening confidence level

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+for i in range(5):
+        print(i)
