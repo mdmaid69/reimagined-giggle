aad0d289c1bf3d757c945e08b04436b183338800
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:09:17 +0000

    address exciting wait trouble climb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
