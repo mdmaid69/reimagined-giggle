a5381a480c22688ce4dae461ace08848d78cae3d
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:08:27 +0000

    share general comfortable like Ss

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
