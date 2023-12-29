f03f467ce916e2d4d11b7255df789169d9a4e7bd
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:58:48 +0000

    include expect burn combination fair

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_max(numbers):
-        return max(numbers)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import tensorflow as tf
+print(tf.__version__)
