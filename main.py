fe9987a95fc9bc7960ef210409b4cf787c32893d
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:08:18 +0000

    mark visit fault themselves whether

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_work(force, distance):
+        return force * distance
+import os
+def remove_directory(path):
+        os.rmdir(path)
