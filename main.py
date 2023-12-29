c42870af2528904c42ef81fb77b1cfcbb36e4943
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:46:05 +0000

    concern 568energy mixture average occasion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
