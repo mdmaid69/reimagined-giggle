39ff3dc286028bf415e2f3cd281d7f741ed825e6
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:55:44 +0000

    unfortunate walk watch change runinto

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
