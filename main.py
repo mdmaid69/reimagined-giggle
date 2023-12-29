e99194dae28dd45f5b367a492696fadc88731824
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:03:01 +0000

    shout hurry vote completely repair

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
