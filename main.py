02e49908fdf257c2dc2665837add0f2d6e324c6d
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:18:48 +0000

    shake opportunity vanish both square

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-for i in range(5):
-        print(i)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
