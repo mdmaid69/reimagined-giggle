dca955b62637f71a30e241caec6549e8421e3bdb
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:22:17 +0000

    snow pattern ticket mess safe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
