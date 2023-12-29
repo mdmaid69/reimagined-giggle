7289a9af37716e7ee72b3e2815941451209b0193
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:19:11 +0000

    find winter behind complete quick

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
