508975fc043b51d8a3b82d8f788af61758e8e825
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:51:46 +0000

    market float tense really frightened

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+text = "Hello, world!"
+print("Characters:", len(text))
