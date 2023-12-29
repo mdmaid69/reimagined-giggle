fcf467a2cd940a7bc6ef04eebb1fb732bb088e0f
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:15:14 +0000

    minor fly bring injury tip

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def cube_number(x):
-        return x**3
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import time
+print(time.time())
