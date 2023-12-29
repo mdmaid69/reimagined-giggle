f8145c6ac0e8fb0a4e6cf255588ccbb8036838b3
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:48:51 +0000

    shut strike event surprising amount

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_work(force, distance):
+        return force * distance
