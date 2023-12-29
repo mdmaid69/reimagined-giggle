0dddcc7e32a7c62a7edc7b3bd45dbfcdb83ad568
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:19:56 +0000

    whichever ordinary swing popular evidence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-print(math.pi)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
