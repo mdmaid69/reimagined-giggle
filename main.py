b8419794cabba183348c45eddae0a7afe8b232f6
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:59:08 +0000

    live comeover shout control force

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
