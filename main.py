b35455df1566877cf664f2e777ff8eb1d93acab1
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:00:44 +0000

    better evening interest start from

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def append_to_array(array, item):
+        array.append(item)
