0d6cd2fbc8bdfa34e5a29c94f508a4a5add55d62
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:43:55 +0000

    give turn arrival touch track

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
