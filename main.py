92c87fe0cdfac064615edf996a5c64dcdf52e991
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:48:23 +0000

    school nigga become begin love

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
