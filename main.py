3ab42614b23f46f6cef6e1febaf9b43e126cef28
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:14:50 +0000

    lot nonsense weekend immediately base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
