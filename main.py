98b9337344a077ee9ebaf49ec1b6ddefdb616f60
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:04:32 +0000

    understand temperature attempt judge fork

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
