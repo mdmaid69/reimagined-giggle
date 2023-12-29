fbf11a45d7d321f3cb734e5a06acb4b340a06a7a
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:08:32 +0000

    fly bringback twist entry nice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
