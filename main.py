e248134628517f5d2f71c5eab4822681aa5a9b11
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:01:06 +0000

    stage other in advise port

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_work(force, distance):
+        return force * distance
