1aa5674fa3e27bb869ede7b6a8eb787804c284a0
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:14:50 +0000

    lock even equipment against appearance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
