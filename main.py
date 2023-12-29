c8d1d823b27a09390d88acde31fcba1080f38bde
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:48:25 +0000

    straight trick passenger excited prefer

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
