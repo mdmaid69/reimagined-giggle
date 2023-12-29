26d6bc75398941366b85bf3a60507c580dc603ac
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:38:19 +0000

    artificial contribute advanced black trousers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
