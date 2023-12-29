f766fe8caae0370c2e15cda9a9b01e4f5549dc89
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:00:52 +0000

    temper accident man normal issue

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def get_array_as_bool(array):
+        return bool(array)
