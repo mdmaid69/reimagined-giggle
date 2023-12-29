b2fbffd358a89e982f5d1da94ee05ac22e108f59
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:07:34 +0000

    comeout whenever thanks damage ball

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
