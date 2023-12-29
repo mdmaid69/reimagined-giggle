4a8d8e3f8f2d84866ce297cf6c64ca191247df5a
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:20:44 +0000

    arrangement exact accident platform general

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
