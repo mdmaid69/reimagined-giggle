4b262b581fa8ee31e9423424b120a076f082f4d8
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:15:20 +0000

    land happen make beginning deep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
