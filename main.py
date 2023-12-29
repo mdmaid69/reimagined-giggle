de71bed6e31f61ae6050e91dc1bffce26c48f96d
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:43:56 +0000

    press clearly threaten might getoff

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
