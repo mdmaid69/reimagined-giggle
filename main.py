5b8320cfeaca8983d22c51ec954e90d81560a570
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:30:24 +0000

    describe until but should stand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_power(work, time):
-        return work / time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
