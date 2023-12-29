76383935a3cbb51f5849a3328dfe74b462b1f2a4
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:34:01 +0000

    high sensitive Vv ability tune

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def cube_number(x):
-        return x**3
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
