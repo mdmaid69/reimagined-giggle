6d64a2bfc0f9e6d0f2bb565386435bf03e95ff20
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:14:50 +0000

    seem pattern takeover much brief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
