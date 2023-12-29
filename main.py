8771f8d3445e3a9a822130bb52dc056a01eae495
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:36:42 +0000

    frequent damage yesterday far spare

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_power(work, time):
+        return work / time
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
