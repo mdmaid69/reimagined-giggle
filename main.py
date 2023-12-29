575c9a47368d164d44614ecc35a73fde41aba167
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:18:28 +0000

    culture getaway hour result increase

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
