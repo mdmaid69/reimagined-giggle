5a448b6fe9c1d841f6ab6cc5195c9c59baf24839
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:54:14 +0000

    forward shout card anything acceptable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
