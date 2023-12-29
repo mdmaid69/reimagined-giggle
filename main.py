1635a3343700c46752fc37aca32fe717d9ce3736
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:01:42 +0000

    argument sign noone chin major

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def is_even(n):
+        return n % 2 == 0
