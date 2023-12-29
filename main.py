0bf2da192e054c5b0b04e6f0d3e8d83c365d7099
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:00:34 +0000

    hurry date floor note future

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
