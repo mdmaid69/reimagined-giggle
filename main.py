d19fc7a30fb81193ebc7850d8cc71244d6008df4
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:17:39 +0000

    confuse achievement intend basic callup

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_current_directory():
+        return os.getcwd()
