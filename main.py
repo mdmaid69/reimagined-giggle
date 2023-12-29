cc01a04fcea3cb1d8e0a202b22e28709e82aa4f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:53:36 +0000

    leg amount answer rob week

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
