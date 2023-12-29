cfde076119ec00eef5574f76f09a80e3de3d9503
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:49:41 +0000

    breakinto shine suppose unusual lookup

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
