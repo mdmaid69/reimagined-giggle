259b7ae7b17b5c54b23f0067dc358413c9ff9624
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:48:59 +0000

    fall failure makeup goup involve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import random
-print(random.randint(0, 100))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
