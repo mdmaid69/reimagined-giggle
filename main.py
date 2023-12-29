7802abb5870b67cdfad1de00c0ae4d5f26693071
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:37:03 +0000

    clean must care mysterious dinner

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
