e967682b2614a8db51549d04d96b167b90371a0f
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:13:26 +0000

    turnon ought repeat worth some

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
