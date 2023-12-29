be0ac362ab55a8fcaa812bed9a936cde81c4390b
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:24:56 +0000

    square remark towards lookaround gooff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_index(array, item):
+        return array.index(item)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
