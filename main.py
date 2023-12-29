e1136ce1d1fb94b2758015e6aeba6abdafc3b01c
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:33:17 +0000

    various 564encourage majority memory clear

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
