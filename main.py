2ffa3e9cecd7dc015da4135318592aa4b9245147
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:10:26 +0000

    powerful 564encourage grade same scrap

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
