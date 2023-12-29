8349328697e7400a5d16a107103538c0e494b4a1
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:01:22 +0000

    of possible bear organize keepup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def count_characters(sentence):
+        return len(sentence)
