5fef6574b027a012d2f708b8de76406d21aabc46
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:33:23 +0000

    ordinary formal though fashion several

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import collections
+def create_queue():
+        return collections.deque()
+def convert_to_octal(n):
+        return oct(n)
