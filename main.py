e60eaabf8c0caaf0ca37bc213f3730e2bcce7d51
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:21:16 +0000

    bringdown true hope stamp result

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
