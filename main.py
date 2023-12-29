a73c13045cde79820fb4cf8ba6f71c282a6a6fb1
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:25:59 +0000

    tip interested cook friendly close

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
