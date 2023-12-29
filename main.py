24514ecad2e98b5cb243eea06f3346f7618b8189
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:14:34 +0000

    bank spring period must eat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_error_function(x):
+        return math.erf(x)
