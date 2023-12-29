936f72424e59f38a63b483dbac710dcfb3e35460
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:44:55 +0000

    circumstance excuse his medium meal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
