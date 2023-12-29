de6713f1412cfa98d18fa56b44050f8589316a57
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:33:55 +0000

    eat related suppose try truth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_bool(array):
+        return bool(array)
