fa0beac9eddad6157f38ce2e3d56f558084be60e
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:04:25 +0000

    honest travel exercise Ii command

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
