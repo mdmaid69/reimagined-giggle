ec770fec2f53398d86e0148ecd2bd0b3aae62a78
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:06:33 +0000

    getback exchange callback this Ii

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
