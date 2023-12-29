c4906960e6014cd86b66f1b5758e7242e07ad411
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:55:40 +0000

    ask soak runover fly desperate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
