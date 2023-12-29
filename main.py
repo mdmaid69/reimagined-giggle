5b7a4e9177c0d5de376510f0c0c7d63b2e292715
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:50:16 +0000

    damage shrink gate lesson regard

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+print([x**2 for x in range(10)])
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
