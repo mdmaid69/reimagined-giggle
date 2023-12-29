20af55fcafa44c05e39fdd2a23c5bcafa3ef37f8
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:06:45 +0000

    damp dry deliver hear conversation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-name = "Python"
-print("Hello,", name)
