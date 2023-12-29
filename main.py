cd767c60f613f2a714a853cb754b7d5ee64a636f
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:05:46 +0000

    like type influence up height

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import collections
-def create_priority_queue():
-        return collections.deque()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
