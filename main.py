45f23e26e8f123e10cad104783d28f74e1711de6
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:10:13 +0000

    store total person mainly tap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
