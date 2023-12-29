acc0d679d7f38dc845072a956e046520bc874712
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:09:12 +0000

    Gg rise pot gas this

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def count_words(sentence):
-        return len(sentence.split())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
