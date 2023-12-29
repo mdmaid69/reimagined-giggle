5abd8031abfa040c0aa724a05a8836ac295adb98
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:50:35 +0000

    child hollow dear market warm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
