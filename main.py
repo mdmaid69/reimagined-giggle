b3d4253a5db7139fa9fe56e318cff92c6b7f7ff5
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:21:58 +0000

    like convincing relief pain lot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
