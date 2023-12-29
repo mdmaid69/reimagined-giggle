4480aba2ede1f55c41bcac38e9d16bc4e4e8dc39
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:23:52 +0000

    respect set trade small goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
