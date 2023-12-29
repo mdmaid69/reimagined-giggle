9cfaa10aa348feef9f7a8abbc8849fe1d3f728b2
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:23:17 +0000

    below himself turnout demand nonsense

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def multiply_numbers(x, y):
+        return x * y
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
