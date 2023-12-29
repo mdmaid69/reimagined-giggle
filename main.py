2e69dccdfe7d8a8f78e9cc7ae2540fb2005b49a0
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:28:54 +0000

    can Vv beneath object maintain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import collections
+def create_queue():
+        return collections.deque()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
