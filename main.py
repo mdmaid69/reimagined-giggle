3a70d7a19b905606c2c85ac803d30bbc33f1ac9a
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:51:35 +0000

    Jj split check settle convenient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+name = "Python"
+print("Hello,", name)
