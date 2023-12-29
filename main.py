dbdcea6ccef779a6c9bcb977dc8c1cb4cae52399
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:15:59 +0000

    dig achievement home cry sentence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
