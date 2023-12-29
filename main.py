4335aa80beba916de5f98aade8209b3fa9dbd389
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:59:52 +0000

    shut reserve task crowd cruelty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def add_numbers(a, b):
-        return a + b
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
