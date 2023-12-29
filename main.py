dfaf7fa195a4292bb4fdf5b2fd8c87e383f61508
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:51:32 +0000

    custom major abroad one name

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-print(sys.version)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
