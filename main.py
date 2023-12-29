022c857f357bce8aa8880feb9eac2def09219403
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:16:28 +0000

    live off autumn lift smooth

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
