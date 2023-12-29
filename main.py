7cc2c0b4e4d18361de38628e1c3c5c08ed7bc8f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:34:04 +0000

    you someone whistle shock pen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
