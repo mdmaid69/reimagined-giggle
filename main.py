1116111b54240ef158f43c16be21e320174654d0
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:51:55 +0000

    flavour upstairs hurt will cry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def count_characters(sentence):
+        return len(sentence)
