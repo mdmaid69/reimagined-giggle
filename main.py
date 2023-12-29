14412bb51f9763811ad3ac4b9306f8514f02360c
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:31:02 +0000

    duty understand separate that then

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_pressure(force, area):
+        return force / area
