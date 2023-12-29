e87d5ab3198fb746bd7e38bfedade3cd53536d33
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:52:59 +0000

    talkoutof suggest evidence makeinto wash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
