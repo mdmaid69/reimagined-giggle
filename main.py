0359d1345a9b1bd0057ca0b718665c1598680432
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:09:19 +0000

    figure turninto will business my

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
 import heapq
 def pop_from_heap(heap):
         return heapq.heappop(heap)
