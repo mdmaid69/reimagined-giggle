b7a10f1d83d82bf65104ee1256275045888f39e6
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:19:13 +0000

    callup talk agreement exaggerate runaway

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
