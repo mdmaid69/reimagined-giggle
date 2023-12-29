58a0712f1a5a0b85aa29be75cab6b4fe96ab6bc4
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:23:35 +0000

    Tt cheap recognize search variety

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_tangent(x):
+        return math.tan(x)
