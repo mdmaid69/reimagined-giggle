2dbb2f549e71a32e26a24a2f4decb294141bc3a2
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:37:51 +0000

    interrupt fasten Uu story cap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
