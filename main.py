dc3b0478f69361eab60fab9e49db011e08955de2
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:30:15 +0000

    consider mention already community finish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
 import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
