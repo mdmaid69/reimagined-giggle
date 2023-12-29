75005134c3a3cf4fac50e7e5f2fc7c3fec3c6904
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:08:37 +0000

    bottom marry weekend smell eager

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
