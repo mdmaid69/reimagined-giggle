7ee6ce1c4b9d144ff5625a430ba668520dd2d5c6
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:52:15 +0000

    only sister special freeze about

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
