ee384d069c947a2587b56631c26a7c0ea9d30043
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:58:36 +0000

    accidental bin area total private

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def convert_array_to_string(array):
+        return array.tostring()
