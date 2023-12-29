9cb6073f9244a3a47f4ce8462f9a7aad3488fdb9
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:34:08 +0000

    confidence ashamed do soft region

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
