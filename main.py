360374809ac6dbf6f914725ea3e48e11462730b9
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:24:48 +0000

    centimetre second hers note attitude

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
