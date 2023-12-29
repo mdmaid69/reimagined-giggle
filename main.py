e5b325e89f1829ab96752913d6c07c6f9b7c7c22
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:29:29 +0000

    tip bunch trade concentrate narrow

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_density(mass, volume):
+        return mass / volume
+import time
+print(time.time())
