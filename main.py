9d02756e89dc4b73afca8545a64e228485065d9c
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:15:50 +0000

    discussion finish mine contents night

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import collections
-def create_stack():
-        return collections.deque()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
