d206a8db5302a9f7991c4dbf5996c2a8375818f5
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:30:40 +0000

    seem unkind hardly perfect point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
