bd4f40cb3441163fb0f29a8b9e5a3435e854f965
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:46:59 +0000

    distant everywhere catch variety moral

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def get_array_as_set(array):
+        return set(array)
