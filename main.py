39f4895c19dd4b121fb239501dd1a7eef5b13ffa
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:30:54 +0000

    child line around produce concern

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def find_max(numbers):
-        return max(numbers)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
