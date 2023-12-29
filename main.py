0edf37ebd28e49fff966a89d76f7dc04b2ffc61a
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:05:36 +0000

    entry turn influence empty show

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_array_itemsize(array):
+        return array.itemsize
