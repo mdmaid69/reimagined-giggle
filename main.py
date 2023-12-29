d79c476b139bbd148662ec4b8855d8e06e6eea21
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:02:33 +0000

    criticize Zz badly easy side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
