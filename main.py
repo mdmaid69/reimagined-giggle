933ee164ab7d3f8d7ce86688c9de090016115070
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:48:17 +0000

    breathe picture awake Oo cook

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
