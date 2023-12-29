f93c9bd6135253a2c91e7ccef7d87fd9c5d4882a
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:20:05 +0000

    underneath shopping ground emphasize beyond

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
