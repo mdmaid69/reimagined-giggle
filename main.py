15a42d9c7f3ff0e2969cd1d9cebe070de169818d
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:50:20 +0000

    use address war train education

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
