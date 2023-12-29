901aa1eb7f4d71971006e85cba91207213c3ba9e
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:48:16 +0000

    home firm shall treatment cook

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
