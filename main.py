b4368df0047f92b0e4e19410ee7beb97da6155de
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:03:10 +0000

    fall fix there gather dream

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
