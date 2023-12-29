49df63d7ee6a9142f9240506cabf5ba5fd207144
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:58:05 +0000

    either occasion thin private secret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import collections
-def create_queue():
-        return collections.deque()
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_as_str(array):
+        return str(array)
