85dfefc83f0e61f8b3312c114f88ca4240e0d02f
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:43:09 +0000

    improve prefer lock skin impossible

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def remove_duplicates(lst):
+        return list(set(lst))
