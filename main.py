9663ab58639e985db6e438b5cb409253414e490e
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:12:10 +0000

    abroad protect delight exist soil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def is_palindrome(s):
-        return s == s[::-1]
+def change_working_directory(path):
+        os.chdir(path)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
