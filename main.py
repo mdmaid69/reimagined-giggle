584fd47fb6df4d6386930cbcb68c25ce5bfb5675
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:07:33 +0000

    holdup summer Rr ought laugh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  def sort_list(lst):
+        return sorted(lst)
