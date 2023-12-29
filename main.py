0d02401be480946bcb91ff9efdc6d1d251235f48
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:13:35 +0000

    hot bet much century temper

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-def count_elements(lst):
-        return len(lst)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+for i in range(10): print(i)
