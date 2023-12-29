dd995fa5126e38f35cfe1388a72d0c0388f85531
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:57:18 +0000

    practice breakout both cheat solve

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-text = "Hello, world!"
-print("Characters:", len(text))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
