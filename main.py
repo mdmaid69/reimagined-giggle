031700f95ae3f06d07294e762b707ce6babf45bc
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:11:29 +0000

    rise single sport scare lunch

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
