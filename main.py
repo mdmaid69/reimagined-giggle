b3d76b340cd0712732132d244e8e7af4737cf7da
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:26:56 +0000

    pour wise yourself education sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
