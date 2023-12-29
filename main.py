0ec809f45f8d2ea333081a6f0b962656649b69fe
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:21:46 +0000

    process offer treatment breakinto steep

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
