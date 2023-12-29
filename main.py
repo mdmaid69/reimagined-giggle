3b6d66da90bd5dd19dea41903466e6fc704c1c5d
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:05:51 +0000

    file top guilty report contain

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
