9e730c0a75a339caf9e836d2055c4f4fbda9baaf
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:47:27 +0000

    relax plastic statement apply breath

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
