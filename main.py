b65767b6c8899cdc08a5b12793e4ce132f5f301a
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:01:17 +0000

    correct style break rest careful

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
