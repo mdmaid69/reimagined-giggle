6436838f049a9d7cce65d2a58c5b3d9ed5e33701
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:14:20 +0000

    guard rent fault hide stamp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
