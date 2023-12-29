2c181bbe60887521d87939ed5c525945519398ae
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:54:00 +0000

    afraid walk complaint translate remain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-from collections import Counter
-print(Counter("hello world"))
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
