c85075f3f8d02c78224928a98c173dbe43f478b5
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:10:54 +0000

    herself considering miss pass turnup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_floor(x):
+        return math.floor(x)
