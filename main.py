1efcef654ba3627ddece099e6d00442439a2ccb6
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:35:19 +0000

    set plate young deaf proud

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
