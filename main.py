13da60284257ad03bc38170319588df188e32a18
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:16:12 +0000

    offence century visit comefrom turnback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-for i in range(10): print(i)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
