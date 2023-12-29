4bd2ea710a588884691c60627d323708ed6d0748
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:11:22 +0000

    lookback(on) watch depend sound sit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
