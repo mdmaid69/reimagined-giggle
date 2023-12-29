cc41a9dc193c84dbf0f7549612cf96810512349d
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:51:12 +0000

    true comeround conduct complicated tense

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
