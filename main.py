66fa690a6d772eb35a5db455c94ef21ce0fee7e1
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:23:36 +0000

    prize or angry anywayanyhow noise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
