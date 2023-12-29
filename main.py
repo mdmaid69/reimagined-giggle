ca371484851189bfbf379b05367bf8686024a9a7
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:18:12 +0000

    value attractive wash file afternoon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
