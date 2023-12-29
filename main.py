5ea2cf7b27e8796e04356f7e3fee7a335c7c926b
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:04:35 +0000

    centimetre both stick dance runaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import os
+def remove_directory(path):
+        os.rmdir(path)
