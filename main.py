8b55a3374992f8f721f093dfac4d5f4ebb973a2f
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:11:47 +0000

    call turnon shallow mother regular

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
