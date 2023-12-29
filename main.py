16c644911b26324432eafd44522ba744be979a30
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:07:53 +0000

    thinkof port though comeround fetch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
