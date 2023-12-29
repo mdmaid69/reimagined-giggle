44887617cde8c92d3d83c8979c7620f0f2d045b8
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:27:00 +0000

    slight shop view breakout funny

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
