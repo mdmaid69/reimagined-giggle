ffc0cceb545be7c7cd0d706bdff921ad31956a30
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:27:57 +0000

    whenever pause style body measure

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def find_max(numbers):
-        return max(numbers)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
