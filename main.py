dd58595118dff4389708bf7d5a6ea38a205f339f
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:30:28 +0000

    this alone chin better next

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
