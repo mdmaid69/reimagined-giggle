6ed41ff7c2853a47303f41e848109609fc7404ee
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:03:45 +0000

    off interested steep pocket bridge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
