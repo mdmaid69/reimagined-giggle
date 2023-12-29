4b0881c4ab53e7d4c16576a2274406b7469e514e
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:54:50 +0000

    brief formal noise joke sad

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def cube_number(x):
-        return x**3
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
