a6619bc7e6ce8c974bf3da3ac438bdddd3f9b121
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:28:16 +0000

    physical much art it completely

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
