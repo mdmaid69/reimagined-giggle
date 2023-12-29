2512c9d2a836636b48e235584da3901f09f4441f
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:52:52 +0000

    capital object lift accident charge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
