96fc5e2561056ea880543a3b9f7e8f38ca2f8ab3
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:10:39 +0000

    much complete before example together

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
