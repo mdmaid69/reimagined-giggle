f19341f683d70968048abd5ac565ca28cde77ce4
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:29:38 +0000

    proud dream because book why

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def divide_numbers(x, y):
+        return x / y
