662e35604be8de18416494eea36e452c158a375c
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:41:55 +0000

    exact rid indeed tax introduce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
