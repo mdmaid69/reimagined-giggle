2e3a5524bdf50c07ef2ccc55ceffc1471fde165c
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:51:24 +0000

    something lose main fond rain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
