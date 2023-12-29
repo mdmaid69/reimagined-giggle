2b7873b7e49b3371f27b53e5e9421f7dc9cc91ea
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:11:27 +0000

    573entertainment badly average man reasonable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
