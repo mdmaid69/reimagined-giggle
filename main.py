cc947b8d8508fb8899ad6321ebf796544b7693ac
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:18:38 +0000

    likely information holdup part church

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def find_max(lst):
+        return max(lst)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
