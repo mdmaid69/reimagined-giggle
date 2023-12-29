e3696b7d55c1ca61097f1830ff84cc165a0a9bca
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:20:44 +0000

    father through arrange suckme section

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
