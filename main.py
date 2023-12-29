17ab6156a8b0ade2a127194ea4098d77e22ea7ca
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:02:44 +0000

    trip slowly fence answer fall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
