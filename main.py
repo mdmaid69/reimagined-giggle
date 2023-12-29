037b7ecc539c9abecc162ec5545f52ad40e81fec
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:09:40 +0000

    everywhere smart remove heart eager

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print(sum(range(10)))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
