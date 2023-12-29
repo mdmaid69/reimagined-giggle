aaa467e2e3a02ea8664f024b713ddf562485276d
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:31:23 +0000

    disturb mystery full point limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
