025255b275c129b360e59a55dfabfbe6fb80a1d3
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:37:57 +0000

    so sink have bringback beginning

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_density(mass, volume):
+        return mass / volume
