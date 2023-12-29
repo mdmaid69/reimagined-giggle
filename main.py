bf0e9fc6c2810adddce0feb390830657ebbdb151
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:03:23 +0000

    rush say smell used teach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
