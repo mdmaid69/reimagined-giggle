807519499bf7abb0ed8b3d178f8849215f193393
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:30:58 +0000

    throw takeout Yy worse better

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import datetime
+def get_today_date():
+        return datetime.date.today()
