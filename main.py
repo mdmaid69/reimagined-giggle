7ec1739fa6017414e603777b6a534d5704392627
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:22:57 +0000

    university comein sleep unlucky organization

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
