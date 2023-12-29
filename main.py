3bef738206f9f117fcb3e2b799870f3e90f65a2a
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:45:50 +0000

    situation murder building scare quickly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
