016189aa9d63aac15b2657bf9cc87c2f8fb22e65
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:15:49 +0000

    kick wander rough dinner sudden

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
