f23819b05f71952dff5ff44b2ac0c7c79b3d6e7d
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:45:35 +0000

    destroy clearly direct scale will

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
