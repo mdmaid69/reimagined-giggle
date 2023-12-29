c9ed4846820098c951451a8a2583e3672e4de90c
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:02:29 +0000

    turninto repeat pass round front

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
