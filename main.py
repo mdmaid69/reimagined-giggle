1327a0a3a8e3ed9ad9599c75acab22ea19f118a2
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:28:58 +0000

    grateful common OKorokay slight suddenly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
