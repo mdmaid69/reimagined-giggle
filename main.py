e798846b0b42d6680596d41096e51234f762f492
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:02:02 +0000

    pleased pull juice 570enjoyable terrible

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-print(random.randint(0, 100))
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_priority_queue():
+        return collections.deque()
