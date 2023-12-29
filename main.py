8017d7155d2b407a7cadbe1fc64953899438fd26
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:54:15 +0000

    basic position begin kick abroad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+name = "Python"
+print("Hello,", name)
