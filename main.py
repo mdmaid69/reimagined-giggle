16a36f234e8c9daf25bea530b97df2cc68bd37cc
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:23:23 +0000

    bill fork Cc present whistle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  def add_numbers(x, y):
-        return x + y
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
