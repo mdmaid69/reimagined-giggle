7320aa9c3d905b877455a80a5134ee53fe514c07
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:44:12 +0000

    long pretty profit play owe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import collections
+def create_stack():
+        return collections.deque()
