bf220c38f8ed5acb5bc98831334f567a35ebfed9
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:15:09 +0000

    strike frighten tongue thorough hear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
