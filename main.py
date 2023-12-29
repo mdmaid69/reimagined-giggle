e93cd2328614c258a4c741d791ef1d07fc0bea41
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:37:17 +0000

    until cross strike them translate

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
