849cc9f2d351d3ba8573137eafa696eb29f064cd
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:11:23 +0000

    goon club government sure deserve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
