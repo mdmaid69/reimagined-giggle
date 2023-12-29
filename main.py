9e72b415e0db6c9c9d718f1c84adda393c018889
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:46:43 +0000

    only comment often across journey

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_ceiling(x):
+        return math.ceil(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
