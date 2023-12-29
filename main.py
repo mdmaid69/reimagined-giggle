5db3046b02bfd3322403a497f9059f78c52eefe1
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:06:43 +0000

    thinkthrough whenever find turnback fall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
