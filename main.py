608aea59996183e57394ea1a016215897eeecde0
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:19:58 +0000

    much turnback usually declaration repeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import collections
+def create_counter():
+        return collections.Counter()
