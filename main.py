42053076086c08c9d4bfb8c345d8c95267b09b25
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:28:52 +0000

    happen water shape fetch direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
