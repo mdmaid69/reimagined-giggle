b379fad700bfad152eaf6fb8b8a4d8d19b16da18
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:47:39 +0000

    building objection offend length more

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-print(sum(range(10)))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
