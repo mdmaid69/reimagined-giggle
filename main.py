08a0cb92cc35e2c1aac876932a97902dfbf6f113
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:25:18 +0000

    giveback doubt repair trust government

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import time
+def get_current_time():
+        return time.ctime()
