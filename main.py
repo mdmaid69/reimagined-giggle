3b8f28413282b24de848e76392714d285cff3bb1
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:12:37 +0000

    entrance excited war tidy development

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_stack():
-        return collections.deque()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_work(force, distance):
+        return force * distance
