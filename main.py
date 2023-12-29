67f18bebea4a1bbe852a5cd52ba2e1972211a3b0
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:13:34 +0000

    be which naturally path after

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_area_rectangle(l, w):
+        return l * w
