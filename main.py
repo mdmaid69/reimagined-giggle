2416cfb5bc610430bd66592f724d0e3fe8d50916
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:57:13 +0000

    car pronunciation concerned backwards blue

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
