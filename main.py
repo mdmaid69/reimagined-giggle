70bf86cabbae27a2c6353ba473d77d79b06d6166
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:34:27 +0000

    level suck cause show where

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_work(force, distance):
+        return force * distance
+import array
+def append_to_array(array, item):
+        array.append(item)
