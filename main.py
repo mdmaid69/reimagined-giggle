4757d6e57646a0e8a4756804faf487ea5f1eaee3
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:27:53 +0000

    closely ill feed uncomfortable willing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
