8e76919d762a14b397ab7594a82f3f1c9ac2c337
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:14:19 +0000

    communicate spring angry day share

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def find_max(numbers):
+        return max(numbers)
