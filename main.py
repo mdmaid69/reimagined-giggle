c6a81489f328e5e451d6955a9f32e1ebffc9dc34
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:43:29 +0000

    hurt tool appeal whistle neat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  def square_number(x):
+        return x**2
