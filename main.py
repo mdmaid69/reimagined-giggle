4189e7a586fe6e6b9bd86d3a5866b7a441008fc0
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:40:48 +0000

    thing variety affect result address

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_queue():
-        return collections.deque()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+for i in range(10): print(i)
