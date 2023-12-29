69b6cab7ed4833e7ac5986ddf2999e6e6f8683c7
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:52:32 +0000

    trust brush sure nice use

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_acceleration(speed, time):
+        return speed / time
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
