cfbd3f1a7ae976f0c1d703ed0948a883dec360c6
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:36:10 +0000

    bath interruption pocket across earn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_item(array, i):
+        return array[i]
+import os
+def change_working_directory(path):
+        os.chdir(path)
