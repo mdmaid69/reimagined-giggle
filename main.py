3519b048880776392370979c6f8c59fc1d0d292d
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:23:36 +0000

    responsible repeat practical spread prize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def insert_into_array(array, i, item):
+        array.insert(i, item)
