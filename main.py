61490251a68e6c98020ae273949493b78c0d140e
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:54:06 +0000

    challenge switch squeeze strength beat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
