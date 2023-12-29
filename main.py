40275d83cd7afaf3f723dbc6da0a2bc9e8ab03d3
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:44:11 +0000

    war vision everybody hate as

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
