fcb194fe5df555579da0a62e9faf93eda6216dd6
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:35:02 +0000

    makeinto consult point land within

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
