a6e41861ac6dfe2211ef17e72ab0e897452dc2bc
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:54:43 +0000

    drink everyone failure on increase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
