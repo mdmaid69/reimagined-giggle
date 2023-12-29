b81d28297014a1516a016fc9c9e1e2649edefa99
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:22:01 +0000

    modern type Ss much instrument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
