724d888d03ae819cd5535bf45dfad8cc14d80e3d
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:26:48 +0000

    bad bye complaint hide satisfactory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_speed(distance, time):
+        return distance / time
