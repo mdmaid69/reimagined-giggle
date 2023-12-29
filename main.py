1cc4dc18caabf48ab0a6187f6925adc05da824b0
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:34:57 +0000

    drink contact bang nose takeout

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def add_numbers(x, y):
+        return x + y
