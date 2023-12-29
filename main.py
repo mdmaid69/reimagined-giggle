f1d977a0b483f0849ffcd615fcfe1d040e53fe03
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:07:29 +0000

    day takeback workout uniform remove

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
