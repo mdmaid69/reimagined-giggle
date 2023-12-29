105292243d359d9b1a41f126f6be24ab7ccf1131
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:32:08 +0000

    force calloff cheat herself study

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def find_max(numbers):
+        return max(numbers)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
