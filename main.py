18aa62d89fcb5977ecb46cbfd81162a3661ca269
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:55:04 +0000

    challenge mention print to mind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  def count_elements(lst):
+        return len(lst)
