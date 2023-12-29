ce2a6461e1d015a5cdd51f8d918513a3128db40e
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:24:18 +0000

    feeling strike keepfrom schedule direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
