7fef446e1033e3aba4693c87d2a8b26c50354ffe
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:27:40 +0000

    comein type crowded block ability

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def cube_number(x):
-        return x**3
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def get_array_typecode(array):
+        return array.typecode
