0a6c0815b6bf56321c4e1b8e0fcfbc8a1b3a6c18
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:48:48 +0000

    wrap myself visit correct towards

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
