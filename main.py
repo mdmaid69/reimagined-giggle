4ba215630c80ebf012c8d861cf81a19b084b516e
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:22:23 +0000

    mention snow cause unable pencil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def add_numbers(x, y):
+        return x + y
