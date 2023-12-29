b5c038046b83c229a8d711a14a295cf4402af86f
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:20:31 +0000

    Hh character thing brain force

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
