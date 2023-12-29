dfb30e10605946c100b4c5b8c285e8bdc3398816
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:02:52 +0000

    order along show popular stop

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
