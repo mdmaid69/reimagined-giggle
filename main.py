e1db0691f06cc605d3a27e635e0b6f94178a1659
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:56:04 +0000

    fortunate waste zero gentle Dd

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  def is_odd(n):
+        return n % 2 != 0
