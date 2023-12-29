b35282d70c94ecf7c4c9d010123f63d7488ef59a
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:36:57 +0000

    convince wild accidental pretty length

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
