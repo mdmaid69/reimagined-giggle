9dc1b68e9c8703e6818585125358bbf1e15d82b7
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:01:02 +0000

    close something centre situation born

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import collections
-def create_counter():
-        return collections.Counter()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
