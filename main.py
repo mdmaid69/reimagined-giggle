7dcf45ceb9de44ce7d128542dc99226c6a061ad0
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:36:11 +0000

    exit content serve fashion along

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
