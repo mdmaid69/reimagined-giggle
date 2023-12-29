44b13766dca985439d64a80904b238b018d9a9eb
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:16:05 +0000

    advice keepfrom book life by

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
 import os
-def remove_directory(path):
-        os.rmdir(path)
-  def sort_list(lst):
-        return sorted(lst)
+def get_file_size(filename):
+        return os.path.getsize(filename)
