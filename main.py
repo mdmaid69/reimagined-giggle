5c4166ea5cb75d80a3694e8e89dbf57954cf6f64
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:52:42 +0000

    unkind month desperate easy doubt

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_item(array, i):
+        return array[i]
+import os
+def remove_directory(path):
+        os.rmdir(path)
