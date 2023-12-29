c771d1f9e732c0689af12b43812ec8c54d72839d
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:11:29 +0000

    cause open knowledge somehow opponent

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(numbers):
-        return min(numbers)
-import sys
-def exit_program():
-        sys.exit()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
