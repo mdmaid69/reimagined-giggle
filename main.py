c81e49211966fa878aee2b027adf6c8f06630e59
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:23:27 +0000

    lead know blame smooth immediate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
