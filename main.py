58d5ecfd19f3c191d82a95fda2d7de35d618f6f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:38:12 +0000

    unfair crisis heat month gap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
