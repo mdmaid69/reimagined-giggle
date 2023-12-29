e4dd6311da82f41a2d7ca3a4cc5ee38bf42f5e6e
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:27:40 +0000

    sink slight own sweet smile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
