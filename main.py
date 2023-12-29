ae508ac3248f56bff326ec01b77e16919794b5f4
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:11:12 +0000

    sensitive seat right shop delicate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import random
-print(random.randint(0, 100))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import numpy as np
+print(np.array([1, 2, 3]))
