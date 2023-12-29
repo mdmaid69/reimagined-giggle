f27c04dd632e48121cba9fedc8c26eee3add2f85
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:33:08 +0000

    run disgusting live slip confused

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
