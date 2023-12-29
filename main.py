b704b23227d9c88c9df8ae5d3130d7a7167cf107
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:07:13 +0000

    evidence nice point hope rid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  def sort_list(lst):
+        return sorted(lst)
