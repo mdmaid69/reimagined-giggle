a973ab1932deea8bc54f2e015cba8711bdbca0f0
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:29:01 +0000

    along about anybody opponent bit

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
