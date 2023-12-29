881ed394aebf4173c8057d0980221ab090528a43
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:14:00 +0000

    spoil love weigh level hospital

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def find_min(numbers):
+        return min(numbers)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
