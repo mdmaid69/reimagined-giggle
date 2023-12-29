de8038cc9d3952ffe903b1b0de9defbe70841633
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:18:31 +0000

    listen date flat justice open

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
