1a29acf2c7cf81f7a38125baa9f0884a79c22337
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:00:05 +0000

    responsibility comfortable emphasis scheme guess

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def convert_array_to_string(array):
+        return array.tostring()
