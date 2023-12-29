d24ca57b03acd8aea9e0e9845aae58d41a40ccec
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:53:56 +0000

    regret listen breakup weak post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
