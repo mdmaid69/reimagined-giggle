ebe408a756e2058b56e0f2aabf49b505c6d98125
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:25:28 +0000

    deliberate ourselves pretend quiet effort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_cosine(x):
+        return math.cos(x)
