aa2362bc515197cbdef79ee969cb1ba645cb4dbb
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:17:35 +0000

    better present speed art like

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
