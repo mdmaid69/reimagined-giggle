da67ae4f9b4a5631862a50c2e736ab0ff8546b3d
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:20:35 +0000

    turninto giveout country yours sweep

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
