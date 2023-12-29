ec7cf2a9d9bfffc9b85df20994c6e2f60f3b1686
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:06:10 +0000

    month pair slightly ride comment

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
