c0a0bf5f7a46d78e8fc04e4d036d30edfa86b1c6
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:49:19 +0000

    kill through blow wake early

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  def calculate_area_rectangle(l, w):
+        return l * w
