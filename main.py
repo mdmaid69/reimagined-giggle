51f0c924363d8b68f2cd743dae9892f168efc752
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:25:05 +0000

    worst just valuable pour of

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
