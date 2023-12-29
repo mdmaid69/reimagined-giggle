d4d9e50a04d0920b6d6b26f4d3eb8cce3a31b8a4
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:10:33 +0000

    present wet include drink interview

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
