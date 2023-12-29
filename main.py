fb99165b4624672e69f2b0bda6cbedf872f8ef24
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:08:34 +0000

    pattern familiar work support try

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
