4a553a484577906eb1bb501fc2de8cad3e5957e6
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:19:56 +0000

    shut surface computer picture except

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
