d12e8fa3e76c9b4ed6dfad7d40b1e5be43474734
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:17:14 +0000

    tune shine attract especially load

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
