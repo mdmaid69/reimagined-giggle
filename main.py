e51f6be29e77dd9949930eeb4249b42d3c6e96b9
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:42:14 +0000

    change about present wash official

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_itemsize(array):
+        return array.itemsize
