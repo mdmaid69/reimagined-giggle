cd6d5da293e9db011a23129645387f5957e507e7
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:12:15 +0000

    set commit degree pain afford

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
