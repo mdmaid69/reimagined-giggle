16061570a03a10d0d3ab1cf1ddee4f999be4724c
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:31:04 +0000

    patience only bad spite breakin

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+for i in range(10): print(i)
