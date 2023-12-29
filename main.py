fa8c07fc93e810c6b445a0e865194031b2f196ca
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:49:27 +0000

    stupid neither sudden weekend interesting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
