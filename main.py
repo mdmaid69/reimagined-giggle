43ba7689fd78edf52bd9bdd20c21d1b7cf740dd3
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:09:17 +0000

    walk mend mark he feel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
