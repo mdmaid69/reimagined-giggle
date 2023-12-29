d4ecaf42210009ab233bc44bbd923c41e2749874
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:03:50 +0000

    early man century simple giveback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
