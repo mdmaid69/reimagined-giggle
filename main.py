4037b81fd68415570d2c82417cdb13a9a0b1b8bc
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:00:31 +0000

    trip cheat wide artificial should

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
