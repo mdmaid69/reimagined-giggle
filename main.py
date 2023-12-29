3e365ce807d8f800e5c6181c07b997c7291fe9ba
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:21:32 +0000

    offer waste start slice spring

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-for i in range(10): print(i)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
