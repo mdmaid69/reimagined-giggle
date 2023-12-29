7d28f0b79f32351a7b4165dded4c15a7b1286926
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:47:38 +0000

    pair declaration degree custom careless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
