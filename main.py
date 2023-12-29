b5b3b483aafe25a30101d71c97f70f2a2d1cf52a
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:34:44 +0000

    home single lonely keep surface

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
