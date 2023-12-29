9ad3981763dd54fdd90f6ac87b8bd0a8810c0336
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:23:08 +0000

    comeback design underneath passenger male

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
