dc5a7feff84b553b6e97cd7aef72bb69e53c8b8d
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:37:54 +0000

    whole man dear pair keepoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def reverse_list(lst):
+        return lst[::-1]
