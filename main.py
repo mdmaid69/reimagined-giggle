61129a6d0d4409b2310ad6b3508e7b9473e57f4f
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:12:58 +0000

    something arrangement necessary cap impressed

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
