d841d3fd7937d2b97ae71c3098a7e72860b3d41c
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:30:46 +0000

    week professional ordinary fall 568energy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
