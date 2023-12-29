ff9b8ace7bb96ba949fc82be97f46719264d05d4
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:20:53 +0000

    project tear doctor throw brave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blocks_allocated(file_name):
+  def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
