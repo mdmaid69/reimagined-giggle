847689ee71114348ed7b9078fe7560e3f7f8cf59
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:23:11 +0000

    sit likely hollow please correct

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
