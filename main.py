328d4f6616ed7e4ad8bf2853d503ee8609600a80
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:40:49 +0000

    love correct boring quickly ignore

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+from collections import Counter
+print(Counter("hello world"))
+from collections import Counter
+print(Counter("hello world"))
