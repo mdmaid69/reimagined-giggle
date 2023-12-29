e4716bc9618a0c103110325caa23b61f367555cb
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:54:48 +0000

    perform ilovedicks count recommend phone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def convert_to_hex(n):
+        return hex(n)
