d70bb4bbb6dff908fdc0f0c5112491069f426d44
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:23:27 +0000

    completely 566end call anyone rush

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
