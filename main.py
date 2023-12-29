59f71719e8367307da4e3a0141cbff417de11094
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:16:56 +0000

    suckme dark better reach false

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
