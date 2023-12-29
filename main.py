8079678b5461c64a34f2a1e1ee80e71dfc64b02c
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:19:52 +0000

    all smoke disturb sticky various

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
