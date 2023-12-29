7bd732edb8869cc45ec962eb05ba6c3def6f4582
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:03:26 +0000

    date temperature butter turnaway pick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
