1bcd1b2c2dfbfd15bb17806e65fd33b8caed55a4
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:07:06 +0000

    Jj right out able mass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import array
+def get_array_length(array):
+        return len(array)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
