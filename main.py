045aa6d2c38d087e24b12a132be43a8070206c33
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:09:44 +0000

    edge holiday wherever pinch not

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
