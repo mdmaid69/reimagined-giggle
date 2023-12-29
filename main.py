633d9856c77b300069b4c710ecc27e7974caa816
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:12:49 +0000

    swallow remark interview smell comeup

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
