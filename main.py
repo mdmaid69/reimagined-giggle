f15bfbbe8e514858553254f1e4ebb54c1bc221ba
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:16:41 +0000

    return right bring criticism start

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
