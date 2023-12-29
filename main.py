aec1d4fc9aa7854c64e9a826435d3e6b90f4876f
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:31:24 +0000

    wait giveback estimate now table

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
