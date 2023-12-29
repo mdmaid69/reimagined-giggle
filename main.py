ce6d7f11c36283fc1973f64857d6fa5dbb333167
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:40:58 +0000

    comment their ignore admit ready

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def calculate_square_root(x):
+        return math.sqrt(x)
