c636514fa09dd0c0290a8f1e437fca82549b1537
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:29:57 +0000

    machine quick pencil television bag

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
