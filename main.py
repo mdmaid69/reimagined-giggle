9967fcf7a8a280b41d506bc71fb2b39bf3c5efd7
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:15:31 +0000

    club fresh possible show lookafter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
