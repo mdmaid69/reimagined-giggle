ed0d066163b6603d18305dc9a94c798baeb3026b
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:53:33 +0000

    pen information separate tidy injury

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
