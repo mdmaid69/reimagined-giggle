fb9e62edb51b5f4cee04edf8583123d155fea507
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:15:36 +0000

    tie study that everything prove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
