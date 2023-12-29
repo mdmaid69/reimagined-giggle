01ad218143ea356f8b739229c076e22a45fd99b3
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:05:40 +0000

    extremely calloff otherwise succeed before

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import array
+def get_array_as_int(array):
+        return int(array[0])
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
