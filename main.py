9ef22955c1991604dbd0f9e54c43485e6a1196c9
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:43:22 +0000

    life interview by fast theirs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
