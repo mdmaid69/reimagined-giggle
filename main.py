93aff5a690a873ba38cbe0d779485815ad68a6a2
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:03:50 +0000

    open holdup great Ll introduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
