feaada172078cfd51c615db032958bf911705be7
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:06:45 +0000

    hungry university activity obvious transport

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
