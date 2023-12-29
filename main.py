f119bef1368e5f54954a2d3d267506b2d024a693
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:27:37 +0000

    boat defend Ss pride unusual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  def cube_number(x):
-        return x**3
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
