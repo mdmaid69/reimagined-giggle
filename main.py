04fed48298c1b501d228c4c175ecbf4ace45e7b2
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:13:40 +0000

    secret realize yes account before

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def count_characters(sentence):
+        return len(sentence)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
