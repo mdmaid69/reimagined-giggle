704ae2fc6b3cbd6fa479ba54106a74b21df82efb
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:19:55 +0000

    characteristic soft clothes peace of

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
