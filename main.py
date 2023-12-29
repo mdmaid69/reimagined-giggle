818b000c1a485dde557ea55e2c8891643f4ddb3b
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:34:48 +0000

    near on exactly method enthusiastic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
