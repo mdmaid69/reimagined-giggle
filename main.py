b89cc741c24792ffe0f1494d33eba0ed7a704770
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:09:21 +0000

    frame wake practise modern couple

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+for i in range(5):
+        print(i)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
