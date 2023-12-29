9e33545f64e2c96380082128e91de9f23be20750
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:14:08 +0000

    contrast juice trap no label

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
 import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
