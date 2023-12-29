2a472afd15ced4e0d2c2d6096e54792557a61de4
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:08:55 +0000

    count punish copy port raise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
