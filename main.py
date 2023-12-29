f289a4a8fbd38e6c5722887f0320f44c0a9425f6
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:30:33 +0000

    convenient awake major gradually only

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
