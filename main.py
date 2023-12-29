109b3ac0e6bdefd70802cfd717c994b08790c4a7
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:58:33 +0000

    exit operate moral call raw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
