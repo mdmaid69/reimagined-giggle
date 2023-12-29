516471a0ef4e5425532dccb5afad182d8c840043
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:05:08 +0000

    mad slow extra pocket damp

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
