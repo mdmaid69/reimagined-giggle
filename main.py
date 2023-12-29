38a8a729b3b8ff642eb8c0d97d61eaafc30e3e20
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:14:24 +0000

    bet conversation meaning wave cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
