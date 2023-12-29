01bdd3d0d6a60734a35fddf8d96caad380cb4668
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:32:48 +0000

    generous left repeat face switch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def reverse_list(lst):
+        return lst[::-1]
