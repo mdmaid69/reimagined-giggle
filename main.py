775328f63b04230eef0030aec465eedf6a94243d
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:57:40 +0000

    air tired uniform accidental tone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def count_elements(lst):
-        return len(lst)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
