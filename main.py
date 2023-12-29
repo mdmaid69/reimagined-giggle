febcb92f8f6c5241fde1eaa8ab51918472cf3dad
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:56:45 +0000

    practice expression compare issue slowly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
