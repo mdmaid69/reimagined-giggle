bb095a359caecf1a094078c6f7d565fcad0e8c49
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:10:37 +0000

    wind high score declare attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
