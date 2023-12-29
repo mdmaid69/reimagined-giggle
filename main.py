bfde61d0a3734ec8ee189dc98090fb8a79efee5d
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:23:43 +0000

    secret mistake maybe drop 566end

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import time
-print(time.time())
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
