6e44851268f1b45e38af2e092aa4bbf71cc4216c
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:52:14 +0000

    complete happen listen steep combination

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
