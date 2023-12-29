5e523314d4aef9095aca3897f71e2f8754a57b33
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:21:43 +0000

    obvious example before Bb spare

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-name = "Python"
-print("Hello,", name)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def split_path(path):
+        return os.path.split(path)
