e8764e97c86a7f1d0517dc75220296ec77e51048
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:39:01 +0000

    zero unlikely length build summer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
