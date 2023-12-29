a0277ebb51ef49a1f4e39d8af189fb43788e8bc9
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:46:35 +0000

    rain Zz spare Ff imagine

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  def convert_to_binary(n):
+        return bin(n)
