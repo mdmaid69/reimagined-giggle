f5101cda8bb1a8d663d89c3df46d2a719d72864f
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:50:05 +0000

    joke sentence confusing every right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
