7712e5ab7f481e37d0c9d26414fe3403615e1805
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:54:55 +0000

    feed shop benefit by clothes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-for i in range(10): print(i)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import collections
+def create_queue():
+        return collections.deque()
