8e895c88bd06307f3377cbc19ba498c164d9496e
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:35:35 +0000

    excuse butter within belief emergency

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
