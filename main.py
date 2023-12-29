f4df254f0420177ead1e3dc5ccbd1594af36f889
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:08:17 +0000

    shock hate fire wrap noisy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
