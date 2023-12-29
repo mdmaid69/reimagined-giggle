4a6b8bb536964b946f391e3ba7f311ddb4c8334f
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:39:20 +0000

    game breakinto hurry bye patience

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  def square_number(x):
+        return x**2
