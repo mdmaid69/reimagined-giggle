15ba0e7e22e6a82fbcc920dc0a41fd56e1f638d9
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:59:21 +0000

    workupto closed port dick still

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def divide_numbers(x, y):
+        return x / y
