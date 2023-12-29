76f397791c1b16a68da8c137ad8dba53abcd5daf
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:36:25 +0000

    brother though whole relative he

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
