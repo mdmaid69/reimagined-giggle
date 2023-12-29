6b994c41149a6cc97a8ccc3bdbcb7ddd4733139f
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:44:51 +0000

    work dare lesson book responsibility

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
