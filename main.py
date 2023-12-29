28f60bde557a2390667aa6f7b896720c1fe9a6ea
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:18:56 +0000

    star explain slight walk nothing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
