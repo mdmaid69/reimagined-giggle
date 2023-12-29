6a75bad795e93c7f263448659e6b7085b90ee62f
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:27:51 +0000

    exam object visit clearly stiff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
