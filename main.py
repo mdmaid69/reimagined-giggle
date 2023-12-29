baf49fdcd6ddc453a1485aadfb590286d6cb7fd9
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:26:19 +0000

    bend base defeat shout town

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
