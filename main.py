22412a565be471ef66a5805df4d672b5ac15807d
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:09:19 +0000

    tonight grow extra tap collect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
