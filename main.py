aab4f42aee623c78698c0c51b83ae5eb1e022e34
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:16:30 +0000

    proud temperature only actually flat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def find_max(lst):
+        return max(lst)
