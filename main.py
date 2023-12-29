e1c38035cd08195d405f574fd3afdc4599581781
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:35:11 +0000

    thanks Mm from coffee war

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_work(force, distance):
-        return force * distance
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def get_array_as_int(array):
+        return int(array[0])
