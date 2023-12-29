969a161b5f0c4e2953a09f09f4505622d86110ae
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:26:42 +0000

    joke towards support circle sure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_pressure(force, area):
-        return force / area
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
