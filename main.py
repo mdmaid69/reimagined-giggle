213476cc96cff425196bf442c0686b29ff80baaf
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:58:10 +0000

    unable many indeed loose weak

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+i = 0
+while i < 5:
+        print(i)
+        i += 1
