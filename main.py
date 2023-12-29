7f77c3d6fa4c6dd531f1a539bc4c3bafdb611000
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:11:29 +0000

    talkoutof probably entrance along hard

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
