bf3cb7e2fb2c893f86bab8619ed948d5351c6054
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:43:16 +0000

    Jj brief replace odd computer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
