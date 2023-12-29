9d892d85fc610920d657f17ea32565b6a76cd4a1
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:25:18 +0000

    comeout giveback popular pride nerve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
