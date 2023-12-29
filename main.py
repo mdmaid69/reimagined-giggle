9cb7af8e662a67130e7933e65b62f120e5672a0b
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:07:26 +0000

    their stamp walk take describe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
