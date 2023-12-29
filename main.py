2c0762eed7337c18e7047d9925f209185c1dd759
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:43:30 +0000

    phone free wish stop seat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_counter():
+        return collections.Counter()
