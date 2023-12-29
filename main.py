acd6d421c22b5fd8fe21d92587d1c55cd257caa6
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:59:25 +0000

    holiday temper force part 566end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import time
+def get_time_since_epoch():
+        return time.time()
