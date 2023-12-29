284dabf8d02fb4adb723d35f00f05e41344cca92
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:35:45 +0000

    Jj twist heart together square

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
