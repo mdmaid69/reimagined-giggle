b29fbb8b2b20fbeac6374a039ab97d3b0ad6fa7e
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:07:15 +0000

    reply rescue account off remark

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
