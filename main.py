cf496afd177bec2e6e9f7cdee8f83ab647aa1cc5
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:28:06 +0000

    substance each ability trap market

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
