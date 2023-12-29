4522bfd900322f309d40a64d7491ebc9e2cdd1d5
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:27:35 +0000

    situation smell connected goon plan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
