a3b3a7d0c3ff8767b5f40bc7a58f3a8868f3d905
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:21:25 +0000

    beside pride group film official

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
 def calculate_density(mass, volume):
         return mass / volume
