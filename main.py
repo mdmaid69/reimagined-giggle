17697ee0f046c72ebe7576dd8721a35edcb95881
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:27:31 +0000

    shock keepoff chief practise gather

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-for i in range(10): print(i)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  def reverse_list(lst):
+        return lst[::-1]
