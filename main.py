4678abf82638e830f25cc3d8c473599422d721fd
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:06:06 +0000

    damage favourite freeze promise 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
