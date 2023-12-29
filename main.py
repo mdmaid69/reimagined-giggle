b3d52793d0b82a9fe018cc9dd47ea4a45a78c307
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:59:58 +0000

    doubt anyone smooth mass most

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_tangent(x):
+        return math.tan(x)
