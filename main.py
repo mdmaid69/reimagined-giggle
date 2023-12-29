9ce9bd6a7b8f0608a63f4057ec64cb1106a9144f
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:41:55 +0000

    private ride scheme check before

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
