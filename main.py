1e91fb163bae1da3fdbcbd6ada76501aa153cdd9
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:23:53 +0000

    expensive slice ticket introduction OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def cube_number(x):
-        return x**3
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
