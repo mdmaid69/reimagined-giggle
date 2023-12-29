3cd7ed52e9800e607335278dfa93c700fbc7eb15
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:41:58 +0000

    talk mean pleasant moon comein

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import sys
+print(sys.version)
