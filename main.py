8334a41d82f7713d62a0b61edac6ff3e404f7b72
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:50:34 +0000

    opposite better avoid contain shall

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
