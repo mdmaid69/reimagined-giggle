adaa559fb7b95c77eaf5b8c0f0fab094eb071634
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:16:11 +0000

    nigga rank path colour famous

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def convert_to_octal(n):
+        return oct(n)
