8f7004f7737f85473da6d7e9c88ee90eaa29a663
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:44:47 +0000

    possible Ff influence early confused

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
