4613b3fa76fa8997411ee669650b4a1dde34f7fd
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:58:36 +0000

    smart tonight heart butter human

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
