96b223c68bb21319fc705c03d500c85c3cae47ed
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:14:50 +0000

    television window each exciting exhibition

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_acceleration(speed, time):
-        return speed / time
+  def cube_number(x):
+        return x**3
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
