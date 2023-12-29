d72744a87a84cc8781001d5feff24dbaa0952203
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:51:58 +0000

    walk turndown takeaway no discuss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
