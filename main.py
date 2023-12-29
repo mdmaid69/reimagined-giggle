88bce52a398a487016675320ad36b08f031f0ee3
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:36:51 +0000

    nose party round enthusiastic ask

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def clear_array(array):
+        array *= 0
