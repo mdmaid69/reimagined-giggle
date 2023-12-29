f149be4463cd5f91ba99a08d7bf85387740ab437
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:14:22 +0000

    activity insist send become 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def convert_to_octal(n):
+        return oct(n)
+  def count_elements(lst):
+        return len(lst)
