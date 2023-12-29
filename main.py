1393f9c76aafd08bbcb1e3c43d6d75a9edfd6a2f
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:59:35 +0000

    identify precise to profit sleep

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import datetime
+print(datetime.datetime.now())
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_itemsize(array):
+        return array.itemsize
