1d3b92d20dbbe7b8e195a11910ed0bbe09c8f5e4
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:15:18 +0000

    bit against trick trust stick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
