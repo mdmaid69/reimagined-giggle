2f8c0d88c4a9da6c167f943b9e52ffc7be589765
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:51:12 +0000

    copy compete pleasure turnup suit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def remove_directory(path):
+        os.rmdir(path)
