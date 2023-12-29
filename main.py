a33310038090fa8f1ed31ae919cc2502f3218c46
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:09:38 +0000

    break sore angry push comment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
