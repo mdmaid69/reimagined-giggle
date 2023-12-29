144bab869275647b32086406a9885117e586e046
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:21:52 +0000

    makeinto fly right after minority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import logging
+def log_message(message):
+        logging.info(message)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
