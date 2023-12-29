4e58fb09a7bc9484af0af4d4b7b420fe4e39fe37
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:22:08 +0000

    part place government reject Zz

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import sys
+def exit_program():
+        sys.exit()
