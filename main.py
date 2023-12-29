31749be6a1c6c66bc48094a25b76b9d9044d4ed2
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:26:26 +0000

    intelligent remark professional leave clothes

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def is_even(n):
+        return n % 2 == 0
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
