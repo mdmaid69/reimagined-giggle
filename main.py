0743f4773149bd2506347588b16a3038259da549
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:38:07 +0000

    table whisper official wealth remark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+for i in range(5):
+        print(i)
