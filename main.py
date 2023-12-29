368b456cde2eb0929506ac469ba4c1be7015d5b6
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:58:04 +0000

    stay frame child cool real

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import re
+print(re.match("h.*o", "hello world"))
