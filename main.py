2da8987708a17d180c0493f93716feba1421d9cb
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:40:19 +0000

    holdon advise lose cheek dream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
