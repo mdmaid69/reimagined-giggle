806035d65ee1d41ee7daac082da38151f218b6fb
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:32:14 +0000

    cold position birthday deaf anxious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
