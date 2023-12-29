763d7c5bb22281a782393b6a9c89977cdf60984f
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:12:03 +0000

    relief serve some prefer stare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
