f4fc95a393ac6bc08b714a3c6ff50b4b1b381f2b
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:13:09 +0000

    bit round share odd window

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import logging
-def log_message(message):
-        logging.info(message)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import sys
+def exit_program():
+        sys.exit()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
