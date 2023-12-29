d6d6e9fb4cbbe2366673195a28784e82b0549cf1
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:08:58 +0000

    hide insist speak runaway ground

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_length(array):
+        return len(array)
+def calculate_power(work, time):
+        return work / time
