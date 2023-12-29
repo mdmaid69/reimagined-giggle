716fcbcf28e888e37d847d0a6ac0fbb850a96e4b
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:41:31 +0000

    exploration fun boat stamp scared

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def convert_to_hex(n):
+        return hex(n)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
