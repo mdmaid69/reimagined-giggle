1a54e04f949de09d7d787c3cfa1fb0c80e52d8f8
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:15:51 +0000

    physical convince worried awake bread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+  def is_odd(n):
+        return n % 2 != 0
