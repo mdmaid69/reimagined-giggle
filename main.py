460fed8db5ee5e0b492225a2b05359261bae7ffb
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:46:41 +0000

    talkback understand rain building aware

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
