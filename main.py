65b5a909fc5e34e4402187ecadc0e4b6f19a31eb
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:57:27 +0000

    rule Mm trick year giveout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
