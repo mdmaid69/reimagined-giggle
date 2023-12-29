e499dab12c16c46b181be4d243180ad7df3cd342
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:55:20 +0000

    advanced to length where lost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_user_string():
+        return collections.UserString()
