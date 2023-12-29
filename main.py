9fe79c3caf11f1efbd629e18a2f8ead625de0211
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:24:36 +0000

    shout rush suggest suffer available

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  def multiply_numbers(x, y):
+        return x * y
