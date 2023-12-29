f86c243abfd06a6926ce3a995515bb1a99d822e2
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:57:22 +0000

    sick certainly on container comein

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def clear_array(array):
-        array *= 0
+def get_array_as_list(array):
+        return list(array)
