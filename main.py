eccb6bcc058255412f6031ed1e18e722907d2882
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:42:57 +0000

    blow practice prepare sense contact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
