f9a63c16be40c417bf82d856341187f426bbc27a
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:53:44 +0000

    to 564encourage trip again tomorrow

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
