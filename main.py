6a1e0eea226dd66d62a83e38bce56f6fb545aaa5
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:29:00 +0000

    collection cry complain she forward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
