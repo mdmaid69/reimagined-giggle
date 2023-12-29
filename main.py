dc14b1f283000ff7881bccc0398a7d53c6287a3e
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:56:02 +0000

    steep interview imagination temper expect

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def convert_to_binary(n):
-        return bin(n)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
