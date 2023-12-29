14112da5f09a3a0a54d28a674cee0871e7cf4b4a
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:48:40 +0000

    worried top steal argue waste

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import sys
-def exit_program():
-        sys.exit()
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
