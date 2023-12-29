cbc486a46fa419843818608f5d0a4ff31a0dd8fd
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:02:56 +0000

    married spin food talkoutof ours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
