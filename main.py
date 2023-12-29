582a8d9d903c2cb813d3f0e3b2c30938ff711dfb
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:19:09 +0000

    embarrassed goodbye marry lunch geton

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
