633bcc0ca3e4ee36f4c903ec4646f0b03fadb258
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:00:36 +0000

    scheme sky exchange tooth entry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
