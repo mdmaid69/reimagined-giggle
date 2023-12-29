a012c438b3e530c08632512ce6ddc809ca44402e
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:13:57 +0000

    free right regard emphasize talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def get_array_slice(array, i, j):
+        return array[i:j]
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
