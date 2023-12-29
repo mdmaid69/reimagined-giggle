875acbcf1b1efc55d4f9f63a49b0b785298c1574
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:26:47 +0000

    advise succeed breakfast hear move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
