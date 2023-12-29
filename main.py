c0fa7763a8b5f55b8bf0890329569f6b7b8606af
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:24:00 +0000

    nowhere tap plenty poison mixture

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  def count_elements(lst):
-        return len(lst)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def subtract_numbers(x, y):
+        return x - y
