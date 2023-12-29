739135b0b11ed946b415bb503cabfe5b5d6e785f
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:19:02 +0000

    loan alive wave wash contain

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
