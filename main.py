0dffcc679742c6f34af184bf3af53b355fc2a567
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:36:03 +0000

    life disappear soon estimate close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def split_path(path):
+        return os.path.split(path)
+import tensorflow as tf
+print(tf.__version__)
