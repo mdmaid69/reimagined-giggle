ecdfc673612d47d1b94018569d2d77b26845bee7
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:17:25 +0000

    bang bridge post uncomfortable escape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_int(array):
+        return int(array[0])
