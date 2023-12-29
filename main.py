ca91f71e0a6902e95414d13401002f360fa2d539
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:13:36 +0000

    apply day course explanation high

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
