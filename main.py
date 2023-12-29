3f93056429d65576352b144c49ac7ea36ed6dc45
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:43:38 +0000

    fly might line platform trade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
