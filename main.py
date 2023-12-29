5f58e2e04286963c40c8ea70f88ce1e9591669ab
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:20:56 +0000

    and neither whisper run that

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
