692468d7fa7794ce892287fb2a7682e77db5aeee
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:50:09 +0000

    face neither form murder praise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
