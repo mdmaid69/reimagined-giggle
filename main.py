8d245bed6b623051b5f245cc151c036acfc354a5
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:40:57 +0000

    tight upwards thinkover and gate

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
