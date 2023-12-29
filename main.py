bf931b41320b88863bce01b3eab574421c81d3db
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:26:16 +0000

    pair one quick this thick

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
