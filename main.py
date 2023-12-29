6cf94c6af994218baabd723114fc2cbe8863f811
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:35:40 +0000

    on storm bang wipe discuss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
