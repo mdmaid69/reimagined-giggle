3f5844e01c8657c6ed09dbd47d63ae9467a14d25
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:59:28 +0000

    search culture contact surface crowd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_item_count(array, item):
+        return array.count(item)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
