476549067f031ec218924bc501e41d94aa0663d8
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:19:30 +0000

    ignore pocket substance expensive born

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+for i in range(5):
+        print(i)
