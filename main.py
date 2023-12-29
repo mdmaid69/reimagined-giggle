f4b4b6ec0ed81d53549bb1a8ea82f21a46c73680
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:07:22 +0000

    development sense increase ourselves book

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import sys
-print(sys.version)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_pressure(force, area):
+        return force / area
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
