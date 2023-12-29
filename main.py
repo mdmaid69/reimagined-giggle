b9e19ea013150ef7b6ba237ff52700be77be56e3
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:09:55 +0000

    side hurt cruel himself set

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-text = "Hello, world!"
-print("Characters:", len(text))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
