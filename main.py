ef63c938e0d8dc8256805b4f4399cae9daa37686
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:38:23 +0000

    about ear disgusting respect need

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
