ca1fb21e6a2f8a072d43f89e9cd370c21840551a
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:51:01 +0000

    software debt pick everyone dress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
