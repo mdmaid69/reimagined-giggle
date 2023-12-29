f2b16420516969d3a04bc7ff3b528be83e0a20a5
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:18:18 +0000

    exploration understand soon mention compete

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
