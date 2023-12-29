d751e385bf2d24865727d99b409b9adf5a4e8731
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:57:34 +0000

    stupid include nigga attempt smell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
