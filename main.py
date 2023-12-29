d6ccb81f5f5a3f1b1c51dc530f5663f8bf789325
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:44:50 +0000

    eye mix second open rob

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
