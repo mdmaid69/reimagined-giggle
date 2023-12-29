ce7493f26053f4f7d5820c7ef4c02a7f633f12c3
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:14:08 +0000

    strike buy memory persuade claim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
