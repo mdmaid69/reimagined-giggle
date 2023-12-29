42e7cae834223bab08210a7fb5d56bd1e608c605
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:50:22 +0000

    first both floor male damp

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def remove_duplicates(lst):
+        return list(set(lst))
