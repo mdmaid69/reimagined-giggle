5e3a743c7086a3130140c739f5b763a5f6c5452d
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:15:24 +0000

    left upstairs equal whom workon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
