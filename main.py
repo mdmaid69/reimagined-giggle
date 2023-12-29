00035fc88ca3d8ec19491ab861b773649031955c
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:03:17 +0000

    suffer ear project view otherwise

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
