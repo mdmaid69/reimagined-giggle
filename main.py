afd0e9390ea16be6067b1af0a007421acc5016e7
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:38:30 +0000

    tap cut it correct such

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import array
+def clear_array(array):
+        array *= 0
