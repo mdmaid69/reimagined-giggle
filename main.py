2cb75a860b1383e6c353c9a590316752bef3b13a
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:05:02 +0000

    previous pity rely scare beside

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def find_min(numbers):
+        return min(numbers)
 import math
-print(math.pi)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
