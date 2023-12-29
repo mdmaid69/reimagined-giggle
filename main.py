1c5fc6ae68a9f54483278694f8e3a23f0e6d0508
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:02:10 +0000

    change left invite luck perform

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import time
-print(time.time())
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
