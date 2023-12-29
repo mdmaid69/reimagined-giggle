3ccbaa902d66b2d80853313ed99e68ba1e3c57af
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:29:40 +0000

    promise calloff surprise peaceful explain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  def add_numbers(x, y):
-        return x + y
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
