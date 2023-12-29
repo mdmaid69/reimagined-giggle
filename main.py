e372fdda8b41a71454f050cf08e323981d904303
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:13:19 +0000

    drive difficulty edge minor charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
