871d4019a2bea26eadc12fa0b5b6ebf56bb06b4b
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:03:00 +0000

    then confidence light introduction stage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
