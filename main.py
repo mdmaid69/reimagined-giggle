6413b080012a54f28dadeed2f086007d22cb1d23
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:27:08 +0000

    down impressed comeback correct book

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import time
-def get_current_time():
-        return time.ctime()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import getpass
+def get_username():
+        return getpass.getuser()
