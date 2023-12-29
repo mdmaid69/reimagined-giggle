c75f6e58c999f2990735d428fff93514b9cf03bf
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:41:19 +0000

    and break cope country shout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import numpy as np
+print(np.array([1, 2, 3]))
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
