745178a884763ca948bc4cdd0ba1d2e4cf848ef3
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:20:26 +0000

    arrive roll offer nose human

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
