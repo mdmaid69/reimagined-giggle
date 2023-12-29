49450474276f8d89ff9c84aec3d162f857f0fcee
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:50:59 +0000

    row closed behave part absolutely

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def remove_duplicates(lst):
+        return list(set(lst))
