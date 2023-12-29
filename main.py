1afb3b619f7faf47e844dda6d2d99066880a8891
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:38:57 +0000

    cheque as previous rarely dress

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import random
+def roll_die():
+        return random.randint(1, 6)
