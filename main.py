e5858411983c6905c861fbe91b0409a469dac117
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:21:26 +0000

    patience fit table black sentence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
