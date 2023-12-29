5d3eb6327f06a8cca09e0619048b94978a249136
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:33:32 +0000

    bury insist close pick brief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import sys
-  def get_python_version():
-        return sys.version
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
