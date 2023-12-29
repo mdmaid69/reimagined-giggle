9a93d6c198d0d6125ae2fa421c06c8a35044c095
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:55:17 +0000

    term glass behind discover beyond

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
