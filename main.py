9f92f391e1e52fae96831077a762a0cba216dcca
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:42:43 +0000

    zero between escape patience team

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
