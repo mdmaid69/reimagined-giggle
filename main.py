a262a2b4ca386811b46603c7ab23258d6144aa77
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:03:08 +0000

    service related takeback little flavour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
