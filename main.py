4fc912bee497b2a589e634416e091b33c2ef45f7
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:10:28 +0000

    build rely dear put almost

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def square_number(x):
+        return x**2
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
