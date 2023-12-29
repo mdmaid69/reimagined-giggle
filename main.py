179ba0ae809f1999ff5c7cf026fc14f2fda1d1b0
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:59:44 +0000

    stage active walk extra mail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
