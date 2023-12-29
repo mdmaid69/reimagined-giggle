8a62f5a9a6bcd4676674f914be53a5121e593124
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:02:08 +0000

    necessary zero harm shape talk

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
 import array
-def get_array_from_bytes(bytes, typecode):
+def convert_bytes_to_array(bytes, typecode):
         a = array.array(typecode)
         a.frombytes(bytes)
         return a
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
