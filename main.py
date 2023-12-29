bb5572bb342e21f56475e23c3d1e5f455c70fc6c
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:50:01 +0000

    peaceful pencil have Ee empty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
