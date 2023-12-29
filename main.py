4c14d8f3036fd1650f37a95a4a03883abd093c73
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:30:05 +0000

    getback many programme only apparently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
