e200dfb266b6991de3e708bd6480252e587b8b8f
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:39:57 +0000

    stop major count wise confuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import array
-def get_array_length(array):
-        return len(array)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def reverse_string(s):
+        return s[::-1]
