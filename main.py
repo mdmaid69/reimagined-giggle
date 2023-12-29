f30fd81a2bfc7f86b2b223fc6f73cf8e21ad3cf1
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:04:41 +0000

    minute after you development like

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def add_numbers(a, b):
+        return a + b
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
