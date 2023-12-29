bffb45caaa7cd717ed7db3f835cfb406ef66c1b9
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:01:10 +0000

    join runaway fence throw justice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
