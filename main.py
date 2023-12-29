9fbfadb50da369a0e01e8a0bc6c155235de9c3fe
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:59:32 +0000

    prepare city should pour else

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_work(force, distance):
+        return force * distance
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
