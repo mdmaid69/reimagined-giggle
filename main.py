77595201b530fbe62888389a6dae72abdf044c76
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:31:59 +0000

    far furniture them gas flow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
