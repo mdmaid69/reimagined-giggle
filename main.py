7a5ea31383d65566d74a3b439737feae39f6e7aa
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:55:19 +0000

    book information man switch flow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
