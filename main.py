1cff1790dea8896c99f6fb53e52e769bae45b30c
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:39:59 +0000

    dry failure or well communicate

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
