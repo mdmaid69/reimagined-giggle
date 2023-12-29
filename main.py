69b6e48cfd4a8d4835caa7e9115df222687735aa
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:51:39 +0000

    soak luck mention at surprise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def reverse_string(s):
+        return s[::-1]
