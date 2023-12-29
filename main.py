d2610c101cb16a019fc928914c25f8107154c59f
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:27:52 +0000

    lower best turndown whom awake

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def count_elements(lst):
+        return len(lst)
+def is_odd(n):
+        return n % 2 != 0
