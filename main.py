e3a2ad2604db7ca39a1b8f7f69e60ff9c43cff11
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:36:18 +0000

    like content certainly hold display

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
