f852a4aea480264a78fd3a508f3771faeb205e53
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:07:31 +0000

    afterwards word somebody flow here

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def sort_list(lst):
-        return sorted(lst)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
