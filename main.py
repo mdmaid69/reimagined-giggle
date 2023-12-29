29d7435e9547f495ec647d404385b9a6cd4851ae
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:52:35 +0000

    letter charge tradition expect border

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def square_number(x):
+        return x**2
