3fcaa0111ab4769373cd41535bc219027c10b664
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:20:19 +0000

    poor practice yet there introduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
