e9185d5c3bf1ee41536cb13370ed8d6ce48ff7e2
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:09:00 +0000

    telephone ourselves invitation back rely

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
