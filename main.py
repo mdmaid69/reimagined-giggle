c750cbcd7628625993a75dd4e79cfcda2ed9d91b
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:48:50 +0000

    eye pull top case forward

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
