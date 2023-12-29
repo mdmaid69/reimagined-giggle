150cdbe16df2fb0de996efa9397a3f887891e6ac
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:27:45 +0000

    switch fair thinkthrough seem takeaway

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
