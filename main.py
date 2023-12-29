5cc835b5ff4626bb17108302e83dfeacc87c4f38
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:47:39 +0000

    question justice meal boring scrap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def count_elements(lst):
+        return len(lst)
