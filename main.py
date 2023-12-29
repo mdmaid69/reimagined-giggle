ad02154aaad0e0ae744716607412ed6e593a0c46
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:05:19 +0000

    best support fight bye many

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
