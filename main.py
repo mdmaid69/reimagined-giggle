e84a45e3162405b773076edba12b34e647e071fb
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:56:45 +0000

    shame pain official sing formal

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
