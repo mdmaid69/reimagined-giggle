b39c9cab8f06dfb4bd7a2db5bbffb6dd182b3195
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:33:12 +0000

    error neck disappear convinced oil

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import datetime
-def get_today_date():
-        return datetime.date.today()
-for i in range(10): print(i)
+print(datetime.datetime.now())
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
