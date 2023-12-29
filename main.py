41f9fa60425525477e208c4df29a80a76cd4f773
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:26:55 +0000

    evidence scene they spread means

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
