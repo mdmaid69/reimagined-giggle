a3589c59675d3b8d58024073f84a9da42a197e81
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:04:41 +0000

    storm refer still last Gg

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
