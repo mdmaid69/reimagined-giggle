ba0dcf29a752360ee2872b2ad557789d910d5464
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:32:21 +0000

    nice water everywhere steam makeinto

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-def calculate_power(work, time):
-        return work / time
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
