adc5d3052f82a673aee00136b35bdccffbef6b95
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:54:51 +0000

    repair plenty turnback demonstrate mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_floor(x):
+        return math.floor(x)
