632978b920d7f2bd4b941aae86ed07c521c9d385
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:55:45 +0000

    church never left reach past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sys
-def exit_program():
-        sys.exit()
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
