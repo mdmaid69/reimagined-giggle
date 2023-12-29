e83b05cbf6dad5b15fb00d42fef46664a734793d
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:01:16 +0000

    match holdup upset air hello

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
