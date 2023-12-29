a03d0c779147a427dce6e063168f9aec08629de6
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:54:04 +0000

    brief little anyone old pick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
