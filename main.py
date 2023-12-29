a592697ecee99539f65bb7608523cfbbaf651983
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:25:50 +0000

    accept sister careful stupid computer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
