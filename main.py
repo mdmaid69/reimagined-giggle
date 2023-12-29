ff5fda354d068b7c8764055cdb1f8b7f03d06f29
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:08:47 +0000

    regular arrive tall cruelty add

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def add_numbers(x, y):
-        return x + y
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
