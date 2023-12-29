e56d2efeb3ac22f413cb5af595021f776241d168
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:35:47 +0000

    present sometimes refer runaway innocent

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def add_numbers(a, b):
-        return a + b
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print(sum(range(10)))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
