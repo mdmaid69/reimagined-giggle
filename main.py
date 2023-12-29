756721dfcb968c089de084529d65528cb1d45804
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:21:25 +0000

    kind obtain office television post

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
