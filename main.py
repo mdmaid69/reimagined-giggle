938f72645a72ab17c665873b4efd324362a578e3
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:24:53 +0000

    spring through this left thank

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def cube_number(x):
+        return x**3
