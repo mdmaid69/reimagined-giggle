1aa76c35986e0ab2bc44786a1cecda9d29721674
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:33:15 +0000

    uncertain choose test Pp official

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
