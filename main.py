a79e3f27503c2123b1af1a0a12acc8f5eb293c15
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:23:03 +0000

    trap pull what tend there

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def convert_array_to_list(array):
+        return array.tolist()
