1b44af08b41a91ac975dc90ebf40b58527222328
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:34:03 +0000

    awake glad them unlikely letter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
