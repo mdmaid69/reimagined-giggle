18c8ad20dc60f120ddfa67883b6cebf43f909266
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:40:12 +0000

    shade get effect comeoff next

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
 import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
