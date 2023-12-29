645ca2b78845ae184240bc922ae66c6177166d6f
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:07:49 +0000

    confused lead distance bag smart

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
