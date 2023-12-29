99fe6b57e7e8cd7d799ed0e26e7e7f92a4ed108d
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:56:07 +0000

    thank tonight crisis sit think

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
