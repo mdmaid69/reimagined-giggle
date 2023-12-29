591665edbac5dd859f58b23308bc71cad49cdc62
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:29:53 +0000

    whistle takeaway custom open turnon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_current_directory():
+        return os.getcwd()
+  def remove_duplicates(lst):
+        return list(set(lst))
