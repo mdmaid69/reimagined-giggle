74f6a9d1571533815b84abf903aad82863af6493
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:06:58 +0000

    discuss or comeround practise tip

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
