f19f63cfaa3656408a89a48ed5c6a7dd1ebfa49c
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:57:16 +0000

    congratulations except hand funny impressive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import tensorflow as tf
+print(tf.__version__)
