4415aa33ff6fea3588862c0cab5fbff4d42f2043
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:40:04 +0000

    bottom but keepout congratulate kick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_base_name(path):
+        return os.path.basename(path)
