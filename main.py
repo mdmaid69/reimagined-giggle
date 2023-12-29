b4cb70b4cdf4032eec5ac1ff6fe9b0c7a99fd820
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:39:37 +0000

    calmly disappointed report error emphasis

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
