5b72670be59b7500bc992bc3294643792aa65747
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:05:00 +0000

    even sleep glad pollution either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import time
+def get_current_time():
+        return time.time()
