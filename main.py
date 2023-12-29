261a9ac214fc9c2331cc5f237e89ad3e96acd0d1
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:46:18 +0000

    weekend minor must near store

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_density(mass, volume):
-        return mass / volume
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
