3cc139d517072999298ec05fa61265d30dce2278
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:25:11 +0000

    height 566end demonstrate grow cover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_base_name(path):
+        return os.path.basename(path)
+import time
+def get_current_time():
+        return time.time()
