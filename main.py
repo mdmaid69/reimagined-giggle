96b32857fe99a32f35237c03d115ea626ae41c01
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:23:10 +0000

    disturbing tooth noise secret scheme

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
+  def subtract_numbers(x, y):
+        return x - y
 import array
-def get_array_from_file(filename, typecode):
+def get_array_from_string(string, typecode):
         a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        a.fromstring(string)
         return a
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
