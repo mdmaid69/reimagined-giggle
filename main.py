ec038dd406f2b846b56265d2e386f9252d5c34ca
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:57:18 +0000

    shop hole wealth region intelligent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
