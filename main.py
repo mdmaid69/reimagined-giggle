144520b121dc2ddc52fee07e7e32bc9a12d9e9fd
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:53:51 +0000

    quiet frequent unless completely sit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
