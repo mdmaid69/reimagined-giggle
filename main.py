25b0e98a49c682e3b9a9481677bb0918456050d7
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:16:23 +0000

    regret period narrow accept somehow

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
