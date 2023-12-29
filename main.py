59a23bc98cea6bb3208f6c5da3a98f74b15a85f4
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:50:03 +0000

    limit tomorrow loss feeling minute

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def square_number(x):
+        return x**2
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
