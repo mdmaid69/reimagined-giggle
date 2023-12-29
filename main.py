f42c79f5eea7af67defc214afe813e18142c84c8
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:33:36 +0000

    equal wide material block list

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
