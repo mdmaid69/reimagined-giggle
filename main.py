83f6de0211e367a4582e4f0a46b3b512d9d787d8
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:47:56 +0000

    entry shopping drop organization down

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
