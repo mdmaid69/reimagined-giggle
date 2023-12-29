fe6ee14c798c7a5cc25f85aca1071e566be7062f
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:55:57 +0000

    switch while ahead reject theirs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
