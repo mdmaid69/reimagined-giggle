a074c82554bd8484cd95b482ad2df6e3b88407ef
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:08:03 +0000

    whistle cruel holdout plan rob

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def calculate_area_rectangle(l, w):
+        return l * w
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
