e13ba79f41b79acaa56ed35352140a2ff1848798
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:07:59 +0000

    complete danger swing hope fair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
