8486983b33600ff9965b61280e81861f8613f9b4
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:59:35 +0000

    radio will plenty order start

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-print([x**2 for x in range(10)])
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
