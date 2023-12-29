23ddfc4b66450e86ddf28c57b07d88a4ce497c84
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:44:56 +0000

    history upwards post tear frequent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
