60e42b64783ff637085213a2c87d298bd5321403
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:06:03 +0000

    cause brush nurse demand alive

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import time
+print(time.time())
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
