9f60a07719b1dc0b5305d599e153d38ebf58092c
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:39:03 +0000

    call touch takeback recovery clearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
