c0b84233ea438394cfff66a51c595ac5317eba70
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:34:46 +0000

    brain fix nowhere work sleep

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sys
-  def get_python_version():
-        return sys.version
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_arc_tangent(x):
+        return math.atan(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
