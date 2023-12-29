682bb225147bc4f1984f056e50948044bfd98013
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:43:26 +0000

    advice remain radio frightened then

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
