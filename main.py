bf003489ba68a43d96c9bfd4c009e2256530caa4
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:44:35 +0000

    stop clean language neither sleep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_unique_words(sentence):
+        return set(sentence.split())
