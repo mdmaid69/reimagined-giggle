a9dac5796dae6ad076cc85f0188b843c8cff1694
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:54:07 +0000

    achievement sharp sting rent expect

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-for i in range(10): print(i)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def square_number(x):
+        return x**2
