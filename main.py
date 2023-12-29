4aa160d64e51a357e9f92e2f4b2eee0bdc02e709
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:29:10 +0000

    skin shopping though bad rich

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
