0f279265245fcee63962ad693551fe85f230ac6e
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:19:27 +0000

    free fit mistake breakinto it

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-x = 10
-y = 20
-print("Sum:", x + y)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
