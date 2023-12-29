39be5d3469efdd0d40e0077f503a34b6fbd66b71
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:52:14 +0000

    winter grateful breakdown stage takeover

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import time
-def get_current_time():
-        return time.time()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
