e709a2b0c9c9941e4414fb75f2cff65761dd771c
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:20:42 +0000

    happen trade education connect smell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
