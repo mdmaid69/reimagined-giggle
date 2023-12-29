60acc5daaaa33b37bbea9e64470cb77abb446625
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:46:49 +0000

    way her disturb increase control

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def convert_to_octal(n):
+        return oct(n)
