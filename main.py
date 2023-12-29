a93c540c8d2300d815379c09e72be21d778a4e9c
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:36:04 +0000

    store my even whatever wrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def get_array_as_bool(array):
+        return bool(array)
