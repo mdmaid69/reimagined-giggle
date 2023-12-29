51ca62923f34dc6d4807deaf5ba87cba847b3142
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:19:25 +0000

    pencil confusing takeout lock plenty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
