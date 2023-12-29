be72abdf6e72e52e8ddfdec579f032e24b1e6416
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:17:16 +0000

    pipe telephone occasion try shame

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def count_characters(sentence):
+        return len(sentence)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
