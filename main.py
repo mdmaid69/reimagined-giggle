45f938d7b150ebc7c6a96ab806d37b0f63b7613c
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:41:50 +0000

    disgusting prefer clear mainly against

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_density(mass, volume):
-        return mass / volume
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
