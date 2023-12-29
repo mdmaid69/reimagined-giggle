7a77925f0ac577d59486fe0b294d75982fffc4d4
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:38:53 +0000

    spin bunch family 571enough comment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def find_max(lst):
+        return max(lst)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
