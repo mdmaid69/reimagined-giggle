0dbe8bfa4eed2249af036f4583b6363198e1202f
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:08:15 +0000

    legal control turninto formal moment

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
