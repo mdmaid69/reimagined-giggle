d04661b744ab351b8ed2b66232e2a4ffe81e7986
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:51:01 +0000

    mend swim moon various apply

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def get_current_working_directory():
+        return os.getcwd()
