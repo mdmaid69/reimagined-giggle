74a1a7bc9c944a2b51ff828693647713775555ec
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:02:22 +0000

    wait responsible cut damp clever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
