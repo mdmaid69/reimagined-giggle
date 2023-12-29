0d787ae170eaf61dd4fe6e546224849ba7b67996
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:03:00 +0000

    contain top close bottom pencil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
