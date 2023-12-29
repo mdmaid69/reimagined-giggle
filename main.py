acc9c679169ec145e843a4b46f6cd475ff2168c7
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:54:14 +0000

    minimum tense weekend trouble increase

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
