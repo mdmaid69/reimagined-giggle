9851178c7b469746a6fa22aabd9969ee3ea2b072
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:52:38 +0000

    order increase large male extra

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def sort_list(lst):
+        return sorted(lst)
+  def reverse_list(lst):
+        return lst[::-1]
