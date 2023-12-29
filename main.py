6914bda1c7530a93098ecf04e345d386b8301747
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:21:59 +0000

    fill shout giveup content minimum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
