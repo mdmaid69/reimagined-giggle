d5c3d506c987cd23d1e3219f125ded0844d9a9f8
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:41:58 +0000

    hair office confuse everybody result

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 def calculate_profit_margin(revenue, cost):
         return (revenue - cost) / revenue
+  def convert_to_binary(n):
+        return bin(n)
