50aef23e6fa660fa2471ca47ea697247a9f29e65
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:37:34 +0000

    impression sky stir off grow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
