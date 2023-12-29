92bac871ef2a373cbf1940b9b09814f124c2fca4
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:42:17 +0000

    because bright comein perform instrument

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def convert_to_octal(n):
+        return oct(n)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
