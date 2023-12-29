54a719012e3a16d43a172c20e122cd53bb88b6d1
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:42:01 +0000

    comefrom meanwhile compare feed lost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def calculate_power(work, time):
-        return work / time
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
