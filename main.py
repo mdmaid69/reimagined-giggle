8667e4daab06cb8b41979e866325defc32ef7293
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:29:47 +0000

    pipe wipe except often remove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
