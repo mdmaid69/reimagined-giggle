cfe1c1d174abdea3b7c5ca1252764b2b1ccbee17
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:54:23 +0000

    both catch way excited getup

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
