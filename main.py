3b98e8d62073464839920c8282f18c2c67d651de
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:40:04 +0000

    bet gas fun otherwise Itself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
