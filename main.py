8888d4919cd557b3ebb8f92890ef607d5a73910e
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:08:12 +0000

    question glad deaf certain later

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
