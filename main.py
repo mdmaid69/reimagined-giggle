e0ec24aaf3817bffc1b85a3446f4d944416718a7
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:20:27 +0000

    business black study cough float

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
