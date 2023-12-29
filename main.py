428bb3519c82039ca318106961f4f8778c2561b6
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:34:25 +0000

    under simple confusing promise organization

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_work(force, distance):
+        return force * distance
+import json
+def convert_to_json(data):
+        return json.dumps(data)
