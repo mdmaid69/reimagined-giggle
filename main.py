4668c618e51a992de3b71ec943806382ef950767
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:16:10 +0000

    couple beginning have invitation news

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
