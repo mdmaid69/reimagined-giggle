f32353a07b6913d7bc45038fc9531c2b3af493e0
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:01:05 +0000

    soft foot squeeze closed certain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_pressure(force, area):
-        return force / area
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
