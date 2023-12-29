325d608eb0c2dfcd0a964ee45278cc7180de6ff6
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:04:15 +0000

    strength reject apply suit under

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-print([x**2 for x in range(10)])
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
