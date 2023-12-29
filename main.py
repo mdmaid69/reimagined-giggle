f5f05c099ede993aadee8ec1cb5db35d039c2efb
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:35:56 +0000

    funny well level brief record

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
