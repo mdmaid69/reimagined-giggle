3f6208e6ad5b564ef275ec60e1714296a8c9e3bb
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:07:42 +0000

    contrast flat emphasize choose pronounce

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import random
+print(random.randint(0, 100))
