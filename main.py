7211f954b59895c9254352f09be725737f51da2d
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:45:29 +0000

    store appear present uncertain tap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
