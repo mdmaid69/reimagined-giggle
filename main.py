b3c5c75605c077307c1ba56be6ea611cd7980862
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:06:16 +0000

    opposite raw shout continue rain

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
