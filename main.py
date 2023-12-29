61c029e19bd58a2910c9b5e3b6a3c7690d407cb5
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:34:09 +0000

    die fire hurry some traffic

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
