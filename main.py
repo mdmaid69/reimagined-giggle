450707dba3baa62946e8e26dcf11d8d44bafa674
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:50:47 +0000

    moral cost minute state Ff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-def find_max(numbers):
-        return max(numbers)
+import array
+def get_array_length(array):
+        return len(array)
+def remove_duplicates(lst):
+        return list(set(lst))
