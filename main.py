d5b5fc00b573e2916fdea43de8321f1a243bb946
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:05:11 +0000

    gooff live them object forever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
