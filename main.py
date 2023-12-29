05b3a33b69591f0f8d48ce7ead382830556f5853
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:16:49 +0000

    past square fire downstairs experiment

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def divide_numbers(x, y):
+        return x / y
+def find_max(lst):
+        return max(lst)
