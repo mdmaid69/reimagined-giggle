7d111a5503cc2f444889e55e4105d657566fa753
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:13:50 +0000

    tread whistle once contact forgive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
