2f7d8fb485c325851f75fa6f11fee4cf2bb7c852
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:39:47 +0000

    scream dear where fix complete

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_work(force, distance):
+        return force * distance
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
