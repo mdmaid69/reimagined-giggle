5257b2c3ef173380f5fd91dde747113ed2eee645
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:42:44 +0000

    experiment against problem angry reduce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def convert_to_hex(n):
+        return hex(n)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_as_bytearray(array):
+        return bytearray(array)
