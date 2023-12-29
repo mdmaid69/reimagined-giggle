47fdbb0069963ac9590007c4c412ddf669719854
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:09:36 +0000

    criticize pretty game breakup outside

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_power(work, time):
+        return work / time
