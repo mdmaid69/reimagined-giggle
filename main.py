5d3cb747d4ca624df2a110706ef918939aafc5b5
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:54:36 +0000

    under rest tragedy limit although

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_speed(distance, time):
+        return distance / time
