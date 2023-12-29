a06f7c1116bee74750c0ca22f5515da1ed555aae
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:28:21 +0000

    still marry pencil slip autumn

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
