edf5da290bc8dd5cc18031aa56b7954243399a15
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:38:39 +0000

    workon afford impatient common professional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
