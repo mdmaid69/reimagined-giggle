055ab568e83a11436a0b774e68e37d3327878ead
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:35:40 +0000

    backwards earn murder across save

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_speed(distance, time):
+        return distance / time
