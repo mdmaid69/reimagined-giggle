50dd9d3399b6addf815fc5f92d4f34cdfeb35898
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:13:32 +0000

    cheque model embarrassing suggest will

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import numpy as np
+print(np.array([1, 2, 3]))
