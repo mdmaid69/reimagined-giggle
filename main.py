4baf499d80b0f82fb31d18459291047417fa7603
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:20:30 +0000

    split offer welcome familiar port

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import time
+def get_current_time():
+        return time.time()
