45529589853507e7f94decc70b1df34c4b6edf20
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:02:18 +0000

    cross glad fact cut by

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def square_number(x):
+        return x**2
