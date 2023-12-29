38cd06b6e669376c9ddbaab2e2efe775ea2f745a
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:00:45 +0000

    though influence wrong squash factory

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_list(array):
+        return array.tolist()
