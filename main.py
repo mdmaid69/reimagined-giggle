24bdc64c14bfcb1de9aac4fb258165f6cc5f0c2d
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:09:30 +0000

    sign behaviour clue bus position

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def multiply_numbers(x, y):
+        return x * y
