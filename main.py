ad81b0968add5e903c06902d9e9d01da96c35aff
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:31:45 +0000

    mind peaceful skirt obtain hurry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
