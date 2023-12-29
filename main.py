152a8640fb2d6bcc85344abbcceea109f1ca01d3
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:00:12 +0000

    wealth upset union move recover

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
