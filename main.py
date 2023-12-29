9ed09626dbf2bc58c70d7dcd78fa5b7bff986a6e
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:21:46 +0000

    tear range sometimes not wear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_area(radius):
+        return 3.14 * radius * radius
