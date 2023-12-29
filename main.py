1a94139337f1afb0f0b6001721c68c6bfbf898b1
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:40:13 +0000

    top brave supply colour mind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
