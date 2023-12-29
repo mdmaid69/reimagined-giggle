091ae9a027fae2e7d3ef1ad34de70b61b8bf3e55
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:50:13 +0000

    identify compare spend natural search

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
