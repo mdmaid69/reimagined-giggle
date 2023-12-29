e647fbea9a58b6face63c37dafa6a346d34fffc3
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:18:29 +0000

    discuss chase official speech pronunciation

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
