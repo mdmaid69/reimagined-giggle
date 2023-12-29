3533e409d55de1bef6355d4adb919f1d807f57ad
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:04:55 +0000

    honest base tend slight price

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
