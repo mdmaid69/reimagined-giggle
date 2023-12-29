f74f875d7156ff524bfec5732d46a59fa936e67c
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:45:11 +0000

    strength more comedown stay sentence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def square_number(x):
+        return x**2
