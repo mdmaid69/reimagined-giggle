1e8b02d6d5a11c0b90f3a467999b3c1552f0a899
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:30:36 +0000

    hang favour spend risk usually

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("Hello, world!")
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def reverse_string(s):
+        return s[::-1]
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
