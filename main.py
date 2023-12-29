65f40a795f444dd08d7b912ce7ec0b3ea35d1f97
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:47:17 +0000

    especially holiday everything shout excited

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
