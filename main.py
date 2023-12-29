a1ee995d7b88d37c961418bc8953955c357787b6
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:08:26 +0000

    Gg pronunciation debt rare rarely

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
