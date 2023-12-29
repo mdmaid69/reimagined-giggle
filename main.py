687da8b909342484f29f1b68e2d3dce88dbdf231
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:52:58 +0000

    appear problem strange someone expect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
