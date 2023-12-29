8ac285f6472ed3accb00aca9e39cdd6376a49e75
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:19:23 +0000

    goaway grand appeal here shoulder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+def exit_program():
+        sys.exit()
