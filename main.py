75bb83bab2e963abc3056abe925f79cf7b840c4d
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:00:17 +0000

    responsibility modern breakout wild return

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
