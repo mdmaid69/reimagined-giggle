0225c148e8ce5331ced643f868f0bd545e111def
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:07:36 +0000

    listen other giveout arrangement mass

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def reverse_list(lst):
+        return lst[::-1]
