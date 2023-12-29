b5eeb4cfacc76cd20298f61d08a2b4a0de688399
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:07:06 +0000

    see matter act clever refer

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def sort_list(lst):
+        return sorted(lst)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
