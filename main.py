641eef88010db79551b4c47181223b8b06582496
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:18:59 +0000

    myself after plenty bound breakin

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
