99238b6be92fc47205cac441a23e7a021f9a2613
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:54:20 +0000

    something eager worth behind so

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_min(lst):
+        return min(lst)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
