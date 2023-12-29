edcc5f92509be972abb39dd714bb8f11528c01ab
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:42:42 +0000

    match prevent put pull join

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
