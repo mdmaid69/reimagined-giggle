7f7382054fae23c27e0ef55629701027adcd4475
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:00:39 +0000

    teach grateful container town stare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
