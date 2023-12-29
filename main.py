bf89a0ea0ce33655df70c61deca22270eff5afe3
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:59:13 +0000

    small cost advise invitation ago

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def cube_number(x):
-        return x**3
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def calculate_power(work, time):
+        return work / time
