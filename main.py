1731357cec7f89e56dd47ac7001f725322ddad9b
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:04:37 +0000

    favourite fast feed slice chase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def count_words(sentence):
+        return len(sentence.split())
