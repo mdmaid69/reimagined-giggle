033c574e2b399f16987848cddcf74c00ea4cd866
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:50:31 +0000

    clear neither sound world below

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
