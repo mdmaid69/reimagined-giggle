1553d2547e0f6ebe494af73169d9f113b575d4d9
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:21:11 +0000

    name tomorrow fresh immediate anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
