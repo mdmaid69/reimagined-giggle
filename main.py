94c36ba972204c6bfb27f4e7fa282c51010170b2
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:07:53 +0000

    label various home mysterious away

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import sys
-print(sys.version)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+for i in range(10): print(i)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
