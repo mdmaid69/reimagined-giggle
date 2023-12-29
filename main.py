e562496f5e7eea6566db504ceb7c6b9010e03801
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:33:41 +0000

    scrap answer fear nose reply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
