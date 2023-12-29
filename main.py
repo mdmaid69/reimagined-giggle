dd8149d87044a7dd2b6c26a9e0e49f6c0dd7908f
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:23:17 +0000

    Gg obvious mark chance round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
