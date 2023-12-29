c82cc397318b76631c094ab3d59586e342f7498c
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:06:29 +0000

    holiday command bear snow experiment

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import random
+print(random.randint(0, 100))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
