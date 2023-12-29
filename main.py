e95197ae4e8484202e043b9901daf740380ad9fa
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:34:47 +0000

    tap Mm responsibility telephone difference

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
