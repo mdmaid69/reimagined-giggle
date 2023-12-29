62642bda6bf39fe6d9bbc40b66173372aa2c7477
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:00:56 +0000

    content favourite theirs print strength

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_gamma_function(x):
+        return math.gamma(x)
