844d414a04a1d16318d83d4d1173db49cecee35d
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:32:38 +0000

    distance straight study eager wealth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
