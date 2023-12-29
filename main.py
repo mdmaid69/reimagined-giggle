9e08feef3df67b429cdd81b3623d1f52d3647169
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:55:38 +0000

    cost week authority however handle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def convert_to_binary(n):
+        return bin(n)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
