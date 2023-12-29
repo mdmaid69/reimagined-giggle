1bbc286ea88f674d397224bfe3515e69f1693d9d
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:30:48 +0000

    actually kill settle yours themselves

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
