0e11a83fb3ca797222375c28b235d584605c9a92
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:09:00 +0000

    getoff account passage mine risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
