2e75abcaec1a32043c7425886f1b42f8046e3c0d
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:46:21 +0000

    giveup better cover winter picture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
