d0ae815300fa481e8cb9cdf27d4e3946b13ebdf4
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:18:52 +0000

    notice unit care serious Ww

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
