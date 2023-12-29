630ec1d59b567809df92f80851921b6b7d1a355a
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:29:52 +0000

    pair view tough previous give

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import collections
+def create_priority_queue():
+        return collections.deque()
+def find_unique_words(sentence):
+        return set(sentence.split())
