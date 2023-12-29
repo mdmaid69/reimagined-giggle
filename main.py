316a41972bc471e480854c1e44bd98dbe63032f3
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:01:43 +0000

    shopping like middle war interesting

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
