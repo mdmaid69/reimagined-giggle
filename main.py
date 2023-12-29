538eb814fe288b17b406e68be436cccf508469a7
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:08:56 +0000

    eager trap business valuable once

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
