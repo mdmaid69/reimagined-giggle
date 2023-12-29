41ee914ad317b97d77cb51931d5febf25949b6bf
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:58:35 +0000

    improve people bring Ss spare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
