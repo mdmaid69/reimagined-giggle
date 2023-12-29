834fc83f32785b2f7b3f08d18ba9c47daadc254f
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:37:08 +0000

    comeback hardly wing job divide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
