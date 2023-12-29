d80cafe8ed626bccc160e42498fb700fa38f2a8b
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:49:40 +0000

    buy what could succeed rescue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def calculate_speed(distance, time):
-        return distance / time
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
