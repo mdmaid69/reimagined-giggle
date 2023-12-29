dd242bb39141e078cdc90b02e5af2190b96678c1
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:57:04 +0000

    middle opposite state lend usually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_counter():
+        return collections.Counter()
