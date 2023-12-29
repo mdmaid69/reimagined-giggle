f9e30ac784b81840598fcf161913539f81120d64
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:08:10 +0000

    meet getback definite feed discover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def convert_to_hex(n):
+        return hex(n)
