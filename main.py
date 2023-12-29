005d5857d61916c4b857c72bb714d3551ae50e90
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:33:58 +0000

    ear Bb available human exhibition

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
