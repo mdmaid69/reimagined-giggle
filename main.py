033c92442d39f979a85ac61e2780de18c8c16761
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:46:32 +0000

    loan consult undo write repeat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def add_numbers(x, y):
+        return x + y
