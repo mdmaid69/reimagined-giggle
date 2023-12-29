df9f30b4a5b4503647e6310506b8358ebbc8b18c
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:26:19 +0000

    rush meal foreign in factory

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-print("Hello, world!")
+  def subtract_numbers(x, y):
+        return x - y
+import array
+def get_list_from_array(array):
+        return array.tolist()
