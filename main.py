b57a0c2cbeba37e933824ceb71bce931d3b2895d
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:30:45 +0000

    damp except everywhere trap error

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def square_number(x):
-        return x**2
+def find_max(lst):
+        return max(lst)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
