9056a5dd5a2fb2e561895f310271b674762840e2
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:03:05 +0000

    radio well mark popular dry

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
