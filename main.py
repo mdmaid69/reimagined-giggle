3b801d6db3a9d61ac114b9f06c0cfee12797a66d
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:16:13 +0000

    balance present identify Rr nothing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
