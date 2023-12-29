066cc6a8000749c02da8f87f547c87d7d22b20a9
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:21:13 +0000

    vanish future scene study destroy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
