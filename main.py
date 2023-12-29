3132fcef0682b9a20ebd1f17c9005a2a2a8d112a
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:22:15 +0000

    length young accommodation godown further

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
