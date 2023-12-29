0a516477fc33b50e14e1df3a2d385040f171e295
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:20:25 +0000

    gather art name mark disappear

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
