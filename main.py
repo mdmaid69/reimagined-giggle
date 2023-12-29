245e87d960ccda9ea85fac9f1db779aefb3e028e
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:39:58 +0000

    Hh land say exist brain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_typecode(array):
-        return array.typecode
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
