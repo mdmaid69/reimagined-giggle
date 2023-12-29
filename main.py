91a88ce59c43319756c626d96f241274f2deffbc
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:30:51 +0000

    centimetre earn worse hand visit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
