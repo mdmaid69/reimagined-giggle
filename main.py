bd483768d1a37e62c4adf46436fc0522a7dac44a
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:25:23 +0000

    responsible age tidy forward health

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import collections
+def create_user_list():
+        return collections.UserList()
