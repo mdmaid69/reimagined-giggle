e8cf543399abc75466317e88f9b986c24509b0a6
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:21:41 +0000

    rule accommodation Bb moment apply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
