24181c35d36d26382fa80eb06ef76c7aa1f5bdb7
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:26:04 +0000

    neck importance remember rank confuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
