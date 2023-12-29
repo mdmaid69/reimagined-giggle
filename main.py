139d622b3138948cfbd44cf8656abd38a271f0f7
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:47:50 +0000

    conduct keep manner pause since

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def find_min(lst):
+        return min(lst)
