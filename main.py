07de179401726dd9edbe7990b9fad75e39ba75c3
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:04:12 +0000

    benefit fresh later herself collect

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
