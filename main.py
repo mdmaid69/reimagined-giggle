09c8cdb5db5fd30d3755e0431a799624c32db4ff
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:08:26 +0000

    afford getup give message my

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
