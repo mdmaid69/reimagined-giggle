5e4d2e942c286131fb13b1bd8fdba2721c2f200f
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:27:11 +0000

    raw ocean positive little anything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def get_current_working_directory():
+        return os.getcwd()
