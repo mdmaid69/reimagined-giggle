cf815f46f7a91ca2f08910ce2e50c89c259a61c2
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:10:10 +0000

    warm memory tread Hh plenty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_force(mass, acceleration):
+        return mass * acceleration
