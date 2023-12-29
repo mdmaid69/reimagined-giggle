35d64eb1792cd3d6f2f263378298590dda0c8f57
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:36:10 +0000

    row ago practise boring like

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import getpass
+def get_username():
+        return getpass.getuser()
