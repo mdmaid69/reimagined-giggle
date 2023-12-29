b0fc1e847331cc0efe043081328ebff347fa77fc
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:15:58 +0000

    precise field left load squash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
