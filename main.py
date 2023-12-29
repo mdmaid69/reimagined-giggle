0524dedb00e6448e37ae8ba2db2b90a457d849f9
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:07:41 +0000

    provide minor influence influence combine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def get_array_index(array, item):
+        return array.index(item)
