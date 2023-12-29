56e8e60e27d718e67c2254fafa66f10673e9c9ce
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:25:17 +0000

    same peace Oo suggest remove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
