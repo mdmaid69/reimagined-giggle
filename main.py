02baae3c5ae8b8931a5a32e52bcae552f99bc748
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:02:45 +0000

    move along touch confuse tie

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
