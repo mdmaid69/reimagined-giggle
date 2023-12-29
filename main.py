62fbb55a12d8ee00c3d9ab733c4e569c53cf361f
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:39:27 +0000

    town warm right tear way

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  def convert_to_binary(n):
+        return bin(n)
