e88eaddd31222af302206918a184c80d9d1cfdd1
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:08:01 +0000

    appeal pump clue scared than

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
