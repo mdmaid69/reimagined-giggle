8ef3f4eea1c13e87fa410cc2b3d465683405c1d6
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:34:37 +0000

    body shelter regard circumstance turnon

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
