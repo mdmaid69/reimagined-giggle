864f5ca002fe246b34eff916185e21db1a9a1e41
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:45:53 +0000

    principle today necessary unfortunate remain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import time
-print(time.time())
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
