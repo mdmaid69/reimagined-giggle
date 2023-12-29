4fbdc0dff8209fb517305742f55f1280bf14196c
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:20:15 +0000

    pen mad trip brother spare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def cube_number(x):
-        return x**3
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_acceleration(speed, time):
+        return speed / time
