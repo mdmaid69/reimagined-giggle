487e07ac6c6fb0c4c2e493552c66766cef721d31
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:49:06 +0000

    often nonsense complain finally father

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
