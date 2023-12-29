a8c6397e1930a05c8a6951c25abeab521029198d
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:54:15 +0000

    opposite switch start range suggest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
