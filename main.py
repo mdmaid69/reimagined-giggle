775a4457178e06448f77fbfa18376cbc94979638
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:02:05 +0000

    heavy weigh see factory test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
