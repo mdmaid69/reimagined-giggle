1d24e9e86c163582a9f1f481de03c9e047ac0f86
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:52:20 +0000

    hang Ff downstairs imagination find

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
