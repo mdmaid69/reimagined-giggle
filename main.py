7eb26127a921504dba428fa24cce18d21bb1c863
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:28:11 +0000

    instruction remove choice move once

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
