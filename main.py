e6ab1090b0b660dabe2e65fec128fc5a39910b79
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:46:55 +0000

    responsible instant meaning harm Ee

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
