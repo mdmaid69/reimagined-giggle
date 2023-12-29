e0b9d795d3bc1c03675f5b537852eb6e8eb6a1eb
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:38:34 +0000

    left getout born water breath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import collections
-def create_user_list():
-        return collections.UserList()
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_acceleration(speed, time):
+        return speed / time
