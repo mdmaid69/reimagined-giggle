2052dcb29ddbe5b93c71aa20f606b2deb6363ec9
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:46:00 +0000

    force hardly tune job side

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
