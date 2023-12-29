debb4976c79cb980d15fc5279122a9c9985eb24a
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:23:11 +0000

    feature light appearance father bad

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import sys
+  def get_python_version():
+        return sys.version
