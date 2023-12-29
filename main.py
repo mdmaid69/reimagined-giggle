ffcca5cf66d6020405aff80835d9a3efd93d1b15
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:55:20 +0000

    567enemy road imagine excellent destroy

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+print(sum(range(10)))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
