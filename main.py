cfa90d195279547c138204e24f8bd362b59dd4f4
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:23:11 +0000

    natural him genuine search passenger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
