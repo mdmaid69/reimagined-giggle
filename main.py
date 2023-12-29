9a59c43d8cf046d08fc2105d8380361be683d4e4
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:02:58 +0000

    scrap noone original subject callback

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
