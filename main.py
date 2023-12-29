5825349d14944a2a2027ab8e51b4e79b4c5fb330
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:40:37 +0000

    visit this represent straight amount

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def delete_file(file_name):
+        os.remove(file_name)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
