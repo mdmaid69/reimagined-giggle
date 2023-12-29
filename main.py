11fe9a731e835c882d99893ab0c6b16d2a838d72
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:29:54 +0000

    exam load issue pressure set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
