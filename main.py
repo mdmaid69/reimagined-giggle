94603c4f812c8f706915e10625462fe81455850f
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:57:53 +0000

    bother mail short corner detail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def add_numbers(a, b):
+        return a + b
