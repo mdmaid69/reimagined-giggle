02bc6663c7536092d4e4c163607535123246b5a5
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:59:38 +0000

    pair tip suffer bored proof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_block_size(file_name):
+  def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
+def is_odd(n):
+        return n % 2 != 0
