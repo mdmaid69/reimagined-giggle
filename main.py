adcb3e6bfc59f8f4ae453625f6124ea6b97784fa
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:09:04 +0000

    calloff declaration brush less lesson

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
