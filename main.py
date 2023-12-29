b9ce64d32e13eeeafa42baa5712b5565d057b1e8
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:00:01 +0000

    flood thing wish frequent centimetre

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-for i in range(5):
-        print(i)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
