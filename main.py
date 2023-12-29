4ba9fbcf998a02fbe226d88a39a1c8f34280748a
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:34:13 +0000

    unfair suffer among shock settle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
