4fb190ab4508fb42b40161d4f882219844cc34a9
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:17:25 +0000

    admit meeting personal I absolutely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
