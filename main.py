8ce5eaff1d091a8a87630ad6ceb3ae246a58b552
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:41:59 +0000

    certain half pain clearly hardly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
