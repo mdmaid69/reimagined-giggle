97b4cc17a5de665e75766faf40acbf78cf2a76c4
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:06:39 +0000

    extra responsibility spring boat recognize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
