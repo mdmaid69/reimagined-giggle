a166649ae9103397b768eb239c9a0f32dfe45177
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:08:15 +0000

    do concentrate food protect mix

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_as_list(array):
+        return list(array)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
