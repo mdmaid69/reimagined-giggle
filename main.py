926052a3a486fe50e3a33cf8ea776bed509bf670
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:45:46 +0000

    skill feature replace education together

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
