790942f7c65df752dbb2b061982ba4850be8f127
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:01:43 +0000

    pretty occur birthday show interesting

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def cube_number(x):
-        return x**3
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
