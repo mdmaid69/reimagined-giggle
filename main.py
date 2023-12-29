520c54fc9738c834a3e5a538d5f6a82d5bfbf416
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:07:40 +0000

    store song load plenty that

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+print("Hello, world!")
+  def calculate_area_rectangle(l, w):
+        return l * w
