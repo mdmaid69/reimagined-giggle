88864bf51ba38c9ab3a3b756082ee3006f9962e7
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:50:30 +0000

    always takeback high reply before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
