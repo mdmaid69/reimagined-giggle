efc9f899f7ddffbcbfd86fced1b24d933676270c
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:32:55 +0000

    mix branch goodbye organization ring

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
