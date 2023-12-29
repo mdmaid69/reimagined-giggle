7cf6d7ecf11b671fa3212771c35009c7f988688c
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:30:07 +0000

    field somehow best tie hollow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
