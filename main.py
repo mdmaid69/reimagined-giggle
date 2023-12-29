dbed6e1e47d7a5019fb78cb9b5668fd9ce2eedaf
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:32:43 +0000

    activity fill upset battery feeling

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def get_list_from_array(array):
+def convert_array_to_list(array):
         return array.tolist()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
