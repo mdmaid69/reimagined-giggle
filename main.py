c7c882fe156d35d113a3f9f09d650b35452dc4d5
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:48:40 +0000

    possibility quick never oil pleasant

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
