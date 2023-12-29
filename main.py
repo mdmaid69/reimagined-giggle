8372c68ff3a60628b39bc9734395304b40624334
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:49:31 +0000

    sweep combination stiff smoke pinch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
