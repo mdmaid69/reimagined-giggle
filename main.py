e9b8181445cf1b56f6469a946de387ba95e0d81a
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:39:34 +0000

    count bye content 568energy behaviour

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_power(work, time):
+        return work / time
