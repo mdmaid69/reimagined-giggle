5a6e7a2f5c4dec22012e26e4a28f3a3db07072c8
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:24:45 +0000

    569enjoy cap gas brave conduct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
