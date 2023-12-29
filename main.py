c348717f0c049888bc3b06ae1ca5cf3c16fbeb8e
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:51:55 +0000

    paper funny recovery them disappointed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def find_unique_words(sentence):
+        return set(sentence.split())
