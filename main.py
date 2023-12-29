d984d9c195c3b603e3ae8f050a042bbd8f4907f6
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:24:33 +0000

    hit upset spite challenge intelligent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
