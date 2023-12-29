22a40f7fcb339fd820ade9417487dc08e457ded9
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:12:00 +0000

    same wide wheel contrast improve

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def find_max(numbers):
+        return max(numbers)
