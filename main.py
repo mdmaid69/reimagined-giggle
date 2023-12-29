8ba6d4bca99c32a706de57701711186296f16e26
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:51:14 +0000

    send view funny nervous pin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
