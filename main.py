303bec41828f266807fcff39e7139abd10489a14
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:03:41 +0000

    back fond inform sometimes inside

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
