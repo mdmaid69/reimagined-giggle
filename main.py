47de5146f0efbb07595e013de9c52c80e557dbf3
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:45:36 +0000

    colour sure aim mark beautiful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
