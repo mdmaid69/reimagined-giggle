738ea4e6d4f442a7067e6e32ad51a32309288073
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:15:45 +0000

    deliberate ashamed spin force yourself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+text = "Hello, world!"
+print("Uppercase:", text.upper())
