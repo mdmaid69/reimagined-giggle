64c484cd3caf4487f25cb015d2457994d4377537
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:53:13 +0000

    easy wish fit use keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
 import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
