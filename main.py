6ca90795917a5748970c34b8dc5879d8522b7304
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:17:55 +0000

    year some allow awake goout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
