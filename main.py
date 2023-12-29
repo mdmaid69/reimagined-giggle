9c125d4e60dc139dee74f863f6206012bf4f9ffe
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:09:42 +0000

    basic dish fortunate roof shock

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
