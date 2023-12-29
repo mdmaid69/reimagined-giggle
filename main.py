931b44039dc3a02cf33756deada43e622826e187
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:10:20 +0000

    frightened detail complain mind interested

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def find_max(lst):
+        return max(lst)
