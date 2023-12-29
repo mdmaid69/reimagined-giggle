a5b5ff3bb71859c7feda5fde21afdec5983a2453
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:49:03 +0000

    away otherwise prepare positive different

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
