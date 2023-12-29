dcd19c582d566b1eaef8bb56024899b8af35ffc8
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:43:16 +0000

    market anyone argue personal command

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import time
+print(time.time())
