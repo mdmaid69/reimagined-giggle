abd953d655edb9b14a0daab70290a21c121c1ba8
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:34:23 +0000

    over determined spring work work

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
