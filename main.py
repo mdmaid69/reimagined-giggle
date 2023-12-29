2c8f2a5441ce0b1bc09132fb26ebb54b55412f52
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:39:43 +0000

    number distant maybe production provide

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
