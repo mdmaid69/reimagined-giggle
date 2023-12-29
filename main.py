94d6d6a76f5f397694072a9abc87b1f40c4022ac
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:45:08 +0000

    565end float variety left how

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_current_directory():
+        return os.getcwd()
+def find_min(lst):
+        return min(lst)
