3b73c717bb4b5373586d6e00d41ae32c3577370c
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:15:25 +0000

    chin table real study pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
 def calculate_tangent(x):
         return math.tan(x)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
