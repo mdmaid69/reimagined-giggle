d37a3d935c293b0ca7a4d75fb750ea5d98eacc30
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:22:45 +0000

    be precise bath ticket comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
