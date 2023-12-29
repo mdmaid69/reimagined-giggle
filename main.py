c197ae9641b55f55f92e36f311d2cfc626f40f36
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:13:38 +0000

    ring dead crowded receive discovery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
