2517f5856ef2c3346c582cabb1750245fbed7870
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:57:23 +0000

    opposite whisper 568energy compete lookup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
