bf1c0b57437d519d9e7239764801b92c745a54c9
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:10:08 +0000

    surface spin warn healthy mail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
