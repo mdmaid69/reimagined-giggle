abd0339261331fff95ecdf4e187198a87d9d0c40
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:16:16 +0000

    brother will breakdown measure borrow

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def calculate_speed(distance, time):
-        return distance / time
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
