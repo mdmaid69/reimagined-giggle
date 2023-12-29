527ecc9807731a4eada9d5194338bf37a4e338e2
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:09:43 +0000

    pain motion experience destroy quantity

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def convert_to_binary(n):
+        return bin(n)
+for i in range(5):
+        print(i)
