954873588aff91e61ef55a9d0f1b163316a16c7a
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:16:23 +0000

    deep female tour Ss announce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
