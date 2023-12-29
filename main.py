91986d7ea998570df57448ddbf09acea216a70b0
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:19:52 +0000

    operation private but heart ability

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-def is_palindrome(s):
-        return s == s[::-1]
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
