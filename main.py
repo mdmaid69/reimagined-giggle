410fd8f0b4c25bfafe19febcd01c7d29cd8c0a07
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:05:47 +0000

    love confident compete shrink present

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
