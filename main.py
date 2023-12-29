1d401f5caece6315602befa383bebf728eb223c5
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:47:47 +0000

    section mystery naturally while moon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
