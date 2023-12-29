1b26ca1c09f2eea05715eaedc022b43f5507f5e8
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:50:05 +0000

    sleep spend you offer pipe

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_work(force, distance):
+        return force * distance
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
