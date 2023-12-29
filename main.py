474a534d34cf23055d1555071deba8a90658b38d
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:44:05 +0000

    after quarter soft spring mad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_array_index(array, item):
+        return array.index(item)
