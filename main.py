af11b8321ed374031f1b0435b570369b2ec0d59f
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:52:32 +0000

    hardly journey important inside file

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
