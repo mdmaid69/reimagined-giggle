3b6d2b11ec707bb58170bab1cd4c5105431f4ea2
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:21:08 +0000

    careless criticism share child short

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 def pretty_print_json(data):
         return json.dumps(data, indent=4)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_set(array):
+        return set(array)
