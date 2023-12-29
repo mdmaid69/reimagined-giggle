2ff475610d0d3db8421b0184f4b024cc4f11d970
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:47:52 +0000

    back bet succeed powerful possession

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
