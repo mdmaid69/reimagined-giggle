87aff263ac348ad24af764e6b79a4c648e6c2c87
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:01:45 +0000

    punishment warm child flight sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def get_array_as_set(array):
+        return set(array)
