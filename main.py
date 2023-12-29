cbeba8dbced80d95b54534d3ec68d28c2ae9bdb5
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:22:33 +0000

    dear outside they sensible block

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
