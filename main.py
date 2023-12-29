fc1a214957891e082cc88d2d14c4c9738357a351
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:50:00 +0000

    this truth belong wave thorough

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  def is_even(n):
+        return n % 2 == 0
