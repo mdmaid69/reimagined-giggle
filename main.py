117402611ce9b57fe7a4b797081c1ec1327c64a9
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:01:27 +0000

    area compete child accidentally we

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
