04d5907849e55675b8ad110e88dc083538339490
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:07:09 +0000

    sad present shape disagree sail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
