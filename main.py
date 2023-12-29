570a635225922346061a7e7fb988192f3cd9d7f5
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:43:29 +0000

    here recently light respect godown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sys
+def exit_program():
+        sys.exit()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
