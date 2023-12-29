1c0f95a0e0532728081fbf08cc9669ec51053b41
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:35:31 +0000

    material talk mad matter passage

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-def cube_number(x):
-        return x**3
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
