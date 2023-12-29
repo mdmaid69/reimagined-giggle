3930fc86bad8c909025f01c648c841310deabc12
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:14:55 +0000

    relative search holdup or once

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
