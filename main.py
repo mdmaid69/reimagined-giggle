fe7fc075549c45ad974fd714896eed077dd11ee5
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:35:17 +0000

    practise car present return occasional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def count_elements(lst):
+        return len(lst)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
