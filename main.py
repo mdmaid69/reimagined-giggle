db719d8163b30580101abcb7c6d90617b27650d1
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:39:11 +0000

    play police travel accommodation behind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def count_characters(sentence):
+        return len(sentence)
+def greet(name):
+        print(f"Hello, {name}!")
