91f536d099adae161556d1f38ded1a64a7cd37db
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:17:56 +0000

    hurry lookup fast cost couple

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
