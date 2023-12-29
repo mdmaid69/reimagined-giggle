b54feddc90bbe4d4dc9be1319ec3133bcb3ad98a
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:10:06 +0000

    suck crisis for minute increase

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
