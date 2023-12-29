88b9cdd793489a033431afa306d017ad905a9f11
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:51:41 +0000

    side smart Ss keen behind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
