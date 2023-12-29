3fcc3ba2352ea43325893bda8977f7111038e587
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:05:43 +0000

    height television clue would comment

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def convert_to_binary(n):
+        return bin(n)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
