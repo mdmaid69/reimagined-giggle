13ffcd2ff8dfa5c4e74efabbdd857a21144b8243
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:37:57 +0000

    culture which single condition change

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
