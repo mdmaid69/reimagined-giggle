18e2a8b80687f83985903336fe5b52971d5b1bbe
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:05:47 +0000

    takeover dull side gift contribute

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def calculate_area_rectangle(l, w):
+        return l * w
