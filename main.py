1df8f5fe3b059e3d86b55f3d081199b595bdcc72
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:27:08 +0000

    central move nothing neither tip

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
