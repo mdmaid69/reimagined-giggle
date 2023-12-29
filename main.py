ad5a190fc8708ecc13fccbdbde73c973a67346e8
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:22:13 +0000

    compete unlikely line day next

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def add_numbers(x, y):
+        return x + y
