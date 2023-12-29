707ad67613360232dbe226c7e66aed56757226d9
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:18:44 +0000

    front stop nose emphasis many

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
