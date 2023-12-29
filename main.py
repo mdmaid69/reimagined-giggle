82a2fff890040b22232fca17c132b616a7a5f82b
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:56:54 +0000

    patient face problem uncertain operate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
