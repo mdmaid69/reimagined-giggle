ee7fc603c6e578db6ef13dcb187e02e53180608d
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:18:15 +0000

    pile feeling beat ticket open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_current_working_directory():
+        return os.getcwd()
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
