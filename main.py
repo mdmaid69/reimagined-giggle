f99bb59b87237a5c74de7eba99ed32ae2184f7d1
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:19:01 +0000

    bother lean alive strict argument

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def convert_array_to_string(array):
+        return array.tostring()
