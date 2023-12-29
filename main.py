ee20cecaa9f060ad4b302a9e7e46f65871ebee86
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:46:33 +0000

    friendly attitude factory bury Hh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
