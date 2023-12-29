6f2537e9d23e200d5de12261187c7ed16e2c6e60
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:13:16 +0000

    refuse world where related strike

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-print(re.match("h.*o", "hello world"))
+print(sum(range(10)))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
