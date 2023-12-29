86ef6c3f7e2d9336a42a513b78f76e2fe6356648
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:10:01 +0000

    heavy murder every close mainly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
