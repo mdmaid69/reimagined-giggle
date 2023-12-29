3eacc7df3175251184ccca7a28f207fa53d9a253
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:19:36 +0000

    container insist direct when artificial

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
