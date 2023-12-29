0983ec41249a37f37b93976106d9e71a13af121c
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:10:54 +0000

    aware enthusiastic ignore earn hurt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_as_int(array):
+        return int(array[0])
+print(sum(range(10)))
