2a0316af517ed27d53062977a3c3a1f582b6986a
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:43:41 +0000

    Qq everyone blue nor smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
