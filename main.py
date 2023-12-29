986ee442bf46316bbe6d3e958189438e6f8adbf1
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:12:52 +0000

    still term fault final account

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def count_elements(lst):
-        return len(lst)
 import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def get_current_time():
+        return time.time()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
