503a882213bc11e15e41e00a1fdd2691f9cad020
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:30:24 +0000

    ought back yesterday try holdup

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def convert_to_json(data):
+        return json.dumps(data)
