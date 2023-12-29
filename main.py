d7314ad52863ad4570e5e5d49ea9f6fc7b902ce4
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:20:59 +0000

    mother male film bury worth

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
