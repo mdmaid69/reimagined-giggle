ffa807f278dbd767d51c66cc0d32011ce4dcd660
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:36:07 +0000

    scrap even unable relax middle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_speed(distance, time):
+        return distance / time
