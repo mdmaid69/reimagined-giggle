c99616cdfe9038cec6f99d156c39653c62e48874
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:41:20 +0000

    combination bet she sort lesson

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import time
-print(time.time())
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
