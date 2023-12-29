a27b392b2cfd2dc56f5c1c3eef44ebef69689059
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:43:33 +0000

    model far join private remark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_float(array):
+        return float(array[0])
