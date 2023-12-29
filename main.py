57aad8b6fabc3bf8dd084b1da852f61bd1556e11
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:50:45 +0000

    closely guess thought plain contain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
