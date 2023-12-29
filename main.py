44addd47b8a4d8a403c43c3e303295a3e29d2416
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:01:27 +0000

    hole school church possible entrance

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
 def count_words(sentence):
         return len(sentence.split())
