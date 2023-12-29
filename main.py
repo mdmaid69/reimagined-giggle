d83617e73b52819b946e031d415b870578436a85
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:18:43 +0000

    suffer calmly demonstrate shock pattern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
