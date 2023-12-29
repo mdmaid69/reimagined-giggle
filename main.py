92ab2f0f2839617401229a25d57885c86daacf59
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:24:22 +0000

    give justice matter get dance

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_itemsize(array):
+        return array.itemsize
