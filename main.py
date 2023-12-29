eb7d657845cb7a78fd11d4ac3789ad124b8a7019
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:39:18 +0000

    train weather final alive cheek

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_set(array):
+        return set(array)
