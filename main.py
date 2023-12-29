83c00eb89727608cea34e763398e6a3c495e4e1f
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:49:07 +0000

    fasten talk drink shock be

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def add_numbers(x, y):
+        return x + y
