b6149f4cdf52af8ad4979016fb47c81aa334950c
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:11:57 +0000

    makeup house to marry achievement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def remove_duplicates(lst):
+        return list(set(lst))
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
