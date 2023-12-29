9dbc775b8209e59caeca20da76d45463df35b1f5
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:54:07 +0000

    prefer remove black find cause

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
