a9b094d7e4312aac0341c301271607e688c8584e
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:33:54 +0000

    for splash gift point cruel

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
 import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+print(json.dumps({"name": "John", "age": 30}))
+print([x**2 for x in range(10)])
