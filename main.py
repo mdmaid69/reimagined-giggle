5616ff854280f28641125f318980f427dbeda9b8
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:58:37 +0000

    flood force piece arrive fine

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sys
+print(sys.version)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
