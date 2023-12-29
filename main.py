4158cb355621fcd2b714f055eb7ff62af0ddef6a
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:27:46 +0000

    machine spring chat mouth seat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
