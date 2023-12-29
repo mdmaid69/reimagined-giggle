61a9562ef7115ad56e86c7b2915509de6757db22
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:18:02 +0000

    frequent negative combine event failure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
