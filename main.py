d71b31ce4596206ce945beb5f9bfa7efaac81db7
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:02:22 +0000

    tradition breakinto none measure pleasure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import sys
+  def get_python_version():
+        return sys.version
+  def remove_duplicates(lst):
+        return list(set(lst))
