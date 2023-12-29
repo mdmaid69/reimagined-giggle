59048868c469cf4b13b6cac1cfc26d017a7ea768
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:51:21 +0000

    grade Bb back warning hour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
