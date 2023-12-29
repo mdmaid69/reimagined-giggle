0c97778d6f0ccabfc3b23081715af7693606d932
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:21:00 +0000

    likely must weather house send

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
