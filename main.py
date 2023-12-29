eabbe241792a4858331ce90d2bd738ab46487508
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:14:35 +0000

    important Ww minor organization study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
