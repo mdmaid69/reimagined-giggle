ffbd05590c5bacca67399bf5f8bb4d4ebde81662
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:51:39 +0000

    miss character pollution fair correct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-def convert_to_binary(n):
-        return bin(n)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
