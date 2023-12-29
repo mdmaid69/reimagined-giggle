8ab08b1dc97fe102f0894f6274b3042bf7112e99
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:51:50 +0000

    range bound serious but exist

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
