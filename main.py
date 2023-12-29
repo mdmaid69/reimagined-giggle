22634b226f0ed48a6f0a6e631c0106d3c4b533d2
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:50:16 +0000

    face forget Kk march problem

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
