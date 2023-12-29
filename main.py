4923372ee5f05fe6b8f3dc2b5ba8799eaf512170
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:52:08 +0000

    promise suppose off necessary effort

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
