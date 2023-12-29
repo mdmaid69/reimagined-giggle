d71f78e2bd22bb171a4ffb0aa4c5828e4b2606b4
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:52:14 +0000

    dare keepoff tap meanwhile bound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
