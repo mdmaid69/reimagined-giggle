2622584b62b0db542c5f08c26c610978a8023673
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:50:16 +0000

    pour mysterious sight belief scale

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
