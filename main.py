2767930f2503e297f309e990e554c968731027a7
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:50:38 +0000

    thinkover blame beneath soft direction

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def multiply_numbers(x, y):
-        return x * y
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
