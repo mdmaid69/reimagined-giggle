949825b0ddae89a7ab91263ee6e61bccbd760dd3
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:58:11 +0000

    unpleasant fault hurt once yes

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import os
-def remove_directory(path):
-        os.rmdir(path)
+print("Hello, world!")
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
