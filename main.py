3cdb04448d2e6dff0970997cf1f4c17a40d047fc
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:08:28 +0000

    break waste takeout today turnback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def convert_to_hex(n):
+        return hex(n)
