c1683af2cfc4a88fe653b7819727373506c9f1f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:30:24 +0000

    inquire regard tongue face fine

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
