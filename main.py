3ee948aa94206f6e284c7e1399e2da715cb752f7
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:09:14 +0000

    repair runaway giveup high promise

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def remove_duplicates(lst):
+        return list(set(lst))
+print([x**2 for x in range(10)])
