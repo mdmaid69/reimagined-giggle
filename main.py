1905be931bcdb41b96ba614aa4718ff6cfda4508
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:10:38 +0000

    pity with reject taste remark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def convert_array_to_list(array):
+        return array.tolist()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
