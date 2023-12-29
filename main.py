f772665bdeb066fe0114fa77d0c8f7d1efb8ee95
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:47:38 +0000

    swim broken language present guide

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
