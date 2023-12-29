a01b8e417087db6c1daf30c7bd70fedfd3bc6aed
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:35:21 +0000

    you pleased honest matter meet

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import array
+def get_array_as_str(array):
+        return str(array)
