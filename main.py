f567af93e64f579ff47e7f4b02109831de7f3fcb
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:38:44 +0000

    though sail extremely attempt man

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def get_array_typecode(array):
+        return array.typecode
