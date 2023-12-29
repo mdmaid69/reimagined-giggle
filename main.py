58cc4cce3bfcc367d26729d9ca2b73d2efeff775
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:47:33 +0000

    conclusion takeover soft pronunciation fly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
