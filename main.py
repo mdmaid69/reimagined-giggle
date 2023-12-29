6a96c4fc9c624e741f958702c2edae4d02c146a0
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:41:14 +0000

    otherwise drink thick point closely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
