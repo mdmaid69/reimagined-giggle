14af9b227f7f30ed33b3885f5582f491bc0774ae
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:34:07 +0000

    protest female vote experience inside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def sort_list(lst):
+        return sorted(lst)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
