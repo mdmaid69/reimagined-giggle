28eae30e1d0b357b92cb7000250c215eb16346e2
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:08:56 +0000

    angry word Ee until mistake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
