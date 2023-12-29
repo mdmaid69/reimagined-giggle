6555430013523a8cca1aa0e336deafe7908ed354
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:49:13 +0000

    afternoon spring impressed ilovedicks instant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
