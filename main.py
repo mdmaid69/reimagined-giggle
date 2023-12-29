957805c17e1b452da38c8e90081a5683e2887433
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:28:02 +0000

    curious myself disturb theirs yes

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def count_words(sentence):
-        return len(sentence.split())
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
