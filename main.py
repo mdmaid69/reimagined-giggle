77cb0831a44932fcc8dbb98100287a18626750e9
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:30:34 +0000

    trick immediate pause there generous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_list(array):
+        return list(array)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
