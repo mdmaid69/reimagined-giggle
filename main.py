8e7b192f2704aeb986574edffb1668df6e5a2e45
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:08:28 +0000

    stop broad answer speed defend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
