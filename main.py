46c656c0a179abc46517f146c6f113f0e9e8d042
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:39:07 +0000

    typical connection soft enthusiastic justice

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
