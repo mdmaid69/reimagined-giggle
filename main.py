843896ab47d90619eb173650ce14e23f356ab515
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:17:42 +0000

    flow run unfortunate them lie

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def divide_numbers(x, y):
-        return x / y
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
