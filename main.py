20fcb85ed42ff8a88ebf89427a4e4115310d14b6
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:33:45 +0000

    declaration mention combination regional special

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
