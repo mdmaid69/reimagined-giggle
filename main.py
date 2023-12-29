439be59b44b273cf54409ac42e37ac105d5aad31
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:12:58 +0000

    kick sea reaction its string

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def is_palindrome(s):
+        return s == s[::-1]
