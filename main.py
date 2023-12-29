71f80e6f627cd6206173d587b43be24297c725f0
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:45:25 +0000

    handle forward also slice emphasize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-x = 10
-y = 20
-print("Sum:", x + y)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
