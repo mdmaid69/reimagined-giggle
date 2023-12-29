252e475eaf22df3ed652817909c407e38a7795cd
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:18:54 +0000

    just male mail to radio

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
