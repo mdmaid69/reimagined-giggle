110d61648a419424afc2b97ddcb7cd474f7312ff
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:21:06 +0000

    start father relation spray quit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  def convert_to_binary(n):
+        return bin(n)
