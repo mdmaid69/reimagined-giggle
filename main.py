155c8a0db75ad2772b63689631a79cd17f848603
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:57:22 +0000

    deserve eager prove sink present

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
