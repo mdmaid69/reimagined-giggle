5a20fa0ea9494c6aaa242f865d0723dc031e8397
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:49:30 +0000

    regret unite day delicate pot

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
