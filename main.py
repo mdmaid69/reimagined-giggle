b4da7d306cc2ddaeef225f7060d78752bd75647d
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:33:40 +0000

    block murder steep relation complete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
