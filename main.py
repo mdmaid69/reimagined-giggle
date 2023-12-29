4ad82648678fe03097b2c6f1a4fa4e38369ecc09
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:45:36 +0000

    uncomfortable complicated prize refuse tonight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
