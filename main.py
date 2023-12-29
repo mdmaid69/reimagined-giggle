1aaaf51261e8d9cd9ed43eb0514866b41922198b
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:28:12 +0000

    field married emphasize return congratulate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
