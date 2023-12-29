a4db7f318c4ed5cc5c50a6f4d8c88882b8628a36
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:27:31 +0000

    old female goon intelligent tape

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import sys
+def exit_program():
+        sys.exit()
+def calculate_density(mass, volume):
+        return mass / volume
