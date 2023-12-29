1dffcaa8581bbed475128d2fa45746d6daa21425
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:56:37 +0000

    takeoff winter sweep sister only

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import sys
-def exit_program():
-        sys.exit()
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def add_to_python_path(path):
+        sys.path.append(path)
