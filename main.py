90b47c1adaf14979072744e91715d236b1ed6ab6
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:04:50 +0000

    than still shop cover rise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def create_queue():
+        return collections.deque()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
