8a05374c410d6283a9ddc28975359cee381dabb2
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:14:07 +0000

    bringup aim turnover short connect

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_pressure(force, area):
-        return force / area
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
