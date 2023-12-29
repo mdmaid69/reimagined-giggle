4700d8af58f85890c1267693a5006ec8174c2d29
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:34:55 +0000

    chance talk admit long communicate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def subtract_numbers(x, y):
+        return x - y
