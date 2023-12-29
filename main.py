7f28b6725d31b7c45b7db858089f46061a858577
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:16:31 +0000

    square rank age stiff deal

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def calculate_acceleration(speed, time):
-        return speed / time
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def square_number(x):
+        return x**2
