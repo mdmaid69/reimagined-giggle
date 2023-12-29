53f6a67711936bfe3f7cca6fecd2b86d76dd1c50
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:06:23 +0000

    activity oil round 565end anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
