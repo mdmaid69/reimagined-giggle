b1049ec3a6005f90eb50b129fd92430ff3ef462d
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:47:47 +0000

    board violent church operation rest

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
