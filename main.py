169b5d080753f3ba3317e5d0ef31d1df081166af
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:30:47 +0000

    oil surprised comeup port die

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
