3b513277904c173fdfcfb96ff43d15f551b3ee8e
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:55:15 +0000

    criticism destroy cruel church door

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
 import json
-def convert_to_json(data):
-        return json.dumps(data)
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
