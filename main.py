a8e450452500cedf919caa24c2b76cb20b856e61
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:54:06 +0000

    generous display backwards present calloff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
