7e8c0fa07423847048d3908486f8b4a49b52f36f
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:58:30 +0000

    breakdown base swim people related

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
