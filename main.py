3516323952d61bdef71fb79e4d31b4c74bd3c225
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:58:07 +0000

    flow create persuade wish shoot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
