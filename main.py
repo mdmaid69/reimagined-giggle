6d500dbacba1821d0527877640ed0ee75872a074
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:23:07 +0000

    anything gradually bottom press spill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
