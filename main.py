5bdf90f7759652bdc534af90b990deb4b81853b9
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:44:25 +0000

    relax escape later press file

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import array
+def get_array_as_list(array):
+        return list(array)
