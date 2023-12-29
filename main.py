39895bc13934c07d1cedbc5bd027ed1c07ee0a10
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:19:18 +0000

    search where avoid ask at

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_bool(array):
+        return bool(array)
