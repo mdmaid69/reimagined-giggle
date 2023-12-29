da9fc5c703e6e7adc3b31cc3f7820f440952ee36
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:14:53 +0000

    underneath handle worry coat takeback

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
