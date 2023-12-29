417eeac1803be3b8c9f87412f5ee888abeca332f
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:34:09 +0000

    plastic giveback spare be yesterday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
