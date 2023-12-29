5c3ad9d1263b51430eea07ad78790ba88eba9c8b
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:39:54 +0000

    spare kind clue approve weak

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import sys
+def print_python_version():
+        print(sys.version)
