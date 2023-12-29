f3f88e948ada26104fb4cfecf55bb380ec3c1621
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:05:54 +0000

    boil Zz thinkof excellent little

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  def calculate_area_rectangle(l, w):
+        return l * w
