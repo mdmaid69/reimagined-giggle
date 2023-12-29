e1a105b66f1f014e0a1746b1c1d293b051af3b27
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:00:45 +0000

    getback translate offer respect careful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_min(lst):
-        return min(lst)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import platform
+def get_os_info():
+        return platform.uname()
