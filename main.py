4934ecd4a094593dcd3cf9709680b98d9aa92d35
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:02:57 +0000

    loud wind various next hit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
