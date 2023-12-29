eb36fb7bc4f37ff8d85cd92275c822bda6912d92
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:28:32 +0000

    wonder machine mean positive natural

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
