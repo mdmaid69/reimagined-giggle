2733474f6f23fe1c275bdaa0ed1d0e7b0186a280
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:57:32 +0000

    keen film evidence strength mad

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def sort_list(lst):
-        return sorted(lst)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
