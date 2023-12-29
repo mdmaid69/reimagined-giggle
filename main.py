85dac8ebf55a2e4ebda0f2d482d5f131f1bf0b10
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:39:44 +0000

    pain difference flavour balance order

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
