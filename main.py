6525ec1e85fc49e247f9bc9b69fc0f02b0eb5031
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:00:28 +0000

    sign tip unfortunate unfair catch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
