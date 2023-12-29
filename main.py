8493646fb27f5f4f6fc18725170bed8cc99830bb
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:26:05 +0000

    class violence shop accept rise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
