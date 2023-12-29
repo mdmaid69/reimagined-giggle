82bdb63c58c346fa23a5f6e139eb31d7636ac620
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:38:25 +0000

    plan fit progress replace take

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
