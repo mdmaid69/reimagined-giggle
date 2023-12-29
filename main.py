b4b29348e4997e4d28ba7a71d5145ae885480770
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:58:06 +0000

    police cinema spare fall thick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
