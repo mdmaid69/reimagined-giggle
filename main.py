14391ca13065f2a7f09f05eed5952abcfd4ac8fb
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:50:21 +0000

    whoever backwards set appeal plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
