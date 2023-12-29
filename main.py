f09df6c530ef7564a82c6f1ff28b766ce25d5d63
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:16:34 +0000

    callout club basically old throughout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
