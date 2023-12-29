c8d98421fbfacaaeb954d376930580d5e58da279
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:54:12 +0000

    workon general owe worried book

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
