4eb1bbb1bf07c15628ce1f3931c7ae60e6ebc995
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:16:42 +0000

    generally luck good surface after

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def cube_number(x):
+        return x**3
+import math
+def calculate_exponential(x):
+        return math.exp(x)
