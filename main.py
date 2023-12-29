7e6c6c2e7ce6ed6cb043b4afa5827f7655d67b86
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:43:42 +0000

    would terrible quick bus trouble

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
