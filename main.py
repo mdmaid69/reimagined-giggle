65b535ae24141ff4e6c5d8ed74259ad77d4132c4
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:49:46 +0000

    Ee hole speed do party

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def is_even(n):
-        return n % 2 == 0
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
