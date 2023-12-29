d3826dad2dbe56eaaa57e21a6e0260e5fb362960
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:17:52 +0000

    step stand party project Yy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_gamma_function(x):
+        return math.gamma(x)
 import array
 def get_array_item(array, i):
         return array[i]
