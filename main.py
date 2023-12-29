7ea855243004e34b00592b1d0501ec35ab22c8d9
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:09:49 +0000

    sentence view reject dress minimum

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import tensorflow as tf
+print(tf.__version__)
