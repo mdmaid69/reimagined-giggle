eb5cd001ab94449466f4cbbb6a5c682d42542fbb
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:28:50 +0000

    loose bottom ball pleasant mainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import sys
+  def get_python_version():
+        return sys.version
