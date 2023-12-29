6fcaf35a4b22ae6b5de1d28e152cbe274ab5acb8
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:31:42 +0000

    choose drive material morning factory

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def calculate_volume(length, width, height):
-        return length * width * height
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
