24603d241b7060192d5e1e415dd00b2374ffa81d
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:15:03 +0000

    instrument fire excellent survive discovery

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
