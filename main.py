cebee79403e4f6f54ed89a703e91b68ab8c01fbd
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:31:31 +0000

    everything stop succeed course hear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def get_array_itemsize(array):
+        return array.itemsize
