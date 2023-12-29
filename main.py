bf1e68608d4399906e815b9eda358298a8585158
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:10:16 +0000

    surprising drive fall pile cinema

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
