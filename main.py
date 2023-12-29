a95bed88b6b26032d1b7cb508924077046b60bdc
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:43:38 +0000

    upset have quantity taste intend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
