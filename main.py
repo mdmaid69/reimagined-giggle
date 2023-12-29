5c98b05a99107ec3ac26f4e4a1d9b5086919b354
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:50:43 +0000

    month disapprove present public community

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import collections
-def create_user_list():
-        return collections.UserList()
+import sys
+print(sys.version)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
