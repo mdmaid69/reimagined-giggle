903bb122a122f08659f68fec6f2604ca3a296c23
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:55:30 +0000

    consist block last bury strange

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
