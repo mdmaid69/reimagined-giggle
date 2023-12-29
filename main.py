683f6f4bbfc9b8f6410178f535f026a32cd1c188
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:30:07 +0000

    Yy work lower wave method

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
