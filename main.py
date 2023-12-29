39f29de2e4ce084c9d8262e0b0ceaf6553aceab5
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:51:49 +0000

    on report wide friendly in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
