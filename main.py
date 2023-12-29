543e20f87c2f678a8635d935018c23ca41407945
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:43:47 +0000

    shut goodbye pattern politics praise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
