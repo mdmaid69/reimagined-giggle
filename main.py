6d88b9ad2d2a3f10b1b5325b2f3ef5732412e9fa
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:57:14 +0000

    alone uniform no stranger action

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
