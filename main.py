be3a40ba733b9810c1b834387917f30e96b11fb0
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:23:21 +0000

    dry port advantage letter mark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  def convert_to_octal(n):
+        return oct(n)
