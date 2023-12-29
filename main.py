c645f623d19c9bd3a2a476b8d293c0e888cc807d
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:30:34 +0000

    post fix future either manner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
