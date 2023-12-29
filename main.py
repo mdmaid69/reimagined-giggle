772858a2d13ba6570ec63f5ae1a4aea32fd4d8de
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:39:53 +0000

    talk light price otherwise serious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
