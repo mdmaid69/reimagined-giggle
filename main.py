4f8011a8bb6fc58c848759e6b19fb2e778d1cfc8
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:57:56 +0000

    stare object afford roof surface

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print(sum(range(10)))
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
