84392e3b3ef5905861a63c7514740768a4a9d976
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:04:48 +0000

    yesterday impressed behaviour in point

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
