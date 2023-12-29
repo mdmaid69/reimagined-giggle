0951cf921240e42f30f386ecf3db50cf0d24ab75
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:41:38 +0000

    moon heart friend sure brother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
