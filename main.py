1f43aaa9f6bcdd9ce421614cd22a3a75af9a88cd
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:47:10 +0000

    opposite minor attempt shop breakfast

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def subtract_numbers(x, y):
+        return x - y
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
