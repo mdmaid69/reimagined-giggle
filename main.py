195ea7d553939a38c4ec11e2e4316e53b444d772
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:17:35 +0000

    exist down ring attempt bottom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
