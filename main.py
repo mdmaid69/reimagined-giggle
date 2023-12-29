a9e85cb15ca4bcb5d4ce1770a37f98b8ddc98f10
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:28:42 +0000

    vanish unfair book generally wind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  def sort_list(lst):
+        return sorted(lst)
