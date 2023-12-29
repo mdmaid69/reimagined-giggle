46bb506f80d76d412f0472bb6784733d6124d082
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:07:44 +0000

    danger happy defeat sharp mail

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def find_max(numbers):
-        return max(numbers)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
