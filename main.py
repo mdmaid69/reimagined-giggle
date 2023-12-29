3d20db746b515c1a38b71c9ffb6a7085392bc19c
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:51:16 +0000

    history stamp entry myself train

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
