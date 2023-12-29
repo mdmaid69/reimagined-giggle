d1d37704ef4b42c6c1b8f63338b340303ecfd988
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:08:17 +0000

    connect loan Pp Itself learn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
