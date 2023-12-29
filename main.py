b3b3465427da1a2a559eed7856b530853071260d
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:09:21 +0000

    land available lunch off offence

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def get_array_as_bool(array):
+        return bool(array)
