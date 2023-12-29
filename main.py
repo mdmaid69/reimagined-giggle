71e43370617b2beacec91f9bb0511ed5d3b3b160
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:01:41 +0000

    tour listen cruelty guess past

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def is_odd(n):
-        return n % 2 != 0
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
