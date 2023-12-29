c354eb39da0ec9630625a78a202c1efa1a8a25a6
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:26:50 +0000

    hello turndown when grateful arm

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
 import array
-def get_array_typecode(array):
-        return array.typecode
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
