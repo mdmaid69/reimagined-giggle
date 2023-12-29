5525ddb418afe9182d5e387601a9cde5b528ac91
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:37:52 +0000

    willing speech if stop back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
