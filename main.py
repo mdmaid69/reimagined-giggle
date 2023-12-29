3f238953a42ede655e966b5f2ba6fa90a988fd25
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:04:20 +0000

    crowded transparent female involve stupid

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_array_as_int(array):
+        return int(array[0])
