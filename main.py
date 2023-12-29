19f36ef43aff60c8b09ee0b68653a3d1cfaee308
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:57:22 +0000

    better pencil Mm trust size

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
