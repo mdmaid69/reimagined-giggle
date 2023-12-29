ea0a0568895a10665f7501456527dafdde408b08
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:21:27 +0000

    real fear picture comedown speech

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
