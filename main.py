f6214e6e14ef0b96767db3e5a5795f660fa7431f
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:51:33 +0000

    attach shade wide morning independent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
