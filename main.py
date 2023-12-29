e20952fcb9d36a336898aae0982f52546b94a551
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:32:29 +0000

    callat unable through brain lookup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
 import datetime
-def get_today_date():
-        return datetime.date.today()
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def square_number(x):
+        return x**2
