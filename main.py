9b2297d86ab6118a1b9acf4d0be80f45a9c0ba4a
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:16:21 +0000

    name stay market couple organization

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
