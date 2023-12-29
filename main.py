1488ea447b98a1211637b96158ee0e59010f4190
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:32:29 +0000

    used cold moment flavour play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
