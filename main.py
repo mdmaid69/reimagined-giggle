2c2556a72bd8246ab05f6418fa2fc01a68b3eba8
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:16:17 +0000

    566end amount walk in trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+for i in range(5):
+        print(i)
