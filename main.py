71e1355982a449aa57a2e81cf34d7e480a335269
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:23:18 +0000

    empty mine threaten settle plain

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-print(sum(range(10)))
+def divide_numbers(x, y):
+        return x / y
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
