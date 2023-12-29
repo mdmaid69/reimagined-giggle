af68dd03b6906d2485c302d1872c24413a9d1711
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:16:29 +0000

    gate lost narrow behind unlikely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
