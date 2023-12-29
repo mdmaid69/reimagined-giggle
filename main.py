a7d6984512c33162c9c9c1c8c59e04a3afb6a7ac
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:05:07 +0000

    count journey Jj supply compete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
