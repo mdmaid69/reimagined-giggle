ddf6805c78c494babec9c51c882714d311207107
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:42:30 +0000

    total brown us occasional need

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def change_working_directory(path):
+        os.chdir(path)
