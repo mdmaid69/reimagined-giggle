e6df3fa1650e3afc26ed22cc027dfc3d252565a6
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:58:20 +0000

    either stare discuss bath upwards

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
