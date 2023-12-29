fb446b8b24ca6c348d421956be22cf21a8c961e7
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:21:46 +0000

    custom duty deaf exciting love

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def find_min(lst):
+        return min(lst)
