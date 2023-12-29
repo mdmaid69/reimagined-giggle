aa9768218464b722ef353453926d9e7d3301fdd6
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:06:42 +0000

    drop immediate rush watch worry

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
