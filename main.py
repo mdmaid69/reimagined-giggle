288676c34743ab8d4a41f71f6257ed3096aa6d24
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:05:49 +0000

    plastic immediately deliberately case public

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 n = 10
 print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def is_even(n):
+        return n % 2 == 0
