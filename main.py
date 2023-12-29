2edfd9e79e1c534790c823eb54d19b8d39d83dc9
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:01:33 +0000

    flat at bar aware school

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
