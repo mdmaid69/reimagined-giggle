e7f8e2340443049606440fd00c01d69872414051
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:01:57 +0000

    glad tooth one repair until

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  def sort_list(lst):
+        return sorted(lst)
