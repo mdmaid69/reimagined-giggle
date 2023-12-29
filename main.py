b7feaf196eb8104745f747fd2ff5a88541d93989
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:13:53 +0000

    breakthrough nearly finish smart recent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
