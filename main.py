a57e95e821de7ab6613035be42527b6546aea40a
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:09:55 +0000

    ocean badly complete sail fold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
