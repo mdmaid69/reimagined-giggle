40fabd6bd2e7e3bd4698451b305ff33ffd735fe1
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:07:19 +0000

    eye visit crowded variety from

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
