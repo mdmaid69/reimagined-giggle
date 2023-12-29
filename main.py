0b8f492ca6bf12df842c3b1e59f0d3c6e31b59b0
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:39:18 +0000

    demonstrate risk what quit even

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
