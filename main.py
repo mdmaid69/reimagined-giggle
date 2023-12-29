289ff315d44803024def707253fe8877007f4f62
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:50:19 +0000

    turninto straight doubt among best

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import sys
-def exit_program():
-        sys.exit()
+import collections
+def create_counter():
+        return collections.Counter()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
