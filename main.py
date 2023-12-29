2b849a095fbcf45ff67fdec8761f097c17c4bde4
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:36:17 +0000

    school quick above wave effect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
