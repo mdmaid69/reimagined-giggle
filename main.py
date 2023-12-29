c92e352d6616c66c6abf77c795324f57b2ddb006
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:32:33 +0000

    wise spend excuse your anyone

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def find_max(numbers):
-        return max(numbers)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
