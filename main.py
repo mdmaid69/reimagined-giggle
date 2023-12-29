556d4d83d4e4b7c10d3bdc4e2705e1fcc9510ec4
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:00:59 +0000

    almost sound target threaten clue

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+print(sum(range(10)))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
