b8441e0e647a8e33027a042000a43d9e1b1bb265
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:50:26 +0000

    wind search weigh precise taste

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
