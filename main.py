1f6fbd5f8e909a81da5ba1e935162b62a765dbcf
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:33:41 +0000

    hurry front depend fine answer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+for i in range(10): print(i)
