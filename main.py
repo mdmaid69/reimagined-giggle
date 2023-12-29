1e717fa0a17957ab159ea5a9b28ecba399f9eb5b
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:57:18 +0000

    whom outside view I claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def get_list_from_array(array):
+        return array.tolist()
