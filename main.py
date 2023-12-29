8ac8632c93032ffdfc3a84955fe3f9993bc86a65
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:36:13 +0000

    live aim violent contain knock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
