0b9e178636f5a6703ebebe799348b273862afc77
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:00:17 +0000

    force today near aware priority

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
