989a99a7ceda1f556e34f474d66f8833f3e27995
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:15:51 +0000

    however sea laugh belong understand

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
