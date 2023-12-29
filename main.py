0977202e5fe1c26d44e552406341e95d33d190e1
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:05:24 +0000

    used pleasant anywhere 566end study

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
