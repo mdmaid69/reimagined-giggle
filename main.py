f7ac1a252e8c4ba50cd53a56a7728c54014eff9b
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:54:23 +0000

    discussion past physical hurry tear

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
