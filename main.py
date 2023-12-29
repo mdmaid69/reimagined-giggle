75b2b46b551aa6bdcdfba1aaf4ffa3b57418c181
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:53:42 +0000

    plain wise someone generally hope

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print(sum(range(10)))
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
