6f96fc9e072bf3db6b5df834dea6c043c95be2dc
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:35:31 +0000

    train task whichever market expect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
