790fde43af8636bd80b7aba33bbb0da117181f15
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:04:29 +0000

    unit hurry rule pride little

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def convert_to_hex(n):
+        return hex(n)
