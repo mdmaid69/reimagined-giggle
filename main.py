9ac46a9f8f55bb2d1740ca031384055ec28eab00
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:39:35 +0000

    best thinkthrough thankyou minority you

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
