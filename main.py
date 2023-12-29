62d9ab91e42a1db4ac7e05fc8fac17196a3b754f
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:42:22 +0000

    advice factory exaggerate practice comment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import os
+def remove_directory(path):
+        os.rmdir(path)
