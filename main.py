69dc193ce25e46fd4549385040968e8e5f445082
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:30:14 +0000

    outside giveaway fresh study majority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
