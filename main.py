4df83d69326dfe78dd103f91401b27accecd2a1e
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:34:52 +0000

    eye injury spite wrong react

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
