e561169f1084ac6cf410259cf27cff9329eecdaa
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:02:41 +0000

    scrap unpleasant turnback well moment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def get_array_as_set(array):
+        return set(array)
