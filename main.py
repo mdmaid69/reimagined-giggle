4fd46285450f52e122dfa6c89e6e74fbeb1e24e9
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:10:37 +0000

    steer unlikely stop while answer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-for i in range(5):
-        print(i)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
