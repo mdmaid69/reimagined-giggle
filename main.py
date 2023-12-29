8020eb2385ebf913388db03f4bbf9a3938de3d07
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:02:57 +0000

    include serious punish colour relation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
