fdcc65f1915e4be169816186dba7e6ab9ae7fa79
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:56:59 +0000

    abroad excellent marry straight tour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import numpy as np
-print(np.array([1, 2, 3]))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
