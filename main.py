78857c8ec977cbd66eba88d5bd0db4459253dcba
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:54:09 +0000

    contain sweep enthusiastic stare tend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def square_number(x):
-        return x**2
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
