3ce3df248b88d00a75c3fcbf9911760aa8c1417e
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:13:35 +0000

    announce regret year myself prepare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
