6c7e80bdacfae3a57aa61835ba5b94eac52413a8
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:19:02 +0000

    objection plenty clean keep tragedy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
