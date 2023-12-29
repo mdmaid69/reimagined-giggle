49a4ee298ef8cb54f2d063c58a24a8d662fb191f
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:54:09 +0000

    ought design limit group wealth

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import sys
+def exit_program():
+        sys.exit()
