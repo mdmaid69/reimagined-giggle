209d4385b30ab2598c6acdfc315307fb8ba67930
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:09:35 +0000

    ashamed coffee study difficulty lie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_power(work, time):
+        return work / time
