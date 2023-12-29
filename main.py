c9c7efcab2911e34fbfe15e6b03632906dff8e1c
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:42:11 +0000

    wash single doctor rain impossible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import datetime
+def get_today_date():
+        return datetime.date.today()
