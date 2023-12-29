5964f5006be87640fb2756686e5464a97f04630c
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:22:52 +0000

    mail collect suggest unfair performance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import random
+print(random.randint(0, 100))
