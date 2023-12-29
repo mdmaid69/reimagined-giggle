803ebd4055a458d9e001f7a2e8a1f14989ced69a
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:36:18 +0000

    soon 572enter wind Ss prefer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  def subtract_numbers(x, y):
-        return x - y
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
