b0946c51d3fcf0bec4b17d0c2a906b4fe732ce2e
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:21:56 +0000

    argue hear accidental accordingto row

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def is_odd(n):
+        return n % 2 != 0
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
