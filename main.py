d98cd06bf31ce3b0db4ff42a34fc8bd4fdaa1aa0
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:27:28 +0000

    along move commit long paper

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
