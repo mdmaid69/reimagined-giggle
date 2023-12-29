0c29c68d342f7660ae211a25cac816a59cd13d72
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:13:59 +0000

    cause lock along alone drink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
