54c1ada0b6d8161e25a2284025b119328c7a33a6
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:45:02 +0000

    bite table innocent add Ii

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_floor(x):
+        return math.floor(x)
