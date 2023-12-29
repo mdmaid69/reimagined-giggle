8ee681d31a10280bb58badff6accf3f2e40cb912
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:22:48 +0000

    demand loss government keepup listenfor

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
