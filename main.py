c0f8b79924e524dc93f2477ad8c6af4b4c287418
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:18:22 +0000

    dream top secret direction birthday

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
