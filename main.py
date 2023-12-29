fa7ffe284b2bb986cc31b7176f64b88994c9745a
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:32:14 +0000

    grow model juice mind exchange

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_square_root(x):
+        return math.sqrt(x)
