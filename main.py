22c3131b6f2befb03bcf591813790c88a2dbbe9c
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:23:06 +0000

    judge successful record remain cross

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_area_rectangle(l, w):
-        return l * w
+import math
+print(math.pi)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
