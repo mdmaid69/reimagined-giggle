10b789bfb3d8eb4a482c8a5480f31afc751ccf55
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:28:05 +0000

    blue comeoff sale job provide

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-print(math.pi)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
