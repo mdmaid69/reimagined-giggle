71d599920325fd164330ec9ae484c64a2e828d9f
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:22:45 +0000

    early join comeround afterwards choose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def convert_to_binary(n):
-        return bin(n)
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
