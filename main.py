8087a679a6bd0b5a89e995addd66d6dbd6137bb1
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:26:49 +0000

    tip comefrom party object hard

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
