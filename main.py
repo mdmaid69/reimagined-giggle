b044529f1b3ce5ce9e610512ce19880fb010a1d7
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:30:26 +0000

    base anywayanyhow label arrangement swallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_str(array):
+        return str(array)
