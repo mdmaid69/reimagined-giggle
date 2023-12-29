c98925c78874e8c49f3fcee9e53887ea1421d0c5
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:16:36 +0000

    act message treat surprising dick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
 import array
-def get_list_from_array(array):
-        return array.tolist()
-import getpass
-def get_username():
-        return getpass.getuser()
+def get_array_item(array, i):
+        return array[i]
