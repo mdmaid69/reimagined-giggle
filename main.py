5cd5a9302f75e571111c569a660410e49562f0b5
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:37:23 +0000

    shut think crazy roll argue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
