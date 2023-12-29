3c267532b59f4214cc77d7cf60fd4052f75fcf6d
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:00:00 +0000

    short rest perhaps impossible report

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
