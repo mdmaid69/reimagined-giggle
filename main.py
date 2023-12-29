0abdaf2a9d6f95ebd4c1581ae1fa1148048c5b32
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:21:10 +0000

    turnoff smooth information speech obvious

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
