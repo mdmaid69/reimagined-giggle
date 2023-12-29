cdae14339103766bf4eafe4b9b19335e88324a37
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:40:19 +0000

    sink order approve compete character

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def find_union(list1, list2):
+        return set(list1) | set(list2)
