1392197922f1afeb7e8ad141857df2f487114bcf
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:01:38 +0000

    grow cope frightened unpleasant runout

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
