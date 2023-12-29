c15dbb18ffccebd2991d11f5085cb27696020f5f
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:58:29 +0000

    ignore boat university mine thick

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def greet(name):
+        print(f"Hello, {name}!")
+def calculate_average(lst):
+        return sum(lst) / len(lst)
