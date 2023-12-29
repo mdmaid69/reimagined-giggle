e4444e2fab5816f1e6e9bca8690d0b3564ef8d3c
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:00:11 +0000

    turnon expensive colour air comein

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
