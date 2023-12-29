942d1042003c0d54e729af4bf2426e0fa94cf4c7
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:54:46 +0000

    considering trade copy complain splash

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
