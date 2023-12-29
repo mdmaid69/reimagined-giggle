bd9a6b4cac398a2c5ee869204edab550b9debddb
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:15:39 +0000

    all uponon stranger back trick

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  def convert_to_hex(n):
+        return hex(n)
