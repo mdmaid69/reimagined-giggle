ef2f8f5754447df301abfa445d13a4867a38ff4f
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:18:02 +0000

    breakthrough still result rather shallow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  def convert_to_binary(n):
+        return bin(n)
