02e739619d20eed2349e8e26153fa39277506488
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:50:25 +0000

    cum square your tonight hang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
