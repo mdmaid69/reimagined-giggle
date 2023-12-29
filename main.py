b744be1509f4b65e4333845585d8a33b27ed84f2
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:11:25 +0000

    serve interfere payment myself breakfast

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
