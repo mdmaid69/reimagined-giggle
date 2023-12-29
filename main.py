6dab61346fe5fef2c29de11da0fe993d2d5b9642
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:50:43 +0000

    authority unfair convinced pair wide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def greet(name):
+        print(f"Hello, {name}!")
