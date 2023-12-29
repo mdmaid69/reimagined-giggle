d47359bc7a1f7cf11f46f41f18bdf33e4ba2e16b
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:33:00 +0000

    as Pp collection keepfrom relax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import collections
+def create_stack():
+        return collections.deque()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
