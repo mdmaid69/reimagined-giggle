a789c5fd9c1b1046dd569ffa36eefdca2ef942c5
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:22:39 +0000

    common improvement themselves frame wide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
