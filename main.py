d079c2ac1be246d495a69b8051cdfab18c37053b
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:48:28 +0000

    sick hollow aim risk bend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-for i in range(5):
-        print(i)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
