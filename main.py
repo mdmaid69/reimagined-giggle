56aee97f677d244b560508a78f9aa8328073d860
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:43:47 +0000

    uncomfortable wish separate feature whenever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
