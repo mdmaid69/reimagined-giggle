8cceb132dc00be3ea3bf0c2d532ebde9f365f3ff
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:54:31 +0000

    drink wake simple creature them

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
