34556f05c48dd8e3922f93c69a8ac0b21202dc9e
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:11:25 +0000

    runaway window spill explanation familiar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
