f09ea1d54c886d57f89fa4b09d993c2bcf77e9bd
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:30:33 +0000

    check claim us otherwise suggestion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
