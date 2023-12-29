7a3439149e7647892d5d5a8414179a3c8589527d
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:03:59 +0000

    talkinto exercise opposite store weather

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
