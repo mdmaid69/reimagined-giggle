ea60c802f6495d07bea586568abbff06024f1e89
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:14:03 +0000

    print pollution low temper perhaps

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
