235d0a9fab142ced65ee0df342dd69c3465dbe2a
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:18:20 +0000

    put case meaning petrol interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
