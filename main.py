b5b71b96db4bf00be102f738268d2c3c79b9900f
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:44:49 +0000

    play chin relationship confusing callback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  def square_number(x):
+        return x**2
