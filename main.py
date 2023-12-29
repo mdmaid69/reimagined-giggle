a471f6898d5f561956ac9c3455c3c391a6c229d7
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:43:03 +0000

    frequent protect authority stop change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-print(sys.version)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def set_array_item(array, i, item):
+        array[i] = item
