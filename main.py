bac576605e456577843f9817fdeb34a792d55359
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:16:26 +0000

    lost hate even wave share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import datetime
+def get_current_date():
+        return datetime.date.today()
