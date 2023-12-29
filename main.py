ff38f5239efc6fc7a19840da26328c47a5ab53ff
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:49:30 +0000

    whistle violence black pipe flight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def split_path(path):
-        return os.path.split(path)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
