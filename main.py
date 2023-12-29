c61ef1dcc9f05009c2d36c9d958f9d2458b1e46a
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:51:54 +0000

    once home friend sleep talkback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
