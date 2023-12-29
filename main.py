eb3ee07845a75c332ad9d6d6b2aa2aa3c00509ff
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:23:15 +0000

    tape finger road finish nurse

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
