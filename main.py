ee115da9fddbb6ba10e585d2f4a73419eff8d17e
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:07:30 +0000

    loss afford lookup stretch love

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
