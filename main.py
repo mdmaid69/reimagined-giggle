4120c253b0ba58b42e851b375183ef526d19eb0b
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:46:44 +0000

    exchange file bath fault rather

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
