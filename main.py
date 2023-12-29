bf510eb84406645424eb438e1df9dcc92b539eaa
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:16:10 +0000

    willing relative gap suit store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
