ad05aaec6b3c53c763aa38409c47a9322018fd80
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:25:22 +0000

    holdon so team key reply

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_unique_words(sentence):
+        return set(sentence.split())
+  def is_even(n):
+        return n % 2 == 0
