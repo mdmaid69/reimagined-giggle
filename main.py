11da98809cba9e67e9b833a77fcfe91083d54496
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:02:06 +0000

    see nearly spread mean offer

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
