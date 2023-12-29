828b7789a89e942276bf690a340df471a2336dcf
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:39:45 +0000

    influence properly bury nonsense sound

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import logging
+def log_message(message):
+        logging.info(message)
