728bf5fae0707f57754f137cc766288c706f3d7b
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:30:41 +0000

    divide even rid crisis drawing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def convert_to_binary(n):
+        return bin(n)
+def calculate_volume(length, width, height):
+        return length * width * height
