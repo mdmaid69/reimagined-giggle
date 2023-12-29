6cc70b20bc58965b74af1ad89967d1545d31fc52
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:04:26 +0000

    plant noone apart keepfrom question

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import os
+print(os.getcwd())
+import random
+def roll_die():
+        return random.randint(1, 6)
