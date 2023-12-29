5ffc709d7f6a10cfac8289286894c34a0687e582
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:02:34 +0000

    sell slowly quickly cause split

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
