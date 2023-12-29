86baf8d5cf8cd19c00b9aab7bac4e440575e88ba
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:25:36 +0000

    admit above deliberately stage term

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
