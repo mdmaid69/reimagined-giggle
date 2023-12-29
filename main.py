b5cc3363417b8007c1ddda4d63fb1c2af81af379
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:42:24 +0000

    fun gentle innocent afterwards convince

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def split_path(path):
+        return os.path.split(path)
 import os
-def remove_directory(path):
-        os.rmdir(path)
+def get_file_size(filename):
+        return os.path.getsize(filename)
