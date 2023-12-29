36426bf8aaa142d982ec903bda492ae84feaf80f
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:07:48 +0000

    complain rely custom sleep pile

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  def square_number(x):
-        return x**2
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
