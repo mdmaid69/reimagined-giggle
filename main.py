15e0cd8b23972c8234c10337af7267e5008e0f4f
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:18:14 +0000

    company interruption sorry yes should

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
