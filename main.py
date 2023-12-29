b1b41c018d7e9fd18d2519c5746ea40028e8d0c9
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:28:25 +0000

    willing Tt organization polite question

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
