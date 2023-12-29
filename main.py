e1af9830da395fb489e175e9a04c5883f1b50b6e
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:38:17 +0000

    wild factory cheek smoke similar

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
