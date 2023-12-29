07c7b2c721fd91f7fcc8a2034663ea281d4db78c
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:55:18 +0000

    rush Nn weekend shop pen

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
