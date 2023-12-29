b345788d6598e0b2ab2f67e5a769b1cfccf1aa30
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:10:20 +0000

    Rr body steady successful vanish

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
