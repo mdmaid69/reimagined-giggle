37cb3a40d28e1577a4594af4ca570eca41c3d7e7
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:29:21 +0000

    shame characteristic bin opinion chin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
