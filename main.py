39ed8c6f24b915e25890fe1d9334640c61f285dc
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:58:03 +0000

    back quality bank effect be

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def multiply_numbers(x, y):
-        return x * y
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
