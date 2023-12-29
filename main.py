06dc02f18b0a669bcd03094c7e76be70230d479b
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:13:14 +0000

    brush treat cinema discussion himself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def is_odd(n):
+        return n % 2 != 0
