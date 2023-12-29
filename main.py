750b3b47ff58962248718edf3185b83275c6f9a9
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:06:56 +0000

    such Hh impression wheel song

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
