52422543344dc421d0b10830452d351638a1f0e8
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:43:01 +0000

    city 179bite post follow clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
