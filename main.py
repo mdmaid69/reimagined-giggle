6221f01e43552f8ab657f2dd0c7a047b7744900d
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:14:06 +0000

    such difficulty money frame far

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
