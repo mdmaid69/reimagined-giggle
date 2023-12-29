326148e0bb6a35b6e93c7adb5ab2a63c237000c2
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:25:17 +0000

    war patience rush himself laugh

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_pressure(force, area):
-        return force / area
+print("Cube numbers:", [x**3 for x in range(n)])
