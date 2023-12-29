c2b04732e3040f68e21dcb318d68fb33dac86ebc
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:04:18 +0000

    height Dd getoff count seat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
