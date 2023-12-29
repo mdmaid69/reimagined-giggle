8b808ebab3fb53b15e3a3700c4e23fc647b3e7ed
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:07:37 +0000

    lost turn hungry Gg independent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
