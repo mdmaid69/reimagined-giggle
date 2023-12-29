201dc97b7cc09a7acd5e688c06601fcc88e6a95a
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:56:49 +0000

    charge environment star run badly

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+print("Hello, world!")
