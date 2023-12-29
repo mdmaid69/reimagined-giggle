8a410d36aabc23c0f19cb754b6c5fb0d58883f09
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:27:42 +0000

    Ww godown hollow century target

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-print([x**2 for x in range(10)])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_itemsize(array):
+        return array.itemsize
