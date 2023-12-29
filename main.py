6796c7c1a9278dad4d659cc7ab539da4594f9f71
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:06:20 +0000

    whether tape habit attempt clothes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+for i in range(5):
+        print(i)
