32065d3b367826d8faf7d9a0b13007a6a16ccc90
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:09:10 +0000

    definite post fine holiday anybody

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
