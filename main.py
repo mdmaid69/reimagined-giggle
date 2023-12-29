7f9f1381c19cd67d302c2fbee267f02a3ed97531
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:53:28 +0000

    limit claim show honest ashamed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
