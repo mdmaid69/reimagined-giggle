ef7292f1754dee525140ab9d60dfe6e16643f561
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:28:42 +0000

    duty heat brown glass touch

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def convert_to_octal(n):
+        return oct(n)
