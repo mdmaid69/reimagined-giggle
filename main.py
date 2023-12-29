320f5ed457c8548b83879e61551dc270918b48b5
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:49:23 +0000

    mark hollow politics smart protest

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
