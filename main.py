19bd0e1e7a3a6e094b27c7d4bf881c51d828a0a9
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:55:15 +0000

    forgive other equipment thought wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
