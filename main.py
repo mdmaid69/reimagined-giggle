eb725e13d0be27e97aa9e820e76d2bcfdce0e081
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:48:45 +0000

    traffic plain drive eye section

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+x = 10
+y = 20
+print("Sum:", x + y)
