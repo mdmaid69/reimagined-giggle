d1d6b4186dffa0cfff38faff337e3daf946174cc
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:53:36 +0000

    especially practical fear realize strike

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def is_even(n):
+        return n % 2 == 0
+  def convert_to_binary(n):
+        return bin(n)
