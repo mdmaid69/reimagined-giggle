d9127c4662208f16fc9beff75585b785d4250bc2
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:39:14 +0000

    suggest since tongue bound flat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
