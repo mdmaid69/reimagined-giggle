f310b673fc32fd5a52a91d524dbdb70ebc6cef86
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:56:14 +0000

    number nerve emphasis respect street

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import platform
+def get_os_info():
+        return platform.uname()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
