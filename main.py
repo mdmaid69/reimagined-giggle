d228b8e47e2e2acc0f99e123074cf04f66ece05f
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:30:17 +0000

    lend collection period mysterious production

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sys
+def print_python_version():
+        print(sys.version)
