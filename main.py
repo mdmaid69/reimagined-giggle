dc7f12effa65e5f8a844d89d5ac7f358181ec6fb
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:32:21 +0000

    middle who compete scale rain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
