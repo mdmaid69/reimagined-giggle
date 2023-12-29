9c7f0a438870b139afb82635ec0c047e0641ee8e
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:27:36 +0000

    breakout check dare important shall

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def find_unique_words(sentence):
+        return set(sentence.split())
