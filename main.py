72cb71343b9eb74496a91113e832c2511d84e33e
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:35:56 +0000

    gradually great special could ever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_str(array):
+        return str(array)
