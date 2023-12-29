026fa731bfd282e45329f7c7355ef1172e3bc336
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:59:54 +0000

    help shopping pump worry lesson

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
