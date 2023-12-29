ca6a931c268369c290985245de36dacdb115411a
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:39:33 +0000

    emphasis busy type below glad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_power(work, time):
+        return work / time
+import array
+def get_array_as_repr(array):
+        return repr(array)
