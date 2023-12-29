6527fa7685ab35e2979ad4bfa0ba562b7b9315e8
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:48:36 +0000

    positive rate smoke foot community

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
