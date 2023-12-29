b44a6c5104b5d99acc390d8f0e2dd1e23cfc2954
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:11:49 +0000

    gas either unhappy impression wrong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
