99c096fbd0a7db5838298215d7002a3450fdc477
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:18:46 +0000

    waste smoke opinion neither warning

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  def count_elements(lst):
+        return len(lst)
