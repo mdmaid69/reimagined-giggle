895dfb5e9486503ffbf5cacb90d1afde79b11615
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:40:58 +0000

    disgusting ignore entry partner lend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def split_path(path):
+        return os.path.split(path)
