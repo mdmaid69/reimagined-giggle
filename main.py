7eb1c0542879a4b0ed5cdd580ce61ab24dfe1eca
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:09:20 +0000

    science sorry ashamed man share

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-name = "Python"
-print("Hello,", name)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def get_array_as_float(array):
+        return float(array[0])
