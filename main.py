2e6e399db15363fa16bf5abd847689b2309fcfe6
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:07:49 +0000

    lost dry thirsty tough sign

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def get_array_as_repr(array):
+        return repr(array)
