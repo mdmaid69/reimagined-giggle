2a540428f88e8176d62ea590132bdb84352487d6
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:55:48 +0000

    smell further somebody instrument feed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
 import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
