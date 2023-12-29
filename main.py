f01a510399550c48d115fbd7279392866f02856b
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:25:16 +0000

    climb season cause fast inside

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_speed(distance, time):
+        return distance / time
