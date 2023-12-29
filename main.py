fe077e53888bbed2f0b501872f34282b8249937a
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:07:58 +0000

    shop improve still more not

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
