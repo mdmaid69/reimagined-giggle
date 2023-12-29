fc96140f76c103f3a4b154577eb1ceec4796ea1a
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:41:42 +0000

    land now hungry clever burn

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
