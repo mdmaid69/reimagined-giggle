12b36ccaa6766b4640746359e4f7fdb085d64feb
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:17:19 +0000

    position with direct back guilty

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
