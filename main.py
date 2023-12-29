bd12e4f984f3759eb3d426ce30e820bafdda3bfd
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:43:20 +0000

    bad common praise outside damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
