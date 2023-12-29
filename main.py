3159d2c086fba8b10f448bc84cd4f4a21d826efe
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:20:16 +0000

    next company level complaint crime

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
