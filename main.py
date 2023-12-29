3f55fbf8ccfebb773fcdfe43c61227927fae0b88
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:43:55 +0000

    autumn little mother artificial duty

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
