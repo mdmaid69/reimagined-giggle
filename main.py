16b2257b6b15742c3168f4d110cc9b652f4d873a
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:16:19 +0000

    private breakin inquire takeout quick

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import array
-def get_array_item(array, i):
-        return array[i]
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
