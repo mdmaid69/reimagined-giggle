5734442ee9142cba114eb20e3e8d1ff12c4c825d
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:45:23 +0000

    public couple powerful apparently familiar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def cube_number(x):
+        return x**3
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
