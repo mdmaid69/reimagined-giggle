306679b82d6db8e69760c4c45b75e1057c09a949
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:32:32 +0000

    suppose prove bringdown clever well

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
