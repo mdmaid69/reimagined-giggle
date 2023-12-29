8e2fb9797b3f9f35b7c9289a44937a6cfaab3760
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:22:11 +0000

    concentrate constantly lead modern acceptable

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
