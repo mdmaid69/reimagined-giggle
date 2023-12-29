0a10beb6da5e05bfe27b04603aa366a1fdce9f53
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:47:24 +0000

    callback collect balance once declare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import collections
+def create_counter():
+        return collections.Counter()
+import tensorflow as tf
+print(tf.__version__)
