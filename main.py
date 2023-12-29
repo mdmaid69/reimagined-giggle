061e21b8daccfdf78559eb087f6f7ef8e19d36c4
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:06:19 +0000

    Dd fail should spite rob

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
