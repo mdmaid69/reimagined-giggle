00890a84e11147af2ae323bcd1f9d5e0cb6eac02
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:44:17 +0000

    neither business involve lean makeinto

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
