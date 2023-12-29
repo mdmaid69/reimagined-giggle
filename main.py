d13385400f43313129270688b6b493622d7b04fc
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:16:38 +0000

    use order case second check

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def convert_to_octal(n):
-        return oct(n)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
