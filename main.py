4279b7a97895cacbad770695917ddef3acf2a3c8
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:06:58 +0000

    service near warn needle damage

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+print("Hello, world!")
