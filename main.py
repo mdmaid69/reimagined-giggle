8c61a8c5f3c845863aa60d9170260337fb83e40e
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:18:27 +0000

    workon heart account female condition

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
