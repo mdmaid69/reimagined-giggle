042887f76c62533499182875a015d22ca43c80cc
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:49:50 +0000

    large noisy deep centre life

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-for i in range(10): print(i)
+def convert_array_to_list(array):
+        return array.tolist()
