2a336f4c3c0e3c1d426f1a8fe7cb644c7d5f7f1b
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:57:05 +0000

    your welcome extremely achieve even

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
