f3e9c846b7bba04d0ac6b70e9bb6a2c0879a1245
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:29:56 +0000

    regret slightly account smile live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def convert_to_octal(n):
+        return oct(n)
