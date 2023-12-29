3aacd09a5f727e95a2c247ffd97382e7cbb1eba5
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:24:55 +0000

    Hh state what song somebody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
