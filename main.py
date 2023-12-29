70f8ed463a60967514061fe641b005bc031f5825
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:59:58 +0000

    rush before raw decision heavy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
