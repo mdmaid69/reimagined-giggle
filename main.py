5a62d60c623da696b61abdb383b367ae2e20ed30
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:19:09 +0000

    organize Nn string church water

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
