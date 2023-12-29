069976ce9c5b1027db6fe3c3d3cd84fcf0efcf5a
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:48:26 +0000

    loud beautiful record warning display

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
