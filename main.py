5d02e6c86d59f5b1ec363e9cbdc8103732447a5b
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:43:09 +0000

    listenfor deep crash ride ability

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import logging
+def log_message(message):
+        logging.info(message)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
