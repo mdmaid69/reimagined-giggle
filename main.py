f570514bb23a07c67f4d3b37aa234001139ecae2
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:00:04 +0000

    rise bank phone agree convince

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
