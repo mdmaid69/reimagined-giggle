b21a1f7d8db80c9014b6f7e7e69354e98ef1c36c
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:24:37 +0000

    standard good concerning national yes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
