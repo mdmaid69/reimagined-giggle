8aad76c89fdbbfc38badb22828b7f6f2f93ef843
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:28:01 +0000

    disturbing 572enter fall appear load

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
