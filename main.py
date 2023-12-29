c8b5b86abf384a85d0dcf354d923b89683c70934
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:06:56 +0000

    correct spend challenge stop final

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_force(mass, acceleration):
+        return mass * acceleration
