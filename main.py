f0041eb3095c4d449d13ff4d926afb2671831603
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:17:18 +0000

    shame result Ss transparent likely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
