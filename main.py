988a9cdb5279d1cdb33ef730abfdabb1c176f207
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:45:52 +0000

    relationship my compete school gentle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
