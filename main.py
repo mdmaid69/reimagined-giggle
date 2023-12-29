cf85b608dda14f56937a64957676913f7704c6c2
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:54:46 +0000

    apparently piece question little tour

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
