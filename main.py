2c67f582bb5d381a7b01e936940a97d658a2cdf0
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:39:38 +0000

    eye computer spoil square discover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+print(os.getcwd())
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_absolute_value(x):
+        return math.fabs(x)
