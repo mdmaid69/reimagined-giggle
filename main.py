af501042230d85e008655561a27564b9672811e4
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:01:14 +0000

    spare unable journey second block

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-print("Hello, world!")
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import getpass
+def get_username():
+        return getpass.getuser()
