0bb45552f43e632524a1eea096f1c3346242460a
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:36:21 +0000

    wide bend makeinto send bin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
