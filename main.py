96037d273f6cc61b57c25c87ed99a4eacd6f7bca
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:41:59 +0000

    noone about Ww fault help

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
