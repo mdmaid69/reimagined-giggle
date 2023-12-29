40fc8a47d1bc9b8080b691cde9e9b7cc756c3b83
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:51:50 +0000

    exercise union payment shoot slip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
