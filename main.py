fbe150377ea7e457147fcc8174535eb3fd742296
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:17:44 +0000

    case there method thinkover stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
