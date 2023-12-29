3ef1bc0fed69c4f1027c1d09618897e73a745b80
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:59:55 +0000

    mistake centimetre unusual earn safe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
