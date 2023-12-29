91e689184a4dff5a8266f8691049cfbfc8e7d10b
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:55:44 +0000

    draw turnaway passage control picture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
