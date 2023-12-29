78d2fd57712586db853419df89cfc1e231fc7254
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:17:14 +0000

    period university instead natural laugh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_speed(distance, time):
+        return distance / time
