5d60124a19504b16f124483ffff435b6944d7271
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:27:04 +0000

    radio female slight march journey

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def reverse_list(lst):
+        return lst[::-1]
