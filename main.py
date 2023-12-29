8e9268bab61fdb116c11621efe63e4ec1c618eda
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:31:26 +0000

    closely 568energy alone relative new

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
