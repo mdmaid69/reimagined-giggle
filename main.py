5595edaa0c78226739a1457769dab501f94f0eee
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:22:15 +0000

    rare sorry discovery minute black

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import re
+print(re.match("h.*o", "hello world"))
+  def count_elements(lst):
+        return len(lst)
