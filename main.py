5a7126fb2fa14fd03c8fa5c62db87dc3fc127039
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:46:04 +0000

    increase dangerous occasion because try

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def add_numbers(x, y):
-        return x + y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+text = "Hello, world!"
+print("Uppercase:", text.upper())
