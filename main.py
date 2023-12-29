1ae0e6f6ac394ddc2ee847a0df0437a5efb42539
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:55:56 +0000

    appeal spoil reduce after public

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def find_max(lst):
+        return max(lst)
