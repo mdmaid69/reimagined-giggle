fdbbbe76daa2d649d904aff787fb1ad43fe23a90
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:31:35 +0000

    thankyou tense fear set bringback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def get_array_as_int(array):
+        return int(array[0])
