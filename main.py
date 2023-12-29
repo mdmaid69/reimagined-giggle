4837c02121f4e99d5c6f024a3b047d914b7fb18f
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:48:05 +0000

    pass like since exam rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_acceleration(speed, time):
+        return speed / time
 import array
-def clear_array(array):
-        array *= 0
+def get_array_length(array):
+        return len(array)
