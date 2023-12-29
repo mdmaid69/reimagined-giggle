a8da2ceb77ce6187a8ac455da42400c8682ce8af
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:14:49 +0000

    hard bridge Hh once wind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
