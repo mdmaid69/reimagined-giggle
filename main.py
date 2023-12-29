9317213b496d684fdc302fcc07b1eefaa197776e
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:36:21 +0000

    number familiar doubt burn immediate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
