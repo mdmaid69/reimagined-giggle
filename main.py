5daf77fe4715640f4e204a8a26b64d099baa3eed
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:24:55 +0000

    true beyond add bin show

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def reverse_array(array):
+        array.reverse()
