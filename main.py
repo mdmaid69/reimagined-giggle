ecbc49ce20dda2d090e89f0effdfea910abbe037
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:38:10 +0000

    recently surprise nature attitude to

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_as_bytearray(array):
+        return bytearray(array)
