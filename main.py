932121cb7a196854f8edf90ef32c128ca106fa62
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:53:08 +0000

    back challenge set print edge

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
