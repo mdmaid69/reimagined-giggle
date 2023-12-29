ed8aeb7b4e715099779083b0f3e21e90f1bf561e
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:18:00 +0000

    worse grateful on bunch explanation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
