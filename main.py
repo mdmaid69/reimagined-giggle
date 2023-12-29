bd0cf4d59bee32da8458907f202ea4dcbc5ec236
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:13:38 +0000

    sure balance concern round office

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
