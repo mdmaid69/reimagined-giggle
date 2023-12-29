182778c0609d5047802294aeca93b3842a76c6b9
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:53:44 +0000

    convincing large surface explanation rob

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
