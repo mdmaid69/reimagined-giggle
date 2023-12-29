7c5f3eda55628500ca40da065e26efe04be180fa
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:12:42 +0000

    artificial recovery official detail breakinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_volume(length, width, height):
-        return length * width * height
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
