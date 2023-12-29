413376f64da01efd5927fe9bdc8276a442df04e5
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:25:21 +0000

    hope start sweep Jj talkoutof

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def count_characters(sentence):
-        return len(sentence)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def convert_to_hex(n):
+        return hex(n)
