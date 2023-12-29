d73db1a647160fc070c082636882dd6d450cbf63
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:02:53 +0000

    silent life hot round square

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
