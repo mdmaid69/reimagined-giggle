1cb60e6429dd362992f855e578063d0afe1aeafd
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:32:56 +0000

    cut slice congratulations twist firm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
