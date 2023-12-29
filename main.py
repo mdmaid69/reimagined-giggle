6b9a14f18d82267e5a52d39e175205c32e2bf599
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:39:07 +0000

    keepoff obvious emergency knock always

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_atime_ns(file_name):
         return os.stat(file_name).st_atime_ns
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
