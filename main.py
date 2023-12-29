650457faee98d3e0d2f8b03b6cc9e3ae604168b8
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:19:57 +0000

    table kind repeat occasion pain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_area_circle(r):
+        return 3.14 * r**2
