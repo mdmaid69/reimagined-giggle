4ddeb4dc348175a8a23621f65289d35a1363e461
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:52:49 +0000

    solve city drink straight union

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
