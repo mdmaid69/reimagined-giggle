542c16327992cb40fcac395685857dfc15f4c4fb
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:48:02 +0000

    include catch crisis satisfactory advantage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
