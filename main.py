06fe571022001658bab69959856cf954287ae4e7
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:44:19 +0000

    position manage object owe late

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
