e6812b8e110bd3070263754436a7e5cb508f8252
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:37:01 +0000

    just tie turn television excuse

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
