7fb966573d499c077718190856c44ad223c52380
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:41:48 +0000

    get favour from pretty finally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_density(mass, volume):
+        return mass / volume
