56df64844cd9b289633c42042d51c129f80ad87d
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:19:53 +0000

    hit attention gather him equal

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import time
+def get_time_since_epoch():
+        return time.time()
