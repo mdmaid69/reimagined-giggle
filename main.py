e0e069442bb871ce4c7c7aad3e667d71f384ea82
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:07:09 +0000

    go surprise careless shoot square

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
