f8a2c6789315706156e86883daa6c56ebea25599
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:44:32 +0000

    delight direct interview somehow error

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
