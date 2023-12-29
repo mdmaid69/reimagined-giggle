ea1bd8cad1b968f83ca8052a3d9ca114ae85499c
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:49:07 +0000

    polite our whole turninto programme

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
