48a78b5fa8729ca357ced02ebc16ef3cb3405307
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:38:06 +0000

    walk serve contain demand train

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_current_directory():
+        return os.getcwd()
