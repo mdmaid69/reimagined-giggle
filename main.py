bb05991f31ace9ce60f8e5c0a6eb6355060513bf
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:58:30 +0000

    convenient wall perhaps murder bank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import datetime
+print(datetime.datetime.now())
