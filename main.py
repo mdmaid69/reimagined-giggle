16ff10fa9702a6667df21d4a7ec001d3741355d1
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:36:18 +0000

    exciting interest clear black deaf

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_max(lst):
+        return max(lst)
