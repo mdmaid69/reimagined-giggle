4bd0a92b4231a2cfa6de34e09ae3dfdbf728d3e8
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:42:14 +0000

    among bringforward clear wall stone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
