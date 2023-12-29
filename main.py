ba127ca9f5301920ec9ea9b2cc68373b520670e8
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:01:45 +0000

    average live nature storm clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
