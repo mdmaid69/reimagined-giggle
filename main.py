b5f20864f9d1bd3e8ff064e7618a6f400eb52d90
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:33:04 +0000

    contents point yes fast flight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
