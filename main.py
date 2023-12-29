18524fdbe1b30fd56dff971b88c4f74857428fb9
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:33:25 +0000

    bank spring tongue surface ours

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+print(sum(range(10)))
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
