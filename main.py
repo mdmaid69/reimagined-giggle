eac9a723f932fdab73de9e3d54be832b6d87ced7
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:08:50 +0000

    visit regular near window slight

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def remove_duplicates(lst):
-        return list(set(lst))
+name = "Python"
+print("Hello,", name)
+def calculate_density(mass, volume):
+        return mass / volume
