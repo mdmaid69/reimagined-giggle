bff19874eb975e397e801bacf4b5acfb7298e37d
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:08:49 +0000

    walk unusual uponon danger disapprove

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import numpy as np
+print(np.array([1, 2, 3]))
