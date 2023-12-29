70f63bbd75a95f86e96d0e915cffecce3c0dd07b
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:09:45 +0000

    sense sure confidence private guess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
