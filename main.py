9e2d2360afbb4b2c6bf3ffb885c0920ae471698c
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:52:40 +0000

    consult display not disgusting announce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
