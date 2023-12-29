9c5dd54971c3de83a9fa59ce8c7796b231850923
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:27:38 +0000

    switch size offer week narrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
