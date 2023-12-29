ae69e0c28b09e64ebc3cd6e61ddb90fdc56a0ba2
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:16:37 +0000

    return her popular secret whichever

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import random
+print(random.randint(0, 100))
