fc646dafcb52711899ae3750677001daeeb3d1b6
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:06:29 +0000

    agree turndown borrow sudden effort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
