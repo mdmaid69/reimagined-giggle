f48c802c684eaa57db80529d14747ac262a6f2e0
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:52:07 +0000

    preparation press achievement wide anybody

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import getpass
+def get_username():
+        return getpass.getuser()
