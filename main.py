fa2deec8a16a0c82c1042121fd34887999add10a
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:59:53 +0000

    woman border tonight occur quick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
