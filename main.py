b0af9ef93abb8b29fd7364ef46f814498bd1d28e
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:08:19 +0000

    giveout female quiet man dark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
