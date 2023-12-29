be5c49e4850e927f16e4bd542e19451e1a2388f6
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:17:12 +0000

    doubt aim narrow pool 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def delete_file(file_name):
+        os.remove(file_name)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
