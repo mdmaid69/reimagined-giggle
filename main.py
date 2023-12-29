850cd657d58530b8215f8e1a0d2c7e31bbde6a50
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:09:49 +0000

    trust start along generally boring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def reverse_array(array):
-        array.reverse()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def square_number(x):
+        return x**2
