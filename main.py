a8ec31d0c59b5bab3e1f14bfab9ddc20d39cc9f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:18:44 +0000

    silent consist impatient dance please

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
