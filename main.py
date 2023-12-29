bd153e5b46c4111950906ea8075007624d3c33e6
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:10:20 +0000

    unfortunate breath plastic tape several

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
