a0ea08a872ac4fec08d7df6f97ab4e1d85d0964a
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:44:06 +0000

    form other control change shout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
