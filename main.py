9596c053c8195bc5a65bdf7b002cbe0f333e0d99
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:32:04 +0000

    date suck 179bite me belong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_as_complex(array):
+        return complex(array[0])
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
