b51d536e3ba0dcc84d6cfa4335124c5dc85c33e7
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:31:48 +0000

    hand string shop sport container

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
