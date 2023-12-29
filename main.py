df51fa79f1d0c5b9904fde0893da076af7de9a87
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:14:23 +0000

    block flat development ready all

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
