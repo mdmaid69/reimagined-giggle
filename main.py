cee4e91789494e78f9416b4ca5eacfd7e2a5f032
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:07:01 +0000

    ever store soft slice convince

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-import array
-def append_to_array(array, item):
-        array.append(item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
