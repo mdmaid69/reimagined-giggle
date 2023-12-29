bbf74d7d3c8457015c9e0c75412e060e376aeb03
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:06:22 +0000

    rest 564encourage complicated likely church

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_base_name(path):
+        return os.path.basename(path)
