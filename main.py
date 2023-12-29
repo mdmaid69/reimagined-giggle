b51116c16f806eb722a6d705bc3f80bdfcda9bab
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:15:10 +0000

    early week in centre clean

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
