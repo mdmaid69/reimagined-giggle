9534990e1004ab1c5371fa318e8982a6df31733c
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:06:08 +0000

    range since talk lie present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
