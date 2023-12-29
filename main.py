d3fc07ccce6673cc37bc2b50e22095671e4b773f
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:03:28 +0000

    debt great apart valuable strike

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import datetime
+def get_current_date():
+        return datetime.date.today()
