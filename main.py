7d3ff3bf8922acd5a3f149e1e6cc6c9238a60ae3
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:14:33 +0000

    fit class fair until pair

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
 import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
