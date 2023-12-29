fd1859ecea9e4544892f3a770f47352ff48d714c
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:53:31 +0000

    foreign region please recovery order

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
