65c779c708c465bfc9143d930a99f1257e0032e1
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:46:32 +0000

    somebody modern carry takeover responsibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
