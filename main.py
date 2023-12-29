940538cfa90a4b8756935ccae3ded8302f3b0854
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:29:32 +0000

    miss cook walk pain send

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def convert_to_octal(n):
+        return oct(n)
