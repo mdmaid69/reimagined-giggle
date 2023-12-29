f286e9c02f0aa69fa705f520429cb9219c9a0387
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:48:27 +0000

    treat score suggestion repeat plenty

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
