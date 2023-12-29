c30712a38c7e5518c466acbcfad8e401c76d96a2
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:52:32 +0000

    quarter shadow bringup seem suckme

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import sys
-print(sys.version)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
