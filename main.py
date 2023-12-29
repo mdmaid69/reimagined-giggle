d4869fedb34cafc5a8601f83b286d9b1b87ec0b9
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:18:36 +0000

    talk they recovery about present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_list_from_array(array):
+        return array.tolist()
