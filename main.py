fd7274b2173ff416d2f69f225df7381b6f4dea2b
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:31:38 +0000

    offer split show urgent comefrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
