43673b52d23e491da664a3f79d1361e04733eaa7
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:18:22 +0000

    occur less heart like minority

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
