b4f0960cc7867baf66d54a8d8251c319195a6434
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:46:38 +0000

    limit final store return doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def convert_to_hex(n):
-        return hex(n)
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
