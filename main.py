9b5d32e6cab72441c285786a46a858c1c748aba6
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:48:19 +0000

    own connect person pleasant path

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
