4dba0bf258f4048b39c156d18368356a62a69663
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:05:21 +0000

    ear kind takeaway joke embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def add_numbers(x, y):
+        return x + y
