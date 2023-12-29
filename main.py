c1aa7f0f94e840bc2b31d8c414fc374885fb9414
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:05:31 +0000

    cut runover throw basic past

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
