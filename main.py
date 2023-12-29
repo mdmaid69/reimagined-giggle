d5f2ace0f379329930c864ef6ae6c7170edcbe4d
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:34:02 +0000

    fond ticket invitation curious play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
