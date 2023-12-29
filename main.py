074512886320fb4f183827fe31069b2b09594002
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:17:24 +0000

    disturbing something objection ride present

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import random
+def roll_die():
+        return random.randint(1, 6)
