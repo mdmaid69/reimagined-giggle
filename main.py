f7f1b6c2f8a71195d3ff2484f88e66d384963de7
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:52:57 +0000

    process know means feel confused

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def cube_number(x):
-        return x**3
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
