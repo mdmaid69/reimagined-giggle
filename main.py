ef7d547aaae2003908074437355f29f03aaa79cc
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:50:51 +0000

    safety challenge song moral upwards

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+x = 10
+y = 20
+print("Sum:", x + y)
