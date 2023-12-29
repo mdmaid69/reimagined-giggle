d8837a267296366b15771f494e1b21ed26440db0
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:33:05 +0000

    back sign destroy callat gather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
