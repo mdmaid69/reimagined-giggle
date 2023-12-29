add1c16dd3bfbe9dd43a189d9eb5401c51a8983b
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:10:06 +0000

    low offence polite yes friend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
