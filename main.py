a5222caa2442ffb3f2216ed9882fa9be6788db58
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:45:48 +0000

    properly official comment turnback 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
