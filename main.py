ac67996cfe4a3259d90cf84fa4715781bae650df
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:00:14 +0000

    sudden drive plenty develop laugh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
