4f7f3c5640dc53ee3d714a8d18260bfd33fef880
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:14:43 +0000

    progress needle why letter march

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
