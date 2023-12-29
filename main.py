357076eb6ead28d4cf8449f5cab97325e0565880
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:15:43 +0000

    communication its considering child visit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def divide_numbers(x, y):
-        return x / y
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
