d3994c464da09622b0c7ae6170f973da9f886e68
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:49:44 +0000

    forget every doubt crash turnaway

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
