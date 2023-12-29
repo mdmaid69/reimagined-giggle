6a8dc6e25c9b7ce4b9e5deaf59f35ecb1e521aa5
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:40:09 +0000

    such breakthrough backwards scared slow

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
