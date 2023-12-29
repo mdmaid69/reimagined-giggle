72aa160cd3f62d82b4564036398e8c607f76672f
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:20:34 +0000

    hurry approval weak sharp allow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
