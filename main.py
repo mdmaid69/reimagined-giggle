7d260963b033d89267cd80c27c06ebb38ea0a90f
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:47:39 +0000

    lean remove image duty list

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
