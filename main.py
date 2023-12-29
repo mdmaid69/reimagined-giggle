67707413179d5f1d852b0339adb190737afc2107
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:07:51 +0000

    card aim suck various often

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_priority_queue():
+        return collections.deque()
