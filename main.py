4c680aac7aaec6b4d1f30e624255652058c57ae3
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:56:03 +0000

    outside label ashamed sign ago

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
