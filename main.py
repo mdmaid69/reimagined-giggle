7c7403dd8d07416b7d01cc2bb2e51f0b52d3b58b
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:02:28 +0000

    father achievement especially its freeze

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
