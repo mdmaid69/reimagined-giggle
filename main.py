26cda9946c34818adde1c26183130d2f25ce9817
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:19:19 +0000

    example petrol rush smile wind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_list(array):
+        return list(array)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
