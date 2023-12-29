f889f8ededbc7ac9c77342896eb30be797c9137f
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:26:22 +0000

    breakin out confusing 570enjoyable pleasant

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
