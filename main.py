330e5cf3197d161fdefb427605193479973d2572
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:20:01 +0000

    storm know free unit exist

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import collections
+def create_priority_queue():
+        return collections.deque()
