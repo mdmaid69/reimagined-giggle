8b6cf04d76f4ed6e593446846968dd34269f87c0
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:10:54 +0000

    beautiful position why square after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
