f1ac773f63c06121786339cf9f52c9c4450b898c
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:35:04 +0000

    invite no pile make vision

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
