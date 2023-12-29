fe36b3eed5b4771027dbf89bfbd9b38a22177f80
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:54:11 +0000

    hurt pause successful main similar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
