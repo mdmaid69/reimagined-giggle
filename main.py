5306e0ef91df30721d0e5a1cfd7f3f6b183c12a2
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:27:43 +0000

    wish rather national centre expensive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
