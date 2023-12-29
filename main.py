d331c0e83e301beab6158ddc5e21ac14781304de
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:44:56 +0000

    runout time contrast smooth command

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
