be721567b9ca449a094863a205d9f37c34b2b2ed
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:13:25 +0000

    commit run previous birthday loud

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
