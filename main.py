4afd66fa81a54e21c8d5ceba58bcf8b17c8d0ba4
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:41:55 +0000

    question us let false mean

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
