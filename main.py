8d86b5e558d9f94af80829f75de57fd3fddffa07
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:06:16 +0000

    development decision smell birth protect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def reverse_list(lst):
+        return lst[::-1]
