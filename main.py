3be16f28e95341c6c8293b7f1e4536d2bbcb84ef
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:01:35 +0000

    bear independent winter opposite turn

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def convert_to_json(data):
+        return json.dumps(data)
