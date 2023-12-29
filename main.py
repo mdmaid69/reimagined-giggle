1a7d84483b087931b0a7c016d0b0e2da5df8c86f
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:23:38 +0000

    laugh nowhere influence cry neck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
