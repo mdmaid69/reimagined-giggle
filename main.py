707f94ce45df43cee47f972af5e56ff74ecf5f86
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:16:36 +0000

    stamp total success callout power

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
