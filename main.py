35a3566bca38af98914974f330ec93b8f0b30d34
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:39:47 +0000

    pot zero since divide as

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_floor(x):
+        return math.floor(x)
