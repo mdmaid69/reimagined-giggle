3a1cda46adbc43b8dc68187b51e465ec5f752863
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:14:03 +0000

    middle leg admit frame instead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import time
-def get_current_time():
-        return time.ctime()
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
