942808494bac0e9729aa32cf6ca56e7ab0f41391
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:19:23 +0000

    ticket after separate expression lunch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-print(math.pi)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
