156d095103ff32396efe392d438304204ab69cae
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:07:22 +0000

    loan miss side along teach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_item_count(array, item):
+        return array.count(item)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
