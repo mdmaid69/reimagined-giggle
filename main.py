d7ab4e845e6b70f453e7448567b4e6a51864c096
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:40:50 +0000

    keep with uniform worse getaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  def convert_to_hex(n):
+        return hex(n)
