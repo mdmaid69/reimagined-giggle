be4347fc13e3757e0baef8d0c1874ab691c5c7ea
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:41:06 +0000

    steer earn split arrive finally

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_acceleration(speed, time):
-        return speed / time
+for i in range(5):
+        print(i)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
