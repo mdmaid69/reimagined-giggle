132e966da3902e495ce290419ea629a90fd249c6
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:18:26 +0000

    swimming Bb confidence leave alone

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def reverse_string(s):
+        return s[::-1]
