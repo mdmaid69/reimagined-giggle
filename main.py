07e05fe27efeff5a0fbfd44fcb0a72aae8900aa3
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:53:16 +0000

    development sign polite turn estimate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
