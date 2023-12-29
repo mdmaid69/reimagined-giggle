596aae03bcab2324ec1444f543bb19a8e1d81994
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:30:29 +0000

    hole about ride invite bag

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
