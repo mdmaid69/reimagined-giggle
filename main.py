799b55f4fbb2da4cea89ff6b3e30e5baa92f016f
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:18:36 +0000

    permission perform add ourselves underneath

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
