ce7b180e38d4eb763932b00401dd81c0c0022994
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:54:10 +0000

    strange breakin truth family top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
