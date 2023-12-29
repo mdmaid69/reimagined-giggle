8dc06bffcc80a6c63f056839b5d31ba7784771c5
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:10:11 +0000

    happen flow rank related dry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def add_numbers(a, b):
-        return a + b
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
