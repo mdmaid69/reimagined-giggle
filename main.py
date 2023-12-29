181b1dcf9a25b2740ec3d379d0de334b74ddc263
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:09:05 +0000

    ahead little information complete round

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def greet(name):
+        print(f"Hello, {name}!")
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
