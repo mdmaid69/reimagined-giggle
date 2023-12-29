890f73109f90611f28881f01ab610d3ddf6b0099
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:38:13 +0000

    breakinto one weigh foreign week

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
