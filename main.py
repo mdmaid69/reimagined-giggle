7fc495d4d0d3a296126f3e4b8317785148b0fe6f
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:03:25 +0000

    wet foot try search error

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-from collections import Counter
-print(Counter("hello world"))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  def convert_to_hex(n):
+        return hex(n)
