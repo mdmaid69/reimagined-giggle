5dd882c2423a7159e774ac9069aacd99834f0e74
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:53:36 +0000

    hate scared concerned profit telephone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
