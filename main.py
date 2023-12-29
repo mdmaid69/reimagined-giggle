d56e3112aeb9708b769d6220d6ab8ca5ecc9c046
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:17:52 +0000

    private abroad regularly play Dd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
