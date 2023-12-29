ea2773c02d0b0c23f5a7e9520d678b7c0dd80d05
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:13:35 +0000

    worth account train exam opinion

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def cube_number(x):
-        return x**3
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
