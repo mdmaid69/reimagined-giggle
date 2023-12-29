d5d293bf1428d66abc05fb2bb2f16610a6f8c036
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:32:39 +0000

    spin stand dear hope gather

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
