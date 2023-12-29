7709c9faf0076f53085d89a664d6346f9c4f7ce6
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:06:45 +0000

    mind rain condition demand offer

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  def reverse_list(lst):
+        return lst[::-1]
