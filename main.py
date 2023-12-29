fc822e084f61889205b67f92434827500526909c
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:08:21 +0000

    emergency callat also action while

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
