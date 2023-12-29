f3852180dd0a6f964102307e22d1c1559c7368bc
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:45:22 +0000

    comedown stretch independent shall sale

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import datetime
-print(datetime.datetime.now())
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
