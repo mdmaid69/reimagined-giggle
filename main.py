4aa0e0ad524dd77938ec00f2d941e74a8bac360b
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:07:27 +0000

    relax whistle second black uncertain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
