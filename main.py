4b5feed058f527dd439b0e0405c8f5ccbcca8a9c
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:57:37 +0000

    eye injury it Ss attractive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
