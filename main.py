b6100a5e15fca8a9ab802b284afda874ecc3c38f
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:38:03 +0000

    represent expensive problem 572enter authority

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+i = 0
+while i < 5:
+        print(i)
+        i += 1
