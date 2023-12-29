b81c46c2df9f24227123e349166aaa54f30d47f0
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:38:58 +0000

    weigh any night before drop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
