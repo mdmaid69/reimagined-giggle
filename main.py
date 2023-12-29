f2fd15132913e41ae02fc27617fcf4e2c49caddf
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:49:41 +0000

    kick nose piece or improve

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
