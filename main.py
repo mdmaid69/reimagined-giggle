ba9c14ad93b068e14fd7f8eb535cd3066b91315a
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:53:06 +0000

    lookaround mother wrap safe notice

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
