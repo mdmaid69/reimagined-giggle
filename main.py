44e91e7486dd28f0bf86410d45deb97e1bbbe78b
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:07:10 +0000

    connected sort official demand tough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_cosine(x):
+        return math.cos(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
