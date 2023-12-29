201a0af76fa8b46014c8c7f2e149c603c5a98b07
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:40:08 +0000

    always recent thinkover apologizeapologise area

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
