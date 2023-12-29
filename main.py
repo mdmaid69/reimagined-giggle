1645f13a41c1fa51863162e2fa83db35539ed503
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:43:36 +0000

    whisper base yesterday now morning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_string_from_array(array):
+        return array.tobytes()
