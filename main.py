a5c8cff360e7f137d07eac107ace4876f7e9526f
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:48:06 +0000

    answer obvious fortunately roof nature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_current_directory():
+        return os.getcwd()
