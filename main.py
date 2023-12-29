ceabc1c90eca71e2a762f813292ccaf8c110870a
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:04:44 +0000

    give continue impression speed several

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
