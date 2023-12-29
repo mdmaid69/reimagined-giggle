a772b0a06de5fd5d84ed70a8078d3ddc10a01311
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:16:11 +0000

    immediately pretty name turninto occur

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
