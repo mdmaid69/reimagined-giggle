846f52df7e35c8ea7412cf0f559995bee7c8d854
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:56:13 +0000

    letter nothing rarely beat shall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
