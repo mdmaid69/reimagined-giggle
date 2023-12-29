00b315f8eab349b98a1379a18a3af3fab13c9255
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:16:57 +0000

    which anybody average permission would

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("Hello, world!")
-def calculate_pressure(force, area):
-        return force / area
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
