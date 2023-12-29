38b3069dbff200afa3d8f17b27933ce12c8521cc
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:31:49 +0000

    giveback influence arrange constantly side

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_index(array, item):
+        return array.index(item)
