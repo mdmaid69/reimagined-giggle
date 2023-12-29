c45241b047ecc01f83efef8f9d07d3cc334051ce
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:53:30 +0000

    little present neck pin seem

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import sys
-print(sys.version)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
