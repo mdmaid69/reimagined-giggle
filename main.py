7e9f1f3e33d28a28ceeb7b9c9caa4cde676d31b8
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:00:26 +0000

    medium nor lock that apply

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
