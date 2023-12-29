517afb13716ec533b9e3205d75e704f38cc8b4fc
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:38:46 +0000

    like tongue write rain respect

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def subtract_numbers(x, y):
-        return x - y
-  def reverse_list(lst):
-        return lst[::-1]
+from collections import Counter
+print(Counter("hello world"))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
