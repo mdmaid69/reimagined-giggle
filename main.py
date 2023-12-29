832361f3adb7fc4d1c7dfe09081f7f2f14af33db
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:44:24 +0000

    round invitation fun interview trap

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
