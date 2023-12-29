d601e7704997d6bcbda445da288c0c11c5b19973
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:03:32 +0000

    flow yes price getup prove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
