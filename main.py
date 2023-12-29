493547168fa0a78615d6d1ea06e38315d753c345
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:32:44 +0000

    his sore rain likely man

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_base_name(path):
+        return os.path.basename(path)
