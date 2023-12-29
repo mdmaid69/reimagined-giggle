f08b7690f1a164e3219dd1ae91243aca75cafe3a
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:20:05 +0000

    picture whistle minimum central bring

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-from collections import Counter
-print(Counter("hello world"))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
