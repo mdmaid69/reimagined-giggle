083e820e593c3b3e2e287a03f6c3cc5a836bb534
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:59:59 +0000

    lead pure yet concerning favour

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
