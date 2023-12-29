15244d034ad15e6106e7ba4ce8a2e9365d60aaa9
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:48:55 +0000

    private physical load react street

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
