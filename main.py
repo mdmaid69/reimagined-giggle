8cd6a6247b4a751bb5fba83ead5c46798078006d
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:36:17 +0000

    music almost wire congratulate copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
