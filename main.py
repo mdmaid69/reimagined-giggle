fa544fd850aa0d8303e164d4f02ee8a6b941f429
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:41:13 +0000

    father apply house uncertain exact

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
