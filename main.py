39d631778ab1d30045b56f4b499096802f81e88d
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:13:14 +0000

    walk belief voice squeeze company

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
