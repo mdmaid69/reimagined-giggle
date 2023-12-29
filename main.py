bfa36f56d2ce976236cff1a035193759fdef54aa
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:32:22 +0000

    Ee prefer sticky confidence list

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
