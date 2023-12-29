fcfb9b4594d08deb095e736ecf693ae0feab459e
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:46:50 +0000

    makeinto spot knowledge stick offer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
