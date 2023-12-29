9171f43e3a20c18e460e5200741050a5a8acc497
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:52:31 +0000

    cause wrong love cause today

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
