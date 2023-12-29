715b1dfb701818f0a01e98802fa7d384ec0c4ffe
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:36:07 +0000

    block mysterious height force everyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
