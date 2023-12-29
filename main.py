1d12ff09c358a526fe508411d4275aadd24270cb
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:42:58 +0000

    use look pay performance opportunity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
