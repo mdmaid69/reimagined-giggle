9b423c7624c5c6e01a92f244631a1ffc064d8cb4
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:41:24 +0000

    turndown interest you spring thinkof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
