d7f06e7710b2ab24c388bd98d269463f4d3199ea
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:18:15 +0000

    unit contrast plenty wide finger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
