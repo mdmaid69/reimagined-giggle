b5c58df05133ec5fe97ead3c084791f196babf75
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:28:23 +0000

    birth lookback(on) react evidence birthday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_area_circle(r):
+        return 3.14 * r**2
