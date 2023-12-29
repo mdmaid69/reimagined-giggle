f7e03c2e09326ba1305cc18f2a74ea47db150275
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:52:48 +0000

    thank trap failure comment trust

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+print(math.pi)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
