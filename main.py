158571884f9e25b87e61d372f1023cd991b29c27
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:03:53 +0000

    confuse stay laugh broken above

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def convert_to_binary(n):
+        return bin(n)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
