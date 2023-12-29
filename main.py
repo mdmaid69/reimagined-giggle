f2fa0df461ec20da8cea1f03076a7cd2c6cfb942
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:41:05 +0000

    human shake bother sudden effect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
