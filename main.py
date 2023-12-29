a426666ea1ca7e1b5902cfd3a5356c63e5a84d79
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:32:38 +0000

    herself answer block opposite get

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_average(lst):
+        return sum(lst) / len(lst)
