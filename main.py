87cccca6dec3b80b04bfba7043f2fbe480f8f815
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:56:57 +0000

    somehow draw mess big accordingto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-print(re.match("h.*o", "hello world"))
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+x = 10
+y = 20
+print("Sum:", x + y)
