5085eea70615a464c94d7061221670580d069104
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:07:36 +0000

    operation large instruction peaceful bin

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
