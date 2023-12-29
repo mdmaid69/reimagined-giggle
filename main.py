d7b511a9bc1f8cc81c67c0a68058db4414190b19
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:45:50 +0000

    force lean wave not radio

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+print(sum(range(10)))
