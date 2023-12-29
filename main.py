49bde0894e44a8a332d286c45b21eb19b9ed896a
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:46:41 +0000

    relax track behind answer telephone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
