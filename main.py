c5da92b06a5b2dc3e7df33090de305c90c9a93eb
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:12:33 +0000

    among fault promise within interview

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-print([x**2 for x in range(10)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def convert_to_json(data):
+        return json.dumps(data)
