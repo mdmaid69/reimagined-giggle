afe791d19b1d3a749af2d0bf0a9a78a3f0947c71
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:57:45 +0000

    nowhere emphasis passenger impression tired

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def reverse_string(s):
-        return s[::-1]
+def sort_numbers(numbers):
+        return sorted(numbers)
+def find_max(lst):
+        return max(lst)
