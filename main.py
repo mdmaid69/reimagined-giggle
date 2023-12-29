dd5f7e07a8d5bf810d7f3c88ff1f7a4257bd5fff
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:58:26 +0000

    crowded noone noone who ever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
