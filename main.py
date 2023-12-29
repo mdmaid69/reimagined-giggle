e30eea44ad7398fc1bfa323a33ae66b757175fdd
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:52:19 +0000

    ring lesson permission swing giveup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def convert_to_octal(n):
-        return oct(n)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
