9e0d8789e05404980cae3d44d4dc497ec9444e09
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:36:06 +0000

    in it luck disturb turn

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
