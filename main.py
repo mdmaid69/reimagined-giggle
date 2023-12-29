69b6ef86136738e0572b6529b2fe9c2fd52ef56b
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:28:15 +0000

    glad distance government Gg land

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import random
+print(random.randint(0, 100))
