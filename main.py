b542c645c2aedef273318ba98b632031ab13f141
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:22:22 +0000

    bound they unhappy 567enemy you

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def remove_duplicates(lst):
+        return list(set(lst))
+import datetime
+def get_today_date():
+        return datetime.date.today()
