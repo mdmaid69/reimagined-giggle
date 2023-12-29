dd8284d4d6771f62088300b0a0d7484058262c43
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:18:43 +0000

    long dry tune invite nonsense

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
