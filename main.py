310c0632ded1d7a0fd8da49f06809741393559fe
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:18:54 +0000

    check dream before bend building

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
