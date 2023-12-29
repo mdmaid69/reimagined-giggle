26ce280048ab594aada9edcfde8f18e25d79d599
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:51:57 +0000

    choose quiet paper must fun

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def square_number(x):
+        return x**2
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
