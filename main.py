d58e6fe8297167f6790b98faf754ec7c2e905733
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:51:18 +0000

    duty along contrast description offer

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
