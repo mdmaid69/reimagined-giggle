c90c697623925398362cd85bc93fe3a1cbf5b136
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:28:14 +0000

    travel apologizeapologise act further behind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
