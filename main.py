d76354801a6cb083392a25e6d977986a10958dfb
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:26:53 +0000

    level shut wander feed total

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def is_odd(n):
+        return n % 2 != 0
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
