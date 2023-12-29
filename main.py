b1c2462e9c85471213998c0dc0d8e57b98ad83b9
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:55:02 +0000

    contrast old promise turn season

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def get_array_as_str(array):
+        return str(array)
