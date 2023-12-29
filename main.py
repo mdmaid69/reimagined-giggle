454409cc479e2d59690cd30aaed8ce047a3e885c
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:08:56 +0000

    our opposite immediately platform report

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import platform
-def get_python_version():
-        return platform.python_version()
+import getpass
+def get_username():
+        return getpass.getuser()
+  def convert_to_binary(n):
+        return bin(n)
