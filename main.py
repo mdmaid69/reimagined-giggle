d983cd20f35286bd0e6a54ae74f037c06722b790
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:13:19 +0000

    doubt factory never emotion prefer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def cube_number(x):
+        return x**3
