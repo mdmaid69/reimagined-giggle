d8b115b4e0bb6f63b6bbb009b133b7a5f4737137
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:56:42 +0000

    effect own regularly impressed responsible

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  def convert_to_octal(n):
-        return oct(n)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
