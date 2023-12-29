db55605bf4c8e0541d0c91bf1e8a09273475ab9a
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:35:56 +0000

    object ground trial workout answer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import logging
+def log_message(message):
+        logging.info(message)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
