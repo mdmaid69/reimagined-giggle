3e90e5b6da75ddf69a8b7936adabe0f6df72a343
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:07:44 +0000

    anything our hurry cost near

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
