b8afe640ddbdd9b744dad29027a7931d1473611a
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:45:06 +0000

    vote under listenfor promise prize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_max(lst):
-        return max(lst)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_length(array):
+        return len(array)
