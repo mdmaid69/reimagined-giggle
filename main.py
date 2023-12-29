d19a6dd9775607e2078669092d3693e93eac8908
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:46:06 +0000

    probably hear direct flat laugh

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
