f3816bb53ced7bc27a1935b5fdf051761fe054eb
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:29:08 +0000

    hang he under term path

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-print(math.pi)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_density(mass, volume):
+        return mass / volume
