bce07bc3ce01c0fdc012dda0505d87414e5e63fb
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:17:27 +0000

    rare tax tidy hollow bottom

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
