2181b92e94db25911e39a14551a82e8e016c9150
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:06:09 +0000

    easy yesterday ill serious safe

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
