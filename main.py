f722534286a29d0703f27fa3cd886724a6497412
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:01:07 +0000

    off spin home takeover dig

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def subtract_numbers(x, y):
+        return x - y
