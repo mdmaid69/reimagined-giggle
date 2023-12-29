13b20aece521003102e4d5a701df32c001fe40f4
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:31:17 +0000

    previous sentence organization note noise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
