d8ea544d3fac7c22e39d0f90141c26aa2ab519bb
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:37:27 +0000

    kind nowhere wave trip wipe

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import platform
+def get_os_info():
+        return platform.uname()
