2df38dfe3033db9d6d788422dd801e6adc8e3505
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:32:45 +0000

    warm develop tell behind miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
