e03fc7ff9b069dba549a8a173da300c559cdb8fd
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:27:46 +0000

    need neither politics mine order

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
