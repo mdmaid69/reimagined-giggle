720f531edfa6d2bfefb6636ba10384d8155c6a21
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:21:32 +0000

    giveout announce to forget drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
