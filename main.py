21f618f516925522a26899a7c8b5f47252fe171a
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:19:09 +0000

    pin exaggerate through during sharp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def find_max(numbers):
+        return max(numbers)
+import array
+def get_list_from_array(array):
+        return array.tolist()
