2a8b745baeeb0e8fae2deefbcc057668c112b11c
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:48:43 +0000

    sweep tragedy cry goout cry

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def convert_to_hex(n):
-        return hex(n)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
