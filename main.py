8818fcb60d45c6f6297786a0db690ad1d7601d10
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:28:53 +0000

    Bb 571enough promise connected complain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
