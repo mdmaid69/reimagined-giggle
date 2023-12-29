1152028fe86e101187fd0467995d857a89613d11
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:29:58 +0000

    demonstrate entry front just factory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
