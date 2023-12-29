39a5e3e0a3fe2b5d354567e87e1e182684591768
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:15:29 +0000

    turnup unlikely note suggestion tool

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def reverse_list(lst):
-        return lst[::-1]
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
