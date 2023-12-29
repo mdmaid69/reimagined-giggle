aa82c1be2dee2174bd626047c8a504dd76fd077e
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:00:42 +0000

    stone record interest discuss unpleasant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
