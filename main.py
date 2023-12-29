c8800db70b6c22b0916944ada22a079932c85bc1
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:42:19 +0000

    square job Ee avoid shut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
