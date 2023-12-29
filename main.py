725d75692bb7c5521f4fb1d32e4c20ada92309ae
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:49:03 +0000

    exaggerate pick fond naturally chat

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
