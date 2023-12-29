af65d4489a83dcf9fe0510cf4c9ec81ff970d34f
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:46:46 +0000

    eager appearance book previous note

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
