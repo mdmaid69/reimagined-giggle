bffb14272d7b8891a6d45cc7bb49d3adf814865a
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:18:22 +0000

    evidence bother compete sell put

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
