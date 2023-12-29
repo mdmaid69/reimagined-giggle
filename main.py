1db4ac4fc3e8bd72431e6433c9ad41159af70e76
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:07:34 +0000

    disagree positive cost flavour limit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-for i in range(5):
-        print(i)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import os
+def change_working_directory(path):
+        os.chdir(path)
