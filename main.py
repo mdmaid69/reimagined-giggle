21008539b7b4a43a04d107d595b5675fec6d1a9d
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:41:47 +0000

    imagine group lie partner ignore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
