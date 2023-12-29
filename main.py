50e09fad6cab7254e450ec3189d5c63bc9d3e3e0
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:57:48 +0000

    otherwise speed possession round future

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
