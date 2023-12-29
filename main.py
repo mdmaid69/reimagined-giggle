7cef8ca781e0d841b6a9ef32f33fadbd7e65f1cc
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:50:44 +0000

    anything hang call introduction hate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def reverse_array(array):
+        array.reverse()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
