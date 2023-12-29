4223616d5a240c639deae4ff24eca1bc51f57a7f
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:34:35 +0000

    delight moon reject oil main

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import sys
+def print_python_version():
+        print(sys.version)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
