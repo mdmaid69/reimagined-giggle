53c172cd1f6f9fc25983d14a8e0c8ed3dcafd61b
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:11:42 +0000

    line dick fault space check

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import datetime
-print(datetime.datetime.now())
+  def square_number(x):
+        return x**2
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
