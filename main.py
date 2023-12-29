0513a75b86913fc99d51e05baca0c3264f3cd2a6
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:34:36 +0000

    complete about workon issue dinner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_as_bool(array):
+        return bool(array)
