0518b9791801199c6e486415363524f05ffd5737
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:33:32 +0000

    change pause accept however point

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
