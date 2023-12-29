7d78e519ebdcfd77ee4e0570b3837589658b1809
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:10:33 +0000

    upset model treatment better like

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
