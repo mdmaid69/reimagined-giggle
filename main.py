3ea00e2e35d32e3f46c4e14bd2c7336484b23c3c
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:29:54 +0000

    afford pencil comfort bringdown order

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import platform
+def get_python_version():
+        return platform.python_version()
