a55cd79ccdd00ffab59a484c4a002680565bd02a
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:49:50 +0000

    colour black in for hear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import sys
+def print_python_version():
+        return sys.version
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
