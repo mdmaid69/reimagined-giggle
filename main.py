8b55cf3113fd4d2a8f2ecdffba25623dcbcaea67
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:47:23 +0000

    experience spot embarrassing judge second

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
