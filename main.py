6835f7de330fdc5fa3a828343eb222ec28eb921b
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:39:25 +0000

    murder difficulty visit concentration whether

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
