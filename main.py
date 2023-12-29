fbff3a7d3c80581704c8e1857d5d75772e0f82b6
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:21:38 +0000

    platform certain office recovery gentle

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+print([x**2 for x in range(10)])
+def calculate_acceleration(speed, time):
+        return speed / time
