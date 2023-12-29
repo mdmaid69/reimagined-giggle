954279e8b7ee4ff37f9ab70ff12365568dec87a3
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:29:21 +0000

    must to smile mass partner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
