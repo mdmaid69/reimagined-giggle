b6a7432c9a9db6f8dde634cba1f24f3cd580d7f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:31:42 +0000

    look belief partner dream gift

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
