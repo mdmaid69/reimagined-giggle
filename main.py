3eb03afd9eb2e68e04a90e28c3179323926436d2
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:02:37 +0000

    hard married copy sight medium

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
