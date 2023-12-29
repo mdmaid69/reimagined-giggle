4b321b7b6a7c5d31f8c9065797b2e986d7b6fd3f
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:20:53 +0000

    for 572enter swing across shall

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def get_array_length(array):
-        return len(array)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
