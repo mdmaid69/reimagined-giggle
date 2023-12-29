06a770aeba4ea3964ab2b38642798d6242b28d04
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:38:04 +0000

    oil contents mass feed skin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_bytes(array):
+        return bytes(array)
