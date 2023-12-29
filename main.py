50e227385aa018f803c9ac5424825b0b7fe11834
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:07:59 +0000

    present pretend obvious wheel distance

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
