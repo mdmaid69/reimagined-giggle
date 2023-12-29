6d429f4097d7831bb2b18f5b231ca2c92ae63952
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:28:56 +0000

    Vv note sore learn confident

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import time
+def get_current_time():
+        return time.ctime()
