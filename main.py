a8ed9cc1a701be93d85c652be46e6a928f7136cc
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:04:58 +0000

    professional because circumstance sudden sink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
