c8054ff9b728a91e177217b10325453b78dfb1f8
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:19:31 +0000

    for platform stupid light frequent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import math
-print(math.pi)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
