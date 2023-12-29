27fae12c4a7e0a074099d578bbe114e46bd6e557
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:57:57 +0000

    test neither under hour old

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-print(math.pi)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
