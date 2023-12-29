031dca2fb0a240dfc30ba09d3d523018c680c080
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:43:59 +0000

    project open treat battery traffic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
