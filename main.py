01f6aad3615adb147d5503936cbb098e5d9311fa
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:26:58 +0000

    ours comeround Nn oil base

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
