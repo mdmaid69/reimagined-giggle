778358f84ec4f2c3b53030cba4e4becf5958a724
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:52:21 +0000

    thought ring side nose appearance

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
