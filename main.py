572dfb7e40f4724f463fe0f971ea9d5173c5e8fa
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:54:33 +0000

    correct regular no bush wind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_power(work, time):
+        return work / time
