2a7aba7e58746494087902612a8905ab2f7fb951
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:18:03 +0000

    today fetch tidy if rush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
   def get_file_size(file_name):
-        return os.path.getsize(file_name)
+        return os.stat(file_name).st_size
+def is_even(n):
+        return n % 2 == 0
