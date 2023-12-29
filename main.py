9ac74885e0850941155b5f5ad59ec03d277fe1b6
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:50:47 +0000

    damage science getup unkind familiar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_speed(distance, time):
+        return distance / time
