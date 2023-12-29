3ef835b6a4d414f4a8bc64e6cc90bf60453e2e1d
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:24:28 +0000

    grateful bus persuade around suffer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import random
+print(random.randint(0, 100))
 import os
-print(os.getcwd())
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
