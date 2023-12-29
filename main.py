3ab003c901e870bc5bfb87fe56564f6c81975af5
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:17:31 +0000

    along communicate amount walk ahead

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_acceleration(speed, time):
+        return speed / time
