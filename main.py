3fff27ba40d7550002d3171a737a695f8ecd2930
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:58:18 +0000

    tongue drink interested combination train

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
