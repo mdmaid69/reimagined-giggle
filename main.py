befde94c47cc9df346981d95b67ef245ea134693
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:30:44 +0000

    move above begin Ss thinkover

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
