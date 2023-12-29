1ddc6de885c0082f64ac5a9e77e35dd84c64a175
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:36:14 +0000

    doubt vote have debt appearance

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def reverse_array(array):
-        array.reverse()
+  def count_elements(lst):
+        return len(lst)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
