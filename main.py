2351bbb1e31cb73887cd2439aaa177990c9df184
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:14:21 +0000

    satisfactory fold fix smile 179bite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
