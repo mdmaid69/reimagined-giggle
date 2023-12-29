a65c939ccea06406dd66db198904bae1e41e23e5
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:12:33 +0000

    would cold light workon speech

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
