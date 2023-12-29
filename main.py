d60f3507469ba0076b5265e53d5270b8ca02be04
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:21:05 +0000

    thank crash pocket do band

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import collections
-def create_counter():
-        return collections.Counter()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_work(force, distance):
+        return force * distance
