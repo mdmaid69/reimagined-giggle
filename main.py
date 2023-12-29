2208eab24fb03f92bc24633d2cbbf6b0e7a153a8
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:10:03 +0000

    serve normal 570enjoyable tax love

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def is_even(n):
+        return n % 2 == 0
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
