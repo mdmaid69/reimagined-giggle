f58ec8b294b91ce3c6af591698973a8b27a6d103
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:57:14 +0000

    about speed more spare they

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+  import os
+  def split_path(path):
+        return os.path.split(path)
