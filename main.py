73a17719982714dbcbe91098f1e9a5a3e01499e7
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:16:08 +0000

    rubbish 572enter want regret regularly

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def subtract_numbers(x, y):
-        return x - y
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
