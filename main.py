a5849457866ab446f7bec4fa0df5449c91c4a51c
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:01:12 +0000

    tune present wipe mind rarely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
