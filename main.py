6652cc8cb4ff92a60cfae3e8a512941671f93776
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:04:55 +0000

    he variety image Ii steer

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_force(mass, acceleration):
+        return mass * acceleration
