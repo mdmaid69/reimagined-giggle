85e6b10a7daa603a20fc9b25c4c7fecac7803446
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:26:45 +0000

    surprise lookup pocket father makeup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
