ebdbfb8d982ed7550014e47ee094c3b7e05a2d48
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:29:30 +0000

    judge equipment brother arrival interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def square_number(x):
+        return x**2
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
