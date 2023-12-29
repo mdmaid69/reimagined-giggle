47be22a678386fdbfa367870c2bc44f4ad6423de
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:42:55 +0000

    properly many tape until lift

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def sort_list(lst):
-        return sorted(lst)
-def calculate_power(work, time):
-        return work / time
+import math
+print(math.pi)
+def calculate_pressure(force, area):
+        return force / area
