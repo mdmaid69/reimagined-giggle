15cfb9d135c56d0906b020354a190a804bbf3548
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:16:15 +0000

    afternoon transport die great truth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
