5de24abbae0d8480d7b9a34cefabf57f8a2131ce
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:49:40 +0000

    chief lookup possibly pinch purpose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_as_list(array):
+        return list(array)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
