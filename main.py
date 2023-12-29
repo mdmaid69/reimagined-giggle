a64baa748cb482a87edf09fe0e235b0aaded51c7
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:30:26 +0000

    giveup uniform breakfast within cope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
