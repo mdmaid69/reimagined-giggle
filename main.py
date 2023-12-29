fb5360d6ff5bd26549f6de42c503f5ddee9d5413
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:17:54 +0000

    money neither Nn wide border

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_area(radius):
+        return 3.14 * radius * radius
