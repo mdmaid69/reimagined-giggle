64c48b28d46c16493978a1576d7a300eea8ee819
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:42:32 +0000

    error quit job prefer attach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-print(math.pi)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
