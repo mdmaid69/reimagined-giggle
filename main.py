14ef36906a52b601164a2a84871e74ed873747e0
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:50:57 +0000

    earth bang kind major replace

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
