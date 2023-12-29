6c3f5cb16de88f627a0aa09e7bdd03ef0bdad658
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:09:21 +0000

    factory across turn accidentally politics

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def reverse_string(s):
-        return s[::-1]
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
