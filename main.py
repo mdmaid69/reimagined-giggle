f0fadb691c4c9ef4858b46e6940de11c9adef070
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:17:28 +0000

    temper cough keepaway effort worth

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
