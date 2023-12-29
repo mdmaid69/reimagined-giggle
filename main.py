c3a76113e88785d538e9caa97f11b2786d0a2757
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:36:27 +0000

    collect bored activity dress silent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
