437fd8f37b759761ad732585d1daa6a23fed61ed
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:57:05 +0000

    runinto disagree wealth memory people

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def reverse_list(lst):
+        return lst[::-1]
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
