12f7c946a34ba2c906267ddbfbcc49172a2bb712
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:33:34 +0000

    sport very unkind excited lay

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
