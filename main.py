d15f413165a863436d6a3a6129ea4142a856133f
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:53:41 +0000

    average us 571enough connect judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
