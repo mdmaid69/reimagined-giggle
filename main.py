1a5044cd07498d6dd38a72aee843445a4001ffe3
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:25:42 +0000

    secret need believe mine else

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
