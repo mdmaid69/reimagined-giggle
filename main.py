72143ec27734fc55d0240aa3f55200bc0797dd94
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:32:40 +0000

    light allow front trust pretty

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
