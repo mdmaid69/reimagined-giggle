33ffb3ad271902f5c9e5ed4dc136b37122d7a0a4
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:08:09 +0000

    involve pinch hurry consider cough

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
