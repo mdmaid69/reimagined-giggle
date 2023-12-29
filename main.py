cce2eabe90e8f7cf4b17e4223ded0f6a96e8ffe8
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:01:23 +0000

    fire appeal gather recover cold

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
