95eb06c8239f31bc41c7d9818880e40fa817e254
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:33:20 +0000

    once unfortunate building television cross

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_min(numbers):
+        return min(numbers)
