1ca6f7e60503e58771644484a1a6a2245722b72d
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:11:59 +0000

    lead why platform actually anybody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
