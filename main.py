38e929a183eb10344ebc709d804ff816d4dba0e4
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:49:41 +0000

    goodbye test correction below regular

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_as_set(array):
+        return set(array)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
