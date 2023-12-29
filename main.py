a46059602cf6d5ec6bac779122666607ebf118b2
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:31:38 +0000

    any stone impressive my team

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
