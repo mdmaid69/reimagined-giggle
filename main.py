8a5d54e9be5d74819e1e1b4ad66781f26d0682fc
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:50:18 +0000

    mood explain clothes contribute cruel

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
