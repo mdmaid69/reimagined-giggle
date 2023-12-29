19c94eaa0ea6f1acf0a106624f3c769483c04263
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:47:17 +0000

    lookup satisfactory 179bite rarely false

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def reverse_array(array):
-        array.reverse()
+def get_bytes_from_array(array):
+        return array.tobytes()
