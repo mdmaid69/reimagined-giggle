e8830d6e62e76311d4b7d209e8bb2db3b8b1db48
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:56:40 +0000

    snow and week path aim

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
