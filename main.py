954cc99368c60fbb7f3c88c698ed619193dfd06d
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:46:42 +0000

    suit term anywayanyhow business group

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import time
+def get_current_time():
+        return time.ctime()
