2cc2caeaff5ffd1c10b1963a2c75b510a53bc7a2
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:43:49 +0000

    fairly opinion geton company during

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
