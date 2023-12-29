53e0e2e8dd32d2f61931414ef5b45cf7ac83eae2
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:42:32 +0000

    schedule shout will expression concerning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
