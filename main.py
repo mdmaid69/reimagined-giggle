09c8e6e32490772e5cc8504848af78f183c34493
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:01:01 +0000

    weigh but steep picture eager

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
