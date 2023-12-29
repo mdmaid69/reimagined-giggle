9f89afce05d30ec6699c4db0d1878fcedcea182f
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:49:32 +0000

    famous acceptable conduct during old

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
