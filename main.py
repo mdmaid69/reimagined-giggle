0f2b8b97eacc21b142e6121478da41a5c75fd846
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:24:45 +0000

    maybe contribute holdup game bus

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
