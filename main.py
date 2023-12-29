6ab876f114ab96a0520e61327026ce207a90484a
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:27:46 +0000

    catch steep tight spare off

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import datetime
+def get_today_date():
+        return datetime.date.today()
