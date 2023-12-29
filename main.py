495a49d6071aa12f7f2168db439ded3720974450
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:32:40 +0000

    not hand wire block hair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
