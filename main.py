f68cbe5ff8ef11159d930809b2883bb06d9913d3
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:21:27 +0000

    bank naturally point smile sit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  def remove_duplicates(lst):
+        return list(set(lst))
