6e240a46ae96c7972f51a3ac31b34520f6fae14b
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:01:14 +0000

    sharp national sting attitude welcome

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_user_dict():
+        return collections.UserDict()
