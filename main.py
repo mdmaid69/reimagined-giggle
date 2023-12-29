d527afc652a548434f55f05bd1e2b224474f0836
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:11:52 +0000

    nonsense stretch advice such race

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
