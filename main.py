c62e7e5821c02dbd980487c3c220f7decf4bfe68
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:20:42 +0000

    show answer statement pattern relax

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  def reverse_list(lst):
+        return lst[::-1]
