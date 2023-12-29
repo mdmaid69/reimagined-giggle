dfbc2fb0ab238d98bfb5d874f638598dc29e2e3b
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:57:50 +0000

    protest exit long whenever interruption

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
