fb5a835c69cf26daaf8db9c5e587f4a32b73cda2
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:36:23 +0000

    exaggerate mention downstairs respect hers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
