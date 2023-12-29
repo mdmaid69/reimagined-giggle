30d79cf950e87a24b9da97b57387f3d6cccd4879
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:50:08 +0000

    estimate bringdown swing list side

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def square_number(x):
+        return x**2
