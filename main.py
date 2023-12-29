26b4e6ddf0373b4e04634bf6fc6d75112cc821ec
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:45:16 +0000

    visit slowly rest extremely instruction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
