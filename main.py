c2d015f35a6be09d66f0670677aa1aec8d223fe6
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:47:42 +0000

    goaway programme Ff comefrom journey

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
