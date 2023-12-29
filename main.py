dbc1bffc2184429ff759ebe43e9a1b3d444531d3
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:30:43 +0000

    study closely share spray issue

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+def square_number(x):
+        return x**2
