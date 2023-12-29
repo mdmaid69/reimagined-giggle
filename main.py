18fc6cede6a836131eb02b541b259a2f75b07bec
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:37:09 +0000

    life easy provide breakfast happy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
