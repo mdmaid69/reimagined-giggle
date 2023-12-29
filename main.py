0ff4205ded91a1517dc80452a335b77fea49a399
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:54:51 +0000

    definite admire crowd relative organize

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
