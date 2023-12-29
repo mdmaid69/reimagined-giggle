d259a9c36d78380db44b0569bd04423080e7ed4c
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:16:23 +0000

    waste else separate professional movement

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
