a1ec326f8553713d0cc0ee38950cad20551453eb
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:55:09 +0000

    uncertain sport certain substance breakdown

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import sys
+def exit_program():
+        sys.exit()
