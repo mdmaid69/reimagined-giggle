d73da687ebb6762cffb1383b570cc60d6089c597
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:24:33 +0000

    patience refuse content edge guard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
