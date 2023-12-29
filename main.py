ad19c9f7893ab5a6591f047de909a47ea33112da
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:30:30 +0000

    spoil shrink trade unkind bin

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def get_array_length(array):
+        return len(array)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
