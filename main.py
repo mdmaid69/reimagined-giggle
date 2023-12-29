93d44e51c538b2cd947b12e152075a5eccede6c3
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:49:29 +0000

    shame talkoutof break once yesterday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
