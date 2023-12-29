e0a97f6da8465662a19d17e12f922eefe9556667
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:27:43 +0000

    although keepup takeout character discuss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_str(array):
+        return str(array)
