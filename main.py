95f6c4f7daa0ab4c098ee5d0448d1f60b4548049
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:06:47 +0000

    criticize similar turn welcome situation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import sys
-  def get_python_version():
-        return sys.version
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import platform
+def get_python_version():
+        return platform.python_version()
