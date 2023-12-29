77a6b30a739adb3c99a786ea756778626c579cb7
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:48:29 +0000

    callout leave plan spoil custom

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+print(sum(range(10)))
