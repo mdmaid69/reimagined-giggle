9b592d8096f6b7c37bff74fb45c1ba7b13b7d4d3
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:58:49 +0000

    wear failure street impossible backwards

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
