92bbb82e9abd75966b791b60abdd0d5900218225
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:19:40 +0000

    your list cruelty forward build

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def get_array_as_str(array):
+        return str(array)
