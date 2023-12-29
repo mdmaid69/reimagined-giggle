b215c617de1ca1c4e99db876f758787a88f07427
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:03:27 +0000

    rich high environment contrast recognize

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
