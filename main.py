bfbee639ded4e6e8a2339c4141cccceefe0a8a34
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:07:28 +0000

    borrow touch possession trick prevent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_sine(x):
+        return math.sin(x)
