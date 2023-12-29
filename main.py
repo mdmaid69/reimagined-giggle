0ddedec47a3fe35d1951520c026bb4b886b39539
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:33:49 +0000

    machine himself confusing occasional shallow

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
