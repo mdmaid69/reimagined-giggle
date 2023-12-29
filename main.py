c9681d950c912cbb58e72eec35b1cf474386300d
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:10:52 +0000

    could cost second secret about

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+def cube_number(x):
+        return x**3
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
