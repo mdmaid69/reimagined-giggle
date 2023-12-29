bc379c8b7aca5ddcb9e32ab7e9a77dc82034c1a3
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:02:38 +0000

    its good breakdown backwards where

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
