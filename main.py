2dc255fa85c17150921258da55766fe930973fbc
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:29:08 +0000

    consist skin wall air feel

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
