ec06ee6b9b3bd73f9a2c472a535fc52243ba4b82
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:52:31 +0000

    accident tired frequent furniture keepaway

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
