ebe4d5ecabb6fa90f6db38585e1f208ac5fb3fbf
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:47:15 +0000

    shout spread nigga position surprise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import tensorflow as tf
+print(tf.__version__)
