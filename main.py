67009e32bc783387b1b6aa277bb0718b6d824254
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:15:55 +0000

    no allow fight beyond path

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
