5e2f08352d013fe95cc0ef15bfca05d6b49e9a6d
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:22:33 +0000

    impressed beside guide 179bite feel

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
