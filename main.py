4dee23c34e01b556c2c02b9832375402197f4143
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:38:03 +0000

    shake ring anywhere dark between

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
