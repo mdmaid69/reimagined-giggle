2c997747f48ee40845b42b4d9af5d1632668b345
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:51:22 +0000

    occur issue smart knock lean

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import time
-print(time.time())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
