f4766e2a12a887c067dd70e8c7f1078799f90f06
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:36:11 +0000

    stupid copy word minority otherwise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def count_words(sentence):
-        return len(sentence.split())
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
