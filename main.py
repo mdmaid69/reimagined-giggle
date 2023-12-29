5e088404fe5880270d808f3997fbdfbf18a68860
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:34:52 +0000

    strong slowly Ff forward previous

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
