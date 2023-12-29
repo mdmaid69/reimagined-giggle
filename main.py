a60301d1e518521cbdfb8e224191d89cc379ced6
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:07:01 +0000

    earth raw pinch rule sort

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def reverse_list(lst):
+        return lst[::-1]
