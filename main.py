8507936a4a13740e5ab352d20a1a73c810d69075
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:15:44 +0000

    reliable bridge arrange develop exist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
