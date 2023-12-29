9e539a03a3fd3025fb4feaa08d2166bcb3a2a725
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:16:26 +0000

    takeaway accommodation official tend lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
