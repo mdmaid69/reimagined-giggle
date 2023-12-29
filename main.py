cea74749245014e0e06ee520c662774075ef97d3
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:12:50 +0000

    consist whole though opposite bet

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_area_circle(r):
+        return 3.14 * r**2
