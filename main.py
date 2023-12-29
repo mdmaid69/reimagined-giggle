71f414fd9f2cb9bf29d34d0245163cf4f5af89bf
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:53:14 +0000

    care boring zero invitation ahead

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
