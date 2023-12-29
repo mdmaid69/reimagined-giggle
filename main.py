227607ceaa469633675ec41a9e3099501673187f
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:01:52 +0000

    feature comeoff young wander money

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def reverse_list(lst):
+        return lst[::-1]
