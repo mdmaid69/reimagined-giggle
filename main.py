d473aa728d092a3343d66cade74f5313c1055706
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:29:15 +0000

    admit past they drink base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def clear_array(array):
-        array *= 0
-import collections
-def create_user_string():
-        return collections.UserString()
+def reverse_array(array):
+        array.reverse()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
