e42aa08eab8cf0c251e382c1751fa1e4c1fef32d
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:23:06 +0000

    consult again certain mine soil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def convert_to_binary(n):
+        return bin(n)
