9b5569a1ed6bc106e2182850dc527bad888f9790
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:52:04 +0000

    mistake muscle breakfast temperature fun

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
