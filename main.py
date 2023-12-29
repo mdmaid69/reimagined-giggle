6e4f290e884de4b9f71f5b85bdd528758b32de2e
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:49:39 +0000

    owe yours top sore smoke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
