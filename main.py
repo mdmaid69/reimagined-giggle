99d499680f9551331619c9fbbada7b4855432282
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:53:07 +0000

    wide thinkover rob artificial callback

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import sys
+def exit_program():
+        sys.exit()
