3cef440d562811696c53d34d8dd084df1692dd26
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:53:54 +0000

    fortunate lose fashion cinema face

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
