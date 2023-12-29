695e3416ff3d0789c897c03a058f897e6c70c595
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:21:30 +0000

    late crime equal behave paper

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
