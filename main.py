96e09e997e8b5440b2944c253e65863dd5c99276
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:53:35 +0000

    takeover be height bank in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
