5c0931ee7a927ce4020f5accca468d1229669a1b
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:08:43 +0000

    willing instead afraid trap worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def get_array_as_bool(array):
+        return bool(array)
