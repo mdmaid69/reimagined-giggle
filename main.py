51066c56580082a4fb516f57d896c1446d6ddfe2
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:52:20 +0000

    heavy rest ear watch wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
