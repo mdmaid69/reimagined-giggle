7eb5adf49e1c3c64b67d326b4c1728622a6b527a
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:50:23 +0000

    opposite telephone emphasize problem approve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+  def convert_to_octal(n):
+        return oct(n)
