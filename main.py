e9324854866e4c034a51069f9a62a09d69eb230b
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:06:21 +0000

    comedown fortunate say receive interview

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
