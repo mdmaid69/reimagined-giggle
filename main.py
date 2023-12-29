6a551299eed354c122704697c797b4267760d33e
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:59:49 +0000

    left reduce exchange steady mistake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
