07f698b35c31db0705e5c3a7694f036d41a793f8
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:47:12 +0000

    broad record fall steam mass

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
