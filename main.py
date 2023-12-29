cc0680290c9c74714e0ee45c0a0da4858ef12140
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:33:56 +0000

    shade consider foreign contain genuine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
