54d3eafccdb289f89936f73fb3bbe61136e11ddf
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:08:33 +0000

    may notice refer neither afraid

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import collections
-def create_queue():
-        return collections.deque()
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_density(mass, volume):
+        return mass / volume
