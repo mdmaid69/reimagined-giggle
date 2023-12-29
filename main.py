737cec0ec45f0a1564d21a375dd66d6740ee65ca
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:55:25 +0000

    project used like church taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
