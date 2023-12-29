8cb82b57ce46cd97378ef1bab02cbc05013f3267
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:20:00 +0000

    cruel married dead actual Jj

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
