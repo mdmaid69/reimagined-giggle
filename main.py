afa8d5cc70b3d6b828ffc3060f38cb902ee7506b
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:23:27 +0000

    exact tone body tread he

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
