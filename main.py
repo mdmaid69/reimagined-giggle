ff85e597a4a7991d5cc89dd0d6760562550ac697
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:06:07 +0000

    beat refer confident feature getoff

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def get_array_as_set(array):
-        return set(array)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
