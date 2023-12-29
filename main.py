6b394bfe58ad11ef0cb3b3f54093b57cb5d331f2
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:48:20 +0000

    arrival environment mad ignore create

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
+import array
+def get_list_from_array(array):
+        return array.tolist()
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
