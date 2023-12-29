c90a81f7f303410c5ff2cf929bc826bd35b84037
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:40:41 +0000

    join report excellent gentle impossible

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def add_numbers(x, y):
+        return x + y
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
