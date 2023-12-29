bdc81ab5d4c59d3314a84f733f850ac49be746f4
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:46:52 +0000

    expression cut consult lock approve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
