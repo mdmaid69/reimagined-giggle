67859f826e4dbd7bb22aa5daa666d476d9df86e3
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:05:20 +0000

    vision refer positive afterwards innocent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
