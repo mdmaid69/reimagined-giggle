defe5d42dd6613d896ea40c00a37af05d1bfc826
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:54:28 +0000

    floor mixture act price exciting

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import random
-print(random.randint(0, 100))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
