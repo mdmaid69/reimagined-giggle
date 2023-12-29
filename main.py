60505def274111dadb3a28d52a0b0a6d78109144
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:48:27 +0000

    deep normal wet task proper

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-print(random.randint(0, 100))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def reverse_string(s):
+        return s[::-1]
