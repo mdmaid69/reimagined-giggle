cfe94630cb3b1a4ec7c3ae58bfc484d953336f76
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:15:37 +0000

    by guess cut empty holdout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
