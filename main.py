9155f6d4753a4970c42753cdf14bb97f9915c3f6
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:29:40 +0000

    related apply impatient hit takeback

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
