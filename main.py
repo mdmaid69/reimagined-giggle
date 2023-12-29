d11f53a8b004083e98d71eab7d6dd9fc27bbd488
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:03:57 +0000

    land memory inform underneath kill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
