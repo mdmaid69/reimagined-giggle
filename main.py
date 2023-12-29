d6bfd88d7bb0d2291cfbd3c84d11f9a13d12199a
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:33:19 +0000

    spare would conduct year precise

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def find_max(numbers):
+        return max(numbers)
