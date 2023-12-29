d2a7bfc45c2085f44dc1c5977d7b8af9abbaa1c4
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:20:57 +0000

    concerning language stare recognize on

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
