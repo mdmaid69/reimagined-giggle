ecb332011cb8245a30393418877c85480cfae87b
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:35:05 +0000

    petrol community exam spread minimum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_work(force, distance):
+        return force * distance
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
