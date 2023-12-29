e3fc9a8db8b2255fecab7469ac388ce1350ec7d2
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:48:09 +0000

    fire burst rough production eat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def add_numbers(x, y):
+        return x + y
+import math
+def calculate_error_function(x):
+        return math.erf(x)
