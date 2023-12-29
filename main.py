c56c58e8accebde070b34a28f4b7524a2c4a1b3c
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:16:43 +0000

    summer fortunate phone pleasant factory

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
