a4090fac54fa0552dc46946a9a4055993d0586cb
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:14:35 +0000

    opponent new transparent so film

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def find_min(lst):
-        return min(lst)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
