c3b8e3a616c639c9ce4196d8b1ef7517abae0031
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:53:10 +0000

    form brush emotion shine goodbye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
