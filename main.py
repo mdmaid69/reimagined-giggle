71d4520aaa759d1afc36c6d7a67ffbf91006566c
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:32:31 +0000

    stage passenger duty nigga basis

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
