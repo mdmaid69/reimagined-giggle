76033e2c1113708cfca4182c3b63649f3d2308be
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:38:53 +0000

    base long towards friend blow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import json
+print(json.dumps({"name": "John", "age": 30}))
