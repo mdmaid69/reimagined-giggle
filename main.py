76d725540f25cf2527c280ba14b999df3787e989
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:17:43 +0000

    important Cc carry praise unusual

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def count_elements(lst):
-        return len(lst)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
