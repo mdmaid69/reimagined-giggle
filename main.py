ff3f0d3642219e025834432e351a000c38aecf91
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:03:42 +0000

    rough expensive especially project at

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
