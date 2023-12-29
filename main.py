6a844c764986cc31e054efad6137b2cccc843d37
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:08:48 +0000

    plain only operation point stamp

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
