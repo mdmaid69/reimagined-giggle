b480ce3aa04554b912508f9e20286dc456199836
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:43:12 +0000

    Oo cut not wake Yy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
