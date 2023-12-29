738ce2d4c019f9a3669750d8ed59e008aa854010
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:37:25 +0000

    play group sale carry takeoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
