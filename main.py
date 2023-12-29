83c0f25eadd6831c9d2a05136af8187f84e63c11
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:49:19 +0000

    talkinto ilovedicks clothes limit trial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
