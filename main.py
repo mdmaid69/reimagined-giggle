63c842082210309180081d1df1d8249648b9d37b
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:41:11 +0000

    favourite rescue reserve approval sing

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_ino(file_name):
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_inode(file_name):
         return os.stat(file_name).st_ino
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
