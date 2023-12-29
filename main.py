a4ab10f1f69ac9e209f9f68af73cb63ae774d4b6
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:11:30 +0000

    debt side breakinto runout obvious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
