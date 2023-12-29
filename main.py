adcebc391bde429520c58095aea6d6dbd938b912
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:50:40 +0000

    dinner soil famous Ff how

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+name = "Python"
+print("Hello,", name)
