bdbe941af8e419938ac157302d25c7642ea576a7
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:01:53 +0000

    confuse honest excited comein principle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
