56e4781cb829dd4155b0fbaa18d428576d40fd7f
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:23:12 +0000

    cause instruction clearly solve friendly

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
