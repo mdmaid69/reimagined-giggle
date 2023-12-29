253587958e0fe9ff347478772d354433a4504979
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:37:36 +0000

    challenge moral unable count this

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import datetime
+print(datetime.datetime.now())
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
