837e7df83b6492851ad7bba2c51f4425ac099a0c
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:16:26 +0000

    polite promise lesson although criticism

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
 def convert_unicode_to_array(unicode, typecode):
         a = array.array(typecode)
         a.fromunicode(unicode)
         return a
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
