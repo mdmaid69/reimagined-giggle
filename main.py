118d5240bf4372148b81ad5aa997fa8a803b3364
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:10:27 +0000

    trust bite luck report weak

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def count_characters(sentence):
+        return len(sentence)
+print("Hello, world!")
