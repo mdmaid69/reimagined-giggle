7211cbc3d80a7c2a782792ba276ffc1677579ddb
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:17:23 +0000

    transparent comment underneath tongue figure

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-print("Hello, world!")
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def find_min(lst):
+        return min(lst)
