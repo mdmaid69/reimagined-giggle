1ab8777ebb5fe479eef03d450d68f7a825af5fbd
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:27:43 +0000

    view would breakup tip ignore

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_area_rectangle(l, w):
+        return l * w
