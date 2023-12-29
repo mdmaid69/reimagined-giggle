cb6731107625dc374f52b0335ce0b7d25b661d43
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:19:32 +0000

    close bar wake compete allow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-for i in range(5):
-        print(i)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
