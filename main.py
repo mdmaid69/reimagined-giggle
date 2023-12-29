12260fd83528d02ef3b6bced50e3ee19733e9b3e
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:27:29 +0000

    dress at uniform manner lock

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
