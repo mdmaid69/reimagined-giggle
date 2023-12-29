73bd05b8339db5e8bfe7eff04a5dcbe8d338d098
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:55:59 +0000

    search happen keep event kind

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def convert_to_binary(n):
+        return bin(n)
