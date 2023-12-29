9257ed413ab8f95f47b28c284523c80947afc5b8
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:04:01 +0000

    track fact rely smile forget

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def convert_to_octal(n):
-        return oct(n)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
