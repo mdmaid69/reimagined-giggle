5e64c7e682814bc0d63cfc83dc7ddd3eb92a3635
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:34:56 +0000

    which mistake responsibility mouth course

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
