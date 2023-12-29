be09dc4ee511ace5b0640965f7f4473c99aafdd2
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:33:23 +0000

    save unusual abroad after bye

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def calculate_acceleration(speed, time):
-        return speed / time
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_power(work, time):
+        return work / time
