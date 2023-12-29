016a75bd06962e2d0fb38fff386d39f9b787afcb
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:20:57 +0000

    each nowhere prefer else stay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
