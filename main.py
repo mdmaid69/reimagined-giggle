894fe70d3cc30906c1048579f541d66741953eed
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:13:59 +0000

    excellent standard block build read

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def greet(name):
-        print(f"Hello, {name}!")
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
