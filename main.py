c44cc6dc14e6c133686cd05484204e601192532e
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:45:11 +0000

    grow evidence delicate if affect

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def get_array_as_str(array):
-        return str(array)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
