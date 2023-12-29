ec70dfb6b9e9fb6b49f98dd4ed07ee5a260a29a8
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:07:06 +0000

    worried quick protect count absolutely

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
