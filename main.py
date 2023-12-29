81135ad62bfd742e3822334110468635a2fe0920
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:39:53 +0000

    duty dear still slowly even

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import array
-def get_array_typecode(array):
-        return array.typecode
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
