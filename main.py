f15f2538b2fcfeb1e1d1f3a06a098c2ae60399c9
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:15:11 +0000

    back muscle considering fresh no

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import os
-print(os.getcwd())
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
