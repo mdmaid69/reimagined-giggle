7b284a40745734bd52de8b07bf8ed336187e2132
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:51:03 +0000

    comeback lean difficulty market piece

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import sys
+def exit_program():
+        sys.exit()
