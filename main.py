13c87b720ca337a482a17d2f24bca0d768a6c78f
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:41:57 +0000

    illness imagination communication wire strength

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
