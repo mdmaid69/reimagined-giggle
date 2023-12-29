dfd33e6d268434d89100f20fd26e22218ac076d3
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:42:12 +0000

    feel firm frighten bound future

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_max(numbers):
-        return max(numbers)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
