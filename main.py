d3d8567849bd36452bed006d2a39b086135757b8
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:05:47 +0000

    opposite Ii ring knock soil

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
