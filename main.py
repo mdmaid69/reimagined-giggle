929ef1209eba47590b0818cbbfed9240032a2af1
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:58:02 +0000

    shout perfect herself its test

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
