edd52a91ad6bb772f2058dd7f669f3f100f5cd44
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:27:10 +0000

    to shut welcome still exaggerate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
