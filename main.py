ebbf48f6dea32b1333f161a1d419d2dfb9b6b477
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:53:02 +0000

    allow chief holiday public communicate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def find_min(lst):
-        return min(lst)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
