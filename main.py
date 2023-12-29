2b848f76c153e9aded776a9e5ecdf05383dfb8d4
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:47:55 +0000

    makeinto disapprove question success test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_tangent(x):
+        return math.tan(x)
