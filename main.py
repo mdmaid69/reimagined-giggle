9304c4ca1f51e530b0c39ed37f63acc79008d6c5
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:59:59 +0000

    false connected base cook black

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_work(force, distance):
+        return force * distance
