d629ac3d9798799d6bc615cbd14c7f172f740244
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:07:37 +0000

    load nearly table attitude the

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
