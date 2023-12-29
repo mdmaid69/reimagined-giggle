28d850f033a9707450963548b694c885656bd2d3
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:58:03 +0000

    beneath under breakup day or

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def find_unique_words(sentence):
+        return set(sentence.split())
+def reverse_list(lst):
+        return lst[::-1]
