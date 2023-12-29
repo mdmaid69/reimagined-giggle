ecdbeaf2d3c44bfd65972e11e798d681e2502235
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:43:30 +0000

    university dry continue advanced miss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
