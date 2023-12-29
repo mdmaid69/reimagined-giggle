11d27a4ee83ccd306419121904d4490248b8f513
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:04:44 +0000

    care thinkover shout repair ignore

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def sort_numbers(numbers):
-        return sorted(numbers)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def calculate_density(mass, volume):
+        return mass / volume
