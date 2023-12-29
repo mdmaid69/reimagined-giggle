5bda4c2f6f17167d842457a0760db5f0b35a960f
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:18:33 +0000

    left success relax dress punish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
