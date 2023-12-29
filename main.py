fe332e12f8ac2b286ffa9fd948ab9091c5df114e
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:38:43 +0000

    special learn somebody already same

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_pressure(force, area):
-        return force / area
+import sys
+def print_python_version():
+        return sys.version
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
