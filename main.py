d3e3683a3b3f925ecf793500ebbd903cf94cc50a
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:18:46 +0000

    control direct callat respect quit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
