aa4e71fa3a07a5132430713426bee8a7be41fea7
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:52:53 +0000

    proud must judge nerve present

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import array
-def get_array_item(array, i):
-        return array[i]
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
