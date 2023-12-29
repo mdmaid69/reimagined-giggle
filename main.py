958a472c1507924f2064e841367d5b3dee5e555c
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:35:27 +0000

    piece callout suffer clue well

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
