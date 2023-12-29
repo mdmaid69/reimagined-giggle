da85ea4f211e06fffc3aae64eb692495d12759c2
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:45:30 +0000

    just tense can let precise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import time
+def get_time_since_epoch():
+        return time.time()
