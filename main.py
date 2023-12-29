aa7bd2e36c04ca6e33d7a65f851087fb9e7ddc70
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:45:57 +0000

    combine reject paper used unusual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
