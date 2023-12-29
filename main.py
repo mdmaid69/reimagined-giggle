888a0821cda026526bc75bd6dfcc1578626f80af
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:54:33 +0000

    rub gooff plan with juice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_area(radius):
+        return 3.14 * radius * radius
