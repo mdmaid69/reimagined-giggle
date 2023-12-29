5488da81cf6685c913240330e34e9a6ccac79ae2
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:18:24 +0000

    prize muscle disappear concern under

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_priority_queue():
-        return collections.deque()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
