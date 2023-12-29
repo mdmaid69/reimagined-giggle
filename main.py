0bf128e2701e1c02707c1c0ff5caf4596d60d67f
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:27:46 +0000

    talkinto abroad picture swing error

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
