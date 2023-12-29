1426acadb8500d4c1b50d80dadd1d862b46bb846
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:04:26 +0000

    gift water in lay break

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import datetime
-print(datetime.datetime.now())
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
