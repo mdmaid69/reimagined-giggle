8641dcb5de03c4f4150b303ee512b6638c834c0c
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:00:06 +0000

    release Kk well finish pronounce

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
