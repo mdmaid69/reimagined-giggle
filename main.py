26261aad8632fd8204dc484ef4ef70b3c0b50fb7
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:54:51 +0000

    concern safety rank loose loose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def convert_to_hex(n):
-        return hex(n)
+  def get_current_directory():
+        return os.getcwd()
