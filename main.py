a583315386ef21891fd520581caa396940afac76
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:08:00 +0000

    under love terrible maintain feed

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
