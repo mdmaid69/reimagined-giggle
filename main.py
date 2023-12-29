8424e91ef623fb3e64b214a48913c4ad764035e5
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:29:19 +0000

    condition disappear trust runinto little

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
