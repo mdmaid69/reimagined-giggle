46d8ecd643eddb8649418e3bfdf8b3645ea4ec32
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:54:14 +0000

    during happen news performance again

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
