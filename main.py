9676570a99a04b22c4b99cb13f58c8051d8c298d
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:25:03 +0000

    clear complain quiet scheme load

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
