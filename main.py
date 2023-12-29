6ed70cdbe8bdee3a9f8ddedcca506040396d0e7f
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:29:05 +0000

    cost drive occasion touch behaviour

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
