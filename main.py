fca6f0670b46c7d99831c4ffb08fef99b59ac9f6
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:42:38 +0000

    OKorokay bottom underneath kick relation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
