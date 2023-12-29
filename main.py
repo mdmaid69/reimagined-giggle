ece64cc90ddae9d1c504c66b8a5dbca814686040
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:28:40 +0000

    everywhere leave sure private whom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import numpy as np
+print(np.array([1, 2, 3]))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
