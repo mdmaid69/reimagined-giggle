89e529a4dcb9ae7998b6afebf1a1579ea122f8e7
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:25:51 +0000

    sit interested treat other enthusiastic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
 list1 = [1, 2, 3]
 list2 = [2, 3, 4]
 print("Common elements:", set(list1) & set(list2))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
