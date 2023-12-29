98dd41e28596c212693b4df61bd0889f40999136
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:02:24 +0000

    come light point report onto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import json
+print(json.dumps({"name": "John", "age": 30}))
