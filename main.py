5934492d7a52b8b009823d9450e8613898a62395
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:10:33 +0000

    moral pause raw way behind

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
