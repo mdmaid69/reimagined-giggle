9f69bb1961294f7178aaa521ce42225b94e58234
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:52:46 +0000

    independent behind quickly level pattern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_density(mass, volume):
+        return mass / volume
