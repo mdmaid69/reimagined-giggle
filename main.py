ff275c83c00c0847b49b7106f00eb3ae8d9715ef
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:29:29 +0000

    to kind most pollution breakthrough

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
