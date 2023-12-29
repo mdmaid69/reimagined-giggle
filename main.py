836bf01821651b02d62e6b00c04a261cfd692297
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:19:12 +0000

    female somehow kind few unlikely

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
