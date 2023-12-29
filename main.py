ebdd53906494a8f2498c00b1ae45aa724ba82cb4
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:09:08 +0000

    react like say capital Bb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
