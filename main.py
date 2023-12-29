592782a1173de8fc9a7778dce3c0186cf7119754
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:55:28 +0000

    obtain far confuse move label

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def get_array_typecode(array):
-        return array.typecode
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
