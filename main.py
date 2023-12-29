1c7d52efcfce008c7e1fc2a5fbd3b3dd59781c88
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:41:04 +0000

    cheap strong trial together lay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
