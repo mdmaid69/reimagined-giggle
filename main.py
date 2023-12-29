fc873c37458611f8559e3425d53cd311df2610a1
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:55:43 +0000

    towards along flight wish between

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
