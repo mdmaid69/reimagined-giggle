5ee3ee9998acdbc22b10737a2b8e734b605eb93d
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:53:51 +0000

    uncomfortable right whenever moment nice

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
