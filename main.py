d1bf83b134ab6e3a78ced131460e40347c9e072e
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:56:48 +0000

    other water maintain business appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
