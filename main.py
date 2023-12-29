3ae81cc6af427983627fc0d712f42eae3d549905
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:58:12 +0000

    position afford treat unusual warn

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
