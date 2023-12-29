67761430f9c8623d327e32f748153540ae603913
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:20:33 +0000

    drive apply squash seem authority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_pressure(force, area):
+        return force / area
