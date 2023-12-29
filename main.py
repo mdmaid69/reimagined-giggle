3c4d8be931660212f8488ab6165e6e86516dc5ff
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:56:34 +0000

    rush sleep town at immediately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def find_max(numbers):
+        return max(numbers)
