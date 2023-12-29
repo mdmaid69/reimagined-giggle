31849af639ec7e85df092f8c749d5104f18b6510
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:20:36 +0000

    listen miss careless section Bb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
