e63900506b2f58c780ae9c514f4b1b75818bb4af
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:57:39 +0000

    draw hole guide doubt room

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
