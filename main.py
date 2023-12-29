b3f11a932fb6468419e91382ea7ba018e231b527
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:23:37 +0000

    agree fence attempt influence kind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
