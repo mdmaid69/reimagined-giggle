e0e8659d69edaa242947450226206dd043d93db6
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:34:31 +0000

    forward free temper fasten push

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
