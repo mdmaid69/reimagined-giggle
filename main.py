0092531312ca6fd2637548168b386efcc01f98a2
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:09:36 +0000

    beat butter change anything respect

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
