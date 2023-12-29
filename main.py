6ad8ec4718ad37a126dce7b2003cba329a20b1fc
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:01:47 +0000

    bit comeup swing glass active

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import getpass
+def get_username():
+        return getpass.getuser()
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import time
-print(time.time())
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
