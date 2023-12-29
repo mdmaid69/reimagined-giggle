4c523a81ab48effee75b17761040946391893ea6
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:36:04 +0000

    second loan mark gate love

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
