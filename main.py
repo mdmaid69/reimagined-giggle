eb271b64d6d6264a238e28b094f3abf1c8b0bc6b
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:01:58 +0000

    walk present suck fault instrument

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
