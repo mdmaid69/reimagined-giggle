7e0c7a205d08511915e16073d1211264bebe9dc3
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:29:55 +0000

    involve trade hot floor arrival

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def load_csv(filename):
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import json
+def load_json(filename):
         with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+        return json.load(f)
