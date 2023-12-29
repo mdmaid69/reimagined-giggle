36c20493f3f17e18504f33947b342f06437dd507
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:34:10 +0000

    mean snow unkind large entry

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
