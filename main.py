eba36f3dd1c16cf3051ad1f336af5e424e7d2cda
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:35:45 +0000

    brush another coffee squash unkind

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def add_numbers(a, b):
+        return a + b
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
