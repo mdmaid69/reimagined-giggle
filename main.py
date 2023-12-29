60d992d7f8e19d89221d0c7438fe1d355cca06bc
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:38:05 +0000

    confuse complaint nurse delicate shape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 import array
 def get_array_as_set(array):
         return set(array)
