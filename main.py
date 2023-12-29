74c4a1f72044eab305dfc93356f18ed3589c6eba
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:57:29 +0000

    game decision world 564encourage police

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
