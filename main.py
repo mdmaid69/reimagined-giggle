d6c1c80de69a3abae2047e2d5190613b429eeadc
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:55:47 +0000

    fashion Tt result to takeover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def subtract_numbers(x, y):
+        return x - y
