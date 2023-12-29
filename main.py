ad100cd5e06f5daede6e454e6b8ca95b036fb338
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:59:14 +0000

    thankyou understand land emphasis minute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
