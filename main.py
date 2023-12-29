e75dee673b771912197d31b56e07d951ec927f1f
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:52:12 +0000

    breakdown motion urgent entry equal

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def convert_to_hex(n):
-        return hex(n)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_speed(distance, time):
+        return distance / time
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
