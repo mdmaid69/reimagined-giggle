527e74703888a855adb9f5720263c6f98c8b035a
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:19:48 +0000

    main lie floor available business

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import sys
-def exit_program():
-        sys.exit()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
