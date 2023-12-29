1a8760a9476cde83feeb06b9ee2b71ffc6b58e0f
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:23:17 +0000

    cheque black awake loss create

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
