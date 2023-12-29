795e08a410af71e57100039e618265a8278108a8
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:42:40 +0000

    this party gas describe welcome

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def sort_list(lst):
-        return sorted(lst)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
