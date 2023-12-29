7c70821b35e0aedc4ad8ec64fc2fe0320e5aa05b
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:14:50 +0000

    deserve solve pile way if

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
