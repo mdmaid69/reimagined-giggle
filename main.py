7a6c7afc25201e6c0ae10607d9ce4573ab902040
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:32:49 +0000

    could date recent fast help

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
