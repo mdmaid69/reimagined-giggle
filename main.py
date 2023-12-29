22d9e574072a41a91a4c24745256d51223c94697
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:20:06 +0000

    harm pronunciation ago bar scare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
 import datetime
 def get_current_datetime():
         return datetime.datetime.now()
-import datetime
-print(datetime.datetime.now())
