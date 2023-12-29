43d98d256ef6218d4abe617c13df961f8420db7f
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:25:34 +0000

    injury vote catch dry spoil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
