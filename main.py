e242b5c0b38aa865c9acb2dcc1c6c596cc1fdf52
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:59:03 +0000

    between workon wheel charge deal

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
