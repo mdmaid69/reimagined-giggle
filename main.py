1e74ce1883cb859eabe7ff18a7a6f747715ed414
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:05:35 +0000

    evidence trust its dress check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
