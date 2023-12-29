c25c93ce70a5b6e0bb5e1197a03c7545ab3df498
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:21:58 +0000

    early naturally bother anxious take

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
