6a1882264ae657753592331cb7ec59df575fd213
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:41:57 +0000

    conclusion finger low intelligent noone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
