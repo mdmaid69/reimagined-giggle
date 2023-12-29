1c4040ecf400d3f55111868648998450677ce694
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:09:15 +0000

    us thankyou show cut tape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
