395fe982942acf064ec8f39410a6d28d9bbe04a1
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:29:12 +0000

    boil ticket bet comfort off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
