8a2cb55e1ed2d684ce390f2cfcc7f8816e8e21f0
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:44:29 +0000

    home pool fair inquire object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
