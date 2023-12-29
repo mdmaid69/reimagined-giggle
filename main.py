e679f25ac122f023013010dd9808d8b3f1015c74
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:23:58 +0000

    activity limit attention soak mixture

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
