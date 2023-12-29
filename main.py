366446286f8914c43662d1d7a0b95b79095007bd
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:27:29 +0000

    human lucky below steer through

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import datetime
+print(datetime.datetime.now())
