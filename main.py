f342c67a81a9387f662bacc58abe1bcc1059eba4
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:46:42 +0000

    fair plan sign power justice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
