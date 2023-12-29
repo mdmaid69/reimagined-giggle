daf28fdbc627fe270be7b92e1de3e035b3cef3f5
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:08:17 +0000

    speak holiday country wing benefit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
