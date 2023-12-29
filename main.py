bbd47252ba00619fa24c6be0cb6c3df658422815
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:01:36 +0000

    ago chat pull impressed live

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 def calculate_work(force, distance):
         return force * distance
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
