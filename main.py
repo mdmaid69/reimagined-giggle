339e948ed2fcdefc1ec5f27b6da6200735cb84a0
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:20:23 +0000

    few jump error drive twist

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_density(mass, volume):
+        return mass / volume
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
