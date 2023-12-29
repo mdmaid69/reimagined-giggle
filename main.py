daf612ac2ccb62f066f23a1b995544a072624ff6
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:17:09 +0000

    cut separate pretend arrangement radio

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
