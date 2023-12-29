0483d4c753f729205350b97d92bbdd56a25befc9
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:51:16 +0000

    thanks push sentence must type

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
