55a028537e43dea79aeb79388f060b42f898b1dd
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:43:09 +0000

    know personal holdon demonstrate challenge

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
