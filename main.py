4482f69ad0c379fbf1eb11fda5272e266722df82
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:00:59 +0000

    brush condition communicate herself getup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import numpy as np
+print(np.array([1, 2, 3]))
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
