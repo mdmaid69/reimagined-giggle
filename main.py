1a06e9059b0f850458ca6fbd88ea49fe3f982de3
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:26:20 +0000

    file waste previous generally artificial

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def convert_to_hex(n):
+        return hex(n)
+import numpy as np
+print(np.array([1, 2, 3]))
