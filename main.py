555c273c1660be9f5c45f101ca0270fccb23cb59
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:19:25 +0000

    not seat continue increase many

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
