a8907565b09050cf08ca931e8e67b67e93be51ab
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:37:13 +0000

    yes famous wash proper violent

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
