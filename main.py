63da79b5a9a282c058c99dafeaf5349378c29136
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:40:23 +0000

    against sell pump alive turnoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
