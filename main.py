6b92b1a73ca9180a4640bb47e87594aa5f83e954
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:22:52 +0000

    improve slowly attention last actually

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import logging
+def log_message(message):
+        logging.info(message)
