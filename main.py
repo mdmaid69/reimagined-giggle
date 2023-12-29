0fc1019674332e08dba95fe5b26bcd1caab8e960
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:28:24 +0000

    scheme forward runinto job sometimes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
