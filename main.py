7df93c2ccb858da5650a5d776f390a03bd1540c3
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:58:08 +0000

    letter themselves ago false translate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
