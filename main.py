39dcbae853d0602593a71cb9b4941b1e222cc845
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:40:26 +0000

    declaration joke back doubt once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
