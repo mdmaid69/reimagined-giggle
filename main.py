96cd51b4d7102d4c7b1f616ffab336f158e91cd8
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:36:42 +0000

    swallow bite talk doubt allow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_ceiling(x):
+        return math.ceil(x)
