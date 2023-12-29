714951c0a4c99456ae3f3e541cf3118a3fbb44fb
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:42:28 +0000

    train clear steer lay pattern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import re
-print(re.match("h.*o", "hello world"))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
