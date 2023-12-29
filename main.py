21948d4bde39e2f89246aee95d50fe6fbd91acef
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:54:37 +0000

    tooth block congratulate fast wheel

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+print(sum(range(10)))
