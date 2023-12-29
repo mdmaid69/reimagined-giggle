afe0f227145f7f74d433912c37176b491389093c
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:19:17 +0000

    trousers display simple equal star

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
