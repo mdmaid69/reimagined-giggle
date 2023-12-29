41ed3d67e6d05e1a843bd1167bb263944e7c8c47
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:41:26 +0000

    empty size sing do comfort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
