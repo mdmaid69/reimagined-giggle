0f415a1d97e5fc819c561c60b91e7f7e09c36673
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:41:05 +0000

    emotion example advice estimate spare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def subtract_numbers(x, y):
+        return x - y
