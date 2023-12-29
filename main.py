99854dfe610825d5d7dd7488d6cf36efceae6d5d
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:47:11 +0000

    breakin post so display thinkover

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def subtract_numbers(x, y):
+        return x - y
