0776483ff056f8ae4211e95f01eb43316e423893
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:59:37 +0000

    remove hit freeze square comefrom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
