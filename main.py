5766b4a135244532f8a1c26df766403c23e47cea
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:08:44 +0000

    awake otherwise survive exit dark

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
