a8c847da89c24ede4d5bae9fcb3dfe2c515f7423
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:04:53 +0000

    Bb set beneath main pick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def get_array_as_int(array):
+        return int(array[0])
