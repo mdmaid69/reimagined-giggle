eb875622460ec5b1c4005bf7bc396ef572b4b910
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:45:53 +0000

    meanwhile during beside sight mine

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
