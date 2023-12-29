4abc024ba5e091adc42d40acff8eb445406d1551
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:03:16 +0000

    objection tone young right keepup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
