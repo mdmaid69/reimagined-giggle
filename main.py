78ff594a8186b67921a9203730500613e5b1ab80
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:53:37 +0000

    guard takeoff unit strong effort

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
