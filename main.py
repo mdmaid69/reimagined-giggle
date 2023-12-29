b249f99684d74922cf21265f7d1c33abb2b29a56
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:12:02 +0000

    seriously culture edge capital dare

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
