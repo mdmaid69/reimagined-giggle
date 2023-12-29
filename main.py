4cab1d16ac57cb78f6cb0ca4dfdd8283de7c92b1
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:19:00 +0000

    live fault cut rare approval

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
