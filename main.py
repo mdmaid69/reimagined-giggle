35d015b0f04014e7fe2feecde101bf99c0d67dce
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:49:33 +0000

    present criticize square no need

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
