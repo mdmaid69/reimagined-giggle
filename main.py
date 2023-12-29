e1302fba78ad0ec2c43b81222c483d89a92408f3
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:34:27 +0000

    number model clever Ll before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
