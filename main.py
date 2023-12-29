76d6e5bc46b99b9ddd965166af5d96f97737757c
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:22:23 +0000

    comeup train hour break her

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
