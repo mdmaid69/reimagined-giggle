d26e9d8e162649e4783d94c4ccb2a06f4b2cce6e
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:09:30 +0000

    statement under trick hurry ring

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def find_max(numbers):
+        return max(numbers)
