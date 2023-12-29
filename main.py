88ba03f808bb7a906f58db2e48c8062b054f89b9
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:21:22 +0000

    immediately pour there everywhere house

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
