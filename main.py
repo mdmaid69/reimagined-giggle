8438e36423175b653a10f36154e7ef06c1800445
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:27:33 +0000

    control search lookback(on) imagination help

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
