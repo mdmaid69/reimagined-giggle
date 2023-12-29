bc980b5eb197b435eeb098d275c586e7b0b951c6
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:24:16 +0000

    month old 570enjoyable certain start

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
