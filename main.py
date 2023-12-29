6f9e29c569d0f7f1ff82ac749f230d7cf0d9252b
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:32:52 +0000

    spot steady desperate train recognize

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
