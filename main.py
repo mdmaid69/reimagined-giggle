4e169d72d5dfc3d1a697eb18962bfca874276c25
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:54:24 +0000

    angry display forward could cry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
