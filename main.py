5bec54c06d1171e44d69f87eb7a8f6c134710ba1
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:26:16 +0000

    as plate either can fail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
