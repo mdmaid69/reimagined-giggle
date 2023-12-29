9d54640831f7475594d52e245b96ead369edac39
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:08:57 +0000

    lie only laugh permission inside

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
