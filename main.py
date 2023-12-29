cdbf1b84bc6bdb484bf6150e77ad8221ed395562
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:20:05 +0000

    evidence clean stage music forget

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def count_elements(lst):
-        return len(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
