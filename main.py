9fcbca0e7b392aab116b3a901030be5a7f76350f
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:54:13 +0000

    experience rest suffer downstairs giveback

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import sys
+print(sys.version)
+def add_numbers(a, b):
+        return a + b
