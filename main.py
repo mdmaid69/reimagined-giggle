8187ab94ba5a36601bb76899eb3983f335ef6fc6
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:31:39 +0000

    do half strength Jj inform

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import array
-def get_array_as_str(array):
-        return str(array)
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
