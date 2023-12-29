5a15e0550abdbfa00fefe693406549b2dcfc4949
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:56:27 +0000

    load similar deliberate Jj company

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  def convert_to_hex(n):
+        return hex(n)
