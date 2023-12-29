1801d9a44382aa5d6aec4c529722831dae23ddfe
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:13:46 +0000

    flow close well decision excuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_volume(length, width, height):
-        return length * width * height
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
