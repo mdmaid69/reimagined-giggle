0c2f587aa0bb8c305233ef9f6fca654e34eaa528
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:10:05 +0000

    tidy flat drink fix deep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
