6db29230f43b018b620a8ba7ad06accade69e57e
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:48:36 +0000

    lend nigga wing mine university

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_average(lst):
+        return sum(lst) / len(lst)
