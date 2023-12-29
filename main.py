30370555ef84b9f923a43da9eea246ddbbaba328
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:50:15 +0000

    rob hard seem she so

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import sys
+def print_python_version():
+        return sys.version
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
