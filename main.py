c16b69d5165ed7285e215e2e413c08620f130439
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:29:52 +0000

    guess disappear fork part occur

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
