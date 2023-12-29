b0ecadaaa6e6653121d6e7aecc9934a13ff47277
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:05:32 +0000

    false anxious lost reasonable no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
