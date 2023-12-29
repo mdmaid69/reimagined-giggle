c96181375f6d9fea33deadc9e74f70c0e7b0fd67
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:50:25 +0000

    recover constantly 565end yet 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
