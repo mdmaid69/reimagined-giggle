13e4d2453b27fff76d931ca20a10246981430191
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:30:10 +0000

    quickly convincing cry spot apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
