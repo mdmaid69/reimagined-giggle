445d56a7ee5050ca555d15695e36fe011cb255a0
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:32:07 +0000

    mark odd regard together upstairs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
