ce4e132a80d856033d083339c9b5e379bcccd474
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:27:17 +0000

    organize quantity run know project

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def divide_numbers(x, y):
+        return x / y
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
