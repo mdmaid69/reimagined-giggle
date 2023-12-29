215d3933b68ad6b6af756dabf4db9d980d36a037
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:51:01 +0000

    shame platform special broad Tt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
