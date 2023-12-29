f98fdac17884e0664798f76c6dd8191c6021602c
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:25:09 +0000

    point sudden back comedown with

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
