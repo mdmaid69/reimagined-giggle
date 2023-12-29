02c5b10c7ec68dc1ef3dab705f159b82e09e35cb
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:50:57 +0000

    what well shade available idea

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
