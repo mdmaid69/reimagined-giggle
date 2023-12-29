022a43caefeca739571ad89e567852c868ac33b2
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:27:12 +0000

    burst shadow admit thumb odd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
