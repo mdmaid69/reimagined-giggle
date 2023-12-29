e025325fdc1255506d2f0c1a9bf0644773192f5e
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:23:33 +0000

    capital spill car possible top

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def find_min(numbers):
+        return min(numbers)
+def sort_list(lst):
+        return sorted(lst)
