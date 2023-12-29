1c3bef73b269b4713de5109422c3b00ddae58cd3
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:51:15 +0000

    use win move method emphasis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
