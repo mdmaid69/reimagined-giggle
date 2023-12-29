f70f6d0d50456dd99c1209bca1f338f704e8d029
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:33:20 +0000

    large full cover standard whistle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_work(force, distance):
+        return force * distance
