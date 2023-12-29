662a828fda06b27c2e7d3a712343816476c7d34e
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:03:54 +0000

    people mean compare equipment completely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
