4be4da2e05bf92630e1711a5fa4c9bb27d6a4010
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:41:13 +0000

    fall stranger size once motion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
