553bf8f5596f47356c4fa0372df76736240452a2
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:37:29 +0000

    use lean wrong Ww or

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import platform
-def get_os_info():
-        return platform.uname()
+def calculate_power(work, time):
+        return work / time
+  def calculate_area_rectangle(l, w):
+        return l * w
