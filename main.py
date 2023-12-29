2d3abbc8ada479e8aacc148a9d64f008e0042b91
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:35:40 +0000

    trap praise soil hear regular

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import time
-def get_current_time():
-        return time.time()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
