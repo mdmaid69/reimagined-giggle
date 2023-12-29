dbb21e2245d81dcfda794736f38219fa55ea787a
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:38:32 +0000

    miss reduction instead outside date

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
