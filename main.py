5ada57a7c5f3e8cdda4d23d71146f22e89bc2715
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:08:19 +0000

    every demonstrate recognize opportunity fair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def convert_to_binary(n):
+        return bin(n)
