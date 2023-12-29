c81f2b71ff641881ebb55ee51d8807d1572689b2
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:41:35 +0000

    some harm force wind fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
