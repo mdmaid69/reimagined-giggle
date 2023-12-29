1c4617f35e35bb18dc0cc0d4162339d3794c76b0
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:18:09 +0000

    private bring will weigh forgive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
