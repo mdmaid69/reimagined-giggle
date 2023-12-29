bc1d6af5176248ccfda7ebf5f6cc747457cea164
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:21:47 +0000

    furniture Oo guess name branch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
