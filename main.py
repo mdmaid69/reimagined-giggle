458d1e45941e507f5a519db579554fb2d880ae82
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:40:49 +0000

    after impressed action crash subject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
