3ad306259887c562eba7ad115b57b0fb83c532cd
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:02:00 +0000

    major simple glass nose forever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
