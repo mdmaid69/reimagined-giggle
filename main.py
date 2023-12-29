eccc14fa789fc4d377b1f1a3eb8c5b3b4373b146
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:29:15 +0000

    bother development there steer safety

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
