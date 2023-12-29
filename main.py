e545d2b625db5f82eae54faec90e6745d564c76f
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:44:41 +0000

    prove able doubt giveback quiet

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
