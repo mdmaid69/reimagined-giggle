2952291e7422022b44e0e1f0cbb65300962d425d
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:24:58 +0000

    history address always Itself male

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def find_max(numbers):
+        return max(numbers)
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
