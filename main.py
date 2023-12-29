2bafd7070a5f86d240069af78d952d4f28333908
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:31:09 +0000

    tear group introduction advise mistake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
