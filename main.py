f97b0c4d2e320c7ec9259425d87cba8bc5db8db2
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:12:25 +0000

    lesson forget now software her

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def sort_numbers(numbers):
+        return sorted(numbers)
