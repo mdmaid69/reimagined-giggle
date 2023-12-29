eefd51c7840b87be52744c8c5d3a55ddff72edbf
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:33:19 +0000

    upwards exact expect arrange it

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
