13c2da121aeba6f8115deaf3a0d404b684cd11ad
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:30:13 +0000

    progress recently present describe soft

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def subtract_numbers(x, y):
+        return x - y
+def subtract_numbers(x, y):
+        return x - y
