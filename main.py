48fe69feebe34f82ed4c0849012cfb3e679c6344
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:32:49 +0000

    interfere long absolutely claim environment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import getpass
+def get_username():
+        return getpass.getuser()
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
