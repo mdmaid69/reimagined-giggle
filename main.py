af6ddbb93992ab6259d13e2fc03654001e4dbbbb
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:19:04 +0000

    light manner party run interview

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def reverse_list(lst):
+        return lst[::-1]
