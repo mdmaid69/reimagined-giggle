1fd738734d29ff893bb82a1742e982f3e1ae9fd5
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:35:50 +0000

    necessary wonder balance pleasant especially

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
