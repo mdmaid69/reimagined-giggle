87dae1d97d7a53494ef99188e6a86e56490ba4f3
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:13:23 +0000

    refer smell money alive love

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
