fec7aa439c947fd4454e11396993a9b68a941f4a
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:00:07 +0000

    base control such uncertain convenient

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+text = "Hello, world!"
+print("Characters:", len(text))
