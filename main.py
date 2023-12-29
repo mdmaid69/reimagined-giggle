d7741a50a2f7d84af9d7d516fed05c0022bb6f1c
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:07:46 +0000

    round claim it brown lot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
