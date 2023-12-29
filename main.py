e9e92348bd637f735deebc6988ccaf1cf69035c5
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:13:28 +0000

    most design reach care cheek

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
