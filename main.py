1432d68ff619b3f6924c1efbd7738b90e343d314
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:20:55 +0000

    coffee friendly emphasis tap body

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def divide_numbers(x, y):
-        return x / y
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def sort_list(lst):
+        return sorted(lst)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
