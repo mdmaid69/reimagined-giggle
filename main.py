e042b0ea93e3944076b49628743c9e7e5c44072c
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:55:02 +0000

    ask space until lose position

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+  def is_even(n):
+        return n % 2 == 0
