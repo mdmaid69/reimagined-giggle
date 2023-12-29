c2a6cac5f21bac3088f1048465cad62298a86f07
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:22:02 +0000

    different sharp exciting immediately occasion

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def subtract_numbers(x, y):
-        return x - y
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def get_array_typecode(array):
+        return array.typecode
