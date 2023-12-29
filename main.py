773a56c9e40f50a136febdc1dc27a1758a2ba3af
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:35:26 +0000

    swimming thinkof underneath paper arrange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def count_characters(sentence):
+        return len(sentence)
