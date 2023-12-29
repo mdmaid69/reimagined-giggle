9e2f7dff7573b1fa42e49f7a72b1ca218d4c3361
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:43:21 +0000

    pronunciation pronunciation regular name sink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
