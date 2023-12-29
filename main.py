bb65676618d46caca27099edc727d07385a500c3
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:30:09 +0000

    564encourage to computer butter remark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
