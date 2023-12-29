172534c620635529a6d5d463b0422f39e108e0b1
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:46:21 +0000

    operation operation use answer direct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_speed(distance, time):
-        return distance / time
+def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
