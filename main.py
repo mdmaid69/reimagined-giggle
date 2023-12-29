d12b6fa2ee5d1513bc89c0ec0128034e614cee75
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:06:29 +0000

    alive medium film stop lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
