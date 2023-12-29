983b13e93f1e181dbfe0a99bc067d2c5acb1047e
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:59:59 +0000

    mean birth schedule introduce rank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
