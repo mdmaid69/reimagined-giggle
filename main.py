6eed4c7629a6057e9893397b4a088b5db7f274b8
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:09:54 +0000

    fall everyone artificial flood journey

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
