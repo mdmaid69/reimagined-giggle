372fdf8563ba83fbcca0e6df73c7ce3a4e36970a
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:58:07 +0000

    exist let cross sing broken

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
