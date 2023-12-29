14067a07e696fd33510b6b52873ac9aeb8d6271c
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:37:20 +0000

    worth watch thinkthrough leave combination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
