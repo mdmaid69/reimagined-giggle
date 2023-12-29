60d4b92dc5f96588d4b2556d801a963b07e72817
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:03:29 +0000

    cruel ocean recommend top past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
