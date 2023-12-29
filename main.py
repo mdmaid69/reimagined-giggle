792118c22b8eaa6e7aa562ef642fb9d73acdd363
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:11:44 +0000

    plain foreign force over treat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def find_min(lst):
+        return min(lst)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
