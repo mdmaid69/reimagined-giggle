bb9574a70876450df47226c976a6d80fcbfcdcba
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:26:28 +0000

    worst waste blame suit quit

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
