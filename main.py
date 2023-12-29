e72b930a312c44d077f96f14bce77924e4cfab87
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:35:34 +0000

    satisfied mixture start discuss night

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
