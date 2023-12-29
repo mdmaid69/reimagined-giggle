64b966c4ce6ffbaf3d60f4744fc84324c4a120d5
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:45:00 +0000

    cover situation content all production

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
 import array
-def get_array_as_str(array):
-        return str(array)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
