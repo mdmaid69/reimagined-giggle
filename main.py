7f3d073f502aec34adc3c1c994efa5d8567fe17a
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:44:39 +0000

    century cold ride their takeaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import datetime
+def get_current_date():
+        return datetime.date.today()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
