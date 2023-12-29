b98ab569c440a4e2c4fe3988df74383cf9699844
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:04:17 +0000

    normal part division edge authority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
