a557e91374ba7e40ca4e979ac3f22b023cb7332f
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:15:20 +0000

    thick face callback answer distant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
