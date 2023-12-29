68327156ead163146ff6f16f5e72d90fa9bbf5ba
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:11:36 +0000

    capital plain along us people

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
