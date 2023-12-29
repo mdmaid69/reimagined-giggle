6448924981ae0b2283b2fe0c2523948569ddf9ba
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:10:29 +0000

    spoil absolutely no uniform heavy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
