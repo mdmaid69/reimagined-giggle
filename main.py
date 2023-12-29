82ffb5040fd91fbe79cc569b293a884481041c23
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:44:16 +0000

    good tie mine honest sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
