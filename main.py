648930277bfe05b171412113cdd86ed1c0453b2b
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:46:27 +0000

    rub happen meanwhile hang feel

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
