bbe918a87f2383db0480649e4d0ec1a1265f6233
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:26:11 +0000

    accidentally pay trial dear dinner

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
