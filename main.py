8f66f8be1c6e710e2ff956fd6eb4630cbda310ab
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:38:23 +0000

    scheme communication love address true

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import time
+def get_current_time():
+        return time.time()
