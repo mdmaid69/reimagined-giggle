6b2fb03e0c2d4653440635369dd3622c5a604488
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:19:24 +0000

    behaviour knock nearly argue position

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
