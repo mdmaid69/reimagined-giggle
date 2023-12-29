248cc6c889ad6672159ecc95ed22f9838fb53a76
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:06:10 +0000

    wild pour unlikely behind neat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
