bc19983a8b57b2965f512394e568226b0f025ff5
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:11:58 +0000

    genuine basically argument fire taste

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
