8fd3b0bdea3ad8e4aa64ca88fae7926a49da9b23
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:58:27 +0000

    disgusting collection legal trousers switch

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def is_even(n):
+        return n % 2 == 0
