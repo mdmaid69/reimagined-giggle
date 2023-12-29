732dc76a8b8b96799351d1a7e5696c9782f2b040
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:11:42 +0000

    lend grateful emphasize ticket remove

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import re
-print(re.match("h.*o", "hello world"))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
