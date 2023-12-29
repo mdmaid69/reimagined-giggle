a8224aa7b521fef211f867501aac7cf7caa8fce8
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:31:58 +0000

    risk see table nervous on

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
