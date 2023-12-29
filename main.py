93f829811106812a767cfe24ede282d266c53c31
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:48:39 +0000

    move sense treat ill swing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
