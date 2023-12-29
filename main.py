c0336acb800a873a40a26e42a1cef36aa3997bfa
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:25:34 +0000

    upset demonstrate prevent weak sure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
