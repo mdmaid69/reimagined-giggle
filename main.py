637e0091e2ba6c802df9d7907919efb5d4dc63e2
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:47:48 +0000

    ready freedom computer shake pay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_speed(distance, time):
-        return distance / time
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
