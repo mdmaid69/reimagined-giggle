1acced8610c1ebd392cabfe3b689f771e8c32e2e
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:05:33 +0000

    height considering surface broken congratulations

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
