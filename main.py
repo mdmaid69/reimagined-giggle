6e510e4b8260d5741b1e2865dd9a03fdc33993cf
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:33:19 +0000

    promise owe character new doctor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+print(json.dumps({"name": "John", "age": 30}))
