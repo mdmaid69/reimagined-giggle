84046b62f88499577ed8507a7a2343a47f26701a
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:09:14 +0000

    meanwhile classroom loose drop thorough

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_work(force, distance):
+        return force * distance
