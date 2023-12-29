7e07195b258735a579d8ed080e733fed82d94c5a
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:52:31 +0000

    tidy none suit ride active

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def remove_duplicates(lst):
+        return list(set(lst))
