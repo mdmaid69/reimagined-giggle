5f9cf8940859c21753a28c826ad015234848ef75
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:00:54 +0000

    conclusion male test various unable

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def subtract_numbers(x, y):
+        return x - y
