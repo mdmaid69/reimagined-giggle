06b67a29380f7d36a00d2fcfb7998f2552a72985
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:25:30 +0000

    exercise display tread now stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
