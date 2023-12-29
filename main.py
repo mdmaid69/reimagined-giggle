e0974076333ce2f1026e29cef648bb768526e7f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:49:01 +0000

    he act track turnback convenient

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def square_number(x):
-        return x**2
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
