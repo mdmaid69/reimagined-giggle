d6f43a453a3cb90215d084ec8d0c4e9e6b8fc07f
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:40:12 +0000

    exchange talkinto familiar really tell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
