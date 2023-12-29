fcf3627b80cd5b6ca39543bd58658778fdf72f97
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:46:08 +0000

    insist body include foot temperature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def square_number(x):
+        return x**2
