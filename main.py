999e25689f8725c57deaf8f9d84c4ec87e312d71
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:25:27 +0000

    advantage brief loan welcome challenge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
