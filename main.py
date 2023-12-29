06f2bb3999d7694bff69638d283d0184913f92aa
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:46:12 +0000

    friendly difficulty yourself underneath forgive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
