89eef43b1e15a9c71ddc0dd00f20b38bc46af295
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:30:58 +0000

    final goup for traffic regional

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def convert_to_binary(n):
+        return bin(n)
+import time
+print(time.time())
