544a5fd752b96c14a22554166e1ef2c038cae8da
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:56:59 +0000

    event wing goback(to) better difficult

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+def cube_number(x):
+        return x**3
