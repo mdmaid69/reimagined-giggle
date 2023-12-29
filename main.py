f31a14b5ef5e33ddc503901375e5f5797aeefd83
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:55:50 +0000

    flight male talkinto general owe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
