ecbca59ba56199c9cd3f70429cc56f747273727c
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:37:50 +0000

    wait reject theirs anything sea

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
