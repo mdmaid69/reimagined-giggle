b0e3abdd85b03488b0033ceaed44778735ecb4e1
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:32:47 +0000

    actual club solve fault only

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_current_directory():
+        return os.getcwd()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
