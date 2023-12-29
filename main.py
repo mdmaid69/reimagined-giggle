a4b5e444178afe7e06acdb7f46fd96188d95a95c
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:00:32 +0000

    through punishment walk for tense

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_unique_words(sentence):
+        return set(sentence.split())
