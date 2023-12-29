484467aed3b2ed21fe4049cbddc6f04a256b8bc1
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:14:54 +0000

    quality safe number fine pity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
