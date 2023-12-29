543121606ddb0640441958f7642a4ca445c50b2f
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:19:39 +0000

    school content fun spite lean

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
