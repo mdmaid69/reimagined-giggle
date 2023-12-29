6025e19539bb5384f3d3605e5bef946b93ca8611
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:34:29 +0000

    black share float crash contact

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def square_number(x):
+        return x**2
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
