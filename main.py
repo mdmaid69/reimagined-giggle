f7486b8a39e647c3de959b35f566c36497551d38
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:10:39 +0000

    mystery best something upstairs split

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def get_array_as_frozenset(array):
+        return frozenset(array)
