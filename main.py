0aab3efab8a12294495f3915ed23c8a9573a4201
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:44:57 +0000

    book whenever few police scheme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
