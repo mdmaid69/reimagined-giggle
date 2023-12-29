196579420083d7c59656367f777bfac1f4c5960c
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:38:36 +0000

    prefer turnback anything message either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_base_name(path):
+        return os.path.basename(path)
