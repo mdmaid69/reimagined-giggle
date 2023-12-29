f1945c2aae8a5f0886fb531376f2c3eafaba53e4
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:29:29 +0000

    meet charge bad snow instant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
