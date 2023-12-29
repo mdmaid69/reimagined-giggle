7babd18b5f4efa9927a5c16d542f049f56a5dfcf
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:16:41 +0000

    side nor insist feature that

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
