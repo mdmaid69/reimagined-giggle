db48b1fd208816f29802a6f868ff47cf834a2e24
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:50:10 +0000

    touch order Ff lot friend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
