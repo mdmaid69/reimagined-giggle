58a03fc2b8c2b44fbffc7280d94e8d85570743d7
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:56:35 +0000

    tall winter clean bag certainly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
