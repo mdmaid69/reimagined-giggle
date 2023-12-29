87f67f2cdeb64bc8770d7666f43e1adedb3ec895
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:50:33 +0000

    possession expect variety opposite quiet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
