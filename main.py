d582d46168b2af91a66b182fe7c2ac3039d05235
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:50:22 +0000

    picture OKorokay turn trap group

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def remove_duplicates(lst):
+        return list(set(lst))
