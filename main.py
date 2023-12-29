f53bf5d454f898405b3e3a0b2bf63364788f03ff
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:57:30 +0000

    set develop strike the favourite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def convert_to_hex(n):
+        return hex(n)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
