ce3a979592f4e71f2c742bc6460b9e62592b4b9a
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:45:37 +0000

    means uniform manner think somebody

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def count_elements(lst):
+        return len(lst)
