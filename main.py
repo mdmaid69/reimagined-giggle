362d6c53d09ca8dab056850f4d5a6c50caf21870
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:58:00 +0000

    lost entry anybody life joke

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
