fbab67c5b42ebf0a955f44373babb4080bc0c118
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:48:05 +0000

    aware earn problem authority luck

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
