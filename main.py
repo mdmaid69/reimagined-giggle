8c7b25efe08f02f9bec6de24bea9ef7e2930e88d
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:16:37 +0000

    bank quality actual surprised choice

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
