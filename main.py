055cf89775b567b83bc7c5913b7dfe1f1960599c
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:54:21 +0000

    age along attach match hear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-for i in range(10): print(i)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def is_palindrome(s):
+        return s == s[::-1]
