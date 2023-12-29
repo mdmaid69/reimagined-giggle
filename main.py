592e7591f1bb311a4050242562ab0e0858c01fcd
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:23:32 +0000

    extremely platform evening place mass

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import platform
-def get_os_info():
-        return platform.uname()
+def get_list_from_array(array):
+        return array.tolist()
