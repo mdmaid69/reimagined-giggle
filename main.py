9e0c65bc8c2febccc02148f54ed865ba62defb10
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:51:39 +0000

    emergency pick bound print accidentally

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def square_number(x):
-        return x**2
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
