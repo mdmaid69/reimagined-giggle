d58a10fabdff890b70ec2fd85ea43a2457116d04
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:28:36 +0000

    Kk ticket bother same mend

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
