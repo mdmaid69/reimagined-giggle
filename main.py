0a49fbe029db68d3ae6d78929769d2ce30dfd8fc
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:48:30 +0000

    furniture complete exchange wish lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
