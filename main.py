745dea4b3b3796b0f72ace08b6af0abb5d4ad1fe
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:20:00 +0000

    manage entrance last mark lot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def is_odd(n):
+        return n % 2 != 0
