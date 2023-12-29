ebf58ae8ca8a19eb25efdcc6b86bcf1fc15a76ae
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:06:14 +0000

    block exhibition wonder Vv flat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_volume(length, width, height):
+        return length * width * height
+  def convert_to_binary(n):
+        return bin(n)
