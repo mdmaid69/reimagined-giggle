03f0eeeb42b7a680c70d812f0ab6da19cfa88815
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:12:18 +0000

    raw satisfied uncertain afford another

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
