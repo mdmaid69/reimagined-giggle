40eb230e98a1d3c7d429d1d5a661f7ba70b83a05
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:58:22 +0000

    bored combination recovery there matter

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
