ab4030b68f130da7b6f982cd661ec428fbad6830
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:03:05 +0000

    criticize feed never exact moon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def count_elements(lst):
-        return len(lst)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
