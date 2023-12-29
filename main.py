7ca381612e3ff7f12e39a57e9dffd024d7ffc5a1
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:53:10 +0000

    keepoff notice reliable discussion bunch

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import datetime
-print(datetime.datetime.now())
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def add_numbers(a, b):
+        return a + b
