02926ea61efedd6560d35010f36730236a53cc80
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:09:47 +0000

    cap underneath control left complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
 import re
-def replace_all_occurrences(pattern, replacement, string):
+def replace_pattern(pattern, replacement, string):
         return re.sub(pattern, replacement, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
