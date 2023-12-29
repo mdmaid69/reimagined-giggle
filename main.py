75085c866ce8269931a19d3644da6abc9e03e316
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:54:16 +0000

    there section total treat rather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import sys
-print(sys.version)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
