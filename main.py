6a91e262460a025a9e4660d86d7d05e81cb4377c
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:59:51 +0000

    manner joke talk basically combination

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
