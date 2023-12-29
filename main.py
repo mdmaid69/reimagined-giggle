161c1dd8d8e5046bba46f21490e8e90e2620a983
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:17:28 +0000

    relative pain mention bag knock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
