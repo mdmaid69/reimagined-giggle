2bb480f73145e0c88ff68ac9069f6a2c7400ff92
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:22:06 +0000

    already Tt prevent worry you

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
