c30c2fab6ae2cbe8a5ed9e6049d3407192f90ba4
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:01:03 +0000

    want some read container stop

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
