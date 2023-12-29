160b87e1bd64ecfd47b0a82681a15b8d8ebe6df5
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:40:55 +0000

    fasten we degree walk class

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+x = 10
+y = 20
+print("Sum:", x + y)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
