c0ed1249dac8146b75c75283cdf5d0dd7706b416
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:17:29 +0000

    lunch or opponent heat manner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import datetime
-print(datetime.datetime.now())
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
