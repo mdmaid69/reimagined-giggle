edc2ae13ff37674608128ed77b6b50b19f49d7b6
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:07:45 +0000

    remain wall spray form order

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
