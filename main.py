f2ac2222287bd71ea3cc6667d9ece7cbd762760b
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:10:06 +0000

    advanced team state excited cheque

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
