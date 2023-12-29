8485d82d5940bac5c89041f85736c73c2aaa5e1a
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:56:16 +0000

    she lower food quarter business

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
