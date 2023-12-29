11c3180e84bc76056321b84d7127fcb33244ec0c
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:29:14 +0000

    wonder comfort road wide difficulty

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
