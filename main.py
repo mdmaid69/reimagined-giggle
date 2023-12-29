858fa80a75c92b9cc16266d52efe431a79978701
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:16:52 +0000

    hardly light lot well along

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def count_characters(sentence):
-        return len(sentence)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
