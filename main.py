18416d945c8154899839f0f18384d60b2fee1177
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:54:20 +0000

    progress kill see condition grow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def clear_array(array):
+        array *= 0
