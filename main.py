77711d76eb9b68baa66905e2a1bcaefb8392ebe6
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:04:01 +0000

    crazy tool her set there

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
