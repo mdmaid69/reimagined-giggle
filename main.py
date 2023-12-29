56fac1f5a0dabf58676220b823d32ea01a2ccfaa
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:50:35 +0000

    base earth frighten needle delicate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def reverse_array(array):
-        array.reverse()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
