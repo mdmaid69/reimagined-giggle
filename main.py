7fba2f59ec731034f4c376644155b5484ec43b31
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:57:46 +0000

    pool flight interest occur word

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
