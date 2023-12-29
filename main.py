023dee0627f2d90e5d9dc4f79dde466d4025f5fc
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:50:34 +0000

    entrance workon dream last rich

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
