1087133489f8a9f1d964031916237b9986c6fbbe
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:15:26 +0000

    pretend bread depend character lower

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def reverse_list(lst):
+        return lst[::-1]
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
