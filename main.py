fa70446114757ede8efbbd4854139f1493688b1d
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:37:17 +0000

    instant you fun comein breakup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_list(array):
+        return list(array)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
