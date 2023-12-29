a12a55770c53a19dad319f5cc065e34068c0cd46
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:01:14 +0000

    normal for dare praise limit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def add_numbers(a, b):
+        return a + b
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
