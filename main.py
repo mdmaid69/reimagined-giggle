1ab445c76d69253ed370c87ab3536af3dbacbeaa
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:26:53 +0000

    dream occasional sure kind smoke

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def find_min(lst):
+        return min(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
