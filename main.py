bab314bf4d4bb61cb9633a75cbbe796af089274e
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:54:46 +0000

    usually them violence until stare

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import datetime
+def get_current_date():
+        return datetime.date.today()
