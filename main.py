ecb3d3f1fdd68165da9f112307af443df94c61ba
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:29:50 +0000

    complete long content tense down

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def is_odd(n):
+        return n % 2 != 0
