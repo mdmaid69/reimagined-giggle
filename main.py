4d791085c16e814b8ced96702cedc698409b4b86
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:16:54 +0000

    language deal along border tape

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-print(math.pi)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def is_odd(n):
+        return n % 2 != 0
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
