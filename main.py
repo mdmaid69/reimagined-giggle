bedf8b5070b0d167d39d1230ee89fc97a7e58afb
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:29:03 +0000

    someone invitation suppose exact face

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_power(work, time):
+        return work / time
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
