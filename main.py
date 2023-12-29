b6485ac1f28c66c627f14a4c4181ee1c79e6a39a
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:28:29 +0000

    accommodation release able burst Ww

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
