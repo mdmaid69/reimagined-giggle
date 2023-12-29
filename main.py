d4acb5992201d3526fa0760bf06b2ed47aad2ca4
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:43:53 +0000

    suppose worse cinema hot invitation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_acceleration(speed, time):
+        return speed / time
