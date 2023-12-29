46013806a9b2816253caa736abda7f29710c86d9
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:14:34 +0000

    word meal scream wing raw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
