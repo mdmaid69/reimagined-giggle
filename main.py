ab3063240770150610423f369d8e1a5e16f917fa
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:52:33 +0000

    shock tall tough write regularly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import re
+print(re.match("h.*o", "hello world"))
+import os
+print(os.getcwd())
