fa5fe5b2f8fc79d19e79ef68a31703b7eec84f2b
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:27:08 +0000

    content light stay develop pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+print(math.pi)
