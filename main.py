a38fdadef288affb0d45644f6afcebbb3ccd61cd
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:50:46 +0000

    split guard suggest illness keepoff

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
 import array
-def reverse_array(array):
-        array.reverse()
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def get_array_typecode(array):
+        return array.typecode
