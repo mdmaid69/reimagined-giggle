96f4e2765b11d05ee1ff96c08bf84298fc212c8d
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:38:37 +0000

    callup earn sort generous lower

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def count_words(sentence):
+        return len(sentence.split())
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
