9f5cfd6a676e31483fefa9e57b674c20360dfeb5
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:52:16 +0000

    reply threaten record such lie

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def add_numbers(x, y):
-        return x + y
-text = "Hello, world!"
-print("Characters:", len(text))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
