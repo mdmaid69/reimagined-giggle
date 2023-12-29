4cac8e619a1d5b96b9bb97634911a20c745ea8f2
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:00:58 +0000

    plan simple limit tragedy nerve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
