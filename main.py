b1209558d1208c03ff59dbc448c90f99732a8457
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:05:45 +0000

    smart broad entrance sort pollution

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import datetime
-print(datetime.datetime.now())
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
