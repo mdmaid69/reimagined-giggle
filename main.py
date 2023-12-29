de222ae7c728e2bd7810ed4f6ffdbf17af29bd8a
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:58:08 +0000

    you fact seat book house

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
