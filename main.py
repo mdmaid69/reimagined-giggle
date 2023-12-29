cac9b11bdf871642cabc03a1d9a53bebf52839c2
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:08:24 +0000

    reaction join steep they bottom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import sys
-def print_python_version():
-        return sys.version
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
