bcd3dce8cec13b5e431126ceba7f2885ff6db305
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:45:54 +0000

    mood takeout reduction into naturally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
