ce475d3ebdf6ebcd5914f5018051d51c221b8f56
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:54:28 +0000

    rarely instead morning unfortunate whom

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
