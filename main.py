ca21f33e1d56e74db48a71b5654b53c90bbc87e5
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:45:39 +0000

    yes age speech patient note

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_volume(length, width, height):
+        return length * width * height
