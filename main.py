ea8c4510067d8fc744c7703a2ceb251661296eb1
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:10:43 +0000

    return explanation sharp chin spin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  def convert_to_binary(n):
+        return bin(n)
