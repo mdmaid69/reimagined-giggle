2fdd83acd048c3007e5c8ec7448b261cfdc42322
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:44:49 +0000

    likely 566end possible able cry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+def is_palindrome(s):
+        return s == s[::-1]
