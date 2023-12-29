ec0c21eb1165db35e11420dc69d19f369183d145
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:31:48 +0000

    manner bored nearly dance takeover

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
