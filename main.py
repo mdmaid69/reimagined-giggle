12607a828151dcfeb108cb9168b3b2c56f868b9b
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:44:31 +0000

    value minute closed bath peaceful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
