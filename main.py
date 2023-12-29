cd83ff760e685e8908676a6973e4c6b5625bbef4
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:29:23 +0000

    supply but average tip butter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
