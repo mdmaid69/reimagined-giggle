a89df43b134668526bad8f3fe5a9e9f1038a93b3
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:18:00 +0000

    pair perhaps upwards minute lookup

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
