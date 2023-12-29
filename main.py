eb68cbf70d7aa9124b15788c00fc56c8cfed90a6
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:49:19 +0000

    finger problem part ill touch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
