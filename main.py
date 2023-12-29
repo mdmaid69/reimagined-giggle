32d3a14664288fbbe98423b0939265c29472f0c6
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:02:47 +0000

    bath important surprise fun for

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
