3dfb993b9b13ac38c4eaf498547d6ca312d0ceab
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:00:51 +0000

    goaway partner emphasis compare impatient

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+name = "Python"
+print("Hello,", name)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
