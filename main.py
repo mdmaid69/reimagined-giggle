84561eb51db32172cae415529945e001af7db852
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:57:43 +0000

    thankyou grand may consult hair

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
