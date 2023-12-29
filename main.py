82dddc0ea7d5b9ca69f131c2e43d52bd6c667afb
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:24:01 +0000

    beyond defeat sorry opportunity mad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def reverse_list(lst):
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def reverse_list(lst):
         return lst[::-1]
