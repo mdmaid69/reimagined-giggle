b1c5f1a53aed9472d24837654a4ec27835945d55
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:19:14 +0000

    extra often jump increase copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import os
+def change_working_directory(path):
+        os.chdir(path)
