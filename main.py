a4cecc9f1aff06dba50deb8629bc418540756467
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:24:50 +0000

    bringup able juice own become

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_item(array, i):
+        return array[i]
