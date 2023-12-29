daf9fc1d26fa2802ccba6bb2c786a62f6ee38890
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:51:49 +0000

    whatever fear rate tread right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import math
+def calculate_floor(x):
+        return math.floor(x)
