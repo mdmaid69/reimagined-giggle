fc96cd921bdda9ebbfc1896c84210ecd572082b4
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:15:30 +0000

    goon contrast broken classroom other

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
