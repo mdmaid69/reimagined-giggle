6b1f1d0bc55434fabe7a100ee79b85e49905bb43
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:03:53 +0000

    politics lunch convincing bringforward exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
