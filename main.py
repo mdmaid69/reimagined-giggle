cc9c8da7954432af14d9bf984aff26549cf190d1
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:58:32 +0000

    admire drop passenger confusing conversation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
