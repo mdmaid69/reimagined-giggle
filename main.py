c87b7c23477e5bcd550c8af16e65b87899885fb2
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:44:33 +0000

    needle square save play standard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
