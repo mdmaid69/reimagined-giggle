f1000c5753f8427481f0f3a3281939a84957ba0c
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:42:00 +0000

    through imagine mixture mixture emotion

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
