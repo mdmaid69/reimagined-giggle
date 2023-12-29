28acdd6db56121e3c39318b28380fbbd333c51e0
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:33:03 +0000

    control workon base best attract

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
