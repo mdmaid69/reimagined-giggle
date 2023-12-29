73b1a9b1a2263a949df320c236f74b0cfbcd2876
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:34:27 +0000

    comeup move unusual difference Pp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
