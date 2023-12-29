a847ce615d863cb2cd3d3274a58585e7f781b049
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:02:34 +0000

    vote welcome spare seem call

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import time
+def get_current_time():
+        return time.ctime()
