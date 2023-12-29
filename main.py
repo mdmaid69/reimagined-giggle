89638371ebb4a823601a8742aba9951e3900ddaf
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:10:49 +0000

    though stretch bad his fix

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def is_odd(n):
-        return n % 2 != 0
-import time
-def get_time_since_epoch():
-        return time.time()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
