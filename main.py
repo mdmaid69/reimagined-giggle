aee49e816767d6bfd23f3c601d5b8ea8cb03f7fb
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:53:37 +0000

    minority hole holdup passage entrance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def find_max(numbers):
+        return max(numbers)
