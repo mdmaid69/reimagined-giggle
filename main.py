d8e9e5057205c148738a9b769b364381d1419692
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:15:19 +0000

    embarrassing impatient care hold train

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
