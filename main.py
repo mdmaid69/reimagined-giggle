82249a5521a93c2d2d1ee9059f2c63909e59422e
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:34:47 +0000

    successful variety impressed shall port

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import platform
-def get_os_info():
-        return platform.uname()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
