d304c70a18cb8cf552db6a524903368e1d2f9e29
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:18:40 +0000

    clean seem friend burn downstairs

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
