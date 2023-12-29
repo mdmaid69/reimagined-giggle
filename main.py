e11b02c123057e200afd3ccf0a3281a86bf0ef65
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:40:07 +0000

    free Ss smooth lift lot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
