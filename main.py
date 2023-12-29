d66fa6fb04813f4078af9224fb7302e3c6eb30c9
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:40:50 +0000

    sentence innocent history will nerve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
