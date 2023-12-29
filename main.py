ed5628ad96f16d97c97021c5da93d22a8765d25d
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:20:26 +0000

    violent carry bad bath keepoff

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
 import math
 def calculate_polar_to_cartesian_coordinates(r, theta):
         return math.polar((r, theta))
+def calculate_power(work, time):
+        return work / time
