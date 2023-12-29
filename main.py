bc7339f30d9fd75ac63a173f7118abf66f45dbdb
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:16:47 +0000

    adjective smell health sport half

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
