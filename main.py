4d7afef766dc0b628b490a5e87a3ae5746e7ff5a
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:02:49 +0000

    when wind light accordingto low

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
