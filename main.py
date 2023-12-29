55935f7e904246ad7a0ccaff5750616ff0c36651
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:20:23 +0000

    area control confidence cover give

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  def convert_to_binary(n):
+        return bin(n)
