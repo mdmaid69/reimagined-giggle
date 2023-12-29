fe0d758defcf866c20fcfe8e677d58c36b1b26a2
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:41:55 +0000

    mistake like single knock opposite

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def get_array_as_str(array):
+        return str(array)
