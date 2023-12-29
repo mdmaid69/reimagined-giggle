7dcf42383d0e0e049d0b791365f0dce60f3b196b
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:18:48 +0000

    major sick once fall month

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
