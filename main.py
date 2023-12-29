657580ed5f3061d3e98d8fa7394fc7f634d6538c
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:18:51 +0000

    crowd situation ill use through

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def count_characters(sentence):
+        return len(sentence)
+def remove_duplicates(lst):
+        return list(set(lst))
