596f7200e5775a3b816d9c9d49d6f968c21e89e8
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:12:34 +0000

    border judge paper Gg bottom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-def convert_to_hex(n):
-        return hex(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
