f10a27cf3183d79e1f893d70bd6a87c25678c90f
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:15:30 +0000

    hope smoke private detail object

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
