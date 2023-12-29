b57d406c3ab250a8c1aa3c9204e590a670935b43
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:22:51 +0000

    loud be plain deliberately union

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
