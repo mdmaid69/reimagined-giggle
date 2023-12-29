25f6d0aa2d5724e46314724113af720867d14e71
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:55:17 +0000

    one woman convenient few kill

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def is_odd(n):
+        return n % 2 != 0
