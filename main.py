72537d99a285bc138bc97b0ac8a0f8aa3b92eea4
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:24:58 +0000

    568energy force tall convinced characteristic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_volume(length, width, height):
+        return length * width * height
