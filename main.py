dffea426946994dc9f63c49d426b86e604d0bb64
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:32:29 +0000

    accidental nor forever could recent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
