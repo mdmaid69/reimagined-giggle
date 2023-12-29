d8aef6abc42a749753d3a8a5daa311e8ff86b513
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:13:16 +0000

    why result with represent plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def count_elements(iterable):
+        return collections.Counter(iterable)
