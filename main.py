8b3f3c6c9618b7114108cf8ec4460be5866de04b
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:20:35 +0000

    patience ignore marry workon everyone

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
