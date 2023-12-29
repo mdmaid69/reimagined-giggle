702efbc0f91f2d6ca666313d10efd4bfd659ce05
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:30:50 +0000

    round call high feed tidy

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
