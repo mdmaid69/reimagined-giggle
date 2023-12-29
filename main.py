3a5d9f414fa452b2d8c2a1888f44ca8fb9926653
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:54:29 +0000

    block ourselves OKorokay protect giveback

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def roll_die():
+        return random.randint(1, 6)
