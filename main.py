af5d2e854657e58e9805753fa636bb0d90242447
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:06:24 +0000

    interesting law new foot tense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_error_function(x):
+        return math.erf(x)
