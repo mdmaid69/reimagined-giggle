d993cc8adb14f8a7342a7a623effb3d78f541100
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:36:13 +0000

    comeoff note final condition prefer

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
