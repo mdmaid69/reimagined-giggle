7216f13548e4584ef4d950c62d4aeeebb1378b3e
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:05:01 +0000

    raise thumb contrast only doubt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def clear_array(array):
+        array *= 0
