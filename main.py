5aa16dadbfdcd99bbb48fafeade2ca849a916842
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:03:33 +0000

    already offer as child score

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
