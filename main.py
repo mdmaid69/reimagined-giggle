6c8b76e5aa69aefb710a88615396bb68047df747
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:27:08 +0000

    connected giveout high picture mouth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
