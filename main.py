9db229873022e0d0a69e940e47fa98e8f0448fc3
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:55:22 +0000

    hand fix appeal keepoff among

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
