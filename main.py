33dc134ed7a3cdb29205b04a7d516590a044d9d4
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:03:05 +0000

    temperature season possibility remain needle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def reverse_array(array):
+        array.reverse()
