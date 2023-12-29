ddb88b2a06cdf8361b1097855fb3a1abb3b45282
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:14:40 +0000

    wheel least plenty why together

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
