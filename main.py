e1e35c01a1d6ba4518bed30e10807ef9b75dfa16
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:02:59 +0000

    usual single approval cost Ll

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import array
+def get_list_from_array(array):
+        return array.tolist()
