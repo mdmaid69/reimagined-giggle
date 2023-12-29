88b1e6e19ec2539ca1e79891d2f7a4e9af8f2ed2
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:46:20 +0000

    funny belief contribute mark disappear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
