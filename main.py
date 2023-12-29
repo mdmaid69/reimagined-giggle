3bb8e41e30aeebd9f9cef58e9e92db86dc42b413
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:26:38 +0000

    move though stare invitation shop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import logging
+def log_message(message):
+        logging.info(message)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
