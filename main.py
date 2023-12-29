df6c6425cc8c46fc3ef6ca3cae7e8e21a2c99027
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:41:32 +0000

    like tell aim university her

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
