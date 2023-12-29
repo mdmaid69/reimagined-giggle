e9afc1eb6260b0b3d57375d262900868bd66ad3e
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:52:11 +0000

    reason correct mind improvement present

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_current_directory():
+        return os.getcwd()
 def calculate_present_value(future_value, rate, time):
         return future_value / (1 + rate)**time
-import math
-def calculate_tangent(x):
-        return math.tan(x)
