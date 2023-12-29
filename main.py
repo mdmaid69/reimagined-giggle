9575be43b537749fa50e9dae24d6dd7c446b1a0c
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:33:52 +0000

    floor roll make generally event

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_as_bool(array):
+        return bool(array)
