28b7debd588e3e851086fcae12d073393fc9ae94
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:46:55 +0000

    bank smooth so wire compete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
