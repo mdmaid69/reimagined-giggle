36940aeee1278f8830da1aec6eb381f5e5d31954
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:02:27 +0000

    read ball best luck aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
