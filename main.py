a421f54e4242956a05d8031907a4ac6084c0e66f
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:46:01 +0000

    discussion minor clothes if visit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
