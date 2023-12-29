7ecbc292267cd0847b21e2fad7fb902a0ae47d8d
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:56:41 +0000

    raise similar rubbish plenty benefit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def sort_numbers(numbers):
+        return sorted(numbers)
