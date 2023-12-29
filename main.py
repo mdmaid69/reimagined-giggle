3a73375084b2a28fde8b610f3c4c3247662051cd
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:49:22 +0000

    available strange like usually hard

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
