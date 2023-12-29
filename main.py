b8de7fbee4e2e897b6283ea6a5cdbb466871d177
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:52:57 +0000

    bunch possibly trouble callup special

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import time
-print(time.time())
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
