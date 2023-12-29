95f6c95dc83f54f1e5115cb76303ea4b04b45224
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:33:19 +0000

    owe story concentrate in exaggerate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import sys
+def print_python_version():
+        return sys.version
