c10f17895b515ffdd7ee82c3e81c220c0412a35f
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:36:13 +0000

    breakin while ball trap copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
