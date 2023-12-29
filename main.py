6ed67e1987b8a6698a5587df5c6d725555aa1501
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:25:04 +0000

    create heavy condition old centre

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  def add_numbers(x, y):
-        return x + y
+print(sum(range(10)))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
