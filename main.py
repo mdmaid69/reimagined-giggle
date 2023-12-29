dee9928a872674237567aa6e75e747e94cfd2bf6
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:51:39 +0000

    difficult form body be uncertain

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
