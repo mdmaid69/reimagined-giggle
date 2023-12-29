af9b5d1a6c44187840c2897baaa193e8f7878748
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:45:32 +0000

    there inside chief tool best

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import time
-def get_time_since_epoch():
-        return time.time()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
