29882f4f7e3d7bc69010df2620baa443f592e612
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:21:54 +0000

    convincing greeting punishment announce day

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
