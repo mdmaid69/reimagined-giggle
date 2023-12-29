e44aeba3d0c983f82342b50742eb6e26244433d0
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:51:27 +0000

    across married system laugh place

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
