666c5c51369d432675f48ef646e910d3df480586
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:45:12 +0000

    attractive most shadow herself cold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
