3b1a9514c67a07c3888f016a64d8fbb1101a71e6
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:23:51 +0000

    over escape present bringdown task

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def cube_number(x):
+        return x**3
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
