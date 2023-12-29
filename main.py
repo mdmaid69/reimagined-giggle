93087a72d0746a87353538b0c6723dac3d8929b3
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:27:50 +0000

    shrink comfort especially print complete

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import sys
-def exit_program():
-        sys.exit()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  def multiply_numbers(x, y):
+        return x * y
