8d1d99834a9ac4de2f65dfba90c57c9283914e27
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:14:28 +0000

    holdout eager confusing win invitation

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
