6d7379ce82fe2a1b0fd172090d052946ed2e36fb
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:20:01 +0000

    next clean cut closed callup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
