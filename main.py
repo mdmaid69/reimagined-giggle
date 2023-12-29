ea840f5fd0a8e3a8bfca28d5a1cbc7970c4b9d95
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:36:27 +0000

    if politics quantity produce admire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
