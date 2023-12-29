7282a6aa0b6dc2debe07e55ef25e11d0c45c0048
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:28:37 +0000

    always move character church steer

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
