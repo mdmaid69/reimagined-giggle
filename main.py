c2663fea6d1b5335a2c0ce12dd0522d936731d47
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:22:57 +0000

    powerful among discover professional forget

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
