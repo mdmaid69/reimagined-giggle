233a8dc0e7428eaf8d735d308197d064877afc39
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:07:55 +0000

    disappointed considering vision ours measure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  def remove_duplicates(lst):
+        return list(set(lst))
