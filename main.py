2fd74536f959fa3ae9cedab791adccb0b78370ea
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:58:30 +0000

    related lesson news comeout accordingto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import time
-def get_current_time():
-        return time.time()
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
