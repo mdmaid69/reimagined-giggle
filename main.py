6dc4bfa8d296c4dbe2724b773ff70a4597d337d3
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:51:01 +0000

    refuse rest treat artificial maybe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
