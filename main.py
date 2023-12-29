8ffd317fabdd80cec3e8bcbf8f29c10b219cd10a
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:49:29 +0000

    bye contribute let angry urgent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
