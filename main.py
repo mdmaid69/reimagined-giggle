6a7dde72164a41ff7706f203aaffa180a50776d3
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:51:37 +0000

    push stop trap negative pattern

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
