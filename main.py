f01ebdea329d39395963cc85db80eb0d2204f9b9
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:38:14 +0000

    while emphasis ocean polite bottom

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
