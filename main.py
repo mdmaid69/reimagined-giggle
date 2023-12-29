e00cf20ab0a84bf036e691b111f5cd44d56e34fb
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:39:01 +0000

    watch among rule centimetre careful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
