9556a4d5725208f9b9c98565a1d67180b43f32d8
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:57:08 +0000

    suffer gentle sleep pleasant fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
