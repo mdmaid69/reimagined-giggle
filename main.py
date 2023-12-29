f28dc8326e030368c295d8690af9c2eee870e481
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:14:25 +0000

    round opportunity catch join have

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def is_palindrome(s):
-        return s == s[::-1]
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_average(lst):
+        return sum(lst) / len(lst)
