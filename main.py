0c713b0d3bcc713b02f94f868ca4e0d03ef10d03
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:00:29 +0000

    afraid giveout speak convenient niggaacademy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def convert_to_octal(n):
-        return oct(n)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
