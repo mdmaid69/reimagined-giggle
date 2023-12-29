96d90676b83d1b560bb2e7c19ccd3d098a99cc02
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:13:56 +0000

    platform stop recovery permission exchange

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def square_number(x):
+        return x**2
+import numpy as np
+print(np.array([1, 2, 3]))
