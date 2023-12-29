494f69c2121ac65912026ab3f52639b9ba478193
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:17:35 +0000

    stretch later protection produce language

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
