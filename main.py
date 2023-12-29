57476eef2482537a003cbc56249ffd29e91fbd04
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:36:47 +0000

    doubt rank accident dry holdup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
