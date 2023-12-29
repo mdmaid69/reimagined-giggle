a2b656500b19c7c46b12a71523d2c7404cc666cc
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:24:11 +0000

    on noise task tap brother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
