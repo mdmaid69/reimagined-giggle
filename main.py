3ac0d58cc3d078696bcfae3225869be09bd7a363
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:29:40 +0000

    hardly she furniture in tired

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
