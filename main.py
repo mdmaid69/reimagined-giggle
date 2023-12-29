af36ebcd06122e407142cec02b9c0455366bde0b
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:51:41 +0000

    back smooth approve inside rate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
