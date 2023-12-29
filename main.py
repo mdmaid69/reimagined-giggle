ebae1a1ee0c0e9f7828ebf2c85b794090ad6fbda
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:44:46 +0000

    kind weekend lead free win

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
