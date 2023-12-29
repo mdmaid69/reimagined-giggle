dc784bc55295826bc90113234d81763b6ec68293
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:08:18 +0000

    strange sign both dangerous nerve

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import time
+print(time.time())
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
