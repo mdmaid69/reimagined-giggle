0f8fc0b0d771078581ffc53cd22fb653cd31826a
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:49:47 +0000

    maximum estimate cut pause such

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
