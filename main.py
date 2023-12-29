d76906fb37558dd321e3c0d1d519d6982cdf7103
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:22:41 +0000

    probably affect afterwards phone exploration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
