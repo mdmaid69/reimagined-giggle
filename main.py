c91f761d3f576ea733613830d08d389884a567b4
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:24:42 +0000

    study lesson world control contribute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-text = "Hello, world!"
-print("Characters:", len(text))
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
