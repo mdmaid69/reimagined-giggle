031b4727b8d865fbd9a64c56d5205a860d65e88e
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:32:47 +0000

    introduce likely prevent takeaway rain

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
