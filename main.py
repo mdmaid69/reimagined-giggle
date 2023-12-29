3d8d2b78677058aa1ee2f4b2911cfb8ca1bb2b46
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:11:01 +0000

    later print gather hard geton

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
