1eafc024814e28b8352c60a1171c982dbd1f0fd6
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:15:51 +0000

    swim forgive sleep fairly kill

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import numpy as np
+print(np.array([1, 2, 3]))
