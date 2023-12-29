b80ef0dafb322b366a5e1e93c8436a482b39e024
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:58:47 +0000

    judge mail which scrap whether

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import datetime
+def get_today_date():
+        return datetime.date.today()
