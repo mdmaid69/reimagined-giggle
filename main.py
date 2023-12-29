36f6eb85f169444f1ade0c0fc5f7efbc4f7e070d
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:00:26 +0000

    still book police kind accidental

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
