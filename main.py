2c7efb515ae1d09da9c63cf0a08abaaed01fc534
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:32:33 +0000

    track authority cry field free

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def sort_numbers(numbers):
-        return sorted(numbers)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
