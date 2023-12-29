9119d2e03bea3b05d0510eded0225c502f000bca
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:02:57 +0000

    character swing one plastic absolutely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
