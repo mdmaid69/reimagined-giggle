387d6985220d1cc1d3491791789ca0ef59c99baa
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:54:18 +0000

    art seat market few address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
