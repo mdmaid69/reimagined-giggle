e7d48aa9236faf7a46831324a99a400adb9f36af
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:21:15 +0000

    star constantly long callat middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
