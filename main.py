f8e305c54e6873c9269435dab5688c4e14fc4c77
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:07:40 +0000

    crash change indeed exhibition dish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
