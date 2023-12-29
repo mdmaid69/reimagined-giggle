83603f80c6a1de57943f650b370dd581a210fe4c
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:09:07 +0000

    connection news stay passenger you

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def sort_list(lst):
+        return sorted(lst)
