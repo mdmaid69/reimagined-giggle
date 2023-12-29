686d635b3cfe5eee09b4a4f0c242796670f2c7ec
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:23:31 +0000

    kind later wave build less

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
