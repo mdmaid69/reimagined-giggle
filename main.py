45d4b3c73687b0d0b5299bca84b72e7153d1ba87
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:06:51 +0000

    advantage there distance region name

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
