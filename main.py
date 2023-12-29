9c64eebfeba64b1ec0d8144ba84e47bc806aee01
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:31:34 +0000

    lonely disturbing determined ours seriously

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
