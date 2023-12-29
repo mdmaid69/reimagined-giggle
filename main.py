280d09913013c49c16c00fc7c720427583575400
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:43:51 +0000

    born along repair gift coat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
