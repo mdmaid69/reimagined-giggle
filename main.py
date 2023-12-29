e9e29505ea3ea3a68fd79321c705ee7065ff3991
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:58:12 +0000

    mistake Rr drawing concerned sweep

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
