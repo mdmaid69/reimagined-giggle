985b3aba7dfa613b2a32eaef3c4b868f28a28a2b
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:38:25 +0000

    reduce mark short fasten contact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
