328bb61d7d88d1124360ec969ad8c8c6067d3bd4
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:32:19 +0000

    mind thanks back decision win

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def find_min(numbers):
+        return min(numbers)
