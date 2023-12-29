ff81ab27625ecb0fafddbc9e1acb6c7f379b3101
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:39:13 +0000

    material thank seriously exact almost

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-def sort_numbers(numbers):
-        return sorted(numbers)
+for i in range(10): print(i)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
