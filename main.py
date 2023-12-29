30c0313ef0b6e56f29428dfb1881cc026e4d11a9
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:44:25 +0000

    side market innocent involve me

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
