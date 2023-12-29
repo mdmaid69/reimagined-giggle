4114b8016ffd28123efbcaecf86fe6c9edbdba11
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:49:15 +0000

    straight lift urgent takeaway field

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def cube_number(x):
+        return x**3
