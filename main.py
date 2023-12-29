c0c8c2e3c42805b8100c06712cfb5786362114ce
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:13:21 +0000

    during manage change rule early

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
