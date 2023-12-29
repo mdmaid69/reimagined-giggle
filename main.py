19b27c65681dd471cd98fcf13e6cbecf29e81aba
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:56:51 +0000

    stage practice lookback(on) he mixture

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def get_array_as_str(array):
+        return str(array)
