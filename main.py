0bd2e8652b3f1ea33262f8751c899f786d963d96
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:36:19 +0000

    through seriously march age decide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
