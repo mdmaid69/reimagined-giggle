a456647887660c128b3c7351aca5fcb4f76a4426
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:09:29 +0000

    trip eat difficult uncomfortable difficulty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
