afbc974f60ca4235eee86c079ed0bb1b497fdee3
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:43:53 +0000

    dress occasional offence colour outside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
