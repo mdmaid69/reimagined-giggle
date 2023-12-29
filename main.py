d8af009295098032e492f65e60254a6dcf6bc5c2
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:39:35 +0000

    block gradually block season already

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_repr(array):
+        return repr(array)
