ea358df18d6e7ac3b11b9d1784520cf8af45c204
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:28:45 +0000

    concern deliberately centimetre bang confusing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
