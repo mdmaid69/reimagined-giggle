6fed1a2f8623a77dc4d735f81c2cc222991386b6
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:20:55 +0000

    attitude sink flat edge loss

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
