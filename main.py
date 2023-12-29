542a6ea5ab85f72b37a0defb57ebc29e1024bff0
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:06:52 +0000

    reserve taste crowd well opinion

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def cube_number(x):
+        return x**3
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
