d4dac94b355bd704c72181958f8b4bcb3f63ca23
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:26:52 +0000

    search complaint expensive question cut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def square_number(x):
-        return x**2
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_float(array):
+        return float(array[0])
