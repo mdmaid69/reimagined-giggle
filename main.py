049bad04bd4dcb6e023cceb24da58ae6e8c9fe2f
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:30:24 +0000

    Mm insist afford drive middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
