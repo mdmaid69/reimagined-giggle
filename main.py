2a1704138cc934acfc214d7033af91afeac1023c
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:34:06 +0000

    dull autumn oil week family

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
