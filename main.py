be7ba1784e02cd981bfd83cd1158ec73693c2529
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:25:45 +0000

    rarely neither dick store me

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def get_array_slice(array, i, j):
+        return array[i:j]
