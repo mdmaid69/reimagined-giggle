a7fb220476ca1f5eaf4c18a85f1bae6dee533b84
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:25:08 +0000

    sight day praise tragedy rarely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_power(work, time):
+        return work / time
