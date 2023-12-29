20d4a2b49094e5f72587a1bce27303a7e5394b06
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:50:31 +0000

    arm meeting turn better involve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_acceleration(speed, time):
+        return speed / time
