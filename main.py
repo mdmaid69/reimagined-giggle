133fbd9fac95f69cb8ed6a43dc7114c7cda31764
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:42:10 +0000

    creature once basis alive recognize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def find_min(lst):
+        return min(lst)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
