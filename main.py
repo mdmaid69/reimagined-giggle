016009625326616bf084bf35d7a85e575c0eff2a
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:46:38 +0000

    takeoff part command giveup appeal

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_min(numbers):
+        return min(numbers)
