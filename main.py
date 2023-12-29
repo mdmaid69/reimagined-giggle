7803dc849a7f231b99d2ad9d4588c8e3ba1bd0ab
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:15:14 +0000

    chat right day mine react

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
