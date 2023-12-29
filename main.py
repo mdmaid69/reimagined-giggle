3da480096f6c4ff60ad12754fd3c86ebad81df9c
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:30:52 +0000

    again wall weak people receive

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-print("Hello, world!")
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
