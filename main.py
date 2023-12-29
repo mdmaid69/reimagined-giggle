6a1f10794e266cb5334b42401473ad964628b516
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:09:21 +0000

    rely shut please regularly complicated

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
