397207faf685d4a5f450900ed2dd2d714fd7ed93
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:07:07 +0000

    cover pair getaway shame drop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
