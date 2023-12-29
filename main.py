9a87512f6844c7cfbd773bc5157ef4c133b9320c
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:33:17 +0000

    bread bright schedule sort violence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
