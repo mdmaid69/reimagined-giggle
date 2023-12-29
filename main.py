7b34b50bda690dfa7b52cb4830a10aee3bf4601b
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:12:19 +0000

    land comment wild fence disagree

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-print(datetime.datetime.now())
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
