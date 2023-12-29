fa0574ed584becbfaf881cb0d9f22a244088456b
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:01:53 +0000

    hot copy lend believe taste

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
