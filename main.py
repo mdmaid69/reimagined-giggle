336070714ed289add692c7f55c5aa4e51325702f
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:48:23 +0000

    deliberate deliberate recent pinch announce

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
