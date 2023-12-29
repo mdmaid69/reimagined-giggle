4670eca847829df135c569d5a428dd6bfdaecae5
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:45:42 +0000

    wish wind post chief Ff

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+x = 10
+y = 20
+print("Sum:", x + y)
