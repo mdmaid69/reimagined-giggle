ff428824ea2d41664a3333eee3f34e4216545373
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:54:46 +0000

    tonight everyone beside lead sore

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
