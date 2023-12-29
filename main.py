9959ae6ee64166542aa6e8179e153ea3aed9870b
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:29:27 +0000

    successful her slip OKorokay hurry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
