1c6900666446cf3ec9cf29b0df2c15f9ebbed8bf
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:47:03 +0000

    manner history exercise before advanced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import math
-def calculate_pythagorean_theorem(a, b):
+def calculate_hypotenuse(a, b):
         return math.sqrt(a**2 + b**2)
