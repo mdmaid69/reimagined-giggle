215202e964d929f861f10f73d652ae30ed1ad559
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:13:25 +0000

    everybody burn consider move evidence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
