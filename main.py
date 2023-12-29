dd9b0f85a9df30df74966d17df4d54d499e2ee49
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:16:57 +0000

    lookafter will weak fit software

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
