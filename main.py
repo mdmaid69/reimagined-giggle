924734dfeb860cc90abfffd143f1db1ff040aab6
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:37:15 +0000

    circle angry stranger doctor class

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-import logging
-def log_message(message):
-        logging.info(message)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
