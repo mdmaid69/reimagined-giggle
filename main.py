fd0db069d8e4b9d6f5c7ce27e5f05782309c0997
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:29:37 +0000

    welcome funny priority bright bury

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
