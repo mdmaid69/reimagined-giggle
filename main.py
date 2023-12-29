b35db2a8e27f2ad6b944e1738e9a6c6459dfbd1a
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:06:57 +0000

    development distant ours lookup guard

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
