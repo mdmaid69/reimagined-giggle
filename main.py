4ef18561b9fe60a4b387fbd9a7bf128a924c3c90
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:26:19 +0000

    band guide crazy new top

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
