9e989c393eeb054fe2d4365f621f68e8dd7427ff
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:25:51 +0000

    name foot broad close minimum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_string_from_array(array):
+        return array.tobytes()
