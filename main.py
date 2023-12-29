bc8cd199a46dbcb3f356bf4ca44dcbe293210ace
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:21:29 +0000

    force life like valuable firm

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def convert_to_octal(n):
+        return oct(n)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
