b2c583e32b7e4b2be46695fb603bd6e174f88715
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:35:49 +0000

    wise belief door afternoon command

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
