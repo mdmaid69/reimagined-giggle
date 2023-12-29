a565f8cac1cec6d0f2c01ca7ba0bcd8be23f3927
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:40:34 +0000

    offer somewhere use sweet science

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
