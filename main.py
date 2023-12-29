abfca27536e87b05d07f6690052313728cb7508f
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:47:04 +0000

    light kick calloff company building

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def is_odd(n):
+        return n % 2 != 0
+def find_union(list1, list2):
+        return set(list1) | set(list2)
