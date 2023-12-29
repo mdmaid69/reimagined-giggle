b5a9f431c8613155b4590e5adc3bf5180064709f
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:25:44 +0000

    pay comeout turndown change positive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
