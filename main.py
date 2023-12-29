bc4c4d30f13d4ce6f925af9d869292320c6f374c
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:17:00 +0000

    talkback strike drive comment beneath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
