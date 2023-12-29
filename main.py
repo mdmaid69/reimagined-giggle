76e811f24a3b05c5f229a6b1c5e56fda8465f339
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:53:08 +0000

    bound left your but keepfrom

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+print("Average:", sum(numbers) / len(numbers))
+def is_palindrome(s):
+        return s == s[::-1]
