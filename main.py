de6cd71c3ecba3411b46126b79d51e7c1fd8e697
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:10:12 +0000

    notice realize price cruel breakthrough

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
