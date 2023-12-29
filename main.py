335434009ff2ef9e2292b5f09bc4a763711b539b
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:22:56 +0000

    pay until though argue pen

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def delete_file(file_name):
+        os.remove(file_name)
