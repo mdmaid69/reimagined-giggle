ce472e04f8e05d5d242be0ae9b3985602e12fce5
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:57:19 +0000

    common tall wide divide community

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
