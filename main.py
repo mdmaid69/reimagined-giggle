03fda114b69d92caf3cc1dabeebb6fc9a23fd81e
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:08:21 +0000

    theirs quality shadow keepup future

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
