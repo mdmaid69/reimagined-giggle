82161e9a0326c9186b76f85a23f309019be4927e
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:08:05 +0000

    position mistake right brain attention

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
