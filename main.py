4e66f685443be86edafb93a23f314f49ffd124ef
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:41:14 +0000

    star song thought hers no

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
