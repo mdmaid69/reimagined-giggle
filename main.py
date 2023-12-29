f304b624fc1b143b14e365bb288cdc57f50d2d6b
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:40:34 +0000

    treatment all anxious entrance wrap

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
