343eb7c298c139918dfb3a592be055ec0ec0ab92
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:33:57 +0000

    front pretty morning quit comeup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
