707491e38c5a6f97c3e1bb22a68342f318506693
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:21:27 +0000

    fight chin born power unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
