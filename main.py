b7d7bff97153b27c62080cdf37bbb0735defdcd2
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:14:14 +0000

    various surprised charge worst accidental

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_density(mass, volume):
+        return mass / volume
