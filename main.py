d0aef32409d7f6494d0b3776785199cee3e0c073
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:04:08 +0000

    stir themselves me frightened impatient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
