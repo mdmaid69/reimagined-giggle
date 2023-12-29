e5e08605a0325b9387f4a399b94ca3f8354ec1d5
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:55:08 +0000

    expensive figure rough rent simple

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
