1c275146781f83e723779868dc7ca08adb58ae9d
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:30:25 +0000

    know everywhere equipment naturally change

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
