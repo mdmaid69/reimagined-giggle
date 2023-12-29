44022b4003ee35af172ab0302c4ff9b796ebed14
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:21:33 +0000

    row holdout waste above now

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
