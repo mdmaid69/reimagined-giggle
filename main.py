cbd950a655073d56b88b0abd3fc10bf4f94eebef
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:41:57 +0000

    comment before interruption stone proof

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
