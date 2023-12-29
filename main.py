2fda14703f31bff0a259e1e61f7a9672decf8e82
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:17:25 +0000

    name win standard away hurt

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_current_directory():
+        return os.getcwd()
