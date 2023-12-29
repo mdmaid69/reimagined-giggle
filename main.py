74c2ad3f6f936bc07c8d4cd9e7f785662c54f20b
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:10:02 +0000

    burn proper very street post

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def is_even(n):
-        return n % 2 == 0
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
