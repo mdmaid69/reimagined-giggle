fb04a72a516bec392932ad3b154dd075525b3f4a
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:30:39 +0000

    accommodation sharp short arrange afford

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def count_elements(lst):
+        return len(lst)
