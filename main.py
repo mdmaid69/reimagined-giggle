4834e0ff293bd618fbc34ec69f85578b120b233f
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:31:48 +0000

    dish friend much performance past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
