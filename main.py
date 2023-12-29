ca168851d672a328346e427c09264200469fb23f
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:23:25 +0000

    crime frequent keep acceptable bet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import datetime
-print(datetime.datetime.now())
+def calculate_density(mass, volume):
+        return mass / volume
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
