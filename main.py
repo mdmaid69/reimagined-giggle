5e143e4131c7293d084b8cd403ae1d69cc9efa7f
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:19:38 +0000

    intelligent hurt contrast hit just

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
