f0834048cdc1ad1d15111ea5a29ae66bb47140ee
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:14:18 +0000

    towards bound hello getback cover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
