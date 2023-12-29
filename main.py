a40f25bde97b45e32160146af94014af67028ac6
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:24:43 +0000

    appeal way none brief pump

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import math
+print(math.pi)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def square_number(x):
-        return x**2
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
