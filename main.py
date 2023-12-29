da9bb564f44fafb0471c31bfe2f918ce24c20f2b
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:10:40 +0000

    permission little respect apparently very

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import math
+def calculate_floor(x):
+        return math.floor(x)
