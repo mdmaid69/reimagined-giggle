c3b93241085f5f877e377f03bb38d0e900ce8d17
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:47:08 +0000

    effort feed usually contrast disappointed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  def is_even(n):
-        return n % 2 == 0
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
