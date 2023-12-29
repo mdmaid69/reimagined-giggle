cba9fca31ff20be41bc593e14ccd112fd2a2f1f4
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:41:19 +0000

    embarrassing giveaway wide thumb yet

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
