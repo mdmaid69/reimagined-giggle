77cbb557840c1b2aef267f4ac308cc908d5c309d
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:35:09 +0000

    any row thinkthrough trust expression

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
