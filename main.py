977ffb71602b7b011c5858c3553c62122467fe0a
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:25:37 +0000

    none special turnup giveaway along

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  def convert_to_binary(n):
+        return bin(n)
