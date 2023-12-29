77f042304f38ce441f43bc1ee8575064a9c08ca7
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:10:38 +0000

    motion genuine actual frightened clever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
