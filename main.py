9e507b47200c1fbda44023c3f535b3fec902e459
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:01:09 +0000

    violent poison back memory impressive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
