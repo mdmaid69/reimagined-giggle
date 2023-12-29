cebe65ff371d751de9ac05863718cc2802c98bcf
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:16:12 +0000

    worried dare wire normal transparent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def sort_list(lst):
+        return sorted(lst)
