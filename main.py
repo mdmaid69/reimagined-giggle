8b4c1601c28e01a62d6437748abe15e8db3cdc4b
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:51:33 +0000

    church fall swimming learn exactly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
