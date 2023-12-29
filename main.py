1b542ff411e3f4dd27c6b12e89236ab058a844e3
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:21:58 +0000

    dangerous leave ground score rush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def split_path(path):
+        return os.path.split(path)
