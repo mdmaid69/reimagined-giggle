4fdef51c9fa98c7f4e4e9b97f269dc39cd4b020c
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:22:37 +0000

    warm priority wander woman wealth

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-for i in range(5):
-        print(i)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
