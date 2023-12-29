cdca6e74936c1222693ca777de6bd5fcd0fcd54d
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:57:48 +0000

    runaway bin demand reply introduction

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-print(sum(range(10)))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
