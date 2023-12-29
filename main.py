96f86cec30875f91ffb745e9c69f711204a75e28
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:42:05 +0000

    should shake before concerning completely

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def get_array_length(array):
+        return len(array)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
