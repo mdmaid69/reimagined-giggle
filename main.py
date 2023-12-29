5fda05c254a6fc1c4731f1b3a7f7963256db0318
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:52:55 +0000

    add cinema attach wash sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
