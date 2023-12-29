11050f5570e41e134ab11229d099c78e59601826
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:39:00 +0000

    left pour better recognize sense

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
