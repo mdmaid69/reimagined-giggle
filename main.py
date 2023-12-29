12ed670e03599be2b12a6b4b6f4d153b929ffa78
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:57:13 +0000

    order term please foot sleep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
