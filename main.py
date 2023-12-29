23dea770a85c589bede165998a6ccb8eb58eaccc
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:29:31 +0000

    obvious trip opportunity special miss

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
