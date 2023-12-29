2b46d1baa5919b3911e01ad235804d5dd7589e28
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:34:14 +0000

    especially to paper only this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
