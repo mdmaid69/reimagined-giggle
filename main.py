058bb1aae844327b339859817d0dfbf80d091dbc
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:27:14 +0000

    either forward interfere smile difference

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_as_bytes(array):
+        return bytes(array)
