539173b92bb2c237e2a1e0e7a15623f05b518077
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:07:04 +0000

    reaction seem search all figure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-print("Hello, world!")
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
