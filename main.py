861c6ef00bf61916a83fb6af95e6b2836b5ea479
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:07:35 +0000

    minor workout rid correct compare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
