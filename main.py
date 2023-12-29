439920395d209bfc97caa2b1abed5a23dfb14fd4
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:39:40 +0000

    only lean inquire cruel past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def get_array_as_str(array):
+        return str(array)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
