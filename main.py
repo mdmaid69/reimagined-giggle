9dfa81f6c4e6f05e9fca8a50ffd29a7e75a2826d
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:35:37 +0000

    cover bright major story represent

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_min(lst):
+        return min(lst)
