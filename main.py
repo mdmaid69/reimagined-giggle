96c768166f0074b2543fcee30a861a8b2df1ba26
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:27:33 +0000

    hope bath voice Rr ilovedicks

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_set(array):
+        return set(array)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
