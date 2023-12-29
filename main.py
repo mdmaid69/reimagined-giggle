62df12b369dd89f295b90a3013ac2d85f0227912
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:10:00 +0000

    where glass record inside little

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
