73406609e84296e8cb53f387da08e7d1c52513b3
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:39:47 +0000

    sudden tight jump nothing attention

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  def is_odd(n):
+        return n % 2 != 0
