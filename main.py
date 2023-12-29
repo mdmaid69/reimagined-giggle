2ca21d382cab95f3b8df28cf00124675832d5d23
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:42:33 +0000

    state strict copy fat winter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
