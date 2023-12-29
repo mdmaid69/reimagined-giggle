33480d82a3e993e98f7a54d572aa2ae931860022
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:22:37 +0000

    dry cheek bring afraid sometimes

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
