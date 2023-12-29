21848c7b8a92052926b1be9fc2688a66198bca32
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:07:11 +0000

    better club effort design rough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def set_array_item(array, i, item):
+        array[i] = item
