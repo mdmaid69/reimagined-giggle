7044be0e9f94c041af3cf70bc212233cee223375
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:19:07 +0000

    free ilovedicks niggaacademy hollow there

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
 import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
+with open("some.csv", "w", newline="") as f:
         writer = csv.writer(f)
-        writer.writerows(data)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
