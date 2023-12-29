dcfa5e3a5a9213a03d0918eb12350bbd827a8a4f
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:45:07 +0000

    care uponon hard zero all

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
