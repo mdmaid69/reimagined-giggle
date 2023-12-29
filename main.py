d49f46f542652215397c8c493e7425842f7a30ef
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:31:07 +0000

    regard side shop division weather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
