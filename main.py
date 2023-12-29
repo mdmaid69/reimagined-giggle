d2fcaae5922ba80f9da145654ed9c6db0235d9ab
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:29:22 +0000

    speech breakout serve sound limit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
