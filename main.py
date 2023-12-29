876dfc3e71b2c5cffeee18a529efaec304b89d7f
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:21:42 +0000

    copy when concentration urgent fun

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import tensorflow as tf
+print(tf.__version__)
+import array
+def clear_array(array):
+        array *= 0
