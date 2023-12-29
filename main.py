18583b1c78bdcaf716d499f3e1062a51112e047a
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:38:44 +0000

    tread tour temperature nonsense lonely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_power(work, time):
-        return work / time
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
