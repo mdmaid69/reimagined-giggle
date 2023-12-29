1a6d81ec47ac84e6379947aea84b3b1d85e26632
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:49:18 +0000

    hear calloff black quantity 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
