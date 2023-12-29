84b9196a2907b5da7e2e593fafcd40df49b2492b
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:01:59 +0000

    approve broken conclusion wake advanced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
