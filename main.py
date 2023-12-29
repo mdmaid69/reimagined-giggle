a1557dca3d4e893085f7a8bbbdeee02bb1a407b8
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:46:28 +0000

    unpleasant father fear its use

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_max(lst):
-        return max(lst)
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
