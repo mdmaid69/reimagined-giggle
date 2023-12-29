961931e7f32ce7a5c73b76fbaf1d46a05e96fc5a
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:21:19 +0000

    Gg sale feel Hh behind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_area(radius):
+        return 3.14 * radius * radius
