edd25b3ba74dd23a25f17390ba5e200770b57b08
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:41:15 +0000

    gift chance comfortable command change

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def split_path(path):
+        return os.path.split(path)
