23f7d2d0011a018c2ae00853f8e43abe4f46c377
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:11:04 +0000

    comeoff blow address frightened tread

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
