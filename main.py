709749d358cad4f089d0a71f38cb805db4d6c145
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:13:27 +0000

    attitude shout me believe boil

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-text = "Hello, world!"
-print("Characters:", len(text))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import os
+def get_current_working_directory():
+        return os.getcwd()
