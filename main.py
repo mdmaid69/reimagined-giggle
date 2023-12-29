12046de36af86f044487218347d7c69afdce7a25
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:33:06 +0000

    anywayanyhow dance combination dull hate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
