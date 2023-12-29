1ca46e4f9dc41ce0d0cea2198ca762ed3be5f040
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:05:27 +0000

    roll high Ww noise suit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def clear_array(array):
+        array *= 0
