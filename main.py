efd74df6ba4c29eab0f398b378b567fc4145a16b
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:36:05 +0000

    arrive rise force target destroy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import tensorflow as tf
-print(tf.__version__)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import getpass
+def get_username():
+        return getpass.getuser()
