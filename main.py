809419258cbc90fd98df9cd6c8a0c02c26cdfb49
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:57:09 +0000

    holdon equal arrive order onto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def subtract_numbers(x, y):
+        return x - y
