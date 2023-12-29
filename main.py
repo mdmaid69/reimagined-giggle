8af1dc72b71327fdb9cb56fe39e5e3244b54bb40
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:13:46 +0000

    payment nonsense fortunately furniture impossible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
