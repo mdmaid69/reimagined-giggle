bf66878942d9144494024b93ad214f7e12962aed
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:39:05 +0000

    to distant blind uniform keepout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import tensorflow as tf
-print(tf.__version__)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
