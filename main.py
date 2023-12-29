8704117a3edfb5325746cf59935736e1a59a70e6
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:07:06 +0000

    scheme comeout style activity balance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
