270ca479c4a99ed7f3cf24f53531d86860d86ab8
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:30:33 +0000

    danger contain then trousers on

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
