60500780d52d470caf4df36326e7c2eded090155
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:32:28 +0000

    minority attach ticket future official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def get_array_as_str(array):
+        return str(array)
