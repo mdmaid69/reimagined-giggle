b63d53b56a08eb823c08efc37982918f0b3106bb
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:31:44 +0000

    dick marry distant escape deliberately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
