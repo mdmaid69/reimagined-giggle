b105b9b7d3f1910db11b734b14930d7b82f3cc84
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:02:31 +0000

    eye or reduce loose wall

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
