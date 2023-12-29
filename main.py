edfebd8f8d46965e0f8837c827b0f08ff4ca30ee
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:13:45 +0000

    public upwards target step ago

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
