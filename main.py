63a5d5a70ef6328038caa99e64ced80be9f23d20
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:27:42 +0000

    objection pick shelter tax comein

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
