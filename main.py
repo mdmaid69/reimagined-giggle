b6991a35fe66f74e37c24ad899d8e6fb9d7e4615
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:39:29 +0000

    group set official fold care

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def count_words(sentence):
-        return len(sentence.split())
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def add_numbers(x, y):
+        return x + y
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
