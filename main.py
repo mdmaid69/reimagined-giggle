4b32e052f23bf738edcac626439a9d34ed328a5e
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:36:05 +0000

    hope constantly steer doubt bottom

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
