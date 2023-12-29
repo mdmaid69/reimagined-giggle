0b9c6833c13540cbb40b842ce20f1a1522ed026b
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:46:08 +0000

    breakinto across book through success

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import getpass
-def get_username():
-        return getpass.getuser()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
