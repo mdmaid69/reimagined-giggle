6a8e913fc6d447d09424bf51b0e09c0bea2e504e
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:55:07 +0000

    damp kind politics past awake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
