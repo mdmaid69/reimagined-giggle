a966ac8a4588d7d3b5cf9643ee681f860d22d7f7
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:40:11 +0000

    angry seriously tour steep offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
