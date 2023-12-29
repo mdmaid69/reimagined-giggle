4907deda5d1da13e8883ebcba9d86b9caa538462
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:54:43 +0000

    number central tonight store roll

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_list(array):
+        return list(array)
