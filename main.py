47da2491388a13b0e6baf078264cedcab544c93d
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:19:59 +0000

    file swallow contents quality strong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
