7a472284fc04cc9921f3c747d84be291a31f7fac
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:02:08 +0000

    warning flood send few label

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
