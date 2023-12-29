c882a268b7388a08995e742fdadc4109b183e416
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:32:29 +0000

    curious late to change friendly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
