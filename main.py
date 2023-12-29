fd43a0d960f3a1dd4cd5f2bd2b702649c7e1bea3
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:18:42 +0000

    waste line name back measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
 def calculate_current_ratio(current_assets, current_liabilities):
         return current_assets / current_liabilities
