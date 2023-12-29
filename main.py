c65596dd0cd0553d259666d4d2405f1b406bf655
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:58:41 +0000

    mess threaten before consist quarter

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def add_numbers(x, y):
+        return x + y
