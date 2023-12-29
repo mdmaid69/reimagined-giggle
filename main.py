8da2f64cbfde6ba042402856154335337486bb83
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:22:43 +0000

    clue pick shall comeround sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
