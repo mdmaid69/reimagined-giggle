9f0d326ec068007815974d1514afc4905c81d936
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:29:16 +0000

    length mistake afraid towards tongue

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
