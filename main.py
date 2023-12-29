60193cb4d37a895987720577b3d4fd06052edae0
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:59:36 +0000

    goup mean crowded introduce pick

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def count_characters(sentence):
+        return len(sentence)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
