24b0a51832584961d6cc54cdfd918a9727f5cfe9
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:33:24 +0000

    day emergency couple careless worse

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
