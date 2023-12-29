96a5c98a27dadd991c5ebe347f5297b46a29f7b8
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:44:49 +0000

    often trip breakin spring combine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def remove_directory(path):
+        os.rmdir(path)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
