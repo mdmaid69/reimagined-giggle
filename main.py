88e572637475514972a615b6bdfd7d8aa170c648
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:09:45 +0000

    journey performance uncertain stiff previous

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
