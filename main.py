f24fa17700a2b96682398e28d121b208d34b41af
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:20:27 +0000

    ready keen glass reserve manner

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
