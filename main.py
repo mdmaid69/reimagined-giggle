f1f13436fab661dc7db1a8556eed53a8beb21526
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:40:44 +0000

    imagination juice pay fierce effort

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_density(mass, volume):
+        return mass / volume
