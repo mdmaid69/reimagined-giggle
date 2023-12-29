abf78ae8d311db4f2d6252db800dd86a00474e70
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:24:04 +0000

    reduction union become connected square

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-name = "Python"
-print("Hello,", name)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
