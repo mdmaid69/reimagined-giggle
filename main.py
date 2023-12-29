d2c943b95f65e68ae8185021d9ca00c882217942
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:52:11 +0000

    free fit dress believe own

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
