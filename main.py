73c8bca3ed422226795275996dbca11943426630
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:12:21 +0000

    in stage callat shrink story

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_as_int(array):
+        return int(array[0])
