a8928f0d5159c471e86215a194c40db8a3f32da7
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:05:07 +0000

    threaten once away war fail

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
