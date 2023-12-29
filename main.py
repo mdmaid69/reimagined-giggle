8c6fd0f9c687c9ff4c578f52cc108c821d47f551
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:02:37 +0000

    excellent wish system run copy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
