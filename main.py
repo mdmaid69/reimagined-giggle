5d245f1f1a2b3dfbbb29e84a624c3b765662d017
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:09:49 +0000

    tonight equal burn few Oo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_directory_name(path):
+        return os.path.dirname(path)
