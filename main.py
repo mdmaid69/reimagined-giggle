f6b9f29fe3d3aade6524045a21466f66e3abaf2f
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:35:08 +0000

    smile motion let nor pause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_work(force, distance):
+        return force * distance
