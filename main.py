5b80ef5c90a006df9174d19289722da142814a5f
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:25:55 +0000

    brief mark lookup season innocent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
