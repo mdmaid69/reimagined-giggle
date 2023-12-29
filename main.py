8ebf38ed10a52c50661c9e347836993feeaae2f7
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:10:49 +0000

    conversation there identify Hh list

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+print(math.pi)
