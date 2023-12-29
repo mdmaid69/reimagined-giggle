3d2c0d0f31e4e1df1cbc18453b78cfa7c61b1727
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:36:29 +0000

    operation anywayanyhow concentrate whichever bush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
