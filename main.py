b3e5e166decb5b924b98e6f5361f80361b8d8dff
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:30:29 +0000

    bringback port minute bath new

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_str(array):
-        return str(array)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
