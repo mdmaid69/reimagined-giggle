e7119892941adf2938c98acb5fb44a85a6aae2b6
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:32:16 +0000

    simple bang anything late wild

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
