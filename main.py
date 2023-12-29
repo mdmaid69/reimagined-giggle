d4ddb51dc2d3441bd81c8bce227dae44b65558c1
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:13:00 +0000

    block steep introduce tap base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
