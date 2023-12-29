f793d2a0fcf60ee3f3a80e70a979185499dd58ab
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:01:11 +0000

    crowded race afterwards exercise well

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import array
+def get_list_from_array(array):
+        return array.tolist()
