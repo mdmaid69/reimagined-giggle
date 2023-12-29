4814b49a2b932a10f6f231711589ff4e9df7d9f3
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:16:39 +0000

    the last behind back combine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import platform
+def get_python_version():
+        return platform.python_version()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
