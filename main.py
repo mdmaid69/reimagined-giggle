17be07f35fe8a557d1aeebf213d9dfb12768f7eb
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:51:22 +0000

    rather recover by pay day

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
