57686d984ba5927880e454237221f6b506888993
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:07:25 +0000

    apply in pattern sure complete

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
