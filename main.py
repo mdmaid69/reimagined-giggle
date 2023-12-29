c014e82444afa8492fe67ca03263777dfb9b67fa
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:15:40 +0000

    right disappointed spite company influence

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
