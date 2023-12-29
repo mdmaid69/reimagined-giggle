7a4b2ab98c06d187d9091dd27f533b3fa1ae92db
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:51:34 +0000

    about song show excellent mark

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_factorial(n):
+        return math.factorial(n)
