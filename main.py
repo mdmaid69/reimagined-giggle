a4e5710aeaf3699d7d321e5c90a36caaea746c54
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:41:59 +0000

    generally slight condition invite full

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
