613ceb441a241515ff8843661ab0fbe40ea68995
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:24:06 +0000

    neither town grand famous beginning

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def count_elements(lst):
+        return len(lst)
