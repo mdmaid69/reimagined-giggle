0d123b1ac55736cc430283e19a2075d636c778a2
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:11:52 +0000

    search remove complete emphasis left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
