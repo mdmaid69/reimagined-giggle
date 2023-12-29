bef0824e3a8d81c0ac89fcfc3052440c15f117f7
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:36:59 +0000

    female stupid lead follow question

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-print([x**2 for x in range(10)])
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
