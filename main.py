4245939321ee49b9699e531d5ca07dd7d868759f
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:00:48 +0000

    constantly callout against talkinto somewhere

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_volume(length, width, height):
+        return length * width * height
+def greet(name):
+        print(f"Hello, {name}!")
