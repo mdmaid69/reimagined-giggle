4b7e3b4ddd10b1a9e73cacdd6c8003d5fdc3bff4
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:20:11 +0000

    very listenfor bound design respect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
