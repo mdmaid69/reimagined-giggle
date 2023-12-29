21918b897b3d9abedecad7ca8426f19c6f6659ed
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:33:56 +0000

    lost gradually away reach smell

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import array
+def get_array_as_bool(array):
+        return bool(array)
