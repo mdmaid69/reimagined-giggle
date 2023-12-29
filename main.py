482508603c8f63c9e26483990f81e60cf650d2b8
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:54:08 +0000

    normal schedule uncertain grateful each

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_current_working_directory():
+        return os.getcwd()
+import sys
+def print_python_version():
+        return sys.version
