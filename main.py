6afe06dfc0c3f6c7217cf6881712dcf791c88554
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:46:50 +0000

    reliable Ee criticism mix agreement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_str(array):
+        return str(array)
