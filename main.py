f34c5338ac25b3568cb502ca1878bfe8b507bcfb
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:21:30 +0000

    official settle trade bill justice

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
