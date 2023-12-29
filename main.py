33a307af4d9aa36aade1fbe67ad43c33fcddc353
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:19:36 +0000

    protest plate total traffic not

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  def count_elements(lst):
-        return len(lst)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
