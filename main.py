5ab50e5b5312591d4a933d2c2105e6b583fdc874
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:25:58 +0000

    Vv hand interfere achievement freeze

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
