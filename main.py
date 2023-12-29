b41b3a0bebf8c32bb926e037f7cc84a66fb7eb85
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:54:45 +0000

    community guess wing basically pleased

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
