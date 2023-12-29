0b0af952f972b447faa664e54b60a5eeb698c563
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:07:23 +0000

    copy without during defeat sight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
