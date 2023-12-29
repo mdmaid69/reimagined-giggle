4f748e21adcd71a710440c77e991745204591eb0
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:23:27 +0000

    computer off sweep minimum do

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
