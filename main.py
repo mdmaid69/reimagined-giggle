e45e17fe4efb1a762cfd309f9dbf5d6a5db0604e
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:12:23 +0000

    but connection fix interested form

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
