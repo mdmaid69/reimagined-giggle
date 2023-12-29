432d5cc61e7f2edd1d4cce1836c1f1ec95572087
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:03:38 +0000

    able remove suddenly key limit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_power(work, time):
-        return work / time
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
