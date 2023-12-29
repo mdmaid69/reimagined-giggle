f3152b94a2bd52a964c587e8b4fc178a4f5f2e58
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:59:15 +0000

    moon dance wind stick discussion

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
