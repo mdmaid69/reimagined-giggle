eba2bc6d6ce1749e05f0636b24f9776934a3e96f
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:39:51 +0000

    keepaway information old no exactly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
