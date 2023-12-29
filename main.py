f1aa8e73faa7717c1080ffa29366211906583270
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:01:27 +0000

    eager hungry avoid last active

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
