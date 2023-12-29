e805f52a47d82a06a878bb9bb998328ca9ef39d6
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:43:43 +0000

    visit tidy watch piece clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import json
+print(json.dumps({"name": "John", "age": 30}))
