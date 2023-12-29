b95ae37b6fd0a2efb0ee795cf774ba3ead9c8ab1
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:02:03 +0000

    back rent most into education

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_current_directory():
+        return os.getcwd()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
