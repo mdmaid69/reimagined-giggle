a6c048856fc2bd8d134e8bf3364f93b3a9609314
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:46:45 +0000

    long excuse think stop fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
