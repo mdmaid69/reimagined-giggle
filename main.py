49b3fdc816a9fb9ba188dad93cb63a5f42a19497
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:42:02 +0000

    talkinto distant upset wind lead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
