437839c8be8a57b57eb78d8d08cb2bc45635b02c
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:42:01 +0000

    broad pin flavour sort capital

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
