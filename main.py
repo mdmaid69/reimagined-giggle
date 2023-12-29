2d06c396a8f1b04539fddc53f689f288694ca6ef
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:26:39 +0000

    team share necessary something limit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def reverse_list(lst):
+        return lst[::-1]
