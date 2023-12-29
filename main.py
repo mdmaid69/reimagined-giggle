a5b51285721f6411cfbc0633410d7626bc48dcfc
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:05:30 +0000

    repair print will sort anyone

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+text = "Hello, world!"
+print("Words:", len(text.split()))
