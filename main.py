9db215ae32c1512977c44efb94eaa34d1bf2952a
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:44:47 +0000

    minority record event regular team

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def find_unique_words(sentence):
+        return set(sentence.split())
