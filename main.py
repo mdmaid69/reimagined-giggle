e3a3cb4cad026e32a028bee3e9c506c70ef170b7
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:02:56 +0000

    shrink bring below break favourite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def get_array_slice(array, i, j):
+        return array[i:j]
