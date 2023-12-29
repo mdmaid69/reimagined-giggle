237c395ea4a93024f9051d1c671013f5b1e117a4
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:01:03 +0000

    connected bringdown underneath though scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
