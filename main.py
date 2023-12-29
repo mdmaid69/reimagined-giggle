fe5a52886c3791f75ba7567d169fb731131cd1ff
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:24:55 +0000

    rise squash peace visit rare

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def cube_number(x):
-        return x**3
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
