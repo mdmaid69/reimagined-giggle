8005f6ef89e3387769dde6ef6615e019ac1c9744
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:04:31 +0000

    sensitive profit design standard underneath

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
