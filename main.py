023913130f9435ab931b9a218b6af5719039b385
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:10:46 +0000

    worth sound scene thankyou include

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def add_numbers(a, b):
-        return a + b
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
