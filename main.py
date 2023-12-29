61fbfc6d0b15a87586d4aa18604323d477d2d424
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:01:25 +0000

    the odd regard display punishment

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
