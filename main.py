55c015e44246cc6a49dfd0659622d8a7d5d5aadc
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:14:55 +0000

    short experience anybody partner round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
