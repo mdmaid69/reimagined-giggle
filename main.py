487d3a2dd7ae208328ff5106245aafc7b23a0ffc
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:04:12 +0000

    ahead straight ahead bad bus

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 def remove_duplicates(lst):
         return list(set(lst))
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
