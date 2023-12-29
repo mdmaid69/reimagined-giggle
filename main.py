4eadd66329ab29d0b9b85969c83c470e12b251f6
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:49:47 +0000

    later flavour conversation book block

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_current_directory():
+        return os.getcwd()
