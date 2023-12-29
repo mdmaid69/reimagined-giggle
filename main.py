838d5cfb37a9460064181ea6061d455235c23389
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:04:59 +0000

    that correction judge wave clean

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
