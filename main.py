1a4441fd3a6528b0bfe7677e2415005a2ef79f15
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:25:31 +0000

    conclusion so live hard talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
