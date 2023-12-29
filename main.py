035c230351aa9377ee18c546d6db8289c623784f
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:04:31 +0000

    Ss this repeat transparent year

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import numpy as np
+print(np.array([1, 2, 3]))
