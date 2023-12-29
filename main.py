164c1cb33ea806fd01c00aa61f36c0e0857c2894
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:14:27 +0000

    software anyone poor trap 568energy

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import sys
-  def get_python_version():
-        return sys.version
+import re
+print(re.match("h.*o", "hello world"))
+import re
+print(re.match("h.*o", "hello world"))
