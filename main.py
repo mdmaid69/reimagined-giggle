95637c5bb6e5afa94509a75b651b77ae6e458330
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:52:12 +0000

    cool guilty save healthy strong

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
