7e6e3c35dd4e00f11c7ed98551b9ed2b0e3747c1
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:36:21 +0000

    road straight range up bang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
