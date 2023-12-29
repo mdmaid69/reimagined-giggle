9369dba7993996a21d95b7702b7663a9bdf73439
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:08:45 +0000

    content help hang believe inform

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
