a3e0e43703acf5bd818b46ed05e9542203dd4702
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:01:35 +0000

    people lesson in track myself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def calculate_area_rectangle(l, w):
+        return l * w
