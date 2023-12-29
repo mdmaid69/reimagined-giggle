c943254939a3e87b5ddce84d6a7a02d501eaca5e
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:57:31 +0000

    exist float computer amount trust

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
