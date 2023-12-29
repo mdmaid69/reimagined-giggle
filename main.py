0af7c1a6edc9ab6128bc0ae3900f63c43cdb9283
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:54:20 +0000

    with flavour hurry argue extra

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def get_array_as_str(array):
-        return str(array)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
