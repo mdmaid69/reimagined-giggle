254a61ed7aeb0ae92350b136eb62ccae1e1d8526
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:43:26 +0000

    turnon false impossible weigh treatment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
