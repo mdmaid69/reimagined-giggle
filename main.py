04e462a16cfb0055c20c34e7994e4f7423786278
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:59:33 +0000

    carry admit arrival dream hide

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_acceleration(speed, time):
+        return speed / time
