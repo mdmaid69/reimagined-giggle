2819374126f8c4f0b70af95e265e68368b42105b
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:44:44 +0000

    hurt load mistake region their

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
