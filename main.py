5bfefeedc9404c7c677868c29249893ea288e86a
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:14:05 +0000

    frequent success turnoff similar gift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
