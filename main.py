da350a8a3d9208189403f90717b894fb7963e0f0
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:48:32 +0000

    goaway explain easy before their

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
