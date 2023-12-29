9466225e9bcfe71ab8b86bdf183fe40906406819
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:35:17 +0000

    wide process order unable large

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def count_characters(sentence):
+        return len(sentence)
