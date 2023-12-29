df84e3980a4d753bb0cb09626176541e74827ca5
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:03:52 +0000

    succeed avoid stop upwards mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
