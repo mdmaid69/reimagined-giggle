4ec42bc0402508042b253075beda85c9cd0ffafc
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:05:29 +0000

    scrape strict container circle even

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
