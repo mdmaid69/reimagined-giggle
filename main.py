16960e9bbe6a1de367a0886657400b6322d4effc
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:21:04 +0000

    like reserve custom workout handle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
