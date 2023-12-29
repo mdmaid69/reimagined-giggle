e62b2aa267978073b1bf8c9098145d10991cdd07
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:52:22 +0000

    perhaps voice suddenly lonely before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
