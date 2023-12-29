4da08b79d8b3a64e4ba5cf2bc611b32d83937bf1
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:25:42 +0000

    first discussion could glass 179bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
