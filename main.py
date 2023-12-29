c79593c6d77c1d3317656ebb3c845abbd151c657
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:20:42 +0000

    behave drink list heat dry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def calculate_area_rectangle(l, w):
+        return l * w
+def find_max(lst):
+        return max(lst)
