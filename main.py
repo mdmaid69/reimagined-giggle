d4956fc74d8cc1a1f8399298be6fee421cd2b271
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:46:31 +0000

    contrast character excuse discover prove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
