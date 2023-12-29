2a2853a4524ada41fa959f495eebca1513652706
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:38:30 +0000

    house dear embarrassed sort contribute

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
