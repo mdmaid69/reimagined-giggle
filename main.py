2b23ed2a35a3c16bf23a92c741381edb45595b00
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:33:31 +0000

    tell complete yourself choice try

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
