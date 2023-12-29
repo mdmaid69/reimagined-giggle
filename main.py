1fdd40271b8cc40ba23089ec97cd000d51659299
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:22:02 +0000

    opponent flat unfortunate fight matter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def get_array_length(array):
-        return len(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
