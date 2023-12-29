a035bdcbf2ea52749e04387dcb1ec7788b2e9e19
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:21:02 +0000

    quickly hold anywhere in as

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
