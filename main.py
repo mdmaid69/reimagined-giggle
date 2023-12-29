d115051335a59589181f0a6f3f917d646dcfacdb
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:28:49 +0000

    giveout sleep importance talkoutof listenfor

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
