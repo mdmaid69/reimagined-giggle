32c095b04810067454b10b5c84a8b86881dcdfb5
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:13:22 +0000

    tip class embarrassing depend order

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
