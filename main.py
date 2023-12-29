db23c846cbcb176130b804873a10ef6e2f8165ac
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:18:53 +0000

    workon bored seem sight pleased

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
