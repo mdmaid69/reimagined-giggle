4a2e6c121d0c8c05319ac22e8ca5476e95be7f45
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:25:45 +0000

    jump shake print ring cheap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
