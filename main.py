3d33d108bcca828b8c3286ced15248c4017809d6
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:29:01 +0000

    boring company reduce leave during

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def convert_to_binary(n):
-        return bin(n)
+def convert_to_octal(n):
+        return oct(n)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
