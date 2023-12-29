7358dd9eb603383dbca7f773a58cfed939705383
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:55:32 +0000

    gentle often excited work heart

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-print(sum(range(10)))
+def convert_to_octal(n):
+        return oct(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
