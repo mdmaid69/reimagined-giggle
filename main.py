b7e9c4b6027c41304f2431e602eda4b047cd02c7
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:46:04 +0000

    happen bin by goodbye temperature

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
