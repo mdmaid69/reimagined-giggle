e49c980463e06e9ac27644cb2732d442af67f954
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:41:22 +0000

    picture Ff attract place certainly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_area_circle(r):
+        return 3.14 * r**2
