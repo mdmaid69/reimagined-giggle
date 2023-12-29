bf5ba444ac4cfe0c0a354bcd4fd8cbf31b76ccad
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:03:16 +0000

    wealth appearance schedule turnback minority

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
