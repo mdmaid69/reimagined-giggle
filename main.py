f7ae34be0efd7f3873992f722a4e3cf9b6a4f41b
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:57:08 +0000

    question thought tired like world

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import array
-def get_array_typecode(array):
-        return array.typecode
+def find_max(numbers):
+        return max(numbers)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
