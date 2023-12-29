0f6f26317c5bcfb2342a706239cfb04c489a02d5
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:14:13 +0000

    ill by politics clean exit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_list(array):
-        return list(array)
+def find_max(lst):
+        return max(lst)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
