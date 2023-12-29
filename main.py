c97d53dfda587d521228792b49e11a03bc410d78
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:47:02 +0000

    shop expression bang sing keep

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import sys
+def print_python_version():
+        print(sys.version)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
