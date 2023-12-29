5f7efc86f1c626fdca60b97dd3d91778c374ad3b
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:29:54 +0000

    goout runout easy move his

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def find_unique_words(sentence):
-        return set(sentence.split())
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
