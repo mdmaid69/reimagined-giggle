0c3fc53d97574ba417444aee1a12733c7c45e971
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:05:35 +0000

    street straight interview all yes

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
