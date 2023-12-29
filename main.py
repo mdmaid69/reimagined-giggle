b4bc45e373be89edf6e470678003fb30e1996fe5
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:33:33 +0000

    pain cold ashamed work none

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  def square_number(x):
-        return x**2
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_speed(distance, time):
+        return distance / time
