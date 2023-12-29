eeb0f6e65abbfd8478d0afcc090bff442c9e14d8
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:09:00 +0000

    funny tip part object else

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
