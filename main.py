bdd560c37077ee6e42ad2b0fa16d98a6eaa13e65
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:14:49 +0000

    such serve contrast mysterious shelter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
