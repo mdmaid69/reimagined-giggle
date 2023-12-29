148d1f5a433531e8a4641d83d8716a814937521c
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:26:01 +0000

    callat bread nature reason herself

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-def multiply_numbers(x, y):
-        return x * y
+def get_array_as_bytes(array):
+        return bytes(array)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
