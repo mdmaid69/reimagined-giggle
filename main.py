7d157aaab7704fdc020447ad86efa7a44f73c060
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:12:08 +0000

    anything government shout mend describe

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def is_odd(n):
-        return n % 2 != 0
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
