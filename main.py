c301d490707d527c9a4588b6b7511406ca4576f2
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:42:11 +0000

    pupil tradition rise stupid may

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_work(force, distance):
+        return force * distance
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
