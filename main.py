ff41c72d4597abd3f95dae408f635f3318c33b96
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:14:05 +0000

    afterwards along accept brave physical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def is_even(n):
+        return n % 2 == 0
