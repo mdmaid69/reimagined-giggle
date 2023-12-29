22ae6f3141d6b723fce8979db259cce424bad72c
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:39:36 +0000

    related act issue ilovedicks estimate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_item(array, i):
+        return array[i]
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
