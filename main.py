500d1d2ead55d8528da61ad4fb761700507e6422
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:34:46 +0000

    believe shrink finally lost zero

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
