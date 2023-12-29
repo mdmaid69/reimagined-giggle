0159f90365029784cf20883aa2513f57aade6212
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:40:08 +0000

    typical Jj male variety indeed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
