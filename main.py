e9397b57227c54ee69e1ad9ffd66d394d1623f28
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:03:30 +0000

    describe he bang clever getaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_exponential(x):
+        return math.exp(x)
