98d7f6d16b01ce216dce9f5813ef8d5323783e52
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:31:39 +0000

    ring reduction just giveback bad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
