f317e343f1600e65c29eacf7f4d99cbde1877bab
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:58:41 +0000

    system tonight trial wash nice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_volume(length, width, height):
+        return length * width * height
