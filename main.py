52748efc3d32a14324c6f07bb1829a6d8bd48af5
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:12:36 +0000

    opinion aware snow appear news

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
