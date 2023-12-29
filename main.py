a70e56cc0a2c325b54a48eed91b25f47773ca380
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:29:52 +0000

    waste insist though solve side

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def find_max(lst):
-        return max(lst)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
