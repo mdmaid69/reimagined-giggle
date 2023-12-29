df99f3d44840b7bdd6ff2fc5ebbae04aa011f7f4
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:34:46 +0000

    fat disgusting quality plate accordingto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
