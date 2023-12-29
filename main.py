fc5398658a179fc091e3f16f94b74bef83875b8c
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:33:07 +0000

    avoid answer space 567enemy live

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
