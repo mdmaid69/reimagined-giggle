ceacc824ebee2db6a10d622047280a4e9f08493a
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:54:42 +0000

    various mark again motion regard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
