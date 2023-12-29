66010e6ab72f88beec48128399db45e43f1558e4
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:49:50 +0000

    mixture opposite disgusting boat hair

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
