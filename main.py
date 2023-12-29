364afda8500cbe4b75dea66cc4629a56bac537de
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:43:45 +0000

    key prove scrape difficulty getaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
