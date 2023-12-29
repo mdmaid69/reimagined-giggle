41e5903d21380ecf29ee14a0a4adb5dd401b1553
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:32:51 +0000

    takeback entry development suggestion thinkover

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
