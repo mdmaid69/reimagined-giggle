9b8fcfaea7afc168e2e2aa30c573cb682697c712
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:33:56 +0000

    hold scream unlikely attract below

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
