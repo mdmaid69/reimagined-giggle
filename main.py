3924f16c20940c7865836621a4e4faeac19b9078
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:56:25 +0000

    shoot skirt rent ring least

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import platform
+def get_python_version():
+        return platform.python_version()
