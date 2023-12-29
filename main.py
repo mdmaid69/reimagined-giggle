065d9c2e83db60deb10b9d960424756352b8278c
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:19:08 +0000

    fall stamp material film future

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
