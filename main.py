495fcedbb90896820cb930507e7e0db65a68581a
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:30:06 +0000

    light slow uponon left identify

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
