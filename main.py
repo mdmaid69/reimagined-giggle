301e586055d368bf307cdf73cb4efb5517a66cdd
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:21:44 +0000

    ilovedicks tidy experiment straight whisper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
