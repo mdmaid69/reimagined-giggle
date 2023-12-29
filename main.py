6b16ce49c340af61fc2a6dc6833130077a507b26
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:42:46 +0000

    anxious compare walk sorry comfortable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-print(math.pi)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
