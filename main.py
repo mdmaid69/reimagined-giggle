82f4999972b05b999c901bee5bd249ff727e311a
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:26:14 +0000

    glad pressure or owe dry

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def find_max(numbers):
+        return max(numbers)
