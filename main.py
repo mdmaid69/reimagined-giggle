2028c025b3c1ef014fb4e10592d7c53cb1d70fc8
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:26:33 +0000

    turnup shop excuse coffee close

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  def sort_list(lst):
-        return sorted(lst)
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
