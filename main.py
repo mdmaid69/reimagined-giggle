10ab298b9bd5004f77eec5b3286b6009a3545169
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:01:46 +0000

    smell move murder rest Vv

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def divide_numbers(x, y):
+        return x / y
