a7b6053d9f410fce5ea6477be10678514bc30e67
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:36:23 +0000

    mouth butter force new 565end

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def sort_numbers(numbers):
+        return sorted(numbers)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
