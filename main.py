d94a4ace8dafaeb43d14811b9a5af3d22a9380ab
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:43:30 +0000

    rank develop thinkover past room

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
