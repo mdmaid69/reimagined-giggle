4e290ff56d0a16994c40805338d55cd80a84ac4e
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:43:03 +0000

    cinema hide label guard rank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
