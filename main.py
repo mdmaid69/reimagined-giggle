853658106965906360655e4b2f5a6173d61bf3c2
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:22:52 +0000

    throughout ground question sign tired

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import math
+print(math.pi)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
