aa4d500feeb4605b790cd55312fc0b1edb5fe16d
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:11:18 +0000

    port strange then under day

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
