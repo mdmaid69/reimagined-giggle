922abbbbca8874cd45ec14ecb1eed60b6a760d6a
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:50:42 +0000

    impressed turn sale coffee terrible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_acceleration(speed, time):
+        return speed / time
