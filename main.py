b3aefb617d26b166802f0d914678c98c6a552825
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:15:34 +0000

    themselves Kk accept foot rough

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-  def square_number(x):
-        return x**2
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+for i in range(10): print(i)
