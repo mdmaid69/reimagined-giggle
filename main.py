a59ad26638143a4b42c5ef3ba48be8ad07926f97
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:50:46 +0000

    answer disturb goodbye expect picture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import random
+print(random.randint(0, 100))
