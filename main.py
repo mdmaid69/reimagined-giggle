75174c0769c5ce1583cf7ed3b0816d22cf382427
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:19:05 +0000

    yours admit fix major rain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def find_max(numbers):
+        return max(numbers)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
