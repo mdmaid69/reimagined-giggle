b7b0025eec5df09c27fff8f0ef555c43521c4f1d
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:26:37 +0000

    upwards pleasant after unlucky back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
