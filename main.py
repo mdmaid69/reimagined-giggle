838687a30e9542ee6774f37deb95e85a4b6e7290
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:52:29 +0000

    my goback(to) future completely glad

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
