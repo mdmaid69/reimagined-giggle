011066e882bb605089ba98c7953cded69bee86cc
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:53:14 +0000

    front let intend minority include

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
 def calculate_acceleration(speed, time):
         return speed / time
