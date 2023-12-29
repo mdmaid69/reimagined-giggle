9f17c5606b0a09c647f211259bbec26fb68d720a
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:56:17 +0000

    damage turnback sharp illness but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
