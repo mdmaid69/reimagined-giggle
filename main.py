9a66bf174dca1fbe4a9a304933dfcefa2ad84253
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:23:23 +0000

    shop neither Ww born stamp

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-print([x**2 for x in range(10)])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
