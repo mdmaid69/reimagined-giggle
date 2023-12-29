f7c473d0513fdb04716ce385744476f7fdf51abe
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:50:45 +0000

    say attempt repair across body

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
