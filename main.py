4f81581442c427bafeccfdd7c24749dafafa3970
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:11:46 +0000

    meaning arrangement safety furniture print

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-print(sum(range(10)))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
