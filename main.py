57a1fddd9d0a4da82f883bf71b3a6ed0967497ed
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:23:43 +0000

    own transport active party mind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
