031a2bd1e21436104385130a391e25497a464c30
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:57:24 +0000

    roof flow squeeze noone scheme

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  def is_odd(n):
-        return n % 2 != 0
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
