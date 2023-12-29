c33a458376747881a92340deab1f1634ff987cdd
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:00:40 +0000

    offer off thanks respect small

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import datetime
+def get_current_date():
+        return datetime.date.today()
