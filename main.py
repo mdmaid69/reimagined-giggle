1ec375291222028b3ce41e20e83eb69ca1a77a3d
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:27:36 +0000

    his be chat smell ocean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def set_array_item(array, i, item):
+        array[i] = item
