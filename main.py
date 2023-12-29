ba35445084a99e96eb3a55aa7a0c2fd9705d0119
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:11:28 +0000

    backwards worry shut advice disturbing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
