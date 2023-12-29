9ee00480354d2a2e6360320a60c69cc1b8030a59
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:41:09 +0000

    address lead fit 569enjoy something

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
