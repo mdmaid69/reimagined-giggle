b95e86932fc778e2cea1e3de1b33884eddc8af9d
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:51:53 +0000

    than very fit giveup ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
