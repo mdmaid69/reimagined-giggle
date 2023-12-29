cba70332f6c4edebc882e7d796dcb78362637fe6
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:49:36 +0000

    original lay runinto neck port

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
