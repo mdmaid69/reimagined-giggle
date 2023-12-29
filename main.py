9da2a43e25f48d2408baeac271c58bb8489d709a
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:33:31 +0000

    advantage murder character importance surprised

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import datetime
+print(datetime.datetime.now())
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
