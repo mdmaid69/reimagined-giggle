a17d86ea1508b64ec2628f6c28d3d2029de840a1
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:42:51 +0000

    hello stand destroy visit feel

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import collections
+def create_stack():
+        return collections.deque()
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
