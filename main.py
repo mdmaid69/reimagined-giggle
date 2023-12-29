5155d7801e1693a66a828fdabd177edea5ee776e
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:59:44 +0000

    influence shopping 567enemy friendly port

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
