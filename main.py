c4336180186d1140ec03470832d792cd6748e803
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:36:13 +0000

    me underneath after bread phone

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_perpetuity(payment, rate):
+        return payment / rate
