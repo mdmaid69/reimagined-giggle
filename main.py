d79cf1708c9571e92a8b21faeb82b078524b3aef
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:11:00 +0000

    turnout truth rest usually top

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
