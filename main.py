7053f916a56b146b2e02e21793478a4291320231
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:25:22 +0000

    wire terrible downstairs behaviour lift

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import sys
+def print_python_version():
+        return sys.version
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
