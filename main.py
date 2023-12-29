a5b0fdee929baf30037a6770192b6e73f152320f
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:18:50 +0000

    certain about when development lesson

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
