ca752a92f1a97eaf7e931d65c44417fad5544c57
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:26:19 +0000

    dance Oo lift exhibition nerve

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
