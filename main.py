1edd8aa5a73aef6123689d6d7f79970012b42be0
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:17:31 +0000

    become repeat once scheme colour

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
