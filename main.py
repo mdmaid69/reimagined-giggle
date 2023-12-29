c70641e065839a8c724a2cc67214b882ddaf36fb
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:41:41 +0000

    underneath rather slice corner war

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
