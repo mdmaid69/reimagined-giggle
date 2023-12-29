80241a39a8dfbbb569244d17ec3dd98362405589
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:53:54 +0000

    weigh blue circumstance worried father

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import collections
+def create_queue():
+        return collections.deque()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
