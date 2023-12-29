dc1df869cd065c5f69f08fdf2ff972059e3fcb08
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:47:13 +0000

    bound wish to herself material

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
