8aac8d8c9c1258b890eabf53e42cc67a8737cbc6
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:55:23 +0000

    movement rarely conversation murder twist

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
