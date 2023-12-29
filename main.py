68a138e055f8caeb36b5fbc83fb570a5fcdbe88b
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:56:10 +0000

    schedule dress trip morning clue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def subtract_numbers(x, y):
+        return x - y
