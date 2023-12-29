f0de81de812d7aa379256e0c74a903f1aa676518
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:44:13 +0000

    cheap before gift cook convenient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
