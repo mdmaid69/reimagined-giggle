47bded7c746b6507cfa57a881026db952d682eae
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:31:47 +0000

    especially behind down try division

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
