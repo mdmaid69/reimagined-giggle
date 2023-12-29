ce0e695991591e918e8b99ee7224aafedac2d6fa
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:31:12 +0000

    suggest wrap correction rather cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
