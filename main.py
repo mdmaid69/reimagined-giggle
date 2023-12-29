9ba34ac58996621590a41cb8e6092adef6582803
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:54:29 +0000

    practical announce deep join point

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import time
+print(time.time())
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
