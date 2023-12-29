ee79da28e2c4738add901d7e35efcff534146938
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:26:25 +0000

    grade slide sky dick final

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
