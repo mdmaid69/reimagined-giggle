77efac5a789b79e62ac365aee9f95fceac953395
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:54:51 +0000

    bar run neck where character

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
