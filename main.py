13f272eb15a6f8413dc5b6acb709b95c488b309b
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:11:24 +0000

    shame wind then second tongue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
