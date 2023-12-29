3864361d6421dca7a5af8c23b1d6d943b80d3e74
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:04:29 +0000

    skirt afford coffee serious back

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
