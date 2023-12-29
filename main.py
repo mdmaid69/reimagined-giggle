2f199772a79df19d195fd65e55b8a135cbdad48e
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:03:34 +0000

    health join properly Dd can

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
