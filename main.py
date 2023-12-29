ec5d42216bd0bd09b8743541cdae06e8e6d11e13
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:19:27 +0000

    spare cross have as stage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
