0c583a660afedb9725857271ca3dd010db6fc75f
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:31:36 +0000

    lead getback fight finally thank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
