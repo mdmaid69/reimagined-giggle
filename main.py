4d1bfe0e14624316af557ad52f1c47d2807d22a7
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:22:30 +0000

    stop long education convenient pick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_cosine(x):
+        return math.cos(x)
