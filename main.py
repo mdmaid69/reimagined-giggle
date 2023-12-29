ba9855d5c621b8f55d8e9cef197979731d13708a
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:33:04 +0000

    excuse centimetre grateful use trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def convert_to_binary(n):
+        return bin(n)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
