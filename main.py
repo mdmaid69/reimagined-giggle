c08a034a4e8432988fc7f6799649c9a533ac4101
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:17:46 +0000

    nerve murder quality path sentence

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+def count_elements(lst):
+        return len(lst)
