c2768d710c90d2cccaf9b22242fd2c34c68c4077
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:20:28 +0000

    succeed keen comedown regret scene

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def sort_list(lst):
-        return sorted(lst)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import os
+def get_current_working_directory():
+        return os.getcwd()
