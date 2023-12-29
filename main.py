a28771dbb53df0e75262c1489014fff20d00b47d
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:14:27 +0000

    away 572enter second spite modern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def convert_to_octal(n):
+        return oct(n)
