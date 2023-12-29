c1de77301dd9302f5e47aac42860a6ba39b93ba9
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:59:43 +0000

    even worst minimum closely Ll

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
