3047b998ba271e886e553e3b682cf846b144cb3d
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:05:26 +0000

    survive lock tonight light dark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+x = 10
+y = 20
+print("Sum:", x + y)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
