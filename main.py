7d5599ebfbeb2fbc151fc9a6cb6fb08b61b55492
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:52:45 +0000

    apply order health eager funny

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import tensorflow as tf
-print(tf.__version__)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
