b81d64f7deb02c0c6c2007d9131637d99d684fba
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:02:02 +0000

    down drive undo then slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
