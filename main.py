d5eb88d9df8caa63a67bef056a2453fe772696d5
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:22:50 +0000

    success slice organization pile giveup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
