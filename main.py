04cbb6d35a0ceb124abf0614c95bb324be2e69e6
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:34:33 +0000

    solve Ll concerned water earth

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
