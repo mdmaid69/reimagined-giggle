8b84e6c727c239d6ecccc8d492195de830badb60
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:58:07 +0000

    only pot mix somewhere positive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
