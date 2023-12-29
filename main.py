d458177d4b6f0e51d2fcabdb00bd0c67728f405f
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:21:00 +0000

    chief disturb bye private lay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
 def calculate_irr(cash_flows):
         rate = 0.1
         for _ in range(100):
