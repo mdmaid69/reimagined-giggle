6510410a5fae65a810a165b4b5c15f396265cccc
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:30:59 +0000

    occasion contrast half towards admit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
