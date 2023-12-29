72efb868693c8d32d643a90c12d3675a97b8d7d3
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:50:02 +0000

    advantage politics pleased shape sorry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
