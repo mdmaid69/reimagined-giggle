49d835e175df05053beab7b7dc12add6eb4393b8
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:33:12 +0000

    least involve run please not

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+x = 10
+y = 20
+print("Sum:", x + y)
