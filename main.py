4a045c0b26396c10ad35c8b3c02ff687f9656844
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:57:06 +0000

    occasion storm mistake into repair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
