91d1b131c0eeba3ca0155932653dc4a3ee425e29
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:00:25 +0000

    hour Zz love home within

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
