ddf6dfe14a0a414456c6c11967b36f679c227af5
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:33:51 +0000

    matter contribute early blame cool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
