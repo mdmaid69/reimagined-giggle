fc198f675ee3308ab1542ea23bb4a3bae26e4a63
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:12:33 +0000

    method hole form point shine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
