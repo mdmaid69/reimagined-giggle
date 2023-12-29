f3cef0e6a8c7fef844b3982404b5a7822a1fb061
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:20:48 +0000

    comeout goaway brush negative return

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
