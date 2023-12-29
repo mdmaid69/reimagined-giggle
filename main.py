65e07b15ffa8725a61e721b28c8eec4655fdfa2b
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:05:34 +0000

    strike around great lucky money

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
