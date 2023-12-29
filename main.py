6bbfaf4faa14cd715c2c2493f990530755510b85
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:19:21 +0000

    test chief collect contents waste

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
