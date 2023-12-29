68cefc501a145b5e00ee1a43726c31d3643d216a
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:36:32 +0000

    squash surprise answer declare disappear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def calculate_area_rectangle(l, w):
+        return l * w
+import array
+def get_array_as_float(array):
+        return float(array[0])
