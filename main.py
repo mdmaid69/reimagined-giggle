4b3410502f5b8ceae34d3b26331990b78e187aad
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:07:00 +0000

    tend scared choose children shout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
