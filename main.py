e8f8ca913f2d8a02754ee6a66e6ccf268a6e1758
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:44:49 +0000

    maybe pay surface means high

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+print([x**2 for x in range(10)])
+def find_max(numbers):
+        return max(numbers)
