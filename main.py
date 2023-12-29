9774d2a963b206e280997d66bf73e3e43f660847
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:53:52 +0000

    unlucky measure seem sight emotion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
