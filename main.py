eac4b525a0fc8ebaaf8208447c52de498cabf3f4
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:08:15 +0000

    blind precise regret left sure

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def get_string_from_array(array):
+        return array.tobytes()
