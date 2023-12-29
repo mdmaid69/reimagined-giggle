0d9975f99c176a1fa65113c101e2f822d8439070
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:12:40 +0000

    capital comment remove really declaration

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
