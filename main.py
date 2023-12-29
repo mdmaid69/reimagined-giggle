84bdc104af99ec2b8cde1906c12a34e883de77d8
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:58:59 +0000

    explanation murder post turnoff upset

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
