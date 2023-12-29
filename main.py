97b294c6dcafa9bb09b424d80aa2b8377de09a26
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:15:45 +0000

    fortunate disturbing colour swimming answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
