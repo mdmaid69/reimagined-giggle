35e02a710ea3118cb2d0799149c73bd85343309f
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:26:47 +0000

    effort trick smoke well minute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import sys
-def exit_program():
-        sys.exit()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
