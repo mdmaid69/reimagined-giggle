4077cda7b098203e04087abc00f4558a18d04bd7
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:36:12 +0000

    people discover street deserve boil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_as_float(array):
+        return float(array[0])
+from collections import Counter
+print(Counter("hello world"))
