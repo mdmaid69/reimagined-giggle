a6b1fca66bee5c1ac31d5dac195574648dff76df
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:26:59 +0000

    change little deal majority vanish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  def is_even(n):
+        return n % 2 == 0
