60ce0c061e6e59cb39ff20969bd6d91ee3cecf26
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:43:42 +0000

    effect minute team this none

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+for i in range(10): print(i)
+import array
+def get_array_as_str(array):
+        return str(array)
