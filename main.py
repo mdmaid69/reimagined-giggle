619c533465844180cebd0a3dd19f5aa93f11fea1
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:11:22 +0000

    taste storm familiar objection ignore

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
