a3957d82729fd81c0c14b292d7816fd97181d7fe
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:42:21 +0000

    keepaway turnout unhappy inquire introduction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import datetime
+def get_today_date():
+        return datetime.date.today()
