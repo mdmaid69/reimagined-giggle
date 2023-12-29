36777edb019db839bf35e12436eb957370bfc887
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:52:06 +0000

    twist argue range spot large

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def is_palindrome(s):
+        return s == s[::-1]
