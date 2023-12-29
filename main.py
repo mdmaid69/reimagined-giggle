18eded25272bef28b84344ea66637eb083f2b913
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:56:09 +0000

    tax Tt wrap separate deep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
   import os
   def get_file_birthtime(file_name):
         return os.stat(file_name).st_birthtime
