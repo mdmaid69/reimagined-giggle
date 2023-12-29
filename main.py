5e45cf740f9feb5da45670d4045355f74300e4dd
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:58:55 +0000

    hard record somebody feel peace

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
