44fc1716f439e4c6846f05c3ed4cdd680aae87bd
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:24:55 +0000

    cheap chase undo rescue blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
