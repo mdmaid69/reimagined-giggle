9ce16d71fd6f7a0df592859cf293ed488fa9a687
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:21:15 +0000

    plain wall closed throw doctor

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def multiply_numbers(x, y):
-        return x * y
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
