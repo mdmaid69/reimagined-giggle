335f3fa30232a8d4f5ce79fa62a58252b568bdbb
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:24:37 +0000

    nothing basic advise vote extra

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sys
+def exit_program():
+        sys.exit()
+import array
+def convert_array_to_list(array):
+        return array.tolist()
