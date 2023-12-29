62b8958e2a05952f0eb47283be00220ec48b6156
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:06:16 +0000

    aim confusing tidy forever cinema

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
