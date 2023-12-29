ac0e3000656bc64bdb6c2096f96ff939ef9fa24d
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:29:53 +0000

    flow fairly out approve marry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
