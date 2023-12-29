eaa4376352b09074710d93a456b68bb9f6270252
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:18:13 +0000

    unhappy night temper trust any

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
