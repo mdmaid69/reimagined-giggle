eaac649573ec1d775074aba149e8c5e65fd4729e
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:37:04 +0000

    concentrate cover really after wear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-print(os.getcwd())
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
