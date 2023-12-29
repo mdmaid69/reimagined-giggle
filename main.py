461589e273964b6860124bb0c866b73d9408aac1
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:03:10 +0000

    honest talkoutof pull bet right

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
