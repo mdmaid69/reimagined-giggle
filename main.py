f008d9d232b9ee3f6e916fa5b79ccd2351ba1004
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:38:26 +0000

    moral sticky plenty some apologizeapologise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_area_rectangle(l, w):
-        return l * w
+def reverse_list(lst):
+        return lst[::-1]
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
