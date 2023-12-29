068401a7a96c383371bd0e1f32536e07774840b8
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:43:26 +0000

    owe extra window oil excellent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
