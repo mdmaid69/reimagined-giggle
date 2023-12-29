9485f196c3a91ee64fe9d804805612a3e6453f30
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:37:28 +0000

    breakup further card talk glass

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_work(force, distance):
+        return force * distance
+def calculate_work(force, distance):
+        return force * distance
