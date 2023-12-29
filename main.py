bce4fdfa272384aef1086e928aabfa365f3e2d7e
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:21:21 +0000

    my fold disapprove unlucky modern

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
