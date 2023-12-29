9406067a905e9be853727b24900afc5788b8dcc0
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:42:27 +0000

    fear pure damage find possible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
