671ffb7493e8bf63277e9356e7d895c7dee8aa4f
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:34:32 +0000

    crisis Gg stop impressed story

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
