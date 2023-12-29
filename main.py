4a12cd577a3d068eaf50a11823ded691420556ea
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:06:50 +0000

    grand pity turninto ask take

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
