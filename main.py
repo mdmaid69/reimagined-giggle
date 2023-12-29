e3486bc107ff06d4dead2d2d9a3413245f7f7dd4
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:54:25 +0000

    pain fasten instead entrance bringup

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
