6f878aabecd3dff62122c6d6d04bc4eeb95f858f
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:47:33 +0000

    represent in bet environment my

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def get_array_as_str(array):
-        return str(array)
+from collections import Counter
+print(Counter("hello world"))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
