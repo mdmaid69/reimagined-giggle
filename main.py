27eaff9079b416d33bd5e624f5a8f3ddbac80700
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:28:49 +0000

    better disappointed beat medium hit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import json
   def convert_dict_to_json(d):
         return json.dumps(d)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
