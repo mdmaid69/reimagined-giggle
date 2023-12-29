9610560c59474f8a5c86cee44707ed95e015104d
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:48:03 +0000

    little measure each spend stage

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
