81226ef92a50af357ccb45994dc4c9c9963f0b35
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:17:22 +0000

    punish good actually after lead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
