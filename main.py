a6d66f7b76c49d2e0482505906ac2d94b5530f72
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:46:38 +0000

    forgive attempt final rough autumn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  def reverse_list(lst):
+        return lst[::-1]
