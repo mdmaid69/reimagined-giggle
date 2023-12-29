7925e9715281279ac8147d1acb7da6f0140e4ed6
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:20:43 +0000

    opponent spray march cry foreign

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
