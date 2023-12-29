d0ce7416864d9334d310126b2c7826397e1bff69
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:10:44 +0000

    goon it task wherever borrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
