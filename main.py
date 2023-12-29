dc93da6336f22b7e5c8fa64e68aa75a9d1e05059
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:57:18 +0000

    in expensive inform show out

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def divide_numbers(x, y):
+        return x / y
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
