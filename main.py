c73d6ed8d81075b3a8526ffe126c847784751cf8
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:29:25 +0000

    clear breath sticky phone wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
