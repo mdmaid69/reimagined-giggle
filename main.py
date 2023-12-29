3917b2c95719983a0ac2faa2b3cc1b87c0207d9d
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:50:08 +0000

    maybe that rough between unite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def add_numbers(a, b):
+        return a + b
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
