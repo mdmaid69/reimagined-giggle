0bf3087e7180fe3627700de92c16fb92e17fb768
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:53:26 +0000

    he night listenfor replace spin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
