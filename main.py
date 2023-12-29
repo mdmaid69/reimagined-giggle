fd61d1e993c50f2e4cd15a9846ffe9f2d856b856
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:21:21 +0000

    risk think power gentle talkoutof

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
