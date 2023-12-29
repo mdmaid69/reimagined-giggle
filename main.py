04b4bf9bbde2bf48bd027d65ebfea884594e7558
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:28:48 +0000

    properly rescue turnon blue instant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
