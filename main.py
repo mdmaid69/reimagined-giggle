d7f078893b1a90a55b574432e88a18a1b2503c58
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:30:36 +0000

    regular money concentration only tired

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
