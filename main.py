ef4cd1be15a9fc0771918b73c5ea926130ac6948
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:10:22 +0000

    such relief underneath unkind mistake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
