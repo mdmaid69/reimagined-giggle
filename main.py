15c1dbc972f92ad070c32c515133854be50921af
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:46:47 +0000

    operate knowledge above recognize otherwise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def find_max(lst):
-        return max(lst)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
