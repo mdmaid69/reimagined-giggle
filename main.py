435d894ac9ea098bd3568884c16bd6ac6dd5909b
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:37:06 +0000

    left nerve my himself goon

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
