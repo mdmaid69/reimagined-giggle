da54635073faa5c6e8043eb51c2951d5e6f5db29
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:38:14 +0000

    blame pot 566end exchange money

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
