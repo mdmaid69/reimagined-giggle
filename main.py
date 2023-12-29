d708a5f3adaf5feec63bcf9cadb0fcdfc557d8b4
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:30:53 +0000

    ball independent raise scale study

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_list_from_array(array):
-        return array.tolist()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import time
+print(time.time())
