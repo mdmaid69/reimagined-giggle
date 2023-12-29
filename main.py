76d9a4f26ae6234c08491fa0f6688b6cd23980b7
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:56:19 +0000

    immediately copy intelligent there else

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def convert_to_hex(n):
+        return hex(n)
