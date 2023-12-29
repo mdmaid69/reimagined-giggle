6a8b29b15981f08c525bbdb908a0ff3c0b1951c9
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:15:20 +0000

    contact top dinner national geton

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
