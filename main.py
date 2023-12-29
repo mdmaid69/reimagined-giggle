28e054ecbc28c8a4dd1ca59654215ec1f138940f
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:11:53 +0000

    square anyone defend weekend turn

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  def is_even(n):
-        return n % 2 == 0
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
