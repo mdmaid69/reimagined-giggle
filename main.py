f02fa88484b19aee0972316a0075b77a9e9deebe
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:29:10 +0000

    disturbing flat loss copy swimming

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
