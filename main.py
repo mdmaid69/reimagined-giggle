525e74239ace8613e691c0bee9c640cf50ed2898
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:54:44 +0000

    closely group area arm fault

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
