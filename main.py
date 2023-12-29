c986619227b573692b75a03e18d6f5c8629c56e7
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:49:02 +0000

    fix down unpleasant comeover perfect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
