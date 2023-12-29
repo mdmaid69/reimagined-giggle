93325c07af7806a897f22e69c0d8bdcd45312694
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:32:51 +0000

    concerning quick breakup hour handle

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_repr(array):
-        return repr(array)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
