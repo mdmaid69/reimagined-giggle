74d953025ef6d07b1f96bc8a80dc3f5fd02cd88c
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:14:51 +0000

    foot upstairs even pick storm

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import random
-print(random.randint(0, 100))
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
