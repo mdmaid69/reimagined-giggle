8c5a15e48d87b4d3f62f61f8754c94497f6c5099
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:29:52 +0000

    sleep once protest together remove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+x = 10
+y = 20
+print("Sum:", x + y)
