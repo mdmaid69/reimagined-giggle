6d4e10cfa74031e55580f7aef5c795160543bf19
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:41:45 +0000

    kind blame uncertain free thanks

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import math
+print(math.pi)
