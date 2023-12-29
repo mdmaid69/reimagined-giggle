ce2d280f5e40084cb835e3b3193732f184e6ecbf
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:08:40 +0000

    water impressed improvement he special

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
