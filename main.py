f45b9be0171e3c15ce499a78439e694276c34316
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:37:52 +0000

    lock to together everything throw

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import tensorflow as tf
+print(tf.__version__)
 import array
-def get_array_item(array, i):
-        return array[i]
+def pop_from_array(array, i=-1):
+        return array.pop(i)
