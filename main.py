19be266ac37fbe013f2ab546ac22377e7973e043
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:38:19 +0000

    find regard side rain band

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_exponential(x):
+        return math.exp(x)
