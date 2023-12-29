44e61121009b1d513e4edc21bfbb3710b46fa413
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:40:46 +0000

    lunch hour fond responsible concentration

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def find_max(numbers):
-        return max(numbers)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
