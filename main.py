73b59e1bd7a4be7edc0ac19921c7b148abea856f
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:09:53 +0000

    flow post dig organize hurry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
