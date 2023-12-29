bcfb44ab2965127aa0d7ee877fb878b4f386690c
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:41:38 +0000

    stay detail during related much

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def multiply_numbers(x, y):
-        return x * y
+  def convert_to_hex(n):
+        return hex(n)
+import os
+def remove_directory(path):
+        os.rmdir(path)
