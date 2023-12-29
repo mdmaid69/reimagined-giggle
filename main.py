c4834b179bf89afe4ac50969a4c413c01437035d
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:28:54 +0000

    rescue because usually trousers embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_area_rectangle(l, w):
+        return l * w
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
