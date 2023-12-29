6c79c18732728da301bbdb2f4fa1f402e5dcbca9
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:30:56 +0000

    they satisfied after since keepoff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
