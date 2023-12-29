72025c79bd637e98020865c12ced2ed07f5e22db
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:15:39 +0000

    embarrassing shall community nearly though

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_sine(x):
+        return math.sin(x)
