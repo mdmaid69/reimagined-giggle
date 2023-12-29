cff8ffdd05ff71f156fbdc595f33ff744064138c
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:05:16 +0000

    limit city 573entertainment will accept

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import platform
-def get_python_version():
-        return platform.python_version()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
