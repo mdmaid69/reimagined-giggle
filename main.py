1af3b607d17e270ef95fef61622eefa33db506c4
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:43:33 +0000

    marry surprising himself talkinto brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import sys
-print(sys.version)
+def calculate_error_function(x):
+        return math.erf(x)
