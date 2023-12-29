608e7596347d6a70e4e3c73de57f9bd7512d86ca
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:00:09 +0000

    body gather shine breakin fork

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
