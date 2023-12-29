17b79a97f9596d3d36377161159a0980589a0527
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:14:24 +0000

    bottom amount arrange open sometimes

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
