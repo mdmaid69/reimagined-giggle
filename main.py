f5ad54bbedbc9daf972786953f6ed15bb36c344d
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:49:17 +0000

    important if mother but nice

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_acceleration(speed, time):
+        return speed / time
