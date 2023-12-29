76c315a29a395f5b61b7dcfc45a44e80c899aab7
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:11:43 +0000

    connect play produce react still

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
