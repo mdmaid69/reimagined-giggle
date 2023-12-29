6749dfd0f99ff363f3d9c8d5f1366325a1a37c81
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:14:14 +0000

    brush stiff sing turnover care

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
