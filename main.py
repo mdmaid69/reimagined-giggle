d93af61a17ca8c87914264feb554aede27600705
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:00:31 +0000

    fold bad nose flight discussion

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
