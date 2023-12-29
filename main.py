7f5d3550ccdcdbde255642ea291e076ddec4430a
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:39:15 +0000

    nurse finally whole practise comeback

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-for i in range(10): print(i)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
