79412ee6a6b4c2f2fc05bcc8ad7860b4d3118d63
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:39:18 +0000

    miss character frame weather have

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
