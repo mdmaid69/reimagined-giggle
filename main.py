cd0a4cba3625cd9ac3e65fcbe91ce447103ec48d
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:41:19 +0000

    rich area they will event

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+for i in range(5):
+        print(i)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
