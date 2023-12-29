ecb36df0e996054e7a54a6c785f01fecbefcb4d8
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:17:29 +0000

    set group divide certain sure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import sys
-def print_python_version():
-        return sys.version
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
