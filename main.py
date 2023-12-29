366a1ff3ef0132e4d14fa30a1d523d5c1477e46a
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:04:54 +0000

    honest goout have cry notice

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_power(work, time):
+        return work / time
