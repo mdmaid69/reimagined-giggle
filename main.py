0e7ee17b748984b6591f68b193764f23e1f6bc21
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:27:48 +0000

    tall prove dead badly yes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
