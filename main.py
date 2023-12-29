19b9e55095bd079148cd7f5a5bcbfacec143920c
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:57:16 +0000

    vote wealth vision breakout average

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
