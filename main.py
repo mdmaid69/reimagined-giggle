49201292b1b95c36b29b7691caa3c05b76cdc733
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:27:37 +0000

    whoever strike used 570enjoyable complaint

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_current_working_directory():
+        return os.getcwd()
