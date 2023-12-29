19a31ec39fd4b1ed6ac600477c47dc5908c5093f
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:24:20 +0000

    switch would pollution store willing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
