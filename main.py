79faa33e32fe042ad93e3080488db9e223b33a7c
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:14:21 +0000

    worried remove as steep neat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
 import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
