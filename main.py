cb3a441f526e401cbe428036cc623e083b834399
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:53:46 +0000

    check lean confidence limit niggaacademy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
