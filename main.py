1b00467ece70e82567217110a3b13c6491b91d68
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:45:48 +0000

    over talkinto surface follow close

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
