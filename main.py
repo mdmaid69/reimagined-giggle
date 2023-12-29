6ba7379eae9f08ad2150cdcda88c5fbf03ddb690
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:58:15 +0000

    opportunity beyond worried reject once

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def find_min(lst):
+        return min(lst)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
