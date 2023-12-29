0bc405e7792b4b7fd1f27bc93d254f641a87c65b
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:03:23 +0000

    physical sure regional anything steady

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
