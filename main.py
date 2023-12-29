16700391c9824de97dda472f8a35368e66f42202
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:37:45 +0000

    mad spot fine exercise oil

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import platform
+def get_python_version():
+        return platform.python_version()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
