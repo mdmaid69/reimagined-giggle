8d90ddaecfefa5d30ec0181b63bdcf80bb1f5714
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:34:22 +0000

    reaction bury own owe gas

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
