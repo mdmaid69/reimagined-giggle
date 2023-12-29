f88f1781ec8989e1a4b66bf2a109bc6f0f4153f0
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:18:18 +0000

    door only Ii conduct wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
