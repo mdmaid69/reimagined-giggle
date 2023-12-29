c85062757de9d9e5eac51cd3f0e34ae5fd1c5ceb
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:14:45 +0000

    anything slip border store short

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def subtract_numbers(x, y):
+        return x - y
