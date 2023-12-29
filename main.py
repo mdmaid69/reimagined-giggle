350e9f5243630696cd0aea12e6a9b2e0b5ef6121
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:53:09 +0000

    opponent up chin train your

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import os
+print(os.getcwd())
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
