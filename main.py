9ec56a05d50e2abea25a8d5b3b11726ab39c1000
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:53:44 +0000

    design stupid part strange step

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
