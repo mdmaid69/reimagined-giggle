84d0223b327c000a55fb0555fe48c489d9d85ab2
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:43:50 +0000

    grow for place general exit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
