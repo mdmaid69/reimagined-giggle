65fb0350ed2f2ff869631819526aed1f40c7a8be
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:35:02 +0000

    through game decision performance entrance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def split_path(path):
-        return os.path.split(path)
-import datetime
-print(datetime.datetime.now())
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
