de8a9dc2d60ab2f9a088b289c128ac9ac57d0d19
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:07:14 +0000

    best show impression contain steam

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
