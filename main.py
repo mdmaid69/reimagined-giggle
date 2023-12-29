36f702de4fdb903156070435a5df9ddbab2b6001
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:38:33 +0000

    Hh pencil argument survive pen

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def is_even(n):
-        return n % 2 == 0
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def find_unique_words(sentence):
+        return set(sentence.split())
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
