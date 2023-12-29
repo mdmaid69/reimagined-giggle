2b24c0651fb6050313aa99f10885b762982f4d34
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:12:14 +0000

    brush hope guess deal public

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def get_array_as_list(array):
-        return list(array)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
