9b4963fcbb34b1ca0179ec7d2b209710eba468cc
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:07:00 +0000

    hit behind ability dare pause

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
