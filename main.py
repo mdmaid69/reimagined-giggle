e4c92e27a48b7e686163558f2599b6d03162db2e
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:43:19 +0000

    firm thankyou position environment life

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import sys
-print(sys.version)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
