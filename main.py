60d5346da2ebaf876bd544ab56a9796680c6c6d7
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:55:24 +0000

    around which pretty delicate relationship

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
