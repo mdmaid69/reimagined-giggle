021735ed4df14cc239e7196bf1ab56521499b81d
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:04:52 +0000

    afford view patience flow anywhere

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
