1fb0345a7bc19e34352638da6c78798c357bf802
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:57:40 +0000

    lead support fierce plenty habit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
