3377b0ba0bff21ca3e51bfe1be1956b828523499
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:10:43 +0000

    phone succeed list unit be

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import array
-def reverse_array(array):
-        array.reverse()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
