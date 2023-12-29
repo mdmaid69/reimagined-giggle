5c7f8288adafe39f6d089b05bd8d27611e7a525d
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:41:36 +0000

    progress party necessary Ff properly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def add_numbers(x, y):
+        return x + y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
