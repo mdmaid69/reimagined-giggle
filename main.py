db3d77f4fa6a149fb22dd1b856a4cbd1d849bcbb
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:58:52 +0000

    ought when drive sticky punishment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  def cube_number(x):
+        return x**3
