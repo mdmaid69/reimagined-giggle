70ff531ecc6bb1120260e8f21bec0aca25c0c096
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:10:00 +0000

    see excuse recovery work guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
