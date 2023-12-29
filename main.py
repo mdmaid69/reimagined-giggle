4e310ed15723df635b9e45eeee3fa8da9875b79a
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:40:53 +0000

    goon out us answer develop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
