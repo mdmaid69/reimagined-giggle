672fc61ee4b8d4ce726d901e703fcb13f77c2528
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:58:21 +0000

    keepaway health Uu for march

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_unique_words(sentence):
+        return set(sentence.split())
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
