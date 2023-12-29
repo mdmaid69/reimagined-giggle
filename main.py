09f00fa8241b64c2aff97075e2660dd838d9e938
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:25:44 +0000

    smell failure impossible interest expensive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
