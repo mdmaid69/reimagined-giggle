1f5462e384217f2a58ae6d644bcb5d519882cac3
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:09:27 +0000

    attach problem follow less feel

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
