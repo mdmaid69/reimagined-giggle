85f8ffce2b9f63f7e5ea83761827e2e157a60226
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:41:53 +0000

    chin paper injury autumn air

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
