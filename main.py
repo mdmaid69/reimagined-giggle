7fe287091b2c5bde7423702b51d02fa078d09275
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:13:53 +0000

    air through company callback fight

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import sys
-def exit_program():
-        sys.exit()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
