31b3dc4b42f0f679f5cf0e79118d277eb7691d7e
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:10:53 +0000

    voice shall chin nearly now

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
