7bc27a3030fe7c4c3a0b0d36a409985fb0ad602c
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:57:44 +0000

    after makeinto close post slow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
