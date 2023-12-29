996c7fcd6540599666829046a7ae82f66089e1b7
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:40:43 +0000

    lay slightly expensive stamp cap

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import time
+def get_current_time():
+        return time.time()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
