d4c7c11230b9483db61bf14ee50d78288e0773d4
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:47:22 +0000

    too hello nervous Ii Oo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
