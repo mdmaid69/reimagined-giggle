4facdd8923ca3c14bdafd5d6e560aa19546598bc
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:48:41 +0000

    well goon ahead crime judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
