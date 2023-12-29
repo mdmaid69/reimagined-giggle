5d91e5aeb343c125055bc3f75f8c8c17a6ea0765
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:16:08 +0000

    ill male mind whichever stone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
