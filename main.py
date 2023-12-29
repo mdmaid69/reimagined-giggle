cc95d40549d02d1abe0ee2a09e3ab46977bc3f59
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:52:05 +0000

    freeze thumb part Ww public

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def remove_duplicates(lst):
+        return list(set(lst))
