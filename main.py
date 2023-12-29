5427fe6548faffb090b54f351970e10605a3c0c9
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:13:59 +0000

    shape emotion stop show meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
