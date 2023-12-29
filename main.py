92a799ac95992cffe0f68b7624033036af6f81c6
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:58:45 +0000

    wish although ourselves seriously help

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
