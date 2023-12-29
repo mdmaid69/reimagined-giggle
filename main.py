d88e9feb61089d25185482f861f763d807700513
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:00:58 +0000

    favour behave sign until murder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
