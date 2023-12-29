00ffe42bf8cd4346462bf2c3b142eca14a65c4cd
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:57:41 +0000

    principle minority uncertain draw simple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
