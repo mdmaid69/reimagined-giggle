fed4e361fbe46aad3255d3eb3b78ec1b1e671c15
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:24:07 +0000

    little bring instant battery school

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
