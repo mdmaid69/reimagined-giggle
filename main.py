7a1776b540f5a925a989acadcc70514ef3b6d115
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:49:02 +0000

    slight think leg breakinto muscle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
