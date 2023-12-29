10ecc1d184a85d2e7f0bddaaccaa114f2b91036a
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:21:36 +0000

    basis move ability turnback border

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
