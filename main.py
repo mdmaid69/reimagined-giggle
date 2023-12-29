7b862fc58afa3e51b3cb3c2b266262f5e2484572
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:56:51 +0000

    basis keepaway gift trick turnback

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  def add_numbers(x, y):
-        return x + y
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
