e3257cc74b75a7655f66e0dcf005a36d91af402e
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:59:20 +0000

    find represent detail art during

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
