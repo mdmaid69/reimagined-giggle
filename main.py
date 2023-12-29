9e5deb9d6c7acfef0d7973bb68482457da45e08f
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:04:02 +0000

    slowly exact pause proud concentration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
