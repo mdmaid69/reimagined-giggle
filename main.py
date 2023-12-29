28b723b9e8034856b33e3c9373f42dace9a3b71f
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:35:39 +0000

    transparent part performance answer needle

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
