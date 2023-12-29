993238168d2005238f03f3cb3019c57bc959dd2e
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:29:14 +0000

    perhaps accident drive how hit

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import os
+print(os.getcwd())
