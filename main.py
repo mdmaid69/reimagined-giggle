3df0ec0f12c7daf6ab6bed2b60b11cae8533e118
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:34:34 +0000

    dead criticize regret convinced turnback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import re
+print(re.match("h.*o", "hello world"))
