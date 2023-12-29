ae91c089af87fdd4bb63cd2410438b049d9bbc40
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:06:27 +0000

    proud unusual moment freeze soil

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
