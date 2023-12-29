0be8a7cdfdfe08cf973229c15fecd85b719fef9f
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:52:46 +0000

    point congratulations minimum lookup deep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
