0bec8c2d3011175142a4c75fe2b431ea725a04ef
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:30:00 +0000

    everyone produce around ocean emphasize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
