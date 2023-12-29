fff8023225b34fd0bcadad2d9f982b95aa75db13
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:51:38 +0000

    suit rub try quality goout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
