44836b42c7911ae153683a171b714c16e91c9832
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:54:44 +0000

    whichever become trousers influence fun

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-for i in range(5):
-        print(i)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
