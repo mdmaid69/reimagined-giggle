9f836b0877227efc53918dbe9be2ec842a32e948
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:36:58 +0000

    sharp mine tragedy trip giveback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-  def cube_number(x):
-        return x**3
