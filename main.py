61a80c718aefca8c4034b00bc5897fcbe7682211
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:00:43 +0000

    certainly promise either forever stay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
