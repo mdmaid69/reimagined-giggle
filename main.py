6f0e8e30cd4553815f34e38d0cf4c9d8c829768a
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:55:23 +0000

    confused produce serve rain centre

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
