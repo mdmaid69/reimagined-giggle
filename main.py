e499a050c1ab218a0200b3767a25afdbdbc28c51
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:35:57 +0000

    before radio ball shout protect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def reverse_list(lst):
+        return lst[::-1]
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
