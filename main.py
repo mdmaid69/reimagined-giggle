935b821f1adc029e43fa35d195ed645137059b71
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:50:14 +0000

    save hate breakin already priority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
