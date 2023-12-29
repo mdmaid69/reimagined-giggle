00b6948784327c3bc6b6d69e0ad57ab33b75db63
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:19:02 +0000

    excited inside Hh false open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
