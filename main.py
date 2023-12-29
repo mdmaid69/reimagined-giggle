c505ce05d25bda090f70962b02f8dc94a9dc9f1f
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:49:46 +0000

    pretty embarrassing along yours strike

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
