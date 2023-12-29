e8dc80db2d04723ac948c4741d105d3b687feb8d
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:56:53 +0000

    intend maybe activity heart pride

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def calculate_area(radius):
-        return 3.14 * radius * radius
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
