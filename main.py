afe8caf74e4b22f1179423c8425e7352f79121ed
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:50:55 +0000

    calmly aim kind vanish anyone

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
