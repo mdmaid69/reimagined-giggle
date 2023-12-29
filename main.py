4782bc2bdb3c8e687d8a999133e91cf201f5a68c
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:53:32 +0000

    speak laugh responsible mess admit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  def remove_duplicates(lst):
+        return list(set(lst))
