3dbbbfe7550c266016d3289a6ac5ab4b4b16986d
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:56:59 +0000

    wing mail chase tend callback

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def is_palindrome(s):
+        return s == s[::-1]
