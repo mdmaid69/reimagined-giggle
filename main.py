b83f6915b10253e37360d84215387f77ce9e580e
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:11:12 +0000

    bring alone somehow against true

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def add_numbers(x, y):
+        return x + y
