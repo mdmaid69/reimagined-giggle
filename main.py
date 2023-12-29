ea2b0af089bf7146c7132ee5f8734c198e764ae8
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:35:14 +0000

    safe cause wander relax character

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+for i in range(10): print(i)
