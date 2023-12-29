c9d3ff304c695fd7c76cc18c4c2ccdb7c8c989d8
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:24:56 +0000

    nigga prove dress promise fix

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def reverse_list(lst):
-        return lst[::-1]
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
