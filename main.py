8edbe89e7ef2489e51aaeaf4946369e1255e6950
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:40:47 +0000

    downstairs successful praise will thumb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_factorial(n):
+        return math.factorial(n)
+def sort_list(lst):
+        return sorted(lst)
