182ad6d4265a2aff82e23738f7a34a8319618940
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:50:13 +0000

    furniture interest fairly area without

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import array
-def get_array_as_list(array):
-        return list(array)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
