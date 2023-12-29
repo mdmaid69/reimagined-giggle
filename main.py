94e0151b7a6fbe482854184149ee87c2abdfb820
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:26:59 +0000

    muscle usual land steal conduct

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
