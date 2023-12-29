3a808c8912ae3ade7e603c0b70d39e82e94ea318
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:44:22 +0000

    set maybe nurse powerful account

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
