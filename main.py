441b45859678329bc9ca098a00886ae3d72779d0
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:57:59 +0000

    calm age below love funny

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_base_name(path):
+        return os.path.basename(path)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
