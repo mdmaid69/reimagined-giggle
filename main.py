aca5b364e13369d3c79cc1005d4d50147577091c
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:15:52 +0000

    valuable ashamed maybe another wave

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def add_numbers(a, b):
-        return a + b
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
