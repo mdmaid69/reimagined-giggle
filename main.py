ed9144948a3d38ff6f388e7eb78881d793222f49
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:39:52 +0000

    responsibility follow total into ball

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
