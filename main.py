9c0e8e171f50cca3acc29da0e4cac48634b06ce2
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:07:32 +0000

    raw plastic dry want argue

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
