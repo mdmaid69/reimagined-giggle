38e4414c663dafa9a12e2ade593d22e19a7e0bad
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:23:06 +0000

    attention here ask say equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
