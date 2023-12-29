7fe6e858b158245ee604c6dbfea28d7fe9fc48b1
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:25:09 +0000

    emergency rain easy keepoff then

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_power(work, time):
+        return work / time
