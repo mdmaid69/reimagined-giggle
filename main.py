f215096015a53fc9e9498a3c14b0428ebfde9322
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:16:58 +0000

    by watch used exit game

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def square_number(x):
+        return x**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_current_directory():
+        return os.getcwd()
