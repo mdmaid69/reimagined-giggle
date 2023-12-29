936cccf14f006394c908201ce44b957208183158
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:14:08 +0000

    factory turninto switch thirsty fresh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def find_unique_words(sentence):
+        return set(sentence.split())
