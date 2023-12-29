9f7349914a626a3250ea9f6046f697692245fe9e
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:38:02 +0000

    gather instrument fire curious simple

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
