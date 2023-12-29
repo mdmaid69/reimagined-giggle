fe0e4ad053df41e9bbc939bddbccc18debe352ef
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:49:20 +0000

    deep object aim standard below

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
