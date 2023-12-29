a9ad3557630588effcd6204ce4740467e9881d95
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:16:35 +0000

    cross really increase lock interruption

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import collections
+def create_stack():
+        return collections.deque()
+import array
+def convert_array_to_list(array):
+        return array.tolist()
