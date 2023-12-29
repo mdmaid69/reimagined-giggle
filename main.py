671f512a75e85230cd41b612cf4c3319ba1d712c
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:06:16 +0000

    nor take shall post holdup

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
 import math
-print(math.pi)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
