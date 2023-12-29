d32d0064e9204b6c4c1115ce968c887ae2fc2703
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:40:43 +0000

    sister whisper gas underneath police

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
