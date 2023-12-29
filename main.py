f6717d321e02988dd51394e90c774db9de844c4f
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:07:44 +0000

    run scare content dick shallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import math
+print(math.pi)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
