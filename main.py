7ae665fd3a39ee87bfa52da1b3fc34ae249fa6a4
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:42:17 +0000

    tip drive cut taste dare

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def check_if_array_contains_item(array, item):
+        return item in array
+  def cube_number(x):
+        return x**3
