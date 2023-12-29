3e2381cad889259cec2f08e333e7a31fc817c632
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:26:09 +0000

    above lucky drop sense congratulate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
