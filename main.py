2896c16e2d4baf3f399e2e8db3278f4fd00df247
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:24:18 +0000

    second steal sight this unusual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
