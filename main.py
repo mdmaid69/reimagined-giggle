f32dc819a8fae1d663c235b44392423025983e4f
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:09:42 +0000

    afterwards match want slice whistle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
