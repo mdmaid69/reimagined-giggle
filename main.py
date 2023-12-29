d304ed4b04ffdb456449a9ff9fb6d8ca3d44d7b2
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:26:23 +0000

    get fall drink right communication

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def sort_list(lst):
-        return sorted(lst)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
