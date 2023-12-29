534bef3b84007682bb1dc4ce8722c817b3389204
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:17:02 +0000

    surprise unlucky replace rank pen

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
