a1e366fb09515472dc00f5c313875bfa82efdc75
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:57:58 +0000

    he mainly catch door blue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def convert_array_to_list(array):
+        return array.tolist()
