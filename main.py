bd7173d9a025a767b8f1ac7e130bf211377b78e9
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:13:57 +0000

    lose close favourite old home

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def greet(name):
+        print(f"Hello, {name}!")
