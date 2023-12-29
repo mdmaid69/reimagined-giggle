df24f712be562657eef9b0b173c2ea0609099d3b
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:37:31 +0000

    land trap attractive stop like

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
