dfa23637ec8b8455bc9f2aeb8dd681de6d0464f7
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:13:54 +0000

    healthy story idea build lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
