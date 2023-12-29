63b821c2fa903fe236008e643940305eb4e343c7
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:37:32 +0000

    left attitude independent direction traffic

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
