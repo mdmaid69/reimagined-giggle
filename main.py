5cd36ef336812bae9d0b6e96acd6f8c6b879c33b
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:52:25 +0000

    machine friendly offend challenge that

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+print(sum(range(10)))
