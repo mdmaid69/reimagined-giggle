c13e36b37f2d90cec6450dc585407edafba14993
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:57:25 +0000

    crisis to notice better brush

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
