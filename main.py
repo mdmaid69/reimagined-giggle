8fb6ab295dcaeba0355ba5395bb19d3578b72314
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:02:05 +0000

    bread quit slight look everything

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
