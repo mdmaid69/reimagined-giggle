c97512c3c3653679fc3f20861130297d9e116be5
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:07:23 +0000

    burn survive through light mean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
