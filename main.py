4c78ba0fc99c6fec68078dbf7b5cbfd2326a5275
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:04:10 +0000

    paper rather about corner intend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
