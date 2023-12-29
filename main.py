ecfa5720950419cadcdf23d3dbe0cb85fcdf366c
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:42:09 +0000

    throw this style tend pay

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_item(array, i):
-        return array[i]
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
