359af99040c4e951156bcebf60d9b8cc02a0d9a6
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:23:26 +0000

    skin science turn normal give

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def count_words(sentence):
+        return len(sentence.split())
