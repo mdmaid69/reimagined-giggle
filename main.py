adbe2e6fa8c09768d42e4d1833ef53ccaef5d5a9
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:05:58 +0000

    basic patience impatient comfort battery

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def is_palindrome(s):
-        return s == s[::-1]
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
