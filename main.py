9593489c78a953bc40c49667d6a771e8dcae35b7
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:07:58 +0000

    whole workon sound increase receive

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
