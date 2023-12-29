04bfaef65b7f508f38b786816c9f92a2ce1298ab
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:49:12 +0000

    drink find step little yours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
