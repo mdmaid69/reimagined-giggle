65946cd4d1ee144ebf09d6e8e44f144ae372b0fe
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:44:45 +0000

    lost sort organize lose tongue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
