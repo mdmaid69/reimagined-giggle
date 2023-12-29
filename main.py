24afe36ea8e3f22b89fbcf13f64839970bb359dc
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:12:35 +0000

    welcome ocean sound communication fear

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
