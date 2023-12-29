33990f1bb5d4f5adb79ef568e4c1a5165a7bc3a4
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:34:30 +0000

    born meaning debt into ball

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import random
-print(random.randint(0, 100))
+  def convert_to_octal(n):
+        return oct(n)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
