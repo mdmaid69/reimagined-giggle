1f8302e6a03d760b129b100e6c334fe73d745207
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:59:38 +0000

    influence wake excellent strength mean

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
