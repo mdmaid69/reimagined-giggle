5036ecfd5ee1d9f5a24c6558a03b608053134176
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:14:42 +0000

    tap hurt other sing comedown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
