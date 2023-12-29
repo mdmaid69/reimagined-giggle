d73d8b64eac1b6123e5695121fd8aa854006e46d
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:23:16 +0000

    earn firm problem face vote

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
