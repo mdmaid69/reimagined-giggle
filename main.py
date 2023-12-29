b6fb2d974ebfb10c2c8269cbb7ccb883e6ba413e
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:27:12 +0000

    neck warning by cinema unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+print([x**2 for x in range(10)])
+import array
+def get_array_as_bool(array):
+        return bool(array)
