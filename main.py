8b52d99e944a036134224645dab0ad55b82dad27
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:43:21 +0000

    soak care behave hurry sign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
