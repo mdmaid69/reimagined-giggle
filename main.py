f8b2c9e000ed0bfe2f3a517e69211fefb3abd9f8
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:42:12 +0000

    carry temper jump open put

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
