49a90a8420595d94e3803c6dfbca0200d93c08cd
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:37:42 +0000

    pile everybody appearance distance once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
