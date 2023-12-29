a551b4374660c0b62abb09d4e256645bc647cbfe
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:40:55 +0000

    innocent turnaway cap file time

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_itemsize(array):
+        return array.itemsize
