558957ab345fcb7bbc58371c9c9e044ad5fcf3e6
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:37:39 +0000

    boil invite 571enough else central

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
