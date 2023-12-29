c3e52091fd75c5cea134322edc57237f9ce46bce
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:41:22 +0000

    learn Hh walk machine Ll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
