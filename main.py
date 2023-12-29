5be14b292119b5529ddf24671d0dbc13ca6c1ba4
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:29:34 +0000

    tone use something draw book

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
