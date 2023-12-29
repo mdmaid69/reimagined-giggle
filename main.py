e0c81469bf414a7bd602860c481c560660bfbedc
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:08:11 +0000

    ground conclusion fact match there

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import os
-print(os.getcwd())
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
