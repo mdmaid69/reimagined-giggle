c6692d6ff0dd8bf8789c9ef1598afd53c92fc7f4
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:45:54 +0000

    settle watch exist sticky worse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_unique_words(sentence):
+        return set(sentence.split())
