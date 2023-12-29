4529baf77ad7abffc0d42f0efcd42633003213e9
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:55:11 +0000

    secret fit people sometimes consult

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
