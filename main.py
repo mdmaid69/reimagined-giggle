0e859d1045f06ade85e5eed69e04a5f886f1f082
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:02:20 +0000

    print description tradition head of

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
