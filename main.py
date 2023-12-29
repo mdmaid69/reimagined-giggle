20c9346ea345c074e8242aadd0564d6c0a70aea8
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:36:35 +0000

    fit fond pattern bringdown example

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
