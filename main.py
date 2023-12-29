5b4c1ca1b21030d4e4e19f07805d72d2cab208c4
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:03:09 +0000

    fault plenty shout material foreign

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
