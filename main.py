f54829855472494522c965ad12325133c1823159
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:52:19 +0000

    about stir stranger confuse expect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_union(list1, list2):
+        return set(list1) | set(list2)
