f7dfe538ab91a65fe843f8d18674bebd8bf14c34
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:14:51 +0000

    stone depend legal square habit

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
