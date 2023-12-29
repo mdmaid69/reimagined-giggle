bdcabdc6fab69bc8210cceef76790f0f9411ce4a
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:46:11 +0000

    glass delicate opinion price both

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_array_item(array, i):
+        return array[i]
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
