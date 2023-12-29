8e079a4e3d6fe1999ee4647ba6b75a07c2fe2697
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:39:47 +0000

    demonstrate scare treat release breakinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
