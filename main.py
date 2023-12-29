b5ecc9c5649141fc99669a7a80406f9fad3d4596
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:11:14 +0000

    concentrate to drink original on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
