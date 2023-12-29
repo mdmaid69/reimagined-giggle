5d5389152356f2f18efc88ebb13df5f40ba896cb
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:51:54 +0000

    me callout tall once lucky

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-def find_unique_words(sentence):
-        return set(sentence.split())
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def is_palindrome(s):
+        return s == s[::-1]
