988759e37088b130c595bd67f5cce4cf497b7a0d
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:28:58 +0000

    home include go spoil ilovedicks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
