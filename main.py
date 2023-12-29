05a0fbc3553d4aaa0abf7aeb21bfbfe7cfb029a3
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:24:07 +0000

    work fork lucky shop front

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def divide_numbers(x, y):
-        return x / y
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import sys
+def exit_program():
+        sys.exit()
