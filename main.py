ea9614fdd85b4d745e9ec82b287243e0329793b5
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:03:40 +0000

    nervous 179bite getoff sentence chief

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def add_numbers(x, y):
-        return x + y
+def find_max(lst):
+        return max(lst)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
