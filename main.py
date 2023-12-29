c6d456a563bcb8c013bfd86b911f10b706077853
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:50:47 +0000

    cry minority team pool lock

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def reverse_array(array):
+        array.reverse()
