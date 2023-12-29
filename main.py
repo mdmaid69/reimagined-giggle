2581f73cdfa7908ba92794e934cc5cd79c02c9ef
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:35:28 +0000

    Ww steady talkoutof last wave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_gamma_function(x):
+        return math.gamma(x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
