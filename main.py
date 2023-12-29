fe786e446c31aab8dc77287de8c8cb72dc369a1f
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:02:03 +0000

    if they violent exactly rate

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import sys
+def exit_program():
+        sys.exit()
