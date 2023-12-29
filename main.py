67f488915d68133fbf90a84e13a5d6e09107c7e0
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:08:49 +0000

    object colour party talk track

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
