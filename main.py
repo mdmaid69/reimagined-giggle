44e3bdcf7362e79851fbcb4ac3dffc73e244e934
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:48:10 +0000

    Dd interruption occur anything skill

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_power(work, time):
+        return work / time
