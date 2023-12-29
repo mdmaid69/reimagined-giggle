fcb36f9d6555d34b088699ed83add54e02c0002a
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:55:39 +0000

    laugh race limit niggaacademy what

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def convert_to_binary(n):
+        return bin(n)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
