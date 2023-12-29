8886727bca2e6f2b3fbefc8274bb7fa48fccca2d
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:45:44 +0000

    slide height front consult wire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import re
+print(re.match("h.*o", "hello world"))
