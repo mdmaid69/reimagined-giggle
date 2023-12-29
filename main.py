a3f08c2d5b533e872f8c7e34b5e5072c47745cb8
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:38:27 +0000

    today dry still burst throw

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
