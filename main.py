cba252ad63b59f06a5c69008807c8cf32946f8db
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:26:49 +0000

    reach push ignore opportunity anxious

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
