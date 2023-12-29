1bd159012850cb79811e762802df2cbbfff6e9a0
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:08:01 +0000

    confusing rest far practical shallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def calculate_tangent(x):
+        return math.tan(x)
+def calculate_work(force, distance):
+        return force * distance
