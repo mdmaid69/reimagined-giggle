32bcff8e34342d50dfec14d7573956be739dcd13
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:48:44 +0000

    coffee loose conclusion fix crash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def multiply_numbers(x, y):
+        return x * y
