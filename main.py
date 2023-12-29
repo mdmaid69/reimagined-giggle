33183a651576b492cbc0a0befa27e0f4a8076070
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:03:29 +0000

    takeoff free uniform lunch soft

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_sine(x):
+        return math.sin(x)
