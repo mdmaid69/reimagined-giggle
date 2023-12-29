920893fac62d552b4971a0aad9232705f1a59d4c
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:21:33 +0000

    ever factory before have respect

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
