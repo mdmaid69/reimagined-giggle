5463a11fff4169ff4965a78011c4449de1ed53cf
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:14:49 +0000

    crash Hh tread however beside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def convert_to_hex(n):
+        return hex(n)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
