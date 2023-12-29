77622a7244ba1ad5dc1530a9e96dbdaf090ddea3
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:12:46 +0000

    loose guard sudden wish yourself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
