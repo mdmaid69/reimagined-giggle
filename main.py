1755f6382361b4dfcf61bde32418ab395e1b8647
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:11:37 +0000

    question pay taste off write

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
