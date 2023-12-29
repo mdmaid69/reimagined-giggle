1b171d412bb02e2e124ba66a9d4f08db15318ae9
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:43:15 +0000

    game taste after when claim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def remove_duplicates(lst):
-        return list(set(lst))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
