65b4dc3618bad04a2e09d675e49ff12d548f94aa
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:19:04 +0000

    occasion Yy tidy minimum defeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-print(sum(range(10)))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
