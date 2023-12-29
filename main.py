7496c7c6427708ed162c495576b20174f79f5dab
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:00:23 +0000

    switch along guess Rr fashion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
