5fdc166842b91d14734559ae45ab107febeb59f9
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:19:01 +0000

    branch yours keepfrom reply 179bite

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+print("Hello, world!")
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
