a8f5ef65eaf9ff377eda57c5505688c76f4f44e5
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:31:18 +0000

    control serve failure bringback shade

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
