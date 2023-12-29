7c04b2b494e63b191d0848c901e38496f49a219c
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:18:01 +0000

    bank keep spin number depend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
