0d4743bd1cbf8d29b20df2ebd006fc03c5f63439
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:31:25 +0000

    dig news punishment reach round

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
