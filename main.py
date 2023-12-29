f9bbdff4daf0f0692c2563fe02a1ceeaea73dc45
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:27:40 +0000

    pot height level sentence shrink

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
