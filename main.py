4058cf6cb9327b1c2b94c5bec3408a00ea310c56
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:36:07 +0000

    depend straight copy copy temperature

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
