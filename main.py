e2bf84d75aeca33831fb3460b9253c6edf8761e5
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:26:36 +0000

    giveout spot surprise it near

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
