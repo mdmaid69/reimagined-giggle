d8bd1eab113790a228b2af92accf6d73354496b0
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:26:37 +0000

    home neck aim press defeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def get_array_item(array, i):
+        return array[i]
+import array
+def get_string_from_array(array):
+        return array.tobytes()
