2c9262acbfb65c53a7152e2f95761d5d70fa9963
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:26:33 +0000

    organization several need announce until

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+print(sum(range(10)))
