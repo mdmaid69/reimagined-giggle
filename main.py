05b9d045952a506cc739d28bf243388c57d3fa6a
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:35:46 +0000

    bill club turninto normal neither

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
