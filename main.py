bc27b47cbb8f59dc2d4c64b10ce1d6bdb4d3bfab
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:52:04 +0000

    involve big book our machine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
