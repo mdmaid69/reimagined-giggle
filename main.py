2efe62efa5f808400097d852ad1de95e04a35836
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:17:33 +0000

    close or bringforward fill correct

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  def multiply_numbers(x, y):
-        return x * y
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
