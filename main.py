eccc5690c2f8412353409ec99c88bca414d2c95a
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:22:28 +0000

    disturb anything gift break hurt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
