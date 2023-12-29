00d27679eb04efba1732f1c7957ea97e74dcc9cb
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:35:43 +0000

    nice 568energy beside pleased cruel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
