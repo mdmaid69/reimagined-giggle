21e20390689df5798a8c55f67167d95904e54ea6
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:43:02 +0000

    pronunciation tip comfort fear Uu

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
