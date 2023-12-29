2a3c7459146ba3a5f092e35ab83d22cd18146a6a
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:40:19 +0000

    smoke show answer interview upwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def multiply_numbers(x, y):
-        return x * y
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
