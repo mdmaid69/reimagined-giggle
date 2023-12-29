232dc454acd79856d93f5d52e1bcf4b2bb55e653
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:43:26 +0000

    direction drive basically still immediately

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
