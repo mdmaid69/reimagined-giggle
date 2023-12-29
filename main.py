267caf80b7d4480d44c3246a50f7d2866d812648
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:38:51 +0000

    water typical just same cause

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import datetime
-print(datetime.datetime.now())
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
