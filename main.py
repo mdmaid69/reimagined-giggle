18db6d9498ae4c35e7c5da2af302093ed2bd4418
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:14:30 +0000

    close mainly involve hate ever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
