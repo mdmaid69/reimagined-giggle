e5d39961121240f6a6f18eec72c86613faf59ce4
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:17:47 +0000

    cause train frightened comein 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
