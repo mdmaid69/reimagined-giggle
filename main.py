e05446ca7b96fb6ecd6a9cf28e438aceffa2ed36
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:45:13 +0000

    store say attention tongue light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
