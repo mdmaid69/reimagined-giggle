9d180000584583929c1df20b8449cbf1e7dc55fa
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:12:45 +0000

    yourself intelligent life so copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
