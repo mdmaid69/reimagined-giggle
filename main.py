350008319c9acc314b95e0137a00e4bc4809f0a1
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:05:48 +0000

    bother sweep plenty stop pile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
