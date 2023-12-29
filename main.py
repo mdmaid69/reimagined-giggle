bac0931599c10944f8b6883d4a2c1b0d5d2015c3
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:07:37 +0000

    order ground store safe show

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
