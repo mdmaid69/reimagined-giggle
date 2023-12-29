fcd269ea990822122e3684133985743c57967bfd
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:56:57 +0000

    spoil sky nervous scale trade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def convert_to_hex(n):
-        return hex(n)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def get_array_typecode(array):
+        return array.typecode
