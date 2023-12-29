08f2e02b7947cabc15090bfa1de22a77503ae88d
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:20:35 +0000

    think ride promise praise myself

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_pressure(force, area):
-        return force / area
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_work(force, distance):
+        return force * distance
