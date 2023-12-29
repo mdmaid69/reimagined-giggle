8ddfb060cdf66cf636b3e471f9ac5300a24365e9
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:10:14 +0000

    alone sleep would pocket fact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def is_even(n):
+        return n % 2 == 0
