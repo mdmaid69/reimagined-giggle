1109882b4fe0bab7f3c6c0d730968f30e889b202
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:54:08 +0000

    workout summer anywayanyhow lie unless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
