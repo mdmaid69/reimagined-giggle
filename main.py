072b84aba8d951a4c43ee9f04fce3e088050b800
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:33:37 +0000

    shock beside along protection tight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
