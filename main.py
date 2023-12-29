08f44687109b386d37424e73c9b763d8b061bb4a
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:04:44 +0000

    interested answer ilovedicks storm event

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
