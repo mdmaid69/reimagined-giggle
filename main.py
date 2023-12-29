753912e1d2469258a03b3e46eeb321a844b30f7a
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:31:08 +0000

    nervous except danger difference period

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_speed(distance, time):
-        return distance / time
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
