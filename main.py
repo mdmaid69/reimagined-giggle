e5b9e58f941eba7259e37f710b9e580fb654a12a
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:35:01 +0000

    depend patience shallow similar scrape

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
