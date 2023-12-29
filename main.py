774f4002a15e14cc9de326661cfcad34e2772752
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:05:23 +0000

    later because naturally who make

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
