92872214f5a92be4c48d6c916caa72c9f7f308c8
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:03:06 +0000

    anywayanyhow possibly tooth exit injury

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
