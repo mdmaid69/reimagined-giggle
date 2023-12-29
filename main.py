eff47d511dfeb5f293d20a8fd5c92288bae1a479
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:10:41 +0000

    sting pupil nervous just duty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
