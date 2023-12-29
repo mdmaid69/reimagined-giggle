791cff766416c598c332ab8b773b34b8013ea377
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:48:30 +0000

    yesterday wind squeeze charge unhappy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
