206ceb6a823f7d312cfc4354678f8affd1bbd589
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:09:04 +0000

    nothing collection goup frightened choice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
