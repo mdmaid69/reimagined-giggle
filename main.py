9bdc4f5b4d3221cfa6fb5109ce449b6da96afbf0
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:59:19 +0000

    couple pocket up bang star

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_acceleration(speed, time):
-        return speed / time
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
