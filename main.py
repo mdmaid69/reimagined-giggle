56bd4b0f7a5374892dfe2a24b82e745ec3dd9614
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:01:37 +0000

    raw miss market stupid gentle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
