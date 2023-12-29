c59c4665f0a6429cb416f7e9e3a84a124b6f0c24
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:15:26 +0000

    angry reliable be difficulty underneath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  def calculate_distance(x1, y1, x2, y2):
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_distance(x1, y1, x2, y2):
         return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
