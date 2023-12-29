024a5bca95b10a751a5b17b5b94e5636e6aed484
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:04:52 +0000

    ball heat deliver long why

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def cube_number(x):
+        return x**3
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
