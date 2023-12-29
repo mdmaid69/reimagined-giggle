c2c2ec03f8c8b77c68a951dd7d79e282b8069b3f
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:28:50 +0000

    slight everybody neither town guess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
