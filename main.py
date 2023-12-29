7b3e95179dc274aed2ff3369dc4cbb9ba76d77c6
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:34:59 +0000

    dry cry language case can

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
