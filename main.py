e191c118afdb358a36363e8c1447909b5ff9af4a
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:10:56 +0000

    unless guilty commit peaceful evening

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def convert_to_hex(n):
+        return hex(n)
