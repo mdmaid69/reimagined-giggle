ee6d58a26a16a4873321eed2aaad5995fa49d2ac
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:18:55 +0000

    protest peaceful generally give slow

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
 def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
