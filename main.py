d53fb91b0719c7543d98addfcef608008fbf6778
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:10:06 +0000

    letter couple place slide sure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
