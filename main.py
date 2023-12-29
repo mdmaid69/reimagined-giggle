8a63b2d0a3258e817fe16f66f5feb63ea2804c08
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:08:22 +0000

    turnback must male project recent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
