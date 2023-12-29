32e5bd4ef55982314931465c7e3b378e74b899e5
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:50:26 +0000

    great lucky translate slice ever

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
