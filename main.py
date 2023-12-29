e400182ba43499c6ffdfd7244956af2b0266f828
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:34:39 +0000

    design affect ordinary judge case

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
