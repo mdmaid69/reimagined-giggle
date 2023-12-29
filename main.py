cb35663aa656ffc73deab61698215af7cfa4468a
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:38:17 +0000

    less noone runover move phone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
