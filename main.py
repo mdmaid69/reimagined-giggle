3283fca9f989210932e31dced8621a8feda9394c
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:13:13 +0000

    minor temper acceptable amount muscle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
