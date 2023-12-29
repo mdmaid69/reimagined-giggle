65d0a846283acb14aa0528cccfecc37201ccfb9a
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:08:56 +0000

    discussion print fashion trick comfort

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
