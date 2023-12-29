cc7c139587e1965d17727e99e5a99b1dcd0b02a4
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:03:10 +0000

    carry woman test contribute apologizeapologise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
