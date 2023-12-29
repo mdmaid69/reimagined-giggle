e0a4a09a4ee992c793780f43c4e9f4dd477ff9be
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:09:41 +0000

    smile lean pay luck combination

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
