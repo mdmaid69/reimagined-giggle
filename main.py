f7c9714a2fe1e1243835ad28547721ad290ee2ee
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:58:49 +0000

    suit excuse spray taste serious

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def clear_array(array):
+        array *= 0
