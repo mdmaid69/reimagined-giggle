61462e08cd7f1ca5090b6f2a5054da476b5b36ec
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:39:35 +0000

    lookup thought favour bush Itself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
