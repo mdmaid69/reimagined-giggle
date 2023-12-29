1517dcfc21f40087aff4ff62e2ab5f98aa4418f2
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:37:04 +0000

    compare breakdown strike breathe tidy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
