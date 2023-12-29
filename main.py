801f56fc89dfc3146a9cbc2ff703b81644182897
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:50:50 +0000

    hold generally near afraid train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
