8d55f0e3f259eee55d61a5b56dce2695d1915f11
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:33:47 +0000

    mystery than course purpose recovery

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
 n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_current_directory():
+        return os.getcwd()
