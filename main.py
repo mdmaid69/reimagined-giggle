751e9fc99f6c477a574c197419f8669720915947
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:22:00 +0000

    guess there bring light last

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(lst):
-        return max(lst)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def convert_to_binary(n):
+        return bin(n)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
