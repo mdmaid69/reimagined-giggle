f742cbb0507051e8982e97332b13a4b610ab7c59
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:15:01 +0000

    drink level impossible spread spend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
