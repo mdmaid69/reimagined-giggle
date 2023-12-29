44cded02373b45734f82290a367ba66767364561
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:26:05 +0000

    cinema low like over die

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def delete_file(file_name):
+        os.remove(file_name)
