14f198389bf4899ffff1e721010f4030de973b25
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:47:55 +0000

    beneath test best pour turninto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
