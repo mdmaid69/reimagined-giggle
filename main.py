6352c22c86b37f9acd970f17510dbc4edd551603
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:16:16 +0000

    bin pleasant murder safety murder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import time
+def get_current_time():
+        return time.time()
