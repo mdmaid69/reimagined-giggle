ae05854d68e35facc9e40a546abc687cf52cbcad
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:35:21 +0000

    inside finally mistake on politics

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_float(array):
+        return float(array[0])
