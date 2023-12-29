1878432d6a4074892d59093b9c0232499f0ef359
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:25:09 +0000

    meeting runout right afterwards before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import os
+print(os.getcwd())
