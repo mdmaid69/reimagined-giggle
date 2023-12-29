e92a92cce4b7749bb4f7a69c999e11adbd3e481c
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:04:31 +0000

    morning frequent draw lay situation

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_item(array, i):
+        return array[i]
+  def remove_duplicates(lst):
+        return list(set(lst))
