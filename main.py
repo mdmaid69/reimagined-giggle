2e77171693cfb6608902ec9cf5f6b6c3c32fae76
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:59:48 +0000

    stone crime workupto foot past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
