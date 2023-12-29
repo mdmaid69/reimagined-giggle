b2d3c953b4399434ba5ceeae87c6fdf32a90fdc8
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:25:28 +0000

    keen machine comeback walk boring

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import sys
+print(sys.version)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
