f2ef7e5e8432b7fe6b3943c74340d03b1aca3150
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:48:47 +0000

    free unlikely boring light meeting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_area_rectangle(l, w):
+import sys
+def exit_program():
+        sys.exit()
+  def calculate_area_rectangle(l, w):
         return l * w
