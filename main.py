40a557f6d3dc73e7f3b9d296f556c10a6eab4939
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:53:19 +0000

    area earth about breakinto value

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def convert_array_to_list(array):
+        return array.tolist()
