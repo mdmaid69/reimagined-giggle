1ec56d606e75cfd6003c0a7c95811947bb41e3ab
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:51:12 +0000

    last harm getback clearly suffer

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def square_number(x):
+        return x**2
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
