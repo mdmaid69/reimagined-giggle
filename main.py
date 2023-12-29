1f848375f33a583648a6b8ed2ff4900fbd472d70
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:52:05 +0000

    equal post cry stranger turnover

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
