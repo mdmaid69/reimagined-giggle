119f0996fecb8f646badf438610ca8d3a22b43e7
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:27:24 +0000

    watch turninto person light account

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
