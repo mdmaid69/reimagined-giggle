88b0dfa09c507de6650893673ff04d39da8a6467
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:08:05 +0000

    whom recent moment there then

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-text = "Hello, world!"
-print("Characters:", len(text))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
