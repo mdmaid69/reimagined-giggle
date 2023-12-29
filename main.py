532c3da088196532c850c13ebbd4e505c5a93a41
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:18:40 +0000

    course mend brain bet after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
