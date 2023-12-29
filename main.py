5c1b3ae397c08f50fa11e9de3c775e4dad9ede23
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:36:53 +0000

    eager squeeze spin advise national

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
