a7cff318a3654441ca898ee35566cac9a13df49c
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:48:10 +0000

    house who dig present home

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import re
+print(re.match("h.*o", "hello world"))
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
