3184086bfcbb65ed860801c78f846e07dac57a0a
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:10:18 +0000

    general exchange physical warm wrap

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_length(array):
-        return len(array)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
