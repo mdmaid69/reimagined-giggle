04012fdb4867b29baf6fe351232559c8ff961a53
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:02:55 +0000

    breakfast search few forgive leave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_pressure(force, area):
+        return force / area
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
