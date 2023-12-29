68b228784fd4ba88f5fb83f0a4fd6485dcd50114
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:48:05 +0000

    interview strike after no convincing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
