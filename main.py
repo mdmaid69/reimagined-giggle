fe5a37b55c4f1bf3f1fa5a4d03bf9f7b60ddbfa0
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:05:55 +0000

    dead fill bank sharp history

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
