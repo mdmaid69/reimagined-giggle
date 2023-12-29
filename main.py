8d5e5ffc99909a66c205b0110dad617ee498852f
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:26:55 +0000

    reject attitude touch degree eat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
