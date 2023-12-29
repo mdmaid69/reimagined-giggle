7ef4939231aa7af17b1c19aa88f6547694dd377a
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:37:21 +0000

    no pronounce minor guard meet

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 def find_max(numbers):
         return max(numbers)
-def sort_list(lst):
-        return sorted(lst)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
