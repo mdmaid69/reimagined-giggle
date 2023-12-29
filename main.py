0e460039bbd05abc73a71239d1e6f768caba3faf
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:48:11 +0000

    brave programme autumn close excuse

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_cosine(x):
+        return math.cos(x)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
