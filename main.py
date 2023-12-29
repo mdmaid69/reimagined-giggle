fa05b06cf691981b3e4adda4ea47e55cff929bdf
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:20:05 +0000

    sad help wide whenever war

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-print(sum(range(10)))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
