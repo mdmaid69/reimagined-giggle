75ae43503b958a256bfefaa822d7ed43fccaf4aa
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:10:49 +0000

    noisy mysterious length skirt trust

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  def cube_number(x):
-        return x**3
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+for i in range(5):
+        print(i)
