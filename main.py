1aefdae017b500d6bfc83c7f41ae677f56579779
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:14:18 +0000

    compare discussion fat human hurt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def find_unique_words(sentence):
-        return set(sentence.split())
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
