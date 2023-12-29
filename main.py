7dc78ca21c882f0827508c552e375d5ecbd459e4
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:47:37 +0000

    quick foot measure bet half

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_pressure(force, area):
+        return force / area
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
