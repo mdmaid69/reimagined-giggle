41f4fe4500fc679af99b2cda61bdb5a9617f3e3d
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:29:11 +0000

    far lead group weather long

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_min(lst):
+        return min(lst)
