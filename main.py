0c0e9fedeced990e391df8208b59affb1fb33cd5
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:04:07 +0000

    comfort until answer unfair store

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
