9e1090eebcbd5e097fb4e51daa6a066f9630c6a7
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:18:32 +0000

    increase usual able read advanced

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_power(work, time):
+        return work / time
