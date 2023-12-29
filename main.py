fa86233fdfc91f68ce1490e5811ee9889e268afe
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:46:39 +0000

    professional else over run married

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
