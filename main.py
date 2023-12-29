a6fc33317f9fdebde1dc7899198c268b6bf7d4b1
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:06:32 +0000

    fly goon bank hold understand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
