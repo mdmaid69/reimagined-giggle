9e9a4ce566f8220dc170e3a7ba614b5f83dbe2a1
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:08:49 +0000

    evidence plastic plain note bite

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import collections
+def create_queue():
+        return collections.deque()
