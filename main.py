425dd8da07863714344c0039943ca4c6f8f3bf72
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:51:40 +0000

    count long talkinto ground needle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
