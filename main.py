18f9015ed8876286a254e5461443b8c660e29f43
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:46:02 +0000

    explain please wish left alive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+for i in range(5):
+        print(i)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
