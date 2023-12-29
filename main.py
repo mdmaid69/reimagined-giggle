f27139bbb8ee650acfae26e59e5c9e7f57fb9da4
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:35:52 +0000

    else scared smooth listen from

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
