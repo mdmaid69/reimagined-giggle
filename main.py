b0eff11eb715e5f84bfabe9aef9834e7ea6555fe
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:44:18 +0000

    strike experiment close demand anxious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def remove_duplicates(lst):
+        return list(set(lst))
