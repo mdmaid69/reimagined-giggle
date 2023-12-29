be199cba4bb30457666d12c47bfdf37b19213d48
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:49:42 +0000

    rare even convenient advise quarter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
