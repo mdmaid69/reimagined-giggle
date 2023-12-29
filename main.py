5923183489580fb58bafa5cb9c938723ecbde109
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:30:17 +0000

    mean cover least suit kind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
