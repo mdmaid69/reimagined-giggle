aa7df0f7b0fe50cc5bce049c72b5aa67a49afc91
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:45:15 +0000

    better lose chin reaction different

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
