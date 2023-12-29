46d5846782aa8be21c2b53d0af3cbabc6722ed73
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:13:54 +0000

    correction accidental stick better connected

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
