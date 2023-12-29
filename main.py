5dd72ab437a55e9b457af289ea5986655b59c97c
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:12:42 +0000

    block fast I young future

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
