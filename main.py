2a089bd4836b183e5a1c6b23056d55401f28bee2
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:34:23 +0000

    black on gentle reasonable thorough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import sys
+  def get_python_version():
+        return sys.version
