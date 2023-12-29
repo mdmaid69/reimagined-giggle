36a40fd2b2894dc1e1c9225a05666f8d4cea58d5
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:09:13 +0000

    colour decide block chase wide

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def count_characters(sentence):
-        return len(sentence)
+import collections
+def create_user_list():
+        return collections.UserList()
+def count_words(sentence):
+        return len(sentence.split())
