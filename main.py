232386cc21394615e38f786c2dd39808faf1f5df
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:10:32 +0000

    important fashion work throughout door

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
