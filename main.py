6ceed2c70848f6c175afb36053bab2d29b27d9a9
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:19:12 +0000

    wise coffee loss eye pipe

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
