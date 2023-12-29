578d16527c36ce6ad4ed79a7c8e634b855384af1
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:57:27 +0000

    community takeover moral if dear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
