374ca46c3b30935aaa6863b9344de9210a8a15d8
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:05:14 +0000

    pour money father switch agree

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_work(force, distance):
+        return force * distance
