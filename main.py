ba6331c2da11d8a568d5b61b3b16b07e88296997
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:43:32 +0000

    as secret dull takeout tall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def subtract_numbers(x, y):
+        return x - y
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
