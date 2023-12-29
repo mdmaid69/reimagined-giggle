39a7d0584ac16fdef603c41d3b9df6c461248680
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:52:06 +0000

    produce wash bit spring through

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import datetime
+print(datetime.datetime.now())
