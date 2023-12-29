2c715ab9e54899c40a66f9fab34bdf759f11215a
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:58:05 +0000

    side trick prize score hit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
