cf40a9c3a067df9ad033cc99fe567f28503aa5a7
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:43:38 +0000

    seem square smile teach wish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
