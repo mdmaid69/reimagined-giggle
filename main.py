08ba8c7c910b477b883dd2987214dfba27f54d59
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:51:44 +0000

    as finish post into vanish

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
