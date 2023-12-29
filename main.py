d8ab93546f021ccc65c8bb8e5c0e086e22a86385
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:11:02 +0000

    back rich beneath ball spring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
