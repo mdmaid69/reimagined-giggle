a9b21b53fb9c314c02c68d45b1025b33543fc156
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:12:54 +0000

    approve month goback(to) trade project

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import array
-def get_array_as_str(array):
-        return str(array)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
