f95fbd5fd77ff660a84f8628c5644050d931f62e
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:48:54 +0000

    allow persuade hate name advise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
