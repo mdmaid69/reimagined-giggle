8b4d0485ae113c28c68976f58b59862dd642d50d
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:51:03 +0000

    familiar loud ill tend steer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
