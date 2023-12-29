c14d1030cb98395dc94c5d34a1879c49548b2d42
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:05:05 +0000

    climb bit try scream exit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def count_words(sentence):
+        return len(sentence.split())
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
