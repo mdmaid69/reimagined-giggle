96222c16c325de70aa633a83a6d46206d6c013d1
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:02:23 +0000

    broken recovery memory both sight

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
