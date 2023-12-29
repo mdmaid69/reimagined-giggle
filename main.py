0f00afab08a25713cb51ba2a9523a4bf4fd6592b
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:05:04 +0000

    finger tired like pocket last

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_min(numbers):
-        return min(numbers)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
