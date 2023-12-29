c6348395fed3eaf795144c1e7ea033861a856ef3
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:12:41 +0000

    build band pleasure curious treat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
