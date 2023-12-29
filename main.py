46ba36a01012e6cedda4f6928a7f4a94aebe0791
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:00:02 +0000

    distance whisper tall be whether

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
