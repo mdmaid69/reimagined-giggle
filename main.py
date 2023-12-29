4d15aa759a04b373b811a27e7ff96d7dc9e118a5
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:57:05 +0000

    water idea scream through level

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_density(mass, volume):
+        return mass / volume
