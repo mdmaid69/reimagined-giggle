a8d3cd0c88432f5dcb442390ded03335f2972455
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:02:33 +0000

    tense frighten whether strange stone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
