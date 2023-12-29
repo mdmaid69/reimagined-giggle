2c64e4befc205041cc8d74b12d989854e90b7f82
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:57:08 +0000

    try chief as heavy children

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+text = "Hello, world!"
+print("Characters:", len(text))
