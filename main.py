ca4b884582bf31618835599c13f3fe856bdcef47
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:59:12 +0000

    crime lay Tt impatient operate

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+print("Hello, world!")
