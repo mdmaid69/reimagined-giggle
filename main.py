182af07a1795309e59c7f7df01572527d332c4fb
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:29:04 +0000

    mess runover at deaf know

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import array
+def convert_array_to_list(array):
+        return array.tolist()
