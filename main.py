8b709bea1e0f637b0e8c306e08b78c92f74bb79b
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:57:53 +0000

    in shade nature habit split

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def find_min(lst):
-        return min(lst)
+def calculate_error_function(x):
+        return math.erf(x)
