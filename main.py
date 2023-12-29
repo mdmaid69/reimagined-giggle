5e378003c2d3a3a5d09ddbe40936081b598da718
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:50:55 +0000

    task loose basis difference conduct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
