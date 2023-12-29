8300bcb62af38484b56d042f7340103cc6b30da0
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:23:55 +0000

    splash heart accident shall sister

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
