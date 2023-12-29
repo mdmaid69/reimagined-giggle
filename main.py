14ac3006b06bc3f1e984123d634bbcf8ba21c62d
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:16:34 +0000

    tend rain lower detail company

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
