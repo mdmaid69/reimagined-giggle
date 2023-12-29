59a43cf6e056ea602fc308b87b7b8652af6b592d
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:22:47 +0000

    entrance back gradually injury modern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
