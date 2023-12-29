3dffa6e75223f54e5a91e420f95570b6143f4296
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:37:01 +0000

    muscle support breakthrough bury direct

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
