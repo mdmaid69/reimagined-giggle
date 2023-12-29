395457dca0f853007c05efeaa4f6575824adfbc1
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:41:28 +0000

    burst underneath development underneath callat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
