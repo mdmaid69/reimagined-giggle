fd8dd2a2690a41c4c3900e6229a97b0b86a84605
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:00:25 +0000

    responsibility contribute shock arrangement problem

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
+def find_unique_words(sentence):
+        return set(sentence.split())
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_length(array):
+        return len(array)
