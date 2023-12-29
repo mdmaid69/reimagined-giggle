1bb60cf4528cfbcb5b8ad6c4f25681d292d99488
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:17:54 +0000

    emergency skill experience belong accept

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
