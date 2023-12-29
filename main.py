dbea89058b02db2b6d8c37014413d1169ac530bb
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:13:27 +0000

    score telephone notice stop step

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
