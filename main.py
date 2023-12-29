ef9c118e01518eec1b3843c8f76b7c133c313ca8
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:31:46 +0000

    choose wear offer why glass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
