31c8385800396e933b3d3e99c4fde4e502eee945
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:37:11 +0000

    uncertain hurry upset breath refuse

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
