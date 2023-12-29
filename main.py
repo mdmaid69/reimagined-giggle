e9a2a0a3f2eebd871097ab51272650bed50d5ac4
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:36:09 +0000

    hard excited manage train school

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
