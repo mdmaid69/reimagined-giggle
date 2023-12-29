0150103574d7d12435eb2f087df4376a4bf32c37
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:58:43 +0000

    brain correct final about run

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-from collections import Counter
-print(Counter("hello world"))
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
