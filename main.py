09c88bb07ed38e1d9fc2ebf63f3dfa1f6fdea0c0
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:39:57 +0000

    might question gradually face Cc

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
