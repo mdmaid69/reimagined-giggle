17fd873a9da5fd73eb540154ad4fe329f18535a1
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:55:56 +0000

    emphasize before keen hurry find

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
