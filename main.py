1c4047e8ddaef153cfbf9a211c70ed7fff465adc
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:05:50 +0000

    away cheap invitation high already

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
