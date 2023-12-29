c07f85a9311c505192db139fca8ef04925040963
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:22:14 +0000

    careless occasional interruption either store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def square_number(x):
+        return x**2
