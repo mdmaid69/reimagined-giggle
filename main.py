0e3ad5061f0a70caf7186af1ffbeee624c71b7ae
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:38:24 +0000

    emphasize share sky shout similar

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_work(force, distance):
+        return force * distance
+def calculate_area_rectangle(l, w):
+        return l * w
