c3093cafe1a53e33051e2e527f45d3a391e39a1d
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:42:16 +0000

    content close face mess check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
