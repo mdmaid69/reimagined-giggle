403a4b790a9ea55e9f427c6c5cdb1461beb6d428
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:34:27 +0000

    friendly some control justice go

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
