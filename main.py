f8c1301c8899f6c30caf46fc3ff180980a9691c2
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:13:15 +0000

    lucky manner use light neat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def clear_array(array):
-        array *= 0
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import collections
+def create_stack():
+        return collections.deque()
