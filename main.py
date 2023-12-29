1c51dd02e902d4ed2978aa0d9a7cf3fdde975476
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:26:59 +0000

    question variety special instruction trap

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
