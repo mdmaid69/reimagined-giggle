952301f4999b1830de776ec14296e16d62381fdb
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:45:21 +0000

    climb seat rise size up

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def square_number(x):
-        return x**2
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import datetime
+print(datetime.datetime.now())
