046962602f47d8ab2af5d5c649bf908bb319585d
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:29:10 +0000

    loose Ee win contents actual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+def get_current_time():
+        return time.time()
