a5cde2263d75b4b592eb889e77b01f8c1bc74fba
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:06:33 +0000

    appearance possession traffic want priority

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  def is_even(n):
+        return n % 2 == 0
