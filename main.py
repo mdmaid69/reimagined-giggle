99f507b6cacfe1a343c94746fe560c167a41719a
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:29:25 +0000

    shadow excited study hair rare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
