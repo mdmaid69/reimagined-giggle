424d71d613a3f7e3e96a105193c45d20265b2371
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:43:07 +0000

    opposite Tt since may tape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
