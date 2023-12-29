4942ecb2a935ca9540b2679de310990d5936159f
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:22:50 +0000

    old geton lookafter question practice

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
