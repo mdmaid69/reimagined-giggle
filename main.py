208c207bf11b79fd37c022bac55c68d0bf7d15bb
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:32:59 +0000

    smile trust glass I heat

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
