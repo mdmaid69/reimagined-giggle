c7c2feca14146f64fec4bb34dda2a01b6cc392c1
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:35:48 +0000

    file identify buy drive hand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
