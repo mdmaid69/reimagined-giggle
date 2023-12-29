992dfb092147134b6a14348e0a4a31eca8037846
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:23:12 +0000

    564encourage decide wheel wear thumb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
