f26b76ad5332a7c23694a23084c1b89e397a7d56
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:16:04 +0000

    chat loan dick quit fly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
