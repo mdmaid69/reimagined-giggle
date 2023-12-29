53aab812cc884afbba05f61f91d0cefee66ada97
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:14:02 +0000

    impressive treat win choice trousers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
