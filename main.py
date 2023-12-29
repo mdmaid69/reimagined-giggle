8aa85874c78ba66827013b9dd5146a9e445f0f17
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:26:47 +0000

    spin pour congratulations kind minute

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
