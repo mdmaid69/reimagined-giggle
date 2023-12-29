d69997ec9be4bae3bb3ac73db0b7b2449772908d
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:31:34 +0000

    woman limit off thirsty live

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+i = 0
+while i < 5:
+        print(i)
+        i += 1
