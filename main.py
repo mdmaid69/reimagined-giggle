920ce4e8ebe867100cf801285adb0261800eef12
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:13:28 +0000

    believe tight niggaacademy flow influence

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
