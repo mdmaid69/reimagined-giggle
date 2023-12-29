d3d456ff3b42b78ede5ee3d3083de42904c4bab4
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:58:36 +0000

    like female Jj means Mm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def split_path(path):
+        return os.path.split(path)
