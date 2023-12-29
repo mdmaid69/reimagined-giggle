2b341125021e4bddc190cede1333f44c4e68fa30
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:29:00 +0000

    taste alive present claim say

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
