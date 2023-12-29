ffd163c0b4ecc288403b9337803ae0c55480aec0
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:16:34 +0000

    by tape base recovery mean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_item(array, i):
+        return array[i]
