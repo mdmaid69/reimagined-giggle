a16164f1015db09827648266c0d11bf2c17486dd
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:52:09 +0000

    plain capital 571enough corner wipe

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import getpass
-def get_username():
-        return getpass.getuser()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
