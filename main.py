3db8f37d7f7338ce5180578cefc8a133a5838987
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:14:23 +0000

    now Nn check present kill

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def convert_array_to_string(array):
+        return array.tostring()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
