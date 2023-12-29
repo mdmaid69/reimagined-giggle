2043a1fc4eae37f7e5e8a60ae2810ebb65165958
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:19:15 +0000

    only dangerous crisis but young

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def count_words(sentence):
+        return len(sentence.split())
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
