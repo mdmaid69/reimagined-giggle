e15ff2c3df487d431450c337789aa7dff04879c1
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:06:37 +0000

    rid section production embarrassing consist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
