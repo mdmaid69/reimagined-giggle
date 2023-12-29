9fe04a0ccf250a91d3466ee8ce61963c646c6dc5
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:52:56 +0000

    deaf impressive forget soft education

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
