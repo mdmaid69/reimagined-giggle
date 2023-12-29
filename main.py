46f328ce2b5225ec0a1b43a597b26d5b173b35ce
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:01:16 +0000

    exam state excuse past trap

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
