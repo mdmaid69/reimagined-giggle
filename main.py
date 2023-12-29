6c38db8c68c0af757dc28e70cba2eae994727375
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:58:58 +0000

    arrive workupto cross scare wait

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
