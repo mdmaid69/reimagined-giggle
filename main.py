e022f8fbe8046268eba6065418cb258429ec805d
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:03:43 +0000

    last several make impression defend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_density(mass, volume):
+        return mass / volume
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
