1da363c51ce9d359fab9aa0ad6c08a261cfcd48e
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:13:46 +0000

    pen what love slide thanks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
