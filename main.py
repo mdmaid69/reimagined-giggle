12998c3dff6b7f0b9d87481b350dd7f175ef09aa
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:57:15 +0000

    and welcome lend still once

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
