2444b39cc75ef926e8bdd84ae8119b8c5c91f6e2
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:58:09 +0000

    possibly use emphasize tall eat

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import os
+def remove_directory(path):
+        os.rmdir(path)
