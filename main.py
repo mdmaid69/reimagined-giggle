7094583466ac9dca7befadc6e9243eed55cc2f45
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:40:12 +0000

    mood world contents once combination

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_array_to_string(array):
+        return array.tostring()
