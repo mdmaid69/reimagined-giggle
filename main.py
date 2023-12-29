dde326838eb0265a93379f53bbf48902e70ac318
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:30:37 +0000

    plastic quiet mark course entrance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
 import array
-def append_to_array(array, item):
-        array.append(item)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
