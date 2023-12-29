3c83981ab21e8629653a9136583f507e30476f4f
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:22:14 +0000

    freeze pity formal along 569enjoy

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-print([x**2 for x in range(10)])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
