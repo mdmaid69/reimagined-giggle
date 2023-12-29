223018511cc2f344aba4f85e32092c6d365dbed6
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:50:06 +0000

    talk wire careless slow after

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
