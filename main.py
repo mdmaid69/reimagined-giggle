cc1b507a8012ee96e6615c028d7cdeabc37e0152
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:10:03 +0000

    practical centimetre fair proper air

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
