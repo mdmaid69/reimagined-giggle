9db5e54026bd8177642d43971c46b3e6c0f6e938
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:23:16 +0000

    afternoon tap from meal someone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
