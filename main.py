3b0a836c680f99a182626f02dc389cba719b7fe4
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:23:08 +0000

    middle field lonely trap direct

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-print(math.pi)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
