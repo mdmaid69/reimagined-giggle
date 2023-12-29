19006a58c97fc46e0856372b610b0a617ab5b6a0
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:41:36 +0000

    frame only style related this

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
