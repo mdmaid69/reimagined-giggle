0038f3b1d2c22f147469a85b7111db3384b36f6a
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:16:46 +0000

    over have female because lock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def count_words(sentence):
+        return len(sentence.split())
