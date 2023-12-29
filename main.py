dc8c6e99160656ad70c794b42b4110053cce0dc6
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:22:52 +0000

    law by judge shout hers

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
