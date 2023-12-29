e128a5b2e8806ced6d0cef9aa7e65a9ab1744bbd
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:39:20 +0000

    town chin trust apparently rob

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
 import array
-def get_array_as_set(array):
-        return set(array)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
