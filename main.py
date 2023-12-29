e8c0afd1840d0e90deca860fbde36648f02a4bb2
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:23:18 +0000

    hurt sort majority long door

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_work(force, distance):
+        return force * distance
+def greet(name):
+        print(f"Hello, {name}!")
