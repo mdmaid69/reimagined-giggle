94663b2f995c576257ed465fce402e17e942b48d
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:16:47 +0000

    circle ball man rate hair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
