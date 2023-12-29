d47b61e6f05d10ae470586d9d490a08128722f3a
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:18:17 +0000

    fun such down them worry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
