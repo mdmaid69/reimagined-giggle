fe4a8e8698435ad0fe082a77217378575c49675b
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:36:50 +0000

    lookaround draw shall collect lean

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import collections
-def create_queue():
-        return collections.deque()
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
