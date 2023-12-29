1d1af55a64a7913e982ffa7d6619321c18ffad3e
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:16:00 +0000

    distant gentle wind wait tap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
