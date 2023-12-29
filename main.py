7c5023f2df82bfdd03f978d41c71a04d949dcf97
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:59:48 +0000

    culture dick prevent into reach

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
