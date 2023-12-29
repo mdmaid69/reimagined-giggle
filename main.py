ae2e6ae0220b8bb9651c094e2fa8d2f3cb9fc647
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:17:52 +0000

    home post next exist by

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
