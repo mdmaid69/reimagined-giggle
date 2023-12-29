bba505fa39b6b7d47f9ab82b79158f1c7c0a1f50
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:57:12 +0000

    it day number recover broken

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_as_list(array):
-        return list(array)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def calculate_area_circle(r):
+        return 3.14 * r**2
