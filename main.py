fa307e813eba06248700b0280420c59c2dd84848
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:17:53 +0000

    leg shock cut above leg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
