b76a1c47d4e4517208ab44cc2b29b1cc0078eca7
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:24:17 +0000

    notice fresh with prefer reason

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def square_number(x):
-        return x**2
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def subtract_numbers(x, y):
+        return x - y
