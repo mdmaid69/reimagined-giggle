83fe9bb8c3142278fdda3b1259fc4f7aa9528bdd
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:45:44 +0000

    grand freeze advantage prevent demonstrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
