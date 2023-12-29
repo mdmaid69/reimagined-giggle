d336b954fc0a465e222cfe74dd8f44a8d299929c
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:57:00 +0000

    breakfast through rush cut national

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
