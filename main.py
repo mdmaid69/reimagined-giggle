cba1809bcb6709b8412723e702cb883f60288794
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:16:40 +0000

    family value thanks record start

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
