18143671b5c86ed7afc9d5ce7f19e4f3e62fc773
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:45:00 +0000

    new later away sting cut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
