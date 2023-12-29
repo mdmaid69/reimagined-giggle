ab9af8408eb66735cc02259fba7fcd27ba4f2d95
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:48:51 +0000

    break yet floor walk could

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_pressure(force, area):
+        return force / area
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
