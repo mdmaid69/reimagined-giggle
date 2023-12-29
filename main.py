d98660bbb4149126af73d0906f111df82e1e97f6
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:35:15 +0000

    winter record borrow improve national

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
