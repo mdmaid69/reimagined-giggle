1f699d57c02aec97fe4a5b360c728f5db9a1f4c3
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:04:43 +0000

    tax sing trap hold trousers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
