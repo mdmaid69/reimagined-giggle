488ddf90152e974ce10417c387271ecdbda72870
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:28:29 +0000

    conclusion hurt approve talkoutof sail

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
