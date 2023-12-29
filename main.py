2c2a88ac46aca33f3f6a2ed6de64cca01292f5e0
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:13:04 +0000

    perform perhaps go sink at

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def count_elements(lst):
+        return len(lst)
+import array
+def get_array_as_repr(array):
+        return repr(array)
