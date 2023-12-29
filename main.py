cac84bf6c3ae00213b62785ed11c8f494c972e26
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:52:50 +0000

    basic ring position door argue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
