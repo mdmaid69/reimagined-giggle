01c651a65aaac9ebb69dba51a8238626a386c349
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:56:55 +0000

    stupid sore lookaround brother hear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
