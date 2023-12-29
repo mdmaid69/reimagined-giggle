98d6183a2d48e7e2cebc1b2a27fb370c99acff16
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:53:41 +0000

    description pinch marry connect accidentally

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_volume(length, width, height):
+        return length * width * height
