6bf16fb87227c4825ddded3e79c9b02f32a3557e
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:00:49 +0000

    worth heavy ours meet sad

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def delete_file(file_name):
+        os.remove(file_name)
