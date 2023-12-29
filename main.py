b5b2ffc5d6da3ca32883e3e3c234031e6fbceab4
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:02:50 +0000

    closed sound regret fail cheat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
