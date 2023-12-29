4cf0b9b55bbc87432f692076f159ff402635fef8
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:37:12 +0000

    unlucky loose effort record plant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import numpy as np
-print(np.array([1, 2, 3]))
+def cube_number(x):
+        return x**3
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
