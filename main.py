2c3eb4044f56c38a724e68d8c90f2cf94b0d8d71
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:16:36 +0000

    blue 179bite picture switch intend

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
