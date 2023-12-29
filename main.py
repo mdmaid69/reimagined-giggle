74e1d0f156465abd7d808ea61e70602d03862e4f
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:29:11 +0000

    reply change thumb convince pretend

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import tensorflow as tf
-print(tf.__version__)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
