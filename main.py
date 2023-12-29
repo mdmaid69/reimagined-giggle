c60ab73994afc329169f8acdb0d9849d3ca58606
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:13:07 +0000

    problem switch both silent getaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_union(list1, list2):
+        return set(list1) | set(list2)
