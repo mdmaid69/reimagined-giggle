3da0a57eb7e8cca1e383f83d00770c72984d2d0f
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:24:35 +0000

    buy wish worse sign necessary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def find_max(numbers):
+        return max(numbers)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
