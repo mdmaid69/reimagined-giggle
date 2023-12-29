1957267a1d60bc67929ca6a91d511e5bf4c317a9
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:08:23 +0000

    takeout dull happy wonder holdon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_even(n):
+        return n % 2 == 0
