707d5fbcf900164560ab70915c03d30f0252da98
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:55:37 +0000

    several exact squeeze ball question

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
