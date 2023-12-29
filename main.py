ad14d402aefc224d98f16249c00a20c5eb5630ac
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:57:39 +0000

    across copy guard communication child

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
 def calculate_hyperbolic_sine(x):
         return math.sinh(x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def convert_array_to_list(array):
+        return array.tolist()
