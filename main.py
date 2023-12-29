7fb4363555332d8f47f916abd6fe8e3405141af4
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:18:31 +0000

    holdout reliable pair while boil

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import tensorflow as tf
+print(tf.__version__)
