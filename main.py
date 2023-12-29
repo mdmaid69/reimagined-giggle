94514a87895231b1abde9481b65dd4bf4ecc991f
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:14:53 +0000

    police guide prevent design which

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def count_elements(lst):
+        return len(lst)
