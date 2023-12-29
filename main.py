7e47a9e3297ccf728d57a0144baf5b283e24f8b4
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:45:35 +0000

    tour round politics among love

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
