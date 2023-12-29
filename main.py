d06a769269fd124d94c3227a7098e5359b81ea0d
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:31:40 +0000

    freeze except physical flavour bin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def clear_array(array):
+        array *= 0
