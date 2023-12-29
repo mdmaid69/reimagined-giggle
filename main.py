bbd97a9c7b24966a99860d35ead3443261615c90
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:38:36 +0000

    convince communication show exact tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
