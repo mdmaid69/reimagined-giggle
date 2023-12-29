1b68ccb6234a80c6434ae414f06c2d89f8292a5f
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:24:44 +0000

    shop smile than phone account

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def get_array_as_bool(array):
+        return bool(array)
