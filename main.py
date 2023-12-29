18064a87cd9a9750f53cc2758d6f1ee981f04d97
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:46:51 +0000

    part after stone thing before

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
