c6515d07d5cd32911ae9386af286d5811c9a1fc2
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:26:17 +0000

    nowhere news shadow kind drink

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print("Hello, world!")
