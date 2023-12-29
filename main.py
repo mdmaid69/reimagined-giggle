c419328482f3f2b6e47e7a8c488eb8dcb3943253
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:54:45 +0000

    blame read related cry pause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
