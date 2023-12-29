d3ec247344fbe9e4921b44d82299be9605538cbf
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:24:09 +0000

    protest something cook estimate care

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
