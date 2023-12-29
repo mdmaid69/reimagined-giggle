1a011fb55d375186fe39a9fa07a1ed095206a1c2
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:46:16 +0000

    exciting platform set mistake corner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
