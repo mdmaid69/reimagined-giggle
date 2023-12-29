2212c2a9a4764d4e0d95408ab3bc52b59c3f9d67
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:47:23 +0000

    be about feature opposite shelter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
