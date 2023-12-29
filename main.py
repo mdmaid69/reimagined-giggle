d92cb2f98f314b21750fbe701a3640f49ac66fab
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:12:08 +0000

    test familiar recommend arrive ready

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import random
+print(random.randint(0, 100))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
