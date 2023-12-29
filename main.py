c038f0bb7ccd75e5943f82c5d1758d73fef31a8c
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:33:21 +0000

    influence tragedy or Mm they

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_user_list():
+        return collections.UserList()
