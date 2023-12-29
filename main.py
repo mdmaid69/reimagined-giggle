5c4d076b98fd5c1973240e6bb201a5c42abd266e
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:51:59 +0000

    callat travel whistle ashamed because

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
