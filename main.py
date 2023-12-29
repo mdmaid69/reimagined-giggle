1f7bdbd180ddc6fc5bd0c04a50dba447b6d91c8a
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:15:09 +0000

    yesterday perfect upstairs straight careless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
