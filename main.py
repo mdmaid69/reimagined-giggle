ee3d0f36966b31e2eaf22ea842c436f246ae10f6
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:02:49 +0000

    half less fact mouth respect

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
