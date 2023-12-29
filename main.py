296f0a14c44c5728252aae42723159971e5cc1a4
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:15:03 +0000

    noisy unlucky them sight sure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_sine(x):
-        return math.sin(x)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
