c6e1c2107872bb02d7ad00f565eeccc6fb51a5dc
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:14:06 +0000

    beneath roof course whenever drink

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
