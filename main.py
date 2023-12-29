fdea9bf1bf2f7860d63d50d40199fb4230b2a0a2
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:43:33 +0000

    either no your yet flat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  def subtract_numbers(x, y):
+        return x - y
