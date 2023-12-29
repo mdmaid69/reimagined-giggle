58d865d1dd625676c4ccd854ab5acbdb11c71732
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:20:28 +0000

    left freedom touch fall thankyou

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
