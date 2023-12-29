8bfb2b9dc097c1579ed5b28e043ea559f2e7bada
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:40:33 +0000

    generally union mark display imagine

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def is_odd(n):
-        return n % 2 != 0
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
