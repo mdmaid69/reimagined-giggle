bb80c280af5a3cb5b213d573ce7900be5a39e88b
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:30:23 +0000

    comefrom decide the equal rarely

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import tensorflow as tf
-print(tf.__version__)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
