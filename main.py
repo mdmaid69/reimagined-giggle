36c8861d4c00027ae9e83cb83a3b56f67f07e192
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:11:06 +0000

    lean blame offence car advanced

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
