35c641142021df75faaf2d94e62b45055ab65c2e
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:26:26 +0000

    aim scream connect own exactly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
