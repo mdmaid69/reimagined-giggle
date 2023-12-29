2a6ec3d0a4faf98f5e3e69015739a835d628e657
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:07:14 +0000

    climb settle threaten here top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
