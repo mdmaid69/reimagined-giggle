db7201f2bffc4286c8ef4e0d14e7795b81a04d8b
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:08:11 +0000

    broad contribute fold hers type

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def is_palindrome(s):
+        return s == s[::-1]
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
