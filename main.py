fcc377a4f95da7d572f8d1affe13851ce4d34990
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:58:28 +0000

    usually smell blind sure strike

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def delete_file(file_name):
+        os.remove(file_name)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
