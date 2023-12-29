ab4a61abca025f99c65fc1aea61599e189bd1e75
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:18:10 +0000

    suddenly crash remark correct why

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
