48273ddf04e21c2fcad32b23d52227dfd9a25a88
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:13:50 +0000

    speak trial wind neither education

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
