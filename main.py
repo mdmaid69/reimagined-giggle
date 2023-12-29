948ba921a78942031d351ce01a47518f819faddc
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:06:14 +0000

    private meeting necessary dress concern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
