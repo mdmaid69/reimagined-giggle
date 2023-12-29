5d0f7503b0bd09bfe81a6c21361d3bcf40abad1a
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:07:19 +0000

    skirt violence gather mad surprising

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
