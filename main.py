fda66826abf2ffa9085b8efcdbe65d8f9bd5da94
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:41:42 +0000

    scene talkinto demand callout worse

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_perpetuity(payment, rate):
+        return payment / rate
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_str(array):
+        return str(array)
