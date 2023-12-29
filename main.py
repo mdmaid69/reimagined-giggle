5be1d64bd3623410a82f3259b643a535ff02bbdb
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:09:09 +0000

    speed meet easy appeal responsibility

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
