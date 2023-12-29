12c996cbac17d9b89168e92597902c4230bbf73d
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:53:08 +0000

    Ff free intelligent last anyone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
