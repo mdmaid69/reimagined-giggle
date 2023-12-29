c7671c1b75a02e3a952c18d0e763e820169ca48f
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:43:37 +0000

    lookaround ball right several yes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import tensorflow as tf
-print(tf.__version__)
+  def delete_file(file_name):
+        os.remove(file_name)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
