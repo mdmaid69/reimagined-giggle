2d565bd6403a4aa921f486b7a66b1699fa3560b2
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:05:36 +0000

    beyond heavy turninto recent family

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
