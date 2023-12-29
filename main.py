654bfe2cfe9b5edfc7c003ea98843d34caef5c12
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:24:02 +0000

    in appeal take over worth

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  def subtract_numbers(x, y):
-        return x - y
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
