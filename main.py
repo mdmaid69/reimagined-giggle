c13d78acee6d0c47439651f536b252f540430bcf
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:31:57 +0000

    we chin compare bet intelligent

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+name = "Python"
+print("Hello,", name)
