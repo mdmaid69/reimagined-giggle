a6f5c96181ab6c8a8035c1dccef4f071e55d3315
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:56:51 +0000

    feeling squeeze trip meal confuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
