e10e3fb09a77603d31cf3bed60fe2e953e5d6380
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:17:14 +0000

    should unkind drive description all

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def greet(name):
+        print(f"Hello, {name}!")
