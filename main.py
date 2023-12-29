401719f7c64a7156160089b60d63c40e635e5f41
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:02:17 +0000

    regret blue class tongue damage

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
