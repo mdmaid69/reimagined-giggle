961e0428d3a106880f3572143a510a9a7ecbcd98
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:28:23 +0000

    hurry otherwise broad part body

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
