48fb168f94f20b352a5129127d47695eca85ff2b
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:34:39 +0000

    pretty point regular climb pollution

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
