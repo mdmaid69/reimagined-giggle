962c4f91b8e17afe96af2d217086339e8303f5d5
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:14:20 +0000

    sure go comein from cold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import platform
+def get_os_info():
+        return platform.uname()
+def count_characters(sentence):
+        return len(sentence)
