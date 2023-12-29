89401ac6b3c7afccfb872babedd1051c210d042d
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:47:41 +0000

    come subject tongue desperate perform

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
