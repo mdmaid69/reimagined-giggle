3076597ca823651ad95ac25131e3a055192674b3
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:28:53 +0000

    play transparent community talk refuse

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
