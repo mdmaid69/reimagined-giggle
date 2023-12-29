b667f39efdea5f094bb77c9b714c8566ddc43273
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:24:12 +0000

    unlikely otherwise Ee curious spot

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
+with open("some.csv", "w", newline="") as f:
         writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import collections
+def create_user_list():
+        return collections.UserList()
