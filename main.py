fcbcd56a030e22061d65a7637db41652387ecda8
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:58:54 +0000

    voice rest basic block doubt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
