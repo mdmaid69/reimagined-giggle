f215c492e81255e60d9e3ac79e9b0cd17a634eea
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:05:13 +0000

    reach transport drop stare suit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
