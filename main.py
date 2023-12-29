d9aaa2f6ae0974406e525db66fd204dec16816a5
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:23:16 +0000

    shut top dare sorry curious

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+print(sum(range(10)))
