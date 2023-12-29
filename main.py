09199dee384c7be4a5f5d91ece845419fe04d010
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:36:10 +0000

    influence practise aware centre Gg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import getpass
+def get_username():
+        return getpass.getuser()
