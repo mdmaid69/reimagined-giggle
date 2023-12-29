1d166a03cc636abfe97aa864934673507279e77b
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:00:58 +0000

    music ready spring ring sign

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
