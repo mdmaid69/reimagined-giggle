e430ed0667e12bf681827ec99df7129831281207
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:21:52 +0000

    rush murder she unit cheap

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
