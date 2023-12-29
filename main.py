1cce8374242e54538b3f21d0de4be24865a9eb82
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:03:12 +0000

    evidence glass after instant breathe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_max(lst):
-        return max(lst)
+def convert_to_octal(n):
+        return oct(n)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
