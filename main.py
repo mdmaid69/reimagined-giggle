4ff88cb916ace2f695e7bf72954c80e8e65f9ad4
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:05:45 +0000

    communicate badly awake dark crisis

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
