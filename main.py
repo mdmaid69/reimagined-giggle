5aacfe1358b774710aea7655c8b3ec2e93d4c93f
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:31:37 +0000

    teach floor better chin below

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import sys
+def exit_program():
+        sys.exit()
+def find_min(lst):
+        return min(lst)
