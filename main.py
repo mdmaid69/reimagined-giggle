ce7408344accd270d316ba2b46372f4086f4b6b4
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:12:18 +0000

    interruption score holiday reduce himself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
