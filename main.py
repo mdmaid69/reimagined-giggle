1f5cfa871653ac1dfe86bdcc147b5e7abb5a25da
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:23:34 +0000

    church goback(to) suit roof determined

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_as_float(array):
+        return float(array[0])
