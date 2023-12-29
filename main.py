119100c2adc2305262f9ddee9153911bfe9b3744
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:03:47 +0000

    black out middle talk goodbye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Uppercase:", text.upper())
