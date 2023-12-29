976cce5bb95a07a3f8cabf827d62f8107f7a6f78
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:04:14 +0000

    smile relative neat approval thorough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import array
+def reverse_array(array):
+        array.reverse()
+import os
+print(os.getcwd())
