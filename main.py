52a52264d6191ab114317b449c8becd87b080877
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:59:08 +0000

    giveout bring pretty leg deep

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def calculate_speed(distance, time):
-        return distance / time
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
