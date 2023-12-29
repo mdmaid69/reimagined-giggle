c2a940b296a8709b7f98105467dcabd5b8f2d993
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:47:32 +0000

    damp power bar belief immediate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def clear_array(array):
-        array *= 0
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def find_max(lst):
+        return max(lst)
