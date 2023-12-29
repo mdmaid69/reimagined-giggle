1cabcd042d390b65519f8c3c439455c6d6e9bc8e
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:13:41 +0000

    motion bang dare science man

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
