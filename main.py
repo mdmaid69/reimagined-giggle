20fbdcabb8216cc39db5c216e1c4338de24a987a
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:10:44 +0000

    callat difficult satisfactory regret accidental

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
