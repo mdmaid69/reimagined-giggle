f88e70c29316bb59655c6e47654a0cee2e4c71b5
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:46:28 +0000

    term comment serious environment rush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import time
+print(time.time())
   import os
-  def split_path(path):
-        return os.path.split(path)
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
