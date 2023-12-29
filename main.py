3e3a1e3d398ea200444da05931a441f82a948c57
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:09:17 +0000

    uniform same work influence live

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-print([x**2 for x in range(10)])
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
