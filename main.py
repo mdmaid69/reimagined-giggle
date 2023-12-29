5b52eb707d0a9bf1e41c2bc7d9bc221a636bfdf8
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:02:03 +0000

    unkind great then fork style

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_area_circle(r):
+        return 3.14 * r**2
