9258a3a740a6cb6d709dd3974e0272e14b5bc541
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:01:00 +0000

    clothes 565end legal fire takeoff

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def reverse_string(s):
+        return s[::-1]
