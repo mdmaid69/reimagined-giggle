1426322b95914bea5c76b864c15093aca8886708
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:05:48 +0000

    conduct become drawing responsibility night

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
