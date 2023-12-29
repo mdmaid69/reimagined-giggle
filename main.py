7219ea184ad52e755ad6d167e8927b823fe9bab4
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:26:17 +0000

    shame date fall several about

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
