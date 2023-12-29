7a5c120a10bd073fdab34857c98f675b630fc317
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:58:12 +0000

    legal great exactly same alone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
