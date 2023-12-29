85f80ba9ba8a93023d9f7fc72a9f602f9791b787
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:49:40 +0000

    where performance blow ilovedicks gentle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def calculate_density(mass, volume):
+        return mass / volume
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
