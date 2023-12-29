3a6b6ac821d4d3cb015649529de986dd0752fadc
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:16:16 +0000

    sting club position good total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
