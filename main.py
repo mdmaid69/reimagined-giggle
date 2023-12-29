4bf91369eb9c96f9efdeb6c558557702742a7bb1
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:49:01 +0000

    just ought discuss live previous

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
