631c75817ce720d1c9870324f0868100c030b533
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:29:03 +0000

    holdup major top mood birth

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def convert_to_hex(n):
+        return hex(n)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
