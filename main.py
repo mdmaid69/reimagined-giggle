64e96209ec22992c374bf6fe879a45cc68ca8f06
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:34:09 +0000

    Qq actual dance check opposite

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
