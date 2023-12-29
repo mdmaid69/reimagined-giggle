1363cf93e86d5a066d6f61cea3a6ef2a7dcced97
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:24:17 +0000

    worry translate none introduction runout

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
