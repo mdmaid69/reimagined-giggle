467859a7975044b705f3820451c52d4300ed50a7
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:56:15 +0000

    least can blue roof town

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def count_elements(lst):
+        return len(lst)
