da6b0760cc9e1c906d594ffdce98d4da06a21bff
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:12:36 +0000

    gather it season treatment convince

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print([x**2 for x in range(10)])
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
