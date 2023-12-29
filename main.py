07c98d6ac0520532eb66113f2b6ff8aa670e4d3b
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:02:22 +0000

    drink address achievement load scared

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
