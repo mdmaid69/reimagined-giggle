63df10c3898b657009a60a1966249fdb61efe881
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:46:49 +0000

    our breakdown leave himself many

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def square_number(x):
-        return x**2
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def count_characters(sentence):
+        return len(sentence)
