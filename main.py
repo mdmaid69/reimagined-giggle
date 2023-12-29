c6c02af67d3c6bf840e4709f79b936ee912caec1
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:10:48 +0000

    poor dare regard must history

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
