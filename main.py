37032584838cf295127c787f8a5d5a7e03e465d8
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:31:57 +0000

    very last base good than

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
