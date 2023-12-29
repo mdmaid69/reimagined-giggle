524720b95e45b5d3605081ab9823313c3498456a
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:44:01 +0000

    whatever exploration tie all include

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
