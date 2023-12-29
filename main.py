10f48d136f0b6ed220946c5c417ab70d8b3aade9
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:17:44 +0000

    murder herself greeting ring terrible

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+print(os.getcwd())
