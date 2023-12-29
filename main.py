16bc45e7d5721e61af2baf0304c0b7cf33b75537
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:00:16 +0000

    drop either size hurt narrow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_list(array):
+        return list(array)
