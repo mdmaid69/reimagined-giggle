20500907e57bf239dd7ff2bee99bec2c4e8e7edb
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:11:17 +0000

    usual spot callat actually welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
