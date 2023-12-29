b32fbd1e46030348eb733e5cbbf83f1b2dbcad14
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:56:37 +0000

    serve let practice excuse question

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
