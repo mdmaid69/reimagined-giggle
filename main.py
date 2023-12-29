d9cba93b9835f7aa6fe75584fdfdd172346c5bd7
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:37:38 +0000

    deal water attract lesson interview

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
