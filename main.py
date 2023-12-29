96ca5a6abd8616006892b2b937814f45c94da277
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:18:03 +0000

    thinkthrough mine each play object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_acceleration(speed, time):
+        return speed / time
