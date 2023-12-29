ef8df24b2b5f2fb6b7141496596024181b559f2c
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:53:05 +0000

    correct thought favour proof lost

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-def square_number(x):
-        return x**2
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
