830722a1cb97a21325bf0f794b5d71c53f26c65e
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:39:49 +0000

    path world control threaten unable

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def get_array_item(array, i):
+        return array[i]
