8d43a50f73fea1bf1cfb01a569f1a32307d37553
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:11:43 +0000

    although flood above conduct anywhere

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_speed(distance, time):
+        return distance / time
