a2a8a383220fb52dacb46d80316c43a8323ebd23
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:34:03 +0000

    train respect possible punishment sign

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
