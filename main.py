a9fd58f70cd799d0cda1bf741d9647cd5c802610
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:08:21 +0000

    borrow Bb worse joke strict

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
