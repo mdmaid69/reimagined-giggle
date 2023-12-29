1a2243aa07fb670d5974b76957781edc58f2ff35
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:17:52 +0000

    surprising education still fortunately refer

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
