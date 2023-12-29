fe0850d41584ade3cf992d53b2f04f2cc55b9d3d
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:46:17 +0000

    accident should tone conclusion near

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-def count_words(sentence):
-        return len(sentence.split())
-print([x**2 for x in range(10)])
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
