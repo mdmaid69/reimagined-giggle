d8b4f07ef535df7c24311b102d2c0398a8b95bb0
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:13:13 +0000

    pay ahead suddenly distance violent

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
