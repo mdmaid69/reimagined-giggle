eb4f067eedbfb11a63aa08cb18c5fbd7e483b5bb
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:09:09 +0000

    thankyou like patient previous talkback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def sort_list(lst):
-        return sorted(lst)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
