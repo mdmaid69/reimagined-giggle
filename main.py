3bba5e49561495784ffe61f29068b4791a63c315
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:06:17 +0000

    left mess tap cheque point

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def convert_to_hex(n):
+        return hex(n)
