b51e8f692b48b6cceb06e216539d0fe5b6fa1053
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:52:16 +0000

    ashamed forward rescue issue geton

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
