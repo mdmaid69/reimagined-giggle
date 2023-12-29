e719fef284401688efcd4c51af7f894408cbe3d7
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:58:15 +0000

    underneath quarter performance beyond principle

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
