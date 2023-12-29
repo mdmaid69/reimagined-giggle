8a2701eda40f912369393e9b73f4f6c468f455a4
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:11:51 +0000

    along since problem key label

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
