41f185d732e377bed3c3140c60fdeba45c830f80
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:47:31 +0000

    rest work post some important

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
