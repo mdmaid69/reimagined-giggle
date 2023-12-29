bdd77e0085a86f12a93de3bd1c9605e76d2635fb
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:04:09 +0000

    black love wander against dangerous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
