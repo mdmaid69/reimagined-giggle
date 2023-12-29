ac999168f0988a40af29d8084ff6ed8f072242c5
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:51:30 +0000

    past spare private emphasize truth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import array
+def get_array_length(array):
+        return len(array)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
