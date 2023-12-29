e4a5b6343db93cb51f1b3e6aec1e10da4aafceef
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:22:49 +0000

    must below cut explain building

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
