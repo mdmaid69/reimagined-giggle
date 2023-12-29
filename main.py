7fd9da0c471ab64645b7d8973835a4e2cf892a16
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:55:06 +0000

    steal bother plant swim fresh

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import numpy as np
-print(np.array([1, 2, 3]))
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
