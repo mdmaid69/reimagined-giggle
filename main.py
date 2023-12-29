8ab5411faeb8eba71f19fa121f8f756ce08c2a5a
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:12:16 +0000

    mend sure under bush worried

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
