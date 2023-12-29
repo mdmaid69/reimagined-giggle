840c35266629a44ff17e8eee48b9d9035fe5fb0a
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:08:26 +0000

    autumn major software behaviour mean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
