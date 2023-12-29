ef51623546cd42244a3b82605a1576f61dc8a6c0
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:31:54 +0000

    cheque gift clue exchange feeling

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
