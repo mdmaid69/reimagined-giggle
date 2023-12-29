e4147a00c6dca7c1f99d97aaaf11fc572dbfd540
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:41:20 +0000

    snow touch touch use scheme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
