bbd25c6aed02e6c8015f1b16406009f42a7b4970
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:17:06 +0000

    sting money spot fat season

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
+  def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+print(math.pi)
