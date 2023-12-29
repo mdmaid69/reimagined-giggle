47b43b3a94c5df1c9363a577c41f20e71cc4ba97
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:07:38 +0000

    free illness order collection funny

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
