6ee4093abc032084e134ae22d7b827705c413e34
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:09:26 +0000

    reduce steal matter base prefer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
