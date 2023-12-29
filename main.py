25a63345ab8f05aa75bb5bdd166d31cf72ee714f
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:48:11 +0000

    whatever bored tired worse welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
