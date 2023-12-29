d66d4ced2c28eaecb7f985e5f3fc103c368d129e
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:40:39 +0000

    small maintain 179bite breakin ride

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
 import array
 def get_bytes_from_array(array):
         return array.tobytes()
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
