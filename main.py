b4de5c5f10100c224e46ac6f0be49e4f0071d8c2
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:53:25 +0000

    vanish base put turninto trick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
