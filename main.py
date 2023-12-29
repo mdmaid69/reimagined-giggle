1cf8a001e73d14a1ca6256006b54fb354207d042
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:16:58 +0000

    somehow just mistake bringback post

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+import sys
+def exit_program():
+        sys.exit()
