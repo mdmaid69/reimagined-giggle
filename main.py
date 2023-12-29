ed4aa63aef6746319bfba4da47c8d47bc3848982
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:55:18 +0000

    nose if listenfor drawing children

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
