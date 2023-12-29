8994bfb651bee4ebd57b2637ee5a59592ab2f371
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:45:18 +0000

    on level foreign many unlucky

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
