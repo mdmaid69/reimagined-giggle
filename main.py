0d80ff407fa2bd40217a5e817e316b79e999c628
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:42:05 +0000

    getaway relax comefrom lot rather

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
