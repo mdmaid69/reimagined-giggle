e45c6790b6578b1460719a20196a646dbf85b5d0
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:28:08 +0000

    part dear please popular table

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
