ce207d835136ad2fb4439734249653a0bd497838
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:01:24 +0000

    turndown disagree contact write emphasis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
