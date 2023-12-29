59f712c35ba1e5a793d313d5ba7cbe6d28c1c408
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:27:01 +0000

    description train stop through wet

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+for i in range(10): print(i)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
