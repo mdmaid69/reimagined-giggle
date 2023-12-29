40d5b04df7eeeb4e65465828fd079b3a00f76281
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:36:46 +0000

    base imagination possession score well

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def square_number(x):
+        return x**2
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
