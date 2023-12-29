e61010d20e7376b40bb27c510105275eb1faa234
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:42:55 +0000

    separate tip separate crowd building

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
