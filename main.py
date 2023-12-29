4649c960c24efa33a3b8e2521aeaf529025246bd
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:08:07 +0000

    hers over occur responsible nonsense

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
