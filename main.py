1a8e23d2474ccb7842d54d3d3156763faa850ec3
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:33:20 +0000

    kick generally thankyou beat shop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
