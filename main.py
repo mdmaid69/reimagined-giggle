ecfc8433a04157cfa5676e1247d4352d3f4da260
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:27:23 +0000

    inquire aware business say far

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
