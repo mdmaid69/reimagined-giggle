c8199d8d010543d980a86f824b0a94da7e9a5df1
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:28:28 +0000

    discover all at rush insist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import random
+def roll_die():
+        return random.randint(1, 6)
