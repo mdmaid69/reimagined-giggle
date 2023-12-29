1b3e51820c50fb68ed9b965e82ff0fb8e026b0fc
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:26:52 +0000

    powerful support win opposite woman

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
