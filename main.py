cffae941d6d0358f527684dcbbcc6481bbd41818
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:36:57 +0000

    church course sometimes maximum approval

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
