ce9c279e1aa1056bd8c7248d6bb075299c4481ea
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:00:14 +0000

    trap proud sharp exit judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
