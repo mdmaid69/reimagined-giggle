dd9ef11fa70a58e593c04e219d7b0e0962c5f972
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:00:39 +0000

    frequent produce family Yy mark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_set(array):
+        return set(array)
