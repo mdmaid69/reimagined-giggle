bb25fb2f35431161c21a77aaa4ed178ffab7c689
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:01:10 +0000

    dear fashion outside machine touch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  def is_odd(n):
+        return n % 2 != 0
