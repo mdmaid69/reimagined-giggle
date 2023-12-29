db797a0d6e565e88e59464b7f68350914ab41f80
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:45:21 +0000

    murder temperature makeinto fight authority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def cube_number(x):
+        return x**3
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
