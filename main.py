21d1e7c28de422f2966076692cb12773a4353106
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:46:55 +0000

    stop convincing mixture division separate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
