493627fad8f90a1cb05bd96be3591282d2ded26a
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:02:48 +0000

    arrange disturb strike sweet support

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
