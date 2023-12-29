7f4b8a2ecdbda2e516581af336177926874c4d64
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:45:39 +0000

    own grade ring accident appeal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
