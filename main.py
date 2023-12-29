71cf3c2a52de1153b95f8c8bb3320b4c76a65c00
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:10:16 +0000

    rain definite prepare comein alive

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
