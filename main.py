60d5a77dacc1995ebe95d11ff0f7f15d165d66f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:30:39 +0000

    regular march turnover with knowledge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_float(array):
+        return float(array[0])
