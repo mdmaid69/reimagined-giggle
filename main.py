953c059c728d6460879348e092e97f06ea8783cd
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:25:20 +0000

    little somewhere teach language fast

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
