838b6e4d871ecd34b74038142b3c97a3838fb72e
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:43:15 +0000

    calloff afternoon tradition organization aware

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
