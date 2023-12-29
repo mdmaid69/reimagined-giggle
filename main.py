28cb82df89986108396586969341d4d1f8fc25d0
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:27:45 +0000

    load value plenty choose friend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
