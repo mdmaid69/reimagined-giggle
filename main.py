54ffc3f0ad7fe49f97125092dbc7b7d2eb051ddb
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:14:47 +0000

    live love urgent means exciting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
