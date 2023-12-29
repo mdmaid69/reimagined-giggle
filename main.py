8307ee1b8c34b82b07fc57c979f4369a3547c9e3
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:18:12 +0000

    language holdon custom square occasion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
