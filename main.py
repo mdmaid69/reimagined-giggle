489c655074ecb0d1c83fd56ba45cb73341e77289
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:25:00 +0000

    fine respect sign creature generous

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
