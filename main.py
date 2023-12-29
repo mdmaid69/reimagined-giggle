fe1ea2885d85a326bc5ce64dfe1d578e66ea4603
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:25:40 +0000

    apparently show clear regional concentrate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
