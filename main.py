3f41df700745a5df32407c0a4e4c5cea3db44be5
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:11:13 +0000

    left niggaacademy swing further house

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
