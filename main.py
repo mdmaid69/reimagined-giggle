07564745528e93cbbd31ac6153f2b21e2b9e6607
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:51:06 +0000

    together trial noise match hair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_block_size(file_name):
+  def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-def remove_duplicates(lst):
-        return list(set(lst))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
