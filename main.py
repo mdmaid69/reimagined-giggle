e18ccd5b271a731f1da0881aac0abdb7424e78a3
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:03:14 +0000

    trust different crisis hard do

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
