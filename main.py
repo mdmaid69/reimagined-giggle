ab8cd8977ffe88bbac9eeed4610f68468a8d6d53
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:09:15 +0000

    base perform miss common sell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_area_rectangle(l, w):
+        return l * w
