6ece8b5ac76aed6b85346eefb6daac4fe3505b35
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:46:49 +0000

    Uu class makeinto visit rescue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def remove_duplicates(lst):
+        return list(set(lst))
