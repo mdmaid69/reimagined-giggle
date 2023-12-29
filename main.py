943c357e5db5b37598d2ad9f9f361ff13ab13c69
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:17:00 +0000

    immediately concern recently instant take

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
