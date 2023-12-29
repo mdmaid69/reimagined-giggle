f33476b4e7edce50e18bef535abdba46d6dbaac0
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:22:50 +0000

    figure coat run neither mine

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
