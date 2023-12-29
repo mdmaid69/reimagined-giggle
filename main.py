cc54b92964bc91c4c3959bcf8eabbdc15e3ce667
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:48:36 +0000

    vanish mad complete contribute skin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
