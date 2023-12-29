0b6318a9b692149ba2a539659cf930da2f80a174
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:48:42 +0000

    pay agreement long lesson trade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_area(radius):
+        return 3.14 * radius * radius
