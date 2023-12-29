dca7e265703fe70bc2c4d183c92204a8812fc10e
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:43:57 +0000

    tune confident work result like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
