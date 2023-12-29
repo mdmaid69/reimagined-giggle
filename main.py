39bc3791b44c8bacd5d2655ea79830bb7d5eb75e
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:47:39 +0000

    plastic needle exam become translate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def square_number(x):
+        return x**2
+def divide_numbers(x, y):
+        return x / y
