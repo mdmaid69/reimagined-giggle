2247dbfc1306872c4b0f6fdad3e5aff1174aafd9
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:34:05 +0000

    rank steep acceptable practical must

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def cube_number(x):
-        return x**3
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import time
+def get_current_time():
+        return time.ctime()
