88309c82d70adf842849f33226e8e4c6869614be
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:22:30 +0000

    treat importance her calm zero

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
