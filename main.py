885ec6890fab2fd0828f9c8f66a46abf389e3d2d
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:34:54 +0000

    platform continue risk rub already

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
