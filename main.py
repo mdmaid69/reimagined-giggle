dfb133f42d230a2b9f8addc43600223a8c0c0f85
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:45:50 +0000

    loud often everybody never breakfast

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
