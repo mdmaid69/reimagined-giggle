fe4735ace7fb9a8c4933bdec409e4225b8cd50f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:28:31 +0000

    wherever reserve blow spin share

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def get_string_from_array(array):
+        return array.tobytes()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
