b4799437f88fecc5eec80c09716cbba4ed55977c
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:31:11 +0000

    unlikely tooth work skirt juice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
