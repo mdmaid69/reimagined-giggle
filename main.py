c905eb60bb5835e001b5df4d43b5d036f335bb41
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:32:14 +0000

    born whenever way available apparently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_min(lst):
+        return min(lst)
