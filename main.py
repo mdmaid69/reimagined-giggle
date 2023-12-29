1335533d22281f3df84848bf232c3a5cc70e5164
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:56:42 +0000

    sale fresh interview foot until

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import collections
-def create_queue():
-        return collections.deque()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
