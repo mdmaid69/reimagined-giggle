0d90fe269d9b8b0ea51a3a27eb4305c4b77bbaaf
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:54:45 +0000

    here vanish mistake type concern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
