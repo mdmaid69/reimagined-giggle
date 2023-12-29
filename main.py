782ca847cfaf52361030e62255b82208688ec54e
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:49:57 +0000

    wave vote let freedom truth

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
