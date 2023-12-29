654214822810028a212fe5ad30a75c6acf86b2ba
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:05:46 +0000

    start impossible lookafter fix front

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
