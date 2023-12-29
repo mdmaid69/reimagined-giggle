372830f5b625e21650fb83bce58b27ffc822ca45
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:04:45 +0000

    spend worst normal breath gradually

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
