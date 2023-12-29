c7eb9e89247d2a36008c80a1491c62ecac5458c7
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:25:49 +0000

    bunch thought protection manner breakout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 n = 10
 print("Square numbers:", [x**2 for x in range(n)])
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
