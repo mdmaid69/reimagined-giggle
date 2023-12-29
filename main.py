56bee2f69be08ebf13a51a96c93bd50b2a3b5788
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:38:59 +0000

    right might left true weak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
