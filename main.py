de0e7bd868a8f616e21218fa83f7a7d67665104e
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:20:39 +0000

    dark month mine ball talkback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
