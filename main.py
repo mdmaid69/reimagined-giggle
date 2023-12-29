52157212a8a10c7b3b06589e7501ef751f006dce
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:06:13 +0000

    fear hard figure sign keepout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
