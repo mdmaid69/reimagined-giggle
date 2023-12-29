d1cf5c2226b803228f2dcb60a97975ce8c1a70a4
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:13:06 +0000

    lead brain promise frighten limit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def is_odd(n):
+        return n % 2 != 0
