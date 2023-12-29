661bdf1637046feed6a7b78979a25916f4e7e2aa
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:43:30 +0000

    storm false hit basis cover

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+for i in range(5):
+        print(i)
