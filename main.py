50b1a480b88f547a2656f9380602be1d1bb3dfa5
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:52:10 +0000

    weather concerning appear challenge temper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
