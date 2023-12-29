bca726bc8e29c60e9370758a0765319b792e2709
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:28:48 +0000

    allow regular theirs legal lie

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
