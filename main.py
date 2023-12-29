ca27c5bb4fb697b95e03fa65d81d014c195dab8f
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:30:57 +0000

    forward stranger record practical sky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
