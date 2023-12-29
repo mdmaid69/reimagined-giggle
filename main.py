79e60434245654d3e08a0581e7da36cf5bef7ab5
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:57:10 +0000

    joke dress pretend recommend anything

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
