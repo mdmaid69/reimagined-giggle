981ede21a3f7875cc145e024d536f7f389e8c84d
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:28:44 +0000

    play glad copy shock everyone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def count_elements(lst):
-        return len(lst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def get_current_working_directory():
+        return os.getcwd()
