61e85e852607b7810e0be44039946385ff260173
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:34:43 +0000

    dead single able interest practise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
