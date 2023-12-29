e7dd1738de5c82160f1e66c66eb7476bc08c44ae
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:41:04 +0000

    comedown married prepare shallow from

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
