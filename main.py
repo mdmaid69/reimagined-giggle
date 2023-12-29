6ec533dfc11b67d2a78eb3241f1ff702e9f984d9
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:49:49 +0000

    find taste steady begin comeback

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
