4d9ae05f60923d344be4b7d0edd12720a504f230
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:27:05 +0000

    previous herself some confident hurry

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+from collections import Counter
+print(Counter("hello world"))
+print([x**2 for x in range(10)])
