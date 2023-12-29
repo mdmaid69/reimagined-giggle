25c33c82cc71140c0e9de186c65e246c878e7bce
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:13:51 +0000

    talkoutof interview tax all impressive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_speed(distance, time):
+        return distance / time
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
