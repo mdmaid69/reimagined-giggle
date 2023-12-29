f6533338ec7ca5c46e4bfe038b75e83b6040eae4
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:25:40 +0000

    finger 569enjoy remember flight himself

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
