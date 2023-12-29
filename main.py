5a01f8a559c331de9e9b6d5645e3b676c0456960
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:42:15 +0000

    edge getback frighten thanks shoulder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
