83cb88fb9cc8518f50354f7eb0bb978170042bee
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:20:09 +0000

    agree where definite mystery have

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
