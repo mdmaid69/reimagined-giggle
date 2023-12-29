a4c21914dda1d91534f1bd0c21359dd1d4ac9517
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:41:30 +0000

    workout door lock shock figure

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
