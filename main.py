9f29cf02280e249661d64b65da22a9e7447c1bc9
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:02:19 +0000

    embarrassed reasonable furniture tall apparently

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def convert_to_hex(n):
+        return hex(n)
