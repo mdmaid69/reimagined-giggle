18790f3eae64bcaf905cfaa7b08f1998de62d81e
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:41:26 +0000

    meanwhile land authority shine university

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
