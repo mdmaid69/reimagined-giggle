14fb2f722980573bb3fb6829541899abf4c5342d
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:55:26 +0000

    unfair result fear turn set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def remove_from_array(array, item):
+        array.remove(item)
