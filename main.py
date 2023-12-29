722b5e1e42ca4545484bddf850c3af41c495ca45
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:05:06 +0000

    cost foot communicate slice wide

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+text = "Hello, world!"
+print("Uppercase:", text.upper())
