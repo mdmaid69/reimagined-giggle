3f632041c6582d0e5f7c97e20f4a6390aad4d0af
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:22:12 +0000

    character knowledge similar crazy quarter

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
