c1aa778a3069fb92dae6bf47fe0ca8d5fbdea3bc
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:59:10 +0000

    fine bus dig power paper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
