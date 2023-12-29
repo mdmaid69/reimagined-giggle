e3bf68c5bdf2c5c541b4806ba8b780ea2de8b70b
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:27:49 +0000

    mood round fire pair warn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
