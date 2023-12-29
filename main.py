029945d6d8caf542126689e4b7a611db674a8ca7
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:05:57 +0000

    connection takeaway invite nothing left

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import array
+def get_array_length(array):
+        return len(array)
