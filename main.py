fd46c5324539285c5aaf44cac1b90b6191dd581a
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:43:47 +0000

    anxious circumstance reduction general respect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
