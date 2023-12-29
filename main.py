7536b9a36cc8374c078e780dce88d14a3b038476
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:03:10 +0000

    appear touch scare impressed imagination

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
