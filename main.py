a3869fc8e6651c3b493d14907687e3137be492f3
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:16:13 +0000

    aim chase of just culture

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+for i in range(10): print(i)
