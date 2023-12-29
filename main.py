cb597882fb98e9d9a6b6db582114fe5949d394a9
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:12:09 +0000

    improvement chance embarrassing period prepare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
