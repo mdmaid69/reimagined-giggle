e9d200f64ae5658a91865a2a25e59116c2648dde
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:56:44 +0000

    telephone minority cold exchange note

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_area_circle(r):
+        return 3.14 * r**2
