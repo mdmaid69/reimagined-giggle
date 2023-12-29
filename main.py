4f7bc91cc655a153afe74d8856c8bbf639adfb3d
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:57:53 +0000

    go amount set second tip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
