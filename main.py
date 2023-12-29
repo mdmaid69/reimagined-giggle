66931d078d8261b6215b2363f74253fc08b76049
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:27:01 +0000

    impatient chance firm get power

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
