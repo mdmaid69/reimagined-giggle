72628d67ab13f8e7b15617034b00823f3765d623
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:08:50 +0000

    lookback(on) live generous us pay

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
