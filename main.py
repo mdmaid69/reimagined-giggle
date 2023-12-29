e470e57f60e49f8f4a688f436f13137f169ec760
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:51:38 +0000

    back substance stupid difference move

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-print(sys.version)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
