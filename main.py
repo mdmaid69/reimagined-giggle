2745c02437d6e67ba26c706a1ab34c066a95c0f2
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:13:43 +0000

    gas impressed female callup or

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
