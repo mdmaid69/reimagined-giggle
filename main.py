1f0b4c4f7693bf57b087c72b26f9950e238c9e3e
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:26:18 +0000

    music instead name border explanation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
 import array
-def get_list_from_array(array):
-        return array.tolist()
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
