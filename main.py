754fd8ca3251a63de9975fcdde2e6900f7d70aa2
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:15:05 +0000

    quantity finally touch sensible tense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
