85242ed2e39c729f8748552a09524aea9ffaf74f
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:24:51 +0000

    glad person create female dull

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def delete_file(file_name):
+        os.remove(file_name)
