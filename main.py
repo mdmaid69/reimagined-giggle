58bcb8b3c24fa3685c5bca14a6a16d52e678705d
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:46:19 +0000

    production various woman lookafter ball

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
