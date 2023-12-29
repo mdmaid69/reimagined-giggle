e5aa06f880973296b0bf485367ac7d78e5311e4c
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:51:53 +0000

    introduce recent ask congratulate unless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
