b9940b453232e18c445df4622453427667b44c68
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:26:02 +0000

    juice complete left flow drive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
