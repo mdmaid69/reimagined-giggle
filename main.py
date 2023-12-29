95503a0e25d8c78a0e86e7e5de4972c0d45b2a70
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:49:57 +0000

    national once comeout late drop

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
