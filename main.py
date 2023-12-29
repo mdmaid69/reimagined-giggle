3fb3aca33abf44ae087206c7defb904a01fb2be1
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:38:00 +0000

    someone attach load rob breakin

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_force(mass, acceleration):
+        return mass * acceleration
