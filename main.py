3dfd005600ec66b66df881e75974228f0320a4e1
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:25:32 +0000

    listenfor strange unit advantage further

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
