f24e9dd853903dd7195d70bf96181225682ba53a
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:11:02 +0000

    clever callback hungry wire extremely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def remove_from_array(array, item):
+        array.remove(item)
