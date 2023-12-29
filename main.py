019298708681b6a2f7c753c81ead5a6272a63ebc
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:54:54 +0000

    dark rest negative both stupid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
