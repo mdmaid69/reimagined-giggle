f0a7d89eb474164a08458b08d98316ac4cf791a0
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:27:44 +0000

    mixture really contents bang soft

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def find_max(lst):
+        return max(lst)
