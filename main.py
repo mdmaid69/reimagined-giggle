a91b7c1dcecd15596b173a3831a16b01bdf4c075
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:02:46 +0000

    famous object buy thick before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
