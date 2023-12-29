ab049c6108248664c4e7cb21d5cb8ce1181c4a5f
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:21:01 +0000

    arrive lookup keepoff cheque introduce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
 import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def get_array_item(array, i):
+        return array[i]
