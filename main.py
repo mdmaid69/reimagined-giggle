3619a9e1efb8bf56cfb59309b01e4e32c6e1960c
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:20:11 +0000

    search across on fat defeat

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
