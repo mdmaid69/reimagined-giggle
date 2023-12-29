185e897da0c7e7df73a54b1ff27304dab1544d54
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:17:24 +0000

    force fast immediately dull turninto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
