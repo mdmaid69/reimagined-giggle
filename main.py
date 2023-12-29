eaa704e49857b3ab728fe559bfcc041290e70125
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:08:17 +0000

    rush off part motion impatient

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
