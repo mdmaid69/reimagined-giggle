1dd15793c79efb7ab07732762d825b821e5f6728
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:43:11 +0000

    praise send begin blow separate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
