b1818bd4f3b09163977ca96b6c5b0582f6b34147
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:15:05 +0000

    already discussion claim urgent frequent

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
