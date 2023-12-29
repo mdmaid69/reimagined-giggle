ecaf194f26605e21b31c9ad8acf819ca2bb5db0f
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:47:25 +0000

    lost ocean contents someone chief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_index(array, item):
+        return array.index(item)
+import sys
+print(sys.version)
