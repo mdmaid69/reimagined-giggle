36918efb56f427ec6cd5921d3e0533d24990198f
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:33:20 +0000

    ordinary attempt serious nose friendly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
