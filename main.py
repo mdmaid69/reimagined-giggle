932bd45799b97ce1698b3d6eab831ba4d13d7d72
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:55:28 +0000

    past our laugh pump pattern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
