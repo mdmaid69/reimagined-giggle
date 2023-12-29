9efd320cf37f3b56b5b59c66f37650706262c4d5
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:07:04 +0000

    production rather branch contrast Itself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
