7a838f4998ae7fd52be909a533c761104ddeec08
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:51:34 +0000

    dream minority my sense bite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
