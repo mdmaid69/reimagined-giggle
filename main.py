1d6ab40c2843323e3b1faa872a0e6375b63657bc
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:38:34 +0000

    base fact set occasion tone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
