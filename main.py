4a722be97488baf81344f33ee5278f4478b3efc4
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:55:42 +0000

    different hold operate quick expression

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  def calculate_area_rectangle(l, w):
-        return l * w
+import sys
+def print_python_version():
+        print(sys.version)
+def add_numbers(a, b):
+        return a + b
