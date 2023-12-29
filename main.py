69830b06b3577340ad517f3a2e3e9e0fb5e2cf46
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:59:14 +0000

    getout go confusing exchange drive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
