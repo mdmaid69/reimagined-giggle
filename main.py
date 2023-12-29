b3a134f9d0c4f6918b130ab06311229e34eeb356
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:07:53 +0000

    shape further underneath effect against

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
