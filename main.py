232835ede877c1c2e7f8d5b7a5e42a17ccc7ab48
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:32:49 +0000

    fall shall image centre steady

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import sys
-def print_python_version():
-        print(sys.version)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
