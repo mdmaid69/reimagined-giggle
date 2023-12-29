40d39ae027ce10860e7f69523e9cc906fcd3c3f0
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:38:49 +0000

    shrink spring behind further risk

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-def is_palindrome(s):
-        return s == s[::-1]
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
