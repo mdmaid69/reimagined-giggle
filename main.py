e6810d2ee2b5e27c3ebac5074c0f8b0f41d4e1ef
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:22:56 +0000

    actually capital false accidentally away

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
 import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
