738db739fef64aefd1c1848191fd671a6f75442c
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:56:44 +0000

    settle excited church crime summer

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def convert_to_binary(n):
-        return bin(n)
-def calculate_pressure(force, area):
-        return force / area
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
