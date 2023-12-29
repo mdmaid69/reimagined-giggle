14aac2ee562e323a2ef06de086163cf166037ddc
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:08:03 +0000

    official replace finger exact community

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
