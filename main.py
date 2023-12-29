38a3748ff4186f046eeb329e8099c0896e35f658
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:18:22 +0000

    save build promise whoever lesson

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import time
+print(time.time())
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
