15151d338f6146025d794a39fe9845663f81930e
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:00:30 +0000

    delicate picture goout square call

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
