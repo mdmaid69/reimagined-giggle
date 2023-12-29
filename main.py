cbb39e21aee755877fc17af65872b625dfb0b5df
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:39:47 +0000

    meaning sad protest apologizeapologise bye

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def divide_numbers(x, y):
-        return x / y
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
