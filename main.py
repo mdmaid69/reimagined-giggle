16307c2d17557318dd9994358873720489c21d49
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:07:38 +0000

    ticket example row my old

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def reverse_list(lst):
+        return lst[::-1]
