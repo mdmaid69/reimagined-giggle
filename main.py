0a929143790572f3941aa861deb8e157f6cb4ba4
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:28:15 +0000

    ticket weigh aware broken yes

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
