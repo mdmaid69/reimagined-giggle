d476fc86f18d5c1c662a3cc01df2c2a4fcbb3715
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:15:55 +0000

    reserve influence late be force

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-print(sum(range(10)))
+def is_odd(n):
+        return n % 2 != 0
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
