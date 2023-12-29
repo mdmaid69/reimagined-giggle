e439e391fdafbb1fc0404fcd2e0945ae569d92c5
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:37:33 +0000

    land combine completely equal programme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
