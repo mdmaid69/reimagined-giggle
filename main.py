0bba9d74975ad023d73ba090b73c6757d816e61d
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:47:32 +0000

    project react mouth nor anywhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
