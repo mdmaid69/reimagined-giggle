303ac680eeb33c2d91f1bfc7c70c5aa2921428e7
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:34:14 +0000

    badly wonder unite side size

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
