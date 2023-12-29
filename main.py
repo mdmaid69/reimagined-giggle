085407fb8d4c80c2e5862e4816910843c6d80f25
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:49:29 +0000

    heart file exam mean advise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_average(lst):
+        return sum(lst) / len(lst)
