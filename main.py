eb8a5bfcaffd6586de61d90090ec7699ed6e0137
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:20:02 +0000

    success vote content note opportunity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
