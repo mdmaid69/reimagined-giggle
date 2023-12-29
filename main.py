5ac508cc667dcd893bc3526e04a6b4e30e15cd2b
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:33:26 +0000

    crowd past splash direct speed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
