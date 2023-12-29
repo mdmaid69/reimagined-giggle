c71099810658d4f53d06054d0dde11f946f88495
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:23:25 +0000

    apparently steam recent rate section

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+i = 0
+while i < 5:
+        print(i)
+        i += 1
