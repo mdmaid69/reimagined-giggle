f0e74deeecca1cfabddaba291915491ffd821f95
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:14:43 +0000

    hurt fix upstairs your apologizeapologise

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def cube_number(x):
-        return x**3
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
