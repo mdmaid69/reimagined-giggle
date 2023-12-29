86e6e076c10fee4f17496b7468dc48ecb63e6160
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:49:17 +0000

    exaggerate dear last case book

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def cube_number(x):
-        return x**3
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
