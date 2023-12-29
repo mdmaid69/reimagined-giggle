9a47de963719e47e20a0bc4a4f58cb9d40edda9e
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:26:03 +0000

    rid sign feed could play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def is_odd(n):
+        return n % 2 != 0
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
