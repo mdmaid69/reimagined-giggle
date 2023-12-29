3231a2205ed8fd4987c768359f8c4cecae51b32d
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:47:07 +0000

    separate lookback(on) pure snow exchange

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import collections
-def create_counter():
-        return collections.Counter()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
