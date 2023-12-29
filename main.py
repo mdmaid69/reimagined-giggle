001ab214a1d1345e2c5c79c3c8946345b96fff9b
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:48:07 +0000

    trade other strange unusual another

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
