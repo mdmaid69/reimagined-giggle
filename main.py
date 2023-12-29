073bfb1bbd5aa5990363122a88743d6f1c1dd780
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:32:18 +0000

    drive serious purpose exit television

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-name = "Python"
-print("Hello,", name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
