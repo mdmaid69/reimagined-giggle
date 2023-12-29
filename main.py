c5a5eda0bb4e5f6013767192d9976d19320c4a13
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:40:09 +0000

    shame bunch job in their

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
