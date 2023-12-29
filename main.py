78faf7dfbaa2dbcd31a20278f3426363ca56e56e
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:11:10 +0000

    weigh order apart repair sharp

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
