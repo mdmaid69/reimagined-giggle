5666d278696623ba8683f9f556c3d3c914885203
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:12:52 +0000

    like each song trick example

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_density(mass, volume):
+        return mass / volume
