74bded4ef7cb6ca5bcce3bdef667615bf20aa121
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:36:09 +0000

    typical aim noise divide admit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
