c7df37f5b250740e0d98f7d4926e62f04a3d0e49
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:55:23 +0000

    border any disgusting last slowly

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
