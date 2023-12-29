19cc80693473b5ff90fa827c8237c41f2a42f43f
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:27:49 +0000

    get colour nowhere early big

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_speed(distance, time):
+        return distance / time
