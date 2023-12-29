2c9291a27858a17760712df4bc965360d266945f
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:36:02 +0000

    last everywhere real image track

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
