bf13650a1001660c8fcd52337798acf6dd11d879
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:26:21 +0000

    interrupt because scare just with

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def count_elements(lst):
+        return len(lst)
