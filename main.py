9c3a9f313c00d65f2f9f0b81ab6420e5ebbfb04f
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:55:42 +0000

    music while anxious choice discover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def is_odd(n):
+        return n % 2 != 0
