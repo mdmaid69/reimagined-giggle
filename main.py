70defbee6af56bc35e621dc0f888a5fbd8056716
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:56:24 +0000

    sure goup objection touch idea

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def remove_duplicates(lst):
+        return list(set(lst))
+  def subtract_numbers(x, y):
+        return x - y
