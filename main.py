cc0edf66f70df8643ead8055804c8add147ff393
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:29:54 +0000

    violent police noisy Gg rain

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
