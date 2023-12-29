4cd9331d691d75f7974ab0465bb05e7982b4853a
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:02:33 +0000

    blame smile plain plant form

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def reverse_list(lst):
+        return lst[::-1]
