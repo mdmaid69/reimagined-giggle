68e42a6f0a052ad6c4e3278f6245cb7f9241a972
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:40:07 +0000

    innocent law weak chief environment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import os
+print(os.getcwd())
