1e126f435aae993e262b2b4e706fb07c607a3742
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:15:48 +0000

    development should class ride Ff

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
