b790b6a895e3ba4a89d3a83d88edef8a588641ec
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:55:22 +0000

    stir value anyone perhaps peaceful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
