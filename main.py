8a97302400aa22b7cebc467df72b1925f8a2611b
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:17:12 +0000

    lookback(on) square getoff control allow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
