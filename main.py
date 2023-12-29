dfc29827d858892fbad3f85861e0171ac50a7ff7
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:18:19 +0000

    acceptable national quantity cost yes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
