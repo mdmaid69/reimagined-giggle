3344aac51a8ee03801b2d2bac69384c77fdc37f3
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:47:09 +0000

    lookafter apparently emphasize previous breathe

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
