b6813f68d137dc23b1cfa6d0796499cb06715e61
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:57:35 +0000

    tie deaf gap urgent comeover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
