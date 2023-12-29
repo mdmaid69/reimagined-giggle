c2930ba1e7133a060011a39a29e7e9d5d598efdc
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:07:28 +0000

    compare press mysterious recovery takeaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
