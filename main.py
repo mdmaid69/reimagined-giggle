a0f9c44a437382dbdc066901110d5af4e6a8bbf6
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:00:07 +0000

    crisis consider method wet idea

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def convert_array_to_list(array):
+        return array.tolist()
