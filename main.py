74a8027317e31abc5988b5a6866fa35f6ae04dba
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:38:04 +0000

    count declaration inquire evidence touch

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
