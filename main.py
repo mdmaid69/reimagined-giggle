da2f661f89d72c163b9dd7b209953f06eb6b6703
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:28:32 +0000

    science plastic sound succeed makeinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_density(mass, volume):
+        return mass / volume
