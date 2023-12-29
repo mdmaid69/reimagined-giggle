e17a252dbb11196f63b32d3597ee1509922c9ff1
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:47:11 +0000

    error opinion disturbing laugh importance

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import math
 def calculate_power(base, exponent):
         return math.pow(base, exponent)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
