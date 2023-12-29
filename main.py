7818b6f5362a43bae9c718d12fc214563580f6ba
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:49:53 +0000

    bright explain but push picture

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import tensorflow as tf
-print(tf.__version__)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
