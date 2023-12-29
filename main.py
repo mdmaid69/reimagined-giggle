6d87e42e465728c1062fb59a6b46a8df1fd5ce6c
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:23:15 +0000

    tough advice perform regular stranger

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
