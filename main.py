9896f5faeabaab017ed3e385e79dbb2bd6c49cb6
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:00:20 +0000

    block delicate interview weigh board

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
