1e5ba7b6ef56408df41a76a2d20916e7f87018d0
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:11:35 +0000

    comeup if press turndown loud

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import sys
+def print_python_version():
+        print(sys.version)
+import numpy as np
+print(np.array([1, 2, 3]))
