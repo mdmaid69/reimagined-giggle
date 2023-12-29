be2fbc5a286d2f54910d62cd69669a28cd86ea2b
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:44:23 +0000

    better plan wet country burn

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
