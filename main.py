0993ea35b2270e5b051210113860e59d86ec04de
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:32:58 +0000

    by tidy whistle comeover little

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
