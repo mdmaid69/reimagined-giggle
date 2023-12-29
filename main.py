b8c8369e4ac3415efedb7b5ce78091b0cddc6ce6
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:22:32 +0000

    walk accept weigh lend score

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def find_unique_words(sentence):
+        return set(sentence.split())
+def convert_to_hex(n):
+        return hex(n)
