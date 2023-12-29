0bae252263c56f23b29e03404b377886b82bb044
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:38:34 +0000

    grand regret go weigh accommodation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def count_words(sentence):
-        return len(sentence.split())
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
