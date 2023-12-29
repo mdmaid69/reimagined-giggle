4c74e9c5797ac97a75f9eccd8d393d8c78046325
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:51:28 +0000

    combination whom kind get force

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import array
+def get_array_length(array):
+        return len(array)
