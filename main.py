452c2b7ed58aad8097529646302d4416abfc33cf
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:45:29 +0000

    worst opponent plan shut exploration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-print(os.getcwd())
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
