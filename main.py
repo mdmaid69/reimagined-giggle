f7985a559b6ee669856b8f07d5bc315215d193a3
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:43:05 +0000

    whenever comfort thinkof book mark

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+print([x**2 for x in range(10)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
