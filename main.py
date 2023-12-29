a34cc8731cf0966b62b441100b661c47dd53d009
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:22:23 +0000

    excuse process congratulate ilovedicks bin

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
