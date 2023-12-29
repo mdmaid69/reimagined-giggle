1558d7e36f8b7683023ddfd0f6f43e7f2445f632
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:14:46 +0000

    present meaning unpleasant surprising proof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
