fcb1a74ac27fb6250d075b6a9765779fedd2ab80
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:54:31 +0000

    book all room speak there

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+print(sum(range(10)))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
