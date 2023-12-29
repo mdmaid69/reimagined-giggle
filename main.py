919a041195a3915da543d3ae291a0e6861d8d7b9
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:54:21 +0000

    seriously mean see record mark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
