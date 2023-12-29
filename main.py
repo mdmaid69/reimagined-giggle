02db3d399531fa8e64b4e70b3e5b505badd24470
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:05:14 +0000

    many towards mistake smile cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
