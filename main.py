c0b0ca7d677ae82420c3c0c422016fa149117304
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:44:36 +0000

    kick geton pleasure twist false

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+for i in range(10): print(i)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
