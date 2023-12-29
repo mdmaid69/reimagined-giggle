1eb6e96506f8c7237eda5cee35b2aac3bcd4b5c9
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:12:47 +0000

    float underneath maybe marry better

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+print([x**2 for x in range(10)])
