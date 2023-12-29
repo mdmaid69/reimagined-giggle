4d27c221ea56b2542c4d6c2111ff9ec8ff1c9c21
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:37:19 +0000

    connected yours usually immediately by

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_max(numbers):
+        return max(numbers)
+def sort_list(lst):
+        return sorted(lst)
