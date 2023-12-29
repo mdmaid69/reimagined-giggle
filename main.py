14ec734a2816bc6d854cb9da84390a60f08c9759
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:14:10 +0000

    shock quit decision keen label

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
