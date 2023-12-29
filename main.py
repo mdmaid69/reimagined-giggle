4b94400111608108af02efab0cdacb58a16e6292
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:02:31 +0000

    quit fear apart change display

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def split_path(path):
+        return os.path.split(path)
