8c02bc72922512275aa78ad1594fdfa50235cdba
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:34:14 +0000

    priority agreement sticky behind pity

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
 text = "Hello, world!"
-print("Characters:", len(text))
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+print("Words:", len(text.split()))
