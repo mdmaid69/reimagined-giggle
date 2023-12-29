3dfaa3ee804710c048bb0fac44fa9e72e1e7e8f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:07:58 +0000

    kill performance effect dream muscle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
