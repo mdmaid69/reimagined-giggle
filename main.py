5df904384bf7f4f207bd4b4179e627f416b142b5
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:33:02 +0000

    cap cheque charge week shock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
