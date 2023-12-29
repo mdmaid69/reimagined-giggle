59239f14f2b51a6a1ed454a57f8c4bab98d26975
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:39:04 +0000

    if first congratulate become operation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  def convert_to_octal(n):
+        return oct(n)
