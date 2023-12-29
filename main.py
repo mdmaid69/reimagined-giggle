27652e79943843c206ab487aacbbbef3ac3d6e43
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:28:50 +0000

    kill sweep artificial drive reaction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
