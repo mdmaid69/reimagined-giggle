b581a5dc479d49f037dc9d2bf3abc5102d3e4b42
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:11:03 +0000

    head wish slow pressure mistake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import sys
+def print_python_version():
+        print(sys.version)
