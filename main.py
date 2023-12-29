3a1521284297386d891eb65d37c3dc748b8c4854
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:34:33 +0000

    proof fear usually powerful confused

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def divide_numbers(x, y):
-        return x / y
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
