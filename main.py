fb2f1af61ce8ac2ce1b3fead92698879530a74c3
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:45:06 +0000

    share someone love lookafter charge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
