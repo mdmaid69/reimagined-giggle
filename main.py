495a3a1593dfc9261e01f467cd4064e4b66be683
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:30:48 +0000

    affect suckme exercise afraid once

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
