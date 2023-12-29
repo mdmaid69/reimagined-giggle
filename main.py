3b51c14273c9deb6ad854fb490922ea55770921e
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:13:09 +0000

    indeed study juice dear target

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
