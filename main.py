90aac53711a41bbc48d664f67b9178d5fdc47500
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:55:16 +0000

    several deaf snow light pollution

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
