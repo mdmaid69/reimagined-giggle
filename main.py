44cbaa8fc27868b5baf53f728765ad973b93558c
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:18:21 +0000

    cause chat makeup wind art

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_pressure(force, area):
+        return force / area
