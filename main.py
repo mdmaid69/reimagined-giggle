4a9cfc5df80b92a8663ebcbd9a470a238fdd87b3
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:38:35 +0000

    goout command why ashamed reply

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
