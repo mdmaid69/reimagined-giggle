4c0b81dba126d45bc115a50a680508eec1f5a528
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:54:13 +0000

    previous sleep concerned simple shadow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
