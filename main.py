19277c7a9fee1bca978675ec75debfaa73ae7f8a
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:57:39 +0000

    sky stamp straight need relationship

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_hex(n):
-        return hex(n)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
