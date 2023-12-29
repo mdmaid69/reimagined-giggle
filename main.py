aab834131b5db8b70ba840e7fa30c387c0949493
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:55:07 +0000

    during protect language news environment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
