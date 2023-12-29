1cccc0b1e65fa2f59a52ea643d6a23a163e2e24d
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:38:27 +0000

    chief birth slow destroy organization

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def sort_list(lst):
+        return sorted(lst)
