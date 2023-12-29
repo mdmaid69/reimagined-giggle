be33099b873f5eeb26cbc2f16cfda4695e761e8e
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:26:01 +0000

    stiff second call correction worst

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
