78e88c7bc06d1b39bb56078f083c462f13530690
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:09:43 +0000

    after material kind feed straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_repr(array):
+        return repr(array)
