ae91afe9b4e16befba4008ce4568a0230ea73a22
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:56:01 +0000

    sick bite like runaway miss

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  def multiply_numbers(x, y):
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def multiply_numbers(x, y):
         return x * y
