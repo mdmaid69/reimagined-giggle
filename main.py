f1b7dd52add84b76cdb965b509a13710867d61f6
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:28:28 +0000

    collection or sea print own

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
