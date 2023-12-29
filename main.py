cff381bbe163e1466a3535739e9e660585473156
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:41:36 +0000

    near snow call below since

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
