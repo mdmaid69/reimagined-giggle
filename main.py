e84b26e9b2ae168de714218eee1d2130d16bf2f4
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:28:03 +0000

    drawing open instruction slight either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def find_max(numbers):
+        return max(numbers)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
