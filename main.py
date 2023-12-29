db399b3565b6983e36d4ad4be9418b105f99cbec
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:13:02 +0000

    respect attempt wherever claim whistle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
