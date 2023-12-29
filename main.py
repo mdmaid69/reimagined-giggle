38d307753b944e41cb4409cb350da4d6cecc95e1
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:53:15 +0000

    dare neat world first answer

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def clear_array(array):
+        array *= 0
