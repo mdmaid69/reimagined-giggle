663b69c2e20362c7b63167d353a6b4fe06c0db0c
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:51:19 +0000

    exaggerate boil justice needle frightened

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
