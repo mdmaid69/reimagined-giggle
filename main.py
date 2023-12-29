38a8d257adae3a9af8370acf9bbb9344d354173d
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:47:58 +0000

    watch increase to laugh description

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
