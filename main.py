0b5b19a73852cd540900794cb69f9871679acc72
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:51:58 +0000

    cruel class trick bye minimum

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def convert_to_hex(n):
-        return hex(n)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
