5840478b49943a99a2087067434e768c6c52a092
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:20:09 +0000

    obvious pretty common pour culture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def reverse_string(s):
-        return s[::-1]
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
