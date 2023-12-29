ed506987fbbb4bdd0d1a792b8ad103a3ab47fa17
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:56:59 +0000

    practice back combination convincing hurt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
