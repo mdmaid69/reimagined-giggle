c51a68a1b9b139d867a5011a49930e10e1e3c8df
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:30:09 +0000

    back wealth identify hand back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
