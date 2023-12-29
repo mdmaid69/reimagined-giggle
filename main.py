04b65a35798c9a3a1f98dba93129fcdd0dd7c2f9
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:49:34 +0000

    watch involve spread description damage

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
