3ee6bcfa959d35e1595db72833e009eb505e329b
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:32:21 +0000

    willing hair rid advise concerned

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
