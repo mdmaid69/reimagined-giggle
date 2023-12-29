849ace730353741a04ae54672ee00b2542e588be
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:03:57 +0000

    music emphasize kind consist start

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import getpass
+def get_username():
+        return getpass.getuser()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
