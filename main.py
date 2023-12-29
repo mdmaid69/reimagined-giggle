54e4ef41b7114608e2ec8395d4f77d2952288fad
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:09:39 +0000

    post debt run mainly breakin

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
