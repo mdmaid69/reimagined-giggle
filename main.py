8bde55d38e4c9fe26c085b80a6f54d3bc904e441
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:38:55 +0000

    sweep score bend job may

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
