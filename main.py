4798153bbeae23252e768454645d5b6543e8256f
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:51:38 +0000

    ilovedicks address snow start visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
