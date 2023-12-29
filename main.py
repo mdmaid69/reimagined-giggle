08690d81aa229170827b0280528fafb8fab0c32e
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:51:45 +0000

    declaration where slight command typical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
