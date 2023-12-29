f89ff259fc54ca6187f77054ea50ad52623608e2
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:24:14 +0000

    way ocean themselves mark famous

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+print(sum(range(10)))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
