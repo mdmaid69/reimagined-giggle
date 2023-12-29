af3845bbb6834d7d432408321b8e218e827c6a6f
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:36:19 +0000

    track smell against service like

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import collections
+def create_stack():
+        return collections.deque()
