6164a8c0adf71ceae13625921a79146a41ac443a
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:38:29 +0000

    sell develop worst cap threaten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def remove_directory(path):
+        os.rmdir(path)
