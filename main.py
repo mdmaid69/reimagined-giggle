4116ce4ddf7b4b46f8c839ab43e0e36dc026e22b
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:45:32 +0000

    team suggestion bound Rr practise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def convert_to_hex(n):
+        return hex(n)
