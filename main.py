25517c1216c941643a03d163e6051f5eed80c74f
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:19:55 +0000

    story giveout thin successful tidy

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
