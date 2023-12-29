5bf668aa6b7091de06089b717f9ad2a1ffbcf165
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:52:00 +0000

    plenty past trouble match supply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
