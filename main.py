e085e1f8ddc2f0336a8b07be93fb9967247be1a4
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:38:13 +0000

    make nowhere nice centre game

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
