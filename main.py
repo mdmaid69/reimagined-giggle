563fd2a5f7a42667c2501fc325fd7c8d4d1a2dd7
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:03:46 +0000

    band wear throughout copy praise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
