be448bf034d6164b8b25bed4cc335f1269a63851
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:44:28 +0000

    press many bend conduct evidence

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print(sum(range(10)))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
