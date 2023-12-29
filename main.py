77849a51a772ec8b74fc48bb30c9ee2449adadce
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:55:23 +0000

    night accidentally distant present tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_user_string():
-        return collections.UserString()
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
