d838e9bf7cdbca9999865de988ffccc4d26e1208
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:08:01 +0000

    soon raw would favourite 568energy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def subtract_numbers(x, y):
-        return x - y
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  def is_odd(n):
+        return n % 2 != 0
