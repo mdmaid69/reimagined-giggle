72867b2afcdd7be2f5d5ba75034a5129284ed5bb
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:45:04 +0000

    shape lock hair brown clean

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
