d4450e260ba2e51a7a5dd190a6f7257d227e52e0
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:21:23 +0000

    they care might feeling at

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def calculate_density(mass, volume):
+        return mass / volume
