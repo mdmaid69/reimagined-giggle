6bf94d60a67b72cb091468948803e29bc98624fa
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:39:15 +0000

    intelligent sorry yesterday talkoutof familiar

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
