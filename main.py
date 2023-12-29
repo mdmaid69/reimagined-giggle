3f54d07086d277979a725eeff5e16a034a4a07de
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:24:26 +0000

    turnback emergency choice almost formal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def convert_to_octal(n):
+        return oct(n)
