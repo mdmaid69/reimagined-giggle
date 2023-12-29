ba6a01eddd45ef9af8f293b8d42b17a3c14841bc
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:49:37 +0000

    quiet complaint level minimum afraid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
