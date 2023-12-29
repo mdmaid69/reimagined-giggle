14470d4c58f6024c3a53e965ebef00ec6dc8b7b5
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:57:51 +0000

    television stranger create summer advantage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import json
+def convert_to_json(data):
+        return json.dumps(data)
