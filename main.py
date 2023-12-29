c271aec9806a4275487995d01588d8953fb7cdb6
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:50:48 +0000

    towards bridge container familiar once

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import numpy as np
-print(np.array([1, 2, 3]))
+def remove_duplicates(lst):
+        return list(set(lst))
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
