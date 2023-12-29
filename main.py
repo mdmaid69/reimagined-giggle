88540f6f9383f2b71a7389afb7db4d3e93e01678
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:09:57 +0000

    study hope goback(to) stop perfect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  def cube_number(x):
+        return x**3
