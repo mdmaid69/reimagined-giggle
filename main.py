df1751b391846cb579677cf8f2e313eaa63c7fe9
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:59:23 +0000

    value gather only blue burn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_current_working_directory():
+        return os.getcwd()
