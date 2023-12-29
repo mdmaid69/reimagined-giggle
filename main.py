c0272aa62977ec1a8ffd7367eecd987759234125
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:05:23 +0000

    promise shrink he count summer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_work(force, distance):
+        return force * distance
