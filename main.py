3f7d8e48032c53889a222520da1ec5094c224628
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:54:39 +0000

    heavy lookback(on) blind television window

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-print(sum(range(10)))
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
