b118666784c0fcd763c354b6dfb42d792002bd10
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:55:59 +0000

    comeout good stone harm national

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
