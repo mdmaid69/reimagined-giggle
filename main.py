99dccf4982d1c25ba1066a3e6b824fb672984b27
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:09:11 +0000

    lookback(on) identify light main cold

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import sys
+  def get_python_version():
+        return sys.version
