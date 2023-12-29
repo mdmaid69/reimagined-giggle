aac5d1c9b8ea481d890664ecbe64789aa6bbad77
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:09:32 +0000

    forget escape later attempt broad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
