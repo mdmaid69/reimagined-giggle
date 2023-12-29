eed99b201ccfcb5be25cad42dc6e2a7dc0a761e9
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:54:11 +0000

    hot practise pleasure material noise

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  def square_number(x):
+        return x**2
