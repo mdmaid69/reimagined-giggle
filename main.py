7c67fe81884845cc21568c88c90807f5fc2bbcc5
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:07:12 +0000

    education worse shut object brave

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
