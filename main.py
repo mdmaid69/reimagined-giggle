fe9c429ec207fbf800a958bda10832e13bc42dbc
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:42:14 +0000

    change tip now dish understand

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def multiply_numbers(x, y):
-        return x * y
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
