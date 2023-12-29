6a53598ef69723575d6153fb9770c170cbdff6d4
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:20:31 +0000

    exhibition breath Vv forward design

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def convert_to_hex(n):
-        return hex(n)
+import os
+print(os.getcwd())
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
