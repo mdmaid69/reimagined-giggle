d7b5d6ced78e5572c3277bbedd26658ed26a90a9
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:58:44 +0000

    other until wrong practice into

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
