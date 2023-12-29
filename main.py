e35b9fbc5cadb3722b5c4812a205ac085a085d14
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:10:58 +0000

    equal between boil culture stranger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
