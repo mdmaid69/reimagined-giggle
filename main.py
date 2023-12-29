cf707a11b0d444aa456eb0842ab5d22d132f3b68
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:30:25 +0000

    lower sleep agree shine concerning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
 import time
 def get_current_time():
         return time.time()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
