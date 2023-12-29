5431834a46d32f2d628ac59c2b9d637f36e8f25b
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:21:38 +0000

    appearance exercise improve even foot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
