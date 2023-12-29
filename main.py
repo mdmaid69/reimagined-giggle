4ee2ec69f5c28a1bfec909a4d7cd2416687b039d
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:22:08 +0000

    interesting prevent command guess strange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
