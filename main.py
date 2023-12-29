74f4ed0f43a5d70999c6b361731528d285112c08
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:06:29 +0000

    roof comeover feeling partner open

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
