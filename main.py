dbb68a3dc7014b9162b3c33f8c93643f50041682
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:50:52 +0000

    further sound pause drink human

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
+  def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
