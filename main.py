8487ce5d8dc1c5bcd9d969014e5966c6fedd5749
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:42:31 +0000

    deep right night disagree impressive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
