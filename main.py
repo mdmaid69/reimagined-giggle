3e81fcfed72ffcead628816db16ea3809c53e896
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:41:26 +0000

    rub although either regularly afraid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
