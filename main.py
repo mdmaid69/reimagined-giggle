adfb85664bde3673a11d8f738c1e95fe0fa22b16
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:28:52 +0000

    stranger true suit murder no

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
