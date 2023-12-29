289199ae79c5e8bac08ecd1fb05d94674848864b
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:38:52 +0000

    century feel balance correct visit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import sys
+def exit_program():
+        sys.exit()
+def find_unique_words(sentence):
+        return set(sentence.split())
