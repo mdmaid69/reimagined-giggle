041548cfbe6e42975bbd1845007f95af54132e1a
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:43:24 +0000

    567enemy content round major confuse

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+for i in range(10): print(i)
