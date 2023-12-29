49312db0dc9375a6a8949a199b68a555ba0a52f5
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:13:07 +0000

    card between vision the although

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_typecode(array):
+        return array.typecode
