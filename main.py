834be150a745e0c541c0073845bc6b9854d3cc41
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:23:40 +0000

    rarely certain bang away dangerous

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_index(array, item):
+        return array.index(item)
