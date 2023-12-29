4807aed801749ddbaefe6cc9d86daff6471a04f3
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:32:00 +0000

    underneath sensible ear hers thinkof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
