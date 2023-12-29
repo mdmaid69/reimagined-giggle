563c7d1b723828ed3010098a9d55ed1d97978724
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:05:52 +0000

    platform rid measure phone still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+import math
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
