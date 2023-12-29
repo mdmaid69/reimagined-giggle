f389782f92e52b2fcd9b0e9ed9667d05108bbac7
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:13:00 +0000

    whether cruel crash no breakthrough

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
