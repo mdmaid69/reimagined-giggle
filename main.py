e4cad96991adf08acef4b1d48e48cc93ae1e06c9
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:53:31 +0000

    wish certainly year sad work

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def convert_to_octal(n):
+        return oct(n)
