0251577b9ef34d26d8e9bb04a2c2468d20db53ed
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:02:55 +0000

    concerning sting beginning talk battery

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-print(math.pi)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
