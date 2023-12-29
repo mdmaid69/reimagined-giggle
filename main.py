baec04faeb7df5b7f338012346a3c5f4cedcc21d
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:44:16 +0000

    hear next plain pipe date

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
