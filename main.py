1029e923d94a16ad2954de9a778ddfb7b7fcd26a
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:42:17 +0000

    report blind calmly turn left

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import random
+print(random.randint(0, 100))
