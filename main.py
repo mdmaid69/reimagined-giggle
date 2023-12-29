1af5e9dccbb6644435c3193de0bd5213ea674904
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:25:12 +0000

    protection busy luck spot watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
