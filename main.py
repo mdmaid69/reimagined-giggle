b61b131ff0a40b1924472ff99efe4d11e43ae7c4
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:23:22 +0000

    noone available responsibility flow whether

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def count_words(sentence):
+        return len(sentence.split())
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
