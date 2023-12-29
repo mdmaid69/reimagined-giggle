7e58a7edc0b7e9e6f396709763cb4af3f0354d25
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:27:30 +0000

    be when sense flight swallow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
