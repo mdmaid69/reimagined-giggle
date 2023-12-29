184c6dcc1334fae4fcdf556e51dedf500e099c22
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:41:32 +0000

    beside convinced block conversation operate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
