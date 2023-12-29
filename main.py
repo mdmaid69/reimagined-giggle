4bc50287b52621b899a913cc1ab78538e59bdb7b
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:32:22 +0000

    sister force complicated round right

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
