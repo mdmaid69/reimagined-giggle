2881d4174c55e0406423a79b11d60cad965561b5
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:50:35 +0000

    culture treat Oo relief move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import platform
+def get_python_version():
+        return platform.python_version()
