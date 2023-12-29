ec875c4ff07c077cb9e451b126b99bb3565ab57c
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:06:21 +0000

    weak you funny extraordinary wire

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
