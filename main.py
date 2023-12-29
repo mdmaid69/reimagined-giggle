6c4331f3c45a2ae84b4cb9aa76b23a105df81cc1
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:16:22 +0000

    middle speed out intelligent early

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def is_odd(n):
+        return n % 2 != 0
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
