0b317790727d29195af3a812ebaaae8bfa7ea9f9
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:40:35 +0000

    quantity apply pair sweep problem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
