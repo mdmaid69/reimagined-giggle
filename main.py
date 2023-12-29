434f7356785ba8bdc3a2921df70fc9a77c56dc16
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:38:50 +0000

    there subject tip look sound

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def is_odd(n):
+        return n % 2 != 0
+def convert_to_octal(n):
+        return oct(n)
