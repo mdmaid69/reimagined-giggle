dfb681011834493a12030276dbf98bbda49e9ae1
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:13:03 +0000

    machine blue battery rush physical

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def split_path(path):
+        return os.path.split(path)
