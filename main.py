ec23b9484d36dfb4b0c57e782d7fa3a1fa94e317
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:52:31 +0000

    onto workout interested listenfor fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
