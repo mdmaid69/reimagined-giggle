94b21f2a0b61002b3593d8afdfe355628db850b7
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:56:05 +0000

    crime start turnon office not

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
