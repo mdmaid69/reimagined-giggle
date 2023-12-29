af94b7eda0a24460ccd68bfd839b78f73b6191ff
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:56:20 +0000

    value wild food less shine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
