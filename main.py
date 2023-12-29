7ec25fbe1c362b779ea21ec794d289e392bc6fa3
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:04:08 +0000

    advanced wipe miss unable strike

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
