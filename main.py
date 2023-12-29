27f71645658526c54d3f21b8d0de8efce7e8167b
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:36:05 +0000

    this it hate any close

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def get_array_length(array):
+        return len(array)
