4573437674cf3613afa673c799dcdbfca72250c9
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:21:22 +0000

    often thinkover protection fast fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
