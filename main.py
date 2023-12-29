6e6a5688efbfa6179b81aef5cc1b6701e474867e
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:59:30 +0000

    shake wrap underneath shoulder public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
