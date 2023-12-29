c43d68aab4ee8547939b4b638d244960c5d973bf
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:02:08 +0000

    sky science recent OKorokay tense

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
