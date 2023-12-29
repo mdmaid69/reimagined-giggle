88ae16e6e0596a1e2748f575ae95f35ec9f3304b
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:30:40 +0000

    takeaway rule build familiar sharp

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
