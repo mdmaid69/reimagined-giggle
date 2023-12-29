b4858394ed202754b7a5377bb1cee65acc19f2d3
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:53:54 +0000

    later manage belong which typical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
