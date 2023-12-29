76f80a302c53d830832b3665e9c5f391ca125206
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:55:01 +0000

    target inquire determined workupto accommodation

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
