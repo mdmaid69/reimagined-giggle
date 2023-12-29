dbdf6b538cb6bf623bb2e6e05edd25499dd76b40
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:33:55 +0000

    Zz cook year ride cope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import datetime
+def get_today_date():
+        return datetime.date.today()
