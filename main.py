dbbd233ffd7f8ab994073ad3879cf306823733c1
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:53:28 +0000

    check authority awake challenge depend

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def remove_directory(path):
+        os.rmdir(path)
