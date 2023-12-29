d40e803dc360884b50cda7b883aa53c731c7b0ae
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:27:50 +0000

    and since operate trousers send

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-import os
-print(os.getcwd())
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
