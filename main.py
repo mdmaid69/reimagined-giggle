60917da0da869cafc40531528a6839c40445a7e4
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:01:48 +0000

    interrupt wander illness never common

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
