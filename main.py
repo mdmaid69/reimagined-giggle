17a012dd4442c8c7b2dd87b8eaa3d7e1240fbe25
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:35:32 +0000

    fashion sound unhappy ticket contact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
