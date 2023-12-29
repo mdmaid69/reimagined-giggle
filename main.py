36a8431464d3ca1caa75e57359119a031e23199b
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:15:35 +0000

    long defeat clearly reduce feature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
