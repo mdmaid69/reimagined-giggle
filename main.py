0b6193bfdcb3b8c589d959c5bce60b62bfef6895
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:26:53 +0000

    what tend disappointed stare close

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import sys
-def print_python_version():
-        return sys.version
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
