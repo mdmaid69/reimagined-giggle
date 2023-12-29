bf982f49005f0b620f5c4bb8e0b18157268430e2
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:15:09 +0000

    add breath slice business prize

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
