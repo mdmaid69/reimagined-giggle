64478b1f7ff8e9634a931fa6ebb5186881874891
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:09:50 +0000

    word spare disturbing house occur

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
