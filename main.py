689f9db90b03a2e5c4fe398ba324e3ec7f0b475a
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:23:47 +0000

    steer piece balance fall knock

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import random
+print(random.randint(0, 100))
