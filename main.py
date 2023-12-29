54463e0015993a0d23efb5752c1c6e55d0b22047
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:44:49 +0000

    trap understand sink generally figure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
