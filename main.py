757be822215baaceca581201cf1e7c21e8a57b5e
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:07:23 +0000

    bang suck near keepaway fierce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_speed(distance, time):
+        return distance / time
