c6554d9be9b17e39c6ffb7da0f5818d235aa087c
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:45:21 +0000

    walk regard suit Rr chat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
