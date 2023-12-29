e506269534db73fd6e5c725ca9040751bcc7b331
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:07:56 +0000

    hope blame worried interested transport

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+for i in range(10): print(i)
