63e23277458e9c78ed2a70e8d9cf84ccb10f7843
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:55:51 +0000

    raw succeed half big prove

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import time
+def get_current_time():
+        return time.time()
