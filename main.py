c406a501efd419f205f14f66db4cc408d6d4a002
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:40:53 +0000

    land wash himself run brown

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
