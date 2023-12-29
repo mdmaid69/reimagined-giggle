fb569ae05d896dcf11f510d35287000022beb561
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:16:44 +0000

    forward church land get minor

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_user_dict():
+        return collections.UserDict()
