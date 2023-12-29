bc81913b3004f8a4e8259e2e82e0710436a06ddc
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:55:35 +0000

    lose history takeaway transparent decision

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
