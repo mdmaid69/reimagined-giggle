f3eb2ca2a50c6c49ebbe2940060672d0d24dc1d7
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:53:04 +0000

    book whistle forget open rich

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def add_numbers(x, y):
-        return x + y
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
