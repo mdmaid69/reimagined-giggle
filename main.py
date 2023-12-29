381b7380a71ae04aa6f446803240103de1442c9f
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:57:55 +0000

    soft mark never hurry upwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import collections
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
