8917a30a0b409cc8d3ac58e9f79ddee8368fe3b9
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:33:41 +0000

    same possession spray government petrol

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
