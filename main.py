cc67857121fe113dd9aa52a7c5d77c1aa51e020f
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:03:29 +0000

    label shadow speech invite recent

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def convert_to_hex(n):
+        return hex(n)
