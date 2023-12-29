7e33464f964e3677bdce917983f6ca034d38ca7d
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:14:03 +0000

    before circumstance skin disagree condition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def delete_file(file_name):
+        os.remove(file_name)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
