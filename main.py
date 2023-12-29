dd020fd4450f98f81834f58adec2d120565e3236
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:41:17 +0000

    culture than insist mistake push

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
