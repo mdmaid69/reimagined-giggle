8bb604c7d568546589be18ed3e2f33d9c83ca8cd
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:00:14 +0000

    lean clue their about swallow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
