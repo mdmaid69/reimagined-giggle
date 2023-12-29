a1804b2abb052a73583ac36bf718230b4acefb75
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:35:23 +0000

    loss worst yet too mother

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
 import array
-def get_array_from_file(filename, typecode):
+def convert_unicode_to_array(unicode, typecode):
         a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        a.fromunicode(unicode)
         return a
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
