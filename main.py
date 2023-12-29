b9a05190dbc445a27bdb151015e77141192d8187
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:29:48 +0000

    store tend gooff himself ground

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
