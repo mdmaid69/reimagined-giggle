162d68d41abcdeb3e25848f31f06bca1abf1b023
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:56:49 +0000

    share thumb steal pot twist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
