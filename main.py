12417621a637cc7d6bfb8ea83495f9526c9fc5a8
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:38:45 +0000

    give show collection print big

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
