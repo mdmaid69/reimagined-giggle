3dba2b0fcf2952e92c90a8d85893e9da873c1766
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:04:53 +0000

    country second notice respect uponon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
