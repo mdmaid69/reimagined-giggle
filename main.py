fec0504e437ae48be0539cd290e176594027401f
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:36:49 +0000

    570enjoyable back strength greeting Dd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
