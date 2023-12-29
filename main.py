720c66dd7727f73451eac1c3134f6ba8fa487615
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:36:48 +0000

    bag pile actually city flavour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
