9201ec745d73b0717e47706d08932c93370dc3dc
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:32:01 +0000

    regret shut song bye wind

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
 import os
-print(os.getcwd())
+def remove_directory(path):
+        os.rmdir(path)
