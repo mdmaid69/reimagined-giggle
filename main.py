3a734f65818a7e6c92e673ab0d09a72fc3ad0d14
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:08:07 +0000

    short callat that realize most

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
