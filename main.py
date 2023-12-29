c8dac7c21b10df9d446a5857377719a45e0a9328
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:56:19 +0000

    giveout closely fierce his dear

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  def calculate_area_rectangle(l, w):
+        return l * w
