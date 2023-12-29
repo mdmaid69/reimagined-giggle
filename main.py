c83b75e66a73cae4227d8365faf93309aa3add49
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:55:38 +0000

    avoid blow my strange entry

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+x = 10
+y = 20
+print("Sum:", x + y)
