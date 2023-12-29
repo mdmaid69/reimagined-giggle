1a264c12d8e9a8315f8a207e3a6fdda1d1161631
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:50:22 +0000

    length him float safety cinema

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_min(lst):
+        return min(lst)
