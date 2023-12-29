5f83cce1763cd5449ea05c9eb01cf2627d6f7397
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:49:33 +0000

    collection dress work famous upstairs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
