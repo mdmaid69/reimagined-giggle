03903e3563a310e124771eb169ba06643d5b0f6f
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:38:44 +0000

    fix zero mistake 573entertainment hard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
