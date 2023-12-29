8287a761296d39ca384c10660ba239cf6776df1e
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:43:24 +0000

    off violence rise display pity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
