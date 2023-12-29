a5934ff6ee23a6eb3e2cb8545039b19d97d7d06f
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:02:25 +0000

    together will belief shall platform

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import tensorflow as tf
+print(tf.__version__)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
