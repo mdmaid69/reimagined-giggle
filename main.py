71e4e0e9adf2de8495574c0c293ddc3224c1e824
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:54:28 +0000

    building steep book far study

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import datetime
-print(datetime.datetime.now())
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
