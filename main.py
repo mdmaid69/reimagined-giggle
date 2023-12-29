ad908163c2d41b74b2482f9f623d90a9754d04ca
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:09:53 +0000

    obtain better amount variety invite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def split_path(path):
+        return os.path.split(path)
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
