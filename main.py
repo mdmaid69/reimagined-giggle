3b70efcd3fffda582b21211284b1c32604f7c5fc
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:51:09 +0000

    bringup knock interview light bin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
