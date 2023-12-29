9328a9bf78607cbd61670a80e8362310548d8675
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:30:54 +0000

    ours paper project broad space

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def count_elements(lst):
+        return len(lst)
