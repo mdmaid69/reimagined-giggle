62f9ae3612e420550fd4517c5b9cb63e13ed914f
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:13:23 +0000

    OKorokay off sink not separate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_force(mass, acceleration):
+        return mass * acceleration
