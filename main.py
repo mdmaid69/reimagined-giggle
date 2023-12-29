9190288825e98145a05659c108a90eff6bf43f21
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:20:34 +0000

    callup mistake miss second careless

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def is_odd(n):
+        return n % 2 != 0
