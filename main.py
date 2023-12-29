35618702d406a045f5bc6db8b944b5f5f6a2e669
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:00:15 +0000

    criticism remove speak guide present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
