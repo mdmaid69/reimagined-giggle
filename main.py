76744481b0477a1104b4e560f0683b93ef2f47c4
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:00:31 +0000

    estimate move circle sing ring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
