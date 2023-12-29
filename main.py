dca3ce53b1413992384b76933c4bfcbf17fe96aa
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:04:11 +0000

    open believe fear heavy workout

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
