b1bdb7501b4808211743cd35dbbf3d6f26b20d89
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:21:27 +0000

    turnaway various wear exist label

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
