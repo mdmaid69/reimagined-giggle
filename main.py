a68a7a980eb9a0d59ce870b146e74ebadfd20d57
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:02:25 +0000

    possibly spring cause direction have

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import os
+def change_working_directory(path):
+        os.chdir(path)
