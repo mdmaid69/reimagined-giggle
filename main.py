540547f98b530da4ca45d166c06cf361b27025f5
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:48:43 +0000

    effect holiday worse share work

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def add_numbers(x, y):
+        return x + y
+def count_words(sentence):
+        return len(sentence.split())
