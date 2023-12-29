301293979c89d6b885b498db4b77c922837d606e
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:47:55 +0000

    brother scene affect willing dry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
