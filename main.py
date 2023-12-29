500ef424e6f5da1be0bc22770faed412c19327da
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:23:38 +0000

    waste clear education keepoff alive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
