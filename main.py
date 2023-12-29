f15a747596657a7d03b22d5aceab4de916b6cc53
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:39:44 +0000

    goback(to) acceptable temperature centimetre emphasize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-for i in range(5):
-        print(i)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
