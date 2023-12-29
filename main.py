ec6b96eff384ab206e66200ee3b542d5444e454f
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:44:44 +0000

    suppose discovery fight cinema sky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
