fc2dc842f9a92acd50b7ca707e7cd3da0cd55aa0
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:07:08 +0000

    holiday minimum improvement pin storm

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
