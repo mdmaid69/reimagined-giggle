4d07e8e48c1e04ff740c6696261fe7726b73a8eb
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:25:51 +0000

    hurry future early sharp spread

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
