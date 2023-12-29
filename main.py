e45d99cc196c9192fced8c640628a6653f6a5538
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:02:14 +0000

    quarter confusing nonsense divide either

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
