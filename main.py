11d6e6eeb588b55128530ad7d471b4cab2a30071
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:10:56 +0000

    truth shock fasten now again

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_square_root(x):
+        return math.sqrt(x)
