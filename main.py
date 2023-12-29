58ed2a995f0859dbbcc24649f9f08658a9778f30
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:47:03 +0000

    home patient trial lead sticky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  def is_odd(n):
+        return n % 2 != 0
