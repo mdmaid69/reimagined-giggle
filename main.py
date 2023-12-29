7895f6a3f29d668ae11872655bd3d77471bd3b02
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:32:07 +0000

    capital load earn ago software

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
