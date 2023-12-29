0d88065719d6d6ce554cff3db3f9ef941088e754
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:10:56 +0000

    comeover picture respect at raw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
