a22387978a17cc8baa353a68ec396a677c29741c
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:09:06 +0000

    typical foreign warn argument power

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
