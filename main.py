ffd1b4704f7777cc077f647c56b90e6a19c8e77c
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:45:13 +0000

    capital practice smell game about

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_work(force, distance):
-        return force * distance
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
