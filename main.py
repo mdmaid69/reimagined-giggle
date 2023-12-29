089c50b6f034f3f20c6ffdbb2d5c33d059b46f0a
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:04:15 +0000

    calm relation suck piece print

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
