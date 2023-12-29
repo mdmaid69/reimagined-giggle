21b0dfed444b1282285f6b9340ae35bd290bc286
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:17:38 +0000

    game show bar favour shape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_typecode(array):
+        return array.typecode
