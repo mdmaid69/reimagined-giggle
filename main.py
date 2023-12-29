463571fe48e2298420e8e893bbbcf6bf7c4edddc
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:09:31 +0000

    lookafter wear unlucky start style

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import os
+def get_current_working_directory():
+        return os.getcwd()
