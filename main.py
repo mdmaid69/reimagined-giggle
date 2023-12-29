5a7c457b6a71075c7adf1036a0cc0e17e85adaa1
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:50:17 +0000

    natural neck free actually bear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
