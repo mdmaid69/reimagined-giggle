912058e46e77634f1e41b4ec8c6a4abf67ba7989
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:55:09 +0000

    failure dark protect who lesson

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
