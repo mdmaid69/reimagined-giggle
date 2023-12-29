04e178ff2d8efa03d5f5b610e63449abd5c5cc3e
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:08:41 +0000

    breakinto ticket mad flat supply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
