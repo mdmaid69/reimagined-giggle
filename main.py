205a4b6fc30dc61f952f18355963f4bb6f7b9a70
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:56:03 +0000

    poison hope regional film continue

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def set_array_item(array, i, item):
+        array[i] = item
