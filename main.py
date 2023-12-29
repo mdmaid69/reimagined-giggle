24e683a964473e459eb3358e84cdbbb6e841659e
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:57:32 +0000

    after attempt read anyone pump

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
