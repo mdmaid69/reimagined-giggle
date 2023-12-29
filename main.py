2f3eb9cb77fc1c3e8ccfd9123cd5941df2835ea6
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:32:07 +0000

    offence paper activity build close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
