68a21e9ee6d0e79bc9c7c314bc6117bd80f33d92
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:39:43 +0000

    mixture little badly deal keepup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
