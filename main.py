dc44982c56a2a29584ff48588b6f36d814f99145
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:57:55 +0000

    thumb rent tired fashion Yy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+x = 10
+y = 20
+print("Sum:", x + y)
