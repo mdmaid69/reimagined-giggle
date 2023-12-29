a8001c22df3c96b6d4f3e28c0497703eca9d378f
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:11:26 +0000

    through minority total absolutely party

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
