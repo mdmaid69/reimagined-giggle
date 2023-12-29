5312f8664056bb8e636b33c26be8d38f0f98ee55
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:33:00 +0000

    better different snow mysterious curious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
