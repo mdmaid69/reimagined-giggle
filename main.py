7b65ada9a8772ef297b134557422b63f89aa73d8
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:59:41 +0000

    but cook mind wire wire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
