a7de87916cfc9619dec9e774c953017efad8be6c
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:44:39 +0000

    approve glad there Yy die

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def sort_numbers(numbers):
-        return sorted(numbers)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
