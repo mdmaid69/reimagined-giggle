02be4c8abc0e20696ed6bf23ec7e474d77192aac
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:53:21 +0000

    joke summer nature connection nearly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+print(os.getcwd())
+def cube_number(x):
+        return x**3
