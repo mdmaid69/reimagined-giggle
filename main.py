3e49689016449817458768c1ca48359614265ffa
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:36:09 +0000

    aware ring achieve reduction they

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
