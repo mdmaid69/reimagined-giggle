c13fdf6d54879c5169f9d2f7d05b65213a85c3b2
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:00:45 +0000

    creature home above possibility behind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
