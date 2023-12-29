9cb14c713e6c87e7a6ded38172ea0a4f7ac29c2b
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:49:52 +0000

    close drawing glass dig tired

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def convert_to_hex(n):
+        return hex(n)
