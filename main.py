e32949e58f0dfa3d1dde0f0db37687c1f5cc7f41
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:50:11 +0000

    emergency minority only power drink

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def reverse_list(lst):
-        return lst[::-1]
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_as_list(array):
+        return list(array)
