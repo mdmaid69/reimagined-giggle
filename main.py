cbd75426d9bca8ce36f5244525a8fb5448def077
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:49:49 +0000

    makeinto successful not serious turnon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+print(math.pi)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
