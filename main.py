6ee97a3514fed011f9dc95ce9bfe34dbee1e72cd
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:40:06 +0000

    wave kind run concern bringdown

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def multiply_numbers(x, y):
-        return x * y
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
