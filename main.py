75f32d4007f93f9e80043ebf058b4cf0fa7a2293
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:18:09 +0000

    report be low public ticket

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
