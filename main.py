cc5dbef7127633c12b6fa361f4d71838c4cbc036
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:37:51 +0000

    second breakout less dead Oo

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
