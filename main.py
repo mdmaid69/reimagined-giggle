bfb33fa7d924dddee3a429a630e6c9807f219e41
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:25:38 +0000

    interview control sense intelligent lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
