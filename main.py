5f62586ea83c8e2634d1f9cecae2c5ed306cc19f
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:17:59 +0000

    practical open flat coat major

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
