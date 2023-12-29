3f7546513144f7cd1589274aaddfc05827d9d182
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:59:46 +0000

    free refuse emphasize example unusual

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import time
-def get_current_time():
-        return time.ctime()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
