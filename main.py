10a212db300767b7f5e64ebfc19e40f252cc657e
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:00:26 +0000

    why goaway pain noise whatever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+from collections import Counter
+print(Counter("hello world"))
