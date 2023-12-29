fccbd7c3bc5e8c53d0ae582e975cd7e68899704c
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:49:22 +0000

    increase mad morning club pleasant

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_area_circle(r):
+        return 3.14 * r**2
