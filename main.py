03c79081bcabec05fe9748f9d3212c1a52aa1cc7
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:09:43 +0000

    plenty bus dress lie both

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
