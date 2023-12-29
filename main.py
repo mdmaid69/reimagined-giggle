e7261acd5252d41677d6c6e7d648a7004569957c
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:49:11 +0000

    rank soak track which suppose

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
