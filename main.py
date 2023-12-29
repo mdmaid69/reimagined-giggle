9655d7f5f5ccf9dce5690678aa67d3837a58c68c
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:43:51 +0000

    right in apart band job

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_area(radius):
+        return 3.14 * radius * radius
