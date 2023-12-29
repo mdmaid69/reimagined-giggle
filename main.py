c57472043612bd27cb630b9dc5552ab14c0ff659
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:21:16 +0000

    valuable swing fetch winter nothing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+for i in range(10): print(i)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
