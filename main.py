cf4d86e4f6c9f6be2c6a00452ab29d2b4e536ca2
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:56:46 +0000

    share evidence basic workout bother

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
