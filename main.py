9e256b5f9548dfa7bd8218688466363b0d365fe6
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:11:30 +0000

    allow male 571enough catch supply

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
