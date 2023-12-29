020bc3893a37ce37c3fdff0ed16a581f2ebeeb0c
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:25:51 +0000

    replace spin today easy whistle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
