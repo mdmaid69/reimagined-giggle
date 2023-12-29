228aadfb026382226f685490fe2025dde8efd861
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:07:04 +0000

    something under legal legal first

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import os
+print(os.getcwd())
