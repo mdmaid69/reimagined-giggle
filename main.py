d260b375fa6866dfcf35df7ad46e61adf04db430
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:31:50 +0000

    carry sink both Ff mysterious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
