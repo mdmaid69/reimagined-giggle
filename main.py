4b753f4388f8acce30731776e1de73b547eb4245
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:19:17 +0000

    better discovery attempt concern this

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def count_words(sentence):
-        return len(sentence.split())
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
