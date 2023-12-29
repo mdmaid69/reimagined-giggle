3b6340d1952c8413639af8d131ce36eed72ff9c6
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:00:19 +0000

    what trip offence inquire clever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
