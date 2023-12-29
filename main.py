1e1be23980192bd3817deed190239ac78c62cab1
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:13:02 +0000

    cry record custom agree black

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import math
+def calculate_sine(x):
+        return math.sin(x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
