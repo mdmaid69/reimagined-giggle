e838887648b5a6584358bf6008aea09f99b025c4
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:08:27 +0000

    separate play charge table mystery

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def subtract_numbers(x, y):
+        return x - y
