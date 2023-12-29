604199b03bc74cbe70421575372cb510d1c71221
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:28:07 +0000

    leave child underneath 569enjoy fall

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def split_path(path):
+        return os.path.split(path)
+for i in range(10): print(i)
