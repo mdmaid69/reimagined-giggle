639a8321436876fdf1ed921b7bb7172821ddf3a1
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:45:15 +0000

    whistle instead our new noone

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import collections
+def create_priority_queue():
+        return collections.deque()
