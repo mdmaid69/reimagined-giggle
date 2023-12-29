0f5afcf0de2e7a77ee9e8c095f5fbf18ec3e8948
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:49:31 +0000

    claim union pleasure confused ear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
