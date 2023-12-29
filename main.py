a6a102b46e520dd647852202a5b7d71e1aa57bc4
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:20:25 +0000

    process slow whom challenge mark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_density(mass, volume):
+        return mass / volume
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
