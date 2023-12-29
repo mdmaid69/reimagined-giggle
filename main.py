2d854fecebdf0e5691cae61a60c7a9c6720c691d
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:55:25 +0000

    drive space gradually warn argument

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
