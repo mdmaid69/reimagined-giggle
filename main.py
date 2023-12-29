0aa1e80d5422cdd3a3fbbb1e32c4d474b52e18ea
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:05:27 +0000

    however path week familiar when

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  def add_numbers(x, y):
+        return x + y
