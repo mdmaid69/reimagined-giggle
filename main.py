9dba5bb56b7b71393d686f70447243298950276f
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:33:32 +0000

    discover between gradually somebody case

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import array
+def remove_from_array(array, item):
+        array.remove(item)
