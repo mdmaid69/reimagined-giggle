da3b21cea8f1b43ac365b41f1a388a5e69b84040
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:40:43 +0000

    either sign bus fat front

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import array
-def get_array_as_set(array):
-        return set(array)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
