f39458b1ad3907eafee39b64a1ba47af88e95a0d
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:18:11 +0000

    label getout giveaway edge angry

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def delete_file(file_name):
+        os.remove(file_name)
