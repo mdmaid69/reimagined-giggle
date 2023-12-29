a3c3d0973e7cc426cfb3ed485c83d52e921b189f
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:58:31 +0000

    probably wherever exact size delicate

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_current_directory():
+        return os.getcwd()
+print(sum(range(10)))
