0b7a92a1fcf36a4ed18a1397f1a05b70c11ac5c1
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:55:21 +0000

    tear pretty march love delicate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def is_palindrome(s):
-        return s == s[::-1]
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
