bd17d09e3ba74494aa37e656df567f0929f0a931
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:30:20 +0000

    write activity wealth remember house

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def calculate_acceleration(speed, time):
-        return speed / time
+import datetime
+print(datetime.datetime.now())
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
