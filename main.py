0b01c33584fcb1b74f0eae6860f559695a2b34a7
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:23:20 +0000

    land drive sing agreement makeinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
