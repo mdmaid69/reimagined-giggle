e83f6c8d32380700943e4cc33ecb99c7911b88ce
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:29:34 +0000

    unhappy reason kick day shut

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def add_numbers(x, y):
+        return x + y
