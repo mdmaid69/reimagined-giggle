7ec95052eda58ab0821bcc346e88bcb753cee9d6
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:44:26 +0000

    young build teach freedom flavour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
