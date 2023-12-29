dadafab708517268e19968fbdd1deeb613bd8efa
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:49:09 +0000

    of flood shop mean tooth

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def find_unique_words(sentence):
+        return set(sentence.split())
