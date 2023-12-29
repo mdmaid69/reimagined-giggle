df6795cfdcd6eba2bdd68d798f73fd8ee4e2594e
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:24:08 +0000

    real children phone wrap dream

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def subtract_numbers(x, y):
+        return x - y
+  def convert_to_binary(n):
+        return bin(n)
