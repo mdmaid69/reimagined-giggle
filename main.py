ccf79a4dfc02b13a933939a6ac26224174329497
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:07:32 +0000

    complain shine plant violent office

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
