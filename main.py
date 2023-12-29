36427f3aed5c94e8042a4bf38349dbf5f1c4d313
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:56:51 +0000

    deserve hide rule family trouble

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  def add_numbers(x, y):
-        return x + y
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_area(radius):
+        return 3.14 * radius * radius
