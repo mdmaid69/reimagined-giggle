a7e26e3e4c08ad9cb08ff464b6586ea157af4c2d
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:26:34 +0000

    danger top but final side

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
