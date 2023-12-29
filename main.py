16587fdd66e7c0db70023edeb2362177110416f3
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:08:58 +0000

    unfair cope tell start flow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
