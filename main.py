69aefd69ca42c1cef4ce76f451ca8975d3ffc092
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:55:51 +0000

    hardly lose keepaway important high

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import os
-print(os.getcwd())
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
