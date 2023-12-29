751054786131007b05f4bc89ed523699a7c10088
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:22:13 +0000

    instead party amount fine taste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
