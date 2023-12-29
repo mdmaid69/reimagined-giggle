d316e240e12401b23cf69befe9ebc1e171aac761
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:21:14 +0000

    send thinkover pause ear listen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def add_numbers(x, y):
+        return x + y
