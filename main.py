59f11f60a8b83748eee155b8156d7d66dd16c9a9
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:40:06 +0000

    home danger immediate flat he

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
