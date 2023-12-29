e5553f70c7a03406f2b884fe40cd6c4e674e2402
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:47:39 +0000

    best centimetre hot everybody stamp

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
