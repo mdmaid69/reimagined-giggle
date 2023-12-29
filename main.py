6fe82ef758faa8d4246659908878a9dd4b7b21c0
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:37:26 +0000

    sound hold move punish damage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def greet(name):
+        print(f"Hello, {name}!")
