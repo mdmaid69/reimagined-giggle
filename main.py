c803e513ebcc114cc41ad776ccf5d60bc86355b7
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:14:31 +0000

    correct advise around kind mark

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_acceleration(speed, time):
+        return speed / time
