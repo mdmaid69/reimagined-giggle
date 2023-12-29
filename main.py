034983f83db049089eb3da6a377b0fc5601894c7
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:37:29 +0000

    want buy over leave suggest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
