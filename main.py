7bc091fb5963ee298b0f160d4c3d30ba80d53dfa
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:27:27 +0000

    collection must breakdown exam performance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
