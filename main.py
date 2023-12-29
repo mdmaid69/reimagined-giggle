05e78982b41bbb90e4323d08fa9dfa52c1618087
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:30:07 +0000

    advice several ball settle such

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
