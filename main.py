5ffba521fcc55ae19b4a3d9e7707f6fb8d14336c
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:35:00 +0000

    favour hollow shine popular proof

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  def is_odd(n):
-        return n % 2 != 0
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
