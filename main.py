e97c8ae6b622e9e808c08bb38a665ca0aa8f0270
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:15:20 +0000

    flow marry sport only bringdown

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def square_number(x):
+        return x**2
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
