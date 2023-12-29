9c9e799466a22838a2dea4dbe0f71e12afb6d532
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:51:25 +0000

    tell your check frame damage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
