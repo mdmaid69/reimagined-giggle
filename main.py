2b2c12510e57d880373b83343df446a9946cb292
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:21:31 +0000

    mine ground calmly kind contrast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-for i in range(5):
-        print(i)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
