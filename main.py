67e74297d2e3fa2e57ffd090960c2b5d97bbf17d
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:25:43 +0000

    smart Jj birth bury towards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_set(array):
+        return set(array)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
