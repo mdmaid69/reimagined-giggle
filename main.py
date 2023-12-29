eb514e93be06864209b04c9885bf562f3e856a5d
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:44:06 +0000

    warning exchange chance preparation rare

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_power(work, time):
+        return work / time
+def calculate_density(mass, volume):
+        return mass / volume
