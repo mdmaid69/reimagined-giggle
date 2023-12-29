a04da38c8884ce6ef8223cfde36f6a13aa73f62c
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:46:03 +0000

    564encourage flavour really left experience

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
