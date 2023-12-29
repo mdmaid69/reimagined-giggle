dcd27eb480bd7a57b6e0d058d4aa4915bc610182
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:04:51 +0000

    degree search community roll wait

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_as_int(array):
+        return int(array[0])
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
