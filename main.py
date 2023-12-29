d81fdb49b1351a656ebcf438c0bb46720a0b95f5
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:57:12 +0000

    vote smell shock family variety

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
