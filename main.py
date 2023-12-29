ed3a424c6f8ee9df218d71357e48821d3534b0da
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:18:04 +0000

    earn study set left out

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
