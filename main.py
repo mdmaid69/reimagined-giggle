d7c4742a653b953a71c17bb8f76d59b536a1b1b8
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:02:51 +0000

    comeround regular seat because talkinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def greet(name):
+        print(f"Hello, {name}!")
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
