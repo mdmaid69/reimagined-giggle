5ec4eb12188cdf5b5d2d1b26be6813d9b2a85b13
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:27:41 +0000

    yesterday prevent stranger drawing concentrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def clear_array(array):
+        array *= 0
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
