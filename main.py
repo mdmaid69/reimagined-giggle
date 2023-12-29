eecdae28b809ddab1efa1b648b340927e9937a44
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:06:27 +0000

    spray frightened along rush practical

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def convert_to_octal(n):
-        return oct(n)
+def count_elements(lst):
+        return len(lst)
+def reverse_list(lst):
+        return lst[::-1]
