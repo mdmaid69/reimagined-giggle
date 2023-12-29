d922617150751b3e713b5c9650c2b911351dde5b
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:19:10 +0000

    brain convincing identify frighten tone

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_power(work, time):
-        return work / time
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
