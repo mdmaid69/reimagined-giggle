ce40c99b7e51e1a2d44d23f684599bc54f8c4f32
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:55:36 +0000

    first race Nn one estimate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def clear_array(array):
-        array *= 0
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
