58ee9cea3fc5f84d2d6b6caf0b8b3cd2c9421be1
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:21:31 +0000

    breakup complain wherever connection intelligent

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
