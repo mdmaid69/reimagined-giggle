fafc690c4e8b498f02edb22c27f5e7bca93fd596
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:17:27 +0000

    alone weather beyond force fortunate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def add_numbers(a, b):
+        return a + b
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
