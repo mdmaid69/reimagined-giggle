93b77efcebc36c4f4f2de86fba254d5a35688992
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:42:13 +0000

    roll space creature under light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
