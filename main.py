c2cc7cf834f913abec4941854a027ea48fd8925b
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:59:56 +0000

    past getoff hit combine understand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
