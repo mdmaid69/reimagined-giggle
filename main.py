ada9ce9bcc3f9556d9bcf3b0c75e186aba0e3358
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:26:14 +0000

    young gas to well hard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_max(numbers):
-        return max(numbers)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
