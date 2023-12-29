74e05234fa670b6c6ec537e3ea6deb0cff2b33bf
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:38:09 +0000

    band stay interested 568energy comefrom

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
