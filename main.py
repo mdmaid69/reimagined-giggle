abcc32e7c683da97810a778b297c4d3c4cd7fc5d
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:18:43 +0000

    classroom lay light test fun

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-print("Hello, world!")
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
