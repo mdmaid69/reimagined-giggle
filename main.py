547e4f5947ade6c73d8528bf39220e79fef68c1b
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:21:28 +0000

    burst later contact luck lesson

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
