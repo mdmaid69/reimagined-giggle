1e52bdb14ce772f019ee8fa20e08727434ca7ca7
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:54:37 +0000

    wave sleep suddenly regular party

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+print(sys.version)
