ab2f42d6f20fa275e81c383e9f3039b114c4b03b
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:56:20 +0000

    sing behind spoil refuse finish

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
