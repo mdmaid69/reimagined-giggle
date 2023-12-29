ddf382b1ea97c0234cfd46966ab66ee8f061061b
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:06:55 +0000

    correction beat aim to unite

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
