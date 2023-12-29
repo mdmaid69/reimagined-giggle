6d1ae41d69398e3f4670cef9fe4e0e486ec38678
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:53:23 +0000

    splash consist Vv scale happen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
