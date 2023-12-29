6a03cf7df591fa8d4bf409c38f8c6dedf375c89c
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:59:47 +0000

    past left support prize ball

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import tensorflow as tf
-print(tf.__version__)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+for i in range(5):
+        print(i)
