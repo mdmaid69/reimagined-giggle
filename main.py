3ffe2e6ad58d1ff4dae367fec57d31317b4987e9
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:00:25 +0000

    uponon artificial class simple tone

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
