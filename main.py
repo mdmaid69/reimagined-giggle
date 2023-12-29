289834cbe0efee972ff833ea591fb62317cb0490
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:02:11 +0000

    phone work backwards treat prize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
