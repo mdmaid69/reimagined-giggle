e90eea2df4b18e4bf475c77cfd89121043074189
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:57:11 +0000

    sight brown explanation used find

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
