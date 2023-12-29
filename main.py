14a46521642b7421cd9080aacf17434190827ff9
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:31:19 +0000

    hurt holdup through order Ll

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import numpy as np
-print(np.array([1, 2, 3]))
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
