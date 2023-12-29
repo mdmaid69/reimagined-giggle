9e7eb226088af0d8d8eb2c2f147dc2188a1a5d1d
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:53:04 +0000

    term stick have official shoulder

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def sort_list(lst):
+        return sorted(lst)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
