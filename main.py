caa996a169b0ca116801a7d010a651e752d40f5d
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:06:26 +0000

    comefrom coat lend belong watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import time
+def get_current_time():
+        return time.time()
