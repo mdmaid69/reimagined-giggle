f6090c8ca31b065255613da1f33c78dfe9463110
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:48:36 +0000

    pupil future switch book confident

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sys
-print(sys.version)
+import time
+def get_current_time():
+        return time.ctime()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
