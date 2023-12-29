b039388d0c8e4b73c50c4055214b1e4d3c439a25
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:53:20 +0000

    translate every steal exercise exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def is_even(n):
+        return n % 2 == 0
