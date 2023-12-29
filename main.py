23a66af1c1fb2119fa0596260648dc8c1b9077f7
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:31:31 +0000

    size again check class comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def calculate_pressure(force, area):
         return force / area
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
