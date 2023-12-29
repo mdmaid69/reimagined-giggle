95a82e2a6ecc525bc0fe86ecf49a61007cd51802
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:09:56 +0000

    afford front pull them centimetre

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  def square_number(x):
-        return x**2
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
