bbc0da80216d2df99c98cc4714b1c9ad8bfb20ca
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:07:46 +0000

    rent offend reasonable cost fierce

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import sys
+def exit_program():
+        sys.exit()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
