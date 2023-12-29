b8091eb1fe5a104e48d274b85e254c1c87c7131e
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:36:49 +0000

    cross sale luck frighten corner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
