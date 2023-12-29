b386ce070a02ba03763f56159e51103a342cdfb3
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:30:54 +0000

    bank really flood ashamed confuse

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def subtract_numbers(x, y):
-        return x - y
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
