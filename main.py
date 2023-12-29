beb0505c21cbd92a4dc344779cb845f80d8592b8
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:28:00 +0000

    doctor suit holdout minority train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def sort_numbers(numbers):
+        return sorted(numbers)
