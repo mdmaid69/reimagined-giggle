c18db606a9795c9841a533ae53696638c6a1f2e1
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:39:14 +0000

    base question love connected programme

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
