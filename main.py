afda42c69b1380f8ee011c602040e0e1dd60766d
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:06:05 +0000

    stiff later base official doctor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
