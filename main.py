289377182e7156dbccee5486becdf1dba05a1a59
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:16:05 +0000

    bunch roll patient build mad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
