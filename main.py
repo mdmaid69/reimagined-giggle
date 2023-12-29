5615b540c884c3ffce0ce4281ed3cd73d0fe1b43
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:34:23 +0000

    suggestion injury scrap but apply

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
