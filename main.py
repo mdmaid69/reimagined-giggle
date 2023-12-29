c3c97c912dba5c585beae234f65ec9985ab1c577
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:35:07 +0000

    return neither through instrument clothes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
