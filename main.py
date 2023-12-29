230e832da8c50dfa89c542b6c113ffb9f9eeff68
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:37:25 +0000

    plan least catch leg pleasant

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
