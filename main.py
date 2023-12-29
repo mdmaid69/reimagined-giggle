d2fdeccf0375adce847d7bdc8d365364e9557002
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:25:09 +0000

    attempt other tip get yourself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_arc_sine(x):
+        return math.asin(x)
