b1efd36510b08a645a97921a904fa0d099d4008b
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:58:44 +0000

    apologizeapologise early quality score pool

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
