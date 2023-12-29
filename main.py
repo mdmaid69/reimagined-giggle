e6b43d6cd698a39295885e9f18ce021ac16630b9
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:00:30 +0000

    impressed wide line twist even

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
 import array
-def get_array_item(array, i):
-        return array[i]
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
