c3c781fc2f026492455dab7e3b12bdf401b47971
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:38:27 +0000

    work divide big other post

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
