30cd0b71ae59b66596b1b60e861b809fdd1be836
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:24:03 +0000

    close face interview communication comeround

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def reverse_list(lst):
-        return lst[::-1]
+def sort_numbers(numbers):
+        return sorted(numbers)
+import platform
+def get_os_info():
+        return platform.uname()
