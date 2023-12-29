7ea02cd5f6371e5c20d3999dc8e51247e49d369d
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:38:53 +0000

    autumn wrap money moment play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def get_array_as_float(array):
+        return float(array[0])
