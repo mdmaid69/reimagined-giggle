2f30d137752479fba05e95b5a2542307b3ee2828
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:08:28 +0000

    skirt style agree 568energy object

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
