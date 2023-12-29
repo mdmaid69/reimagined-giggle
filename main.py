dfc53eb923e9413ab2605740b916897a6778b4c6
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:08:39 +0000

    abroad same century happen maintain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def convert_to_hex(n):
+        return hex(n)
