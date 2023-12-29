b319b01fdf92a9fcb52968ff53ae5541d936a91b
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:16:31 +0000

    name freedom matter angry rule

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_item(array, i):
+        return array[i]
