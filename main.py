177ff29f7b9533c48555094255546a3fadb25782
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:39:51 +0000

    I uncomfortable avoid pin flat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
