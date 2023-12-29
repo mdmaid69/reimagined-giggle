17b5d275a119336b537db29bd3390d65546f5a60
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:47:50 +0000

    boat far cold advise hope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
