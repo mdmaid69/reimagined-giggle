5f9202bec0c05d49f369fad8e7918fb0657d1dc5
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:05:40 +0000

    invite fast mixture acceptable rank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def find_min(numbers):
-        return min(numbers)
+  def square_number(x):
+        return x**2
+import math
+def calculate_floor(x):
+        return math.floor(x)
