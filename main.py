1168fb50386391788bc0f6c7913a56d26ffe1a32
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:18:49 +0000

    minimum please determined even smell

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_current_directory():
+        return os.getcwd()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
