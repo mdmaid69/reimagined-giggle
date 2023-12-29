53518964d700b3cdbb6a3305fbe8a8ad85adaef5
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:07:17 +0000

    maintain hurry careless adjective usual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
