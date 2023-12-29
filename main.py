80676f7e3ea60514d6dc10083e201af4b2ff38c9
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:52:18 +0000

    strike scream bar by scrape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def get_string_from_array(array):
+        return array.tobytes()
