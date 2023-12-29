03be691bd2e7b182914adb0949f9d5ae1b889919
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:01:39 +0000

    tired task thin in bottom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
