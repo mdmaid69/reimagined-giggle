00bfdcf4f3ca6191fb4f0c9d5abd88a6e849ba90
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:59:24 +0000

    fairly afraid finish loan somewhere

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
