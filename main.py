231609bbb7ebd0f6abcfadf0c359dbfc0f86bc8b
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:43:32 +0000

    note used means doubt accept

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
