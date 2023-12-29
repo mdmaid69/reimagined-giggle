b34d327f6b3dd18a66aac037600968ac5c4d565b
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:09:53 +0000

    possibility sister tight limit not

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
