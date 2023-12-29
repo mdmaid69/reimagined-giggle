409e898850b755d313a8273fc51bd568fb60ef2c
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:03:12 +0000

    disapprove bottom reason image deserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import os
+def get_current_working_directory():
+        return os.getcwd()
