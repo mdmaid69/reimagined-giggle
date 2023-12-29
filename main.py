246658258aec60595d85b6e7cd0593761cb7a749
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:23:35 +0000

    recover behind weekend sharp push

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def get_array_as_int(array):
-        return int(array[0])
+for i in range(10): print(i)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
