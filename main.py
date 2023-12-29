f506110fdbeb91e996b1dd40b9ad7e97188712c3
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:46:25 +0000

    loud smell surface country male

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
