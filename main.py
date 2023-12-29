493f5267a85ee932188381da05a963c2eec8a2ef
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:48:45 +0000

    may takeover stupid arrange small

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_area_rectangle(l, w):
+        return l * w
