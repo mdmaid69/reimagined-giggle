04c14d049b6d061bfca73adabc615063ed6405cd
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:12:19 +0000

    shallow picture creature impression explain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
