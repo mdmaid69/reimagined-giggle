2916caf3d334373ab4905761e03023d03aab33b6
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:39:07 +0000

    tour mess sail matter nervous

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def remove_from_array(array, item):
+        array.remove(item)
