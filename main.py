9e16563f31975f227905068977f4ab0c5dd6ceed
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:56:26 +0000

    so behind clearly emphasis circumstance

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_as_list(array):
-        return list(array)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
