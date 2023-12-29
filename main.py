0a6a317aa5a79b41ce34c00a24d1d8dd60a48687
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:34:37 +0000

    that sort accidentally out alone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
