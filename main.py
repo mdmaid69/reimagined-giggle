5c66fb703fd3d6d2ed3bb54797b1681a04658aff
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:22:26 +0000

    unlikely left glad towards list

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import os
+def change_working_directory(path):
+        os.chdir(path)
