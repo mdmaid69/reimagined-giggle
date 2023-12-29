f53809075851dcc93b70711583a78d78a4f33de8
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:36:11 +0000

    believe average healthy level avoid

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def reverse_string(s):
-        return s[::-1]
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
