1b8e84ec496d957c4b0f8ba436d5e3d32f8324b2
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:08:11 +0000

    basic copy borrow figure skin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
