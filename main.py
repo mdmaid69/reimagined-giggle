056a9a22512e2f8f3f4a47ad2f9d4d4b9a8132cd
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:21:45 +0000

    use expression print getoff giveaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
