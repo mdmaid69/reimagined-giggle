691cfd824faf70bb444825679565e43b4a796724
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:05:01 +0000

    voice powerful giveaway for break

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_str(array):
+        return str(array)
+def find_min(lst):
+        return min(lst)
