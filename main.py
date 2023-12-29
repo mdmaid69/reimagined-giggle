35116bf2e06aca908a8ce1485f88f3a17ff328a2
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:16:52 +0000

    pool bin relation respect bridge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_floor(x):
+        return math.floor(x)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
