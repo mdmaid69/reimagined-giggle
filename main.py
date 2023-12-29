e4a9ec0ed5ffb2320ea145e118c733757431ca0a
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:33:38 +0000

    actual complete story building free

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
