2cc8a122a4796f9476a731afaf40b6b2eefa47f9
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:33:21 +0000

    everything between fear interruption aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
