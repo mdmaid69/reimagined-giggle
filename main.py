77804142dc17167ed266f55201f037da74921e1a
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:51:26 +0000

    blame from quarter cross care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
