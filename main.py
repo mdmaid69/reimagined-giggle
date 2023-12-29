057fd727d2ac7453211ced8f933c54b15ce509e7
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:23:40 +0000

    achievement loose which sing whom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
