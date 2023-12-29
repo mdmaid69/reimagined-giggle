8fd86a12a92d9601af36fa3f4c66e4a716d256a6
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:20:57 +0000

    profit week spread town whole

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
