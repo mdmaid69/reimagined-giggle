3558447933a451644e27537be91e86a33f39c990
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:55:14 +0000

    own prepare real balance fairly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
