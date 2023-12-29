82237cf9a18f038e11807a1eae76303ceb1b24c3
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:08:33 +0000

    frighten surprising swing however minority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
