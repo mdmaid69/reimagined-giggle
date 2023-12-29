2a9b44cc146c27108b5087e356b47140fbbe475b
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:11:34 +0000

    passage weekend rule spoil nearly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
