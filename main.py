7710fc8d5e551dc373fad68c980ff9e132d2760a
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:52:43 +0000

    considering immediately yesterday talkoutof line

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
