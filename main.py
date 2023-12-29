3b597edcec20c030a8b3470f89323d62610fff05
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:34:19 +0000

    door central stone apply sit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
