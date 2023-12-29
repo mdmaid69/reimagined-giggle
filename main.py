ad39d515a331fcb18e67672e9265e20f03d78bd2
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:37:38 +0000

    basis never best flight cross

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def multiply_numbers(x, y):
+        return x * y
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
