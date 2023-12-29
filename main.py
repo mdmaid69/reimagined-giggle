0f29d691e9e495537cc98f0b4cae99c35cc8a908
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:50:48 +0000

    battery accidentally certain standard mind

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+text = "Hello, world!"
+print("Words:", len(text.split()))
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_as_complex(array):
+        return complex(array[0])
