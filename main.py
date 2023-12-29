dad20c0464511a0a30815e29ddb3ace279f5c357
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:02:21 +0000

    order power makeinto safe definite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_power(work, time):
+        return work / time
