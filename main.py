37108330a0b45e4301c75b144d4c1147ce5ecaa0
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:15:40 +0000

    appeal corner train where unable

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def multiply_numbers(x, y):
-        return x * y
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_acceleration(speed, time):
+        return speed / time
