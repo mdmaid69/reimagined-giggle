cddaf3b91403ee69c519ca4539d8cbf9a80e3fc5
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:24:54 +0000

    sweet frightened mind shopping new

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_acceleration(speed, time):
+        return speed / time
