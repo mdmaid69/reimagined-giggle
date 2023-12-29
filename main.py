37a0441dd1d4a18c689ff5e704fbcb5b3beb777f
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:48:07 +0000

    unusual tongue cum pull stir

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_user_list():
+        return collections.UserList()
