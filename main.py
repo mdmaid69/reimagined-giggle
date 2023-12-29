f21f3889207cce0fe0ffe72e68a87e9d3df50e20
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:19:21 +0000

    callback wind step cope hot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
