73ee3eb903561902e5905882dda2450b5f7cd0cc
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:23:47 +0000

    night shame raw ilovedicks give

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
