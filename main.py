e790b46c6b670a90ae56df642002ed57cde0ceab
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:21:53 +0000

    up confidence wise common regular

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_power(work, time):
-        return work / time
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
