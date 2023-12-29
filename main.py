8267b09b7636114c0be117227cf4cf32442a5823
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:51:04 +0000

    onto concern legal ever arm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
