8283d92b95f9b2afd3868259942961abb754664f
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:16:02 +0000

    hang divide Jj else wipe

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def sort_list(lst):
+        return sorted(lst)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
