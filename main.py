f12f6ea405faed18b0c0a748a71f2a34bbfdef7c
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:38:15 +0000

    Kk share hungry comein congratulations

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
