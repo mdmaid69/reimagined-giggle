c28f9bcf4fc3719289b589049690db33a32d6957
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:38:10 +0000

    machine tap too steal future

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
