0311447554538da50173288d848b2485fc3001ee
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:36:24 +0000

    touch material beside ill failure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
