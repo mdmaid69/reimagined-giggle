a4b7fa873bc6488361e5e48eacbd0f1ccff335a5
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:34:49 +0000

    custom like so test clean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+for i in range(5):
+        print(i)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
