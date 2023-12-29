420d095b408f34bb2e1be564a43f3e30467f975f
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:06:48 +0000

    oil in worst keepoff home

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
