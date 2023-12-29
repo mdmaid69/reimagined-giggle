b6b19141091c243ab7fe586360842cd454be2e7f
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:31:49 +0000

    temperature country but sign trust

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def divide_numbers(x, y):
+        return x / y
