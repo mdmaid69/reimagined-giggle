238cd708909e0dba7c1ac9bf49a2a42be6a1f048
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:17:23 +0000

    prize Ww back matter statement

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def get_array_item(array, i):
+        return array[i]
