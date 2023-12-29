e6ddc1fe0dced2a72d61180f9aa5890b651a9653
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:29:13 +0000

    deserve father share delight satisfied

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
