a24f9602f314444bebff91731879b6171c7d17d2
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:25:01 +0000

    remember moment refuse eye rule

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
