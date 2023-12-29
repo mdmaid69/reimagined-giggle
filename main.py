8839f13709aaa89dd7046266e8d4ec3605f0051a
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:56:04 +0000

    tear spray name spare terrible

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def greet(name):
-        print(f"Hello, {name}!")
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
