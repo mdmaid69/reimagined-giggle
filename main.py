2ae086dd91665b52733eea8204d3f22f89b1be28
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:54:21 +0000

    waste rely search unit down

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def change_working_directory(path):
+        os.chdir(path)
