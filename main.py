057751f4cf9d5201f97c447809844d266243b63b
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:12:56 +0000

    little regional snow manage find

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
