0ddcdc6e4bb0489e4fdd8bd8f78d15e1342833f6
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:52:35 +0000

    off peace callat promise excellent

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-import re
-print(re.match("h.*o", "hello world"))
-import os
-print(os.getcwd())
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
