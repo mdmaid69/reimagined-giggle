e31a063df605fbd83e3e536d20db0de04e40c0a9
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:40:13 +0000

    real proud approval compete standard

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  def convert_to_octal(n):
-        return oct(n)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
