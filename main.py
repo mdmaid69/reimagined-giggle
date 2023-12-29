d251bb2376cf6f62a499c57616d617ef3ed21354
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:33:10 +0000

    joke pressure takeout Mm future

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def find_max(numbers):
+        return max(numbers)
