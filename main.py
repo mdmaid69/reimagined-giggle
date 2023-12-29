e99a99537b94ac544edb5e721a9e4b08f0038daa
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:26:10 +0000

    by Yy thumb prefer description

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import math
+print(math.pi)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
