275cfc96de4b78dab933b81fda7687a7721dc37a
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:18:20 +0000

    deep cost sure woman top

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
