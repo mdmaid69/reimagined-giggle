4381fb64692b4367fb0447cfc22ecb730b3f5ab1
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:28:54 +0000

    me pencil pretty fill blame

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
