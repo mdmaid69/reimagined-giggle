c14bd3c4d9ee53ac1216ce7ff4719eee09124406
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:10:27 +0000

    mainly cheap minimum better cheat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sys
+def print_python_version():
+        print(sys.version)
