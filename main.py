aae38c79de2b1032538d200ecf48f752ada88bda
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:43:19 +0000

    godown whistle in enthusiastic memory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
