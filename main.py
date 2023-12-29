2836e0232fef754f4f9baa4a6bf0b3bd731ebb9a
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:39:27 +0000

    plenty surprising perform debt violent

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
