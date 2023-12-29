97c6ac6dbb52b28e7c2fc1d735c0ee054d558623
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:01:30 +0000

    dance workon later lesson remember

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_list(array):
+        return list(array)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
