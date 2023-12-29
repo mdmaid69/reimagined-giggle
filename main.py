76c05bc1ad4edb5ed4d793ed554e8f7384fa24f4
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:47:25 +0000

    tired persuade touch bin own

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
