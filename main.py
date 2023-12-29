b0c5d6bd7a02e89f6531258faf2167b412b1e2a5
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:55:21 +0000

    turnover middle rough suit wide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
