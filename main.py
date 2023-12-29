291c95bb80efa4ce83907887e3bb3b180603c160
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:52:07 +0000

    actual phone tone destroy exist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
