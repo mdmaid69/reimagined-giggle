5ae7b58a42a2942c57d5fd83f0119f437c8e1de6
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:53:50 +0000

    you replace Ll could advantage

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def is_odd(n):
+        return n % 2 != 0
+for i in range(5):
+        print(i)
