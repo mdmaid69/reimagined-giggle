82a68440e62e08df49efbedd470a11913be12592
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:48:42 +0000

    course makeup state rush proper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
