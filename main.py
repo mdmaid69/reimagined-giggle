5acd98a18e0f7acf0c9f492e4b420d63a1238add
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:54:49 +0000

    place little official against window

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
