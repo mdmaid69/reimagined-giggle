dbf3cca452a6ca197a04a3af3d43673ae9c96955
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:45:00 +0000

    sink suggest large keen along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
