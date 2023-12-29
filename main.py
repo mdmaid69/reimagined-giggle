56740c1e8264a8e19a7879e5cc4c34ea873a96cb
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:45:26 +0000

    fit too distance Qq nothing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-x = 10
-y = 20
-print("Sum:", x + y)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
