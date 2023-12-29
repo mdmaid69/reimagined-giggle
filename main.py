0b349e0c1660cdff2e7e8f82b94f5d2d6cf616eb
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:55:26 +0000

    undo exactly easy worst change

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
