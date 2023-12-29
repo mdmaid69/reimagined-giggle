f2f969b1af1adf7cc949265a12ce16964bd2ae3d
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:45:55 +0000

    at acceptable die mend attractive

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
