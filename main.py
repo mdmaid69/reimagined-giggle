e5bebc007c6c0f49135b2be01aaa6cab74a59622
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:09:01 +0000

    wash argue whatever name professional

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
