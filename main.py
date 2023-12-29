f204fdbb3801a6bef06167c63c6e1365feaf461b
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:30:38 +0000

    content score failure autumn journey

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
