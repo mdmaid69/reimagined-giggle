71e6fd663d6dfa523c54c8082037d5ea556127ad
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:51:12 +0000

    fail mail exam dance gas

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
