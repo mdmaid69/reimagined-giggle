c4aa2c28e420d90b056ebae49522fa4bab4d2d5c
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:32:25 +0000

    famous limit hard press makeinto

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
