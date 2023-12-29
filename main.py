bc2e29e8d70565e3270c200aed043f80f70d59ec
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:00:24 +0000

    vote comfort container because Bb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import time
+def get_time_since_epoch():
+        return time.time()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
