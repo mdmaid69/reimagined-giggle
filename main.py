c9238966f5e86411a0379e57848d2c89d678000b
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:58:03 +0000

    strange clearly gift declaration wake

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
