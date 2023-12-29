8815d4fda6e9a9a24a79c1deeead48e0869204ce
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:59:40 +0000

    do young better turnout dick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
