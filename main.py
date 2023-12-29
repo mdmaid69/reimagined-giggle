2420a686386491f7f0ac2fd0ac81ffeccfa017a9
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:49:30 +0000

    yet single 571enough sort love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import numpy as np
+print(np.array([1, 2, 3]))
