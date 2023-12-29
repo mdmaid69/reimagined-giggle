3ed7b7e2a8b04badfc5ab26e273de913a9ecfbf8
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:53:08 +0000

    occur kick gather depend borrow

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
