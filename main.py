72d9938dabe41fd6281313f996af03f75d899f41
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:34:39 +0000

    concerned right behaviour raise become

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import platform
+def get_python_version():
+        return platform.python_version()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
