710bca092468c0e34df9fc5ad1345519bc334cdc
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:58:46 +0000

    reach burst summer stupid unhappy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
