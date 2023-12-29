5d3c2d9a915a3438bfad65b2067424cc4bc4e92c
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:25:59 +0000

    disagree public phone direction constantly

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
