e611a829c397d72ab47432868912e5063e052c23
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:52:25 +0000

    road inside survive town wave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
