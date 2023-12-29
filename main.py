3c9aee3fe0cab95cff1dcebe9891671e11e24ac5
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:11:05 +0000

    supply closely sure off probably

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
