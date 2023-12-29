41c2831392100d4ba3a728e563a3dd8f8067231e
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:27:07 +0000

    star difference there appear calloff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-name = "Python"
-print("Hello,", name)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
