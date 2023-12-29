64d1a9c7d356f957f546f100f7bc7b89a2335116
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:14:46 +0000

    carry hope squeeze block store

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
