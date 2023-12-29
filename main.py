de843e9e1f5257a571ecc83385364802b519fc4e
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:48:17 +0000

    final unable tap regular consult

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def square_number(x):
-        return x**2
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
