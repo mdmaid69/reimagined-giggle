ab1c7e10a448d8450227e39f604a5e51278e9e4f
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:35:28 +0000

    exam invite proper search head

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import sys
-def exit_program():
-        sys.exit()
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
