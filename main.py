abf5264cfa8033c8d46fcb4827f16bcb12b4ff23
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:01:22 +0000

    say through accidentally display come

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
