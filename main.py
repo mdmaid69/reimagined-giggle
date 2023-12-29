e5ca19ccfd50f4fc7c1f471624d1ab68cca88f46
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:39:20 +0000

    unkind visit certain still considering

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
