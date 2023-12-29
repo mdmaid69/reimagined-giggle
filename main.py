f6bc132d4a87353cd61916279ae1cac6d0b87e2a
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:48:02 +0000

    tall behind string goup switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
