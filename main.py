2aa325a0260db668c6ce5978b11ff4d3d5dcfb1d
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:27:02 +0000

    admire while disapprove difference telephone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
