20d386df632ddf7212f36da26f4afac1c4b25e42
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:51:53 +0000

    note but job physical abroad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_queue():
+        return collections.deque()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
