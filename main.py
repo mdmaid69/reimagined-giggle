8ad8d2ef2a556339afd7914c6b46852937bb9081
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:11:14 +0000

    family supply relationship tooth drive

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def add_numbers(x, y):
-        return x + y
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  def multiply_numbers(x, y):
+        return x * y
