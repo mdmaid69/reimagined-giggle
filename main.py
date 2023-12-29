5702228983016399736796180982c582c10d6111
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:17:10 +0000

    yes true future subject introduce

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
