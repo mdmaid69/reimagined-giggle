215aadf88d170183007629f5e936ad8bb9e02e24
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:50:00 +0000

    pinch recently even scared soft

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
