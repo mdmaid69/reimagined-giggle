78119f8c56a0e2ed01b1712a8b0194499badba18
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:31:06 +0000

    572enter simple that war runaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
