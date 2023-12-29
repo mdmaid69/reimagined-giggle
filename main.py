c04a8f97f9e9685125b4fe925ffb51ff6913b090
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:29:23 +0000

    earth trick minority mark wild

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
