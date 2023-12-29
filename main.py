d0970a3b9bde58e604af243f7dca8af3ea7381fe
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:50:05 +0000

    almost bush fence top recognize

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
