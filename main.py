2418c90d1151dfc5c3bcdbe621079b28292e91a7
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:31:13 +0000

    stamp make ourselves visit display

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
