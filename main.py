881dd5e2f5ac5a66eb144152c02690553692196f
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:48:53 +0000

    base spite Mm bill bang

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+print([x**2 for x in range(10)])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
