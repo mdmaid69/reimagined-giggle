9e3a2e30a7bf0b3e4b3a75eb760236121fdf50e2
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:07:24 +0000

    free contact effort behave possible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def get_array_as_list(array):
+        return list(array)
