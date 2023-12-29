dc2ac11bd105a686823f2b18aee2df837b2a3b0e
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:59:27 +0000

    damage excellent takeout top height

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
