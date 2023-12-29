139d13faec4c3f3ac95dc4c20675a08d729886d0
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:34:35 +0000

    petrol stamp true ashamed agreement

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
