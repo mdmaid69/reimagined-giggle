1773e128fac9d758683bb6b8fc52168b93a4c458
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:02:25 +0000

    though goodbye any Ee gradually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
