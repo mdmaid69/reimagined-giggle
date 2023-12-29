7d90147a430146446207e1eccf5a3dee023d0998
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:00:55 +0000

    over calmly Rr sound connection

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
