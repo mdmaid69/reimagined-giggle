0e17cd1d1069639e0b1cc88e13b1bf0a62129401
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:57:10 +0000

    admire complaint future swing sorry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
