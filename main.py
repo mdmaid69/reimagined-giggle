1f17509b6a515ca014c1375987439724082c95d7
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:41:44 +0000

    profit game grow hide pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
