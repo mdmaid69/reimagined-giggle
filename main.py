4b91be1b9a9b4740f0617d18720fdc6b3dc1c876
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:18:09 +0000

    within clearly trust later create

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
