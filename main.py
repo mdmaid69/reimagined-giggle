51dd84413f609205a1d8b04722511207e376d140
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:56:24 +0000

    woman people or long even

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def remove_duplicates(lst):
+        return list(set(lst))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
