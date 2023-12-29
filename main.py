7dd2ac53ec7fffc3099d0bd4ba4e2e501862acf4
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:36:39 +0000

    burst than leave other operate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
