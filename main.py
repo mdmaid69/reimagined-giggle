79a08de5e03e9843f2a0bb6a31d851abf1afb17d
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:24:23 +0000

    flow one excuse job 565end

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_current_directory():
-        return os.getcwd()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
