dd3323ea639d1fba9e08eda98ef73b368677b4ee
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:10:16 +0000

    motion wild Yy life thirsty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def convert_array_to_list(array):
+        return array.tolist()
