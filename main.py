285dcf2e3897ab2a2399bde0824a6487a53ddf59
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:52:21 +0000

    case cheat left hair appeal

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
