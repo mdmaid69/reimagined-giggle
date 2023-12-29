f10c9b2ef5d2c8812a1e4d50a4ab892e583ee1dc
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:23:58 +0000

    hurt right delight strict difference

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import array
+def get_array_as_bool(array):
+        return bool(array)
