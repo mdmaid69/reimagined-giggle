3e4697b3d899908993fe5e7e47e237b9978af006
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:10:18 +0000

    level limit middle juice attach

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def convert_to_binary(n):
+        return bin(n)
