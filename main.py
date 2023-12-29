016676ab7f5ec8e89930aad8411f4a211bedb0f2
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:27:35 +0000

    medium beside boat Vv male

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
