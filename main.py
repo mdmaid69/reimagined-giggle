166b1a1f69967c3bceec712ae855076114c81f96
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:06:57 +0000

    middle politics train apply yours

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def count_elements(lst):
+        return len(lst)
+def calculate_power(work, time):
+        return work / time
