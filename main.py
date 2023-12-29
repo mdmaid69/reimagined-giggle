2a1eeeb06b5406855ff45a8832cdbf458be22ab3
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:58:25 +0000

    outside lie fine throw comment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import os
-print(os.getcwd())
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
