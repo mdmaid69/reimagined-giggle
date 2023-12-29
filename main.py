e0ac414b5fa7dd50f4bf16a299fe4c83a10bc815
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:43:18 +0000

    petrol bury whoever quit shop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
