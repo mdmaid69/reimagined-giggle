ba295329b50d78f57aa7cffb20ddf5f3364b678e
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:27:21 +0000

    because steam recognize guide yours

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def add_numbers(x, y):
+        return x + y
