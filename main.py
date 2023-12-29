8b21b648d442504415476f5b505e7b54ca539cda
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:23:09 +0000

    inside belief thankyou board eager

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def greet(name):
+        print(f"Hello, {name}!")
+def reverse_list(lst):
+        return lst[::-1]
