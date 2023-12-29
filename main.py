40b317a04dbd1bf49379cd81dab52c92019cba81
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:11:04 +0000

    refuse whatever character just represent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import collections
+def create_user_list():
+        return collections.UserList()
