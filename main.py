e8ae9918d74d83cce4d555bf8851a7a9a696ed3b
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:48:42 +0000

    crazy show swimming lock contrast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
