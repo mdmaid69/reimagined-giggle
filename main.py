828443a50f91c5f6b40e9ba90269e4401b06696e
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:36:03 +0000

    breath would blue win please

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def convert_to_octal(n):
+        return oct(n)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
