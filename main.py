c3ace7054cfddf224e4a0f299474469feea67548
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:41:17 +0000

    trick what along edge finish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
