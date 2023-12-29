302dc5730d2f36980241287f110bb1a3e7d113e5
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:16:31 +0000

    underneath pressure cut distant long

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
