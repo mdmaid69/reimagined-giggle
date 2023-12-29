5a870c77a9e20922194338c0eadf85fd46658da0
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:05:31 +0000

    difficult comeout interrupt remember getout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
