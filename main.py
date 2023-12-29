e8478c5b6010be6f95a9bf799354fb6bd04355ca
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:49:15 +0000

    cruel centimetre 572enter introduction wind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
