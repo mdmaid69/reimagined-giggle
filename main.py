19303d6682f70c4c03ffbecd03e0e09de36bc7bc
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:50:22 +0000

    extraordinary stop simple burst explain

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+x = 10
+y = 20
+print("Sum:", x + y)
