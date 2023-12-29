a318d2068d011f923a21cf38c12f22e094c8d7b6
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:56:12 +0000

    phone near slow direct bang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def reverse_array(array):
+        array.reverse()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
