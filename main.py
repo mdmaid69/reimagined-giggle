6a2062dd38b0fb8268e22f0a436a3d598182b66f
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:56:20 +0000

    chance confusing 566end early always

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
