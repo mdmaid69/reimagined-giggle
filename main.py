946f5b639ccc63b1724216432cc45d29fab9126a
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:10:45 +0000

    unit tough fight try cost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def is_palindrome(s):
+        return s == s[::-1]
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
