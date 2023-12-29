8f4d71dc7ffb6098f87bb26078a1a9059dd1da94
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:19:00 +0000

    walk test desperate steer noone

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
