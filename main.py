6d4d0bffc36fedf7d0c65a5047a983adee0d90cb
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:56:55 +0000

    record exaggerate recommend 567enemy book

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
