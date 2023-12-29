3d93907bab1563066c77333bbd8efe67df40d156
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:24:00 +0000

    later throughout in half they

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import logging
+def log_message(message):
+        logging.info(message)
