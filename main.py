cf3313763e6df699c1e69f21a6af63e95271fa9f
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:50:28 +0000

    acceptable Dd music purpose result

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
