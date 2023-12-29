b35b48a95ff68a3ab76a1eb3c90c1459933f906f
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:46:56 +0000

    object 564encourage squeeze break comefrom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
