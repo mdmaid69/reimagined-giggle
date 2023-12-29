bd6a0539aa9a8ebf3227a604ffe2af1692a0761d
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:53:57 +0000

    nothing air uponon bite than

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_length(array):
+        return len(array)
