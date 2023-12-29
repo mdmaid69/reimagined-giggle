199c9bbaa7f8c2ebd70cca8baa084041437df4ba
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:33:46 +0000

    like coat distant church every

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
