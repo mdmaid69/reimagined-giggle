90da887f408bca7eb443c7d11f0e4846136e7e18
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:39:09 +0000

    contents through pile what country

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
