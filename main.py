d87c24aa59297794f75b1d8f8509899f425e88f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:20:24 +0000

    any teach bite block spin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
