d92ccc9e9e2781624b44952a98f82ecf2b97c7ad
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:46:41 +0000

    count breakin understand but slowly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
