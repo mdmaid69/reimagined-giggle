2123dbfed95b3f234cc2be69280393082554cbd3
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:31:00 +0000

    section file confidence stay convincing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
