0432921239bb57fb416f44091e9391ac03113d17
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:07:01 +0000

    slide expect wind exercise sad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
