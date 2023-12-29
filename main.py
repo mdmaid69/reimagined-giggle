3613f74823d6f2edc2e70af1e310a2136c1258a2
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:41:34 +0000

    standard report otherwise gradually speed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_sine(x):
+        return math.sin(x)
