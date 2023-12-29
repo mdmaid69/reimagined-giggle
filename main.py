0a48438b825d11fa8e696b6b06d1a13bbf4d3f6c
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:23:35 +0000

    until achievement exciting material other

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
