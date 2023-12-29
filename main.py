ab2c9d933ef0d54cc86b806b90490ef4cb9c658c
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:01:26 +0000

    measure seat anywhere doubt demonstrate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_as_bool(array):
+        return bool(array)
