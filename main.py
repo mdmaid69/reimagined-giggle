d1d9f01dd0ecbebf38899be28e471f8c963cf272
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:28:11 +0000

    left service act could along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
