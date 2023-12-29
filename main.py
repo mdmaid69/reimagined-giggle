4338039906a48ab61132b6f36923e789fcc0303e
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:17:09 +0000

    myself neither shop himself getaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-print(os.getcwd())
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
