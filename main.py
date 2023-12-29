36cfaebc81adf1172496c71acf2797e23b2e6745
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:03:06 +0000

    natural generous refer handle probably

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def convert_to_hex(n):
+        return hex(n)
