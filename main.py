3731d19aff99579c95145719c85f8594640a9550
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:32:05 +0000

    square runout shame project feeling

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
