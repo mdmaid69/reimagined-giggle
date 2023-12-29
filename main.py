b641ae8aa45a9b7b01fc801b68143fce382b3dd4
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:07:48 +0000

    fat against operation attempt perfect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def add_numbers(x, y):
+        return x + y
