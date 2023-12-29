99e46a7851d24c428c3fdbe2cd80f39ce54f3e39
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:25:22 +0000

    snow miss leave bit much

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
