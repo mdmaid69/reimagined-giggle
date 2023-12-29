9453e69a5821f2d0c349e5e343daf1c607b61e1b
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:30:25 +0000

    nerve common dish unhappy go

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def calculate_area_rectangle(l, w):
+        return l * w
