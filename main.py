0c57a354504898ad6960d237f57ac98cebd92e9d
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:27:03 +0000

    club pay whistle return treat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
