9fc79044a62c6c5df6c8e5d49ac2dc3f9898d943
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:52:18 +0000

    ear usually it close argue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
