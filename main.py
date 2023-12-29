7e53b18cb2bf4dac92b4c95580ebf0c59f7cb8a2
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:31:21 +0000

    turnback herself fork uniform Zz

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_work(force, distance):
+        return force * distance
