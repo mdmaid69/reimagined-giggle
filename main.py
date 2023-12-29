10762201a4508c0e4760a7d1f9655f944b812792
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:19:44 +0000

    middle rid accidentally that across

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import collections
+def create_user_list():
+        return collections.UserList()
