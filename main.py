b93f7926683bcc4c56dd02dc1c1f44eb040421b7
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:11:20 +0000

    bank boring couple lead steal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
