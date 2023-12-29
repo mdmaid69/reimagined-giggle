24877e19303978896f671e75aa2229b62a3ad828
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:50:27 +0000

    roof rub cheek deserve where

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def sort_list(lst):
+        return sorted(lst)
