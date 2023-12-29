dcd0d524a7862c8060834c8e5954bf499eb6170d
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:41:30 +0000

    relax so worry tomorrow funny

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
