e64eacc4db46ba11b710e9aff55d8a1e7b6c3dbb
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:26:23 +0000

    recover match distant possibility settle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
