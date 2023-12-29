4f93f604a09769e7a27c5f92feda2f8667c188db
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:01:39 +0000

    past dance inquire promise sleep

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
