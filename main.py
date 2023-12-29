8d8d2f7ea886ac4cce8033b5676ad08b9e23c65f
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:57:08 +0000

    present even tap style arrangement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
