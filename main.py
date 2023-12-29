345ce84f47851c6f8480080459906d976dc74b3f
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:19:08 +0000

    wind learn like healthy dig

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def multiply_numbers(x, y):
+        return x * y
