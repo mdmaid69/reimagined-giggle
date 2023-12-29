0cb57fba16c1aa216cbb6dc5b0170ea79ff2747d
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:24:30 +0000

    understand call control study age

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
