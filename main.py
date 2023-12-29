d7210f5488c82268cb41ebc7fb820545281d6f37
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:54:21 +0000

    grade someone eat cover right

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def greet(name):
+        print(f"Hello, {name}!")
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
