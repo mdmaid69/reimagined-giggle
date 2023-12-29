b96b34b5cbf378cee3029d0410149060f37a0c06
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:46:41 +0000

    manner runinto traffic guide one

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-import array
-def get_array_index(array, item):
-        return array.index(item)
