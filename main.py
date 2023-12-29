a61ba6ceae3bf5c83e162e16498ad2ad49f64728
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:29:40 +0000

    badly please steep effect border

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import array
+def get_array_as_list(array):
+        return list(array)
