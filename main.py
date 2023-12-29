16397bdea7de333fcb8ada7015aafc62f54bf61b
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:32:05 +0000

    occasion scared awake comeoff limit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
