cb22a33dc2b76a33e278d8afcacb39c8438db8ae
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:17:17 +0000

    rather use sink birthday unless

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def find_unique_words(sentence):
-        return set(sentence.split())
+def convert_to_binary(n):
+        return bin(n)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
