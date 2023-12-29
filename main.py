a8b51e6d85f5ec21474377f75fd7f6662f79bfd1
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:10:41 +0000

    Nn once preparation interview total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def reverse_array(array):
+        array.reverse()
