45e12b17df0c15ac1228138bb7445f3a3117ee03
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:54:10 +0000

    home judge guess drive port

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def add_numbers(x, y):
-        return x + y
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
