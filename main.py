9b1429f534b83e4e93eab6db4ae1d4df496816e7
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:34:59 +0000

    loose likely discuss slightly interview

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
