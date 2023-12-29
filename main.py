e50c2494681e42e4bcdf5b9843b1ba6abe38e494
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:09:45 +0000

    nervous constantly teach 566end whichever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
