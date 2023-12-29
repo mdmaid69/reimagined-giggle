859292dbdac2002d46643b855e40cb0d50b1dbd0
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:47:56 +0000

    understand front yes especially excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
