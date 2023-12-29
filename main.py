e7966c78d4a1d57d18b9d751ba974b5d3f591f5f
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:43:06 +0000

    as repeat in tired excuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
