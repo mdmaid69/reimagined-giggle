ca10cc2d72e6b32fd902c828a4c216e5e416e594
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:53:29 +0000

    back open congratulations can flight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
