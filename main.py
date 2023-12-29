d94cc65d8e4c6ec89ce2020a02f9c61821b9c67a
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:55:18 +0000

    the involve goon little splash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
