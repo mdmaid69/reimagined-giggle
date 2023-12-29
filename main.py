981067899d7f10bf1ed3ba5f034562823e30a37e
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:03:39 +0000

    professional shock poison race fill

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_pressure(force, area):
-        return force / area
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
