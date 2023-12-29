81ff7c1c6d04927c98060b747a9b11c63acea0de
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:43:32 +0000

    get juice disagree terrible Hh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def is_odd(n):
+        return n % 2 != 0
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
