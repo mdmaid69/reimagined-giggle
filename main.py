ecbddd9ad9ff2630bb952700b97f1721f3dba4a9
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:17:00 +0000

    understand become bar embarrassing declaration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
