f12c441ea4f606e9e8ce82d06efa16ce79c91e0d
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:20:12 +0000

    smell giveaway challenge high measure

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
