2b1ecdb1ad55ce26a9f5bb20f5c8d3d4e005ae6c
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:19:43 +0000

    plate blue talkback down hide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  def square_number(x):
-        return x**2
+import array
+def get_array_as_float(array):
+        return float(array[0])
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
