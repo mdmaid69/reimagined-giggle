5122c578077762246028bba72d3bb7eeac9abffa
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:29:24 +0000

    turnover mean yes neither form

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def split_path(path):
+        return os.path.split(path)
