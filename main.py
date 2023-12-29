4bd27475cb0ca73293d3f513294d0a8a7a3c049d
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:45:59 +0000

    round building behave edge joke

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
