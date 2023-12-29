8f5a46b15141d227583dac103dca7ce49713580d
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:56:48 +0000

    pocket common sick since afterwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
