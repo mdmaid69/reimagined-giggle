a54845050151321f5d990f6240f885671232f306
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:24:53 +0000

    goout importance other recently someone

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
