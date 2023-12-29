6ca26856275ae3f8422c85a322d9a044f553352e
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:33:55 +0000

    mark tear unlucky gooff describe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
