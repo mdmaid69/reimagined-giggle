a0b6bcdf5c980fa53efe97039c48eb67aed9c4ef
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:17:58 +0000

    divide crash die side nothing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
 import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def list_files_in_directory(path):
+        return os.listdir(path)
