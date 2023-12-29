b476462a02a62b422eaf30223ba70561e3b84eb1
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:59:04 +0000

    takeover conclusion moral regard emphasis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
