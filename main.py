2f14f1ba614327d93c3cf05bdff736e955ed3429
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:39:41 +0000

    mess could used shame house

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
