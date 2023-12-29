1e3d19d8ba19c6499e760d98fcf645133c60e126
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:19:56 +0000

    band after terrible hair every

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
