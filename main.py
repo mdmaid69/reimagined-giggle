dc374940a953579397e48c2bf1b8cab5e843f163
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:12:02 +0000

    stranger love perfect square cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
