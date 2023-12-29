b1d0c370ae1ee203c451b0efedaff3ce5e786d9d
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:14:54 +0000

    chat deaf hope fresh unfair

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
