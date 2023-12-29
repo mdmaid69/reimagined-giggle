34a3e172d3863630e166d777f1c8e68e6a8cb787
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:15:16 +0000

    mainly sensible thorough rush table

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
