266ec1cfa0e4cf3c95dc8fb4ccac232b681bbb5f
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:22:32 +0000

    bye brain create meaning anxious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_area_circle(r):
+        return 3.14 * r**2
