5b1267ffb969364c116e9ea33f0f8b6b8a06ff6b
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:44:47 +0000

    without winter in rate hard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
