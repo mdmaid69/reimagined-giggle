b82dd3383c4a6054e7389309428583242f3d83d0
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:01:57 +0000

    introduction close nurse his give

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def add_numbers(x, y):
+        return x + y
