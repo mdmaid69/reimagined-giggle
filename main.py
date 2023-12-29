7cd5cac20995bfeda9a9e6b2fd8cbf03a50ced92
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:24:48 +0000

    live concerned importance book keepoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
