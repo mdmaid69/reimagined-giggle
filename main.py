92d38a9be6169e4482ab9a9f5b660de634b2d74e
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:30:34 +0000

    discuss protect possibility mad have

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import sys
+def exit_program():
+        sys.exit()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
