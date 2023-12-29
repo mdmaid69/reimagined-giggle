5df05e3ef89d68470aa7fda32340916400b3645a
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:40:48 +0000

    confident underneath border education study

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def multiply_numbers(x, y):
-        return x * y
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
