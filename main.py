c68e37ea0b8f1e75c1fc1699c09faae27e774da0
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:58:31 +0000

    deliberate right comfort bus emphasis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
