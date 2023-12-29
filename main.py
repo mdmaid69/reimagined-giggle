fd3061f848aec7252ce17dc5e924f089c9bad8cd
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:27:04 +0000

    godown succeed but odd smile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_max(numbers):
+        return max(numbers)
