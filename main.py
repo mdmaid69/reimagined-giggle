fc47ba1ed5562745d58f8682276299e493d09417
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:27:17 +0000

    introduce there rarely wonder little

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_perpetuity(payment, rate):
+        return payment / rate
