1443c3933e17b96e6ab2a259c6886584314e80e6
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:47:39 +0000

    gap circle certain question detail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def divide_numbers(x, y):
-        return x / y
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
