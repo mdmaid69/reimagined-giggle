51c5bd04bbd540e2534b76efb741c4e22e94a00c
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:40:25 +0000

    address main flow poor into

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import datetime
+print(datetime.datetime.now())
+import array
+def get_array_as_bool(array):
+        return bool(array)
