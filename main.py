9bae49f92b4c87b66bf351cec1373e8a5187edd6
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:28:35 +0000

    earn right us form crash

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def subtract_numbers(x, y):
+        return x - y
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
