aeeb83bd6d148a6fcaed21e7a145cdaae614ac92
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:59:11 +0000

    pencil noisy look keepaway physical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def clear_array(array):
+        array *= 0
