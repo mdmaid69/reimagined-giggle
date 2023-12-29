5bfcff70426c4e97edfff7e07257e94391819728
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:36:31 +0000

    failure embarrassed unit succeed switch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
