e6d4b913b18e16e58d10793a52cbec69bbfc414b
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:32:41 +0000

    be special command light weigh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def reverse_list(lst):
+        return lst[::-1]
+import collections
+def create_user_dict():
+        return collections.UserDict()
