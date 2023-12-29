26e5008b4be9f0b379c2b67196aaceee90896939
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:04:31 +0000

    ride machine convenient summer unhappy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def square_number(x):
+        return x**2
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
