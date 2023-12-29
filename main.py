8d6085c62c5fc4ee49ed4a5b154e87cc129df2cc
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:31:15 +0000

    exactly taste gas mysterious wire

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
