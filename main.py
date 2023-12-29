74d9cebd60e540d83cf058b792bb52aee20c2fa8
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:36:43 +0000

    walk string away somehow tune

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
