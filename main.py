169ec32c8b0a7aaadaa41fa51d34d905ab0d5c94
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:16:47 +0000

    temperature Pp move floor up

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  def count_elements(lst):
+        return len(lst)
