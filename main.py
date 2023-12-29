da5e7b2b43634016bf6e617664dbed8b5881ec36
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:55:44 +0000

    hold shop plenty invite pool

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def count_elements(lst):
+        return len(lst)
