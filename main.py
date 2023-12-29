bcd8760707252347af4e176550e23213ef1114f2
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:37:05 +0000

    only culture murder store claim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
