0da32ae22bf1888baaa572537376be3f05a5170c
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:18:46 +0000

    entry geton negative sure identify

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_as_bool(array):
+        return bool(array)
