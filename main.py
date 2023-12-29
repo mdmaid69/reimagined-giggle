96e75f869ffa9ae4f5b4f577b32234061beffac5
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:09:27 +0000

    very basic lot treat mainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
