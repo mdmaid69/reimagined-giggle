a9c47f7d690c953a2eb533defa106502d6f4c737
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:19:23 +0000

    ride method study past declare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
