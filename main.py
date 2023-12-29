53ca00d3126f2f7b412991b9e34c8afe86988830
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:50:59 +0000

    consist feel communication finger debt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+x = 10
+y = 20
+print("Sum:", x + y)
