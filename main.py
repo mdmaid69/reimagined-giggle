94f112c6f1e838825d07618145793948f33794f2
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:58:42 +0000

    warm keepout main no argument

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def convert_to_hex(n):
+        return hex(n)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
