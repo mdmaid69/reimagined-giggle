0c08ece4b3d5d902b4454499266be4b42fb6a047
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:02:05 +0000

    rob within day wind month

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
