b29d7d520703170228ef423daa5e824d8027c4f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:06:41 +0000

    best Nn run why passage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
