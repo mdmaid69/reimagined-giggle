8e0a04fbdd6c52df75c89edbafc96ae77a7de6a4
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:30:01 +0000

    never wealth replace guilty eye

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
