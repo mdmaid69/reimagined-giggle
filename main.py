43f8b9769bb04a9eae85a3e619f14b68dc6f7e52
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:46:30 +0000

    demand action earth Cc exchange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import platform
+def get_os_info():
+        return platform.uname()
