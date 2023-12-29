cc8c2e785fe8d5af4a8deddcfcb9baf7aaa5908d
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:39:20 +0000

    display stop present preparation weekend

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
