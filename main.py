44ced347236588c545dab48fbd9113429fe475ee
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:21:40 +0000

    whichever move anyone treat hello

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
