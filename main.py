61db9496968c3c8e0c74dbf5fbb287718f03c859
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:57:20 +0000

    sign geton runover remember violence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
