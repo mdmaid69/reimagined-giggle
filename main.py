62ff40126a48a2d3a743f811ca4a5c1ded12a409
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:22:02 +0000

    goup any note schedule different

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
