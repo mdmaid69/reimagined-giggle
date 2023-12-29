b017285da5f0e54db98907e5a6519fbeca6c7b5a
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:33:26 +0000

    better turnup switch several she

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_power(work, time):
+        return work / time
+def find_min(lst):
+        return min(lst)
