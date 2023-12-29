dd64721fbf6e891cbf84a04c4f73833d1fbbcbec
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:10:21 +0000

    report over uniform treat imagine

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
