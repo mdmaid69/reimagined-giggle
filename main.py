7d63dfa9054f4c90f91c290f25445e9c581fa5a1
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:50:25 +0000

    rest previous workupto familiar small

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
