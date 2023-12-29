603f4932ba081f5d63f5f47e81c563c7758655e3
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:02:48 +0000

    trade equal lift bath afraid

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
