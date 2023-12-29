a59a62bd6fc1238bcb3096956131460520271dd5
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:59:49 +0000

    slow reply survive quick ring

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
