00287c7b234df75e74160a54ea9db3612a23e1bf
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:03:34 +0000

    confusing eager nose must Ee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
