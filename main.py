2dc5f5b46f543815ea5f9843d7bfa7290db266e7
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:36:52 +0000

    any anything benefit married build

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 numbers = [1, 2, 3, 4, 5]
 print("Squared:", [n**2 for n in numbers])
