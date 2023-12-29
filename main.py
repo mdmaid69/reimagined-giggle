54ed1b8a3295a38acd5768f236c98ccd1591f5c5
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:41:52 +0000

    stick pass tongue apologizeapologise immediate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
