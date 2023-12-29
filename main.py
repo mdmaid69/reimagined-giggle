a05989f9dc08f9c88a003a173d81a06b82250ffc
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:40:58 +0000

    movement ring dare display bye

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import os
-print(os.getcwd())
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
