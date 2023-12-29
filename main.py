e16c4aa86f0f5631624f074823eef36bf39ce68e
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:13:16 +0000

    level maximum goaway considering view

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
