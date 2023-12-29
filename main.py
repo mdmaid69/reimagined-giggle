23f8aa66d27f839ffc57ce2f66784b2161cd25c3
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:44:14 +0000

    continue bite while body sight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
