c051ed7b649f7b181b9b3e12634430386468dd4b
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:26:22 +0000

    defend goback(to) this bridge play

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
