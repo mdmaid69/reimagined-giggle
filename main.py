b05990ae0af1c093620b757fe5e9423630d0069d
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:16:01 +0000

    run tomorrow me side port

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
