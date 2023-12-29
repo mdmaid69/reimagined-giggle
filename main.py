7dd41bf1d9af0b7e6b168439d7e885929c5b6182
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:57:56 +0000

    ask proud force in bank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_exponential(x):
+        return math.exp(x)
