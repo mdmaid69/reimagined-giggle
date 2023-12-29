3df18fd583ec2e8b33b52a344c241b95558fa935
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:31:21 +0000

    movement minimum about closed top

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
