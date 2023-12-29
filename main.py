d9aee0e47788b9900c0829f5054cf9a8f0b5631e
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:29:25 +0000

    avoid month takeover both unit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
