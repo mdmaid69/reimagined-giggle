fd8e97dad2cc09b46985e72489427f2d7104b155
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:27:12 +0000

    often Bb runaway weigh Ll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
