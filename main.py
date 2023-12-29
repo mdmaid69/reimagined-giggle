8f87adce405199ebe44b0454ef1960767ad863e9
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:46:30 +0000

    responsible on whatever people bunch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
