20e4b96cd94c1b9f94e2db2498aece5f3ad9b890
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:40:24 +0000

    agree ball anywayanyhow join shape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
