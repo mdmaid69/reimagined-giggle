c038dd546a031bb6aac92ba58c1e30221b92912f
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:21:02 +0000

    but absolutely occur fork say

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_power(work, time):
-        return work / time
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  def convert_to_octal(n):
+        return oct(n)
