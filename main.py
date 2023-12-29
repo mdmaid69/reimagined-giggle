997b3dd63709e9ba5396926dcc350e4d6cdf073e
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:07:25 +0000

    exercise Itself stretch answer hold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import collections
-def create_stack():
-        return collections.deque()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
