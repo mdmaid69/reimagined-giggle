9485a023b3407cfc35e401afa030da4be675b47d
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:23:53 +0000

    yesterday bag ride plan apart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  def remove_duplicates(lst):
+        return list(set(lst))
