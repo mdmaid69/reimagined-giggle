69c6adba932ddaec2791bc3cc0cdc0924f473efb
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:09:09 +0000

    seem gradually Ff empty upstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
