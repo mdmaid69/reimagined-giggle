4dae7f056b2b4d227825970944efebfee598dd5c
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:00:32 +0000

    already notice although love steep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import sys
+def exit_program():
+        sys.exit()
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
