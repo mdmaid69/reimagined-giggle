1bce41076e348c051927595dbbb91d6aac13f5cb
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:57:43 +0000

    extra I violence sign before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import time
+def get_current_time():
+        return time.ctime()
