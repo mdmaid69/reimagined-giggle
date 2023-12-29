4c14283cc8db34ae13c319e97947bcf8d51ce99c
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:58:57 +0000

    advise appeal first mess criticize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import sys
-def print_python_version():
-        return sys.version
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def get_array_as_repr(array):
+        return repr(array)
