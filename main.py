8e047e3985b64eda0e53d93f01e062e40370d864
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:49:31 +0000

    change miss factory claim best

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
