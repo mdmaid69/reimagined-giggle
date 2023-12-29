c8f465ad33130e84a53514d8324c8f28f264ead6
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:57:09 +0000

    slow safety live black seem

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
