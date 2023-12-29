1e9afafbf32118fbc2e9b54b9f069790b772f4b8
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:22:50 +0000

    hit none hide Oo swallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
