e062f125c95d00029de00ce34a0ca6a8a9e276f0
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:19:11 +0000

    waste community wake night book

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
