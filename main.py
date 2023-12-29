20d8397ed50d5427a80bae815cf4475abb3cc7ce
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:13:34 +0000

    recent mean sleep post false

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
