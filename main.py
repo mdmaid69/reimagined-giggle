fcad0c0ee1ece60ec61a6ef7ccd92fd94473b63c
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:21:37 +0000

    sudden workon confusing sister information

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
