3dcfae254682d356e0d9d4ae5684a4cf159231b9
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:07:37 +0000

    prove manage disagree pause bin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
