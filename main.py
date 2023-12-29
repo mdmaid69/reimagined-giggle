286469e911ee9ba625aba88e920f1a328962e324
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:12:59 +0000

    roll owe food represent direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
