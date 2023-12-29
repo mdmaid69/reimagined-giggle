a1acba65cf10de0183e2d49a6b498faae2e41dd1
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:47:22 +0000

    handle recent getback somehow explanation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_index(array, item):
+        return array.index(item)
