1bcb8ea972536d38b6e5f2fe2549c7cfabae00d6
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:23:00 +0000

    beautiful variety moral smile serve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
