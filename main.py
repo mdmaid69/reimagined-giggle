f3510b8b1e6eabe12b6bcfd84723b7acfe83cacb
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:35:13 +0000

    special play borrow plastic otherwise

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_current_directory():
+        return os.getcwd()
