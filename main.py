2e4da4794501d5d91523d26f1c6adb565b7451aa
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:39:11 +0000

    add question reply tell danger

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def add_numbers(x, y):
-        return x + y
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
