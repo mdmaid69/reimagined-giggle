17be34614fc50339b42f3af29785419666a2cc9f
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:58:09 +0000

    normal call worst broken nose

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
