d459dd77b761f7929ae335f113f0eced2b5adb94
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:59:24 +0000

    sorry change feel post abroad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
