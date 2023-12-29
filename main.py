2373568e0301d171e5d285264f03f8954527ea37
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:48:59 +0000

    door happy break bite failure

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+for i in range(5):
+        print(i)
