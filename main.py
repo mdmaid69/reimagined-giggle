11511b6d0794c693b1923baba6ea463a23307e94
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:39:04 +0000

    authority same rubbish contents unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def convert_to_hex(n):
+        return hex(n)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
