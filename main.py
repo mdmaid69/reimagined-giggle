955e648f6ef1d4cb9539a0ced75ef9edafd45c46
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:01:05 +0000

    might old unfortunate subject wheel

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_power(work, time):
+        return work / time
