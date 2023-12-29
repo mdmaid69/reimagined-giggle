e1f595e0365c4c215126709fc02053e05a12bcef
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:17:08 +0000

    happy workupto fix until vote

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_work(force, distance):
+        return force * distance
+  def remove_duplicates(lst):
+        return list(set(lst))
