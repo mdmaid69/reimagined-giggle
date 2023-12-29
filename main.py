f7f7dfcbfa5e04c107d6e9836f075d4b1e80db1b
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:14:12 +0000

    reply slightly content condition wealth

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import platform
+def get_os_info():
+        return platform.uname()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
