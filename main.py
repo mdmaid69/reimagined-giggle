1934458c2c0a497a8206c27f29b05e785ad84985
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:28:53 +0000

    dish repair lookup inform report

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
