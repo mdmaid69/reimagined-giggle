cadd79b585e05ec4060e77e9dc8831ac43f42118
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:46:41 +0000

    divide frightened opinion marry slow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def sort_numbers(numbers):
+        return sorted(numbers)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
