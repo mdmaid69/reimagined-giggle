f3e32b611c7209c7d964305542ec45d32499ceb8
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:54:25 +0000

    runinto try proof spray foot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
