db105dc50879ec16057bdb768368ca6b0f34be98
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:17:35 +0000

    clothes frightened contribute sight takeover

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
