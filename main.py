978f375ae2e9fb11491eb3bd440fc8d5f404943c
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:21:50 +0000

    creature in guide object plastic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
