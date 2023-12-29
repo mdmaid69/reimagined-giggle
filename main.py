a98d493e7cc85d85c41c51a37146d34cf99325cc
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:59:24 +0000

    extraordinary accept hers tend pronounce

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def convert_to_binary(n):
+        return bin(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
