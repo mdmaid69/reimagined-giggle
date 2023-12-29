83a14d66e803a46190f848dba45a362cda773b49
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:38:50 +0000

    injury for joke classroom afterwards

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def multiply_numbers(x, y):
+        return x * y
