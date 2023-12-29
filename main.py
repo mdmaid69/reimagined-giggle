e42e805c97b5956180ce435f2aad76d24004e9f5
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:03:07 +0000

    science agree bank once normal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  def convert_to_binary(n):
+        return bin(n)
