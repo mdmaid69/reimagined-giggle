b72b8ca73c2e6770561dbaf6f9a79d32e7a5e855
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:13:55 +0000

    original issue trap supply within

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
