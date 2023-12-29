25557924bc0db17ec45f3b1abea057c3476a6e87
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:16:29 +0000

    negative instrument tip goup raw

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
