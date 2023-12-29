1d2dbb8f9c537f245e74a36f4787d2e3ed0de78c
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:37:56 +0000

    read store art scare charge

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+for i in range(5):
+        print(i)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
