91e336312d44f0ba3be524cd2240f172ce5a75e2
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:21:08 +0000

    experiment inside shrink mad home

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import math
+def calculate_error_function(x):
+        return math.erf(x)
