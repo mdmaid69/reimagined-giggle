6ffe306a93273129d9ee34afb28fe0f25a7ce889
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:02:45 +0000

    post charge fly part exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
