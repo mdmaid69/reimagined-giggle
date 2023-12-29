5fea0ac6f25d43ba2cbf726831294e18124673fb
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:31:18 +0000

    Dd us contact likely copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_floor(x):
+        return math.floor(x)
