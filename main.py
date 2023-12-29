6db4fabe9588db23d9b7f4c3ca8588b0a296d5ff
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:25:18 +0000

    unlucky left criticism steep sign

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
