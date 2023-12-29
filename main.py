31f0921b5374bd227d7c878f62dc3f105e3f7cfb
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:23:50 +0000

    divide model strong swing later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
