5ac313369aafe4eca46caa2d0d8fe5b69d93a41a
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:41:54 +0000

    discuss high try Ss up

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import os
+def remove_directory(path):
+        os.rmdir(path)
