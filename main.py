cc69c2400dd4b19fc4abd9e6bb2f942fb9667ea4
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:18:46 +0000

    stir measure programme choose imagine

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
