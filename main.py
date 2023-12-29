6540355dd4f10524992091f0ca2b3182aac23da8
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:34:59 +0000

    juice somehow bottom happen top

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+print("Square numbers:", [x**2 for x in range(n)])
