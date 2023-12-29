aff27994e0464ec60c024289ee828dbed578f9e0
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:35:26 +0000

    turndown indeed invite mysterious word

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
