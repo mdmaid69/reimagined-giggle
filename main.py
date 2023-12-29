ae88033251ee0e172171ddf7e327843748084977
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:25:01 +0000

    smell noisy section could into

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
