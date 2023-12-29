63d5f82c9a5b93e0619c83ccad62fea7fea929e2
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:38:06 +0000

    cry manner laugh thank sail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
