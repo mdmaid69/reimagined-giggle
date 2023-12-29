5f321abb67ecbc6ba791bf0ce80c933b79c452a1
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:12:33 +0000

    success less name improvement satisfactory

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
