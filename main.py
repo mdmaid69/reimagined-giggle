187826a3ae9e0256b4f134245e3b42c07ce99cd3
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:02:41 +0000

    simple lonely circumstance knock play

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import logging
-def log_message(message):
-        logging.info(message)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
