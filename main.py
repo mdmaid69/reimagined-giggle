a6f3c413ad3c0fe0a627f3c0147bdc464c8e47ae
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:57:54 +0000

    interested meet curious know shallow

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
