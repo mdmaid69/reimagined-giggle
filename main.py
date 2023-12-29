ce90ea737bed8c2b9309e38b4840774dc29842f9
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:56:03 +0000

    steer birthday deliberate event unlikely

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import random
+print(random.randint(0, 100))
+def is_palindrome(s):
+        return s == s[::-1]
