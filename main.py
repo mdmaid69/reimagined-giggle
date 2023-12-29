bb82f6f69dcaf44207e01084671de7c41ad5f27a
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:24:27 +0000

    steady wrong combine set border

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
