42245daa2215d67979df09ea35e8858673ab30b2
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:43:56 +0000

    aware to action handle dear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
