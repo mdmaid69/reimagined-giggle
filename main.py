be1d62619102a899a539a31c580b541aa32b7b95
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:52:07 +0000

    closely maybe travel report her

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import os
+def remove_directory(path):
+        os.rmdir(path)
