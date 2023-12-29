d4589b4f420214c940b3b5c1a0e3815459f75221
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:31:37 +0000

    means smile occasional admire part

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
