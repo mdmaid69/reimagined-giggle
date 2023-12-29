f1706c818f2ad648a767830fab42abca33594e7e
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:08:25 +0000

    light mood question frequent sport

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def is_even(n):
-        return n % 2 == 0
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
