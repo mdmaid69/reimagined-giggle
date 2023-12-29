c2b6d904154b69ca4b9522313ca61519f5f30e8c
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:00:16 +0000

    emphasis middle confusing approve cold

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
