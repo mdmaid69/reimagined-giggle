457d193a6e001e4d77e4e7dc874198aea1dfda3e
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:54:53 +0000

    now explain song manage cry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
