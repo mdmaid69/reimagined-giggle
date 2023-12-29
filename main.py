b6e65173b562aa327d4b2f86417c757a9549e512
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:27:46 +0000

    regard hollow refer pinch general

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
