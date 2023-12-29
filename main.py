ea47d2ff21edd5b929a2e02c5725cfdeb92f9243
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:04:54 +0000

    score blow pause pretty careless

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def divide_numbers(x, y):
+        return x / y
