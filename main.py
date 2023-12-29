4c0f9a4d325ae98a712fa94adfb908c6e588cdd5
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:48:43 +0000

    behave game sad aware increase

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def add_numbers(x, y):
+        return x + y
