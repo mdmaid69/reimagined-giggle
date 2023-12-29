931bce6d883cd864e420e9020f783b8377621f74
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:24:08 +0000

    project owe underneath handle cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
