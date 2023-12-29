e797bb0649235233a1300855b966df285d1b4afc
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:27:14 +0000

    excuse plenty guide at also

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
