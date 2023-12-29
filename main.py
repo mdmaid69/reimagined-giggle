5445c4dfc943f413c451ba3f97cff0284d753f16
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:55:46 +0000

    fear mouth accept complete become

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def sort_list(lst):
+        return sorted(lst)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
