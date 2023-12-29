ccbb895eae5b748794c906aba543b64f589b5c27
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:13:44 +0000

    keen care police course last

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_volume(length, width, height):
+        return length * width * height
