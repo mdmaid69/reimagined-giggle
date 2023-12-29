56a50b9286dbc621cb91f5ee5fc0f6121b413730
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:33:22 +0000

    friendly intend life trick suit

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def is_odd(n):
-        return n % 2 != 0
-  import sys
-  def get_python_version():
-        return sys.version
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
