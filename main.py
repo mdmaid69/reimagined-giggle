b5833f50662c95ce96ccaa53a2e981caa813394d
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:11:06 +0000

    comeback anxious behind grateful build

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import sys
+def exit_program():
+        sys.exit()
