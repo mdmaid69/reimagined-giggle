e5249d6a7456741dfa153449db1d2cdc32ac03c9
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:21:33 +0000

    head band different law arrange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
