fb7990ca3daa5afd95b1997fdca7d647ed8e7e89
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:06:23 +0000

    cinema carry health serve question

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
