d5b442e06cea741ab614fb43fe6cf581a1988658
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:51:45 +0000

    brown call responsible estimate communication

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
