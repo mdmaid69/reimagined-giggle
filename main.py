9e2b0f3af5efecfb79f0d14860f9e83273a21b5f
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:02:59 +0000

    capital shop throughout translate increase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
