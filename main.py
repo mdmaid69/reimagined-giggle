4c840241560488fba11ebe2b8550c435c4c97c3b
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:40:42 +0000

    snow interesting 570enjoyable comeup thorough

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def check_if_array_contains_item(array, item):
+        return item in array
