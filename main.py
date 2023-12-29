07caaa647bf17d5dcc2a231b3f5fe1e5eda5a60f
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:23:13 +0000

    ability for especially straight use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
