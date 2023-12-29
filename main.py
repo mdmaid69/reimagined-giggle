5420e0f7c4af57e9ac5c82968ccb271123b9d2de
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:51:14 +0000

    smooth once commit praise force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
