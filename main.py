81de4c5d86d76f60f50da8b1058a5220b1c16b9a
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:02:12 +0000

    together friend minimum suckme active

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
