be60887940e5205acb0a320182b64ff00a4d578a
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:48:02 +0000

    thin awake suddenly lesson open

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import sys
+print(sys.version)
