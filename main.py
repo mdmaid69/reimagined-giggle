654664441918e35c84441be0a67c071181e5422d
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:21:09 +0000

    connect gap really combination journey

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
