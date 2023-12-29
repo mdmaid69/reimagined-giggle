9e483745cf2a66cdaba2dad48828ddec41139953
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:04:27 +0000

    father happy bound whole find

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
