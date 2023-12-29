21a096c25df69b015c72fa066f8c09827a7405c6
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:17:04 +0000

    calmly over at combine hard

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
