2a6e3f3413af729171b748aad119c188521b330c
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:48:25 +0000

    switch mind hospital wrong right

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
