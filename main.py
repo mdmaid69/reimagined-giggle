4d2ed850f484bb6a432d14bba257ed160f5c3dc9
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:22:31 +0000

    handle appear shut relation also

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
