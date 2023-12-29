18ff71521bf338e4764dbdb522b5147e10e408e9
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:17:43 +0000

    nearly long generous thin yesterday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
