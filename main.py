17d985e541ba2f35cdb0d3d251de2c93bb6cb28a
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:33:34 +0000

    turnoff reject land light square

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
