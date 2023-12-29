6d41502daedac365af65d102e20d20ec89a33799
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:55:46 +0000

    autumn lower soft content chance

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
