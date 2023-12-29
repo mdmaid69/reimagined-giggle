5ac312c4d48e0ecbf8ab1f5a61e9c7131d1083d1
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:28:50 +0000

    tone quit mix store contrast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def divide_numbers(x, y):
+        return x / y
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
