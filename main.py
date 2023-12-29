b5f105d23e617794c1cf4a97748607a9cd6e8690
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:48:03 +0000

    succeed sit style ride call

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
