2e74ee5f1874985047e3b1479eadf759a8fbc26a
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:59:39 +0000

    burst community now between ask

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-print("Hello, world!")
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
