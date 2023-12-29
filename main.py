39bc2f55810c6c35e5c70093bf407d5fa946cace
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:57:46 +0000

    respect holiday some active runinto

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  def reverse_list(lst):
-        return lst[::-1]
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
