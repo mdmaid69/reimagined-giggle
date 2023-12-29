c2952241bfaecb4a13989e5f0996d1421cab2206
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:36:59 +0000

    pollution danger church persuade room

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  def is_even(n):
+        return n % 2 == 0
