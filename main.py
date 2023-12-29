f22acf2e1ecda30072627396848641d97971f728
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:11:15 +0000

    dry perform terrible pencil how

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_array_as_set(array):
+        return set(array)
