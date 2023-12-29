b34d4acf16bceb74a64b4d06237ef8646919cea7
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:07:50 +0000

    someone office who relation over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
