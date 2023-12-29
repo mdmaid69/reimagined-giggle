4d6ad15f75b8e323fc1479203a6d17994038d542
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:01:51 +0000

    into difficulty direct trick impression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import json
+print(json.dumps({"name": "John", "age": 30}))
