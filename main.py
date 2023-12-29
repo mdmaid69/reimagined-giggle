d76ca301e0a6517d63ee2425bfbbafa3bd720bed
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:17:50 +0000

    nonsense hurt bin check exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
