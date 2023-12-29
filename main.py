b027012f50e066b8e755b5f6cd5316724aff5709
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:24:42 +0000

    geton possible yourself twist tense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
