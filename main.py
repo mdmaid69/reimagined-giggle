62a5e8218925419ac125eccd5e15a7a14ca94b5e
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:57:24 +0000

    comeout complaint phone perfect place

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_list(array):
-        return list(array)
+def find_max(numbers):
+        return max(numbers)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
