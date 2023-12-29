5ace41cd52dc9e1ef0fb1905f8d6568f27dd497c
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:23:44 +0000

    bother alone begin herself object

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
