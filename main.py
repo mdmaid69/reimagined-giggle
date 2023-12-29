ce90afa34fffb9b95bba01ee9a5400f2bc6acc4d
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:59:33 +0000

    complain cry better herself limit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
