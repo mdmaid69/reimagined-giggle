e9b0ae97d8d9190af765c77259b4ec3ef29c74e2
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:35:47 +0000

    getup search influence outside increase

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import sys
+print(sys.version)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
