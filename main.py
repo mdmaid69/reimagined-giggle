65e8df5d6b4b486a96fa330bf880c9b51d6752ce
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:27:31 +0000

    week compare concerning spend attitude

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import datetime
-print(datetime.datetime.now())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
