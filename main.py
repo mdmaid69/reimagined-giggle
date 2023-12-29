0967d68b09707c237c259b29cc3bd5094ab85ca7
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:20:47 +0000

    clothes behave secret Hh turninto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_acceleration(speed, time):
-        return speed / time
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
