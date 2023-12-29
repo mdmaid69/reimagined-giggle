ff1a1b71adee5e530466d1f2bec7f901671d60e4
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:59:10 +0000

    hardly variety the medium excuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def reverse_array(array):
+        array.reverse()
