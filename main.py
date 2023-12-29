6fb49edee2c01cb3a9d7a202947a136a9fae7e92
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:06:37 +0000

    sink workout but busy correct

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_work(force, distance):
+        return force * distance
