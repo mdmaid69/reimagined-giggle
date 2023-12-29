15a44a78b77825fd8615dfe45fb6b3180daefd5b
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:52:16 +0000

    still comeout great field activity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
