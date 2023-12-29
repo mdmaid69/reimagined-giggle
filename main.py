521f042cf641e6bd4d9a3da50cb4d32a2c40d930
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:36:16 +0000

    picture trial rule emphasize temperature

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  def subtract_numbers(x, y):
+        return x - y
