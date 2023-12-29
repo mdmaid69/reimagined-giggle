0c3299a72cde33110c2d8ce9a29b08a618be19fd
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:10:07 +0000

    sight equal some top steer

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-def find_min(lst):
-        return min(lst)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+print(sum(range(10)))
