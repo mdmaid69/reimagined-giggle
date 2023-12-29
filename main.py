86fa557e38034a3e1ddc2591325cc290e076a658
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:40:32 +0000

    organize than shop already country

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def calculate_power(work, time):
-        return work / time
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
