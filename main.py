71f877d988e6a13a1eab4f8a4035b98c1ef5bdbc
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:12:32 +0000

    refuse familiar desperate the disappear

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import collections
+def create_priority_queue():
+        return collections.deque()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
