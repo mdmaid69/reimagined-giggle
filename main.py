28be6c38fed912693f831f8a92527915e38754ef
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:04:07 +0000

    speed sight accidental mind any

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_stack():
+        return collections.deque()
