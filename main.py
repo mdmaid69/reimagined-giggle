5172c94d43a83e06f60c39c16f57171ce42b39e6
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:38:06 +0000

    nose along I backwards attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import random
+def roll_die():
+        return random.randint(1, 6)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
