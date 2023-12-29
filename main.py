1a548dd149c3141e222c6377384fa93f4bc89e86
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:51:46 +0000

    unfortunate most escape support future

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def multiply_numbers(x, y):
-        return x * y
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
