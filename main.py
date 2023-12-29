6461c5f81f7d312bc2620882f910dc920fda6538
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:23:23 +0000

    phone cause worry official yes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
