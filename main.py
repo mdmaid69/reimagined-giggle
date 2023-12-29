b251fcf74785445134133ca2f02181aa263bc3e0
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:19:02 +0000

    perform turnon record when return

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
