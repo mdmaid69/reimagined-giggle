39d28ba045a4e1d3db4c3220d764abf35e4b6fd5
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:37:47 +0000

    flow century ordinary compete mark

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
