940f723ed2167919811b8b7cc841f6fe5747178d
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:35:18 +0000

    search tread change respect rule

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import collections
+def create_counter():
+        return collections.Counter()
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
