70aeab6a69ea7954ae4455b8716d079ad0b7130f
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:55:36 +0000

    winter train and flavour path

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def cube_number(x):
+        return x**3
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
