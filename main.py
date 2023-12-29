c643900c14b2a4c07b2c341bd1a9dd7988ca53a1
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:18:58 +0000

    form talk powerful little error

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import numpy as np
+print(np.array([1, 2, 3]))
