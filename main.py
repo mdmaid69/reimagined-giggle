6307ec9cf57ab771336cbdad5d57549699a8f728
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:24:26 +0000

    hurt pass sort worst bang

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
