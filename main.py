5235b07d1dfbb91906f107e814376a40e43ccde5
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:59:08 +0000

    material precise card past report

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
