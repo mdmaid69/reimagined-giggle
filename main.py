1779bd69ddb3c9d3f10c85fc8e059fc58c9ce83c
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:53:20 +0000

    wide spill succeed tidy disappointed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def remove_duplicates(lst):
-        return list(set(lst))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def find_max(lst):
+        return max(lst)
