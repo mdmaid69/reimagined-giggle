a24f06c3c536c4d87d59df77d77b1ca9da65aa90
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:59:00 +0000

    formal appeal occasion rank term

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
