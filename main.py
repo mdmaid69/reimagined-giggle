0c1536b0ca7bb701c40d566aa381686c77eeee60
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:31:00 +0000

    lesson name ring safety track

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
