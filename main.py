a64a5047bad2cd4004b3c21effd88e5ff7731285
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:28:24 +0000

    still stop several Mm busy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
