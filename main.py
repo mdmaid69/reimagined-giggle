b3855899c6f3a9309fb66d56d7b42ba0f77a7b3c
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:45:21 +0000

    record murder drink high sister

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
