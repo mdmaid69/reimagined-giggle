9e904436bfa473e49ee94dda748a0a975c6f5a6c
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:54:00 +0000

    middle comeup hardly wet move

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def convert_to_binary(n):
+        return bin(n)
