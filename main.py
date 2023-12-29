d9548ef0e952462aba2eff0bedf8d2f2eb894b79
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:33:53 +0000

    stupid turn on see judge

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
