0698cab75eebc5dcc986ecd3f7a69eef0226435d
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:22:11 +0000

    ordinary opinion politics heart top

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def multiply_numbers(x, y):
-        return x * y
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
