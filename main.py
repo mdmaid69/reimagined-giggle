017b64f5776e6db1944dec522fc5e7f376ddb297
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:41:26 +0000

    will traffic then fold sudden

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def sort_list(lst):
+        return sorted(lst)
