624119670bac5c5925634cd8a97c8dd972bb38f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:14:15 +0000

    Ss scrap within apparently lead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
