6ec1930542b98b78d8a2a2e2c09a4a7ba8108d6a
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:52:49 +0000

    willing because earn steal thank

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
