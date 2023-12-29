4b40c5b0626a200515af9d82383a1fb09ec5579f
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:27:27 +0000

    sort breakup collect middle demand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
