8eef7c04e953be8ff7a1fb2f24031c8c335fa594
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:56:19 +0000

    round central match tomorrow probably

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def is_palindrome(s):
+        return s == s[::-1]
