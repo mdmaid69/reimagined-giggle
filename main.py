c0bb223fc6c06ca93dd199798278a8e5f74400c0
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:42:39 +0000

    carry swim easy talk big

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
