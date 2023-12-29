f6b3cf19ce86d644d548dba5aa6461bbd7e4a8bf
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:24:53 +0000

    nearly between trade criticize either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
