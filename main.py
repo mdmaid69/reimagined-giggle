52a7bb57a87f8bc683a9dc205c0462e41abeb6f6
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:11:19 +0000

    relative region office win platform

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
