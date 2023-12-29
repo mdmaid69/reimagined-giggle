7b21ebbb7285c21c1661b17cdb63e327f93e2ead
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:41:54 +0000

    joke vote foreign mention 565end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
