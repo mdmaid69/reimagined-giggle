afbafc6f1a897653abde670e5c9d792cb706b752
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:22:12 +0000

    single inquire union similar organize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
