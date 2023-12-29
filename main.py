127da42f846e2630b18906269e978c2ac1017fbe
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:02:47 +0000

    gap sorry convenient shine Ll

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def is_even(n):
-        return n % 2 == 0
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
