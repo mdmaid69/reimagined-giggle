81a5326b12adc5c60342720d985edc336b1dd96d
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:16:08 +0000

    container phone evening downstairs breakdown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
