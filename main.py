7cedfb07c5be59f85e9744f1dbe69e4676cfea5e
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:28:06 +0000

    lay eat maximum inform nature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_acceleration(speed, time):
+        return speed / time
