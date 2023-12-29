012e95c285571ecdb2403bfd4316c1acf15b2008
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:36:53 +0000

    keep disagree far go rub

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
