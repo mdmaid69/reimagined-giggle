de1bf2ec5955f32e5beac5cc6b3633e394174792
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:29:23 +0000

    scrap space talk reasonable connection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import sys
+print(sys.version)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
