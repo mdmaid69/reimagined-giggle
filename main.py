c64827774d24f24f4471b84043c26b9fa3cc6063
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:23:48 +0000

    crime wake neither crisis sentence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
