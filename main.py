05b6b62b6c51714b986633f778bc0e0726826988
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:55:43 +0000

    address prepare experiment drawing main

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_pressure(force, area):
+        return force / area
+import collections
+def create_user_string():
+        return collections.UserString()
