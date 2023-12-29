60ba8a0219023d20be16b996b0104be1f67c30f0
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:56:02 +0000

    combine noisy error original mix

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def greet(name):
+        print(f"Hello, {name}!")
