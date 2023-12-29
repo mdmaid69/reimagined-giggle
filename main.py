b70f59cfc37c0ef649bfd0b1518501780fb17b10
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:57:29 +0000

    book intend thing control second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
