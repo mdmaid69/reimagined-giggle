139637726e5312c3a2b973ab105461b52fdcd38e
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:03:51 +0000

    know dear gas cheat production

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
