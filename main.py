b44482290e1f97222311c32e9589e9a126283eaf
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:47:17 +0000

    measure century mad exit float

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
