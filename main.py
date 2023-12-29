b2fb4147549783a44ea17e81aea71cd829edc007
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:33:28 +0000

    plenty hollow best legal this

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
