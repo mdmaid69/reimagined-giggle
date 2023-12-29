5e1280e4805b75b09d5da016f985794bf46e8604
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:00:48 +0000

    live run acceptable throw dick

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
