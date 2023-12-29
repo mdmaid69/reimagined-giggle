4099b3b1c8697a25b52bfd654bb918f0bd3b4d1d
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:19:24 +0000

    need failure classroom up except

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
