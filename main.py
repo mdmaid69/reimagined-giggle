39d2e18180746ec4aa43471f7387bec43a2701fe
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:37:54 +0000

    side now full put swimming

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def square_number(x):
+        return x**2
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
