231dbeaf017e9205bd70f7d0db55a80403067af2
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:19:15 +0000

    comeround themselves pressure doctor walk

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
