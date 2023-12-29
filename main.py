7c4d4f7071930e5d8ca1f17eebad70e2c5ce9d0e
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:52:03 +0000

    reason travel guess measure check

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
