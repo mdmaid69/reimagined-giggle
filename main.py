2de301116ba2d3db7ca68bfaafe3c5d3fc2b5952
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:33:35 +0000

    spare supply dear second best

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  def square_number(x):
+        return x**2
