374b2d7bb9ddd397356fc0e684df76408bc9316d
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:01:43 +0000

    slice everyone runinto pencil nice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
