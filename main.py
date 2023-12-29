37b2e7302fc4f09157ce3157682bb9591d86b9e6
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:05:03 +0000

    Kk kind play takeback unpleasant

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
