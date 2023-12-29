7205557f4dc572f684d3b57fb642132acc585812
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:48:15 +0000

    communicate steady language claim woman

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def sort_list(lst):
+        return sorted(lst)
