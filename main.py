69e51dea40a3bb6fbacedaf0e13312bfe7322da2
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:38:06 +0000

    black true surface can cheque

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-i = 0
-while i < 5:
-        print(i)
-        i += 1
