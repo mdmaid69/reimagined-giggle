d4f8eab8349282f2beacc7935abd58054d858fac
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:41:43 +0000

    improvement sort complete soon any

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_directory_name(path):
+        return os.path.dirname(path)
