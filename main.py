10a4a977b3bfbe301f81f1b6024bfdd4203dbc53
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:10:58 +0000

    ourselves relax piece pupil light

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-text = "Hello, world!"
-print("Characters:", len(text))
+for i in range(5):
+        print(i)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
