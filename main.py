f4182e9550910416f9a1658c240b8cf4fa61fe31
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:40:58 +0000

    morning use whistle within occasion

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+name = "Python"
+print("Hello,", name)
