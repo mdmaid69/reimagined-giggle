8232782a059bfbc04461135268525f9bbb86a9b3
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:46:04 +0000

    suppose Zz turnback mouth consult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
