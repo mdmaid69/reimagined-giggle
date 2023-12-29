cbd127a905ec303b0ba9abbe8f1dc997c29576c2
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:19:16 +0000

    foot light agreement goodbye takeover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
