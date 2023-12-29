694b7847a7b301e03d23050e2c3327f5cf7ce3d0
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:20:28 +0000

    brush statement practical far difference

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import random
+def roll_die():
+        return random.randint(1, 6)
+def remove_duplicates(lst):
+        return list(set(lst))
