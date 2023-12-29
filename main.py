e01eb98104d8a3d379bde21a57cabffbb6176608
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:53:38 +0000

    by long square entrance roll

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-  def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+def is_odd(n):
+        return n % 2 != 0
