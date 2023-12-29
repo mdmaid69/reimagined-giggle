a9c119cedb6263fe9e1eb89a0b6d22a8be9d889c
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:30:26 +0000

    cheek suggestion mad hour hide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def convert_to_binary(n):
+        return bin(n)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
