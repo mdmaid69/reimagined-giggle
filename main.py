0150a972380fb2756a7837a080e9de4062c600a1
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:09:15 +0000

    peaceful shallow appear strange upwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
