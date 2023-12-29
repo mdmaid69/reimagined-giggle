1b9450c4bb9f5473e2b75e0b01910da14e8f4c2e
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:35:24 +0000

    measure behaviour please discuss rule

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
