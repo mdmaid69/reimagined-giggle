561f8de67ee040fcaa5400ab5ca368812879d041
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:29:08 +0000

    team destroy mouth kind couple

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
