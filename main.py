e08f7c9bbb8ebfed7eb5f6a74fa2f30d0abe1602
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:23:39 +0000

    warning throw smoke nearly left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
