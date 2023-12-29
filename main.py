cae94c532beec803c877d06b4145255b47256a75
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:07:33 +0000

    container uponon physical unite keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def convert_to_octal(n):
+        return oct(n)
