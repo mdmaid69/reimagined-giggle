4c9d5bab3da031540bf44f2ac2c348c31c83af29
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:25:57 +0000

    why close crisis get plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
