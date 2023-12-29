c31aacace3d22aac2e644ca754d3dbed3fb3ff4a
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:28:59 +0000

    rough condition split believe talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_tangent(x):
+        return math.tan(x)
