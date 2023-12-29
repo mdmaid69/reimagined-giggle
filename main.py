ccab1b659cf4f305288e67b6c03ee6a15430ba56
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:53:12 +0000

    save job stranger battery kind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def sort_list(lst):
+        return sorted(lst)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
