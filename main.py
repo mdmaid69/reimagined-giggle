b6f5fc28852272b860b5073b4ec4a97af8cfa9d2
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:39:57 +0000

    stop declaration argument mark build

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
