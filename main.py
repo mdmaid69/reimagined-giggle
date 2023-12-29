136650cc81c9f8c114696a9a3d51388913eb08a6
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:54:16 +0000

    roll announce late judge really

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
