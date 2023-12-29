5d616c5c66c745eed841d52e95214ed23a92a353
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:57:27 +0000

    condition up turnout drink breathe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
