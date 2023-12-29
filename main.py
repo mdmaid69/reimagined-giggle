5c3e57f6f51cf0855767d653cfae1d43c963015c
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:06:03 +0000

    clear along pretty view judge

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+for i in range(5):
+        print(i)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
