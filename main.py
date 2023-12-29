a01ae496f8fa49599dbdff6b9c05df3393b25901
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:10:53 +0000

    rule quickly formal tax ashamed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
