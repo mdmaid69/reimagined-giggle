6cb5b18dc0174889f3277a9f5528ca91b7fe0ce0
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:50:50 +0000

    basis luck risk shoulder distance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import getpass
+def get_username():
+        return getpass.getuser()
