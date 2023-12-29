e855a69aed99b48ec54db8b92417944cc9bb163f
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:04:48 +0000

    ocean information Nn evening upwards

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import time
-print(time.time())
+def find_unique_words(sentence):
+        return set(sentence.split())
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
