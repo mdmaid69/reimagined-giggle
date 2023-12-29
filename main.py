a7204a9ba5d77aaf133b909bee23d49513e79238
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:59:38 +0000

    question bread success length begin

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_characters(sentence):
-        return len(sentence)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
