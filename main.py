f31402a8f58f314a5d489c08d57acf3e01a31a03
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:38:44 +0000

    pronunciation earn eager train delicate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_speed(distance, time):
+        return distance / time
