c1c19fee707baf052861c9793e29f95b362eb2fd
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:15:11 +0000

    quantity tough wrong afraid organization

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
