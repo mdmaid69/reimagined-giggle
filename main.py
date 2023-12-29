3efb644ebe5fc517d8f64c7d5175bf1ddfd9684b
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:30:23 +0000

    keepout rely pressure ill defeat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def cube_number(x):
-        return x**3
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
