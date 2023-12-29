79fdee2d1259f60e923645fbb17131b8d8ad236a
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:53:20 +0000

    habit check think during consist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  def count_elements(lst):
+        return len(lst)
