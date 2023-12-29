b2d788095183635bd3c16ff52f8fe35a2d141361
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:02:58 +0000

    form Jj dream suck ill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def reverse_string(s):
+        return s[::-1]
