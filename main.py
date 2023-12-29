e54e58f20a8e2e43b7cbb83b9c48deaaa097857a
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:56:41 +0000

    Dd evening pen glass compare

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
