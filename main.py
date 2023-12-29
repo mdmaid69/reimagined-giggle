d061c7f95271e1376d7341fe24ff45b81a2d81e3
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:54:04 +0000

    Ww mad rank workupto recently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_list_from_array(array):
+        return array.tolist()
