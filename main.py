894b2f6173078b183e8172b48123a0c1844d3c1d
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:50:03 +0000

    content keepaway clean finally bringforward

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
