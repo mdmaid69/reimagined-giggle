8534613172760eab55295e5ab050119f48b8eb25
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:31:00 +0000

    shut free draw foot joke

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def count_elements(lst):
+        return len(lst)
