2dd374c3240b1cbd2c79d0e0e0d1d331c6e78b8c
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:19:55 +0000

    trip disapprove keepaway firm expression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_list(array):
-        return list(array)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
