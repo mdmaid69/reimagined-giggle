438f16c63751b21db1bed2894d657cd17b781566
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:21:46 +0000

    full tidy ordinary car prove

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import os
+def get_current_working_directory():
+        return os.getcwd()
