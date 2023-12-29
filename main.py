09189662a84d0819fefd8cf60c03478b0935f529
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:19:34 +0000

    already hot near awake scream

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_force(mass, acceleration):
+        return mass * acceleration
