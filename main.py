f3e2b561a629668b4edf50aeb3e275cac8a594e2
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:05:53 +0000

    treatment his education spend temper

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
