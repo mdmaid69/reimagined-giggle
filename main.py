29cc350b6c609ef830d24d5ab74cd8dfdfc4d2b3
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:35:46 +0000

    country previous unless measure upwards

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
