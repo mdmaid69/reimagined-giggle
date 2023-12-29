2bea2d8051f454a1b657a0d48efe4fdcf1b83482
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:55:27 +0000

    break standard generally so dare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
