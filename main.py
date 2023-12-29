26766b505559bfa852e8e63aebed44ec16f2f85d
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:39:45 +0000

    share match scale capital opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
