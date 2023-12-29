8c6929143a1a6bb3021198d5e0edf0f6cd847fb8
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:41:29 +0000

    deliberate judge medium expression age

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
