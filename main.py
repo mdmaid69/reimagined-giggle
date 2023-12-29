050e686ec53c8e75ad20f3679c48f817c8681c14
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:10:50 +0000

    fond range ground wish level

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_power(work, time):
+        return work / time
