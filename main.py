a48e16d4872d43bd97314cc404f0722e2f21bcc6
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:41:00 +0000

    wind order morning favourite remember

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-name = "Python"
-print("Hello,", name)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
