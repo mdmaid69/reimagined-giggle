bd740f0db6ea8458e36c8fd38bb2dbe191fa7d8d
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:13:16 +0000

    anywayanyhow study wing outside hand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
