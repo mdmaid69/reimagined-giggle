cddcebe9dba0cb27e22b463cfef2796de185ec5a
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:17:42 +0000

    exciting runinto 179bite deserve rare

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
