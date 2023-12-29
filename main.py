75fd9dd75094242f86041b91fcb8c4459cba5cf6
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:08:52 +0000

    used left Nn fork create

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
