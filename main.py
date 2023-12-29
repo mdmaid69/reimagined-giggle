47698548fa4ba2bf9dda3d0424f8d68a8fdfed8d
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:53:41 +0000

    nearly chat service mention present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
