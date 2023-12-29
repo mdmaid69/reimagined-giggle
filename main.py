54ac5300d301860e3781c7da9e2fc2d30ea5fa51
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:12:06 +0000

    turninto serve afterwards wear tour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def is_odd(n):
+        return n % 2 != 0
