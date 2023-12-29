c479fa64d13de02ee527086210540a1de38cadd1
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:56:19 +0000

    debt star around guard weak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
