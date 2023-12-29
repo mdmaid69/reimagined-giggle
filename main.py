81175b7506783308eb5757bdb8ba198621fd5ed5
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:50:23 +0000

    generous list wipe dance experience

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_priority_queue():
+        return collections.deque()
