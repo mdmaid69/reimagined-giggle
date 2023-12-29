43e08002cbb8a455b3f15ad169939f121c913f67
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:34:21 +0000

    bag eager space correction neck

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import math
+print(math.pi)
+def reverse_list(lst):
+        return lst[::-1]
