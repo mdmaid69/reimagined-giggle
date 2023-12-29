b93e2e4e3ab895164743cf3c37aa1f59b9198f65
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:10:35 +0000

    tough yet large odd belief

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
