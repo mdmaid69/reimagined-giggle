8aa50e9118dab4b811cfafe1e633f32df2e212bc
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:32:38 +0000

    easy relation bus thirsty ought

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-print(sys.version)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def reverse_list(lst):
+        return lst[::-1]
