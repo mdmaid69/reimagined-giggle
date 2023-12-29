e412d4be17d9e5c63b85f44cf83dee0c56e492e0
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:56:05 +0000

    reason main waste stand anywhere

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
