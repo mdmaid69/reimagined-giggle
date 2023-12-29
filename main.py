0ff275db818b3c02210aba17659d5f2a1d6e32d9
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:19:32 +0000

    forward car lesson convincing too

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
