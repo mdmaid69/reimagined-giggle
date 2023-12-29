8018cb45915c0aef6f7629474de07c4744fcf594
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:32:11 +0000

    Qq each sister cut wrong

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def add_numbers(a, b):
-        return a + b
-def add_numbers(a, b):
-        return a + b
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def multiply_numbers(x, y):
+        return x * y
