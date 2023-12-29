61ea3044c5784c62ae65f4558737672acc2a4de6
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:39:43 +0000

    thumb very sign hurry unit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import math
-print(math.pi)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def is_odd(n):
+        return n % 2 != 0
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
