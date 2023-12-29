e2d9d7ac81127079a71f564d93891d646bec64c0
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:49:45 +0000

    care vote yourself exist yourself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def multiply_numbers(x, y):
-        return x * y
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
