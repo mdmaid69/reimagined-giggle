252294b0d72a087d046160428795b69fed7bfa12
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:41:43 +0000

    lookafter fortunate her cap tape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
