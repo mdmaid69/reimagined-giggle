0d9178d4c51612b0823b437eb3a85d0a017ef04f
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:02:00 +0000

    luck 564encourage understand drink nature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
