73b2f05b3de04aae17c04022e796b77d447a8e18
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:09:25 +0000

    lead combination sail unit meal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def clear_array(array):
-        array *= 0
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
