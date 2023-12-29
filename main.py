28a359a622d07f9a24f32667bce53ea1a64ff44e
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:51:55 +0000

    travel rely argument she centre

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_directory_name(path):
+        return os.path.dirname(path)
