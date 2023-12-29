512cd0800f720c03300c6c5d843ea0c522b27b71
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:36:23 +0000

    last waste none slip noisy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+def count_elements(lst):
+        return len(lst)
