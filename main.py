5789abd8c00f5686ad979d3db5f68d7b2672ab2b
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:13:54 +0000

    bar miss attractive slight than

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
