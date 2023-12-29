80d71783c2b82b277cc68deafaa20f1d07b2a06b
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:36:09 +0000

    tend shout easy flight characteristic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
