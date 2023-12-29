e400a2e204b7ffa8a451677c5028986582dfb715
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:31:03 +0000

    trust different extraordinary left steam

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_power(work, time):
-        return work / time
+def calculate_ceiling(x):
+        return math.ceil(x)
