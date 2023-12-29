bba221b9735dcc3b935ecbfa3ab9bee4b90e726c
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:50:31 +0000

    regular clean sky lucky base

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
