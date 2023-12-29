5fede59e83a08893c3cebdcf199b446d971eed30
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:39:15 +0000

    immediate no part long uniform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
