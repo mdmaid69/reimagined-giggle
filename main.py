17b277479f07d165cb1b3c85c233855dceb8f916
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:55:37 +0000

    stranger spot image increase complicated

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
