ada11627394a22fcac9875329fccca238d4ef8bb
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:06:38 +0000

    share consult clever lock disapprove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
