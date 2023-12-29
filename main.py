bc6e716d8925239eb9a533fe2490a28a6105eb60
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:27:37 +0000

    scrape turnoff welcome piece add

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_area_rectangle(l, w):
+        return l * w
