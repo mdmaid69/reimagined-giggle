118b93d88169d43c6720d663d92eccaa2b2c6f47
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:50:43 +0000

    work turnup however beneath copy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def reverse_list(lst):
+        return lst[::-1]
+  def sort_list(lst):
+        return sorted(lst)
