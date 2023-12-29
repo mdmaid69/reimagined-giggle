5090fc547f1b9f6d0ddd2969801f4e370ee4aaed
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:06:13 +0000

    effect ring responsibility chief fasten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
