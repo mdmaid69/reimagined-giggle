b05291f1ba02e446f80388707975fb1a2713b19a
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:01:36 +0000

    walk slightly advice by dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
