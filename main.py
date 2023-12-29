dad2ec806a382c8bb9477b566b64a1ebc545668c
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:54:35 +0000

    kind regret cruelty choose centre

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import collections
-def create_queue():
-        return collections.deque()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
