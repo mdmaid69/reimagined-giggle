5a096c8c8a81f986a50b348bb1c1a6f81e585f63
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:31:49 +0000

    every fact near onto curious

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-print([x**2 for x in range(10)])
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def greet(name):
+        print(f"Hello, {name}!")
