62d318e720d8958560fe9d6f5fe5ea13c6f6c43c
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:41:28 +0000

    sky butter beside law famous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
