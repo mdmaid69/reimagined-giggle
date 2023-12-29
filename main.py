5d240ec6f8320ad019bbad638f67d995f0db777a
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:37:27 +0000

    open suddenly reduction peaceful dear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
