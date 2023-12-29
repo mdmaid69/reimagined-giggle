d767e1814b0aa4a183a649923e0835fcac2e352b
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:51:33 +0000

    office concentration whether something under

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
