c036ce73acee76e1f622146417d0c01193b89837
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:27:41 +0000

    work breakdown straight quality ready

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
