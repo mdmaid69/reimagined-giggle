643818444dd39c75f7235577f1e0b57aa85e9dd6
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:17:38 +0000

    brother tomorrow suddenly mark Gg

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def convert_array_to_unicode(array):
+        return array.tounicode()
