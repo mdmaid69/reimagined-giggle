80199271f1ea8bfe1ea81c8fb2e541718d20f757
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:14:07 +0000

    evening cause amount blow neither

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
