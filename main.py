b3eeaaf07f3b9ecb23b0c144444d7889ee54fc37
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:27:06 +0000

    seriously combination wire argument cold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def sort_list(lst):
+        return sorted(lst)
