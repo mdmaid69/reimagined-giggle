7f371cbc8a6b885937fc1eeb44322e44e33818e0
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:40:23 +0000

    sharp spin passenger glass mad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import numpy as np
-print(np.array([1, 2, 3]))
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_as_repr(array):
+        return repr(array)
