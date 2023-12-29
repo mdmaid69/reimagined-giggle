5c303181d6ce0f080c9bda5c4a95f675110d3f79
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:47:35 +0000

    to crowd improvement replace remove

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def find_max(lst):
-        return max(lst)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
