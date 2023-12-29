f5a68d286a5147e3e5e719cfd958c3a3216c82f9
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:10:45 +0000

    often home leave takeover he

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
