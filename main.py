aa94389448ee9198aabf3e96b439cc6ddc3f4eb4
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:01:27 +0000

    limit off snow muscle follow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_area_rectangle(l, w):
+        return l * w
