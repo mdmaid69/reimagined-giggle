48bfaa480f5036952c595655078f273345a4ca23
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:59:23 +0000

    tidy reasonable outside demonstrate picture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_index(array, item):
+        return array.index(item)
+text = "Hello, world!"
+print("Characters:", len(text))
