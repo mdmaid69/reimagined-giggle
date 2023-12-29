e82749c4dc894bf54f5cd2db61be7d8f2e2a3c04
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:31:33 +0000

    fix bush lookback(on) error miss

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import platform
-def get_python_version():
-        return platform.python_version()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_work(force, distance):
+        return force * distance
