0b9adfe2048afb3c081b63637eeeb7b1d3101e83
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:57:22 +0000

    criticize mess practise mood command

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
