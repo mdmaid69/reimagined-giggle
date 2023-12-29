76e01d6c5043aa915a43fe928bb32d819d2e3d15
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:35:32 +0000

    goup please mother Jj pretend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_acceleration(speed, time):
+        return speed / time
+def find_min(numbers):
+        return min(numbers)
