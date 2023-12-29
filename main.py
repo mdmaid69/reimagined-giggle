12fc51404cb690dbcf055aa3622de1cee1d87f81
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:21:47 +0000

    voice pleased means the anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import collections
-def create_queue():
-        return collections.deque()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
