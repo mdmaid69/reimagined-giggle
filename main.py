8c12eb0ca06ab4df680f05faf4a51403c4af2a34
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:10:45 +0000

    relax crisis rise top seat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
   import os
   def get_file_birthtime(file_name):
         return os.stat(file_name).st_birthtime
