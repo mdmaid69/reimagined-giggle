52663b67ca0dd8b6beebc6f9460adb0818637fe7
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:16:51 +0000

    tone worth call peaceful praise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
