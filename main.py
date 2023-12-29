4ba615e0c6266469410c3e2e4ef84add9d1ca953
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:54:00 +0000

    age century plan concern delight

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
