8695a84144997250d476dfc9079226feb1ea6cd5
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:27:07 +0000

    before police trap schedule thought

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
