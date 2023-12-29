8ecab1781424072c8ee555df283106bdceb0700c
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:09:00 +0000

    control even activity wide type

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
