65b87c7ee1a4dfdd61c6cb8236aedc717943bfcd
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:07:11 +0000

    border interview abroad obtain approval

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
