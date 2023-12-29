f839c96ca7901ce59c052acc89c968ca304b59bc
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:09:42 +0000

    control drop length check impossible

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import tensorflow as tf
-print(tf.__version__)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
