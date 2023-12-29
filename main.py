95a858a2d032da7990d7868992c1cd475138d776
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:04:05 +0000

    exaggerate knock might reject modern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
