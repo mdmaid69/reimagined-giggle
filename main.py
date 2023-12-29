16cd43a5295fc1e51f159df3f0fdf5e164e8669b
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:10:24 +0000

    ability health urgent far promise

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def multiply_numbers(x, y):
-        return x * y
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+print([x**2 for x in range(10)])
