67db6e9e0ca2eed06af2981fd40e085fe2bed0f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:26:55 +0000

    ask year slow stage control

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
