68517256532dc9bcbab73a1e4a707d3371e12e35
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:59:52 +0000

    judge second tonight enthusiastic us

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def is_odd(n):
+        return n % 2 != 0
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
