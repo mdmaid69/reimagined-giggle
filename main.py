45d677aed32e5fbfb97aaa17f0411449307c2951
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:31:49 +0000

    building brush generous strong bend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
