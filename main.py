b49ff6c198bb00b031277fa9c56123086dbcdacf
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:33:45 +0000

    crash produce design do port

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
