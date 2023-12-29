467e39adaa3ab98c7f757c826c956f9e0c845ed4
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:51:34 +0000

    party stretch touch clear broken

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
