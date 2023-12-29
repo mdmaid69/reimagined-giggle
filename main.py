d07396d21827aa5206b175a3d3e7e4c7469c81c1
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:30:32 +0000

    appearance effort wet target brain

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
