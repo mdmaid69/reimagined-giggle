da0a8f72624ec6f0927ff7e17c7e0c521eeca888
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:30:12 +0000

    bite take develop since aim

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
