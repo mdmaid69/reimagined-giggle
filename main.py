ffb31c6eb4b6d173c9b34897fcf839bf124c3ee6
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:52:05 +0000

    sudden weekend opposite apparently contents

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def sort_numbers(numbers):
+        return sorted(numbers)
