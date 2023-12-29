c9fcb66faf613d7551673f6e3d5176fa61e3cedc
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:05:58 +0000

    because tend better cross yes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
