abf24a093d6cb47f66486d61f7aa1ef5bc1860ae
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:24:04 +0000

    bang Ii paper soft share

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
