d92c54eb475a824bafe562a4facee9b6712079f6
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:07:59 +0000

    middle soak improvement uncertain turnaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import numpy as np
+print(np.array([1, 2, 3]))
