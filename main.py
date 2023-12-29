7e6018ce3b871bde06bf6c70bef12ffb3582c1f4
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:19:07 +0000

    gradually pollution measure least inform

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
