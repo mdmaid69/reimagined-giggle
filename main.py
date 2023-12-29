5b9df2e58f0058f1756a7a802c5f81b754d6f7a5
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:11:50 +0000

    spend means naturally push piece

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import array
+def get_array_as_float(array):
+        return float(array[0])
