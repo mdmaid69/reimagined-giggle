cbe4522b90fc0bf46693bed9873f620136ed3d58
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:02:58 +0000

    letter rush breakinto evening beyond

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import platform
+def get_python_version():
+        return platform.python_version()
