2bcc88ad7a5476d45a8f4acea5921dad5553b065
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:24:03 +0000

    curious scared motion experience begin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def add_numbers(x, y):
-        return x + y
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
