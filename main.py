ad515f2cff0adf5080a4e9b9ad071213329060cc
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:11:45 +0000

    closely step right eat petrol

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import os
-print(os.getcwd())
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
