f26e24080aa019bb9467d153e712769ed65f6997
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:21:31 +0000

    bunch turnaway towards moral form

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
