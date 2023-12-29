bc20be50f1ff83d5d240cd609beffbc9041bf5c3
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:10:38 +0000

    getaway content closely loose exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
