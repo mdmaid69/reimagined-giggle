d9da540031ba561bfbd09b04c152b25ba17591d3
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:57:01 +0000

    lead least through permission discussion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import numpy as np
+print(np.array([1, 2, 3]))
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
