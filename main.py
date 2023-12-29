9f8a343d2b99caec114c8f9643b23f0702fbad1c
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:48:15 +0000

    upwards centre favour face pleased

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
