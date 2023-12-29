2786d19ee173e96e86d948c573f43e7bc01623c5
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:53:42 +0000

    interesting heat shout proper goodbye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def find_min(lst):
+        return min(lst)
