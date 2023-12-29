ad0ee966cdc32faae68ef0d046a3a2965ae3d1f5
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:46:18 +0000

    so recommend Nn permission fairly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
