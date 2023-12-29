75d781d8fe1e38794b0c9cbeadf4e0eb3e278464
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:21:19 +0000

    hope chin interrupt sky catch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
