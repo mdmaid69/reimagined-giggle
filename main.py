8043d6427049bd11df0e3e276334a719f3ff37ff
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:47:40 +0000

    message frequent journey paper performance

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
