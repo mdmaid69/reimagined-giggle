e9112ed67ced358fca3f7d3b5dfdd60e78d6d080
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:06:49 +0000

    emotion mean post might block

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
