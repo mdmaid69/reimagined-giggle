99097992968c676966a1b968a814a2c40e2153bb
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:39:43 +0000

    advantage Pp model ocean relative

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
