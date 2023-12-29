ddf4e9af3e9cbe83309c83d4d3bb31dc838caaf6
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:12:48 +0000

    completely cruelty market single guide

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
