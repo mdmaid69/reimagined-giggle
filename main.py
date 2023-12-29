0d2fc8a78b2e5482e50fefbf604c05efdac3f126
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:09:34 +0000

    culture workupto snow treat pollution

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
