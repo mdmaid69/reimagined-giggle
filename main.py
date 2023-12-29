e66b346f76570cb4364df4bea4f273777fa6d66a
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:55:48 +0000

    passage fault apply their pain

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
