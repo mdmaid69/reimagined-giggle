d0f7d414680c1b45d60921781b7dcc6f14c3c738
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:14:03 +0000

    hungry Ii prepare switch trade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
