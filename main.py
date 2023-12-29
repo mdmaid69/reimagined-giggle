95d6870f70b5f3a1fcbec56be5bb06e3dbaf9d51
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:15:31 +0000

    whistle peace hold mistake row

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
