e2649be0b198a2957ad05c6f7de2d7c4892141fb
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:22:23 +0000

    transparent distance Yy disturb scream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_area_circle(r):
+        return 3.14 * r**2
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
