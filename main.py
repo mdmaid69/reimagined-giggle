47bdaa53bf315ff4862f542c06cded5de91c988a
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:23:23 +0000

    flow harm treat for blame

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
