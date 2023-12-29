c15f7edace3cb8dfa091090c5961fd7d2c458231
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:24:21 +0000

    closely same under cheque future

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_acceleration(speed, time):
+        return speed / time
