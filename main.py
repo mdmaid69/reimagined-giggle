c348ea15594383a53b8ce49aeb98f8dd74c5cfed
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:58:47 +0000

    indeed emphasis round trouble Oo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
