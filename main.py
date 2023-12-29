8d175a665f8662be0cec08cebee9bd6bd4fd2f0a
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:15:04 +0000

    congratulate reaction pen of sad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
