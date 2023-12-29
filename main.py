e5c95c34882c776af66d5cc44820251eaf6b463b
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:00:46 +0000

    plan check walk similar catch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_speed(distance, time):
+        return distance / time
