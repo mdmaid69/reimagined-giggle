b8104e577f752f1b5453d95226f4b8f29ab3aaf9
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:37:38 +0000

    shadow anywayanyhow person trust workout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
 import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
