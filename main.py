44d7ec91f98ffd891f14c504a60e5ecc75cc66aa
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:04:59 +0000

    near bend embarrassing type occasional

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_density(mass, volume):
+        return mass / volume
