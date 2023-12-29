92ab9f5c0d74b1820371e1320d3197f45fe0ffdb
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:28:09 +0000

    left nurse branch within funny

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
