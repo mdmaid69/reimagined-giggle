3f7e6cb201abd293c487f9d9b3d9e83ef0551634
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:26:41 +0000

    house practical act still cruel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
