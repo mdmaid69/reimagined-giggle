f37108051cb15300826c223e654df5bffa7bb291
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:43:12 +0000

    Cc attitude then lie first

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
