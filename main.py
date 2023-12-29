1293fc87afeb7d7d036689de3add5e8ed5530949
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:06:18 +0000

    concentrate dare neat quantity one

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
