537d628e237ecc1c6fc3e70c37cea48ebb1def04
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:00:57 +0000

    chance fear holdout downstairs least

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_min(lst):
+        return min(lst)
