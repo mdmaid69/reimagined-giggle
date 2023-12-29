a364c725ebf329d16b4cb19bed74cca1bd17f91c
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:34:54 +0000

    intelligent comein fix level distance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_density(mass, volume):
+        return mass / volume
+import collections
+def create_priority_queue():
+        return collections.deque()
