0f97e2408cf5548f49625b87d893c94feb1cc73b
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:46:52 +0000

    explain hurt quality float treat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+for i in range(5):
+        print(i)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
