2b549aa680867ebc02bddcd804a5e9d48eaa996e
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:41:39 +0000

    uncomfortable instrument invite shape cheque

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
