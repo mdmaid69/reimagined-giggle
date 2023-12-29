c75cffc2eb1e144d624a2335ac3c5b1ec952d6a3
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:28:28 +0000

    valuable poison basis tradition mouth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
