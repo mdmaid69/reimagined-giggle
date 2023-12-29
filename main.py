19e71491ef97545210ffd530f1e7252569d3990d
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:17:31 +0000

    underneath hold willing less limit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
