d9d09513c6feb0efadb7343e696c496e1f2963b0
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:49:14 +0000

    failure test occur sort laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
