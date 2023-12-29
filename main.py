617afffa9d6bad7737b3d55919cc203674bcddfd
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:33:31 +0000

    appeal sound street certain entry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 def calculate_pe_ratio(price_per_share, eps):
         return price_per_share / eps
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
