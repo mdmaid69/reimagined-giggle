03c636aad54327300694e9936f86db8f4ff8858b
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:39:36 +0000

    holdon tap consult Kk school

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
