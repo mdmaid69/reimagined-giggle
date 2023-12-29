601cfab4edae160d30205a919c8a9d564da6e6fa
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:31:59 +0000

    anyone recognize subject takeover broken

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
