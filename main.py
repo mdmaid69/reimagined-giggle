8e5e5e62eb739fd843ac50046df21545c4aa9f01
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:08:33 +0000

    practical feed exit just anybody

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
