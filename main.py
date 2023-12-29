799dc92f15dab3ded96247c882a162aa0fb9f3e6
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:26:32 +0000

    speech stand art drop season

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
 n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
