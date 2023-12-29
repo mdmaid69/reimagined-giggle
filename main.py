562b58a7035ec701f42e71f905cacb560162480d
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:24:05 +0000

    mainly broken lay pile accommodation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
