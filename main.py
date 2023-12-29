2272eb5c603cd21f96167bf3f95998d725962f0b
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:22:21 +0000

    office close smile want switch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_item(array, i):
+        return array[i]
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
