b1f874f3f33b1ae6f1f024d7a6d6e2eb0cc6e91a
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:09:43 +0000

    once back nowhere skin cover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def greet(name):
+        print(f"Hello, {name}!")
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
