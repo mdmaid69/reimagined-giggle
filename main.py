d74c0ce464ea753e8a1a31e58c5ab5e88e3e5880
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:27:47 +0000

    never warn lead noone excellent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
