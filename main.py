3a116eb4c6e35f79db05b73afa6bc286397e0a96
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:59:46 +0000

    sink school sensible frightened freedom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
