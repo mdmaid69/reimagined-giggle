6a6230cdf9dc8e0a1e9b94cd3be33b3f20350059
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:01:03 +0000

    everywhere tell play typical ever

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
