d4129381c40a7c418788aa7de0e0abfe7ed237d9
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:17:12 +0000

    wheel actual reject health moral

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
