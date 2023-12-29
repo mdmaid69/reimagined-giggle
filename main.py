c7a2a54fb051a1b07ac960ee4623ea9d140a4f9e
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:38:19 +0000

    start chat action noise ocean

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
