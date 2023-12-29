a63f0771275e82a69ccaedc61f747dff1a380d90
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:03:46 +0000

    translate why private Ee bank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
