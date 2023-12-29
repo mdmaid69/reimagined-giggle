7ea260418d2d94d3a5573878095f1c87a514224c
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:51:33 +0000

    interview tap charge ever Itself

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
