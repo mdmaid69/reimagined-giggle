8a5fd47d0af8905a72b91701377d6b6370982369
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:03:30 +0000

    distant comeover tough sense 571enough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-def convert_to_hex(n):
-        return hex(n)
+def convert_to_binary(n):
+        return bin(n)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
