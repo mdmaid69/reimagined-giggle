1e5e757957d1cd352cd0b6f20ad134cea30e1576
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:02:53 +0000

    though concern also boil broken

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
