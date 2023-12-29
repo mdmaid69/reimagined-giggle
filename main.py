7dfeefb14433a1fe7ac789b49d76cac0a94ad518
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:47:10 +0000

    glass row regularly noise base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import time
+def get_current_time():
+        return time.time()
