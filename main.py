8630c611878cbaf681acadad7c46a3d9b3b7e59b
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:54:13 +0000

    pot admire downstairs sticky nothing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
