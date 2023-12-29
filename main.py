8099a814c6b056f5ff77a3219586c23ed6e63a8b
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:35:30 +0000

    567enemy male pool yourself difficult

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
