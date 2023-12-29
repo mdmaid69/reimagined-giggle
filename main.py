597283f7ec632be55585298230d54a2ee7b46239
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:33:47 +0000

    reason probably strength spite tip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def subtract_numbers(x, y):
+        return x - y
