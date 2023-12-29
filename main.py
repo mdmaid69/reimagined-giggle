019818829adfe5ee5e070528ecd66666e4fe10a5
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:19:34 +0000

    at turnout send pretty callat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import math
+print(math.pi)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
