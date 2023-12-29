564c3fc18fc492e4d940981aef9d27138558fa53
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:51:37 +0000

    passenger ill ring usual artificial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_current_working_directory():
+        return os.getcwd()
