e98b04d5ad8cea0b478c7e93f753c93354accc61
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:41:26 +0000

    suppose conclusion apparently holdon relation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
