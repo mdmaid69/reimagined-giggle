d27fe3c459aa08ada5939b0e9cb0ff8577168055
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:07:03 +0000

    chin desperate want lean so

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_acceleration(speed, time):
+        return speed / time
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
