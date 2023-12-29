cb76020b04f21164e678966e6f3cc384c53c4cd2
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:08:18 +0000

    tense fill eat dress modern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
