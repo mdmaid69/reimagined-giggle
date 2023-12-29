a7b810cd36916ac9417c946621b543b7c3e507db
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:45:44 +0000

    slightly frame visit old habit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def find_min(lst):
-        return min(lst)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
