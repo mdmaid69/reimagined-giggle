878bc0b49fd22fa28dfaf5c9d94a4075f47a3073
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:02:19 +0000

    convince worried cum exploration delight

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def square_number(x):
+        return x**2
