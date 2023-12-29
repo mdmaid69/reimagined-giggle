776fddb86924795cb9ead643dee557b34a408e5d
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:03:25 +0000

    later country nowhere embarrassed dance

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
