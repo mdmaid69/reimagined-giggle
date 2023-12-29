a6e9e2587ff8a5d64a4de7c671ba333c38d9bc3f
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:26:09 +0000

    shut floor keepaway meeting thing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def is_even(n):
+        return n % 2 == 0
