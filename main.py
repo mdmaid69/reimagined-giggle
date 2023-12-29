ec68ee5afee14266659ba1e2a08d1763c5767edf
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:12:02 +0000

    tap direct change mail admire

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
