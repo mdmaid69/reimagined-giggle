db2d93c1b3b866b36ffe007e442e97bb9e0454b4
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:51:53 +0000

    address part even minor sad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
