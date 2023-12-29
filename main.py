3d2c0920cff290efcdef24948bdc9a99025d13f7
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:52:27 +0000

    that how strike poison compare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_acceleration(speed, time):
+        return speed / time
