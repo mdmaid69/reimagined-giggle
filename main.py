8ab64c2bdaf368d47ce92d9a6873683a5a2e426e
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:50:22 +0000

    address independent contents necessary measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
