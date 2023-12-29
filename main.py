61f3cfee00c50695ddaf9714c90eb3afcf288605
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:40:54 +0000

    match add angry juice copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
