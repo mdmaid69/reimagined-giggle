12a4bbcdf11e40558f6c172f4f654fcc944ef4aa
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:49:12 +0000

    that such case unfortunate accidentally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
