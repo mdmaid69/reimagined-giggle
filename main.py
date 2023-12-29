d23a371bfd5be85c927dcfe824f81bb4020dbeb0
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:10:21 +0000

    mass man regular generally speak

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
