01f3a15b871b21b0c9a0132454053c9f61099f92
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:05:17 +0000

    necessary juice shake market mixture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
