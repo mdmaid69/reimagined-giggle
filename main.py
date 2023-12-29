6fd0b3c245971849d134f409444bed6a2ac8b332
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:24:24 +0000

    tap or occasion disgusting bright

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
