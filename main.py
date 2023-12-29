0c562fc53c3adeb7735a45ca8bff38c779cfe8e7
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:16:56 +0000

    without fear it gather breakup

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def clear_array(array):
-        array *= 0
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
