a9d3e1e355c1f4cdf0ab802b7963176af1851e7e
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:26:44 +0000

    press embarrassed round claim mind

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
