16c19a2562dadeca8f836755f277f7f68d5ef46c
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:47:24 +0000

    cause sure contents sad side

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
