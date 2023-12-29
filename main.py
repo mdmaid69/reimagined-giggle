d866b2f041aa155ac26857cc8bc95b3ac6756613
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:15:32 +0000

    refer calloff too suck stage

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+for i in range(10): print(i)
