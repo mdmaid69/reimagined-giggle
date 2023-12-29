5237e9af7d2cdfd7f3a6ffade4003942e45002ea
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:08:52 +0000

    innocent dish grade union real

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def clear_array(array):
+        array *= 0
