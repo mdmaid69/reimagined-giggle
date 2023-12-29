66ab818618001b74887b35adb17ecf438df9ab5f
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:21:13 +0000

    strike speak turndown Kk shine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
