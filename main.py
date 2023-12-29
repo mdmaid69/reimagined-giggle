296cbf3c8155462b350cbc2147e8731a82d7ec81
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:23:40 +0000

    by godown trap upstairs behave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
