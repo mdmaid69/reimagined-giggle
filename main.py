810db60d85fd16c5ee52a622596ed3fe1b0fe047
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:59:08 +0000

    unlucky really influence both judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
