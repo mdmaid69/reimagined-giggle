02bb96060c23379637ef5b48411ecf6da55a6b5e
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:45:42 +0000

    plant man ear emphasize definite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import time
+print(time.time())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
