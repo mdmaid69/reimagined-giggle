c00c14fdad27a232ac3ca25bf67fd6254dffd945
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:04:39 +0000

    round arrange common stiff certainly

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
