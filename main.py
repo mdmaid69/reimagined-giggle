60662941d26ea1ce0157503bb18491e4e4bc6de0
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:12:17 +0000

    track tired thin welcome flow

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
