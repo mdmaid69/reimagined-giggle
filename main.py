bf8d1dce0b70ac847eb670fafcf43fdf898b72a4
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:34:41 +0000

    getback next rise Tt style

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
