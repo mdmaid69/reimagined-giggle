927d9c3444fb9518d0838ee2df91a5a0bd993237
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:35:32 +0000

    thinkover free gather mark genuine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
