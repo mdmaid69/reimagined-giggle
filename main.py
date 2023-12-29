1e04eaf19a501b253d305aad1e703d56c7c669a7
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:21:57 +0000

    post grand all water release

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+print(sum(range(10)))
