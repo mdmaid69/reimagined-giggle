228a6fbb7d2de47d1867f78f846e3a7e6b719c6f
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:26:10 +0000

    anybody turnover us shout detail

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_current_directory():
+        return os.getcwd()
