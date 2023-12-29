d3afc3f40bf6f6efa7bc0dd40ca9b334a4da6ad1
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:06:47 +0000

    night peace provide opposite rich

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_user_list():
+        return collections.UserList()
