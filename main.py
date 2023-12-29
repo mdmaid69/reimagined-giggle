a58ee128171eb120959f496daa1027d78d762c03
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:31:58 +0000

    pollution shade intelligent quarter pocket

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_pressure(force, area):
+        return force / area
