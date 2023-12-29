c2de81bb8125c943bbd1e71f8b22116b1224fd32
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:56:52 +0000

    ear instant empty exaggerate hard

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_speed(distance, time):
+        return distance / time
+  def multiply_numbers(x, y):
+        return x * y
