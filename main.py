47615357cd6dd672c7ee66e52496fb965042a4c5
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:59:29 +0000

    567enemy many steer block loud

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
