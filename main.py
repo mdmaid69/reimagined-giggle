fd4f299aa4a905ff03aebd29a8dc6d9cdbd267df
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:01:24 +0000

    square neck release live disagree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
