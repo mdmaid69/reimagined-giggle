2df503c5a4a150dd597b7ae8762259cbd5fd98f5
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:44:42 +0000

    side busy money punishment off

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
