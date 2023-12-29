c2f25caf9f424a10e9d22fdaf29cb7d47f8ebc47
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:44:55 +0000

    beautiful announce feed land sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
