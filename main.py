ed07ce326e08b245d55273dab01d40290182520e
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:58:08 +0000

    shake persuade stone otherwise limit

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+print([x**2 for x in range(10)])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
