65874d6ffc91d68dc18f3ada61ddca97a6a45e2a
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:05:59 +0000

    behind dance what history afterwards

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import os
+print(os.getcwd())
