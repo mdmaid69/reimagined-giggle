81a53bfe8b2c723b5bbe6b5470c60e6e30d0216a
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:05:06 +0000

    finish measure lean break frighten

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def reverse_array(array):
-        array.reverse()
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
