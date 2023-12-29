b34ab8939931b4541b51d0fca2d9b0a62d77a25f
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:37:47 +0000

    circumstance nowhere colour close callout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import os
+def remove_directory(path):
+        os.rmdir(path)
