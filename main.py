118cb74d94af0e6334f7493328079baf373c1534
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:43:36 +0000

    underneath suppose quality use interest

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_power(work, time):
-        return work / time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
