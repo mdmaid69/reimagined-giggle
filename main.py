e4a54511c634fba7b26eda0654493a9a2070be8e
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:23:40 +0000

    somehow joke yet go disappear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
