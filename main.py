d889ddc4febc0af57a625a0e36ca137c93252d30
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:06:25 +0000

    effort prepare perfect keepfrom ourselves

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
