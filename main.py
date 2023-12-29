29f1494f7999d98305a5a4b0d64b7cfa7c876f86
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:48:51 +0000

    dry lose rarely makeup hollow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
