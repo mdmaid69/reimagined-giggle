a59f1df342827660e4263290d9e03408991d3263
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:41:03 +0000

    now takeover behave forget rub

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
