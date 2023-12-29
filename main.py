93a876cc39a19f85b5c5b55fb9539cf15d257953
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:41:02 +0000

    draw regular turn store apparently

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
