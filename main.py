7e84a7afc81e4e0f38a99ada4f687478e37a41c1
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:47:16 +0000

    plenty shake later respect rely

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
