873b5af7d09a339c23f882a1eb56a98d57072376
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:37:20 +0000

    seem exhibition in immediately dull

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
