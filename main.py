2636e03e78aea7d2b4048702f11ee26b888f3f0f
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:32:29 +0000

    suffer hers spread defeat tell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def count_elements(lst):
-        return len(lst)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  def remove_duplicates(lst):
+        return list(set(lst))
