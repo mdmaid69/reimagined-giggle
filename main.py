f2c296ff8064c2bb37b4ac163e39b4d5782b1d63
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:45:27 +0000

    smile ago study heavy temper

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
