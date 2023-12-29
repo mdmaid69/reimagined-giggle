9da64d9dfaa965556f80650f5417a973ff42eb56
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:17:59 +0000

    level complaint ring tradition connection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+for i in range(5):
+        print(i)
