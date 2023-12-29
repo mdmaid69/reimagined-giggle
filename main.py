7c4143bcc3bf9b59a8b0a79aabd5c390f9f0162c
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:56:48 +0000

    during fortunately young organize winter

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-name = "Python"
-print("Hello,", name)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
