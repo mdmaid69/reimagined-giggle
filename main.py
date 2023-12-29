5320323ae4db5d46bc8356f2c0906d762a706d48
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:23:52 +0000

    talkoutof meal skirt computer substance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_current_directory():
+        return os.getcwd()
+import platform
+def get_python_version():
+        return platform.python_version()
