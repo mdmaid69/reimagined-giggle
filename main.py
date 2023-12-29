de6a779dbe9ffe7e006fdc9c68b84ed7eb8c5b7a
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:58:56 +0000

    goodbye sharp common rub need

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
