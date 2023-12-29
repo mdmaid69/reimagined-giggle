ae2a80bf1cb393f88f889bd15009d937048dfa01
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:57:23 +0000

    equipment concerned already keepaway usual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
