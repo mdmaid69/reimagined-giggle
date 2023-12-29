073c530230dc29d7d23b9187d51c3c75cb799ffd
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:53:09 +0000

    Mm protection hollow 567enemy characteristic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+def is_even(n):
+        return n % 2 == 0
