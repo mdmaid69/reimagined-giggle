8963b1ce55a0a6dec0f82b020103547a0e6a239d
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:36:41 +0000

    she tip absolutely indeed suppose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_mtime_ns(file_name):
         return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
