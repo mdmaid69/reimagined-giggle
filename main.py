f3ed8169f5d21a307ec9f8fd1ffad575de008ff1
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:45:03 +0000

    swim close judge promise inquire

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def remove_duplicates(lst):
+        return list(set(lst))
