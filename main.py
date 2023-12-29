10bb496506abf35d9739af8def5cba54ee857269
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:03:17 +0000

    lead truth happen compete thankyou

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_power(work, time):
-        return work / time
+def sort_list(lst):
+        return sorted(lst)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
