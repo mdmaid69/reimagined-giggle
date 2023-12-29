0ded8d26c8e7df3ebc06561a046eb460f7c6ed1f
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:25:57 +0000

    handle on punishment during area

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def count_characters(sentence):
-        return len(sentence)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
