bf7ad4d845f6cc488cfc40ac3f634851bd766557
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:19:05 +0000

    abroad trade runover ring dare

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
