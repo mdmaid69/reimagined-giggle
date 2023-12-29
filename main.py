77014aca4c2502ed517a57ea1e88133739b4b7f2
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:53:10 +0000

    smooth card indeed frightened several

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
