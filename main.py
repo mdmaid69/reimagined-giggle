901a08b5a0468bc0f277454a600a116b5bb178b2
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:17:42 +0000

    frame heart prize not badly

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import sys
+def print_python_version():
+        print(sys.version)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
