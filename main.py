af4511949b43151a791c694821831e4f816688cc
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:36:03 +0000

    report bad entrance only ring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
