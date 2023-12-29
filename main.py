7ee283e2d275478a37f62ace37247260d29bba06
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:06:18 +0000

    ever shock theirs correct honest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
