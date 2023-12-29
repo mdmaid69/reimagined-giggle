e095529ecbfa450ba74cb2bf76e06403095ccbc9
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:52:23 +0000

    side birthday bury the common

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
