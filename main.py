e5739999f950b562984bde92548327678f803529
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:44:57 +0000

    discussion previous exciting business song

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
