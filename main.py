4b50914f4ecf806b51bf8d67886c7f57926c0cb1
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:57:30 +0000

    goback(to) wherever school demand clear

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import time
+def get_current_time():
+        return time.time()
