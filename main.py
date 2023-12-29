0c420f1895f68e39ab74e3931a235418bc442503
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:25:03 +0000

    lead hope slowly comeoff hot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
