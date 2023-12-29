bacb6fc61cdfacf8902b481a30bac1f88230ec23
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:28:39 +0000

    break prepare reliable wander go

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def is_even(n):
+        return n % 2 == 0
