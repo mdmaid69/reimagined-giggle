740fad600f67c52f7f7a54c611c12761de2d13a9
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:31:59 +0000

    which settle brown convince original

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def delete_file(file_name):
+        os.remove(file_name)
