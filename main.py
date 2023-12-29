659c9faa95d812f3f8aa9a048af0fcfc9b5bdba2
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:51:22 +0000

    strict round order still care

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+for i in range(10): print(i)
+import math
+def calculate_sine(x):
+        return math.sin(x)
