2c9c1cefe44968f1203ecbbc0d02c95b3b3fa2ed
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:54:03 +0000

    uponon mess nose yours convenient

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def multiply_numbers(x, y):
+        return x * y
