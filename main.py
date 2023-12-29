5470f0823039fe78960e3dd98f3d2cf398506d48
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:08:18 +0000

    strange Rr tired whole mainly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import logging
+def log_message(message):
+        logging.info(message)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
