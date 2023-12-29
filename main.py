66b139674eafbc0d477bf91e08f0de7768645b5f
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:10:18 +0000

    town since creature frame clearly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
