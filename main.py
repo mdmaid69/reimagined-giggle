395783fc9db30766272df4b4ece658263d06997a
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:48:44 +0000

    breakfast flat while popular breakdown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
