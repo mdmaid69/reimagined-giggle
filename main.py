03e972951657535344eafc5c2ae051481816ce16
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:12:50 +0000

    age hit suit OKorokay nature

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def calculate_area_rectangle(l, w):
-        return l * w
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
