ccd7262cc175029b8d36b30486e44ad48f6893cb
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:21:38 +0000

    stand answer expression be more

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def subtract_numbers(x, y):
-        return x - y
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
