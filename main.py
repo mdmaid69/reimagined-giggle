8b0b5e35434a5052b6561a5c03e70f50236a509f
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:26:31 +0000

    recognize typical type own meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
