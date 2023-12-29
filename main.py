0b5da0e033e1a152927761b231d1c9237fa610ac
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:57:22 +0000

    second find there record talk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  def is_odd(n):
+        return n % 2 != 0
