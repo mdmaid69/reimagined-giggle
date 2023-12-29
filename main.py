b93bffb1fe5c71abdca5195f6e5e65dcbbf9ff3a
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:55:55 +0000

    port excellent appearance spring journey

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_power(work, time):
+        return work / time
