df7302ab9e74db03ccbee685327d980873456f65
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:03:33 +0000

    each well duty ever door

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
