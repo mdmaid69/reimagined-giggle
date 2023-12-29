1affbf68c9fa9092d401de2b23bbe31c82da0e92
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:04:13 +0000

    sale introduce tap valuable visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
