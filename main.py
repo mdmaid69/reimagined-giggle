0bfe5a2f02b40b3a39c10552641a1ffd78eb0b39
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:36:46 +0000

    other plain anywayanyhow mainly lesson

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
