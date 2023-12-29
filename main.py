2e6f2a5d397b3003bc30948c0d83fc6ba91c7bfd
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:57:22 +0000

    swimming worth vote cum achievement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
