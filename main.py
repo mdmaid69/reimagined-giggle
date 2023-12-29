0d647c3e1ea978024b9f330a93f42a4e3d1d65f4
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:51:45 +0000

    really nowhere protection contents birth

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
