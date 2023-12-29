13bcc6e9b86bf32a3400248f9ea5ac0c2e93f183
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:40:40 +0000

    congratulate Zz mind professional hold

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def convert_to_binary(n):
+        return bin(n)
