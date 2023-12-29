a4ef390b77df3af6d055d71ad9a76eec6dbcc808
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:26:47 +0000

    past dream official genuine brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
