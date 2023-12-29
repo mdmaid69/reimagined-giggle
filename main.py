0b5197b0534b7dacc4000ade16044348bd099d84
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:03:12 +0000

    pin contain brother hold not

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def reverse_string(s):
+        return s[::-1]
