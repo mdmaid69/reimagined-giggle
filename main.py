77754781352b278ac99d20a8a6512462d8c75079
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:08:58 +0000

    problem pride too snow situation

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
