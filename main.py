123d929326e2f39262279e6add8a737447c29efd
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:18:58 +0000

    machine Dd total ear lonely

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
