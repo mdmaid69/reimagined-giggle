88df022077e6edab1824899dcdd0e157fce370d9
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:23:01 +0000

    clue Jj hit fill hit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
