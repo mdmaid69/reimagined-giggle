06aa17ead37b9c8a7b92f47569ac88fccf035b0f
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:04:56 +0000

    stamp downstairs promise depend 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
