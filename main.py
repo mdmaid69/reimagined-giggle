121e758413c86cbab6946b52455f08424fbbc6c6
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:27:37 +0000

    least force unlucky swallow confident

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_density(mass, volume):
+        return mass / volume
