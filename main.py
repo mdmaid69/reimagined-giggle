b36ca59ff74d3876e597ab82f5a6e855c91accf6
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:34:31 +0000

    smooth hope noone bring turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
