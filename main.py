636899ee3cbeab704100ab15150ece399cb9c7d3
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:00:47 +0000

    match fair fight inform among

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
