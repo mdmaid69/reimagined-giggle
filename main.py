86fe48ad29f36ae7fdbd4ab38ebeafc3a0309464
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:34:45 +0000

    means true further control takeout

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def cube_number(x):
-        return x**3
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
