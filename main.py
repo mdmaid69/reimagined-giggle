a7c049b8208afa2def4ba142166f716660ad06de
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:10:44 +0000

    recognize fasten interesting time close

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
