b4a81cf63a7afa9bfaae016b68f03ea47d5df002
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:39:07 +0000

    in consider roll movement bottom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def remove_duplicates(lst):
+        return list(set(lst))
