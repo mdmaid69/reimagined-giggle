1ca4d227eab13a237be409019b92907ae6364623
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:12:51 +0000

    ready hold praise post slice

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import collections
-def create_user_list():
-        return collections.UserList()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
