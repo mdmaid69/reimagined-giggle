f845fb6baa5aa1d5ea366d62d6dfa8c3f5fab9c9
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:09:47 +0000

    tired lookaround play within rush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import array
+def get_array_as_bool(array):
+        return bool(array)
