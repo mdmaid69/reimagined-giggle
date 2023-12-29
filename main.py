01202791d31748ffca8fb774d9fe91d7d1c0c632
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:44:53 +0000

    early possession care importance sometimes

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  def convert_to_hex(n):
-        return hex(n)
+def is_even(n):
+        return n % 2 == 0
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
