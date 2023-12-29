1565e79374f1709e6e81d869530cbfca6e0d15e8
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:37:48 +0000

    excuse motion smart genuine charge

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
