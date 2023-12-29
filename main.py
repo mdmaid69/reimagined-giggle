6b5e9c18ed64fc733e1aa18ae6ceea65ab131f19
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:00:31 +0000

    club common pupil story die

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
