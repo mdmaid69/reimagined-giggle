e0efd0d2d054d3c169c493f94d32a9eedfcd1300
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:40:35 +0000

    store push talk any famous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-name = "Python"
-print("Hello,", name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def convert_to_octal(n):
+        return oct(n)
