160029d790a205aaa9cdea7b5807982b7c3c2e2b
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:52:00 +0000

    shame attitude attach buy obvious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
