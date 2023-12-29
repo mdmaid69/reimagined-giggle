8377931bef3f97f77995d4c291d29f107c15c00a
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:05:06 +0000

    lie machine expect getout mean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
