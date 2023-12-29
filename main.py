b5795c01c5349c9c73bd770c2d9a3db6f958567f
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:29:17 +0000

    dry any keep offer pull

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def convert_array_to_unicode(array):
+        return array.tounicode()
