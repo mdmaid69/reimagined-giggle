ffdf4ac6f040ffdf2143a25b74753e23150ca8cd
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:32:24 +0000

    extremely style hungry police nose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
