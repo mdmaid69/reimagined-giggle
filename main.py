35fb84501223841e424fa7db4526d5e775a49bcf
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:57:01 +0000

    fresh hole communicate takeout proud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_volume(length, width, height):
+        return length * width * height
