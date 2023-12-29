ff138674169352443e1300c12902d57f0bd6805b
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:49:40 +0000

    odd chance surprise draw exist

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  def subtract_numbers(x, y):
+        return x - y
