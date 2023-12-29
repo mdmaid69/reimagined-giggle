20717eb655be02959f05f748ff15ec1ee81a5f0c
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:45:59 +0000

    brush by praise behind yet

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def convert_to_hex(n):
-        return hex(n)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
