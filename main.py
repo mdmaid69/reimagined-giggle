61dfc4406b5074e02ae1a2d687c40294c1aa7510
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:46:39 +0000

    complain field left dry school

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import time
-print(time.time())
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
