ac2cd2ebc55fac7f7b4abf0e94acfc34b3e34bd1
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:22:55 +0000

    empty complain runover object rely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
