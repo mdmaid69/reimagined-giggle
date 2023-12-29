b010fae1490d0b0dcbf70d998fc2411ca2575a33
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:02:29 +0000

    understand arrival wherever ill comeup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-for i in range(5):
-        print(i)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
