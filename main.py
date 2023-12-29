458d9c660db7bd926262c5eac930b0bb6fc2855d
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:51:10 +0000

    contrast action stand direct conclusion

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def find_max(numbers):
+        return max(numbers)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
