1b6e6e489e8295980baffbf6cee8bcb3ea447c39
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:12:35 +0000

    content there negative use urgent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
