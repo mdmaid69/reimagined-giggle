556b0bd4b218dadea2fff825bf9a07dbac4a0ac5
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:00:31 +0000

    life introduce responsibility sink holiday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
