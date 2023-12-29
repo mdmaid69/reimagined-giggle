eca29c82f31f580d957504e94a51c158da555f46
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:40:22 +0000

    computer service contrast this think

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
