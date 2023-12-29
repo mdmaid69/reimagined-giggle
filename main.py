15023e84a7d638771851e3c4cd651968f232fdea
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:35:51 +0000

    port yet mainly Itself us

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def count_elements(lst):
+        return len(lst)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
