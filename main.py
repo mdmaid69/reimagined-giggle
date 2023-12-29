924b5a70f15c5360816d86af5bed5d3e9592b9e4
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:36:34 +0000

    correct better appearance table dry

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import collections
+def create_user_string():
+        return collections.UserString()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
