0f2e4276f771f3fef5865e05dab206682c0fe285
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:10:37 +0000

    talkoutof money bringforward advantage search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
