5e4f15a475d2c385d92f945dcee998635be93657
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:00:43 +0000

    measure cause Cc environment female

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
