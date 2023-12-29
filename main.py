93ff1db70e8d3dec60cd44f617e3b45f5c6e90f9
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:43:10 +0000

    start pleasure stay loose direction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
