5c40f97c8a1b89a919257c4915e08a8da15b64e2
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:18:21 +0000

    beside gather advanced characteristic decide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
