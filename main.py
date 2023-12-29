106fb1ada043d822a3586bf5875813eaaed46ad8
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:59:55 +0000

    bury suddenly any smooth approve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
