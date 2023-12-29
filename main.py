4d6b14b736c2f8f939ac122b87bb57e99b5e4b86
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:09:40 +0000

    sure fight ourselves likely fit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def delete_file(file_name):
+        os.remove(file_name)
+def find_max(numbers):
+        return max(numbers)
