567ce71eabe7a699d017ffec299ae604c6378714
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:05:53 +0000

    admit fire spite consider tool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
