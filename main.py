e2206b563c9b0f18caf6d9ceb796205b856e7277
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:58:37 +0000

    familiar getoff personal like apologizeapologise

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
