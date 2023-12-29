139a502a26d0ad0e4ee526b745f21e791ee72784
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:28:58 +0000

    suggest now finish exciting arrange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
