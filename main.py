587d17b0ba2b587c29598d02479f67cc6d6a4e0d
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:59:31 +0000

    quit thinkthrough health usually level

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
