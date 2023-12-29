62410f62964b7bbe80d60a4028e6a43378f8ae30
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:43:22 +0000

    theirs present tear damage degree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import collections
+def create_priority_queue():
+        return collections.deque()
