b22c7690aa5074a24bc84ca00adc0bf0b5079a9e
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:57:36 +0000

    agreement watch quick genuine turnout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_min(numbers):
+        return min(numbers)
