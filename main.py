782566647915fd288b5fbc7ef4b1844109f8a2b2
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:44:53 +0000

    on ocean immediate fresh petrol

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import platform
+def get_python_version():
+        return platform.python_version()
