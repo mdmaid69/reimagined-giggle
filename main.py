35d8e203c60117daa5724829c72f592359042c0b
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:48:53 +0000

    detail smooth bored content nowhere

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
