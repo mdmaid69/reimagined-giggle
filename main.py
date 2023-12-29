7d1e965fac3f2e69c4c12108d8f1b5f61976fa7f
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:36:07 +0000

    blame taste his use float

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
