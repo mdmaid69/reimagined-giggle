7f37a4ab950484b91a1466493afbf7049bddf9fc
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:31:37 +0000

    degree impressed smile look inside

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
