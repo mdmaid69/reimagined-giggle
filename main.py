31db74a316ac5b3f80088b5a7a8d5498294f43f0
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:28:39 +0000

    closed promise protest prepare unfair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
