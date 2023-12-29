96caa20fea65f64c5dba9c72ecf74439a4992b4d
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:38:43 +0000

    get main strike take before

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def convert_to_binary(n):
+        return bin(n)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
