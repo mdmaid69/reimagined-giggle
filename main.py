cbddc1186f3b3fb0bdea563661572a54b0678516
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:28:11 +0000

    near bet notice hollow discussion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_as_int(array):
+        return int(array[0])
