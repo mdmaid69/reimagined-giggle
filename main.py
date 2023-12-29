40fb4b6683c0102a396b9c7866011b8ea76d7547
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:21:56 +0000

    needle although on heart lie

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
