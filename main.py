8602b09ab42b74415c0ac1d0f2be322b4cffe10e
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:43:34 +0000

    steady plant suck exciting sticky

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-from collections import Counter
-print(Counter("hello world"))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def convert_to_binary(n):
+        return bin(n)
