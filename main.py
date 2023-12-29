2b54e1d0c8fc7c9f95d6eaba086afd034b4ddf4f
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:14:38 +0000

    admire anyone label success force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import re
+print(re.match("h.*o", "hello world"))
