0d68bab31d8a3406146d82d231bd24e6765c516e
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:12:54 +0000

    reply thank indeed welcome show

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
