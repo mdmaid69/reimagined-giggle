03858c1a724fc0c926d862c0c309f6babf7a0717
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:28:33 +0000

    fetch familiar success break regular

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
