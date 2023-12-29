5267f951415634fc4ba6b5aff6338c46268b55c2
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:06:35 +0000

    value programme store dark talkoutof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def print_python_version():
+        print(sys.version)
