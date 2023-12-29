0ac12c90383812a2ed10f6988072882d6da977b8
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:47:51 +0000

    welcome prefer organize develop tie

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
