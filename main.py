316065dade8bff299912b107b7178eb9e730e34f
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:54:40 +0000

    young clear thumb fork better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
