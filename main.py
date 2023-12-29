9e7d86ec26d7e8d18a3e21f7781ca9782a5430db
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:31:19 +0000

    sick wind flat whoever delicate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def count_characters(sentence):
+        return len(sentence)
