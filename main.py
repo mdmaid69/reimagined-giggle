fd090cc63e90001823c665f8256fb7993291cef1
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:53:52 +0000

    truth play film base nearly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
