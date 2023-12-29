0104561f2cbd0b4fb7d709b59e0911c2734ba483
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:06:18 +0000

    unusual excuse cross destroy discuss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
