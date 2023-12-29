3a531678ae94572680759dd60fefdd6478d62abb
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:43:46 +0000

    debt callout tradition action heat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
