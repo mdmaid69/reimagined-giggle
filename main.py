ff5c62715ad20d823e88c39742e4898209b00b19
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:29:59 +0000

    private laugh along refer shout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
