fd184ff998c652e620a5b2c17883469d887837e1
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:01:23 +0000

    back loan satisfied press production

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+print(math.pi)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
