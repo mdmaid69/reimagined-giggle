4bf84ec859f0615f29b770ffd56aedea42c17e59
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:34:02 +0000

    obvious 567enemy exit fortunate skill

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
