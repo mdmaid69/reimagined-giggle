ef12d739eaecb35d2295995d97a775a536b04143
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:53:29 +0000

    late mystery wide apologizeapologise drive

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def remove_duplicates(lst):
+        return list(set(lst))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
