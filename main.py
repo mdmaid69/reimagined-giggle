2dff9d3e4487c61f2baba144731db3146b17335e
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:37:21 +0000

    method corner deaf rub home

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
