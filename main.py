ef9a90477280ef22a6bf3088cda79df9598ca62b
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:49:08 +0000

    further yourself geton bye burn

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-print(sum(range(10)))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
