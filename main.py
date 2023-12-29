e9ea7333cb7b0677307228e63f26e6bc1f0e8d66
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:51:37 +0000

    steady spray general answer pleasure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import os
+print(os.getcwd())
