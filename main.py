ad821a7ea66422027499bc4c6786ce7ee37bac5f
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:22:25 +0000

    none mix capital wise split

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import collections
+def create_user_list():
+        return collections.UserList()
