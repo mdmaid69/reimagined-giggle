32d44239178a6dc82fd8e8153c3558dd7c92252a
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:27:45 +0000

    professional search determined minority undo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def get_array_as_str(array):
-        return str(array)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
