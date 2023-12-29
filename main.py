ed88c4cf0fc4ed69f793b104e3003af695c229dd
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:43:56 +0000

    repair earth lucky prize speed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
