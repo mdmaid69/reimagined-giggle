70e3dfa8887f6da81c1f6bc0df4cb197aa9fba4b
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:52:47 +0000

    present storm prize unkind original

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
