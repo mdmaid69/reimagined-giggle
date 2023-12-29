72a9da8bb083b097c9f7fa0e4502c591df2715df
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:40:24 +0000

    attempt none chat somehow knock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
