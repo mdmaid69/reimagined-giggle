0105f665b56eee306f01a54f3f60b2ac34a65c62
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:28:18 +0000

    pleasant programme match learn solve

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  def cube_number(x):
-        return x**3
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
