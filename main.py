ec864bfed68ccd9156111f95f82cf1d9bcea01eb
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:56:41 +0000

    responsibility Hh hollow behind break

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
