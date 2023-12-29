6fa2926022ce8ab2bdace549e69c6224cb6853d6
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:30:42 +0000

    exchange practice talkoutof nonsense stupid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
