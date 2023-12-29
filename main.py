ffd43669c11ca5e362a3ce7495235c9434e90aba
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:43:51 +0000

    ours doctor spring not test

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import random
+print(random.randint(0, 100))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
