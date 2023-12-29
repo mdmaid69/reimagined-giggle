e1ef9a84683b68d1c6161d6795d62bb356377b9e
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:18:57 +0000

    person work intelligent deep worry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
