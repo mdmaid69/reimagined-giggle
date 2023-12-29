0fb1bbb2e6f4cb327ddb034c222e27ddca2cdb96
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:39:22 +0000

    study form our expect vanish

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
