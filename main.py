6dbb6444813143c9b2e822c05d4efb59caa74f8d
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:17:06 +0000

    breakup black talkback fire favour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import random
+print(random.randint(0, 100))
