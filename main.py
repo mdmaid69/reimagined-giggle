78db843ab22356517787973f34fc2976edd0a5ce
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:47:30 +0000

    close gentle lend act neither

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import os
+def change_working_directory(path):
+        os.chdir(path)
