ccd5cbd5bee0c1dd980b7c860adde8e9fca28c2d
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:30:49 +0000

    collect alive trousers talk fly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
