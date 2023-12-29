cea9f084a9b7aa60440a2f2adadd3bb24836a2cd
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:48:33 +0000

    square example key cut responsible

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-name = "Python"
-print("Hello,", name)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
