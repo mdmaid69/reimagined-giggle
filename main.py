eb27ec187eb0fa4c42221eb1268e851e33277a36
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:20:16 +0000

    bunch friend situation nerve upstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
