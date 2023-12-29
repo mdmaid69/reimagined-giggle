f3bc27a5909ca2892e518533dea6018636e3f1f9
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:48:24 +0000

    flavour entry outside spread explanation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
