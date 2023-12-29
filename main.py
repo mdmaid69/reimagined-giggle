4c9a86f09546bd9044a5e2433bb90b1ad27e76a7
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:21:32 +0000

    ride deal file thorough after

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def convert_to_hex(n):
-        return hex(n)
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
