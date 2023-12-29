1ed7656e3a375eb0d36fc0e80cf89f01cef61964
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:19:46 +0000

    everybody press strict goup coffee

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def add_numbers(x, y):
+        return x + y
