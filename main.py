7e47a7b751569ef60c83c700ae871fe74fe85624
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:50:58 +0000

    more to community different godown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_work(force, distance):
+        return force * distance
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
