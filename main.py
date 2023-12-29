29cb4ab05d131e5d8c332ccf03de158cb499cc75
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:58:46 +0000

    alive slowly and contrast fall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_max(numbers):
+        return max(numbers)
