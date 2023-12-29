2f8fff4d731714d4cc1dc12db0bd5b6651bb66d4
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:45:28 +0000

    beginning agreement spot finger trip

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def remove_duplicates(lst):
+        return list(set(lst))
+  def subtract_numbers(x, y):
+        return x - y
