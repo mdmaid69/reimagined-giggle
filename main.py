5edec9e92a46a5560decc7f1be41520ed4a1b7c6
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:17:42 +0000

    situation niggaacademy whatever waste lesson

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-text = "Hello, world!"
-print("Characters:", len(text))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
