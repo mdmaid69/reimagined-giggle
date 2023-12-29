4850d0eb5ec50043f7b34d62b4bdb5bdbf5f89c9
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:56:28 +0000

    area prefer holdout increase attempt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_density(mass, volume):
+        return mass / volume
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
