682944df001c26460b45271d249d5a442e30de2d
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:20:25 +0000

    workout effect talkinto niggaacademy fairly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def convert_to_hex(n):
-        return hex(n)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
