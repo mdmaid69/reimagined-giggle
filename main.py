4dde1af0954f1885af7d0fe25788af4bf4c420ee
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:22:16 +0000

    substance loose promise cover mass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
