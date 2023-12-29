aecdb9d0efa7b6f5af89dcaa7e2df79986d6dabe
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:46:39 +0000

    improvement pronounce admire copy guard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
