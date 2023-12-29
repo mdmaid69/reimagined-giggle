bf672e48d49ea2b51c8d1003ef390897b8febc02
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:31:55 +0000

    organization fork tongue shopping everything

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
