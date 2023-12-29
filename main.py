7c64e36f9230ee24ce6b6ef1985426c383b89cd9
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:39:47 +0000

    type Zz at measure answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import logging
 def setup_logging(level):
         logging.basicConfig(level=level)
