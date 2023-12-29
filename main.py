744874145d5e56e88837a4905d121df7ad3c2f46
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:16:44 +0000

    swallow protest limit inquire Ff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_power(work, time):
+        return work / time
