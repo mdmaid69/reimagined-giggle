2ef17b64aa73351d682c641f74f83703ddabdebc
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:45:52 +0000

    plan where grade carry off

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
