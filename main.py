049a714f3bdc1976a43cdc677a7ddbdad84f84ff
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:52:13 +0000

    stage plastic sharp Hh 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_unique_words(sentence):
+        return set(sentence.split())
