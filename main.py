605028c7dbb18e5ea498789860827bcfa51755fa
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:07:04 +0000

    hurry bite settle division criticism

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
   import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def create_numpy_array(lst):
+        return np.array(lst)
