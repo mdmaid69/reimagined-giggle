55a40a3e813bfb05ae17d57fba9c2715193a0ef2
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:02:48 +0000

    today discovery friendly office reduce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
