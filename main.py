f5e6bfb4ffb2fc857bed5a9f26ad45c46019aefd
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:10:32 +0000

    man key exploration after start

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_acceleration(speed, time):
+        return speed / time
