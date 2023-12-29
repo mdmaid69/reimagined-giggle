74917e1f2b614af496a67c410701efb0d6010356
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:15:07 +0000

    crisis your statement Ii low

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
