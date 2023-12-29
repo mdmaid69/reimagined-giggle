1901adc949e0320e43ccbb4adb5ffab77b04440a
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:11:33 +0000

    stop wild without music embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
   import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
