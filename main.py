960e26003f52553ebc543f684155d2aa47a8c6d3
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:35:07 +0000

    raise hear guilty material tonight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def find_max(numbers):
+        return max(numbers)
