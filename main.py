ac5a0cb076f6478a652cebce1b77372084e73d78
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:17:36 +0000

    machine estimate finger university whistle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
