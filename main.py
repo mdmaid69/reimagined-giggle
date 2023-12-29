0565bedebc5c5f0936707ed1d5984bd8bdb38934
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:43:58 +0000

    insist cheek regular now though

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_work(force, distance):
+        return force * distance
