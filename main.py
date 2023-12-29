65e9098befc9ca6f6e9c1f9ba1e7fa48c0c7a208
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:49:53 +0000

    negative crowd mine unusual branch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
