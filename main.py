251574a3393696f92b064c59124f8eb3d9153db6
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:21:40 +0000

    pass light shame divide capital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
