24b09e0a28076b560a67a9551192cfd315865719
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:56:26 +0000

    person contrast flood reason follow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+text = "Hello, world!"
+print("Words:", len(text.split()))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
