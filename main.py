ffe531287658887fb24e6a7a0f1fed91084549ae
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:03:45 +0000

    trip fair left along pinch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_work(force, distance):
+        return force * distance
