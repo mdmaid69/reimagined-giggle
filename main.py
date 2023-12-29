ee4a591eff0b16db481ffc8fd3ba88a56bc6edfb
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:55:35 +0000

    crazy thanks rough young calmly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
