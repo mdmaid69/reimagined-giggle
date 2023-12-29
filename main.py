9727debd6e83f6fa80f91f321b6cfc2f8ce6f8f0
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:01:13 +0000

    warn present constantly home mess

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
