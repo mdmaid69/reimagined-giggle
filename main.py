aeb34974c7b5ef701116ffa92c3eef08a4a7f60c
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:42:05 +0000

    roll walk through urgent last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
