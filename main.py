202c82b32245f4f2a52f88cfdbe8fe6c41578688
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:12:59 +0000

    makeinto copy threaten why comeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
