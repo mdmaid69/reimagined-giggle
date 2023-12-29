11414176a9bc38f4057be68027bb9ebb24df6fff
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:21:34 +0000

    guess rent long someone wrap

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
