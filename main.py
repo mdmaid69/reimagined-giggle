1a0a8ba36c930883bc1a1ba37bf47431023155a5
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:16:05 +0000

    wide learn prefer line name

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
