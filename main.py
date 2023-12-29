75f7ad4ba1044ffa123131585072c59c2a758b3e
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:11:43 +0000

    draw progress admit trade single

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
