f3918cd3045a49ca4812ab86832f0c8d85139a2f
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:16:55 +0000

    stop improve Tt dig event

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
