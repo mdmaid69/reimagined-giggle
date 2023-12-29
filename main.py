71305de20ddd75ebed33708b4d3f7a0fc46d455d
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:23:10 +0000

    second congratulate independent must off

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
