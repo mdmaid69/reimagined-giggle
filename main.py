4c513427143a95788539f12623dd07f86bcfa304
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:45:48 +0000

    object argue perfect front skin

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-name = "Python"
-print("Hello,", name)
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
