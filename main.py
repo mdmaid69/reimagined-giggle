4fc642d3bb5172ab11aa288979cbfb165526f1eb
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:32:34 +0000

    production space disgusting dear emergency

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
