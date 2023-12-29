6643ce11eb5202aed905f6c85759412aefca031b
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:33:01 +0000

    breakinto aware yours release loose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sys
+print(sys.version)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
