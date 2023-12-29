09c089f8557ea81d50d926f52921c7530df910a2
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:27:51 +0000

    cruel food use and order

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
