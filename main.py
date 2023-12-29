24789316b0883b04518d9da8ef18a229bce36a7e
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:55:41 +0000

    tune police calmly flood people

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def split_string(pattern, string):
+        return re.split(pattern, string)
