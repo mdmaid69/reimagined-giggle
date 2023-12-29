ceb1f49d41fd2eccb83af164a5b074498a5fc61d
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:52:22 +0000

    disappointed dance want disturb disappear

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def get_array_length(array):
+        return len(array)
