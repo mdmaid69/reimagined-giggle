2ee9763b1489e1a706157dc410671f0ee445a0ce
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:48:16 +0000

    never boil score stay justice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
