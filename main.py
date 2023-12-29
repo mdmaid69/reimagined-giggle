b49c20552309c15a93bde9a55a5e8a9ed5a6063d
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:33:04 +0000

    introduction accident send read conduct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import re
-print(re.match("h.*o", "hello world"))
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
