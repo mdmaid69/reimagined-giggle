ad7eaf059bbd65f9a7f85688607e8a529bd360dc
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:26:22 +0000

    with alive bottom impossible I

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_density(mass, volume):
+        return mass / volume
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
