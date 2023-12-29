528cbb90fa174764cc36de5547a8dd41daf59890
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:49:40 +0000

    remember godown beginning ill invite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
