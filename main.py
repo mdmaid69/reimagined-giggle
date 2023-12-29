23e388a1214ef406e37035d1afa67cd0e0007e93
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:15:27 +0000

    yourself passage price unfair stand

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import json
+def convert_to_json(data):
+        return json.dumps(data)
