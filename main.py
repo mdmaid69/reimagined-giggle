3de562ec09ce0a3983e219e9b6b080345e1d65ea
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:43:07 +0000

    read reject control hurt knock

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
