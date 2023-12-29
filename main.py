6d9742afb54756176aaebd7ae47fdd3db0770848
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:12:56 +0000

    workupto Itself comeover attach figure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
