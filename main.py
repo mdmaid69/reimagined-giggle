8da21612e995a88ae10684f58f1e6cbfba116e72
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:33:44 +0000

    complain most clue illness pleased

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
