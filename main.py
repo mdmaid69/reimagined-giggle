40820467348ef32bf1dda51ef8be1bbf6a884668
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:15:50 +0000

    mass professional 572enter ball eager

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def calculate_speed(distance, time):
-        return distance / time
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
