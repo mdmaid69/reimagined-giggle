e0bbbc2f1b33f6256a1404a2d769c4724b84949e
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:13:33 +0000

    related pipe pronunciation turninto culture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
