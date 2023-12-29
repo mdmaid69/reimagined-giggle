28f6527c876226e8d3dbe9adac3e6757e05b20ef
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:53:28 +0000

    address container order use protest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import sys
-def exit_program():
-        sys.exit()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
