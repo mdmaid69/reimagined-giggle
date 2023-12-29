1436ca7e484fb3012e816f87b31b32684f27ff1c
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:33:23 +0000

    child unkind depend criticize each

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def is_even(n):
+        return n % 2 == 0
+def convert_to_hex(n):
+        return hex(n)
