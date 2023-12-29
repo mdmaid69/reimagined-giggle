a13aadd9f01a4203c7cdfc723691a8ef92481866
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:52:06 +0000

    wet possibility book below nerve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-print([x**2 for x in range(10)])
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
