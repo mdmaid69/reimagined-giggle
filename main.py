56064d03c311bee4a866518ba5f6f62616aec026
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:57:38 +0000

    style track include break wave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def is_even(n):
+        return n % 2 == 0
+import getpass
+def get_username():
+        return getpass.getuser()
