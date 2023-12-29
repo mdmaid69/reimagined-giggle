a251a608370f7b216b77fefb59ac9ce424c9867e
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:25:08 +0000

    Qq correct pocket call whichever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_uid(file_name):
+  def get_file_owner(file_name):
         return os.stat(file_name).st_uid
