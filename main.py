9e65a457d101461e491f921b57fa62cc42a63602
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:05:18 +0000

    if off mark excellent top

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
