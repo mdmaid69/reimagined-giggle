c54caf600ee31db13cde0d9ff72e160777af4f43
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:46:44 +0000

    flat slowly lean sing ear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
