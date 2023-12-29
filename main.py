e58c662155e0a0baa2fe8a501ecb05f0e6664414
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:39:55 +0000

    scream throw sale write light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def set_array_item(array, i, item):
+        array[i] = item
