1a79953931251cb6f03611368ae9af1a61214705
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:12:14 +0000

    where regret giveup taste comeout

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
