fc7009ca1c461acf04fac5e9561ec16353b4cc9f
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:18:32 +0000

    callat progress between wing clean

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def get_array_as_str(array):
+        return str(array)
