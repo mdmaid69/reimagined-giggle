74e86ad4a2bb9d7da8ebec3db1f0f700dec4838b
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:53:12 +0000

    front transparent still minor drink

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
