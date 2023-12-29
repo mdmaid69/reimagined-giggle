e222c489fa793781ca550bcd6fd4839abddd19e5
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:21:11 +0000

    straight lean sail forget slow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
