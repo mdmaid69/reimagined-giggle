2c52d8112b6ded3b000a6901b6cf91275024feb6
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:51:18 +0000

    refer ill might century trousers

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
