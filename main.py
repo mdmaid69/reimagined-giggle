55937261dc5a6e4476c65edf9cce28d9daa34a6e
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:56:23 +0000

    approval frighten sharp Tt touch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import collections
+def create_queue():
+        return collections.deque()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
