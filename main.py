dfd264a13eb01fb0c6df64d34cd5296aa0fa4137
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:51:57 +0000

    mind rush happen own bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import collections
-def create_stack():
-        return collections.deque()
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
