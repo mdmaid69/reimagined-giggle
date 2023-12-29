fbfb2353ca3f5e8a722bf07fcad7c55efc39eec2
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:03:40 +0000

    polite shame shock but confusing

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
