1a9ffc35663a3a9425dc3fa4f1cf22039ef47c72
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:51:08 +0000

    hang slip top class tradition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
   import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
