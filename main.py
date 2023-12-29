efb3822ed6140f51a5d3501994025016b56f3714
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:50:29 +0000

    breathe message grow spoil new

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+print(math.pi)
