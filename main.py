6547f384639bdb6aa72d995b344ed0363612cb1a
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:38:28 +0000

    cut unit anybody step we

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
