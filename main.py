733657fce921cb00f6298e6a8dd7e1e40ae98200
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:47:52 +0000

    score motion freeze absolutely shadow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
