72e4b907a92fb63769ee58a63a4c49e85854ba9b
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:52:02 +0000

    upset file private straight definite

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
