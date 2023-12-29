3ca73d85f050701baaf7f7aca23d53a26680ba82
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:07:48 +0000

    switch embarrassing traffic habit that

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def remove_from_array(array, item):
+        array.remove(item)
