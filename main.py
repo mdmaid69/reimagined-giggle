e999b221d49e927915d5ed0a983b78caee6b1636
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:19:26 +0000

    below sense Rr platform relief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import json
+def convert_to_json(data):
+        return json.dumps(data)
