a1aa3874f0f7b20e29605c14c66d6668cdf0f508
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:24:11 +0000

    central split combination old owe

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
