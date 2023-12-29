83c8657ad0c9049ad37a6771e417278a156e545f
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:05:01 +0000

    constantly Ff talk slice tax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
