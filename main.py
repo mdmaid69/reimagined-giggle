e1e433eee4fcc9c3decf4f732e8700b546052614
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:39:56 +0000

    once arm land discover when

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_typecode(array):
+        return array.typecode
