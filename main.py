ef4a4c110d1540db24b5768c05e69c64a624fd0f
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:18:56 +0000

    dull run explain relationship shelter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
