6861b095db6a723d7fe7e9fc39cf272d1491326b
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:51:18 +0000

    house congratulate need mark regret

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
