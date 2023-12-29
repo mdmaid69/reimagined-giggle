0c272803c621bb2700265bea80b9c6dd60512cfa
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:21:55 +0000

    active none object spite room

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
