38c0a14bd46caa2a78a1274e63e71efff1465a62
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:23:46 +0000

    against basically operation else live

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
