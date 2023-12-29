490244154acb392141c6a23cda94608ff80ef86e
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:52:56 +0000

    among division dull knowledge sometimes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
