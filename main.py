de6151bdcba0f23e7b2c95b4006579e24b92a63d
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:41:09 +0000

    limit note demand murder benefit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def calculate_area_rectangle(l, w):
+        return l * w
