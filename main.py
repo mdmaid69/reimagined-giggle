43854b42acad787b2d50d311fdf4d7974c8a0a11
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:18:42 +0000

    ought embarrassed spin workon flat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
