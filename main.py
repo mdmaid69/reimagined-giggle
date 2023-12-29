1d7953f50e73521c1d6c32254f7b91da3c0fbb56
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:32:36 +0000

    circumstance juice how fetch break

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import time
-print(time.time())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sys
+print(sys.version)
