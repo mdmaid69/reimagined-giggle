7a609b9aa595f4e35381f7e79b1a5def75e6effb
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:05:29 +0000

    careful train build year warn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
