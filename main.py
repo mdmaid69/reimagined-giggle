a32a28cd664eaa58e4b07fea2f579ffa780ffa59
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:49:45 +0000

    impressed try turnaway stare person

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
