e360113093e6b105959f7a83725e30209b6adf09
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:59:42 +0000

    human practise deliver shape we

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def reverse_string(s):
+        return s[::-1]
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
