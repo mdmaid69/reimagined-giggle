b23f866af6270be5a763ebcdfba9183aa9cb9d9c
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:43:07 +0000

    Kk judge study prefer payment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_bool(array):
+        return bool(array)
+print(sum(range(10)))
