7bdc70bbc0bbba7afcaa5edf8e2531a52fb35548
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:30:03 +0000

    combination act between body mean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
