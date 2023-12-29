3bc2783dacd21a8f21780eca422842ed03986790
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:02:53 +0000

    embarrassed boring behind watch future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
