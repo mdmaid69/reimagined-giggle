076227c8b095e5dd69bfb38ae0734f1d47d8ab07
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:52:36 +0000

    book neck land congratulations tooth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def find_max(numbers):
-        return max(numbers)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
