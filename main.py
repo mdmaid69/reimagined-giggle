4607bd25a52d39eea85a45baedfc2c9d63d71f81
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:56:24 +0000

    start watch sticky makeinto sudden

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
