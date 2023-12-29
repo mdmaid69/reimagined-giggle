a761c30d7920cc7ddf9e1c95c770aa5e82cc3591
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:38:15 +0000

    lookup contents display object story

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+print(sum(range(10)))
