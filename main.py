049e1f6728497e68a911a5b2a8d0a17e22883ad9
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:32:05 +0000

    punishment wild experience 569enjoy press

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def find_min(lst):
-        return min(lst)
+def count_elements(lst):
+        return len(lst)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
