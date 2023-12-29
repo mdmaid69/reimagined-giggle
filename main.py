ca105d9f214deb436e6cb97c3887c398158d6f15
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:18:18 +0000

    child admire keepfrom front recognize

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_area_rectangle(l, w):
-        return l * w
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
