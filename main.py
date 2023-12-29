8c5181e68aabb13d8ab4a9fc3d5292c6a0956f38
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:55:28 +0000

    figure disapprove admit consider needle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
