7e76d25b75642b241531f658d96e60bf557f0c4b
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:38:40 +0000

    grow cost close people dark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
