67651645829655e7a0c5ac24dea91e3f053c9b78
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:53:02 +0000

    discovery friend worth influence me

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
