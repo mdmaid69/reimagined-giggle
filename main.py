ed409d28bf4d76ad3bed3ae00c4f8264e4a3e5d1
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:21:11 +0000

    mysterious mean line damage doubt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
