ba53fe79815233c2fa90185059309962218a02ff
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:34:21 +0000

    schedule offend tough lay also

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
