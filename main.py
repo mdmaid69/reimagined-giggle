e7f6b4002ddd58794b662a0b43eaf5298541e47c
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:33:52 +0000

    prevent agree Oo bank getup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def remove_duplicates(lst):
-        return list(set(lst))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
