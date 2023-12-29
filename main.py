add867fd08de9542572a181e2814e9f068c28fce
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:34:34 +0000

    exercise your hit year ever

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+text = "Hello, world!"
+print("Characters:", len(text))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
