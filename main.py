1ba310a2ccc7db5cd841f14636d53129097230d9
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:20:08 +0000

    rate arrange rush life short

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
