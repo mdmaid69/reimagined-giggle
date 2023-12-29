bc11ec1011767abba0aca520279f00a0ed206800
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:24:46 +0000

    heat block best Jj window

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def is_odd(n):
+        return n % 2 != 0
