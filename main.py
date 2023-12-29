1a85d9a57cf4ebeeb3f4fd59021dd6777fef42d6
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:53:01 +0000

    usually side freedom jump woman

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
