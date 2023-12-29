3ac582e3f8a83621f3e2cee9fb67f6b661d86deb
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:22:10 +0000

    check glass disappointed skirt whichever

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
