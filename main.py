4df512b5da3b999def598c417d51e313181f831c
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:25:21 +0000

    fight typical occasion work copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def count_words(sentence):
+        return len(sentence.split())
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
