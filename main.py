e8e6e98486bc3c414f993a28f2ddf8747ed71c31
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:47:48 +0000

    kind sign spare there hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
