c78721567304f20ccf93b396e0c1160c00454d7c
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:26:44 +0000

    far natural send sort thin

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import collections
+def create_queue():
+        return collections.deque()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
