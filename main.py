a38287803c4e183a1402419d0cc85cb5a2f00d23
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:12:03 +0000

    unfortunate rest earn weak prevent

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 def calculate_future_value(principal, rate, time):
         return principal * (1 + rate)**time
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_density(mass, volume):
+        return mass / volume
