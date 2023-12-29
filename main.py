75ee973c5c5be2bb05400c319fe0bd34e057b969
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:50:06 +0000

    opinion operate bringdown Pp delight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_as_int(array):
+        return int(array[0])
