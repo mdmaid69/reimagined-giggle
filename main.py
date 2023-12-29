bdd91b1f836dbaeb26d34d907628468138f18e88
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:23:32 +0000

    loan weak contrast top however

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
