9495cf303dbbf5ef1dc01b72751021dade8e61f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:23:42 +0000

    drink rate unable rise guard

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def count_words(sentence):
-        return len(sentence.split())
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_work(force, distance):
+        return force * distance
