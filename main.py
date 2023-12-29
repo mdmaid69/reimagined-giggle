15a8ef15d0117f62cd4e37ee0680daff3d4b6afe
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:10:54 +0000

    favourite should successful contrast sure

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
