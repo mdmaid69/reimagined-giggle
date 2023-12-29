e77f8bea7e5c69108cd6ae385050ea9e4fed368b
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:32:23 +0000

    test content challenge position nowhere

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-for i in range(10): print(i)
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
