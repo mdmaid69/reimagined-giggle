559e69fafad7b389555906e06d32b16d25c3dd56
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:16:40 +0000

    wipe record law giveout kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
