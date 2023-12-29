41fb0f23d5a0fe1f824d1fdcc9e282b6c60496b9
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:39:11 +0000

    operation embarrassing 567enemy finger fail

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
