4f7df94c0643841ace39804b7ada3d70c83f623d
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:06:34 +0000

    wrong medium move be mess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
