436462c74aaba99de7e342c69237fbb32966a520
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:23:21 +0000

    case skill frightened come pipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
