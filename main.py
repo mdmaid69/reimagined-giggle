f2d3963a144eda09c4e081ebef1b0d22f63dfc82
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:03:49 +0000

    weekend answer within undo estimate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
