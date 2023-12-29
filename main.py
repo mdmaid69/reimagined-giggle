c1d9edcc2d83d93716c4de37549111d095aab185
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:10:05 +0000

    luck side year ready harm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def convert_to_binary(n):
-        return bin(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
