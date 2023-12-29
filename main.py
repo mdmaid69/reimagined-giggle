0ba3386d2e23d8028e5334d658523e7b0d1648bb
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:50:20 +0000

    definite party door we ride

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
