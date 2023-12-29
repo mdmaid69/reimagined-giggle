006d0b633eef56d2717630e1f9bcf8f404ce2178
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:20:16 +0000

    declaration upstairs wish at smooth

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import math
+def calculate_sine(x):
+        return math.sin(x)
