7d2e9ce84749dbbf3bad484cb6c096605c59b105
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:44:00 +0000

    middle tooth tape represent equal

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import sys
+  def get_python_version():
+        return sys.version
