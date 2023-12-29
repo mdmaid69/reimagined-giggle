dbea7d676f5aad5afe122e2e296fcc9a38916bea
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:53:13 +0000

    tone wander wrong room welcome

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
