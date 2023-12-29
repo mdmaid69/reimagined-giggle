2155e31b0d8257dcff3a76c106a77c2870fb21ad
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:47:16 +0000

    tragedy cover cinema claim hair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_power(work, time):
+        return work / time
