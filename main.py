f8a67fd9d0cdeebaf6946cca2925c9f6109eba75
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:57:59 +0000

    control correct basically pair fortunately

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
