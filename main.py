ee77386c037ed6953fe04cadd76f0183303a5d3a
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:25:47 +0000

    Ii term callat trouble advantage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
