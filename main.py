726f2cc750c43caa27b1e0e40105d4103dec0d1e
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:15:48 +0000

    total spring prize and letter

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+for i in range(5):
+        print(i)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
