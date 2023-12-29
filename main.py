d4ff9955e6c1d95488f60cab973f914736ab7c44
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:35:17 +0000

    chance approve brain apart backwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
