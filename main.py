114364e8cde4ddec469013c3a6b842ba64a5313c
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:42:52 +0000

    wonder hungry near however detail

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_length(array):
-        return len(array)
-import random
-print(random.randint(0, 100))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
