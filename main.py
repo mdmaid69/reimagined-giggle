c0de9fb5558790e8bb79852dfcececece2090bc0
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:22:54 +0000

    well down Ss clean week

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def find_min(lst):
-        return min(lst)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
