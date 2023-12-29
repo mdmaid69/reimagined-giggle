5188fc1e67159f23a21bbf506b6153c0e871367d
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:08:36 +0000

    connection right comeover trade light

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import tensorflow as tf
-print(tf.__version__)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
