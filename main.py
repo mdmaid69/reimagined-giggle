40e2a48cbd71e51413841ca38f1b17c76e03d598
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:57:33 +0000

    appeal minimum stage snow achieve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
