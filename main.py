a58272597b2843cf261efa6bd47f30c4a281063b
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:52:28 +0000

    every nose direct translate great

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
