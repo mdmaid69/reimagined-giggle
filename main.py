21a9eb74b4b0ce3bbd4cb79b46698d03274d2455
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:10:10 +0000

    disgusting majority accident command reduction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
