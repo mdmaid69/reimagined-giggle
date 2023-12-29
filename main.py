d89de9b3ffdc7b78b95da9c0eb866af90c3ee207
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:16:17 +0000

    bringup happen base recommend giveaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def get_array_item(array, i):
+        return array[i]
