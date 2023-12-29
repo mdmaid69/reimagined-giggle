c4932d566d4e2f274e7336bfa6f75e3813cc547c
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:56:29 +0000

    dare match safety problem fill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
