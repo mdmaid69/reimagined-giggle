24bbad3aa3e7969e017af607f02e449ce314f8e5
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:06:37 +0000

    Ww everyone laugh practise slightly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
