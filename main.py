e2188e9c42889c192ce839aeac83dfe6b4e251d1
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:04:47 +0000

    pollution check support square also

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-print(math.pi)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_area(radius):
+        return 3.14 * radius * radius
