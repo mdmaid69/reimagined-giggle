5162ff1ac690afeac93fff9be82b14087f798314
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:07:20 +0000

    someone hear strike mistake rely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
