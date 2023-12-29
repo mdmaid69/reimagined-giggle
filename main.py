8cba133b86d15130c46f5d6d4764cb2867dc2453
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:23:10 +0000

    house genuine exercise phone doctor

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def greet(name):
+        print(f"Hello, {name}!")
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
