4386ef475f34f5acc5572fc231ad282798abc9a8
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:50:41 +0000

    order whistle complaint demonstrate into

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
