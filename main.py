9276e4056132fd3409d08e7c59b963d81f388273
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:42:19 +0000

    glad passenger notice declare sometimes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import random
-print(random.randint(0, 100))
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
