d4aa01c8a5750ed9e3dd8de237a543d11cf03a43
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:17:29 +0000

    skirt Itself discovery attention top

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_as_int(array):
+        return int(array[0])
