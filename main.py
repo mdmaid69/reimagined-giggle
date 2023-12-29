3ee2cf4176e479f5fa2dd34143111b060d002ba0
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:51:35 +0000

    ability when variety expect however

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+print(math.pi)
