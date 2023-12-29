342161397184cfdc8bc2262a95f8e614fae027c3
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:22:31 +0000

    wealth part while average before

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
