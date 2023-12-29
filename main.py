81aa243afca2bd9579114be52510ad46109d75f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:38:28 +0000

    equal dance ring block constantly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import os
+print(os.getcwd())
