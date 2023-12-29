90abd901102744704a8d3a8e7cdbf3c83f7a8b12
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:23:33 +0000

    throw produce mail company contain

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def add_numbers(x, y):
-        return x + y
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
