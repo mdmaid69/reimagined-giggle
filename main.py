4e380627d72f015e046fd598c3ea25612f669fe0
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:51:43 +0000

    march telephone underneath nature matter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import array
+def get_array_as_set(array):
+        return set(array)
