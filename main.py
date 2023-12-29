4de0d78850edf6405b1ee920975d40a4909800a8
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:07:11 +0000

    buy thin nice day serve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
