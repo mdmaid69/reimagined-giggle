605fa9b405988dea013b8e3b9289992a317bdc3d
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:33:38 +0000

    Uu union positive fashion relax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
