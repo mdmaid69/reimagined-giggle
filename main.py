a23a42f98c3253bb141a0a515618c46b83f0b4ae
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:40:53 +0000

    tomorrow disturb definite character high

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_area_circle(r):
-        return 3.14 * r**2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
