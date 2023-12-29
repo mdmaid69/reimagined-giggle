93b7f03dbaa2eda213d207e1ca4332d05fc2d8cf
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:30:44 +0000

    otherwise suffer comefrom store stamp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
