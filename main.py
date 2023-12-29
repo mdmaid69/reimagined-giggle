552e95912fdb1c4c11573f635a81d508e8244925
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:09:38 +0000

    dead frame reserve make force

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_max(numbers):
-        return max(numbers)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
