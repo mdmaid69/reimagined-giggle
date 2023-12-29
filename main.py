0bee399e46defb7b6740314ad09d036bd4279cb2
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:01:42 +0000

    bet get hers cruelty compare

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_work(force, distance):
-        return force * distance
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
