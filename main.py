6b2d17ace951011de947426dbbcab673e29988d2
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:29:35 +0000

    contrast surface meet scale while

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
