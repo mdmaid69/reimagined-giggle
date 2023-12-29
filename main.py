a0ce725879db6f8703e434328a62a5b11976a291
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:36:24 +0000

    father inside add interesting sing

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import logging
-def log_message(message):
-        logging.info(message)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
