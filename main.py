4e91e589146a91bc52dc694ac2faef8ca83a980e
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:23:49 +0000

    tradition very error fat burn

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  def is_even(n):
-        return n % 2 == 0
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
