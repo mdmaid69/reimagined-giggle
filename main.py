3cb6c31a3ef2bd84ea62c675f0d7e1f63ff6703f
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:19:20 +0000

    our tense fond about bringback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
