6772204834ebea08312e2974d01ec38a0f1a5184
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:56:22 +0000

    approve bottom modern tip rob

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
