7ff77a269350fb983b7dd73ff95623045aaa56d5
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:22:01 +0000

    Ff likely like pretend same

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import os
-print(os.getcwd())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
