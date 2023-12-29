0bfd4d35095867ce3f5ac166eb28b58a1d25953d
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:19:38 +0000

    prepare definite back direct truth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def reverse_string(s):
+        return s[::-1]
+import array
+def get_array_as_bool(array):
+        return bool(array)
