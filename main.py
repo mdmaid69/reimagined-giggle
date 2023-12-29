6f06fb8c81e54d7e5197a95067c1fb7740889c04
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:12:59 +0000

    round permission there translate shut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_str(array):
+        return str(array)
+def find_max(lst):
+        return max(lst)
