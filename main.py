de3bde377e462aa6dcf37803ee41a65dfb263d12
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:34:40 +0000

    hurt manner indeed satisfactory criticize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-x = 10
-y = 20
-print("Sum:", x + y)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
