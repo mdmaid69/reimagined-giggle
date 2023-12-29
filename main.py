dfca27b27660abb919295376110f728fe3ca1f88
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:38:34 +0000

    sky narrow there size science

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
