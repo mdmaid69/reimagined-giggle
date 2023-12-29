95e10c6b4251e0d532e9a68d093d3ea6ae55c375
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:39:16 +0000

    used leg collect own figure

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def find_min(lst):
-        return min(lst)
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
