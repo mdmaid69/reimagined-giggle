fc5be6282296c9e076185453a70d83df6d5884c8
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:14:16 +0000

    apply run frightened spoil behind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
