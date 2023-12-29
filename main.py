312d44c43a32aebd6a3712c3b7f02980ffb07a33
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:48:03 +0000

    scrape 565end complain century level

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def clear_array(array):
+        array *= 0
