c3ad88dde4052494a0e75b3b08ffedcd998bff73
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:26:49 +0000

    solve tool trial vote nerve

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
