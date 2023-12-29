10df9b99508c668c59c234ec4960b8058c068e0b
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:17:24 +0000

    piece improve meet giveout nothing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def add_numbers(x, y):
-        return x + y
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
