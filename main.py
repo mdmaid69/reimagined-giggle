8708547d48a56918ddaa6c440448c790cf2205a7
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:27:05 +0000

    free tidy tomorrow have detail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+name = "Python"
+print("Hello,", name)
