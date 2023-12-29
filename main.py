e376656f3d71dc6f4e8854756a405b5d98f47989
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:16:14 +0000

    born what stop than doubt

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
