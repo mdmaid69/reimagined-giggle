b86943dda54e83b3a720a44adbc60a7ec86c4c6c
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:37:46 +0000

    rare recent runout scream else

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_speed(distance, time):
+        return distance / time
