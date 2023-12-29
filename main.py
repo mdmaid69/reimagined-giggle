db9edac38cc6c89e17680dd2e8bb2041ff480440
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:38:13 +0000

    thin defeat past last have

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-  import sys
-  def get_python_version():
-        return sys.version
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
