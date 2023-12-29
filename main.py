36b8a5aaf7865e5070da0724d06225fb279a78e4
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:02:51 +0000

    sudden male snow office disagree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
