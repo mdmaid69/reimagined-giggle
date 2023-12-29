04770e9211fdcbdc81452f17d9bf4f5f0a388cdb
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:23:26 +0000

    special imagination giveup 179bite accommodation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
