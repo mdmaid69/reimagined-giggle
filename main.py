daf59b97ef922c0d63fc4df195c626fc91d2c49c
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:40:58 +0000

    meanwhile middle cheap bend peaceful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
