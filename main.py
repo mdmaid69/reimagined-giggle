c2312ed1105d574ab357d20e9a88456c0bc73bd8
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:17:04 +0000

    move callback congratulate do and

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import sys
+print(sys.version)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
