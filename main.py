7ec085e329375c303341210ac14c94ac3742890b
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:49:19 +0000

    mean success search connection cost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_density(mass, volume):
+        return mass / volume
