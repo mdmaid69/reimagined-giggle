f210441d9b9a8a49a391ee73c518576e81ec04ab
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:24:47 +0000

    cut warning finish pass like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
