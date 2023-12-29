af31944e99376abaede00980f0a0939ef2cd065c
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:41:16 +0000

    ill report pocket transport fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_unique_words(sentence):
+        return set(sentence.split())
