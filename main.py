c74b351bca4af9a3005bd1a3a8599960c2e81063
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:25:05 +0000

    Kk tour control lesson run

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_work(force, distance):
+        return force * distance
