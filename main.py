2e85d0840a005a74d30409cc9ce637e4b7922407
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:31:07 +0000

    too basis statement fun but

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def count_words(sentence):
-        return len(sentence.split())
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
