64d6a319fbcb17bd91b4acadb54a88efa3f7ded5
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:10:44 +0000

    software closed tool turninto lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_pressure(force, area):
+        return force / area
