7cdfe069195d732bce5356aa7e4b83660fb9e2c6
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:25:10 +0000

    talkoutof stupid over hope band

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_set(array):
+        return set(array)
