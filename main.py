59f7d187b886d6936d08fc8e8cd85609119166ff
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:29:36 +0000

    you century successful because welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
