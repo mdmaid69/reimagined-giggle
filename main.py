44f8825e628c93f08f8d0b5c9881cf7376622c33
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:20:11 +0000

    comein 565end minimum concerning few

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
