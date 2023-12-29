3b61f09b170e6b0ff8f206a74b163b233ebe2913
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:26:18 +0000

    fold far cover muscle objection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
