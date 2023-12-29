2535d4c152cb90cb830e4cfb8117e17086b6f003
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:43:22 +0000

    factory voice somebody but far

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
