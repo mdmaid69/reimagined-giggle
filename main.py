3ab0d8dbc51f7c0b040d9a418b1f8e431f502a4d
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:18:49 +0000

    warn quit length estimate thing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
