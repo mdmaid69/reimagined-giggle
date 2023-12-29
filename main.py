eb8418e2dd23abde45dfb481efcd48823bae2639
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:20:58 +0000

    eager direction intend however square

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
