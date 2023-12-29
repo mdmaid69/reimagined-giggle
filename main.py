71c0eb24d919e4b6e245d44629131bbc857f0508
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:55:54 +0000

    account wire measure surprise properly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
