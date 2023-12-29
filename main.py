6bcec1f13eada65c13495e236fd89dde0a3b12ea
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:50:28 +0000

    experience time squeeze face prevent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
