fc7556f77c659e02ca762c8722b1f76786944991
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:42:21 +0000

    translate able except finish will

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-def add_numbers(a, b):
-        return a + b
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import logging
+def log_message(message):
+        logging.info(message)
