4e8acb6c2da1786cbc7eab100a09c1e6b9c85313
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:20:28 +0000

    change smell well black whistle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def convert_to_hex(n):
+        return hex(n)
