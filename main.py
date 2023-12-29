ce530d579463ba5bf6de0f328cf8c371c3885ff7
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:24:26 +0000

    hurt shadow decide bringdown leg

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def find_min(numbers):
+        return min(numbers)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
