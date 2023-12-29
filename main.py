7a818afd002453a31d9e7152cb984aecbf275f2d
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:47:21 +0000

    drop contrast reaction achieve care

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
