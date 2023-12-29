c3e96ba04831fe6341c189844cb59f4a6f96b65d
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:07:32 +0000

    thick Ii disturbing worth similar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
