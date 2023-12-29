3c382fbb3c5e20904fd8cb1ee29a47562b98e446
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:12:42 +0000

    instrument shop breathe chase shape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
