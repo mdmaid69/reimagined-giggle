ab2b85c15a62bfc8a49bb083ff8b93d7f2ecd292
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:55:38 +0000

    false honest might though clear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
