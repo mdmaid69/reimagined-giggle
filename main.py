ec7296bd17aaf4ce103e1dce1df5046873e2952a
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:49:39 +0000

    quickly take relative wave I

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import sys
+def exit_program():
+        sys.exit()
