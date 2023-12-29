33952b7601ba0616451a44a95d5e6b655cc2fe3a
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:52:02 +0000

    weekend use foreign simple plenty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
