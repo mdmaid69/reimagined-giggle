757da41b5df5c5c476871c05705772aa4299e0b5
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:30:54 +0000

    wave variety admit fun promise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
 import array
-def append_to_array(array, item):
-        array.append(item)
+def check_if_array_contains_item(array, item):
+        return item in array
