ceb9bc7b0ab0dbb6a37001f49c5f8240dcacb301
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:47:59 +0000

    benefit lonely practical over split

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
