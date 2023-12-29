509ebdaac273010bee9a7cbd39bc0991fff56c83
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:11:46 +0000

    walk fortunately after relative hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
