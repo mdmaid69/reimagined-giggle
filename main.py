09bed0b5686b12d8d33def031567f4e9bdd9653d
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:41:46 +0000

    cry power wave degree object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
