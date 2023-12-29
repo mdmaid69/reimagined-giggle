429248e6e2172842f0beba074662a1802f4369b8
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:43:11 +0000

    both admire persuade noone enthusiastic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
