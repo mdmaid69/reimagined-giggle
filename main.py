51a372c5f67f63eda54a0a10aec00b4deeb5e424
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:48:55 +0000

    young object cut criticize slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_exponential(x):
+        return math.exp(x)
