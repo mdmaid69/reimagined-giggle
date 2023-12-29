18a6e605bb12d4240520e1e9bf9bd1208b126d78
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:50:14 +0000

    target opposite ours for like

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
