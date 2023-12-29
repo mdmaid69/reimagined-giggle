9a18ce17ea3c285ad7628fdd70b3035e56b2b51d
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:55:02 +0000

    country satisfactory calloff Nn copy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_tangent(x):
+        return math.tan(x)
