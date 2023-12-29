541df730a3be36e9793aa38d0f6d182e907c4dc4
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:10:43 +0000

    exact fence march silent female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
