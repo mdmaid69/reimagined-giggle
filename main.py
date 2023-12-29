1bb06a8e3ed7413bd22af8e31622376e598f36e7
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:36:21 +0000

    perfect better whistle good carry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
