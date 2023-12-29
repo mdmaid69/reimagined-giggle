586139c6fe4f5ae54de783e975a43d19c748baf2
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:32:19 +0000

    impressed undo appeal unhappy my

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def cube_number(x):
-        return x**3
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
