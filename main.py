027c5e6f0ba7aa3f69e66a51c19b818280370174
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:25:31 +0000

    discovery song surface guess shadow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_pressure(force, area):
-        return force / area
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
