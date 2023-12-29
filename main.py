4f805f96427eeda5139ecfb6a0a8384f8ff1d743
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:06:02 +0000

    forget fly needle die everyone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
