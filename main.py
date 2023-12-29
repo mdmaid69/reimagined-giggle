3e441ba63db1239eca5fb6a9681f108195d11170
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:26:56 +0000

    Cc previous offend rank as

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_acceleration(speed, time):
+        return speed / time
