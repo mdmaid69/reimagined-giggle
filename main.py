078de1137b26179a806e86bc0e03017754df75bd
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:46:10 +0000

    record study through discovery direction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
