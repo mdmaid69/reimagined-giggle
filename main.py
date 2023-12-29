b07378c4d94ee50b20199fd39651938a85aad439
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:52:13 +0000

    study phone buy priority effect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def count_elements(lst):
+        return len(lst)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
