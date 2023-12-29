ae7835de7861cc1414af9c98f4deef27f9064218
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:16:53 +0000

    interruption own attempt card thankyou

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_current_working_directory():
+        return os.getcwd()
