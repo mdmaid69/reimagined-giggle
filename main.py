6a8a923c907c3c861a2ca5ad53e745708280fac6
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:50:14 +0000

    dance happy change cheap remain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
