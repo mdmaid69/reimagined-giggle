f33c0eb5b8f8fcfdbc75ed08d5314af562224ea7
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:10:37 +0000

    skirt education attempt suddenly excited

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_as_float(array):
+        return float(array[0])
