212f6979bd6b391837d4cacfac4e047f73b3946e
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:00:04 +0000

    abroad direct your base thanks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
