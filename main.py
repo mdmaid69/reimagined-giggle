c3e729512649d19e5a03d4b45b82141f65f4c734
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:54:16 +0000

    smile gate respect exactly eat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def get_array_as_list(array):
+        return list(array)
