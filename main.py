fc258e6c4f29ed6ee20e69564921aeb45642df87
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:39:22 +0000

    think possibility wait own this

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
