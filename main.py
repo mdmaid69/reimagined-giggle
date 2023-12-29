3d22bdd11848e1a980eeefd96a0be5a2e4076cd5
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:30:46 +0000

    concern boat board laugh bath

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def find_min(lst):
+        return min(lst)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
