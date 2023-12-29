e46fa0184c9d23962a0240c65653c6529a47de65
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:58:00 +0000

    black read opposite stretch church

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+for i in range(10): print(i)
+import collections
+def create_queue():
+        return collections.deque()
