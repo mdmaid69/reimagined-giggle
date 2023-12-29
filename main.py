eade67d7d26c7061b8a3ea51129b16056a6bd412
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:40:47 +0000

    square interfere character machine narrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-print(sys.version)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
