2b5da597a62eced2b5d0a1ddd0bbc8f60ae95806
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:44:47 +0000

    tidy introduce very impossible length

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
