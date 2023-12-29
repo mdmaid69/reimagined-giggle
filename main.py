c9fdc38d847368ba0da12909733a1c4aa7cd9fa0
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:47:57 +0000

    experience promise pretty side content

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
