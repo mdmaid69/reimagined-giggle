50a12ffd229d346ed04ee8fdbe764c255c9021f4
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:57:25 +0000

    unable straight post second stick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def convert_to_octal(n):
+        return oct(n)
