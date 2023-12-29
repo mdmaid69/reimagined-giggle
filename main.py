e50c04ad0195c7031866af6bcc24f11755d36a2b
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:05:34 +0000

    question crash such trade sense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
