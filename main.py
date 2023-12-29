9cd0830886f5af75153c7524e413f24624bc3c01
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:06:53 +0000

    block various Uu familiar unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
