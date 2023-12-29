2fb2304a44eb395b7f0d26b901c28c424fb3c1b9
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:35:28 +0000

    base central hour central contact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
