5c803d03a624ee9d7b4b9b70e104378867c75133
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:15:41 +0000

    wave pretty threaten design together

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
