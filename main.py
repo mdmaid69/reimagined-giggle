515324e993052f749f767149713283af58ad7c17
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:16:57 +0000

    second arrival nonsense urgent possession

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
