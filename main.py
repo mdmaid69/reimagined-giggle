5d022ddc6c94d0c42a9240012c4b2d40605d61fa
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:35:24 +0000

    sky embarrassing snow hour run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
