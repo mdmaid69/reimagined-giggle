2eee0cee9746c337fdb6628fd2002dd85935722e
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:21:06 +0000

    point respect myself stretch mine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import numpy as np
+print(np.array([1, 2, 3]))
+import time
+def get_current_time():
+        return time.ctime()
