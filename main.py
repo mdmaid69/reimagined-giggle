47a2385482e814d9f5e2a742cc0c7a042a6abbdb
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:10:34 +0000

    gentle visit tool sit emphasis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
