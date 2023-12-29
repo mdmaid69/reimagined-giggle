6b13cd3b8163641881d83347e927f0b7b475fe73
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:29:09 +0000

    land tradition sink imagine speak

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def divide_numbers(x, y):
-        return x / y
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
