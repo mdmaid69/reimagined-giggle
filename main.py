e213c04807de794f2aa1c645d2450d0c256c65a5
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:17:37 +0000

    short star afterwards back ask

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sys
+def print_python_version():
+        print(sys.version)
