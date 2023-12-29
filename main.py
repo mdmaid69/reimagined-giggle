a311d80d1ebf4c76c2f0146faee4f92ae8986812
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:37:41 +0000

    word develop turnup guess gooff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_as_float(array):
+        return float(array[0])
