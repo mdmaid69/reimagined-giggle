0e43cc0488326e29d548d96b1e08dcacfbd1ddc3
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:13:18 +0000

    afford effect work suppose wall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
