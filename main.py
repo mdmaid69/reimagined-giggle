b534a00e76f6fff72e9badb4a2e788728ed7c565
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:50:19 +0000

    mouth somewhere near consider fold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+x = 10
+y = 20
+print("Sum:", x + y)
