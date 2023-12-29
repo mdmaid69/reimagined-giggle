12ef7fd7eb6ea704f32e37604d443edbd8f15d31
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:55:55 +0000

    violent charge key poison safety

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
