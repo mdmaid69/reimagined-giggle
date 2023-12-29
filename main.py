02f5608b5ffa69338fd81d448ee9b4abd8e4b62f
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:53:42 +0000

    shoulder aim debt male hers

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
