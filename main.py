75ea5d93a3930304bd99544c9e76b6aa7ea8ebdc
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:18:22 +0000

    follow tap whenever scale silent

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def add_numbers(a, b):
-        return a + b
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
