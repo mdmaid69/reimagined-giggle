ada8c5706abe115f26c84ac784491984ca42726a
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:07:23 +0000

    reliable general must low decide

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import collections
-def create_user_string():
-        return collections.UserString()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
