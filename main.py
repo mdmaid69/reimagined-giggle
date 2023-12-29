7e0b1a0713d69ea7b9f2446ee2f1a5d809e524c3
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:50:24 +0000

    ear practice subject today adjective

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_combinations(n, k):
+        return math.comb(n, k)
