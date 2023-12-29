e221cb0a3216935860ec9714294a74437e9b533f
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:50:10 +0000

    chase plan fire platform affect

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
