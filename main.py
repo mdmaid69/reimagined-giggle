12b23a79a3bcb9c6c5a4f2967baa668aa9149887
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:50:36 +0000

    loss goodbye stand pencil mine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
