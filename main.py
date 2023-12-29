a03c7d4927281047edc6b1e8949d222bd1e1dd05
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:24:01 +0000

    leg dream goup chat sit

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
