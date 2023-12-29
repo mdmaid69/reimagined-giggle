4a07550f625815dd78fbc6f87c413a0147957b9c
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:34:16 +0000

    emotion giveup turnback soon friendly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
