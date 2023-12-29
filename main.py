17502b1695deb9cefcb1628a11228eea0e3d2b6e
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:21:29 +0000

    unfair under concern ready risk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
