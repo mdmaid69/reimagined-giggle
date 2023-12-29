bd9790289e8f5f57e1e6cc1eb25d85bb67faef82
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:45:32 +0000

    change separate comeround instruction care

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
