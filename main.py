1b11f52e14c7ddb014e1ee9abad51e8133540f35
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:08:09 +0000

    much glad normal laugh depend

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import os
-def get_current_working_directory():
-        return os.getcwd()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
