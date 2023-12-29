3eab2131883a378f9f22ba8ba1ad04864a4ff7fb
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:12:14 +0000

    custom plain lesson brain thumb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
