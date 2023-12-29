a85dcd8bb9ab251fe6529f850aa8ddb54167a786
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:06:33 +0000

    through Oo comeout intelligent rush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  def convert_to_hex(n):
+        return hex(n)
