6ead3b123fadce8375a81f9d5c98c947de7e7cb7
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:52:51 +0000

    couple blame brief safe total

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
