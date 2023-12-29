82733c30ad6210954f3a32b27e1f79f21d560fbf
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:39:38 +0000

    someone pinch angry positive die

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
