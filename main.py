b7e16fa18cb736f0f89b6d7e5b8be132bc2b216c
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:00:48 +0000

    clear thick black police real

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
