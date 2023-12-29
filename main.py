626bfe67c13765f8564a5f2e9e4668c183bbcae2
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:25:36 +0000

    type software someone sight sit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def reverse_string(s):
+        return s[::-1]
+import datetime
+def get_today_date():
+        return datetime.date.today()
