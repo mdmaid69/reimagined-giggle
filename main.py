5c52ba80d3feff11ec42a43129619d95f3dbbdf6
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:05:21 +0000

    upstairs wing reject unhappy everything

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
