78f30d80d486157958772a0fb452e8c588fc7983
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:26:48 +0000

    about night scene report depend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def square_number(x):
+        return x**2
