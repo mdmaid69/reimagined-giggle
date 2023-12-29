de3453f654ed2b4d39556ab030c05b4499f72713
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:15:38 +0000

    ear apply blow make near

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
