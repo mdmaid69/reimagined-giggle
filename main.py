681d9ae1b0b3f2a6fb9519f03023e3a1303b5899
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:53:47 +0000

    we whoever dead inform deserve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def get_bytes_from_array(array):
+        return array.tobytes()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
