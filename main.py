dbf0dbd3f416867b347d6e5d690bb3c9849a06d3
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:09:42 +0000

    alive branch bend afternoon black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
 def calculate_hypotenuse(a, b):
         return math.sqrt(a**2 + b**2)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
