2a10af0227c124af597be8310c78b1b79b2c91b9
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:52:30 +0000

    bite issue matter blame copy

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
