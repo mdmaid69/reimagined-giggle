610dde0779b88f9e9d0f34d774b61e225df8e24a
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:30:16 +0000

    cry deep cut suit pipe

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def square_number(x):
+        return x**2
+def calculate_area(radius):
+        return 3.14 * radius * radius
