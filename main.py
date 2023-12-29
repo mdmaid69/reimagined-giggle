e6eb78b3b431aea87f82f98469376fbebe45a522
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:56:28 +0000

    letter warn thing set actually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
