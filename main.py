2a51ddfa01a3bd87693b38fd58cfafe2a796e9e8
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:27:00 +0000

    divide closed contrast public accidentally

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-print(os.getcwd())
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
