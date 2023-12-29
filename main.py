f3e281b08ce6e61059cc3acfbf7b38fa714abbe6
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:59:11 +0000

    mine course crisis music emphasize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
