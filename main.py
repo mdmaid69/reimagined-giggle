cd11df53e4830d7fbfcc856f089b1ef62a32ea80
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:35:33 +0000

    more immediately however near track

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def count_elements(lst):
+        return len(lst)
 import array
-def convert_list_to_array(list, typecode):
+def get_array_from_list(list, typecode):
         return array.array(typecode, list)
