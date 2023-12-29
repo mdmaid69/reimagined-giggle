54bd910dde4a3cb1d30511d7749217b23c8b4ac7
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:10:51 +0000

    modern talk twist centimetre object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
