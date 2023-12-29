bbc98f9a32fb4da7aa70a76385c547c5dd259788
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:14:24 +0000

    mail place dick value formal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
