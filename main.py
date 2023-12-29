8b89c7b1148449820998adbb5f61b409cd0eadf0
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:52:26 +0000

    gate else sometimes door gradually

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
