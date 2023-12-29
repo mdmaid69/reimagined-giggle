78249a578a92958d7298d6bbd4316d1e67a1116d
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:23:21 +0000

    Ff some stay cool rain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+print([x**2 for x in range(10)])
