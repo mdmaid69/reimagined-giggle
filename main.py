7bb9bd1384fc7ab5785f88cd9ab8d1877c024dbc
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:55:15 +0000

    ride bottom spoil rubbish excellent

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import os
-print(os.getcwd())
-print("Hello, world!")
+def change_working_directory(path):
+        os.chdir(path)
