1b13eb3fcab7bbb4e4d95253532c5919ff6e60d0
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:31:15 +0000

    thumb boring strict trust reasonable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
