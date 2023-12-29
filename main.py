65de44c8f852023b977c73bcd9df65214521eb06
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:51:48 +0000

    likely luck rent help emphasis

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-text = "Hello, world!"
-print("Characters:", len(text))
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
