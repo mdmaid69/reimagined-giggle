07675066e44870d26ab53b7f0c9a0d94ebcf2cc7
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:19:01 +0000

    comeback walk throughout explain getback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
