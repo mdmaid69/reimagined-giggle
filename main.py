43b46b83b4833321eaccc7a8f2e2a7812ef4718d
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:14:55 +0000

    late work smile occasional pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
