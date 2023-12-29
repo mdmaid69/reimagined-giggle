8e7f36c9b7768f5d94e945733504b3384ca7212c
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:40:21 +0000

    they pin club sign rush

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import time
+print(time.time())
+  def count_elements(lst):
+        return len(lst)
