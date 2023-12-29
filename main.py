f70ed7c0e251de01e92c18e9a60f3abc75637c12
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:35:11 +0000

    permission hand judge minimum sign

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
