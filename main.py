7df986c88c9b0707ba1915404f68503340c3fc2a
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:10:04 +0000

    market Nn small float thanks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import sys
-def exit_program():
-        sys.exit()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
