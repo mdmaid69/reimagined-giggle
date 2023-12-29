ff1a6af74854c0b299dd05ca795f8a334483fd4a
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:54:26 +0000

    visit system door eat load

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
