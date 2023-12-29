e67889e7384abd513641e41346e99f32787097b5
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:26:18 +0000

    speed 571enough scrape unfortunate complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
