2b48ad664631a336690714cdb75e36fb312416ae
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:22:05 +0000

    downstairs close glad shape try

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
