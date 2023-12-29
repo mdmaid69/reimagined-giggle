1f5273eb8583ae52cc5d8092ccd808ca0b168cfa
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:10:19 +0000

    would bush peaceful callat method

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
