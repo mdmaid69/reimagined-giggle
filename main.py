bafb1c0c536d7c4b01fc988e206c9b5b7af427e7
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:59:02 +0000

    loan note woman field might

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import datetime
+print(datetime.datetime.now())
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
