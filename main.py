cebdd2a5f4f4a5254a5af1ebd165cade563536e9
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:22:36 +0000

    hit lose excuse power fortunately

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def count_elements(lst):
+        return len(lst)
+def calculate_density(mass, volume):
+        return mass / volume
