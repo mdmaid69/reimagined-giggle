dd4ec9d52c602922e4aa80c74d0a70037de55430
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:41:55 +0000

    model maximum comment wait view

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
