a9f0265a3bdb8aa2d14fc34240b4b943795120d0
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:23:41 +0000

    Pp train lock must quality

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import sys
-def print_python_version():
-        return sys.version
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def reverse_array(array):
+        array.reverse()
