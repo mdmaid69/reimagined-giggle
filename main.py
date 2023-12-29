9c257316165d78d15bf315124f66f28ef2b651a2
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:22:45 +0000

    cross hurry pride culture space

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
