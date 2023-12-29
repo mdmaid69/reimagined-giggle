5d00345b56b51d779f1ce62ad48fb9aedfdae866
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:18:36 +0000

    Zz step turn ourselves balance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def get_array_typecode(array):
+        return array.typecode
