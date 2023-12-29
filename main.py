31ad0e7ccec80e6e7d44fcb7bc79945dd1d9effe
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:56:43 +0000

    in lock please exaggerate keepaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
