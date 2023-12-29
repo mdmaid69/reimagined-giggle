58a608552f76fab85c906c51eac48b119a1773ba
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:56:43 +0000

    past solve have comeback know

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
