a5d870b66b68a0de7a72a89ac2b642ad412a9f9f
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:30:49 +0000

    radio subject picture calmly round

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
 import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+print(os.getcwd())
