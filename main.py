43b10b6de39ddabc74e189c856e7e497ef8f1a6e
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:01:43 +0000

    polite party suit listenfor use

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def add_numbers(a, b):
+        return a + b
