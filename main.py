9184bc4e8311a93bea94e06efe0de6fc8e48e370
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:01:55 +0000

    language amount embarrassed war language

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import sys
+print(sys.version)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
