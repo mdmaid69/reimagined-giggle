4be296d777f7a6484cc0ed9b68739525b7272b24
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:22:00 +0000

    measure her shop food sale

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def get_array_length(array):
+        return len(array)
