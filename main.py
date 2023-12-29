ab1821f5e38b6469d3dd83cbbb580169eea3b347
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:29:48 +0000

    everybody compare environment improvement happy

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
