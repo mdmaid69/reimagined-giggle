0f8aaad62eb46ce2595f8363d2f0932640404304
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:48:46 +0000

    tap sharp art shout subject

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def count_elements(lst):
+        return len(lst)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
