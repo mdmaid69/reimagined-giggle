4350f9bd103cfdedec59fe892d36bb02e65b5715
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:12:25 +0000

    tour bringup need direct escape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
