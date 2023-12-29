2b9164e37a0b4d5fa08d25520b300373c10f1304
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:51:02 +0000

    draw Rr fast confidence future

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
 import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
