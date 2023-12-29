45c30698ba60a4341685fe58eddef38aebdc2b5b
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:47:09 +0000

    Qq style might correction receive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
