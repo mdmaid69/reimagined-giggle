4c8fab1af91cf7012c57cb1ea563809f8b6b79fd
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:33:16 +0000

    blind scared legal judge benefit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
