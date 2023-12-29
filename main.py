bb04dc39654186549153fce6a3ace0a3961a6e4d
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:35:55 +0000

    face run beyond Mm up

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import time
-def get_current_time():
-        return time.ctime()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
