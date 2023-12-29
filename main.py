26e57b96dc46c61e9057c4a210a23f927a215cee
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:43:05 +0000

    bread final soak reduction delight

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def reverse_list(lst):
-        return lst[::-1]
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
