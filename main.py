30dfac9cedbfc0de9d9db874a73943da7a155a6f
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:57:40 +0000

    allow cool forget admire urgent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
