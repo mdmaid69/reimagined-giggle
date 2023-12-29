b05e164de28efec4ce19010f0cb648b5e6ab7e97
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:55:05 +0000

    clue target software safety legal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
