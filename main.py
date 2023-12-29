7a1fa5aaed93afc1d35e13469128e1cd53984838
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:07:36 +0000

    callup eager neat string entry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
