07cb8787cd54e855c5965238400b632a0a0eccb7
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:50:24 +0000

    distance recent minimum leg willing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
