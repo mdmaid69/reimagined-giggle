d8fb36bd9f09046f1f77e458339fec13cb8ea19b
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:54:22 +0000

    dick gather hot lean fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
