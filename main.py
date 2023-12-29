008232b597fb4b65aa50590b803a31d0ea4bf784
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:45:00 +0000

    onto before move rain report

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def square_number(x):
+        return x**2
