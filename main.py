c6b79c688eed8d3bb3d7fea09ea82498732a104f
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:41:55 +0000

    their shine warn between run

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import sys
+def exit_program():
+        sys.exit()
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
