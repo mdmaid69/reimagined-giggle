628c7108bd6e3b842f402c3d0ca3a25301f8a94c
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:33:00 +0000

    bound lucky band giveout cover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
