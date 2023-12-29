d9f39a380e0f211f45e5c7888f8d461c7907ce36
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:49:00 +0000

    letter any cause apply runaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def convert_to_binary(n):
+        return bin(n)
