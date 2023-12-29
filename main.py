ea9e6e0de8233cfd45811d718d16b7235daa2622
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:28:02 +0000

    move shut thought feature squeeze

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
