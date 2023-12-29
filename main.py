71c24c3c7698fefdd3177c11913aa13b62b4d1a5
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:11:25 +0000

    test ride hurt suck quit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 def calculate_future_value(principal, rate, time):
         return principal * (1 + rate)**time
-def is_palindrome(s):
-        return s == s[::-1]
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
