0bcb33f1970542fe25c8117aa3101b50d667cb45
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:59:25 +0000

    crowded affect eat shout shut

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-def count_elements(lst):
-        return len(lst)
+import collections
+def create_stack():
+        return collections.deque()
+def sort_list(lst):
+        return sorted(lst)
