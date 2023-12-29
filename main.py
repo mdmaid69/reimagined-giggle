2d245544f1fb9296fb6700f0fd25f957692dfccb
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:40:48 +0000

    comeround until contact bus convinced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
