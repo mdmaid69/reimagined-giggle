b290de3562850bf528c021e1bbc421217446cfe0
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:00:31 +0000

    under draw necessary none relationship

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def reverse_array(array):
+        array.reverse()
