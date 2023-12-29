0bc61563071989f43181c2d0f0090cda10fe8b49
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:29:14 +0000

    attractive battery nose second future

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_min(lst):
-        return min(lst)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
