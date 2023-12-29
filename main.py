374c8dcc432a3519c11a2ef1aab516dd1f6c929d
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:40:26 +0000

    band smell Rr advantage goout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
