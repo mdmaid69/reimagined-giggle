a4897b631390bcba6156a31c2a24a0f64578acb4
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:08:35 +0000

    claim trip fetch breakfast maintain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def convert_to_octal(n):
+        return oct(n)
