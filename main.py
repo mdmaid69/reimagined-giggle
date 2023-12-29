57399587ed18482007e93a328d6607e93a1a968b
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:06:50 +0000

    entrance through shadow impossible apparently

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_power(work, time):
+        return work / time
