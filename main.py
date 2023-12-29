addbc5aa18983481d873684d9b57e3e621c4a6fd
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:53:33 +0000

    private comein in Oo attitude

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def delete_file(file_name):
+        os.remove(file_name)
