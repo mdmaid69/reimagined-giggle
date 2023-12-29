7e9353f7d41be328c8db11051b0faee7362f0fc5
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:46:10 +0000

    Ww correct whatever shout telephone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
