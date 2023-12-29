ebfb9d972285e2792eebaa8c170f94a2ddae9dae
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:58:23 +0000

    reasonable this brush me holdup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
