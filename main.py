368d41f5d4e320bf0e8658acad78a1fb0a04d950
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:56:42 +0000

    impressive behaviour journey stand near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
