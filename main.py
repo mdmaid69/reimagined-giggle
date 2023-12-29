f26050b4ffb57d15602bbd1b09f7ea383e426f14
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:53:42 +0000

    weigh afraid blind circumstance upstairs

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
