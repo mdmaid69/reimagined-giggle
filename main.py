beffd25978cd3f330622c3c5337f11de8436fe2c
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:28:05 +0000

    free union keepout respect them

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(numbers):
-        return max(numbers)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
