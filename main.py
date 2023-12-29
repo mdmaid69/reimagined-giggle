22db008b877f3f3d7245b9266ac4d36ac59a1690
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:01:23 +0000

    criticize respect bread intend towards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
