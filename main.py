2db72999d315df5bbbf3088c4945c044d24700c8
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:52:25 +0000

    upstairs respect blow Yy frighten

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
