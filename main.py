a1e66f0cb7abfcdab02a5ade9947bd60e7e539ff
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:51:12 +0000

    mysterious not expect fair no

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_queue():
+        return collections.deque()
