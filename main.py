41b1cf6c78d370c344a242d8d8554c64546652b8
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:11:38 +0000

    taste ours complete traffic cruelty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
