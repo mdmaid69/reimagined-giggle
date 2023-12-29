9b90633d95d79e2b4d109acf4dada8027474aef2
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:11:20 +0000

    accommodation burst unpleasant closed heat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
