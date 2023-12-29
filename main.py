9b066ae5f206bc826e217f728c6b77dcc08c846d
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:50:05 +0000

    sky clothes hear yourself delight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
