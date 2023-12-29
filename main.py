5f53171347dada703b080ccce3326f2ec6bb1a09
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:43:03 +0000

    turndown branch beside someone mine

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
 import array
-def get_array_item(array, i):
-        return array[i]
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
