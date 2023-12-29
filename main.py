a199cb646c5361a85b5d51ac358129a206003c5b
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:18:28 +0000

    sharp spare Bb search thinkthrough

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_power(work, time):
+        return work / time
