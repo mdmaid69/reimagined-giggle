c7e2f382d6ed0824c02f809324d60229ca48bd69
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:18:24 +0000

    always search set stir display

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
