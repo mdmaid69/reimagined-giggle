ab32f02a0700aac883c7f9251bf4173c9bedf9be
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:08:09 +0000

    frighten considering image comedown welcome

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_max(lst):
+        return max(lst)
