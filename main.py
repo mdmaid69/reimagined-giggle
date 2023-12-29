998e6cda37a7a091e8231d909a147ea4bcf65c2c
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:16:10 +0000

    flood control entry suckme sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_current_working_directory():
+        return os.getcwd()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
