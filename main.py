b44a1033b3e99df4c53d0a76eb7781b772bef890
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:57:56 +0000

    sure notice increase declare help

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
