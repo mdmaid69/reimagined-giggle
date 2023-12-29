27e49fb86c35f79e81c6fc67fb935f306562f9b0
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:57:47 +0000

    protection satisfied confused describe mainly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def clear_array(array):
+        array *= 0
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
