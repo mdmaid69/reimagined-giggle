e7f0e08de1575aff6e0d3b88ce3d9faa652c283b
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:10:39 +0000

    big idea tip save cruel

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
