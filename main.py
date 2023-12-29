37c89477e77a4fe5b9f5512673ed3ebfaf12465b
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:22:38 +0000

    taste very force act order

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
