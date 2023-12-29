2e0fa63913a4ab8b934ebad672959f161fb70c48
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:03:30 +0000

    566end rest best characteristic on

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
