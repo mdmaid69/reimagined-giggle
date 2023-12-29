a741dde57857459181294b08e59209b3e16f873d
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:28:58 +0000

    father ring keepoff pocket getoff

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
