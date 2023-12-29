abe7c0c7a1b8ad658fd61ecdf72a278f4d2e51da
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:33:39 +0000

    task block complaint few rough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
