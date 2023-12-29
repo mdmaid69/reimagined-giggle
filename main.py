c39fb4c6901f3c878ea4b1a28bc95398886d4589
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:24:47 +0000

    fire forward theirs 572enter sudden

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_current_directory():
+        return os.getcwd()
