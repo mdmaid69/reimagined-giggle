0af9a5419a898b3d807329c659e592843946fdba
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:40:36 +0000

    food 568energy wire interruption lose

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
