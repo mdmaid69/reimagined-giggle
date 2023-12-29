0255e4fc9314c0ffcf3c98c390e02ce5cd22c15a
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:39:48 +0000

    past 567enemy quarter attach standard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
