fbb4c3f84f4f28e7a065add32bcff750fb373fea
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:05:01 +0000

    region related family evening choice

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
