4938b9ec9a5f1755c4ad8f69b2c8ac5b717e1665
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:48:22 +0000

    payment production send borrow already

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
