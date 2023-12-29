22faaea759e03eb2b71e05d2c0eaac91de58b7ce
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:08:20 +0000

    or hand tidy respect town

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
