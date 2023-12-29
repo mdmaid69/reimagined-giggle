4d0ee0b5716fb15e0d7ae643cee24b1a97e2c486
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:58:00 +0000

    exciting hold age before getback

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
