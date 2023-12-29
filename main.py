5cf8e2f01e924886c43cc5ec2befd76521dca816
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:32:00 +0000

    send nearly shock responsibility birthday

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
