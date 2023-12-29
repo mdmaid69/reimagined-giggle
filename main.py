fb4f64350e18010c9a25d9d2b16cb21a059eecf7
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:57:48 +0000

    deep means concerned regard keepaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
