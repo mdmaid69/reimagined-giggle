b2385863ec0c6a3c05bc845fe6e28103e5243177
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:55:25 +0000

    perfect stiff organization side block

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_area_circle(r):
+        return 3.14 * r**2
