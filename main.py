a8c5d9a367ae5887abf435087cec3c6637d1b2b8
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:44:42 +0000

    himself from embarrassed exploration impressive

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
