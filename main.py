1546f039568fc69cfbf33ac68ef9b0200f2a4bf1
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:38:45 +0000

    pupil answer react failure movement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
