96fd42405de4b7212b743055a9c4b905124604c1
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:50:26 +0000

    silent wrap cause system tie

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
