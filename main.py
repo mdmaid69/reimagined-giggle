b147f0e76ccb811ab7b23af145ae3b31d1a7e564
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:31:17 +0000

    object praise please advantage rate

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_work(force, distance):
+        return force * distance
+import numpy as np
+print(np.array([1, 2, 3]))
