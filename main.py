f9f381b653beec3c997cf03cd099af9d6ce7cc68
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:51:13 +0000

    pollution imagination cook curious autumn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+x = 10
+y = 20
+print("Sum:", x + y)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
