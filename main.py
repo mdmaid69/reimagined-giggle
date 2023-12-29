88e4a4effc37a03aab981ea46deb44aebad797a4
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:50:49 +0000

    long rely message born couple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import platform
+def get_os_info():
+        return platform.uname()
