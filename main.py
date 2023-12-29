11a1ba561def79d04f598b6ec0c0462559c21b95
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:46:57 +0000

    walk storm sister typical switch

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
