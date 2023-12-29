98521e4fc04813bd89eda548dcc569e56636c4af
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:08:48 +0000

    rain distant this lean measure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_typecode(array):
+        return array.typecode
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
