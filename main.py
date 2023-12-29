c170e02846d2798087f143092fd8b25e4ed7d351
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:31:06 +0000

    left rule run appeal value

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
