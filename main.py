55e1500ee99c32f9564e6c8f486050b7587e4112
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:20:35 +0000

    any no form middle rub

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_density(mass, volume):
+        return mass / volume
