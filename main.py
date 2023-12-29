d0c73ce3bee1c2078cefa494373704fd8634e2f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:01:32 +0000

    discuss tape rough discover fairly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
