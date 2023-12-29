a0e59ec8e65cfc011d3bdae36bb6093656e2f0eb
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:28:30 +0000

    attach bringback about tap realize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
