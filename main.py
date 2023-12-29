9bc544f6970eb5032f3a7b7e4906ac25dec2c7c7
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:56:07 +0000

    pressure answer match decide move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import tensorflow as tf
-print(tf.__version__)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
