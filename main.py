f938139104ccccbd617b57fc75e29e1d4a837351
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:51:27 +0000

    minimum sea lie attention hungry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
