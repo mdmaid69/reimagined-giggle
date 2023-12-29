42ec16ebe92b87fead24e92edb8ee7ca00c7a8e6
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:31:56 +0000

    shout successful fold order steer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
