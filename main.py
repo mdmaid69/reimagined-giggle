d051aef1aa885d1a75a3815d31e53f126ad1bae8
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:10:04 +0000

    key form away cut sensitive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
