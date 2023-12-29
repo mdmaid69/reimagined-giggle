a51bf0cb48c73433489d42039bc449bf22d0ded5
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:06:28 +0000

    round woman only greeting change

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
