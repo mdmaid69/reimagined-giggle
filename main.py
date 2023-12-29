28088a0a420d728637e93a301452a42a8ebdc544
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:48:56 +0000

    callout peaceful delight nowhere as

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-print(sys.version)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_volume(length, width, height):
+        return length * width * height
