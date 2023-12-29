1499954728b53a3ba60d8af366758aacfd46c918
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:05:27 +0000

    empty though control fork my

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
