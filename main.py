5580511c518f377bfc93efe3d23b18d66577d40f
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:44:03 +0000

    base joke cause conversation hold

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def clear_array(array):
-        array *= 0
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
