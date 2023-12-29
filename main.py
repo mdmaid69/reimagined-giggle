3e78ba361045df04c2d74c2a281a12dfe25c69ff
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:39:27 +0000

    offend whom night display hurry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import collections
+def create_stack():
+        return collections.deque()
