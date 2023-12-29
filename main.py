06cf41cb41134fa16a18d40126b101438dbacd11
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:37:10 +0000

    here Uu something along close

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
