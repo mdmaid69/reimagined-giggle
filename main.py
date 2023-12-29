96f6997e10d1a804c3bad4e56561915d125850eb
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:29:06 +0000

    knock emergency past breath all

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def remove_duplicates(lst):
+        return list(set(lst))
