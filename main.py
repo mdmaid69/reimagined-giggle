c7e9628a541500454cb05df0f06b04ebb312da42
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:47:27 +0000

    pour music shrink light Qq

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
