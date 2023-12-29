0fa8eaa71daa8c07f4d083656ab7f51f732edafa
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:10:49 +0000

    opposite typical much thumb goon

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
