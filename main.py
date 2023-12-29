74eeec61ff8e89451e2754a5d4c02f1469548103
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:22:46 +0000

    conduct with disturbing considering callback

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
