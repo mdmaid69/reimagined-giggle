94e9841c5fb3a5d1cc71d13696284585e2418ecc
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:38:31 +0000

    behind shine price rest major

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
