b8dd2898a8bd1036e3882ea2e4755da2f87479d8
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:17:33 +0000

    polite pay search against whisper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+print(math.pi)
