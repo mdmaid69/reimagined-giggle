800340ce657eadaad749b7760e505bd4281a62eb
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:37:25 +0000

    month brush transport open police

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
