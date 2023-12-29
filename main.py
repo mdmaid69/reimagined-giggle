edd577e01a7fbde907c6ba685e8c62146cc9cf67
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:07:10 +0000

    experience like party point rely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_density(mass, volume):
+        return mass / volume
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
