92ff63f0aa615429d6c564e8777defe981d686f0
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:56:04 +0000

    bar production mention sale road

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
