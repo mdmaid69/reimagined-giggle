1a1194315c31b65b1aaeb6dac604f9dee10153b0
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:34:18 +0000

    sink wander involve size phone

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import datetime
-print(datetime.datetime.now())
-import datetime
-print(datetime.datetime.now())
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
