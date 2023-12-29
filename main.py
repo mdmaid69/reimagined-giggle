d56633f06d46274228e48ba1a57f4bae8658e7a4
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:23:50 +0000

    lead drive terrible wrap talkinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
