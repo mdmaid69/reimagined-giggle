bf1cf397b89723fb69c828cf8d55dd76f24121c3
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:48:52 +0000

    door terrible telephone information deep

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
