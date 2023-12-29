6dd3b17d70232c07c6bfbfbd40a04717c7eb60dc
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:37:20 +0000

    straight backwards appeal weigh brave

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-print("Hello, world!")
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
