a8b8742996afe3a5c866a1b88f8286367733fc3f
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:48:36 +0000

    summer base defeat could provide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def count_characters(sentence):
-        return len(sentence)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
