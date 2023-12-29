eeb9519fd9f58c872944df8e3c79362e3a5418b2
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:48:30 +0000

    real deliberately method strength touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-def get_time_since_epoch():
-        return time.time()
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def get_array_typecode(array):
+        return array.typecode
