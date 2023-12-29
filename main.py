f3c34285c51246b001edbd2b2c71f2aa6612f6fd
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:16:34 +0000

    herself Kk detail herself 570enjoyable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
