5fdc09e96f3ca198186fc9ab8ebb73836c42c80e
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:47:57 +0000

    label permission important event hair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
