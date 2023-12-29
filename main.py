785f2c249003fa17691b8d5d83ecf29fc400bc4b
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:58:17 +0000

    generally use then such turnaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def get_array_as_int(array):
+        return int(array[0])
+import time
+def get_current_time():
+        return time.time()
