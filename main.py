217dad229064718e0ff7355f044527c83f61cab2
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:59:21 +0000

    suffer way eat spite such

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def convert_to_binary(n):
+        return bin(n)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
