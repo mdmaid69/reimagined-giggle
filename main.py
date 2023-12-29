be7b6d1b3f1d13990c2344bef3f8daf9e1626fcf
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:02:39 +0000

    direct closely pay persuade drive

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
