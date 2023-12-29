47d63e0d133faf8f44392e7623130f0e00a234ab
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:54:11 +0000

    protest future unite keepaway tear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
