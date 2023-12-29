adc440e9bbd6d7cdea4856286e255d149021c974
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:23:59 +0000

    switch destroy tour university open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
+  def get_file_permissions(file_name):
         return os.stat(file_name).st_mode
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
