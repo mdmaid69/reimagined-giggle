bdcd4159517acd51bdfc25ce315463e9c94dc9bf
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:48:18 +0000

    wise tap shoot angry say

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
