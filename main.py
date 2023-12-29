9d5231949f1f68d25400d6f3ba6968acaacd2f35
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:51:13 +0000

    operation fill top callup pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def calculate_area_rectangle(l, w):
+        return l * w
