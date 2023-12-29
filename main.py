970504ecd3995d95495490d482c99e1ac9ab7fd5
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:47:07 +0000

    sign curious event sick lead

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def greet(name):
+        print(f"Hello, {name}!")
+def convert_to_hex(n):
+        return hex(n)
