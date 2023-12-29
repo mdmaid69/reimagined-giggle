cda4cc86b3aad3d1a8fd9f43ca7563eea63edaef
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:38:48 +0000

    along turnup careless 566end increase

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  def is_odd(n):
-        return n % 2 != 0
+import collections
+def create_user_dict():
+        return collections.UserDict()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
