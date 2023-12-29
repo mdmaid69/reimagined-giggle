03ea76e9072f776d23471a3563332976157e39ae
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:49:24 +0000

    that during behind interview about

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-def is_odd(n):
-        return n % 2 != 0
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
