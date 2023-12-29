e790b849fc89e58ed81563ec1a207965df0a5b24
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:00:15 +0000

    upstairs date none path film

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
