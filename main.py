267c4e38d0de62fd6060342c85f74b05282f6d9c
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:43:01 +0000

    comfortable for tap splash punishment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+print(sys.version)
