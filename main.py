a36e311ed8b439ee4d3d093f165fd5f03c6004f9
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:06:22 +0000

    please our tie 573entertainment doubt

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
