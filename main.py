a3bc367c76b6b3bfd413fe95dc13eb03d455183b
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:23:12 +0000

    sign project violence over close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-x = 10
-y = 20
-print("Sum:", x + y)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
