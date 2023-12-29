30f192b81dd2e81dafea34e479efeaecdf24f32e
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:48:21 +0000

    send lucky difficult loss answer

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
