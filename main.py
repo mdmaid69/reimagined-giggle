2c9c90708faddbdea156075227816a2c7a1be8bd
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:15:34 +0000

    than with hope smell picture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
