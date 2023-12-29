0127ad7386a63906c7251728eabec22faa9cc610
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:28:29 +0000

    callup judge count taste exactly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
