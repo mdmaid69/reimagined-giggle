13eaeb649f6e35c60c9708d21cda7968d0393e43
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:46:42 +0000

    organization stage innocent fetch brief

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import array
+def get_array_typecode(array):
+        return array.typecode
