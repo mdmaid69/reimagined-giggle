c44e20eba560d4c2440950fbd1ad7845b2375146
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:39:22 +0000

    hot left watch typical seriously

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
