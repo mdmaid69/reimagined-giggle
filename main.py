8f63b0a4d9feec057e2833847029836fb1c3f24c
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:30:47 +0000

    actual name factory country myself

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
 import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
