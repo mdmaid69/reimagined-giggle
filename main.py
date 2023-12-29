4c0e71f6f4ceb6db930469f30bc65494f0bfeb74
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:54:51 +0000

    tone letter none cook regional

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
