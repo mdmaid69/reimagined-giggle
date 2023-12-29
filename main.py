3539888752663944f1939602f2457c03c71e2301
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:45:14 +0000

    prove region within post comedown

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
