b0609f4f9575f5c86825cb94ea9abaf5d79e4d61
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:13:04 +0000

    wonder shame Nn about complicated

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
