f658332af210dd25484ea15a9f942f1983811b95
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:25:47 +0000

    drop precise crowded view talk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def reverse_list(lst):
-        return lst[::-1]
+import collections
+def create_counter():
+        return collections.Counter()
+def add_numbers(a, b):
+        return a + b
