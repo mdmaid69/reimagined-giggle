4afbc168059f86229996f8fe6a6d4eb099fdd9f8
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:57:01 +0000

    cruel flat pot stare keepup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-  def convert_to_octal(n):
-        return oct(n)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
