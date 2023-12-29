20a00cbb8cde6c54529542d86767b6736ba47ed6
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:51:27 +0000

    play recently book cool that

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
