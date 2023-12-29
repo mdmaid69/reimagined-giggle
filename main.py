9ac31c43f9e421355c06ae16d7357f3dbf129464
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:11:17 +0000

    high quickly normal later injury

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+i = 0
+while i < 5:
+        print(i)
+        i += 1
