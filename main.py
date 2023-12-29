32a0ad67bbbb5051ebce3db6f524c2a0eb1252a9
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:26:37 +0000

    push bringforward convinced doubt experience

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_work(force, distance):
-        return force * distance
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
