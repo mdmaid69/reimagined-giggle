a2687ad1ca2dc2950460f3817219bce3d0d8bc8c
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:48:19 +0000

    boil the fold past pair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
