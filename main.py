f8df76b3be8a726ae3e6e4b8f1cf726cdb0bf376
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:13:32 +0000

    upwards normal discover conduct slowly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import array
-def get_array_as_set(array):
-        return set(array)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
