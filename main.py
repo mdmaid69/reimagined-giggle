36caa3ccf95538f069f8220d4d3b8a54c3f5cea4
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:47:30 +0000

    attention proof all anxious quality

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
