f2d4dc9c5cdd75749fec5cb7b0aa30a3cbfe869a
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:18:38 +0000

    argument much occasion system climb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
