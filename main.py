516ed0dc6cdb60a7cb7afade1f4589006c32c71b
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:31:34 +0000

    skill moon attractive name shout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
