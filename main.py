12ecac47a85d42efe93a8514d761a570d7fa0cad
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:20:33 +0000

    tip rate cost time breakin

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+print("Hello, world!")
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
