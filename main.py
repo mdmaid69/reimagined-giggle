3cac5448fc8e21d48f4f4da7f8cfb523138980ac
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:14:59 +0000

    big sweet freeze permission break

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def sort_list(lst):
+        return sorted(lst)
