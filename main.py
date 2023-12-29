f98153696237edb5347701d45426d81c2320f47c
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:23:07 +0000

    squeeze company weekend only invite

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
