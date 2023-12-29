93610d69df161ef163972d82a316f4e02ab5977d
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:33:32 +0000

    expression shade situation peaceful dull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
