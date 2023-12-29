96c5e79594534abd00994f930c00eb818988aad8
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:29:04 +0000

    content grow beneath innocent knowledge

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_current_time():
-        return time.ctime()
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
