36fc377a880a878932d78fb9d448d03ade547cc4
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:30:50 +0000

    intelligent worry stick 569enjoy bringforward

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_list_from_array(array):
+        return array.tolist()
