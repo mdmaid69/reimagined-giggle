18e911ee77abe7ac467dda6f0d35127a528ef7c3
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:11:47 +0000

    guide downstairs address firm square

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
