3bd552089ab819dc854c08cd4b1aed21d44a67ae
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:41:26 +0000

    payment lonely sticky will finish

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import os
+def get_current_working_directory():
+        return os.getcwd()
