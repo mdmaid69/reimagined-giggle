2b71904fc8f7b6b8568a95f2559e0b1de9687fc1
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:01:07 +0000

    breakinto equipment law own typical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
