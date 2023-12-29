866d13b33c01b1422bef59e05b94732d25ea8a5b
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:08:54 +0000

    project steady fast determined wall

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_as_bool(array):
+        return bool(array)
