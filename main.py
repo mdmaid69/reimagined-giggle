1209b2fce500b12ad6669880a16ee5dfa16d90bd
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:06:39 +0000

    fork gift tonight meal evening

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
