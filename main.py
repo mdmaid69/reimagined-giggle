a8e075a2f747d61351b0b7629fe8154388f6e2df
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:45:23 +0000

    force flood programme male measure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
