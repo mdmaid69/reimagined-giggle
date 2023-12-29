3e697351f34debf76a10c3710d4daf07b4577217
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:37:41 +0000

    sense well hurry back hour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
