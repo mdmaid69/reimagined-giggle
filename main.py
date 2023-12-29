f9d4ed893c4f4ecafca6709a1f7a6dfdbc82df8a
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:20:32 +0000

    die comfort common reply fear

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
