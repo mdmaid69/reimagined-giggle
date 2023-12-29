ffbea39e09c54c0ccb1bf9ee9bfcc086399abd78
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:03:07 +0000

    calloff dark possibility your breakin

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def get_array_as_str(array):
+        return str(array)
