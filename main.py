b511c5a7331b7109f95fb35078e23e4f8c281b78
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:05:26 +0000

    wire bath priority film throughout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def is_palindrome(s):
-        return s == s[::-1]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
