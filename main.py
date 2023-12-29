cfb9c9363aabad80b66b98cdd7d01b4ccf67012e
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:08:54 +0000

    control dream free content start

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
