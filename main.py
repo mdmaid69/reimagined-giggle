c6b67fa81ca6be52827290fadd1cfa8704b100a2
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:35:15 +0000

    for laugh office easy accept

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
