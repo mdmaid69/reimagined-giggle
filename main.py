79a8e6173aeee2cda5fc574536fdb16046e13102
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:18:17 +0000

    possible shape squeeze carry boring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
