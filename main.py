2663bff5912a613b57c6e96a71e95b015b0c47d3
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:26:46 +0000

    still pretty consider allow before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
