d421533797c5d2790014c3cda4ae79338e482f94
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:32:03 +0000

    car warn power hard strike

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
