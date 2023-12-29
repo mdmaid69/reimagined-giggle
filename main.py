a6be16f7a1bb8cf43aca43fe61edbd3f75e12477
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:54:05 +0000

    fine heavy closed could Qq

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
