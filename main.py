e5439d6cd81e9c5cbdc24bc1dc31f6696bc46586
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:54:05 +0000

    deep patient list deserve welcome

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def is_odd(n):
+        return n % 2 != 0
