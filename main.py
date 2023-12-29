25c208500746090bceb2f1936a79da6f67530320
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:29:27 +0000

    land used explanation emphasize correct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+print([x**2 for x in range(10)])
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
