b725fa4f03880bf56b79d50729284b2ad923fc4b
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:42:34 +0000

    right poison bringback juice common

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
