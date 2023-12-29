bb056bdcf53ae40ee83be1084830812c1723d7f8
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:33:32 +0000

    into past family achievement protection

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ def save_csv(data, filename):
         with open(filename, "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerows(data)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
