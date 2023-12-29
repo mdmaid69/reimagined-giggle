bc08af95c984c2def2e489d1142ba9b817d84e9e
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:22:28 +0000

    test between frighten below legal

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
