1ae9e861e0d54627509a6c06d5f1830557a1195b
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:29:17 +0000

    nigga come able charge send

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_power(work, time):
-        return work / time
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def convert_to_octal(n):
+        return oct(n)
