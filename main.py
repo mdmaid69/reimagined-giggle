ed832ba778655776e3061fb55569c0070be7ddc1
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:18:15 +0000

    design figure news may improvement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def find_min(numbers):
+        return min(numbers)
