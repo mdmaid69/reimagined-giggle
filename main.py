f5afb7e13a3376136bf70aaf720e1080b15909c8
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:19:22 +0000

    wonder fit coffee rain business

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_nlink(file_name):
+  def get_file_number_of_links(file_name):
         return os.stat(file_name).st_nlink
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
