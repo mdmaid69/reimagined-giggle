eda3ba9784aa88803ffb1e534bc236dce7f72456
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:09:56 +0000

    somewhere whether sight even busy

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def is_odd(n):
-        return n % 2 != 0
-def multiply_numbers(x, y):
-        return x * y
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
