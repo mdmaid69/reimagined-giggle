93044b379ef18ccdf4638cc914ba5762b10a6d7f
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:23:56 +0000

    attempt finish lead show please

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def get_array_as_bool(array):
+        return bool(array)
