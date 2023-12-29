0be23bac5903a53a62de761a266ac108f77ba5db
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:13:36 +0000

    copy motion early hurry last

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
