c953761a36eefefad4d343f3e3e4690c4c303002
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:12:23 +0000

    Cc pen early illness greeting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
