ca796a6a44ef1ef5c1f249b0a9db8eb58be9a793
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:02:12 +0000

    march shadow distant sing 564encourage

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
