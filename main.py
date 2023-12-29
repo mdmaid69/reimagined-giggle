b8777395bc42f26d5956c8104b3121bef8219d0a
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:40:35 +0000

    concerned breakin comeback normal accident

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def reverse_array(array):
+        array.reverse()
