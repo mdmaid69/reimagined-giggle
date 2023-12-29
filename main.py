8dd2a4ee94440d65d9693961e276abdbc2214827
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:30:32 +0000

    clean anxious on century service

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def convert_array_to_bytes(array):
+        return array.tobytes()
