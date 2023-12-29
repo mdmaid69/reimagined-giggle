e3cc74bc96f233803d5a73c650c14a1e2cb34eb0
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:23:18 +0000

    long you fix official dig

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  def add_numbers(x, y):
-        return x + y
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
