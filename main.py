301dbd3d8d2bfa3a2bbf9d04d65978bf694ee242
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:26:49 +0000

    reaction complicated turndown interruption first

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import time
+def get_time_since_epoch():
+        return time.time()
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
