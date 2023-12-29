8f8f5b1c56e24b54379552183cafa2da8764676e
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:17:58 +0000

    believe manner culture relationship university

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
