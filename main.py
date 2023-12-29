b4e61279533ed65720eca189757685da75a245bc
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:20:53 +0000

    guard lucky over crowd production

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
