d6327a8235c05f4399ba3473e55f2ac673390691
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:48:07 +0000

    thanks turnup slice snow mad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
