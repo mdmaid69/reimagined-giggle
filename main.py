a2d6cab1c72c79b39b81b4f1ef6b0c1cd558c1a5
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:59:17 +0000

    city confused centimetre least release

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
