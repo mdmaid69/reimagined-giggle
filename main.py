2bc6f23ac0b2d5261dbe536f7b02ccdff4460883
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:53:17 +0000

    sight agree count angry day

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
