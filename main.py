1bffa1ce10f11dc028b3ffd1e44e1c28f834286d
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:01:59 +0000

    consist declaration vanish scheme opposite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
