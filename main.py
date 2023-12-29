65bcaf553abff54cf1066b689197ca436aefa505
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:29:10 +0000

    comeback stage care show perhaps

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
