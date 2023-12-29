81d58180ea4efa3d7cdd6292f7d96487bf535fd6
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:38:16 +0000

    personal autumn argue lend cheap

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
