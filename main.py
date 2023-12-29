7901ed8abb0ec5ca57afa1a0fb7d2e810e3de4d6
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:48:41 +0000

    may tooth foreign storm myself

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
