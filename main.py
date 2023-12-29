7f294c6482f0948c194a9fc0d270560493d8d724
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:34:25 +0000

    forget call country run needle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
   def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import sys
-  def get_python_version():
-        return sys.version
+        return os.stat(file_name).st_size
