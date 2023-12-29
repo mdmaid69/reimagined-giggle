8aed69da03ef0e467db86a2f93c92286bdce43fc
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:52:27 +0000

    this mystery remark bet year

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
