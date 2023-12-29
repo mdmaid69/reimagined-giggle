4041acaa936bf0e847b29fa9720161e489f1e626
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:57:28 +0000

    minor comment mean embarrassing comeoff

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
