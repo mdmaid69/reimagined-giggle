77a6163a1fd66e54939b497c6a20eb325d897f48
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:09:05 +0000

    cheap walk treat rob swimming

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
