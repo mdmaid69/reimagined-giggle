4e0cd3866ea9937d68495711de8ee506f9f972e8
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:46:55 +0000

    change purpose sure stupid nerve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  def square_number(x):
+        return x**2
