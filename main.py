fa7ee91a558d36aed10451023e7bc3cc847baf0d
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:20:58 +0000

    sink appeal certain soak proof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
