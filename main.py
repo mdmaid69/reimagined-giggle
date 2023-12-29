60202afa3925ca018233d92a7586d3be537de2c7
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:23:38 +0000

    season bin about upset walk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
