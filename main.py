c9e66c412b190706b344f21a31990cdcddde3289
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:02:18 +0000

    lookback(on) practical determined crowd that

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
