650e920da0a795d86a0c57411695e8b7c084a5f7
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:52:22 +0000

    bite runover sense over danger

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+print("Hello, world!")
