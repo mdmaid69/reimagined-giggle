d3693ee53a820a81d870826132f2bf67f25e66de
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:34:18 +0000

    mixture indeed clean bet bar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
