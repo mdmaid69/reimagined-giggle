5c3874d78a8cf11082d89e2ac17dab35eb6d099f
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:27:05 +0000

    to order below tomorrow left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
