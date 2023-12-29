8a6e54dd4fb3c6abe639437d6655936acbff97a4
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:27:59 +0000

    wild neither die refuse neither

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
