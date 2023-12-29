d322719e59ea0b333ce7d70a7c1b6987170041a3
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:31:45 +0000

    rubbish lean move coat back

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def is_odd(n):
+        return n % 2 != 0
