1c3406d1238dc17ab0b3e1b57f82b7732abf260f
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:24:18 +0000

    interview well giveback use gentle

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_density(mass, volume):
+        return mass / volume
+import collections
+def create_user_list():
+        return collections.UserList()
