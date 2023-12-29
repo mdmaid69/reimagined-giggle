a5910a23536937eb5d3a67b70189b55bbe3f6bc4
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:11:42 +0000

    defeat bottom black start 566end

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
+import os
+print(os.getcwd())
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def split_path(path):
+        return os.path.split(path)
