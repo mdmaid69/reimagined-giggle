1a6d567ae3d6ec6b602300698ce58be9658f6e1c
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:57:44 +0000

    cheque part cover frame goback(to)

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-print([x**2 for x in range(10)])
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
