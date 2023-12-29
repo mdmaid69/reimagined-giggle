7a206688d2cc99a9593e4a5463b6a391b131cf40
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:48:35 +0000

    by deserve must appear form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
