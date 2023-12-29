e78f54ccb5c856c9039c6e30f210b53cb316563f
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:23:19 +0000

    broad form urgent lock awake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
