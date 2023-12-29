b444f235cbf292d52dd1ab6932db9304b8943235
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:31:27 +0000

    number left group coat tap

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
