bc74f463cd17ff93cda7666b3afb2dd7b04ab40f
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:41:00 +0000

    suck guilty ignore clearly city

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
