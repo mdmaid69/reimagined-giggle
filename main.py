b7d57c995051a3c5bcbbbe6b186f6c202ae0dc33
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:30:38 +0000

    convenient compete meet record hurry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
