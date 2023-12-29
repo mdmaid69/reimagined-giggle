2e73462577607af2417bb5834b9ba7b0fc81a46d
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:10:53 +0000

    geton measure wind authority wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
