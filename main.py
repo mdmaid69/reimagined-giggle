50dcfc3fa5b1e3525a114a03bd5f0c3ef6f2d3a5
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:14:05 +0000

    accordingto way sting search deliberate

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+def calculate_exponential(x):
+        return math.exp(x)
