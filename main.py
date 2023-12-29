d4d746b2ee43b7d83be79f08453ce1526e2f2336
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:37:31 +0000

    date fold responsible 569enjoy catch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
