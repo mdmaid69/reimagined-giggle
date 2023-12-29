f649b05769989129dac2f9292ad96523d8134d7a
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:53:37 +0000

    giveaway experience division while nowhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
