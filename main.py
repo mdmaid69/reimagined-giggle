8b7879f6734ce05286c97aa233840ad443655261
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:29:48 +0000

    few pencil breakinto priority increase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
