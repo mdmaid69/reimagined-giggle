2bfa1b506f12b17941c0682652663f5423a9ee3d
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:56:18 +0000

    relative neat proof negative giveaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
