4666042d7429f7f4f96fc8a679cc74f8486c78ec
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:03:20 +0000

    satisfied unfair kind wipe wash

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-print(time.time())
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def split_path(path):
+        return os.path.split(path)
