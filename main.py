4633f0b47862f62e0f1d87ba3319341094d8e425
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:50:43 +0000

    there pot eye exactly wild

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
