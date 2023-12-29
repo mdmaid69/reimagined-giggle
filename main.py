b92b5beb402bdd6919ae7e0cd22997c7f412f311
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:48:19 +0000

    work ground offer her whole

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def split_path(path):
+        return os.path.split(path)
