5af58416011a1b2d8504830efca73959781c7f42
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:43:59 +0000

    lesson mystery talk improvement trip

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
