f93ff3528f9f6894148652f4731f66b983da367c
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:07:40 +0000

    natural it estimate group recent

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def reverse_list(lst):
-        return lst[::-1]
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
