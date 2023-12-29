26c0044f6a9aa0beddb844fd56bcf494b56fb626
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:05:30 +0000

    interesting confidence home impressed attempt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
