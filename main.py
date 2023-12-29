9accd9fdc3092e3cda1924868e9bb936f56cbc71
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:41:36 +0000

    fast exchange move steady beneath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
