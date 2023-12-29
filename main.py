cedfed1f54de0c3c8447ce255e5884199a4f4312
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:50:50 +0000

    plan congratulate work where brother

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
-  def is_even(n):
-        return n % 2 == 0
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
