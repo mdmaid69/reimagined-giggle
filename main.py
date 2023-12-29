230f4e28e963aaa57d25561aa73f9473b7c9e5a6
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:43:30 +0000

    home imagine thorough accordingto correct

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
 import threading
 def create_thread(target):
         thread = threading.Thread(target=target)
         thread.start()
         return thread
+import array
+def get_array_as_set(array):
+        return set(array)
