3c7c8a769ff3760cb51c386ec63f057c46f635b0
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:27:28 +0000

    cool damage cost otherwise schedule

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
