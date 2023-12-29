a67a532a50c96000d9ae074ec98774ad64f8485c
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:24:21 +0000

    scare trust blind front swim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
