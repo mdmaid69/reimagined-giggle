c3591935fffb0481f5f2699f49973b29e357e425
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:51:50 +0000

    control worth fasten top turn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
