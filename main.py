85567226565d141839c4a72faace3c97b228b19d
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:22:58 +0000

    steady press once bin method

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def is_even(n):
+        return n % 2 == 0
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
