cca536043a9271a5686384b23c466b28403c022e
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:29:50 +0000

    frame content port picture art

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def add_numbers(x, y):
-        return x + y
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
