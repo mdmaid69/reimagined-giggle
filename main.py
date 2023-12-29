a438ded2b8f121fafdd0415c520c8b10de8ae455
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:41:00 +0000

    matter stamp put fun occasional

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import collections
+def create_queue():
+        return collections.deque()
