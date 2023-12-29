ca9cd157a18f68496ab8295183415532dbab202d
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:08:45 +0000

    side border hole combination actual

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
