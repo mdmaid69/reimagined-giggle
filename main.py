9b1060d4dc0405222b573d488a62d5d57f7b6ab3
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:12:46 +0000

    temperature like trip this 179bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
