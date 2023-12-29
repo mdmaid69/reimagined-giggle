b9c41f7f180baa3f880c71d4e185b9d3930b8ff6
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:01:57 +0000

    ours satisfactory doubt hurry rain

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
