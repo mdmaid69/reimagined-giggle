c68a429c8541ec2ebbe2fe6ab290271bbec4c9ba
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:41:45 +0000

    sharp solve club mention opposite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def append_to_array(array, item):
+        array.append(item)
