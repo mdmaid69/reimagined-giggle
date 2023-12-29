dae108540803a814feea4863ee53ad092c3ab934
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:35:54 +0000

    release wave confusing level pick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
