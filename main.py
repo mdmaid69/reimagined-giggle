270237c7968bebe133a7eccb91b395aa5f9ec448
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:32:35 +0000

    lose use allow realize plastic

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import array
+def get_array_itemsize(array):
+        return array.itemsize
