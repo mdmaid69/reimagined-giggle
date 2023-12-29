3f3c76207a163715c53ba2ab78d879eeb3685faa
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:50:31 +0000

    dangerous survive defend power fix

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
