d519f5ae4ef476065aa628fb51b56ede6924f07b
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:39:00 +0000

    great getoff nigga stamp ashamed

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+text = "Hello, world!"
+print("Words:", len(text.split()))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
