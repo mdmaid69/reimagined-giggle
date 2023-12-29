374ecdb39843b2d9268da15e24d61861f57e6c4a
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:29:03 +0000

    leg forget bush confident another

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def sort_list(lst):
+        return sorted(lst)
