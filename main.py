2a32b435afba7ef2444661ed59799340e7d37ba5
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:23:12 +0000

    deep information cheat book raw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
