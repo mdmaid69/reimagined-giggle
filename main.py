bfc4c5285a0f2ff5db3d13f13c8726c6962fb3af
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:22:32 +0000

    team pretend emphasis punish repair

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def find_max(lst):
-        return max(lst)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
