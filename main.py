dba628e2a9ba7f33721d854002adaff380822601
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:11:08 +0000

    proper other wet consist emergency

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
