a0faf01df5465632b9df95325ad5d509ac2af9f6
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:55:06 +0000

    that uponon nervous goback(to) throw

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
