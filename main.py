ca7858f926884e84a199163632f28532d72cd362
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:26:58 +0000

    bang special keepoff stamp chance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+print(os.getcwd())
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
