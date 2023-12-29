992bbe520b4d1b11fbdaf4243f275c2b643dd8c9
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:11:53 +0000

    on welcome body black explain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
