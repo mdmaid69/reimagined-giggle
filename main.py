871515a1a011e25884165bb58847b0baca42ce86
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:22:10 +0000

    content female reason bus relief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
