d5f087db2f3e708b382ad702830d00d21abec205
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:09:55 +0000

    value arrive worry change fashion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
