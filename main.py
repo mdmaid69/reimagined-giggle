13c46aed0ce74796e3f63f57f6f5130f9f4e30c6
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:51:27 +0000

    environment 571enough position different famous

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_typecode(array):
+        return array.typecode
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
