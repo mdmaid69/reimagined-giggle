87e1b7a35b42ac0c21e4444f121ac9d7b5935f15
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:28:20 +0000

    only interest degree hide carry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
