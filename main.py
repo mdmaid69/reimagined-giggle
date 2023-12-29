ae56d7efe320efe7494629bbd0aca041732f7af5
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:50:55 +0000

    factory conversation either warning leg

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
