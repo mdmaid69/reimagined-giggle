06ea95720e7a51ec9154bf32885a839d0a1f7caa
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:23:20 +0000

    speech noise ball standard criticize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
