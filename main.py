91c04c7ad9123e6deaca2d5c7bbd31e4433d86c9
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:04:37 +0000

    Uu dear just body cheat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_factorial(n):
+        return math.factorial(n)
