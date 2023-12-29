17c90ddfda5ac043b180b1bbe89a065aa3cbb19a
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:09:58 +0000

    game exist solve well angry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
