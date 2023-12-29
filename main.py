791c79647f35ed4eccfe093b636a377465b92d0e
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:44:59 +0000

    necessary die surprised judge rid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
