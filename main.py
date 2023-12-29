5cd98caf614fb8d9b1dea4fb6017580ba72d5124
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:01:16 +0000

    share care score shadow 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
