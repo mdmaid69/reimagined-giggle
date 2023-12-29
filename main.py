0ce5b30a34c47fecc58c9561866f2e7c39bb0583
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:33:01 +0000

    calloff like connection could sting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
