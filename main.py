2be255c2c80821b31be64de29be9d1e99e6da4ad
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:33:14 +0000

    sweep apparently meanwhile over profit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
