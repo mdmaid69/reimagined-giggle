35da1c13849dba88005574f0826835784269cba9
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:19:22 +0000

    high best course concentrate immediate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
