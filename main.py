153046854f456b4076cbe5e7f83717e91fe46e4c
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:20:09 +0000

    typical himself store deaf there

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
