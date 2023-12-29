b875cf7b7d5c0667e5284a13fcbf40bc497eb3df
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:43:05 +0000

    flood male smart shock join

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
