aaac3472df6e23a0c5938d9fe87101d40161555a
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:47:09 +0000

    pause heavy save busy ignore

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
