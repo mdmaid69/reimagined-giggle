dcdf70aa848a3d58d393210266e69e462c9b64aa
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:39:27 +0000

    spill perform 564encourage space circle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
