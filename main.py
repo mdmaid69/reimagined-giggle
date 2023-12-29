ce15e789ab8d5d6ca256c8b33aa81175c086a1ed
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:51:41 +0000

    cope raw task much recovery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
