b6a5a93af45b507abf71cfdcfdcf6a38601b5e4a
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:07:49 +0000

    male live expect check birthday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def reverse_list(lst):
+        return lst[::-1]
