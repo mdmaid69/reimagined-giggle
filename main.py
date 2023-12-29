081a82dafbd54f2e6b93f11d4478ecc0d5c2d707
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:28:52 +0000

    insist man takeout recently 570enjoyable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
