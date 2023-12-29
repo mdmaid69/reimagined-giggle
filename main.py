a67ab740eadbc5e48658d7f38d903ad37ae225b4
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:26:16 +0000

    steer middle destroy yours whisper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
