e156dfe2276788f4fefe776e64d97313e7a7d348
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:40:49 +0000

    Kk usual comment swim convincing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def get_array_typecode(array):
+        return array.typecode
