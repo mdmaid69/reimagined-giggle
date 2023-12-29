72c8b2ce5f883aa0f62251da2ecca17c98cd0e14
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:52:30 +0000

    wash enthusiastic radio rob always

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
