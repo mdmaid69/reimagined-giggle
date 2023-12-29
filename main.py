822ad9e040238d52f203ec4cc9a83abd20685775
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:04:51 +0000

    realize between follow block danger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
