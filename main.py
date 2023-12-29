6be0784ad0a85d0c71eb0936c9acf13e7e6fef81
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:15:08 +0000

    personal depend back return lot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def convert_to_binary(n):
+        return bin(n)
+import sys
+def exit_program():
+        sys.exit()
