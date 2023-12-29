844eceba52da8900cd36dfa56890e37f1dc92c05
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:01:34 +0000

    reserve see roof sort water

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_str(array):
+        return str(array)
