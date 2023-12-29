11f524c554df910e21ee4afd3658d5d8c8194075
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:17:08 +0000

    man breakinto lost plate busy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
