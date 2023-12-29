eca1f553e6e7d31b5801601019a298792b0c7f41
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:32:53 +0000

    country myself tip right flood

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_speed(distance, time):
+        return distance / time
