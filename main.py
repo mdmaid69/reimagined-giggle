204480ca3d05aff33813006dcd37b62672914bf9
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:02:39 +0000

    body criticize moment kind cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
