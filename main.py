4f43cf9d42d08f9ea19ad6a81bf2fc6389a1e81a
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:47:00 +0000

    callout squash second fire otherwise

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def convert_to_hex(n):
+        return hex(n)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
