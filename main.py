3ae6a542be36f91f57483dec36d29002c16834e9
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:08:44 +0000

    bottom warning trust swallow regard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
