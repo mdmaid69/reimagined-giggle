0ca3b25dcd3a3f2d6afbf1a5439e7addfd81e933
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:17:48 +0000

    measure cause beside interest characteristic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-def add_numbers(x, y):
-        return x + y
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
