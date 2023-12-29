602621f5523619b443849d9ab50c01bf6406aedc
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:18:43 +0000

    defeat rather sight constantly suffer

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-name = "Python"
-print("Hello,", name)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_density(mass, volume):
+        return mass / volume
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
