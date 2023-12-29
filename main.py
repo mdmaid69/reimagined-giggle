c346df7c93694633bc7aaced1737d6209fcfcd8a
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:23:42 +0000

    human pure few succeed once

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def sort_numbers(numbers):
+        return sorted(numbers)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
