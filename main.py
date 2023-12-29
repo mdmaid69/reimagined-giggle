459dfbeceed2fe90979c4527033fa532128a92c4
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:11:36 +0000

    us for thought no game

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  def multiply_numbers(x, y):
+        return x * y
