8c15ad12d0fe4486ddaae41d985c636607f813b0
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:15:39 +0000

    nothing rough print murder lift

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
