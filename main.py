0286498a736851520de6d6048490b972c1be17bb
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:19:44 +0000

    advanced emergency recognize city else

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
