8156aeb03e3c12ed7eeadfd37e9052721f774ec3
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:07:04 +0000

    convince fire everybody anxious instant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import array
-def get_array_length(array):
-        return len(array)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
