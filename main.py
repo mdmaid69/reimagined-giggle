adc717c292481404f91f962d770ed16dc58c12f8
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:49:27 +0000

    noise line professional however all

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def subtract_numbers(x, y):
-        return x - y
+def greet(name):
+        print(f"Hello, {name}!")
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
