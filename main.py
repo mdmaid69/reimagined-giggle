e8b0c19454ee743458344d0ee2b5f44ae4d4c73b
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:24:37 +0000

    shop couple burn hide star

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
