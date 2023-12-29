cc0ab69904249d459e0888ea4f2c161cbbb6d7ea
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:49:27 +0000

    and temperature return intend style

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import getpass
+def get_username():
+        return getpass.getuser()
