bbc7c93a9df9497ef9889268e1a59f8a9ebe30d0
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:19:27 +0000

    basic sort from wind personal

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
