43fe0f379ed7ad2adf28179f1ccaad5e343ce22a
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:12:57 +0000

    total influence work juice bang

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def divide_numbers(x, y):
+        return x / y
+import sys
+def exit_program():
+        sys.exit()
