b575c66480f4716ce3be60fdb0ecad54162c5921
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:04:58 +0000

    blind judge noisy object thing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
