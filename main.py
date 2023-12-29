8afab4e695d6aa0dfe6fe3426f7665595db27595
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:24:01 +0000

    nervous live direct load at

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_error_function(x):
+        return math.erf(x)
