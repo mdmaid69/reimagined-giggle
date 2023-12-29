0b56c57f14214712f5ed916cc9275a435a0ffa48
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:54:47 +0000

    skirt noone workupto open guide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
