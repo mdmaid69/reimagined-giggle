08cbfa4149a5f578618990ca2f116ed3afee5b50
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:56:30 +0000

    Oo speech theirs wise edge

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
