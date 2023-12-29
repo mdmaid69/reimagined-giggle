34ee91e06ff9078bf34617399811f0848e9aa919
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:14:41 +0000

    bad effect take succeed recommend

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_pressure(force, area):
-        return force / area
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
