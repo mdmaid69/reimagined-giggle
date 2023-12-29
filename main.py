360c359afe48728a30412cbd5ef8c87f7ce1a852
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:37:02 +0000

    pin when strict bringup somewhere

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
