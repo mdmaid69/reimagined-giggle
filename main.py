ddc028db023ff11434246088a8e2aecf450e95a7
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:35:38 +0000

    visit project relation pipe trick

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+for i in range(5):
+        print(i)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
