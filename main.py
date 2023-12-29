1f7ac0cf09c82af070f333c7383d9515f4fd66d4
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:04:25 +0000

    shock organization certainly variety warning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
