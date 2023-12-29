aafcc28964731ad6c5842ef4894af0cb3d99b9e2
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:01:04 +0000

    workupto no worth break next

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import platform
+def get_os_info():
+        return platform.uname()
+  def reverse_list(lst):
+        return lst[::-1]
