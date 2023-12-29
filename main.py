2bb566f5674f59506ac8654e2d241b8bf60b7e18
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:13:48 +0000

    embarrassing reduce failure find hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import collections
-def create_counter():
-        return collections.Counter()
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
