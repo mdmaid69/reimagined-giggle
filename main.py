71b92baadf4b7b88c958b0a2da2988eb1ea58a96
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:26:14 +0000

    class friend there usual straight

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
 import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def get_current_time():
+        return time.time()
