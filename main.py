8da4f08a7db2f7ef66756c0ecebf5d3ff2757d53
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:38:09 +0000

    bend plastic valuable shoot rubbish

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_number_of_links(file_name):
+  def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-import array
-def get_array_as_set(array):
-        return set(array)
