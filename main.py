f05d334e24759646794fa9e6b35e062a40e359cb
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:21:48 +0000

    play lot false workon reliable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
