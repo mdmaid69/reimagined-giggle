07736833edea2063dbc38b6918b122536fb4c162
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:30:15 +0000

    operation spot limit minute arrangement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
