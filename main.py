b0f673d726781f043eab1e236e502de175e86980
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:23:27 +0000

    value process instead computer stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
 import datetime
 def get_current_date():
         return datetime.date.today()
