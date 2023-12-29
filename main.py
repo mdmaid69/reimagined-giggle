2505997a967dea4056e54da240b57d6ef63f807e
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:42:07 +0000

    degree friend call either age

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def count_words(sentence):
-        return len(sentence.split())
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
