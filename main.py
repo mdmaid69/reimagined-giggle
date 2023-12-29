6332116befcf412c373eaec8e4c7972bd3768a70
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:43:18 +0000

    mouth spin inform sleep game

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_density(mass, volume):
+        return mass / volume
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
