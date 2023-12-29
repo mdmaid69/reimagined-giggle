ff5f8d47d181768e5da969a2031d61d3f4bbb45e
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:18:19 +0000

    part recommend contrast mention project

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
