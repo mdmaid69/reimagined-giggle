6ac09cbc7b27ed36c7ee197c0c5038068515ab47
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:18:12 +0000

    cause supply rate practise excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
