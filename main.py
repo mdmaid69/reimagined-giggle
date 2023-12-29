6438037e9dbc1523abf0669c093ba673acdb139d
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:54:24 +0000

    apparently party will different that

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  def sort_list(lst):
+        return sorted(lst)
