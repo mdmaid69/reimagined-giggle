3147662d1c58e2c1bd84ac0234d00a40088f1020
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:01:28 +0000

    change shoulder again cut path

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
 import collections
-def create_user_dict():
-        return collections.UserDict()
+def create_counter():
+        return collections.Counter()
