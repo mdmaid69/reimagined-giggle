f3a399200b391a566a37a7a828fd9db2b0c678b2
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:39:19 +0000

    close ball swallow pile main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def reverse_list(lst):
+        return lst[::-1]
+import collections
+def create_stack():
+        return collections.deque()
