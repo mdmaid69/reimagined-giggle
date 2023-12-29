5c8b9e78741a74b9147fe18ec0f4351685fc54e9
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:26:50 +0000

    charge shoot us off until

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
