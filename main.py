8215e76b4615dcf7d330528460584643e7656f10
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:57:20 +0000

    alone switch world pressure line

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
