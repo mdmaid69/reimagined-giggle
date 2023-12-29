b82350fbda0da814353d14f685b1e935b8f37e8a
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:14:45 +0000

    movement however properly pure rid

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def reverse_list(lst):
+        return lst[::-1]
