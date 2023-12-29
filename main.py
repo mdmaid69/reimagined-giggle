7ebf6d8e527576186439ffb0f387e5303b1bc88b
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:20:24 +0000

    petrol reduction present whether concentrate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_max(lst):
-        return max(lst)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
