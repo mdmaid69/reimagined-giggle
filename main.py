954109a98348535cda85efb1782895ea03f07834
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:16:21 +0000

    official would wide rubbish shame

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
