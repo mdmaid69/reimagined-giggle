0714c80eebc08a4ef8576d986182159e9c91e4ad
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:31:05 +0000

    takeout rarely stare label but

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
