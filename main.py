cf0ff302455866c5b866c3e69d91f51a6706a6bc
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:03:39 +0000

    right further opposite reach fly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
