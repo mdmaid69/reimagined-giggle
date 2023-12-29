fb6b52cf4d6ea66d683eb40fb5bcfc651d63d8f7
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:43:21 +0000

    slow different like film worth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
