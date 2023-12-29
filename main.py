367339d1e5e7b6d1ae579c860824714615986178
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:20:04 +0000

    compare arm offend team then

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
