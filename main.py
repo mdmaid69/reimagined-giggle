46dc066c4b8f055099239bac6ee09a92ed3f3a77
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:02:58 +0000

    something equal regret difficulty bored

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_power(work, time):
-        return work / time
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
