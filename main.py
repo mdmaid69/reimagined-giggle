b4f32db83dc4056b8d2720943e006925c602608c
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:41:11 +0000

    the boil live become plain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import collections
+def create_queue():
+        return collections.deque()
+import collections
+def create_priority_queue():
+        return collections.deque()
