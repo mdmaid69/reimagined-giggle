5f147ab9e7f63457849a142bf33342f8a2ecb35e
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:49:27 +0000

    but want clearly blame food

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  def convert_to_octal(n):
-        return oct(n)
+print([x**2 for x in range(10)])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
