16252c0e885237bb5ab94d98002a164181ac8797
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:23:02 +0000

    feel sight rush trade round

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import random
+print(random.randint(0, 100))
