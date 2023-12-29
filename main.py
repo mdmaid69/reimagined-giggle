f15c24c22d9b264524c8dc1bbb6891215e7db7aa
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:21:25 +0000

    lookaround expensive myself on plan

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+text = "Hello, world!"
+print("Uppercase:", text.upper())
