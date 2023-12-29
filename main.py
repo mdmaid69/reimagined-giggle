e521ccc76090c4a073d19bd1dd4347f74ac4bd07
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:39:05 +0000

    expensive hide land neat sink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import time
-def get_current_time():
-        return time.time()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
