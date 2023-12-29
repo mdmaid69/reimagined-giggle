654fc8c84a6fe48b6f822cb47398fbe62ed516cb
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:27:16 +0000

    use available flow blind proof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sys
+def exit_program():
+        sys.exit()
