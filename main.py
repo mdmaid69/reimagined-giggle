a88960f135948b5a5721cd2e3f452c3bb25b200d
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:30:11 +0000

    port complaint question benefit clearly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def multiply_numbers(x, y):
+        return x * y
