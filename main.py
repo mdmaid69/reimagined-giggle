e198317dbf92adc1d09b6159cf7891fad9a7bbac
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:08:10 +0000

    stick raise tread sometimes well

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
