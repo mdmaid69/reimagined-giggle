0ccc53fd4f4e0a103ad8f88d9d697badd0681283
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:20:22 +0000

    kick suffer round crime contact

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_max(lst):
+        return max(lst)
