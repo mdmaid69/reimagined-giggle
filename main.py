800f6de77b9f806ef98e05418f727137da76e5ef
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:05:21 +0000

    extra raw press wise neither

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_as_frozenset(array):
+        return frozenset(array)
