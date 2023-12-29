75ff503a580b1019a6d49862dbdadcd31af885c8
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:35:16 +0000

    trust clue run sweep like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def clear_array(array):
-        array *= 0
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
