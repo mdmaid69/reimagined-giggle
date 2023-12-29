4fc1d7fecfb5b3dcd9992e0c2dc4cda2abe000c3
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:14:35 +0000

    number be hit although shall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
