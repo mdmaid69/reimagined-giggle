20e05b11d49e2a994410b635ce969bfadd850428
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:36:29 +0000

    pot event hot worth argument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
