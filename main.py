90d78a140d5805c91e8f4d89d8022b9509860fea
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:27:00 +0000

    rain treat joke happy maximum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
