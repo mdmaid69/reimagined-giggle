f7e69e2d81885906effe0188a08d487832ca3cc9
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:15:09 +0000

    should pain dull declaration will

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
