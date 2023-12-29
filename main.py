8957e66b37917ebeebf714191abd490b0e720202
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:14:49 +0000

    night like without still about

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
