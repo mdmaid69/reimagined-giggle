74ce3290e6c6b56f8f42ff5d7fb390a2a51bf849
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:37:58 +0000

    interesting cover difference form promise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
