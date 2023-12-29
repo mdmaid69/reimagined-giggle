2148a37f795d9d7ce75f474a3222e1d90df947bb
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:51:12 +0000

    appeal basis comeout century confident

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
