b369a058fc400d9b3260cbea8fe99cef549b2a92
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:30:47 +0000

    object reliable side like know

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
