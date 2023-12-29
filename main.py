d9360c038285ff3fcdddb3b73ee91c61a390fc07
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:15:34 +0000

    spring callat go same reduce

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
