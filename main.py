e178e50e22dc07ac808832791132888a2b442dea
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:19:54 +0000

    second see one mark our

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
