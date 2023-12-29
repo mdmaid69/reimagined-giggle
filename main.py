5e271f71a8ff301987f9a53cfddcb30b841e3c22
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:41:26 +0000

    mysterious workon contents style famous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
