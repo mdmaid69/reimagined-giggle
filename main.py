c1eea27a56922f4bafeddee29ec441f9aa79c016
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:33:17 +0000

    polite takeout continue adjective remain

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
