1015cc013a43422a279a359d2648f7573c61f339
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:44:52 +0000

    immediately rush average shoot explanation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
