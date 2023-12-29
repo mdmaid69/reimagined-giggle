b4d81bcc8e927c05644075e3cc42db1af61d850e
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:52:03 +0000

    wing turn bottom share please

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+print(os.getcwd())
