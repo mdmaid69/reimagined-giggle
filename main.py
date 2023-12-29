e9037d7b412a345aa36cf8107a7f10f567b6523f
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:52:40 +0000

    start sound factory safe market

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+i = 0
+while i < 5:
+        print(i)
+        i += 1
