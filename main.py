9ab48fdc790cb3ed6c8cdf54cde089d973732498
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:47:32 +0000

    interruption stop awake change petrol

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_power(work, time):
+        return work / time
