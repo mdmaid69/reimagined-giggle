f3f3faa21dc4889e587448758ec7ecc00cdb8e58
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:35:27 +0000

    along minority worse then cruelty

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def convert_to_octal(n):
+        return oct(n)
+def calculate_area_circle(r):
+        return 3.14 * r**2
