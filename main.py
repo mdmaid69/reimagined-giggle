9845c0b92f727cfc3c3e3f69c0b522f36bb88d9d
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:31:56 +0000

    size mark range water pollution

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
 import os
-def change_working_directory(path):
-        os.chdir(path)
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
