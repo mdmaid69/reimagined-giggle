f70efccbda8597a4f1a864ffdfde66f7a1b41f83
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:08:50 +0000

    constantly convince call minority sudden

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  def subtract_numbers(x, y):
+        return x - y
