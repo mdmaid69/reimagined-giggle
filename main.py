278d6e74b2c7cb3a2e5cd8ecbb34941864ed4787
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:28:22 +0000

    quiet better copy embarrassed rush

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
