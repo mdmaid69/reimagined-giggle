10eb242c0137a232991babe59c3488cf1b484782
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:47:52 +0000

    murder tune soak prove action

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
