ea0a951269637b12121a9a142020b9d933b99d0c
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:42:10 +0000

    professional still there afraid nothing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_max(numbers):
+        return max(numbers)
