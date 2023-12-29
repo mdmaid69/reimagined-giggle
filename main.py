3f86c1a6305d430f0dddc931751c15562d7b787a
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:17:36 +0000

    vanish soil science swallow may

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
