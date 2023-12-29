8b3c9577f9f1b2eb3f6fd60ad2e5164a34d23a4d
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:44:44 +0000

    good repair basically welcome perform

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
