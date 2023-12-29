cc91c4b9b9915dbe1c1466d78525452369922ff8
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:11:10 +0000

    damage study difference dangerous turnup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
