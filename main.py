0eac97c30aa6112333118f95dacc493f4da4bd68
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:20:18 +0000

    reaction school church quickly priority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
