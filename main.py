19779ea99caf4e52f966b99d5a3076e9465fe543
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:41:48 +0000

    small imagine keepoff prize go

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import time
+def get_current_time():
+        return time.ctime()
