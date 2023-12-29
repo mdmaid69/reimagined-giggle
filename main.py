4690df2a9522f5951c6ff80f21eba509e09e159e
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:45:45 +0000

    impressed furniture among wide produce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
