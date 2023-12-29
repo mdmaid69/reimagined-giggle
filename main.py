473025354d4d5239f1c8f67784a6bac748474bcb
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:39:31 +0000

    name for direction mistake forgive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
