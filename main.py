283bccda908cb973dd615aa475a2a969d6cb030a
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:32:02 +0000

    dear date off goback(to) needle

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+for i in range(10): print(i)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
