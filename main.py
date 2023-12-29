fffc428b275f3688ce614de0b8e3b72a76b82fb2
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:49:28 +0000

    occur our remember depend it

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
