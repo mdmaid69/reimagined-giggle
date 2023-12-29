ddc963ba960b61dc278700d6d12e9a2f98813151
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:01:08 +0000

    sentence care myself describe sad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
