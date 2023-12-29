e7d76974d7fb03aef693410882bd238d524341e9
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:54:43 +0000

    weekend rise broken myself morning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
