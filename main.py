32f56d89bd0eea17e35baaffaa4dd7cba0286f37
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:08:49 +0000

    bringdown continue tap aware reduction

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def sort_list(lst):
-        return sorted(lst)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
