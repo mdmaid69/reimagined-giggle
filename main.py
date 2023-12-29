7b0b12136ce1289a122e13bf046b13e4651a1a9c
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:15:26 +0000

    gradually someone follow serious goout

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
