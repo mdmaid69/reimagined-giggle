13189e7f06a83b0384ae0557c52c4203ae804b63
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:12:03 +0000

    tape health various means meeting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import time
-def get_time_since_epoch():
-        return time.time()
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
