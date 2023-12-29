cf2639a7a41985ca5df9a8d8d77154e6b7302a53
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:06:12 +0000

    let class sit crash inform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
