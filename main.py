4b370246c5c92bac884875f8eec28489fec4c6fc
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:27:13 +0000

    disagree pay wish hospital rescue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
