50e9d8d3bc391b448652e4fd9303fb57b454203f
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:49:19 +0000

    lunch on later ourselves goout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
