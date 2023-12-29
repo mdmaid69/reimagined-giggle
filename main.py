aa92eafaa9f332397b85894c81c8047701e0eb79
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:08:06 +0000

    will return foreign marry interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_current_directory():
+        return os.getcwd()
+import array
+def get_array_index(array, item):
+        return array.index(item)
