cad45db6346c5dbb519addb50109ef5fbfede08c
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:49:43 +0000

    getaway gentle stir whom crowd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
