3e686b521896cfc119718e5bf9d761e603e62337
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:40:24 +0000

    public action top means mess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
