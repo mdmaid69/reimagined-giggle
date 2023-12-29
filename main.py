48cd93979188717c8531bc32385f23ff72bdbd43
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:55:29 +0000

    crazy group take male pen

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
