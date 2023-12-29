300eb2b647b6890b49fc059c233694a03d40e334
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:26:43 +0000

    duty nose face serious carry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+  def convert_to_octal(n):
+        return oct(n)
