90a0b27571c749f6dd2b175efb4bf30fb62db619
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:07:23 +0000

    show busy present fail cry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
