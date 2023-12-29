8bbf567dc57f092db9b4307cfd015be3904b3d4a
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:10:24 +0000

    change worried comefrom study period

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
