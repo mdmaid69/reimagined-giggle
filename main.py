a6f5810d5527f94f579265649c44f22f9866f9a3
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:05:12 +0000

    nowhere shout ocean pin inform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+def calculate_sine(x):
+        return math.sin(x)
