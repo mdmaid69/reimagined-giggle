837a2a7015b47738e503112f2fcbf5c7de08d811
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:31:10 +0000

    maintain wash fly raise callout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
