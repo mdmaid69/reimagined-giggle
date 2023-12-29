b5835cb47971543d9720385d56b0877210f3bf98
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:16:06 +0000

    bright female measure remove purpose

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
