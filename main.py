0a24a6da7ddecb800b8e99180f716c3bc9623c1c
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:10:17 +0000

    report busy brother style Hh

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
