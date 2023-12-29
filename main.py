2f4bb84d3e57f0230cf9c0f86df0d8edc3934b31
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:33:41 +0000

    side list allow coffee fat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
