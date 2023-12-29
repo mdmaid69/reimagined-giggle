985f1ba7bb30b68d9f00b661e67fae1a9582617b
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:43:11 +0000

    language wash possibility create delight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
