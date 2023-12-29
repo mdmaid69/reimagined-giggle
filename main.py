f18bbd37aff19310bba29a44d26fe97273ba6872
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:45:44 +0000

    most warm brush exaggerate minimum

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def create_priority_queue():
+        return collections.deque()
