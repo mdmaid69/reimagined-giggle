eaec34575ef31017c2dca987da25953c0e8a4d1c
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:49:19 +0000

    sort place estimate callout challenge

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
