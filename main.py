853c996b520c9817afd799ba3850b088992c6569
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:16:58 +0000

    yesterday thinkthrough step protest clearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
