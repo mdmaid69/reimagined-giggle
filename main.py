4ecd165d14151e6f93a5c4b76766589a438bfb98
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:12:40 +0000

    ours adjective where smooth father

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
