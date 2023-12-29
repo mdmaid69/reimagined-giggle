2d1a350d9f65fc78991d0331a04df7f22bc897a6
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:00:48 +0000

    herself read figure range contact

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
 sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+def calculate_area_rectangle(l, w):
+        return l * w
