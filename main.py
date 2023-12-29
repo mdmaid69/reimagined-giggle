199ca36bdb43b12f56608b7be727c17d39aad575
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:40:52 +0000

    mean club guide even left

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_work(force, distance):
+        return force * distance
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
