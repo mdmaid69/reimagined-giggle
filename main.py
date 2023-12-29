10afe362515b709981a791dcbbbe682c05bb0d92
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:41:15 +0000

    turnback Bb cheat goaway criticize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+text = "Hello, world!"
+print("Words:", len(text.split()))
