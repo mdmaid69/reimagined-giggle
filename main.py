7c35236b8c2d6738c3e2c4586d8a2a9939a544f2
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:50:47 +0000

    container keepup create invite comedown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
