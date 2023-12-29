2b827240d233afeb7b8cfd2e6526d6e65b947f07
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:13:33 +0000

    belong treatment petrol obvious middle

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
