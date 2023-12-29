8ee0f52939efa3eca221fde75da1fcb60bb0f70f
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:02:49 +0000

    relax branch scrap battery his

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
