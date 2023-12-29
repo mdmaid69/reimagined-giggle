fd9f39ca2bd8d4a14e38a175e9298d82530f7155
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:29:42 +0000

    sure available label jump hungry

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def get_array_as_list(array):
-        return list(array)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
