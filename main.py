32b7064c9f1f16a31bef1714df2095d7bf0be76b
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:19:46 +0000

    immediate trust boat boring official

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def convert_to_hex(n):
+        return hex(n)
