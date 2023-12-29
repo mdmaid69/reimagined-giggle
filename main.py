8e41f10758e52bf622093b99cba1abac915e85ad
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:37:07 +0000

    doubt reason live estimate otherwise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import getpass
+def get_username():
+        return getpass.getuser()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
