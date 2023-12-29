30e2a696542c09ceae6e612fac65881a7a12c875
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:57:36 +0000

    slowly spray average wash wake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
