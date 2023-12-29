f2903948a53415fb3dda3fa83ae1c868a919c16a
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:57:26 +0000

    in judge cold ring once

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
