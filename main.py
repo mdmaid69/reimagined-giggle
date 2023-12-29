e967efe801fb55193b615be357d33db98ce5a629
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:19:14 +0000

    anywhere plant tear downstairs description

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
