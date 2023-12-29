b682b7437e6a95270f530cafaf79e7247c13ecdc
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:35:48 +0000

    fear offence choice beneath empty

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  def add_numbers(x, y):
-        return x + y
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
