3ea2033dce6a22a9a68f539c3f745a0b22b045d4
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:01:37 +0000

    communicate develop increase funny practise

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
