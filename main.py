ab1059ecbecc4f6a9e2760e486dac235ae58b8d6
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:55:32 +0000

    lookback(on) yours nervous giveup no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
