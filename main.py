1b748200616d1671d90ba2bbbfcc36475946fdf4
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:25:21 +0000

    physical lookup Jj part below

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import time
+print(time.time())
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
