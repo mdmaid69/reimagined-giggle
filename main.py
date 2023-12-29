53b68fd2501b0adcd0d7bf04a340ef70ebb725a6
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:49:29 +0000

    radio gas glad name guide

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
