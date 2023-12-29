f179b40b05b4b09a132d8981197e64fa0d9f0a46
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:47:20 +0000

    imagination about offend fear film

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import datetime
-print(datetime.datetime.now())
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
