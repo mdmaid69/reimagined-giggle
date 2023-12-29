1bdf8fa358b7b996b2aa04aca8340344893d0f7a
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:14:38 +0000

    will afford when comeround listen

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def find_min(lst):
-        return min(lst)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
