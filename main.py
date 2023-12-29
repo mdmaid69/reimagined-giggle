c68ea996ac57365afcc02908f31f26b896409f35
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:28:37 +0000

    complicated sharp talkoutof injury remain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
