022e1f552905120c71691b5436762d978384642b
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:34:17 +0000

    the safe obtain connection forgive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
