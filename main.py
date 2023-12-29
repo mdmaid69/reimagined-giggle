e36aea80ff888b8e893fcdc5158307fd981ff5b9
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:24:23 +0000

    sea wish left occasion pleasure

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
