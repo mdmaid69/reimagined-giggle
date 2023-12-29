85aaea6594057773e32f354f06eac8fd27b06d7b
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:18:17 +0000

    my rain sad identify treat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
