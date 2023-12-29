4d4941d80f0df3cf06ffd9af4f1a3b5c92e1a270
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:59:53 +0000

    star common match future town

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def is_even(n):
+        return n % 2 == 0
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
