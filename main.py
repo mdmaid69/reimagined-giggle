01f15ff9d209275fdd7140781c5c038f785d58a1
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:40:27 +0000

    sleep person set wave anybody

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import array
+def get_array_as_set(array):
+        return set(array)
