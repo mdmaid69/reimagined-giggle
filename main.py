976aa9ae925e6203bf35ae8e3dbf6ca238f38277
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:40:03 +0000

    pen snow keepfrom mysterious firm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def multiply_numbers(x, y):
+        return x * y
