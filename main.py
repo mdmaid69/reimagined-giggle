eb6681cdd17e90ef37747f574383272d3e82ef2f
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:45:28 +0000

    themselves male dear look interesting

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
