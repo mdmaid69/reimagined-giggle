5119af54135f1a0b5914ade7a8ed65b94e794428
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:16:19 +0000

    sight female OKorokay nerve secret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_as_int(array):
+        return int(array[0])
+import getpass
+def get_username():
+        return getpass.getuser()
