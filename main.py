ec508790edf7d2fe213b600c223ee27a254f8776
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:52:57 +0000

    tall bag sort moon delight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
