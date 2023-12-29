dbc0c3cc46229499ffa29f6af17ce8fbd2c57e6f
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:18:56 +0000

    train year remark bother tool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
