8a70fbb86b5799e9c0e6f33bdbb1d63e495921c4
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:46:14 +0000

    proud off divide clean expensive

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
