9057b0cdf3f695b25249fcadfacf4e0921f14281
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:00:37 +0000

    replace issue progress spite can

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import time
+print(time.time())
