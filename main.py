19d67cc5a98b66fdf58760460395672b3b0fb9c7
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:47:39 +0000

    generally tip other accident fault

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
