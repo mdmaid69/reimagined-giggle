49c910b953eb7c390d4d4b21272800100cc25ffa
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:36:45 +0000

    reply extra because friendly light

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
