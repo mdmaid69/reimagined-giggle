5f358d255da6ffffc970b12013f82a6e82e22751
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:31:41 +0000

    workon together male cough discovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
