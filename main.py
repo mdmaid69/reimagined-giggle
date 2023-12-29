b07d0a2dd1af696702eb523cd2e01fe6c6de9241
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:28:05 +0000

    stone rush stand rarely before

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_density(mass, volume):
+        return mass / volume
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
