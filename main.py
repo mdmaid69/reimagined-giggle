530ec357bfadffd1ead772d09041b93e81d87213
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:07:58 +0000

    drink female child comment front

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
