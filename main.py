e7c252442051fcf42610d2402d5ac5071a73e9bc
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:40:53 +0000

    justice high ago shall nigga

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
