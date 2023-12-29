c82f7df373bc7e231bc7064cde08ce71795ef099
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:16:54 +0000

    both wide climb breakout bringdown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
