f51d2c104fcfd149b55ecc186ab03cabab617d8b
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:03:44 +0000

    club its smart increase danger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
