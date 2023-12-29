732ce58e3785470bda50eb540fb1eccc57eb80dd
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:36:09 +0000

    shock perform excuse service cruelty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
