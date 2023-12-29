d648558a22b150fc078673af1d1bbe19b7d1fc26
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:49:29 +0000

    might difficult improve shock deep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
