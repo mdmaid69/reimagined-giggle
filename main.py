9c1492ff4228e58308be7d54e4e6b1c88289b5c5
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:14:46 +0000

    consider sleep else minimum storm

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import array
+def get_array_as_repr(array):
+        return repr(array)
