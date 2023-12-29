0ca038ef2c515f88583ed8794720e3a4e2717c4a
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:00:50 +0000

    mystery concern use note study

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def clear_array(array):
+        array *= 0
