1467c8b6401b56a6131e7697e32e157b626d0fb4
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:56:44 +0000

    someone down generally comedown brief

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-print([x**2 for x in range(10)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import platform
+def get_python_version():
+        return platform.python_version()
