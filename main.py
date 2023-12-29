cbe663d020d25d540798ecef88c8e6934ca53e70
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:18:09 +0000

    argue show use hit confused

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
