7e8b2ec9380cfbcb953f60eb081d308910ff5da7
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:03:57 +0000

    bright except contain regularly false

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
