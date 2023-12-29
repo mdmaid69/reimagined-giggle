309bfe89addf71aaee143ed908dd65804519f68f
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:46:45 +0000

    giveout war stage Ff thirsty

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def find_max(lst):
+        return max(lst)
