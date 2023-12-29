1355fe005f2c0e47eba35d96726480d62009370f
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:44:26 +0000

    boil holdup through plenty remove

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def convert_to_octal(n):
-        return oct(n)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
