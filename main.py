1c95d332095e974bb4e72e4dba573e6e9cfe87ca
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:40:47 +0000

    public summer time smell shallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
