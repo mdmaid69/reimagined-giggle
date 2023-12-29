083831854ea070120f3b861ca39bc5d9367e544b
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:00:53 +0000

    every thinkover herself achieve offer

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
