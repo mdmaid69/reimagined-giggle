4a548be4bd2f85fd6fab31707ba76cfe58be627e
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:14:52 +0000

    danger occasional comfort ignore let

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
