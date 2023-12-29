025b344827036fe9db39a18253e688b36f726437
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:09:27 +0000

    ear fall away juice memory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import time
+print(time.time())
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
