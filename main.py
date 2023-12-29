8ef9505b6ab3e3364a4d6fa005eda69fb73de49a
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:23:26 +0000

    typical minority wash pair fairly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def count_words(sentence):
+        return len(sentence.split())
