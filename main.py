ab810aaaff9816a6753e15983bff284df2824d82
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:15:44 +0000

    cause more treatment finger through

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def reverse_list(lst):
+        return lst[::-1]
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
