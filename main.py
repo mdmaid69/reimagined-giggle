1f35d32796815363729cf29abfdae07394aa4fb4
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:29:58 +0000

    healthy delicate money pronunciation place

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_current_directory():
+        return os.getcwd()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
