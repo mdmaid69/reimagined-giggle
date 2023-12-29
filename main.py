7b0e4878c27c4ef75f420da27050aa9dec04f26d
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:16:49 +0000

    pool go ring ear cruel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-text = "Hello, world!"
-print("Characters:", len(text))
+def calculate_power(work, time):
+        return work / time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
