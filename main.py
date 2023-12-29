c9a422ce4c32bde168d9ab2cfc3097a0ff202b84
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:31:18 +0000

    world relax detail 565end project

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def calculate_pressure(force, area):
         return force / area
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def append_to_array(array, item):
+        array.append(item)
