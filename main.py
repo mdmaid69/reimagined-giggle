772cc7b9743b1b71abeb715002dade19fb8df1c6
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:41:07 +0000

    live home challenge accept face

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def get_array_as_list(array):
+        return list(array)
