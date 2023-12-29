23954ef2e6ca4b601ece4aba2df5a254a41d9739
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:48:46 +0000

    actual lucky common hold birthday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def convert_to_binary(n):
+        return bin(n)
