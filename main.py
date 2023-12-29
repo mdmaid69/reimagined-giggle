c4758bcacee9eba387afed5667feff6eafc44d64
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:54:57 +0000

    obvious ball compare lookafter inside

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import re
+print(re.match("h.*o", "hello world"))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
