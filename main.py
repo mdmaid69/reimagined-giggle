75cd94b07fa91611c846a1619e5840691f75a2db
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:34:09 +0000

    whistle Rr record workupto firm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
