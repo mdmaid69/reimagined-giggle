658987788ac4dc5c77d2f9a4b49b7e64384a7eac
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:58:06 +0000

    sound himself bring shout before

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
