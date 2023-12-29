cd64d0312555584461ced1b3cd49b8f90401fae3
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:51:51 +0000

    accept yes promise crazy false

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
