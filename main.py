499430a74235e0cbf38d30319e60db15b4be2123
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:01:01 +0000

    Pp pity manner whoever journey

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
