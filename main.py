c4fb4b382cd82dfe98a0bd46331791b3423e4cef
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:44:45 +0000

    ever fair achievement strange careless

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
