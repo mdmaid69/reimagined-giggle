b841fde976acc0c1161c586940e3c8419565103a
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:51:07 +0000

    visit gap steam breakdown ocean

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def sort_numbers(numbers):
-        return sorted(numbers)
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
