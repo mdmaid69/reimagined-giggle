a3d7983ad414277836ff00a17252d8e62ad713da
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:41:53 +0000

    extra such mistake negative Cc

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_pressure(force, area):
-        return force / area
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
