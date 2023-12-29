1cb5e6d6c98cc81c55dcc2a399c770602eb59180
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:28:02 +0000

    company spin reply hurry high

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
