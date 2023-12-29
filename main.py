320c359df46a9d6c16a52085645f26fc240b0377
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:06:22 +0000

    school top through minute yours

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_max(lst):
+        return max(lst)
