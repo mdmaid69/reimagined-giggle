052af26fdc29467bd169209340c8930d1ab15be5
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:24:41 +0000

    protect takeoff need tear turnover

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
