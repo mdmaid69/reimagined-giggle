02ea130a3ff71f5ecb6aaa8a26e227788c3c9f59
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:04:43 +0000

    belong home trap rush preparation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
