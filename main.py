b57dfb1924f68775529bc3132fb8d71e94fa4698
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:24:16 +0000

    my emphasize shine main firm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
