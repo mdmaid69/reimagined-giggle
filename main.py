60a3d267d36f2da1d20ed714fb2b17fb122fff76
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:47:14 +0000

    sweep join manner above field

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
