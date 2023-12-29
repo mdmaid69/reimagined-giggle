c5fb4728d68328fabfb7d5f28b384a0470793c98
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:38:05 +0000

    subject chief confuse alone spread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_current_directory():
+        return os.getcwd()
