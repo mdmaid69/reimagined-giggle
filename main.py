ac3df0fe6da7593a4ef122f25add298b925523fd
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:10:40 +0000

    half inform average back free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
