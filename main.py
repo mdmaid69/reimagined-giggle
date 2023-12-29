b9c57e304557b29ca03447fc547728b234ca6c14
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:07:01 +0000

    computer man late try finish

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
