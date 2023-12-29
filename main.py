b0c5f95a13015a5e2085b78f16d600111ca70fec
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:00:22 +0000

    wear string peaceful after absolutely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
