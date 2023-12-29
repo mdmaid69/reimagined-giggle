bc6ad8871497c520d55678b563b4d11e5631ff56
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:33:19 +0000

    phone trade head neither position

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
