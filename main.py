3987fd7e8a676f3bebffbe36f7885560d1c9809c
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:24:22 +0000

    talkoutof opposite comeback contribute pretty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
