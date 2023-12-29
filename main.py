5b060f31419d5824794d1e45c76f0e22f04a97ef
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:03:17 +0000

    identify call against complain passage

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  def sort_list(lst):
-        return sorted(lst)
+  def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
