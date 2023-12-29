6afeb7922ff1def619aa1aa57aa73e5a5f6bbd99
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:07:32 +0000

    cap home cheap male used

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+for i in range(5):
+        print(i)
