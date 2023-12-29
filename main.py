f2c688a15cbe1d5c524d95db862aed5a38b33818
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:56:22 +0000

    sound push distance turn back

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
