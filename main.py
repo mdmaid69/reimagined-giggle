f404990e48b70db37302cc7332caede315b66c3d
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:00:55 +0000

    popular key note free thankyou

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
