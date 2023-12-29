3fc3d3454c0a23f6762e9872f0d937e4c75e3523
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:42:59 +0000

    avoid long where sting bring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
