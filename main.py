d0295f38cfa545a49f7ba1789ca462e55c6e5f5c
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:42:57 +0000

    sing direction television limit experience

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
 import sys
-def exit_program():
-        sys.exit()
+print(sys.version)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
