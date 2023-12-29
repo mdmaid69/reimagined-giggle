7f75bdd4609349a08c9ddeba10036f5002e8de64
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:53:34 +0000

    criticism dinner trust practical strike

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
