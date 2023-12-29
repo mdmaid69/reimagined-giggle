013825d2341698fc72e3357aa4c94e0f62b0b4f2
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:22:28 +0000

    nervous game available cut slightly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import os
+def change_working_directory(path):
+        os.chdir(path)
