56229d28d1851eb2da2f3b4931a4e731e1e1558e
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:12:32 +0000

    substance machine young fall imagine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
