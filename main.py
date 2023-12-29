ce72280c197f5333c9f4b22ecfe97465e3c77fa0
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:15:33 +0000

    terrible whenever appear to equal

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
