fbe553d929c0ec8fb4839b29489e2d8e5d4ab394
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:24:32 +0000

    move unkind skill throw near

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
