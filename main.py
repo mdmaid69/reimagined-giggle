e3ca61bbe20df9a53b6ef79476deaa307eb7ad7f
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:37:58 +0000

    dry ring bet tragedy month

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
