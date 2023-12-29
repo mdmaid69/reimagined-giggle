34f85cc72fd64031e498feb3aa747a0ffd3dea6d
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:04:34 +0000

    hold her create fasten crash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
