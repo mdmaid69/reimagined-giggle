d3dcd5781b85f7d2e995dd145ff6ace6c63b386b
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:00:56 +0000

    announce experience mistake now laugh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
