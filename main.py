e9896f43d46cdf0cbeb0ff5f22f5223014178094
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:56:16 +0000

    surprise reply train positive murder

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import sys
+  def get_python_version():
+        return sys.version
