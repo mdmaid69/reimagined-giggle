21bb54438e6436d20218151f4ea3722057d47269
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:30:30 +0000

    respect warm wrap war public

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+from collections import Counter
+print(Counter("hello world"))
