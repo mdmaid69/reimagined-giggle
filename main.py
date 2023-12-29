9dd0505541671169b1e46b932e022e1dc64c425f
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:07:54 +0000

    temperature affect backwards track listenfor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
