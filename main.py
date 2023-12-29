2377e3122de1dafa29ada18e3f4960a304588ed9
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:25:52 +0000

    run long quickly freeze contrast

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
