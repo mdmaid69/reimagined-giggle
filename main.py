85f59c3f100c975c39059e4c4c794d0442d4e438
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:37:24 +0000

    takeover comfort nose 573entertainment callat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
