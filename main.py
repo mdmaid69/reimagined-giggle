a143dc639a1c77afea2dd2fb872a18e3a76abef7
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:42:49 +0000

    bringdown nurse plenty actually prefer

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
