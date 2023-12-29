4e9d98350206074f9438e49a67096e8aa813e56e
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:28:48 +0000

    suit better Cc turn among

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import sys
+def print_python_version():
+        print(sys.version)
