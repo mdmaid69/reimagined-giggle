cb8800480b820fc0cca815b2c7c9de301ad12a58
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:08:07 +0000

    whoever address firm tune risk

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
