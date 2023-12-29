edb43059aeead46ac233160c311a157d9b7eea53
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:20:02 +0000

    makeinto love loose neat nigga

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
