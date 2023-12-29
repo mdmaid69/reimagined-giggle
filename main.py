3e9e95765abfa57806a8ef0e5eccbc6f748041e2
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:16:10 +0000

    strong price office very edge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+for i in range(10): print(i)
