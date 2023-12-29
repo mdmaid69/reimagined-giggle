8619bf4c52a8ff94d2bccf5d77bbf8436ac349c2
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:17:48 +0000

    please aim ourselves pin cruelty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-print(random.randint(0, 100))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_max(numbers):
+        return max(numbers)
