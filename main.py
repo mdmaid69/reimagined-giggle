0b280636e42a12bfc50e689957aafebfbe6fa351
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:16:09 +0000

    workupto late underneath border breakfast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
