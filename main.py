d0559faedf544668a6d4a0a00aa119fecb07133d
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:46:26 +0000

    suppose name gate lay instead

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
