3d5029a291560c6abc04a62879b8a491aaf7d1a7
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:52:59 +0000

    precise unkind present tune pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import collections
+def create_user_list():
+        return collections.UserList()
