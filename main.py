aee6be8f9f3c2e7078ff6eb3e485c36789ddf41d
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:09:07 +0000

    allow car birthday quarter paper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
