44e6365f3464d797fd2473b2308ea501cb4de64f
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:34:17 +0000

    fond test worth hers makeinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
