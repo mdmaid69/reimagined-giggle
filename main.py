874454655b99492407730b6dfde236e39fe08756
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:33:11 +0000

    ocean suggest question coat gooff

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import sys
-def exit_program():
-        sys.exit()
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
