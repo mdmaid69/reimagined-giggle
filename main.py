9400e77dad3d6acca372f73dabb69befbfe82f30
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:29:58 +0000

    fair before Yy block mend

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def split_path(path):
+        return os.path.split(path)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
