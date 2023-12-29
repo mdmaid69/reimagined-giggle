7269e73efcc9ceb795f22f1cd7416ff83ebadd3e
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:12:38 +0000

    insist in cover action warn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def clear_array(array):
-        array *= 0
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
