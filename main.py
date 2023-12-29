e61b3cb01f50ab540d4523506adfa9a3327f8b8f
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:28:20 +0000

    concern brief glad service thank

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
