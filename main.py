7dca678f61cbb3a81e7abebb05cb608d692a1f71
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:17:37 +0000

    tread miss there control bag

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def count_elements(lst):
+        return len(lst)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
