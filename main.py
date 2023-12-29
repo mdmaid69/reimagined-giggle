0b1a9a7284b14c9d5edbdb847ee639819982d109
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:48:48 +0000

    various something experiment suddenly switch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def calculate_area_rectangle(l, w):
+        return l * w
+import array
+def convert_array_to_list(array):
+        return array.tolist()
