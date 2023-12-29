8e32e8dd66e3177c9acededa4abdae317b2a7753
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:59:14 +0000

    most fast belong next note

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-x = 10
-y = 20
-print("Sum:", x + y)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
