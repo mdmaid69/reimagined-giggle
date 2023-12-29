f8ea3b9233c1a76341be17416daaba8980ca86f4
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:25:04 +0000

    tap expect true better plain

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
