1d3bd30e3abbca676ef783c7e960495cb6532d9a
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:36:06 +0000

    closely callat count preparation plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+def count_elements(lst):
+        return len(lst)
