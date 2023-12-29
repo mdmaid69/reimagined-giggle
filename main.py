9d48b3f9eae80ccb0854443d1c8e5d5599f4cfd7
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:29:57 +0000

    minute aim makeup nose sail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+for i in range(5):
+        print(i)
