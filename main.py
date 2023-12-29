2512ac7501a34a9796b6c3c1b43dc4fde594e705
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:32:03 +0000

    fun responsibility peaceful bringback table

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_acceleration(speed, time):
+        return speed / time
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
