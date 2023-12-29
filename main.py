40b73d46e3391a8d17bee50e90d7a3199fbd0589
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:00:32 +0000

    hurry must classroom over age

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-print(math.pi)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
