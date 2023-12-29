d7d79eba391eaf2eaef1a134580ca1384fd642d5
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:35:22 +0000

    sale unfortunate might decision complete

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+text = "Hello, world!"
+print("Words:", len(text.split()))
 import re
-print(re.match("h.*o", "hello world"))
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
