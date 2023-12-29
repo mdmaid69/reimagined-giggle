37b4b7029d31261ca4b6097632525d49cd2fbebb
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:49:11 +0000

    excuse claim different motion concentrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_typecode(array):
+        return array.typecode
