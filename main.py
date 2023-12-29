737ef5d788e819523b660fbea440512cfb3468fd
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:39:44 +0000

    keen easy arrive ought meaning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
