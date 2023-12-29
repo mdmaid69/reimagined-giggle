6ad03f61727c2393cd490db067ae698f2672b39b
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:13:00 +0000

    invite takeaway exchange public underneath

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
