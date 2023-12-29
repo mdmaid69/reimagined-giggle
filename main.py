ae1f4a23eaeac445ed178e1e2f10fd88a72d6b88
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:19:18 +0000

    top clever range needle conduct

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
