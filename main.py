8cf949a33baa5000e40ea58ee89171c8305178d3
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:41:09 +0000

    unit big well invitation estimate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def count_characters(sentence):
+        return len(sentence)
