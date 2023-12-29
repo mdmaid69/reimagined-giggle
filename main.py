296c57aff47c6a0126193176c0e50cf384442794
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:05:04 +0000

    uponon fun plastic bank factory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
