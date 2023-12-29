dac0bbdfb41f052f03d1dc167c694ac319cc4a55
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:03:51 +0000

    spite enthusiastic burn manage decide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_repr(array):
+        return repr(array)
