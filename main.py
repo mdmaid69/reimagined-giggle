0d59eceeafcb57390a856060f17c7a21ec6a7f74
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:58:28 +0000

    rest us home steal future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import time
+def get_current_time():
+        return time.time()
