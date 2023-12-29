9e4b428858a475d544a13c861ee460d61cde745c
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:56:12 +0000

    pretty direct laugh tough land

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
