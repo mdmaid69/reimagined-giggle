c0d7338afaa70e973189598c096563c397c913f3
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:42:08 +0000

    573entertainment total correct cause slow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
