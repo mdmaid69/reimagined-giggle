cbdf90d0c30cc3a0fb2517539e3e7c030091e208
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:52:44 +0000

    centre platform afterwards care beginning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
