6763194f8b61462a8dd565f336c83f7c14ed18dd
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:51:43 +0000

    waste rich Ii method pretend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import sys
+  def get_python_version():
+        return sys.version
