e84f1ac12a83c32fcbdef7581e33491510d90ccc
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:50:29 +0000

    miss breakthrough centre confused whistle

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
