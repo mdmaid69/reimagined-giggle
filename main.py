166882af5ac7a49fb97a8c6b313717c1c04737d1
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:17:20 +0000

    all touch show sister ill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_area_circle(r):
-        return 3.14 * r**2
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
