d03a0d759ef3c0db711fc96041fbb6b7cc7b5f92
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:48:11 +0000

    way meet difficult name glass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_current_directory():
+        return os.getcwd()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
