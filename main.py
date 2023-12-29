a721ad63d74aaeef48c698ea67e252f2dd97af21
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:14:16 +0000

    strike ill spring time sensible

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def find_unique_words(sentence):
+        return set(sentence.split())
