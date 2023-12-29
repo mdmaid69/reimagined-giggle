2de0fc937b962963e464d92a66cbb14e153a8615
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:09:02 +0000

    vision keep long question contain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
