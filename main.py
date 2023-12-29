e63ede517a85f162462de7e2cbf3a974549e1886
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:02:16 +0000

    development opposite talkoutof child dry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
