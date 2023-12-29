9d024ec662d2b288551595756c7456b5bc8b128b
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:05:45 +0000

    live extra imagination young laugh

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import array
+def get_array_as_str(array):
+        return str(array)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
