095c6031c1d42b32757c61410e77aacc81291ef5
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:28:09 +0000

    loan thanks straight lot willing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
