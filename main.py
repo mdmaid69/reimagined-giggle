e0d142072b5cef5887bbc86b18e4aa6436e366a1
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:35:36 +0000

    follow actually wide estimate centimetre

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import time
-def get_time_since_epoch():
-        return time.time()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
