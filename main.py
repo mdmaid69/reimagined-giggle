100f1ee3d1fd5ea32d23c469458e2423cb85e67a
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:23:23 +0000

    whistle famous sure telephone cook

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-print(math.pi)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_speed(distance, time):
+        return distance / time
