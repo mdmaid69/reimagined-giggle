0c6be0eaa0633371ea9c8fbfd31d579f2e62af08
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:44:48 +0000

    behind branch difference complicated conversation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import numpy as np
+print(np.array([1, 2, 3]))
