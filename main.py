2759858c5e1b0d766c87c8f28cde5fd22a12d591
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:47:06 +0000

    love decide thin damp arm

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+for i in range(10): print(i)
