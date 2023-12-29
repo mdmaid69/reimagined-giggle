ed41b17bf9059e65e7f0f5a5809816699c0334d4
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:31:14 +0000

    fill point 573entertainment undo skill

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
