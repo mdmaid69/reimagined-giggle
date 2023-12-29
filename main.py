1f86aa6e69895a7e707339a4a36290ae1c995bb0
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:37:17 +0000

    put favourite ground spend at

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
