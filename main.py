acf9d3b63bdb1bd39717dc58dd49097bf76ea836
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:25:32 +0000

    big account holiday go second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
