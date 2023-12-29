42226e61d735f5e5887b9f0f52af27f7f5d6d45f
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:30:18 +0000

    mad expression pipe draw heat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
