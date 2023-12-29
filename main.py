51a1edd419333f6327ce9d0501b701187690dd8c
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:37:32 +0000

    lay central store tragedy equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
