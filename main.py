673d49b16190efaf78bf79a5103400a327e6ac38
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:54:09 +0000

    change story either spite achievement

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
