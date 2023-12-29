f45979ecf44415c8fa886662b583ea86312838b2
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:53:25 +0000

    fact score yesterday share gooff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
 def calculate_equity_ratio(total_equity, total_assets):
         return total_equity / total_assets
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
