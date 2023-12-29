08aa9380b4b9aed1e00edaa970de6d19e7853ef9
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:23:29 +0000

    pattern just refer hang blow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_work(force, distance):
+        return force * distance
