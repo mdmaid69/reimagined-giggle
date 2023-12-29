982cac2e0761a9729d7a15083fb3c8fb23fb6207
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:03:21 +0000

    involve goout open eye holdon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
