a93e8b5bb5d6ba5964107d90ece1393d03b97434
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:25:36 +0000

    freedom quick go tread Kk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
