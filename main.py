b7329338e02cbcbdaaff34f43c65e5bcc384d2d4
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:23:58 +0000

    shut comefrom ticket tip bringforward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def reverse_array(array):
+        array.reverse()
