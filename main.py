a1307a78dd333878174309af67de59c7ea0304e7
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:01:14 +0000

    lock yourself emergency snow complaint

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
