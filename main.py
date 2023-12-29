5072e5c446d32fa7b8d75b1d57bb6900da4b7c20
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:51:55 +0000

    fit interesting wealth class pay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
