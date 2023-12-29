7abae3933dd09550386410d45d67872df8d61f4e
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:54:33 +0000

    ill set nearly aware doubt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import collections
+def create_queue():
+        return collections.deque()
