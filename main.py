fe1e731d756448bf06a6924cc1bd1cfd1501a192
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:22:15 +0000

    concerning steep flow eager year

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def subtract_numbers(x, y):
+        return x - y
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
