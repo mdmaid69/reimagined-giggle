955c7cb38b260b6a2c971971733017d6147b1cdb
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:13:08 +0000

    politics coffee eye declare instead

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def convert_array_to_bytes(array):
+        return array.tobytes()
