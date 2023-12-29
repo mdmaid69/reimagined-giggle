4482de61d5f82662123e3c790b2315fe7ba32346
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:24:39 +0000

    club attitude crazy cheque unlikely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
