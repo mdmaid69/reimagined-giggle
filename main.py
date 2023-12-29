d1fcf3c18f43831846d643991f5d46d1df3e09c4
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:28:09 +0000

    neither positive first left peace

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
