9ae1758eb1291934699c52c5ad9e72de3443a8a1
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:46:39 +0000

    store deep store boil measure

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-print(sum(range(10)))
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+text = "Hello, world!"
+print("Uppercase:", text.upper())
