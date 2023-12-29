b76f5511f8787826fc0bd25581ddc37c08c0cba7
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:39:09 +0000

    fast last crazy description stay

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
