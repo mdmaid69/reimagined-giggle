740789741adc4450b4ac4ced30883c778b796d54
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:47:36 +0000

    lot moon dish head disgusting

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def subtract_numbers(x, y):
+        return x - y
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
