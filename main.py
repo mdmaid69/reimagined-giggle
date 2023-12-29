05dde8513f1449528fd65676a2d0e416d3737c54
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:46:03 +0000

    family high brown raise stone

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
