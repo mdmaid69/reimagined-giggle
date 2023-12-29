3c4ab8520e56121363e05a12f724d0cec99b76e3
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:39:29 +0000

    bin black meaning otherwise callout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
