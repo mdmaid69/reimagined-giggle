3ab1612816f2130218234ced2a1625502ce40ba4
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:44:25 +0000

    process classroom breakthrough unfair thumb

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
