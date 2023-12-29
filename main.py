5b9d26261c833ad1d3e5fe8ba31c6ef593da9bd1
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:06:53 +0000

    mind problem prove damp group

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import numpy as np
+print(np.array([1, 2, 3]))
