7aaf70efa4c8fd654e5c7029394928becc6fe424
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:54:19 +0000

    sign equal interesting sing basis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
