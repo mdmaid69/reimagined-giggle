00a56501362496470276082a5712027ce7beb3a8
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:38:37 +0000

    guilty roof dig spin sea

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
