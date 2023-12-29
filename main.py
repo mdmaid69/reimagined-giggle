afdaef66c31ead0c7257133a52b561de400f309c
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:42:24 +0000

    true its store although myself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
