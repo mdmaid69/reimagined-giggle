9013afbca84bcfc61011f57acc4c6221a1bc80e5
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:53:54 +0000

    platform poison normal sting relax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
