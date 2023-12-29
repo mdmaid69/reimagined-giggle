7c6784fe8344ece5d685325c0d2200b31821d3b1
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:38:40 +0000

    pay sink advice outside rubbish

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def square_number(x):
-        return x**2
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import time
+print(time.time())
