8fd420bbc750d255c743f129b91bc49b5a002e71
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:19:00 +0000

    reach soft owe surprising period

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
