c06a72a32ca7030a82ad9cdfb6cad55120701fde
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:45:55 +0000

    once who decide none confident

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
