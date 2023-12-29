c2ef3a4a90e62f9a0a6e22d1eed5462582b1747d
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:30:39 +0000

    sport lost production day question

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_min(numbers):
-        return min(numbers)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import time
+print(time.time())
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
