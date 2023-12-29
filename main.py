7ae0e1e6f2b9428a498ddc8eab6a8516bd8c8cbc
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:57:34 +0000

    attractive coat himself area about

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
