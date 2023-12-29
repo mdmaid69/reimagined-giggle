87d72a7e6367d4a3f83c8e6e332918e398378f27
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:36:41 +0000

    door letter attract getup basis

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
