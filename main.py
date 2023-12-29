9a522a70301bf15e907f7f1cdb7a020bc90180c0
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:51:39 +0000

    peaceful you agree lost office

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-def reverse_string(s):
-        return s[::-1]
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
