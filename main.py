9010649151f462c9b82f32b58b72cace8ff5481c
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:18:09 +0000

    slight can interest through project

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
