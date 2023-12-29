912f2dad2a8774ad4ad9a093b73a193397e3fe17
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:49:56 +0000

    principle spite hello whole worst

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
