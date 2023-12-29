33539894ba86e06b86b317f63c1572be97068ee4
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:51:56 +0000

    successful where weigh stiff nervous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_power(work, time):
-        return work / time
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
