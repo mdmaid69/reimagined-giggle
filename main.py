96c2ec4e3e83a07ec050163ece65a80cf24a5488
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:21:20 +0000

    rain spill meeting bright slip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_power(work, time):
-        return work / time
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
