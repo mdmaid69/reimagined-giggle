7bc86eec34df490c84725486b5f38e361abbe1d9
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:31:07 +0000

    regularly direct film refuse game

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-for i in range(10): print(i)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
