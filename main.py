3be3e6abbb9d8cb3ba8684b9df615076d71b758b
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:51:24 +0000

    season used I twist pretty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_min(numbers):
+        return min(numbers)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
