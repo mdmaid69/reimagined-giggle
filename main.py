9bdfec8130cfa9ab05a13413902973b24ef131f4
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:27:11 +0000

    affect business usually stiff ago

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_factorial(n):
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_factorial(n):
         return 1 if n == 0 else n * calculate_factorial(n-1)
-import getpass
-def get_username():
-        return getpass.getuser()
