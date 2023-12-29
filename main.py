c7df9e7eacc60e20f48381dc5270660d01377f3b
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:05:27 +0000

    attract drive rubbish to discuss

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
