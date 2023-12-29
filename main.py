689eda0edae2d23ac9919b30156844e22890b1d4
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:44:27 +0000

    trick height style cum among

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_as_list(array):
-        return list(array)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
