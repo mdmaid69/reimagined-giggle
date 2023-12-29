48f070c6dfdb708e77781fa8a686067faafbf4a7
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:46:56 +0000

    town answer brother country set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
