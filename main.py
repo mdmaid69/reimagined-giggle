884f03c28f6fb4d34603a941b1644ab3dcea1739
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:03:12 +0000

    recognize mistake agreement healthy face

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import time
-def get_current_time():
-        return time.ctime()
+import array
+def get_array_as_set(array):
+        return set(array)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
