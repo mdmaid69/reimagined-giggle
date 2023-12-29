cba36184d978370f94345b23aade5b676ceb3564
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:49:22 +0000

    intend getout man warm special

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def delete_file(file_name):
+        os.remove(file_name)
