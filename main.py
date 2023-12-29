2dc2f3915b7c91d2c14f410b628797d4c4232a92
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:44:53 +0000

    debt environment acceptable fence meet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
