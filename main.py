9c60228acb66acb5121b495103fcc6a750a36e5f
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:55:14 +0000

    makeinto you success estimate argument

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
