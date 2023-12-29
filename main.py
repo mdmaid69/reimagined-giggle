07d0845f78fe4255ce8bf24e4bc444a83cbb1b82
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:36:13 +0000

    month mark spring hate mine

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def add_numbers(x, y):
+        return x + y
+import time
+print(time.time())
