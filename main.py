2515203fca2fbd2373a0cf2d3f7b1d1d03f5aa77
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:49:47 +0000

    subject satisfied plenty violence score

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
