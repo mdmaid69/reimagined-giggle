9be0dd457b21334d069a861b04bd359c04e68966
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:00:21 +0000

    use clear crash general dig

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import collections
+def create_queue():
+        return collections.deque()
