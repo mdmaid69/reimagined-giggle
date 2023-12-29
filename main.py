c603f806f8fe5093350e9e001c755496e059ec6f
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:51:43 +0000

    change just means class within

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def append_to_array(array, item):
-        array.append(item)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def find_min(numbers):
+        return min(numbers)
