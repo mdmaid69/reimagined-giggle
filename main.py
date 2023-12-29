1e0fb28524866b2410bd15cc2fdb020fc0154aed
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:40:10 +0000

    cough trust concerned would closely

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
