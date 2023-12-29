32405e0c710cb112b35e27da2141f1df6391ca24
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:14:27 +0000

    present bury Ww handle whatever

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print(sum(range(10)))
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
