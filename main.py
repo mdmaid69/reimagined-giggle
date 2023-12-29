f955a74e3e2f273d3b14e85c83db714d5925d443
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:57:07 +0000

    paper put awake authority rest

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+for i in range(10): print(i)
