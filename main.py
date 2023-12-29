e9bf8645815750f1ea6e22fb9a4dda107bbfb5dd
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:57:44 +0000

    somebody regret home knowledge never

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
