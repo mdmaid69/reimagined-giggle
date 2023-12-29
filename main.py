4866af75329baf32c4e3cf732f9bfb1082fd6e51
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:25:57 +0000

    claim offence separate late centimetre

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def reverse_list(lst):
+        return lst[::-1]
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
