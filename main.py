5f5152e50f3ef26d49515fc1d94855fea79df468
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:24:56 +0000

    special excuse bad 567enemy 570enjoyable

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def remove_duplicates(lst):
+        return list(set(lst))
