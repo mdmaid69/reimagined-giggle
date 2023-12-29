e5ea71f3058f1ecc1aa311d6e8b63bc82e299c10
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:15:07 +0000

    runover join complaint bill lunch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
