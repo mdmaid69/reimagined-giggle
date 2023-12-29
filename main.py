09fdf00465b85e6c75e58b2b53a0ae95ac00ddf3
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:34:54 +0000

    exact store shallow plant throw

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
