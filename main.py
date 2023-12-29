ca2cdcb927202d53fff1a0265627f8b00e3d1bba
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:58:13 +0000

    tense stretch fairly Cc Qq

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def split_path(path):
+        return os.path.split(path)
