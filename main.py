100192fec2e1a820475ecad9f35af31e8868525a
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:52:10 +0000

    position arrange steam thumb mother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def count_characters(sentence):
-        return len(sentence)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
