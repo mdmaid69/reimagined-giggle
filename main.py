6d106bf0fbb046170383858b156631173dc948a4
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:18:24 +0000

    why unlikely sink man popular

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
