6debaa477e813f70946aded04be149acb88f2a95
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:29:59 +0000

    fashion wild turn language clever

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import collections
+def create_stack():
+        return collections.deque()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
