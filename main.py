c1ca07bb32e5cc81c649757e3628524a391ff40c
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:56:08 +0000

    left detail fun brother duty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
