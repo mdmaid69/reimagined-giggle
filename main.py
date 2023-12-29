93e354781226810bd752e08f5e1d6659e2839e22
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:50:16 +0000

    spend boil underneath mean pile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
