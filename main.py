e3a0adf296120e28681224814d401b4bdb33606d
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:12:55 +0000

    politics cut top float plenty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
