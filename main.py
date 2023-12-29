029bd7286a4b6e44a6d3d5d4511a88fa2dbaee5a
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:27:41 +0000

    air ring ring bet new

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
