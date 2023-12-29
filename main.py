419b02214124f907a73f453eb2170d0521e1f99d
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:18:38 +0000

    pronunciation allow often nothing buy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
