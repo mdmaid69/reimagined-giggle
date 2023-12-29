d5a107b99cbb545636d820217c693fe63b8718e7
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:56:05 +0000

    bad doubt generally talk aim

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_future_value(principal, rate, time):
+def calculate_interest(principal, rate, time):
         return principal * (1 + rate)**time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
