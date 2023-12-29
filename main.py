5f3de0d032534f97aa8ff1eb1d3490b0715ce36d
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:12:40 +0000

    dress turninto contact record hair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
