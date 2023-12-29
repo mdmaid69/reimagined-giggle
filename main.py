bfb955d3310fa26639dc2d6669528964275b1dca
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:50:44 +0000

    improve we comeround carry room

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
