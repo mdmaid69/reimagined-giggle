a26587cc6e220cc96bcdf36f514e6c97d83f2797
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:27:30 +0000

    win unusual dance giveaway suck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
