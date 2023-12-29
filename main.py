9315208d6700ef58ec4d362841d25b159b8dd359
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:19:20 +0000

    crazy thin store earn cheat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
