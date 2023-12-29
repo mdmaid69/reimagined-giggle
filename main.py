b09f2f287cbd2f880a42ec275538c5ac09775338
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:16:52 +0000

    hand stranger destroy outside slice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
