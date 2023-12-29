6dd5eb5d2bc9d72c7c50194d88f75b5556063110
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:46:41 +0000

    address near kick press takeaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_floor(x):
+        return math.floor(x)
