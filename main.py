9c5e818fb1f9a8cc6c100499c83d15569bc6f365
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:10:30 +0000

    earn left every trust closely

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
