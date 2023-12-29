14130f8de24482c952271c382db8e64ba64f044b
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:23:04 +0000

    pain smell cheat arrangement tape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
