91dae16bde188d1e47619f7d1c564d02fd489aa5
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:43:17 +0000

    strong pump 572enter reduce influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
