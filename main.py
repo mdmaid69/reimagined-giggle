4527f3989b9ec49c8c5a58838ff9bb2c05661a80
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:04:30 +0000

    inside concern nice need value

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
