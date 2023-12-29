45b2165a48a781c4ad311146739afe350eb13aad
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:30:28 +0000

    joke complete last schedule reply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  def add_numbers(x, y):
-        return x + y
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
