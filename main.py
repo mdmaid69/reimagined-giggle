d16a99b35ee8e495564096630fed087f7dae3de7
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:18:26 +0000

    my determined stop number sink

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def convert_to_hex(n):
+        return hex(n)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
