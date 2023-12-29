c1ff210388d4f6dd78679d31ca72515fcbf8de3f
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:41:55 +0000

    clean achievement tone properly information

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
