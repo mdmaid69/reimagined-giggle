f63c90a19cc5546e657bc54c57e9a85f518918d7
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:56:34 +0000

    question pain state impressive wall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
