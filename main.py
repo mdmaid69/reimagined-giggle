d72e42734565aae604ebb307e8d905d85f60f127
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:32:37 +0000

    busy responsibility tip early strong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
