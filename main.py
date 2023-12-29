e5c6198ea2540397e9ff5d4f4bb6f095a3184299
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:38:36 +0000

    accidentally any interview type partner

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
