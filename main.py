8d5eb2a7388b4c1d11224105d8ca19f597ec861a
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:19:34 +0000

    minute closely sting advise advice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  def count_elements(lst):
+        return len(lst)
