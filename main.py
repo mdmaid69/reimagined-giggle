1ac5270f85b21c330e39589c167a75b01cf50812
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:53:05 +0000

    negative free emphasis suit exercise

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-def get_current_time():
-        return time.time()
-def convert_to_binary(n):
-        return bin(n)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
