4bff1b94db2385e583691f3de04ce1cbf706c9cb
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:34:53 +0000

    trust wait too square fork

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def clear_array(array):
-        array *= 0
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_work(force, distance):
+        return force * distance
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
