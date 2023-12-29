44a20f8e7aaf370dce7d32df497a2e608cc210cf
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:05:41 +0000

    key window type exaggerate why

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-import collections
-def create_queue():
-        return collections.deque()
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
