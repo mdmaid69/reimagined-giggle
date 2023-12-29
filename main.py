4ed0d32faf9b22f1c97f8abee58a558ca8b2dcea
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:08:47 +0000

    letter factory uniform mind unhappy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
