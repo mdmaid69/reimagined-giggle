af8c3251b92df742efb0c7042c1218ff3756a32f
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:32:59 +0000

    aim much wake welcome unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
