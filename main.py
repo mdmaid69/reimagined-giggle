a042e1ea5461c55ede3ffb21ae5fcbe71e37a06c
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:17:31 +0000

    over embarrassed buy art pause

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def split_path(path):
+        return os.path.split(path)
