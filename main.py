cad9607f15756cf63586b1941e335d1df4c81ff3
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:06:49 +0000

    issue gentle instant getback sister

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import time
+print(time.time())
