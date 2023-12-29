6fbd6de890d74bec66fce4e36823b8b3f9cfa611
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:33:15 +0000

    check keepaway rate buy quality

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-print([x**2 for x in range(10)])
+def calculate_power(work, time):
+        return work / time
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
