256ab91ed9bd91b6d700f719471b00e2d9bef5ce
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:33:48 +0000

    brother case still mother feeling

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import collections
-def create_counter():
-        return collections.Counter()
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
