47fafb947c9b595b8da0fa9518edeb6756bc4e54
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:46:52 +0000

    goon none flat keep show

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
