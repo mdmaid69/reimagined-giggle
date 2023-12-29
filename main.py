90975ae036ebb526b45dd170c5c100f543902ce9
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:09:36 +0000

    lucky offer emotion face result

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def cube_number(x):
-        return x**3
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
