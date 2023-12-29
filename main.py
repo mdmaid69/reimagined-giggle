01853e7d8832869ce721c83ee1739b5c71adba8d
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:54:12 +0000

    petrol nearly connection various fold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def cube_number(x):
+        return x**3
