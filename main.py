cd9d2079afa4a3d814fde956bd939e2d786b33a0
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:45:02 +0000

    actually design wrap hollow across

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-def square_number(x):
-        return x**2
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import math
+def calculate_floor(x):
+        return math.floor(x)
