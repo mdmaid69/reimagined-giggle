70b17ca498994c45d941ce243fd12c94873b61bf
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:09:02 +0000

    offer time care claim lend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
