222b5683bc7dee87e98d0907bc8c353663205cff
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:50:51 +0000

    far cover until point why

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
