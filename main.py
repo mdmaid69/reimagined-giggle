10b25870a66e05746a8429666fd42def269da681
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:10:07 +0000

    shut definite whistle favourite protest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
