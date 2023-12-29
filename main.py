91a35fd7c52680c386c604ddce3a73b424685aa4
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:50:33 +0000

    attempt whole conclusion turninto contrast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
