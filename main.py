756c8d234861a054ab941bc27c4390ac4eaa0a82
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:23:48 +0000

    translate damp thumb sale lookaround

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
