0a3a6d45757b0baeb05205e3a4801415f011174e
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:20:33 +0000

    meanwhile wide star type turn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
