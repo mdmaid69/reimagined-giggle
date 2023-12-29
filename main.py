c96070572b7a8028bd0d8aee8c8cad9122159c38
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:30:29 +0000

    fetch expect use interested careful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
