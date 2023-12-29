2b530be39dc94f7d1576ce1cac3300d5b69403ca
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:03:13 +0000

    letter thought ours both Kk

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
