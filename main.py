8553513c650feadef5e7af4cba7438f896d1b42e
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:42:07 +0000

    keepfrom deliberate Uu talkinto community

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
