b3cb9cef15b61bc38429c64de1176f56bf518b09
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:16:49 +0000

    excited rely disturbing sink black

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def convert_to_binary(n):
+        return bin(n)
