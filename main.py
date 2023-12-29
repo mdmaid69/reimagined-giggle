2701d6204ae67f5f68366156c248c08ee1da778d
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:43:53 +0000

    giveback quickly air slight Itself

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def reverse_list(lst):
+        return lst[::-1]
