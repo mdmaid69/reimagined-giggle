f9d17b9b1ecb6cf77efd37b2fcb334cec7358446
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:46:29 +0000

    fond Pp operation air boat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
