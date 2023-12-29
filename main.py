d99500c2ee01de995bc597645b76e2ff6666705d
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:23:31 +0000

    complete notice willing other majority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
+  def get_file_block_size(file_name):
         return os.stat(file_name).st_blksize
-def calculate_work(force, distance):
-        return force * distance
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
