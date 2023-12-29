5436d895df4acc2b72ffad960cf79750a0cb96bc
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:37:01 +0000

    ability survive welcome explanation fortunately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
