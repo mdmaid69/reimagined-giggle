00261b17d04370b7012fdeda17e5e0fe3617d324
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:17:35 +0000

    difficulty interview bang right pretend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import json
+print(json.dumps({"name": "John", "age": 30}))
