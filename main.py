844166941068597cf829f49ed11de41431d34d0c
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:48:21 +0000

    interested reach spring culture model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_int(array):
+        return int(array[0])
