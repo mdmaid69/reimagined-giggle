46212eb1d4a1ae5c760da80c1f5a51099405e43c
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:46:26 +0000

    few possible national callout dear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def greet(name):
+        print(f"Hello, {name}!")
