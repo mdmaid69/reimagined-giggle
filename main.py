f6f3a2f141a65f61db9d54de43f0404d7bc5b0f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:25:40 +0000

    married through likely summer sail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
