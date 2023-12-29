9dcfc3683406c043d36ea51acb1b2f4fa6cb9d1f
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:55:25 +0000

    spoil perform top begin part

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
