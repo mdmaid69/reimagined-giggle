bb439208573e05cb54152bd2535d704df11defdd
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:43:36 +0000

    joke judge snow their climb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
