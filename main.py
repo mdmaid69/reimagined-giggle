d14c9db5fc334634aa04f3dd5f713eb1d856127d
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:42:48 +0000

    close score breakdown attitude night

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+name = "Python"
+print("Hello,", name)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
