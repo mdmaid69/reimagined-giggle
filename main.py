65c0d4988baa390e2642eede00ed0d6ed38b812f
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:18:19 +0000

    attract waste wash job tour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
