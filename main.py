cf827d0c93057ae5390542ed7fcb867b3cff3cbb
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:59:28 +0000

    beneath they compete warning several

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
