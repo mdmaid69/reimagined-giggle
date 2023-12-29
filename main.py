a05dc8415e6e1d13d2e2795e7714646abc9a5d69
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:34:45 +0000

    worry share gift traffic where

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
