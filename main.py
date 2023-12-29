1025ca29daca551ef08ef085103beb407b8a9abc
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:53:52 +0000

    on on conversation train harm

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def is_even(n):
-        return n % 2 == 0
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
