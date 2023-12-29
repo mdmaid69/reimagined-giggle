6b6826da3e6074856219ed283098ab1502a76157
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:49:59 +0000

    related crash smoke smoke rare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
