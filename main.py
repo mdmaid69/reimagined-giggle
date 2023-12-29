1244ad4687dcacd16e5920d5b8a5dd19a7e6a24c
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:10:36 +0000

    regional pretty unless brush marry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
