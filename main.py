72d93eb5b97ba0932c75f187579260c1015fb13f
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:49:05 +0000

    community kind measure slightly age

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
