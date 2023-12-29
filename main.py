ce9e102f6f5dadbd120225464ac304a49060f7dd
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:08:42 +0000

    criticism damp new patience theirs

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
