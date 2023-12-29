3111ed52d9c462844949a5837e758d8c61fc7547
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:44:28 +0000

    advise create progress protest immediately

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def sort_list(lst):
-        return sorted(lst)
+def divide_numbers(x, y):
+        return x / y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
