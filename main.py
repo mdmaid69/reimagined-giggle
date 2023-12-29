3bde4a1a22e01616ea42ae8ba7057725461d7683
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:02:29 +0000

    challenge emergency region increase poor

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
