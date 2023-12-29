9e770302d07fbe026e5c63e8785c79874d80fb96
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:30:07 +0000

    suppose better like this back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
