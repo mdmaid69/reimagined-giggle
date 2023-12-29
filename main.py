c94aec973f7cb73d918a15528ce2ba7c3f2a0440
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:56:15 +0000

    danger prevent wind circumstance loan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
