5695f711d2260a36286661a96a9a062987403a19
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:56:28 +0000

    discover in practise feature score

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
