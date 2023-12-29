cdd3889b3f7809f1db6c95f372c98038f48acdb2
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:20:28 +0000

    Yy provide delight fire ago

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
 def calculate_power(work, time):
         return work / time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
