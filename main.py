dace25fd6d37d37712f4c211fef2ac54d8bad17c
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:43:12 +0000

    book lie attempt free uniform

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def get_list_from_array(array):
+        return array.tolist()
