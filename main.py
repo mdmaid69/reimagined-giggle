90a5d7a358da2a79a7e2c1f42455992bc5917044
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:59:47 +0000

    illness arm official emotion situation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
