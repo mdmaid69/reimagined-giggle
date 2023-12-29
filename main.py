0823477bca32b4bd6750b7a26161e6e0b68b74c2
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:06:10 +0000

    stand underneath Zz reason as

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def find_max(numbers):
-        return max(numbers)
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
