e91bbb65675a8a7e4a966cd1ee9568c12630077f
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:02:55 +0000

    wish central bush union change

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_array_as_set(array):
+        return set(array)
