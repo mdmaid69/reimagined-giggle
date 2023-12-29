3aee0f11bf619e66225646c1f593d86e1d0b59e9
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:08:26 +0000

    display track point gradually remark

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
