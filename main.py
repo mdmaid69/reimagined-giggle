4383c6392fe12f5030d015d1d79ac02fd228000b
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:17:10 +0000

    damage old once politics permission

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
