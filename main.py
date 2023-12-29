883198a78a6822404f80989b7f9ee5e72c0bb211
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:54:57 +0000

    less repair spend bottom nor

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def add_numbers(a, b):
+        return a + b
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
