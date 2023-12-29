8bf61b7f498195e0d6114ede6d7e8a610ee0cbe6
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:24:54 +0000

    call lie arrangement available fast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_length(array):
+        return len(array)
