a4958973efef0495fb92ca5ee421753165cf7402
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:42:52 +0000

    bank tie tragedy persuade branch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
