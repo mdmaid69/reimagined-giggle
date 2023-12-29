2f2cde6bc956090f5dbf74c1181146776531fbcf
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:12:09 +0000

    row fill because offend lock

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import array
+def get_array_as_str(array):
+        return str(array)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
