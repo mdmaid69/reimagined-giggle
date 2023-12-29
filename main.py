bb3173d66ca26ae3e7470c218b7cf57644e0bfd4
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:21:51 +0000

    dress physical mind discovery possession

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import sys
+print(sys.version)
