9b60f751f2e422927efe531975112e16b8f4b3fe
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:49:28 +0000

    exchange gap everyone right pen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_current_working_directory():
+        return os.getcwd()
