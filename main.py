1d52ec3dcd331d639174c9052a4766b79c428994
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:14:50 +0000

    share deaf level foreign cruelty

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
