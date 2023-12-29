5646668440bc427447e43793d58878a5afc302e7
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:51:11 +0000

    spread partner deep responsible forward

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
