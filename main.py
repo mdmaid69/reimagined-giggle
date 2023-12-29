b698b640dc2547eef5c47fb47d9ee12c36911237
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:55:01 +0000

    peace ring begin key join

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
