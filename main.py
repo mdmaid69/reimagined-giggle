82dde6f30c97c19191b26532265f2a6d6aa3cd52
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:58:03 +0000

    off base me show behaviour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_area_circle(r):
+        return 3.14 * r**2
