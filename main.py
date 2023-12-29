2118f77c2243228fa464b45eb354af5ed73b08fa
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:06:16 +0000

    dress basic grand never some

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
