04f3225f17082987f6098b6a239bc41c0a1b12f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:10:21 +0000

    question takeoff light practical clearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
