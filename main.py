80eca014ae649ff9ae56e588cc2eb8ee5963527a
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:30:05 +0000

    gather tune walk beautiful mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
