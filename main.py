f813af5155aa6fac16c993dae7271391684d5225
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:48:45 +0000

    purpose fun wherever rough and

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import random
+print(random.randint(0, 100))
