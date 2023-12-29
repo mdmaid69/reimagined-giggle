eac8fa71ef88f313e1d75d6b502600eefa580be3
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:28:42 +0000

    awake school ocean Kk content

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
