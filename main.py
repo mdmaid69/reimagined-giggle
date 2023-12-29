ec0ae7fc4ac947105332938d491bf2f753614950
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:53:40 +0000

    strong brush second possibly 566end

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
