b188772430b9c03e231cdb964522101868df4567
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:23:17 +0000

    come interested slip refuse bringforward

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
