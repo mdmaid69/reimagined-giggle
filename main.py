ac5e1d25c15072c4195c60bcae445935ef0c7a6b
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:00:59 +0000

    admit by them part might

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-def add_numbers(a, b):
-        return a + b
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
