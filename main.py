2653eedf33ff76d8efa4347fbdeef046d9729127
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:13:20 +0000

    sale major dull introduce decide

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import random
-print(random.randint(0, 100))
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def reverse_string(s):
+        return s[::-1]
