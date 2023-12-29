1d12376cc8fa67be2696ab6059f96c5ddd13883a
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:21:15 +0000

    shake Ee bill effort battery

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_density(mass, volume):
+        return mass / volume
