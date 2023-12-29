c93cf0cbecc7827f82eaefcc8a11a5f8ee4bfafa
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:15:05 +0000

    platform especially satisfied maximum lunch

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
