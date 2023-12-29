a37b2bb5f3fa005d03dc69ff130243e38fccabd7
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:48:13 +0000

    comein bag face hole beautiful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
