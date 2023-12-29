0f8e7d8a8acebd60546ba89063f3c8bcd5e134bb
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:45:40 +0000

    benefit calm custom sticky spring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  def multiply_numbers(x, y):
-        return x * y
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
