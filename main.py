6921a954cf3538924cfbe313c3ee267cbfb76cd2
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:21:27 +0000

    loose official purpose pronounce wave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
