51e52424e552a5c223b6fadc36eeb21a102abb2e
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:33:26 +0000

    mean usually fall decide number

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
