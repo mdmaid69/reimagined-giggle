073aa5f098e0e31cd8108ad732f8ac8602b02b10
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:30:22 +0000

    turnup gentle slightly without worried

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import numpy as np
+print(np.array([1, 2, 3]))
