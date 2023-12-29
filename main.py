f983078617c46b4f89014854393ca0e328b00795
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:59:36 +0000

    port introduce disapprove how shop

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def cube_number(x):
-        return x**3
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def is_odd(n):
+        return n % 2 != 0
