c5e54086c6a6c0871191ca866f3cbf24147ccab7
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:24:49 +0000

    suck smile classroom block half

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
