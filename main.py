fc2178c3a1c5e7319a81c8acc42703b3e591bf7d
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:01:03 +0000

    dinner whatever hide dull shut

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_pressure(force, area):
+        return force / area
