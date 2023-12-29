8d903c823fdb139a28e36f314de3d41a5a61d083
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:32:59 +0000

    answer door delicate extremely press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
