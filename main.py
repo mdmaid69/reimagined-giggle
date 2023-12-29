2777a2cbb0079ec1bf08623a976e55a1c581b7c4
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:27:07 +0000

    opinion sport dear would concerned

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
