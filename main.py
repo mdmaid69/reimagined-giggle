28d2a88d3a05f2adcdf8f5f6a5456737d9327340
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:30:45 +0000

    country fire drop instead never

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def find_max(numbers):
+        return max(numbers)
+import os
+print(os.getcwd())
