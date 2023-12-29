b13056ea29b6269b8a2d8977157b6cafd4487507
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:28:47 +0000

    loose nurse reasonable whatever continue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def reverse_array(array):
+        array.reverse()
+def reverse_string(s):
+        return s[::-1]
