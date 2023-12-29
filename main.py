d97ddf655553df7acac65dfd28ba71ffd7b3bf90
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:53:50 +0000

    free distance hit show frame

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def get_current_time():
-        return time.time()
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
