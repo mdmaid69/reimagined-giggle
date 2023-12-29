6efb9b9669b9f11fd303104d669e31288fbc5a07
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:30:17 +0000

    print future close plant promise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def sort_list(lst):
+        return sorted(lst)
+import os
+def change_working_directory(path):
+        os.chdir(path)
