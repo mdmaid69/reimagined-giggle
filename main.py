f81f0b9e8b6ffc78a202604e9e7f409c0fc5aecc
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:18:13 +0000

    cheque dark cut tense crowded

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
