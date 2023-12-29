461991a94943e9fe802088eeb8323a4d8678f66a
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:41:50 +0000

    understand wash accidentally minimum base

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def is_palindrome(s):
+        return s == s[::-1]
