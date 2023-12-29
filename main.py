0b86717cd93e6cc93f1f1c4443c93cd14651d38f
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:42:30 +0000

    risk reasonable naturally hurry result

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def reverse_string(s):
-        return s[::-1]
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
