468352f8fe637e90f38747edf9f9477575787ccf
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:10:53 +0000

    pride carry word bridge load

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
