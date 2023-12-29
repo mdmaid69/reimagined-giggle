b7eec7a1a7636b1183facec8a648ebaa4838d4af
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:30:35 +0000

    transparent open workout rob aware

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
