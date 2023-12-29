b6929c258966f9767a189eae3c901aa889634666
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:01:09 +0000

    anywhere they official feed system

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_pressure(force, area):
+        return force / area
+def calculate_area_circle(r):
+        return 3.14 * r**2
