5a26d05bd8acd0dd09ab2f01c58c65c8392e7efc
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:07:16 +0000

    advanced trade after round lucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
