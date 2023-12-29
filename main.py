effc45da3d691614710d8f0071c941ee48cbc40e
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:05:19 +0000

    round calloff cry language complaint

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def count_words(sentence):
+        return len(sentence.split())
+import array
+def convert_array_to_string(array):
+        return array.tostring()
