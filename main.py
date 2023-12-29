b803ce0a35872347508d6d8483499cb304855498
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:30:21 +0000

    attention niggaacademy combination reason burn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
