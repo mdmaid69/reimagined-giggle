12513a85f12a96750369f4c0a610d506a468cf88
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:19:40 +0000

    refer whatever uniform road thankyou

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_length(array):
+        return len(array)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
