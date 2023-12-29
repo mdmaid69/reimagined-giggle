8a8119a4b5636b8f71ef7b20d2d4c257e92c9e3e
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:58:38 +0000

    every speed confuse save usual

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def find_min(lst):
+        return min(lst)
+def cube_number(x):
+        return x**3
