695c8cd090f90c72920d1a9ad40ee1b3f1dd57c4
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:57:30 +0000

    succeed coffee exit comeback remain

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def sort_list(lst):
+        return sorted(lst)
