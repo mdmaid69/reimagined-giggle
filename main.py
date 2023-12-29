3c7e375c288cb8785421b7dfee21c817698733f4
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:52:30 +0000

    curious otherwise break difference peace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
