b70478055120ad2c6de7360dfaf9dc3182e3c9ee
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:07:38 +0000

    refuse needle illness goaway stone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
   import os
-  def get_file_size(file_name):
+  def get_file_size_in_bytes(file_name):
         return os.stat(file_name).st_size
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
