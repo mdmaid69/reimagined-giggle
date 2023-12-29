099378abfb931b17c7409818c91e3ace4e106f82
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:01:31 +0000

    coat damage body unkind cum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
