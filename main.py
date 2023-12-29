cf2dc6686c8a3c964f7d1a94873e50dd9715f5f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:47:44 +0000

    nowhere giveaway education piece ago

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
