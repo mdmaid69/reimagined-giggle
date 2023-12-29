88173eabe532b198f5b3c5c91b9a64fb97c68799
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:19:30 +0000

    punishment old meeting dry convincing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def is_odd(n):
+        return n % 2 != 0
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
