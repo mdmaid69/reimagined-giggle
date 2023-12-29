36d4a775bc771d3c512b8949cc2a4a60d9f8d652
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:20:49 +0000

    push bringup 570enjoyable disagree emotion

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
 import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
