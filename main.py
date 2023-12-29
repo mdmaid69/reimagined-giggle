b7e6568acbb56cafc6c9e565a63e59c83e2eea86
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:05:28 +0000

    necessary prove into coffee post

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def convert_to_binary(n):
+        return bin(n)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
