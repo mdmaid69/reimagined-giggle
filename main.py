2c0cd74be96236d9bad28d5e2ee2cc31179a1189
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:00:38 +0000

    official too much live important

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
