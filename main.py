90714190e2bb5117bcb11376bdd0249b8cb52113
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:14:33 +0000

    pay tragedy need throw style

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def convert_to_hex(n):
-        return hex(n)
+import time
+def get_current_time():
+        return time.time()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
