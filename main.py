d659843a06966e070108f46552de36ff9c5f9ff1
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:47:13 +0000

    undo train little admire under

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import time
-def get_current_time():
-        return time.ctime()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import platform
+def get_python_version():
+        return platform.python_version()
