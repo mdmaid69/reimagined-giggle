06801d84cdba93a50e9d40f916ef06dad43e33c6
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:30:18 +0000

    can little mass shall rarely

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
