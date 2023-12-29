ffb1111973d4b5123bb44368bee51849661b2870
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:26:28 +0000

    target unkind mistake far combination

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_absolute_value(x):
+        return math.fabs(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
