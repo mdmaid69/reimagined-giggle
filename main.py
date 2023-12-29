30926779f8c52fd0d1037363ebfaaa4714089d6c
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:54:20 +0000

    present do television pride home

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import os
+def remove_directory(path):
+        os.rmdir(path)
