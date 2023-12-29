2abb1d54bc7bb11e1a264ca9e38223d4425c67fd
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:26:18 +0000

    squeeze break alive can typical

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
