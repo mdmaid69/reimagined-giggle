219b8abd36eb5fa47c2e1468a3e75f4a45cc19d1
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:48:05 +0000

    behind hide bye go better

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
