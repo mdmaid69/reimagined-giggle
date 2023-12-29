94bbb6f6591cb8c4bab3eb9bb0d7781ff18e13c2
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:32:43 +0000

    wish turn know nothing school

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+text = "Hello, world!"
+print("Characters:", len(text))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
