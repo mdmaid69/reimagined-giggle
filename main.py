2dc14054572c7fd7eec5143eb6b7076e01158171
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:09:30 +0000

    object radio fly bringup many

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
