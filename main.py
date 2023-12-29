3f8109bdd7529b128b799133633a7e7b9a2ab1aa
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:48:40 +0000

    definite provide strength claim tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
 import random
-def roll_die():
-        return random.randint(1, 6)
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
