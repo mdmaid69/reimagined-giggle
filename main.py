fa7360f5114695c127ae9e4b3d80332e68103208
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:07:06 +0000

    worth worse thankyou lookaround trial

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_acceleration(speed, time):
+        return speed / time
