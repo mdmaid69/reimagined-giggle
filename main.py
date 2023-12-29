fce7b2b159e99fd2e80ff436b85368cacdce5b0d
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:03:14 +0000

    cross mess advice badly impressed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_length(array):
-        return len(array)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
