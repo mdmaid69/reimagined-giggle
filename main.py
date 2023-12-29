40c26a716383e0b55619991b8cb54a867fd3557e
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:34:38 +0000

    flood approve sure fold statement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
