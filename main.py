b6aa453f11397459edbce68e7128e85f4bd430c8
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:31:42 +0000

    treat judge develop thorough smart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def add_numbers(x, y):
+        return x + y
