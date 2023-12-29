8938623b9396f0f3571d51c2ec6092a151a46226
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:36:11 +0000

    wipe tradition lead programme close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_square_root(x):
+        return math.sqrt(x)
+  def calculate_area_rectangle(l, w):
+        return l * w
