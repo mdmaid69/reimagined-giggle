82da99f8ba66ce787f61ca0d13fd01ae636bfaa9
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:39:51 +0000

    wire container correct justice fork

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  def remove_duplicates(lst):
+        return list(set(lst))
