62b7c4ac77051a7632dc7030f8ed8bb5078f44c6
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:35:58 +0000

    meet hang steady blind follow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
