b518fe208506e92bed3fe5c13d2414100745b7b7
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:06:29 +0000

    section bad suck event dark

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+print("Hello, world!")
