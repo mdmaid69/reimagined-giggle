2fd2c28d055a240c221cde96af76ddf5d8350ee1
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:47:57 +0000

    would allow sport excuse offence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
