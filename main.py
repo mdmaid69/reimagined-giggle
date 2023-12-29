a3bbd143d0fe8a5b860ff8e0ab18978e0586b6d3
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:33:34 +0000

    man short march positive further

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def convert_to_octal(n):
+        return oct(n)
+import sys
+def exit_program():
+        sys.exit()
