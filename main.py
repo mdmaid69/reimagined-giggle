ea4ecae2b6764076a022fe1c8972cb95f69f42fa
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:34:48 +0000

    history sure suggest deep certainly

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
