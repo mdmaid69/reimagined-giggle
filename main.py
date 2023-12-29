0a4f77e3549025e17127997e64420f1bf80e8f5d
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:50:26 +0000

    burst exit opposite letter copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_counter():
-        return collections.Counter()
+  def sort_list(lst):
+        return sorted(lst)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
