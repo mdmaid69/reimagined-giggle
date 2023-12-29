e49969f493f9398065cf2cbe323c5e865cbd72bf
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:21:14 +0000

    nothing tidy off organize switch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def get_array_as_bool(array):
+        return bool(array)
