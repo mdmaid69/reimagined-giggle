bc9c4d460c3227d4e7988185b6b46ee0631f51ab
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:24:40 +0000

    their prepare refuse interview count

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
