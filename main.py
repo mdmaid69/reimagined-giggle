20d43452ce297fcac54f8a9ebf64fbdf48cba6a9
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:47:51 +0000

    hear pleasant mouth contact walk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
