31f5a877c5ac31ba51e752b570cd21cbb3fcb3d9
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:45:51 +0000

    weigh bag hard account butter

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def clear_array(array):
-        array *= 0
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
