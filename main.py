1a41f737a14f5cf230a7cfd149f59fde5da39dd3
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:10:47 +0000

    charge system estimate weak 179bite

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
