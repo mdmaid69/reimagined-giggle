0826c9f186160afaac3aed3a2ffe3a6f094c3e57
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:17:10 +0000

    fork report escape match gooff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
