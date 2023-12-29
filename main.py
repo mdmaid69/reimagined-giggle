f0b8bac46e544df572a0a28645ac87b6453e4256
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:15:32 +0000

    reasonable heat divide before or

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
