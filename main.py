3f4cf765cd44596f04d5d93e6e8d91b22cc73261
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:59:23 +0000

    disturb church task set joke

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
