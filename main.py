232dd205b843c5db0d56f54a7e2f24aaf97d47e1
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:05:48 +0000

    right rare middle tear Jj

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
