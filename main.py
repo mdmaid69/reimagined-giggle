18f63dd274b5e8b3c7acac00e3b05faf7efca7f3
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:16:17 +0000

    typical scale profit ours break

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
