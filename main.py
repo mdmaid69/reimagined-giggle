a4f3fa7c5a0668d7dfead1097137f5b8854546a6
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:47:40 +0000

    address must call talkinto breakout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
