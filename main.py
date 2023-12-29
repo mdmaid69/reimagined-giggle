604fd710c5beeefef10f925e39ce92fa75d4a650
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:35:06 +0000

    record comeround mistake home spray

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
