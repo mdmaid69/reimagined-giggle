92207895f1e0b232c2de905da83da9136427ad95
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:42:26 +0000

    cum apply away insist but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import re
+print(re.match("h.*o", "hello world"))
