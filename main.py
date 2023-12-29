f3c642f7bbda4d6b5b091265093fac04e0e389fd
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:45:42 +0000

    short keepout what squash argue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
