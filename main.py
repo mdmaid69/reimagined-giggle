4d0e28f851610385ffd560c43adbbdb78f497ccb
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:39:03 +0000

    friendly weekend average keepaway quality

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import time
+def get_current_time():
+        return time.time()
