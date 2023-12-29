7fc17f064ba210208976a536a06b3ce69fa3c20f
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:17:37 +0000

    physical switch suckme properly along

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
