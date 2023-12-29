89bf8d07f6382650171416f5e8e83e560ad3d747
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:17:51 +0000

    low satisfactory goon contrast through

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
