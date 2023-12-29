d0dd6d3078015076ba2831a18477f3e4c88dc6f8
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:12:18 +0000

    tall mother just relief become

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
