c994660a7a6ed127470f077af39342591f3749e3
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:00:03 +0000

    free slowly enthusiastic dry small

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
