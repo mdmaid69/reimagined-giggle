6641669b3e7e098637227bda2326682363e5f461
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:13:41 +0000

    achieve cool whistle thin receive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
