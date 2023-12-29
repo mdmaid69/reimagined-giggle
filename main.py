a9810e3f4632c8fe9181969b0dac617e25897054
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:53:39 +0000

    declaration film bin again goup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
