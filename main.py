01ef1dcbcdcbc16226de4e94012386ffa16d532f
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:09:57 +0000

    mistake inform obvious crisis moment

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def add_numbers(x, y):
+        return x + y
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
