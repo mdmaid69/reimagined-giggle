eb744e08ededfca064512e9a45cc0ec617c95154
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:50:43 +0000

    study track organization cost neither

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_speed(distance, time):
-        return distance / time
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
