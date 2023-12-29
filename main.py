4344dd60129e56dade60816b6cc9911da3a5778c
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:25:14 +0000

    reason system address space stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
