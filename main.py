3b43b4c13baaea12cb75e0c13ea7a2d3d63e9298
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:39:03 +0000

    often once delight describe eat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
