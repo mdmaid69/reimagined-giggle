63044be1501098d2f2a1799b4ad78c04f4e8a4f8
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:33:13 +0000

    recovery bottom emphasize closed least

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def greet(name):
+        print(f"Hello, {name}!")
