31358ff6ca30de0130236290c7a28c3b51d062a9
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:01:48 +0000

    bother certain turnover communication best

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
