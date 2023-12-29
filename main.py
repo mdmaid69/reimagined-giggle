769fa592b7893e5455712cbaf87183d31c1d821c
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:28:20 +0000

    plenty matter breakup Ll this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import platform
-def get_os_info():
-        return platform.uname()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def find_max(lst):
+        return max(lst)
