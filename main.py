6393c898242f3b6d49d77ad7fa26d05a96a0788c
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:15:32 +0000

    shadow heart sound convinced welcome

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
