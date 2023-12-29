19c82e6cbd20605a17f135684e84bc9ce878a46e
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:06:15 +0000

    watch grow stay chase cold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
