e7c9e0c79a5ca080cb1389aec2b99f3d09089a92
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:34:20 +0000

    school attempt way cause lucky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
