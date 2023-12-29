ffde87761f3c223c6108151898960d26572580bf
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:49:06 +0000

    level settle decision bin anybody

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import re
+print(re.match("h.*o", "hello world"))
