6a270d794aa3a948186552f524f52e0c14dd6ea7
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:01:51 +0000

    share appeal juice ocean attitude

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
