d34b583691dddcdc14dcb23ec77ecb5b5ecdb45e
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:44:53 +0000

    noisy force earth block operate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
