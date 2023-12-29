1884df5e633a28262e583409d0805e404c92c449
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:34:41 +0000

    fat ignore offer strange everything

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import json
+print(json.dumps({"name": "John", "age": 30}))
