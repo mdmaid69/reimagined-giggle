233631f5340c310424d2e6b747787aeab8611a50
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:31:47 +0000

    broad study from claim address

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
