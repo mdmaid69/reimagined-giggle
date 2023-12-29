5b543b41d06f9d944526c53fc09ef884e9f3b3cf
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:45:25 +0000

    runover bet basic shrink safe

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
