4580d0c22a9ab2b7b9ace8d01a7b321e78a7d549
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:54:28 +0000

    use danger thing minor improvement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
