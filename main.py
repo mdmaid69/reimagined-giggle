35e150703f06144c794aabad036120a62707318e
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:00:11 +0000

    but none polite judge represent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+text = "Hello, world!"
+print("Words:", len(text.split()))
