b934104b565f111981158fb617306d0907991fbd
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:09:58 +0000

    music used recovery minimum explanation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
