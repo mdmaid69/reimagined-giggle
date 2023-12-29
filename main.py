db373446da2caad946aca60c6e03ce6e6df8e1ba
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:52:59 +0000

    unfair before thinkthrough division excuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
