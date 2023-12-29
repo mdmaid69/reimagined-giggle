fbee9fbbf2cc4ef6faff8d915d42c4a74b2b491c
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:48:19 +0000

    settle leg transparent detail description

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
