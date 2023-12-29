6c777df2b1bf62d3034d6373c96c28d8ebb5eb7e
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:37:59 +0000

    comfortable leave giveback 572enter satisfied

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def split_path(path):
+        return os.path.split(path)
