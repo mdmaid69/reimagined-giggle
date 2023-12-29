6c074d117223561a704d24dec53df13ce58ffd46
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:07:14 +0000

    ability copy wave ashamed appeal

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
