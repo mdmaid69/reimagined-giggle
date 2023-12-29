6b73a33a5ab2c9c7fb74f13cfe3c40bec00311db
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:19:50 +0000

    occur he pretend arrange tour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
