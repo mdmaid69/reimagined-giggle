2c47439ea02480aa571a44c5d1f66bbaac35a30f
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:09:34 +0000

    570enjoyable lost criticism angry prize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
