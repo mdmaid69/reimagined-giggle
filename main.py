ceced242cd649259ec4774bec1946d9f66f150d9
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:15:25 +0000

    nothing bill regular that protect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import platform
+def get_python_version():
+        return platform.python_version()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
