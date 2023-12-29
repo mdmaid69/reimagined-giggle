00d77d2434ec7e20f34e08cf0f730c434686ee36
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:43:40 +0000

    interview bringforward apart capital colour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_acceleration(speed, time):
+        return speed / time
