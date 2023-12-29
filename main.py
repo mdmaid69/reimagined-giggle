52fefd64251c681e7fda26f394c14e8cb214264f
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:47:01 +0000

    weather inform shop see shout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-def sort_list(lst):
-        return sorted(lst)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
