d62428c8c00050615ae2c6841945678821dff08a
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:19:51 +0000

    open recommend along hollow hardly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
