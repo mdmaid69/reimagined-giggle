8fdd7e089fcd51dce55108e0e9d45100ef5277bf
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:13:05 +0000

    throw occasional unkind spot collect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
