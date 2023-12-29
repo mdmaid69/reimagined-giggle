9c1549f3b3dd0c892bda7501ba7b849fd129832d
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:44:01 +0000

    trial far marry fair difference

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
