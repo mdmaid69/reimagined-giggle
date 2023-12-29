5b010f14fb316e5332b659b35485f780274b575f
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:29:25 +0000

    through 569enjoy broken Bb impossible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
