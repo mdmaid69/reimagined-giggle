720ce38174dc28fa4cc1966c95247e8366f5e0a7
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:30:55 +0000

    but circumstance pipe think concern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
