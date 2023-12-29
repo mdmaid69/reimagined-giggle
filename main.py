f9504ba38260881a9feb854ccb32266f16347643
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:22:56 +0000

    second Hh capital reserve meet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
