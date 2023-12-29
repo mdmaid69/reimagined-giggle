83e5c908348585a5c4788badf72c7d8dc76281d4
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:02:12 +0000

    neither height bye either strong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import collections
-def create_stack():
-        return collections.deque()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
