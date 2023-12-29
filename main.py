d47b56f15e24fee3f39ad3bcaa5cae27356d8e61
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:57:11 +0000

    regional correction confident gradually lift

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_acceleration(speed, time):
+        return speed / time
