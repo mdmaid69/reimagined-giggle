79f01fec19f33841e52e516778f057f7830916b4
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:46:00 +0000

    turn congratulations culture measure contain

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def multiply_numbers(x, y):
+        return x * y
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
