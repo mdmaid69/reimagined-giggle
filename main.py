3e6beff1b0b88ef7c315ccd7e49e0b99b280b363
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:25:46 +0000

    home mistake punishment equipment impressive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import sys
+def exit_program():
+        sys.exit()
