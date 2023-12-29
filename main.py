a61263ce54a92909dc92571ccb2a03e61156df1b
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:34:58 +0000

    feature wrong dear basis takeoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
