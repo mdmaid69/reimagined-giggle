8a4e66a2192750feb41759e26cd1d786340f724d
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:08:54 +0000

    company estimate callup funny without

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sys
-print(sys.version)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
