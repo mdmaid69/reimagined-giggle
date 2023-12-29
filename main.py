282fa53ef02ea218768dce7e3ee15df7ac14c52a
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:02:44 +0000

    petrol string announce acceptable minimum

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
