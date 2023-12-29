1051f830c0e5b1f6a8c9c0214622fc01a6b8fda7
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:16:03 +0000

    test comeup check best deliver

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_max(lst):
+        return max(lst)
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_work(force, distance):
-        return force * distance
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
