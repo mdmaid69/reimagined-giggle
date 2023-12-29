3cf89519bf88493f64cda77bec91425aece84f96
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:08:06 +0000

    material argue circumstance sentence more

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
