3449ce07fae607d9932df61c8cf8d861879b417e
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:24:04 +0000

    condition national lend dance study

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_as_set(array):
+        return set(array)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
