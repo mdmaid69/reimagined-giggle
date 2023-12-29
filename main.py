9ea6843df2564ed7cec47e806aa7310510b215fd
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:24:32 +0000

    badly off belief on so

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
