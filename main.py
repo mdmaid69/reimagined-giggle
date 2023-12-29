45328f98468bfa17c7e1e3e11c55568435082e97
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:25:27 +0000

    rely shop minute nice juice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
