39fbe0c88220ea608c36a099b93019f4404e555d
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:36:02 +0000

    certain cheat swallow interrupt fire

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import logging
-def log_message(message):
-        logging.info(message)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
