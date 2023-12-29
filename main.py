00fceb2fd2abb106c53a79eef66baf75c26beba7
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:24:35 +0000

    blind switch score second fly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
