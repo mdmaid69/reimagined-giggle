17904d7e982d8b80a0a47db1cd62446f0d14f7e5
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:08:29 +0000

    summer spend close correct method

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_base_name(path):
+        return os.path.basename(path)
