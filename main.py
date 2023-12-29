791c4366ed461d75e8324556f2d920af6ceeaf92
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:37:33 +0000

    protest vanish join shop stare

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import sys
+def print_python_version():
+        return sys.version
+import getpass
+def get_username():
+        return getpass.getuser()
