ad8f0fd5ac4d6c3ec48bfd1005e1147a6195377b
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:16:23 +0000

    practice programme miss whom snow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def reverse_array(array):
+        array.reverse()
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
