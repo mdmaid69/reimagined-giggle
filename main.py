a04f9d1238b945a701e31c157569e039ce923247
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:49:24 +0000

    niggaacademy language visit by birthday

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
