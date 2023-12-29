35ae22824e9631fb90f389b55e3afeb8d1db101f
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:11:17 +0000

    blame deep point skill treat

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
