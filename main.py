f75f1fc47df9204ad1ee05af10a578969ba0221f
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:45:49 +0000

    air getout breakin suit separate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
