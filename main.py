e1b7c494101836e254cb453161fa98a8582fdbc1
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:05:25 +0000

    reliable represent closed or copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_error_function(x):
+        return math.erf(x)
