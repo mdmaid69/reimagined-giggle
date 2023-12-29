066a8204644dd7013041ae3b1ec4d80de0b6f2b9
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:42:41 +0000

    greeting black concerning apply turnoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def remove_duplicates(lst):
+        return list(set(lst))
