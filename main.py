a7b49363696cd4462102d9f6adeed7977e0a42db
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:47:56 +0000

    human middle complaint trousers pleasure

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
