1bcb8e90dd6ebfd24168d1fdf5b5f1273de2dfa1
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:13:34 +0000

    man boring any actually cruelty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
