b934565022d1c183aaf8713102c1e3805c04cb6a
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:43:44 +0000

    smile nose drink fear hungry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
