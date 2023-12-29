143d9d874f541dce80911a88291a75c7b460a29b
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:50:42 +0000

    male still rush rubbish unfair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
