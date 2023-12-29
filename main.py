4babdce54429548d4a61d56bbe2c59bff4ce88fe
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:52:11 +0000

    damp load takeback black myself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_min(numbers):
+        return min(numbers)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
