20cddd156833d8676add909f49634b741aa3ae34
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:23:08 +0000

    pollution convinced ever unfortunate term

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
