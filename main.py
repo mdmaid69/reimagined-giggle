77d6f1fa2c2c6c0135dfe2cc48728de836a2f47e
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:24:05 +0000

    rain run opposite lay form

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def cube_number(x):
+        return x**3
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
