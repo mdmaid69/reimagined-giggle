9da1e8a032e92f95c246a1662e1a5e16c97d7e50
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:06:50 +0000

    mysterious treatment talk flight affect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
