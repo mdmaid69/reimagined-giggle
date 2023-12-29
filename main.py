253ce5bade9062f50913ac9eeb3878655346ab5f
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:37:40 +0000

    used charge perhaps early until

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import time
+def get_time_since_epoch():
+        return time.time()
+import datetime
+def get_today_date():
+        return datetime.date.today()
