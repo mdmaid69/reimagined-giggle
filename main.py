375c5fd12403d1d1c7ebaf2dcdd3659a8778208d
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:42:34 +0000

    concentration burst top separate model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
