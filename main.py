d1d2a10963181f573bcfbc856c0c2d93a21c8f02
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:58:25 +0000

    slight correct attempt raw fat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
