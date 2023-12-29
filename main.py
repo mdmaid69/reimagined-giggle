48fbe247586746e33932ca81cc360a7a1442a779
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:41:54 +0000

    shut opponent smart pride sign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def get_array_itemsize(array):
+        return array.itemsize
