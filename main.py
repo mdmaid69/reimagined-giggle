ce419b98ba585f7630c248976ff89f43f411269e
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:11:15 +0000

    admit fight joke avoid position

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
