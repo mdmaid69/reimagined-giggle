4c37e300218a21775284e3c4c5c311c56c1450f2
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:46:51 +0000

    visit finger not style proud

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
