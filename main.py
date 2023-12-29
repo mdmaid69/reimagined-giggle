7f7da669d8b45df82b355c205ffe5754218de59f
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:16:15 +0000

    picture put abroad defend head

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import os
+def remove_directory(path):
+        os.rmdir(path)
