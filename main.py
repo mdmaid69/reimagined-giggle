cf5534beaa7d87847227cca3702ab25ac8286a6f
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:40:09 +0000

    fashion exit stare upstairs tragedy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
