92a12734f89978c6a8cee77283fea475ffa03268
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:09:11 +0000

    peace ordinary uncomfortable avoid drive

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+print(os.getcwd())
