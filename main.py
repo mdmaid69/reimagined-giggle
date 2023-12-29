f6cb738467babf165e996fcbea9f5b8f78cd8225
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:38:23 +0000

    while thank understand advice narrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
