89a5c0b6c459320d48c43ae039574a88ce02fa5c
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:58:13 +0000

    peace middle upstairs excellent behind

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
