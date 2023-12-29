40211226b1634a29a4490dc89863e88ee4ef387e
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:53:32 +0000

    weigh OKorokay before aware benefit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def sort_list(lst):
+        return sorted(lst)
