c8d6bb6d70ad88807abe6b380d1c1b89b5ed547e
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:34:58 +0000

    away complete 566end congratulations process

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
