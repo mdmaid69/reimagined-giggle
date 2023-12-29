726030f476e4e2aac16eb56ff21941e6b4959e45
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:09:28 +0000

    remember break connection impressed scrape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
