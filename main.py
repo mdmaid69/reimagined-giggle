1f7776bb0f10c6ec60750b0291e4558e380cf2c1
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:20:42 +0000

    expect cover but confused sign

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
