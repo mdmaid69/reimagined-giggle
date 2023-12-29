009899e318d275abc7538dcf95e89af120765b30
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:28:19 +0000

    computer crash break skirt on

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_power(work, time):
+        return work / time
