d2e3188a50b4aa34aa523cb4d41dcb01b20266ee
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:04:22 +0000

    loud pocket national whistle purpose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
