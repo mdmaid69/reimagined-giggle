2b14d4a1d37e8d2748d98b84dd819ae1111a501e
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:13:49 +0000

    call figure waste separate fly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import collections
-def create_user_string():
-        return collections.UserString()
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
