0ab29eb1f764319b7adae5f5ebc6b9cd41b9be05
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:47:05 +0000

    swallow regional any search process

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import time
-print(time.time())
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
