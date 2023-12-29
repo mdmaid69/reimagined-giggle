1c51c52fa29d3143b965ff0a2e1160f4a5d630e7
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:06:30 +0000

    long generally conclusion disagree collect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
