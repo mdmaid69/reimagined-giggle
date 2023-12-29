989731c5f71534660e460306acaee68b4deef8d8
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:59:35 +0000

    rush combine acceptable exhibition know

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
