83872756886139c3f38aec90756a36cac97e4785
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:06:02 +0000

    female claim realize uncertain finally

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
