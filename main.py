71afa63b51c96661f2b0c7a3385055e307896300
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:37:45 +0000

    knowledge exchange colour block fortunate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
