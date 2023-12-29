80a8b6f5d727b0734fa83d8257b35973a8daa7c2
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:06:53 +0000

    produce performance fall later punish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  def remove_duplicates(lst):
+        return list(set(lst))
