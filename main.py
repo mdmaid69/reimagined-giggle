8d15c9729115f68e5fab7d10aa8c7083a879c6ad
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:43:40 +0000

    name typical television blame little

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def reverse_string(s):
+        return s[::-1]
