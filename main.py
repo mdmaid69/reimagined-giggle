812787b0109dd9fef43e7d960d76ce44dc388adf
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:08:14 +0000

    generous free trick giveback remain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def remove_duplicates(lst):
+        return list(set(lst))
