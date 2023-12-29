c6f3711f2dcc7c9b3387be13094705fe7cccd5b7
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:05:13 +0000

    attach cinema basic attract play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
