42f2214bda5f69624f970869e18c38affcf2ffac
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:42:24 +0000

    block Pp completely interest ahead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
