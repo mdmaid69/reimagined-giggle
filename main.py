373e69b3fe53d02ce22b86bf93669be979e47f77
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:49:59 +0000

    exchange usual offence frighten stage

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+x = 10
+y = 20
+print("Sum:", x + y)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
