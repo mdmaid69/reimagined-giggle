26d10a11d4596aa79284ccb09a7a3ee2112b5395
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:29:56 +0000

    value exchange question fact beyond

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  def convert_to_hex(n):
+        return hex(n)
