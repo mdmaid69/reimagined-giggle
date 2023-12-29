30d98346676974c79a5e14ab6207cd11dbc86efa
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:18:30 +0000

    chat government little this window

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def is_palindrome(s):
+        return s == s[::-1]
