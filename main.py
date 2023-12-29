959aa1924e92155364fa88b9a5e60da3d7e4230a
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:44:41 +0000

    watch declare divide terrible about

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
 import array
-def get_array_as_repr(array):
-        return repr(array)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
