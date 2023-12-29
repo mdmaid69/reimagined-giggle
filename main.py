54f746fe60e4b9f0768fe325c478fd0e9826ae45
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:04:25 +0000

    health collect confidence learn important

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
