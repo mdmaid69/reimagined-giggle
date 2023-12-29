f0ce6fa32104cad07006f9fc240dc6b70dfa952b
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:26:35 +0000

    permission indeed bread nerve edge

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-print(sum(range(10)))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
