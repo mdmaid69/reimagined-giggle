d305216493f878bdc4c5fc328fe015745cdcc30b
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:53:15 +0000

    anywayanyhow sport brain recommend bound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def get_array_as_list(array):
+        return list(array)
