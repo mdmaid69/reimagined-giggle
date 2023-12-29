6a650022fffd94eeb886f3ffbceb1392d887626d
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:04:39 +0000

    damage lookback(on) size several date

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
