563452151c2c393b889133ee0aadf8fd4d22a5ae
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:31:52 +0000

    sure mess same what physical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def get_array_as_int(array):
+        return int(array[0])
