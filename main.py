dcea93ff4530c3f0c83559be9de74e611747f6c4
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:35:34 +0000

    wet measure opponent joke free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import time
+def get_current_time():
+        return time.time()
