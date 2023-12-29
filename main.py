c8616bb6b436a646c2cfb4768e903b859d99d87b
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:19:48 +0000

    minor complete definite lot treat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def convert_to_hex(n):
-        return hex(n)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def count_words(sentence):
+        return len(sentence.split())
