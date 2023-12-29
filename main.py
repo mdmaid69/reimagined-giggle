e5156172c7ea4994c7b9f6707e84e12d1fd1f1df
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:53:32 +0000

    reach string dish minimum care

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
