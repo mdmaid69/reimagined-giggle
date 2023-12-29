cf4e7b70cb95dc0a1c5159e9226a41c8be9ada7c
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:17:21 +0000

    fine do record destroy rise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
