d5ac1e0dd1bd5665930af5720200679eeeb550ae
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:25:01 +0000

    invitation dress culture spite circle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+print([x**2 for x in range(10)])
