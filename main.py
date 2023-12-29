e6ce53b26d2a9787361403e70233584b605b34c4
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:00:28 +0000

    maintain increase hollow remember song

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
