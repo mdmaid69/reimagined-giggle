c1791456995d0d98350f7ad34722d8270590a073
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:28:31 +0000

    tune movement either seat identify

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def get_array_as_repr(array):
+        return repr(array)
+def find_max(lst):
+        return max(lst)
