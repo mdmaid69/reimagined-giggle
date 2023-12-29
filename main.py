df4789bfab312b86bdcebf3d8a7a26afa61959bc
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:41:03 +0000

    if steep quit later warm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
