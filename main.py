28907dfd8683624fca239e3aa48a85f1cf712041
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:37:32 +0000

    comfortable male trust very situation

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
