d5a0a3b5d51733544f6a0d0ed3bc0ccfb361fac7
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:17:03 +0000

    generally discussion it spring underneath

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
