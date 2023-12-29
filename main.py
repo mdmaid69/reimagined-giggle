1bee1eaa4a810262407e4db69a6ac7bb1d68c1ec
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:14:37 +0000

    callout yet win interview soil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
