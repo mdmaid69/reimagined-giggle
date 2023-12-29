bf122593d9722040bf78d35dfc354f792b3aabbf
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:33:31 +0000

    about minimum own charge project

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
 def get_array_as_dict(array):
         return {i: item for i, item in enumerate(array)}
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def square_number(x):
+        return x**2
