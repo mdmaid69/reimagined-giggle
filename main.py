2bac7b9e850d34f60accdc0fedfceee93072f3b7
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:41:08 +0000

    storm sink weigh confident promise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def get_array_as_list(array):
+        return list(array)
