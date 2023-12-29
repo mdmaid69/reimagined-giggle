35c9be7d39c6e8b0a0c71f7760af721eee7a3569
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:21:04 +0000

    boil shade dig profit up

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
