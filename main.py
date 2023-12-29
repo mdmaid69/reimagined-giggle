2025f9942d5bf3484d3dabbd43746c9291f323bb
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:19:01 +0000

    knowledge mix famous computer comfort

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
