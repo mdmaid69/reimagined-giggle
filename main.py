2a42af8fbc2ce0fed61107d10105ccbd64b1e596
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:37:58 +0000

    hair rest couple design temper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
