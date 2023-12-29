87119f9b937f1500b44c66d36053d6bd086f4fbb
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:13:23 +0000

    part complain scale possibility maintain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
