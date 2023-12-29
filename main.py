9cbd3eca61ea43941d6c4d12635f8698149ca81a
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:31:59 +0000

    similar free confused understand hour

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-name = "Python"
-print("Hello,", name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
