589395ba867e350d6ca62eb048647cb6f629b6cb
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:16:59 +0000

    skill company while choice swing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import re
+print(re.match("h.*o", "hello world"))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
