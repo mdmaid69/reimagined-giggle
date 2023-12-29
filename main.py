ee4c45918b43a68233a4590b15428b967e97fcfe
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:24:58 +0000

    cum point call ability disturb

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import array
+def get_array_as_float(array):
+        return float(array[0])
