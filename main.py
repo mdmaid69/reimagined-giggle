8374963bcf06ad8e59178b9e53aa2af6540c7a81
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:38:03 +0000

    new drink for callout off

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
