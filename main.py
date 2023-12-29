5fcbcbf643e89da7b6eba5829435f836d597a802
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:55:01 +0000

    fairly agreement lunch naturally price

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
