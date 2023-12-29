89f9f05670207360308a0f6f3bfe812e6ba3a535
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:52:23 +0000

    protection sky half immediate sweep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  def is_odd(n):
+        return n % 2 != 0
