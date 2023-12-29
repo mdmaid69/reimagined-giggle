aa1e19ae4708d7066d8532718700bfe558aaa1b4
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:18:17 +0000

    attitude border cut far clear

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def cube_number(x):
-        return x**3
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
