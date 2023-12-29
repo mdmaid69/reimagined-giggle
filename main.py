66c4f7204a1e135c23dcd122cc9ce226b9001a78
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:08:03 +0000

    neither before opportunity keepout feel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import platform
-def get_python_version():
-        return platform.python_version()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def remove_directory(path):
+        os.rmdir(path)
