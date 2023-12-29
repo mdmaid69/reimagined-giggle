53bf781af42e20bac34c31637cd0e35cc2b4488a
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:14:09 +0000

    range pressure marry forget no

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+for i in range(5):
+        print(i)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
