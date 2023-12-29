104314ead561a61e6cca79419967ebc24b81fdab
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:01:43 +0000

    zero sore right form throw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import collections
+def create_user_dict():
+        return collections.UserDict()
