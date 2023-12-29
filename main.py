a2409c637ff764052699bc07f86244930d62b430
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:13:47 +0000

    medium warning they waste flow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
