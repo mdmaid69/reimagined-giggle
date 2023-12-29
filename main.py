c4289ea1f6d404cae888ac3dac8af74092fa1f8b
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:02:55 +0000

    permission lock up file outside

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
