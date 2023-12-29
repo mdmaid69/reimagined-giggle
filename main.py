a2123d85d9a5d31ebfd8bef310c794cccf1b0c0d
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:11:10 +0000

    drop characteristic uncomfortable regularly inform

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import platform
-def get_python_version():
-        return platform.python_version()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
