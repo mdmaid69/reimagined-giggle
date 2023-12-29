48ec3121b7a3fc8faf40415de21e1b71d8f55d73
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:40:28 +0000

    means and rain mind back

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def convert_array_to_list(array):
+        return array.tolist()
