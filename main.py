6bfebb2913800c141aa85e909131d2710140d6fa
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:14:57 +0000

    on careless congratulations wave old

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_volume(length, width, height):
+        return length * width * height
