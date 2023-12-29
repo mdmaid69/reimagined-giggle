5fa371d3d8464588dd3c5c3dd8eec7db2af3e9f4
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:29:35 +0000

    star trick lower custom line

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
