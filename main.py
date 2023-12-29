c8bcaff6cdaff7661592ba364b244168b96f91a9
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:49:23 +0000

    group control cruelty somewhere principle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
