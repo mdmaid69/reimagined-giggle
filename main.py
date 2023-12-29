c10fd609f8541b7d80d4800d4b5abf501a95ff63
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:44:21 +0000

    prove worried material include war

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import datetime
+def get_today_date():
+        return datetime.date.today()
