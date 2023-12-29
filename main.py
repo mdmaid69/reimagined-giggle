6e78b59afa567fa1b913419573f57b2468638808
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:53:46 +0000

    later century love avoid during

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
