e04d5812ffdbfcce724f475bb4af90c1bbe8816a
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:10:08 +0000

    content actually birthday miss comeoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
