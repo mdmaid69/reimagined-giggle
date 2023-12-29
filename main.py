9b28db6887012710ac8ba660b896a7e18f2380b1
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:23:18 +0000

    maybe bus nature project unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
