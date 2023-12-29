1f569d938b5d50fc7a33b81cd890b9a26852396b
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:11:57 +0000

    disappointed shock trust couple design

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
