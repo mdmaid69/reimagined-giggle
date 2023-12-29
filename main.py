28f164b3c22194f05a4a587b69cef68d2d6c230a
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:00:13 +0000

    woman and total rain preparation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
