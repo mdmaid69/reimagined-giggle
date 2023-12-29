5ff4f33b5fe8cca5f18869d00e0cce5f5cf480f8
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:23:04 +0000

    means cheat breakinto difficulty voice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
