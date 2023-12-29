7893a11f67a1b869cbe6a34e571b7494788f36a6
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:27:57 +0000

    OKorokay interview listen run deep

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
