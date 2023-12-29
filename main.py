2f49c5b12f2d2faf5fb035fac6e32e6ccea0de32
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:52:38 +0000

    play post fly waste compare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import getpass
-def get_username():
-        return getpass.getuser()
+import sys
+def exit_program():
+        sys.exit()
+import array
+def get_array_as_float(array):
+        return float(array[0])
