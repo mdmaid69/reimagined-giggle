c1b7281195ce7896df9b820defe113f33ae51361
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:32:13 +0000

    goout correction recommend love from

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
