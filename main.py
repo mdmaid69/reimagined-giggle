bf4334004b45e9e6025c787c832b00124c725899
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:44:56 +0000

    world quick stop too light

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
