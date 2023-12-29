6c65112ee99cab2b483854e493a9d45ae439d07f
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:42:25 +0000

    push objection difference print stamp

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def divide_numbers(x, y):
+        return x / y
