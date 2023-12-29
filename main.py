5d9930bee816cba82060bf3a5afa2f87352ce4e3
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:42:16 +0000

    float loose offend or related

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import array
+def clear_array(array):
+        array *= 0
