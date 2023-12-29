85879cafbcd98eee079fe1f501d065cc9a4267ff
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:44:56 +0000

    common else improvement certain imagination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
