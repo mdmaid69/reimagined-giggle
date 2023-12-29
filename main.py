d770ab893ea4e75ec3e89c87e67ffa2386d15f64
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:44:53 +0000

    characteristic relax imagination comeround every

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
