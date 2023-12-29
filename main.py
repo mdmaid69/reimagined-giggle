0111be5e09c4590b26ba13076ebdc3c1e708bda0
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:58:34 +0000

    forward careful mind street experiment

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_length(array):
-        return len(array)
-import tensorflow as tf
-print(tf.__version__)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
