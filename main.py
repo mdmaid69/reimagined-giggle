70358824548fe148da5aa6cb7dc46ab87b72f8d6
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:40:02 +0000

    brush in I national cut

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
