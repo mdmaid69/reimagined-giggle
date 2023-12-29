bc1b7249b2f18cedaf4430eb6a716375e7ef1dcb
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:59:49 +0000

    sad few concern spring married

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
