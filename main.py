49a78404356964720386a4532bb0f223c53edbda
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:44:57 +0000

    just birth vote steal earn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_current_directory():
+        return os.getcwd()
