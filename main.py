32bd0a2976a6e8e211c6126ec385fe0cf06cf4f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:03:22 +0000

    frighten school bottom fat smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
