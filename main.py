cae6328b70d92a4be3774befc6d6a4eaefa32d46
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:50:28 +0000

    contact determined stare waste loan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
