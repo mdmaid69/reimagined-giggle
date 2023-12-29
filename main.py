e9ce5293b76d943be5e68a31970ba4ae8601f8b1
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:26:44 +0000

    because price talkinto force follow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
