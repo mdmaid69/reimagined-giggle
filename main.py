ba8d94b94914cca2871b49c5196160f8db5f347f
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:22:26 +0000

    else hate convincing produce contain

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
