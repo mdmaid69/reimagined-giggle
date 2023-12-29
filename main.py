8a3d0865bfc511b3477843270713e1bdfb491be6
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:41:25 +0000

    kick now workout thinkof border

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
