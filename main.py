c9cdf6868f237e7c922a6f2aefd4a4a2fe37b35d
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:33:07 +0000

    talkback protect record further Pp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
