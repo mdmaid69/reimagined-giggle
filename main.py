a5a6b5a9ed33d53790568614cf5660f25e38118e
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:35:10 +0000

    war black purpose hide secret

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
