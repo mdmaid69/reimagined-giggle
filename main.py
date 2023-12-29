574f10c2795b5be97240fc24b2160cee3296c50d
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:21:07 +0000

    evidence all pleased upwards strict

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
