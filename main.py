45ff9a9fcb2277bb0805cd32d9ef3753d0ae5433
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:49:24 +0000

    everyone sure explain decision Zz

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def reverse_string(s):
+        return s[::-1]
