d6869672bccb7f620032de26389743194ecf45dc
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:53:13 +0000

    179bite top flavour funny difficult

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
