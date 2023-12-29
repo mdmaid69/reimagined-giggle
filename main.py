7dc6662a8e74c5f768d05f23acd3cd7be830e7b8
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:20:20 +0000

    bunch spare declare pin clearly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import array
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
