d846e3fd70db258c5766db1dcea64a880c9851d5
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:41:34 +0000

    pinch simple hair shadow position

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
