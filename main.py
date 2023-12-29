4366f9fb657fc7618139f382dd08f74a18fedc92
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:54:17 +0000

    bet characteristic somewhere space under

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-name = "Python"
-print("Hello,", name)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import array
+def get_array_item(array, i):
+        return array[i]
