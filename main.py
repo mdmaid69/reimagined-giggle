f976addb2845797b95c3eca905bcfd3d8e46808f
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:31:38 +0000

    label beyond game stay keen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def convert_to_octal(n):
+        return oct(n)
