3f7dc3a5ad5dcfbc4e40b5c476235f9a1a2bcf97
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:51:46 +0000

    trip them too consider nearly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def add_numbers(x, y):
-        return x + y
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
