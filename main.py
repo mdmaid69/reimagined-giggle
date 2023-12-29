42b3464f8df5bc8f25db50945b8c271645359d54
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:23:57 +0000

    let time sensitive factory touch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
