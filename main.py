b221b583ea95ed479022692af81299b98e2476eb
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:17:46 +0000

    fear loss carry tomorrow different

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_sign(x):
+        return math.copysign(1, x)
