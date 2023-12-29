995fc66f8adacfa62c6b1c7138f38c139cc4eddc
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:58:28 +0000

    by embarrassed your hard just

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_volume(length, width, height):
-        return length * width * height
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
