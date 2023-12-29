058b951e623a01a9ad1c56680157546ac5c6324c
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:32:09 +0000

    present person confidence listenfor support

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
