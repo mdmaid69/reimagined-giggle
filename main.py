33192086a2bf9067d8df1c8db10ec30114b96ca3
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:16:30 +0000

    minute please value half taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
