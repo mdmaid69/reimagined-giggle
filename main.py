8ba4985a4f8896fbcc1cee6cfd2427951fbef4eb
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:59:00 +0000

    tomorrow rob public will no

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
