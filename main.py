9f86db6a0282f6ee107ca773fa46798ca2a21a18
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:11:17 +0000

    unfortunate influence spite reserve thumb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
