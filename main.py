e00de1cec03af734ef5644b491acfc0e1ed28542
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:29:48 +0000

    bright weather tidy usual wipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
