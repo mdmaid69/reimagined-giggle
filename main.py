84096cb6ef5c20587016ea54d3cdd72f59fb8a44
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:36:49 +0000

    pause work stir report story

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
