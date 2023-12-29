bc049f29b3a80942bd011bead94c0474ec37fa00
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:58:17 +0000

    grateful few separate proud straight

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def is_odd(n):
+        return n % 2 != 0
