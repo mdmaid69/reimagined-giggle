0070a4c05b5edc5fa864b1d0a8ba136952865370
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:34:24 +0000

    easy region drop turn steer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def add_numbers(a, b):
+        return a + b
