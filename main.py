307e4919f63fac4e970a21e650b132b15b437115
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:28:51 +0000

    now majority area slight together

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_current_directory():
+        return os.getcwd()
