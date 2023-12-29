56b17ee3004173005adf0a71e09559c5c3b45d3e
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:37:22 +0000

    expression disagree weak generous this

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
