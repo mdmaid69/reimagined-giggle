6c157bc1a2dd7759b420909a78c2e2bbe381df02
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:35:30 +0000

    time carry skirt other deaf

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
 import math
 def calculate_cartesian_to_polar_coordinates(x, y):
         return math.rect(x, y)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
