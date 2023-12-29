dee266a2b3d44d57272fd73fc2b870e0c3ee800b
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:39:47 +0000

    yet round talkback silent bad

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("Hello, world!")
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
