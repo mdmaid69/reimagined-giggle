3b985c16ca57377446b4ab0798111377646594c8
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:10:08 +0000

    sure honest wide pollution onto

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
