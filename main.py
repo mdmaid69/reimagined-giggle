07bdc7b1728fb46392a320362f7bec2170580d92
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:07:20 +0000

    bit tense middle copy control

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def find_min(lst):
+        return min(lst)
