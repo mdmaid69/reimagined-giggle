73d82e3180ff5ab61dcaecb7590452669473d975
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:41:41 +0000

    project seem suck cheat 566end

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
