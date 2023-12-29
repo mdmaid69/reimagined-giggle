9358c9fabe1678eb8f93106723d21f0773b12196
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:58:56 +0000

    570enjoyable switch often popular standard

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
