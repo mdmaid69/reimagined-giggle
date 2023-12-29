a4aa12d27fef39b751327867b30b24c8479ed563
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:14:48 +0000

    safety upset choose attention minimum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
