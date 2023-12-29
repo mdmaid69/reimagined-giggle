32e026ff157209cc0bafc0b5fb88a70ab7af9a91
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:52:06 +0000

    notice side sensible spend scheme

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
