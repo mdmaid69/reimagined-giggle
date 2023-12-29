3b92d3766deb9e2420f0f1896ca69d89a862072e
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:10:10 +0000

    emergency beneath border create concern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
