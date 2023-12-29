9e07e13f4f7ab48fbace36db3ca84ad184f4c80f
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:00:57 +0000

    pleasant report less takeaway hand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
