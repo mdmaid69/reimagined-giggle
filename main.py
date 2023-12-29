543d65902e89c62f21eecfbd93ebc3d413b0c53c
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:18:16 +0000

    try explain wash trap previous

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_area_rectangle(l, w):
+        return l * w
