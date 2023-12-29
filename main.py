e688e4442af9b107b598d5bf1b9f5834d6571fa1
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:46:54 +0000

    offer bag curious try object

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
