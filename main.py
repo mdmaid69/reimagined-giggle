dbffaf3821530727b6c6c5096f79fab9a1fab9e0
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:05:44 +0000

    bottom indeed belong pot sometimes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import time
+def get_current_time():
+        return time.time()
+import time
+def get_current_time():
+        return time.ctime()
