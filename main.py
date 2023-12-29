d3d87e78046e0f731f3308792b3848310f43eabc
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:35:24 +0000

    truth close immediate light later

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import numpy as np
-print(np.array([1, 2, 3]))
+import time
+print(time.time())
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
