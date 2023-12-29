b9802e428631fd3435685ca68e6edac748d013df
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:56:07 +0000

    touch bright 568energy warn regularly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
