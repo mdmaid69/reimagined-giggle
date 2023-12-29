788cdf664d6ed212c10996eace1afcb5c6f4c14c
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:02:11 +0000

    increase ignore vanish beginning thinkof

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
