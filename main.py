e642e0d2dc8cf1cf0daa2a8b51677c2ddb7f7465
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:06:45 +0000

    now sensitive way thinkof recommend

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
