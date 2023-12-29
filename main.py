d31d04487e35f7097109d058e2d521b3307074d7
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:55:40 +0000

    mark slightly field offer appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import platform
+def get_os_info():
+        return platform.uname()
