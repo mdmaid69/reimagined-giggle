6556ef3722116ba81be62f31b557715e06c2eafa
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:01:42 +0000

    nurse except ask account but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
