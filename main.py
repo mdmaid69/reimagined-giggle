e662d3ff5919a667b2b48438fa177704f326cf48
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:26:12 +0000

    song term direct broken food

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_max(numbers):
+        return max(numbers)
