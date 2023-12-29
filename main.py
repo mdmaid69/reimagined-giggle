7b6df28139c5531248ca597dea77369fbb73aea2
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:39:35 +0000

    land beat upwards cap talk

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def convert_to_hex(n):
+        return hex(n)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
