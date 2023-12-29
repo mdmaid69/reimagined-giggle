6735294518b78c0e9cad33af8ecb606d7e9b96bb
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:22:36 +0000

    comment minimum interrupt effort 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
