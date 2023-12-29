97deeeb28521a0d3d123ae55c517fc86a91db4e4
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:20:48 +0000

    geton look trick this afterwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
