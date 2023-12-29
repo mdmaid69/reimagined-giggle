3f8eba0c70895d4f34dda4f5b8c43931bb9bea98
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:19:55 +0000

    error friend hard talkinto talkinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+print(json.dumps({"name": "John", "age": 30}))
