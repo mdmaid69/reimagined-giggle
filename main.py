ba1d6d5120374d6b5edb876fa49d79d481a1da80
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:01:26 +0000

    below Yy but right alone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
