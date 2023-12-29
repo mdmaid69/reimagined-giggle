b655e20b92f32d07c82caffd716783b5ee84a2c7
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:49:57 +0000

    listenfor arrival another scare careful

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-  def cube_number(x):
-        return x**3
-print([x**2 for x in range(10)])
+import time
+print(time.time())
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
