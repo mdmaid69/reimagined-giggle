816928a8134eba8f5faf5a91636f32dec7b743e9
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:42:50 +0000

    himself not bye form march

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import array
-def get_array_as_list(array):
-        return list(array)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
