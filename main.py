4be159d3f29176a10461f6550ce6644542ad3f88
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:36:52 +0000

    care fetch season length concentrate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
