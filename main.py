dea8ff7ea081b14f54ffbcb70e1f87707f1d64f4
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:59:25 +0000

    blow slight send wise over

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
 import array
-def clear_array(array):
-        array *= 0
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def convert_array_to_unicode(array):
+        return array.tounicode()
