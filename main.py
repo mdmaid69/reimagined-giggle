784e2fa64bd300574647cc98d4306adece51cab1
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:23:00 +0000

    store mood mistake club century

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
