aa6a4754da01190d4b80902c8ac4d7db696ba4c2
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:27:39 +0000

    564encourage roof thing decide interesting

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
