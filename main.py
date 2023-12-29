58f7c3d77d568ffab1a0264a6900df61cb3fa79b
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:35:54 +0000

    neither excuse mine swing tall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
