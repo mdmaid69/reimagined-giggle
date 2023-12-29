784a55b92361690b64d96c2e01ad03cafed746f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:14:19 +0000

    borrow nowhere agree spread extremely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
