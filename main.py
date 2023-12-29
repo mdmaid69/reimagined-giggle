490af29781bf5a410c9b9e332cf1e876f346e83e
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:07:08 +0000

    just comment dull direction brown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import os
+print(os.getcwd())
+import array
+def convert_array_to_list(array):
+        return array.tolist()
