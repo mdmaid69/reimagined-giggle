6126d014aeaa783ca4ec73b775ea19267363b041
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:36:38 +0000

    minute Qq will remain left

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_speed(distance, time):
-        return distance / time
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
