34ef1ba86dde2659689e3fad3cf2c71f56700530
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:58:43 +0000

    emphasize keepoff worry modern private

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
