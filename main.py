fd83d0a356dcd62bc7f806a0c119d3b9dad13b6b
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:23:21 +0000

    office bored region true deal

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import collections
+def create_priority_queue():
+        return collections.deque()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
