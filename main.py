7c94dd57f4c0d5bd6de94ff1d4414b2a05f63fa5
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:23:40 +0000

    push Rr uniform 567enemy disgusting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def get_array_as_memoryview(array):
+        return memoryview(array)
