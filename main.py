32abf5656dd23924f16e2ca2c3dcf205a0dc7da0
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:59:38 +0000

    brief laugh file supply burn

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
