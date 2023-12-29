a00ce93218cb5c34de4c47ccfac37893fdac916b
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:37:18 +0000

    occur understand worth neck seem

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
