6bc85295751e902537acb5732395d14db58da45f
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:55:34 +0000

    admit male schedule board charge

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def is_odd(n):
-        return n % 2 != 0
+def reverse_string(s):
+        return s[::-1]
+import tensorflow as tf
+print(tf.__version__)
