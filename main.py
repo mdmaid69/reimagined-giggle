42a251ac8024961775682d270b903c4aef269f85
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:33:15 +0000

    phone guard stop community interview

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def multiply_numbers(x, y):
-        return x * y
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def remove_duplicates(lst):
+        return list(set(lst))
