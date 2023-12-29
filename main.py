cd3016da1ac782600e6e5bfc6ad31b0e4ee7f2f8
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:32:58 +0000

    considering far activity run settle

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
