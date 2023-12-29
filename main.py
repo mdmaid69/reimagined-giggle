075dac971f739f12cc45fc709f8bdb0e8b218877
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:30:41 +0000

    path since centimetre skill furniture

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
