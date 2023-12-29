df45704d06ac1d0e49cc5c6b65021da053c964b1
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:07:57 +0000

    somebody occasional medium suggest divide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
