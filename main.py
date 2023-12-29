0a5200e2f8e3b611601d8978f864cd777abbe41b
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:00:42 +0000

    length agree purpose future yes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_acceleration(speed, time):
+        return speed / time
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
