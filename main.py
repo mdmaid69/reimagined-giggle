975d53bde1627b46a75b0e53cd54755033b08524
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:01:32 +0000

    exactly not live bottom moral

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+for i in range(5):
+        print(i)
