f17a7716c1bf68c32dbb1dcbbbfd4c9443f3248a
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:57:26 +0000

    dress thinkof cover male powerful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
