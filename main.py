60823dcfbfd190864cf51715f49a38cbae17ddd6
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:44:10 +0000

    combine morning swim now while

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_density(mass, volume):
+        return mass / volume
