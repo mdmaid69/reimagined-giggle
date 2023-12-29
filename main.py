a6a8ccc0512c85f8e3495e602d0a8e8b6d8aa2cb
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:32:42 +0000

    568energy business line exchange combine

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def subtract_numbers(x, y):
+        return x - y
