c768f2751881e9d296ee5b710a87e7353f3320bf
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:06:19 +0000

    communicate Ll squash here present

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
