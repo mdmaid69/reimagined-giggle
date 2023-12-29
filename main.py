bd0e3516bb1b41bec6a1d6dbe7e0c147590a7be4
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:49:45 +0000

    damp trap scare getup excited

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import os
+def change_working_directory(path):
+        os.chdir(path)
