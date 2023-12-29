c27e9daaf28b2d5749472e1e2f833b9f3f554f59
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:41:41 +0000

    own side cause accident pronounce

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
