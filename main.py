27055b4ea2f60192fadc6a795a7674e418e7027a
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:05:48 +0000

    shut dangerous risk rush load

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def subtract_numbers(x, y):
-        return x - y
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
