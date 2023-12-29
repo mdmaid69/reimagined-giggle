7f7ded1c6f53b558e48719bc7b679ff4dba6869f
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:02:22 +0000

    act case length thirsty benefit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
