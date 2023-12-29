9590f7e5e6aadf3b6e9332bb14944ea95b64f296
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:01:57 +0000

    breakout error tune reach house

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
