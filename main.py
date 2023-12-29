35db2573d5b417f407ff2cce98910bc78c13883a
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:11:43 +0000

    whistle shut century keepaway disappear

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
