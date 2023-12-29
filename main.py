80708f33c5a4abf3296368fd20b849afd212b6fb
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:31:40 +0000

    achievement cinema post contents though

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
