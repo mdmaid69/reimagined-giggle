d1827043f793cff19bb2029ce9c6367b0750ce68
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:32:01 +0000

    factory stop thought other rain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
