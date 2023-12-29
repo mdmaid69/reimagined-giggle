837dce06eae9ad6632a30e17d58133506f646d68
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:07:19 +0000

    question should whistle cut Hh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
