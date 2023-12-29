bfcfd2ddbcf284c18ee38e18fd4b964c58d110cc
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:23:15 +0000

    sort union part front fairly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-import re
-print(re.match("h.*o", "hello world"))
+import json
+print(json.dumps({"name": "John", "age": 30}))
