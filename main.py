cf23b8d194e7daf1488c936fe5da3d2c335b6f27
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:28:28 +0000

    avoid downstairs below object begin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
