6aa1a1a3efb4b6ccab314ba1c573a0f9741990d4
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:52:47 +0000

    responsible go male fence recently

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+print(sum(range(10)))
