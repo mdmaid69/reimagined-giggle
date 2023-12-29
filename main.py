019000b70f115052a71cab7b98a2caf15c65e1f2
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:25:10 +0000

    community violence recently possibility day

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
