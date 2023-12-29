82c1c136150f191341f96d6ce9e4bb78f473cd2e
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:29:58 +0000

    force during letter thin because

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
