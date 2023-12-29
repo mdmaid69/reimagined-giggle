1238ae2da738d55a4e6dfac7c312e63c9c895a65
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:05:09 +0000

    anything interruption suckme indeed television

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import platform
+def get_python_version():
+        return platform.python_version()
