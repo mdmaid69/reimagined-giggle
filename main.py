9816c86dc651ba76e7bdac6f2a8af889cf1812b6
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:39:45 +0000

    godown acceptable left weigh cinema

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
