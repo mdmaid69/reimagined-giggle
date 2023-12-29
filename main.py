e2b30afbebfa6919cf2dcd1587bb401bd3f4ecdd
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:09:29 +0000

    keep soak wish hold sweep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
