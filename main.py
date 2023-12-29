0c4ed1d957f916a37af07770e6892f45ccc71de6
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:28:10 +0000

    emergency anxious workout fortunately worth

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
