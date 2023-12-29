b246fdf9c54af2f82438643c40be8a46825573ea
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:20:54 +0000

    relative common actually event break

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
