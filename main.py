dc42cdc2bf5f1809f4c5ad4d257a707d808633cb
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:44:40 +0000

    count hurry Hh nothing bound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
