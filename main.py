d3067db9e58094b79c5a4004f3a6491a62a20ef4
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:18:30 +0000

    partner hers whatever serve later

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
