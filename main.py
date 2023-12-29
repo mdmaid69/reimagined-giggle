4b590e492626db4fba91da95c90ab603c01227ca
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:48:47 +0000

    disappointed dangerous broken feature produce

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-print(random.randint(0, 100))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
