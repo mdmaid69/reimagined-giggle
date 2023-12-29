92ec130c83db04ec40f6fd715e2e599869a7e154
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:56:05 +0000

    blame their speech chief left

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
