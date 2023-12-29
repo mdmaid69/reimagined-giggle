4e4f7c77c230b5426729cc46f46cf4f973d01d6f
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:53:26 +0000

    finger man Zz card mystery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-print(math.pi)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import collections
+def create_stack():
+        return collections.deque()
