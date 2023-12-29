9f716552c26c914c992b47106bb8bc5a8949299e
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:52:24 +0000

    welcome hit check public stick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import sys
+def exit_program():
+        sys.exit()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
