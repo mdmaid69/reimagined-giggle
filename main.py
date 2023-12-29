ecc04181fd86f1aaa25bf68837afd7ac069321a5
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:18:31 +0000

    base fairly unkind tool neither

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  def multiply_numbers(x, y):
-        return x * y
+import time
+print(time.time())
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
