35ba0ef3c98bb920f6e0e67d414199b9d0270ee2
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:37:48 +0000

    getaway near regret game sea

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_speed(distance, time):
-        return distance / time
+  def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
