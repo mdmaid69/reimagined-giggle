1465991d9f97ce4f83f9efbbdff6b60d468c58d2
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:08:39 +0000

    sleep himself about stop suppose

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
