a0a1d2b4d1c5e05f079694e44eb7a5197408830f
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:10:50 +0000

    still suit base neat somebody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
