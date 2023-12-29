d3f438a5bdb2ccc0970d562714cbbbf7c6aa8022
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:35:59 +0000

    wash reach typical experiment trade

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def is_palindrome(s):
+        return s == s[::-1]
