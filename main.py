0c0504f67ac771120db92eaec246a4ff8958fb3b
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:01:47 +0000

    badly effect press disturb normal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import platform
+def get_os_info():
+        return platform.uname()
