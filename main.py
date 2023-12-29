42810be872f5dbcc1d9ec5dc4709cf3066f7bf88
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:03:16 +0000

    soil difference pretend goback(to) suit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
