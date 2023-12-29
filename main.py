e81ac32ab5c3c3fda31541780902a34c2cc46f25
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:39:59 +0000

    again fear road thick experiment

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
