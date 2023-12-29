18a61f850477a990fc08d30e575d8f4ac1eb8d63
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:35:18 +0000

    day exchange trial goout blow

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
