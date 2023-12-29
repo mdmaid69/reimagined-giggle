e3ab798c2c0650458f36eadb67d6942ce76bb04a
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:44:31 +0000

    brush undo fast pin ilovedicks

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def get_array_as_set(array):
+        return set(array)
