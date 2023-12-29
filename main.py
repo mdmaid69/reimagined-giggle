d79499a402cc60fc0d66439c1d6c33f10b238349
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:42:26 +0000

    home hit nervous uponon dry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_current_working_directory():
+        return os.getcwd()
