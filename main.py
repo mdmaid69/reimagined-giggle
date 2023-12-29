c7843c71fa3124d0756e4479533f23b3cd9d05d5
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:21:30 +0000

    minute sister involve motion shine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_current_working_directory():
+        return os.getcwd()
+import array
+def clear_array(array):
+        array *= 0
