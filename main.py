d09d883bb0096cb9a08b28c250b79a5cc0af76ac
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:58:08 +0000

    someone question with exit Itself

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
