7abac1746f7818b4c0417c55558e46a2227df7c2
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:45:12 +0000

    address admire Zz mixture plenty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
