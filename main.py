e2e1c695753d2d85dd1815f4e16ef4de37e5ca53
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:20:41 +0000

    talkback joke gather grateful dead

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
