290864b8a1cee1f8dbc2063eb5a580297ebebd3d
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:17:41 +0000

    plenty object 568energy no turnover

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
