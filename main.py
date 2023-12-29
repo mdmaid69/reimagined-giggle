d24405c639627213c4bcaa2ba78a7a0619b40c79
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:07:45 +0000

    weak under advantage eat expression

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
