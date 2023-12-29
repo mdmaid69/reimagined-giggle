f63f99e3c0a1ac2980de74eee95f72b3667117a6
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:49:10 +0000

    active holiday best land party

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def cube_number(x):
-        return x**3
+  def subtract_numbers(x, y):
+        return x - y
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
