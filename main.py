b914ccbe8f51dfd57a292fc98cb793a441996694
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:39:42 +0000

    accident window become fail keepout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+for i in range(5):
+        print(i)
