a72a1de748aa70b4d89ac4047533369d94eafeda
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:44:27 +0000

    people willing ashamed almost forgive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import sys
+def exit_program():
+        sys.exit()
+for i in range(5):
+        print(i)
