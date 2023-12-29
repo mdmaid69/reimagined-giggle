877db45a8089360397b8a945448b736b2f92d163
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:45:52 +0000

    manner beginning emotion 572enter blind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import multiprocessing
 def get_cpu_count():
         return multiprocessing.cpu_count()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
