c84265195b6e2cf258df2c57309b068909bcc821
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:25:05 +0000

    then able much issue statement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
