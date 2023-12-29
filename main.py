1ab811a4eb09579b5e2ea721bf2949284a635042
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:53:37 +0000

    practice right juice tragedy face

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def is_even(n):
+        return n % 2 == 0
