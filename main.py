b29d6a407c336017a0c38070ebd6655bb58380c1
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:09:32 +0000

    watch spare hear basically person

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
