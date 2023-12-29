5380f5d48841b9d0d12ef3a2c6968606d892e47e
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:26:00 +0000

    same cheque gift cough size

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
