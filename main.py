f8fb2f265ef488d8e336ad9814ca75811dcb9004
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:17:37 +0000

    intend part cheque joke seat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
