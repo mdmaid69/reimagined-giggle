9ff56279254cbdedf11822b50aece63a84492e8a
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:30:32 +0000

    amount lean store thirsty offence

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
