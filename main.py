f6aed6c03ec23eeb2bf699037635294ac12bbe05
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:00:38 +0000

    hair pass about rescue upstairs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
