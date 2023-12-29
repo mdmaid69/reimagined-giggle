c19a8cbbc96e03cc6355351246e54aeae832fc35
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:49:05 +0000

    her goback(to) welcome tomorrow surprising

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
