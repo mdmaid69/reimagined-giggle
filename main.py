98ce1ee377f566c40dfcf46f5157ed3c928b2b72
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:27:51 +0000

    answer draw quarter recovery performance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_power(work, time):
+        return work / time
