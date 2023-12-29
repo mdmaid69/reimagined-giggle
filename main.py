a1bbf2a9f012bdc007d10c8573bb843bc30bae4d
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:27:43 +0000

    peace success sort region while

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-print(sys.version)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import collections
+def create_user_list():
+        return collections.UserList()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
