e19164b34efac7af22017389225fb29b86e8eef7
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:05:28 +0000

    draw expression watch thorough surprised

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
