fb897d6ddbe85bdb314a430de623220a2d11acb6
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:24:44 +0000

    surprised region calmly family reduction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def convert_to_hex(n):
+        return hex(n)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
