95a284b7b601c0cb4377eba98e7a765dc21795e4
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:05:36 +0000

    there valuable represent deep mix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import datetime
+def get_current_date():
+        return datetime.date.today()
