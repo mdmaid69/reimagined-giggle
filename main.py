4210be421ed811803fb70ba1ffd1d3634dd7f500
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:32:45 +0000

    lookaround discover cut Itself understand

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def count_characters(sentence):
+        return len(sentence)
+from collections import Counter
+print(Counter("hello world"))
