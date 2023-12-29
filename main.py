d48fda33f94e459c1eb5d39b785c534acfebce3b
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:24:06 +0000

    special warning second advice ocean

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
