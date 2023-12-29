1c5f6760cf67495157ac07c71cdf73f2b7e1e0cb
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:44:16 +0000

    figure boat back murder commit

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
