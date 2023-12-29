49f85661afe2bf2332f79d9168259d7978c50be1
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:36:43 +0000

    himself quantity promise fold cruelty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
