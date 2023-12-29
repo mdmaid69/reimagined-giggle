a07cc86404ea3455499f8fc8f4f29cdca87fdd66
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:50:04 +0000

    lookup round surface aware suddenly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+x = 10
+y = 20
+print("Sum:", x + y)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
