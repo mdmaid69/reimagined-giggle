e153eb46dc13f1877db909c709bb77754751862b
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:06:59 +0000

    past strict reaction deal freeze

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_ceiling(x):
+        return math.ceil(x)
