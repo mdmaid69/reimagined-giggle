9c8181c8e07c1130f33d51b48d5dd90a6ae6e03c
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:19:57 +0000

    should advantage similar bright back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
