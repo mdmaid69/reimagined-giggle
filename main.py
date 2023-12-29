1f69f866296cc9a77566dab1631ed27db6f7624d
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:13:18 +0000

    form almost getaway turnon either

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sys
+def exit_program():
+        sys.exit()
