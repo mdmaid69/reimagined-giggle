4d73d6a197abda4a6266cf9569ec22e12cbc22d4
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:11:24 +0000

    else fat connected defeat post

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_current_working_directory():
+        return os.getcwd()
