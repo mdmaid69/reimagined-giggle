a4ebb83ea86cbbd7f1e4e635c93a68f3465581a9
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:15:54 +0000

    perfect account trousers roof might

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def append_to_array(array, item):
+        array.append(item)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
