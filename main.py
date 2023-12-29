cb936b18d99bb65e1e406224bab67e44a0f88972
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:17:24 +0000

    gas grateful immediate frightened word

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
