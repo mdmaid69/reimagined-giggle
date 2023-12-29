e6efa2f176efcfc13e3097ad8e73574cefe6a343
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:03:19 +0000

    base too along deserve sweet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
