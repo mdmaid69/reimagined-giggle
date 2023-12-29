6234db4e97ab3899a203197a6f916da730a8ad1b
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:34:25 +0000

    mean onto produce winter embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
