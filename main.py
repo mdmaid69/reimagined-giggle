284f97ca70b2d1e43cfa074738c237c7036c7177
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:17:52 +0000

    later plastic know neither large

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
