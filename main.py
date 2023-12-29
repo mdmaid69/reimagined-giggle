ed63d7bb4f450ee51ba559602c41fe11c10010ec
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:26:36 +0000

    second free through some better

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+for i in range(10): print(i)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
