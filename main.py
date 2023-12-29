132196fddfce82cc2c30359cec2956f67784e1e4
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:54:55 +0000

    573entertainment ask no score show

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
