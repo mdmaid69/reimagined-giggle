0bdb61bc127091747e8682c4aa6ae2b3f000fb91
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:46:30 +0000

    question past fun Ee complicated

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
