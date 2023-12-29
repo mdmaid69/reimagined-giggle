5631be9f7b62ccff47addc6f7fdba97ec1f98c82
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:05:12 +0000

    hospital disagree pile repeat power

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
