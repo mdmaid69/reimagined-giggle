494ff49dd05909a0a0b1c8c489ac82bd5d523e2e
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:32:33 +0000

    secret hole somewhere rarely admit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
