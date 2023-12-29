ee337f719f096236c233c858164fc63d177235a0
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:17:33 +0000

    purpose way stamp right leave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_error_function(x):
+        return math.erf(x)
