991e2de5aeeaa3ee61c4203c89b3d2ab109e6da5
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:09:23 +0000

    dream finish country danger subject

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
