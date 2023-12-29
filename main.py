e06957b4d07ca12d58d28ede1072e08f55711bdd
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:16:56 +0000

    leg nothing rest takeover keep

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
