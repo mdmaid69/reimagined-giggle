589ca19f8632a9e8ca7ea3202a381bbfd3c05fa4
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:17:59 +0000

    buy swallow opposite line within

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
