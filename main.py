779cd9f5e437530cb21d77a060db3de923595bb9
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:15:06 +0000

    breakinto visit crisis mine keep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
