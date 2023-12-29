d7ad847e0901286c6d4177dcb14bdac2d7c23d43
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:52:03 +0000

    whole unite part connect ignore

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
