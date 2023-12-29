725196a2845e45d856a10c1248624b955d8199f4
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:35:24 +0000

    from after comeout walk schedule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import numpy as np
+print(np.array([1, 2, 3]))
