e30888fa2fb89b31946ab4d8ddd1bb76ba5dbf01
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:56:51 +0000

    software piece charge through satisfied

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-print("Hello, world!")
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
