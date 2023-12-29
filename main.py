c936cf6a6d1f676d4b06c0e27f06884d079066fe
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:25:19 +0000

    mainly yes slice bush design

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
