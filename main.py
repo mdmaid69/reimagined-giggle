f2065155b176ce11c8e273b3a4c5faf7f89fc03b
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:51:05 +0000

    holdup so pure radio will

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-def find_min(lst):
-        return min(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
