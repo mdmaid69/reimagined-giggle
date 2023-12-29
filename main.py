74688cf463d778fdc368cd0f871795b594d5206c
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:09:21 +0000

    successful equipment along punishment unhappy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
