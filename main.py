426649dd392478a5bde66de02025e89c7b2f1720
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:14:34 +0000

    official practice brother choose comment

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
