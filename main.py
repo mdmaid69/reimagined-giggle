49dab3d6260aa53964775b0c9b322ec35c76b5bc
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:03:59 +0000

    back coffee anywayanyhow hard estimate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_list(array):
+        return list(array)
