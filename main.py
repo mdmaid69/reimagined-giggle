530fb2ac222861d80fb34790d07f451344f72ddd
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:42:34 +0000

    smell system unlucky loose difficult

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
