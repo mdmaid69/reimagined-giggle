e896b2f8d6549e9eaa7c672461ed5c507bcb2774
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:08:30 +0000

    which bread soil shock petrol

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_base_name(path):
+        return os.path.basename(path)
+import re
+print(re.match("h.*o", "hello world"))
