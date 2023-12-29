5d01cbb83b5ae033e0283329f9a405ec93267336
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:42:21 +0000

    spring Ww wash visit prevent

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import time
+def get_current_time():
+        return time.ctime()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
