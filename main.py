8796935b813faab0d1d33284b6560139c805247a
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:11:43 +0000

    giveaway machine appear thanks convincing

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def count_characters(sentence):
-        return len(sentence)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
