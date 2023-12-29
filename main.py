f383e74c35334d4b833b8b7e9bf841a92cdfce6e
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:16:55 +0000

    form regularly view please radio

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
