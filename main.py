e6fe7c463ffb0b1305837ad9f8d2b25d6a610cad
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:02:35 +0000

    relation tap statement final bother

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
