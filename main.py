6ab6e3966e6a8f3dec5761dd19f649e86b895ea1
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:29:53 +0000

    snow like choice main freedom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
