59e244b2cf911fe4433b0dad4a7ac4de46d43b5c
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:01:08 +0000

    arrange dream impatient plenty team

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
