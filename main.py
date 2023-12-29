194782dad78c0ec3214a01b4a2e4bad1cba09377
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:43:38 +0000

    to complicated reduce thanks hope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import sys
-def print_python_version():
-        return sys.version
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
