3c96b7529189c367d1bcf4d2de2b6d6832ca6ab0
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:59:22 +0000

    mark pull time up begin

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
