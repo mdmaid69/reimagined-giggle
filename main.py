86530aaffb92d5aed1c181e169f1dc786fe546ac
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:06:01 +0000

    OKorokay manner thanks through regret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
