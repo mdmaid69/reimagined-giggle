e489271c97bd78eabda8a0fd819b0575374836ec
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:02:31 +0000

    book calloff getup file bring

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
