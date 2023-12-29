291ee9e02e57d6c9cd4778b1cef1bea33cfbef81
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:03:43 +0000

    together crowded fall across refuse

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def subtract_numbers(x, y):
+        return x - y
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
