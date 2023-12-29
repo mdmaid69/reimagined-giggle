e0b45ee6658aaab75a0522ff76ca16ce38f8d51b
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:52:09 +0000

    his union breathe yours wake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def get_array_as_list(array):
+        return list(array)
