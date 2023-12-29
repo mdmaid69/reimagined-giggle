8ad36fde8cc4365bf40759f58017bc396eec7aaf
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:34:33 +0000

    thinkof content fierce guide tend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
