6f3887b39202b134e0d4718e8b0c62948133d95c
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:31:26 +0000

    regular later my really behind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def is_palindrome(s):
+        return s == s[::-1]
