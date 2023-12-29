2ca089b7c63ee540d0e8cf808bb163aca9b2aba1
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:11:49 +0000

    false concerning underneath exist club

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
