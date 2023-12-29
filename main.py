a386cb0880bb98397ea068f88ff4087d06491635
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:53:53 +0000

    pay bottom change comeback seat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def cube_number(x):
+        return x**3
