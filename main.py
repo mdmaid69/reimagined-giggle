dddfbc94031b92e204aa103b7d4d91ce6aef283e
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:24:22 +0000

    fairly ability opposite heavy anybody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
