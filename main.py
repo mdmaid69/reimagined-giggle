1fc5f956c778541d32a33747168493eda0f1541e
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:24:05 +0000

    anybody glass your goback(to) hard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
