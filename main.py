f0d9cbc8f19d7a979a5f791b5975435c6239b55c
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:16:34 +0000

    best bottom shut frequent neither

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
