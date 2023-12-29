608436f5b98993ca5c9377e4c7eccef9c933334c
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:24:11 +0000

    middle fortunate steer deliver extremely

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
