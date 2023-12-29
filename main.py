7b1345944f101f4d36aaf2343193b8874c6ce8d5
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:40:33 +0000

    surface perhaps fun gooff extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
