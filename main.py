ca060f510c034176dd459c71dd42ff687f3a23fb
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:37:34 +0000

    region flavour influence clean match

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
