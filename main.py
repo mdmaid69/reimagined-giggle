a219981a7acfbca7edb9e5af71f7b7a99351c11c
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:17:57 +0000

    lift record tradition take eye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
