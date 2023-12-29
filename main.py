fa228183c864e667c88339c6a63fc663f5bfefd1
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:42:13 +0000

    think precise instant also invite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  def convert_to_binary(n):
+        return bin(n)
