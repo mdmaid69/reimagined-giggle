506ee5500ca32eb4382da4e4262e25370ce9523d
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:16:00 +0000

    test talkback serious uncertain might

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
