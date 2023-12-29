92c3f656ab129aaf3e814cf0da1052409fd80aa6
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:44:30 +0000

    careful fun angry physical perfect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_pressure(force, area):
+        return force / area
