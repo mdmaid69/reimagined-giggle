92a14c9bcdfca2250c1a10f9fa313b20d648a203
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:54:09 +0000

    important shopping demonstrate waste help

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_floor(x):
+        return math.floor(x)
