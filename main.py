107283cdb4a16f86de634fae87ebf32e6d79ec29
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:29:40 +0000

    Bb worse principle full powerful

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
