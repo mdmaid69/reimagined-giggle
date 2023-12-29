3e7c20cdb0dc956d54be152f74f27cac40b562e9
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:30:38 +0000

    rely software interview authority yours

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
