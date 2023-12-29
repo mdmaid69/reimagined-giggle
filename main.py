1cf50d69ef64e982338c899497ecd4f5e2b44913
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:07:44 +0000

    wrap motion scrap performance fun

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import random
-print(random.randint(0, 100))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
