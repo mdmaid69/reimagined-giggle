a66248e75311eb0862694888be50b171edb60e94
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:24:37 +0000

    school my fit rank belief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Characters:", len(text))
