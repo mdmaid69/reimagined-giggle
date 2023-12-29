818cf330e530bcc2826e6e3ee9de2f183f45fcef
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:16:44 +0000

    dull habit bath bored here

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
