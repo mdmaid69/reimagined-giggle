7ee2a09ca3161e9b65b8d77fa453bfdd7bde7c13
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:15:20 +0000

    situation lend favourite strong Cc

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def count_characters(sentence):
+        return len(sentence)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
