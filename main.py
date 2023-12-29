003b3af0d26f3a234d34bebc17f70a5fabbf1f80
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:14:01 +0000

    unhappy record strange demand successful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
