59c07d0d78ed765115af1e48ababdd59872708f6
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:41:13 +0000

    office us vanish empty nice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import array
-def get_array_typecode(array):
-        return array.typecode
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
