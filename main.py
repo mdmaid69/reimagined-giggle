be901a4a3f29cd7983c404589b8022803d93ca9d
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:03:43 +0000

    pollution flat fall mine system

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-print("Hello, world!")
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def change_working_directory(path):
+        os.chdir(path)
