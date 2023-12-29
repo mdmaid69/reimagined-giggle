85b35b9a7856e6c3e1ef1f0a6e71e385c72234bb
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:46:26 +0000

    table match live Ww disagree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_max(lst):
+        return max(lst)
