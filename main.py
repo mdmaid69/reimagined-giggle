c07ebc1cc7900512a34a0da1de176d75fcaee595
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:52:51 +0000

    look bit law transparent part

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Uppercase:", text.upper())
