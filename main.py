6011d1ea713d7f3d3413f59f712fae5365d1242c
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:25:26 +0000

    project lucky story quick rule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
