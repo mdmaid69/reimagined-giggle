27c14f12fd507a714c277bc3d79cc8473bd40301
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:04:09 +0000

    goup change another yesterday clean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+def find_min(lst):
+        return min(lst)
