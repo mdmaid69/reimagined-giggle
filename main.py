a155733e2a8924f7ce1e545d4ea767334ae05156
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:30:18 +0000

    bottom community family plate opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
