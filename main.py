269bce6c1e8535d3f94cb5aa603e83ad3e601bc4
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:02:49 +0000

    interview clue fork ticket than

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
