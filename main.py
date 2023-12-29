798c0912e8b8c48a131b714309d5c1efb7f0f94b
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:07:16 +0000

    comein frightened flat voice grateful

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
