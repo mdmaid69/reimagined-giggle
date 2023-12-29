01d19bd81434ce7349b1894edcd9769419113224
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:11:05 +0000

    lean suddenly their achieve direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
