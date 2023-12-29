8a51c0ee4a32d43ebeea38587efe5f63e0e029e9
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:18:28 +0000

    married meet relative waste estimate

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
