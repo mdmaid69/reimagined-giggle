29a04c597bdd327b98d47d0ddcd834e6f86ed551
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:10:13 +0000

    workon react receive embarrassing deep

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def multiply_numbers(x, y):
+        return x * y
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
