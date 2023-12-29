78c92ef30710d620c191f0233a66d197df286559
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:21:16 +0000

    go worst spread punish lost

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def calculate_density(mass, volume):
+        return mass / volume
