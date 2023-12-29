cf2e6f2e0611b484a1b50fa7bcb82efc2d5845e5
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:26:07 +0000

    treat reply minute dark stone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_power(work, time):
+        return work / time
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
