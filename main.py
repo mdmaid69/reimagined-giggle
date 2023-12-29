4a666bb522ee432e380bf586db48a3246ac19d83
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:29:14 +0000

    bet date tongue worse light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
