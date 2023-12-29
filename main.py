e71e68a1551330dc5d8976d2c78181050504e2a3
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:29:52 +0000

    more cinema tread everyone can

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sys
+def print_python_version():
+        return sys.version
 import random
-def roll_die():
-        return random.randint(1, 6)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
