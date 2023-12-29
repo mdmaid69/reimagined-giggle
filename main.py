e34991fc9e96ddd0f023ba1dddb21c19c62a6459
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:47:07 +0000

    willing man finish pool meal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
