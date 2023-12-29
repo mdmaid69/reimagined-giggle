b601666ebff7cb18556fc3317877f9bdc85aedf4
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:36:51 +0000

    definite intelligent what cover come

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
