c67869b17b7a03326162b2ddecc861fab2c93149
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:53:16 +0000

    challenge where fly behind several

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def add_numbers(a, b):
-        return a + b
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
