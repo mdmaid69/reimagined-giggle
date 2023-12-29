6c3370d63368408b7e3c38e639ce9d0bbaae4b06
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:36:14 +0000

    best song maybe bother relief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import math
+def calculate_floor(x):
+        return math.floor(x)
