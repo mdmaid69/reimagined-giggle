9631fdab16bd90f86480587ff76cb766864c4626
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:23:36 +0000

    furniture neck equipment sky on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
