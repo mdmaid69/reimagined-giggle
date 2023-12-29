c0defe97df99776ff29f14aa1070cd9565a9ca12
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:16:25 +0000

    square godown wing black cope

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
