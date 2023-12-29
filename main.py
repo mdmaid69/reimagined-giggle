dd0d275306d3427e8b41f87a95d7237f8e6f0bca
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:19:56 +0000

    mine upwards mad handle vision

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
