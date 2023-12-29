97709ae7fc64b1e9dae431081ea4b68882cc9ad7
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:14:19 +0000

    throw suppose fact inside why

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def divide_numbers(x, y):
+        return x / y
