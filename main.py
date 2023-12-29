24cb730fc105586b045c396f9f1cf908e014fc0f
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:12:24 +0000

    description provide instead plan normal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
