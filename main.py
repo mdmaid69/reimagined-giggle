34b96dc3b8eb3d2519a6fa903ac74ce924668d62
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:05:59 +0000

    pick comefrom press strike schedule

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def convert_to_hex(n):
+        return hex(n)
