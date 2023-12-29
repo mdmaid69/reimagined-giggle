72b54c5cc676c70c16b1419c95dc055d1591fb24
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:43:53 +0000

    catch late whole slice really

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
