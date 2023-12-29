4f87a16951e85618a3744cc33c40585f2eccc6c7
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:43:49 +0000

    excuse introduce mess step perfect

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_tuple(array):
+        return tuple(array)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
