322a1245c41c3a248578a042e7aadc52ea1335c4
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:47:30 +0000

    shadow weekend information I cost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def divide_numbers(x, y):
-        return x / y
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def convert_to_octal(n):
+        return oct(n)
