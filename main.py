e02d5da4909d7a9eed8d581fe7fce199b910a358
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:10:43 +0000

    crowded gentle equal generally fit

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_floor(x):
+        return math.floor(x)
