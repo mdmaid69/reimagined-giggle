d2a18edb3f6c311824d61467470bc313c2d6686f
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:39:03 +0000

    pretty tidy confuse either make

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_area(radius):
+        return 3.14 * radius * radius
