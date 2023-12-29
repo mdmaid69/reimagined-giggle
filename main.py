55cfd41ef3631918e25c60db255a3fba74a513bb
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:52:44 +0000

    throw divide ask receive 564encourage

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
