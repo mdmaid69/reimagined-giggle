69b2e99b2a13e40b44ac8ba7b09e3ed855c15fc6
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:28:44 +0000

    hour rain breakin free history

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
