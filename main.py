b701e0832f97398c122a540e28a12e8ee12a5fcb
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:28:21 +0000

    sign board serve shut our

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
