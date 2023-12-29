962cbd01e081ca151013aba54b35e4849d896268
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:42:00 +0000

    reply female talkback body none

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
