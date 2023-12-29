46150ed1f9a994d6dd18fd03d039e4993d0ad0f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:44:37 +0000

    slight interview pay mysterious debt

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_pressure(force, area):
-        return force / area
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+print([x**2 for x in range(10)])
