a14806a12488e215beea932c2a38f65b18896a85
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:13:22 +0000

    steer usual habit close secret

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
