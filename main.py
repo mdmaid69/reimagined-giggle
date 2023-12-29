0e4952dbc0141099f72a7732114c87da4e1f4cb8
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:56:11 +0000

    wash interrupt wish workon Pp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
