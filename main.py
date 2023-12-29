f3df3e1e93c1c1ad601a02649bc3b874394df663
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:25:26 +0000

    sea congratulations national deliberate neck

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_list_from_array(array):
+        return array.tolist()
+print(sum(range(10)))
