c657a48f8bf08303a74ce6dce15ca4ddb019a602
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:32:35 +0000

    positive splash patience bringback holiday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def add_numbers(a, b):
+        return a + b
