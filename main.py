6565df659c26987764e7bd89453398281ac0e987
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:49:56 +0000

    needle stop cut temper judge

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
