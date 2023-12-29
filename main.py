166ad5ce340d051618062886d5e093f4c6b89e9a
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:27:35 +0000

    computer national none free peace

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
