2b5fb77b93b4fd5c646f04ecfd95ef7c8c1e003a
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:57:15 +0000

    rather interruption warn along dear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
