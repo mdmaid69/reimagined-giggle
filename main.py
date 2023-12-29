9749e8ac45158704d91456e8972bc667e01e21ff
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:41:36 +0000

    father accordingto dress open born

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
