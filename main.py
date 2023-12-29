9f04fc44e11875f28013e3dca2d52d9f55af4ffd
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:40:04 +0000

    leg you skin soft accident

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_area_circle(r):
+        return 3.14 * r**2
