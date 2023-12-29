927e7261f0b8016709b9bc04f4094222fde20ab8
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:03:14 +0000

    university knowledge wherever battery Itself

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def reverse_array(array):
+        array.reverse()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
