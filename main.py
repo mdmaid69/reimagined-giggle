94a9bb8a8288cf792d0989ca5f0850b14e903cb9
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:32:49 +0000

    gift rent perform hit silent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def get_array_as_repr(array):
+        return repr(array)
