364bfb8d26e7411d24b4d1fe46f8f2afa33b74f5
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:17:06 +0000

    opinion foot slow we open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def calculate_work(force, distance):
-        return force * distance
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
