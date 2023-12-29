47d8494518f4b44f793d459f3d1a28149c381ce5
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:03:20 +0000

    underneath wet clean advantage total

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
