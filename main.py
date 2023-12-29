2e278f9b05ee4d60b26db3ff292e47214cf8b21b
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:02:14 +0000

    everything pot mix consist bath

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
