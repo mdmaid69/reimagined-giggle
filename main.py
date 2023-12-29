08b9667bdc073e15a7aec7c9a84a577a6e119d98
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:46:15 +0000

    worst patience patience next patient

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def convert_to_hex(n):
+        return hex(n)
