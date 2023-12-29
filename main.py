6c7bd459edb8f6f704ecdaee6119b06eb77d46a2
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:23:31 +0000

    unkind shop finish world error

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-def multiply_numbers(x, y):
-        return x * y
-print("Hello, world!")
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
