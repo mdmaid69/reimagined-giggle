7b6da0637181b5cadb55de864d4cfed475993cc4
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:33:38 +0000

    party race listenfor add active

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
