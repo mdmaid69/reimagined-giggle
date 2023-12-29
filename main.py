53d86be487a8106bc725bb10dcc8b884d8f985e0
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:35:05 +0000

    to container bridge characteristic so

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
