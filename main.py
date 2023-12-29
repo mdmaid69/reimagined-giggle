3488dbe2145baa76ba148e54d8ecb603dd5a5674
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:15:19 +0000

    relief answer place suckme reserve

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
