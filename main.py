530f10fd1d3bd7199651e1643ece49223b74be84
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:23:47 +0000

    fine wave alone job unkind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  def square_number(x):
+        return x**2
