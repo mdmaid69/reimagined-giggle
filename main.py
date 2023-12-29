9832f16b37df74fb6af5815fae9d9721d4424427
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:54:55 +0000

    forward refuse continue OKorokay meaning

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import logging
-def log_message(message):
-        logging.info(message)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
