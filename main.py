8c8a4df697f732b0442f5c0f015dfa7bfd49937b
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:32:07 +0000

    nose them broken pure success

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+print(sum(range(10)))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
