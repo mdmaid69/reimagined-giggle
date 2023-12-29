789ae43c037bec0b7d3c61c01593e84a4bfcef1e
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:43:10 +0000

    Oo centre drive chase call

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
