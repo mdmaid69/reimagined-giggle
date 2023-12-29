4a92f79d2633d8ebb0c751c637087529a9f1d39d
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:49:26 +0000

    occur oil breakinto unhappy surface

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import getpass
+def get_username():
+        return getpass.getuser()
