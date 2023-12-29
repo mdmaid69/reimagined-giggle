9fd2092d7a74b5634d9f4efcd4e7578f822dc6ec
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:42:05 +0000

    earn confident health anything majority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def square_number(x):
-        return x**2
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
