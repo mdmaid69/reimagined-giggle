9e5e73020028c7e61f645b046f35d52ed93f2d9a
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:28:30 +0000

    notice hand attempt damp water

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def is_even(n):
+        return n % 2 == 0
