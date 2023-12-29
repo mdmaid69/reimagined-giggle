c12ecbf169c991ed219ff773b623c459d0dc3bf4
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:40:18 +0000

    amount line image runinto confusing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import sys
-  def get_python_version():
-        return sys.version
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
