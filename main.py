ae9b3ea35a2e38d1a0f0f1e7b2583d3e2677a1c9
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:12:40 +0000

    secret comeup callback machine abroad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
