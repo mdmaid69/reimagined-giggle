2c393b219259cbf4c4e72971541b9d36ab97d785
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:57:29 +0000

    smoke lay mess fat clearly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
