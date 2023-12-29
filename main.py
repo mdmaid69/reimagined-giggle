17a81323a53cf0b4d404ab39f947758d84efcd5b
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:29:06 +0000

    content lookback(on) waste progress worry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def is_odd(n):
-        return n % 2 != 0
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import math
+print(math.pi)
