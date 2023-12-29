b3eec4a4ab8359ac1947e422755fec3b7757af25
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:21:17 +0000

    air entrance telephone there science

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
