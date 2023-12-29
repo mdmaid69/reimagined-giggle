12c864406094e13b97b4a71ff3ff6d388bacccc4
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:06:57 +0000

    pronunciation sail land national tough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_pressure(force, area):
+        return force / area
