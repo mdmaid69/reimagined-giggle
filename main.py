48922c01cb8c8a690ea427545b7d59019fa62fd8
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:13:12 +0000

    still instant drop simple out

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import sys
-print(sys.version)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
