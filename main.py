2d6a55cc9afa7bead5d54a9533918b367f45cdc5
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:28:48 +0000

    official shut sometimes short stranger

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
