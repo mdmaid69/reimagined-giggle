48922d22503aea80cd67908dc0e1b2a234e5d943
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:20:58 +0000

    mix familiar check regular past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
