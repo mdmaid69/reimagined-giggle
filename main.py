5d24322099858d4f0d5e9dd0f99458e0ba03173e
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:50:29 +0000

    apparently concerning why blame funny

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def sort_list(lst):
-        return sorted(lst)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
