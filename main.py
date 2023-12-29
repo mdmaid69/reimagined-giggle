f1c587b32a74314ded1113212582a47ee2c69d17
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:29:06 +0000

    smooth night pay marry flight

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
