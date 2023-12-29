44354bdcaf64f0a8ba4de7fcd7b6cb33585ed624
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:28:33 +0000

    future last concerning used worried

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
