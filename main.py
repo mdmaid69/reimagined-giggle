36b915c6bcb6d97a81491d4f64a9ab641fe4a2f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:08:44 +0000

    its meeting really against favourite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def count_words(sentence):
-        return len(sentence.split())
+def count_characters(sentence):
+        return len(sentence)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
