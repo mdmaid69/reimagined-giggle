a86258017a7cfd3593029930022b0bcbb73da682
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:25:35 +0000

    shock only beyond sense closely

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
