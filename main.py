e94ef52a393b58fed774ff4998d10d46fe8ecefb
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:46:26 +0000

    frequent interview yet some this

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
