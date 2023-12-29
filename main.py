6c78c6ea1b2d428cb069e9cc316bd9db823df168
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:15:29 +0000

    transparent early obtain gas confuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-  def subtract_numbers(x, y):
-        return x - y
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
