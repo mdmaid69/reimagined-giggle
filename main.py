567f887cac652f58d9d61528ca16588d52cf0f16
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:37:17 +0000

    571enough similar wipe demonstrate comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
