4b103bb039de7565d5e4645337c2a5fc05c262a8
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:41:36 +0000

    either then learn level completely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def multiply_numbers(x, y):
+        return x * y
