03efce78d93991c8949a6c76a83cba86d3d73669
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:38:34 +0000

    embarrassed spring constantly miss sale

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
