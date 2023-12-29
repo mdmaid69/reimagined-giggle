9c2d4a38979118d816cf5ee42a8be4fd5b43b46e
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:33:05 +0000

    owe its yet by quarter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_error_function(x):
+        return math.erf(x)
