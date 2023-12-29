ab8abad583ff495fbfc6511a67fd0eaea9a74e3a
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:22:25 +0000

    beneath patience hold tend talkback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
