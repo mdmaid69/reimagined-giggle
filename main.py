9258510e724a4e3139ee5cc4d19a5360de1dda0f
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:27:03 +0000

    community clear stop lock see

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
