ea281e185995f5deb27c41fea9422f898d57030a
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:39:25 +0000

    street discovery Hh consult convinced

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import math
+def calculate_sine(x):
+        return math.sin(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
