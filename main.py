39560e38999334ab953d58bf655d1d81cc8252d3
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:48:58 +0000

    your him stage branch comfort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
