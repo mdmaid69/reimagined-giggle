2a142bc20862d3d7227cb53b5166972df94ae690
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:38:54 +0000

    cross especially concentration meal die

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def calculate_pressure(force, area):
-        return force / area
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
