0fce30f4a96e0d7e4ce0ebbd6839b4009396e497
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:49:09 +0000

    date train turnback fault dangerous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def remove_duplicates(lst):
+        return list(set(lst))
