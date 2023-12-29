e242e6129257128cd14cfd7b3f60a33a14761729
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:32:53 +0000

    embarrassed ahead sing lesson born

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import time
-def get_current_time():
-        return time.ctime()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
