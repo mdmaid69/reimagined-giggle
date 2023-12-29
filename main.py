89ce4e9ba9f3a48a4ac57a9807625121ea4dfe5a
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:01:09 +0000

    spin least until arrange study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
