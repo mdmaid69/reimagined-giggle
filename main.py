9fa088931741e65fe86cb9cf91d5d8b8f6d40ef3
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:08:49 +0000

    thankyou run town brief begin

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def is_even(n):
+        return n % 2 == 0
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
