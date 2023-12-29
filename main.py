10cdc62474cba5f9250a2c63d0752dcd14a258da
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:07:31 +0000

    deserve enthusiastic wire away behind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
