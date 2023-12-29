4796353e757f6bd4793f4489c954be7bfa8ce744
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:35:49 +0000

    neither pleasant content herself uniform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
