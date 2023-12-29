103818188aa411e5358950990ebdc2e32fdd11c1
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:19:31 +0000

    poor plenty quantity home classroom

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-from collections import Counter
-print(Counter("hello world"))
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
