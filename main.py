192ea33580016045338d94ef9b981476736c8138
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:09:57 +0000

    pick wish habit argue pay

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
