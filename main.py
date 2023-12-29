1a68a1e418c744ad0b06769ce3bdbee1c8119e59
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:54:09 +0000

    Kk argue Dd passage burn

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-def convert_to_hex(n):
-        return hex(n)
-def sort_numbers(numbers):
-        return sorted(numbers)
+print(sum(range(10)))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
