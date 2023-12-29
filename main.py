9059accb39d36e1e1c87a1721d92b32ba2ace444
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:37:09 +0000

    close cause noisy fast include

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
