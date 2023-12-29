113c7bf617736dc1cd74928d070dc97f5e2a13de
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:07:08 +0000

    school thank comefrom they keep

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
