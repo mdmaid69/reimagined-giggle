fc66adfd49e53ea2268b4bdf2f5fad6aa4b90ad4
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:05:15 +0000

    say turnout rescue war building

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
