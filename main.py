02dcefbf8021c49c5470431e2c7f3b35715c48c2
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:44:35 +0000

    answer one off check attention

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
