e7c68dbcc295a58490184fe10a432e27216b2462
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:09:04 +0000

    unhappy turnaway record search but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
   def get_file_birthtime(file_name):
         return os.stat(file_name).st_birthtime
