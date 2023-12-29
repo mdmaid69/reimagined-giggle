9f888494bcdb6393d0b8e49a65d7a1d5cd36ce68
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:39:45 +0000

    sensitive point treat temperature when

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
