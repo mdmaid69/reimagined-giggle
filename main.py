375122801e3f3fefad0e1dd9531036ee5fb6d270
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:16:38 +0000

    Cc Tt willing bread bottom

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
