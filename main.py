677c7585844af9544389fa37857fd2d36a7027f8
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:52:21 +0000

    begin attract vanish slice handle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
