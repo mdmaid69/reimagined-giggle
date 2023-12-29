c62641c3cef061d866df288116fdfd16525162ba
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:48:37 +0000

    calmly scene damage while sight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import math
+def calculate_floor(x):
+        return math.floor(x)
