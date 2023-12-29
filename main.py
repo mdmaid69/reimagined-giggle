351bb2ef78c8fb216d3c3a9125c82e8ff1cc1f89
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:11:06 +0000

    feeling excuse slice runinto repair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+i = 0
+while i < 5:
+        print(i)
+        i += 1
