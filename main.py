5ff7cab96599bde52cb890a4a9406f479bd9631f
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:45:45 +0000

    walk mess disappear advantage feature

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+name = "Python"
+print("Hello,", name)
