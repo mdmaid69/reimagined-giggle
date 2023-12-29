a723919a905c47e9d054c465d47b93ce3461323b
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:50:20 +0000

    turn profit tight getup after

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+print([x**2 for x in range(10)])
