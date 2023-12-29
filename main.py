01aaabcd0f91f6f5e1bdf43d534a22e6c5877b58
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:15:16 +0000

    be carry professional find progress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
