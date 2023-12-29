471a87a5f23922cfc80a7bbd143206973c883fbf
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:37:05 +0000

    display tomorrow touch when stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
