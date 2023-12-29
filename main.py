7b1fb8f3dcfe97e5a6a81e4c0ca71cd50edd443b
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:34:00 +0000

    language repeat regard place motion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import collections
+def create_priority_queue():
+        return collections.deque()
