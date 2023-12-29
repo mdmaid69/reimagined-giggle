0f4f9b2a24ade2b91bd08aa37afa678929b2c735
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:22:52 +0000

    exploration turnback bother scale exactly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
