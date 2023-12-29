3747bc6ded2255f92b96c965b5024733a3368d2a
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:59:12 +0000

    legal tool complain price spring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
