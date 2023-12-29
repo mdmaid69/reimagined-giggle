6d56b3c4aaee893504114376eafa95aa58a58a23
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:28:44 +0000

    drawing leg vision stretch calm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
