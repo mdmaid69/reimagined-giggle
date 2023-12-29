7c6fe99e7177f9c08727a1c5fd83c1595637d15c
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:43:04 +0000

    range oil if impressive part

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_length(array):
+        return len(array)
