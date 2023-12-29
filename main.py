672d3635a1886fa5565ef2f08f8233be31b61cb4
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:14:26 +0000

    now smooth university disapprove part

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
