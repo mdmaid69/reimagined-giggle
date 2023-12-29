d48a2a999448cf6be2afea8080e1691664d5a023
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:33:55 +0000

    cover team group difficulty just

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
