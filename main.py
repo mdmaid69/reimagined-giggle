78fcd1f94e652aedd58b97ff7550b53cd014a646
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:38:56 +0000

    cut anywayanyhow foot wall joke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def set_array_item(array, i, item):
+        array[i] = item
