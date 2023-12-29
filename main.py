2991f29fd389fde4e007e8938bf3c871f03cc71e
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:11:25 +0000

    surprising leg side bringback affect

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
