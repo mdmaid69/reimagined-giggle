d432947a531bb227c2a6456581f0bb92087addf1
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:31:05 +0000

    central call exist along accordingto

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_factorial(n):
+        return math.factorial(n)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
