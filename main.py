6b12ee174526245c0562636cbcf4d5f7501c9862
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:23:57 +0000

    buy fork comedown value either

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_acceleration(speed, time):
+        return speed / time
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
