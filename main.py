c02dc6bff535ee0169ad00204433503c5a5a7d1c
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:03:36 +0000

    immediate present murder independent theirs

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
