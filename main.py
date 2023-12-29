29e794723ebdc8d6842ac5546b760f0deaa82b99
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:47:34 +0000

    war skirt as sea combination

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
