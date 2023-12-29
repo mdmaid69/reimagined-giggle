4878d8e3272d6d49b0d49d46bd07d26143da1ede
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:50:35 +0000

    sink either anywayanyhow idea identify

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
+def convert_to_octal(n):
+        return oct(n)
