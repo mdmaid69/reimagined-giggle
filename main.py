e212fa20544ac1ac8653f9fce571786afb4edb2f
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:18:45 +0000

    hand only ago interruption continue

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
