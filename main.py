93a3234e2007abb24b10912d4c9cefb8ca8451fa
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:04:28 +0000

    sleep goback(to) form slightly sentence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
