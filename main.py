731f56876473637fa27d105d7973bcab6656f445
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:36:17 +0000

    remark high much calm Cc

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def get_array_as_repr(array):
+        return repr(array)
