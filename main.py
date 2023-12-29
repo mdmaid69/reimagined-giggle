935b8907705cffc9a245cc13e01135a3d52d8886
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:16:41 +0000

    stone certain opponent upstairs add

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
