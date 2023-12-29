0035e5630a1040d81e3626d72c7953d5bc99c8b1
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:08:01 +0000

    nice cold start grow hold

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
