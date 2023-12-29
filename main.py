da06dcc99c15ace5f73e2831b43b3afa74f09e1b
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:14:44 +0000

    know arrive calloff gate rarely

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def calculate_pressure(force, area):
+        return force / area
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
