88b9f1dac15667b47040e5652596738ce24bdffc
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:28:08 +0000

    freedom protection blame possible bye

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
