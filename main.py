7d39fb9c9133be1262cca0e421bd87bf3b373cb1
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:52:04 +0000

    before compare write belief Cc

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
