3cdcfff3b4462a4ee37dd9af1ea76d44073eb9fb
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:13:52 +0000

    phone ready person wrong secret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-import array
-def get_list_from_array(array):
-        return array.tolist()
