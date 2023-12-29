4d7c4578810e80660b610d256d4b9d0abb311d00
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:35:04 +0000

    takeoff mixture apply interview method

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def find_min(numbers):
+        return min(numbers)
