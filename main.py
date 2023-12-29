7279a5604e273ded94b55cbe736cfd99fdaec7f8
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:23:54 +0000

    Ff wing sensitive transport meet

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_int(array):
+        return int(array[0])
