d8533ef33f60a9e8732894ea3932d3403623ddbc
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:01:02 +0000

    excuse whoever shoulder guess declare

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
