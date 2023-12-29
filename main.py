a2e2c3e0b6f4d3727bf9a8bd2a8e7baa3d29f168
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:52:30 +0000

    identify ilovedicks pleased wish complicated

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
