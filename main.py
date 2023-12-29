1896f68413974de6039426d5b377006ce88b848d
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:38:56 +0000

    frightened load fat aware alone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
