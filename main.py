1901e78360d3088a820410f652d4ad38e25d1c4a
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:18:00 +0000

    better slowly vision them exactly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
