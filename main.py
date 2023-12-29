527b663223bd5d3325eb1070fd2e776a46bdaf47
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:44:38 +0000

    congratulations pot familiar worry confusing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
