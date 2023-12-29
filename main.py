630431c0011f79dc3eff7f8a4184758cfce6895d
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:36:07 +0000

    attempt comfortable total copy shame

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import time
+def get_current_time():
+        return time.ctime()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
