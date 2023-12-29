51db5848dd51ab26a98069fbd5d8bd423ca59610
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:11:54 +0000

    order mean reasonable order whistle

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
