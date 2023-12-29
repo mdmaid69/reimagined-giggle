b73fafad7775fbf3a12457e7f9621f6f884d152a
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:21:07 +0000

    pay balance approve any fortunately

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def convert_to_binary(n):
-        return bin(n)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
