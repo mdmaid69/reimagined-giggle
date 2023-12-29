9b0df445ac191e4e8d082deae8770f393a72d499
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:42:16 +0000

    legal drive contrast simple translate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
