d55fb4c2ec770137fdfde16dc3003799a7440195
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:01:25 +0000

    accidentally bother profit dish wealth

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-print(math.pi)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def count_words(sentence):
+        return len(sentence.split())
