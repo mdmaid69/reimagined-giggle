2f3f8f1f5671e5d405b7e2a79773bdb57f07229a
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:56:32 +0000

    practice cut present laugh picture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_unique_words(sentence):
+        return set(sentence.split())
 import os
-def change_working_directory(path):
-        os.chdir(path)
+def get_file_size(filename):
+        return os.path.getsize(filename)
