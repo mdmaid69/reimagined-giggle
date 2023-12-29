cfa0dfa34758ffcc63f22c41b88737bf3b7629eb
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:40:08 +0000

    everyone introduce regret their sensitive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def calculate_work(force, distance):
+        return force * distance
