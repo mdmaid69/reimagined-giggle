afd28380d649201f026f3ea28f3976763173a593
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:29:54 +0000

    on environment we convincing its

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
