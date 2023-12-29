761406ddffd6c1bf91b31f24c841b35e2bcf1307
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:25:17 +0000

    keepoff keepout face reply heavy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
