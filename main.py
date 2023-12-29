6241f1665896ad8cb6be5840a2182d5a97813a18
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:31:22 +0000

    tone holdup holdon shall achieve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
