5078a45066876182bc51e4b849b80133e2a6812d
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:42:38 +0000

    example quickly lookup fly height

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def sort_list(lst):
+        return sorted(lst)
