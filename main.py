54b2fb980d35282ea8efd78532c19aaec680362f
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:11:02 +0000

    wait afford rather holiday make

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
