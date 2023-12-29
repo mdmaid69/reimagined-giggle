d4a85917f1e132452432a583869eddb6acd6d1a8
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:06:08 +0000

    transport myself worth accidental paper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_as_set(array):
+        return set(array)
