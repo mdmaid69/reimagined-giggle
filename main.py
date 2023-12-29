55902b50240ec2b3881c85ea1b17a923a9811af9
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:52:16 +0000

    otherwise as confused creature stir

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
 def calculate_inverse_hyperbolic_tangent(x):
         return math.atanh(x)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
