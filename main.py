c8ee5dcd3ec1bd79e712c72c233a15b43da2e8a7
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:47:37 +0000

    priority job order risk regular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
