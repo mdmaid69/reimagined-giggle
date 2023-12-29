c1a3f4a93a1ccebf68fd2bc87b11b401809a7f5b
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:50:36 +0000

    pump sleep off strong food

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
