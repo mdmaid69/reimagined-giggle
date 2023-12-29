e568e11c76ddbe9b6fc2343df0d0c8ca286d29ab
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:25:15 +0000

    entry break nothing family hide

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
