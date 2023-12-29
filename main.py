81bbc8af81b5374c5b8c041a06b0154e771045ad
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:05:02 +0000

    damage suppose 568energy building round

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def convert_to_binary(n):
+        return bin(n)
