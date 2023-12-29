917863f1fb0726d5622ac04b060f3c973e3a99a1
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:12:45 +0000

    vanish nervous disgusting safety search

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
