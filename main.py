21e5beaca1dc466ff19dfe423193aad964cf801a
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:20:29 +0000

    fail run contents print square

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
