9fa017aaa764c9b374c717461435549a67c57f16
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:43:46 +0000

    town calmly passage phone broad

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
