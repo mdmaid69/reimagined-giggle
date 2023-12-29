66e28ab4aca43a20d962451e2054aaae08ebd483
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:20:54 +0000

    exchange service ready may talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_base_name(path):
+        return os.path.basename(path)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
