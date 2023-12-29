1aa6d31108e8c9c65a2c2c08049491db78e69651
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:04:42 +0000

    this disgusting belong Bb crash

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def convert_array_to_unicode(array):
+        return array.tounicode()
