2b78bcc99607956c716d83f7cb726c1165011fd3
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:37:33 +0000

    display point treatment beside Kk

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
