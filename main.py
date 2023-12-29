7580221b7061a03cdf89c815d1e2d3385dcd12b1
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:29:26 +0000

    learn target smell consult conduct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
