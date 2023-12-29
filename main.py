3ad8e05912e8bccc270eb94c75e77a71d9a893e0
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:47:52 +0000

    factory Ss food guard principle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_max(numbers):
+        return max(numbers)
