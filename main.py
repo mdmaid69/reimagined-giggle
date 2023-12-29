8ef4832bc1c887ee3777f9ce663db2e296ee9edb
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:10:10 +0000

    this dare nose protest stir

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
