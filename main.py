5b091549f9b2a1aa5719d25b84f1a924f83e00d9
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:47:01 +0000

    run opportunity cold tap ahead

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+print([x**2 for x in range(10)])
