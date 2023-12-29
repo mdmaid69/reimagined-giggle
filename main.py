65c6676ef8ca2251f400235597ba4ec28ad21df2
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:32:53 +0000

    coat read backwards gate minority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
