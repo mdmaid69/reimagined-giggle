ea608ecf31c356782502ebd8e7a35bde3b9f993f
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:06:31 +0000

    cover such own wrong turnon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import random
+def roll_die():
+        return random.randint(1, 6)
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_length(array):
+        return len(array)
