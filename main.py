71055b38748bef23629c3a6edf5f2606f239c220
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:07:58 +0000

    handle here interview bright quick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def multiply_numbers(x, y):
+        return x * y
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
