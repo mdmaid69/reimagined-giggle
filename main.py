91eaa3f180a8337ac0f9d31ea4380e032722d4a6
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:51:29 +0000

    interruption behaviour work fasten runover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_typecode(array):
+        return array.typecode
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
