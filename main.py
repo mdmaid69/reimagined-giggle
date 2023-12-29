477c739007f214c30f3d0ee247df56cce1156b25
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:48:29 +0000

    chase guilty could home straight

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
