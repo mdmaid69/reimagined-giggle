aa5bc8a396c6b5dae1cdcda5dce5ca9a10cc6c8d
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:41:34 +0000

    except furniture cope wear sharp

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
