105fe15317fbd8503d748f8d34387de24ebe7b39
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:35:53 +0000

    plant base happy off dress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
