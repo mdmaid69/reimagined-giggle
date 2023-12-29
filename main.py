40a1f3f8e77b3548e315835f75b60fd7a43dfa27
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:09:44 +0000

    backwards straight repeat tool suit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
