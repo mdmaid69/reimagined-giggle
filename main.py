d5471ad01f7498a91163e1237b52bbaefa5089f6
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:16:29 +0000

    interruption sound exactly new awake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
