46b962c911ff9c77b5fa6503a3f517f88de5035f
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:27:58 +0000

    add anything occasional either whom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
