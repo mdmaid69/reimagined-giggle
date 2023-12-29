413caf2307e61c60d4a1ca6f9c7ccd54a2e28c15
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:59:02 +0000

    everyone force miss complain weak

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+print([x**2 for x in range(10)])
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
