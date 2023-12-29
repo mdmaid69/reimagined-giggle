6766df718596bd14974b4ace75e22d25d6503c5d
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:00:59 +0000

    dance pay stand sensitive teach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def get_array_as_repr(array):
+        return repr(array)
