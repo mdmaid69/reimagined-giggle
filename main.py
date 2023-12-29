eeebb23068b8cfba61d24a5b8d2776c645309fd4
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:44:40 +0000

    if any tradition intelligent relation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_repr(array):
+        return repr(array)
