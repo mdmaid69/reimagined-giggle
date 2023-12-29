7c8f16b63f36860f6f44ea74164318144bff8f3c
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:45:47 +0000

    persuade wherever neat choice instant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
   def get_file_modification_time(file_name):
         return os.path.getmtime(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
