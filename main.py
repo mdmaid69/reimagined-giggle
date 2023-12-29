b84264a2c372b4f4e66321f765101f4be3ec9aa3
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:07:26 +0000

    repair frightened giveaway sale division

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
