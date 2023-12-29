7eb7191ba26f599c9f7c97ebf41c4494e1015cdb
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:38:26 +0000

    switch face evidence tomorrow later

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
