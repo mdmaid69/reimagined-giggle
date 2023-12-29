2db9d7c4ee4c04dbcd4becdee253b1e859804c88
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:43:11 +0000

    advantage promise bag bringdown key

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def remove_duplicates(lst):
-        return list(set(lst))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
