7a13ee8a16913576b045405e2227286bb1589e0d
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:12:52 +0000

    mood without chat Ff if

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
