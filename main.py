ed4deb31d363d57cc895cc9da947e2bc9b231b00
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:03:47 +0000

    goup familiar temperature limit lead

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
