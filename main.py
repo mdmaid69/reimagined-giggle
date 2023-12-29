90d8852eb79a36f5a7afece7bf8facf0bef98296
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:25:34 +0000

    especially sharp criticize explain with

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def multiply_numbers(x, y):
-        return x * y
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
