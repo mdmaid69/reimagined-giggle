43a6cde2ba8d32596fbf8c1d3fa860a1e42e8fc0
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:08:28 +0000

    independent region wherever lift drive

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
