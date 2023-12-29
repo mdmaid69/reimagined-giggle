66aeaf063a4a44150ddd2893c89ebb9f06f28d67
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:50:25 +0000

    summer deliberate peace not whatever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
