5d4f9f6a23797833b69e1789b508a3907f651a05
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:33:07 +0000

    rate narrow performance rarely road

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
