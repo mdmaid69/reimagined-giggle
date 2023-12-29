703d15789b0a77f8ddae73eae3692eecb5407ccc
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:52:59 +0000

    comeround term complain job tip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
