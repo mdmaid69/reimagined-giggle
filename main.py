04d43668880ad712f0b94297b8a808a8a335f6a1
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:46:43 +0000

    over friendly serve communication speak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
