8ab3c9217efe6902a51614108172ae60abd3f9dd
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:06:54 +0000

    apparently foot yet maximum butter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
