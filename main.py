7523c8ac9de3949fe6f8775a22a27d9903a0470d
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:08:31 +0000

    spray suit sharp type favourite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def convert_to_hex(n):
+        return hex(n)
