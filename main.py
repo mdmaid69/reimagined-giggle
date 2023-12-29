7a6ff0546dc6c1596e022cb9a85656fd94e3f7db
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:09:28 +0000

    guess contrast sort border modern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import collections
+def create_user_dict():
+        return collections.UserDict()
