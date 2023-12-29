e966c045826d476790082712fe1ccbe88ea8ac98
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:51:08 +0000

    classroom return could team cinema

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import sys
-print(sys.version)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
