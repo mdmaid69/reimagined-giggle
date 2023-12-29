b01d4311369fcb2b23249bcbecf6717ee4c1266a
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:39:43 +0000

    comedown be human there crowded

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
