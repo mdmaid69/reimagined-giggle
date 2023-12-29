59bc944e46f3136627edcba9f291c5d3d64471d8
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:45:31 +0000

    judge put somewhere consult similar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import collections
-def create_priority_queue():
-        return collections.deque()
+def cube_number(x):
+        return x**3
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
