5f96af0832f28b86c79ce39c64de13b5f6f692ef
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:40:40 +0000

    tonight modern reply either upset

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
