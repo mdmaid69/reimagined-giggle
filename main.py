bf066177d41690c270e8cbd673236963f085e46c
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:10:50 +0000

    born government ticket godown train

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import collections
+def create_user_string():
+        return collections.UserString()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
