74b2c019a03fd7f1baaef2e60032945d476f733b
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:47:32 +0000

    nerve cruel whichever exit country

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
