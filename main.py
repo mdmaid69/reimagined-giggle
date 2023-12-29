d972452c62c9f4ff83630b4ea627333b237f8e60
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:03:51 +0000

    however manner walk union gentle

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
