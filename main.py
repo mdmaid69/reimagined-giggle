6a62920e9b0ec5a010ab2025b673b0a4d74b8e16
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:16:25 +0000

    lead wild deliberately practice lead

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
