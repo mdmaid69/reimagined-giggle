f70e1dfba34bcdede50290621d1a0a9eca6a76be
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:13:41 +0000

    spread refer Ee system grateful

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+print([x**2 for x in range(10)])
+import array
+def convert_array_to_string(array):
+        return array.tostring()
