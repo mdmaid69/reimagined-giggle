a64f7f783756bf9cd47219b3ce62317e89719199
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:57:27 +0000

    570enjoyable sound complaint across bottom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import os
-print(os.getcwd())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_string_from_array(array):
+        return array.tobytes()
