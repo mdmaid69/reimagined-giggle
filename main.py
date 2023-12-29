f51e07ffde29a0f6ca407d27fbbd18bac451683c
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:42:21 +0000

    door preparation fine wild worry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
