4c23eea772289c357d47e0843ad4ba076b426466
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:13:52 +0000

    underneath along demonstrate excellent water

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
