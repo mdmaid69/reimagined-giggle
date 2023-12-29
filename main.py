dd40bda1fd169e2d3a2520b640118169147091fe
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:55:59 +0000

    attempt completely false underneath excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def convert_to_binary(n):
+        return bin(n)
