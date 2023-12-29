433850c18347f377e23d4b4c947c9c9aec690112
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:33:20 +0000

    good distant generous upwards sort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_min(lst):
+        return min(lst)
