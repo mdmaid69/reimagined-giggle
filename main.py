22d799793cad92611c3fec402a954838d4a47bc2
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:50:02 +0000

    connect shadow dark emergency office

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  def convert_to_hex(n):
+        return hex(n)
