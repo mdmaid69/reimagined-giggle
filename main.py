f65d7c152cf127bff028741a1587ea19ca9aa6ff
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:16:19 +0000

    private bottom learn then exercise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def convert_to_hex(n):
+        return hex(n)
