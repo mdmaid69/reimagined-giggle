8e0f14659f8725c41c09b100c7964b48192cd5ce
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:57:51 +0000

    deaf before bringdown odd teach

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def convert_to_hex(n):
+        return hex(n)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
