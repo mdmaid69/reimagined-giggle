d860e5c9fef663bf8e81d6e48570c8e2856039e7
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:38:02 +0000

    report turnaway as comeout cover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
