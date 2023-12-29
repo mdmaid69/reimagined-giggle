f9bcb85aa8711b083ddc15b9380938b898ccb5ae
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:57:58 +0000

    runout safe anyone hard offence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import getpass
-def get_username():
-        return getpass.getuser()
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
