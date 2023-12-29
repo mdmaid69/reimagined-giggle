74cfd08bb3589d85971e349fe318e1cb0c504987
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:04:57 +0000

    fit warm fetch measure stick

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
