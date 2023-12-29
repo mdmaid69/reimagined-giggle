840b519d2b1a279d6b7a2088026c144910dadb48
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:26:10 +0000

    ago ground hard long about

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import array
-def get_array_typecode(array):
-        return array.typecode
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import time
+def get_current_time():
+        return time.time()
