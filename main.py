0960f6819ae82950c58f9ce9d17875c8e4f6ba4d
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:04:09 +0000

    should worried mind unkind image

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
