f6ec0c5da46a20673b2e92b7b5e30e8e9ec6a6a7
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:37:58 +0000

    sit plenty busy mine appeal

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
