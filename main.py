659118cfb182132ea53587e3eb0f13f29bf0a252
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:13:03 +0000

    square upwards display up pencil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
