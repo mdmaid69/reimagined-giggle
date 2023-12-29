de65b2facf2f6f41f9e95c696e97260d68a296ed
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:00:41 +0000

    thanks turn poor knowledge typical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
