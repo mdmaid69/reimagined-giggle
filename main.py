d964e1ac4a72cc734c62c231a848164211ac744c
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:57:22 +0000

    sink bath into relationship disapprove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def is_odd(n):
+        return n % 2 != 0
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
