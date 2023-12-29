328154a10ef219c18aceb41ca706a4617def73f0
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:57:42 +0000

    mistake necessary answer moment sort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
