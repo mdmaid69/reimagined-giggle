7faa70a88865178c2a6145a621f8f1365443e475
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:00:53 +0000

    lunch match bang stone matter

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def count_elements(lst):
-        return len(lst)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
