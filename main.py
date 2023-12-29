4331ab05c146b7994275c855063bb9220498e30f
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:21:09 +0000

    communication dull battery offer spray

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
