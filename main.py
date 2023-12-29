5fb760b6d6cb9d8832485676bd90409acd7f0249
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:21:50 +0000

    disturb expect police consider comfort

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
