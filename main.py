f2642f3d4e52205fe4d8216a7348f6a558b4c8d5
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:32:32 +0000

    face harm intelligent dream practical

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
