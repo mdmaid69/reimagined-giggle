bdb78b6780b97cabcacf020cf18c0df3363b24a0
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:43:35 +0000

    nerve opposite advise chief listen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
