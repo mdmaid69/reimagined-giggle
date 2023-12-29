3f9d791506a6ec38159bc2fbc0ae60f65abbbf41
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:00:38 +0000

    apparently do shop content post

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-print(sum(range(10)))
+  def split_path(path):
+        return os.path.split(path)
