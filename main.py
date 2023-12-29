b18d303c20bceacb07fa0959464a718ac1067c70
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:45:57 +0000

    all anything spray probably takeback

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
