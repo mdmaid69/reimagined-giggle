5ca1243f131dbacb020632c07104310d5574e6b2
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:30:30 +0000

    target knowledge unless trick question

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-print([x**2 for x in range(10)])
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
