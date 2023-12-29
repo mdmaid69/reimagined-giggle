cd50584637163c69ddc7fa4ef46890c10171d477
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:46:35 +0000

    national track schedule food scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
