dd49fb4704d3e63342ff62675ad4ff241bb8972b
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:41:24 +0000

    message wing clear brain criticize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
