2933a794e25cf613a4fc930dbb27bb349e3c9a56
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:56:20 +0000

    different visit wheel safe let

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
