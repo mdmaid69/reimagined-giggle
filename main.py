f7d006876ba329193a12a378d2b4632758ccc4d7
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:38:51 +0000

    age also land until think

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
