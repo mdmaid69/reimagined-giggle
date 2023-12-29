2f34d79bf55fb9a20c9f4c838ec7c33fbd3e6c64
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:07:45 +0000

    afternoon family tidy bury search

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
 print(tf.__version__)
-import getpass
-def get_username():
-        return getpass.getuser()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
