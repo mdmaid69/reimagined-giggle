0cb58069bad429c2048744c5ca4f99df149b0132
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:46:20 +0000

    report past difficulty flight available

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
