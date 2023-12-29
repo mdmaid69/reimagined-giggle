6c6ed57a3b84418a00c9a46d5900d8b461e67147
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:00:03 +0000

    risk flat now male deep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
