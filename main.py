9eb8aa112dfac4ff91c352ebf5126d3d3f6db275
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:24:34 +0000

    we about yours wire necessary

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def remove_duplicates(lst):
+        return list(set(lst))
+def find_min(lst):
+        return min(lst)
