9fcdb2389430936b565d12d03fc82379e584ae2b
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:02:40 +0000

    height scheme preparation turndown past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
