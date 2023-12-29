2cb0fe8a495aa18840e2c36569ec5075a51fccf5
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:18:32 +0000

    interview sudden ground shout colour

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def find_unique_words(sentence):
+        return set(sentence.split())
