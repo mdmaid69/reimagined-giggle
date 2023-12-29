21fec850e907744aee7dcd611cec74b61974265b
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:10:49 +0000

    conduct hungry goup peace both

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-print("Hello, world!")
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
