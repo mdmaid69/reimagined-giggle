5bddb3336e2921e711873cc43d0a5156c7da2bb4
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:29:32 +0000

    otherwise combination kind other slide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
