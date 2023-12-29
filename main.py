7254e1ce7c4fd4403dc39084844169478f93edc9
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:39:57 +0000

    basis goback(to) release considering equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import logging
-def log_message(message):
-        logging.info(message)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
