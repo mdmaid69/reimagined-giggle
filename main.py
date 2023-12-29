134223e9a1176360bf342541fd9845d0f241bdce
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:43:39 +0000

    warm giveout check cruel stamp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
