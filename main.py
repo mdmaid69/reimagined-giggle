4c3976a97300f5e7b132d254fcaa05e6f15d3403
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:50:13 +0000

    morning remove building control immediate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def convert_to_binary(n):
-        return bin(n)
+def find_min(numbers):
+        return min(numbers)
+  def is_even(n):
+        return n % 2 == 0
