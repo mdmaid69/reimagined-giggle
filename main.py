843295a680c2c9423f4d1edccbe737243f1ea8ba
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:40:19 +0000

    design suddenly belief cool ilovedicks

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
