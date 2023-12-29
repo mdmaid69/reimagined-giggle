e441dd8b579e766f922fa59cc7d9070dbc74f579
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:29:05 +0000

    address middle boat commit rely

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
