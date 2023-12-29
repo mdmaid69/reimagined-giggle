e0e067aea94d00f81e9fb3e94ee72adbf5b3bab6
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:35:58 +0000

    worry attitude report reason think

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def convert_to_binary(n):
+        return bin(n)
