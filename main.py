93500fe77ab2d688ff3f4f0c1dc251f9101a6a02
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:28:39 +0000

    fold afternoon tough grade sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
