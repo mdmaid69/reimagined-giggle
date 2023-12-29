715755124ce6a9299b0a078ed4e33c89fa731427
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:33:16 +0000

    charge lose reserve doubt fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
