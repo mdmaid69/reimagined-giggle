951827ed7dd76c08180637434725054e5e320c2e
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:31:48 +0000

    uncomfortable direct concerning fair gap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def square_number(x):
+        return x**2
