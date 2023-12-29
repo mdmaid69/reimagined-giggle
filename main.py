f515110ff48990bca50dbd31d0ca302a2bd01f24
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:05:28 +0000

    black unit surprise activity who

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
