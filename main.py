52b22ca86d2a7b6216fa82559ab52a669ba0b60a
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:58:52 +0000

    polite brother spite somehow rise

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def is_odd(n):
-        return n % 2 != 0
-import collections
-def create_stack():
-        return collections.deque()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import sys
+def exit_program():
+        sys.exit()
