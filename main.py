b53949e0f08b244045b46b74e091fa7849818417
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:02:22 +0000

    touch positive breakfast only term

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
