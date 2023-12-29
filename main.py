65f798851f1b20c3b0c19bf2e1a1756349761be2
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:34:19 +0000

    form method direct bit Bb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
