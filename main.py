94157f75b692da2a3e8cb9ac7c10ee9fec5d1bbb
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:15:20 +0000

    top shock congratulate scrape transparent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
