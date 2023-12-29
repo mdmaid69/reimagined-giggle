9fac28702ab86de2fdb9e5ffdd7e5228beb1fb20
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:13:20 +0000

    hers return even offend introduce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def square_number(x):
-        return x**2
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
