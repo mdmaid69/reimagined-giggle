9c165fd7923a3de2c2d9783c38a9ca8972f1cc39
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:54:59 +0000

    direction into charge raise possession

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
