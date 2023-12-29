6583a8d26fa4bed7d5c215d828109802482683ee
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:01:53 +0000

    demand spin before murder somehow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+  def is_odd(n):
+        return n % 2 != 0
