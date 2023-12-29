879723c46c51ad868f4d7969ff33d2428cdad59a
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:11:52 +0000

    equal hope weather arm branch

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
