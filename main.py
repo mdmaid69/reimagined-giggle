94c438553e564935a420c24881a6b90e9c77f5a3
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:11:09 +0000

    steady born serve breakin action

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def is_odd(n):
+        return n % 2 != 0
