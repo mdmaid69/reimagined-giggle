ab08f44dc18c336838ec1bd2965cd15f8b9d57e6
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:18:18 +0000

    battery squash while heart disgusting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
