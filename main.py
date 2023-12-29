544b1b5daf1dac784798af2f0185d2d97cb43810
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:42:03 +0000

    error take start blue flight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_array_length(array):
-        return len(array)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
