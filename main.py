4d130abef1f519b625635eed9b5523a7b18e6f3d
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:52:12 +0000

    steady collect improvement public step

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def find_unique_words(sentence):
-        return set(sentence.split())
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import sys
+def exit_program():
+        sys.exit()
