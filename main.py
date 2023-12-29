88f33afa51b03715fd28e1d92af0f45651849360
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:33:52 +0000

    relation I turndown noone feeling

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
