755431ede038fa69ba49479d063dfc366c007950
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:51:32 +0000

    price over comment already pause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
 import array
-def get_array_length(array):
-        return len(array)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
