2bbdf027f8d18b7a33af7975da77bccfc6a78a25
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:02:54 +0000

    term become temper afraid switch

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
