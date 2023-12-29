b15f38b7df476cfe6441d10684f3c05478067cdd
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:42:28 +0000

    need mend seriously workon exam

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
