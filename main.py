3477ea97eb8e34a52fd9f91acb9862ed31abc3dc
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:33:42 +0000

    refuse next takeoff circumstance time

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
