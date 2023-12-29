8a8f7d9474e1c2af90bc379dc9722cbedae8de02
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:23:30 +0000

    language must murder this judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def multiply_numbers(x, y):
+        return x * y
