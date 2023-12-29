873e3d8acb3c9e848db5225c14d85f1c11f2fb9e
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:34:16 +0000

    house work keepoff bored genuine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_power(work, time):
+        return work / time
+import array
+def get_array_as_list(array):
+        return list(array)
