cadb599e5193325814a6febcf86b86ad5348cad0
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:31:21 +0000

    Yy rely field consult almost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import random
+def roll_die():
+        return random.randint(1, 6)
+import numpy as np
+print(np.array([1, 2, 3]))
