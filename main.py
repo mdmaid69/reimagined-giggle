423b1b5242f1ccb014be9098a8a4409e92a9eb82
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:48:10 +0000

    listen ought activity giveup progress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
