ec2a4df449bee21879d68edca9fcbdda003919f8
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:37:42 +0000

    tomorrow lend spare estimate priority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
