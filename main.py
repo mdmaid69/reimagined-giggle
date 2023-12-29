e0aaecfa9087a989f62c860b631f01e9cf9decc6
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:46:26 +0000

    rent past side similar now

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def reverse_list(lst):
+        return lst[::-1]
