ab3d5ab91d3cbb490fcdb25d6572df9f714f3160
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:34:21 +0000

    neither plain shine runover obvious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import time
-print(time.time())
+def get_string_from_array(array):
+        return array.tobytes()
