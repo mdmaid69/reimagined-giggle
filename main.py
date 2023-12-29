3e70bdca4a1c5ae49bcc1746367b349df65be8ba
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:18:12 +0000

    intend 567enemy once usual period

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
