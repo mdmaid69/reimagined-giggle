cfaaa69efc808de64ed3a9fb36fd75ee23b5b822
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:04:13 +0000

    perfect Tt concerned view practise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
