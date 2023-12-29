4f44e1ced19e4413468ae550792ca0f9ba1b1a3e
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:31:55 +0000

    afraid unlucky let demand juice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
