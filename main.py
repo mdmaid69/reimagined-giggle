acfcd7f2f430b9877b616049c983d6b95f7f78c3
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:41:26 +0000

    contrast unable operate genuine remark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
