88c2297fd1d74c6198dae0aafe7ab0e3c6ce665f
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:57:09 +0000

    cheek stir straight platform Bb

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-print([x**2 for x in range(10)])
-def calculate_speed(distance, time):
-        return distance / time
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_list_from_array(array):
+        return array.tolist()
