6ffca821da0ba99413a5c32bf752c1ba9a791d56
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:02:12 +0000

    check apologizeapologise order stiff dance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+for i in range(5):
+        print(i)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
