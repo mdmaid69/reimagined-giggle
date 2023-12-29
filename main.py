2342b195749029d39354992cb30b1c48b085a9d1
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:28:51 +0000

    soil city neat under they

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
