406ef957ba1e6dd39698f26d2c18c2be37fef450
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:50:42 +0000

    performance service matter destroy deep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
