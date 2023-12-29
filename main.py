cdd58efeee3bd5133824bb71c67df4f9870537df
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:20:09 +0000

    cinema safety speech throw runout

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import platform
+def get_os_info():
+        return platform.uname()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
