99ffd8f4ae9e316d6fa25827aae18ee6416d1289
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:41:16 +0000

    soft soon drop considering profit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
