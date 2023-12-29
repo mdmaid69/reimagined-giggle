f2bb7c71c5e0e04a99beba358b0b1d368ca9d858
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:19:52 +0000

    practise label voice community obtain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
