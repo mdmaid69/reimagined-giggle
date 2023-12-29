ac1eed9a3994f49b1e0af03716eaa7de047c9f8c
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:11:38 +0000

    Kk conclusion through sign sensitive

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
