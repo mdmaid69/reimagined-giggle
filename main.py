02014e4a816f26a48f002d0b4289c9326b52f624
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:26:26 +0000

    refer news computer attempt motion

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-for i in range(10): print(i)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
