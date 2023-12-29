528737ccbbc37f44b718a35e34651b9efb7a8e63
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:52:27 +0000

    worse uniform about heavy apply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_stack():
-        return collections.deque()
+def find_min(numbers):
+        return min(numbers)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
