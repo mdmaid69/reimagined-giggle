0a4728285d78d4d59ff9acebd82bcebc602529cf
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:56:22 +0000

    usual equipment pleased they balance

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def get_array_slice(array, i, j):
+        return array[i:j]
