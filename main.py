cb6563f0e571dea2e187c9bb1f679822456a2286
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:08:39 +0000

    address ill announce excuse makeinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import sys
-def exit_program():
-        sys.exit()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
