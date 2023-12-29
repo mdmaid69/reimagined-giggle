3525c1ac0f993314d1543ce3bc82b3526868d198
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:40:49 +0000

    match tired appearance unable embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+print(os.getcwd())
