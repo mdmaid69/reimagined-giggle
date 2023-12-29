e9c47ecbb6961049c3fec6753939160b1e65ea48
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:05:31 +0000

    window another protest 570enjoyable it

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_as_tuple(array):
+        return tuple(array)
+import math
+def calculate_floor(x):
+        return math.floor(x)
