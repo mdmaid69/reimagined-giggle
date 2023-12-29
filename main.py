fe63f052a814d6389a4449e0f76fe2ef137aaf44
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:46:01 +0000

    thought post partner name object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
