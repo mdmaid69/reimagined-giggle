4afe1783551e487d768e09462f60ae5edb358876
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:15:10 +0000

    this apologizeapologise neck bringback occasional

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-for i in range(5):
-        print(i)
+import array
+def get_array_as_str(array):
+        return str(array)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
