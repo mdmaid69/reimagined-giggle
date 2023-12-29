6e19c47a74f792cf6158e5d4f1581401eed2f9fa
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:25:32 +0000

    injury search urgent serve reply

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+print(sum(range(10)))
