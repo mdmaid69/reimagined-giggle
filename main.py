c06ed03a7bbc4fc4c9ce591e728f42ac9aa24b99
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:19:19 +0000

    office satisfied breakout crazy you

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
