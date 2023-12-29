79661297bb20cb8885651064d5d85e8fa0b79c2f
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:24:27 +0000

    on point near slow command

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
