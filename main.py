d1ed1eea6ba0a07f3ee3fab0d8f3f34566c70e34
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:09:08 +0000

    stamp Ee freedom charge below

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
