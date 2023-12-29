655c3f818d1d572f97e9da16c8c9700805b4a5e6
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:18:08 +0000

    content trousers cry message sleep

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_max(numbers):
-        return max(numbers)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def find_min(lst):
+        return min(lst)
