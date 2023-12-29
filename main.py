5e82cfd91783cc1738fd8303b501b0a3aaf3e011
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:03:10 +0000

    watch above better in day

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
