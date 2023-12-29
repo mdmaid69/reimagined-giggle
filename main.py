1696c79b952c86b8122369868e5a7dd60a9ddce9
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:04:39 +0000

    round owe boring past keen

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import os
+print(os.getcwd())
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
