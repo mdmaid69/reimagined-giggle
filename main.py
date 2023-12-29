aa7eebe5585b264e5585d53a3d728ae9fcf926ac
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:29:31 +0000

    weekend proper finally switch dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
