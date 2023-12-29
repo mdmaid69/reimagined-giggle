aa0d6645c102dd10ae4c31464fb30f680d9c740d
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:26:56 +0000

    lookup underneath treat leave example

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
