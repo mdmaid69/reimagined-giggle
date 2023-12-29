c7d5d8192401c11e313466e5ef1dfd2b9b9a505c
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:57:03 +0000

    impressed tense neither pull if

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
