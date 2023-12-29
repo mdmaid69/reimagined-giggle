7ff01665914b3b4fc0316da5c6b50730e37388da
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:10:26 +0000

    correction together past mood cut

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-print([x**2 for x in range(10)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
