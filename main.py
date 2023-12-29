f7dc6b0752fe5780412803b3cd7d48243587fe9e
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:19:28 +0000

    flat announce fresh expect wall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
