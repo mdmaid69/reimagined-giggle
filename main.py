b159aaf25e749ecb59915a522899988e1d7db837
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:59:17 +0000

    tight appear spring slowly pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import logging
+def log_message(message):
+        logging.info(message)
