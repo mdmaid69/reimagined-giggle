6d0d5ddabc2028f33c4dcbbbeec4cd7fa1a3bd75
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:28:41 +0000

    interview question disappointed giveback used

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import random
-print(random.randint(0, 100))
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
