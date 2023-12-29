a4fa1eaf600adb64ea7d6df4c1e78bf319963f78
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:16:06 +0000

    flow within object later wing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import sys
+  def get_python_version():
+        return sys.version
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
