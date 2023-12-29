b446caa73b9fb87ac995397011759aaa912f6434
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:58:11 +0000

    sure hit hope mass become

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
