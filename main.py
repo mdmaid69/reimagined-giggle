3266e37f592857962fd47846e17e3c260cc2311d
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:55:34 +0000

    near crisis new own race

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_speed(distance, time):
+        return distance / time
