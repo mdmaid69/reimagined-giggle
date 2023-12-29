b1df1b6e98193763eb3875f8ca3467e9802f79ef
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:30:54 +0000

    rush environment on action children

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
