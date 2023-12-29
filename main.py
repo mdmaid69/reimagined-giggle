01ba9011c0e240e1d150ff6b46d59a47ae1039bc
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:05:08 +0000

    ability protest opportunity long skirt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import numpy as np
+print(np.array([1, 2, 3]))
