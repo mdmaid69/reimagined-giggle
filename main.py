fe35634e4ebef04360a98c6517f4a9377e8b836a
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:46:29 +0000

    date cruel punish inside bring

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
