c2c4095df00250cb9a70db884047590977644dd7
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:51:43 +0000

    please organize straight late well

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def count_words(sentence):
+        return len(sentence.split())
