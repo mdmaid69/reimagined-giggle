f8674e046574b9939c41fd3287d2ca42861e104d
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:06:14 +0000

    sad my excuse measure tax

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
