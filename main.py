cd91d426535adbd6c2a63ff5a3749d1984b2e47d
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:17:18 +0000

    copy authority famous size return

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def convert_to_hex(n):
+        return hex(n)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
