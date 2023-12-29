a78fcca4555ef10cbf0bd69053afe3e565fa2066
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:47:13 +0000

    sorry poison transport wall walk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
