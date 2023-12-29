1984799cf6a41de9c7d59afb68a156b80358b2dc
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:16:19 +0000

    story sport report open information

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
