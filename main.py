89cdf45c08580a10f6c434ff9ae2e343ab18277f
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:14:03 +0000

    home habit none check name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
