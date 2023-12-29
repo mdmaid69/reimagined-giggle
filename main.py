fc2eeeb527c61c286687efbd85d028e38c197458
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:18:17 +0000

    cry rule pronunciation tread fact

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def square_number(x):
+        return x**2
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
