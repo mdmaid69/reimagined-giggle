0e3c83b2b278479f908e6f446169e3efad07c648
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:04:57 +0000

    recent cost breakfast balance holdout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import tensorflow as tf
+print(tf.__version__)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
