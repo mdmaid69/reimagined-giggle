11ac72f0fa02012b838e80f08bb213a3982ca00d
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:59:13 +0000

    anywhere slow proud young open

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
