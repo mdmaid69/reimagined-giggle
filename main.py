1cca2d63ee5431fa7031fcb2f3020aeb6164e146
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:55:01 +0000

    responsibility build earth artificial cheque

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
