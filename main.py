b811945e2414200d9ea3ff8756ee84f3dbdbcc2b
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:27:03 +0000

    appeal today job operate wire

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(numbers):
-        return min(numbers)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
