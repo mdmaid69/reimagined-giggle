ab1db4054d0c8f6fe3c7847cb30d3aab01e1823f
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:49:51 +0000

    new up stir fierce realize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
