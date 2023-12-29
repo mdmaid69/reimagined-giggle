2a21beeba6bcd7e656583ab20e532fe7351e0af7
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:30:28 +0000

    relax brown reasonable person interesting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
