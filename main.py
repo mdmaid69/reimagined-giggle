005814f02a7e5c66870934868fd61ae82c1f56b3
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:13:56 +0000

    praise place Ss clear bin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
   def get_parent_directory(dir_name):
         return os.path.dirname(dir_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
