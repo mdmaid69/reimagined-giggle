ef749baf66571158e0ea2cbd66898b3861a0aa82
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:24:37 +0000

    leave beginning same kick back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
