792001a8d77e286b71c51f8542f2bcc9f109a88d
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:52:51 +0000

    hospital car where load key

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def calculate_area_rectangle(l, w):
+        return l * w
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
