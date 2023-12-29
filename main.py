7ea23bcfa647613656b9fb531b2ccc3c70b2519e
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:25:38 +0000

    result tax survive or mistake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
