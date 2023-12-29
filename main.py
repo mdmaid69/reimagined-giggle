2893e3e0df9e7bc294e83212aa1ef57667094f83
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:34:14 +0000

    slow dream unfortunate spare independent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
