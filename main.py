95ff94cc436265982fae6d8ad811bbe43a1b4de2
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:06:42 +0000

    big shake well contact judge

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  def sort_list(lst):
+        return sorted(lst)
