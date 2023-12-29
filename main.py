cef81dda3eade906c9fd1f317a9ef786e73c2929
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:07:14 +0000

    happen attempt rush underneath truth

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
