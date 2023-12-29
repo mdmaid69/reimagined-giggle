210ec491a1b9d08ae357c791b02e6aa86e7ca88a
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:20:59 +0000

    welcome Oo remark service plastic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
