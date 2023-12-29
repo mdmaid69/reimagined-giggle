482e93e02e42c654b205c4d76620780d3ed04b19
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:19:37 +0000

    confused quick bit below suffer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
