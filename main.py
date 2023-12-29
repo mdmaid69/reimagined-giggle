b992eae656897acee9c26759e01f5321bb52027d
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:56:35 +0000

    strange sore comeround advanced hold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def find_max(lst):
-        return max(lst)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
