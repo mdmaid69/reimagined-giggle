d231e8b958af7b40be88e36d2c0f8fc251666a38
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:33:11 +0000

    suffer argument nature stick left

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
