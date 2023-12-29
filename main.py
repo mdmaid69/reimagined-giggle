10307a7c86316ee7e7123f7be2d6a766c223f308
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:30:05 +0000

    point separate represent comfort Vv

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def convert_array_to_string(array):
+        return array.tostring()
