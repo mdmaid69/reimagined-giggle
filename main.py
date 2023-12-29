9ccf6ca22a2afa0feae8839c047a21ad9891a1eb
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:20:21 +0000

    exactly object skirt upwards further

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
