9388b78b370a5ab176b06b55ba19d36b73b290ad
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:21:59 +0000

    greeting Ee from talkback neither

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
