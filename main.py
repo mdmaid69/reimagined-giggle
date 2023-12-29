f4552f69763639e094ec76418af18a37023b7ad0
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:23:36 +0000

    deliberate concern harm would brother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
