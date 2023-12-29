f8ae99024e58a0a895bb41ed03806fa43c415bb9
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:13:29 +0000

    anything blue serious aim die

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_area_rectangle(l, w):
+        return l * w
