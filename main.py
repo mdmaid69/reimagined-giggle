eb7411a7f1bce28e6876fa02f8f8bd4abafaeb14
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:19:46 +0000

    prove tongue wash other sign

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
