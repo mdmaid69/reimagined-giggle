ef705bd76c29c44281191c4be7fee33878c44006
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:42:59 +0000

    on basis complete blow instrument

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
