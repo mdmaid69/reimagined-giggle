66f596df6dabe8c91d8e1109793109f8df599bc9
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:53:58 +0000

    special arm beside none match

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
