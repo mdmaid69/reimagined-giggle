d12d7193d68351c4bc3793380ef075789891cfae
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:59:37 +0000

    occasional study education bar silent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
