f7c01ddeb903a6b5bc815dd0fd0c400e48d5dd78
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:09:23 +0000

    famous yet shoot careful concerned

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
