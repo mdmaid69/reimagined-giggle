594d39284788cf0927ea11c5e180964cb84f046c
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:47:04 +0000

    arm quickly objection crowded contribute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
