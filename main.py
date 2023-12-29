a887ded29c275c7d5233ace0293464d6f204e652
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:55:01 +0000

    rent hurt cold achieve future

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
