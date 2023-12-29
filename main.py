d0074ebc8b4b3555d4e531b34feace4882b1acfd
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:57:05 +0000

    file basic nothing later song

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
