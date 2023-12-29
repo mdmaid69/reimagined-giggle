7239e0cd5e617ba03f774e42b30a5bb0177f50b0
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:35:29 +0000

    introduce reason hope Zz us

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def is_odd(n):
+        return n % 2 != 0
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
