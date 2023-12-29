942b632f53dacbbe70829d5ce1ba1e371e059e3b
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:28:00 +0000

    contribute question contents out train

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def convert_to_hex(n):
+        return hex(n)
