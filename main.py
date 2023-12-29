22c2bf83b51fd68690abad906454b7908166c9c0
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:23:39 +0000

    splash discovery main match summer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_pressure(force, area):
+        return force / area
