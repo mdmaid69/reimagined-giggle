5ff00af14b58ff6c9e0bb671bb569ad43710f1f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:17:18 +0000

    agree wise soil grateful control

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
