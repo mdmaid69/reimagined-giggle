bde572cca9344ccda59b02c74d32f016de239de3
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:24:16 +0000

    hair recent convenient period interrupt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
