5bff939ceebcbb552daa7477c1369afe789ccb34
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:18:24 +0000

    getaway our not material lonely

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
