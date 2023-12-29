919297b7a0409151cee5793f715dfc3c7a06be46
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:50:37 +0000

    whether inform makeinto better last

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-print([x**2 for x in range(10)])
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+print(sum(range(10)))
