95c18faebb42c01f6250b85229384214de1c6f2f
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:19:58 +0000

    finally also tradition glass score

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
