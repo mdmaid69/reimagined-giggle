25d3bafb374e378611a25388aad57a6134dcc7b6
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:45:17 +0000

    fortunately dress effect splash fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
