c96745e77484a7941cad98ac387eedccf654d359
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:43:00 +0000

    collect type hear proof ring

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_stack():
-        return collections.deque()
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def is_even(n):
+        return n % 2 == 0
