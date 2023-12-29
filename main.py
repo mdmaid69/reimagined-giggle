11a0a8dcd730c744e1c0e3cc3c0d396f4c27062d
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:40:16 +0000

    sure basic broad season close

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
