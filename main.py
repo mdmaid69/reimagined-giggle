2915d555fa6b70d98555cb1cf8b81c441cc9d804
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:54:17 +0000

    space illness prove yourself mix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
