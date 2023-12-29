541ea462cbc95d91974a7982d19782849442a192
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:32:30 +0000

    board into unfortunate develop best

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
