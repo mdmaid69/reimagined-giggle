151b403077ca560878b09f32e919374ca055fb04
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:37:52 +0000

    Zz convincing sister store involve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_area(radius):
+        return 3.14 * radius * radius
