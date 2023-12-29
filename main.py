142ab2c2e026ba2c3c5ff1f83c9301776c22eb48
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:13:19 +0000

    mood step brown likely cap

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
