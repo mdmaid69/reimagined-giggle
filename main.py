98457cf1c36dcdbc8c4db9ce0bd90d0b3a8112a1
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:30:57 +0000

    entrance after grow error grand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
