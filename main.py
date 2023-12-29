14876939ed0d4cebeca6633a4b9ce155be241da7
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:08:06 +0000

    otherwise raw determined class doctor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
