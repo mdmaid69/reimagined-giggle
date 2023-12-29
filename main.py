8ecb0cb18834e32bc258997246e5192bce79113f
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:03:49 +0000

    important glass pretend whole live

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   def calculate_area_rectangle(l, w):
         return l * w
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
