d7d6ab498aa3a4dd38395d5bd1b0a2f47e2909aa
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:10:31 +0000

    justice out collection supply home

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
