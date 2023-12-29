b73f8f87c3d72839e685113697eae000cb044224
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:14:44 +0000

    full outside acceptable difficult wish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def convert_to_binary(n):
+        return bin(n)
