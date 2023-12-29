2329b1b3e72590b0cc9928fd3b5e34bb0d69d874
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:58:10 +0000

    takeoff window attractive calloff environment

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import json
-print(json.dumps({"name": "John", "age": 30}))
+print([x**2 for x in range(10)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
