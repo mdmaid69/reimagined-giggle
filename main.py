de1353ad68b5e72bc89a5805ec813e8673aabe86
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:14:42 +0000

    account off through properly switch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
