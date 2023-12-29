ce1d427c875e037a125967c783e8c60af1a5b361
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:13:21 +0000

    politics speak persuade fit ride

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
