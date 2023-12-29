dfd72d40d0589e493368bb64002f948d6c27ee16
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:47:48 +0000

    receive yourself message could see

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def subtract_numbers(x, y):
-        return x - y
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
