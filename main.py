970cdf181b87ef94784168cb810aba98dedcddf2
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:41:11 +0000

    offer belong police stiff murder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
