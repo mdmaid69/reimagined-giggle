f6150e5ca28b92f8b9acee9f894639137ffe5a9a
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:23:40 +0000

    about stone row boil bringback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-name = "Python"
-print("Hello,", name)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
