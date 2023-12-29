4353286464f9ebbf7a092af1b2dfa01158fd8ceb
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:50:31 +0000

    surprised noisy feed world difficulty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
