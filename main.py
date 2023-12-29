afc88901ee849cee50313dad9a92bec3685760db
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:47:49 +0000

    him tax confuse well tear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
