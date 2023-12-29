4c66567808ee6f00de2852ca0c3894dc64531d4e
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:04:10 +0000

    key completely meanwhile goup breakthrough

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
