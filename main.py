9dfcce7a9d2206a354b3eef35cf779bb3833c635
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:57:45 +0000

    cough strength dangerous tall used

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+print(os.getcwd())
