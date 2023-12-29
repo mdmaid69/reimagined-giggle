7eec11c82cd0e3a79c6ac18eb54287ff312afd21
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:45:51 +0000

    star object estimate kick Kk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
