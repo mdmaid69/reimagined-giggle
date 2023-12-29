e0018d7ec1fcb2d4e6b107b448391a8abbe0f37c
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:37:20 +0000

    previous edge software birth delight

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
