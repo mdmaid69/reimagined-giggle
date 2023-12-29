62ad285c2f693903ca214f71bece35cb2084d54e
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:30:04 +0000

    Tt sense country theirs there

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def convert_to_octal(n):
+        return oct(n)
