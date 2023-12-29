47293318f6681c8b7856b74488e6777908da9827
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:29:42 +0000

    matter foreign sore shine regular

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
