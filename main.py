3836cca8930ff6e87cddb00007e5df22758453d2
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:40:46 +0000

    family vanish picture case hurt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
