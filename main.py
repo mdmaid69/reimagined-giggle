472a848720be1a44995bfca248b6844fd35ec697
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:43:18 +0000

    land progress hit basically face

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
