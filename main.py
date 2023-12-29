18befa1e7387999dfb93c8634f602e94e2268870
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:55:00 +0000

    wet doctor lead rest pour

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def count_characters(sentence):
-        return len(sentence)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
