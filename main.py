d1ff848f5706af5ab7a5ac343e44935953b3aa08
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:36:16 +0000

    quiet sense dance wire bye

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import platform
-def get_python_version():
-        return platform.python_version()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
