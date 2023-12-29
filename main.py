14a062907e6ec6d52845e123f1eb067b23869bcc
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:15:42 +0000

    boring ilovedicks waste familiar unit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import collections
+def create_priority_queue():
+        return collections.deque()
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
