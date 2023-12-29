cb78b52a3b5993ebf7fae2b842e5fd5d18b7fc69
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:00:17 +0000

    create wall obtain community after

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def split_path(path):
+        return os.path.split(path)
