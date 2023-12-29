3a9c9b78959d466e97cb38fa5da4bda0b548faee
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:55:35 +0000

    duty path report tape lean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
