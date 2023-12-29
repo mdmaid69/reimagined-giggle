00357552fdc91ed93b8594c6f33661d4b91cf414
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:49:18 +0000

    wild goout goaway agreement tool

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
