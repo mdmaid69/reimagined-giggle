cb48543e2de86d91f31b90b29b0db2bc09d52c6b
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:22:34 +0000

    exercise right train reaction press

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
