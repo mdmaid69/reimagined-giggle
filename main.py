7a32517b52ff417a411cbb360f7a4d3b3d9f9934
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:14:42 +0000

    discuss top thanks frighten wonder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
