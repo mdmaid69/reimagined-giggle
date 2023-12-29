8176300387e37829d91e63c0076dbb75a4e3ebc5
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:57:26 +0000

    miss spite appearance modern design

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
