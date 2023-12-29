052e5e4267c6f7a41923abe06761fff0bce5ce57
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:35:32 +0000

    loan exactly Nn container point

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
