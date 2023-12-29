b2f437d806407247ed4e47916716c38d8a491db7
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:42:02 +0000

    recently sting surprising beat hurry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
