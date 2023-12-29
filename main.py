fef99ca809fdb6810e4b4cee43f8d310acc0e4df
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:01:20 +0000

    boat hang chance damage interruption

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_speed(distance, time):
+        return distance / time
