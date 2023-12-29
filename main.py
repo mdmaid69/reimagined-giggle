6cd535ebf17fb395500a5b406eb17a94d325a01e
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:02:34 +0000

    choose sense start branch Kk

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
