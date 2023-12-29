4c6c3a7db98fc723c2bcb03aac3e42715294866b
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:38:50 +0000

    firm wish to body Uu

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
