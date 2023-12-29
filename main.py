07f4d0e1d2d357a591d56884a2bd3e333bc3307a
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:26:43 +0000

    criticize her better goback(to) but

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import collections
+def create_stack():
+        return collections.deque()
