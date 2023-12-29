00327ab673b4f67f3222de3f6fad26e53bed4bd5
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:51:53 +0000

    profit 566end little wear plastic

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
