1216f2a5f7e75a33a410248de5fe03eca28a1062
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:23:34 +0000

    continue top whole reply effect

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def is_even(n):
-        return n % 2 == 0
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_volume(length, width, height):
+        return length * width * height
