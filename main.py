e727de585912a6567753bb1faa244bdf2413be4a
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:24:15 +0000

    declare attach fair hole so

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
