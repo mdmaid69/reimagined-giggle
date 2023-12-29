f3b595e098f0c28a064d819f2f95b590300c1c90
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:53:16 +0000

    describe like circumstance ball 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
