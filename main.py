2ca9fbd9555542cfa8a1a5bddbcbb42f6d746e72
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:57:39 +0000

    quarter start transport brave rough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def remove_from_array(array, item):
+        array.remove(item)
