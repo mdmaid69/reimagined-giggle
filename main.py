0abd21438f76bc3228654b3babc2470c1fe468e3
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:05:45 +0000

    neat so lucky certain journey

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
