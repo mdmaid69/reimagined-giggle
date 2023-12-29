699d5807ce50bffcc1607163660e1aa511706ca3
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:32:53 +0000

    push second answer nature regular

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import tensorflow as tf
-print(tf.__version__)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def convert_to_binary(n):
+        return bin(n)
