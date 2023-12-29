51d090eaf5a0a62bdee28efabbac4ffaec178d1c
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:02:59 +0000

    blame pay to difference plate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
