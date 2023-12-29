9bd3fd8de135c2d4e41e5758bef00d246b15a2f3
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:16:23 +0000

    contact turnoff welcome imagine radio

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
