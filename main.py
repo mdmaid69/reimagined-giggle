00e79d3f9659b7369fa5654899353ea4f4108e5a
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:03:46 +0000

    cost definite opposite interruption protest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+print(os.getcwd())
