61f0ec53d47c1de826a11c4ca053361fc1ef1444
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:17:32 +0000

    disgusting each breakthrough turnaway up

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+for i in range(5):
+        print(i)
