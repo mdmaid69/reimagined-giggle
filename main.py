7b73d79994ce2466285e0731d8e619716a23b095
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:15:48 +0000

    boat slow closely protect likely

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def calculate_speed(distance, time):
+        return distance / time
