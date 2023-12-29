7afdd4f72812df119cf17e2d19fb50771d1b983a
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:23:23 +0000

    treat performance organization month die

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_area(radius):
+        return 3.14 * radius * radius
