40ef90c9ee8940cae9314fc24c0f0dcc9c815f60
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:54:06 +0000

    band represent Ee concerned rain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_repr(array):
+        return repr(array)
