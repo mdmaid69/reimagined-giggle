da2a1de3317a026bd09eaa0aa8c903d71bef5228
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:44:49 +0000

    opposite pain sticky fierce will

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-for i in range(10): print(i)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def greet(name):
+        print(f"Hello, {name}!")
