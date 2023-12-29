77ae303fb168a987ca63c49d69dc2fa004cfcb7a
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:37:31 +0000

    Ff either best question finger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import re
-print(re.match("h.*o", "hello world"))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
