a13e37e3893cbd6a64a5096fd26b170e858dd86a
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:51:05 +0000

    sail judge release Uu cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def greet(name):
+        print(f"Hello, {name}!")
+import collections
+def create_stack():
+        return collections.deque()
