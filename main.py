baca2f9084bd0a316f9411c355e3d627e05001e0
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:40:13 +0000

    old open hard force choice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_area(radius):
+        return 3.14 * radius * radius
