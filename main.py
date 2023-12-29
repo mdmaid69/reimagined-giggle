c342d52150695b556dbee5ee4b4ad64fcd52c48e
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:55:49 +0000

    century peaceful classroom block getout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def subtract_numbers(x, y):
-        return x - y
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import os
+print(os.getcwd())
