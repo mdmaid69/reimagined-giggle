167d61858ac7d8659297140eee613b5e2e73e298
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:23:54 +0000

    law tidy police age uniform

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
