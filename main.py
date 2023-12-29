a0c3aa7a7b929dfa7f71bb8b7a890cb7b61bbd73
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:39:15 +0000

    aim sing trick unpleasant recognize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
