ed221844ad78204af42aea08ce6b8143732339ed
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:57:26 +0000

    keepoff feed explanation condition comedown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import re
+print(re.match("h.*o", "hello world"))
