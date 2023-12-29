168c47a33f04478915afc92eedbf61ebe16a9c75
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:35:39 +0000

    lesson benefit play quick form

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_sign(x):
+        return math.copysign(1, x)
