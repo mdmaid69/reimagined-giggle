98ad71d593d49439805aeba5b9ad8fc7d8bb29cb
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:33:41 +0000

    scream holdon genuine side getaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def append_to_array(array, item):
-        array.append(item)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
