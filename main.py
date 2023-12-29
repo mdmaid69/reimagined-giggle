daeaa0f93df157499665522a90dc8c20d13f5bb5
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:23:31 +0000

    public gas near luck circumstance

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
