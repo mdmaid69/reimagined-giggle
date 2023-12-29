3e63ee9894a993deefa23f173209c65306ed33c1
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:48:56 +0000

    talkback tip sleep top create

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def set_array_item(array, i, item):
+        array[i] = item
