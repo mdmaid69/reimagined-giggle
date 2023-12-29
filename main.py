1cb374428342bf1dfbdfeb06b289ef81aa07a0e5
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:04:24 +0000

    like confident sore friend scared

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import datetime
+print(datetime.datetime.now())
