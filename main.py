52cb24627ee43ae314b83eebcb1190d80a74c893
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:39:43 +0000

    slice quality mad flood contact

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import numpy as np
-print(np.array([1, 2, 3]))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
