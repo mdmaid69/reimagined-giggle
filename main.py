d69e627365ac96254bcfa5f5d7dca6ec08d33c8f
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:36:17 +0000

    such shop free offer spare

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
