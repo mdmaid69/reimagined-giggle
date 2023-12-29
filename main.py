8cf8de4d94e7ad6ea4a0a901d17ca2bc22104c37
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:36:46 +0000

    heart angry nature quick problem

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
