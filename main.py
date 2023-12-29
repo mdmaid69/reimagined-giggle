b117a33e0799fafda26dfc14f815a16e90630a44
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:38:41 +0000

    organize pronounce nonsense which capital

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_pressure(force, area):
+        return force / area
