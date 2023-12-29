c8f63c572490afb3c4177efc3ef64a8ea3457547
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:33:30 +0000

    confuse mistake meet blame belief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
