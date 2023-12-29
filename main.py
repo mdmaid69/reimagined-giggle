57ad206b3e9ebfb2721fa508611fa782d41bb47f
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:46:02 +0000

    thumb rough pleased start visit

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
