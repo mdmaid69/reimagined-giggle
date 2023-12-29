bd16fb63d2f889ade8d0e7d13982d9f0d7537116
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:52:09 +0000

    change stamp drive favourite furniture

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
