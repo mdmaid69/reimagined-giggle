5c0b6353c10222ba755f50b9751306dcfaa7f3a3
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:55:13 +0000

    behaviour slow crowded too goodbye

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
