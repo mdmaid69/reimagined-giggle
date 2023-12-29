fec41781417a43ab1d0305930066a2718bbccf7b
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:50:51 +0000

    open whether move surface buy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def calculate_density(mass, volume):
+        return mass / volume
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
