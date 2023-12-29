a172f9924c677de39097c5a228b1582679c1624c
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:53:45 +0000

    between live drop rid move

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_power(work, time):
+        return work / time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
