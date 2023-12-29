c7aa3edaad9f20d50b52ec278379992b1bc39a61
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:12:41 +0000

    dress close however possible constantly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_even(n):
+        return n % 2 == 0
