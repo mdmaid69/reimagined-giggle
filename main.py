64cecf8bccb32f9f32c7532cfba171d1cea5be7a
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:11:01 +0000

    commit bringback crash dress sail

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
