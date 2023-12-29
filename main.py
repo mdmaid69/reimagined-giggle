a43ec344df0cc694e568d7f3f05f9041c96fa87e
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:33:11 +0000

    union climb frame basis comein

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+for i in range(10): print(i)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
