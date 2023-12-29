2be7c80173a00a4cca6b5bdf3e69dda9967d8d27
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:26:11 +0000

    total sell pay bin flow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
