cf533d4160904ada03847ea685427f8d1106aa06
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:20:37 +0000

    night die recovery close first

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def get_username():
+        return getpass.getuser()
