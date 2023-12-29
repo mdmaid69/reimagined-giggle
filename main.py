a81e240d74c9917be53dcafddccde4e2f74fad7c
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:08:16 +0000

    crowd smile wild bottom write

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
