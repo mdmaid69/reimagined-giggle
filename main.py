7cd67f69e8aad9b75108482ff32984d6dc67319e
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:16:59 +0000

    mark dig lost dream niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
