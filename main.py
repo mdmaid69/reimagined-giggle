155ac910a3dcac8032aa0b5cd2993c3bc4f98a09
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:40:52 +0000

    smile bet giveback hour stone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import sys
-def print_python_version():
-        print(sys.version)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
