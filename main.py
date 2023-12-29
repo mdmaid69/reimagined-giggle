235ba11139f2dfdb33647d5576dd83d95d01ed6e
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:07:22 +0000

    already enthusiastic neither grateful morning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def clear_array(array):
-        array *= 0
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
