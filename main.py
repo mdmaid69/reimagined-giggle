0b272ceeae22885ec152e3b3044ecbde13cb4a13
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:26:24 +0000

    better dry them area bin

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import sys
+  def get_python_version():
+        return sys.version
