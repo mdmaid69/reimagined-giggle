fef0ea95c4892fc98848950b924590e48f7bdbea
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:28:39 +0000

    meanwhile ill left other raise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
