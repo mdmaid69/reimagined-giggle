a44e92866409546ef983a5ba62e4ce98752b693f
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:50:13 +0000

    accidental mark meaning rough regional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
