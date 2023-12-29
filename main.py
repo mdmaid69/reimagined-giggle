1cd19f42b1533d9a483748d698e954fb4b056f9e
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:05:44 +0000

    after yourself opponent comeup post

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
