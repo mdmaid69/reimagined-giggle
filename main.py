d6b35e15d5ab318599b9486d7c8c91ca83f29bde
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:05:19 +0000

    slowly give gap human decide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
