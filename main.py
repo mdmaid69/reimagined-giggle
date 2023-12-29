90c30cd59882f4b0e2b525f2f6738f0f2d317896
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:24:09 +0000

    realize safety nature corner list

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+for i in range(5):
+        print(i)
