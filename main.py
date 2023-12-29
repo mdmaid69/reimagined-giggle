4f0b0ac5d480d94d276adee86f9fe733924d9895
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:09:22 +0000

    remove shut stop tell modern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
