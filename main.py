eedc0323659bf26f64aaa69887bbcb7149db3e84
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:43:03 +0000

    next wind concentrate 570enjoyable wave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
 import os
 def list_files_in_directory(path):
         return os.listdir(path)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
