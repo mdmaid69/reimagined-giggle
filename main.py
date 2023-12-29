ae81396d3cf6d626a27e76dc76eba5794bee4aaa
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:30:13 +0000

    listenfor tend rare skin deep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
