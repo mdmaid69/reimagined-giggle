994e8752fdc462c12c487de6ead1f436feba3c8a
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:58:09 +0000

    protect pretty keep range raise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
