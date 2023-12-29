3186cfcccab598aeabe6caeb77c5e0407b2fb3fb
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:20:32 +0000

    object again area improvement without

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_speed(distance, time):
+        return distance / time
