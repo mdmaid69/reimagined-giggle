afd45c3dfd7bd5012d0ec0d513c019f503e69d93
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:49:33 +0000

    tough callback moral judge top

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def reverse_list(lst):
-        return lst[::-1]
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
