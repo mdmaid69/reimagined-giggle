c5c7ff147f7b6fcb3bf9fc851dfb4fe82698f15e
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:24:29 +0000

    takeback in yourself fix shout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def cube_number(x):
-        return x**3
-def find_unique_words(sentence):
-        return set(sentence.split())
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
