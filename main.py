ccce560de7544d92706ab30defeb27bce0af0e72
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:17:46 +0000

    serious summer pump however slide

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
