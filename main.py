cd05c0cfa4592aeaa0e984c9bf3acd953791f664
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:19:39 +0000

    invite season bread prevent hope

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
