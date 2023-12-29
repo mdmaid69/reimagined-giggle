a95fa48a50be05874c613af1c6f9dfbdca33ca1e
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:44:03 +0000

    sink immediately project Qq rain

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
