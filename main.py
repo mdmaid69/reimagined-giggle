b216e5cb49e621b1cc61a06b947219533a41208c
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:26:58 +0000

    draw solve job way brush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
