2108925ea49220f2c64231b449747827ab107641
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:21:30 +0000

    basic nice lookaround concerning control

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
