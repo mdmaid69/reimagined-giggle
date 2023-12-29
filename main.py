ad67a57ce55a3ac17b5b131e22bf4b66d70a14c6
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:00:54 +0000

    obtain wear temper bye before

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
