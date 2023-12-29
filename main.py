1d6e95339b857ec7c8d272af4d83f9e4c9067c07
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:15:42 +0000

    patient pour they closed Dd

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def multiply_numbers(x, y):
+        return x * y
