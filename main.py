2b9d60f9a8d4e88f49df35d4e082c131ecef9feb
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:26:33 +0000

    agree train in holiday month

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+def convert_to_binary(n):
+        return bin(n)
