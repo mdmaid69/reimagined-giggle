5ed6f711d8eb86bda6625c8cac2977895ef9b2d0
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:52:10 +0000

    skin amount tool door deaf

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
