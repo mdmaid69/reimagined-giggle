8d3a1a02f92fc026744be90619611791de3bc27a
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:31:57 +0000

    occur protect include satisfied play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
