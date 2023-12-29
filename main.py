058510a10c5a68ee70588c10d4430a6de7e4a967
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:37:53 +0000

    appeal pleased within then beginning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import datetime
+print(datetime.datetime.now())
