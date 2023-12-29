e52633b3df9ae44df43a92f0cc03f43192e05ce0
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:07:59 +0000

    conclusion create process field Qq

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
