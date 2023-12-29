6ab57c87f8ee118c006497e90ac4c71a87a2544c
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:51:26 +0000

    hit future difference bread failure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import sys
-print(sys.version)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
