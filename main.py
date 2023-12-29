8d1f2ac9057fd021808f6c8fb855cb1c2b1c2b48
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:45:53 +0000

    break pretend ahead fail course

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
