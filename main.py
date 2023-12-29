dff64f6cdb51921e7a0fb9288731beec34df9f16
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:49:34 +0000

    fond direction hit adjective gather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_typecode(array):
+        return array.typecode
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
