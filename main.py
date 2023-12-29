935a903ef52196ab9edbdc58a159b0c523ff5752
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:23:25 +0000

    thumb formal long lean just

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
