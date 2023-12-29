097435906f6022c922a45196160a5d0af6c48b88
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:12:03 +0000

    consist switch replace next clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
