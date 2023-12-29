f465c48b65cee5c8068c459b1d56d00fa9baa927
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:44:03 +0000

    charge evidence grand than mention

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
