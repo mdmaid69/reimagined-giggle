47bef4fd14bcd22e1d64093025f9bc1794069eb6
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:44:35 +0000

    doctor later declaration motion possible

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
