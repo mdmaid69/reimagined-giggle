5364a5c06e17c0c1e34ea9ff56bc775f8147dcd6
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:38:40 +0000

    will bang look hard do

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
