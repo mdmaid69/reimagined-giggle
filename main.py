b4e09c72752855879fc992c0b983645291335ffe
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:41:05 +0000

    always shelter fear strike careful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import tensorflow as tf
-print(tf.__version__)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import time
+print(time.time())
