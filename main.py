d7b6746e8b960f01e9548f36efd73a6e4306193e
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:46:45 +0000

    ever splash leg invite failure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
 import sys
 def add_to_python_path(path):
         sys.path.append(path)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
