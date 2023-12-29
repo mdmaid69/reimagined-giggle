4ca5c99ba5d4ddfbd2416c2de2d37a5ded898864
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:58:39 +0000

    thinkof travel height minor consist

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def greet(name):
+        print(f"Hello, {name}!")
