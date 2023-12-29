873e30668d166d57718e0f6f594112ddcd642cb6
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:39:10 +0000

    country very persuade yourself exam

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def count_elements(lst):
+        return len(lst)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
