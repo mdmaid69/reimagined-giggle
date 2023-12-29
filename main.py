3c660ef02511beff3bf052765232a483635d1d26
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:45:58 +0000

    load influence beat ignore turnover

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def count_elements(lst):
+        return len(lst)
