084bbd2f514cc060ecce75b188d29cc43a8f35b8
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:32:48 +0000

    honest answer upstairs callout medium

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  def is_even(n):
+        return n % 2 == 0
