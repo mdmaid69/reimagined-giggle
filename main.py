530b0b66da6a991161e62b5d402b36b047beddbe
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:42:57 +0000

    needle bright loud gather past

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def count_elements(lst):
+        return len(lst)
