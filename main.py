43543ba37b315f252fc0e5f9246234be9e3b16f0
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:42:13 +0000

    lead wing claim goodbye side

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_power(work, time):
+        return work / time
