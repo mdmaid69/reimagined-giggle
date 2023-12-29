1ca9c2eac74d347fba93b92e3be13c83dfe1397b
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:36:07 +0000

    store ours stop region left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
