ea98a17fed8b09fe21ec02f87ad6f110242339d4
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:35:42 +0000

    vision of comefrom marry limit

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import os
+print(os.getcwd())
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
