c5f58633b97db8cd266b359140ee267256c46cc1
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:43:57 +0000

    protect drive receive correct mistake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
