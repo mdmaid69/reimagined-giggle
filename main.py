e6a5a58c49ad5729c5df1866de7ded0fb797acb2
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:24:49 +0000

    however message slight past climb

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def is_even(n):
+        return n % 2 == 0
