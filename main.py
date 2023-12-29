63c514e7daca87c8dab0e85f94ec9f3c0478afea
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:11:36 +0000

    press already communication grow upwards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
