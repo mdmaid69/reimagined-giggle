227133a3ef94f2b047882400a39d7e6c919d2e2e
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:08:29 +0000

    news zero artificial represent accommodation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
