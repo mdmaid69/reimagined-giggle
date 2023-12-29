7b97ceaa1e2de31a8483dc18a08eb281f2bd5a33
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:36:46 +0000

    Oo count repair appear spring

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
