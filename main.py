4240703a101e208f2171fddb71411d4394913c77
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:09:23 +0000

    straight live ask certainly company

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-print("Hello, world!")
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def find_unique_words(sentence):
+        return set(sentence.split())
