34666038a1d1d84f84bbe864fdeec5fd3ddb8f9c
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:31:03 +0000

    Jj sort fast goback(to) spare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
