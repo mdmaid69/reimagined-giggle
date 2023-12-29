e0b16d08a72c4937d9b20291ab962b9dd88baf5b
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:27:56 +0000

    proof why damage deserve room

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_sine(x):
+        return math.sin(x)
