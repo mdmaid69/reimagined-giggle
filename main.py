6a20b567734515aefb5ef106a97eae4c3e3c4fe4
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:26:42 +0000

    Dd offer road through copy

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
