ef49f9f0de3abf34ca65eb999adcc46eb70240c5
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:41:12 +0000

    holdup support basically book taste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
