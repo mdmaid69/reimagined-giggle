1997324ed3b6860bce08f87c07361f43fec2ed4c
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:01:36 +0000

    schedule note school combination pity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
