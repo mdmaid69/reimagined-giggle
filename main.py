df70b81636c63f20f1b026b32acdc658d7bb57e1
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:25:11 +0000

    effect project prize meaning within

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
