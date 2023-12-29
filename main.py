5d238f7e6b7a461b7fcc5d1d97a1273a0b809043
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:35:16 +0000

    twist marry rid convincing arm

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def convert_to_octal(n):
+        return oct(n)
