262c7dee15043d4c3f9bdbf094408f58175f263a
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:15:56 +0000

    off unable tell impossible worse

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
