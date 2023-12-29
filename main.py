43c5231f231cb4ed0c02e03a6fbb1d260289416d
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:22:57 +0000

    crisis shelter wing delight half

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-print(math.pi)
-def reverse_string(s):
-        return s[::-1]
+def calculate_power(work, time):
+        return work / time
+import array
+def get_array_as_int(array):
+        return int(array[0])
