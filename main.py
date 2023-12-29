fa8db815d434b27eef92a0b3cc2150f99a9a7e25
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:12:00 +0000

    dance free slice our fair

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
