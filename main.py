deba5051f6b9175fce90649a223c00edcd9e1835
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:46:00 +0000

    waste now far fresh unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+from collections import Counter
+print(Counter("hello world"))
