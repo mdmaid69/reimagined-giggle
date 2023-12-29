5992515392bef72b89eea76c1d969f56f88b52b7
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:55:21 +0000

    emphasis near fence congratulate smile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
