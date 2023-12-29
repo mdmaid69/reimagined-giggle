b342228bf9fe5507b90f8f9daee9a291c301ad85
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:59:38 +0000

    interest breakinto young knock not

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import functools
 print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
