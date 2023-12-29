21315397f96ce077e6d0eb5ee9e753c36ad786df
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:04:10 +0000

    style concentration breakfast wish trade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import platform
+def get_os_info():
+        return platform.uname()
