425b516392a5b42596b1c1022ede7578d9744279
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:58:53 +0000

    familiar receive night raise public

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import random
+print(random.randint(0, 100))
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
