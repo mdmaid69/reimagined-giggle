bc1eae5bcb5c5281c0056b63ecbcd0751e9113e9
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:46:44 +0000

    normal damp charge burn improvement

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
