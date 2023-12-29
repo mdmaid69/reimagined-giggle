f2f65cfc817fe58ada9253658119b6935dabf435
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:05:10 +0000

    runaway all ring point accidentally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
