c86fb81ddc7b4bfeee017c1784a7f182aac22539
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:18:07 +0000

    sense dream hurry disagree urgent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
