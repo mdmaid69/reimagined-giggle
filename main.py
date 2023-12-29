0ca7e2d879213edd8511ac41c27285b4a1debd10
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:57:23 +0000

    tomorrow Nn what cinema talkoutof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
