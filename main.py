0e298edb99adc2b155159b01a40e910d09e89bcf
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:16:12 +0000

    form think time quiet 570enjoyable

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_speed(distance, time):
+        return distance / time
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
