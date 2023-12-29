cd6dfa2755c29ca8c4b21768ae80d8753e08756e
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:50:03 +0000

    in offence valuable swimming turnaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
