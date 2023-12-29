83f64a7a98b92f2798ea76e6984af3a76ef6c746
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:06:19 +0000

    alive female fold clearly along

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
