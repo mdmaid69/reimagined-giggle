9aada6b22b6cf58c7f18cea5c57262ce4c89e972
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:12:14 +0000

    set generous mistake life give

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
