6679a529f02b7435828de881b84bad3ac4cac653
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:28:07 +0000

    level loose flat practise neither

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
