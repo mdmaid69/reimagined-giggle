333e589981127b108c766f530f39e9a7efcaf384
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:30:09 +0000

    look tour band wrong face

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
