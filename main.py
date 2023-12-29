4d0cbfe1922035bb76015391572ca12b244d9d8d
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:17:18 +0000

    drop task admit like shame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
