5bfab0d08cb8a407842290f755b80712feacc1a3
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:37:58 +0000

    cough mother perhaps quality section

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def cube_number(x):
-        return x**3
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
