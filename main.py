e5f112ffa602f2502da0843afab8067f7144d29f
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:17:11 +0000

    dry 573entertainment to careful before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
