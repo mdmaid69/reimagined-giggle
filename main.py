09f83a3987105bb45fddf47cd0ad7cca02d72f89
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:28:20 +0000

    under save anywayanyhow hour stretch

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
