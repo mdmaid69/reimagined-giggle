82a36bc80c2d22348af930c28c6b2f3247510444
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:46:33 +0000

    level gentle comefrom happen light

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
