b2f202843a16ea8f119bf58cc33979bdece15399
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:29:04 +0000

    rob nice minute joke exchange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import logging
+def log_message(message):
+        logging.info(message)
