37a6e10482eaa59090d17a5e03cd4a749f6c384d
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:25:32 +0000

    upset mass 567enemy both fold

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import re
+print(re.match("h.*o", "hello world"))
+import platform
+def get_os_info():
+        return platform.uname()
