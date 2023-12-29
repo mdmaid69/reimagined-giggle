9ad9144a8dad801ed66f282ac2bb4cb35efa0e34
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:12:30 +0000

    worry lie write throw shut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def convert_to_octal(n):
+        return oct(n)
