d15d2fe0452153d8b5ad85f368d637fcae57f26f
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:06:25 +0000

    Bb jump passage computer whom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
