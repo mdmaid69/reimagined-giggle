d0e501d7d94569161e9b3bcede1955ddff3d4941
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:01:19 +0000

    Qq lift platform plate lie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
