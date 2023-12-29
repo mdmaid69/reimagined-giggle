d1125707710980c8abee55e83d159013e913e8ef
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:55:32 +0000

    natural weak share convincing proof

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def is_odd(n):
+        return n % 2 != 0
