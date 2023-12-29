62ddf3821f035317e473da3e3d116bcf3f3bfad4
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:12:21 +0000

    cover course punishment discussion storm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
