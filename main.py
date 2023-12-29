f8aed5d718d9f0944883484f00dc3f1fcf552f97
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:54:36 +0000

    rescue bunch behave talkinto reason

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
