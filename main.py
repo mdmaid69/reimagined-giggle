174649d818caf1547e02c1eeabeb5e3e1f3fe1b1
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:37:49 +0000

    takeoff sound hello edge previous

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
