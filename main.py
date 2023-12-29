91b9aec1e133b204cfb92e6b02a78fd40a465669
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:26:31 +0000

    turnoff decision sentence press load

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
