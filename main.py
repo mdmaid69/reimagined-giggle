e96c8f520ca0033f78febf9df996c292361e91d2
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:32:52 +0000

    look wish service risk female

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import tensorflow as tf
-print(tf.__version__)
+def reverse_string(s):
+        return s[::-1]
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
