6ec1daf1a70cdaad0f841c8af5cc88394e82b76f
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:32:06 +0000

    but bend love splash region

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import array
+def append_to_array(array, item):
+        array.append(item)
