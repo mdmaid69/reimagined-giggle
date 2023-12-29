bb00b4a45a562b71af8c8a0f19f08698580e5f3c
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:25:16 +0000

    your negative sound sure standard

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def is_palindrome(s):
-        return s == s[::-1]
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
