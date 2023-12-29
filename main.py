f04ce728eaa45d7871ae22c1c1a22ce8378f8ac5
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:07:00 +0000

    practise recommend wait row change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
