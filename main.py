ef1508147d341e1db41d1c13fa55609e571aba1c
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:06:19 +0000

    right and container give vision

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def find_max(lst):
+        return max(lst)
