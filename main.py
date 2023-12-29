3b8df161285eb7907925143edb47f6704f398334
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:08:34 +0000

    confident where move poor unpleasant

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
