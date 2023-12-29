5b822f568c855ea4187243b41e33541404b2186d
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:50:45 +0000

    dream polite female down copy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
