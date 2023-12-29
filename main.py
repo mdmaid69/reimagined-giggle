c164a0b349d41722a66d58ac894389f474d0724c
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:55:55 +0000

    fix very near rub listenfor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
