5341be0cb6a97cf7f98e3bf692da628b8819a06c
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:31:24 +0000

    smooth sure purpose cause capital

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
