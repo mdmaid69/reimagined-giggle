5425badc9267636f454c5ed3144ed5d4c3e26931
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:54:15 +0000

    Oo nor weigh uniform record

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+print([x**2 for x in range(10)])
