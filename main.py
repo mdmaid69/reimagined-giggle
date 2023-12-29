5196527aacd35b17478a6d821302096e85f9badf
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:58:32 +0000

    plate trade should every such

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
