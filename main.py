c67ad22ceaf44d170243608da335671b5a50fd33
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:08:29 +0000

    contain through anxious closed another

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
