c19d5db99ede4bed102d89f8f56b8f59398fbda4
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:51:24 +0000

    region raw statement subject mention

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def multiply_numbers(x, y):
+        return x * y
