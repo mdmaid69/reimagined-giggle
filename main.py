fe057e36370c235cb1980aa81e5beae39fdf7d10
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:27:20 +0000

    regret please year him character

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 def calculate_density(mass, volume):
         return mass / volume
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
