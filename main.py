e0eb42ebe1232abcc8bc86074bf6b7129e0cb187
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:57:08 +0000

    turndown cruelty thought formal second

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
