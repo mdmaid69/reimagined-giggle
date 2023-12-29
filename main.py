d3e79ff9f8ea63060748efc5ffd78b77938ac5c7
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:13:45 +0000

    brother mess heavy trick equal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def subtract_numbers(x, y):
+        return x - y
