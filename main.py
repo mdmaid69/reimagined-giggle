286a4763c2e6cc2c887d7abdba05cfcb26758edd
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:56:47 +0000

    result either depend recent imagine

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
