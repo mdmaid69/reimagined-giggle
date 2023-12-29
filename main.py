52bc769590e29333367948fb962c5d9685954c1b
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:38:17 +0000

    preparation gradually maximum considering blow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
