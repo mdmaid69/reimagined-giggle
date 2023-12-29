278ad86fa6a540b47020b6bce319d60b76f9954c
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:55:04 +0000

    even broad dare worst worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
