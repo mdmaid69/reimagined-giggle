dd00c6d137b3f982208d89b06d8d12c442884369
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:51:49 +0000

    listenfor congratulate fast bit tour

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
