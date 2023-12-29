aef205baa89251fda3c55ad8f59b91c9b4901eb5
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:50:56 +0000

    violence sale giveup under uncomfortable

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
