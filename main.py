caca3de5a5d06abb69efc9aa7ea880a48cb31908
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:23:35 +0000

    store rise without joke blow

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_min(numbers):
-        return min(numbers)
-def sort_list(lst):
-        return sorted(lst)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
