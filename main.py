6b5cf875c5a6d1802c3da62e6dcb48aa43c55f93
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:13:20 +0000

    slow tense rubbish variety appeal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
