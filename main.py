08d0a55cf1336e06cbd127faa61d070756aba2f4
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:32:20 +0000

    temperature bank concentration centre weekend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import platform
+def get_os_info():
+        return platform.uname()
