e7e54572484cad85f17affc7947b0740636b6e76
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:34:11 +0000

    declaration flow Ss square loss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
