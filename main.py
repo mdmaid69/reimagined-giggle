06bad736fd071859fc5382918d89949508d068df
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:25:30 +0000

    slightly noise present teach slice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def subtract_numbers(x, y):
+        return x - y
