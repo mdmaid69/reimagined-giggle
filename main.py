851e9ce2fb2c05159494e40c7e089c484d9287e5
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:06:01 +0000

    smell beautiful mix experiment chin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
