4a90125ab7c8ce38bbbe4eac653f2aba94eda2df
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:49:45 +0000

    terrible female disapprove may difficulty

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import array
+def get_array_as_list(array):
+        return list(array)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
