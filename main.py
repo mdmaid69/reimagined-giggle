429bd256a4fa1b02b010408c38b683417c1f9843
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:59:09 +0000

    callat move trade before reaction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
