56a23ec3e7f275ae9342921d5f2bd3cfad66ddee
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:32:41 +0000

    waste decide manner subject train

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import array
+def append_to_array(array, item):
+        array.append(item)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
