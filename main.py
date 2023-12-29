1250e29ca95a3597d4d18475f7d04a9cfa66d521
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:53:25 +0000

    sick material price waste comfortable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def get_current_working_directory():
+        return os.getcwd()
