756edc79a1f7e1a6abeecff1a9fd27a25c6d2bc1
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:51:03 +0000

    steal leave today honest all

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
