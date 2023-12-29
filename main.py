795877aa7f6a7f7218cc2cfe2efd77c20a881e24
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:26:24 +0000

    rain sore waste development how

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def append_to_array(array, item):
-        array.append(item)
+for i in range(10): print(i)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
