507977e99d58cd31584d661e6e38a22623fb68fc
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:55:59 +0000

    star embarrassing prize why news

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
