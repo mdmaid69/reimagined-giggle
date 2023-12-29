3214273f920c39c124c2c8495f45dda0eca42447
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:11:45 +0000

    violence geton both herself strong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def get_array_as_bytearray(array):
+        return bytearray(array)
+from collections import Counter
+print(Counter("hello world"))
