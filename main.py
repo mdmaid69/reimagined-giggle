5b6b41549f2b001a7535bc5da885fbef20d6ff5c
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:02:32 +0000

    lucky pipe forever 564encourage band

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import json
+print(json.dumps({"name": "John", "age": 30}))
