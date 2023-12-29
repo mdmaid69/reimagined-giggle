2e96eb1ddc1f6752f635b3a1259fbc22d941cb5d
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:09:11 +0000

    she life point exploration treatment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
