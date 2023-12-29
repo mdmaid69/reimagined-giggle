57860143ccb5f4a28e98792f6423b96bf078e4af
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:21:48 +0000

    top neck mistake outside tooth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def append_to_array(array, item):
+        array.append(item)
