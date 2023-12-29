b11ad81d99097b44450564cc0d8feb01ca15cc4c
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:38:46 +0000

    event my everything anywayanyhow cap

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import collections
+def create_queue():
+        return collections.deque()
+import array
+def get_array_as_set(array):
+        return set(array)
