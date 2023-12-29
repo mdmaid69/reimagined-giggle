4337b75676561ca0e6b3dc8ed2353429fe649371
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:28:30 +0000

    silent active speed shallow like

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
