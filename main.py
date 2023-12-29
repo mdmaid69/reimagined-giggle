b7ac14bc383e495c2f75e18e39273333f00ffd70
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:36:29 +0000

    definite market among return club

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
