e6fa72ab4211fc4fccb33537bbb56f8b1adb2e31
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:47:23 +0000

    notice well another plan mend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
