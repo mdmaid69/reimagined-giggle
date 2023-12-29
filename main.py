df850c4eb86ed0dc4051f4e723af50febe09f978
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:20:04 +0000

    far mouth wing grade send

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
