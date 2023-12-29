9173d9acd39a68326dec7c36d0168d1cf43da60d
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:09:38 +0000

    later weigh image wrong put

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+def print_python_version():
+        return sys.version
