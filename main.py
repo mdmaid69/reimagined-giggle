6e2a9fa75baead8f1dbe2ec41037379b84773664
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:43:24 +0000

    plate love market storm refuse

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import collections
-def create_priority_queue():
-        return collections.deque()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
