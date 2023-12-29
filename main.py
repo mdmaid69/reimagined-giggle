011eca5b28f85f4905401fa21f2185eb3e22b17d
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:00:17 +0000

    in coffee disappear tour reasonable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_current_directory():
+        return os.getcwd()
