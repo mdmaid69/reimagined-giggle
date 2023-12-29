19128ca74a683a50ccbccf44ef3a14e8396c7e02
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:01:47 +0000

    comfort authority grow thinkover watch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
