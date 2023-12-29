f2524eaeca41be101fc0e8a81f9bc4105f55f506
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:36:26 +0000

    little tough beginning food fly

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def count_elements(lst):
-        return len(lst)
+def remove_from_array(array, item):
+        array.remove(item)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
