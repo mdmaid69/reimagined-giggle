272f7d99e7307becc21c7c26b4dfe9b17c002284
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:14:54 +0000

    pattern provide majority awake more

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
