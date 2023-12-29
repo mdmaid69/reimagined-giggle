e63f28af240e1cfe70d20a59f1f4c52044678111
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:51:05 +0000

    occasional central explain trip plastic

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def convert_to_binary(n):
+        return bin(n)
+import sys
+print(sys.version)
