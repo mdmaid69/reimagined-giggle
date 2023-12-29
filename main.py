0d95535c92e6b968e5a9a7967bdbb9564005a7b6
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:18:28 +0000

    mixture pattern service night shut

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_density(mass, volume):
+        return mass / volume
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
