f7f7412b099c7e04bcfe35346afe809cfc0cb9eb
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:15:33 +0000

    normal trick tip fear to

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
