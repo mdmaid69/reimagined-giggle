ff4d588ef3591cabb14cc44026ace16a49aab5db
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:12:37 +0000

    appeal national turnout suggest bus

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
