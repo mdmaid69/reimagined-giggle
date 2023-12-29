60a40544f7e1a5a58a12efeee6724a4a47165a96
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:12:35 +0000

    doctor break cost hate number

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
