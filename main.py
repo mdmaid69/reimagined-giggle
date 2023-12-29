42c388cd724a98782d9bddf2d07bf7f688380e4f
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:18:19 +0000

    confidence correct add period place

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
