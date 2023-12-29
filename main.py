91082b9a6412178dae7b042e7a61d38eac247e5d
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:59:24 +0000

    let trouble official pleased sing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
