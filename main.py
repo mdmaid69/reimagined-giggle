9cb21d7d765889f2d2d3ffdddb3f6e3eea6eff53
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:44:18 +0000

    yes regional hope culture past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
