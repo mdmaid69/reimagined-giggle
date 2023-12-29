a2d21c1e16cdb92d025aff0d2f480192478c1663
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:55:40 +0000

    office impression no fun group

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def convert_to_hex(n):
-        return hex(n)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
