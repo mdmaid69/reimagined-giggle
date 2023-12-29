3d3b716c52b3b2ec42d2743ff72c6c78282716be
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:22:05 +0000

    seat fairly spring embarrassing serve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
