6b135d60c311c79f53a61e196152d18642189b06
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:00:11 +0000

    night takeaway careless present environment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def get_current_working_directory():
+        return os.getcwd()
