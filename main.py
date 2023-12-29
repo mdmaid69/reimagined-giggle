0254130b0f0d03ac0a3c130be12fd04922f45e7c
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:24:15 +0000

    charge bite unfair turnon needle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-print(math.pi)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def find_min(lst):
+        return min(lst)
