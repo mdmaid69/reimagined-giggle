b7785d8669e2d53ed305b51770139e49e42104c2
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:03:22 +0000

    holdon goup strong somebody fault

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
