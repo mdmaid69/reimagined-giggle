b43dd2dbab5ea27b6b547953db5fc8b34d9119c2
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:04:18 +0000

    little detail trick Hh everywhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
