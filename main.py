a2dbf583fcb11d5663cdddad9ca51db6c92a7592
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:26:36 +0000

    likely store favourite dress nerve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
