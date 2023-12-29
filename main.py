7a4e8f80e234b429e7a7b5908087d3e2dc1fb5d0
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:16:22 +0000

    near pupil yet direct possibility

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
