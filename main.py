b887abf62a31d76a546d7864dfa8d9bb4d8f7c78
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:09:39 +0000

    total afternoon face talkinto dance

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
