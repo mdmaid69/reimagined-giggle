b15f0d4cbab861983fc4423e4643332e45e8179a
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:52:00 +0000

    discovery voice aim out fond

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import logging
+def log_message(message):
+        logging.info(message)
