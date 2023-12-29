b677971f77047f25f1f9cd6b1d30af3421bce580
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:13:39 +0000

    remember exam land organize hard

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def add_numbers(a, b):
-        return a + b
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
