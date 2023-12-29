0d3436c47396743449f4aec4b901ba6e84d9c335
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:49:55 +0000

    pure twist rather secret exciting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
