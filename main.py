0b7aff0225c64b962f8437936102de525636add8
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:46:31 +0000

    remain take tidy cry aim

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
