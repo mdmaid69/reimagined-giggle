83743c7be7e233a97b7efbcd23b91a176fa83f09
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:37:00 +0000

    characteristic clue disturb same adjective

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
