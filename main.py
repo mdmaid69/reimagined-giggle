abd01abf0ec29cf42dabb2179bf39858a76921a1
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:41:57 +0000

    ride window 573entertainment measure they

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
