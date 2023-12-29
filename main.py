632ba50aa4e5b361bea0c5ffabe5d9ce86d47ebc
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:35:58 +0000

    edge suffer thank help injury

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
