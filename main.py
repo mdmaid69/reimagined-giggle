2defeddbfde394fc75b7389fcac5b2235e70ff33
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:33:37 +0000

    television collect object container dream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
