69f493151532efc532f86eb6984eccf0047e0c19
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:15:21 +0000

    achieve bus scared smell possibly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
