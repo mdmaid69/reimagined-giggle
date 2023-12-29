1a9cd44426f64072dbdcad84175c6af771b0e959
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:19:13 +0000

    community past regret previous sell

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
