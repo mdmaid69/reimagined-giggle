2bfcf95584ce53e8499b674bd4f8249b4a18664b
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:27:44 +0000

    such least forever exact noise

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_acceleration(speed, time):
+        return speed / time
