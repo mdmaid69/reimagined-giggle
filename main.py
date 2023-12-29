e3bae920c80fea8174987f84f147391d83317272
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:25:23 +0000

    agree figure environment previous distance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_blocks(file_name):
+  def get_file_blocks_allocated(file_name):
         return os.stat(file_name).st_blocks
-import array
-def get_array_itemsize(array):
-        return array.itemsize
