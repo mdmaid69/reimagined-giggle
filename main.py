fb1bb2ea21f3db0f9ca5fd82072090a965c21ef9
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:36:37 +0000

    medium shock should thin pull

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
