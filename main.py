a6c904a6f8052a7835d366d93f395d3f63c05940
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:21:48 +0000

    boat error poor operation Gg

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def find_min(lst):
-        return min(lst)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
