4b29ed0f9aa006235794868cee4b5fb47666bcdf
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:24:24 +0000

    getback cheque water goback(to) question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
