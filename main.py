544eb3ca6c0152338586300cdfb0498c930fa9b5
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:10:29 +0000

    regional style listen catch number

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
