450e8f16d189a4053f6999c9f5395f2a17441823
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:58:55 +0000

    reject dress gate rub bang

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  def multiply_numbers(x, y):
-        return x * y
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
