cad51a46298dc62d9b0eaa7b4c919cf29f7932be
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:50:51 +0000

    almost truth kind you Oo

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_perpetuity(payment, rate):
+        return payment / rate
