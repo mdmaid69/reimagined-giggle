6114effc0722f5e0ec108173ec11c8896c6e128e
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:34:33 +0000

    duty tradition science shall form

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
