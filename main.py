0f7daf2f601ccb560a30613e8100774a5411685c
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:20:51 +0000

    harm begin port rub narrow

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-print([x**2 for x in range(10)])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
