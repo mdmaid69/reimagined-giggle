0ddccb3ea6f0e00c46f2f36051ab0e07f0b29383
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:20:17 +0000

    care car wherever whenever branch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import json
-def convert_to_json(data):
-        return json.dumps(data)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
