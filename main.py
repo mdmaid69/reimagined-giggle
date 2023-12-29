ae349e065631b482d00d278fe96e0a194ded99f7
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:27:06 +0000

    breath port consider feel receive

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_work(force, distance):
+        return force * distance
