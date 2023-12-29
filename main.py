f28fb9127024a5eece585d75ad27ddbcda1802cf
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:35:12 +0000

    on expect drop lookaround turnon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import math
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
