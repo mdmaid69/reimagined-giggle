416e65495fe259530e2235226e4faf7632f82960
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:21:39 +0000

    part throw clean genuine serve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
