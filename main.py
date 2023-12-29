a88723d2934deedecb16286f54f74ca00b5ba608
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:13:29 +0000

    advise unlucky load about entry

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
