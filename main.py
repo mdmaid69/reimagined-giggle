d61742cc519bb06c5969ed7defbf3753a1e11ef6
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:05:00 +0000

    wing main develop supply handle

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_user_string():
-        return collections.UserString()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
