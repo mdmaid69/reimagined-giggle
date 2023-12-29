e59f3fa380aa7e4a4d9df0354453ceede97c669e
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:52:20 +0000

    Ii town evening pen board

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
