b44f3122ae1bc06bd71728a813c05bcd970d87cc
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:11:51 +0000

    replace takeout respect brush normal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import sys
+  def get_python_version():
+        return sys.version
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
