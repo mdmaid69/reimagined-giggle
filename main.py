144663d4fe8de07b685d90521ae7ce57b4d33629
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:11:46 +0000

    clean ago dance week innocent

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
