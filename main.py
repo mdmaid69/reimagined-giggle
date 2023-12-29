d98956a264a72b221fc1d1ad81890d22a76d2ab4
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:31:13 +0000

    control attention entry squeeze demand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
