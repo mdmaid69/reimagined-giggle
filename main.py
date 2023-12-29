638fd7107d40074187d98f9bcc4f25bfd92a0418
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:55:31 +0000

    quiet past light good surprise

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
