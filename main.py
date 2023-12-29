6895c757b6c07a78b906cc1d6b81b91f8beb2123
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:50:24 +0000

    distant unkind cruel question drive

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
