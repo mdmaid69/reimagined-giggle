9eacaa139e4e4a40699d93f660eb04289606ea99
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:03:01 +0000

    conclusion another sensible cover regret

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def add_numbers(x, y):
+        return x + y
+def square_number(x):
+        return x**2
