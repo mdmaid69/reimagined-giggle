0362b332148846874d87e4c7046ee7a8565347ca
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:31:17 +0000

    pass breathe expensive thought fit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
