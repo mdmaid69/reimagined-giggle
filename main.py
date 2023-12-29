1260d9887686c6b53fc61db994f090a91eae9f3f
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:28:41 +0000

    him bringdown next Oo clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
