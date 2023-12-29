fbb34dd85040d7963f964885b610842ae2b19634
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:39:07 +0000

    every 572enter touch view tap

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-name = "Python"
-print("Hello,", name)
-  def multiply_numbers(x, y):
-        return x * y
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
