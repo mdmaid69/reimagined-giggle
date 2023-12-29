da333b45a8f2c2bbb970a46f5f21d276b9ead35f
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:26:25 +0000

    storm vanish replace while circle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
