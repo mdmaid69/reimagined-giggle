fe56bca62d986382ca7cc2c44f104e17e687252a
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:54:35 +0000

    round pinch supply people power

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def calculate_power(work, time):
-        return work / time
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
