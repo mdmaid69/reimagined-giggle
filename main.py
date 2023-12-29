06a0e6e17e25cb34e6d2ecce7be7132bfa0e2bd6
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:46:59 +0000

    contents flavour dish on protest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
