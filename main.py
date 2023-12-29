f4e850ca373ba9bcc3b56f83a6190fbd8e4651cf
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:25:16 +0000

    important careful turninto important her

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
