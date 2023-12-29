bff97d2e0d2215e8a328e8cd6bdf7180cca689c4
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:00:49 +0000

    combination bet right marry second

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def count_elements(lst):
-        return len(lst)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
