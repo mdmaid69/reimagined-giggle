db4c973b8037c1e41609f1056499c288b667cce7
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:13:39 +0000

    height 564encourage find watch government

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
