55ea75a0790516bb33033e4dc80fde5f3043c1b3
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:44:07 +0000

    style play tap nurse wrap

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  def reverse_list(lst):
+        return lst[::-1]
