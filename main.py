c7b60504446d3556e2551b6c7d38b0af796e20b9
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:49:11 +0000

    search dear flavour community behaviour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
