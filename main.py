f0cc1ddc01f058742babbea4d7a82cd0e91094da
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:25:01 +0000

    narrow talkinto bread nothing design

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
