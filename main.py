5b8ebc6381333c409e0c237ee9f3344e1a99131a
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:48:52 +0000

    callup persuade visit inform report

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
