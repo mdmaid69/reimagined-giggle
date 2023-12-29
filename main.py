37de84b2f9c0972f22a062f03227cd2bd5379724
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:23:47 +0000

    admit pretty ball runout stupid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import sys
+print(sys.version)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
