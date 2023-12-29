182c86fcd28e4c8941652646a846489c6aaaa672
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:27:30 +0000

    false push doubt gradually from

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
