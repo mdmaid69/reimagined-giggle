4eacf4266f6facb049e3704fe49c50dddf62bad1
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:34:44 +0000

    activity every well reduction passage

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
