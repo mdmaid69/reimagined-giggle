01320c149adb34e8be1245c4d27fc02cfbd264ed
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:18:05 +0000

    escape the its will hollow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def is_even(n):
-        return n % 2 == 0
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
