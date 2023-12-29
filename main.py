27174f16d534ea5acd9a018bac43734c3c457218
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:26:16 +0000

    welcome serve plenty light skill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_as_int(array):
+        return int(array[0])
