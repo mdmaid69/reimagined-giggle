c29995ff32b378fcf2c34a5f3c636d52b20648d9
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:11:05 +0000

    shall square maximum breakout rubbish

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def get_array_as_float(array):
+        return float(array[0])
