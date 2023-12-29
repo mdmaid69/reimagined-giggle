85fe622e8fb1d7c94cefb93dc4e894942d955d21
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:13:39 +0000

    look confident city mistake feeling

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_base_name(path):
+        return os.path.basename(path)
+text = "Hello, world!"
+print("Characters:", len(text))
