12b1005ba14b5c794aa3a51d17be2c80a9bc4bd1
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:27:25 +0000

    treat unless middle spite brief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
