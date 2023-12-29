a6584f692cbbe90061c409e090a08d0d15c34df7
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:11:27 +0000

    refer health pause confident plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import array
+def get_array_index(array, item):
+        return array.index(item)
