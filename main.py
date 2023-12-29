f0c85edb9b001974b5491f1b5e552b41d77b50dc
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:21:21 +0000

    comedown worst turnaway ticket 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
