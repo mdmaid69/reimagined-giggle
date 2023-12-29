6bea4b08baf06d8cd640a090e297a9d358d3605e
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:34:52 +0000

    cross if such after little

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
