c84d56aee6a45da0311ae5512a6ea5e0984adaab
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:36:22 +0000

    frightened make soft lunch position

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
