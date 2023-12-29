c086a67512ba75db4773a51a5751e44d4f4f7ba1
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:38:02 +0000

    famous concerning Cc event emergency

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def convert_to_octal(n):
+        return oct(n)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
