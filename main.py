a0cf9e2eb7854794084ce71b04521bd8c5d8f18b
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:41:05 +0000

    crash worry motion feeling fold

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
