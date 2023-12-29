8437c7b1684c029c8a8252939136ffa94df41376
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:33:05 +0000

    development middle release minute bringforward

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import datetime
+print(datetime.datetime.now())
