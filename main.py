7db0b449b45c53614af6962e36a8ad157b611400
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:42:47 +0000

    rank there awake 565end search

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
