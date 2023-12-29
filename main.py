a5f20caf81e554cd249ca0e47ffe2ad9ea5fba57
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:16:01 +0000

    less intelligent hour corner protest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
