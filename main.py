a0310cfe42ed693937f167468013d532c3722da1
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:04:53 +0000

    list serious drive improve body

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
