773312709e535f4bf14b26803c78fddb8587b714
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:41:28 +0000

    approve material Vv medium piece

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
