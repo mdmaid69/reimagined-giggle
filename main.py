91c928d0845e0b01d9152b64fee31b427c0498b8
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:55:31 +0000

    independent exactly whole flat risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
