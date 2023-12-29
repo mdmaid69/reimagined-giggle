6c772acb2c5f686330ca884613647e158dfc0bf0
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:51:51 +0000

    serious sharp open cook float

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_unique_words(sentence):
+        return set(sentence.split())
