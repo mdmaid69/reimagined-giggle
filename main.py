3465d8966cca570e834ce425516cccb5e51457c0
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:22:48 +0000

    calm tough rough rent Zz

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def reverse_string(s):
-        return s[::-1]
+  def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
