11f33c4d7ac20b8bcf65e6d7331bb2c9e6ed0143
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:28:01 +0000

    but delicate upstairs emergency fortunately

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
