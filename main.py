af9493e2c7ef4b22f6b56263dbed917ebf8461fa
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:28:41 +0000

    comeover set noise worth true

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  def convert_to_binary(n):
+        return bin(n)
