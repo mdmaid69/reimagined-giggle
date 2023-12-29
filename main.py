f700ea0307bf9c479ca5fa24d12f8858ad474c05
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:48:14 +0000

    herself man cover storm its

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
