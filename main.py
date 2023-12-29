fff315ac26c34cdf033144687fd4b27a15ca117e
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:59:02 +0000

    innocent past brother trust sell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
