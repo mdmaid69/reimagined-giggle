87405abedc129315126a69df416fc32c9bb6e923
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:05:30 +0000

    blue authority dream group face

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
