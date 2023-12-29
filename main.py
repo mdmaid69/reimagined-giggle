31b51611a54ce8f91fc4ab520f835a38f89b5659
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:09:13 +0000

    sort no oil judge but

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
