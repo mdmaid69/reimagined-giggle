f6efa47344158b315a7f90462a34d00bd9ecfd23
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:37:48 +0000

    bad defeat comfortable second pocket

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_perpetuity(payment, rate):
+        return payment / rate
