abcde8a231cb9d5a5e1aacc1abc07aebc77fa109
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:40:27 +0000

    powerful glad satisfied off frightened

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
