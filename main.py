001b517a623c75d0a034fe2bfb29fe6396e01f6d
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:31:29 +0000

    accordingto ill go dream win

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
