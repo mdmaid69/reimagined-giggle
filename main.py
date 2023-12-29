be5742b12d8bbde9b5130862e7b3ce0196af05b2
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:05:26 +0000

    apparently answer secret failure backwards

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
