b048034dbd24d43c18ed351eba025a440d2a88ce
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:35:58 +0000

    run translate complete report early

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def remove_duplicates(lst):
+        return list(set(lst))
