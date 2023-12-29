fd32e5041425a4bff7588a88237ba729ffbd3e95
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:09:29 +0000

    plan sweep bye factory basic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def convert_to_binary(n):
+        return bin(n)
