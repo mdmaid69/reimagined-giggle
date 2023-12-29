7b5c88633175cab5d307ca941fe826fccb08c847
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:05:35 +0000

    intend best first on field

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import time
-def get_current_time():
-        return time.time()
+  def is_odd(n):
+        return n % 2 != 0
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
