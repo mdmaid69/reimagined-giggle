311eedcc373070b4c6f2cdfd12052b35dbcda9d4
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:28:01 +0000

    plastic provide threaten intelligent by

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
