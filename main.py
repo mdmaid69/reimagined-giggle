f8a96b7c56fcb251f7c8438473930612cc507970
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:36:21 +0000

    brown Zz big work peace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
