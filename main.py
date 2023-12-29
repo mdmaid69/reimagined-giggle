e25c91239fd1129ab0b5938050602c41838e84a7
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:19:25 +0000

    success front male could string

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def calculate_area_rectangle(l, w):
+        return l * w
+def sort_numbers(numbers):
+        return sorted(numbers)
