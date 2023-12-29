f654cff6a9573222c9287b7abdcb02455aef4a5e
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:23:10 +0000

    dull ready brush whistle base

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
