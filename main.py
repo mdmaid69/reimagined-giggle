c97fcf02b1f10067b0c1a28c65e9b4a7ce929064
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:04:41 +0000

    smile tradition tough here study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
