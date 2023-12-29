96e0bf0cd64543438e2d4b36803da5851655aa94
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:42:30 +0000

    smile represent vision lock period

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def find_min(numbers):
-        return min(numbers)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
