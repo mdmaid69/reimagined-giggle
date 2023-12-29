53abfead0d000a0553cf07792ca614b723420f09
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:58:57 +0000

    happy repeat sentence light brother

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
