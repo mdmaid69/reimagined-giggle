17cf9c8d47a2e041393a5f7a5518b7136827e833
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:44:48 +0000

    forward very convinced goaway opinion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def find_unique_words(sentence):
-        return set(sentence.split())
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
