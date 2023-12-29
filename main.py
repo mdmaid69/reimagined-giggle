f003f2b192732e240fe97014b6c06dbea47d43d2
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:49:50 +0000

    dance thick rush experience sit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
