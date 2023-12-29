b0e3b0d6696c936b022034278e25609351352ea5
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:50:11 +0000

    and order 569enjoy minor ignore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
