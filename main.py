bc13406581ab8dce5517161e938e1c80def7442b
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:43:25 +0000

    preparation brave both attempt actual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
