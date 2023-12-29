f2cfcc3062a9266aede12440e4e2dd80fbf4250d
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:35:33 +0000

    to rub discuss of lot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-name = "Python"
-print("Hello,", name)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
