a390093d4376b72b9705d187a522ccf0214b7fe0
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:04:50 +0000

    supply introduce corner centre favour

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
