4cb4290fec9b0761cdc201a4756c49d5baa0078f
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:30:47 +0000

    turninto urgent stupid rid star

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import array
+def get_array_as_bool(array):
+        return bool(array)
