8cfdd453b86e6c3398500d7795857ba5313f9d49
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:16:42 +0000

    tough display invitation by second

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import numpy as np
+print(np.array([1, 2, 3]))
+import re
+print(re.match("h.*o", "hello world"))
