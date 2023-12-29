2b5dc63576de018eb753011624c4c4326e78362e
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:23:19 +0000

    information friendly fall operation willing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
