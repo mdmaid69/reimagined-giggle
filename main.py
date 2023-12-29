bad3a2aafedd146a6cc630631e73f9a6a63fecbb
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:47:19 +0000

    during count popular naturally regional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
