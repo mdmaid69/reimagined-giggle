8708654ca615b496cfa00ed8bcc52aedc0c88c64
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:26:45 +0000

    feeling attractive introduce kill measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import logging
+def log_message(message):
+        logging.info(message)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
