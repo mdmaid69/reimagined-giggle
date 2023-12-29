76a5070fd8522ff0d8fd1e841aafd7cc58913520
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:08:31 +0000

    store careful disagree brother honest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def count_characters(sentence):
+        return len(sentence)
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
