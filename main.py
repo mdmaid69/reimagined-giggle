a896ca05192d6bc2ae6ad30614e10a720c1ce272
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:16:59 +0000

    once Pp invitation sort description

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
