ef2c5f3e6ace341f848eeb695487f4f80b1a96bd
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:13:02 +0000

    contribute same so scheme basis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
