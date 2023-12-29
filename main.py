4cc779b40694891daabb1275178d666f7d453b71
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:32:03 +0000

    himself connected seat delight you

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-def add_numbers(x, y):
-        return x + y
-import collections
-def create_priority_queue():
-        return collections.deque()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
