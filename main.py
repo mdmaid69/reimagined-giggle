6056d7889d608cd3b7c7592ef7666814261046d2
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:02:29 +0000

    load square private right black

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def split_path(path):
+        return os.path.split(path)
