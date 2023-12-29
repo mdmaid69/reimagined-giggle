476c99ff620d9d736c6ba3ec00236466b06a7af6
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:54:56 +0000

    more school term stare emphasize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-def calculate_power(work, time):
-        return work / time
