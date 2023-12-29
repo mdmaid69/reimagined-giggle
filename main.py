8b41a4ab568ab391107916e3704a7ad97ac2e5e3
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:25:23 +0000

    commit move instruction cost talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
