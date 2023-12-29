7a5ab7ff03eb1bb1697ad16ca56b91e4aa50fc39
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:43:02 +0000

    course contain apart circle return

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-text = "Hello, world!"
-print("Characters:", len(text))
+import collections
+def create_user_list():
+        return collections.UserList()
+import collections
+def create_user_string():
+        return collections.UserString()
