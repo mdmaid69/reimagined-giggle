49b3f43d92300864e78cc1fb29a5a552490de259
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:50:11 +0000

    sometimes willing peaceful advise earth

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-print("Hello, world!")
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
