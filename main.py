583b1bd9a65af7df01ab2efe2b4121222dc9a0fb
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:40:04 +0000

    dress model uncomfortable frightened spill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
