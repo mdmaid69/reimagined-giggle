1cb209b1f6743b9e684e1244ac7ba35800dd9fa4
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:54:39 +0000

    live shoot 571enough clear comeup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
 def remove_duplicates(lst):
         return list(set(lst))
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
