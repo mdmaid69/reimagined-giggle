1394732e21f16e8182f9efd4654b82a04d047370
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:39:05 +0000

    angry maintain common whole fashion

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def remove_duplicates(lst):
+        return list(set(lst))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
