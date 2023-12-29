9fb3afa44ec2dbbf8c6718f8f68fd85d9c13e86f
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:25:30 +0000

    flood same complete legal apparently

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
