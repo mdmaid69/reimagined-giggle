cd0275bfdd794faca31762a518b0a0d4711d7a15
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:13:17 +0000

    trust scare tidy 564encourage perhaps

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
