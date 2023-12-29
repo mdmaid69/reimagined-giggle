d4ab56270de19685fe8a1cf34a0aa15ff6b66ad0
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:19:48 +0000

    press read creature precise plant

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
