d7ecb9db9830c067638dda75259ec8dd96914d35
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:47:47 +0000

    with doubt fine read crazy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_acceleration(speed, time):
+        return speed / time
