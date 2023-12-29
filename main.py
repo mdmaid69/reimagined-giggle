44265228b0edef41ea7446fff2ec715728c861b2
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:52:56 +0000

    point back earn kick visit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_array_as_set(array):
+        return set(array)
