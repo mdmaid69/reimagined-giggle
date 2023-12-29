71e7c9d6e45608a2a573e776e7425ee1c026a888
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:46:04 +0000

    move door rest board teach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
