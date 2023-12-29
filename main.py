42df2b0e521a50882018c8168f0847a315d36c8f
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:47:27 +0000

    quick inform drink try example

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
