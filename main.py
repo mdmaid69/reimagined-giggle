bc8d333af88cae0aa524665f49b5bdbfd19887d7
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:14:09 +0000

    soak nothing treat quiet comeback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
