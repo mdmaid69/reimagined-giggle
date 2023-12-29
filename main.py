6d300aba12d916872df7bb8b2754501c0eae1309
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:54:42 +0000

    matter thin dick pour way

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+for i in range(5):
+        print(i)
