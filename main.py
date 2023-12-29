0980998b892feaa201331b8078ea9ee5f30bfebb
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:34:43 +0000

    official oil pencil swing mood

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
