c1eb8fea9fb52665b12843a4ce44cbf589aea9c3
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:11:28 +0000

    modern guess final reasonable draw

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_force(mass, acceleration):
+        return mass * acceleration
