5f3f971dc4992352b4a17551b72ddfee8ae89e74
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:21:02 +0000

    light show while child wash

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
