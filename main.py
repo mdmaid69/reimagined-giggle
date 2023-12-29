f7e3588fbf9f3c6072a652bf486533195102c9c9
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:07:55 +0000

    information worse sink positive burst

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import platform
+def get_os_info():
+        return platform.uname()
