5baec5aa16395e19af1b8ab40a599ec0474bb85a
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:33:56 +0000

    brush programme influence no trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
