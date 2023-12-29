3b93427d7159709a90757d0a3221b2c080fac726
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:54:42 +0000

    by high neat scheme promise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
