9ac8a53cdaf157c2b62b6f20d9e9256835d26ae8
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:22:30 +0000

    improve burst design cost against

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
