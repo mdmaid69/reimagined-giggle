ecdb63af0bb43aa515d574f65885d33b46f6872f
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:13:05 +0000

    quiet safe stand drink what

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
