c392086fefeb393034d6adeab33d79f1ce62dcbc
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:15:09 +0000

    suddenly wind choice provide head

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
