52f8ed26eea423fba51567910f034b75af72103e
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:17:56 +0000

    movement much goout tongue finally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def get_array_as_str(array):
+        return str(array)
