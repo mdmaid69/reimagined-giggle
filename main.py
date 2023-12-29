03858f1cd412c81d9b843dfaab092a1f5da528c4
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:48:25 +0000

    drink black of few narrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import sys
+def exit_program():
+        sys.exit()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
