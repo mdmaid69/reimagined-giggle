a6fee94c1453ffb13e1e7be9041a065820a19f2c
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:13:39 +0000

    complaint custom sky lend calm

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_typecode(array):
+        return array.typecode
