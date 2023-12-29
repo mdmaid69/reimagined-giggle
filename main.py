e751035ac393f2050239eac03acdf59ea18c3bd3
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:15:51 +0000

    delight emphasize backwards city burst

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
