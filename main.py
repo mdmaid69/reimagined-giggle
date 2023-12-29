6c4531df30abd8207019bbdaa04d355107d586d2
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:55:50 +0000

    feed time extra score own

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import array
+def clear_array(array):
+        array *= 0
