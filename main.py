de9f17b4a73156cfb9679dd535ff7939284d674f
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:48:47 +0000

    favourite television secret estimate walk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
