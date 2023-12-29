676bf9c50a436ad523e696cdf9b2902a9b7517bf
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:31:31 +0000

    better over reduction remove ground

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def square_number(x):
+        return x**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
