48e4a28256f6fb79e6ee3e431782ff9ca26930ed
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:25:29 +0000

    bin neck contact admire grand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def remove_duplicates(lst):
+        return list(set(lst))
+import os
+def remove_directory(path):
+        os.rmdir(path)
