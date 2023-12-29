907ab4a9af3b15e9d322e0d7f432ac28574b4428
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:10:03 +0000

    ever total except benefit impression

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
