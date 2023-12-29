d832da178cbc6bf09e694d3b8f7c569da6fbbd32
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:34:25 +0000

    even character exploration result minority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
