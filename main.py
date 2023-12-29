1075cadd61265bbf545ffeaaf7eabba610dfe99f
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:55:16 +0000

    thick getback dream false attempt

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def find_min(lst):
-        return min(lst)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
