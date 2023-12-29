a788f1fbdcbffbbea434ac93165b959b7abf2726
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:23:19 +0000

    organization live result last think

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
