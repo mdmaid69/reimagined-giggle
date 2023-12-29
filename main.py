ec31ba793da80c394877a962ed5be08c445be759
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:59:23 +0000

    edge walk fierce grand objection

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
