44349eaf0c62e48c82074b874287ca51365c41e7
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:25:39 +0000

    provide spend only total breakthrough

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def remove_duplicates(lst):
+        return list(set(lst))
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
