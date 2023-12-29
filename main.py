a4eab9f5e07cb76d683b7ec533652574a999c9cd
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:23:57 +0000

    design case preparation spot add

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
