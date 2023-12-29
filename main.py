16af2e18deebfdc92a4b91e7f9d2fd1c50924b25
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:28:43 +0000

    war book relative estimate oil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import array
+def get_array_as_bool(array):
+        return bool(array)
