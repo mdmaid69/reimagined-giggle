03304bd7ff0882fc8aa530bc13a8626f5350279a
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:56:57 +0000

    appeal shut organize juice vanish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def find_min(numbers):
+        return min(numbers)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
