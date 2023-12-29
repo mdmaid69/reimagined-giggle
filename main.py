06f59c92224fb401cb1035b78f6e566f487474f7
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:55:32 +0000

    react miss judge actual explain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import array
-def get_array_typecode(array):
-        return array.typecode
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
