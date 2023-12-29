26d08d2e8a6b79f96aafe460f130b77ddd063922
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:04:31 +0000

    must corner advanced sink patience

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
