c7310ac823ed33da2d6ecfd20f61f60dc6e3c71e
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:40:26 +0000

    Zz judge upset breakdown calmly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def sort_numbers(numbers):
+        return sorted(numbers)
