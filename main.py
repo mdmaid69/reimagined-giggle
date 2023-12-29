c44bb6ffe6fe2d961ab8b6894e167514834c5138
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:26:29 +0000

    holiday Kk know circumstance she

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_as_set(array):
+        return set(array)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
