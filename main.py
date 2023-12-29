33992f8d2e38fb2a3d585507947f0b48fbc703b3
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:26:43 +0000

    try statement Ll create system

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import random
+print(random.randint(0, 100))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
