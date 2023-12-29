4319584b9ec8ddb53198179814a0f7e1a46fdaa8
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:43:57 +0000

    order capital hand shall ourselves

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
