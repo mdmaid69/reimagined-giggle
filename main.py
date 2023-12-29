f916aff0a8dc976ff97caa33114c5069a0df1259
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:25:36 +0000

    comein question near slice trick

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def add_numbers(x, y):
+        return x + y
+import time
+print(time.time())
