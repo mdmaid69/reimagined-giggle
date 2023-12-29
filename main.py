f863b34cc7d9c917dc247ac6b17604d3d9f291aa
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:42:17 +0000

    hard ride hurt attempt peace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
