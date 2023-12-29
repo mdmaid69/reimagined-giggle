476bfab987eac93fbbfafea65579a1b15ed68481
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:28:10 +0000

    another who scene whether blow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def convert_to_hex(n):
-        return hex(n)
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
