6ed3ede66b6ed5da66775312a0e28eaa70b97751
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:44:45 +0000

    gas unlikely custom future unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import sys
+def exit_program():
+        sys.exit()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
