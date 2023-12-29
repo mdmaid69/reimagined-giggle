73611e85581140d6e13918d996fb8dcd28af13cc
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:39:24 +0000

    strong turninto size possibility avoid

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
