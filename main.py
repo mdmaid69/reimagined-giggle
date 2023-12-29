509e163aca79cdaa4b41888c207434156a6e99d4
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:04:08 +0000

    life size powerful reply kill

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
