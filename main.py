e4a6f84a4713e7e62f90b64eddf1352ca1d89ccf
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:04:38 +0000

    after mess depend manage cheque

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
