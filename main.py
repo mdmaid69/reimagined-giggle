f7b4f57f6819d87d9a4723ff8947c1a35574fce9
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:03:59 +0000

    some bin regional telephone arm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
