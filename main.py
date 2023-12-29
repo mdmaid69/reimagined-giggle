4f630b9e9dbc41eed4e63b36cb85d2ec79d2318c
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:20:31 +0000

    content communicate club Ee inquire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def convert_to_hex(n):
+        return hex(n)
+import time
+def get_current_time():
+        return time.time()
