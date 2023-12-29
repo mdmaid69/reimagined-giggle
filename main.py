b0b00cc9125e19b0443cd512e7de855b57d04134
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:41:40 +0000

    translate nerve follow understand then

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
