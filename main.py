43f877b8c4c4c2329f7ca17f46544d65d6eac22f
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:05:21 +0000

    trip use week happy must

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
