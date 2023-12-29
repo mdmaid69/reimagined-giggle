2db410b0440d8cbbf175340a3387be806d961cb0
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:51:34 +0000

    ready middle this minority trial

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_speed(distance, time):
+        return distance / time
