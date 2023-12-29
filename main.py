bafae9a351d91f792acb49ecdf1d245a2ca1a49d
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:58:11 +0000

    trip matter man wing clothes

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_str(array):
+        return str(array)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
