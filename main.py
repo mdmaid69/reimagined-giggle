7e80c5dbca7f63bc396724ccc2d0978b2e93aa2c
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:58:08 +0000

    window business male that laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
