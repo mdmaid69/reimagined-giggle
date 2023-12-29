0ceb6c1260cd9dfd7e53d5c1a268085234ae9bfe
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:58:21 +0000

    list turnaway comeup wake would

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_power(work, time):
+        return work / time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
