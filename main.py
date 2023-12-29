4d949e9f075e4a5a6829a71db25698b20e35d008
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:47:35 +0000

    value single sail bringup slightly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
