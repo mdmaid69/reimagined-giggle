f3c765e226ccc8db27d710f0cbfe0835feb16a3a
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:47:09 +0000

    worst miss compete soak bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
