86292acbca4484060d89bccf3e098f938543c756
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:47:56 +0000

    error gradually interrupt about most

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
