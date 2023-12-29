ee49fee9f7d32bdebd471c154c5863a0aa881f3b
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:09:39 +0000

    burst great actual stranger save

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print(sum(range(10)))
-def square_number(x):
-        return x**2
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
