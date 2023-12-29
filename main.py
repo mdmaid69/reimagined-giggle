cc0db8597a2a37fc89d1190c60258bd0ec6eb879
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:41:46 +0000

    road hold petrol cruelty bet

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import os
+print(os.getcwd())
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
