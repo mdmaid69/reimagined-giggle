d035d5c0257c88e2aadfd9b3a1ecaf44fabfdcf1
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:36:57 +0000

    own bridge discover lead note

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def convert_to_octal(n):
-        return oct(n)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
