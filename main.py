c78671dede4f8467c9b0ef0dd8f6050fa912b3a5
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:13:05 +0000

    forget far floor or loss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
