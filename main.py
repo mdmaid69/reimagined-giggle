b1ba5a76f33b74d16021cbd6fc7e846955b50a68
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:05:57 +0000

    into thirsty feeling present fall

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
 import http.client
 conn = http.client.HTTPSConnection("www.python.org")
 conn.request("GET", "/")
 r1 = conn.getresponse()
 print(r1.status, r1.reason)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
