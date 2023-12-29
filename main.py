1d857f2ba794bbdce47c3663a0814946eab0aec0
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:59:55 +0000

    this easy butter work further

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def convert_to_octal(n):
+        return oct(n)
 import math
 def calculate_permutations(n, k):
         return math.perm(n, k)
