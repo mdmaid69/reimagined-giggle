cf617ab56386d31ad7eb15d41e61a65c991f99f6
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:12:51 +0000

    secret only repeat natural tense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import sys
-print(sys.version)
+def get_array_typecode(array):
+        return array.typecode
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
