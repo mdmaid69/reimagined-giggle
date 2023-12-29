1e054a600899233b5c618cd1b42db98e8f94048d
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:43:03 +0000

    continue exact react hers strict

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
