e22bc256e381642e792a5f6e6f166dd46cf4cbb4
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:12:34 +0000

    instead workon roll takeout properly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def is_odd(n):
-        return n % 2 != 0
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
