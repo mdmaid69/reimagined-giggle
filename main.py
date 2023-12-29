5c16ba6f3a63d13709ce37e9de02cfe70621c26d
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:11:23 +0000

    produce market smile load face

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
