4ef95d07390bd7b350c57e380156930c2702a122
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:37:53 +0000

    bring century curious believe model

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-import time
-print(time.time())
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def reverse_list(lst):
+        return lst[::-1]
