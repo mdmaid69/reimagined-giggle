e2d10a5232b1deea5ff4faed20e23add9d4e810a
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:58:24 +0000

    bend persuade open brave contrast

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
