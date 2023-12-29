d8355b434e1b879c7c4555b2740ac580fa19eada
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:46:48 +0000

    bringdown change breakinto length scene

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
