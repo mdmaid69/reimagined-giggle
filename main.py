24fe7b8e73db048028c20413fd363a1c2b13bfdb
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:44:05 +0000

    sit complete correct bound cost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
