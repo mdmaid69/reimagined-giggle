b053d57f7b4898f5cd98290140bdd0a0fe12b13b
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:30:03 +0000

    meanwhile exchange cough affect fast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
