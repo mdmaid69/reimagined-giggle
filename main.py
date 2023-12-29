7c7118e9c9223dd5e7d656dc22c160982c91136c
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:21:52 +0000

    affect sport dare comefrom splash

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
