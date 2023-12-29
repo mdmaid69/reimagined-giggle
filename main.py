8c5cd10f6e46bc8c301c7c146d795adbd3d3b7f8
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:04:05 +0000

    coffee bread already measure however

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
