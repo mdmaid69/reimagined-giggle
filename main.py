97703cf101f40b55c7270b0aab46b1353e81d209
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:25:19 +0000

    closely pay delight breakout furniture

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def reverse_string(s):
-        return s[::-1]
+def is_odd(n):
+        return n % 2 != 0
+  def is_odd(n):
+        return n % 2 != 0
