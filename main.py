472733527b00fbb7ecda068498f5521849b760b3
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:51:43 +0000

    could might Ff burst high

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
