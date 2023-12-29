fbc9914fa7432e951eec59c52265a49b42dfdb78
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:47:37 +0000

    underneath relationship seat rich object

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+print(sum(range(10)))
