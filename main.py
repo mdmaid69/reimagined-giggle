7b14103b0aed2ae2b43a151c7946436d407b3255
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:20:02 +0000

    note weekend along runinto natural

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import math
+def calculate_floor(x):
+        return math.floor(x)
