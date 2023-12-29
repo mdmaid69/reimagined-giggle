56bdaaa8bc90c201bbdba1ef693b6c9db743448f
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:12:40 +0000

    space previous noisy turnon indeed

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def find_max(lst):
-        return max(lst)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
