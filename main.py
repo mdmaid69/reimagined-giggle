ad00fd1d5cc9094d11484aa6d21cadb9b267188a
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:51:05 +0000

    immediately set mess rub pause

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def sort_numbers(numbers):
+        return sorted(numbers)
