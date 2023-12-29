dbf760c380fb2cdb0e101538e649a5c9881d579e
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:28:27 +0000

    invitation slide turnaway without oil

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import os
+print(os.getcwd())
