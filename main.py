d75120b99b2deecac50b64f551b4517dfb5a4d64
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:03:28 +0000

    become used process loose tend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import sys
+def print_python_version():
+        return sys.version
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_as_bool(array):
+        return bool(array)
