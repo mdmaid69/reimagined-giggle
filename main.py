7c437ec7c9b4521b74e46cb6c08a893938452b62
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:37:35 +0000

    anything state when aim perhaps

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
