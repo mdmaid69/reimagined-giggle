e651b2bbc940fe01bdc4c5dbddea5905446f9451
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:26:19 +0000

    cap intend die record calloff

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def reverse_list(lst):
-        return lst[::-1]
+  def remove_duplicates(lst):
+        return list(set(lst))
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
