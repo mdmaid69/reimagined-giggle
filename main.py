a25b79bb72e5652f5761f78d62e6face55992789
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:53:16 +0000

    close account petrol always suddenly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
