c67d69e3e780173428c88ce241127005a1d526dc
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:38:53 +0000

    achieve free whether fairly anxious

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def subtract_numbers(x, y):
+        return x - y
+def multiply_numbers(x, y):
+        return x * y
