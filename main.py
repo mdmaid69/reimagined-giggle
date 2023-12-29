ae19dee1801bc2a8c35d6d12dc79639a2986a7ad
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:41:27 +0000

    many thin Ee gate memory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  def sort_list(lst):
+        return sorted(lst)
