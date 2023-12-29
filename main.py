24a240bb840f735fcf03bf3a22c7543cc1a922b1
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:02:15 +0000

    advantage cheek rest key recover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_array_to_string(array):
+        return array.tostring()
