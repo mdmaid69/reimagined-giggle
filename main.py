7f41f0ec32e5aed149ab892b3a4d423e11c74bc6
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:53:35 +0000

    improve runinto dare blind lonely

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
