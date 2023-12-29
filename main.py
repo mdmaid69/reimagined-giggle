296d2cd51e37673d46149fed2388cc44777d8f32
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:28:07 +0000

    somehow nowhere shout round arrival

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
