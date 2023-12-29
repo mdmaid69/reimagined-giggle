95a0abf4cc58885cc12174b8f6bfcfaeaec6ebd9
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:55:23 +0000

    put interview need slide bringup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
