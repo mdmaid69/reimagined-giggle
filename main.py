16e7c1f1a0062086bc1635a281a98efd8be70be3
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:06:11 +0000

    recognize object geton head major

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
