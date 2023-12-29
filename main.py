1e12dc3c27b74d84ddec944f0d9840d22b67ca26
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:05:48 +0000

    bringforward wave system sensitive contents

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
