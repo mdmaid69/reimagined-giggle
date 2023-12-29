273e5e310df4cf8598240f548e6f4354fc69a27f
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:15:29 +0000

    food invitation someone Rr bath

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import numpy as np
+print(np.array([1, 2, 3]))
+def is_odd(n):
+        return n % 2 != 0
