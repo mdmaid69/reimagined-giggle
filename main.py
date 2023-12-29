8204a5750c8f25ce88fe69d6a4731c79213681f4
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:29:01 +0000

    direct now ask march matter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
