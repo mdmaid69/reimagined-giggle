b0a7d9d88594fd7a4dc09779b72a8c814526fef6
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:12:23 +0000

    note sting business mix emotion

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_power(work, time):
+        return work / time
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
