ef95357b4ae562caf3b2bd2910f5b6dfb4bb3326
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:03:31 +0000

    neither Rr sentence turnover react

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
