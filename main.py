8ae26748b9ced7bbf95d7c4792a185c33cc6e037
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:45:31 +0000

    afraid rest direct basic short

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  def convert_to_octal(n):
+        return oct(n)
