55d0be32afec2d26e5dfaff80e2de672c34da87b
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:29:37 +0000

    beautiful cheat hurt keepup goodbye

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
