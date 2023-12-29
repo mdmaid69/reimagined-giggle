1be33c731614627de1ce56fd7773eedc2862613e
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:36:41 +0000

    knock band mixture Dd surprised

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
