16474cab74d86ea8e6b79e39e118bc13365aacf5
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:18:29 +0000

    progress container pocket old angry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-print(time.time())
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_density(mass, volume):
+        return mass / volume
