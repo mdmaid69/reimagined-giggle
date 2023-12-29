2996dea569d45e0eb7a4c7ab49a8f66b57725d2a
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:48:00 +0000

    over judge drop attention bank

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
