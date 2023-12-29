59b7999f4792b93dbe59cec6f7a62eec0cb6b6aa
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:21:38 +0000

    embarrassed satisfied too brother pinch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
