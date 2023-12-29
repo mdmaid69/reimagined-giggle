799f298566f1dfe667731334053506c62375da1e
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:26:16 +0000

    achieve turnup geton pen mainly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import math
-print(math.pi)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
