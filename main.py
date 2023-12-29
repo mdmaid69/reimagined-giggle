a140c68a47ec3a6ac65d222bf373b908d4309ca7
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:36:11 +0000

    leave practical alive some either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def get_array_as_bool(array):
+        return bool(array)
