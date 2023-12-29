05e8e01e06f9dd10a7339f51550e019a87ca74f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:12:42 +0000

    future wind hate guilty confuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def get_array_length(array):
+        return len(array)
