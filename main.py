b2e535a792f0100391ca7777c1db8c3b62de5649
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:36:08 +0000

    after hold workout crazy getback

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def count_elements(lst):
-        return len(lst)
+def reverse_string(s):
+        return s[::-1]
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
