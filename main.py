f735ffc65c1b87a6f0ddc4d4e48e5431a3475b32
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:15:39 +0000

    embarrassed war design safety platform

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_max(lst):
-        return max(lst)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
