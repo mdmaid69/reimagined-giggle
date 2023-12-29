aa08a3f0c6f6c924b0eb8c864a39eac4f00c0208
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:51:50 +0000

    centimetre keepoff myself study land

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def convert_to_binary(n):
-        return bin(n)
-def find_max(lst):
-        return max(lst)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
