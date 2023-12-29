c8ba0db5ac5f798d4b215cba0a45264e837090ad
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:00:18 +0000

    woman fire convince advanced extremely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_list(array):
-        return list(array)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
