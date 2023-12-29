b203f4eae847997c26f637a9510afc1fc74fb20e
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:54:24 +0000

    deserve getout previous agree small

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
