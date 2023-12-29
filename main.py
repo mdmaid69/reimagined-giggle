911a17c1cb051be541ab137039e22e5a54b7e702
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:26:41 +0000

    need ignore dick 573entertainment radio

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
