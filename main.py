756c030793cc5be7fca200feb4ee5631aa7e620e
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:46:53 +0000

    present suit pretty spoil proper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
