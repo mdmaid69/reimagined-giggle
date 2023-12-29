5843bee92bbc2d66a2ec3541024a56110e7c643f
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:23:51 +0000

    promise slice all possession trouble

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import array
+def get_array_as_list(array):
+        return list(array)
