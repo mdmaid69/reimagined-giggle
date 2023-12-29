69c3f88fbfe32a85f9ae27b5920aa51b6068b831
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:33:38 +0000

    deaf party praise extra though

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
