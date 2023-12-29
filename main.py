30f1865ea8f3bf8d6e2b54ede633d2892f3eea77
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:15:38 +0000

    giveup suffer scheme unable smoke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
