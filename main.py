6ea70927f2827100a620ce189bbbc144c4493ae3
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:04:30 +0000

    path recommend want afterwards please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
