b3b87119a553be5a3610188ce0939ad56408a2cc
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:01:11 +0000

    yours talkback party ilovedicks concentrate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def count_words(sentence):
+        return len(sentence.split())
