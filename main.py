a6d5d160e2d67a7b60c6fd2530210141abf13587
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:20:09 +0000

    nose comfortable teach Pp afraid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def reverse_list(lst):
-        return lst[::-1]
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
