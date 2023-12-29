0efa69b0db23bcc9c6853abb266794d2666dc315
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:37:50 +0000

    usual target tip pretty negative

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
