25c1b85883b7aef883a030db0cfb6fd54460673b
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:29:41 +0000

    clue callat sail careful shrink

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 def calculate_roi(gain, cost):
         return (gain - cost) / cost
