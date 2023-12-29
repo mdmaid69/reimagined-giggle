4fb707d0c43e8d214bc89a8e6b4841f09fb2cbfe
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:55:23 +0000

    pattern risk lonely level table

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import array
+def reverse_array(array):
+        array.reverse()
