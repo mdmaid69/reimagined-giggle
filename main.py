e556c1bc20c27dda782a9b3858387dd2939d39a2
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:26:44 +0000

    on land square dream train

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
