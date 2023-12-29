cc1f1a1749f74add600adde8e637ed74326ff42d
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:06:30 +0000

    scared discuss date pollution completely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
