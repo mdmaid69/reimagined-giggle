aa4e8f913af90ca5d4a7a6359ebf5e6c39012885
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:05:31 +0000

    blue return means push ring

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def add_numbers(a, b):
-        return a + b
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
