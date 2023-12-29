55f298f581535856e5307140e14503cac5b156b6
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:03:48 +0000

    indeed comein walk rescue runover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
