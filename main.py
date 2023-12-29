a9feb942414aa93981c4491c7dfe26632188c4ff
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:41:13 +0000

    point doubt urgent care strength

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def is_palindrome(s):
+        return s == s[::-1]
 import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
