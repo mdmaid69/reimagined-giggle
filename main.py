ef9a35f2bfc70b308f443e1d31ba1e9704dc3ffe
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:20:21 +0000

    pencil force straight worry drop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
