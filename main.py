8dbacab49da09809f6eaf764037a5a491895e33f
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:30:01 +0000

    laugh runover bringback who afford

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
