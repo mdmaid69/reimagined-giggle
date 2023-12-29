5a0cf4391c5f1059f0f818ac585aed3516298754
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:33:44 +0000

    introduce crisis combination relation urgent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def find_max(numbers):
+        return max(numbers)
