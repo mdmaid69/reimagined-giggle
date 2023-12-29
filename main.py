79dc9113e861ccfa067233628f32b51604c85112
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:42:59 +0000

    wonder I play appeal thumb

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def count_elements(lst):
-        return len(lst)
+import logging
+def log_message(message):
+        logging.info(message)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
