3d277de134f4f42cb3e547d0d88473c29a83ecdc
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:00:24 +0000

    cheque copy her difficult consist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
 import array
 def convert_array_to_list(array):
         return array.tolist()
