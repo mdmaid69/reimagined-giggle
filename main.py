c69eb43a381a78d0a861148ba079f3a468a33c9a
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:20:13 +0000

    corner Zz takeback proud flow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
