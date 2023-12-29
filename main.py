474df662d3a31a8882606d8b0b8c60b2c4b49701
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:11:23 +0000

    pick weekend determined unable lean

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
 def calculate_future_value(principal, rate, time):
         return principal * (1 + rate)**time
+def is_palindrome(s):
+        return s == s[::-1]
