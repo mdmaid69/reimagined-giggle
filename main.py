9a858983a7b7e1dfe43a6b8d49dcd69156818814
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:39:56 +0000

    steam distant fat brown strike

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
