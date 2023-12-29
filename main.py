7de69b70056986a641b23258e738169c48005915
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:11:20 +0000

    save loose favourite scale determined

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import datetime
-print(datetime.datetime.now())
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
