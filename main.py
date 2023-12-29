74d6c07a573adb03b390ecb78c291099b7778de9
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:55:00 +0000

    switch keepaway unlucky spring repeat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def count_elements(lst):
+        return len(lst)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
