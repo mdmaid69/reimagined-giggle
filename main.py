7ef7c38f4f053498d2959b6d8b7f8594bebac57d
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:20:36 +0000

    whatever offence personal rid picture

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
