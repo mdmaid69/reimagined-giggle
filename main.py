1103c5543a3d21663f5bf5124368ed5f40d27d69
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:38:29 +0000

    fill broken official compete most

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def sort_list(lst):
-        return sorted(lst)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
