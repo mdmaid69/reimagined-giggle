216b43c7075c2554fb8c531673b0a2406074dc6f
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:35:28 +0000

    Ww choose run blame any

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
