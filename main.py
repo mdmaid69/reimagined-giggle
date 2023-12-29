7b3ce68eec49ca088ff8fa17d662db1e76fea51b
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:06:12 +0000

    light passenger float figure fun

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
