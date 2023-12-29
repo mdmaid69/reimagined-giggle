c32bea55fe36324e6d477b3aaf28b1ee2ef9e246
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:19:18 +0000

    afternoon else book shine achievement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import sys
-print(sys.version)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
