cf73dc085639a9115586444e3db3ecf2b1088cbb
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:19:05 +0000

    surprising false original complicated coat

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
