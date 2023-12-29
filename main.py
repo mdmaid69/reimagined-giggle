c0aa2b5e680081e4381d7a42d86083e9196396d7
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:17:50 +0000

    sleep child list vision nothing

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_area_circle(r):
+        return 3.14 * r**2
