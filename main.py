91ef977361d1f04291ec7a62d6411fc52e2a884f
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:57:39 +0000

    remember slice perform low issue

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
