dc39c77bd5fb869fa60cbd466a26a93d4b74690a
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:59:55 +0000

    offence hold disapprove form check

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
