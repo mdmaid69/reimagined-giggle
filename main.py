c98a4185eb3e8fc22cb348ac13de7b402dc1e465
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:08:20 +0000

    nervous thirsty joke forever usual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_acceleration(speed, time):
+        return speed / time
