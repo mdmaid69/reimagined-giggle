e95f5f6f3c00794bb8e1954305de6df4bcaeced2
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:18:53 +0000

    medium protect tone upset example

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
