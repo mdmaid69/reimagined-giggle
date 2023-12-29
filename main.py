1a26bde4f4ad93cc150f26899da02c4a52d74468
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:09:23 +0000

    cinema attempt funny already perhaps

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def convert_to_binary(n):
+        return bin(n)
