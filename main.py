09919b4458713fad02025161bd7315f5ff65dc05
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:34:48 +0000

    practise comeround shake piece protest

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
