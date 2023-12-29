2b3c0c836a8959a74a29fb7e4921d66745a30de0
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:52:42 +0000

    software swing problem feeling by

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
