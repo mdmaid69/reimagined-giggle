c4de96f4286ec30263c67f0a6fe9303a85fc6457
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:38:48 +0000

    vote fight ours level badly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_max(numbers):
+        return max(numbers)
