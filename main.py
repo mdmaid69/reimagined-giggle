3d8c9d62fe10604103601e87f479d204927f01e3
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:38:56 +0000

    urgent upstairs region neat sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
