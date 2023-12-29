54c775d91cb526d5149a5422e4cabf8f3555e25f
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:46:21 +0000

    rough certainly Ll lift storm

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
