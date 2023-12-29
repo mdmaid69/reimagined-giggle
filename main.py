ac3343823435df76ee27838e908cdf559f6b7f2b
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:04:23 +0000

    warning edge affect priority furniture

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import re
-print(re.match("h.*o", "hello world"))
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
