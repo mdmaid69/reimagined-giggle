7eaec433ec22492398d4a1781007196ba1cbf779
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:14:10 +0000

    heart tax scared total language

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_as_str(array):
+        return str(array)
+def convert_to_hex(n):
+        return hex(n)
