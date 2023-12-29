4a49d4a1c3a2f7a5943fbc3b04ab869ec6be5400
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:06:30 +0000

    edge project prefer 567enemy neither

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import datetime
+def get_current_date():
+        return datetime.date.today()
