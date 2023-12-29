271173feb23f4177f2afbbe9d60e711c9a4227fe
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:44:29 +0000

    plenty hit container best around

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
