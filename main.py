c14c64dfcf7f673224a1917603dd334050efcc8f
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:56:58 +0000

    contribute limit instant criticism same

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
