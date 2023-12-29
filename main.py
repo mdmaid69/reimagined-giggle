5221fe2e63553ab58bd04386d74ecf2d7a34346a
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:55:36 +0000

    temper wind above travel brush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def add_numbers(x, y):
+        return x + y
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
