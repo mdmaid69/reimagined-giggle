2c345f099e49968e49e8b713182badce7072d949
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:04:18 +0000

    grateful certain claim moral disagree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def change_working_directory(path):
+        os.chdir(path)
