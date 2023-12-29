0604659073f4ccfb779e25a288c39c840d888e9e
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:54:55 +0000

    that takeoff blame demand fill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_string_from_array(array):
+        return array.tobytes()
