413fc19307a231160eb9e4244199ad2e72c5396c
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:40:49 +0000

    cover port pot takeback sport

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-print(sum(range(10)))
+def calculate_power(work, time):
+        return work / time
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
