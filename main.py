31e3101c077ada6a3e40d0ed9c3d42a766ddc121
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:17:44 +0000

    round spring catch runinto Ii

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
