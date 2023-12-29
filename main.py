8224813ebf54999157723fb53e1f8e35c2b00c7b
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:02:39 +0000

    key check blame early impossible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def is_even(n):
+        return n % 2 == 0
