a9578a5eeb4f5bbb4b4c63063ebf2c2051319424
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:58:47 +0000

    raise talkback excuse Itself bridge

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
