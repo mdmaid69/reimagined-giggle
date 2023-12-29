d4b2b36f4562e392566f12ae7587756bea8f4af6
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:44:51 +0000

    holdon any I instrument tongue

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  def convert_to_binary(n):
-        return bin(n)
+  def subtract_numbers(x, y):
+        return x - y
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
