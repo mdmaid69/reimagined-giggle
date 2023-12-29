dca74439077fe09ec5d285d583df2faa0029d7f0
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:13:31 +0000

    after difference value service delight

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import time
+def get_time_since_epoch():
+        return time.time()
