caf4dd98f913f5ebcb5fdeb0d32ed16960c8ea7d
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:34:55 +0000

    wild kick autumn opponent rest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_pressure(force, area):
+        return force / area
