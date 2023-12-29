4867da6e6d9e9c3c4639805072c3f4ffe8082d58
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:35:30 +0000

    afternoon compete state copy trip

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
