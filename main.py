91ce4a2ca526a31250bae09ecac63317c62cfcb7
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:47:28 +0000

    same bet claim terrible nothing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
