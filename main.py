82e1a672f64327288b40c55285e9de57e93a9ce3
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:03:24 +0000

    strict excuse instruction tragedy tie

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
