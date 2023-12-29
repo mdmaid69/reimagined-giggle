b20fd49d0983f9bd91cb68d371244e27ada06f98
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:38:05 +0000

    unite tonight ashamed cause whistle

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
 def calculate_logarithm_base_10(x):
         return math.log10(x)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
