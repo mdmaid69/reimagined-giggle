eba91376b2ae0d2a2450847161b00c08b6fadd2e
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:54:04 +0000

    news really feel transparent on

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
