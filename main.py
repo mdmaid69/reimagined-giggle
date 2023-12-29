caf3b041d41986e683a919b1fecd5bc4fc95a83a
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:00:28 +0000

    already name embarrassed sweet try

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
