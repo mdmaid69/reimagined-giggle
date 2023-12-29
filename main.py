859a9018dcb180b323cc3e1dd08335bef419bdbe
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:42:53 +0000

    his scream value there minimum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import collections
-def create_user_string():
-        return collections.UserString()
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
