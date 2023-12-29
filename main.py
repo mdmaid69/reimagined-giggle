4cc6b3daa5d2aeb0513a476770f857bd1f1034cf
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:58:49 +0000

    inform Mm answer office row

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
