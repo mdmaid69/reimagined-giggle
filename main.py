b354f3b6112dff24a514a6522d008f3fcb8ae50c
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:59:40 +0000

    free shade basically ball him

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
