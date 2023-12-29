94dbeb6df4581cd28eccdee75150d9f4567c9c48
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:56:36 +0000

    sentence steal dinner transport hospital

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def reverse_list(lst):
+        return lst[::-1]
