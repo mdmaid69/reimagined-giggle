5c9b854287ed60273dd33031bad7bd2341034e54
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:24:32 +0000

    journey brief produce classroom ahead

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
