974f656c22e93021a838ff8f25b6c8919ea76d1f
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:33:26 +0000

    story excellent reduce against allow

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
