888d7d6858b2c1d22a387e2deaec1e1954bb6e06
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:16:39 +0000

    chase burst all circle bad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import getpass
+def get_username():
+        return getpass.getuser()
+def square_number(x):
+        return x**2
