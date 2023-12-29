db4fad7ada208b8aacf183fb975e40bbd97ea788
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:22:27 +0000

    regard through my see recently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
