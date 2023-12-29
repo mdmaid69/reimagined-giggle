37c8745c8c18ea785165eae0ed7075f03c974137
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:40:38 +0000

    waste clearly split middle available

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
