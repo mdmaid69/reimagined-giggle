fd05afa74d1dd328bffbe97cc5c23e2d8d55fed6
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:00:09 +0000

    deliberately mix dream compare about

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import platform
+def get_python_version():
+        return platform.python_version()
