2c2c667874d43f6614b9293521e5fbed1286b36b
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:15:15 +0000

    force port bye course bank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
