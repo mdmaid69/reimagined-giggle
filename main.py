ca2f032ad90d9284f563876f71d67252c62fb7f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:46:59 +0000

    congratulations callout either movement pass

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
