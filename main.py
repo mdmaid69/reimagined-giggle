ac1558979c05f77744fe0b8a8d205dcb3bd8f3a8
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:25:33 +0000

    love move piece instruction television

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
