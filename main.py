3afc368ff343a94147af20df866b494f123643c8
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:06:18 +0000

    kind love journey naturally fun

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
