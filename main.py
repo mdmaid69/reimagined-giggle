dc69237fabb3a7bfa5c51a01b9e366643ed850c2
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:17:21 +0000

    rub Ff lookafter complicated embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
