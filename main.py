9fe39dca56a90669e5297f3fec5b7df273eb881f
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:07:15 +0000

    take mass Vv pair concentrate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
