c019748f0a0b33b83d4c609408364cefc5d0e577
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:27:12 +0000

    smell arrange objection let uniform

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
