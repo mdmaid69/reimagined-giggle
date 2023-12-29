eba37781098c2bc5cd8c5370cbba031737fba686
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:06:36 +0000

    science punish hers around especially

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
