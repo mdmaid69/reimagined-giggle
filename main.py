94d9ba3fc312cdcd5431a6bb3dd66700b6b7f425
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:36:53 +0000

    dress exam definite he day

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
