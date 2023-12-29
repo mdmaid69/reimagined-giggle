5eb52d228f4ee106d725585e03fe0e138a906bdf
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:20:13 +0000

    people than approval keep take

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
