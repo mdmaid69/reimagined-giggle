2517f5499485f326b009255cee1251626c80172d
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:32:23 +0000

    winter except wave fact somewhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import collections
+def create_stack():
+        return collections.deque()
