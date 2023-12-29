4ab6ad19b2e5f54158d75baad611657539b0fa15
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:42:13 +0000

    problem something wet measure open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def convert_array_to_list(array):
+        return array.tolist()
