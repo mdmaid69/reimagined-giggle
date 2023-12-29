4ce73fcb41e89ddda3cab4fd84f84c206cc05b9e
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:26:55 +0000

    account callup union never shock

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def find_max(lst):
+        return max(lst)
