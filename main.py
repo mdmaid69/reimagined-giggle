14c56e8e0a7e3430121e9ac6164041900404aaa4
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:10:03 +0000

    visit something comfortable question lookaround

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_tangent(x):
+        return math.tan(x)
