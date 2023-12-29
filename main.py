b5866b689911ace9dc747ab43ed094da16931cf5
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:18:59 +0000

    lock note set contrast couple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
