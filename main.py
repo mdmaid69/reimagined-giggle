c934a981f806bd0b0656bc3935ddf0d772c157a6
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:31:11 +0000

    wash great goodbye back pay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
