430d9dba3f9f0fc4de21bb5cb8fce0fe9872b4f9
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:33:49 +0000

    here attempt whether legal runout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
