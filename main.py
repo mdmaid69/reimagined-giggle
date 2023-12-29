7a043a0b4bd00e0d6dc248eeb88f95783ad61993
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:38:46 +0000

    depend sticky raw type anything

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
