3bd0954a0cce44c97fcfcccba87e26b710c32f5a
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:16:27 +0000

    upstairs declaration waste sentence chief

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
