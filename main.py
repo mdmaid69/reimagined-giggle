58c4c825cd16355675d6b06267d7be2576d98db6
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:37:58 +0000

    well stamp sound waste region

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
