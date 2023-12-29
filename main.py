fbb538498817298e16e603540e7702f06c67c8f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:36:48 +0000

    keepfrom reaction meal joke language

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def square_number(x):
+        return x**2
