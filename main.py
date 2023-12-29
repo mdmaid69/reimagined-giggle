1f2a9591f9a9f1371408929483c76965071976cd
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:03:23 +0000

    hear dance he how circumstance

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+from collections import Counter
+print(Counter("hello world"))
