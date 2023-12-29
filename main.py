14e650ec9a62b117457dc1543aaa9ad6c72016ae
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:09:24 +0000

    dear plate lookback(on) finish giveup

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
