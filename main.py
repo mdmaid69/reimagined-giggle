e3d4612178ee07029587a0dca39b98c2ea6d2293
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:13:10 +0000

    prove limit win communicate understand

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def set_array_item(array, i, item):
+        array[i] = item
