3f996954a7b5b4b92127cadfc23e575c00ca368b
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:25:29 +0000

    workupto myself date rank shall

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
