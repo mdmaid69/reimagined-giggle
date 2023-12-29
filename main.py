822e79335717752e45ef9730193b605f67706ed6
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:06:47 +0000

    boat present practical please instruction

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
