19c0d58eab8b2cd6e22416e0d25169edeb4d5aac
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:26:24 +0000

    pass sit shout share late

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_tangent(x):
+        return math.tan(x)
