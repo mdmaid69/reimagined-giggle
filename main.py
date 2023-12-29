5c27e67e7c7d3f84fb3aa99922d23da4ae77c6c0
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:35:43 +0000

    hard little sweet Uu Hh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_area(radius):
+        return 3.14 * radius * radius
