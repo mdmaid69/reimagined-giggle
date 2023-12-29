8d6fff15aef1fc30e395354a9a1e2646fa5796c8
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:38:22 +0000

    expensive reply union late shrink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
