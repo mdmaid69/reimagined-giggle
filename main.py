177e20fc855b0a2b9200797e42117cbc1575207e
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:56:53 +0000

    treat factory upwards cap what

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
