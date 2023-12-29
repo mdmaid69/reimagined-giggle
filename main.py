67be9b6bcedf1f08ab5185820e81c59b327ae3d9
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:52:33 +0000

    someone family above operation himself

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+print(sum(range(10)))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
