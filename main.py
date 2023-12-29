22086bdbbfabcd0a63d3b1e40c6d549f1c3eb5b6
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:42:24 +0000

    birth relationship body quickly watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
