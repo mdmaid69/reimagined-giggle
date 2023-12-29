24c21ae423b7ad4853d03d96afaafdc501d0e202
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:10:25 +0000

    public track identify imagine wish

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
