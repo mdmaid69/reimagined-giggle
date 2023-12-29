952655402c28846cbe4f3d60ab9d273962bec058
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:17:06 +0000

    traffic right switch exact recommend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
