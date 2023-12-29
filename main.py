ad5bed1b10e6fe83570ef9f4df5c554bc759fb98
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:12:51 +0000

    keepoff giveback damp each near

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_force(mass, acceleration):
+        return mass * acceleration
