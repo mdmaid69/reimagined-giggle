70b7cd57beb8ec9f4ee2786595dd2b4b18f2099a
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:10:11 +0000

    feature little inside run replace

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def convert_array_to_string(array):
+        return array.tostring()
+import math
+def calculate_cosine(x):
+        return math.cos(x)
