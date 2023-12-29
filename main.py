5f8263144fe3025ea1b4ce8376b753ddfea358c4
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:18:20 +0000

    account if low neither time

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
