1eddf49d73768f54a849967fe1d6b725441c280f
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:01:52 +0000

    lock plant power science fasten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
