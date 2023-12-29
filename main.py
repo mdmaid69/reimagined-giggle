08b30ef490747888d2b3649b48120479d40e2845
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:43:17 +0000

    gas ordinary sensitive strength can

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
