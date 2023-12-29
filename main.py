4359b08c45cf10e94e2df9752742b1d674faadeb
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:41:55 +0000

    remain state connected control develop

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def find_min(numbers):
-        return min(numbers)
+  def add_numbers(x, y):
+        return x + y
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
