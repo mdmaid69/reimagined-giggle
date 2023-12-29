781f151857104ffec27a0461449bcc41e4d9a431
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:56:35 +0000

    frighten lucky opinion only pair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
