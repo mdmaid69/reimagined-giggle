fa3e6aa1664a651568594012f41638270092e538
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:44:17 +0000

    wrap mark taste free mad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
