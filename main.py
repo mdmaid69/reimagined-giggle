fac1ede1843c4c99fe8c87d342c9ff9749786e91
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:36:18 +0000

    display pleasure curious phone home

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
