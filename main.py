6b465db86aca53944e4a943d567c62d6d2e69394
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:04:52 +0000

    comfortable sound suckme really unlucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
