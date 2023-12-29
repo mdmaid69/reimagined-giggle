4442813bef18a48fef497b22e0aae92372e46b38
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:51:13 +0000

    female chin base active achievement

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_area_rectangle(l, w):
-        return l * w
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
