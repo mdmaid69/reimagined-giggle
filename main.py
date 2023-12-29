80f48d691715058450bbed8f8f06e888db30e0aa
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:46:27 +0000

    tired here true punishment smell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
