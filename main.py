1c301bebab7c3e458d967f579f8ba88735e6a1eb
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:26:39 +0000

    apologizeapologise perfect television under suffer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+print("Powers of 2:", [2**x for x in range(n)])
