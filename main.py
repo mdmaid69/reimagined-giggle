b4dce8d7a6abeb014953dddae2b5bfb20ca950b1
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:50:44 +0000

    blame comment back catch design

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
 import array
-def get_array_typecode(array):
-        return array.typecode
+def convert_array_to_list(array):
+        return array.tolist()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
