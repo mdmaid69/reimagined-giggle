f27ce3e0df06bcdb95832add241a945fe38388f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:17:29 +0000

    walk act cheek valuable exciting

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
