31f8075d441ffd4a9480e1a1585a1b7f58e43453
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:17:53 +0000

    public ride petrol such shape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
