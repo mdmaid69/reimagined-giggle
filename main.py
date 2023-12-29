8331b399a3b326dc3b4814b5d26a9a173a74dcec
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:13:02 +0000

    provide we fierce hope belong

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def is_odd(n):
+        return n % 2 != 0
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
