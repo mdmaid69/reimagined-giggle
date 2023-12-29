7d4ea065122833f73bf7363354ef9bf712d4ccb2
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:27:37 +0000

    dance suit win shrink female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
