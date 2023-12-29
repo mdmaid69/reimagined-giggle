cf18be9225bdd6ddc844fb528c4ff8bffa63d66a
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:39:30 +0000

    keepfrom operation offer combination there

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import collections
-def create_stack():
-        return collections.deque()
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
