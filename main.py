49feefb9e16f5c2029df9b6c42d025b956307619
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:51:51 +0000

    suggestion replace so smell chin

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
