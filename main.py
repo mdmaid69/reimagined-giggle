7efebbc466670deeeec9e1de4be371d98f6296ee
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:55:04 +0000

    impatient myself form Ss variety

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import numpy as np
+print(np.array([1, 2, 3]))
