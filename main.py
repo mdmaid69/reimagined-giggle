732852d6da7a229efe9bc162f8290eaa092ef51f
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:03:19 +0000

    rush risk trial connected fight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
