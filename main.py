f1b97c9a5682ecb9dd1a13ff6b85e2288fd49442
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:34:15 +0000

    immediate over professional settle attract

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def cube_number(x):
+        return x**3
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
