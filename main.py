452c7f5f273e6f8889d073cec16959efb67c49f0
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:34:25 +0000

    ability rent round sleep call

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
