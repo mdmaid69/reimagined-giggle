536dcbf30a67a36a9aa3a68d7442c854632c60ca
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:07:20 +0000

    breathe scrap warning wrap moon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def cube_number(x):
+        return x**3
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
