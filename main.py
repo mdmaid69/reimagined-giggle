d80139c9df2c5f08ab1a6c21dc5a50943a4059ea
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:45:55 +0000

    habit it sorry spill Ww

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import datetime
+print(datetime.datetime.now())
