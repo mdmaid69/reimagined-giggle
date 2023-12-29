5d951ab8ee46ce8f77ad8b52263a11922f8216fd
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:05:11 +0000

    careful noise open to shock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
