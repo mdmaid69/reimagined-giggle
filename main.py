b697a56cf781517bb64d5bc56cb13b4e61cfeef2
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:48:09 +0000

    would clear rate more stay

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-print([x**2 for x in range(10)])
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def get_list_from_array(array):
+        return array.tolist()
