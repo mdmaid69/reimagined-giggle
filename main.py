8f64f621ae1709aacf6f1663d29f49d1a1d3ca3b
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:35:56 +0000

    step discuss exchange project direction

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
