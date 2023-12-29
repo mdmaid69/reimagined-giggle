c9130a90ebdfe4c5fe2305480c44afdc46301112
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:44:09 +0000

    their safety drawing argue because

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import array
+def get_array_typecode(array):
+        return array.typecode
+def sort_list(lst):
+        return sorted(lst)
