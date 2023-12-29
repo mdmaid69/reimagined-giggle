6ab2b1621c1f35cb2d1275c4b5c26acd587fe6f6
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:34:19 +0000

    public whistle wipe bag single

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
