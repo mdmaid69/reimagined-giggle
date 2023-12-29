0aedf492f8adba61cff447e02fe904e435c12a29
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:53:21 +0000

    clean spoil target glass on

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_floor(x):
+        return math.floor(x)
