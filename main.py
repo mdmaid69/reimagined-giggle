8dbb2cf7c46e6466208fe519869ba41dc7d24206
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:23:44 +0000

    near correct wash extremely run

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def divide_numbers(x, y):
-        return x / y
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
