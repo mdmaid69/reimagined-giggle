d6745683712bac4b6b5f3c32de20382c616c3e60
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:07:27 +0000

    fire break workupto address somewhere

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+for i in range(10): print(i)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
