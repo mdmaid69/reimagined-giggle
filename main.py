c1cc488a222f27af5891a8958c5a99e97fd183cd
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:49:02 +0000

    shoot learn benefit flood this

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
