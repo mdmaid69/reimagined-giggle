323e9d5643c6984d20890193bfd3b279e2131f9c
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:25:50 +0000

    moment whoever laugh offence before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def reverse_list(lst):
+        return lst[::-1]
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_square_root(x):
+        return math.sqrt(x)
