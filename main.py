6c6d6d3a87734ba22a92f6c8116ad924e218b392
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:09:27 +0000

    live press breakin target comeover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
