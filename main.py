6e1591181b8eba0defac6369a404d36784387a82
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:06:11 +0000

    callup of opposite comeround abroad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def sort_list(lst):
-        return sorted(lst)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  def subtract_numbers(x, y):
+        return x - y
