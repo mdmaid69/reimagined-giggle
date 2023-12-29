020ae5ddb5e25ef6e16aa14a06ca3a1a7caec05d
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:14:08 +0000

    shut appear firm shallow closed

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
