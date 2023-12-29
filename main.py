5cde2985e48eaae6fcfa90d548194792308c76ce
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:24:04 +0000

    group fault burn arrival determined

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def sort_list(lst):
+        return sorted(lst)
+import datetime
+def get_current_date():
+        return datetime.date.today()
