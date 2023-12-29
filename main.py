d1ea45536c0080e1ff8cc175b7cf490428366a6b
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:17:54 +0000

    listen Ee paper block television

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import os
+def remove_directory(path):
+        os.rmdir(path)
