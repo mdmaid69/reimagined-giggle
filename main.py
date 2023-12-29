c33936fbd8af195429f9e2d26e15f0ed06447f2b
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:55:08 +0000

    level where report spring fast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import sys
+def exit_program():
+        sys.exit()
