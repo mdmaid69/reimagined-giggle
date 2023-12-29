3cf122e41a872aee7cfe734189865eb66bd51320
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:22:23 +0000

    fear rush view happy arrange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import os
+def remove_directory(path):
+        os.rmdir(path)
