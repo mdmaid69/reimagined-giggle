4f2269110706f61967da476d6bc1307f403a135f
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:33:27 +0000

    splash escape unit scrap turnaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_repr(array):
+        return repr(array)
