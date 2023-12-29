9981b9f077c8c4852835b30b4a5a1c290b9947d2
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:54:42 +0000

    involve touch travel system nothing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
