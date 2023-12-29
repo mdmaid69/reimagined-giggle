6f08625b07c21b1d91e4ffcb8b6906692ee718f8
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:12:12 +0000

    human consult odd deserve deserve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import sys
-print(sys.version)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
