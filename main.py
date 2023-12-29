56d338329bd98b4f02a978fc8f445004f3002050
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:46:17 +0000

    guess couple every birth scream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
