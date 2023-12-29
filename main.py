80f48cc4418499694d299af0c45124107b8b4a60
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:04:52 +0000

    tidy tie declaration almost ring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
