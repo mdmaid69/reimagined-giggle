4cf1dfd874dc727eabfb5927cb26e28405b2fade
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:20:04 +0000

    crowd summer mess nearly discussion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
