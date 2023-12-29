7e785c9a8f9599044be9b21ead3e091f7e8ac703
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:08:29 +0000

    laugh several label shop of

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def pop_from_array(array, i=-1):
+        return array.pop(i)
