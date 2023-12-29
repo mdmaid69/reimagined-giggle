76c149cdff659c4769c433bf3018d090c6745699
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:28:43 +0000

    suit even pot need too

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def convert_to_octal(n):
+        return oct(n)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
