4ef9ad909b25dc97c7362f194c5cb1af82bf40a5
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:42:20 +0000

    doubt damage appearance proof sort

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def convert_to_octal(n):
-        return oct(n)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
