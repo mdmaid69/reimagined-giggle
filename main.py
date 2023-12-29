0c8b743d63f09e1e76a97dc14efe6f6885c64ef4
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:56:37 +0000

    foreign offend name soil spoil

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
