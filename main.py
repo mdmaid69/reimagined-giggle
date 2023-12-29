3a8b7adba6024b459d30c015bfab2dab1779ec21
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:55:59 +0000

    actual takeout squash consist physical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
