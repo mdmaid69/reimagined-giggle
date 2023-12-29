7b86c6814c0207aeb28e01c7bc4052cd92dfeb5b
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:27:06 +0000

    aim comeback damage after mass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_error_function(x):
+        return math.erf(x)
