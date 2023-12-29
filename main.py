216ba16208127e843bbc563484c1fcc55fbd1c06
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:24:15 +0000

    skin colour recently fire transparent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_list_from_array(array):
+        return array.tolist()
