cd7dc56220d095ce0dddd31a9dac2e79f778d011
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:23:05 +0000

    fire though same communicate beyond

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
