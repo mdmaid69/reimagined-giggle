86fa264f73d541c4bc8c83a5d0de91ff6fb43270
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:34:41 +0000

    excited off deliver trip set

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def reverse_string(s):
-        return s[::-1]
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
