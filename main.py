eaa6720f40db0f580421a7f59ba56ec66c9c7580
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:56:03 +0000

    lesson miss cap acceptable waste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import sys
+def print_python_version():
+        return sys.version
