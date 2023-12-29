0a75aff383f91274ed93d5107024652e9b20aea7
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:59:44 +0000

    term wonder finger turnup note

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def subtract_numbers(x, y):
-        return x - y
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import tensorflow as tf
+print(tf.__version__)
+def calculate_power(work, time):
+        return work / time
