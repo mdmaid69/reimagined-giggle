95891e4d87036ac68e83274a1b0b94eca3fc7b51
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:32:23 +0000

    treat miss they until something

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
