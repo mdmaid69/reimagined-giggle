fd43babb2d81e28069d7298d7d18e32f07a11d3d
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:04:28 +0000

    cover whisper change increase block

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
