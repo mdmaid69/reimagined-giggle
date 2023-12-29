86ea23aa89891e4dbb610337496d280a93063df0
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:54:13 +0000

    serious splash harm anywhere family

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_current_directory():
+        return os.getcwd()
