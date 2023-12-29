32839846fad59c0bea9bcf960e11e04e8171d19b
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:08:45 +0000

    terrible face over accident dig

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import os
+print(os.getcwd())
+import math
+def calculate_exponential(x):
+        return math.exp(x)
