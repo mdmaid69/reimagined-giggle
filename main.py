03e508937da0863516ef7009e621f3eee581fe92
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:07:11 +0000

    average often left whistle about

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
