a9c8ea8488b3a8a0ec8e40969976c61de5bd91bf
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:49:47 +0000

    public corner clean project friend

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
