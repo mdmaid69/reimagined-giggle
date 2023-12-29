eb1ebb07a553b1b8475c2e67684e1c7c6c6c7153
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:28:54 +0000

    lock material turninto breakdown behave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
