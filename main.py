2b69108ce69c67b8a53daf6646b71fa20856eced
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:33:52 +0000

    fear whom lookafter dry protection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
