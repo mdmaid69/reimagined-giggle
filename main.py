745823f234d135be2dc68ccf1ad080685151f70a
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:13:10 +0000

    rain left runinto any stranger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
