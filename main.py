51d084393e2d74facbb5e83fad8d75f77bbcb4fe
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:14:52 +0000

    report couple work soft consult

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
