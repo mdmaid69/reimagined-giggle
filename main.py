ad12484272cfc4ece101552348b0390cddb11bed
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:59:12 +0000

    kind bang why clever every

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+x = 10
+y = 20
+print("Sum:", x + y)
