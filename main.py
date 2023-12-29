e0922b8753a6d2a96c0c575a6a1e5c8a35688ee8
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:08:17 +0000

    press discuss off eat slowly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def find_min(lst):
+        return min(lst)
+def reverse_list(lst):
+        return lst[::-1]
