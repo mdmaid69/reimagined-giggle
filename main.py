feb6df65b85a44e16dab38e1528f6311b5f7c371
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:13:13 +0000

    mean float sticky Itself reply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
