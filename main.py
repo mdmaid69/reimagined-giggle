e80b7657bc25f1ffc1905b1c09d962801cdfa3b2
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:35:34 +0000

    community teach practise mine on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def find_max(numbers):
+        return max(numbers)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
