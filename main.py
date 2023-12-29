e306007428f676178cf3ff892e46d52fd8382ec4
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:07:50 +0000

    goon 568energy rule consider mainly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
