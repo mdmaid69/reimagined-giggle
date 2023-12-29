d29cfbd8673619b631cdcd1a2372f8883c28f429
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:05:41 +0000

    stare breath receive environment wide

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def multiply_numbers(x, y):
+        return x * y
