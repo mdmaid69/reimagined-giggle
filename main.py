0f8dbdc48ffcc2c999aeaa178fe05d987aa97f5e
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:53:54 +0000

    importance battery communication already break

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
