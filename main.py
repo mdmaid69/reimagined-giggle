f4afd89b5554cb0e6bf9bb25931ce0edd5fb911c
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:27:38 +0000

    moment tired wander old advice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def convert_to_hex(n):
-        return hex(n)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
