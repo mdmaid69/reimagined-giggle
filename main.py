cd3fbabfc8b97ed16f86703b6f2d5286008f0884
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:02:21 +0000

    near attach today inside translate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
