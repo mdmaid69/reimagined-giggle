cc4a324568f98036f7e60a4ed0375251c966c796
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:07:50 +0000

    across interruption hang pull memory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
