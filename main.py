7279a69e5460ec90dd404987eaf19e74bbf99b65
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:55:00 +0000

    view little Hh tape over

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
