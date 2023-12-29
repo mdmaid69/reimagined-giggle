405235cc2522fb810f9c78878d26cb41f2897904
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:28:57 +0000

    confidence height underneath pronounce spring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
 def reverse_list(lst):
         return lst[::-1]
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
