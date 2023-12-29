2f45fff10a282c8025489cb50880adc59953e354
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:41:05 +0000

    move level achievement involve description

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  def cube_number(x):
-        return x**3
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
