b7662dbc08edb035c27260881edfe1e2d6c44bc7
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:22:40 +0000

    damage turnoff callback level ability

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def calculate_force(mass, acceleration):
+        return mass * acceleration
