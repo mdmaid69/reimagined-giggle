3bdc2537f45b3bdc63e9145ca6dfa2803779a306
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:23:49 +0000

    surface deliver choice Ii service

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
