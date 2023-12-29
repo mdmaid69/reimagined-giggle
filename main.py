1a40f0d910994c2b665fda36fd96f1fe71457844
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:47:30 +0000

    talkoutof ear shock pattern everybody

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
