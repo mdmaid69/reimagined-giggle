3119edf099aa970c69e80bc5e31a28f88d76af3f
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:37:46 +0000

    talk minimum reply damage than

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
