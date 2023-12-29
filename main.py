fc554d41e3687436f90897ba22aa8aaca5e1a668
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:49:22 +0000

    its guide book story admire

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
