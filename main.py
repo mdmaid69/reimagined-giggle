aadad8241fb41bb7ec70e49f656deb7b75d19940
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:56:59 +0000

    classroom information crazy workupto learn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import array
+def get_array_item(array, i):
+        return array[i]
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
