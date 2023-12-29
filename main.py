5d1c6967dac8db7f66b3158341107f5c8c2306b6
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:53:26 +0000

    ordinary Jj about direction divide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import sys
+def exit_program():
+        sys.exit()
