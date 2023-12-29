9354bf605dc501bc1aeae4e71184fc8b87dd4814
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:38:44 +0000

    top neither apply greeting luck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_stack():
-        return collections.deque()
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
