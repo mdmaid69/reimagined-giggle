9f446d6e7291f025ef160ec1b624c9882cca7f84
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:01:08 +0000

    activity squash system scheme once

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_work(force, distance):
-        return force * distance
+  def cube_number(x):
+        return x**3
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
