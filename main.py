8cea5153397ca54b2a7ae453e944f6b1ac38c990
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:06:58 +0000

    explain wide sensitive listen grateful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
