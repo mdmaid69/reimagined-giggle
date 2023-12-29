645a6d7de75b7d20ceed2f999a51c0243593f443
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:39:27 +0000

    573entertainment finally quarter weak this

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
