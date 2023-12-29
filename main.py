7143e1945f34aaaf2ea2375f250bdd5b49a935a8
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:36:06 +0000

    task characteristic quiet anyone heart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  def convert_to_octal(n):
-        return oct(n)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
