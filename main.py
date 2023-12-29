53f4af2bd1bf744f32d144be5190255ceb679e0d
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:07:55 +0000

    memory study report gift mine

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def calculate_pressure(force, area):
-        return force / area
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
