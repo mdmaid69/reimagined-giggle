3ae8bc96ce4f226bb180930dbe6f936f772c0343
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:57:23 +0000

    sport yes workon value famous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_speed(distance, time):
+        return distance / time
