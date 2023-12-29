16879d6e978cf9072a28dd00503bf26320d4f1ee
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:22:37 +0000

    know gate such for behind

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import time
-print(time.time())
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
