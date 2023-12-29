7144ce6c4c603a000c175a55e85d2ef73255ccb4
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:16:00 +0000

    heart limit between clue trade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import time
+def get_current_time():
+        return time.time()
