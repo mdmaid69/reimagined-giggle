590abbf2017b1e0f030e0821609b792bf2824ab7
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:46:41 +0000

    injury ashamed think run future

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
