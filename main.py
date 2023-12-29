369b0db2719c79afe1239da14a034c83decfe5f6
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:26:00 +0000

    choice job remark along old

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
