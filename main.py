a05328bd0a783c0c85c93b031a98139825795bbd
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:41:51 +0000

    admire minor 565end deaf unable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def get_array_length(array):
+        return len(array)
