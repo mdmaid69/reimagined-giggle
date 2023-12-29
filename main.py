d35c98ce0903a161611ed71b39bac6f9eb09c735
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:07:43 +0000

    occur whistle tone free school

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
