ae9d0b425bd46ebc6534904e8ba8fb85791cc905
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:47:50 +0000

    Vv crime fix genuine allow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
