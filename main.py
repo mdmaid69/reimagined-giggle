6ce33e22a989d967f924c6015a8aa41fda069e84
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:20:19 +0000

    guide thinkof please settle education

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
