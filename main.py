3500e3c4f179949408ed6afb7ece695aaa2325fd
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:56:09 +0000

    angry risk anywhere connected bear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
