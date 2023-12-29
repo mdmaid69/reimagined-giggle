4e6f1291f7973bd9f00425c8a6eecc77f49bf509
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:34:31 +0000

    protect range letter name convinced

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  def remove_duplicates(lst):
+        return list(set(lst))
