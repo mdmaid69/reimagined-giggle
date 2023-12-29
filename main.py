b45a6e88447f519d3810ece9ff837b9e996b8101
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:26:00 +0000

    forget flow cost charge but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
