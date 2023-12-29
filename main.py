1f3622830530cf4c5e3ab59e382f19e0185d6624
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:49:41 +0000

    serious contact card nice peaceful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  def multiply_numbers(x, y):
-        return x * y
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_current_directory():
+        return os.getcwd()
