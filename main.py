1bdecea0317c4d33d468b1dbbd0cc7831fd6e5b2
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:12:27 +0000

    dream side yourself Ll drive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
