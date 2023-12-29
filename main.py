77785d0c7bfd4d21ad285321b74d558e77172336
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:37:36 +0000

    somewhere hour dick hair position

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
