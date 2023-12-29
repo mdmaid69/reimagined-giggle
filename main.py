832ad77a1606722a4a9cab33c4f18bb2508707bb
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:32:38 +0000

    separate distant the quick meeting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  def subtract_numbers(x, y):
+        return x - y
