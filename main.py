729daaa528645f98bafd169d2426129be65410be
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:11:44 +0000

    report taste talk speed because

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
