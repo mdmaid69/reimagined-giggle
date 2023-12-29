c2fb2fa1b834ad1ebf073033c4340aa6164aa399
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:24:14 +0000

    apart charge wind effect 566end

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import platform
+def get_os_info():
+        return platform.uname()
