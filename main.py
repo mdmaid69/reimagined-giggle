78b15cb546832456ccaf02b2a638bb5cd86c0318
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:42:03 +0000

    tragedy there familiar unit thank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
