4dd5ed7e5a7283d137990ec1aa6e9934f4f9c65d
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:11:49 +0000

    guard most quick combine unkind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
