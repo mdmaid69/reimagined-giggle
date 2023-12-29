e65252166e5e642a483bcf3ddd6a490049a7f3a7
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:11:27 +0000

    its object brush like pile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
