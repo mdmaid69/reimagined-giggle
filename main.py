da262500a5a48d8035bcc5deb5800757cf29ad5b
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:44:34 +0000

    throw pump string brief slow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import collections
+def create_user_dict():
+        return collections.UserDict()
