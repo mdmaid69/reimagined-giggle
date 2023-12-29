f679764e6163ec220dedb3192ba4fb24bfd40555
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:57:31 +0000

    objection goback(to) coffee dance appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
