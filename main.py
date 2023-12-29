25ca6699592e5a43fe18fd1829f85f7c7f691b09
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:23:19 +0000

    shame flood certain hit dream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
