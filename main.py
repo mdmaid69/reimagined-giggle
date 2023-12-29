1d86ef7d95e9270ad34d0acea614793844e0a1a5
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:07:36 +0000

    there might pile pronunciation declaration

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
