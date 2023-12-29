369677a3e052b61fd3cb34a61bc4e8cce7f02461
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:12:21 +0000

    child mean tour makeup pool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
