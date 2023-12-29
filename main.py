9181a2027f895bfdf5b834d0cc684056390aee33
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:00:46 +0000

    effect ring radio foreign refuse

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
