b7e9317fa7ce5e8be920047a49f5cf8b88c1931d
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:02:42 +0000

    past pool see thumb floor

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_speed(distance, time):
-        return distance / time
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
