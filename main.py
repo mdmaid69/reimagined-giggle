c79bc314f65c4c80d513ce2395a31e3bb18bde61
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:46:10 +0000

    brother Rr rest however under

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
