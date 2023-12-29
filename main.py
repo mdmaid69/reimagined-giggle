52b0a91b1c3a2b2904c9768ba1efd1aacb72218d
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:49:53 +0000

    difficult law soon rain claim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def add_numbers(x, y):
+        return x + y
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
