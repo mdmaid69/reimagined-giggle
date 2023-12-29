248925203c1d8d23a5979d7646d1e3695b77e314
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:53:34 +0000

    skin solve accidental modern hardly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def sort_list(lst):
-        return sorted(lst)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
