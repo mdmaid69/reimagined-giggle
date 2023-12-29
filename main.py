4861b0f8421480806b35c1c821545deb845dc8c8
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:54:59 +0000

    turnon respect such exam wild

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
