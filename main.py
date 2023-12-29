37ce5cea90a0c7592ff885f0baad44be81087824
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:17:18 +0000

    powerful raise create expensive wander

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-from collections import Counter
-print(Counter("hello world"))
+def remove_duplicates(lst):
+        return list(set(lst))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
