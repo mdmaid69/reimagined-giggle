efa418dd5580877fe9f20122c7d17537b405d887
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:47:16 +0000

    used sad choose call right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
