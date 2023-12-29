c21ae65c5a37a3c4446aff0780f437cbb73a3e2d
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:59:26 +0000

    waste black trust morning score

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_speed(distance, time):
+        return distance / time
