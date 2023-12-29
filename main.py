f653b09dc5bc5f8860895569045ce3515b6beaed
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:19:09 +0000

    free work moment everything left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
