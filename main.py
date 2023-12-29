fcfab1ad3833d6ce7168d0eae5b4b15769485c1f
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:42:09 +0000

    sweep certainly train raw Nn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def is_even(n):
-        return n % 2 == 0
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def add_numbers(x, y):
+        return x + y
