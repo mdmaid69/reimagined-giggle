90c3e16e23e046a0ce0879975368c415793c5f57
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:47:11 +0000

    shape else it Ii trust

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def sort_numbers(numbers):
+        return sorted(numbers)
+import array
+def get_array_as_float(array):
+        return float(array[0])
