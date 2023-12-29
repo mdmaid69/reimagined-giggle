b7535d2694861a8d25fa517a2591d2631b6a46cf
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:22:39 +0000

    fast false conclusion storm nothing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def count_elements(lst):
         return len(lst)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
