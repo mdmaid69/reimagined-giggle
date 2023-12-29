34cc4dc7fc7427791649a1f946465da38deabea7
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:34:31 +0000

    funny if lend payment our

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-print(sum(range(10)))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_exponential(x):
+        return math.exp(x)
