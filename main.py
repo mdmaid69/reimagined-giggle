a42babc4bd41676a45f91191ede090b990548b7d
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:19:39 +0000

    in exchange runinto worth expensive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
