ff7568a9fa703457c2772436e613db85cef1b47e
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:55:27 +0000

    explain border direct less wish

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_work(force, distance):
+        return force * distance
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
