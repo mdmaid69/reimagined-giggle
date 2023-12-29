d6b0f7cad26188b443931e1abd279571b5efcbeb
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:02:10 +0000

    worried work odd team left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_speed(distance, time):
+        return distance / time
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
