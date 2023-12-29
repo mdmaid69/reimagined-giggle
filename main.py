03f4707057c6c5cea72b08a040b42ae9dd9daf4f
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:02:38 +0000

    sore flow appeal visit bored

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
