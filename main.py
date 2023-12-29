a3ed02f5e5a6b62bcb9db7811c3737066cd91880
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:34:37 +0000

    without you wise breakfast cinema

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
