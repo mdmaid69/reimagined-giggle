d515ae1cf2b2cc51d5dcc4dc5d71ff16c64d0a60
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:26:37 +0000

    just by enthusiastic bin entrance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
