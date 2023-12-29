fe02d90f75da705abc883db4d76ec257f0cb4ec7
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:10:12 +0000

    radio artificial dance shut face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
