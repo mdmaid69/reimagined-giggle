bfbac8663c98227e82b42a494440de03bc56014a
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:19:37 +0000

    Itself definite strong friendly however

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
