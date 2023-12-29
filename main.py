f39b442c5fdd3ff1b2620058a40f05d6b6471159
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:49:33 +0000

    whichever culture underneath contribute shock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_current_working_directory():
+        return os.getcwd()
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
