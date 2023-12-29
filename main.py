3a361f2a2cccd586f091cc4f44e7de70f494372a
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:37:08 +0000

    tax nothing trip peaceful middle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_speed(distance, time):
+        return distance / time
