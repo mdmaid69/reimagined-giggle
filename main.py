19d09b2373344764ee6d7753972542e44a633c35
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:05:35 +0000

    spread correction before car punishment

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import math
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
