5614ea9d1f99512f17fd2f65e6a81f92444ab8ea
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:55:07 +0000

    history test easy dance clearly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
