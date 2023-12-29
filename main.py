6a62704cb218cc3cafb7ef31d6fd7aedef5b40b6
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:04:45 +0000

    their our reply kill task

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
