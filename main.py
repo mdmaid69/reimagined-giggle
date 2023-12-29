5f5c4eb89fbbb7b62bfb21e40c51f14233d546b4
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:00:51 +0000

    identify smell copy coat likely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
