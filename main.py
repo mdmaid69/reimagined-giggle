f13afbe984b32301eb2faf7d8a33b364fd4a6ec2
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:40:18 +0000

    taste taste top introduce also

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def delete_file(file_name):
+        os.remove(file_name)
