2de466ac04e33e2b5653780bc4c1c379264e7ded
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:28:27 +0000

    spare brief alive shrink takeoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
