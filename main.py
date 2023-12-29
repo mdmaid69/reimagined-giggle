9493fa9b787d12fae6d8abdf3888ae97aee4d391
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:13:30 +0000

    question work let neck fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
