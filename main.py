b8fbc18505990da2c98bb5c4d5eb6ead7ca7cf9a
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:47:17 +0000

    mark pool personal cost detail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  def subtract_numbers(x, y):
-        return x - y
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+print(math.pi)
