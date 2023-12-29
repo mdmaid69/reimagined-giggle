4a9a01edf7b4147ac37803b76d392cab3345a56b
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:57:05 +0000

    tear pure grade criticize noise

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
