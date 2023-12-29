b31acfdaeb3baa23d0928752892bfd6cfc031de3
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:41:01 +0000

    big picture cinema valuable choose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
