7742ffe03735ad9fa52ff615f852e1849a1485d1
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:02:50 +0000

    admire clear if usually fashion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
