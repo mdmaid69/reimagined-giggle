e232d877da1ac859b31e520d070f2c8f3927c38d
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:19:07 +0000

    runinto polite advise actually second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
