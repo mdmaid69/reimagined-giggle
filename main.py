e83eb0d7e21c13bdc6c393dd87312229500beb1c
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:44:17 +0000

    pencil across divide stand cry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_work(force, distance):
-        return force * distance
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_density(mass, volume):
+        return mass / volume
