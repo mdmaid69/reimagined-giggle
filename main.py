d16a7dc31731dde9d436de41d1a62bd90e688c61
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:23:52 +0000

    commit consult bright board from

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
