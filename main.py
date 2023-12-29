f93e4a2d56d0b3d69a668d818dfc2756437bd8f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:58:11 +0000

    ago convince rain press mail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
