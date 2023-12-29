a9f5960f50dfbb20360d7a955f09cabd38657a8d
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:51:51 +0000

    convenient shrink neat profit immediately

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def get_array_as_str(array):
-        return str(array)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
