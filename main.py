03fc5c9b7589cf942671f6e8352c277a1a201967
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:53:05 +0000

    bottom outside change weigh future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def get_string_from_array(array):
+        return array.tobytes()
