9c63e9b90bdd14029b22ed5eef9b575521de5d26
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:55:16 +0000

    Pp when time method connect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
