4f33feafda078dbce3cd9ff3c6145bd446eadec8
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:20:30 +0000

    laugh club whole bang start

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_max(numbers):
-        return max(numbers)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import numpy as np
+print(np.array([1, 2, 3]))
