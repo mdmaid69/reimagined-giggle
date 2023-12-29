b825f7f413df39190464ebd7b976b3769a4b4453
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:28:52 +0000

    callup hurt fall possibly new

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
