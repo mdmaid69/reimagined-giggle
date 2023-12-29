dc65cecd91d911da13e56b2ae476bc4b286edd7d
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:59:25 +0000

    past whichever by steal murder

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
