a9a35decd3b0100319f0cccc838a4b3411205a49
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:04:51 +0000

    generally sick gas balance keen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
