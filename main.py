dba24ad4ed2e6f125b67557762501c229698091d
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:04:49 +0000

    fat powerful nurse bad getoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
