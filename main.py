2c77dffa8e8bddadd6f82d49cf7d5c1094a9ed7c
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:14:09 +0000

    over hurt catch their passenger

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import sys
+def exit_program():
+        sys.exit()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
