88c19cae985276d1967fdf7bac4d8edff7852b4e
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:00:18 +0000

    pin find clear over help

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_min(numbers):
+        return min(numbers)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
