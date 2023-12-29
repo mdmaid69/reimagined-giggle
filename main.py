cc893ed98ea99dd69234e48509e15114c7fee8e6
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:40:36 +0000

    owe target beyond joke true

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
