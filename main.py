5f283e709d790a944f158574fce90bdd7da41bff
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:28:01 +0000

    notice promise tie make exchange

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
