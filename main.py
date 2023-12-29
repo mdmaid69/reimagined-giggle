f43cfdbe7d8716a7edf0ba23da470da3cc2bd360
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:58:53 +0000

    important dead angry rough side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
