6fc12d1d37d74eb33472c45b52c6be7adfdbce43
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:28:33 +0000

    lock borrow stage minute crisis

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
 import collections
 def create_default_dict(default_type):
         return collections.defaultdict(default_type)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
