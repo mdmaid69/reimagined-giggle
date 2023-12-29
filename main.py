ce725f574c249a884af4e2df5d27b91f37bc048a
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:47:33 +0000

    never thing low final join

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def remove_duplicates(lst):
+        return list(set(lst))
