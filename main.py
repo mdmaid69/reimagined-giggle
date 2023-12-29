5caba28cb1d12dee24118cfba98285ca0f6c5097
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:28:25 +0000

    beat club further chief lower

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
