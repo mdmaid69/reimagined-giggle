6916991c3582e8db983988404151c438d3af6d95
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:30:44 +0000

    occasion herself discussion sure wonder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
