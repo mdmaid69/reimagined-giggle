131c79b61af3471a4484cbbe8361f2f18c58d8ff
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:17:43 +0000

    star lot convinced estimate regret

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
