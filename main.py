1260187f7583a86981112a7a06b6fefd8fb85eaa
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:17:10 +0000

    trust snow wherever rush seem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
