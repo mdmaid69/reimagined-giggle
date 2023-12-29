190b96844d02959f70c5b5b05edc48cde944ea0f
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:01:24 +0000

    contact right stretch any improvement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
