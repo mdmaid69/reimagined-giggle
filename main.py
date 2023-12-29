2d2e39a09e22cf896785aba5b058f29639d4f212
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:45:36 +0000

    knowledge justice traffic tidy aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
