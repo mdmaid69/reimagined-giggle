a19e826a0ecbb01a86cd8471e75956a5889a999d
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:57:28 +0000

    harm hard look fit hurt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
