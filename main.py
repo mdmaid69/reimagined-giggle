3838ab13b74a42d3145204e9f7f56e9cc8d5e3c5
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:37:46 +0000

    always object name compete actual

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_pressure(force, area):
-        return force / area
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
