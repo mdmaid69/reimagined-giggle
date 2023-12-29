46519ca6f1057e330111460bc6cf7dbbe0b79b9d
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:59:23 +0000

    calm square priority order roof

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def divide_numbers(x, y):
-        return x / y
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
