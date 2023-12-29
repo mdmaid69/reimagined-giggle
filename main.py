f6dc317d2457c4da4f6640dcbfc71578879d43b8
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:42:37 +0000

    usually disapprove valuable do pen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
