7ff7330f3f325907c89dbb1e24d63fab9999eedb
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:41:46 +0000

    scrap accidentally winter bit reaction

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
