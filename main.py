db1da50f501663b3b4489025547fa493cadd93a1
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:59:52 +0000

    main prove less hardly expect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
