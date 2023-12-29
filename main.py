39701fd7930dd7019b25ef59fb1f40fb237793c1
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:23:54 +0000

    fun case air grade destroy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_error_function(x):
+        return math.erf(x)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
