84c7b7f062dd42a42ebe442e481a291665399af8
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:58:58 +0000

    bath colour stupid wait hole

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
