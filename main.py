ee8669db629f4516a0db322f437649a471c6ecac
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:30:10 +0000

    into accordingto month thirsty Jj

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 def calculate_power(work, time):
         return work / time
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
