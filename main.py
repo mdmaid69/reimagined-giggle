51e32510607d942fae8d6b1f1a1311380d4d9bc7
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:56:06 +0000

    exercise vote half hers give

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
