8256dbc3978ccb105a63dc75868720bfefca5660
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:36:20 +0000

    hospital bringforward charge prove speech

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
