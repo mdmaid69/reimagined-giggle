6e3948029bd303e266b150cd0f1acfd607bb93d6
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:59:46 +0000

    clear precise steal imagine row

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_as_set(array):
+        return set(array)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
