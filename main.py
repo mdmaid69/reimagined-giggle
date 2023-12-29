b422e24e86ae3dfa76d1083eb5d6fd78ba6e2207
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:52:14 +0000

    natural issue develop fair married

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def remove_directory(path):
+        os.rmdir(path)
