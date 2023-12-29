3113af4a13c313e62844760cb4415febc8b1ed4a
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:15:52 +0000

    several quickly project getout come

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
