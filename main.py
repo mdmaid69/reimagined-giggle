e9db1e28c3f5b9f82cd276c0639b18c90b811fd4
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:31:01 +0000

    anyone less another Dd behind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import json
+print(json.dumps({"name": "John", "age": 30}))
