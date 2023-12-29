7b08278fd62e911c7534e02f61fa58d962981fb3
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:57:16 +0000

    trick very display sink wonder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
