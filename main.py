5af2336edc5fed3ee1b998394c9c2a8db2e1b5d9
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:22:54 +0000

    definite repeat best ring schedule

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import random
-print(random.randint(0, 100))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
