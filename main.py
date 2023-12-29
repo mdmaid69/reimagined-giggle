61127a845ccd4c91ae042d5794f731a22be75bbd
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:58:05 +0000

    picture turndown little rescue hope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import sys
+def exit_program():
+        sys.exit()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
