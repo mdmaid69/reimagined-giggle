4d296ef1e13b7cf96bdaecf6d9aa8ed661adfddf
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:06:56 +0000

    bath tonight still keep ball

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
