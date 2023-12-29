15fee4d8602c6dce7bfb5847931676b713221745
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:50:02 +0000

    choose temperature abroad case polite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
