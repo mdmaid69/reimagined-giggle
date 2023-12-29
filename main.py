5c9bfb738a0586949e3c92b38fc32471453f11f6
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:31:37 +0000

    prepare total skin during opportunity

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def get_array_as_str(array):
+        return str(array)
