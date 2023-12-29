93fb8663865033b8addac5628115f2587a038a46
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:01:29 +0000

    order apply recommend kick bright

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
