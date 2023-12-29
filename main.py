9bf4db3db6c653b6b63984df7432dc28e8055c60
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:37:49 +0000

    corner song oil ours set

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import random
+print(random.randint(0, 100))
+import json
+print(json.dumps({"name": "John", "age": 30}))
