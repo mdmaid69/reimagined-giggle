3560c761ba4e8492a4adecbcbf1f8e578ff051d7
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:19:06 +0000

    connection absolutely male better since

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
