ee415b6f96b1d807f3c65cae45cb46b40b62098d
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:08:22 +0000

    exchange separate form repair prove

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import sys
+def print_python_version():
+        print(sys.version)
+import sys
+def print_python_version():
+        return sys.version
