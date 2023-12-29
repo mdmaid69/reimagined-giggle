7317396ba35a3caacfde8adc803dec418314521c
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:15:11 +0000

    565end zero serious embarrassed wish

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
