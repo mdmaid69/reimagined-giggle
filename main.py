4c080a67a7c9287c8f029bb012872d9b1256b838
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:37:46 +0000

    other switch then condition upset

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_work(force, distance):
+        return force * distance
