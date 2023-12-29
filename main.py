8e2ef209480fafa9f4e58980b4991e1077342cca
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:04:33 +0000

    food shop classroom similar at

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
   def get_file_blocks_allocated(file_name):
         return os.stat(file_name).st_blocks
