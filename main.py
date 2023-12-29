c17dc1e5141e14cef9192e6f9f7fb5d75d592788
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:37:21 +0000

    petrol belong shame murder and

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_username():
-        return getpass.getuser()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
