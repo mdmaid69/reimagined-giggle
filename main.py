9220d6afa0f4b314a4ff06d1a5078515e9527d89
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:50:14 +0000

    habit regret her turnon side

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import getpass
-def get_username():
-        return getpass.getuser()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
