f2287e9214a3778ddea7d1d0f5b77576eed2baba
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:47:15 +0000

    post bringback damage report fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
