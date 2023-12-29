552eb1f53e50ebe4512df257c2ee9876b7719305
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:24:15 +0000

    dead refer towards 572enter takeaway

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
