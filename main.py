f313f6f460d38555597ac211e15ac024f2084fe5
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:02:37 +0000

    list holdup commit sale no

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
