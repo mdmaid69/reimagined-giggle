ac31c7b11ae5e90505cb9854df042ab56b1fe62d
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:42:44 +0000

    bite find pick pretend emphasis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
