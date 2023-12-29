b128c708adcb67792e48cf10f99078dacfc34e89
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:14:10 +0000

    squash 571enough clearly minimum workupto

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
