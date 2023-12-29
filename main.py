e4be6f82243f7d2f5de9990788db796a8bd89522
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:36:31 +0000

    suffer education polite form uncomfortable

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
