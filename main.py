99db16f8080abb11ea83b0ab1105a5f4ebd04d0e
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:10:14 +0000

    foreign spread careless teach cope

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
