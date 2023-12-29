014cfcdd9dcd374bf32898ad5f2ef9f4f506585b
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:27:53 +0000

    body fun collection excuse mention

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def subtract_numbers(x, y):
-        return x - y
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
