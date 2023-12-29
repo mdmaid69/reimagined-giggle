6e346bb3fd58d9c6cc22e25f8f7ea40c02be9c99
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:32:33 +0000

    succeed fat comeoff shop sharp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
