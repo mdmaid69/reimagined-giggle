e5cedb6c3078b3685f5265886c436f06e5809c2b
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:40:06 +0000

    can basically worst tough work

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  def square_number(x):
+        return x**2
