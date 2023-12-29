80925ef2378e73dff00cb974c7c0f8ef3490308a
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:31:29 +0000

    challenge please introduce high turndown

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_counter():
+        return collections.Counter()
