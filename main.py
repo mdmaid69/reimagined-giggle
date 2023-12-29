e2d182f1c963995afa17046ae46255995a7bd9ee
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:57:16 +0000

    free television bus water unlikely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
