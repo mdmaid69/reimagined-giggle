d6222c8b1fc3a7494959ff3f426849ad46a2361c
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:24:16 +0000

    standard relief gather bet underneath

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
