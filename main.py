44cd3472e32e6725fdbf3fb98a9d2791606d1fe2
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:26:45 +0000

    callup mass about separate stick

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
