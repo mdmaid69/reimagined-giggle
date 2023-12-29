73e2ed73906eab8a1f9dff7acca5d89349b04b5f
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:11:21 +0000

    takeoff weigh far trap wet

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def divide_numbers(x, y):
-        return x / y
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_current_directory():
+        return os.getcwd()
