e0d9e71a082405137811d44174a700f9b1b17796
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:29:25 +0000

    concerned interview vision science intend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def is_palindrome(s):
+        return s == s[::-1]
