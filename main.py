5aededca1ade5796b42972683e7752bf6e540a26
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:02:17 +0000

    way callback turninto chat able

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
