ad848c626e8fcc740749d45ecdfcdd854eb35119
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:23:50 +0000

    stay government bringforward pollution probably

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
