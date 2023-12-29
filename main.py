8239271309ab9de0fa5293c5c424b844592c394f
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:59:46 +0000

    drawing follow sink own breakthrough

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import numpy as np
+print(np.array([1, 2, 3]))
