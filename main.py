20d808b71a63c0d701c74985e56a92aeb2ab7d93
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:56:48 +0000

    leg total scene seriously share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
