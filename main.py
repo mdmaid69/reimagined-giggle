6aec2f0866346b880c82784d4d50ab2efda0ae03
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:17:31 +0000

    consist giveback lie on shadow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import time
+print(time.time())
+def count_words(sentence):
+        return len(sentence.split())
