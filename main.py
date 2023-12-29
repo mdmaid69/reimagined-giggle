692c6d155ca98169ca368ba148fcb355bfca8b7b
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:46:20 +0000

    protest first or behind help

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
