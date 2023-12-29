5b4f8cbc0fd11b13e0ec977e1d6ab3af2dccfe50
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:43:47 +0000

    pencil pool cold picture work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import array
-def append_to_array(array, item):
-        array.append(item)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
