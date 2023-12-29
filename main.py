3ecfdd59b237306c482db9ac00d520bb4ca6ba45
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:35:55 +0000

    join string foreign entry cook

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
