c1e9d1e479102ecf1054224b91a3ed3dc51ef9cd
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:16:41 +0000

    gentle comeout wish blame language

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
