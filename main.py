9cfa980362c43f3ed1a7d673107026b6f0924531
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:29:53 +0000

    lead point tongue remark left

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+for i in range(5):
+        print(i)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
