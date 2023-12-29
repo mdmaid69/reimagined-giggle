b4d14106de0dd4b7e438de649d928cf3d3ac42ad
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:27:39 +0000

    instant succeed central sure fortunate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_current_directory():
+        return os.getcwd()
