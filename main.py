23286854ddffb3af26b6e228d01fb513b76a04a5
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:22:49 +0000

    dream convenient relief rush godown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
