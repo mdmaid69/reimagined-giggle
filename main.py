3a1f84e8f3506e0364afc68327431ed7623b68a9
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:43:57 +0000

    pair ground education film workout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def delete_file(file_name):
+        os.remove(file_name)
