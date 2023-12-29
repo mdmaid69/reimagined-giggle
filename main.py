58018cf172e5bcd3a76aca72f02c0c22ca2161ef
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:58:31 +0000

    otherwise exciting chance habit flow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  def reverse_list(lst):
-        return lst[::-1]
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
