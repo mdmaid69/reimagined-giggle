d9002c7e8baa8b34de69729252e6872b340cec59
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:37:29 +0000

    slide record honest call freedom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
