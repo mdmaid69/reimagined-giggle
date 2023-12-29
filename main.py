65c1142b064b1f47ac795088d92a0d2faca11fc2
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:54:34 +0000

    genuine punish room appear gather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import tensorflow as tf
+print(tf.__version__)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
