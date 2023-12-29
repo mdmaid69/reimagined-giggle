004cbd35ed633dbd0008bd20029a845111e82bb0
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:23:55 +0000

    scream software walk post ticket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import random
-def roll_die():
-        return random.randint(1, 6)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
