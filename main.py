cf3a1ea5e9685f91f4160c93d7a8dedd4b4310a6
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:44:34 +0000

    concerned produce comeoff breakfast turn

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def find_max(numbers):
+        return max(numbers)
