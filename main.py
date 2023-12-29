3a33b321cc6c1ef43c9a01731c2aabfb98014d68
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:12:40 +0000

    keepaway upstairs opposite thank drop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_volume(length, width, height):
+        return length * width * height
