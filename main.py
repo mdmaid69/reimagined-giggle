eff77b4006ddb122cbc3734ff9734af52514feb2
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:58:51 +0000

    rescue extra disagree satisfied variety

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_bool(array):
+        return bool(array)
