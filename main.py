8c10b6408c563b27a7fe73c5d0bcd0c934189e9b
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:11:24 +0000

    only collect arm expression represent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_permutations(n, k):
+        return math.perm(n, k)
