405fad5c233a6f07e205389fd766346d9ac1d753
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:10:35 +0000

    plain middle beyond gas practical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
