c3cb35762bb6bdb5b57c3c2bfc237cd9f8f9b1e0
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:44:38 +0000

    temperature glass comment comfort please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
