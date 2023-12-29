fde35a10a00767cc985ae5f472d12b3bb93c1f78
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:21:54 +0000

    careful clean through pause weigh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
