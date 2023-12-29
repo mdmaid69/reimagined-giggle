fdb40208df4e702cbec4d1650fa0f4fac7398436
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:52:47 +0000

    rise rather mix upwards protect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def subtract_numbers(x, y):
+        return x - y
