34ccb54fab0d13c16180ccfd934cd0790dac0890
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:16:02 +0000

    common glad mend divide clothes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
