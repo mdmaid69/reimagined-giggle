06e505c0b9de71cd28a238b41b50fae8e86819ad
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:34:18 +0000

    forward switch morning responsible occur

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
