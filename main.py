61ae2af5695d53b36e101ea7f3dfeb1a6be43df5
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:48:09 +0000

    practise national head grand age

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
