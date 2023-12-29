3c84bcf755039c8f2625e3803f7d001ee59c8c73
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:02:42 +0000

    smooth mail admit different steam

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def square_number(x):
-        return x**2
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
