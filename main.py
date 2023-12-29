b291a024ce8d04614e746d5178faa6e2e5977194
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:07:08 +0000

    capital worst thankyou shake separate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
