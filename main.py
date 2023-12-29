ad6c8e4cd160fed53afbf37006485c4bdebaac5e
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:04:26 +0000

    greeting tap near if relief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
