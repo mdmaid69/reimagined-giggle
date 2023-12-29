dcca498423d2354991880a3ed3831bff0572c0cd
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:03:17 +0000

    sure want principle easy keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
 import sys
-def exit_program():
-        sys.exit()
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def add_to_python_path(path):
+        sys.path.append(path)
