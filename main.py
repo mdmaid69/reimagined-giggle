f028bcf07fb292c1c3bca692f7a5ec1ec197a57b
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:24:49 +0000

    save comein common shape recover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
