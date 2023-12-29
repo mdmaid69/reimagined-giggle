11b685b093e6d395345b6cf07b0a12f18a14524b
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:47:23 +0000

    minority support use press Rr

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_current_directory():
-        return os.getcwd()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def add_numbers(x, y):
+        return x + y
