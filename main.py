65a38f8a347617c90fffae54384e43b543915ee7
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:35:54 +0000

    money wild medium avoid base

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
