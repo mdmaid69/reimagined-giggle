ab17fad80a229878428404940d86a84240515e7f
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:52:52 +0000

    picture wait both harm nothing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
