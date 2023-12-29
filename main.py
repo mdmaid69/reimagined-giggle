7ae62f245e096551c8fd9578ec0613d58e7cf0b8
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:26:40 +0000

    more separate scheme tap injury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_density(mass, volume):
+        return mass / volume
