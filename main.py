e088749f102297a2c49e7f31952781a1236bcb04
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:56:46 +0000

    neither outside shrink big pride

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
