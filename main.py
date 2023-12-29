e20a6ab98f43d95d06d0c8608f7a682a4a6175b9
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:37:22 +0000

    battery consult radio blame continue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
