4852256e18b2d38578defadf7088460ae819f7ee
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:29:02 +0000

    dear fall sister fast fault

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
