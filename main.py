b4ad5d7e9cf386efc17735de3bf6f48f7dcb96e1
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:25:19 +0000

    organization dish scheme street wear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
