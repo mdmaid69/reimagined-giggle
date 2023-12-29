ad23cbf0c438db198c0a338e028308ec3c2ce5a2
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:30:22 +0000

    bet mistake cough angry worst

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def square_number(x):
+        return x**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
