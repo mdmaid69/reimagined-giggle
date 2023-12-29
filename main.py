0bfebc1088969696a9cd77d77ddf81cedb6ec91b
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:13:50 +0000

    turnout hot muscle sore impressed

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import random
-print(random.randint(0, 100))
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
