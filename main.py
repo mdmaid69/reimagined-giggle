94b2631d84caf0f8c6aa4d038b82c8c7ec1d0add
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:33:55 +0000

    nose sleep team anywhere perfect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
