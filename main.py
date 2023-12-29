d7c69c9b0e53c9f9bbf1ae7227823239904e464b
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:57:59 +0000

    many central thick basically advice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
