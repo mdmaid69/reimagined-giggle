9d3cc41a3a9793e307a456d9ce3de5cf88a956cc
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:23:06 +0000

    flood remain only complaint short

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def count_elements(lst):
+        return len(lst)
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_arc_sine(x):
+        return math.asin(x)
