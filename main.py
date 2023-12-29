ef6ae49feb45450b809ec51214327bd25ce32843
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:01:49 +0000

    second father telephone crowd himself

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
