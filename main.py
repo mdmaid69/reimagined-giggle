49eb4329a3ba34526e9c998489e4f62865ccd243
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:05:37 +0000

    theirs injury talkoutof twist common

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def find_min(numbers):
+        return min(numbers)
