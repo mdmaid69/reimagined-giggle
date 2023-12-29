0a3e49d2213d157993ca927b6929c918ac6a94fd
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:52:28 +0000

    influence sound eye tour body

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_item(array, i):
+        return array[i]
