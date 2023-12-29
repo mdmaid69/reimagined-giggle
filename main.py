b9843f2ff7441f856b219184b0c573390fb4ea0d
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:16:11 +0000

    its corner general rarely talkback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
