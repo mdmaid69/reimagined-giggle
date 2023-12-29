2bd243dcf74c48ae2d680f618ba50a8115c29843
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:50:23 +0000

    generous so check generally worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
