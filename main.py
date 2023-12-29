e41d36e8f06cd7f4a84323c66a4ba06cd38cb55a
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:25:59 +0000

    comeback invite book rate myself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import json
+def convert_to_json(data):
+        return json.dumps(data)
