667cde843ce9902efecb94f49af6cc2d4127376e
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:51:11 +0000

    even keep protest wash real

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
