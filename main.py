65e1896de2c1db6572bf44762b678cd53d7526bc
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:05:38 +0000

    receive education stamp back impressed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
