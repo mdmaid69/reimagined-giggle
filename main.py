bd07299f731f3f84e03652a59dee3014545afd6b
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:00:36 +0000

    mistake strict lucky getup deliver

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import time
+def get_current_time():
+        return time.time()
+import array
+def get_array_as_set(array):
+        return set(array)
