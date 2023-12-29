b24ec2ec97afec8839b6d832d983b5d9b18cd652
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:04:23 +0000

    cost it summer keen file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
