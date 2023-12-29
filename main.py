2300d350835008118fca0be2591480307955fb24
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:28:50 +0000

    brush business rarely impossible let

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
