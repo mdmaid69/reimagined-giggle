1ca40be9d1b059fe29bd4f24ec56b4f1b22f04cc
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:31:54 +0000

    art criticize constantly as experiment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
