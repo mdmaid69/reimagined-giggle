f2983041e7c4199cbd3fdf6746add3545c8c35b6
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:52:27 +0000

    at bother amount apply Dd

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+text = "Hello, world!"
+print("Words:", len(text.split()))
