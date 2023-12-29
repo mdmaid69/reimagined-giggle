471644fe248a623c61a858388168916f89b17c11
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:52:45 +0000

    suck anxious he while common

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
