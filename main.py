355d57a1c600b243b04b0a74f20392b9f0f31c6c
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:56:36 +0000

    ring joke wind vote design

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def find_max(numbers):
-        return max(numbers)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_work(force, distance):
+        return force * distance
