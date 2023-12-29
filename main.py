27440e41eb1aaea82e39afd9e5091f12766311c8
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:26:14 +0000

    frequent for warm fall play

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
