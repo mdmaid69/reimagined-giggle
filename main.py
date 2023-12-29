9da50f92bbc2dfb948970c59e47f56b181eb2b49
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:36:45 +0000

    wire sharp chance without bye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import random
+def roll_die():
+        return random.randint(1, 6)
+def count_elements(lst):
+        return len(lst)
