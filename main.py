a9f4eb378e0451efc70421dd3e19bc6236a0d9c4
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:32:02 +0000

    measure Ss holdout wear burn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_tangent(x):
+        return math.tan(x)
