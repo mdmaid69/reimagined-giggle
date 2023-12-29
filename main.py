00c1e829a1624e7da35ffd073117327638a1d69b
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:43:08 +0000

    fire recover future popular quick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def remove_duplicates(lst):
-        return list(set(lst))
+import sys
+def exit_program():
+        sys.exit()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
