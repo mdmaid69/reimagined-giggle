12c6220f2e5fbb8655869c1be56e3661d52b710a
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:56:06 +0000

    right however stir friendly protest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
