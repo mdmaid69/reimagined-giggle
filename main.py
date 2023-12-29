65fde1061e315191bee76da4f599a966ac9c9365
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:45:32 +0000

    aim trip getout appeal bang

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
