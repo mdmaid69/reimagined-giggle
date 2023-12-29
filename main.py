ad49ea2abd53e2156d7ff1aee02c0211f3d6b3fe
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:02:04 +0000

    573entertainment shake as afford accident

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+name = "Python"
+print("Hello,", name)
