4479d870cf4022b02c6d3b4a7950ac0ac3173004
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:18:12 +0000

    touch section account maintain remove

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
