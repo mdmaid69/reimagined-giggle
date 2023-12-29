23a570e7f5d7434b89d92e36632ac5838b29cea3
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:36:02 +0000

    drive goon window equal life

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  def square_number(x):
+        return x**2
