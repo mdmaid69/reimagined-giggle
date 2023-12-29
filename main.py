d17af6048cce92a453ea79fef4012c89db4ecc25
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:57:50 +0000

    phone do decide basically guilty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
