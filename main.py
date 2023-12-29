a6ffa3202df850b4884be5045c0d026fb044647f
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:32:58 +0000

    behaviour clean special centre surprising

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
