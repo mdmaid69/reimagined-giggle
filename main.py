7b80ff84540cced9725e73b2bd55861865226b16
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:20:32 +0000

    giveaway sentence part bag word

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_array_item(array, i):
+        return array[i]
