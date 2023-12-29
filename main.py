a30f6449e21961f4934aae556068cf29a4a7f6b7
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:01:07 +0000

    nonsense remove connected feed mysterious

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
