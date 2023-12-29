186b563f6274cbb3d23bf83742641ce30e1fc1e6
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:00:26 +0000

    inform shut extraordinary top direct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
