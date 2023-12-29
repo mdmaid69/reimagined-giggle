87b53e10cc3fbabff15564426497f53aa57f72be
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:14:21 +0000

    increase by advice independent keep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def count_characters(sentence):
+        return len(sentence)
+import os
+def get_current_working_directory():
+        return os.getcwd()
