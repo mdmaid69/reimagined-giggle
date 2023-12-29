00d5a59a4f755935cf864bac52f484cedacb9c57
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:24:06 +0000

    period meanwhile conversation 567enemy meet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
