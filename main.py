8fa2e8c12b30982c453bbf3878695d43ec536191
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:01:05 +0000

    report flow joke guide public

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
