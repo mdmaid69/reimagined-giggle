65a581cd9db30c5acf286511e69a6f85a2ed7dc2
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:23:03 +0000

    brush upstairs however sea sensible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
