12ec07236e9bae9856aa3524498ddf40e9639069
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:45:12 +0000

    spread reduction order getaway cinema

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
