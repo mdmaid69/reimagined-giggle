c0c03a98706b8977845bbcff4b00f45ac066b93f
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:46:32 +0000

    fair wide past change prove

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_index(array, item):
+        return array.index(item)
