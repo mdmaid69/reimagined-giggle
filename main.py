a0ad44137bc6687178eadbf70b0eb8888bf157b0
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:40:56 +0000

    best spray meal limit sweet

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def cube_number(x):
+        return x**3
