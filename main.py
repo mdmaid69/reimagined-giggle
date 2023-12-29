38f27c9f0e72c45aafe129e016aa8fa8a5a28d31
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:31:18 +0000

    put responsibility through fast dry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+print([x**2 for x in range(10)])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
