a3d1a379799d95056646c874d224b7822dfba74a
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:47:54 +0000

    comfort suddenly awake lie solve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
