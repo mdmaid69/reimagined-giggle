b6bc6279855684e377cc309ab06ea54516cf6341
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:13:14 +0000

    church bus surprising shop pile

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
