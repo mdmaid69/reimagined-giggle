09437856a34229e601bb29b2c811cbaa9f788976
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:46:48 +0000

    lonely lead nearly steer right

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
