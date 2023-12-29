a981e1ea445e73c1bfd681874ace715c06b1c101
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:20:51 +0000

    study crazy situation design commit

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
