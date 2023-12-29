a3ece9ab1ebaae199d203577265b4743e2d02667
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:35:15 +0000

    keepup soak time include appearance

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
