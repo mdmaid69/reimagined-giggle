5992aa5f2505683f1b323e97347bc2c4ced3c1e7
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:11:28 +0000

    later impossible process rest visit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
