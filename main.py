4ad0b7d7bcd2e2e59409fd8be27d5b8b82e1b264
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:54:08 +0000

    for calm excellent dance general

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
