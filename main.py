8da3ba78e6242fa67c51c098aadd059d345b02c6
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:21:42 +0000

    frame heavy excuse holdout furniture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
