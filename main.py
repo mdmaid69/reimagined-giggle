a5490bc8065e3aba5e52170f88b0ef25aba74735
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:40:08 +0000

    decide Cc have underneath concerned

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
