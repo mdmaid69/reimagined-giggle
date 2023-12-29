c06ee5c0444fe5579ae35b01059b83223b266b4b
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:10:31 +0000

    broken hard pleasant weigh board

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
