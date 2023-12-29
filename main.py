3e003de35da34fb77ccbd2dc85cae9688fdd824a
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:18:43 +0000

    ago persuade practise properly considering

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
