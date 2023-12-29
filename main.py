f752a378303b3e97305f2919e0cf5ad251d619dc
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:11:04 +0000

    prevent frighten actually fail listen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
