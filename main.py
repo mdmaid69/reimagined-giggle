bd9b06c57ee8741aaf02b29bfc225588a7d21b69
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:03:21 +0000

    familiar noone dark corner whichever

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+print([x**2 for x in range(10)])
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
