69c30d12f87d43ce348ddeeb45066c151ec68ddb
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:19:45 +0000

    add compare tread happy general

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
