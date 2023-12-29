b970f06c7719db834710737ba658925f1953ece6
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:35:04 +0000

    present practical angry hospital wealth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
