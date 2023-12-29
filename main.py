ea1e326a8acdd2f0a90ddd5bffa468021f0dde89
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:47:40 +0000

    nothing whom shallow stage take

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
