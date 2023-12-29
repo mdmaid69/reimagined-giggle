3ab6c1d221372daddf41e7959e760d0011e320f0
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:21:25 +0000

    children handle store moment nor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import sys
+  def get_python_version():
+        return sys.version
