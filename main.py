cff04a105fdfbb5069cf7fed96eb08d6cad40a8e
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:46:55 +0000

    any steam reply properly quantity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def cube_number(x):
+        return x**3
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
