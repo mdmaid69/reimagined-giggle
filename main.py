7a6e5caf805ca34dffaefc4ec6804dabc401594d
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:57:27 +0000

    disappear favour separate present old

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def delete_file(file_name):
+        os.remove(file_name)
