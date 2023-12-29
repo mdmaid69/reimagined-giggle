e783efb47c6d66f15010a2153fad8efe422799a0
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:31:37 +0000

    border breakdown suggestion provide die

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
