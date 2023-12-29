7507b727f674741ac05ea717418eea0b0db92f86
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:08:07 +0000

    corner worth wise excuse attempt

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
