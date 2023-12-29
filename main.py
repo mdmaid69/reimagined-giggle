10928eb8d27f715139356d73d5140501c45a5942
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:19:50 +0000

    listenfor for lose statement attitude

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def count_words(sentence):
-        return len(sentence.split())
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
