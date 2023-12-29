27c2b9f7b1aebe6b1c84e5aa7e61de9206b2998e
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:20:31 +0000

    black proud closed situation want

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
