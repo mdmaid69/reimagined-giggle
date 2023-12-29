acd3fa9c9b4493064d87088fcb1cac9701e45433
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:58:04 +0000

    tired inform rid protect safe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
