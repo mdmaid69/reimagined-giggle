3c8420b76f7fafaf008384d27cd043f9e6d6785e
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:06:32 +0000

    lead better while admire hear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import os
+def get_current_working_directory():
+        return os.getcwd()
