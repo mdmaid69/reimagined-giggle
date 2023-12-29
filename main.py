d499b4561371133f96a3787d2363f62f0d0e23d8
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:06:16 +0000

    disapprove art top section feel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
