e04f69303eec891c8a6962d84c9c87f32f3de47e
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:13:40 +0000

    hurt roll rely move first

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def calculate_pressure(force, area):
+        return force / area
+import time
+print(time.time())
