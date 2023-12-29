70014fdcca81f5a9ffd2f162630c3dba1cc68399
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:59:35 +0000

    protection sticky turndown furniture slice

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def greet(name):
+        print(f"Hello, {name}!")
+import os
+def get_current_working_directory():
+        return os.getcwd()
