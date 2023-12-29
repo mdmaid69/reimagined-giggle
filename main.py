9e8f5bdd9af4d168be0fb7b6d861775d36c80352
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:47:42 +0000

    dead safety out judge stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
