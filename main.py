b88f2dafee589405dcc60dad68dfa4cd784d5361
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:46:51 +0000

    front during correct before band

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
