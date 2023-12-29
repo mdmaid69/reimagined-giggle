2cff49a3744f11a46b18ef9c2c09449d0718c919
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:43:37 +0000

    excuse kick dress through lock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
