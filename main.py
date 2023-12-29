f80f4eb26e88217c1883c4cb9ee6c6028784bfc5
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:50:23 +0000

    road reserve system taste 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
