0b70fcf10ea4751c93862c2ad951c6336b94ab39
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:49:32 +0000

    previous change limit black identify

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  def count_elements(lst):
-        return len(lst)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
