6f079707aa4a77a07a2fc0b725ced850dffe88d0
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:39:13 +0000

    plenty worse throw certainly it

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
