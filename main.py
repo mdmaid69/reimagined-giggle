a4c48553daab046cdbfc60916d765112116e9b9d
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:38:22 +0000

    advantage godown remove difficult nearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_string_from_array(array):
+def get_bytes_from_array(array):
         return array.tobytes()
-import collections
-def create_queue():
-        return collections.deque()
