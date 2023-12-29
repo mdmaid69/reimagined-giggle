50634bee632fd8a26af17a98665b6fb359cd2089
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:34:29 +0000

    time amount breakinto yourself underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_square_root(x):
+        return math.sqrt(x)
