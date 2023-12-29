5e69cf0e74bc3984a580c6a122300999be28e2b3
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:51:22 +0000

    cope every dish base ring

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_area_rectangle(l, w):
+        return l * w
