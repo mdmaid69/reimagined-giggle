4a0f67ed48dfe9d62581743ae1338694df14a39b
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:19:14 +0000

    how view this question anywhere

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def get_array_item_count(array, item):
+        return array.count(item)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
