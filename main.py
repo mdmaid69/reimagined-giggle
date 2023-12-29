8b485f1680da6928d929727b5956c98eb5e98a7a
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:47:19 +0000

    difference doubt getback approve behind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
