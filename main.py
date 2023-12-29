6ae159f4e6b8e485ad1c2d279d493fd6b9b60487
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:10:15 +0000

    stir set force whole know

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
