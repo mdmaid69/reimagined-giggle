7591e2ab409ac0cd50b1118aedd7d10a8624fd4b
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:44:15 +0000

    Bb switch trap that turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
