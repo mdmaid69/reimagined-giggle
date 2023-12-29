8f64ba5756b8917741ebe8912b3f035b590cc02b
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:26:53 +0000

    use judge tonight rub spread

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_volume(length, width, height):
+        return length * width * height
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
