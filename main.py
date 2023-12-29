9f2a719c8d07a3c11ea0a69a58cf65d637211176
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:06:43 +0000

    tool interruption meeting who blind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def sort_list(lst):
+        return sorted(lst)
+import os
+def get_current_working_directory():
+        return os.getcwd()
