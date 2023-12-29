344344eb9cfdfa03f4a84b20102ef550a34bba8e
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:10:57 +0000

    deserve object complain general stir

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
