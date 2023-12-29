2de944fe0024557a6e8c48db8f1cfee5946ccc0e
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:58:55 +0000

    protect fresh flat unpleasant second

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
 import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import datetime
+def get_today_date():
+        return datetime.date.today()
