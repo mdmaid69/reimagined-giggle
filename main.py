fc2bb47007fd42c2ec0127a364a37eae844cf5e6
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:49:22 +0000

    patient speech bet university instrument

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
