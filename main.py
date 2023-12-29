20bfca7457d771e20208e0172a770a32c54e06a2
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:54:08 +0000

    wait pleased whom myself impression

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import sys
-print(sys.version)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
