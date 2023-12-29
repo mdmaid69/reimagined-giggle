dcbfabbb3f619b5eef0be2cb57b101353cb1e82d
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:39:33 +0000

    hospital hand hardly relative check

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def is_odd(n):
+        return n % 2 != 0
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
