de5c593107c76636fabd64d4c9014725d971a408
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:12:12 +0000

    dick fasten suggestion fail peaceful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_typecode(array):
+        return array.typecode
+import datetime
+def get_today_date():
+        return datetime.date.today()
