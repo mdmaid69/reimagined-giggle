e7a3f0e4cd17343333f764dbf8818b412d07cc35
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:53:18 +0000

    awake level noisy road realize

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+print(sum(range(10)))
