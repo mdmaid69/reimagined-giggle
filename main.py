771ce83ddca9a5c013e498f6c7bf5e3a78b6c3b3
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:59:09 +0000

    birthday maximum level niggaacademy fortunate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
