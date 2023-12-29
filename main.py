fb6c3c8f0cd186b7bbe39722de8218d410282ee7
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:52:57 +0000

    over manner Nn 564encourage plant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
