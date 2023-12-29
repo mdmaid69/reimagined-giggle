f2cde565be51af2b9ee4044f8f02a090fb31c0a3
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:46:32 +0000

    hot measure some objection pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
