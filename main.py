d75afd013fd20c4767fd9e0a832528e2e6bfe6d2
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:11:40 +0000

    badly poison the would someone

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def cube_number(x):
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def cube_number(x):
         return x**3
