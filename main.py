ba340612ed8db7a3eba04c66affc30291384770b
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:41:07 +0000

    sort trap modern connected clue

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
