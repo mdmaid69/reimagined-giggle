b7c0f6d9e30a102b87cfd311ea2ba317bb901897
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:33:47 +0000

    condition slowly pretty country congratulate

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import os
+def remove_directory(path):
+        os.rmdir(path)
