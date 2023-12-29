9996292b5e44b757efb7cbe4209dce1377e85b7e
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:09:15 +0000

    post since quiet stamp war

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
