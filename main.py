3701307d6d98ed9e68f85680d656b3c8e1cb28ff
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:14:57 +0000

    late judge 566end whoever common

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
