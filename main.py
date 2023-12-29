b617090d5a33d3242be92be4a4bed004415560d2
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:14:46 +0000

    shop deal draw each hardly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
