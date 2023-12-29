8b0ed00e1edfc079ba229f3a6f4b7ed1848e4809
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:09:26 +0000

    bright what which fortunately display

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
