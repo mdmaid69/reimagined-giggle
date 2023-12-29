c42faed363599e617c783e996c666ebce7f72d88
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:45:41 +0000

    discovery twist Gg border convenient

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
