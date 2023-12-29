ddcb4bb087230cb521a10c3364ecaa5f183d7f3d
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:53:58 +0000

    plastic comeback spring disagree correction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
