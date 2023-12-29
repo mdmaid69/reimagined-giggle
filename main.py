d3cb2e3872244ccf0fc72978850fda3303a4fbec
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:58:31 +0000

    send stiff smell complete sight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_length(array):
+        return len(array)
 import tensorflow as tf
 print(tf.__version__)
