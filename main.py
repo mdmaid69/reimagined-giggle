2a02a6ae746d7fbb5d3112fb34c3dfdccd96a6d3
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:05:50 +0000

    guard challenge let determined so

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import json
+print(json.dumps({"name": "John", "age": 30}))
