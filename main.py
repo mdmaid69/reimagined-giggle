c6ccfb473fb006352033fb282aa806aef65e0af2
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:25:24 +0000

    swimming run complicated up either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
