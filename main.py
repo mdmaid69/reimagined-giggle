1c59a68ed9372318dcf16559e032bb7fa48dc0bb
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:25:26 +0000

    cause front nor build stone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
