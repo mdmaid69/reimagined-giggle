a97bb2497a494a748032156f885f695642cf7c7a
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:25:14 +0000

    suffer hit scrape teach easy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
