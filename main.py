53ee24ecb8f1880d274bde6f533f487171a10c3d
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:02:54 +0000

    distant cheat wealth confused pen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
