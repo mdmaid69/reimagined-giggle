e21b7fd6789e4eecdc59ccaa1b152c7d79137908
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:45:38 +0000

    many change distance hour force

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
