65bc21216e2fd30dab626eb7b47111fe6e4adad6
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:57:12 +0000

    congratulate turninto upstairs goaway also

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
