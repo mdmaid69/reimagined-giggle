3db63469697243cffe9c0167a07c16c5d106b0d5
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:45:45 +0000

    listen operate worst means sweet

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
