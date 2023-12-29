3d83cb5835741fb784fd401c5e97168da76ccff1
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:08:34 +0000

    price door 567enemy keepfrom rent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
