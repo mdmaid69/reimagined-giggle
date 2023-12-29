7d13cb89849c884ca7f477504c5662b25adcc167
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:40:52 +0000

    combination indeed lesson pass push

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def subtract_numbers(x, y):
-        return x - y
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
