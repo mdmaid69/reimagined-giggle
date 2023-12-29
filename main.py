dacf3734f039de8ad1ae3457da208b545d495ef9
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:54:36 +0000

    news takeover geton bin Ll

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_density(mass, volume):
+        return mass / volume
