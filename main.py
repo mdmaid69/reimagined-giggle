16f88a4b3d85dc9d8aa77af65e99c61082b84b1a
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:25:42 +0000

    swallow important practical moon space

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-import array
-def get_array_typecode(array):
-        return array.typecode
