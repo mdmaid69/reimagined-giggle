fb334b8039ea05a9a143f9ada0f4bff5c820a5da
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:32:16 +0000

    rarely tall border detail drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
