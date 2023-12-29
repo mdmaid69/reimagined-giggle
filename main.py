5602eddcc33241fe3b2b60b5bea4a798f93e2809
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:58:48 +0000

    guilty thinkof inform closed long

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
