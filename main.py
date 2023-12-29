7a6019edaa048e6889391ca85ae83239e72b1c64
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:39:18 +0000

    it obvious Gg full preparation

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def convert_array_to_bytes(array):
+        return array.tobytes()
