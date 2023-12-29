87fd7a4216824a12e824ba1686974797c96d23a9
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:56:57 +0000

    here benefit dream bang turndown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
