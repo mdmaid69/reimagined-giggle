5c0cd46e4e53e4d62da1b2b7908555354003814a
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:58:45 +0000

    eager convinced central building calloff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
