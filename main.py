ef6fc0b7c33fc8ddfe540e6ed0605ad787d7eb0d
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:47:52 +0000

    operate breakup Ee gradually worst

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+for i in range(5):
+        print(i)
