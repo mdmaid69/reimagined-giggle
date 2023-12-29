74d8b89ed8afc5e23acebe1a07273c4feb717380
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:14:08 +0000

    national rub return process store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import platform
+def get_python_version():
+        return platform.python_version()
+def convert_to_binary(n):
+        return bin(n)
