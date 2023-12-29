a60b4e369681363d91eb401a9a5155aa51f17d07
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:26:13 +0000

    water seriously schedule I minute

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
