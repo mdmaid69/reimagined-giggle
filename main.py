aae770cb5ea8d383641132783e0e7fb2d83afe5d
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:01:44 +0000

    file future right murder but

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def sort_list(lst):
+        return sorted(lst)
