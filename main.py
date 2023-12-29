64880a51c8356093901c1f6683072310d1427eed
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:08:16 +0000

    prefer whoever history bring wrong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_itemsize(array):
+        return array.itemsize
