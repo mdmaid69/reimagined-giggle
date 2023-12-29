034b62c799f93687cd6bab64d91902e495b7071b
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:50:52 +0000

    them giveup brave test final

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_as_list(array):
-        return list(array)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
