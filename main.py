372a8772a3fd407864a6da2e0ce4cc5aae7f543c
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:49:08 +0000

    speed getout neat everywhere guess

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
