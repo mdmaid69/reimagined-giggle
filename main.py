794ebff153d5c922d8368a0ece3356f79a05deb5
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:47:04 +0000

    unlucky instrument hide average accommodation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def get_array_as_str(array):
+        return str(array)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
