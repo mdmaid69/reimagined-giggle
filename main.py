a131c64d553687848db8fdeb09d0a1c9c2df4260
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:24:27 +0000

    bank private show hour quickly

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
