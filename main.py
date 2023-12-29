fdac09d7cdbbaf0702ef92ab2507f0adb3ff661a
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:40:10 +0000

    figure under arm getback male

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_sine(x):
+        return math.sin(x)
