ab2f0d75055f6e4334252acff3508f352d5e6532
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:33:49 +0000

    judge manage vote autumn power

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
