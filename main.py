780bada4f25270d6b6011346c29c7f1d003f1f3e
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:36:36 +0000

    right rise turnon money embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
