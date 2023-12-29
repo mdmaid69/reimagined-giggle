29416eaac83e1702fb0238d4fd0fa121334b7a57
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:15:42 +0000

    border damage taste special smart

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_acceleration(speed, time):
-        return speed / time
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
