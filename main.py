1e9d0a1c54a076018c74d15027a262d88cd8ab87
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:59:36 +0000

    frequent gooff fortunate praise quick

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
