27443774f63f958dfa952493c55c54ffb2ed18af
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:23:15 +0000

    complaint declaration congratulate threaten line

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
