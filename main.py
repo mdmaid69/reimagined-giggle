2630ebe7fd3f7ddf516118c99e55380f3b4817eb
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:39:17 +0000

    deal show breakin stone unit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
