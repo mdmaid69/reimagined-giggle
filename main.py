16b3c7f3e103fce2a3c109fc7d2b088a91dbc1e0
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:08:07 +0000

    loose somewhere advice should hollow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
