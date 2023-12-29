840938cb9830280117672e28cb6e2bafb8e8ed27
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:29:08 +0000

    war callat clearly unusual trick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
