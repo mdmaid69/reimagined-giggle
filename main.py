4b53916338e2b60cf6eb58d44ecb91aa95043d17
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:31:37 +0000

    forgive pinch mistake release family

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sys
+def print_python_version():
+        print(sys.version)
