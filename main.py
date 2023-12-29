78c3ad4cbc797b6e6a915f509b684759c549de11
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:39:15 +0000

    convince fix fly considering tonight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
