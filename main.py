137c69db953eaecbca0f3759887dbea2f3ceb2ae
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:57:43 +0000

    promise nature year uncomfortable nervous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
