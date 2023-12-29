cd81144a207526ec375603b68ab05388a814ed9c
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:08:37 +0000

    something sweep shock with chance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def count_elements(lst):
-        return len(lst)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
