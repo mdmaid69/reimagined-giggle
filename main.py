6a403ccd51db8f69ff3e78d2804408e1afe87a70
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:07:57 +0000

    used trip chin visit union

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
