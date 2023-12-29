ecbe230ab7437ea03b2c56bf2b0ba0ddd5390d2c
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:36:19 +0000

    steep light question afterwards mine

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
