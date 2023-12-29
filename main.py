953a12eb944bb404de5cf044f5058465d697df53
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:28:37 +0000

    inquire rob Vv floor here

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def is_even(n):
+        return n % 2 == 0
+import array
+def clear_array(array):
+        array *= 0
