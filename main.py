c79890006f64d8a97f7cec0a568eca8ef5ddbaa0
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:24:14 +0000

    read slide present instruction slip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_block_size(file_name):
+  def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
