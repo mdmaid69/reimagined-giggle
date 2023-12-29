70c0b7967a31ec3667e185f155c8f599e576c4e2
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:48:11 +0000

    claim lost face takeoff coat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
