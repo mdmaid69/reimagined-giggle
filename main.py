d56830cbc32689897a4df090bb67d135e8caa88e
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:38:58 +0000

    thorough go available properly different

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import os
+print(os.getcwd())
