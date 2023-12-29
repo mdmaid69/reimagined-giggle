cb818c33ea7c8672353a33eaaa3fbfd7d686c22e
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:09:55 +0000

    mean stupid expression wire correct

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_force(mass, acceleration):
+        return mass * acceleration
