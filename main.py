a60d5a70a077830380d82401a14eb77b84c1c70c
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:56:04 +0000

    mysterious brief major fight embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
