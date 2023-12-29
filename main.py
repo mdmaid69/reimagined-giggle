edc4bf99161a4dd5122666a03ea96a49d7067f46
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:44:08 +0000

    mother swing increase shoulder transparent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
