66f03993b3b14a295e34f685b5e8447c554d608e
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:14:28 +0000

    provide left please up somebody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def add_numbers(a, b):
+        return a + b
