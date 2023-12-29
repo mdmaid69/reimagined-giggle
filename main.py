3341d93831570ea6520391f68d599f9f9bda924b
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:53:01 +0000

    outside argue geton hold taste

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+print([x**2 for x in range(10)])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
