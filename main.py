9c99b745d57b0c5db78d3d77c0bc40b1beb450d1
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:30:43 +0000

    Ss pay top keepup class

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
