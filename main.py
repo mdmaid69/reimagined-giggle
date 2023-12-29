e4e906ac28b603200fc37db5cb25339581c76ac1
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:09:25 +0000

    male scene dry 572enter never

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
