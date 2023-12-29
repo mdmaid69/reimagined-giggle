a03765196d85b049d198c65cd765eef1c723e855
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:19:40 +0000

    conclusion wipe forget power work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
