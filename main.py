d43a3ee035e73d2f8ede453008637f57cde5ce14
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:26:55 +0000

    finger argue record talk street

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import logging
+def log_message(message):
+        logging.info(message)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
