fa7a31cc0503fa1495f848ec05272ed2cd07ab39
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:45:40 +0000

    both rare opposite bored turnout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
