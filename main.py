dc1ff89e330d5375863c349b62524afa10798ee2
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:10:48 +0000

    bit cap beginning estimate look

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
