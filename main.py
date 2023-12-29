5ec948cc0f1adf461d73e078b302a72b9af9a8ee
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:48:28 +0000

    keep welcome principle stupid unite

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import array
+def get_array_as_list(array):
+        return list(array)
