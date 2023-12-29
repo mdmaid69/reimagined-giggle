46a18a4059c96f469bec8bbf8e7267c726abc2cd
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:57:33 +0000

    stop settle rain during petrol

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
