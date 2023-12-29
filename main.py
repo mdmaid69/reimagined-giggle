d53524cda39567304191a9ec0c87fee56d07ce10
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:21:58 +0000

    picture fresh trip translate report

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
