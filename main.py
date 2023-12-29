2b2ea2baa375e4df80b8e1365add407125448870
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:34:01 +0000

    nature persuade sad their hope

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
