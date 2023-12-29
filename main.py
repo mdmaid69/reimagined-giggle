48a85155e7025169b362c998b9ebf22fb09164f6
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:16:07 +0000

    describe breathe freedom area immediately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
