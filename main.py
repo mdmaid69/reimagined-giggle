1c510b8f046ffceddde48cccf39589325be7df81
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:13:46 +0000

    appearance rubbish clearly drop world

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import collections
+def create_counter():
+        return collections.Counter()
