6f27486b8faba1d799540a0c27e7cc2f9f70f269
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:12:48 +0000

    should second natural learn forget

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
