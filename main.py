a905209a5719570be036056945d1da5dcd267dee
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:08:28 +0000

    push undo exit glad go

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
