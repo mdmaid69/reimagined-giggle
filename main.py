d06e27b26762930e3f71712638720ccb051bc868
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:43:34 +0000

    nonsense once herself unlucky computer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_power(work, time):
+        return work / time
