3947af9351dbe511a79f291c690b0da8ef7aa3f7
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:05:22 +0000

    experiment experiment sure little little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_set(array):
+        return set(array)
