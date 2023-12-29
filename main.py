6107a2f2ebf48776077ff84b5199c71d6ea360a0
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:50:29 +0000

    stupid payment Cc between interview

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def find_max(numbers):
+        return max(numbers)
+import sys
+print(sys.version)
