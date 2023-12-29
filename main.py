55bbc489e8341468d386f42dd23808ade98eedbb
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:36:33 +0000

    book by usually sentence above

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def reverse_string(s):
+        return s[::-1]
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
