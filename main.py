408564c1a95c4f5ef15c4020ee44dde285798d45
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:55:55 +0000

    better next proof crisis along

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-def sort_numbers(numbers):
-        return sorted(numbers)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import math
+def calculate_floor(x):
+        return math.floor(x)
