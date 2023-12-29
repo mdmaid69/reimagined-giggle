c368d8faef922907b3b892bf422dfaf83ec7a292
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:17:41 +0000

    mouth cinema central common noise

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
