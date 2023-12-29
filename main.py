7c0905ad45d6ecd6308b795c29d631599e726b06
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:34:43 +0000

    male notice name watch future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
