bc5fc5146a57ce7624fa1c6a46f662b03eec9b79
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:13:47 +0000

    light them bad certain unhappy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def convert_array_to_string(array):
+        return array.tostring()
