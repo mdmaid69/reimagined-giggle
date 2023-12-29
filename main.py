c727a7d0ef2c84c46ce88571f566a962f862c1a1
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:58:54 +0000

    nose true object weak mouth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  def cube_number(x):
-        return x**3
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
