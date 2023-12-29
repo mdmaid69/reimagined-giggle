173ee178ff66f0f78c26e8f451a2fadc4666827e
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:05:47 +0000

    second right accept problem excited

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import sys
+print(sys.version)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
