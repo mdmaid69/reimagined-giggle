5e01fe9494ffb8690a18b25104dfb6fea2c4c2d7
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:42:22 +0000

    crowd same also interview excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
