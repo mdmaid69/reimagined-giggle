191d480c2fb2db4588ad280d5c6caca8b845c59b
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:47:35 +0000

    press interest wide total song

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_itemsize(array):
+        return array.itemsize
