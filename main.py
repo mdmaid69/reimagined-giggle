55e0688e5793ddd20e44b7e7b2bd43d4dfb89ab2
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:41:32 +0000

    shock shoot switch usual passenger

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
