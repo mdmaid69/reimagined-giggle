30e79d37c896fd72cb2c93cccdba06493ffb29e2
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:03:00 +0000

    slightly undo send left interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
