74bb55e378ac1b5ea0273224afbb502facf3c3d2
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:41:40 +0000

    force party cool cap contents

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
