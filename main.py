79069c28e4929213edf7a44b21b3d697d7936c6d
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:27:56 +0000

    sore exist night nervous play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
