3ce86bed459f670f3df6544a0ef76cc61a7f6d3e
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:13:38 +0000

    pass claim patience opinion upstairs

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
