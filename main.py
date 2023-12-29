b8aca35744c6192c41671907b940693b6427b384
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:19:43 +0000

    bar finish mention supply remain

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
