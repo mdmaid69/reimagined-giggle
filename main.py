797497dec9faf9db0f962da23e3646802278a70a
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:16:31 +0000

    change burn callup single he

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
