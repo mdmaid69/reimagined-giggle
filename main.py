bd0128974be3ee876cfed4c485c65262c294a8af
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:06:03 +0000

    herself noise bringback welcome somewhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
