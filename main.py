1d5b7110c802a282088e0199986c595742a9cf04
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:36:47 +0000

    leave comeup charge pull phone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
