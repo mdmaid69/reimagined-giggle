f0efb9495d2bc6654e6ff31320864939dac09d99
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:05:01 +0000

    Ll stare determined watch head

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
