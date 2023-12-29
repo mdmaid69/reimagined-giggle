bc8a5b3692cdf6de6f2c250f9b7e1dd9c0f88c29
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:04:03 +0000

    ill whistle seriously either cruelty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
