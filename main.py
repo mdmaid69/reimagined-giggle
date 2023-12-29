62d49241860ab0895955c85bad5f9ade0ab085f6
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:35:24 +0000

    visit good breathe desperate instead

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
