6a9d3123cde9bc42e0572d3a941f485555591a58
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:56:53 +0000

    freeze new further jump anyone

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_float(array):
+        return float(array[0])
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
