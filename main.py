91a3db6d060fbc07820f0abe6f14bfa8b3e97482
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:30:11 +0000

    firm issue slow lonely information

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
