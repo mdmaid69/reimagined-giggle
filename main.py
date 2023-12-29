c9a1514b4a22c0500a081618f1c28c43e210adde
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:46:21 +0000

    waste style beside opponent lock

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
