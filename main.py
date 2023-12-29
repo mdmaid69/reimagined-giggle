288aa43cbe361e86536b32acc6679f35d1e5f46b
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:47:27 +0000

    dig university 568energy appeal pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def get_array_index(array, item):
+        return array.index(item)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
