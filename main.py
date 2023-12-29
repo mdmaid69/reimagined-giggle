cd94ab0a6bd2a8a89e84d0d855dddb1536819a7c
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:26:26 +0000

    tend gather handle post willing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
