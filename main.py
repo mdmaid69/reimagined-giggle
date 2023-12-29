0f39dead60df403db51753a4b27f706c9b05448e
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:07:43 +0000

    shut holdup pump dry chief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import sys
+def exit_program():
+        sys.exit()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
