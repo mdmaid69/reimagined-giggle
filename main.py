2ec4a15f5a8cd1e1feedeb158fc9d269280ef57a
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:34:07 +0000

    list deliberate wave play workout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
