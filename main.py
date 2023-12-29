fad9dd2471aefa2e45bbd515169c822ae0209d72
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:03:25 +0000

    defend have through shade precise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
