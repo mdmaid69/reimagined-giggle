146ee1a73cfa16d7f6b11afd6a3ff99254abccfa
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:29:06 +0000

    better dig fact pressure comeoff

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
