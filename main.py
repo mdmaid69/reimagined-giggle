a4f5ec83f7a3fda66435c707ca4438074706efb9
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:10:06 +0000

    brown fall anxious smart excited

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_gamma_function(x):
+        return math.gamma(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
