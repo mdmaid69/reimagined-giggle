7591df0b6e18a868ca0ab936f263c064d849618d
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:42:55 +0000

    long pupil story Zz passenger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_pressure(force, area):
+        return force / area
