0cb5145f18b886b71563c49f1a8e22758cf7916a
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:24:18 +0000

    before breakdown support comein science

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
