b6dc14a902e4fa093a5f6c00092007d9d2925977
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:52:13 +0000

    black woman though question spare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_average(lst):
+        return sum(lst) / len(lst)
