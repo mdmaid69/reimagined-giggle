65aab6a8518760d3651e34f3ee28074c87f0ce76
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:44:54 +0000

    ever borrow method judge confident

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
