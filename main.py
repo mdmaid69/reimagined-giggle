621b27b1d2b4e6552e178e0f90e002020db12ca1
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:33:39 +0000

    nigga person right print direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
