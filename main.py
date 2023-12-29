cbd3df90bf6987568e16f8ca2ee57cd6273deae8
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:01:36 +0000

    moral each praise shelter breakdown

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_pressure(force, area):
+        return force / area
+def calculate_pressure(force, area):
+        return force / area
