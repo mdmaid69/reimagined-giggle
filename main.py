53d23b99ada12126d4a540ad02c418af0e07825c
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:35:47 +0000

    operation exploration very the already

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import os
+def change_working_directory(path):
+        os.chdir(path)
