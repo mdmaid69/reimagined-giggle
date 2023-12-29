2f0154ba4e344780c33e7e31b7fc5f8f3647d96a
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:04:14 +0000

    hide moment hungry able before

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
