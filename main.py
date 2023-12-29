4620afe755c2c93e7514a40180b0ee7e7e0af6e2
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:26:51 +0000

    arrangement rare charge interview platform

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import random
+def roll_die():
+        return random.randint(1, 6)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
