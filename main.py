e38ac295a7a4d8b303be17306f3f10b610936a95
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:05:38 +0000

    hungry up rarely suggest back

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import tensorflow as tf
+print(tf.__version__)
