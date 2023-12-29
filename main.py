6e104b6215c3e4743d1957bd71dfedb018ad8330
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:48:59 +0000

    path pay rare sensitive class

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def get_array_item_count(array, item):
+        return array.count(item)
