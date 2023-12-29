76adae15e3dba9e71da580360d3adff042b5a98b
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:43:53 +0000

    drink although call whistle protest

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
