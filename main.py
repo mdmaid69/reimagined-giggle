f559168a14cfff9cbabb0d2135ff49444258acff
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:43:38 +0000

    programme change row noisy off

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
