d187a99da537ded210d9a396c4a3c3c01b27abdb
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:54:41 +0000

    mass sign relax young him

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
