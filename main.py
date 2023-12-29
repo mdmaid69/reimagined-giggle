f76241c7bdcdb76415945e88ec0eb26ed56fb32a
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:28:58 +0000

    school report hole Ii very

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
