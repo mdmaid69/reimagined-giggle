4db3eac6ff2ec67e1fbfdabc2c175206f4c1ec28
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:48:22 +0000

    equal side field issue let

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def get_list_from_array(array):
-        return array.tolist()
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def get_array_buffer_info(array):
+        return array.buffer_info()
