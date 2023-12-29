08d356141df2bf5263bbe47dd36fdafe5e2f18a5
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:40:16 +0000

    fierce runout side blame platform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
