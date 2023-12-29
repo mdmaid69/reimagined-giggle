0129134723281fdac7fbc72d410199fbfcb523ec
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:33:28 +0000

    thankyou promise wherever someone support

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
