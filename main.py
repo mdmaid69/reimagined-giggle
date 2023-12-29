1df8e08c10d74919015cb74c2ef14e5ccef54d94
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:31:05 +0000

    smell song objection various considering

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
