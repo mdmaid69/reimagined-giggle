1eff2f39d560ee4d60df38d1f02cb53d50a8b136
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:36:27 +0000

    wall wrap report organize mysterious

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
