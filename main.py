e14cc29470803d51d8d62bab23d30112e6d6f43a
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:19:00 +0000

    handle fun condition niggaacademy evidence

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import math
+print(math.pi)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
