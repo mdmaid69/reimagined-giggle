e12718147740c8fd3202931f71484f0b9957e390
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:49:09 +0000

    true guess produce church clearly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def get_current_time():
-        return time.time()
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
