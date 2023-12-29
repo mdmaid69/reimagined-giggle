6c88feb11d8c4187777664bdd88390b52c47348d
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:46:11 +0000

    with nowhere terrible confusing laugh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
