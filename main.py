09f1dad5601a3aa937c31df959d269e0391cefbb
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:59:11 +0000

    supply impossible serve crash copy

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def is_odd(n):
+        return n % 2 != 0
 import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
