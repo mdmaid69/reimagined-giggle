946c00d560ec24c19c4aac063cb0f74615df368e
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:08:54 +0000

    wake free variety disappear somehow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def add_numbers(x, y):
+        return x + y
+def is_palindrome(s):
+        return s == s[::-1]
