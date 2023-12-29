a442843896c2b9cad06d600c523351764f38e0e9
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:07:00 +0000

    interesting wash trousers cool this

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
