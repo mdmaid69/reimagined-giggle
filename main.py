7b90f3cb20b0299c5c1fd67f7df5bfc6dcbe95ee
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:44:20 +0000

    famous bringdown well kind brave

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_work(force, distance):
-        return force * distance
+  def subtract_numbers(x, y):
+        return x - y
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
