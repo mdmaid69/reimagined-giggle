a25f29485dbcc592883d9b376402eee61088660e
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:34:00 +0000

    Ww goaway print impressed fierce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
