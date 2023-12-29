1f97e523c7c941292dc9a7135b819e96272304f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:52:00 +0000

    breathe holiday common region traffic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
