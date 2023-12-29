b2d942e8c67c54bbc4eacd1647100d825cab5bda
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:26:18 +0000

    nigga beginning consider soft carry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def cube_number(x):
+        return x**3
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
