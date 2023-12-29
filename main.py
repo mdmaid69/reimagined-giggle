455faa0d7cda8b5a489f236ae80fba9e1e479ec4
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:13:29 +0000

    rub somewhere bridge suck food

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
