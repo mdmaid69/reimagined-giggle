4581f7947e0562cd6573283e0257bfa8b9499b0d
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:21:29 +0000

    direct break major eat government

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
