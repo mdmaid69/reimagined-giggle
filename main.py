27130c4abf9e2069add6ed1bfa005d63175eb9b8
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:58:32 +0000

    hole cause emphasize reach phone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_current_directory():
+        return os.getcwd()
