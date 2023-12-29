f5796998de0858b2baff8ed51571fe024b1f960f
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:17:55 +0000

    yet trip production with plastic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
