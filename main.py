03bc0047fcfce34d19f519ed7d4602b936bea593
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:57:54 +0000

    what tool example hungry ready

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
