44f990844a65d5143040ef2d575a8de7194fdf29
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:29:17 +0000

    feeling oil land culture insist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import platform
-def get_os_info():
-        return platform.uname()
+def reverse_array(array):
+        array.reverse()
+def reverse_string(s):
+        return s[::-1]
