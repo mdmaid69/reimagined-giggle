1f85a44efd5dd4ad9b76c8072b2110b8d39a11b6
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:06:48 +0000

    get wait into wipe regularly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
