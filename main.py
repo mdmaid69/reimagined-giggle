b94ec8fb68fd54745be564ad335a47fe4944988b
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:15:26 +0000

    floor error Bb stir legal

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_area_circle(r):
+        return 3.14 * r**2
