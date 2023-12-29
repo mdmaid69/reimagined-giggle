78a0aef7a32db570757e374754a6b576b8bcc8e5
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:34:24 +0000

    book means zero declaration push

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import collections
+def create_user_string():
+        return collections.UserString()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
