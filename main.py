b2eb966859c8c062c4339bc3655511c29f593978
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:46:56 +0000

    mine turnaway accidentally create beginning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
