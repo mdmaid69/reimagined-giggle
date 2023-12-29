8de81bbc8b603962c580885cdc056509855bcc76
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:34:10 +0000

    label think last finally quick

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-print(sum(range(10)))
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
