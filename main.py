4cd21edfb412739684b120cf5cf13bd58c1e2a19
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:42:11 +0000

    possible challenge central blow behave

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
