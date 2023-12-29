fa67764e48164aa8baf5ef70ed711c21291faaaf
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:31:38 +0000

    bury turnout ill fork various

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
