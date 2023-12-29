50e67398ff45777cdac292b742fbe4a52f5e4844
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:50:17 +0000

    confidence uncomfortable cause tax comment

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import sys
-def exit_program():
-        sys.exit()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
