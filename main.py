4a697cb9b21875dab0d7f10f2fb5de60e9b7ec71
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:07:34 +0000

    flight your this estimate laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
 import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def remove_directory(path):
+        os.rmdir(path)
