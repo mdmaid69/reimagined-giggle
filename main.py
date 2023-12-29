b275ca6ceda487d24d830ab76c0685c9b2ce8b23
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:16:42 +0000

    fierce Dd order sight herself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import time
+def get_current_time():
+        return time.time()
