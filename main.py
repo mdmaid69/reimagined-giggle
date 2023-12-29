e9b807c5afca8bd59a1cae9acd7b6f53806665a9
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:10:47 +0000

    behind money fact godown brain

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
