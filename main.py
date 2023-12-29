774d129627d7ad3aaf5eaa0f304fd605175451d3
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:55:37 +0000

    gooff tune threaten way wear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
