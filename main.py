d4bf480c0b5624431fab9ec3c533e1cb864c8734
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:02:53 +0000

    level change blame shelter plain

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  def add_numbers(x, y):
+        return x + y
