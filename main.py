29b77a8ea00d3586a76c3c32b91b764b3c6591ae
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:14:11 +0000

    string slide vision murder holdup

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def find_union(list1, list2):
+        return set(list1) | set(list2)
