ab87b2f00644ce39b821dcc8d5bb49ffc40960af
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:09:02 +0000

    distant rather doubt unusual high

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def reverse_array(array):
-        array.reverse()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
