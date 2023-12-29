a7931da22149dfb6c8293b4a86d69e6cf2079ff2
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:22:43 +0000

    they he ask shade job

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
