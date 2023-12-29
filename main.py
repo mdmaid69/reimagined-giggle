96da93036805836daefbc35b2ae2fdabd3462da1
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:44:37 +0000

    from way concentration like quarter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-from collections import Counter
-print(Counter("hello world"))
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
