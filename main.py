b162f5b588495f6999c50a60b0fb6220b4f67bb4
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:41:37 +0000

    criticize uniform fall organize unable

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
