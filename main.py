6561d49dfcb4e7c69bc1f521c11f81068a316f8c
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:41:01 +0000

    bottom cost eager fault lock

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
