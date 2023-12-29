fcbc0769f7d3e4a7d0f2d4586137d8400222b2b3
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:30:08 +0000

    protest grow strength expect congratulations

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
