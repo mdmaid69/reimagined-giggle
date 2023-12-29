974d9a704c0d3908e031216ceed118c7ab18a3df
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:53:15 +0000

    conversation land moon keepaway worth

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
