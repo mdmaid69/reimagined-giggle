201eea2e3184219c4efcb8e3538b381d198ae765
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:46:20 +0000

    whisper cruel interruption reply few

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
