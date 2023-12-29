47b281ae92f2540b09b44e4f70b4aed8069d7054
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:30:48 +0000

    unusual imagination pure turnover hot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import time
+def get_current_time():
+        return time.time()
