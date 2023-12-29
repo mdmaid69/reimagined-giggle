0d8f89732c2222ca934adce2bd5b824e0fb37af7
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:15:14 +0000

    chase characteristic call sport rent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
