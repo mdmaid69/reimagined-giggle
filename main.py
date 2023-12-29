461b2e72b49186693a65f1924fb1ace6023af0ee
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:21:14 +0000

    grade top branch minimum television

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+i = 0
+while i < 5:
+        print(i)
+        i += 1
