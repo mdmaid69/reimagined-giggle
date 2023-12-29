c8128368a11da4d66bdb4d3176986966406ed4ff
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:21:00 +0000

    splash fast motion cool clean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
