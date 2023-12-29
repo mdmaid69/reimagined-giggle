bc0cd760ed7c81dfa886bbbdba0a0a8909a2a8e3
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:13:53 +0000

    store community foot age proof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
