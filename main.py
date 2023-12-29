dca6b63ff8dd258ae8b89e96a2ef121360f9f7f1
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:31:05 +0000

    the ticket thank next Kk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
