d25d7777b4e6497d0a4d1ed15a43c59be5be598c
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:35:30 +0000

    patience fault hide damage whatever

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+name = "Python"
+print("Hello,", name)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
