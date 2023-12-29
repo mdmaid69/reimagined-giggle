badf2d32f0154b3d266c083ba7f20cec5344b2b0
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:18:46 +0000

    middle ours wind mind there

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+for i in range(5):
+        print(i)
