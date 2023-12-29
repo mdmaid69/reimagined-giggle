40d530a147b63201ccf92eb7232fa5b4e67df453
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:24:27 +0000

    pocket immediate man sound exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import re
-def find_pattern(pattern, string):
+def find_all_occurrences(pattern, string):
         return re.findall(pattern, string)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
