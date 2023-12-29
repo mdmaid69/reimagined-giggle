bc1859f1299f2686412be6f83adc9cc00db40cf0
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:06:12 +0000

    cost attempt open block through

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
