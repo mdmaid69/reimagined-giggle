ff4983ccd570ebea4b5e592e7e4775804dae2cc3
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:27:55 +0000

    range long run explain ordinary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
