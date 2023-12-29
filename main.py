428b63acb34305be610d7bbcc9ef3f3b14bc1821
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:57:20 +0000

    emotion behaviour medium mistake general

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
