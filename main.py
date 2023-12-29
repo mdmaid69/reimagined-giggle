631dba6ec44d04dac4f338e6d27ae13139aa804c
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:30:42 +0000

    frequent advice run aim 568energy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import os
+def get_current_working_directory():
+        return os.getcwd()
