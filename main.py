dd05bec8ee951d9b52da1ba26edb2a5f0428c219
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:51:05 +0000

    among operation arm roof understand

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def convert_to_binary(n):
+        return bin(n)
+import os
+def change_working_directory(path):
+        os.chdir(path)
