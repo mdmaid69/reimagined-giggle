02c1b220ad6467251adcb3ebf4322b9155815ce5
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:57:50 +0000

    eat which connection Zz match

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
