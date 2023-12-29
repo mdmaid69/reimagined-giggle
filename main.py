8b62e7bbca296d7eefb5d07d30363b5ab7a2c392
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:36:08 +0000

    already remain argument holiday wrong

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-print(sum(range(10)))
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import math
+def calculate_floor(x):
+        return math.floor(x)
