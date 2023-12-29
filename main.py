9e37480c8c771102947727235f9c5d0928a5a5cb
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:51:31 +0000

    nor surprise fond pity strict

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
