70296dbc4e2762343043a111961e42b1287daba5
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:05:17 +0000

    pressure direct promise thank spread

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def remove_directory(path):
+        os.rmdir(path)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
