c87c7c8c5f2a84dda184d1fcee172dfa7583cf4f
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:12:53 +0000

    style walk guard chat press

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_base_name(path):
+        return os.path.basename(path)
+def count_words(sentence):
+        return len(sentence.split())
