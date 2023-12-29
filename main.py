f7784b18014be1230ea1a3734571a67ccf26f436
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:27:55 +0000

    foot school escape whole eager

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
