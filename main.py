df2b0a161abf33819ef51d9b8176dff78b82c341
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:45:32 +0000

    doubt roof attempt when unhappy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def is_even(n):
+        return n % 2 == 0
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
