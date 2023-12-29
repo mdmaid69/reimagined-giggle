bff4410a5e2971d6eb8475c3c2184f7d5cd77c39
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:47:49 +0000

    later drive congratulate low split

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
