537760e03488fceaeb005f20c5a02f29ac8be7cb
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:53:46 +0000

    own lock own moral secret

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
