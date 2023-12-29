22b19e7cacb17e77bd807faec2dff5bb4c59e23a
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:48:54 +0000

    curious Gg protection shall shelter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sys
+def print_python_version():
+        return sys.version
