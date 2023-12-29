497ba538a1c253cb6414879ce7aab72b6a694d7c
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:21:30 +0000

    kill answer anyone murder word

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
