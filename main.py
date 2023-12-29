09d84ab1e6aced7f502810bb5aabc212e4395bd6
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:39:28 +0000

    exit thankyou invitation fit depend

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
