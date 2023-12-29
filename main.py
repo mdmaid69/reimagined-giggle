e7ee10ef75c27d143e57d88dbd3f5ad45804e61d
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:57:26 +0000

    world smell effect steep change

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
