b48e2e6d019ed42463d40635fedc4c649009100d
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:14:39 +0000

    coat married wide produce steep

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
