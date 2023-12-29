ce3f5902268e104a020aab373467eb7088a45db9
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:45:13 +0000

    ticket fit badly gooff soak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def square_number(x):
-        return x**2
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
