7a59e09e89d9288cc446192d2ba8badb69021d9c
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:59:11 +0000

    share model leave trip ignore

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def multiply_numbers(x, y):
+        return x * y
