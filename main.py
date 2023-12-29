f33e0ef84e96bddb52580387aec58a3aef0c3371
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:47:03 +0000

    consider responsibility weigh car equal

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
