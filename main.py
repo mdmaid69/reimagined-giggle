68923f6cca89e365cf302904047ea5f1f10ced7f
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:41:09 +0000

    low concerning care exaggerate avoid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
