405a9a1e03fe81a8808c6691cc4213989f86f0b1
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:25:49 +0000

    opinion government damp trousers no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def convert_to_hex(n):
+        return hex(n)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
