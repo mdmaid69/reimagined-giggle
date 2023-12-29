43b7f43dea2529f43b2f3003bb291d13f123868b
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:14:34 +0000

    makeup issue translate recommend cross

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
