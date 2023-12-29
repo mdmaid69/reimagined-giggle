77225d843eeebf7670a51731a7b22b259a5fb331
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:24:54 +0000

    arm sore bang ride pronunciation

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def reverse_list(lst):
+        return lst[::-1]
