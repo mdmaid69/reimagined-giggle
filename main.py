8a565d6e57314a2b1569272a928fbdfdb12176bc
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:01:26 +0000

    floor announce see noisy eye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import collections
+def create_stack():
+        return collections.deque()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
