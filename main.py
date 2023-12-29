899ce1d60ca330951a04fb9b88470fa8319f268b
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:31:12 +0000

    bunch burst rest stamp fear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
