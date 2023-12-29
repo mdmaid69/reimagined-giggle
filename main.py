679f3d9b566d9a1c1a07166c17ed61cadb195cc9
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:13:41 +0000

    break fond invite cold kill

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
