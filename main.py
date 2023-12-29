1ed3d904b2975007e8fbac6ba6d86bec9c9afdfe
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:06:01 +0000

    you lookaround really official profit

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def convert_to_hex(n):
-        return hex(n)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import numpy as np
+print(np.array([1, 2, 3]))
