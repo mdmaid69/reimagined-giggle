ff434ef47522f70c9e81da7675a820c8f7b2b29c
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:41:23 +0000

    private worst effort right ring

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
