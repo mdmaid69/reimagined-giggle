ee4faae7f3278f3bb796facb980106991b872853
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:14:29 +0000

    sleep expensive touch thick attach

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
