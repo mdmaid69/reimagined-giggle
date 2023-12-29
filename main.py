6288c2e3e5012a2bf96f1190f42ab52f9d64f506
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:21:21 +0000

    winter slight spin reach late

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
