7ef7123973a89eba7f6ea416d9d95cb8c2dbd413
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:15:22 +0000

    cap unfair land conversation improvement

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
