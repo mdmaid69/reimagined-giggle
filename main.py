fe1b15b6fedb671bc53209fdaf96345be67ff0ac
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:33:19 +0000

    niggaacademy dark regional base lookafter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
