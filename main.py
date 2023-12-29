3e022961924adaf13830f88153db430fbfc6e44b
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:31:52 +0000

    suck avoid pencil sight giveaway

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
