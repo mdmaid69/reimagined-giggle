dd578dbfd9f6c425bc763dda8cf70f1595604eb6
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:15:55 +0000

    use seem behind stand instrument

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
