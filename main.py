8e114136ac31d21a182f2f27be384bddde2d8f39
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:28:12 +0000

    bad lunch OKorokay interruption feed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def find_min(numbers):
-        return min(numbers)
+def get_array_as_bytearray(array):
+        return bytearray(array)
