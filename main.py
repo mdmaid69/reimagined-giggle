6a72e023e4f09a5924595bb299197205c567a677
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:25:03 +0000

    know increase rent satisfied centre

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-print([x**2 for x in range(10)])
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
