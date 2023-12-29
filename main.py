58e7812a4b7d4c23040756ef60cdf7454004cfa6
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:31:17 +0000

    thing sleep special jump vote

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
