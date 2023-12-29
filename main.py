b4676dbc2cd2cbe4db8612a5830b99fa6871c85d
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:51:11 +0000

    neat hold base year able

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_area_rectangle(l, w):
+        return l * w
