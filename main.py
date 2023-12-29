a2e0361a76859471548502b8c75fb801a0536163
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:45:09 +0000

    callat simple mouth speak throw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import math
 def calculate_logarithm_base_10(x):
         return math.log10(x)
