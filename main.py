5dfd22577b7fa0c0796372554768b166554615c9
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:35:11 +0000

    unusual flavour act worth travel

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
