fd888bee972ef487dcce71503ec9d888249b362e
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:43:28 +0000

    middle only point young occasional

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
