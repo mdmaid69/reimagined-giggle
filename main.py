4938b5ab3951e9c0409ae8030d566a6834dae3bb
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:31:19 +0000

    slice believe behind question hard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_area_rectangle(l, w):
+        return l * w
