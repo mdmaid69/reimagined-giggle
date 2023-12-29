9ba892e7fa5aa1d957935286a0a447bc9aa902c0
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:39:32 +0000

    similar cause mind unit material

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
