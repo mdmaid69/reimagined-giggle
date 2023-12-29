cc013ef7f23e6defff914f5fb96abfc9b53d7850
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:44:21 +0000

    572enter light bush really spray

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
