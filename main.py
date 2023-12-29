0a802d7c737e79d7120548f3abe77b5e16ee08e8
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:10:23 +0000

    unfair scene everything quit trade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def greet(name):
-        print(f"Hello, {name}!")
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
