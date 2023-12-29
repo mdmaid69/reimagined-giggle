7485e88e7e186cefd33ed9d5c17383ee0571a3f7
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:30:31 +0000

    science concentrate earn file exhibition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
