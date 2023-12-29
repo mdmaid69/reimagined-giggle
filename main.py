632c04943d5a517a90d74d63278fe1fc59bf749d
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:21:51 +0000

    add with march offer be

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
