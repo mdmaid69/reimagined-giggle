06343777cd6326211057c752f3aea627aa534d82
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:26:30 +0000

    bit smooth rank for low

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_work(force, distance):
+        return force * distance
