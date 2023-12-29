5d29c61fb22b077856411d6ccef02506ebcdc94b
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:04:54 +0000

    may pleasure importance relative split

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def count_elements(lst):
+        return len(lst)
