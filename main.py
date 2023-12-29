d23ba659311e89b6a8c72af7a5a7cfba050a58b2
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:49:16 +0000

    impatient onto power grand scale

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_speed(distance, time):
+        return distance / time
