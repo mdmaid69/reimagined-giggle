35033eafc645332e023b5a31a7f028301ed93858
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:44:18 +0000

    satisfied rub major Ww bound

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-name = "Python"
-print("Hello,", name)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_work(force, distance):
+        return force * distance
