94e19a3fe966f34bff99a66b442e3e3b327604e5
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:49:27 +0000

    gas legal spoil anybody bright

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_min(numbers):
-        return min(numbers)
+import time
+def get_current_time():
+        return time.ctime()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
