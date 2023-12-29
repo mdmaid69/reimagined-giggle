2943657c0cc00ba04f22066877862e55f19459a9
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:54:55 +0000

    past fat performance 566end warm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
