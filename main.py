2724cb74cd7b18fb6f890865f593d8d0a1990ac9
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:15:06 +0000

    circumstance copy insist rich ring

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def split_path(path):
+        return os.path.split(path)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def get_array_as_memoryview(array):
+        return memoryview(array)
