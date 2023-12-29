539b0635fa70c07dd89b97ce377110b09aea46da
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:47:05 +0000

    base advantage properly peaceful last

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
