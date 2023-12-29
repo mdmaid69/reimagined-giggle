9cf52ebb23bfc2d6363e37b8dd3df44af8b776a6
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:58:01 +0000

    drink scared round right recognize

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
