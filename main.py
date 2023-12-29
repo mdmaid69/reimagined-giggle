065070982fa9da4070703047620edfbef7f6200a
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:36:57 +0000

    lunch guilty nervous attempt identify

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def reverse_list(lst):
-        return lst[::-1]
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+print([x**2 for x in range(10)])
