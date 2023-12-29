5f004b046ebdebc7d2654c599120f130f4e85834
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:25:47 +0000

    can beautiful everything minority evidence

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
