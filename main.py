2d92b23d1b0490448121c1ddb64493b1798ef485
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:03:41 +0000

    because speech foreign human side

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-name = "Python"
-print("Hello,", name)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  def is_odd(n):
+        return n % 2 != 0
