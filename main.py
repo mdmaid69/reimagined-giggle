3c5728f6409b9b091ef4cc9a61686fb00a3513ff
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:35:44 +0000

    major immediate worst weigh let

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
