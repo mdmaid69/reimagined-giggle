9c0314990c82d142f93b6b202c53fb2db99a54cb
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:24:59 +0000

    degree write journey own film

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
