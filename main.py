d7ef948ddc5959557f0cc610512f95aa1748097d
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:31:47 +0000

    careless burn slice press getback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
