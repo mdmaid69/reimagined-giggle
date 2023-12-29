efd36ca61c3a9728511d2cbcd647506422d84596
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:46:45 +0000

    against plenty attitude Ee pencil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
