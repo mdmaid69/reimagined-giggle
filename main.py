7fe34f9dd955a62478ed7fe8d0213f016d42c345
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:42:07 +0000

    Bb squash drink pity dig

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
