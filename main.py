c0f9ab88d838e4a012296a41216d6a39503249ab
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:39:40 +0000

    software rate account dream lookafter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+def convert_to_json(data):
+        return json.dumps(data)
