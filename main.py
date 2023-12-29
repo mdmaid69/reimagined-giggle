d803e9df0d97a3b0d9baff6c60a28ab716f65a31
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:21:01 +0000

    remain sick love clear temper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
