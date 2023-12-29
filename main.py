d92e994134b5af866497d5e5129b377eadf54451
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:07:17 +0000

    dance permission habit shout tour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
