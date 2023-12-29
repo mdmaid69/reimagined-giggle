022b9b1cd57b182d4ae7b5e1f256d27a41af02d2
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:05:24 +0000

    within even must part sense

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def reverse_list(lst):
-        return lst[::-1]
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def is_palindrome(s):
+        return s == s[::-1]
