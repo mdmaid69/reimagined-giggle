1777e8ef078590e170d6d4a54132bc6a5730afac
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:36:38 +0000

    spin undo combination bank tread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def convert_to_hex(n):
+        return hex(n)
