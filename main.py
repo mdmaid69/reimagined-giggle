493feeda81ffcf796fb43539fe11e49d4a6cc272
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:03:47 +0000

    road raise church work accidentally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
