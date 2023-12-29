86399085ce445a78a02e5b08b4755cd36f4cc02b
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:58:51 +0000

    offer safety wander open basic

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+print([x**2 for x in range(10)])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
