b7a4687d87775305673a40d35984b5a888d9785c
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:09:02 +0000

    show sound date burn cruelty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def multiply_numbers(x, y):
+        return x * y
