7fbd3e43af9c5035dd902a4df7bed450451ab680
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:19:13 +0000

    help bringdown size joke active

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
