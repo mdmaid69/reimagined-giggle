79c0c16b184f8a883d9a92b178d2402abac1f88e
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:37:45 +0000

    list task inside open culture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import time
+def get_time_since_epoch():
+        return time.time()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
