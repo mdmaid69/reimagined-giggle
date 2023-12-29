a748bea85723805663e6d38ed1ec84977b2c83f5
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:27:23 +0000

    move tune remark like train

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def divide_numbers(x, y):
+        return x / y
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
