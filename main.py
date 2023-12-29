d149a2c3131f7f963473d7c91f9b914d460fa81f
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:49:34 +0000

    urgent deliberately Zz lookup involve

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
