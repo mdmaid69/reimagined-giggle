c8429888c438bd725367038ad5a9371865e5c3fa
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:47:32 +0000

    573entertainment slight match likely lead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def is_palindrome(s):
+        return s == s[::-1]
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
