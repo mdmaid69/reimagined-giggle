62474bbcae3bf7e36fad1d2ae5a107cfc777bc46
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:14:00 +0000

    hand need stop score swallow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
