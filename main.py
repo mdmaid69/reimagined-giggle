0ebd32710414a7822071fb3ae174b2ceeeef25f9
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:55:56 +0000

    fit punish ourselves impression total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import platform
+def get_python_version():
+        return platform.python_version()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
