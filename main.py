6c854101e68aa31f97d2e81c1c33b1db29b86627
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:40:25 +0000

    murder tidy true impressed important

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def is_palindrome(s):
+        return s == s[::-1]
