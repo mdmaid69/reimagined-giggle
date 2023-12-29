671a08dea820f67d499f6f4229f97192b62bc01d
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:54:54 +0000

    trick minute attractive look apart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def multiply_numbers(x, y):
-        return x * y
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
