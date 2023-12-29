14a1974f9e35dd999083398a1043acab87a582e4
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:49:52 +0000

    sport achieve fall error base

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def divide_numbers(x, y):
+        return x / y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
