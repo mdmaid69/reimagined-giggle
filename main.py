7c92f37782f8bf1eb0768781c22f921dd2f78824
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:13:03 +0000

    goon choose content cross wake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def calculate_pressure(force, area):
+        return force / area
