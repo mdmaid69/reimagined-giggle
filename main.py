069b5dbe24f93965ee949020f29209dd505046f3
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:26:57 +0000

    smooth then birth easy public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
