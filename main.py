0b9066c27155e0bcb92e93e09a44a4a2f6072acd
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:32:51 +0000

    question move somehow rough return

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  def convert_to_hex(n):
+        return hex(n)
