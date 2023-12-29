d4552c00891084e490e79b8b3809c6e6a53eefca
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:37:43 +0000

    sell centre just die spill

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
