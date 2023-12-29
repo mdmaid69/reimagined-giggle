de48c254ae0c446504d34f31d7bb9bfc24609836
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:26:34 +0000

    bus forward drawing basis hurt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def square_number(x):
+        return x**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
