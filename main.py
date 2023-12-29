3d48effd68df241bae212d06597ffc96e997877d
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:01:48 +0000

    key unkind favour fly ought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
