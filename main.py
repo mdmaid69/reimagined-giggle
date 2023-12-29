22095e6f9c9dbfc2602a25a81fd8e39716c05198
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:41:42 +0000

    across win floor area common

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+for i in range(10): print(i)
