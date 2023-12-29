3e977cea383560cb527aa4fcdf09cbdbcf9ae904
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:23:02 +0000

    exam rely suit demand owe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
