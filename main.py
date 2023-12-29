0894f4bdef3ec4b1f4c2a62d0937e4947a333a19
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:52:40 +0000

    class voice pile switch pure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def reverse_list(lst):
+        return lst[::-1]
