5fe3578306983190c2045613b637e8ee644023b7
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:08:34 +0000

    consist simple copy introduce sentence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
