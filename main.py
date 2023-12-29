f904925b47ff49f4d5639aee11f84db3499fd1f2
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:13:40 +0000

    either touch keepfrom petrol goodbye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
