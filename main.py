a1daf5c3355ad7e7f99973ea2338919d65bd1e68
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:42:58 +0000

    advantage hang increase cry breath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
