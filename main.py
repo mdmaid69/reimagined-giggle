48fa3f6947ec400b4c58f2aa22f3e49b3d9b316d
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:14:16 +0000

    free future feed welcome small

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_volume(length, width, height):
+        return length * width * height
