1ef0b49ec46e372f6b6bd891966df83ed3a3917a
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:40:00 +0000

    embarrassing prove activity deal even

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
