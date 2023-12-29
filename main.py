2a7613f5859bbfc4bbec36ee474486ee94fbc661
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:23:02 +0000

    trouble breakinto price entry from

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
