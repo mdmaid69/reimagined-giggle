029b7400621e6de201e2fb3c52c795bd755fef2b
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:51:17 +0000

    late spoil card average science

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
