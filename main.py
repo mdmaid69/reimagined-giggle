d9ae1e69f927dbbf9d6007bc64026563c65ec7c9
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:47:08 +0000

    uncertain ride argument worse time

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def add_numbers(x, y):
+        return x + y
