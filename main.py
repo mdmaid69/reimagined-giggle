26bce88f2599ca7d1e64d3a9128520316bddd39c
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:48:14 +0000

    straight whatever develop slide trick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
