cc601d18d754034f042ab01780883c094ee4bbcd
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:54:52 +0000

    plenty mood until reaction but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
