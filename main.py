d6cfc871b84d7f4af2681e7b3c44a2c8b5b09e69
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:04:53 +0000

    pleased practical ever centimetre regret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
