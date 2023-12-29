6b4eaac45de2c1cfa57bd13e7bf31b4154d52f92
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:00:54 +0000

    weather confidence ordinary great design

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
