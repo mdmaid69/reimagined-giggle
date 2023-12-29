ff34e8eb3907ee46a2edc5544f6b3fae341e5610
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:14:43 +0000

    face important busy standard show

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def is_odd(n):
+        return n % 2 != 0
+  def square_number(x):
+        return x**2
