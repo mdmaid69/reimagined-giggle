b09cbbbaf60e53a1930b8e265bbd9167060ad82b
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:41:01 +0000

    true piece transparent any bang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
