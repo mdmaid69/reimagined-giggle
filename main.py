71f967fdf0769ae47517a405806e5732a91617d4
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:58:41 +0000

    turnon unpleasant distant reply cost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
