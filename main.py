ee4d3bde250eed3a06c67d5d389eef421c7ca400
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:50:35 +0000

    clothes brother takeout second agree

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+print([x**2 for x in range(10)])
+def count_words(sentence):
+        return len(sentence.split())
