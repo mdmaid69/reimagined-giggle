62aba3009b638b182789adfea151476d6f1b8ba6
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:55:44 +0000

    pretty accommodation I material natural

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def append_to_array(array, item):
+        array.append(item)
