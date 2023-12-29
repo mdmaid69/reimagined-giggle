2192b3c7e9d1ee3fcdda2da4bbb3f324d6f78d5d
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:56:20 +0000

    phone release relative speed by

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
