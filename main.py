9d9c157df45ad919cdb9ebc4de47bb684d4ff19a
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:54:47 +0000

    breathe suddenly comein tense smile

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
