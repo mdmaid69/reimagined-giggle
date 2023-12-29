408ef4ff50396fb5b124f2015e6f8cc983545305
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:57:21 +0000

    worth block away direct give

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
 import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def get_array_as_bytes(array):
+        return bytes(array)
