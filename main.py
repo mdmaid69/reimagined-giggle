eb4deba13e27c758748af51203265073c18a65a6
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:43:00 +0000

    depend this in shock difference

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
