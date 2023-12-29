8232f00b400259ce4a823b9939d75499d100279f
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:08:45 +0000

    government persuade instrument workupto apply

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+print([x**2 for x in range(10)])
