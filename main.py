954935b5a0bdb4d234c88a7106e35eed20f2b2b3
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:19:42 +0000

    storm branch design permission worried

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
