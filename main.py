4e99dc634bba7358454ee91dad6262d8db04d157
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:44:10 +0000

    comeback on skin rain back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
