27547ad8b2234f8a7c412674c3d45d8b169e69f8
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:40:20 +0000

    autumn importance characteristic improve improve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
