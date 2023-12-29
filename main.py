6f711a95055a5408fcc73bf8c672b77363fd7959
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:08:08 +0000

    interested undo behind violent night

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
