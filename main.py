a976d371226b9ae548fad05f2cf3b5bbeb5ede67
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:51:11 +0000

    back something load public listenfor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
