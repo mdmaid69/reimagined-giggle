e16c3c465f6455e9da07c31fc0e465134becf6e8
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:13:50 +0000

    deliberately impressive chance Ww achieve

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
