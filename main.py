4733282fa81a9bd0d830dd3d603881bb476fcbad
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:28:41 +0000

    necessary Ss hate machine perform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import numpy as np
+print(np.array([1, 2, 3]))
