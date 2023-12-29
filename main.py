e7bcd140278e8ae5347f364aff0bc9a7bc25a30e
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:31:35 +0000

    support emotion share anybody attach

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
