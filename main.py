0b0cd5d128eba53a606dc0fbd82cde3380bfc3c0
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:12:48 +0000

    length wave niggaacademy violent original

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
