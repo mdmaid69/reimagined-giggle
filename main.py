61f125996c813f32dc488e5a120a8457c466395b
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:36:35 +0000

    say often describe throw mass

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
