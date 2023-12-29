78ee4c60234d017771d0fd6c01526d42bef89a22
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:12:15 +0000

    OKorokay freeze safe possibility culture

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
