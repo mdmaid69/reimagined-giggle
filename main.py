e34b201aedcee4b3d3b04eb8c36d92a892cdacf2
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:18:07 +0000

    what agreement sign subject boil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
