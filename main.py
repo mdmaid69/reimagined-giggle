454447dfeb514246c5012c9ac71aed12bf2535a0
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:07:53 +0000

    language admire cough workupto family

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def find_max(lst):
-        return max(lst)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def calculate_area_rectangle(l, w):
+        return l * w
