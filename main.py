2b6a239ac99821b649bc3588d8f596dd48ab1891
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:46:52 +0000

    minority manner run song alive

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
