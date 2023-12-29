e1fdd5164266b48828f161fcca7389fea8c737fa
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:23:30 +0000

    last nothing one mouth wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_work(force, distance):
+        return force * distance
+import os
+def get_current_working_directory():
+        return os.getcwd()
