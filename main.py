0e83e631c6f657ac7e89fa4f84dbb80bac8966bb
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:16:20 +0000

    comefrom prove lift mainly takeoff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_list(array):
+        return list(array)
