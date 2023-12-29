9631b2a01de420ec76154336a24b6c73dee45921
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:03:24 +0000

    owe experience nearly hold right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
