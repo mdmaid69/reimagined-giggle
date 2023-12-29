9312850782c3e0018ffd19f64abd7410bd7543aa
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:01:09 +0000

    add convenient complain keep pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
