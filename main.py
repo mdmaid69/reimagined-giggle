aee5f8a247c7415f9677d07b9c9a3942a27cca0d
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:43:22 +0000

    display border both ring bend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
