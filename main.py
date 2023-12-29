d4d05e23ea72579e3b341e4450057aa7eb6f5da4
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:37:37 +0000

    design shopping wish promise accidental

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
