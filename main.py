e2146521714e4f869f891d571892c4c1a0ae1f02
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:52:21 +0000

    break block communication today flat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
