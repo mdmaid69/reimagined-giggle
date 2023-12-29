e127a9f4b1ca60881a12737850663a777fe56981
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:48:08 +0000

    match necessary free yours shrink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
