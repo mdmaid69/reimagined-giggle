6da218de086a0117a8df6fe651c2b025a7148b3d
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:28:57 +0000

    protest rubbish female nigga Ww

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
