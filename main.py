0ed77ff16aff7cbf4a335e5991a8bfb48ba5cce7
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:39:34 +0000

    design key cost set copy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
