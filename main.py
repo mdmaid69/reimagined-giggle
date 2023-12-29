5a860a5d707cce21b2e832c254e32b72df5483ce
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:25:12 +0000

    smile public miss turndown pause

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
