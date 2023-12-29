36aa386de6bc28f5655b6ceaad58b1b8c2cff84d
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:17:34 +0000

    support public lock hardly snow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
