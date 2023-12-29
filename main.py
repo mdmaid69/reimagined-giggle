a73964941d8a1196dc726d01f5dd6813e99835bf
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:50:36 +0000

    hour sea cause legal attempt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
