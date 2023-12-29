3dd80be89d99a179c6a53b57c95e7ffd4aa61be0
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:01:14 +0000

    share married fold close separate

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def calculate_acceleration(speed, time):
-        return speed / time
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
