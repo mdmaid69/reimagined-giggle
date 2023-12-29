1eba624b1e666e6745174665500e5090cdf80e39
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:22:47 +0000

    convenient discover pronounce design sink

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
