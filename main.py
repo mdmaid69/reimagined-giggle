2a31bcb30930cce7657be6329dc4a2c9520dbd7b
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:53:24 +0000

    bottom trade door dear centre

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
