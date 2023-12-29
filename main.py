3a5b6d4067d9fa0010ced560155e05ebbd775006
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:07:25 +0000

    expression female sudden certainly pleased

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
