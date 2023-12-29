d2d467600976b3fb9c9938523989cf40ffee656a
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:09:25 +0000

    transport call hospital smoke only

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
