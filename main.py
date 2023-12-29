fbbd9a40fa0fbed683bdd51a12986b8aaa1bce0f
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:19:12 +0000

    rest impressive flight what fit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
