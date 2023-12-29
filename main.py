9c6bc7a90b3585298dde1a3ef14f764933806b55
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:38:00 +0000

    live force score risk may

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import getpass
+def get_username():
+        return getpass.getuser()
