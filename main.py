e05254f6c9b87732be8259b34093e62ab1b733de
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:51:28 +0000

    wealth heart chin agreement quick

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
