0d6d806ccfbcb0daca886289f139c0553ac7673e
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:11:40 +0000

    talk tragedy teach fast loud

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
