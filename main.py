9ef5b6762dcf6e0aa8c99fd8c3e9a26db3a3b41b
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:09:28 +0000

    error tragedy detail alive game

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_length(array):
+        return len(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
