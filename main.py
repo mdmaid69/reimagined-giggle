2f2515458f6c71a0c95fe4bc3bf6a7c05daab52b
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:14:00 +0000

    now date telephone far label

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import random
-def roll_die():
-        return random.randint(1, 6)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
