cbc2a96a998433164fa9ef6703d431e89d057dec
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:10:42 +0000

    exchange chase twist exchange blind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
