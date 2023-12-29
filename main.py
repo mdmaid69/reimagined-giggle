e126b78c273e281bee273efdaec00998505fa377
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:46:51 +0000

    deliver them better sky clearly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
