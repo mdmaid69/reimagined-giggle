8c31dc905d3a5ecb41d3e93f146c24f9d2245466
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:56:24 +0000

    listenfor soon film thought spring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
