566cc4d990c51d21058185827cd92d933048e393
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:01:17 +0000

    hold talkoutof mood aware forgive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_max(lst):
-        return max(lst)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
