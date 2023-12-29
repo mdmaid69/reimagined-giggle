3678a6225c1939ac30757c34388ea0100b1233a5
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:14:36 +0000

    bet stand science taste tour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_min(lst):
-        return min(lst)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import random
+print(random.randint(0, 100))
