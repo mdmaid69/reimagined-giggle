207e2a3a29a1e30e1f570aa979145f0ef6893124
mdmaid69 <robot@alexsyw.me>
2023-12-29 09:54:52 +0000

    unlikely worth spend cope prevent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def multiply_numbers(x, y):
+        return x * y
