f64d95ad0bd8f9f77d22aca9673c64d6478e56c8
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:20:37 +0000

    official clever though world person

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def is_odd(n):
-        return n % 2 != 0
+import collections
+def create_priority_queue():
+        return collections.deque()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
