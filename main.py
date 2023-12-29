f8cacbdaa1ce0345113d0ece1be81a57b6ea21d2
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:00:42 +0000

    movement close intend any speech

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_min(lst):
+        return min(lst)
