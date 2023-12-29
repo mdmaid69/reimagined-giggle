2e98ae9e60ceea6198c1c9256c25eb345ebfeb8a
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:38:58 +0000

    lesson hurt morning room often

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def is_odd(n):
+        return n % 2 != 0
