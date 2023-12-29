98c2d4a40d3f87737f9bb3818b7c79d244e3a510
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:50:21 +0000

    nor decide possibility catch spare

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
