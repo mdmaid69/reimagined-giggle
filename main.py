36c466582b6eeacb671960cdb464aa9c9b75d121
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:21:27 +0000

    over cold dream set carry

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def calculate_density(mass, volume):
-        return mass / volume
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_power(work, time):
+        return work / time
