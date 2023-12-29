2e766c02a93d38088ed3a4fa5486deddddb5f7c9
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:59:06 +0000

    dream variety project bunch wind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
