1ec6706e5d5f54d8dc701addaf603abf36a2f022
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:16:14 +0000

    miss hold towards conclusion slip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
