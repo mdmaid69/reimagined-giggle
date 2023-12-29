8007285a12a98a44973586873a55c1d993e9f4aa
mdmaid69 <robot@alexsyw.me>
2023-12-29 05:35:54 +0000

    light open fun warm lower

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
