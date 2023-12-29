e30ee535cf789877cdc9ad159fea0d8b43d7414b
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:41:07 +0000

    with alone some general instruction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def divide_numbers(x, y):
+        return x / y
+import array
+def set_array_item(array, i, item):
+        array[i] = item
