0da509bf45e2dadca106efdd0f64175c1cef05d6
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:45:54 +0000

    crowd first medium ring issue

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_words(sentence):
-        return len(sentence.split())
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
