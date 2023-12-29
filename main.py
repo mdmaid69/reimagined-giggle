04ba976e9ab90a04499710a12208c88eff3f6798
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:12:33 +0000

    nigga adjective lock against crowd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
