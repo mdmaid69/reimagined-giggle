a0d665db00338ad48bfbe2f2ca5ce2310e95f901
mdmaid69 <robot@alexsyw.me>
2023-12-29 03:11:06 +0000

    clearly doubt callback trick regularly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
