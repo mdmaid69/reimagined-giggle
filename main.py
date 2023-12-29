b849767d95b42d355e4f16d724ccc16f1a4c7f7a
mdmaid69 <robot@alexsyw.me>
2023-12-29 21:14:59 +0000

    trousers method bus busy belief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
