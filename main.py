510ab1406545e6bc19d9b596559acaa88e4a198b
mdmaid69 <robot@alexsyw.me>
2023-12-29 17:20:30 +0000

    show open sell Ff opposite

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def square_number(x):
+        return x**2
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
