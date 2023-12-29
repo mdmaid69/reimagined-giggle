f087c694bd48d7b76684b18f7097a219f4d1d185
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:57:47 +0000

    phone relative guilty lookback(on) wonder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-print(os.getcwd())
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
