3967d4df1af14c3da45b56ddde643c3b79256ada
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:58:36 +0000

    go sentence crash live stop

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import platform
+def get_python_version():
+        return platform.python_version()
