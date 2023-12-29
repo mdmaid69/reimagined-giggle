723034b913020fd45d742497bc32d3ed8a4a8556
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:26:38 +0000

    flavour neck related collect cross

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_current_time():
-        return time.time()
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_area_rectangle(l, w):
+        return l * w
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
