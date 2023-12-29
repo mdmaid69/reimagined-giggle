147d026e362418ab5875dd80bbdf2aaa2ee287c3
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:22:03 +0000

    deal godown thanks arrive die

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import math
-print(math.pi)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
