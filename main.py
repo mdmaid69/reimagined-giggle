275d30f4b20fe6fbdee93c903e3d0eba915c9133
mdmaid69 <robot@alexsyw.me>
2023-12-29 12:46:26 +0000

    beat afterwards lot brain amount

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
