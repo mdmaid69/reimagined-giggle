d45311d9beb645deaee3aee4d07407255a4ab1dc
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:07:57 +0000

    yes wake near tie drop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
