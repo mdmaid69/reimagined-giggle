7e107fe8d5787ab5935202951a575452ae25bfce
mdmaid69 <robot@alexsyw.me>
2023-12-29 15:29:04 +0000

    error capital complain write thumb

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
