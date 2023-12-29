c9040b7212a81d13cd2a84b21927a4dbd3077f4d
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:01:38 +0000

    aim thought distance body design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def find_min(lst):
+        return min(lst)
