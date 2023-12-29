b2b716fa17befa7a2958cf469033866e12ea6297
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:21:04 +0000

    squash impossible price arrange star

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
