2709827676dc38b65249b404693708957bf33980
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:05:28 +0000

    belief clean broad ear pull

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def greet(name):
+        print(f"Hello, {name}!")
