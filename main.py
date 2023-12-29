7423b1c227025b2c7912dbd1d3c421acd82771de
mdmaid69 <robot@alexsyw.me>
2023-12-29 06:36:02 +0000

    exciting really fear pass holiday

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  def convert_to_hex(n):
-        return hex(n)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  def multiply_numbers(x, y):
+        return x * y
