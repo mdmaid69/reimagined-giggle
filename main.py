df277991e8fc1a3c407299f05d43d5165b85abd3
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:37:03 +0000

    score joke takeback tradition nose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
