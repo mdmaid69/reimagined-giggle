b24055dc5b45a9baceb53ab47a1de29fb1ef5d2f
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:36:05 +0000

    anyone sound culture under absolutely

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
