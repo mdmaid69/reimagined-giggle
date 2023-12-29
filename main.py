1f991d63469e8812f9bcf01f2f38b74a29212c60
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:23:27 +0000

    address schedule live large low

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
