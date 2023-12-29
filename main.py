2e3f77e2fe8de94737780de3635c95c185703559
mdmaid69 <robot@alexsyw.me>
2023-12-29 01:16:47 +0000

    accommodation impatient lot formal several

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-print(random.randint(0, 100))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
