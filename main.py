812ef75484b2a48264d8bd4c7397fcd91722007e
mdmaid69 <robot@alexsyw.me>
2023-12-29 22:21:08 +0000

    workout read thankyou mysterious earth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import time
-print(time.time())
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
