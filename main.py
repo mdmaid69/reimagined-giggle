7ad6cac98d7cba64aae69798c5595170804e0a79
mdmaid69 <robot@alexsyw.me>
2023-12-29 08:53:00 +0000

    breakout feel typical plastic once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def reverse_list(lst):
+        return lst[::-1]
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
