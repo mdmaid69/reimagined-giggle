419e61a397e7dad67cd32a18f12ca5172f671a0f
mdmaid69 <robot@alexsyw.me>
2023-12-29 11:49:03 +0000

    guilty gate Ww instrument reason

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
