9bf7e9f20319571c0335a3deb2ecd589b811c021
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:43:07 +0000

    flat that sort equal deliberate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def find_min(numbers):
+        return min(numbers)
