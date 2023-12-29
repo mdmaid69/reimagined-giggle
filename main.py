992dd2eb94dc2fd2442496e7db841708fd600f95
mdmaid69 <robot@alexsyw.me>
2023-12-29 04:22:08 +0000

    smell sad fashion together fear

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+for i in range(10): print(i)
