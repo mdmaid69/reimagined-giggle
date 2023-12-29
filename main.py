252449ce8a7e1a092bc2265eabf9c909b5d1c76f
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:02:09 +0000

    in watch tend wing unite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
