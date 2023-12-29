08163b5a27702540b15969a5d3a3f604fc9e3004
mdmaid69 <robot@alexsyw.me>
2023-12-29 18:38:07 +0000

    cross later meal old refuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_length(array):
+        return len(array)
