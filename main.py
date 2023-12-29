8f62859119dcc7c9030bdf57e55ba5f62dba2ca1
mdmaid69 <robot@alexsyw.me>
2023-12-29 19:06:35 +0000

    fashion original software nowhere aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
