56545a79f7be79ccacbfda83aaa118dd19ccdcfe
mdmaid69 <robot@alexsyw.me>
2023-12-29 10:37:56 +0000

    suddenly track address how they

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
