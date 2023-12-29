9598a35b1638e7a060a1b63e0b51bf27245ff0ef
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:22:33 +0000

    find soil buy serious relax

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_current_directory():
+        return os.getcwd()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
