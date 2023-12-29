664eeb9fe5dcba090124a59bb5b81432a8498698
mdmaid69 <robot@alexsyw.me>
2023-12-29 14:16:43 +0000

    ilovedicks imagine build quiet behind

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
