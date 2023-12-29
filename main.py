88f080dde992030a1817e12757bed3cab3f0032b
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:27:11 +0000

    other miss spend open callup

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
