8cde08e40dee03b0a5958afae0c05806d2773e79
mdmaid69 <robot@alexsyw.me>
2023-12-29 16:02:49 +0000

    room wall imagine spend comeover

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_area(radius):
+        return 3.14 * radius * radius
