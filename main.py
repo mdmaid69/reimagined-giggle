fd4da4f98ad5376a6b734c1f87b7f5c85c6a9d7f
mdmaid69 <robot@alexsyw.me>
2023-12-29 23:58:39 +0000

    sky lower soil excuse cruelty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
