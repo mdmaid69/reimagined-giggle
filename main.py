5e371a5c35ae4c6e7596b63b47ae85b12c9945fe
mdmaid69 <robot@alexsyw.me>
2023-12-29 13:05:42 +0000

    getaway certain interesting anywayanyhow doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
