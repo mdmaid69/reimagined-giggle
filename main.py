09d7faca20bcfa07deed4da7d2211eaa91215cfa
mdmaid69 <robot@alexsyw.me>
2023-12-29 02:51:06 +0000

    unpleasant touch sweet bunch thing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import sys
+print(sys.version)
