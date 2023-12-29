d7f2bf3d01e0740648b81f1adc0ce4f3c044bae0
mdmaid69 <robot@alexsyw.me>
2023-12-29 07:42:45 +0000

    lookup importance especially shopping by

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
