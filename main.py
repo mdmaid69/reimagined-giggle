f6e98c6ad7ece0037159fdba2b6223fc00fde252
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:18:14 +0000

    country deaf board attention thumb

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
