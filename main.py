931beb43c1e3809b601de576ae3f06d52bd7421e
mdmaid69 <robot@alexsyw.me>
2023-12-29 20:25:15 +0000

    rescue welcome interruption different you

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
