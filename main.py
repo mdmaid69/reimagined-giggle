be6cbffa5cebbe9d900267b298fa1e50d647fa0b
mdmaid69 <robot@alexsyw.me>
2023-12-29 00:09:48 +0000

    save brief if effect profit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
