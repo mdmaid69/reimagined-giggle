e4eb263e44b62234f2a94c88dd1b9eb1c30661fd
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:44:30 +0000

    connect equipment flight too believe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
