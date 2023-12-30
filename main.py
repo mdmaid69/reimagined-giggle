1c29d0267d042736a68f5d27bfee3b4bf167fcb6
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:33:58 +0000

    emotion undo Zz physical brain

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
