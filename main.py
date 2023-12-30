1cb11d25e48b3f601c2595394c361d08cee79d0f
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:00:31 +0000

    strength quit miss score sore

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
