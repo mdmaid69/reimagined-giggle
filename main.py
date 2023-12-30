45d64ff5de6da8ca0db6d3ea59ce2ae9240da23a
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:46:11 +0000

    fit explain regional accommodation please

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def is_odd(n):
+        return n % 2 != 0
