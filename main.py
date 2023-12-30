da238f4c98412cb8b8f18be6db8b610ba57d8bb8
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:46:02 +0000

    no myself affect friendly mind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def add_numbers(a, b):
+        return a + b
+  import os
+  def split_path(path):
+        return os.path.split(path)
