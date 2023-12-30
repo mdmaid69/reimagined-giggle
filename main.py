5bfce6cbf9de381e714441b0732b16bee1cd290d
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:13:48 +0000

    threaten characteristic pass fairly slow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import random
+print(random.randint(0, 100))
