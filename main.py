597887ef99655a8f9ac3905722d4b05e01d7f6f4
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:26:37 +0000

    flat compete sort consult emphasize

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+for i in range(10): print(i)
