43ff0fde6c1d4020fecba76ff895a9d67b5e9656
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:06:16 +0000

    close difference rarely best judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
