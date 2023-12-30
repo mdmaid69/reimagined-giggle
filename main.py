1cbb0fedc89ec8531f4e82072dc05614b1a9d369
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:34:11 +0000

    spoil card fail further its

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def greet(name):
+        print(f"Hello, {name}!")
