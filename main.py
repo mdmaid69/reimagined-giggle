2a9a7db2d3d59579db6b19d096e7686711abafc5
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:07:21 +0000

    centimetre slice private welcome boat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
