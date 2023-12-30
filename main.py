4c94eede2cf10857b0c5bc1df9d6aec359ae02c8
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:41:57 +0000

    cross shopping oil arrangement clearly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
