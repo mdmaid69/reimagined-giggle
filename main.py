387541d442c2d6e76d769339142d2942ca63cd32
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:49:19 +0000

    handle physical comeout part while

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+print(sum(range(10)))
