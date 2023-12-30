f7fd1fe6eb0ffec057f3877eec7fcfc41b7dd020
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:28:13 +0000

    well wonder gate pay bin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def get_array_as_repr(array):
+        return repr(array)
