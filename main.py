81b3e2f8647f92be92c1dd594c5175b3ad8018e3
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:12:17 +0000

    responsible brother name right instead

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
