656b43ea33223d8e805f376b5dba496dc5275c77
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:19:40 +0000

    rent where habit importance Pp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def convert_to_hex(n):
+        return hex(n)
+import array
+def get_list_from_array(array):
+        return array.tolist()
