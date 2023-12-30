9248d233de628c4f8c505eef5a6497770bea9a2d
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:40:57 +0000

    communicate warn judge fall run

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
