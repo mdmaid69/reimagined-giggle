6289605bb3a00900652fcf33cbb8a3150c9144af
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:07:47 +0000

    scream physical software compete she

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
