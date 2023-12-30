1e34298e12f212cd115460c9b93a11c1e258ed53
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:20:51 +0000

    visit through generally ought rain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
