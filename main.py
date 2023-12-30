2f27f8a7723fdd5fe7d53844ee1456fa2283b212
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:13:22 +0000

    exciting winter measure ours history

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
