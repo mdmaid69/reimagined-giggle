718709e1da6a464988282bb4e86310cc7905f902
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:45:15 +0000

    feature truth behaviour band transport

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def append_to_array(array, item):
+        array.append(item)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
