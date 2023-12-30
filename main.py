7f3368c3346dfe9baa81176c9bfc917fe3ef95c3
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:28:22 +0000

    platform brush correction storm rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import re
+print(re.match("h.*o", "hello world"))
