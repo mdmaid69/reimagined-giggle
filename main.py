6c0428f16170855d3add1ce66383fffb83836365
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:20:42 +0000

    downstairs to forgive acceptable traffic

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def convert_to_hex(n):
+        return hex(n)
+import time
+def get_current_time():
+        return time.time()
