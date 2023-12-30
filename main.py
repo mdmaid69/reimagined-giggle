7113bd3e7c3c3812e4fff3d36e8e1e3b50bd8494
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:17:24 +0000

    anything bang unkind hurt doubt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
