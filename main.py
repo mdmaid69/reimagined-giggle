e5ec7dff9988f2989af2f37a9c9bbbfe409e76a2
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:39:18 +0000

    declare summer actually boat attractive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
