22c6771619fcf3f4d8cb4943638dd11d117e2682
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:14:57 +0000

    after upwards life key swing

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
