a8b0d693b5c4380508fb12dc7de23f66da165b3c
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:49:49 +0000

    improve custom wipe Qq crowded

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
