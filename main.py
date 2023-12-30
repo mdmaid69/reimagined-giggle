72effb794944e9f79fbc24e236716bd203265c70
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:07:19 +0000

    underneath lunch march female full

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
