a81bbcf05d59d600da40ad27674e5281d357658d
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:22:26 +0000

    rain definite block look coffee

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import sys
-def exit_program():
-        sys.exit()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
