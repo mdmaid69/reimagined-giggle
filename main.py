d0380edbb074e98bc43eface09a567050374ca1a
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:21:14 +0000

    blame drink next accordingto bringdown

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
