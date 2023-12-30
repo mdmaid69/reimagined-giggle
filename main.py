c3517f6d33fff7b78f3ff0d65eb4245a32b55357
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:20:23 +0000

    high history content arrive flavour

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+from collections import Counter
+print(Counter("hello world"))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
