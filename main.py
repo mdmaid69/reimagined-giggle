8e18aaccb4b96961fdf55a48b515b03e486023a7
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:20:06 +0000

    compete common conduct tip whichever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
