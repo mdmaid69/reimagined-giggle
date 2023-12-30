350c319565fe90cf6b4a53d5c2b19301346f58bc
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:05:54 +0000

    trip character lock friend 566end

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
