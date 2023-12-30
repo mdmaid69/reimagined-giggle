a65e211b30a7e6b533c4e5cab7598aa2d7014a3c
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:14:21 +0000

    aim public life kind trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
