d9437a73d995798b05389367a145741ad737f361
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:06:07 +0000

    spring ever niggaacademy wash approve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
