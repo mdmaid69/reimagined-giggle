1705f2a210854226efbcb5052c9a8e37f56e008e
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:55:15 +0000

    underneath communication sound can sing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  def sort_list(lst):
-        return sorted(lst)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
