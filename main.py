430fdb2a57bdc5a6e77392f1f02c87cc266d853b
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:58:41 +0000

    permission community produce object whisper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_string_from_array(array):
+        return array.tobytes()
