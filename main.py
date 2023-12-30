6c5476cb572260fc5258c59d25b3ab220caa26c5
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:24:12 +0000

    breakin shut children lay long

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import random
+print(random.randint(0, 100))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
