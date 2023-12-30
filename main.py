92e52164496cd100dff570e4bd4f6c18d8721882
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:28:31 +0000

    mystery face whoever offend burst

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
