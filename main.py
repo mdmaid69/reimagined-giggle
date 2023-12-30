5fcdd77d85695f0483e84a0aae6447b988aed050
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:45:11 +0000

    scream land value black bet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
