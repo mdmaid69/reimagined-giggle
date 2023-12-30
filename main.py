6ca140ccf302b43840febab9f2003f10a9fb4c25
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:00:19 +0000

    dick quick characteristic section trip

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_area_rectangle(l, w):
+        return l * w
