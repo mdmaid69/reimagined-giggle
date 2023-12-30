cabd5a84cd4695df0c477a7613c72842be558926
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:44:32 +0000

    century greeting coat shop shopping

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def find_max(lst):
+        return max(lst)
