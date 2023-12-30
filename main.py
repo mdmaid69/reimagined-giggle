bc55274ac7dfe22deb56fc566a242096dc84397f
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:36:13 +0000

    snow ourselves 567enemy obvious match

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import sys
-  def get_python_version():
-        return sys.version
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import time
+print(time.time())
