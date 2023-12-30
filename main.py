e7246f5891e7a3fd136e71adb9a174264e71a4b2
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:03:17 +0000

    deliver card open war around

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
