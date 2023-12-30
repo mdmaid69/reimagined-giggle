69f61d123f098f20e35f05266387db2c44b9c8fe
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:10:00 +0000

    completely memory earn exercise wet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
