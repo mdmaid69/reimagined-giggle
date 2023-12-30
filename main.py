635713dde67f57992135c8f5f9726d75d138acbb
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:20:08 +0000

    apologizeapologise refuse provide field invite

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
