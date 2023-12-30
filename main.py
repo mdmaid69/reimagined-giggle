f83885b64bad9b06bd464be0f88ab12dde0f5c51
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:01:06 +0000

    quality risk occasional taste left

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def count_words(sentence):
+        return len(sentence.split())
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
