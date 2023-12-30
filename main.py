5e59dc63e4e8c9821299e14e66c9fe45acd27075
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:45:21 +0000

    certainly sensible juice wish on

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
