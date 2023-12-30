5a714b23cbdf72564d243ccc4fe9220fa1785eb6
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:26:54 +0000

    ask underneath pump bringup about

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def sort_list(lst):
+        return sorted(lst)
