56f46054c6f765e9b15e0de789231f71846ca72a
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:04:41 +0000

    treatment underneath careless discovery empty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
