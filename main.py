fdec2776d5b4a8a4abad8d215698f33dc4c96086
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:19:16 +0000

    fail before steep street connection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def exit_program():
+        sys.exit()
