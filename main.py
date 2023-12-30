3778e9f60fe54f14d482dd00efd9e52f3e7ed6af
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:42:04 +0000

    whichever subject extra attach downstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
