58ce946c877c8375deb8136de0d5ac2b9b337f3f
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:23:42 +0000

    name past all shock medium

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
