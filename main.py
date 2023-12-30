71bbdab629b34659f229de3a4781ade3b71da396
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:31:45 +0000

    worth accommodation second strict today

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
