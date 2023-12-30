49331ad42797402c58b4e457ff0975e70bde8691
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:50:22 +0000

    term fit complaint comeover neither

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
