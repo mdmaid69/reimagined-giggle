9fd74e110676de5fe128ccf0000c100ffec63d7c
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:31:21 +0000

    room conclusion keepup attractive flat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
