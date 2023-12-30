b741f4f7da8b55d6a491cfe5b75b5f118e38c8d9
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:24:23 +0000

    file tap her maintain there

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
