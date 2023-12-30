ec47a2a2d0cc9fd3dc5d01ccd0734c6c95cebf75
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:19:03 +0000

    conversation equipment tell disapprove turnout

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_pressure(force, area):
-        return force / area
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
