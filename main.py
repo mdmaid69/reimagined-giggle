498ee6ad07767a0a122b8f5568b729dca7e7e8b6
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:27:58 +0000

    getoff terrible breakin bottom emergency

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-  def square_number(x):
-        return x**2
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
