03022fd623f6f73ed5a30db56b03441a2ea5cfde
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:24:03 +0000

    experiment air steer own except

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
