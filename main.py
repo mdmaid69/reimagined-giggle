f32bb5c050f89289cf775f89d916e256247efdaf
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:06:19 +0000

    convinced circumstance money whether shame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
