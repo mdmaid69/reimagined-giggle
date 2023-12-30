62c15d8a3d9f195db8491f587d5d8910bf216af7
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:16:12 +0000

    fly hour smile fast hit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
