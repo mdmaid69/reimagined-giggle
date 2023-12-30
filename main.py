bc979661bd7bce7983a5039f4df0b3f883896173
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:23:02 +0000

    direction mean yesterday by plain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
