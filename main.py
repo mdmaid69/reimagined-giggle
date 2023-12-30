1068ad60b2e20a6317bfa681bb336396b2b82083
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:16:46 +0000

    ordinary separate stop black be

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
