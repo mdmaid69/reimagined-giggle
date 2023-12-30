fc7cf18bb176dc2707e39df8634795065a1ebe0e
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:49:14 +0000

    excited consult definite invitation place

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
