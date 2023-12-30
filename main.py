d44a8c85eb687fd19205b5f1f581b678ebd2048c
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:52:45 +0000

    decision injury rubbish rescue hurt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import sys
+def exit_program():
+        sys.exit()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
