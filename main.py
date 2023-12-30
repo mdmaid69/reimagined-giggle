4cc353777d1a54d99bc5ab21d82e9d8055a44690
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:22:12 +0000

    skin relative what still into

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
