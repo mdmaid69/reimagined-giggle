ee08a1bad0d666df21b7b069081c7173800e5632
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:15:17 +0000

    refuse she stiff excellent answer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import collections
-def create_user_string():
-        return collections.UserString()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
