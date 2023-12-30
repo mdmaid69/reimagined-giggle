f5d9d1b9f0061835b2276fc1160872a5ece7b4a1
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:31:25 +0000

    plastic usual decide cut exaggerate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
