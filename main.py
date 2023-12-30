43a82c4a4a23466897be6bb3055205ff80ffced9
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:21:09 +0000

    wrong cut least opposite curious

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
