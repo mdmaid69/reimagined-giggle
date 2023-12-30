1718642576bcc1f6c44a53ad2afd4667466d7f9b
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:55:08 +0000

    comeout approval between soon truth

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
