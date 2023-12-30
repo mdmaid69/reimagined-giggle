d79b1ac9d117750b1a98f4b5661ed06daac08426
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:02:33 +0000

    entrance cheat care unusual spend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
