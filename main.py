dd79ea56da7f656870f892b24485914bb8c69a99
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:59:41 +0000

    preparation wall 567enemy rise lay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
