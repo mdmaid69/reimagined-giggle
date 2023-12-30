470fc6214edeaab05b40ec77a9fc3dab5bccdca0
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:40:27 +0000

    likely full hold bright follow

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def square_number(x):
-        return x**2
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import time
+def get_time_since_epoch():
+        return time.time()
