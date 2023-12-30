8937f29fba391975c3910ffa07ab8c83cc47cf8c
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:45:20 +0000

    whoever bang minor another shine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
