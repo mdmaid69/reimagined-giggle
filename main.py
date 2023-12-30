01fb902481a7520e600ca8d861e831f4132a215f
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:28:26 +0000

    right beautiful near uncertain kill

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
