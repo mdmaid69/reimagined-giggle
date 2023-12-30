6a09bc2bbbd37f53b2311f28a97dd859369d2b09
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:33:12 +0000

    size neat official suggestion shopping

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-print("Hello, world!")
+def square_number(x):
+        return x**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
