b1fcd39dedefb7c1a466e5fe15e450fca42fb979
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:41:58 +0000

    listenfor bother touch importance discovery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
