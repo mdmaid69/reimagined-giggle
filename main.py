fb1ea9f3b7d49251a9bb8faff1a2e00e9cc45bbf
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:11:00 +0000

    guilty squash feature section in

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
