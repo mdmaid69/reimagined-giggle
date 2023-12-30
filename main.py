ca32b2980632ee939a7fdd5d282dad75d4b69bdf
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:08:58 +0000

    size direct content intend flat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
