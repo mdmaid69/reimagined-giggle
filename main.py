a8da139361a67976187336be33011cd5de4ba487
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:18:39 +0000

    warn feel no apply married

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import array
+def get_array_item(array, i):
+        return array[i]
+  def count_elements(lst):
+        return len(lst)
