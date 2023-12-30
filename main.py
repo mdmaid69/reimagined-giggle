efdf0ed6e6106ad7395a76d18ab3601074fcd159
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:13:58 +0000

    stir use hair still science

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
