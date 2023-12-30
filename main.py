fc54b06b9a4f130c3a70eeb54c4f04eb54360a4a
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:39:04 +0000

    about appear equal swimming rain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
