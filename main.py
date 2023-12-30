7791a42addb7ed9075f44b14679c8de3164a151d
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:50:56 +0000

    somehow naturally settle crime 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def find_min(lst):
+        return min(lst)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
