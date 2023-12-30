2345184063a005f73036b2f31c2e146ef49d5dcd
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:40:00 +0000

    hollow belong reasonable priority load

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
