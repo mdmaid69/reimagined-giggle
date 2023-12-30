b0bac1fd375121d8b8d1bde9d178ea2fb5e5aea1
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:55:06 +0000

    not interested thirsty ear fold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
