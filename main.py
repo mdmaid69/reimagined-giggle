471fed72030ae26592f385feb4028a38b35b63f7
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:14:01 +0000

    will scream thick name whom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def convert_to_hex(n):
+        return hex(n)
