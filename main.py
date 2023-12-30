77fcbb575ef55e234dd526a8501c3f53fa2d1bd3
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:48:12 +0000

    politics 564encourage both new goon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
