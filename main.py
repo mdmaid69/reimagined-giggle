a699be3ab1d177fc80bc432a72d7449393ca50a7
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:35:03 +0000

    secret statement story round turnover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
