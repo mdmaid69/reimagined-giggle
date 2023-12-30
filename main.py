da548f7c41ca88613f34fd00d49a4505563fd188
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:02:47 +0000

    another custom tragedy on shoulder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
