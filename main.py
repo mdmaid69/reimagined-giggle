24b3fea035b92c3ebec7900951b1fa82feb97196
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:21:30 +0000

    praise consult mainly neat operate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
