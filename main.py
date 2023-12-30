79528cba2bcec4632fa3513ff84be5ccca1f5960
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:48:52 +0000

    unit base quickly team carry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import platform
+def get_os_info():
+        return platform.uname()
