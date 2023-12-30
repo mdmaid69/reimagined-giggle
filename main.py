c380a278f14a611e459d57de3cadcc79dfc46202
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:32:27 +0000

    certainly share instruction getup scrap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
