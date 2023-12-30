a938fd00013e78f6e6a172f0b35a8cd219d2acef
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:21:28 +0000

    clever laugh goback(to) birthday runinto

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
