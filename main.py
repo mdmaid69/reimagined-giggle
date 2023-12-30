d30ed71e7d9083fa279cc2af5c8f633fd00c16d5
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:42:05 +0000

    over neat advanced cross payment

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+for i in range(10): print(i)
