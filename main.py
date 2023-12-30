7aa46a6ecad3bb234340e49b7d6c092fb84f6bf2
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:18:18 +0000

    great old strength remove this

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
