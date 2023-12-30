5f3d8a1c636b444e1fe0193a866b7cc29660a026
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:06:54 +0000

    decision confuse reasonable surprising hard

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
