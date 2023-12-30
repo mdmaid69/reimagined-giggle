be132bd39470dbead14c153c0c4643de26dbdc90
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:35:24 +0000

    ago water big afraid vision

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import array
-def get_bytes_from_array(array):
+def convert_array_to_bytes(array):
         return array.tobytes()
-def cube_number(x):
-        return x**3
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
