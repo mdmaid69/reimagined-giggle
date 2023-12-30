8b2d90ba20e11a9d5fa01532dcadcaf057892206
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:14:50 +0000

    contents stay evidence connected land

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
