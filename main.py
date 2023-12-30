0d6a54bbca695f888178460b4ab743e36ac67fb7
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:07:40 +0000

    wake pretend guilty cook great

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  def is_odd(n):
-        return n % 2 != 0
+def is_even(n):
+        return n % 2 == 0
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
