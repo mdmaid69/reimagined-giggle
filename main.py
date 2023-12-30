e496ce60d4b433cedfb374325016c4e46c27e5a0
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:40:28 +0000

    various light scale edge sight

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
