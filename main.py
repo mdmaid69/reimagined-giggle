fafc3d551b36b4c2bdb298a79fb328efa3dd2695
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:57:09 +0000

    account plenty Bb arrange his

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
