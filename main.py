61317e47dbbb64771d57548a975bbd05abe6f8dd
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:53:01 +0000

    expect lead winter word order

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
