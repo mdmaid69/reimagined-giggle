92bf1ea09b52ce23ff665df333d621cfb6e97a5b
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:14:44 +0000

    turnback interruption flat belong recently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
