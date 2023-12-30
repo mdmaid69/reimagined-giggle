ee719f9d928cb68e174c5222ae7272e0b8067c16
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:09:56 +0000

    basically lunch enthusiastic takeout fetch

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
