61492095274d85e078a78b279e5a470f1ea39f15
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:00:13 +0000

    he suckme fair habit crash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
