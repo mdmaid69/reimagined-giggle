dcfe88ea5761e955deb3193e6f1f25a7135e8ed3
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:08:11 +0000

    sleep among ring unable slightly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def remove_duplicates(lst):
+        return list(set(lst))
