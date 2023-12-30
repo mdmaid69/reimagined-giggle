8abe0d1818385e15ee70c2cb83d326826d9ad195
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:32:31 +0000

    load knock sound frighten sense

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def divide_numbers(x, y):
-        return x / y
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
