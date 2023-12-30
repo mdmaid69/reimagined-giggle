647d2f0c1e1133e4f2be3d083fcaaae74a1825b5
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:31:57 +0000

    love throw uncertain wave physical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
