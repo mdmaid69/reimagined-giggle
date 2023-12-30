6ca771609d1c9e70efb3e285e3050a9dca9607ce
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:19:31 +0000

    spite experiment blue period meaning

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
