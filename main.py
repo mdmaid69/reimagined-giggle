2748943ea46d7bcdbe75c9e50cc1be16eff80c00
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:48:59 +0000

    somehow cool exaggerate everywhere level

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_power(work, time):
+        return work / time
+import platform
+def get_os_info():
+        return platform.uname()
