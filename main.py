03274e17f7cc32aba992d2bd681bd655a6b1b01f
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:33:18 +0000

    stay provide suddenly block fresh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
