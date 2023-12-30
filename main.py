92b5f7f0220ffdca11a1aac6bc3c0581197f0f49
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:51:00 +0000

    top vanish ago shame friendly

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def is_even(n):
-        return n % 2 == 0
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
