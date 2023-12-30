5c292ca235453421db3ad54ce1454db46ba77080
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:56:56 +0000

    suckme lookback(on) behind almost indeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def multiply_numbers(x, y):
-        return x * y
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
