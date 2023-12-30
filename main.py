36c46614fef89772a4ff6812901e47e283d916bc
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:04:48 +0000

    thin call impressed pocket glass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
