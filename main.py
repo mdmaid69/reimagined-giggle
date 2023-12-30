a89fbe489e2f751e0a8bf16a25c2289bdf8a8328
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:21:12 +0000

    basic will firm pollution room

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
