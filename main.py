221b4bff67337fdb3c1b3a5b80e5ae08bf7599ff
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:56:20 +0000

    start have failure recommend only

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
