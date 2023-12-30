2d6009556ac05fb56cecbd4252480053382173b9
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:44:53 +0000

    illness pleasure hold stupid look

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
