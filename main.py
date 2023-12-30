8ccda6f3480d7db001bcb3f538d21d7ec2168ae2
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:01:25 +0000

    project damage free mistake proper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
