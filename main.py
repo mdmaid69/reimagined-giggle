5571f543dc9b9e4e1b5cc826846fe4dd7de476f4
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:42:47 +0000

    side card dinner throw why

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
