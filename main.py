6a7487391677039f903c248f2b4a54ea8cbab2d1
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:41:55 +0000

    later visit maintain breakout pay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
