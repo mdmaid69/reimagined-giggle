eaccedeb767555b3943410ac5e2a01cbe209ef40
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:05:25 +0000

    change surface estimate crowded carry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
