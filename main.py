10886274a094915e2488979b162c8ae09807b30d
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:46:50 +0000

    though refuse snow he obvious

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
