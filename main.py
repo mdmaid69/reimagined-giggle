d048c55a2d1d2ab0ee995bb9dee7665693675e79
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:26:32 +0000

    pay opposite we sting confusing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
