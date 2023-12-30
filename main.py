f4d1b163e0fe2af18b386a1f45731de7fa809f43
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:06:15 +0000

    world protection succeed rob away

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+text = "Hello, world!"
+print("Words:", len(text.split()))
