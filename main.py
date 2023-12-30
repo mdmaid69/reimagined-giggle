faece4db2f32daf1b56a99efaaeb9df14874689b
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:47:30 +0000

    alone crowded onto old join

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
