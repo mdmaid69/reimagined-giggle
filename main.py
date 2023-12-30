d3fb4b30892d2fc2c1d3769acd7b0d032a643e09
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:45:54 +0000

    battery discussion left door race

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
