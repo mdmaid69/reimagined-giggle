804caead0f47be0accf19a9b013b6562ce213a11
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:58:11 +0000

    collect tonight test interest they

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def add_numbers(a, b):
-        return a + b
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import sys
+def exit_program():
+        sys.exit()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
