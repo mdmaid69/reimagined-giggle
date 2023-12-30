2931ae374b8a09b9f1c67ba509600a52359ad050
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:15:40 +0000

    squeeze give Oo return hers

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def square_number(x):
+        return x**2
