3b4c59ba9e9d970a0888d62eaa1cd9f77aff5bc5
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:59:58 +0000

    keepoff flight past whistle everybody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
