1692b2056b60bbdc8e4c2695dd1ed90b592697f2
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:15:59 +0000

    direction convincing us rob keepup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+for i in range(5):
+        print(i)
