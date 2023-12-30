6d740a8f248c999b3dc1aa5cd3d9f83ba3214cde
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:56:25 +0000

    sail keepup strength smoke offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
