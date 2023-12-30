15c73932c4c1d4eceec36691fa4a099ccf086949
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:40:04 +0000

    comeround duty arrange worse wherever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
