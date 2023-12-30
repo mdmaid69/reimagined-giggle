abe0a931bc522bec06b0234bbcda580b47ca456c
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:00:05 +0000

    score gather concerned scrap thin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
