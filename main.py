8fd663ff8115583369038ad491c2d9f97ab57ea7
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:08:29 +0000

    worth now argument team turndown

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
