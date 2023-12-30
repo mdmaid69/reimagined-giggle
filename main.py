54edd9c76651cb81e2efe3222f31d142819e5080
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:26:16 +0000

    concerned statement open visit goon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
