b6b9b209f08dbf8c30e39f8bcdae65027b3a9df6
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:31:36 +0000

    meaning nature runaway fun embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def convert_to_octal(n):
-        return oct(n)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import array
+def append_to_array(array, item):
+        array.append(item)
