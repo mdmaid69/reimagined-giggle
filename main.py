89f38b7c96c20f1689bfe76f6e36c179c74b0db3
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:34:28 +0000

    brush nonsense glad message worried

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
