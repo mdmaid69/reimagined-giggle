5e9e49cfbdd7a07f7111c44de7358098e9be052e
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:09:28 +0000

    afraid hand command bright boil

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def convert_to_hex(n):
+        return hex(n)
