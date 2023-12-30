d858ccee080122be88c2f94e410b59687a6b945b
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:04:11 +0000

    miss estimate protest emphasize belief

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def convert_to_binary(n):
+        return bin(n)
