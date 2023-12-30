a541fb124f20fdb1ee57dc5a485891895facb8df
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:14:45 +0000

    sure juice project tidy treatment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def multiply_numbers(x, y):
+        return x * y
