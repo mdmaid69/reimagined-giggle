2c24a6923ae72c23c7b9b11e8b406e26eec0bdcb
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:32:24 +0000

    basis duty may rate push

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
