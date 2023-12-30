0949ea146af5c4a92c9065d4f0b5c93fb2d91704
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:55:01 +0000

    breakinto roof row tread importance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
