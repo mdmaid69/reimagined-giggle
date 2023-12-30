ede4c734e02756ee6c482912d1c6abe3d79f73b8
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:39:41 +0000

    interrupt tongue edge maximum aware

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
