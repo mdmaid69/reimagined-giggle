f16344f19e154135d24a0b8025f75cdb264a8c27
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:48:47 +0000

    activity combine continue wise scream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
