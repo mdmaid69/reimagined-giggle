fa88a677e31f0f86c69a9fe061e5e36cc6ff8506
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:50:05 +0000

    float talkoutof reaction move to

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
