0247d0cfadd0bd3c995097dfad9beeb5c5514f8f
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:54:26 +0000

    hour standard worth fall stare

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
