f42b928591646a713da3c54b13a65d1832c44787
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:10:35 +0000

    generally strength beneath soft close

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
