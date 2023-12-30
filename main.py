c8884808d5688291365d0650402a1c750d6cf7be
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:44:49 +0000

    total careful light brave dull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
