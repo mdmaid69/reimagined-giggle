d6ff05fb36c415663555da0caf33f662039f1360
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:19:36 +0000

    pour side rent bury conversation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def calculate_area(radius):
+        return 3.14 * radius * radius
