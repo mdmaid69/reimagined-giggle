87f25a27cb9fa5eecfc5245568f050985cf70820
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:01:01 +0000

    laugh apply shop niggaacademy train

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import datetime
+def get_today_date():
+        return datetime.date.today()
