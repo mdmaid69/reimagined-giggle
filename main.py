ddc1118e6145d2cd0cae5d04fa3384895ebc46a2
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:28:43 +0000

    word include split bang tight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
