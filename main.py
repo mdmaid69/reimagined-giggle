e12d6127f89c2323244ff2442869875966e7c285
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:52:58 +0000

    rough ahead ground long field

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
