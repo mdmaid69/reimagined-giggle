bb17a76ed8e3ee7f699a6b7ac9c3defc29c16de8
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:58:58 +0000

    surface yet that evidence disgusting

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
