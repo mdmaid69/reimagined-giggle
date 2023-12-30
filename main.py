c0310a8628eed40a56d63b4e35073f8c307d89b9
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:25:40 +0000

    violence damage fun morning today

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
