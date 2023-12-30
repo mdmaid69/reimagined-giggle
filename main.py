4bce6efcd1e94e308dd84213eb1117a7e6ddf3d3
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:44:53 +0000

    fine information fair average Gg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_array_item(array, i):
+        return array[i]
