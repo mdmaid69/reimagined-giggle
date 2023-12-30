7a6268eb69c39bcf25055262f94ff272848588c8
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:05:54 +0000

    address interview somewhere computer quarter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
