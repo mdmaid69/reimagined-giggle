e3dc33a742bb7db2d2f91a196322fc925385b297
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:56:19 +0000

    confuse task information season information

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import os
+def remove_directory(path):
+        os.rmdir(path)
