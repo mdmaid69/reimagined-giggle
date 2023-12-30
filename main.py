95321711640d2f8b91386a5bfddb76b35395ceb9
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:47:36 +0000

    dry brush ours piece suggestion

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def convert_to_binary(n):
-        return bin(n)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
