a9505ff754f5f5d8836314233c23e60c8b9d4588
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:47:11 +0000

    mass only teach everyone port

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import array
+def reverse_array(array):
+        array.reverse()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
