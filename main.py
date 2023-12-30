87c7b4a1d8acdbd8f61b0479390bfcbb5f6b86f5
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:47:05 +0000

    past criticize theirs deliberate chat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
