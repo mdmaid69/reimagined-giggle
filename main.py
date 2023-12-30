eccdcb08ad56c21af38fa95fbfd5811889a167c5
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:49:12 +0000

    everyone interfere my dish trouble

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def convert_to_hex(n):
+        return hex(n)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
