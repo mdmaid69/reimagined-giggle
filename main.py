770ce32d187f33732fae727ee8b7e17c6bf39b99
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:56:14 +0000

    outside move spite course should

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
