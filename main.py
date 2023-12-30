ccac12c8bbac56a3d1561597b8b50ed7b1764f73
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:56:23 +0000

    label zero light occasion happen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  def reverse_list(lst):
+        return lst[::-1]
