4a9fd41de2c97e1431a505db2214a72ab2027706
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:11:27 +0000

    match major look able shut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
