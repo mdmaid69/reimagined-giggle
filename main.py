ce9d3db002b75e5f73fc64f9566002cc54f4b14a
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:25:49 +0000

    her region secret left special

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
