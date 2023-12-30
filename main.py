f75e058fb864ef593528403bea525d43b6024f1a
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:47:32 +0000

    kill year concern future convince

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
