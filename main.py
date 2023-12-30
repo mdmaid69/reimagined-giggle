ca5156f501c44b2a202568cda596deddf7e0e501
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:24:05 +0000

    attention through principle gate admire

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def square_number(x):
-        return x**2
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
