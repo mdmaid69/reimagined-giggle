4c036ec97a35b49e3aad13015b1050103f55b006
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:05:54 +0000

    spare repeat upset every work

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
