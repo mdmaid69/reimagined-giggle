3bdbefc5da2cb2fdc4c0220365c0788ad77a8b83
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:42:04 +0000

    today head over cool block

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
 def calculate_area(radius):
         return 3.14 * radius * radius
