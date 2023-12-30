a1fee805757fcb175a545c7b5953a8b48edc00a7
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:02:55 +0000

    loan insist joke answer giveout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def get_array_length(array):
-        return len(array)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
