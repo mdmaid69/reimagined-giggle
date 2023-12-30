96c7f3baf5511206c998077714307da6418c5061
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:25:40 +0000

    cry well carry comefrom shape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
