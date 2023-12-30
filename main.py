4c7623c80d790af4bb09a68ce2852692e8ce4f10
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:22:33 +0000

    authority head major chase terrible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def remove_directory(path):
+        os.rmdir(path)
