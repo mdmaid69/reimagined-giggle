087a769b2f2c9604e9ee98707136500d215ab81d
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:33:16 +0000

    event debt fear slice chance

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
