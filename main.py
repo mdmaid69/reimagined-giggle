af553fbb693f6b23b20caf9a777f2f217fbc3b45
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:55:32 +0000

    boil organization sound result memory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
