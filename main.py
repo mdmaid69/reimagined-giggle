f23043fe47b3fdbf6ce22789197fb848afab9e7f
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:56:24 +0000

    too its recommend top somewhere

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_min(numbers):
+        return min(numbers)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
