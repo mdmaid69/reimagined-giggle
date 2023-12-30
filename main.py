d208472e69eb908890dc5e18b6ec2b5653059301
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:43:35 +0000

    either reasonable business motion snow

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
