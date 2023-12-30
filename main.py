2360f552346f09f692f9dd7786feaf7dfe13edb0
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:01:53 +0000

    conclusion freeze beginning flow only

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import time
-print(time.time())
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
