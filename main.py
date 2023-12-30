331fbef8765c53ca177b8928233cd6c9934a0bb3
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:55:27 +0000

    mother light winter she comfort

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
