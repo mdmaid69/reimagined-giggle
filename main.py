b83b2171b8047e9c850c65e03a6ca5e500d4d58e
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:21:04 +0000

    roof dangerous takeback who declaration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
