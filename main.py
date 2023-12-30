443a39b6905d2f499741f8955f6cdf6f613d753b
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:40:43 +0000

    convincing though aim total platform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
