b50f5791a9c1c39795378ad22481f1bf435ffe52
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:03:23 +0000

    sink runaway light mine punishment

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
