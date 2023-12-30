874741ad25599b4e1579e50909f84e7a3f78641d
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:52:59 +0000

    little goaway practise herself season

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
 import os
 def create_directory(path):
         os.makedirs(path, exist_ok=True)
