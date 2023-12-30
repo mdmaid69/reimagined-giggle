c5a95357a7358a74320711f6a49dd5ce3f6ceb40
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:05:04 +0000

    off mess yourself beneath nowhere

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import time
+print(time.time())
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
