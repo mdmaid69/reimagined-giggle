34e2ae45a34f612d96f9d49d956824335885fc08
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:37:04 +0000

    dick dear either involve consider

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
