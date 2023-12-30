229e8d777953cb2adf08d938fece2ac35a32c65b
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:30:36 +0000

    left temperature wise fault pencil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import sys
+def exit_program():
+        sys.exit()
