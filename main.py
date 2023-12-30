7f8f448d93c61aa57706e9f247d127b237bfb256
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:33:31 +0000

    complain dead permission back Rr

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import math
+def calculate_sine(x):
+        return math.sin(x)
