f395bef95a950c9c8d7c6b4db0f1b24a99684444
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:34:00 +0000

    ought statement Ff promise sense

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
