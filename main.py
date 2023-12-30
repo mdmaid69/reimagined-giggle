96bea7ae5871f5f752f005beed4e7f6f65e67955
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:39:20 +0000

    provide explanation official curious react

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
