93e9ea7c640c26e00de553beaa77b211bd7513e5
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:42:36 +0000

    careless peaceful tone figure science

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def find_min(numbers):
+        return min(numbers)
