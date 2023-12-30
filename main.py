7febc89cdd573f602b91189be222fa0efe24cc3c
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:13:05 +0000

    order hole maybe effort fix

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
