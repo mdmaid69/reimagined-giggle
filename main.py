30f2058d60855af19351a21b262649765d562a45
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:21:01 +0000

    freedom fond cruelty work law

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
