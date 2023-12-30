7bcc30326d39437b0e880d7255ff844fd2164d05
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:24:44 +0000

    among while but will herself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_error_function(x):
+        return math.erf(x)
