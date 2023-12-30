812ea473f9bec84fc451a529de7322218968c6a5
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:26:59 +0000

    book occasion slowly criticism something

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_str(array):
-        return str(array)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
