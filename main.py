60cb4b6425ef22f3a2c1b36c28d05701210633f1
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:09:17 +0000

    start fasten fasten improvement Hh

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
