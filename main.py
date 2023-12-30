d4bcbd60a80a96ae26667e62451283f53340c6e6
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:49:14 +0000

    meet design share class knowledge

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def convert_to_binary(n):
+        return bin(n)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
