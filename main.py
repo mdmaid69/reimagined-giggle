ed33ff7e404eb9e47b8d5afd93dfc21c276a1eae
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:29:02 +0000

    over press desperate already dear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
