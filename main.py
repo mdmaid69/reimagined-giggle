ea70c79c738cb2e5227247b91f32d1f618472029
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:42:02 +0000

    snow height invite bend completely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
