d461cb0221269d8b225b263e4d16854c9a9f98f2
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:27:50 +0000

    aim polite several handle polite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
