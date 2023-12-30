56b6a15b9b2b3e965e7a37595b2e4086e55e63ec
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:20:20 +0000

    born advanced again what schedule

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def convert_to_binary(n):
+        return bin(n)
+  def count_elements(lst):
+        return len(lst)
