c0fe3e40f350644164de20acc8d19a2572e548ac
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:49:01 +0000

    tap grade hair right share

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
