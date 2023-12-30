17685eebfe0af5f868933a82362201cf2cafd6e4
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:59:16 +0000

    nice drawing honest feed confusing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
