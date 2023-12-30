2fe8dbb5c4cf76b80227fc108458990d569083e0
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:00:25 +0000

    about brave clothes now object

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
