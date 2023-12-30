1c4e917976e58d69ee45ef611c90e47e04e3f8ba
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:40:12 +0000

    reply news consult talkback do

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
