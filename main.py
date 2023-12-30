5817c65ddba12552f842d2d2a09d50582b67b490
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:52:32 +0000

    satisfied born necessary feature top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
