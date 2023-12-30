30b58d69a8e8c2ac246d6d7f5cfafb94eea6ccf5
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:02:06 +0000

    disapprove walk trap limit this

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+print(sum(range(10)))
