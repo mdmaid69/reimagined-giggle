ba45d7b7e96ec2f24b2d60b3d7e56b8c707c2634
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:51:17 +0000

    prepare trust Tt provide badly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
