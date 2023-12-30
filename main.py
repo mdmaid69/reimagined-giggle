baecb9f76ec498e1b610cecdb64056061466381e
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:44:02 +0000

    skin broken try dry disgusting

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
