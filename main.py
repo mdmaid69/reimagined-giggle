67256bccf57df923f63126fcd20b97d7524318d0
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:58:49 +0000

    concentrate trousers education whistle excited

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
