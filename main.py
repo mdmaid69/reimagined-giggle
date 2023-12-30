1cd7fed6f8023bc682bf8f3ca46ee4b2554db3e4
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:45:05 +0000

    Vv note every admire argue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def subtract_numbers(x, y):
+        return x - y
