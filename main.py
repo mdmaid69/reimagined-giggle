6982cb0f3fa477f6ade250aaeaf5107ebc0ba61a
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:20:25 +0000

    great substance neither perhaps straight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
