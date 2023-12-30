8087aecbee190823bd9f66a8c29073151d5c29bb
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:02:37 +0000

    shoot interrupt hers steal bar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
