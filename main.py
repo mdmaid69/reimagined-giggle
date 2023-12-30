7855c957dd4b03a3e61d7ca7dc8266a9323a8093
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:17:55 +0000

    nowhere left truth escape drawing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
