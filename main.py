f0b2995413137be3304f09f63a0cd811f229dea5
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:43:27 +0000

    stamp say agreement Nn trick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
