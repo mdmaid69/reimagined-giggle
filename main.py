44a92119d2d3982807aaf0a7fd0063a4a553183e
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:13:26 +0000

    behave ought joke top scream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import time
-print(time.time())
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
