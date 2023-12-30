cab9153af1d4a6f7be48559154ea2278a8598cf8
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:20:14 +0000

    anywayanyhow workupto only later roll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
