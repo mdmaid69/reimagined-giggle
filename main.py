62a1710672fcb560433e08e8f61dba1ad9922321
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:18:34 +0000

    print behind worried hit short

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
