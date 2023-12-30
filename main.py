e311c50ed9206bb67644337ef50a6a737a0ddfc8
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:06:13 +0000

    Ii clean advanced remain callup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import json
+def convert_to_json(data):
+        return json.dumps(data)
