95716cafa4f7169aed31badd05943953e9415d43
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:24:39 +0000

    good out draw turnup workon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
