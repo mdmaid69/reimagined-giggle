9baa9cc0b820baee9cee9e62542c6b24af6b9cb7
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:50:57 +0000

    smile before below concerned impressed

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
