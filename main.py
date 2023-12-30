ddc2a1ab76b0aa40e8238f67c38f77ba32e1097f
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:03:00 +0000

    persuade television light land cap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
