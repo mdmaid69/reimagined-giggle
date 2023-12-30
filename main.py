e7b803b615211152f783cd28509a2a2029b79d02
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:13:01 +0000

    unpleasant punishment yours difficult ear

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
