45e876b8d9a271455c8c5043d19cf16a4d41b8d3
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:50:42 +0000

    shop skirt prefer plant judge

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
