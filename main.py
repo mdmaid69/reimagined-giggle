d8e7efa01893b9ed979b0e7114a9a6a5d32f8ee5
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:41:28 +0000

    near tip male achieve shop

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+for i in range(10): print(i)
