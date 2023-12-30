74a396250c6b8e684f7fc018d7672e3d6cdb92a5
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:08:57 +0000

    show later translate become bit

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import math
+def calculate_tangent(x):
+        return math.tan(x)
