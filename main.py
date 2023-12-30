10654753dd5a96a91fd8d5af60310bdd265e1989
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:34:23 +0000

    range breakthrough home experience interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
