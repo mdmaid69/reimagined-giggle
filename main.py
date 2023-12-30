51e1f1169a4da6b51f9e27037b06746ac7b9985b
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:08:12 +0000

    imagine under front front Ww

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
