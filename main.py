513187e253cc501fa5a8f26f0b46ce26634848fb
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:03:26 +0000

    rubbish basically ilovedicks offend always

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-print([x**2 for x in range(10)])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
