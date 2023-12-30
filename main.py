7973f9d6b0e9aaed1b3aa50933f0b00f9e6a9cbf
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:15:29 +0000

    blue connect children pass record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
