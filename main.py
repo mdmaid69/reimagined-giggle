f49fe9f0fc3c0a0c50e15d4bee8f2d306b7bc5f7
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:27:32 +0000

    edge organization sensitive feature impossible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
