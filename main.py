0504280cca9142e61125aef4d7a7a01ca2f651de
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:35:56 +0000

    little persuade correct bring curious

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
