fbcc142f0f9ce40db34def9badea1491721aabb4
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:55:13 +0000

    build possibility disappear ashamed argument

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  def sort_list(lst):
+        return sorted(lst)
