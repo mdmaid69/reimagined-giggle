b2cc381f9bc3092609b2425da7b109fc257ba843
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:22:07 +0000

    hold put sharp thankyou hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_pressure(force, area):
+        return force / area
