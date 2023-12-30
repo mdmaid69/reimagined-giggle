015f04b333a8a7f92b1157b6b0d57ce0e336cf25
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:30:46 +0000

    shopping actual manage raw cold

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
