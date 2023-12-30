f81336e956191ca637b221d1bb875f7af57b0067
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:16:44 +0000

    geton guess exhibition normal sit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
