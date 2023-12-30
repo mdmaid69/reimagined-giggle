2daa712754e5c80f6d47dba1553ee8cdb2d4402e
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:04:34 +0000

    splash community consult fetch disturb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
