15f1bf1491ce55aad10bb480fb6b298c2ce8603f
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:27:48 +0000

    go survive after between form

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
