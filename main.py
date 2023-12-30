2b1cebc9dd2dd88c42f8bed8ed1959e876203856
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:43:00 +0000

    unfortunate important do send form

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def convert_to_octal(n):
+        return oct(n)
