52ce207e8ffb44f07dca794153efbe9df0b793d5
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:07:03 +0000

    sensitive cost perhaps anybody feel

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-print(sum(range(10)))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
