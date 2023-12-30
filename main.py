7371c142473977d62f962499434a409326aeaa3f
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:57:46 +0000

    sweep use drive direct among

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def greet(name):
+        print(f"Hello, {name}!")
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
