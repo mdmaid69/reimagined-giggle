1488f37386fb7d3dcf2f220aba0a04657d71b572
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:42:18 +0000

    stage challenge crime judge approve

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+text = "Hello, world!"
+print("Characters:", len(text))
+  def is_even(n):
+        return n % 2 == 0
