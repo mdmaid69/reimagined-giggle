3b2ae91da8dff38ec6db790e041d3fde921e4a4b
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:24:24 +0000

    trouble swim trip concern firm

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
