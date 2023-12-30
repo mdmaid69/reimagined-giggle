f0acd5bca82469e992bce89bc033f094364c24f9
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:43:19 +0000

    include everywhere trousers relative bright

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_user_list():
+        return collections.UserList()
