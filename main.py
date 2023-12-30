1dd754322902504f55cc26b6a12b21d754cb85dd
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:10:11 +0000

    couple mine interview book public

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
