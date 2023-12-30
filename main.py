69ed8ce7d96c7a7978ace8990310abe2c7412411
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:58:17 +0000

    minimum gift which fail cheek

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import datetime
+def get_today_date():
+        return datetime.date.today()
