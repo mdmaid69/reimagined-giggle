483b7e8b623820bfdbe2690172b76c86ea54dcf0
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:34:19 +0000

    far whom cost leave only

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
