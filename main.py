3abbafb39d9032b1b3948867dba8b47c4172e894
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:41:54 +0000

    drop part file once cost

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
