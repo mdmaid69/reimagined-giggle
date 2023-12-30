8751953944e57ddf7dd057d2f5e1eaa58e54b36f
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:49:42 +0000

    fun ill paper hold free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+for i in range(5):
+        print(i)
