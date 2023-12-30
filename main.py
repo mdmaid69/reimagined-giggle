96fab4d7fedfa84a422b737372fff092fc9dcac3
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:50:04 +0000

    treatment many thank include personal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def find_min(lst):
+        return min(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
