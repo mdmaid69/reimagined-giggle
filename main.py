23f6e02baa68fcfbf5881a1889e0d6d5d43a74b0
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:23:55 +0000

    relation regularly system contribute attention

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
