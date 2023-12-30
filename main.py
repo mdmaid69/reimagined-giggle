7ab5ad861f761377dbd2ef07ee4b550847b2ac1e
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:50:33 +0000

    shut music about rely be

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def multiply_numbers(x, y):
-        return x * y
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
