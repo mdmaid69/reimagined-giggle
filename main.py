a53206e9852e38ef8451a4634b34ebbee9da3282
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:25:59 +0000

    loud computer ahead lie smile

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import collections
+def create_stack():
+        return collections.deque()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
