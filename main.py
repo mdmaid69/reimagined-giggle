82554c37bd02efc7f18531ca4049d0f904180042
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:07:51 +0000

    roll frighten age pull murder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  def convert_to_octal(n):
+        return oct(n)
