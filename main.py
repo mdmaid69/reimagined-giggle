2f9140fc38181536bd56e719256b191d0f92d1b4
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:07:14 +0000

    complain tight trade minute guess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
