571193314bc220d1aac5b3546c950fb36fa5f222
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:35:41 +0000

    sweep yourself change slightly Jj

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import datetime
-def get_current_date():
-        return datetime.date.today()
-def greet(name):
-        print(f"Hello, {name}!")
+def get_current_datetime():
+        return datetime.datetime.now()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
