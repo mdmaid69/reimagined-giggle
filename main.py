b0df9e33888926ad921e14e5b2bf21af812a873e
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:00:12 +0000

    live street boring nowhere lost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_counter():
+        return collections.Counter()
