37ce505960353c60bbcf099a6d426a5640640d7b
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:29:28 +0000

    poor education so careful declare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import array
+def append_to_array(array, item):
+        array.append(item)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
