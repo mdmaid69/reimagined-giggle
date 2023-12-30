9db408443a0dbca5a1aa9204947408ca858adee0
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:26:25 +0000

    possibly over explain earth minimum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
