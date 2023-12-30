ed41c32209d84ddccbd40396fca26aa0cc5431f0
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:04:02 +0000

    movement underneath interfere pull rush

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import array
-def get_array_as_float(array):
-        return float(array[0])
+print("Hello, world!")
+  import os
+  def get_current_directory():
+        return os.getcwd()
