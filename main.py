2ec435ee90376e69f524cb9b5b2315715ae96b9c
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:33:32 +0000

    weather limit getoff means scale

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import sys
-def exit_program():
-        sys.exit()
+import os
+print(os.getcwd())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
