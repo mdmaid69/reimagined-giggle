1f68d04c1c9ab39f6e7b495eff14695a0ed46ec5
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:42:22 +0000

    fun minute circle godown innocent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
