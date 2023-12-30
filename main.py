4084de641029a11c16ba7ad8e51a57bdd74cb3b2
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:11:31 +0000

    detail impossible flow force Hh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
