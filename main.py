6f7fd7cc4039f62e334ccb2c8b4b87f5e73601e3
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:12:16 +0000

    unpleasant turnon deep replace public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
