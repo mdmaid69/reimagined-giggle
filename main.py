3701691ac15b9bcc24261eaaa1f3f6d1b62a94bb
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:40:31 +0000

    behaviour cheek sticky behind turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
