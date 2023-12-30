35173b3ae4c8ed5940d7bd0b3b227bed1e91da69
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:43:47 +0000

    true area shelter arrangement fit

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
