6e392fdf5c66649cdd15f3bd1d2250d597e70e57
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:20:31 +0000

    rarely until correct shoulder laugh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
