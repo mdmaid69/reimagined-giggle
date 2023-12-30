3e4590de52a5cafa0b4c09516581a7e6c581ee5b
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:50:19 +0000

    unless comeover description scene left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import os
+def get_current_working_directory():
+        return os.getcwd()
