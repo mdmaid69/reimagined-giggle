0e9c0b51fce0d2b35ef2b8745d044826de5e2a36
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:19:09 +0000

    education copy direct regular usual

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
