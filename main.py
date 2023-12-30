d205a2857ce30ab1fd80ce41e4ea3284788563c8
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:36:00 +0000

    dress father his price listenfor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
