b25d90c4bbe5471bf2a7450fb9a4999cd89614f7
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:23:41 +0000

    offer grand gas sign shout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def calculate_pressure(force, area):
-        return force / area
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
