c30cdc7c198abee4d3fbf7339e7b4f00721f0cdc
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:47:08 +0000

    glass dinner image music flat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def add_numbers(x, y):
+        return x + y
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
