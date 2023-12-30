986118256f37851362a0338b7a799b8edb6b7aa6
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:23:23 +0000

    train surprised point left space

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
