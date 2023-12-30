bb82f85faf517b5de28cffa896fd0756749052f1
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:43:00 +0000

    sea stamp swallow characteristic geton

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import logging
-def log_message(message):
-        logging.info(message)
+import array
+def get_array_as_str(array):
+        return str(array)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
