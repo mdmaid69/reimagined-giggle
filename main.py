234593965bd433eea19de016cc6fe57ba8814b21
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:51:26 +0000

    cross post force project combine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
