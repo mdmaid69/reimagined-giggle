086643323cb2191eb0d6047268d2ca0f1c340d19
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:59:32 +0000

    one pay deliberately why raw

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
