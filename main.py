77d5ca3f144631424d467fc786bdd3f41dfe2d87
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:14:25 +0000

    note spring sensible hurt tend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
