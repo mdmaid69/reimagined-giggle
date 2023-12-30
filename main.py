8cfeed1b4d56af0778de566aa84b19cf61019318
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:42:37 +0000

    cry claim physical sea about

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_str(array):
+        return str(array)
