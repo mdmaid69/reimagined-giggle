32414ec020bb1e6b4120e9ef749e7698b151dc78
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:14:24 +0000

    grade idea away question finish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
