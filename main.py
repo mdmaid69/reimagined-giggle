dce8518497a3cee5c6f4585279abe0f42822aa07
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:36:04 +0000

    Oo race principle frighten either

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
