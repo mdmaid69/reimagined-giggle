80a45a6d7a2caaa9a02ee3599366d4a729f965a7
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:03:08 +0000

    runover surprising circle under side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
