9f5f9255a38dfc2fbeb9b5ad8379f662f9b293ca
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:28:36 +0000

    either lucky fly operate ago

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
