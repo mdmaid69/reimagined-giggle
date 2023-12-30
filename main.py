43045756ca764cf3d851b5d17170cf5abf453d5f
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:54:29 +0000

    bite moral stir tragedy contribute

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
