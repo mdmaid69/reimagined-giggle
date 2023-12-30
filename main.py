2cacddecdafe0c83c9dfb16067dda7c61b6ecb86
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:15:26 +0000

    then Qq wrap turnon yours

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import collections
+def create_user_string():
+        return collections.UserString()
