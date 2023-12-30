d1ad4af4ca789bd2e59b48aedd173b9670cdd056
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:59:54 +0000

    cheque contain week through if

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_pressure(force, area):
+        return force / area
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
