4acfbc5f756a1aabcb5061461c4c9817e0ad3abe
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:06:18 +0000

    gas book still study impressed

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+name = "Python"
+print("Hello,", name)
