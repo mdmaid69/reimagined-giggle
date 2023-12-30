b80d613888b7d606ecd1d364fccbc77a135a6910
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:53:22 +0000

    unhappy match software fat bring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_list(array):
+        return list(array)
