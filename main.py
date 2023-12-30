77869342c66aae9d9a73e936ae2c04ff180c8f48
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:55:35 +0000

    fork skin we chance pull

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cosine(x):
+        return math.cos(x)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
