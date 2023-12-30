1d27a15dfa8bf77489c8eb3984a831adb93db779
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:05:28 +0000

    attract gift must health light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
