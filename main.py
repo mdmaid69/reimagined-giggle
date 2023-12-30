0ff87ec2656e1fe37e9c2ac9d11b20e6f6f7860f
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:45:19 +0000

    maybe yours style separate around

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
