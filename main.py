2021321022bad7e6015f88139a2fcb8ff5a0d7cb
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:29:46 +0000

    rarely do less easy send

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
