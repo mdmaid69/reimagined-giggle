5553bbe694d60fec85c139a65331a445ceb80061
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:56:18 +0000

    wherever talkinto funny name inform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
