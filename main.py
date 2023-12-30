32244e2cf3fc40134ae41cd88dd52bc26c053039
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:06:00 +0000

    mass score Jj guess goup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
