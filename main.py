1d9debd85132fa441f0af79bb61bcf201f89eb6e
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:05:06 +0000

    town sound idea pressure embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
