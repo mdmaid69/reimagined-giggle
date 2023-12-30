ac4124debab5385de48f824ef7dbfb8e3f65996d
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:00:32 +0000

    before your excuse injury trick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_item(array, i):
+        return array[i]
