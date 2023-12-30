81565577e5f3779437d026ccd0380ffca5403228
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:55:50 +0000

    increase surprising dear city by

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
