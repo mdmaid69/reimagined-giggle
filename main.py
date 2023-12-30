1dbfc51082b8f50f8934b60ef0c2e3321f958cf4
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:03:07 +0000

    yesterday control beat under prepare

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+i = 0
+while i < 5:
+        print(i)
+        i += 1
