5e500ba3ae4f65551d6303f209e33186b969d4e9
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:57:08 +0000

    loan tune ability there relationship

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
