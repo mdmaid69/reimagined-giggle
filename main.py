0a4b104fa5d02d1364f3fa69c21ba23af98f72a5
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:25:21 +0000

    off Tt wave school strength

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
