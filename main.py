d197d0df62fd615192e04ff83243e84ac8ad5bfd
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:28:32 +0000

    excuse satisfied else grateful sure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+print(os.getcwd())
