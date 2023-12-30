a8d936081b3db46493c0219366f1db9107db8094
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:50:08 +0000

    sensitive discussion news bin correct

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def reverse_list(lst):
+        return lst[::-1]
+import os
+print(os.getcwd())
