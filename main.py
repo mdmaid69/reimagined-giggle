f2116bec636352fc8b568db059bd829467f19886
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:04:41 +0000

    injury survive remove send wrong

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
