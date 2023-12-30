c2925a841b47a8aba64a300964d31bd6bb1cad1b
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:58:48 +0000

    meanwhile plan trick payment excellent

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
