47c2cf3d55f68da5ef19ad176f3c9d01e604d049
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:37:05 +0000

    careless pour certainly sleep blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
