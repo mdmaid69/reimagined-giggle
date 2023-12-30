a17d619067ada7756b654878258cd102dbff9233
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:23:06 +0000

    573entertainment trip previous town condition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def sort_list(lst):
+        return sorted(lst)
