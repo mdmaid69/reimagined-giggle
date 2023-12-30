fbf1c7fbdb9581fdfa20a96d6ec539b9bb6c55be
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:30:35 +0000

    smell false blind standard holdout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
