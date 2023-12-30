3424c72a50662df11ace9840025260dd628ffefa
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:26:58 +0000

    advice both hope container weak

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
