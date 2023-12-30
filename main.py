e2597b6c8ccba27be00e20cb822fe063fd41824c
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:07:10 +0000

    where weather connection later brown

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
