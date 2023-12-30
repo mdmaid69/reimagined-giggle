a299538125f19e334d06a60a47b960ec54199df4
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:03:24 +0000

    news ought across official vote

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import re
-def split_string(pattern, string):
+def split_by_pattern(pattern, string):
         return re.split(pattern, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
