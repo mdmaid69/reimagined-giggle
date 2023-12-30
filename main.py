bf016dd4cc6861ebe25d19a2b181b7fc936cf6a9
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:20:47 +0000

    offer breakout somebody passage pronounce

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
