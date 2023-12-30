ffa730fb3f47daa3e15113c380d37f9de9394a38
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:14:01 +0000

    city wide dream able meaning

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def count_elements(lst):
+        return len(lst)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
