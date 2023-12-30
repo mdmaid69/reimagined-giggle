6ecf8e662e5273987e946d931e544f8fee0bcbfc
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:38:50 +0000

    set born burn sure correct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
