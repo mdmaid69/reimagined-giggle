3b6d7bd6ea69f77f06143bb6c69ce599f8bb11dc
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:42:23 +0000

    suit bang head measure pretend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+print(math.pi)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
