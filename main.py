5006c23197c27565652b493b3cc6633976e51b42
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:45:22 +0000

    frightened duty bin ours frequent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_floor(x):
+        return math.floor(x)
