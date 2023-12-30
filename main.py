4ae919c211ab2b8294e03b31f0041d2e495848a6
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:02:52 +0000

    exciting age contact comedown worse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
