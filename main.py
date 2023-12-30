23e655729101835e6e651afafb82f7c0a5c4a130
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:36:44 +0000

    there goup race under across

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
