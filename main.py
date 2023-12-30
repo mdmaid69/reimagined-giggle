e2e42ea4ec1a7af9bff743ee9560b99b090496e1
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:16:09 +0000

    commit minor water further possession

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
