c2fbe4e14a6c647fff7cb879289c742262dd176a
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:15:17 +0000

    sound his sky Rr live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
