20ddc30be39051be154dd447ff6db1181af8f361
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:56:58 +0000

    ago land most himself neither

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
