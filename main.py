d74c70b9588692a85f711187f78701977216302c
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:08:40 +0000

    569enjoy direction sweep direction left

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import time
 def get_current_time():
         return time.ctime()
