d243e30046bc7146d76e738048952cfcd7c14504
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:32:50 +0000

    injury prove lost tap type

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_string_from_array(array):
+        return array.tobytes()
