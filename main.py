81ed14eb038dbeef165151359eb6d0e718d02a7c
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:44:48 +0000

    space react convince callat believe

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
