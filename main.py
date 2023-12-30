885399947e4a22eb7e33df415161cd2ed015fd9a
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:12:52 +0000

    happy commit plenty children gas

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
