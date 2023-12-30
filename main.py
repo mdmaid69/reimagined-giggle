62da946c14b9c721b35cfe4807cbd3a2cf6558b9
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:17:47 +0000

    high on problem upstairs goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import random
+def roll_die():
+        return random.randint(1, 6)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
