edc4c77adc77e8fb340b19fa95144a5cc9e9e5f4
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:43:25 +0000

    opponent detail cinema message deal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
