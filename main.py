529f709f92a045e6013a5574e4704a069a80c2ba
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:06:50 +0000

    comfort if knowledge after past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
