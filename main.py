8bcbd48ac44e05f34f26177d548bd9f7dd29bf7d
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:15:01 +0000

    breakinto sell proof Rr attitude

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_perpetuity(payment, rate):
+        return payment / rate
 import array
-def get_array_as_str(array):
-        return str(array)
+def convert_array_to_bytes(array):
+        return array.tobytes()
