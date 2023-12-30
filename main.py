263db6045b3c7be44c18e1eac7038e2289e2cc06
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:55:37 +0000

    drink month vanish run view

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
