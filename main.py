d7e5be9bb6f10be13fe5f260d16356412d04555f
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:50:13 +0000

    health convinced Jj minor live

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
