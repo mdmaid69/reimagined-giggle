e756b807052abbc0377bec247ad31b829592e203
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:52:55 +0000

    kind so everywhere point prevent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
