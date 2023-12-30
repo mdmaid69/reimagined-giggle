58fcd90e67d7b71e8df988c7505fdd64dbf224a0
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:20:33 +0000

    shop mind likely region concentrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_index(array, item):
+        return array.index(item)
