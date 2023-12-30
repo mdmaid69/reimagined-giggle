691475da84e2f54a50e89b6c9871843a865e0d52
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:32:39 +0000

    lookaround distant fat thinkover of

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
