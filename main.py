e9f48118bf03371102dcbf5372fc525924e8c2f2
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:13:36 +0000

    radio neither disturbing convincing conduct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
