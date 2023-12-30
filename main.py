21de8db27b2101e1bf396a824e4183d1b8061c23
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:36:56 +0000

    spin rob impression impossible seem

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-print("Hello, world!")
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
