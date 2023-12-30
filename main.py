fdd1f04f4e313bdf6a9b5e151fbb4cfa5f31c784
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:12:48 +0000

    deserve young one already on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
   def get_file_extension(file_name):
         return os.path.splitext(file_name)[1]
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
