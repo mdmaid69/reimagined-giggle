0682e19a802d6b4ffba217aacfa7e169a050e507
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:18:33 +0000

    coffee operate talkoutof object rely

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_pressure(force, area):
+        return force / area
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
