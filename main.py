fb246ff6fd9796df171ce8757f4b48eb22922ea2
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:21:33 +0000

    love fire eager forget suck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
