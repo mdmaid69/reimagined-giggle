aaa5cc362cdd5f2793cbeee99cfd73b1b86701ea
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:54:36 +0000

    need this in live none

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
