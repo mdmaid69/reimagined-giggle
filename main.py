89779e15c34cbff7772cd3b2acb3fedfa056175b
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:31:28 +0000

    later fine open little violence

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
