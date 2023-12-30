d904119aa1699f528bbacd65c5b7582a1c9288cc
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:41:12 +0000

    separate quick transparent show instead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
