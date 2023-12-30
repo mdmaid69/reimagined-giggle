fb5a033f853cd65bde3627172a3296e3ba391cce
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:57:49 +0000

    tie rent shrink range manage

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  def sort_list(lst):
+        return sorted(lst)
