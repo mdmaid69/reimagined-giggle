d26a85ac22b264f3d0d7e7825e66c0bacd8c6ae6
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:36:23 +0000

    float enthusiastic care female Tt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
