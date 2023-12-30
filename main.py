ed11183a485f5fc44217ad3001c90daf0df9f338
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:25:31 +0000

    quantity whole basically blind against

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
