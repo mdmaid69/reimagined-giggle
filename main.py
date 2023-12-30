09a99b65f35a000468b14a5b51f1b722d0f861dd
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:00:00 +0000

    give definite platform full whole

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
