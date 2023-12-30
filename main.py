336c7b9ab9d0144d3eb96182379c52070cfdee41
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:16:53 +0000

    my wear stick fact make

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
