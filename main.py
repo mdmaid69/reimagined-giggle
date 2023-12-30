c24cc4a6d247bff289b598068499bee64b4bc3d5
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:58:41 +0000

    priority middle telephone quantity can

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
