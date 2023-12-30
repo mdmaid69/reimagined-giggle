e3262bc15e673384ed142351d49199cb0d6a7d31
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:14:59 +0000

    fresh push body slowly Vv

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import random
-print(random.randint(0, 100))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def get_array_as_str(array):
+        return str(array)
