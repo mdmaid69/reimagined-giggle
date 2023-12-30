a3af0df82ba377447f7bd9d0c42d52b2a72d2635
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:38:03 +0000

    couple breakup manner 568energy branch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_string_from_array(array):
+        return array.tobytes()
 import os
 def get_file_creation_time(filename):
         return os.path.getctime(filename)
