309e4082e2c9e55ca0f0bc4b2a9ad7b038efaf8d
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:42:16 +0000

    twist dish even call aim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
