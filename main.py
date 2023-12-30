684d5e623d27658318fd46837555f32cba763b25
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:25:20 +0000

    anyone arrangement never early entrance

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
