322bea827b5c35af244a0dd58b2b12f077cd3e99
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:21:48 +0000

    offence hand count bringforward delight

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
