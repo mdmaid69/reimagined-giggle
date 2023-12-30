41294ab2e3aa93a2b2ad3a603f6beb995e2b0fa7
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:37:12 +0000

    dinner nature bringback prefer anywhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
