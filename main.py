e21adc6eb2c6fbc1047a9c84371798bcd73be08e
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:27:46 +0000

    advice hurt single reach ring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  def is_even(n):
+        return n % 2 == 0
