e728fc9328f629d940327e7a94f9d6b975a231cb
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:16:19 +0000

    squash happy platform subject test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-print(time.time())
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
