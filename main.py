bde67c1a8ddc0892bac77ea773bfa553eaf7fd89
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:19:05 +0000

    praise action slice weather stand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
