e9bbfec6554aa78cbfd5cde26cf83f3a85ab1c2e
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:23:17 +0000

    waste official attractive flat pleased

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
