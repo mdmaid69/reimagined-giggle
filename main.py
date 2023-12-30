6dd30860d5475f989010626dd321b5aa93a96b38
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:30:58 +0000

    translate post over rare dangerous

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def calculate_power(work, time):
-        return work / time
+import array
+def append_to_array(array, item):
+        array.append(item)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
