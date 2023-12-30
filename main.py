2f92d3b06578966f54af4c970f6963f1de78aa98
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:40:56 +0000

    relax regard name statement possibility

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def find_min(lst):
+        return min(lst)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
