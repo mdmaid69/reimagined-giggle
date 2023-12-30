47e89558888a8bc0cd60ad720e5549068538997b
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:49:13 +0000

    danger extra degree process post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
