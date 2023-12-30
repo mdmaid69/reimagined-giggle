01016045750ca52cafd08be6b1c80c024a114d2f
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:49:38 +0000

    mood many just steal married

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def square_number(x):
+        return x**2
+import random
+print(random.randint(0, 100))
