313009d07ee360ca1ae07b39f73e96aa6dcf7776
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:56:28 +0000

    length tap aim crash interfere

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
