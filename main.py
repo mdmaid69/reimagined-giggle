84c2f2e563bc58498d1cf831d6c5ef7f363cbe39
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:06:37 +0000

    feed event word stop hope

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
