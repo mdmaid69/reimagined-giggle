24e26c1d4b639b47397d6342122ae5a8b3f6717e
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:32:48 +0000

    drive forgive food call set

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
