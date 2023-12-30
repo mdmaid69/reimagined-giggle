2265fbe15a4c2bdfa90e44dc0d67272f54e0bc45
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:12:37 +0000

    wake Pp brave tidy vote

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
