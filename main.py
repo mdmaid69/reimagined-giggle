871e748014edd3477fb0d59c838d859a2445e802
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:40:22 +0000

    regular hurry dream even agree

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
