07e60d953261073d7aecfe9fe513c98005675cb9
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:24:11 +0000

    idea private train ball apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
