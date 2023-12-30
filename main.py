9e4e4188a0d1d69d38885dc51791bcf949c80efa
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:05:51 +0000

    her over price message catch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
