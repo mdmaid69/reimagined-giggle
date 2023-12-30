c47cc8cce7eb119069eea9d7c3b0f2c2b74351a0
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:50:44 +0000

    surprise sale ask practical become

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
