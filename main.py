df178d3ab4b5927ed1c91a682db741f5c5f84f6c
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:55:04 +0000

    runinto married vote tidy port

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
