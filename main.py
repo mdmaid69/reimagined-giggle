3d3cfb427acfc6c80a089dfeba5818e358deeefc
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:15:08 +0000

    leg win try worry 567enemy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
