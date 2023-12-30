995fbce41e200ef18b1551874971573590a252d2
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:37:25 +0000

    second bottom reaction plastic slightly

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
