fcd37c6d9ffb23d5185ce7dbc34793e8a1d0e49f
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:53:19 +0000

    woman poor sound phone behave

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
