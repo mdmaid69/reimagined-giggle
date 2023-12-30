4caf26c014257c5c6161737f22a8200f371d7686
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:09:53 +0000

    hard obtain reliable force fail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_dev(file_name):
+  def get_file_device(file_name):
         return os.stat(file_name).st_dev
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
