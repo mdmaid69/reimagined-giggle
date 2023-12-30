294ff3d21822789485c69c9d970b43a7e8bbfb79
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:18:34 +0000

    natural board put makeup below

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
