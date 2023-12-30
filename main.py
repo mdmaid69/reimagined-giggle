20e23bf016c9893561824f746e1dc60124e8f8f2
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:27:58 +0000

    spring attract discussion release benefit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def divide_numbers(x, y):
-        return x / y
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
