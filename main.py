5bff5e4e577c8f4ab07161c30e4072b9b238f0ce
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:09:42 +0000

    detail takeback marry drive friendly

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import random
+print(random.randint(0, 100))
+def reverse_list(lst):
+        return lst[::-1]
