da14d913997d9bc33b750bfc1b51f4f6e5d2b108
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:50:25 +0000

    innocent round trust thanks seem

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
