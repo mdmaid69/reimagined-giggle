18579edfcc8538eead8eca7c83413e76e04df190
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:30:36 +0000

    born mouth gentle sit illness

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
