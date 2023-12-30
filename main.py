941b460a305e4cc360d5f7283afee09523bc8b1a
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:16:01 +0000

    account matter interest tune cover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def count_characters(sentence):
-        return len(sentence)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import time
+print(time.time())
