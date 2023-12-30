5fe08e65ac744f0d0dddad508a550a8816b04ebd
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:27:48 +0000

    holdout anybody straight study club

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+print("Hello, world!")
