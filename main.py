3294c6b67edb4ac39ac1a3e9162bead66c76797b
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:14:35 +0000

    whichever emphasis priority gate dress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
