97ef869fd0a670525d4cd50e3b316b635a707d98
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:15:44 +0000

    burst rough software better watch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
