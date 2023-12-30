22ae5c1ed6c087a3c7ebaa9145585f5e4220fd2f
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:59:45 +0000

    hate later ocean breakthrough join

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
