ddcc4b8c6da77a91a43370b7f1d9fdbe7702c3ff
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:10:35 +0000

    ball true whatever meeting flow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
