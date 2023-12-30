ed7b55a720253a2c8dea99d866d4cda2a5959217
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:23:33 +0000

    clear pronunciation comedown feeling lost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
