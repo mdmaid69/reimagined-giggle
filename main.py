cc7266630402097a1f5ad0fbea5ae73a339f54c2
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:13:38 +0000

    modern injury tread impossible capital

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-print([x**2 for x in range(10)])
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import array
+def append_to_array(array, item):
+        array.append(item)
