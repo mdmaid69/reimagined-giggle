2ec5c318b825491e9cdad73719ac1e6ee34b3a18
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:37:43 +0000

    minimum burn opinion sting error

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
