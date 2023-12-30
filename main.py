5a5a182f4408a680399305445b4b35aa6de605a2
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:23:57 +0000

    boat public history sort centre

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
