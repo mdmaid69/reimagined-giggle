ae607b9064ddff4f84198c2c202515fa2b002518
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:02:20 +0000

    something payment high dish explanation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
