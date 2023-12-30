ab1193e0cac05b2033cafa9f088aea8cc37bce23
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:04:41 +0000

    takeoff kind wish blue official

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
