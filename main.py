dd712f427ed7da55fea4d705487eab1960162f99
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:22:00 +0000

    badly keepup Gg little spread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
