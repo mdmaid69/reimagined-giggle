a08030bbf92a9d99fae96c36f91b94d42d8e1a1c
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:19:17 +0000

    regard experiment blame effort order

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
