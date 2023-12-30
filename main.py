70fecdfe0cffa72f1633da82d9b82d18c77b6831
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:00:38 +0000

    low shape put game expect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
