2b2f150f81c1881b4648f63f6f4a433be1a6ed67
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:52:18 +0000

    evidence foreign birthday true distance

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_list_from_array(array):
+        return array.tolist()
