34c2b2285136b6520312e4f69c5a861c1ac2ac88
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:59:06 +0000

    comeback equipment calm piece hurt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
