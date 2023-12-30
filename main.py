2307223a46f008d1190f31f14ab1c78df5d1a114
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:21:49 +0000

    convinced concentrate lookback(on) basically crash

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_speed(distance, time):
+        return distance / time
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
