6f16c9bb3c0d4e0d338420fe83de729e7d64ff72
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:35:38 +0000

    bet shade vision true pretty

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
