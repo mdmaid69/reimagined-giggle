3a1df55dbce4017b3be2c4cbb25aa3af2289ca62
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:33:17 +0000

    ordinary keepfrom cry show skirt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
