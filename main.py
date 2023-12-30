8c6c89020b35c741734c566b67e6e879ca89b7d4
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:16:19 +0000

    comedown often meanwhile shut people

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def count_characters(sentence):
+        return len(sentence)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
