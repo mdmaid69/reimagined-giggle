5a9340c2ec6030b7c339e3a392315e0ade227ca8
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:25:15 +0000

    comeback 566end which tragedy hair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_area_rectangle(l, w):
+        return l * w
