9acb33bd7abe8b25ac3f38ee65d0480522a985f4
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:23:11 +0000

    workon mind journey choose flat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
