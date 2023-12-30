7b462656f223088e8d857cea8c0e4bbb165ad219
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:30:41 +0000

    spill remember game shop turn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def cube_number(x):
-        return x**3
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
