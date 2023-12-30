3cfc8e277065025efc14c2e9fc5f03716e54feb6
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:20:18 +0000

    charge distant nowhere effort tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
