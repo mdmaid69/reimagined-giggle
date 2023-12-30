0e2ba07191cec34d77b32ae8a69d979d7a3230e1
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:27:15 +0000

    drawing set pain name wide

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def count_words(sentence):
-        return len(sentence.split())
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
