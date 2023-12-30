cca2cf3dae86a343460694ade191e569aa7f022f
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:31:02 +0000

    limit plenty plain share pour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def remove_duplicates(lst):
+        return list(set(lst))
