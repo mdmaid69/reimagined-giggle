efc83d2e0337b431e678469bf10042649f11e361
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:07:20 +0000

    insist opposite dream comment how

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def reverse_array(array):
+        array.reverse()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
