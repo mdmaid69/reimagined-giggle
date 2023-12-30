4d7389f7c570e021ac0529e5531ae5a22b1c1ebe
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:08:08 +0000

    surprised wall measure hide there

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-for i in range(5):
-        print(i)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
