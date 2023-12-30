fae00efecfdba73ce150b50c1eb5513f69c6647d
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:53:02 +0000

    cause mainly bit slightly load

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
