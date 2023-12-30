e95c9fef97973bdf99e2643dd6382e477fe4f343
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:25:01 +0000

    children message hurt tell persuade

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
