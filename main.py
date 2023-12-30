75ea989d87719be091b61a425de53b4762719855
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:48:50 +0000

    base couple task issue clearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import array
+def get_array_as_float(array):
+        return float(array[0])
