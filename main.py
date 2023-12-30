b6776403e6be2d2e6d2fcc370dd69cca8cd30e94
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:02:26 +0000

    clothes breakthrough then healthy lock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
