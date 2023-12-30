eb26856cc978ed75b744d316cdcfe01e8bf29cff
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:49:39 +0000

    pure strength Uu attach tongue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_min(lst):
+        return min(lst)
