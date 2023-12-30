17e3fbfae2567a2f53d58ec5d0dfe4fd134a3f14
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:13:52 +0000

    excuse fire trick crime start

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+from collections import Counter
+print(Counter("hello world"))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
