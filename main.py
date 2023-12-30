a89b41cc2db9ce45ff2907a207901e53377c3807
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:51:55 +0000

    calm rest lead pollution welcome

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import platform
-def get_os_info():
-        return platform.uname()
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def convert_to_binary(n):
+        return bin(n)
