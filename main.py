38fba70758b0bc8e1233d0f6f02d66863bd8c703
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:25:03 +0000

    emphasize breakup him difficulty day

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
 def calculate_speed(distance, time):
         return distance / time
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
