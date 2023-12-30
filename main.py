ba7112c80ad86f3c38422962dfa58c475c1efdf2
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:09:57 +0000

    fast your brave breakfast sign

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
