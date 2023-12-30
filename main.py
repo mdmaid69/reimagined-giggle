621007bd6c6981de13c4dec8b7322a2ff67811e3
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:01:33 +0000

    view mysterious ground sort wear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import datetime
+print(datetime.datetime.now())
