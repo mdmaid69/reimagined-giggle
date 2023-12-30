431ecb0207e2e883cc558c58b281efcc918da30c
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:12:08 +0000

    song pencil benefit fence pipe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
