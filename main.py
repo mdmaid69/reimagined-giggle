64a6d2c7dffb2e447450b63077f82095cd03ee35
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:00:22 +0000

    he block improvement every next

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
