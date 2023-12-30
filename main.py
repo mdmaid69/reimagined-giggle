e67cfe6f4d35cf991716b71d3cfc83b7b0ff5e1a
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:18:51 +0000

    breakdown central mine attract Oo

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
