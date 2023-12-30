c6c65142c78190f0fbe42925e9752e9acae0ff8f
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:34:14 +0000

    improve motion warn maximum leg

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import array
+def clear_array(array):
+        array *= 0
