429dc9addb683cd48fb1a98f028d105d67bee215
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:36:15 +0000

    length pretend consist yes transport

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print(sum(range(10)))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import time
+def get_current_time():
+        return time.time()
