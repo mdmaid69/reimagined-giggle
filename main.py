e61703b437724c95a90d15aef3ab770b71bfb167
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:20:57 +0000

    choice respect reach tour out

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-import array
-def clear_array(array):
-        array *= 0
+def calculate_area_circle(r):
+        return 3.14 * r**2
+for i in range(5):
+        print(i)
