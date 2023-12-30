d58906588bb284efd9754b19f9a9d6bfbf16fea2
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:24:58 +0000

    drive wire protection store finish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def get_array_as_int(array):
+        return int(array[0])
