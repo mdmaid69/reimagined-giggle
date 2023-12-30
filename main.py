32504c8fb4f527752b9d26dd443ede02dca37570
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:58:27 +0000

    meal deliberately unhappy culture situation

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def is_odd(n):
+        return n % 2 != 0
+import time
+print(time.time())
