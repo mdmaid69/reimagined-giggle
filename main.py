11aca8f3cfd77898edf165a74106118641e4c482
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:16:49 +0000

    bend us arm mind breakin

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_area_rectangle(l, w):
+        return l * w
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
