9c2d23ed2c456d9a40f4024456f11d12e63e8d4c
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:12:28 +0000

    strict practice contain free old

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import math
-print(math.pi)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
