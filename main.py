effdec61e2abcc39f96f4ba3f712ec2b9b552591
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:11:52 +0000

    careless control healthy be smell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
