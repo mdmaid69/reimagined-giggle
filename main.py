ee898ce12934df930425f1b46fb2f264eec75602
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:27:40 +0000

    phone table keen certain knowledge

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  def convert_to_hex(n):
-        return hex(n)
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
