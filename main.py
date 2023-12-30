bdd9e8d9dcc5d9af500f01849055608d87fa0c68
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:23:44 +0000

    able note question opportunity school

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def convert_to_binary(n):
-        return bin(n)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
