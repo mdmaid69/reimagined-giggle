fe461f36e7e0241c00c34269c6f17152e529a6b6
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:04:43 +0000

    come age issue maximum no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def sort_list(lst):
+        return sorted(lst)
