9a2e8efe0acfd191aea468d04927073cbe97cce7
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:47:41 +0000

    bright sound flat again person

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import os
+print(os.getcwd())
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
