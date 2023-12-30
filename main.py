a2b0143b06963785df208fb95a26546ceb3b0306
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:22:29 +0000

    shoot effect subject hard drink

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def find_union(list1, list2):
+        return set(list1) | set(list2)
