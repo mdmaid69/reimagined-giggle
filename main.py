c6e934da50e8df78b7a3fdd86faaf95c04a4e9f2
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:03:15 +0000

    charge Ii appeal lost after

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+for i in range(10): print(i)
