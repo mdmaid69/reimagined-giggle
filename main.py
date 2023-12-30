6c19148a56aa21e371cb212a4ae9e38ab0092181
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:30:13 +0000

    exist float estimate upset quantity

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-print(sum(range(10)))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
