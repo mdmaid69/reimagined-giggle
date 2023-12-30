2b35dae2b64dfd31b6ead944d3f6a256f4d9d195
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:50:11 +0000

    tidy recover father peace transport

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
