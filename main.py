fce1b1baca2787f9a78bca87a8eb77a8f8f482f7
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:53:05 +0000

    unless 564encourage neck where street

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
