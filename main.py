628242a94637ced207dc13d7650c8e39efdb7e87
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:14:37 +0000

    better neat break stand friendly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  def count_elements(lst):
+        return len(lst)
