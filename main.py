50356ec6e74fe90abbd8fb92cbf4547552dbf32f
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:20:42 +0000

    shock clearly mine unlucky book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
