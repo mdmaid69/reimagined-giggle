6ba78b2b5f49fd70b18cc6652ca9b64044488fc4
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:21:45 +0000

    criticize unless state on and

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+for i in range(10): print(i)
