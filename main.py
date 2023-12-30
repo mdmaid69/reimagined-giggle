5043e3909aba51c3d017e37d1aa0115b76a2afdf
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:05:34 +0000

    post amount common obtain yourself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import array
+def get_array_as_set(array):
+        return set(array)
