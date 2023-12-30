93a79778a0e6dd0f7993a2695f997b71019c50db
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:10:09 +0000

    must gradually tonight wing mark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
