1e4ae45e005babe39e9bf1762c9b4a8d710efc3f
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:08:13 +0000

    regret noise change expensive private

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_area_rectangle(l, w):
+        return l * w
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
