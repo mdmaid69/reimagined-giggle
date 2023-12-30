ecfa83264599c0f08360c549e111d221acd16f2c
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:44:38 +0000

    please comeoff once city treatment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
