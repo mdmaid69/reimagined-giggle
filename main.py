ee24a894349e1068d3c11660884998ccde384be1
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:14:36 +0000

    Nn mysterious with mistake fire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
