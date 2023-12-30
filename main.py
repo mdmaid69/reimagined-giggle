4ac78d37ba539e3fe22ab63b25c2ec40d26c9ea6
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:32:53 +0000

    spray club increase strike dull

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_list(array):
+        return list(array)
