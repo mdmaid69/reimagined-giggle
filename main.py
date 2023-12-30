99c9119833a5e0f3d44a8f16aea0c21254f36f56
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:11:17 +0000

    far warm remove shrink second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
