1bcf3e37c41faaedc1256b1688218dc4bd5adc53
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:34:30 +0000

    whenever lead maintain scene bin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
