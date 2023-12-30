fe69e18116d65a65e1aadabae5a40adfc2f6dae5
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:09:43 +0000

    bottom interview win shade delicate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
