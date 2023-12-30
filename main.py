ad65953f348cb132a3bca4f2d9257f5e6c9204c4
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:11:45 +0000

    ear war fun independent turnover

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  def square_number(x):
+        return x**2
