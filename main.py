f56be7a3a6ed2ac1d920c91cf970f198981f6589
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:39:15 +0000

    fortunate grow strange instrument everywhere

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
