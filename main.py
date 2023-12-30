28b7163701d320c5aaf645e864d039a997312917
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:07:41 +0000

    serious summer activity loud honest

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
 import array
 def get_array_as_set(array):
         return set(array)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
