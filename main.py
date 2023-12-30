ea4dcb6a13d13e247221f839735176e0d056ea7d
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:31:35 +0000

    pencil accidental us hard workout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
