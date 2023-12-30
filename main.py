6f4de2a29f906a5efa7d82169706351fc504b948
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:23:18 +0000

    awake coat pupil freedom count

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
