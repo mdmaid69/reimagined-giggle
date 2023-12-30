b39b0345a704d2b4a2941cf88514cb4cc65d2ebd
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:44:54 +0000

    choose shape skirt service ear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+def square_number(x):
+        return x**2
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+print(math.pi)
