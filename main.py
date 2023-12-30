ca4fbb9ac4da192f2e035ed30603068507a3da78
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:07:06 +0000

    by word exploration past goodbye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
