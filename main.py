14816ae4d52ffb0247f5329458fd7c89ac49d858
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:16:08 +0000

    drop bring total takeover positive

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def add_numbers(a, b):
-        return a + b
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
