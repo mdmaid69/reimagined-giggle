0a7101b1793dec1162c414aac85b7c544d4a1ab2
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:46:35 +0000

    call raw everybody land myself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import array
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
