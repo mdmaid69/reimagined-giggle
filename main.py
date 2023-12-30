2db742051809d31133a3ec8526957a98e805cdde
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:41:01 +0000

    knock miss idea system once

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import time
-def get_current_time():
-        return time.ctime()
-for i in range(10): print(i)
+def find_max(numbers):
+        return max(numbers)
+import tensorflow as tf
+print(tf.__version__)
