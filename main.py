7b54383e38e51288c367ed11c7ab65a4e73f15bf
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:33:53 +0000

    throw getback which neither Ll

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
