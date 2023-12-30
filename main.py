52fbdb2cc59552486fdb8c257947971a20f891e1
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:13:47 +0000

    man ring mother reduce principle

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
