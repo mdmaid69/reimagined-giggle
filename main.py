daa69f74addcdcff7372ebfda23d9cfdfe2da3ca
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:10:36 +0000

    trust uncertain pupil satisfied wherever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
