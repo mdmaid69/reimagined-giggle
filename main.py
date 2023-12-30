d158b63571b7add533ffac9f4f142df07a791a9c
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:46:55 +0000

    habit ago about corner fit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
