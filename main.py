572d37c1e46d0b64622f7b7df241538cf7982d7c
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:42:14 +0000

    software lookafter beyond giveback fetch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
