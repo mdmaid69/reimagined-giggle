ce13d65e8ccc56925eb2f87f4f721d151fa18bf3
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:33:04 +0000

    confused case introduction die shop

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
