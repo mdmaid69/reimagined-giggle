5933ea652eb78fda88534a6f8c9c7a72ce0e5301
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:00:46 +0000

    term keepaway splash area getout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
