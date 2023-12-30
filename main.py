795bfe9e68def4eca62f469db563dfdf49e157f1
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:03:35 +0000

    arm surprised need sister realize

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_work(force, distance):
+        return force * distance
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
