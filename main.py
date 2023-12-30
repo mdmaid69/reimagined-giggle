dd265a9ea5890488c317f70423fe1713c3d74e64
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:34:37 +0000

    flat thick reserve convincing backwards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def square_number(x):
+        return x**2
