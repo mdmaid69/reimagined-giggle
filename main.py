be30ed8dd89b4cbd44f8c0e4eda645bfca2f0685
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:03:48 +0000

    couple neither total half cause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
