70cdf98baf60158cbbc0a201ea18913f7d0c2674
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:18:55 +0000

    breakinto perform smoke hurt defeat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
