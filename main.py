8bacbb771c866a9b1e89067a44ffc0067dd7096a
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:55:19 +0000

    passage bright contents slice exam

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import collections
+def create_stack():
+        return collections.deque()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
