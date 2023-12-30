96c6ba57f3bc6f137afb139bf6cefab3d18344a8
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:46:51 +0000

    bright well rather possibility scare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  def convert_to_binary(n):
-        return bin(n)
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
