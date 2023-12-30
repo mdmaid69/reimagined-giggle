c7628766fc3a3af931c278ee1b1f7afdd5afca4f
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:05:38 +0000

    sensitive around steal telephone side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
