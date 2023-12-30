8a7656bca7463b35134f185a5942dfe553c81b0d
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:10:30 +0000

    tall appear doubt certain dull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
