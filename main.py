43cefe7e52dc556b8c97146487fac353e4400828
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:42:14 +0000

    extremely compare harm wire winter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
