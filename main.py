94088b5a9332c8a097d15fdf78215589d3f8e465
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:28:00 +0000

    prove flat creature equal cost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
 numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
