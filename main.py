aa33b6a3dfb22d405372a7e7affd8affec2daf86
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:19:32 +0000

    happen who our important at

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import sys
-print(sys.version)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
