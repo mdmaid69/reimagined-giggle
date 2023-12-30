e4d30c8a10a9730045888328411ad5dc589307b5
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:22:03 +0000

    patience relief butter correct first

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def delete_file(file_name):
+        os.remove(file_name)
