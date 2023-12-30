daf7f517bbebc643d7640d3b164ad933f7f66f32
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:14:55 +0000

    blue stage try record release

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
