8ba1835b389bac3a9fb5a0a8aed751e6d0bf82c4
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:32:49 +0000

    holiday creature last though would

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
