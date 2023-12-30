61167368c93d817c5546d1cc2009f52cdf4b9514
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:02:54 +0000

    enthusiastic size warning sort contact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
