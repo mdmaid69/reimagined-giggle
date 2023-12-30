9b168846b0497ad6f713f5e9ab4d3b6adc4fc818
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:56:31 +0000

    and impossible partner undo Oo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
