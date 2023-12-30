e59221c6c7c5699041f4e8492cf60cdf05ea881b
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:12:40 +0000

    brief port mean us wise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
