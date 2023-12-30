7654ff6f95e14d5e8150e0e09be5ad6b0e186e0b
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:12:30 +0000

    sister cum mark sound kill

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def convert_to_binary(n):
-        return bin(n)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
