a4d229dd1bfad8e3bacff747f0ec1d38ae26b57d
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:56:23 +0000

    fast especially door she word

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+for i in range(10): print(i)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
