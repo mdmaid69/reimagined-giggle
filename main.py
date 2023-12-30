fc0e1c5e3af3527c2ba5b7291c65734c2f6ab00c
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:17:50 +0000

    ask dear business heat clothes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
