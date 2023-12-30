b83d99915eb07d771953514641208d62431695b6
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:59:50 +0000

    throughout onto closed and translate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
