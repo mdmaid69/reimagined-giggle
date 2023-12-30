e5e6d46e3661ac06f3ffb1ca3d497243bbab4357
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:12:01 +0000

    act meanwhile giveout lot demand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
