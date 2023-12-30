8531430f0adb8e2ad3bb879bd6c6a3a9044bcf86
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:34:38 +0000

    watch release play forgive at

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
