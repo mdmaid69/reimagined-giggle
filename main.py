ef405c66fa900963be5c4d0933211d9a58c75a6c
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:27:15 +0000

    worth nigga accordingto film address

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
