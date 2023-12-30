ca720f67ad0b24a7860fa839b519eaf96b615ae3
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:20:08 +0000

    turnaway rather bag possible backwards

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_acceleration(speed, time):
-        return speed / time
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
