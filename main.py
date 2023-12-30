a628e0ca7cfec808a592d3c75e3bb98b94373412
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:23:21 +0000

    want firm fierce smart tap

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import time
+print(time.time())
