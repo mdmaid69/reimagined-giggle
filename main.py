357dcaca16b283b6a7ca6ce756d5b1b870d2f548
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:59:37 +0000

    ours feed breakfast 572enter arm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
