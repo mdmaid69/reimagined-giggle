73d7ca78f09c4e2d24aed2813ab6d6e112a297c8
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:58:10 +0000

    fall fit 565end translate include

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
