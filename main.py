ce5188b03749d2620397732a7d94609026290fd6
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:35:11 +0000

    flow stay unfair bridge fall

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
