401d3149b52677d98bc4a43632738c684606347f
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:26:47 +0000

    recently cover recent Rr with

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def clear_array(array):
+        array *= 0
