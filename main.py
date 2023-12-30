e730b6cc393e6ca3c8e23caa52db12a6b8d77c67
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:36:56 +0000

    regular thought offer afraid by

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def get_array_length(array):
+        return len(array)
+def calculate_area_rectangle(l, w):
+        return l * w
