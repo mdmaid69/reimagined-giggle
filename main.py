ba372d0d3e9bd8579dbb42bcfd646878a6592d6e
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:33:03 +0000

    type crowded estimate scream pupil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
