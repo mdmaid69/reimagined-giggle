f9137bebb5a246e8caf9b9afaf49fa0bb54446fa
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:20:28 +0000

    funny offence stage takeoff separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def get_array_typecode(array):
-        return array.typecode
+import sys
+def print_python_version():
+        print(sys.version)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
