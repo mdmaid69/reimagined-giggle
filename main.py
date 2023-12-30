ad8a4c2aee96d2f9eaf4506a866ae53b1c00dacd
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:34:48 +0000

    awake fold content principle less

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def is_odd(n):
+        return n % 2 != 0
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
