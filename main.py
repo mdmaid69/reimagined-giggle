66b5985b16b78e39611c1f648ad728d6f60f6d21
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:20:01 +0000

    face foreign reach minimum bush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
