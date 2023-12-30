7295932f7236ba4764e3cd5c41ee92cd7cf034cf
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:14:54 +0000

    healthy talkback young one rubbish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
