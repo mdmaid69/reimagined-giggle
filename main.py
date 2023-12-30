33c05e7169bef2b326608562b808f0f9b9c50547
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:05:09 +0000

    destroy expression bye partner temper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
