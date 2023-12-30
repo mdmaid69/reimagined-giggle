c9a7beed1c16d606adc8b1f41fd80ee098d32a17
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:21:55 +0000

    swallow theirs interested in platform

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def count_characters(sentence):
+        return len(sentence)
