eaa96b0a7bd9d3c4acd9d2c2b684c6f4684f6f5a
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:26:34 +0000

    race them basic contact whenever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
