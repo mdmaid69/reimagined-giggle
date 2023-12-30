8c39e6418f25242ee4f946207077936360a01ae5
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:38:23 +0000

    better thank quiet bye problem

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_power(work, time):
-        return work / time
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
