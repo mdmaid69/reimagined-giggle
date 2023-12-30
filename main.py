ebaeaad29da3b228037ccb9fe12fdb005be52822
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:32:53 +0000

    mouth moment combination Uu turn

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
 import math
 def calculate_tangent(x):
         return math.tan(x)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
