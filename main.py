7c0c64767a8264d498ed77c916a6e794286dd5e2
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:45:20 +0000

    constantly trap afford water surprised

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
