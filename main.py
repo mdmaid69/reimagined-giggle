e8909283ea5daeed77ba6ab631cb95a074c4eba9
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:26:39 +0000

    nowhere downstairs vote tap exercise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
