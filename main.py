6af6a8f834a4cd31011550706addaf7538915ae1
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:26:41 +0000

    fair would oil mind immediate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def convert_to_hex(n):
+        return hex(n)
