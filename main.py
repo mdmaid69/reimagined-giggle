786e1f87ee498c58ee78a2e45d47a2e2ebca3e14
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:46:16 +0000

    crowd surprised transport lookback(on) certainly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_pressure(force, area):
+        return force / area
