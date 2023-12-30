7a2b5c357c20462b5ba96ed74f6518e6e16bb118
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:16:48 +0000

    likely calmly soak land Mm

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-print([x**2 for x in range(10)])
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
