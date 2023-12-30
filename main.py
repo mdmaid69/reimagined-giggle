5796ed1080ff8dde06338dbf4ef06a306c940a1f
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:11:46 +0000

    ilovedicks sit giveback slightly environment

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
