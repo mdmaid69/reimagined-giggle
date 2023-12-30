ad87cb0994e19ee720b4e117244d4658c651e8cb
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:53:06 +0000

    ours minute suckme obtain worry

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_current_directory():
+        return os.getcwd()
