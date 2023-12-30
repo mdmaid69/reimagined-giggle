bb7b0bb564786e112ab0e6575a9bc1ded48c8b6d
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:35:26 +0000

    wander fail however give runaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_current_directory():
+        return os.getcwd()
