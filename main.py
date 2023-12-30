1b80346bed234d42671a8b9f18835ce910b37b34
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:09:34 +0000

    remember dick look contribute live

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def subtract_numbers(x, y):
+        return x - y
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
