2deb85ac0d628062531d8e0ae8779700624aca7a
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:31:53 +0000

    flow classroom model coffee loud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
