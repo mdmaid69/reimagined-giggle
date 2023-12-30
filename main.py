3aa94bb7f8fca4a993530d69835b09ca4e50d515
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:38:04 +0000

    wrap attempt mix glad behind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
