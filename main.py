6ee50828bbb8b83bae095c4e3ca37764673d1807
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:38:01 +0000

    cut scared discovery dark myself

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
