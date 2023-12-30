a4e5ad138892b8941952536311b1c91aaf2ed244
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:45:25 +0000

    load thanks introduction early suppose

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import os
-def get_current_working_directory():
-        return os.getcwd()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def is_even(n):
+        return n % 2 == 0
