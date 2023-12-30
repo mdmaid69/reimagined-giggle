72901b1b3bd55c6ecae7e8b8bb6329158eea7f19
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:54:36 +0000

    service project hide content react

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
