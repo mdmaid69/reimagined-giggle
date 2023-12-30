7582d61bac0890b6e261ebd125317b6c79bb288a
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:58:21 +0000

    convinced children disappointed string natural

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
