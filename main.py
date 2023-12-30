01756fcc270b5b75a2c952adf7aa41298192d721
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:57:59 +0000

    charge satisfactory chase breakdown divide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def change_working_directory(path):
+        os.chdir(path)
