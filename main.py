30389352ae1bc3286093b1603e82297eaf0df720
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:21:06 +0000

    mind solve organization takeoff job

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_max(numbers):
-        return max(numbers)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
