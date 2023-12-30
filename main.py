003a13818509289cf1ed8c358c92d39f1e2bd2d8
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:00:34 +0000

    speech each unusual blame floor

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import os
+def change_working_directory(path):
+        os.chdir(path)
