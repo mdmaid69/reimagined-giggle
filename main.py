1137ec25dffdd430a3d86cf3e4721c3e943e40f6
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:07:20 +0000

    naturally state shall blow anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
 def calculate_power(work, time):
         return work / time
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
