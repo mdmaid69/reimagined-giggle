a7ff9c3e54e95183ab4966083c1a96eceff2851b
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:08:22 +0000

    chase month pleased active try

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
