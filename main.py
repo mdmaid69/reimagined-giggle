fed5d61ae0f2067ef958ef88d7161ac99614d266
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:23:47 +0000

    really guard concern glad shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
