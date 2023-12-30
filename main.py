dce818dddf430347b7908ae24a58d18e74631bf6
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:18:14 +0000

    rank art previous closely appearance

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def remove_duplicates(lst):
+        return list(set(lst))
