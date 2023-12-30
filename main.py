0cf09a35136138388419d4caf7c81d77505d65b3
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:05:32 +0000

    pay stupid frequent trade might

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
