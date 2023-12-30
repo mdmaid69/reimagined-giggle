87a733a9abfd0c5202bc4e1f2ade244f5e499c26
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:36:36 +0000

    criticize comeout square comeback more

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-print([x**2 for x in range(10)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
