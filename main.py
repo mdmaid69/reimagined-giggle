4b0c5652882388dd7280825e3d827afd0c96a342
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:33:28 +0000

    interest street cap surprised payment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
