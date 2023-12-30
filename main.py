b0cb72abc268348ece1d20f113eaecc51380d22a
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:08:01 +0000

    shout contrast rather previous tradition

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from collections import Counter
-print(Counter("hello world"))
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
