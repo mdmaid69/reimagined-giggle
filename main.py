907e4e41a059dde5abecded67611b048e9c905b1
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:40:30 +0000

    neither method protect sort late

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
