97db084316673f8588d682074ec5a1c505fb7c88
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:43:00 +0000

    noise content final relative innocent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import datetime
+def get_current_date():
+        return datetime.date.today()
