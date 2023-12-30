920d646d41e880787603607e0b10cbe97f7f9b05
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:05:09 +0000

    occasional pronunciation certainly drop solve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
