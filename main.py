b85c715361b6581c2df79e56667872bc89944712
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:32:58 +0000

    offend usual beginning themselves find

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
