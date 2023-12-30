3e3e30235674b7565c8dadeeb66c086c4983e6db
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:24:58 +0000

    spend him company spin over

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def count_words(sentence):
+        return len(sentence.split())
