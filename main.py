a4e72c03dd1405cde59eaa109157c3ab7070e737
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:51:08 +0000

    appearance runaway impatient impressive choice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
