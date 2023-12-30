37e850325769601519d418413028f7e8833586b2
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:59:59 +0000

    appeal on trap shall active

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
