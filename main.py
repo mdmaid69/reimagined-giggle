cb15bdb591ce894782f8aa0662a9a70263406f89
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:43:28 +0000

    agreement positive settle eager professional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  def convert_to_binary(n):
+        return bin(n)
