a7fa24b0af771baf3614f9b6d4412b2357d272e5
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:00:51 +0000

    objection people invitation pretty almost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import os
+def change_working_directory(path):
+        os.chdir(path)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_string_from_array(array):
+        return array.tobytes()
