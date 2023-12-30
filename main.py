4b7a511882caebba7f043322f4e7a6370bb6461d
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:55:49 +0000

    slide post spot behind above

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_int(array):
+        return int(array[0])
+  def remove_duplicates(lst):
+        return list(set(lst))
