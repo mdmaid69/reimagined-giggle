9efb5db8346e81fa51e7153095000c8f9b05f096
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:20:01 +0000

    suggestion seem description day calm

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def reverse_list(lst):
-        return lst[::-1]
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
