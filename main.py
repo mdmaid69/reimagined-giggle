4b8180e750a948b7027afb438bb8ecd917ad15cd
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:51:53 +0000

    instead woman feed come mouth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
