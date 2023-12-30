0d009662fa64b93bb7d61cbaa4ed4bac3860db76
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:42:51 +0000

    stupid climb sort bath used

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
