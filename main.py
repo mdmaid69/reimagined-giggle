543efe566c9ea91a729cd43a7efd2074d1bf1914
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:02:33 +0000

    actual wind left shout priority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
