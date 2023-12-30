27487cfd5a83fc524a46dc9395dbb0332baabc31
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:20:02 +0000

    achievement shine brown steady giveup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
