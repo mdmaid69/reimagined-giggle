afb45c404171abc9d27679ab70ee9ecad7138e4b
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:44:35 +0000

    switch Ll flow 179bite will

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def multiply_numbers(x, y):
+        return x * y
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
