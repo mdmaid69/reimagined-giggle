d22ebf3a5a3076d65a7d77370e41189bf5046907
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:06:55 +0000

    description name mend crazy complete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
