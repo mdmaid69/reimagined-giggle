3532e790f579ed8e65d911dbd87f51aef122236a
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:55:04 +0000

    light too relationship race comeback

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import os
+def get_current_working_directory():
+        return os.getcwd()
