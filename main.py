7902b1f90c5976967da43a5688dd2e5786a3a16a
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:55:39 +0000

    if second will majority takeaway

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  def reverse_list(lst):
-        return lst[::-1]
+print([x**2 for x in range(10)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
