f06a34668787e072489e85e024069cbe192960e3
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:25:01 +0000

    confusing bear upstairs decision left

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
