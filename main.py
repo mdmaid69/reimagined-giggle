75d081c91c68c20245ef54cfd3b65dda6d03f50f
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:43:37 +0000

    rule advise work pipe release

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def add_numbers(a, b):
-        return a + b
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def remove_from_array(array, item):
+        array.remove(item)
