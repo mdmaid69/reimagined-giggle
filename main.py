b77e85c5526920ca986c4af6b72f168162b13e17
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:30:34 +0000

    proof face then his deep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
