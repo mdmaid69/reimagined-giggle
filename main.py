95659445ca596dc018a0230e5e907abdc35227ce
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:16:24 +0000

    fairly brave flow expression at

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def find_min(numbers):
+        return min(numbers)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
