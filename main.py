60983ae9667265c3af870d64c9e849ba175adc93
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:21:17 +0000

    brush fail fix doubt slip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_error_function(x):
+        return math.erf(x)
