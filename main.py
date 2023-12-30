86353053158b909a1ce72ca4b007c9a50520dd00
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:37:25 +0000

    study discover party card easy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_power(work, time):
-        return work / time
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def reverse_list(lst):
+        return lst[::-1]
