70e3a2c3ca0a0754278f2ce99f4a6d4d35a48973
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:58:49 +0000

    goback(to) estimate punish branch Rr

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
