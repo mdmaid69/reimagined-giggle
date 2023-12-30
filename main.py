3dfa9998667495dbc5b8f24ea6231c5026c1e274
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:22:46 +0000

    cut guess estimate prove remain

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def find_unique_words(sentence):
+        return set(sentence.split())
