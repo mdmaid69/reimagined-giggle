169e489fb25ded2751725765d74b442af81ac5bc
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:14:48 +0000

    name persuade round wise everyone

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
