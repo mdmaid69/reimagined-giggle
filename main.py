261cf10210af1d25375fcc6e5b5eab79343f9475
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:40:41 +0000

    clear get even free underneath

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
