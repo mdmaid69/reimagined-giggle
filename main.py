88cc54628437657d0ba18a9c6d246ba185988f35
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:35:06 +0000

    bringback sensible best just another

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
