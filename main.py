244dba3f4ee1144ec712f4da3f7e9b47d4dfe10a
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:19:15 +0000

    tight task window my sorry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-for i in range(5):
-        print(i)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
