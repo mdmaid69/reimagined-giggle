da7397ec58ac388ab03cd0941ccea2b7b0b2f8f5
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:33:13 +0000

    bang towards family big afterwards

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
