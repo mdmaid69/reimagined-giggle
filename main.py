eb27eb5e65c9a200bb76e27f9863f15720968f22
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:35:22 +0000

    size number muscle minimum but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
 import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def get_array_buffer_info(array):
+        return array.buffer_info()
