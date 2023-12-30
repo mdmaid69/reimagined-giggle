cab60abe6f3a921084637b90d1f59272cd9b24aa
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:11:37 +0000

    possible scale degree purpose pay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
