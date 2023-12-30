8bb0eb35bd05d52327925adfe3912bb6d3f0af6e
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:25:56 +0000

    plan view pure load relief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
