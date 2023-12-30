ba5f13a460a3349427d5d6f2c0132a29b6977d26
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:58:21 +0000

    consist wire my move fit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import re
+print(re.match("h.*o", "hello world"))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
