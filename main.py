d2dd10c7f107cf4200494050cea00ba823cc48bc
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:39:29 +0000

    thank danger reaction step true

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_density(mass, volume):
+        return mass / volume
