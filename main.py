c19c643bb7f15aa237b36756367790dc562acbe9
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:05:03 +0000

    hospital greeting hurry shop Gg

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
