8f1432445ddb2b4a454d0651977656f7e7fba60b
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:25:51 +0000

    corner what cross accidentally impression

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
