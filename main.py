c9c3a1f34cf456f9b1cde045017f54c561fa3f26
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:03:45 +0000

    bringdown upstairs that issue wind

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
 import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import datetime
+print(datetime.datetime.now())
