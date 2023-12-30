3867f16a6be649bd13e431a524a4ca3545f8bf2d
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:56:13 +0000

    talkinto factory later desperate quarter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
