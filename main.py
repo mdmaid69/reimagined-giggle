82b7326d31c6486aa65d380f9c7ca844f02e81ae
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:21:31 +0000

    battery glad minimum taste shrink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
