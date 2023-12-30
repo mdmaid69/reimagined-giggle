62b5dbe13a29333d152e405cca802d529bae4c11
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:54:21 +0000

    together excuse clear replace into

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import os
+def get_current_working_directory():
+        return os.getcwd()
