f922d052310b0774f7c756e28e75c174f64b49de
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:57:53 +0000

    what this eat unless lookback(on)

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
