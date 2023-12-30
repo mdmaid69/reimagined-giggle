3da7503389ab997d9b358b02dd4a64c4ded50ecc
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:25:55 +0000

    urgent serve long store base

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
