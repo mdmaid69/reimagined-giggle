c2729ec9f41bf887224a11dabdaa3f34231f51c5
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:45:45 +0000

    already group better reasonable trip

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
