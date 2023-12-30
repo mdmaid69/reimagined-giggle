9c219aadf2a3e47263fc2431fe4305ddf141f72e
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:07:52 +0000

    clear appeal lean lie phone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
