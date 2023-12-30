0ead5fd9ff5cbe7d54d5f34548285330d45f1404
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:47:22 +0000

    miss warm bag favourite turnover

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def reverse_list(lst):
+        return lst[::-1]
+def multiply_numbers(x, y):
+        return x * y
