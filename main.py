26032087c4f0500ff0cee55dc71be85940c0bd24
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:50:54 +0000

    punishment satisfied really fasten pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
