7f471aa52c8b7cbccba48f2670e9513d66505e8b
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:26:15 +0000

    extremely century tidy centre violence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def divide_numbers(x, y):
-        return x / y
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
