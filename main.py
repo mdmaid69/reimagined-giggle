c475b34f7262dc0e31d16788696ecd4d2edec823
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:52:40 +0000

    belief declare shout needle work

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_work(force, distance):
+        return force * distance
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
