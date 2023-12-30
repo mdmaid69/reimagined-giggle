c643570036809cd1afc00dc82ff7aaa32e2f2eed
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:59:29 +0000

    sad path computer sick trade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
