c294cb1b30e954811b218fca32f85348fa8a8add
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:47:13 +0000

    receive also education confusing gooff

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import json
+print(json.dumps({"name": "John", "age": 30}))
