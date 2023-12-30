40cab239534a83851f446f343c6e3fc7eb45ff73
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:07:07 +0000

    seem high juice snow freeze

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
