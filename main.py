d4c36fd43bf8a71bb0df38f511f671f7b4ca7d79
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:15:42 +0000

    suit even repeat section several

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
