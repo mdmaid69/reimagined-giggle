0d1e751718d4020206d1334b4ba952511388feff
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:09:04 +0000

    people patient confusing too cool

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import sys
+def exit_program():
+        sys.exit()
