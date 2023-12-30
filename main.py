d38e6a8f68227451aef516b3332e64190efaa7d8
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:50:11 +0000

    great bill conversation proper this

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import array
-def get_array_index(array, item):
-        return array.index(item)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def extend_array(array, iterable):
+        array.extend(iterable)
