20257829e58422eda6d7e30d0fd2e0fb6f694d8a
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:05:08 +0000

    stage wonder yesterday lot confuse

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
