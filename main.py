6fa253f040a0ad59276165ed1fbd7f2efa7cc5f0
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:23:40 +0000

    warning test right idea organize

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
