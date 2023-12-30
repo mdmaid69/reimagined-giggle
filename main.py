747a906082c0906e0a30583ac250983a7ea59af1
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:42:22 +0000

    quit offer walk off cheek

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_density(mass, volume):
+        return mass / volume
