17f186e31c93ff8833cd016ab394c08823ab7bac
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:33:41 +0000

    effort smile luck centimetre goout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def calculate_area_rectangle(l, w):
+        return l * w
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
