cfb221d42a88e6de4be5766cd7197f30ccc82e2a
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:47:22 +0000

    cross smooth note drop backwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
