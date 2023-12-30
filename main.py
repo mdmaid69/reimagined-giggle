9dc0fddaa19398375a6f421b80c8ed7102477a6b
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:16:09 +0000

    continue please total 568energy turnover

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
