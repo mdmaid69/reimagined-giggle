39ebdd03090b9aa8309f228c67005d509f11d0f0
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:26:35 +0000

    professional remain swallow wind land

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
