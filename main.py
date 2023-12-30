0335c85c5f0321663df00e661a2339f992570d62
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:17:09 +0000

    laugh range long busy wonder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  def multiply_numbers(x, y):
+        return x * y
