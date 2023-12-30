803d5dbab2a01f958b12d4b9221a152c87fa7f87
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:39:49 +0000

    story event uncomfortable truth sing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
 def get_array_as_bytes(array):
         return bytes(array)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
