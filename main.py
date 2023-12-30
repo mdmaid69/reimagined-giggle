ec0a3e9548b9df5e0b66093a7699340d12db82ec
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:55:44 +0000

    instrument before hang cook drive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def find_max(lst):
+        return max(lst)
+import array
+def get_array_length(array):
+        return len(array)
