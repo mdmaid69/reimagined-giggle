938ec9b63c7c3901e62fd7b456689a0e9f9f8825
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:51:40 +0000

    neither great dangerous funny gift

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-print(random.randint(0, 100))
-import tensorflow as tf
-print(tf.__version__)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
