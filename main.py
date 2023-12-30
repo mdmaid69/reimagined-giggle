28ca569a701256c5cca1c42a4c85f670327c1b9d
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:25:43 +0000

    test they unfair considering own

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import re
+print(re.match("h.*o", "hello world"))
+def divide_numbers(x, y):
+        return x / y
