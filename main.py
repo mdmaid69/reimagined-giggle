273774e425357296c93bb12d8f4b9b9112cd349a
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:43:44 +0000

    necessary quality silent meeting broad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
