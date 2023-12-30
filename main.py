a64e5f6cfb9275880b0add8b0f7ee60e7a31213e
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:41:26 +0000

    joke opposite secret I help

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def sort_list(lst):
-        return sorted(lst)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
