cd86f7e4b9fc8f5ccb90a58dda36b0f525316ed1
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:23:15 +0000

    legal present waste cheque prize

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-print(sum(range(10)))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
