310530fc5e5b26c27f280c9cf73da777519084fe
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:36:25 +0000

    announce space under half hello

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
