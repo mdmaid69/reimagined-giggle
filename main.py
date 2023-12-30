ef9c9ed0cfcc0da050be5b5dc0df3bd75631c134
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:03:16 +0000

    key appear among development confident

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
