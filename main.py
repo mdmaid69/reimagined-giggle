d041b26f04b8774328320b8eb785ca9468e6f3e0
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:59:09 +0000

    classroom miss bunch dry substance

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
