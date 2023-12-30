511eaa650a23f53832aff30dc8ce3626fb4e8ab6
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:54:58 +0000

    cum light cope effort Ll

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def count_elements(lst):
+        return len(lst)
