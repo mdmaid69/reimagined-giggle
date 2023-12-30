82a610e9e553bde735022d2d6398d13e82876186
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:20:07 +0000

    day today bright that will

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
