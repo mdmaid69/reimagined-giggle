c7b4e594c69de5281be85f2ecdf029385ea04141
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:31:42 +0000

    brother will matter now spring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def find_max(numbers):
+        return max(numbers)
