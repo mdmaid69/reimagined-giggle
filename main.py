16576d895586d8acbc23cde2b89871685b3ee4a7
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:51:26 +0000

    its capital pair direction couple

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
