89c9dcddb8ea9c517b1e912a2195143308fa2210
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:47:23 +0000

    least joke unlucky trade also

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
