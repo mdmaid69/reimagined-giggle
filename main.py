eca2ca567189d5262d438a88510750ab1acd3473
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:27:44 +0000

    strike waste contact march fault

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
