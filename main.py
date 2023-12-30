c2643a1d94453569ad1582c013fe03d810cdb1e6
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:29:04 +0000

    only impatient part recommend convenient

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def subtract_numbers(x, y):
+        return x - y
+def count_characters(sentence):
+        return len(sentence)
