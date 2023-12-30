d5e56d55a4390b44824d9c77f30e5c48cb603dce
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:37:38 +0000

    love turnup uniform strong tip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
