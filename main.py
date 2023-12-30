c7e3b154e4c4f6421f10c684c733046a333f8d2c
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:16:15 +0000

    clothes seriously office draw proper

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def divide_numbers(x, y):
-        return x / y
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def square_number(x):
+        return x**2
+def calculate_area_rectangle(l, w):
+        return l * w
