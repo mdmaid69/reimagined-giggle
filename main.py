9a70a462428494984b642448aca5b2b764fbb849
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:06:19 +0000

    maintain swallow tone stop yes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  def square_number(x):
+        return x**2
