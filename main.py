c0ac7e1c4191bab632a959b56cb148a26395caea
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:47:57 +0000

    bin discovery grand criticism impressive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
