bbbe2b00b574c0f25bc80ef8a7ed0b52c824f504
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:16:10 +0000

    transport slight degree break below

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+name = "Python"
+print("Hello,", name)
