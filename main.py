ae7bbbd4df1c680bb96baea7920c032f0c6555bf
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:48:34 +0000

    anyone knowledge roof insist tax

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
