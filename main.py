fe8ae4a7b059ac0d1726e1b54e544677d55cddaf
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:14:52 +0000

    serious smell excuse turn upstairs

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import os
-print(os.getcwd())
-  def add_numbers(x, y):
-        return x + y
+def list_files_in_directory(path):
+        return os.listdir(path)
