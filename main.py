647e14733df100827a615ca5cf2fdb36f6354fd4
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:03:49 +0000

    today die same embarrassing comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
