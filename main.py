4a03d04e208a00bf9cdb906bbbc59548800cb2a5
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:02:49 +0000

    pipe copy else call level

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_min(lst):
+        return min(lst)
