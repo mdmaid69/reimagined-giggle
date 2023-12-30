8b099b9d8486f2bd88ab74487bb93c21fc27b314
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:09:52 +0000

    Ll lost available share praise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
