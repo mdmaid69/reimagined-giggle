9e6c3ce837d5071b20ea5a7e7fe0640deddd61ac
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:43:37 +0000

    check suddenly emotion climb low

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
