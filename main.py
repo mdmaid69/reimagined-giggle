d083b88692c5f49e3b78591393f1f8ec52aed026
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:01:41 +0000

    occasion after favour cheap communicate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
