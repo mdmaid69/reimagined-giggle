63e147c2523b3a5f0ce68185d6990dc711c7f947
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:20:35 +0000

    hot contribute urgent related dare

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
