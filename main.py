d279f2858be6b845194184d97252e5b1918b344c
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:38:39 +0000

    last cruelty offer check watch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_current_directory():
+        return os.getcwd()
