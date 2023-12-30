7a5254fd5c47f53425ccdf6a20ac39f9c038c2e4
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:48:09 +0000

    trip dangerous centre keepoff neck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def convert_to_binary(n):
+        return bin(n)
