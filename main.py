7e911bbece59839acf7e585e690ccfd85666ca0c
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:11:46 +0000

    lead wide then long continue

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def set_array_item(array, i, item):
+        array[i] = item
