a2703aefe96cb243fc5c0f77251759b4639c221c
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:08:15 +0000

    late cause slowly furniture would

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import platform
-def get_python_version():
-        return platform.python_version()
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
