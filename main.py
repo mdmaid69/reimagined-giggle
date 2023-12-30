1bd2d8133db534e48d6424ea3c1d2a2b2f6889b2
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:21:43 +0000

    middle surprised total proof thinkof

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
