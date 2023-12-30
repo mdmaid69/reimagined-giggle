a262f4e38b0d8f668bac53fb25c5be816ca0d432
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:17:13 +0000

    loud winter land eat scream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
