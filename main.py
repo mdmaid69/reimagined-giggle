b1971d05caa44e433bf5e6ae90acbe062527b6e5
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:33:41 +0000

    process invitation rubbish type much

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+print("Cube numbers:", [x**3 for x in range(n)])
