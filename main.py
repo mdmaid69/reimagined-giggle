2b592c7f2c32eabb8dbf2ca0abebfa8982615c21
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:32:57 +0000

    lead mainly school secret joke

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def find_max(numbers):
-        return max(numbers)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_work(force, distance):
+        return force * distance
