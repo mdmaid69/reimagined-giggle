0fa7b94990d8b449dd6636a4de5a83e65f3455df
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:12:46 +0000

    about situation whole patient doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def get_array_as_bool(array):
+        return bool(array)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
