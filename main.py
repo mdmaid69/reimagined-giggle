634393403267efc3339ea45ca9d77d52140a60b9
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:20:14 +0000

    abroad tidy disapprove opposite word

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
