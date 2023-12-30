eb0ac08b463b481b8722a18147b5dcd82afd70a4
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:00:53 +0000

    threaten description window doubt relation

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
