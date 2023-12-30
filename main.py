fefc01ebf82a5cca75ce4ec48ff19cbaf77a57b9
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:53:39 +0000

    connection comeup quantity regard occasional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def reverse_string(s):
+        return s[::-1]
