10dad3efbca6ab6f58adefc4fae5042c41ac432d
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:30:15 +0000

    tip stiff so around name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
