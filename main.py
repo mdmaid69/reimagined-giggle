d75f79e899833cabb72d66b618f4a20d3d106b21
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:28:26 +0000

    hollow plate soft makeinto destroy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import numpy as np
-print(np.array([1, 2, 3]))
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
