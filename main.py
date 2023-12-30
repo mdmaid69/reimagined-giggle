d5cfee0ba511693b4ff16ad282ac994a8659b28f
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:42:55 +0000

    sweet cough sorry willing door

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
