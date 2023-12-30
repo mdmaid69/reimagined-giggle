01e19c1b2a48ab86cf7fbd68565189da1f393405
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:06:36 +0000

    yours final hear might trick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
