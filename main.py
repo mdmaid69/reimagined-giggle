fd5063c6c480b6af86af26e5cbc558ad1b58f0ad
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:39:41 +0000

    too chin goback(to) ear bath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
