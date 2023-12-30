0d1c0d8bac2912d6fcff0008fd973d619184a7ce
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:25:47 +0000

    poor Gg container left past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
