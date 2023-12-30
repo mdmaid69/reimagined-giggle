01be5153f3991af8459b7ec28f999b721b85a4dc
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:35:41 +0000

    lunch final succeed slip comeoff

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def find_max(lst):
+        return max(lst)
