e0c86b80f755b0dbdeea95e83c88788f2ca05654
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:22:51 +0000

    worse breakin single bit label

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import logging
+def log_message(message):
+        logging.info(message)
