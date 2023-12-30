1442fe2eba80354e3ebae7a0e1e469d494a993d3
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:54:55 +0000

    grateful touch field mess pour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def convert_to_octal(n):
+        return oct(n)
