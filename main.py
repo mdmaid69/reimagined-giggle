a6376dcf2b67ad735335939bce8cb784ef6abc01
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:53:02 +0000

    anything shout low language lonely

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import collections
-def create_queue():
-        return collections.deque()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
