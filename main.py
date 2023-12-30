ed49a36e9122048afe9ab2a693a7b542cdc2e1c5
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:07:55 +0000

    179bite next shop catch comfort

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sys
+def exit_program():
+        sys.exit()
