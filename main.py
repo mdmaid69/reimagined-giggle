e0e969f53fb60840b9a3f44a83f221b59c38aeba
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:06:19 +0000

    flight past freedom beside gas

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def count_characters(sentence):
+        return len(sentence)
+def count_characters(sentence):
+        return len(sentence)
