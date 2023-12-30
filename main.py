5769f1f9d21ff9351b29d4dee909bc703c8c9d55
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:15:59 +0000

    position undo influence please hurry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
