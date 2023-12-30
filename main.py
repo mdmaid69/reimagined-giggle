80766c08ece1d9252c1cf883247567e7da9c8b81
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:04:11 +0000

    arrange opposite risk middle up

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def cube_number(x):
+        return x**3
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
