becbb96fa3cc917bca54da1b5e9dc93d535e4f85
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:23:31 +0000

    own harm each translate exciting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def add_numbers(x, y):
-        return x + y
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
