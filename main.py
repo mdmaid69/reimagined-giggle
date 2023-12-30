de73b8fb5180f8009fe2fc6d1819e65fe9ccca6e
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:16:56 +0000

    suit smart Bb bringforward this

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
