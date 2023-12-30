b86f76480c0b4cd870dd98d93339adc6f5028cac
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:19:29 +0000

    tip skill care holdon connection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def add_numbers(a, b):
+        return a + b
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
