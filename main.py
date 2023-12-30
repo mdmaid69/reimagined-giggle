f117b0d7dafafa4573792be305c3adceb87d768b
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:48:42 +0000

    insist result oil trust and

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
