6663bcfd49c4333030004961139cc9f038842c16
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:29:29 +0000

    company trip fit pool 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import tensorflow as tf
+print(tf.__version__)
+def convert_to_octal(n):
+        return oct(n)
