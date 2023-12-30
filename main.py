bb9854abdb22db5e45bb152c6497ee4b0ecdc0c5
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:49:28 +0000

    downstairs say fear high busy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
