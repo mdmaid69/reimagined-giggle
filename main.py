c6b3b56d76bf2b170004911ce63b358f5078b5af
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:45:43 +0000

    fetch marry thought drive Gg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
