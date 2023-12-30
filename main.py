2dc56794abc9178c3563be7da3e12d09ac212f12
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:34:11 +0000

    old turnover tired unfair impressed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
