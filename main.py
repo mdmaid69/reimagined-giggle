bd66205f5fc2ebe6c21b22a040fa937a24169a4e
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:43:11 +0000

    lookaround keepfrom change forward mad

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
