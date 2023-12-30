0e07070ebc293e4c9343e5bf18e7f7d62108b243
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:51:18 +0000

    press shock ordinary sure occur

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
