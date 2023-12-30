faefaf2227b61e29fc486774bf5df8a098dda925
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:50:02 +0000

    everyone doubt matter feed interrupt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
