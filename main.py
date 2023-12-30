5a0a89977ef9ba8eedaf9f787b24f41129ef0398
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:26:20 +0000

    mine kind lookback(on) bottom rush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+from collections import Counter
+print(Counter("hello world"))
