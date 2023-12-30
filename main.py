1e976e9fbe12ade8b8da6459362d5a33acb61028
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:06:15 +0000

    damp tired mood develop produce

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
