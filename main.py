934b159064d971709864498d6ec1074f78b403f4
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:41:04 +0000

    skill hurry fast Ll smile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
