45910d27bb0a25e4e18068438aa812f533eecf43
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:31:36 +0000

    impressive slide usual settle today

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_work(force, distance):
+        return force * distance
+def calculate_area_rectangle(l, w):
+        return l * w
