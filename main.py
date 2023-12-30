64865f03303b5b529b2de8937c43620039b47cfb
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:24:11 +0000

    expect suddenly pass pressure exist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
