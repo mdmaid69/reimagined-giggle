56522ba519fe956557a9739d8cbc20129e28980a
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:34:57 +0000

    play fresh thick pencil care

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
