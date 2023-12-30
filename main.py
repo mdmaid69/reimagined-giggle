9d627fa18255388206627201f9f43f8b269c4a02
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:09:21 +0000

    mail feeling sell film entry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def append_to_array(array, item):
+        array.append(item)
+def find_max(lst):
+        return max(lst)
