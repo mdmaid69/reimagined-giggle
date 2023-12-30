62686ce7d7eb601e5a30967702bfaf6943c829b4
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:11:56 +0000

    equal except comeoff yes only

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-print([x**2 for x in range(10)])
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
