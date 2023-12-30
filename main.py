1bd0a3481491d5daca6350fa35bddd45cfd89e44
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:31:12 +0000

    advice OKorokay interruption roll calmly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
