acd1f1a905b907f2846dc1556e2a6fedab733c80
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:53:18 +0000

    various soil shoulder up standard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
