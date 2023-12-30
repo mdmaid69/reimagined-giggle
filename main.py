c55848290c6d24e1a930a3d4b2468892b28861a6
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:18:21 +0000

    confidence announce near fail song

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
