143dd89c266f61dd3446b0a204750d3abea46434
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:31:42 +0000

    takeover nothing start practical lean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
