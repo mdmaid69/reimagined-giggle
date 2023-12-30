365ddce44e520003d6b4c36a75c40bcc1f2b8d53
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:04:54 +0000

    bear action love actually bye

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
