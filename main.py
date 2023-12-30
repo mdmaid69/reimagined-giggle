29fabbf02dccca9eebf51abf26993f622bb9bc22
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:09:20 +0000

    tool crash game bush confident

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
