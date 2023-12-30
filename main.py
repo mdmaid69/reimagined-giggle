54f4b4df1de0859695b04fe2769b670af0559421
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:49:55 +0000

    comedown means bush rough community

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
