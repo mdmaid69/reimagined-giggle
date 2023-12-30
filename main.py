c6d000b4834e18c53f131c25b0f99e5e6713577b
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:52:56 +0000

    clothes send past let number

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import tensorflow as tf
+print(tf.__version__)
