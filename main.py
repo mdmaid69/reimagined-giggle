58f6cbb87850b2ab4755713902d84ce94e969a19
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:59:33 +0000

    around threaten arrangement flow image

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
