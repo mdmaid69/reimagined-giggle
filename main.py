cf59a184a78c3ae2ae87cf46824c9127e98d5ac3
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:23:09 +0000

    suit gap thick detail computer

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
