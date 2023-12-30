bbfa1eaac62fdd7dd05f2d39598023a98fdcb340
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:57:36 +0000

    manage difficulty ticket style abroad

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
