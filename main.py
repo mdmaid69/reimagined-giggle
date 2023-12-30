b02ed2db056f0b8888cbd86430ca1f529594b495
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:49:08 +0000

    regularly pleasure suddenly easy progress

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
