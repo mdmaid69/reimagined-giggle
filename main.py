fbaba47ec23232af6cbf4f52fae918808a6eae53
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:55:25 +0000

    unit himself exhibition clearly rob

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  def cube_number(x):
+        return x**3
