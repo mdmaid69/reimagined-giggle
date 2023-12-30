d017302003d78525906e9066fb0a0e8622a56f6e
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:18:39 +0000

    many temper cause leg film

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  def is_odd(n):
-        return n % 2 != 0
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
