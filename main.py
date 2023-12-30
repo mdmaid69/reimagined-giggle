d60191e0e8315d37f280fd370041adf311c5eede
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:40:59 +0000

    open regularly happen anxious on

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def convert_to_binary(n):
+        return bin(n)
+def divide_numbers(x, y):
+        return x / y
