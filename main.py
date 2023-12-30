c355437fcd189865011211ca1604c83e41eee0e2
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:13:41 +0000

    flat responsible straight apart repeat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import time
+def get_time_since_epoch():
+        return time.time()
