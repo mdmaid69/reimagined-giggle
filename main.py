c81c6f3d3b18db431e0d98dc01d696941e99134f
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:25:00 +0000

    instruction fit married deserve fix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
