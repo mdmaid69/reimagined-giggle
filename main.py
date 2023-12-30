14b055c077488f372142f942f09f872c90ddd973
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:03:48 +0000

    scheme study closed disapprove somewhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def convert_to_octal(n):
+        return oct(n)
