05f60d20d10511c93dab1bbe1c6d7ad913a50078
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:26:52 +0000

    experiment bread breakthrough comeout leave

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+for i in range(10): print(i)
