1f1fd3db265906250842d7a7d0a750ddab3ad184
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:22:39 +0000

    bother across should congratulate patience

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
