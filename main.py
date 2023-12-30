6bcbc05278243621fcc38a351cd6beba4783c77d
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:43:48 +0000

    test earth object judge disturb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  def convert_to_octal(n):
+        return oct(n)
