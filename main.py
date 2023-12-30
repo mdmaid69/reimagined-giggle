8492de513d462a8568933c86f2d7f87d27ccfbde
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:31:43 +0000

    awake side blow broken where

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
