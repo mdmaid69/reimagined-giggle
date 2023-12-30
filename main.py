2ce880529430230b37a3e3e8a0b075e4725dabe0
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:18:59 +0000

    dry load general unhappy takeout

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
