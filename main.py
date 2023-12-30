e3f8d823c0c94120f7fc40dd3135537594ca33fe
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:20:19 +0000

    medium pay towards naturally quick

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+print("Square numbers:", [x**2 for x in range(n)])
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
