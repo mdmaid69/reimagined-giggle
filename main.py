f392bec14905079885ab59e075d0f6a04fe0f852
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:56:34 +0000

    occasional male joke trip go

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
