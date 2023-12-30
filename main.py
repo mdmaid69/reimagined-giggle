d30f137faeedbd740df84ecf1bab17adcbbef107
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:14:10 +0000

    pollution soak 569enjoy now nothing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_density(mass, volume):
+        return mass / volume
