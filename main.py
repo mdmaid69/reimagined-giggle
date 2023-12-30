98f995cb157317a3a4e47600a3ecd078634a7c5c
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:26:40 +0000

    admit table table reach argue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import random
+print(random.randint(0, 100))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
