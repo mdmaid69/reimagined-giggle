e16f0e1a4d4c4a80e9acca2787c78e50d7768e58
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:47:33 +0000

    apologizeapologise cap swim round replace

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
