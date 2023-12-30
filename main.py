85c4090b8bf6f00a496426c92300f277ffc42065
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:46:38 +0000

    rate transport makeinto official congratulate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_current_working_directory():
+        return os.getcwd()
