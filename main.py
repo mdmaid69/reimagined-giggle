65954dfc85483e02f549c2e37182920b24f898d5
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:30:44 +0000

    hurt also brush wall on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def is_odd(n):
+        return n % 2 != 0
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
