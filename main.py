893f2073cf34d77f19c0b3f665180e2c9908b1ff
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:48:43 +0000

    false lay convenient suck star

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
