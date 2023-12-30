405730fbae0e444f5863f742039c217f633fc3b9
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:05:48 +0000

    recommend back characteristic far among

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
