a35e3c570c2618c77d84125f00905390d908094b
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:28:59 +0000

    unite may improvement quickly comeback

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import time
-print(time.time())
-  def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
