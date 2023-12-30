5ce5b2673dc9eed7291f5fea8b97c3746ee6d3da
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:35:01 +0000

    away city thinkover deep takeover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
