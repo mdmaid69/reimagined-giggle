36ab12e8aa922ceb5c30895a8139c9aaa496c613
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:26:45 +0000

    equal money reduce calmly film

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
