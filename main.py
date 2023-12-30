41e1c2f61bc84c498fae0a136e7a1c4dd036dec8
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:24:16 +0000

    succeed cheek trick comfort copy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import sys
-print(sys.version)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
