60f4816173d1a0d40b0303750fb115d6e230e198
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:45:05 +0000

    here ticket hold him comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
