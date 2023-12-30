255e60c4b348b0cf4f17b8eaaa451540b250d703
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:04:16 +0000

    estimate stamp represent recommend mind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_float(array):
-        return float(array[0])
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+x = 10
+y = 20
+print("Sum:", x + y)
