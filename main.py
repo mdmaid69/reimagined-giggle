47db38f653b0b94a64a8c9161f0b21623e21e84e
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:44:25 +0000

    if announce OKorokay sign sail

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import datetime
+print(datetime.datetime.now())
