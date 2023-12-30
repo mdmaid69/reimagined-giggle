a27b5ec3b22f231f4318b55a4e54641465fc92e6
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:52:22 +0000

    closed Gg produce artificial extremely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
