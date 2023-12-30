a90086c65f9987eba7088e4713f4b01832a451ee
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:51:33 +0000

    tomorrow talk seem pretty defeat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_power(work, time):
+        return work / time
