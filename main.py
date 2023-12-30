4c40f191f03b3a6157fe1104aab5a29dd8d77750
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:14:02 +0000

    getback part handle group trial

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
