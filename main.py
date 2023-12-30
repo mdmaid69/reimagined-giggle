d2a8bbd15e3e7e39584a7d864893b2da9072600b
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:52:16 +0000

    switch bringdown square only test

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+print("Hello, world!")
