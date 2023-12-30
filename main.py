6660c54ad920842d3dda6370c6340f063df8d33a
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:34:22 +0000

    seem mystery pretend wish song

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
