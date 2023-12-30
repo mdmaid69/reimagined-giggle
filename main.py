acbb44a7e8c3ff31e1d60bf49a44fecb19ad1f2e
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:12:25 +0000

    identify steep move majority height

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+print("Hello, world!")
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
