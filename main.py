2cff4ed6f17e3db4795242954490431e81c37068
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:56:22 +0000

    suit mind patient 570enjoyable connected

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
