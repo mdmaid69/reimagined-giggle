99616e93200ae6593f5bc030ce3465d291b56d23
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:34:47 +0000

    behave meet often bound lend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
