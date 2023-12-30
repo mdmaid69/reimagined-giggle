d1834d05d31fded1e30e3d3fe7669fba340c89b1
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:42:00 +0000

    comeoff material face full achievement

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
