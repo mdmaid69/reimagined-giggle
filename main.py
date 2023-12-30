25e9362c6c054a7a92b58e677ff8e8cc7343baaa
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:28:35 +0000

    Uu offer study everything people

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
