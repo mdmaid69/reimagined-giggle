98a6ea141117363c594d6b14971a5feabe5b3dd8
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:16:24 +0000

    agree recommend platform wander actual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def count_elements(lst):
-        return len(lst)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
