e7d47846314c79d2da821df12efe921b0c1d4dda
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:23:12 +0000

    offer frighten eat content shop

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def is_palindrome(s):
+        return s == s[::-1]
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
