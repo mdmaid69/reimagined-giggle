69e73a3a4e117788e8520534a78a2c56ddde9c7f
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:51:32 +0000

    machine hospital win Cc broad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
