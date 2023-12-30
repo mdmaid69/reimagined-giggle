d44229e07d057c40ccd4347a506cba3cf7491cbc
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:32:03 +0000

    study bread middle perform style

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
