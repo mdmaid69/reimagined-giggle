9fa23816111535adb3a8acc556c4c3d87ba476cd
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:47:46 +0000

    improvement remark joke much strike

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def multiply_numbers(x, y):
-        return x * y
-import platform
-def get_os_info():
-        return platform.uname()
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
