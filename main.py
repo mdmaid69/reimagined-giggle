ade2f443f8a00205c735409e54377f27627c52d2
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:26:46 +0000

    dance improve point shall shall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def square_number(x):
+        return x**2
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
