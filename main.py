c8989b39559e0a7ccc1fbbd593a2be3f9bfeb529
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:05:31 +0000

    active deliberately modern interested contain

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
