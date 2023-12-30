2bf4d5b836c81a1fb0e0903225a4c30259dcdccc
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:12:56 +0000

    fun walk least common project

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+print(math.pi)
+import math
+print(math.pi)
