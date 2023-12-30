bbbbb253a2d61cbcb3ce5f7d81446e5333881aaa
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:21:45 +0000

    aim answer next right face

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
