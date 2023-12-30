23a256843847007e302571178abb11226161e8e3
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:50:54 +0000

    sticky problem fit speed tone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
