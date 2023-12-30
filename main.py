b3e0b12a204043ec587657eed9e42d7a0e1f3e6d
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:28:11 +0000

    public relax sport doubt same

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
