cc4e9ffd485640bc4f33fdb2942c596f022b30c6
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:42:20 +0000

    likely address special long rid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
