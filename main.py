18f446c5896bcb8ad24ce871adfdb3beffa158dd
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:50:28 +0000

    deep conclusion reduction file exact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
