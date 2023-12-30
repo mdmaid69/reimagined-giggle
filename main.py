46c68feaec12b704f6d8df09b5fc0275dcf58396
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:11:14 +0000

    born objection burn thank poison

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
