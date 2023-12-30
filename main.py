5b4bed37b1797adb4908f4d7f91ae4ed6661f1ba
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:50:10 +0000

    slowly study wipe smooth poor

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
