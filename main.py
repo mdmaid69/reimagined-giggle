f6c76620b312078fa99191a68c068c1ffae20365
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:00:33 +0000

    whatever part smile make congratulate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
