c4982e1624538223dfae3d8aa1171217567d29b8
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:47:34 +0000

    face price tune shallow last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
