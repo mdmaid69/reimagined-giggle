5b3b8529793a6ef0f709ba32cf0c9b5ff150464f
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:00:17 +0000

    duty hurt accident bother favour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
