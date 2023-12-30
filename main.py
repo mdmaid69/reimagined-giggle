5e810ac23732430181d0c54d206fc20587b68fd6
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:39:27 +0000

    sleep money mark into against

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
