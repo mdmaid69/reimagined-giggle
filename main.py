2992c523c6dbbeb3dd1246c81ab5005bea909796
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:59:09 +0000

    black round unit exist clean

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_string_from_array(array):
+        return array.tobytes()
