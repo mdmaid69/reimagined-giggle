dd22eba01f8fad98722f0410e8ec048b181d1271
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:20:43 +0000

    drop handle OKorokay sound funny

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_counter():
-        return collections.Counter()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
