d79618abacc55918e25a3caaceaa806294e3fb0d
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:04:22 +0000

    react switch create commit record

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
