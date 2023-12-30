d271b687c2a7e0c9acaba9f367e5ee1a43eb425d
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:03:31 +0000

    favour charge defeat control arrangement

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
