2ae474ba6b59b58b3bd5240ad4a2f6f19076c7ca
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:12:36 +0000

    take trade few actually trip

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
