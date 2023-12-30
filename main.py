9ad7742120b4ad009287eff724534e2a7060e2a7
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:41:37 +0000

    exit talk all advise accidental

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
