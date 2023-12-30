55dd50ce24cc1236deaad5b5c43457ca42e42c2f
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:12:27 +0000

    sad thinkof shut discovery work

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
