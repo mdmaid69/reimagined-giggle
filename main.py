dd0aae2d7f1e3d373bb0d8f7d82008aa6b46da89
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:47:32 +0000

    last hole education hurry unit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
