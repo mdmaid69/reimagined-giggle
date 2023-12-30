d35ceb5301a2a8840504733c840084d27f6e8f74
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:45:22 +0000

    instruction everywhere ago position issue

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_arc_tangent(x):
+        return math.atan(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
