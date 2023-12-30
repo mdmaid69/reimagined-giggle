da48ddabafaa522cf575afd5e381d5c02b23d64d
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:06:35 +0000

    work private cool to danger

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_list_from_array(array):
+        return array.tolist()
