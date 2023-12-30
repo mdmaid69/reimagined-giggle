8d126fb040bf67754abc0d31af37ceefc5ef6edc
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:49:10 +0000

    possibility rate criticism balance instruction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
