96d49049fef5e4531029b8eab488c2b8fd388bd6
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:19:51 +0000

    closed modern pretty need goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
