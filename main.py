bcc96b71bdc58c86e5bb641ddc71d454c7db6a0f
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:34:52 +0000

    please declaration badly blame glass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
