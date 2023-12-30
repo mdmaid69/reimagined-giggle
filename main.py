8c1886d09d173f8b76e6597dd8843f38ff527736
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:07:20 +0000

    place to estimate material direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import tempfile
 def create_temp_file():
         return tempfile.NamedTemporaryFile(delete=False)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
