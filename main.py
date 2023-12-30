1e0089960a16f707a4aef4b7073d673241355f1d
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:47:34 +0000

    question shape birthday its takeaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
