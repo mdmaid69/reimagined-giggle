0b2ed2bf3713dd5a339b5befe2fc4fc9bee8ab10
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:07:36 +0000

    turnup exact describe upstairs content

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
