be949e4d1e45529a85a532eed0f7ecb85a88d81e
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:44:53 +0000

    before safety discussion patient challenge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
