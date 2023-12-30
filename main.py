7a169ec4ed65bfacd109454466a55954c09b6582
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:31:34 +0000

    finally after us unite year

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
 def calculate_eps(net_income, shares_outstanding):
         return net_income / shares_outstanding
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
