c0ba177554f63608e8c3340a8debb3aee10931e1
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:08:58 +0000

    lost equal bury satisfactory speed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
