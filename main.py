b09f327910ebd52093e27a120fbfd1ad590a2efb
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:15:48 +0000

    fast report mix drop close

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def get_array_as_str(array):
+        return str(array)
 import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+print(list(itertools.permutations([1, 2, 3])))
