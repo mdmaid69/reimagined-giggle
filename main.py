1104b9fda6c5c17ef58ce308cb6933ebe8efcabc
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:49:41 +0000

    pin true fierce march lose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
