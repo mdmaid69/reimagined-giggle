14e585c12d6a0e2b34b31bd1edfc629d0424694f
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:47:20 +0000

    along no of land lock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
