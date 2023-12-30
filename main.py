34d34fb6bd796c24feb0838ea6251fe2db09bc8f
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:19:19 +0000

    equal advise squash Tt belong

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def is_palindrome(s):
+        return s == s[::-1]
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
