7b2aa5fea5bccbd8497cfd279ba5f09772d58648
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:29:52 +0000

    tape second bringup cross opposite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_slice(array, i, j):
+        return array[i:j]
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
