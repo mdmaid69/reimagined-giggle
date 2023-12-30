f7511d437014dc0bc6956014d81b15441ca97e6a
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:35:52 +0000

    ball comedown content break narrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
