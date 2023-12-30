3101701a5494ce6699e074f689b4361bab0e1928
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:03:30 +0000

    point sick active perfect worry

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(numbers):
-        return max(numbers)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def square_number(x):
+        return x**2
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
