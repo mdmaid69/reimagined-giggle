61d33da492870f49b852f0f5a91ee6621f70ee31
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:34:41 +0000

    danger file sound cruel exchange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def multiply_numbers(x, y):
+        return x * y
