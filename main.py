482935b5947cfa99a75272e9755f4218cc93c475
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:10:48 +0000

    cut square tour frequent laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import platform
-def get_python_version():
-        return platform.python_version()
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
