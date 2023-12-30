8c8293bec4e99201fc2857279664f32acffac9a4
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:02:12 +0000

    objection bury emphasize whichever report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
