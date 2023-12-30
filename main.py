63dde152cc5326467443b3a57e09588b13e6a9d1
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:32:52 +0000

    smell remove ask surprising recommend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import sys
+  def get_python_version():
+        return sys.version
+def calculate_power(work, time):
+        return work / time
