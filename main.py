af08f835bc6026240cac5680dea1ced7faf14533
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:48:32 +0000

    Ww length journey machine limit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
