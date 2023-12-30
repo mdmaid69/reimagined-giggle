4f8f8d555d802a3a8190af6eef78ec79e6663bfd
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:37:27 +0000

    father slice star neither arm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
