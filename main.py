6d43d0cc974910aa21beb2f12906470cd677cf4a
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:30:43 +0000

    please Ii trousers treatment deliver

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
