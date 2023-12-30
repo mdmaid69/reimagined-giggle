b96dd1b2f4a411e74ecc48d8142ee70d41584acc
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:59:17 +0000

    opponent into include attempt pen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
