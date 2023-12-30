e1fbfa360e62eb33b8aa50496d5bd33140e14dc6
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:57:56 +0000

    exercise receive much report getout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_stack():
-        return collections.deque()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def find_max(lst):
+        return max(lst)
