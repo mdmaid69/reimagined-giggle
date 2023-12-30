d16113effc2f8e6bf585c6bd9369b214e1c622f9
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:01:03 +0000

    gift string practice lose either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
