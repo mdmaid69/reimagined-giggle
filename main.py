f30e64009e3dc89a185a376839b59f2606d3c484
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:30:12 +0000

    by minimum stupid true ashamed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def convert_to_octal(n):
+        return oct(n)
