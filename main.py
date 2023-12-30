3292b3ba968ca6d0e88fe5ca36149037d8d3d84c
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:03:02 +0000

    imagination comedown 573entertainment view view

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
