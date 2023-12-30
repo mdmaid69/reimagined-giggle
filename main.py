38e370f8c13264876b08be8a7a089b8ebfba7136
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:20:11 +0000

    anyone eat possibility card brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-from collections import Counter
-print(Counter("hello world"))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
