93b3e21cd9d5882e85d60a1db41ba31d14562ae1
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:26:41 +0000

    without bridge while through total

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
