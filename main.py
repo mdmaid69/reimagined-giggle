387041cb5b09dea9d97916ceb0f31b43ea879225
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:09:39 +0000

    normal without in little occasion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
