4b7713c5c6f245c36883d2dc6a5ae4f2413c4f14
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:19:26 +0000

    limit disagree proper unable split

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
