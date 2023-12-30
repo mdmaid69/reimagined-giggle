0d5feba9ec18c3d9f2430e4870ef317a3a146cb7
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:17:01 +0000

    hurry view single breakinto cinema

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  def sort_list(lst):
+        return sorted(lst)
