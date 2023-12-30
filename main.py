8ce79cd44f176e1ca0341bae48f0e53517ccf8f0
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:41:54 +0000

    stage grateful settle sort interest

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_acceleration(speed, time):
+        return speed / time
