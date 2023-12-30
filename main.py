0b5013408ef88d8a415145b211d6717a55100c39
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:29:18 +0000

    bored noisy never scrape emphasis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
