fb95035c5ced4acf4216975cc813ef6586cfa432
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:34:39 +0000

    throughout everywhere Uu in excited

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
