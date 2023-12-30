829ae76eaedbb177879c9b142119dfa810f9c464
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:19:52 +0000

    short damage weigh that hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_arc_tangent(x):
+        return math.atan(x)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
