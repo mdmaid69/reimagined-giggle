98fff549c4eb92318f877d09843299aaf64f312b
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:03:14 +0000

    comedown great future long sensitive

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
