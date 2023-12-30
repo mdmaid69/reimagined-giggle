7f0aa284ba7fdcd712bb3a5e4344040aa6b1c946
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:10:00 +0000

    apparently fairly relax frequent benefit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
