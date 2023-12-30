f781c0ef2b236cc6a7c9b28b12eee2cb92d2311a
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:56:53 +0000

    behind loss estimate solve difficulty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
