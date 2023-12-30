4f25323c03980e14a92555faed67c5af29022b21
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:12:55 +0000

    hold pause lose community figure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def convert_to_binary(n):
-        return bin(n)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
