ae25e244f5b65eb2fb1676e2865d84ac5823677d
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:46:59 +0000

    broken music lesson university picture

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
