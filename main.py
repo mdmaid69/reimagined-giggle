c8d7e761efc0a61d97c23b611dee836f30d9dd6b
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:09:37 +0000

    first eager stamp small follow

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def sort_list(lst):
-        return sorted(lst)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
