33b779c99eb3a75ca65f3871026b3fb6d80b3198
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:03:33 +0000

    bag mail discovery treat line

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def square_number(x):
-        return x**2
+  def cube_number(x):
+        return x**3
 import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
+with open("some.csv", "w", newline="") as f:
         writer = csv.writer(f)
-        writer.writerows(data)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
