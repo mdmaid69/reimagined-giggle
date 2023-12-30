e3d33d1797fd8824676a5abdcf6beec02bd88bb5
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:21:03 +0000

    repeat quit count proof finger

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
