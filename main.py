5edd52e1279c18aef85af6b6dbff8dd8a178241d
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:23:15 +0000

    middle match father receive protect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
