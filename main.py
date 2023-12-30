eadf16a54efb136a594f5348e0e8ac936d6c0bc5
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:06:22 +0000

    I well properly think practice

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def delete_file(file_name):
+        os.remove(file_name)
