9035c0d0c5e42405e378da9458a0ceb1b1c5b1be
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:33:45 +0000

    wave sharp follow board themselves

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def count_elements(lst):
-        return len(lst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
