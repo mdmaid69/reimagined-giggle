3ee493cbba9f5364eae2b6dda3d054116dbbfc66
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:50:10 +0000

    age she godown train purpose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_bool(array):
+        return bool(array)
