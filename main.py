80a5fdebc81f790bbe3a47f775dd9a42c0b99ba8
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:49:36 +0000

    565end may everyone big tend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
