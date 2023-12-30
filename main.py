93a785c8f2a7d9318cebb559d49171238cef24d7
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:36:49 +0000

    rarely clean influence please until

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
