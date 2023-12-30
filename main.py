d08d2a87d3686d99d32beb679b1be30acd788cc1
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:41:49 +0000

    study burst maintain from many

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import sys
-def exit_program():
-        sys.exit()
-for i in range(10): print(i)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
