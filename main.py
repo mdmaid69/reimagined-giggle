fdbbc76e63538ca1fa1e472c807483e188e88d2d
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:52:22 +0000

    careful bar tired and hit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import random
+def roll_die():
+        return random.randint(1, 6)
