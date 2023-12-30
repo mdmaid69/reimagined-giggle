f4fa97ca8026b2012b8b2b1931b935b3b98d4730
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:56:23 +0000

    scheme sensible completely thinkthrough leave

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sys
-  def get_python_version():
-        return sys.version
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
