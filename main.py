f9341e0efbdc53c409273d278dbea4bc3f6c5c91
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:34:35 +0000

    solve cook and sticky scheme

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
