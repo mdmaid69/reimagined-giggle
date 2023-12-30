b47294209457e45e037fc2e009d088befdf7bfd7
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:12:16 +0000

    ask last insist use statement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
