6f82abec8f004923223961da32c118d60ea2c560
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:53:00 +0000

    base declaration form sting about

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import collections
+def create_queue():
+        return collections.deque()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
