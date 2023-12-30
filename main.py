2ef3ca47fd3ca97a2fc42ce6a7adb203104776e2
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:41:38 +0000

    bored ought clearly influence otherwise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
