ff5ed9befab91ab7bad5c3097f5016942248d0a2
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:22:47 +0000

    instant stupid out union disturb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
