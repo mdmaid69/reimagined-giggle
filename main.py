72ae8c9a5aa5119ee2fad3a4c4dbc9e58f70a834
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:54:09 +0000

    rather second should majority tell

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import random
-print(random.randint(0, 100))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
