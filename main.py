689cde9a2c0c4889d5918905a98ca439d21c8f6b
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:12:02 +0000

    part giveout discuss what language

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+print(sys.version)
