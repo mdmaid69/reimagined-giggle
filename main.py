f1ecff012c516c87ee178023c8ced439e2d4455a
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:50:38 +0000

    mess warning exchange there service

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
