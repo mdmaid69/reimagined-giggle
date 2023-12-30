e55c6119b9fdfa81826f3682ac7ba41c39cba09e
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:37:09 +0000

    part centre takeback fill live

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
