0c9d4dba250485279ad30954505f81c6a74069a9
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:24:11 +0000

    compare die distance supply miss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-x = 10
-y = 20
-print("Sum:", x + y)
+text = "Hello, world!"
+print("Characters:", len(text))
+import getpass
+def get_username():
+        return getpass.getuser()
