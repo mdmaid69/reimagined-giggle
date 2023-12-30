ad1a455575cfe36b4ae1d40abe2ac1ada5e3d994
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:59:18 +0000

    bang 568energy payment pot bottom

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_volume(length, width, height):
-        return length * width * height
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_as_bool(array):
+        return bool(array)
