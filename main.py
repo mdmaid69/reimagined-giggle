1c15495b8939da2b0b5fb15130affba82f0bef15
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:02:50 +0000

    sign office answer ear thinkover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def subtract_numbers(x, y):
+        return x - y
