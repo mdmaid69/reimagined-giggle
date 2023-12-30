1d4baea510fc1c1aafb58c243ed1300f4f81c49e
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:59:59 +0000

    forgive principle mysterious takeaway report

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import collections
-def create_stack():
+def create_priority_queue():
         return collections.deque()
