01de5af21936e83dd9895bdbf860ba666cdf0c15
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:07:28 +0000

    570enjoyable any exchange win gift

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
