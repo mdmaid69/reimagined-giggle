d491325e84917a5bd4a4ad9e06bf6abc1124ca46
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:02:25 +0000

    during cry shut deserve direction

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
