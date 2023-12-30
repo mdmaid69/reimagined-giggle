e554764a04c010b5fcb1ddf75874e2b9a5733b68
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:54:23 +0000

    right passage cost laugh nonsense

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
