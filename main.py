3449dab7552994b2054f2174cb6e93c2925a990b
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:24:56 +0000

    only available dress sudden round

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
