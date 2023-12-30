f3dcf357be9c5ca583e87bb0b743da37736b49c0
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:28:07 +0000

    maybe demand loose town control

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
