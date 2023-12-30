574a308f04738528ab8e05e01f41d8c7617b75fd
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:52:52 +0000

    guide high sail dare phone

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
