f0403484a3878a3642b7721dc86aa7512bc3dea6
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:13:56 +0000

    try lesson hungry advice criticism

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_directory_name(path):
+        return os.path.dirname(path)
