d58b8850236d98a4460eec5e4563ebaa34bc19b4
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:57:57 +0000

    art likely transparent building slide

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def count_characters(sentence):
+        return len(sentence)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
