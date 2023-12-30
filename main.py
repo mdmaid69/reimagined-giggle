c296b4837266f8b2ce2acf7bdbffee9ed930a15d
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:16:01 +0000

    children keepaway letter replace cover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def multiply_numbers(x, y):
+        return x * y
