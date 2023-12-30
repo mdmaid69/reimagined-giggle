bde21ac8568f9b485b4151b2bdac16abddc3f02a
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:59:46 +0000

    season report even proper use

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
