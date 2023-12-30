ec00866570821155ff5b67ecc187eafc2dadba92
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:31:15 +0000

    national contain formal eye roof

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
