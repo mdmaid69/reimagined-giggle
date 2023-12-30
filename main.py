9e1b14b58f7eb2b61383b39497f7d08b8f6af5bc
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:02:26 +0000

    something course correction she hard

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def get_array_as_int(array):
+        return int(array[0])
