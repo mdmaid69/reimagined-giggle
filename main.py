15bf329b0af19d29c9b86e871b5a6aa34be651f1
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:03:23 +0000

    remain beat waste shake artificial

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-for i in range(5):
-        print(i)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  def add_numbers(x, y):
+        return x + y
