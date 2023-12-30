2b9dfd68421227561e8ca1571ec3d508f154efde
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:28:40 +0000

    surprised many wealth hello measure

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
