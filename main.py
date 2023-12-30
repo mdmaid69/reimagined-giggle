d3e45d37f54eeca17025a954cb40fa8a40d28b8d
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:09:17 +0000

    thinkover late goaway show rescue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
 import array
-def get_array_as_list(array):
-        return list(array)
+def check_if_array_contains_item(array, item):
+        return item in array
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
