f278d397eb2c85edf49ee192954ab807a665acd4
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:52:45 +0000

    question remark lay race cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
