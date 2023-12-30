ee4394d49bcb2afbbe76c9136ddc2d200676d3d9
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:52:20 +0000

    pencil thirsty naturally environment balance

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+text = "Hello, world!"
+print("Characters:", len(text))
