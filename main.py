132ea25972f744dc00a59870978d52cd2037e9c6
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:09:11 +0000

    until smell please equal throughout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def convert_to_hex(n):
+        return hex(n)
+import tensorflow as tf
+print(tf.__version__)
