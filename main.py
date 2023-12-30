962e61051f3b3085af72fb082a0c791a5c6c9e42
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:54:21 +0000

    sister traffic dick cruelty blow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def delete_file(file_name):
+        os.remove(file_name)
