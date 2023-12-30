5769d13e874ea927d769831ee1399b2529527ef3
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:31:44 +0000

    empty so 572enter speed lay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  def square_number(x):
+        return x**2
