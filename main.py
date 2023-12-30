383f1dedaf4afeba6b20f923cab5fd03ec11bf02
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:26:17 +0000

    near best ever hot burst

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def multiply_numbers(x, y):
-        return x * y
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
