f6d507951e17036543adc5336b901b9d284ef6d6
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:05:48 +0000

    cause throw belief couple exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
