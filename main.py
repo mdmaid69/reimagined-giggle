487297f9c266906199f25606e2a8117f3d1c30b1
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:42:40 +0000

    pattern comment 567enemy runover fun

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
