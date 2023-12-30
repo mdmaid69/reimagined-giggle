b9a2e6f907ffd332871b92ec03915b9c538126ba
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:10:21 +0000

    limit anyone regard near difficulty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import sys
+def exit_program():
+        sys.exit()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
