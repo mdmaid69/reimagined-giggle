936ceafe720873c2aee815b8f6ae2652de8d1ec9
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:56:39 +0000

    hate carry offer spend impression

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
