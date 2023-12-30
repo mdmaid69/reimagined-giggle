745d1d2a3a48c29f3d10e85a9bd33b099db714ea
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:11:28 +0000

    mixture history notice feeling sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
