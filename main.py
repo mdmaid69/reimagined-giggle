6509f3cf56785c64e56b08a06a562bec352771c1
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:32:43 +0000

    stiff tip separate already beginning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
