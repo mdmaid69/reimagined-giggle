0d96792fcf96a5ebe1099e58aedcda187b5020fe
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:41:07 +0000

    important accommodation smell declare extra

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
