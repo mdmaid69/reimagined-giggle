e2a5a7fea243b6582743b7e6fbd4f5dca268735d
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:01:17 +0000

    study thought original dress scene

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def get_array_length(array):
+        return len(array)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
