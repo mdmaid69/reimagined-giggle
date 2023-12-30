7b5c32129c7e17de50981649fb9bdc14b54398dc
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:43:44 +0000

    waste consult along never move

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
