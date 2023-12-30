349edbe8a8608bb7f019af4418ee95898ae6f6a6
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:22:13 +0000

    raise thorough each cut clean

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
