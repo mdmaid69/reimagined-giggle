37590a6ac10d6a988d1d092441ae2a796f328250
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:56:10 +0000

    family float contrast increase borrow

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-def subtract_numbers(x, y):
-        return x - y
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import sys
+def exit_program():
+        sys.exit()
