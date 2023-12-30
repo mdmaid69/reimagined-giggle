6984423191b9bc9e7c611e8dd980566e037f6d01
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:23:24 +0000

    send effect correct rise exactly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
