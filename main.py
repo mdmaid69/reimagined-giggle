56a758417ee53438ea27ba18a4f58cc05e35a6b3
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:35:42 +0000

    hardly imagination constantly whoever suit

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
