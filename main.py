a37f39fddb6d8dc752e31a1ca7640c0b11329c2e
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:54:56 +0000

    complain laugh accept difficult translate

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import os
+print(os.getcwd())
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
