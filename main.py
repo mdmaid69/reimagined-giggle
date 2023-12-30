e8bbd80825fc220f41c4461b414b5d7b96271790
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:51:32 +0000

    contribute appeal runover turnover read

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
