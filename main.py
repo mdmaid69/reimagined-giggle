74cadb0e09e04b74577addb2a7f7ed38104b9cb3
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:41:00 +0000

    dance represent partner certain declaration

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def cube_number(x):
-        return x**3
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import collections
+def create_user_dict():
+        return collections.UserDict()
