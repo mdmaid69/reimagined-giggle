c4c220a278d23a84b4438fce71544e7cc784fd53
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:15:14 +0000

    feed appeal sensitive bad goon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def remove_directory(path):
+        os.rmdir(path)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
