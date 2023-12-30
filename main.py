c028b63f2f62b122a5ff782f5fe3d8a9fdc61ff9
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:39:13 +0000

    voice build emphasis slow city

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-  def square_number(x):
-        return x**2
