5c4cf49c46a7479c1a1eff29cb2b9714ddb9df66
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:54:14 +0000

    famous nurse again prove house

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_density(mass, volume):
+        return mass / volume
