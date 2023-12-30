1a492aa8d7cd4cfa349c9ef7994df8fd802c3157
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:00:58 +0000

    polite least split yourself all

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import sys
+def exit_program():
+        sys.exit()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
