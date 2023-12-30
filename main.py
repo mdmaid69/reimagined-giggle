5d3419bf3ae343172208df6a70d5c5ad83adbb63
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:52:29 +0000

    without smile condition necessary brother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def get_environment_variable(var):
+        return os.getenv(var)
