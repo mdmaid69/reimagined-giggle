39a889143c4de658f3901106992b1fb0a064551b
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:25:51 +0000

    noisy turndown face work partner

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def get_array_as_int(array):
+        return int(array[0])
