7ea87350e8296fe59873ff2c99392c03b9a5d622
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:47:00 +0000

    grow tired Pp breakinto most

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
