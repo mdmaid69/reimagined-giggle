f28dc61abcab821132345f700b8930073dc2410f
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:20:00 +0000

    may respect goback(to) meet dress

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
