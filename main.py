e6788fa53085ffd31661fae41c6bd33ed893066b
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:36:13 +0000

    whisper section shape behaviour exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
