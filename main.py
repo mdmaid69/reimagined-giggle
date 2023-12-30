227dcc8fc5b5597ddc404050372197d81cc0c2ef
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:19:37 +0000

    custom tip bother major trade

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+print("Hello, world!")
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
