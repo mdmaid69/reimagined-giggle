a12b5f7024b6536709b28cc17b223c042e1c2bb1
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:25:20 +0000

    comeback emphasis produce funny joke

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def sort_numbers(numbers):
+        return sorted(numbers)
