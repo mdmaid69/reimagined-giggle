a9a897291b9e72ef1695c7bfe27ac3777130b868
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:31:17 +0000

    pass wet over basis similar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
