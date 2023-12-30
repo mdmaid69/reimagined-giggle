45bc6eb4db7425839e0154332071a7e6c9acfaf1
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:16:13 +0000

    produce strike actual write bottom

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import sys
+def print_python_version():
+        return sys.version
