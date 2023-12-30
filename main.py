2db1fb02206470b13c2ac47cbe9639744c5d8251
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:25:26 +0000

    do unlikely professional forgive regret

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
