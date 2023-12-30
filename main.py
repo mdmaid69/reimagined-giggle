0e0f4e564b38ca634c5abe1008214f0058802e3d
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:44:03 +0000

    slowly thirsty transparent talkoutof related

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_density(mass, volume):
+        return mass / volume
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
