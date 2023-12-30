80a34f27e82460d3cdf1854a59e700a7904bb23f
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:58:56 +0000

    deal minimum building her case

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
