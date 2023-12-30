9bab6a79e87b25ca04b5a264fa63ab893a18df94
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:36:14 +0000

    can generous estimate throw waste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
