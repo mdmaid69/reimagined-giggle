896e1659c66fc85ed527737912715968c52c9fbf
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:19:46 +0000

    attempt careful whether delicate without

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
