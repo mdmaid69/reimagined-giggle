baf21659a706339b7ca4e61f6f1b4c2a547e3722
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:03:18 +0000

    cut makeinto capital without sore

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import random
-print(random.randint(0, 100))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
