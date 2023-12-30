1425b8d751fded7bf52e581e29a4e644ad6d967f
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:46:51 +0000

    cruel accidentally dinner watch bottom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
