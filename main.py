2a8ae547fb7956e0d5a0c64b34a93781bbeb35f8
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:18:15 +0000

    almost possible male rough nurse

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
