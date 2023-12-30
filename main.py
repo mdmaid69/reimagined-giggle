540167c1f07d25bc3f0fb4847179ad7f5faeb930
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:44:09 +0000

    soak surprising knowledge nature brave

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
