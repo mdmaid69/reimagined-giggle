f5ad0a2c07b0173f7f1601f4d06e066f72778b52
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:03:23 +0000

    record succeed crazy rare now

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def get_array_as_int(array):
+        return int(array[0])
