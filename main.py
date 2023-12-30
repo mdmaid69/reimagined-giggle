64322c469a33b9647d668a56ca43baad9c94db5a
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:51:23 +0000

    private present scheme later oil

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def reverse_string(s):
-        return s[::-1]
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
