a3f0a7ddd55391423f18e79c44dde1969316bf7b
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:55:21 +0000

    accident target game stay miss

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
