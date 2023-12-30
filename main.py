e4637ac38fdd2e3d93fc43ac2ccc5d17beb2db37
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:04:30 +0000

    little bringdown wall pass feel

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_length(array):
+        return len(array)
