6b5a57e7524ca94531e6af42b08ef672e2b9514c
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:53:50 +0000

    book excited doubt substance attach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
