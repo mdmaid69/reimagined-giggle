ef2dcedd737ffc437205d4918d9a502956b5d674
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:31:46 +0000

    country will kill narrow into

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def count_characters(sentence):
+        return len(sentence)
