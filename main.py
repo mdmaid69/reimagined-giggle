1e9425b00d6b4c2e60dd65f62063801b55d632c1
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:58:52 +0000

    perhaps challenge basic month unless

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def greet(name):
+        print(f"Hello, {name}!")
