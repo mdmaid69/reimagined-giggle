2a4efe6d38b91b545a77dd6ff08b7e148d9a0840
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:07:56 +0000

    breakup patient getback pen his

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-name = "Python"
-print("Hello,", name)
+  def square_number(x):
+        return x**2
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
