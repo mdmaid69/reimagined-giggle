49c6cb99ef320ade32015f49d3752e74c06b5bb5
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:58:44 +0000

    estimate drop who same shoot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
