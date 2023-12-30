c1bc0c1149b2c800c0edc61aad93191aa720519b
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:27:09 +0000

    Bb much nervous jump depend

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
