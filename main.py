89dd3a01efbdca6ff099b4e66338931e34b4347d
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:04:00 +0000

    couple today story crisis final

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
