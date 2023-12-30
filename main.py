56c4f902c99ac15141e6cc2db088eb83145e9d17
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:02:40 +0000

    away back show space force

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
