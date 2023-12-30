7f7c4f372a0a58bb207526e7f526cc2431e4f97a
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:47:23 +0000

    cruel test back calm after

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def is_palindrome(s):
+        return s == s[::-1]
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
