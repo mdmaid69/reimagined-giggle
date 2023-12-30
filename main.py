3a780ba034c7cc6ecdafe310e9e1d4686079b730
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:15:13 +0000

    bring none arrive regard mass

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+x = 10
+y = 20
+print("Sum:", x + y)
