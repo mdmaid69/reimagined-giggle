f9162d992a3e8482cc246db0c4cd4ef705e28174
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:16:17 +0000

    out vanish protect another close

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
