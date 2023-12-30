3656d0f4fd8fe56dbef127b0c30bab85b7ef7ce3
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:01:30 +0000

    jump else question quarter sure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
 import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
