21fd982ceaca713826f3c38e64940f856a2f2a5d
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:13:18 +0000

    apply foot level fast base

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import platform
+def get_python_version():
+        return platform.python_version()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
