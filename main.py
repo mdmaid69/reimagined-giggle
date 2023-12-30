c15a5d83a6ad7ce72fe62b4a01e970a244306769
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:47:30 +0000

    crash remain classroom might consider

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
