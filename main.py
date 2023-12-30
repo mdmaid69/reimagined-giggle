771fba56c9eb1d3b9775a3bf9935cec70e8621af
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:23:46 +0000

    best plate sensible collect uniform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
