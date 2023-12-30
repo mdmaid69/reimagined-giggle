159ca01dae91ffb66b8c6a9510f1bac507ec871b
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:27:16 +0000

    measure keepoff reason respect left

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+for i in range(5):
+        print(i)
