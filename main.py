3222679bc7294f45bbddf78320bc23771bc762d5
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:26:26 +0000

    young ready traffic Ww important

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-print([x**2 for x in range(10)])
+import sys
+def exit_program():
+        sys.exit()
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
