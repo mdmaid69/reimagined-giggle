883844caa53080c5b8874781b00f045f9ddbab84
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:16:33 +0000

    should think pair walk keepaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def convert_to_octal(n):
-        return oct(n)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+text = "Hello, world!"
+print("Characters:", len(text))
