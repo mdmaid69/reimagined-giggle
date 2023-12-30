ce01e49afc49ca76a3e85714fa43be3b3fb5c39b
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:38:47 +0000

    someone quantity lie division table

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def divide_numbers(x, y):
-        return x / y
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
