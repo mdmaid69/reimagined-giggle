491ee5b0db1baab58b8481ec9478fe4637889f10
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:49:29 +0000

    direct copy rather hurry punish

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_item_count(array, item):
+        return array.count(item)
