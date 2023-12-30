52b90109275a13966010e4845e0dbbbdbc04052e
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:40:28 +0000

    taste breakthrough length great pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
