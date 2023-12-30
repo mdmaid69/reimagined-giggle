03dd78c4725fdb1f52739fc8dbf6e437304da508
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:50:58 +0000

    callout hide trap undo home

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
