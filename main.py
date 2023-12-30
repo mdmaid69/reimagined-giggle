82b3c8c5af07ad63a79b24bd86b97c34a77890fb
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:38:36 +0000

    take study estimate food again

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+from collections import Counter
+print(Counter("hello world"))
