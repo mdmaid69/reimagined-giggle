216725a3ad5a508c1d5df174aff542ecb420d5e6
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:34:41 +0000

    consider he Nn broad train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
