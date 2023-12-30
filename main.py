585d0769a5a86c6da0ae65e32c518c7cb29119a9
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:33:18 +0000

    high check drive top but

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
