321ff443bc0a54ed07194db8902ad02d2c6a2d5e
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:28:45 +0000

    back building declare rather but

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def is_even(n):
-        return n % 2 == 0
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
