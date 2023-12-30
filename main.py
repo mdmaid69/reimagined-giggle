f0a383cd598a0ff706bec5923a22fd5e0895d84b
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:21:01 +0000

    dream damp file everybody tell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
