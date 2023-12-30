f506140c6029de985dd77032c37b5978715bb86f
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:00:48 +0000

    weigh active this wish attach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
