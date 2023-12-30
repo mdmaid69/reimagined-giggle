f06beba68ec7acc202ced9798abd6c324abf3ac7
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:40:09 +0000

    criticize low worst can hair

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def count_characters(sentence):
+        return len(sentence)
