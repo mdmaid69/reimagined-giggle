afca75a8891059025596d7166006cc17c9fc461f
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:24:51 +0000

    emphasize colour defeat nigga after

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-x = 10
-y = 20
-print("Sum:", x + y)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import os
+def change_working_directory(path):
+        os.chdir(path)
