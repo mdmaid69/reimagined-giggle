06458c8e196e01f468ca774595a442d7cc907dad
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:01:20 +0000

    sure deal excellent band Jj

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
