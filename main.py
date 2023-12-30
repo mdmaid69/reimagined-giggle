dce041d4fba725bd1bee277e6ca45102025ce041
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:06:36 +0000

    as takeaway cruelty them official

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def add_numbers(a, b):
+        return a + b
