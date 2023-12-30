e4f8dfd5ca1ca0c032bf2dd76ee2ce2aec6ddf12
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:41:09 +0000

    offer various comment reject generally

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
