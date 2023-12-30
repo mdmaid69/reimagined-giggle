2437d76bead0b273ae024b5d773e1787bb857107
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:29:48 +0000

    all statement declare concentration approval

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import numpy as np
+print(np.array([1, 2, 3]))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
