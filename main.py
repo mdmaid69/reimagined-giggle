b4985c4d81821118851b626824812095cbc8680d
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:52:18 +0000

    tear might understand squeeze record

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-print("Hello, world!")
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
