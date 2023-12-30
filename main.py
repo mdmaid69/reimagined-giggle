99bab1cbedbe38fa4463144a5ee7ec04881a5b34
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:43:26 +0000

    polite inside positive account sing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
