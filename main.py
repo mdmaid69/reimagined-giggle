b663295c7bc2f36d986b2a9698c1703239871568
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:25:52 +0000

    steal container love damage bar

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
