9c511044e08b9038ee17c3d5062ba0e0a7886d28
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:55:51 +0000

    stamp identify trial put sense

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
