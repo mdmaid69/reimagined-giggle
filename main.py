0b6d4279e2d0794c6e7eb9b73b7ac11e2c39e88e
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:28:01 +0000

    chat work strict speed force

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
