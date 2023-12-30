60e8a39caba0129d9107d1ed796dd51a8df57c57
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:25:12 +0000

    forget remain else slip argument

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def reverse_list(lst):
-        return lst[::-1]
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_itemsize(array):
+        return array.itemsize
