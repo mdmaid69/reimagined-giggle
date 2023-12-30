46406f8058f65fb84038710ed5783ed730a6620a
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:21:01 +0000

    contrast achievement bath polite pollution

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  def is_even(n):
-        return n % 2 == 0
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
