359cc1837a0e5f1dab64940ddde40e27ef368ca9
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:17:15 +0000

    regular equipment flavour lot scream

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import sys
+  def get_python_version():
+        return sys.version
