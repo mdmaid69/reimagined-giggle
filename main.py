5c7ff7e5b3f7f984ddbc3ef2ff4afcf23cb85dae
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:07:55 +0000

    underneath surprised popular funny note

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
