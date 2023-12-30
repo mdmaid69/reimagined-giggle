4d73718dc19205bbb24470e1bb59f5d60a9b7c4c
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:45:38 +0000

    store hand trust surprising Rr

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
