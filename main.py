ba9431862efc09dc0b9390f6166c7e2ca3ba2512
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:11:28 +0000

    trade hold matter lesson bring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def get_array_as_set(array):
+        return set(array)
