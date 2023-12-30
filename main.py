55d5b55a195a95cb643db42dc2c26ae832dad56e
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:02:19 +0000

    nonsense wire but generous occasion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
