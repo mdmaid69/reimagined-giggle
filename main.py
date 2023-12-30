8afc9e6b433fc02d2527c6f3c0f6ceb7a35c53f6
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:51:30 +0000

    proper plenty broken marry regularly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
