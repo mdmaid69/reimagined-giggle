6cb4a5eff32e881252e255c86f51ebebe5a59faf
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:42:04 +0000

    protection appear or boil will

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
