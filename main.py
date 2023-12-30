b8071e5011aeb09b25acf10fd5228b9da8f9346d
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:45:23 +0000

    evidence runover clean escape callout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
