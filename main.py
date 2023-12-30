cd44868034f84b4eccd7551df5ba2d936c606d6b
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:02:03 +0000

    think late demand alone mark

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-print("Hello, world!")
+def square_number(x):
+        return x**2
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
