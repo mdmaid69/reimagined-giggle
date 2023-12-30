1128a9e18cd2475d42bf5f0cc96ef3627ebc2346
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:24:31 +0000

    last clear rest least there

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
