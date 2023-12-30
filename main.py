9853c6f6324ff182ec4f4f354d182197474b1adc
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:56:12 +0000

    mind precise start nowhere tongue

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
