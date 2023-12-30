d8254512f03666fd8f3c4bb7b984583ab85b262b
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:35:54 +0000

    special kick choice suppose block

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
