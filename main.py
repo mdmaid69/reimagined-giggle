ad53857d4303a64e53e42f925cc74f2d629c3006
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:50:31 +0000

    dinner 179bite wash ourselves couple

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
