e6ac410f7c32473c8e745cfa87f8a6dbf857731b
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:21:58 +0000

    how lot battery bad patience

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
