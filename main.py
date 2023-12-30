b1195e302c0a6331bcc94b46f147551b550f1375
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:08:19 +0000

    man somewhere just handle substance

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def multiply_numbers(x, y):
-        return x * y
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
