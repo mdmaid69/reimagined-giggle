cc285cd07abc59b8830704a5d08c375f9ae290eb
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:42:00 +0000

    act week guide insist pour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def add_numbers(a, b):
-        return a + b
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
