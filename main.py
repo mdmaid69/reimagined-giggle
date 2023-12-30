4d0d00d5753158b7b90fde1f1154c49ba1b0b232
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:47:45 +0000

    fun blind bother head scheme

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
