c687ae6496d660a2a20420a7b558a8e7eb63f862
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:30:49 +0000

    satisfied memory save swallow wise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_density(mass, volume):
+        return mass / volume
