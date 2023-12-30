631d744b1e921d902e10353eac9b5fba7fddaca7
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:58:56 +0000

    spare sensible supply plate sight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def convert_to_hex(n):
+        return hex(n)
