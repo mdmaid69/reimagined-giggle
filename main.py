71c4234c00ea1fc6b5b6bd5b53219b19ea1c4489
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:57:24 +0000

    by class disagree much off

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-print(sum(range(10)))
+def calculate_acceleration(speed, time):
+        return speed / time
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
