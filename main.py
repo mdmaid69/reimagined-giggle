0b0e3bc73d68d93f205fa8c69c525c63fcd83675
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:05:53 +0000

    turnup callup now earth against

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def append_to_array(array, item):
+        array.append(item)
+def square_number(x):
+        return x**2
