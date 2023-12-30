37d4aa3ff3504900977abe85249d93bb3818f40f
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:22:03 +0000

    normal by brush bit order

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
