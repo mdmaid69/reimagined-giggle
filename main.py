bfe534466f23a2362a257c89273a1abff60591bf
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:33:50 +0000

    fail goodbye stick freedom discovery

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
