d512de20479dcf885265e2acdfdd03acfbfdca35
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:25:16 +0000

    564encourage blue mine broken disturb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
