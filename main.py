95b17595003d8848a1beffa06b0b4b48878d64fc
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:48:57 +0000

    wide flat shout world either

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
