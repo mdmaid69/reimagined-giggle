db2dd333a5fb0e7e5fc87fbc6b134587480a9568
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:47:23 +0000

    whether content sleep contribute equal

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def convert_to_octal(n):
+        return oct(n)
+  def is_odd(n):
+        return n % 2 != 0
