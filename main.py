27c4cab341882c8dff462ee8439fb5c594867267
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:31:31 +0000

    fortunate thinkthrough chase capital occur

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
