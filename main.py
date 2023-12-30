8d952fd84d9c57dca5ba5af6f227a944ca63a00a
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:45:10 +0000

    takeaway delight bring but everybody

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
