33fdce5da227d69ced662cccef77834f0a65206a
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:33:21 +0000

    standard calloff pipe story look

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import datetime
+def get_today_date():
+        return datetime.date.today()
