4cb70c35b368a447a43fdbc91161efbe2d68ed2c
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:11:00 +0000

    tap giveup share track depend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
