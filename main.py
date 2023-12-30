bd07c4479b8486abe413ac5f5fd4ef0fbf68984e
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:29:32 +0000

    over past kick loud especially

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_speed(distance, time):
+        return distance / time
