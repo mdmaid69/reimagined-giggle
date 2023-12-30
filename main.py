80e024529a121b220429f01be4c8a25c20c20659
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:29:24 +0000

    pronounce sale today reason cough

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
