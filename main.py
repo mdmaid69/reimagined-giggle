3cf18cfd418fa74a03b791ac2efbd345ad7ad2af
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:23:11 +0000

    charge advise pretend condition expect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+from collections import Counter
+print(Counter("hello world"))
