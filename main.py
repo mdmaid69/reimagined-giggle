eb31c85ada8a9dad8f6db476931939bbe50221c9
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:07:08 +0000

    influence spoil careless bored sign

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
