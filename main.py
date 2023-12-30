eabdd6094bc1155305c5d6d6d34157b5c7409ed7
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:00:20 +0000

    stage example workon real man

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
