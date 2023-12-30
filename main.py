8e7f3ca335590ab6c376322d90fc900453e2c28a
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:07:53 +0000

    effect damp intend accidentally between

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import platform
+def get_os_info():
+        return platform.uname()
