4bc6a84b5f53209b7de3f75188943ff097f8d396
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:32:19 +0000

    search like violence unhappy weather

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def convert_to_binary(n):
-        return bin(n)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
