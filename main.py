9c1563f9164214b783ee6515857ffddee0cafc2b
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:49:57 +0000

    meeting build necessary wait impression

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
