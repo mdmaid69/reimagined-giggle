a805dbe0f6f047561c317800b8962f93fad4daf6
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:40:30 +0000

    common trip pride duty expect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
