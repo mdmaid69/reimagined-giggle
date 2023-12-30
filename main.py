2880740df61fea1a2ece67ae20fef9cd3f7b3505
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:03:51 +0000

    exploration fear think touch bringforward

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
