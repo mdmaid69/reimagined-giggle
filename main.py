029dea1ff6583f5d721e4e3c44876941e79393f6
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:41:22 +0000

    lonely practice mystery be surface

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
