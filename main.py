cc2b65f5a87c30ebc34603411ac2d2b0899d0255
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:37:26 +0000

    fortunately protection past just cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
