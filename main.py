85378a17467d28ec61fc241e49108f1271804758
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:24:11 +0000

    town surface easy release big

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def reverse_string(s):
-        return s[::-1]
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
