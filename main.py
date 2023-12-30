6a3d783b18055739100fc92c43c432c0f5bd8de1
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:51:56 +0000

    position early rich correction knowledge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
