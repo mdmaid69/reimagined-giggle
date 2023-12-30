6828aced5f5020b0770e63a5316ae65eebb62f1f
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:38:34 +0000

    want run contents only square

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import array
-def get_array_as_str(array):
-        return str(array)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_area(radius):
+        return 3.14 * radius * radius
