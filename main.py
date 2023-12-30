46449b4400c7c9e71e202ad4f327ef6304b5b967
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:46:27 +0000

    shake flavour material whistle accommodation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import json
+def convert_to_json(data):
+        return json.dumps(data)
