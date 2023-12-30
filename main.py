e85087e37e0e6a512da2295df135c270ddcf89a0
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:10:18 +0000

    uncertain bright paper brown thank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-import array
-def append_to_array(array, item):
-        array.append(item)
