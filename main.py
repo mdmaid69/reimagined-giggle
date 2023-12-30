a0c5429748eade6697b9c98d917b28f982f7a6e2
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:34:55 +0000

    round beautiful depend hold sleep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_length(array):
+        return len(array)
