4b32dc72da6e6ae825cfbd2f00180d0de9a30ac9
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:55:39 +0000

    path for amount swallow insist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
+  def get_file_device(file_name):
         return os.stat(file_name).st_dev
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_cosine(x):
+        return math.cos(x)
