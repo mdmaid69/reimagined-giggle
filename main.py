0a19b6f06f449e39783fe5cae49f021c4c9a5c37
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:35:38 +0000

    takeoff breakfast road range deliberate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def convert_to_binary(n):
+        return bin(n)
