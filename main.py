06fd88a6d2aa58a715c66cb1dedf706280d86602
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:14:44 +0000

    please preparation along suddenly cover

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  def convert_to_binary(n):
-        return bin(n)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def subtract_numbers(x, y):
+        return x - y
