b19c08411053fe924d3976c4d6cf8952cb793aca
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:16:24 +0000

    push suck central owe neither

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
