102f886191539ceed6a19c9d4a095794285995ca
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:15:42 +0000

    expensive release impression ilovedicks refuse

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
