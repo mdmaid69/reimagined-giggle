bd261f55284ef6f340160be84d348063f70d66be
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:17:53 +0000

    edge part equal attempt behaviour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
