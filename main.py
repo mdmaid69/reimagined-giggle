dc34c0643d50eaefc299db14e4af4ab8785a80d3
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:40:32 +0000

    clear coat commit part mouth

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+print(sum(range(10)))
