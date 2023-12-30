bfdaed5a153e60aea4ae0c6693d70b26df07f2a4
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:38:30 +0000

    contain term slight bet clean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
