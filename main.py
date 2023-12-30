97cdb607e0e0eddf97cc7838b9f9df8a23f0b1cd
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:39:51 +0000

    term address famous when stone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_area_circle(r):
+        return 3.14 * r**2
