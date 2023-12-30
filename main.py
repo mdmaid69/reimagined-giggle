5cd166452267ac439362def4214d38c765699185
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:42:48 +0000

    type snow sing control table

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
