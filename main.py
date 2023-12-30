103557a7944c9271e6cabd1ccb74e155064e689c
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:51:38 +0000

    mean wonder shoulder accordingto enthusiastic

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def multiply_numbers(x, y):
-        return x * y
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
