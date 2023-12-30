8e71fd075aad00486ed18213d4b1e3531cd2ad0e
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:03:30 +0000

    this tear free live can

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
