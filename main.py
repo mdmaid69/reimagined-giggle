964ea4326a493c35ba295f2f2f529cbcb63c9666
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:31:22 +0000

    round invite base disgusting discussion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def calculate_power(work, time):
+        return work / time
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_as_float(array):
+        return float(array[0])
