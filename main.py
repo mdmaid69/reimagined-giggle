1b1cc029922cb651c7aaf5362d7b43b20c0e93d6
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:30:24 +0000

    offer unfair confusing song speech

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
