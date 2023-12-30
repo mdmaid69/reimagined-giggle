fc3e7b5886b9cc6ba676784aabfc6cadcc998735
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:05:23 +0000

    top less punish theirs help

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
