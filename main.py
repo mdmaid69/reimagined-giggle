de4588c7225701ccc6badfce6beefa9fc1200a03
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:17:44 +0000

    shut hot consist throw everywhere

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
