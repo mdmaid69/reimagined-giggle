f1587783d546eb9497487b1963efe9bd9f3b0b0b
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:00:30 +0000

    slight depend exaggerate else pen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+print(math.pi)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
