9f91b4164b19040121d756b3dd31eb982f49bd57
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:26:34 +0000

    test laugh past law he

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
