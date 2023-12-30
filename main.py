ffaed0692bac5cfb121931fefdcfbc55fbda4ae6
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:19:09 +0000

    drink reasonable close forward coat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
