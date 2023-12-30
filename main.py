d5c2366b13013409971c8e9a7b9d3218ef0b373d
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:32:28 +0000

    suit lay walk which Rr

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import collections
+def create_user_list():
+        return collections.UserList()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
