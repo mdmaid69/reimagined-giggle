54cc4fc8693ae04b7aadf04071fc5b8a0aba7533
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:26:39 +0000

    shallow market unless like national

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
