48fbd7cf8422aaeb8cf445543b5d9ca2044f5de6
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:14:18 +0000

    justice shadow trial brush fail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
