3cd286f1657690bfdb09d7dae8c157b9e0fbcf8f
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:02:23 +0000

    life pencil argue arrival attitude

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-x = 10
-y = 20
-print("Sum:", x + y)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
