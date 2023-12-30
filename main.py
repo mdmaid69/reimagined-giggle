4ba5b3bc74962e2f958bc57dbd3340dc37e6e325
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:48:03 +0000

    even hurry plan bright quality

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_length(array):
+        return len(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
