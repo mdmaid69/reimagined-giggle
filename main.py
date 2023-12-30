17c537451864b6c30a992c5a4b984c329517711e
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:18:24 +0000

    suppose happen distance design cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import sys
-def print_python_version():
-        return sys.version
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
