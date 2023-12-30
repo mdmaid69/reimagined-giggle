453bdc3a245d7e5793689064f291e64a3389f625
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:54:29 +0000

    benefit 566end case instrument tape

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import sys
+  def get_python_version():
+        return sys.version
