758a211ee3197c45c87963062259b1a15b762bb5
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:20:51 +0000

    skill comeback church away hit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
