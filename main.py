f980b6b1018c189d26defecddc699d1e75309b5a
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:17:16 +0000

    allow male scene frame else

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_circle_area(radius):
+        return math.pi * radius**2
