88f944bd6a682384f935630b79313690dde93de6
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:11:26 +0000

    moon lower drink pretty probably

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
