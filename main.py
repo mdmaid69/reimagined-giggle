54b65a936067a38ac44722865991e013c71c123e
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:51:47 +0000

    react vanish use smile claim

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
