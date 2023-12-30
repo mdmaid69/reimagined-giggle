3940f73bca6c5fd542cf2a57b6a319e58a4cc824
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:23:06 +0000

    speed reason hard gather by

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def subtract_numbers(x, y):
+        return x - y
