77f05cd22738e201e8c68686a13722f3dc7c4ebc
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:46:36 +0000

    watch watch deaf exhibition soil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def is_odd(n):
+        return n % 2 != 0
