92e183c6f103892780f6ec835bcccc0c9e57e8e1
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:48:57 +0000

    set by rest exactly reject

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+x = 10
+y = 20
+print("Sum:", x + y)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
