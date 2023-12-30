b10af0925dc07fa2eb813ca42cfb3c7b39809706
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:09:43 +0000

    used battery you gradually snow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
