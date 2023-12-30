8959735998b79569f796d5a8b5fb4a32844755fa
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:08:40 +0000

    space appear build exercise bring

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
