e1d7e461d32e8decb1c5adc82688f1fe674f31a3
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:12:59 +0000

    here motion gooff main spot

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def convert_to_binary(n):
+        return bin(n)
+import tensorflow as tf
+print(tf.__version__)
