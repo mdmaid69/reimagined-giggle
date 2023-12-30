5d7098ab2ad826e3b1b082b23ada05ec22ed5489
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:44:51 +0000

    alive bin bother pressure if

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
