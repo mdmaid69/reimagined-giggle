96f33160804f760a0b93addc5ba5742e00d5330c
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:13:43 +0000

    deep destroy steep good area

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
