867ac2661567bf2f5aa203d6ff51cfb699042443
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:59:59 +0000

    tough bang turnaway fairly interfere

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
