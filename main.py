e84f09f4f496fb9f0bc068885e5f0b3fbddfca65
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:31:45 +0000

    among insist apply hole read

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_work(force, distance):
+        return force * distance
