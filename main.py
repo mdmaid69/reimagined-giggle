954f26ea927c655fac58ab2e591234999c929bbc
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:14:46 +0000

    set pen then desperate pocket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
