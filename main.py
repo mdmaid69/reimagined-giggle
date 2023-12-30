2b267be22a08eccb59cfe4b21ce42e215473fffd
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:39:44 +0000

    material by class image century

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-print(os.getcwd())
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_acceleration(speed, time):
+        return speed / time
+def add_numbers(x, y):
+        return x + y
