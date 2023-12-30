f75d80c0f7840c5178e937309054c985ad086dc0
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:31:30 +0000

    somehow improvement progress shop lie

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def count_elements(lst):
+        return len(lst)
+  def reverse_list(lst):
+        return lst[::-1]
