55c3fa9bb644463cc828334dc83b7ce3e143f91a
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:53:19 +0000

    pile name careless disturb spray

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import datetime
+print(datetime.datetime.now())
