5191bd72704530071a71f0564c07a45794836351
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:19:01 +0000

    excuse press hurry test sure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
