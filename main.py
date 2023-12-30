e3223828a86cca99fe614fc96d6073c2fe319ec5
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:55:52 +0000

    wheel after negative train attach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
