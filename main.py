e2d78083b8cff8dd8ca6f0f6a7b7a4035f01ee8b
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:22:21 +0000

    waste closely be comefrom opposite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def convert_to_binary(n):
+        return bin(n)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
