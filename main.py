a6b95bff879c661a8639553ade6786c126b5b28b
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:39:06 +0000

    square swim listen she able

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
