910f1703bf6354bcbef65e0d1591322f48502623
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:06:30 +0000

    sharp breathe above excuse any

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
