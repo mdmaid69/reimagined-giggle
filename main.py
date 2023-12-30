e6128e92db5c153b4f55aac511fec140a24b9181
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:33:36 +0000

    regular force relax address worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
