e913f62555841125ba07ac58c5a0bc911fdccebb
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:52:40 +0000

    joke trouble understand operation fine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
