1a073e2015fc9330b11474cb30b18fda9cbdf796
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:36:06 +0000

    doctor sink large weather clear

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
