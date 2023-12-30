135dc40a7cb96feb1962cf43d4f2b2c37132b694
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:05:05 +0000

    disagree bunch break consult unfair

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def greet(name):
+        print(f"Hello, {name}!")
