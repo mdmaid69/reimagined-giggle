e8cb554a7d8f8548de47c5e1741d96d3d9305f68
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:36:52 +0000

    I answer draw bye runinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def convert_to_hex(n):
+        return hex(n)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
