e25207d539aa0cfcb525bc9f0241d1bd52a2085c
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:06:49 +0000

    bringforward tap punish sail nonsense

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+for i in range(10): print(i)
