72377060dcf82029ca6c6cea88acd530114b6f06
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:45:39 +0000

    shade crowd contain rub power

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
