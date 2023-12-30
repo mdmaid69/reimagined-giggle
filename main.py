bc2310eaf76dd8351bcd6f22eed290f85ee1c3ee
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:12:20 +0000

    566end prefer never talk piece

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
