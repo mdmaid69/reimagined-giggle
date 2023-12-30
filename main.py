35c6a0c925cbc870542395a5470efd717d2be445
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:47:49 +0000

    stick instead typical road straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
