2df40d58059baa70f6edff41b72b7290ec28c7a9
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:07:20 +0000

    thumb Ss cum report letter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
