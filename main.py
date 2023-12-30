6b6ce3ea9c7ed0f8070805e660698570f883e3bc
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:55:33 +0000

    class fairly office buy side

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
   import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def create_numpy_array(lst):
+        return np.array(lst)
+def find_min(lst):
+        return min(lst)
