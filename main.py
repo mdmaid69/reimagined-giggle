0cd1030039346fad6250591bbd008b0ff9d1a137
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:43:56 +0000

    give score receive common report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_counter():
-        return collections.Counter()
+import sys
+def print_python_version():
+        return sys.version
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
