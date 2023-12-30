79f0e344ee5b7658e04a1de01048e5aec7850240
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:35:12 +0000

    emphasis whistle pronunciation exercise trade

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def sort_list(lst):
+        return sorted(lst)
+def add_numbers(x, y):
+        return x + y
