a6b0085993dd0eb6e8b16c88e82d3411b033121a
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:59:46 +0000

    her lesson protest grade long

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import re
-print(re.match("h.*o", "hello world"))
+import sys
+def exit_program():
+        sys.exit()
+print("Hello, world!")
