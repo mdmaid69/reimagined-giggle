667392b9e5bab1402ee5e958e0c8e5e350501f41
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:41:20 +0000

    practical judge medium teach gift

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
