e76b2470fb6179c47c5f3de6a7f72a8978f547f2
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:54:31 +0000

    professional time under accidentally getout

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
