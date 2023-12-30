fedaa0daa8288eb8d696e15eee0ed427d56d7242
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:50:59 +0000

    recognize commit people tip seem

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  def add_numbers(x, y):
+        return x + y
