9abea6abd661238268fb3110f4d6071107a9d693
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:57:23 +0000

    geton present bend suffer spring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
