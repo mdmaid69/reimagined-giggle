4ecfb2619304b65fe0798c0cca2f1638a5b95fb4
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:47:18 +0000

    necessary busy block noisy lunch

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
