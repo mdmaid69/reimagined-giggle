625ccb1c652f5d14dd18757b70778faa7652e545
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:21:41 +0000

    by understand satisfied emphasis noise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
