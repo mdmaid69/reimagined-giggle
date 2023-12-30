c13a5a4524b32070cf0cc3f16db3b0b897ea28e3
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:50:59 +0000

    slice guide final wide Mm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
