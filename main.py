eec2d6266d7c1ecc571a4e3358b7308a2c9fc765
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:07:34 +0000

    deserve direct but trouble sick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
