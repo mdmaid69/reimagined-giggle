4ff185abe5d0d7d66d95a45d4b1ae86ed616ddde
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:33:52 +0000

    popular plain mend use earth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+for i in range(5):
+        print(i)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
