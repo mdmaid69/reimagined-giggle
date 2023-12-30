11e7841d42885bffac284d79de28134c53c845a7
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:25:55 +0000

    creature error shake everybody choose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import array
+def get_array_length(array):
+        return len(array)
+def calculate_speed(distance, time):
+        return distance / time
