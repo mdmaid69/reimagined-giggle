d38fe5f8d8136137d7b84c3c4ea566343d19062b
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:48:04 +0000

    Hh name comfort spare plain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
