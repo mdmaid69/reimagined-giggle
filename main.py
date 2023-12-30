d4175c65cb8bb6a03471b5839ee70fa81a1dfc18
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:59:15 +0000

    rain no polite letter keepoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
