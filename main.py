a3c69be9c01b7d5f7b066f8e40ae8f16db28ec7d
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:29:45 +0000

    month attract care burn wealth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
