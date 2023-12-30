e955a034ac76f54350aa1d4a777a098f99a012ad
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:48:49 +0000

    realize turnaway spend develop skill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  def calculate_area_rectangle(l, w):
+        return l * w
