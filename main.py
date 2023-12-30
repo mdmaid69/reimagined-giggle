622c69d657e3ce140ae1362252c55c0755c52964
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:10:58 +0000

    necessary sore work letter bin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
