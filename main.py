7a96f7d4353594c373a22cca2731515d29b9e3a9
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:15:27 +0000

    thank leg label yes attractive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import random
-def roll_die():
-        return random.randint(1, 6)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
