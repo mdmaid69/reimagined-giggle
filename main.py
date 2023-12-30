5e9caa1cfbed683ead602910662d0d8af567a281
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:06:01 +0000

    normal part cause amount survive

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
