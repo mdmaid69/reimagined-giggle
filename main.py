ec725d21a11f74e9924ad03ba9506eb431601fb4
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:09:05 +0000

    of top year hurt her

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
