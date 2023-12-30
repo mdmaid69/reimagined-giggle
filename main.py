6c041e5a4d92ab59a71d0dd7dfe0b9b8eebd78a7
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:18:07 +0000

    responsibility correction relationship appeal equal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
