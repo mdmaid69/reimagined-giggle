2a55eff48bd1e91623a704dcfc3ae231be6f89e1
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:20:39 +0000

    drawing lookback(on) consider Mm yesterday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
