8ad6513df236a0559a5ea83651047da86174b9ce
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:59:05 +0000

    disapprove pain movement joke worth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def get_array_as_str(array):
+        return str(array)
+import time
+print(time.time())
