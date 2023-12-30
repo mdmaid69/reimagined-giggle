3a61db6c2b761f55d115c1c84b4aa9ae6b0a3fba
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:32:40 +0000

    godown number reserve makeup trade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
