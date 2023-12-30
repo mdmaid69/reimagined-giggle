f98b13507e8b40131a61268851c53bc849822b6c
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:30:07 +0000

    hurry serious motion main post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
