31083c4a996b8d3bb1c7cb4df7ea3d40cd3015fc
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:24:22 +0000

    serious reject safety Dd yet

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import datetime
+print(datetime.datetime.now())
