e42c473c78b10217382aa07b94ec055c4f0b8241
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:25:49 +0000

    read manage comefrom influence march

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
