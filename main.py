9589f9e64e5b3846f925032ad29315fc4dbb6d00
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:32:30 +0000

    rarely advanced again couple appear

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import numpy as np
+print(np.array([1, 2, 3]))
