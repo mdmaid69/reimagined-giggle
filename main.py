6c49ca41243393923c536803a4060c1bee300ec7
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:51:04 +0000

    ocean none it flow interfere

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
