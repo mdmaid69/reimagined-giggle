8a2b23a2d5abe0beaec177c3324267d8c7540a9f
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:37:13 +0000

    below comment classroom 573entertainment bring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
