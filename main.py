2bed716b708d1ebd7c75e1a92fe6c7f80c462136
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:31:12 +0000

    thumb object petrol law shopping

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_work(force, distance):
+        return force * distance
