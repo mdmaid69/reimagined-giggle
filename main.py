e17196104a6fd0fcf2eb659da0085b4234565e95
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:43:48 +0000

    movement shopping possibility more face

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def convert_to_hex(n):
+        return hex(n)
