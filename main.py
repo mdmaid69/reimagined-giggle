aad57f9ef54af172dbf603c77ef0ae46bc6f0035
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:04:06 +0000

    dead before prevent pot 570enjoyable

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_item(array, i):
-        return array[i]
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+  def subtract_numbers(x, y):
+        return x - y
