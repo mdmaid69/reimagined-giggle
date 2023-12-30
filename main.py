66097cff9102a9be15f58033fe1e11d0c12c8bdb
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:50:53 +0000

    breath frightened short see lower

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
