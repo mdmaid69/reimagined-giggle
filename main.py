f30a0413883d471ae2980bd5b4f857b1134dfb86
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:43:03 +0000

    test inside general connect to

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
