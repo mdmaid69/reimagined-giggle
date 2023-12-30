2512c960eedd561021c57b26741a4f3bc6f5517c
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:43:04 +0000

    comein live single nose square

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-import re
-print(re.match("h.*o", "hello world"))
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+for i in range(10): print(i)
