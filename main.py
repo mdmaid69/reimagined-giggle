9aed178158e3d537f3b0948fe63a8ba3732d8ce1
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:21:52 +0000

    spite community storm name part

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
