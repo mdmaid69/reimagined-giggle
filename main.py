90e2a31e9f807ca056a32f4ad7c959ab0f61064a
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:15:07 +0000

    part mind exact off false

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-print("Hello, world!")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
