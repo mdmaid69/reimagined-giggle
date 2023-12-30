21a8cd4d50c3e84bf0ad8f65b719a3472072db26
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:16:34 +0000

    later main concerned opinion trial

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import os
-print(os.getcwd())
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
