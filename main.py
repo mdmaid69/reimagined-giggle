35a6d49461f8aa3eb28b3de4bb618db5ad84d3c6
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:48:40 +0000

    study content someone variety must

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
