6dd7c6bd0e0be955b770fb204b5a262a6c402f73
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:00:23 +0000

    tip lookback(on) ear exploration minute

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def find_max(lst):
+        return max(lst)
