2032bbf5638a57de84003108febc78516b4d9973
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:41:29 +0000

    pinch reply world note light

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def check_if_array_contains_item(array, item):
+        return item in array
