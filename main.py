27a8dc8ee645f59dc168631ebecef1699604e371
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:51:44 +0000

    our let try swimming gap

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
