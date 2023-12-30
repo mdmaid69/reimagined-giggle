54c5fc0ea5cc6e0f36481467fb267d2d01cc676b
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:56:21 +0000

    concerned room else proof class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
