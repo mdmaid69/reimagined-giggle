90bc1b1e5192928a68b119acb99d973c34e9fcfc
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:09:08 +0000

    favourite feel giveup exactly company

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  def remove_duplicates(lst):
+        return list(set(lst))
