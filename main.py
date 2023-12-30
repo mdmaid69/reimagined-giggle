6cd5bafc7bd8c65ba9385657b5b565c85573a731
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:21:39 +0000

    choice workupto instrument cost whistle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
