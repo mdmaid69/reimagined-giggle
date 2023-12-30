2947bd1925935ed3289c73c1614eabb8f0a3d2db
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:20:18 +0000

    speech copy tooth call ordinary

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
