bab30d183edce9efd24b77d3309ca639e2dba536
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:54:24 +0000

    suffer attempt material prepare content

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
 import time
-def get_current_time():
-        return time.ctime()
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import array
+def get_array_as_list(array):
+        return list(array)
