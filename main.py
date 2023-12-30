0dc07368855d56f58b430d68da5041c07f7f0797
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:37:45 +0000

    ashamed direct accommodation succeed comeround

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-print("Hello, world!")
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def reverse_list(lst):
+        return lst[::-1]
