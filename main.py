051d60a682b554a97753d24afaf0896ff00b57de
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:51:10 +0000

    break term breakinto breakfast obtain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import getpass
+def get_username():
+        return getpass.getuser()
