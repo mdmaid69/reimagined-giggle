ec3d10476a7fa449a866073e927c011ce40d5381
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:34:59 +0000

    tour train mouth cut finish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_work(force, distance):
+        return force * distance
