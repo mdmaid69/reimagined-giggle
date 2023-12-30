83201778186ea9ee3bc28b82d9bc55954b54d807
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:07:15 +0000

    disappointed careless shall say gentle

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import random
+def roll_die():
+        return random.randint(1, 6)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
