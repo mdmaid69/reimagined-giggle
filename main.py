42e91b8f5a8c84170872d44e4cdd99ce8bd36b1d
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:52:22 +0000

    track plan all back confusing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def convert_to_hex(n):
+        return hex(n)
