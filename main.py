96ea034121b7287da9b4a69d700dfc432e8b7de4
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:40:50 +0000

    along supply thin trade must

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def cube_number(x):
+        return x**3
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
