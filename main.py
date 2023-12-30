a81ad0a879f0d1a7006028b0899a3e46db3c8540
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:47:33 +0000

    next straight strike strong below

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def calculate_power(work, time):
-        return work / time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
