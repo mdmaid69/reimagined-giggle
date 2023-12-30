37f2bc6d4e6ae29a844150e1084749586aa5af17
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:48:06 +0000

    you subject health pen it

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
