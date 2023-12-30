efa134511dcb472bad7362c859a6d0b49661a41f
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:48:28 +0000

    edge obvious sense genuine skill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
