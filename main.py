10db53c9d2ff5e74d6b6f64321eb7fe1a3de30de
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:27:51 +0000

    myself Kk frame makeup getback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_directory_name(path):
+        return os.path.dirname(path)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
