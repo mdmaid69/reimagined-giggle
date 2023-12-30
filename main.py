7076d1f11a6218cdf610177ac611c93fd70da15d
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:26:44 +0000

    pocket wear accept goodbye wall

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Characters:", len(text))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
