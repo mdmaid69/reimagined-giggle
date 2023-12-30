6673a69a702c75838b9be0eb5f12a76e2bfb867f
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:51:09 +0000

    return speak legal contribute fix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
