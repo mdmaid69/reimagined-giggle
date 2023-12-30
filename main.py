4c83c75999d4fc6a2f62719609fac67d754b0632
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:07:32 +0000

    police direct use face judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
   def get_file_size(file_name):
-        return os.path.getsize(file_name)
+        return os.stat(file_name).st_size
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
