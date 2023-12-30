3787f7fda458578f10c156335f33c3abf5573f28
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:41:19 +0000

    earn almost report scream level

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  def is_even(n):
-        return n % 2 == 0
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
