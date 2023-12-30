97532e1a4b17719f225e023369c765b3a5e2520b
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:04:46 +0000

    attempt majority giveback sore wear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def subtract_numbers(x, y):
-        return x - y
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
