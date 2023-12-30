a3abffb1f57c546c0b58331f1dbef035aaa0eefd
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:14:48 +0000

    stand study total exercise minimum

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
