6c0b08495b8f122e2f8bcaf60b2fce4b22304963
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:18:11 +0000

    everywhere runinto yes command getout

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
