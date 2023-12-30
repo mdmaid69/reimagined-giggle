420a7f79c2884418c6b17d88ee9e01f1dc80bd8e
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:45:22 +0000

    Bb responsibility comeoff tear breakinto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def subtract_numbers(x, y):
+        return x - y
