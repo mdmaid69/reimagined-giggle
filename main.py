bfbcc6bc44d9981de94266ec4663a68d09aee0ae
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:28:49 +0000

    language mark pain flow unless

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
