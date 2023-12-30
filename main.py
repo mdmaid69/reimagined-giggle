861e8cbbfc95018ef52a3668dafcd192394fe9f1
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:39:59 +0000

    town breath like sea vision

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
