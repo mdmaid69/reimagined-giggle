ecfa27e9f5b3b82889548fae48b571b72295680b
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:20:59 +0000

    anywhere bin point Rr meal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import collections
-def create_priority_queue():
-        return collections.deque()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
