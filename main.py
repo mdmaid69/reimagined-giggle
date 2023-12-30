44900beb2fc53f9b04070fcaed1dcf3db9a597e1
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:17:24 +0000

    around narrow object behaviour comeup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  def convert_to_octal(n):
+        return oct(n)
