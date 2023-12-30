67d198d1a74e75e13a1bc8b9ad8ae7dcd380c3c3
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:59:44 +0000

    no accordingto room good after

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import sys
+print(sys.version)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
