bd20f30b761e2bd47355c13b1be2dce738e325b8
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:37:32 +0000

    much injury Nn use ought

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
