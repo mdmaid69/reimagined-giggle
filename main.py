22e09eac7a87709d679c506395ee3b6939765615
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:28:16 +0000

    throw intelligent finger rent reaction

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
