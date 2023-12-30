9f5e3060c1a1e1a5a5ba070f2f4cb7bc4ea0c072
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:16:11 +0000

    holdup calloff ask influence comeback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
