519affd730ec24966fafe416057c2b3a7295b3ac
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:12:20 +0000

    force standard and mainly obvious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
