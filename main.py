f4e0755c55ec44676d4e710e65488f154e13364d
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:01:33 +0000

    minority paper thumb part burn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def find_min(lst):
+        return min(lst)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
