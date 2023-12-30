eb3385e769bae9569ea665e0ebafe09273c0b0ff
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:50:23 +0000

    region be miss pot wake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-for i in range(5):
-        print(i)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
