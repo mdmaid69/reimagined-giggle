dd1965fee3b508517ad489a54e591c76d0235d19
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:40:55 +0000

    nurse mind impossible thankyou rate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
