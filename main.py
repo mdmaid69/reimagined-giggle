94c58a38f824ad7b9aa96c33a9f3c5b5d47ff0c4
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:50:47 +0000

    beginning confuse kind lend live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
