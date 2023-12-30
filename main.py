86718cafa7a6d55927bec0ea88879a5e0b2fdfd5
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:37:14 +0000

    heat second report afternoon soon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def find_max(lst):
+        return max(lst)
