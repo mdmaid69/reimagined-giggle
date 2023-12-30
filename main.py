66aec6aee486a14c293a84c6a1e7572f65215a60
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:51:05 +0000

    Tt fortunately slide shop container

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def add_numbers(a, b):
+        return a + b
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
