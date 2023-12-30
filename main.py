7e4cfe25e466aa44b07b7ad3bff75b1e1df68660
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:24:07 +0000

    surprise seem sea in without

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
