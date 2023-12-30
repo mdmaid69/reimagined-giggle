49d1d258cc9bfd14f2945e6d2d6d78fe5facb90e
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:23:20 +0000

    connect bread creature block suggest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
