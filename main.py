347bf7c24e89506caf45bf3a77a6970bcb6a226e
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:31:27 +0000

    juice pleasure future thing switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
