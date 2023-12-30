47f0df8c407e8c4d2abf69b3983fa65c4f937982
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:40:01 +0000

    upwards bringback bear stop prize

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
