a3535f0993119514f468e3a0699e31a88f1ee427
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:48:35 +0000

    behind female treat person walk

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
