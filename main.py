d28cf674d2a93e6d774e239df7b57faa1c1f948a
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:06:50 +0000

    watch value professional still thing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import os
+def get_current_working_directory():
+        return os.getcwd()
