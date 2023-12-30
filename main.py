2d33cdeadabafc09e2306cf8278d6068ce2056b5
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:57:37 +0000

    mean goodbye brother on middle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
