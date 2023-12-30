cdfe309ae8b0a30a8ed59632faf701ce8b271a19
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:57:14 +0000

    flavour great thankyou satisfied eye

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
