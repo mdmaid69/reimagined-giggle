50a97d5557cfbab980a2573bd7dd5efbdef58cde
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:27:04 +0000

    brain here tense eye trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
