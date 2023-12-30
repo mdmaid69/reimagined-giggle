f9b36b59d36aac0c8989fa9129024150f9121b0f
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:53:53 +0000

    broken bend 569enjoy congratulate state

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
