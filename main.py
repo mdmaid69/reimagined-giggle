a3592e09440a2f964af8f200e22533579a5673c0
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:20:19 +0000

    key child yes flat upwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
