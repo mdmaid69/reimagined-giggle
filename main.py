ff0f5b320b089701b559a15e463ebf553789bc3d
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:07:49 +0000

    think whenever Tt copy Pp

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def count_words(sentence):
-        return len(sentence.split())
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
