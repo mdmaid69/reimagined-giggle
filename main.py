533ed5632f8e18a409028f57bee1759df8de4b3c
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:58:08 +0000

    collection everyone nerve only public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
