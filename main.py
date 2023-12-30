c64b2ec631e4328dde8d52e72a82c593b7add25a
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:31:35 +0000

    trial support considering 565end mistake

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
