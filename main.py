ac675a2d3967b84feeff93db2b71f34da4a075dc
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:28:38 +0000

    shade level peace platform takeover

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
