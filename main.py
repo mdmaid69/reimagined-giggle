cd404f2cbf3c4b05862eacb449a432b96d9705e2
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:11:43 +0000

    fast problem before 567enemy worry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
