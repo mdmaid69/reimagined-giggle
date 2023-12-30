7bcff6cab3d5e483a6276582a1fce6b5134da355
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:14:48 +0000

    Ll decision strong plan right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
