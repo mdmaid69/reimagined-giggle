bf2fa86bcf6baf3a29bc85e35fb6194b3d357865
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:47:15 +0000

    stir the 179bite slice sentence

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
