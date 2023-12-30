497fafa4b544c732910332274d721b2d7811f15d
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:01:26 +0000

    difficulty fun perhaps frequent dangerous

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def calculate_power(work, time):
+        return work / time
