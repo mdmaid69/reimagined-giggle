b51fc151aecb13e5c58647d0f61d7284f67fb35a
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:38:28 +0000

    exactly acceptable colour thought smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
