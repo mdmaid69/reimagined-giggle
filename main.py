fb7f34b76777f34e444355a43a713f63fdcb754d
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:31:18 +0000

    shrink damage praise way listen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
