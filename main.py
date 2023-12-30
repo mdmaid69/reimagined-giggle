e4809be09f06f1492c3567c8e2d92cca79900b83
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:21:58 +0000

    affect thinkthrough problem whatever future

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
