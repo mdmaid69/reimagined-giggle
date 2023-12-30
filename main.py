258d2e5de9844f7827cc8a46f4443173e325e116
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:12:40 +0000

    any hot slice shout tear

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_density(mass, volume):
+        return mass / volume
+  def convert_to_hex(n):
+        return hex(n)
