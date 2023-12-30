82c0c76e1f2c5304a9a85c8bb2c9dde042060a78
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:21:39 +0000

    tomorrow provide exactly concerned flight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
