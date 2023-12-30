8ea63fcb179fef89cc010b2514853ddd17f0271e
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:21:30 +0000

    forget sticky bottom personal anything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
