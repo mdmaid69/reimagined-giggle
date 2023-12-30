9d54f3e69813b24eb9991a0d12019f6d9ed27490
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:04:41 +0000

    report tip miss interview payment

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
