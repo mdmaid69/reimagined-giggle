b67c97503b752357b609c3a28e7653130cc4aba5
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:38:44 +0000

    willing try stay tidy often

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
