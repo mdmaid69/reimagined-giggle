ef30a7fe45ea128c004beeea0ab92f1de65541d2
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:26:59 +0000

    repair order quality square carry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_as_complex(array):
+        return complex(array[0])
