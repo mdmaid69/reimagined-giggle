223164acd21eebe3873ca4859358360f92aba86b
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:37:10 +0000

    chat path turnup view march

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import time
+def get_time_since_epoch():
+        return time.time()
