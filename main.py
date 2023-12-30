ad769ec18a25403cf311ecd5462f1c0255ef6d38
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:16:23 +0000

    principle play introduction pause by

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
