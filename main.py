c35e4a3a9017b3d717f3c27792751e50d648be9e
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:05:14 +0000

    exaggerate herself style make accident

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_item(array, i):
+        return array[i]
