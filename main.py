3321638a5aec7cad1a71fbb511ec3d70173616d4
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:25:31 +0000

    tongue fine rush winter similar

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
