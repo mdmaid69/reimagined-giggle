8d985b467177685e6ae6252b8fd156d96471b1cc
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:04:23 +0000

    uponon whenever slightly confuse young

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
