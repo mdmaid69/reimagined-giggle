4909cfc990f804e993cf47f68c704a1e3a75dfa3
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:53:40 +0000

    right circle spill general special

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
