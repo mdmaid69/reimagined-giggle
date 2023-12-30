a2347aa836cb10aee2abff91bf4345b4815493f8
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:52:12 +0000

    continue shut Vv geton authority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
