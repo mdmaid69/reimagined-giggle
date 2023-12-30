45daa8e3a60ab76d6f87176f71c8f520114e32a6
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:44:17 +0000

    remark himself basically slightly swallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import time
-def get_current_time():
-        return time.ctime()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
