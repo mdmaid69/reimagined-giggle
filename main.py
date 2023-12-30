e3fcb74d5315e952418c9d947498ee057e106aaa
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:06:46 +0000

    imagine sweep message reach rare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
