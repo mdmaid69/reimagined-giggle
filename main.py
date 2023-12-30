3e07b2f5239c6154889919beebc8423a06e19b16
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:02:19 +0000

    conversation exist exam reach correct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
