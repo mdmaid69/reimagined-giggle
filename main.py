2473a4aac486993d8aff489800037cac83794192
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:09:47 +0000

    fierce warn hurry performance lend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
