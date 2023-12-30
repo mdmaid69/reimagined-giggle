8f4b49cec4b2c0392515959103afe61b71f49413
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:51:39 +0000

    comfortable science 566end able base

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def divide_numbers(x, y):
+        return x / y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
