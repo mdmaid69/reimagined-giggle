557923cf6aaaa5917c589555c8417cb93352d528
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:49:12 +0000

    getaway moment steep fault need

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
