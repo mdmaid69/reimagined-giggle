10629a729dc5df295e6878b829dc9906ee97104f
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:45:03 +0000

    through regularly cough effect today

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
