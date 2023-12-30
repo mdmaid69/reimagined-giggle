8bb1b65fff40fdd2b831b7dfcb6ff5a856906918
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:44:05 +0000

    advanced understand live best touch

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
