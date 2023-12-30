4f4ac3e362e307951137290b8f60b516565ac0fb
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:23:00 +0000

    sign avoid float belief principle

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_max(lst):
-        return max(lst)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
