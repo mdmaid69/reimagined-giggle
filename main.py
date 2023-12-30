8358f6acbf524dcc9c093373ba2e02132922f2f4
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:21:34 +0000

    race wrong steady transport attempt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import re
+print(re.match("h.*o", "hello world"))
