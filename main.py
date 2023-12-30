f545357dcd4c25a63604b454618d43c622110f8e
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:13:01 +0000

    amount goout equal whisper raise

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
