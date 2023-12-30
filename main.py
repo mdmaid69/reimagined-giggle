fc296a2d0f74acd276248531c2bd61d452424ca5
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:23:11 +0000

    shoulder beside stretch mean programme

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
