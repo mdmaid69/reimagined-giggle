2955ee82faafadefbaecf2ae837eb9c8adaab145
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:19:26 +0000

    share breath fresh phone rich

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
