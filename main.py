4e29eb5df170bfdd5d8597758fc3d41d49783e21
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:49:47 +0000

    satisfied surprised field narrow complaint

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
