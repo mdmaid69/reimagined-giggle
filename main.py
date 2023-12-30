593a1802107f4e7aef9db4cb0c143d94a51bb173
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:36:26 +0000

    increase major swim silent big

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
