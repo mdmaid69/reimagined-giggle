a13d4932bcee77dbf6376733ae60fbba0bd94d7e
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:52:53 +0000

    operation complain so hardly find

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
