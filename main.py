ae2e5b45f3976dfb04db07edf8be88d36f7a705c
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:28:59 +0000

    bottom call together upset runover

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
