351e9497ddadd6b6df8ec56f4f82202fab63d125
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:48:05 +0000

    battery near fix feel sign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
