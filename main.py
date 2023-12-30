2faf1a19ea16273ffaa5483b79c2ad796aadbdd0
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:23:37 +0000

    runover plant ahead clean clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_current_directory():
+        return os.getcwd()
