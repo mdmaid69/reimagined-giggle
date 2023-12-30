2b457ac77e51207e1a6ad3f3ce9fea79f5bfbe9f
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:29:53 +0000

    address tradition part square delight

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
