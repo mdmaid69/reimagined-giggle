5e346427e36b6728376ea976d02504aed09f27fa
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:53:56 +0000

    after similar that distant thinkover

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def reverse_list(lst):
-        return lst[::-1]
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
