819cb94fdfa37bae84818c534a9315cd4aa41bf0
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:21:09 +0000

    pollution off rate thinkover ahead

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
