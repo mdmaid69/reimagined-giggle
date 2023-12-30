1a13f139d0eb393481af3ff1269edbaba8d2a0e9
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:38:00 +0000

    narrow that warn doubt violent

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
