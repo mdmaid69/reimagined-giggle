ef5ff44a5831c8832ef55b9ebd90f233422ff28e
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:56:04 +0000

    twist herself year needle used

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
