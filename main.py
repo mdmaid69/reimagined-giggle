7a42aa052e01e310a86cd3742340cc12fb8098f7
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:06:16 +0000

    soft whole unlikely nowhere line

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
