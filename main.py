9225c9c70a978045fd8ad2a3f6d88a42659ab3b2
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:54:53 +0000

    workon this shade basic exaggerate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
