af9fd1ec6e14d59de7f20ba26f04990d2130017d
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:32:31 +0000

    anywayanyhow godown especially road practise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
