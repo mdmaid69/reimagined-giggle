320c9a3fcc3995b6ae3fe802fb884d0c91ec49c6
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:58:06 +0000

    slice conduct mood sister prefer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
