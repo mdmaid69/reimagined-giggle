cdb67ad49a6cd8c134f521c341043b97df3ef242
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:23:58 +0000

    wash feel stupid bus settle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
