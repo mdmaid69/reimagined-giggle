8a4b7594fcc331c5d15663675e5167c167e74077
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:39:29 +0000

    project comeover wear discuss along

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
