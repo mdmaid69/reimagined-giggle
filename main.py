7b624120f8e099451406788c900ef5d7cfc99687
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:01:51 +0000

    production memory fact spare smile

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  def add_numbers(x, y):
-        return x + y
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
