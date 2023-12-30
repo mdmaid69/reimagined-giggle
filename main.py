f5d48464d57000a4e24a22c651c6c8a1cb30252c
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:33:31 +0000

    Ii level compare deliver slow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
