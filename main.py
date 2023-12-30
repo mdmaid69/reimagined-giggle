bb67547c37fd9909059ca00d6076845493cfe352
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:40:43 +0000

    Uu wonder cheat dead die

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
