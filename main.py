160ba045e19f46c287f2d28de5b53eaa2ba32cf3
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:36:53 +0000

    invitation takeover target information there

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
