d96bb8c3263c57b88ee9e989e0a4796b8d878391
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:42:37 +0000

    from lonely expression town safe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def get_array_as_set(array):
+        return set(array)
