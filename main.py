8449418166d435685042fddb91397e3b489123f6
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:59:02 +0000

    trap unfortunate finger contents note

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def convert_to_octal(n):
+  def convert_to_octal(n):
         return oct(n)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
