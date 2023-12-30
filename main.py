5a5d1f0d9f2d80ee937ce0c046d5eb2babcf69f0
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:11:05 +0000

    seriously closed shade thick close

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
