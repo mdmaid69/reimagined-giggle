8290c5a305013f74fab7540f4994d381b19fff71
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:25:34 +0000

    anywhere total war myself pump

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def square_number(x):
+        return x**2
