0bb78e349447c04ea6010f3633bfcb487783483d
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:08:57 +0000

    loud minor sweet murder tape

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import sys
-def exit_program():
-        sys.exit()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
