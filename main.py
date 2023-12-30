0bebffa02f26243b782e58d0097618e6702f0de1
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:09:34 +0000

    beautiful main friendly set meeting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
