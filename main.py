06cdd07230c2553564962b12d0483d45fb74eae5
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:22:28 +0000

    protest press every touch swing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
