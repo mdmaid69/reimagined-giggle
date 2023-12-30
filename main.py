f0a1d753062ca12891e0ca156f6a920ae9b1e8fe
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:36:57 +0000

    convince no slice which now

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
