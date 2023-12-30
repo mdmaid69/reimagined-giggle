2612728e01ab33265d62567d14d63938e7dc5311
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:34:14 +0000

    someone bit room his rise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
