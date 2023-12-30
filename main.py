3e6bddb7fad585a959d2c9b285ea6ab03f24cfeb
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:46:03 +0000

    custom punishment bin goaway regret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
