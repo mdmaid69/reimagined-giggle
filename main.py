cb2cc5a7868cab4be9e385b3c69025414bd61f9b
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:22:19 +0000

    serious minute turnback bored someone

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import datetime
-print(datetime.datetime.now())
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
