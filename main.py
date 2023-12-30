4d3c6c643f64395c4c8f805d9d1950dc0359dc60
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:19:20 +0000

    fix brush sink embarrassed figure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
