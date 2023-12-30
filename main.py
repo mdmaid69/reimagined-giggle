e95c81f08fed7ab0a9632e82ec589fdd70de9b8d
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:51:32 +0000

    calm top yours rough method

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
