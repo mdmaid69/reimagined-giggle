c215504d3130d132aa68631e7f8009ea41518969
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:23:46 +0000

    not yours broad prove slowly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
