f9592592a528412f1443b29fd6ec2649d586fbb4
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:15:16 +0000

    silent long least bringup expensive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
