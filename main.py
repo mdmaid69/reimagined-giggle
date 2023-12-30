4633cf0daebca40d15c9481bf55c4a6e8c4b0204
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:40:17 +0000

    bang necessary thankyou positive thought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
