4a286d115550d6d4de227a4c22cbd0f2e704388f
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:22:17 +0000

    ground between like limit sell

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
