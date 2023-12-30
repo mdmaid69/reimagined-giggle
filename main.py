ec8b715c1e0a526ef16782a5e5e35f574603cf2f
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:46:30 +0000

    discovery wide fire custom shelter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import datetime
+print(datetime.datetime.now())
