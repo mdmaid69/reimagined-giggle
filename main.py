8de05fcfd4436bc3271945385107eeb97f41e1e3
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:06:21 +0000

    upset project contain last keepaway

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_ceiling(x):
+        return math.ceil(x)
