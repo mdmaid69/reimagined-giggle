66a846ef507575d6cf81a2e5e99311822abde034
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:02:54 +0000

    character behind ready negative practical

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def is_even(n):
-        return n % 2 == 0
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def find_unique_words(sentence):
+        return set(sentence.split())
