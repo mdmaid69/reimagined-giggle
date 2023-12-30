7b1e42620c93a2631107e7e142605426ab1eb27a
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:14:05 +0000

    hurt split business remain reserve

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
