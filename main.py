cdec5d0362a4d4e54f3758661503389f09b15665
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:08:58 +0000

    outside train team region city

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
