d90c06a580467c3d78bca79e77e623f6434a752e
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:00:16 +0000

    cost entrance street weekend likely

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-text = "Hello, world!"
-print("Characters:", len(text))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
