43173f083b807fe959557744575b85aabb3c6bff
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:49:57 +0000

    radio impression however its pause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
