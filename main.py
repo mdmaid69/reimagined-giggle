1fe35a101f6e017ba56f99beb3381bf633d32cf8
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:36:50 +0000

    precise squash strike range suggestion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
