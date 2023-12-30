7c0d9cff1c54c4c4feca4a0e6ff4f5ef13936f39
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:05:54 +0000

    post vote afraid entry accidentally

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
