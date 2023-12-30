ec4b9a9a4a4264f2a5fee451ea7ff1b774db57de
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:06:46 +0000

    race part once past accidentally

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def cube_number(x):
+        return x**3
+  def multiply_numbers(x, y):
+        return x * y
