dc1d42afece4965fed83916dd58519dc12d427b7
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:36:47 +0000

    too finger possibility height squeeze

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def count_words(sentence):
-        return len(sentence.split())
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
