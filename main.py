d94a594a20fe2f2da6f187e5fc78463096bcc2f8
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:36:43 +0000

    mainly group clean cry suit

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
