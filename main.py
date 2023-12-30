bad482af678130abe62698a19c2f0fd4796a811a
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:39:37 +0000

    worried bound speed general wind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
