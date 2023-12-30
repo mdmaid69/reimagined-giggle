8b9c4019712225ccab5e2e19f5eb04305f18f24f
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:53:37 +0000

    correction face limit before lookup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
