ce6ac36f7ec8bd89b82de1a1dfb50515c2b47384
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:50:36 +0000

    spring passage instruction swimming touch

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
