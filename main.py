4e21f047643446a5378d565df15f91a3a184610d
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:17:46 +0000

    congratulations limit ours fail stone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
