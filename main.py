90d886c7154af69e7038312f96acfa9b474bafd7
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:03:14 +0000

    characteristic this remove scrap popular

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
