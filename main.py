e9b23a3c3f5f7133f339e4cf53da4e6776f53417
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:26:29 +0000

    scrap before talkback possession up

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
