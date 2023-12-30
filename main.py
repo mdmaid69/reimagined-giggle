15de967dd7b40810079a79cc8d45d7a872e8ff76
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:03:38 +0000

    hard destroy whole so her

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import os
+print(os.getcwd())
