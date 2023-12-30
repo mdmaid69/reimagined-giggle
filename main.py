b8a5f6125479e6956199cf7aa5457e90082da760
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:18:38 +0000

    defend search load dinner week

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import collections
-def create_user_string():
-        return collections.UserString()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
