ad8dbb0ea2027006d89dc148e7b6358512dff4f1
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:40:45 +0000

    emergency address culture rubbish waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import array
+def get_array_as_set(array):
+        return set(array)
