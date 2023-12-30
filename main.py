a7d788696e8c8ed54491cb71b20ff5e2eff367c5
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:29:22 +0000

    blame perhaps quiet we suffer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
