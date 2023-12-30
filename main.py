0b61db9d86296a49c2bd06341aa28483b181e0c6
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:48:03 +0000

    risk advice escape middle design

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
