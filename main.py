6be999ac1abe917fc6f5257b50e47ce383f9aa19
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:42:59 +0000

    fix fat word to on

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-def count_characters(sentence):
-        return len(sentence)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
