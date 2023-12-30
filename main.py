e6eb82b64da1cbb826e3de622075dc34147bc635
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:53:44 +0000

    Rr would opponent relax add

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
