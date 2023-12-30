257b8334502ea287b2ad8f4092f7a3c7e66a0754
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:14:00 +0000

    wear proper tap normal indeed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+i = 0
+while i < 5:
+        print(i)
+        i += 1
