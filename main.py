965e784fbfeb56f2006159c3d11b14a054c5f894
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:58:34 +0000

    reaction use wherever apparently he

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
