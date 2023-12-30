e5cd8f62e4b22efcddad43e6030f6eeea2ee487d
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:30:09 +0000

    cough emphasize everything rely whether

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
