768992b73c01572f5101d4a4a2d292b038dc3080
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:17:37 +0000

    pay valuable worth shock present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
