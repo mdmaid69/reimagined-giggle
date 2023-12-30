b18a815e5b78965c3f3d811485f31dbc8924a54a
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:01:14 +0000

    band model frighten grand strict

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def calculate_speed(distance, time):
+        return distance / time
+  def sort_list(lst):
+        return sorted(lst)
