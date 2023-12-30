d0e851ec52da4bd7d80c5950249c69c9b4760d33
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:59:12 +0000

    blame expression shock exploration practice

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
