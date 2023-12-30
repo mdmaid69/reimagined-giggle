7c8a4f8bf88a8a8acc3f9941d0e12fbfbb99f8ab
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:50:19 +0000

    accidentally principle regular hers street

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def change_working_directory(path):
+        os.chdir(path)
