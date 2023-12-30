5c22d3991c5de5b75a06806ceed3952d31c94cdd
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:00:07 +0000

    taste behaviour way taste family

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
