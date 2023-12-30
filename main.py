dc089048195ca46b1834871f25d7107d3a9e4db7
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:38:23 +0000

    principle swim pity suddenly declare

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import sys
-print(sys.version)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
