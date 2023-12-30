7160ce32bd322dd8a52b4c1000eb89468059569e
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:57:56 +0000

    unfortunate new seat fork forward

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(numbers):
-        return min(numbers)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+print("Hello, world!")
