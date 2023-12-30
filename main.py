5c6a14acf62c6d7b01198abd67a0aa71db29b666
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:28:11 +0000

    majority guide worry pen contrast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
