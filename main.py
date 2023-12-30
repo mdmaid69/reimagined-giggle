d13e86548495b1e02411b67097f92d631357d229
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:32:07 +0000

    same discover properly score ago

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def square_number(x):
+        return x**2
