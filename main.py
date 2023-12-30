15f739afc6cba9957d2b861333b41d60f14142dd
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:21:24 +0000

    neither shine sure part fire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def clear_array(array):
-        array *= 0
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def reverse_array(array):
+        array.reverse()
