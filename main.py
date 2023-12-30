9bae36b460742638caf8cba0f6b3856b2c07ed8b
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:35:02 +0000

    welcome takeaway argue cry generous

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def divide_numbers(x, y):
+        return x / y
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
