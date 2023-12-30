c3defb7b7855b578f987afcc70451d1f7565bacc
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:31:56 +0000

    matter confusing fast hide many

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
