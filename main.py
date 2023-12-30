3f35a44bb88bc5225cd94f67865142aac164af50
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:00:38 +0000

    favourite bridge freedom soak bet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
