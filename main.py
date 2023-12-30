feee508bf2e5b9300a005d82e34f2698326fb1d3
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:03:12 +0000

    tip brush hospital ear previous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
