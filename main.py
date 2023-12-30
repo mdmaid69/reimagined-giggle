72f9f601016243125363b93872618cfbed3d2c3c
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:48:53 +0000

    stop combination cause or sign

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
