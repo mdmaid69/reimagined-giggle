3bf7960584680c056acd22271d79044ad5c737ae
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:35:18 +0000

    sing bush form turn clear

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
