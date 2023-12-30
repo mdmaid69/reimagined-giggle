758bc31f727da7e6e16c4ab5188f36d6b487d480
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:44:39 +0000

    train finally control wear near

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def divide_numbers(x, y):
-        return x / y
+def find_min(lst):
+        return min(lst)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
