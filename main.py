23c9344bde113596141810ea33f93e01974bd8d1
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:36:52 +0000

    I edge holdup style excellent

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
