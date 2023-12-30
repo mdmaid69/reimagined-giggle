547fdb0899fb88c48ffdf4d2cce3bac8fc2963ae
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:57:56 +0000

    play remember best birth quantity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
