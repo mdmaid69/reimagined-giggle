cd216e943342fc8e029d4fd2f713bdc842a7469c
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:17:36 +0000

    argument getback shoot before memory

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_unique_words(sentence):
+        return set(sentence.split())
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
