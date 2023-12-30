956904d671f3272ab35b61373f3688ecb6dd06b6
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:51:12 +0000

    ring married circle concerned glad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
