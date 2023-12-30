a42e2f0033279982fed0d16547a891fe9f29999a
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:00:41 +0000

    comeoff sharp street build forgive

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
