2efa22338ff580f3cb053f2488cf0d58a5ee3e3b
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:07:50 +0000

    coat equipment least exercise run

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
