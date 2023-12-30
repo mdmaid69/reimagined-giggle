1c101c2c592092f7402a4e0c4814197b22f89b38
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:38:56 +0000

    callout hate want knock deserve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
