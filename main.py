11a7e109bcc69327cb7e36e4a632eb2e5d50fd76
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:15:05 +0000

    criticize government question head feel

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
