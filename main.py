997f492750e335b67626467fb9aa0cbf1ab35c59
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:51:42 +0000

    contribute thick no over warn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
