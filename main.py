7753d99eb34a6797934fe8be1d555844b98ea4f4
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:47:32 +0000

    order scrape switch onto apparently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
