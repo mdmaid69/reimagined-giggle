0f5524ffdae5d9899ae90f6a7d74838e3d134f4e
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:54:17 +0000

    divide accept rather gap sometimes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
