eb52c638e2788857edd108f51a50a0083903324d
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:00:16 +0000

    count crash talk impossible lucky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_current_directory():
+        return os.getcwd()
