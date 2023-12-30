88e54aac7a5ba81b35bacf88d1632205508c7d72
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:31:31 +0000

    excuse well contain fix switch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
