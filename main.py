d20f5b3f0fe22935ee2b3739b47df16a776f63fe
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:30:17 +0000

    spring style sure boring cruelty

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def cube_number(x):
+        return x**3
