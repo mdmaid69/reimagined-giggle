1732ee2ae81b11451c8fa32ac755cd9133faaf6d
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:12:12 +0000

    group occur building comment frame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
