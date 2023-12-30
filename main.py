636a676146484fbe62cd7ae27df6cd533acbc47f
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:27:01 +0000

    immediate inquire then trap forward

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
