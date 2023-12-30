8cfe83ccd4292761176045c27c91eddc4cb175e4
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:02:46 +0000

    sign defeat its what speed

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
