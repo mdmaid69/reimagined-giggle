cdd5c35d3a0b228c2a358add8e309a16c1c6c420
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:34:54 +0000

    lock get hardly charge fine

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_density(mass, volume):
+        return mass / volume
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
