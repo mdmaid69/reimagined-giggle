504d6d3f5107fcea6f9f591631c3da54c5bce8f9
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:34:14 +0000

    cover who arrangement foot taste

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_perpetuity(payment, rate):
+        return payment / rate
