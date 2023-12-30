9ec9b9a5b8ba3279b53faa94a1ba32c3ef7c9377
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:54:42 +0000

    tool bury fairly nurse trip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
