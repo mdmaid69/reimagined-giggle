08ff061dedcad348592815f4e60a27b6e70e325c
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:06:31 +0000

    hello closely hour behind race

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_power(work, time):
-        return work / time
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
