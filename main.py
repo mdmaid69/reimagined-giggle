13784c364f1787eb9ab2cab7c01eddaea898719f
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:24:26 +0000

    exit tidy unite ought start

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+for i in range(10): print(i)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
