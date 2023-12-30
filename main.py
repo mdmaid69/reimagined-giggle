1eed254b92bb6f255300152913e1df3f19a1842b
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:13:36 +0000

    listenfor chase preparation knowledge down

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def clear_array(array):
-        array *= 0
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
