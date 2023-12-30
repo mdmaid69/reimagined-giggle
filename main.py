af1957a9bc72304164c8087259c53dda1862ff89
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:04:31 +0000

    catch spite loose straight classroom

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
