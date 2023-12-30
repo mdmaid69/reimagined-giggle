559a19b0bb81528f6272db8e83e0e9617f1bc565
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:09:36 +0000

    getup dangerous careless past regional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
