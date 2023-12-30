25180c375664d6ffd00e693f5a653d63fa78937d
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:32:29 +0000

    through breakout unite male only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import os
+def remove_directory(path):
+        os.rmdir(path)
