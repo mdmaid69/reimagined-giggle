368e8e8ad54fb7cc24819309c9d7a30ed9aaeb31
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:47:49 +0000

    poor age create address rest

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def is_odd(n):
-        return n % 2 != 0
-def sort_list(lst):
-        return sorted(lst)
+  def remove_duplicates(lst):
+        return list(set(lst))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
