6c80df4d9b65081021f76ec2aa3033436bf53bbe
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:35:23 +0000

    yourself scared likely hot fresh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def get_array_as_tuple(array):
+        return tuple(array)
+def add_numbers(x, y):
+        return x + y
