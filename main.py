a3de7d1f10e3ae5191917287a9ba345f6c1094b5
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:37:42 +0000

    that break marry plastic her

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import sys
+print(sys.version)
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_item_count(array, item):
+        return array.count(item)
