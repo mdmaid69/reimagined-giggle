f0eb92aa4de6300b10935115f25ea5bc97a82b81
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:04:20 +0000

    consist noone anyone wave weather

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def is_odd(n):
+        return n % 2 != 0
