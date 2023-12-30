0318e8967a00af9db7082d1521262a50d19ca50a
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:28:26 +0000

    under embarrassed deep respect note

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_bool(array):
+        return bool(array)
