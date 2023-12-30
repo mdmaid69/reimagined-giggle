7de2d147448f691466dc6eebfb6f8d95a6baccf4
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:05:19 +0000

    ask keepup least break deliberate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
