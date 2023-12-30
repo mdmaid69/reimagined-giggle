ca4f5a77858358a2a6590c179b0ffb0bdcec32f4
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:54:15 +0000

    come picture consider round burst

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def multiply_numbers(x, y):
+        return x * y
