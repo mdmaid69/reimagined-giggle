9b47c0d806ec89cb7680d170a79764ae9f25a069
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:31:29 +0000

    clever partner friendly concerning calm

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
