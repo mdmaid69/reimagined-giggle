7dbd7e76c2cd7fa75190db0da228a5d8240895d3
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:12:55 +0000

    Bb somebody concerned hair ear

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import math
+def calculate_floor(x):
+        return math.floor(x)
