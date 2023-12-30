cbd5f634159e348f93b0e0e2286507cb8065372a
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:51:28 +0000

    up reject breakfast field arrange

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
