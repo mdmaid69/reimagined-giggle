236cd1bd649cd98eeeb4ec45c0a9e1938a2b5741
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:37:32 +0000

    impossible cross load satisfied giveout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
