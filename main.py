4a688b4051123f575e2c9e349b73d3e6105ff7d7
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:31:53 +0000

    fit consult pay course likely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def calculate_area_circle(r):
+        return 3.14 * r**2
