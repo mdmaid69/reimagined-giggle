d5ae45eba3d1ea3b82d164676039fa38f773a530
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:57:49 +0000

    opinion contents you birth represent

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def sort_list(lst):
+        return sorted(lst)
