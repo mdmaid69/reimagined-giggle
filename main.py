ba5d64cd95479e0f2785ae42836f088a0259abfc
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:26:54 +0000

    knowledge division regularly talk because

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
