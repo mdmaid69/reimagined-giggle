138e2947c2f0f075ace398c907e0d95200cb78bf
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:19:38 +0000

    seem sense prize bunch wind

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
