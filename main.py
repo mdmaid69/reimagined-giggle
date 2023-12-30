eb37d9ace9658cd4de6efdb02f47075d7bdc4692
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:19:09 +0000

    stiff region border speech final

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
