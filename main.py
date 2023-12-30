17b770bc05232d5b9308213ebe427dd739126915
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:11:49 +0000

    rich point state snow doctor

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
