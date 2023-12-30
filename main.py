7cddf422ac4768453dd37d21d7db511dc262c1df
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:37:22 +0000

    gap valuable loss live foreign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
