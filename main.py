8aa0e7698a4aab1c0b322988231013e0837b49c0
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:12:57 +0000

    tax earth demand join bad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
