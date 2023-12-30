badc5ecf5074c7103b84460432640d1904dbc98b
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:35:20 +0000

    mean storm disappointed gooff takeback

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
