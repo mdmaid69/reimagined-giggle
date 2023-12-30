3eceedf12e0c647ffaf8cf7570c60dc849b4c6d0
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:22:18 +0000

    gift 570enjoyable mess hollow try

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import time
+print(time.time())
