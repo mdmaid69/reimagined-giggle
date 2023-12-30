c85bba8c31f4452d710e8e6b8109dba9fba7308a
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:25:11 +0000

    kind fresh increase experience it

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
