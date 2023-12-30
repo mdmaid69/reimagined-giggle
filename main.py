1b197003062eb0f97a22fae9c9d529a0701c0a17
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:42:40 +0000

    meaning where lay step dead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
