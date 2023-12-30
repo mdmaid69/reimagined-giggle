90f1f1852da6701e78c067fd7e771cc1ffee87e7
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:35:27 +0000

    without about comfortable success have

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
