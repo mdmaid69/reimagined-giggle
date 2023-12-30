dc9bd3868c4452a3fe712de019323bd9e7184359
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:51:01 +0000

    do second equal exaggerate many

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
