d540bd2e03ab0ea18fd4dcc2b4ad465137faa2b8
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:39:13 +0000

    earth married disapprove stir explanation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
