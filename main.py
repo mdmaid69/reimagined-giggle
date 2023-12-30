24070732aa7cb3863628a90d867aa23ae24e8618
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:17:35 +0000

    needle fun size thick rich

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def convert_to_octal(n):
-        return oct(n)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
