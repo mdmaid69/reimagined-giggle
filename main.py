92fd7760d3b989a743ee96ef1deced3e951f4df3
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:02:27 +0000

    programme pretend knowledge impatient past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import os
+def change_working_directory(path):
+        os.chdir(path)
