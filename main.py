666f0db0569b7292bae0db7ad03ff1cd9a97006c
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:42:01 +0000

    step light attempt hit desperate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import collections
+def create_queue():
+        return collections.deque()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
