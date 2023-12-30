081603eff5e2b97ad5eaaf5949283f080cc315fb
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:23:07 +0000

    control amount shallow identify thought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
