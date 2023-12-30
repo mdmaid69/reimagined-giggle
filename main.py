19bc5a6ea4ea4bd745ed4056b90303cd085027f8
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:41:44 +0000

    impressive tool introduction except drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def is_odd(n):
-        return n % 2 != 0
+def get_array_as_complex(array):
+        return complex(array[0])
