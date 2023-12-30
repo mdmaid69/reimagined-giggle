8ecec84b4cd5d20e57bc2891e0e45b89a6ab004d
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:22:23 +0000

    confident open gather motion yet

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
