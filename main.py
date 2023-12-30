4009f76efff92df34def144391c96a0b48cd7fbe
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:05:04 +0000

    crash board advice behind cough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import collections
+def create_user_list():
+        return collections.UserList()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
