8878cd39ba2e9f81b3a4aae9255c0e54195b20d4
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:04:24 +0000

    keep further pollution instrument disgusting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
