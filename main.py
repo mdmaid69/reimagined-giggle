1cdf4ef32f491e3a41dacf27b4574e3034ad6d96
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:38:46 +0000

    score under arrival artificial decide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
