75492cc6c9d1c62cb149d70079127267db03cd62
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:36:23 +0000

    geton habit confusing success as

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
