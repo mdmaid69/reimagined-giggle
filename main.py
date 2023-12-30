17389c97ddaa53e114f9c3c8a7164d99de78964c
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:01:45 +0000

    active 179bite exhibition gate different

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
