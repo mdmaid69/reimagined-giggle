645293b1d072c16a52b4d195811a44b6177d5a89
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:51:04 +0000

    cry goup almost must jump

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
