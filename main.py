1db768b9b415a660b58c20f7bf06aa13f5a4b535
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:05:47 +0000

    supply branch centimetre regret work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
