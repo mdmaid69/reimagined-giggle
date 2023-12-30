2761e134e1051bf88fed8b10a955e7d09f3a0a19
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:43:46 +0000

    combine handle silent callback slice

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
