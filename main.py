279aefddb1724005b70217a3c46f237edb079762
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:49:55 +0000

    skill first tend develop responsibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
