e0051c821e92b883a6fd30f69aaeaa760728719e
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:29:07 +0000

    convince people smell callout warning

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def count_elements(lst):
-        return len(lst)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
