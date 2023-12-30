8e47f387f62c8c6d06e3e496bef17a9a065114e6
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:23:18 +0000

    Qq attempt number team why

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import platform
-def get_python_version():
-        return platform.python_version()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
