5bd7cbec1ac551930808ef689074db1efe78f078
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:37:07 +0000

    average consist fortunate float whistle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_density(mass, volume):
+        return mass / volume
+def is_odd(n):
+        return n % 2 != 0
