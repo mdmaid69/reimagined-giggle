9831d9317e285f5ab322bc11f3a13d3878acf146
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:09:13 +0000

    union whoever there joke look

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
