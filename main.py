7080e2a2e0ca77c8da3a66913c321c09eabc70d8
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:05:19 +0000

    point movement within danger easy

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
