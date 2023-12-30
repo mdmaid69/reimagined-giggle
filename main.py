36c471f225e28ddd06e188d5096809ef4225b7a4
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:11:40 +0000

    team free often office thinkover

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
