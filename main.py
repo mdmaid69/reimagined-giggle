bdf99fea50b5685b9d22eb9c04652c75741d11f3
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:42:48 +0000

    age pool delicate hers understand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def calculate_acceleration(speed, time):
-        return speed / time
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
