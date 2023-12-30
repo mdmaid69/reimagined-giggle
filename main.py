3a1d6ae5e285c520fae5c60c619db0ecb003628e
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:11:03 +0000

    work attractive compare problem callat

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
