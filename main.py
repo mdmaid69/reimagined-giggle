6f70a1d661de488c9a065f00f1e8a4b5e3617e8a
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:40:52 +0000

    detail blame limit pretty laugh

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def multiply_numbers(x, y):
+        return x * y
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
