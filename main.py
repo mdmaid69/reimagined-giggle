2e2d8fafd50acb15418088cb2c3b27a5fe57a559
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:12:17 +0000

    claim method sink both group

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
