2c661739c57d4fbc97fe4ed25a2fc1f2c893547a
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:46:45 +0000

    division towards call spray contrast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
