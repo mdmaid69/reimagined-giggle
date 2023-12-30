cb5eb4d0bd421e71d812a80cff20f9b50de372c4
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:25:25 +0000

    pen straight low thank right

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import platform
+def get_os_info():
+        return platform.uname()
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
