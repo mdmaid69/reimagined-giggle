e212ed4f9bd2f210dea31de118b74a5efe19daa3
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:46:10 +0000

    storm mail behaviour truth object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
