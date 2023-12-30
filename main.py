ff11653290ff7eec7a11cd78622122c05a6c0b95
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:15:19 +0000

    second callout clean chase blame

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-for i in range(5):
-        print(i)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+print([x**2 for x in range(10)])
