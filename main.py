e22d120665798f5123f75d3eddedc3f5e3047d96
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:23:23 +0000

    bear tend respect same answer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
