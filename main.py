e05a3671dbcabac0558c8dd6c05666134935d96d
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:19:18 +0000

    trade description shock hollow choice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
