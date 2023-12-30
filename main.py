ee40b63fb72a0c40f9803d48265c27fafbd0f932
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:15:16 +0000

    have bye damage star variety

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import os
+def remove_directory(path):
+        os.rmdir(path)
