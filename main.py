79508a18e92843b911bb6abd048bae57625f4fcb
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:20:38 +0000

    space scrap profit here machine

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
