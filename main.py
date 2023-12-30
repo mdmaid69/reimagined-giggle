7471a75a07d3b3ec3170a6ec97541b11d590144a
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:02:46 +0000

    through cause warn emotion back

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
