8fd8a57f3804ce5c034403d0cf3bbcdb431670f8
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:50:30 +0000

    same separate check brief abroad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_as_str(array):
+        return str(array)
