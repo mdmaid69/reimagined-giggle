c8bede87ebe7ee3afb8440674a9eac7d4d465efd
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:56:52 +0000

    stage sensitive sight criticize Uu

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
