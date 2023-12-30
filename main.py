c6a64296acce4b5759baa7815e83e346f4d9c749
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:05:08 +0000

    spot we tired valuable bad

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import os
+print(os.getcwd())
+def calculate_perpetuity(payment, rate):
+        return payment / rate
