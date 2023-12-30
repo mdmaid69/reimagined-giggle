064da85d8aab1c32edb6cd43245b017ffcdd2427
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:33:00 +0000

    prize talkinto needle deliberately getup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
