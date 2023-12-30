b68a8508c8b5586f083514358be66d6e0b59e09c
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:54:24 +0000

    post possibly hello such wheel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
