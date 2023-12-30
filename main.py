4f15e597167a0ecdafbaad6ff01aa97c939f512a
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:56:53 +0000

    drop few right block serious

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  def subtract_numbers(x, y):
-        return x - y
+def calculate_gamma_function(x):
+        return math.gamma(x)
