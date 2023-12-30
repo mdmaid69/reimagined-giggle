0580a351a6e01f30b706b35c29fdc2d712b9752a
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:34:27 +0000

    prevent height sense physical print

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
