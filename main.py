e3d9d10f574a1e9344e5337dca5801368d27ec96
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:06:51 +0000

    flow port common sure chin

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import time
-def get_current_time():
-        return time.ctime()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  def convert_to_hex(n):
+        return hex(n)
