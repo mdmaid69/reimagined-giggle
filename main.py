cb63e70f6c231417df168bd67c39a65b650b8a33
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:32:23 +0000

    blow guide finish they surprising

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def is_odd(n):
-        return n % 2 != 0
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
