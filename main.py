965326eb56fd3ea62c930931d480756ec287bdd7
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:58:25 +0000

    crime cause visit memory trousers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  import sys
+  def get_python_version():
+        return sys.version
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
