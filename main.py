5bc7ed7a00a9223a9577609ee39bd009be56e197
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:55:52 +0000

    trade emphasize office keepup aim

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def square_number(x):
+        return x**2
+print(sum(range(10)))
