73bdce30a0acfd389c97b995ee05bd8b5fa898f0
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:11:20 +0000

    combination battery miss really chief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
