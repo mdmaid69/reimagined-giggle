c500eb6c78282aa4f32a17f22edfdb62e37d605a
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:42:05 +0000

    get invitation lesson further offend

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
