e0188006c13cd90c1d0b17163694fb7012761a54
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:44:12 +0000

    thick cry part progress release

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
