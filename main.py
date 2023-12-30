ee51c052149c41a11c749d5d396a6b59e2bb0a7a
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:29:24 +0000

    exam shoot repeat trade bang

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
