541b21c67194df6b0ebff7238f6f51c8b242b520
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:39:45 +0000

    part middle disapprove fit fond

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
