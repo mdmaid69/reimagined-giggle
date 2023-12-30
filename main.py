0a1ab034fe8a1922ab793f9c8fac13a9d5e754bc
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:30:07 +0000

    boil however that noisy happen

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
