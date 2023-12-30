780bfb1bc5d239fa2c467acb4a85a577a7fea116
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:53:37 +0000

    approval contact increase guess last

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_as_list(array):
+        return list(array)
+def remove_duplicates(lst):
+        return list(set(lst))
