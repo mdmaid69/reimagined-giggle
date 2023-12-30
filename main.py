18bdd0c1f5084e923354bc354a66f51db8d78b67
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:03:43 +0000

    Qq lookaround kind temper repair

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def subtract_numbers(x, y):
-        return x - y
-name = "Python"
-print("Hello,", name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def remove_duplicates(lst):
+        return list(set(lst))
