f69249bd97d74fc0c1f71bec5a53ec97a36ecc45
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:13:15 +0000

    post edge vision crime middle

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def count_elements(lst):
+        return len(lst)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
