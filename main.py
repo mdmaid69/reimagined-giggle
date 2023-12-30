b612a29da8c3b1f298b70b3baa352cba390ccee8
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:19:48 +0000

    lookup however glad excellent seat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_power(work, time):
+        return work / time
