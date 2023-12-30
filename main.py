f83154ae4b46ba911b92b9fc719f056d3699de5f
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:08:32 +0000

    minority behaviour emphasis modern entry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def multiply_numbers(x, y):
+        return x * y
