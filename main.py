60ee3f1c841cef1a8a66f04eb895e34f9251de39
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:35:01 +0000

    considering thinkthrough heavy sleep sport

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
