9b4d3e48a7c2bc15f8a1d2cfd26ac4f6101fbc78
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:53:11 +0000

    steady hard experience direction catch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
