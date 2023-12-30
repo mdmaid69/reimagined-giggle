93ac0dc8a9b618c849ad6cedc56f875c5861cdea
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:52:45 +0000

    belief organize me talkoutof long

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
