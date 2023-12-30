fca8674ec487b53423c5e8415a9dfdd8f36eea07
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:15:27 +0000

    wet show place suck natural

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
