5e7c4dfe1400fcbd9ca174e84e44812c1d368706
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:07:19 +0000

    friend shake special Tt tonight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def get_array_as_repr(array):
+        return repr(array)
