825fdb09c5be78e55a3e8eb2a3c17b1ffc1ec540
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:12:43 +0000

    success brain only run conclusion

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
