629afdb9fc7dbe48274fc6f6d5e0fd28808e6da4
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:39:01 +0000

    debt whatever level during at

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
