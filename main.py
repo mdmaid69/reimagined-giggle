0e89f11098f150abc6944abffca57a2f90539fdc
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:24:27 +0000

    inside trick notice base nerve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-name = "Python"
-print("Hello,", name)
+def square_number(x):
+        return x**2
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
