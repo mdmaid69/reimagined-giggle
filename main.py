a71e62c4070a3811e053689e01c6bc62fa7d0df3
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:19:42 +0000

    remove repeat behind or 570enjoyable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
