45c21cbbf4754b2ac60eb028c687f58de36bbc9f
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:50:58 +0000

    ring unfortunate impressed favourite state

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
