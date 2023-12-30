37a0a63caa2dccd72ce63a87fd0d53cf87f9512b
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:23:04 +0000

    hungry better bang mainly ourselves

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_octal(n):
-        return oct(n)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+print(sum(range(10)))
