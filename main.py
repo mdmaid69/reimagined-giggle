4edd4f65f09ea2d5aaad1f658eabded94714c9a1
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:21:11 +0000

    fond cheap however break except

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def calculate_volume(length, width, height):
+        return length * width * height
