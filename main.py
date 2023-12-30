feea96235b73cc652836b780c54a2e28b3801c07
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:18:29 +0000

    slice responsibility drive completely conduct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import platform
+def get_python_version():
+        return platform.python_version()
