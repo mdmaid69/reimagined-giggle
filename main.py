08b6bf57c7165e7858ac8b4fbece0385420238d5
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:36:56 +0000

    seat perform sweep influence importance

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def convert_to_hex(n):
+        return hex(n)
