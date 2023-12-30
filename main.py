22afab07ad584988386dd1baec40850a52df7bb6
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:13:05 +0000

    level public angry hard disgusting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+print(math.pi)
