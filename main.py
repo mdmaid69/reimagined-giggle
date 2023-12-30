2ed5c29bead843dc34f3cfd66dc5816c81465307
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:06:03 +0000

    task condition tonight convince oil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
