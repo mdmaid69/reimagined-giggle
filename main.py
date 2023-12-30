a4387347fb608dda49d067346e8987a80ca0eda6
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:46:47 +0000

    lose quick field right yourself

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def calculate_density(mass, volume):
-        return mass / volume
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import numpy as np
+print(np.array([1, 2, 3]))
