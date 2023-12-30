dff279fb27a250a53479d31fdf037db15d1074cc
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:18:11 +0000

    train throughout practical fresh stir

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
