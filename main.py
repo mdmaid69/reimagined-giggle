8c69379b2b160407b7afd42a0cbcd8e016d33e25
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:34:27 +0000

    law of loose music improvement

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print("Hello, world!")
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import sys
+def print_python_version():
+        print(sys.version)
