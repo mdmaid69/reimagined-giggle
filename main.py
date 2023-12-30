c1afe5255b115689ddc89d0821393ca808b2950b
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:20:55 +0000

    dream block often strike her

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
