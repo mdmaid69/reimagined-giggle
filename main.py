8542f82ea78748244ae4780a41a5234be7e2e394
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:02:25 +0000

    habit behaviour estimate confusing other

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
