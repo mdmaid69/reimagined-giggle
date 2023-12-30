85b9a9ffb3abcf657cb429bb5fb3ed943fa99b2a
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:43:11 +0000

    whether declare failure achieve effort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
