2d164ddd68d204284047a693529b0d0af4e3cbda
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:32:05 +0000

    reach repair key travel criticism

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_area_rectangle(l, w):
+        return l * w
