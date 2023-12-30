f4c6f1c2b0fba4b2c54c544eb19796e4229523b8
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:37:10 +0000

    fun memory shock part near

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import os
+print(os.getcwd())
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
