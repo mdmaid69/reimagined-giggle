99c7d88d966803837b464f260c947c6a59db9248
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:34:02 +0000

    bear case public factory so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
