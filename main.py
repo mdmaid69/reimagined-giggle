86e76172ddc7645fe19b54f3d8ba82069d85eb4c
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:20:42 +0000

    anybody mood pass how strong

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import random
+def roll_die():
+        return random.randint(1, 6)
+def find_min(lst):
+        return min(lst)
