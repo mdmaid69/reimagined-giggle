ee6527a90168a3216a126ce5cc0917592f552d88
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:32:49 +0000

    side cut central desperate target

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
