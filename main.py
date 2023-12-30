63876ab443b6b809771843939b6370d57fc605c5
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:44:05 +0000

    difficult satisfied health race wish

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import sys
-  def get_python_version():
-        return sys.version
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
