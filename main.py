6af5f4fd1919a29f2178b606dfbe1c563ce57445
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:35:55 +0000

    thought serve press generally top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
