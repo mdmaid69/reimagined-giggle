21be26d1f04a01956cbe4654e3fb9c455d9283f4
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:30:51 +0000

    pretty even well slight which

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import sys
+def exit_program():
+        sys.exit()
