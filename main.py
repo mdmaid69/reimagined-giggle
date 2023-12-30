62390610330997547b499fa947815ab1c3138e78
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:26:27 +0000

    choice comeround know grade move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
