2e4f31f37c29989c54d1830f41ccc7513aca20ed
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:55:28 +0000

    measure dull case tread advice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
