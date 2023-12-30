69e5410a0573e89b262609574d321ffdb77d03c7
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:22:20 +0000

    fault it cold anywayanyhow level

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def is_odd(n):
+        return n % 2 != 0
