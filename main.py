f5ba4680a933c3e5939870a70b5017eaaa965d64
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:00:34 +0000

    fast thought unpleasant comedown occur

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def remove_from_array(array, item):
+        array.remove(item)
