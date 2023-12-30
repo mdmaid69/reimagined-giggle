5bcbcfaab5162a933127bc0fbfa69a196dafef8e
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:28:02 +0000

    storm wipe late cope pain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_device(file_name):
+  def get_file_dev(file_name):
         return os.stat(file_name).st_dev
