fbbd496d6b6fe74b5a7cfbf18d34bdc120c38186
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:40:10 +0000

    buy open female fond not

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import platform
-def get_python_version():
-        return platform.python_version()
+def square_number(x):
+        return x**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
