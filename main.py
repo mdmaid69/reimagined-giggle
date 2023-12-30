32bcec58f7b433c2e52fd93375cfc9fe4e54414d
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:06:31 +0000

    neat blame loan practise platform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
