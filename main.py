8031cd4171e9abe7f1676ffc557f34ebbef5c403
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:18:52 +0000

    bin no prevent period nor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_work(force, distance):
+        return force * distance
