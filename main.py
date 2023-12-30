0d9f36ee31ee62e833fd882252639ff6e42ac4dc
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:19:43 +0000

    muscle smile yet separate past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
