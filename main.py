249d2e3d9c239331eaa37c04ddbb2ba84bfa5e44
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:59:02 +0000

    length still blow brain harm

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
