caa084c92b4a031aac08166be0d4ef999f7a9843
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:21:54 +0000

    advice beat file bend giveaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
