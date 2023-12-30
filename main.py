fa7a05aa4b36750d66b9ec420b6255bec880fd49
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:24:11 +0000

    cheek team present away true

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
 import collections
-def create_user_list():
-        return collections.UserList()
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
