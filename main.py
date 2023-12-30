a116f6a186d0b20de427273f1d2bf7fc4f594fde
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:32:51 +0000

    fear children criticize close know

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import time
+print(time.time())
