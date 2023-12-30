8c221fcee482d37e71b8741765faad67c6bb7d19
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:42:12 +0000

    rent injury pupil rely though

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
