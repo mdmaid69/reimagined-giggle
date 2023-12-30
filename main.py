c59231f6d68ee28914ef9e2e6a1855f58cbb1c6e
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:19:04 +0000

    wing necessary personal not dig

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
