1b66bfb50f8843bd0e2e36825d82f0790185eeac
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:46:23 +0000

    first stir lunch partner lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
