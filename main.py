850d966d3fbde96513af37de6eb84438e314ee94
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:08:16 +0000

    rent relation interfere deep try

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_speed(distance, time):
+        return distance / time
