203050cde40788a89ee1cd608396d8905a013d50
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:45:49 +0000

    gather cry gift holdout deep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
