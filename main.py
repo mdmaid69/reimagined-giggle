ea7f5ae93750cb59eafc65e1ef9a99ad7ca96167
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:55:18 +0000

    hurt confuse wander large cope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
