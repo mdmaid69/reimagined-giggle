5b8c35affa3cb0cf32d511ddb5154ace587ed58f
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:22:24 +0000

    cold Tt back recover delight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
