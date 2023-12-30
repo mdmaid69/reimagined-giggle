8867493b74a5715d297e3d77a05ac3e5be6a1613
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:45:35 +0000

    blow exchange slip scene whistle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
