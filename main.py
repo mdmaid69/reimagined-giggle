32ca51b761d5bacab27117671c9fddc0c429007b
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:11:51 +0000

    phone present worst draw mine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
