2a445e40c23d76ea264ead4c2ace0a1543dbd0f2
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:51:43 +0000

    container reject lower urgent treat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import numpy as np
-print(np.array([1, 2, 3]))
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
