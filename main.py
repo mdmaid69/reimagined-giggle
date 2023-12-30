3a59b9c080bed1e7f79713b2490b10b80fd78f59
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:53:03 +0000

    needle nice use breakfast carry

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
