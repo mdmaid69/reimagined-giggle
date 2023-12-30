460af9edc146b2eefbba6ed6feb7b1d383496c53
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:29:21 +0000

    imagination cut ear point spring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
