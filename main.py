5ce5bf38742486f6d4578534016508d9e2f32bab
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:43:59 +0000

    left so clear small breath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
