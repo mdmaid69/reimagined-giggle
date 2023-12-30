e0596778721182f93b6c99208c2181d7875dcd07
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:52:38 +0000

    false list soil conversation square

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
