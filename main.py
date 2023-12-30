5bc161303215924df257f3b73dd54e5e682f432d
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:22:56 +0000

    yourself discovery godown forgive section

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
