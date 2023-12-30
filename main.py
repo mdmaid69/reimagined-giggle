c3f0fc6f67f8cf291406ca8b15a6a3531d3ff927
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:50:46 +0000

    branch task combine kind mystery

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
