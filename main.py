bbf06193a7cb3c58356bf88428bf5abe94682729
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:59:02 +0000

    recent comeover introduction occasional realize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
