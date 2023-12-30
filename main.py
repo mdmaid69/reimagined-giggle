c216fc7aa44c40a906b65e33c97d220b35f0379d
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:27:30 +0000

    sleep relative dig advise last

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_list(array):
+        return list(array)
