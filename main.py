969a7b1d9e6e53659608e8c29470b57559ecae3f
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:42:01 +0000

    talkinto lean friendly your advantage

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
