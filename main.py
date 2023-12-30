c5a639c6d4e4978416beef5949d64ff947745724
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:43:20 +0000

    Qq off few exercise present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
