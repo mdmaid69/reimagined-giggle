affcc81c1028439bb75c2694819db4c0a399670c
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:40:06 +0000

    even judge impressive visit apply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
