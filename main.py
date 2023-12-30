7f8687cf962e966ee47b95f4530d5e4484a60fc2
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:16:03 +0000

    grade stone century the change

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import time
-print(time.time())
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
