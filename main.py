ee04d2e77a78a9bf97f5187a0ddbdf174f64e105
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:09:04 +0000

    balance board Hh mine large

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import collections
+def create_user_dict():
+        return collections.UserDict()
