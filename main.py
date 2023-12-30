fadb928db2a9ef31f4a0404f98d1a00d468cdc56
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:40:57 +0000

    duty basis refer communicate home

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import numpy as np
+print(np.array([1, 2, 3]))
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
