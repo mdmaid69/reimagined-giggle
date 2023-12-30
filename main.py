a7f55ccdcd2795d14eb7cffa469960861b06310f
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:46:36 +0000

    ignore event responsibility backwards arm

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import re
+print(re.match("h.*o", "hello world"))
