cf017e8ce4d534ce2b6f2c8b8a55a6dc659e94b1
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:47:38 +0000

    gentle tired sing short help

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def add_numbers(x, y):
+        return x + y
+def convert_to_hex(n):
+        return hex(n)
