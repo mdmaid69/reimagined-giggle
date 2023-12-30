4aaf41795a412238e6914aecc1cddbf91dd27f7c
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:07:41 +0000

    roof pity ought plan smile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import numpy as np
+print(np.array([1, 2, 3]))
