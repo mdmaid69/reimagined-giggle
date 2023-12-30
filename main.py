c4c570d30dd3b28b9e46c820cbd70785fb1d78fc
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:18:01 +0000

    cost welcome crisis fork beneath

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  def calculate_area_rectangle(l, w):
-        return l * w
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
