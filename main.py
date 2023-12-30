805a1e78065e4273f69599db2a1b1b43678050d6
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:21:13 +0000

    far early flow size convincing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def square_number(x):
+        return x**2
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
