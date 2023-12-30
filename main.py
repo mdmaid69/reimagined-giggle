bea09926a6aef4fe61e89359fc04177186fcd673
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:25:17 +0000

    correct reason mean form emotion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import array
-def get_array_item(array, i):
-        return array[i]
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
