9740a9b4faa66971897bef3b3b8485a52ca96e30
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:01:02 +0000

    sleep formal instant dress Yy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
