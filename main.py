9d90725c499bcf709fedeb2fb9e9903903c258a0
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:18:24 +0000

    doctor Uu exact shape me

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def is_odd(n):
-        return n % 2 != 0
-def is_odd(n):
-        return n % 2 != 0
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
