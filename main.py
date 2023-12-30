8999b0b2410de25eb24e014eea8d607ba0a34eb0
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:49:29 +0000

    Dd apologizeapologise or getback test

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import tensorflow as tf
+print(tf.__version__)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
