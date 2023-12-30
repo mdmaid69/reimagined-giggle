caf6ef03cb773913861c659634a0d8eaa80ba297
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:40:21 +0000

    fence design crazy lonely rather

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import datetime
+print(datetime.datetime.now())
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
