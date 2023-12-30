f96684825973cb4155eeec49b186c79d2d88c7ba
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:21:52 +0000

    cheek Cc separate foreign whistle

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
