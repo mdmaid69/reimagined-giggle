17734b9ea1de48481e442c5e59416a7f8f620e76
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:33:12 +0000

    children previous interfere getback ground

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
