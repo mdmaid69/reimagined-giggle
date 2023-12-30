0a032e344501f749606f761b7767fa6422ebbe2e
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:53:04 +0000

    area cross kill sort phone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+x = 10
+y = 20
+print("Sum:", x + y)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
