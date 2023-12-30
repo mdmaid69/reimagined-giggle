c47846ac81961f0ec5c371fb401da4df3d0892d9
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:13:30 +0000

    phone border measure wind sentence

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-for i in range(10): print(i)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
