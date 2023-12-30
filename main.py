3787909471af857c202527643d82f5f4288374fa
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:27:32 +0000

    reply risk warning react smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
