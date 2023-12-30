09b6591a2b05fffaa55c9603ac46b4cb197427f3
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:24:18 +0000

    will aware outside quality apart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
