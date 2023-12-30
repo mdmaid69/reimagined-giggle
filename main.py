fc62b74b44859f31d123991a4f2bf32656412647
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:06:34 +0000

    yet bank appear pollution Rr

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import math
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
