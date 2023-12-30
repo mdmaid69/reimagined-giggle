a17d6f7713f291923393a4fb05abfdbc4fb088df
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:50:21 +0000

    connected Vv 565end profit comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
