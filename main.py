48e944f192a4eb08321ae959449ea359914a10bd
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:12:25 +0000

    normal bother study necessary cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import collections
+def create_priority_queue():
+        return collections.deque()
