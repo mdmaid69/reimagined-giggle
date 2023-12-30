b49f71fc449cc196c84e5f53e96159075fc34d27
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:55:44 +0000

    house ask confusing soon act

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def calculate_area_circle(r):
+        return 3.14 * r**2
