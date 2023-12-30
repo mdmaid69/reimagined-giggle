1af92f92afa6e33ed1adf69f27ebfb3a492c79f6
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:27:34 +0000

    do operate guilty check and

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
