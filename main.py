fbaeb782fb29ffbb1194e504ca39558048d40dfe
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:54:28 +0000

    punish best reason Uu Ee

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
