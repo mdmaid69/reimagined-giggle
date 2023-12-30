ab7b209cb3104f46110cc9be328bc4ea9def8f72
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:35:01 +0000

    effort activity lend easy hour

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import time
+print(time.time())
