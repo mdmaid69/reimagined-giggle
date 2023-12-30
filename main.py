4cc5d9851c00bed152650cafa7520a533fabe69a
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:49:48 +0000

    once shout female pair leg

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_area_circle(r):
+        return 3.14 * r**2
