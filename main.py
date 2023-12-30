9a4260355a01c2c7a0afb277af59c63c8cefe143
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:34:04 +0000

    fire path possibly deliver cause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def remove_duplicates(lst):
+        return list(set(lst))
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
