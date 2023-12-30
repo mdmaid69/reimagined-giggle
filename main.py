1a1dc31056a0474cdfbf8fc70e4294f4e93c867e
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:17:59 +0000

    Ii me by other post

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
