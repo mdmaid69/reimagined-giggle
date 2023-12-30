d5b0a67fab78984a8a042b39ce03281f8258bf54
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:50:06 +0000

    doctor let century fast say

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_min(lst):
-        return min(lst)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
