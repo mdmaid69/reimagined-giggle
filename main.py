dcacfcaccc88efb1dfa187eae98d843130df2fc7
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:54:02 +0000

    value fine pattern away balance

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_area_rectangle(l, w):
+        return l * w
