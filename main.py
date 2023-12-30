63d212bb41b6cefbcb2e22c36e9a1fc7370ce045
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:27:55 +0000

    father breakdown godown store popular

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
