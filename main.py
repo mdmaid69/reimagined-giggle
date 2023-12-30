d3406e10a39cb4dd8e826988c9bbdaf2b9111908
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:27:48 +0000

    direction address generally office occasional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_as_float(array):
+        return float(array[0])
