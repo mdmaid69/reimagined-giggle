d2d613c22bd56539634cd1ef68822ebe7928af2b
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:28:38 +0000

    direction skirt exciting obvious relationship

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
