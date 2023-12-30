7749b75d57164f1144c852c09c93d73141cb9ff7
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:00:11 +0000

    guide physical ask they mistake

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_float(array):
+        return float(array[0])
