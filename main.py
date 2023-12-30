2e94201fe536031bc49f936748f42f57cd0a268a
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:55:35 +0000

    cruelty talkoutof Yy later recovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
