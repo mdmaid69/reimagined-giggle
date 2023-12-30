d6ac4d62b3fb7f2b07c199d03f579761daaed743
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:02:11 +0000

    criticize what evidence slowly 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def sort_list(lst):
+        return sorted(lst)
+import os
+print(os.getcwd())
