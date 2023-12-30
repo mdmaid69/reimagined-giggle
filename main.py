76eafc8a64aa6bce750343e1e9620a4fc76fb4fc
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:14:52 +0000

    record grow discovery money ride

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
