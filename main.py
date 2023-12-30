81e3d81f73546911eef985d91eb56abc95994148
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:39:12 +0000

    petrol switch glass turninto cheque

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+for i in range(5):
+        print(i)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
