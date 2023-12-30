bb1edb4f8ecb1dd1d882c78388ea2e0439b8d417
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:03:53 +0000

    thanks correction destroy design independent

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+print(sum(range(10)))
