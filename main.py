78181984f9ff94c77e49c2bef4e0cb1291f80781
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:25:43 +0000

    permission child mass protection feeling

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_tangent(x):
+        return math.tan(x)
