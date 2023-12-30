e4c72f054dfd583fac42723c0b4dbff1905ae1ca
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:50:22 +0000

    turnover sea introduction clue bet

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
