1444bb2c5d2069800a724634759d33a5d802e4dd
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:26:18 +0000

    town city why reply appearance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
