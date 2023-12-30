e928fa6f2854ac817a009630c6b35f9ac83a8145
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:54:39 +0000

    move block remain statement walk

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(x, y):
-        return x + y
 n = 10
 print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
