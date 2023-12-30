ef81bbeafe50a5f31da7c2ca0c7f3fab44165c06
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:17:02 +0000

    mark round bottom church challenge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
