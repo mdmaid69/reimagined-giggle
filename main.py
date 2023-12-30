49f2c6faba2ba5b1c1104a881d409260b5d4c237
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:38:27 +0000

    past recover frighten Ii turninto

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
-print(os.getcwd())
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  def calculate_area_rectangle(l, w):
+        return l * w
