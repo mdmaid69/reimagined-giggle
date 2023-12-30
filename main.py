e3f979c8cc7f95cbe4fca436ecc3a39683a1c9ed
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:02:29 +0000

    give possible bush price remark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
