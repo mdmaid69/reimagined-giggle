06619a83a4ef4c4c9b70501e305cd1ec91f9dbf1
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:27:07 +0000

    round protest forever contribute himself

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  def multiply_numbers(x, y):
-        return x * y
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
