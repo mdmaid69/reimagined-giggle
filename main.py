527bb3e0a753740b66204a6e671a6a3eb04a3290
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:56:57 +0000

    upstairs yourself habit train react

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
