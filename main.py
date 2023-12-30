e6e5b32cb7d8cf1950fdd75efb6ef0796fc2302a
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:31:56 +0000

    stare thirsty ask summer show

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-print(sys.version)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
