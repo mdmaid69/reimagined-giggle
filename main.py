2ef0292a8f0cd4e35377dd2d0b098a17b73ab102
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:40:19 +0000

    score famous strike praise boring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
