b622cb1f27dd12df874800d688800de7928f9760
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:35:56 +0000

    wide age crowded switch strength

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
