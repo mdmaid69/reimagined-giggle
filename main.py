7e66ba290ad3b5c34bc9d64311bd42c8348d5aaa
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:08:49 +0000

    instead damage lose buy afraid

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import collections
+def create_user_list():
+        return collections.UserList()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
