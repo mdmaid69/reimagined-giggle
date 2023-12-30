0313809afc3a5302978eae41c0d16f58b8e68369
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:55:09 +0000

    there comein content crisis generally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def reverse_list(lst):
+        return lst[::-1]
