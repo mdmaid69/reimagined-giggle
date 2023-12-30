250203681f03454dd6a520c187153c18c29a5e6c
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:13:24 +0000

    wide scream although kind broad

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def multiply_numbers(x, y):
+        return x * y
