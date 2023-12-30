6406ead2b95abd346a8b9685aed7efde053e40c5
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:48:24 +0000

    partner pretty disagree recover regular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
