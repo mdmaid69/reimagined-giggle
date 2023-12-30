326cd88065414c25b05d4aca8bc74c6c3c6c6bf8
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:14:29 +0000

    shoot punishment praise thin runaway

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
