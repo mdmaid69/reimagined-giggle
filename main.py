8b70b1abce5a78fe8c9d93a57b6252a2119057c5
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:23:11 +0000

    pencil provide image wind forgive

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
