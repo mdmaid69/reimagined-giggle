97e06289f6aa26a197d014e3a7755c55de7ff5c5
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:40:14 +0000

    last teach along pleasant pinch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import time
+def get_time_since_epoch():
+        return time.time()
+import os
+def change_working_directory(path):
+        os.chdir(path)
