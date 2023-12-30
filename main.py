6388527e0e7a34961426bcfa6b52bec4ecf1fc4b
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:15:57 +0000

    attention greeting false unite responsibility

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
