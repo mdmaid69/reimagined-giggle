6f7057c9a78e22154c095383d377c44bb32b24ca
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:36:14 +0000

    judge environment later tough future

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
