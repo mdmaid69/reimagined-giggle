4714698af8a1b55581e3381d9e079901a77d87f7
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:34:49 +0000

    class difficulty apologizeapologise pipe holiday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
