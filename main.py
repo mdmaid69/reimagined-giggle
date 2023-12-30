36014dcc0cc1e023bc0ff9d8547c7a0ce9dde9c1
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:06:29 +0000

    radio shallow base walk your

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
