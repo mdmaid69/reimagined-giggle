b8acf249b696249812e1ec91f6a71d7fe84098ae
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:20:50 +0000

    attach Kk another comeout squash

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-name = "Python"
-print("Hello,", name)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
