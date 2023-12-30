640d96db23f20152f9685be91f2779bd414d337d
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:07:37 +0000

    sore aim explain runinto basis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
