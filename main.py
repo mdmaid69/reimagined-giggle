beb765c32ef303afe45a748c2bfcad780d1a1707
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:52:49 +0000

    possibility get want past fashion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
