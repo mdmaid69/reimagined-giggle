2f82f2c356430c516da01227680143d2c1a30b6c
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:19:36 +0000

    emphasize clean light let long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
