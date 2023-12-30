531c01b7d9bae4724db2c8412465201ed0aaeb43
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:45:05 +0000

    kick spare exactly container bet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
