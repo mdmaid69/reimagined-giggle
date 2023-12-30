1623d5e0da8f81939d340af93736c209bc98b928
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:47:18 +0000

    imagine success no related year

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import sys
+def print_python_version():
+        print(sys.version)
