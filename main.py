9b8ac64d510b041d7d49f732f8bb1e61a2bddde4
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:22:28 +0000

    live past flat again event

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
