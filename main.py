17cb6f544709d5e9bee616cf6fa871f0712c7647
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:44:49 +0000

    aware exactly yourself neat far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
