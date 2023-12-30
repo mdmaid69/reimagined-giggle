3f8aea90ffd795be4def24c73966f38b0e6e6326
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:23:20 +0000

    start pollution sore should ilovedicks

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
