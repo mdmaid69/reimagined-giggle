8e7057e1e377a43f5820dc205129dbc5bae5edcb
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:23:38 +0000

    disagree extraordinary black punishment joke

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def sort_list(lst):
+        return sorted(lst)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
