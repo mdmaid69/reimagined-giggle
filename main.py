95fbc7a8ffac6a4b7cdd1ba8fe95398b14aaeb4d
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:21:28 +0000

    avoid fun fall design complain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
