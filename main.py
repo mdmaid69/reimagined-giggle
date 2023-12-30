45626d7adcc9af23821bcd4c59c731ba2eab242e
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:48:38 +0000

    introduction traffic physical understand road

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
