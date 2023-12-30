0a9c8a84204805c1163a9ba766ab014822641316
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:56:38 +0000

    impression business rub somewhere miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def reverse_list(lst):
+        return lst[::-1]
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
