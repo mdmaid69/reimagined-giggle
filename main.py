dd6b147311a1705f72ec6a9a8b85eb45f170ac2c
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:36:54 +0000

    herself dull offence bang choice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
