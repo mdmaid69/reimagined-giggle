aaeebd12b1b6f004df1e95a0e945e1a20a9debfb
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:44:33 +0000

    unfortunate properly summer natural movement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
