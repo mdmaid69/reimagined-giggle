98978b045867e6aeb0d7d60db2542d392fe5d520
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:24:05 +0000

    press shine payment want sweet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
