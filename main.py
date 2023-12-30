d3d5af7d7e12e45b79006b3f167dfbf593a50d74
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:12:06 +0000

    interview spare just turnoff cry

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
