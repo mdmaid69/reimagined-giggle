4eccb6fe8163d252e70a91c56f79f71497a3502c
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:32:11 +0000

    interview freeze window finger quick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def sort_list(lst):
+        return sorted(lst)
