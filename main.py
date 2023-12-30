03661fec507baac06ce3f9be4254b7653885211a
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:52:33 +0000

    pause failure affect boring drink

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-print("Hello, world!")
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
