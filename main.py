c349b398080ae6f5116d9ec661461e765cd3afff
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:48:17 +0000

    score rely dry crime bringback

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
