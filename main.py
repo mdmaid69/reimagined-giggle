332abbfc7a30c4f91ee30b02719ca198830da061
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:05:11 +0000

    whoever level low reasonable action

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import time
-print(time.time())
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def count_characters(sentence):
+        return len(sentence)
