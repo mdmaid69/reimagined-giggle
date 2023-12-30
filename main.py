9711ee6e6db56fc1d4093acd974652bfc313530f
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:06:12 +0000

    Mm method computer trap walk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
