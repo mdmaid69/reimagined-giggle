9cf43443af27a5d5a3471e839bb89f7331914820
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:01:27 +0000

    reasonable fond fresh realize part

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
