6b14ec534ba3ebf0b107800497028387d06d7422
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:05:13 +0000

    anywayanyhow can loss present love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import numpy as np
+print(np.array([1, 2, 3]))
