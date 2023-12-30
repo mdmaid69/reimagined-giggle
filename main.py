e84bec3a2d1aa034660612ed523feb410a92fd3f
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:15:40 +0000

    want bored admit home unhappy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+from collections import Counter
+print(Counter("hello world"))
