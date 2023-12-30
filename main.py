b3f4d891142bee1a74ff2de757bbd0439310bd73
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:31:16 +0000

    read translate development hurt see

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import platform
-def get_python_version():
-        return platform.python_version()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
