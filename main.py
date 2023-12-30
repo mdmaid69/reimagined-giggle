486774dfd3bcd0454475c50e4275e3ba80264410
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:53:45 +0000

    aim play idea date production

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
