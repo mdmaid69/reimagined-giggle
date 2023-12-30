0a052c87c7539deaa26195bd5b7c331e5f548fbf
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:17:06 +0000

    head criticize warn summer turnon

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
