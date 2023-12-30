5730d66ce645f6ad5bf7d3fa5f4c099a057f6db6
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:17:48 +0000

    rush makeup contact question cry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import getpass
+def get_username():
+        return getpass.getuser()
+  def remove_duplicates(lst):
+        return list(set(lst))
