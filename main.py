cd86e563a8c884e30521347af64aa058cbb85d14
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:44:01 +0000

    ours direct exhibition cover job

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def sort_list(lst):
+        return sorted(lst)
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
