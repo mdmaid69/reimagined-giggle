fa570d4371c0d2dc887d74a477d3e6d7bce64d81
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:49:58 +0000

    bath accept view contrast bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
