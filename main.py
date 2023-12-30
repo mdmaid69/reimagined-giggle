96b3b4b89fcb4c78a41f32cdfde0a2e3a58e461a
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:40:47 +0000

    bit open 564encourage once weigh

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
