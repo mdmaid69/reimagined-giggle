b178e64da4abefea210b07aa6ab32a5aaea6ca31
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:56:35 +0000

    story grow up unfair goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
