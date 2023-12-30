8de8c2a410e1ceebed663b5f63708ec9b43162f0
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:02:27 +0000

    stage guess suck town smooth

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def find_unique_words(sentence):
+        return set(sentence.split())
