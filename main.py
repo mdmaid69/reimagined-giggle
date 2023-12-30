d576e3d0a1ce39f7398b4fe0fc59b9e861071164
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:12:36 +0000

    odd both breakfast embarrassing glad

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
