b8abcb88a4648467ac4b2cafdf24b062c6949250
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:10:46 +0000

    me fear medium home load

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def reverse_array(array):
-        array.reverse()
+def count_elements(lst):
+        return len(lst)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
