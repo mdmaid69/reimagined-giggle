0884bc03beebf06ac408ad85a1a548c46ef74034
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:24:05 +0000

    cost getoff valuable share song

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import array
+def get_array_as_float(array):
+        return float(array[0])
