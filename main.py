11ec0ab1d89eb7fbe4ca94502398d32ffd509822
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:44:32 +0000

    strange promise answer runinto spend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_float(array):
+        return float(array[0])
