355263b95f8bb49c775b5fba3af9b61fa2c6fdea
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:03:22 +0000

    hold embarrassing task sorry in

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
