21ed42082e9bde40c8ba69980770a0d6cff81dde
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:53:00 +0000

    major aware any ready ought

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def square_number(x):
+        return x**2
+def find_max(numbers):
+        return max(numbers)
