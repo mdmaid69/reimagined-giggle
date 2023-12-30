d1d82ebfb92ce387516f798aad9a0207fc51152f
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:46:51 +0000

    herself before ear score model

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_area_circle(r):
+        return 3.14 * r**2
