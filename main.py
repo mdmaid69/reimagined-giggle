5e349de86bd29a79e6865509e1bb7f31238f47f1
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:28:05 +0000

    turnoff rather rest busy politics

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-print("Hello, world!")
+def sort_list(lst):
+        return sorted(lst)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
