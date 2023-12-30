08b1b6463b551c972692c610396e51f78b201a03
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:59:42 +0000

    strange goout substance appeal best

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import datetime
-print(datetime.datetime.now())
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
