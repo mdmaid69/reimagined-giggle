95214a165c01c881d21f78926f12c6adf8fadf3a
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:25:29 +0000

    everywhere rub arrive and see

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def get_array_as_int(array):
+        return int(array[0])
