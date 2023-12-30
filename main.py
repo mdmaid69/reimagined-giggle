e367894b8d2f82e19b73961000b8f49ab07b8281
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:47:53 +0000

    justice list follow cover object

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
