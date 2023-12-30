078db8b5bb116a87b976a33291a4f7ec17bef101
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:27:33 +0000

    fight guilty minute fashion ask

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+for i in range(5):
+        print(i)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
