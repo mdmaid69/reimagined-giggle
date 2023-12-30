e243103d1a52b5031713fdd189d278268feba9c0
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:21:44 +0000

    pocket immediate past concentrate dare

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import tensorflow as tf
+print(tf.__version__)
