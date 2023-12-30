2c0cd314551e6c1943058e9169d89403bcfd3469
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:16:43 +0000

    system morning exploration admit design

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import datetime
+def get_today_date():
+        return datetime.date.today()
