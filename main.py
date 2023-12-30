a1aad789c4ef1d80a7fa73f5a3af3a6fe5762375
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:12:10 +0000

    mine rest feed less closed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
