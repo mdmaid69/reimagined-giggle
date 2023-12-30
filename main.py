11d5009060e98a07f518ddd21d3258d08bb7f0c9
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:09:55 +0000

    public workupto jump summer actually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import collections
-def create_stack():
-        return collections.deque()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
