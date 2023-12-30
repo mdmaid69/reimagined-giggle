74421711ed73ca9377f7199f74615c166500b359
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:06:52 +0000

    ever spread achievement drive sorry

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
