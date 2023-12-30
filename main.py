a998324ee5095045918797286ae8ec83b424b323
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:42:02 +0000

    pipe wealth until good mass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
