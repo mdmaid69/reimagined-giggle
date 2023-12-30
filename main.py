d37705a678ec09d5aaf05ad4915fcc3fe50faa45
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:06:51 +0000

    variety dish introduce of favourite

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def is_even(n):
+        return n % 2 == 0
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
