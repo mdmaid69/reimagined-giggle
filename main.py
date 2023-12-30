be686817a368e54c8b2df7db6140e4f3a5a0be63
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:39:15 +0000

    concentration to peaceful absolutely window

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def sort_list(lst):
+        return sorted(lst)
+def is_even(n):
+        return n % 2 == 0
