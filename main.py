9c957c94d93a272b12e6f8896c86e3c541a07ee3
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:32:05 +0000

    crowded business too achievement advanced

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
