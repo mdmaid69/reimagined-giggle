43ad79572985a4e317f7c8dec3b24978855b1f9c
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:20:39 +0000

    short trick warm clue noone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
