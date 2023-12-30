693881ddef55ef97467a1af0a1f728dab2b0d9ea
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:43:33 +0000

    sad tool hungry equipment home

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_max(numbers):
+        return max(numbers)
