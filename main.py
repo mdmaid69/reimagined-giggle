da09ebe34739c583d43f4e576f920e0ddf85ed3c
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:11:06 +0000

    use favour brown trousers test

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
