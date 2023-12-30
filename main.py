cddf336e35bff248b3afc64f526f234a6a1ead34
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:16:18 +0000

    no if ignore generous nowhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
