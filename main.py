1fb4d62510bb61a2f829542488c4fc5a25ab0041
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:22:46 +0000

    second attract hold scream disapprove

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+for i in range(5):
+        print(i)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
