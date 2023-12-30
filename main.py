ec4dea4f8ea70712adaf5d461af5a0cd662b374d
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:21:23 +0000

    sense mess combine ground police

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import time
+print(time.time())
