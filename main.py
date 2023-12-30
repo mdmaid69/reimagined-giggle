aec76bd070d8d5d0fe21c7cb2601d4f4e3d9d5a6
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:09:56 +0000

    properly complain variety excuse 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-import array
-def convert_array_to_list(array):
-        return array.tolist()
