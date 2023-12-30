e7ac7b7e1cf3e0f586c1f5e6d43d3ec490731ec1
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:26:37 +0000

    destroy earth on factory pressure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
 numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
