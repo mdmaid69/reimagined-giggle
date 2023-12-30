6559e22aabf4d232c5f14a38bce6dbbbdc5911b4
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:09:33 +0000

    next phone spot male harm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
