bf17617d0448ffe151113fe8ca675c5cf028a873
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:59:04 +0000

    moment bright thinkover pronounce sense

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
