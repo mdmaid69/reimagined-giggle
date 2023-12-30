04e2ae10333f519bfe731de3bd1c82fffcd29acb
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:25:39 +0000

    television bridge rest arm abroad

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(numbers):
-        return min(numbers)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
