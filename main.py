dfbbf0c7447ddc3fdf44e6a3d437fa0d3e926b7a
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:15:56 +0000

    hang please dish soil track

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-  def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
