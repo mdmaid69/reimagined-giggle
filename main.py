9e991d017294f24935b6625e24941b632e9a67e5
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:37:55 +0000

    before adjective turn contents Yy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_density(mass, volume):
+        return mass / volume
