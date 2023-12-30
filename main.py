9275c51fb34e7ca80376d311693971d30d144017
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:35:40 +0000

    introduce failure watch callback interesting

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def convert_to_octal(n):
+        return oct(n)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
