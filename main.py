4c40b1b561e3f7801047e5d724330f6288041177
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:46:07 +0000

    down popular talk excuse anywhere

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import logging
+def log_message(message):
+        logging.info(message)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
