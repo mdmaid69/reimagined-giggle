921267c2ebba09972a6e2c4b583c888d4032c94a
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:23:25 +0000

    draw dish impatient answer only

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import json
+print(json.dumps({"name": "John", "age": 30}))
