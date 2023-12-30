9827c443699567b3a13e009b8c1ebd0cc82c9ffa
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:31:36 +0000

    completely strike week anywayanyhow whistle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
