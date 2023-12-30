6ca6b1156375cde8927cef8408243f2c0fb0493a
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:41:44 +0000

    joke home beneath closely process

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
