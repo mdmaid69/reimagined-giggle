001b2dda212979b3dd6bb018ef4a8ec2f6b48e4a
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:23:09 +0000

    snow level failure boil exam

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import collections
-def create_priority_queue():
-        return collections.deque()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
