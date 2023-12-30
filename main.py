790f371c5f892424bc0b94fb5efba1be9ce21f6a
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:40:36 +0000

    boring ignore suit majority welcome

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
