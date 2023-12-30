e191cc816f1e93254935d073fd4eb7a71cb51d9b
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:48:03 +0000

    sentence female father aim actually

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+for i in range(5):
+        print(i)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
