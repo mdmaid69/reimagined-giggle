b4c6df41b44ac6db0ab1a1c1dc5028ff757b52b1
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:32:55 +0000

    health behave prevent joke police

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
