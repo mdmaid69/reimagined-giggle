bfb9c3277bfd0e84bc286c41f7981fa2d51bc2f0
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:32:55 +0000

    claim design murder independent noise

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
