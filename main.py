ff69f12dc851ec472a63099ff6a4b7cd83adc753
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:07:38 +0000

    test breakfast talkoutof below dream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
