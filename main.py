09c2b853f5774b34b89307a3b87b5ba082d3547a
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:33:57 +0000

    operation steal bend also peaceful

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
