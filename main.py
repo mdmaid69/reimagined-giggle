279d8168e978c5348794ef233e2c8de1a8f52366
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:11:08 +0000

    benefit operate disagree style whom

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def convert_to_hex(n):
+        return hex(n)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
