a62a3fd983009ca2d1d62822a41b5917df1559af
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:58:20 +0000

    action entry dress prize burst

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_area_rectangle(l, w):
+        return l * w
