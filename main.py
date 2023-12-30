7a212e96b6238374faf6fd1784d86c6d8319e26e
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:50:42 +0000

    ill away plastic shrink blow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
