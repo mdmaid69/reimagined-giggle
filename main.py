91d7b5d5f55d96b69287da8ea0600172f18ff924
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:38:33 +0000

    contain pain extraordinary shock talkback

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
