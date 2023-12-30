22b3d58d631e7ff76ba3d6685d876b59f6c55f56
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:15:29 +0000

    deal strong crisis bank fence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def convert_to_hex(n):
-        return hex(n)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
