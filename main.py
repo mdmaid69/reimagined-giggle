2e5623983705e2de56b437cacaa3b2aacb1b8c2b
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:18:07 +0000

    disagree recent blow none dry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_perpetuity(payment, rate):
+        return payment / rate
