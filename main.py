dc2769b93032c06939be699984fe24a1d1c76a04
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:55:08 +0000

    plate speak out now influence

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-  def multiply_numbers(x, y):
-        return x * y
+for i in range(10): print(i)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
