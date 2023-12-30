70344fb1dca41d0ba842e821c1b5c1560a88999a
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:37:20 +0000

    lookup no better better finger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_slice(array, i, j):
+        return array[i:j]
