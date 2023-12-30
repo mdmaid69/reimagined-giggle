6b1c196a52ae8808ddc5182e8a7795bd73eeb57c
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:40:52 +0000

    Cc sentence connected meal gentle

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
