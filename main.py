86a70ac21ecf007178c48c75c343833353fb2e8e
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:45:43 +0000

    wing giveout here impossible air

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import numpy as np
-print(np.array([1, 2, 3]))
+def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_as_float(array):
+        return float(array[0])
