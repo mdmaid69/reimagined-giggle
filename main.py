7c7beeba51f6c823eac4dec8709a4c5be1ec5cad
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:51:40 +0000

    detail rush ball tongue smile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
