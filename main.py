0b2ab64db5563b7f104b6acfc9b195f4104c5771
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:40:48 +0000

    possibly turnover meeting ashamed rain

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
