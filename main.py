f71e0b1b9d108cf4b683b8ec3335fae8404cb9f4
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:34:11 +0000

    their last may can above

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
