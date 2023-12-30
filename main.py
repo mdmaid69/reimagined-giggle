eca81f5c7b353567e1988848d301aefbef861d1a
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:13:48 +0000

    my smile justice safe still

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
