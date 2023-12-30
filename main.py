d129ce205b67021919154d473152a610d05dbae6
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:26:43 +0000

    attention in other task action

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def count_characters(sentence):
+        return len(sentence)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
