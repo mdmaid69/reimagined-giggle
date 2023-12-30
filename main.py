ab55546925b1f1331bff15c3d1ff8bbbca4e0152
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:01:18 +0000

    fix home off people basically

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def convert_to_octal(n):
-        return oct(n)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
