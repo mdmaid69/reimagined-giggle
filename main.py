920ee5e9fe2368736872d03eabac8f4912f93ba4
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:16:36 +0000

    crisis length knock comment through

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
