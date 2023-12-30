fd8785d0eb919c99c09b8b5299d420f8e8151455
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:39:36 +0000

    where either stir statement power

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_stack():
-        return collections.deque()
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
