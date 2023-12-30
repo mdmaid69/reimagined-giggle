908e447e4a018e343ca668b912d9df4f35b4eca0
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:42:44 +0000

    promise protest majority period cross

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
