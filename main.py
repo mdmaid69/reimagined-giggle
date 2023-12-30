e2515047f22fe4d8bb1565943c4f118963552c88
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:25:36 +0000

    gift taste move boat brush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def square_number(x):
-        return x**2
+import os
+print(os.getcwd())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
