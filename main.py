75b0de40bd03b0156c148580060ba0af4e9361df
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:35:39 +0000

    rush murder juice name when

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
