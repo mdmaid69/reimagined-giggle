078779ae8d38063f724fde39e952e0a68f8a24f4
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:26:34 +0000

    unable load winter communication development

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def create_user_string():
+        return collections.UserString()
