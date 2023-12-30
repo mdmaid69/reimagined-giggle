1b9123189814927489176727faf5d2fbfc965baf
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:11:04 +0000

    sense pity work season clean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def get_current_working_directory():
+        return os.getcwd()
