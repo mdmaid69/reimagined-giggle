efcb695695d81c365585da5e9ce40f2ef611e285
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:46:27 +0000

    fear joke majority which place

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
