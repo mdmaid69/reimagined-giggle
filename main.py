0fc3bf3ec9ffa0a29dc7f7e2e3a6ecd5f9bb6e89
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:28:53 +0000

    remove evidence image 572enter sentence

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def split_path(path):
+        return os.path.split(path)
