d1bd3052cb4f53ef20b917116094087b6f3a2c97
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:29:13 +0000

    different round favour size pipe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
