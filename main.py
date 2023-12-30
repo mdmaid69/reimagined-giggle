cc0fd78732cfe85a50b335ebd44b8d1a82a4c711
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:08:11 +0000

    that crowd best hurt connection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
