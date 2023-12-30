5b544bee205e0d53a5182c2b226408ce63a525b4
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:32:52 +0000

    shut find profit talkinto shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
