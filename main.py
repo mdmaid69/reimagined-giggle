8ce6666ba08455f3355f99dd2fcc0bc459e52eff
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:04:05 +0000

    we past punishment study shrink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
