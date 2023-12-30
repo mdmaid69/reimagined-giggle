7db542be62927278367122e34423d18619044017
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:57:55 +0000

    partner practice contain respect with

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-text = "Hello, world!"
-print("Characters:", len(text))
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
