499bd7b8462ce3bd699be50b7f82557748e345ac
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:33:59 +0000

    bet beginning complaint imagine square

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_power(work, time):
+        return work / time
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
