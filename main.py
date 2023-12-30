b5dd777879a6526439a54a2b8b88ebb62e02470f
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:44:12 +0000

    how disappointed activity secret large

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_cosine(x):
+        return math.cos(x)
