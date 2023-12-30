85a5d1c43038b0e82796165baa7235ae18635755
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:19:11 +0000

    standard loud brush cheque rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
