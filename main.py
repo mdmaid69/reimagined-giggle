d9fc6b652827d7223a16f387069b6bd299fe25a0
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:22:43 +0000

    tap human lesson turnon issue

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
