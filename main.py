9760a8c21c09a12ed431887d0b7a513ad7875c66
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:02:42 +0000

    reliable taste whether Mm burst

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def square_number(x):
-        return x**2
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import collections
+def create_queue():
+        return collections.deque()
