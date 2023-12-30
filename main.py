f30f9a8aef0bae531426fdcf514e108798deb315
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:47:36 +0000

    Ee naturally contrast match ready

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
