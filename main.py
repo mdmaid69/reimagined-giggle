d154fc49e71d5830a6e43e157adc2f6f87f29010
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:43:32 +0000

    weigh right sorry lean closed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
