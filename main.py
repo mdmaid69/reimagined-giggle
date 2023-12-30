cb08907ea9bf49dd1ceb99260345573ef80fbc36
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:47:10 +0000

    put grand weigh that none

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import platform
-def get_os_info():
-        return platform.uname()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
