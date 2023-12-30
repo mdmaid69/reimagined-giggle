b4696349a88251fb8a7418968b149528ff42fc4f
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:04:49 +0000

    speech none through picture although

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
