b0f6ac35d830d824a53aa5c5b1f073b834b3db76
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:01:27 +0000

    similar balance interesting point transparent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_array_as_str(array):
+        return str(array)
