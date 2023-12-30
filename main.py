ef93649c8a2658c543d651799b4e06098bf780e2
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:48:11 +0000

    comein phone crowd noone sting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
