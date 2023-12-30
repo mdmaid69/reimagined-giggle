3a63926de2877d760ad4ae68c0a91bf00093f83c
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:33:23 +0000

    silent away temper runover waste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
