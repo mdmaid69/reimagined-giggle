5e08035dfc873915db44bf056095fe6ff8fc37f9
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:04:01 +0000

    watch shake habit belong charge

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def is_odd(n):
+        return n % 2 != 0
+import os
+print(os.getcwd())
