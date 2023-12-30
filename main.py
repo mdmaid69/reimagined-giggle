81ac6594ef17d6a9be0f6a4e169376902bec202e
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:34:14 +0000

    statement lower sure true spin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
