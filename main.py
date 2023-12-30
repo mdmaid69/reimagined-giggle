d4e6b60fdec9b0528c1cc66cd696a6ebff1cc8e5
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:36:41 +0000

    means part stop violent reach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
