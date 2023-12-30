86cb2a585bd99041ef8038e8fa9fce4e93af8c4e
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:39:42 +0000

    agree target rarely legal smell

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
+def sort_numbers(numbers):
+        return sorted(numbers)
 n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
