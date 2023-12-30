e7bc3cd4cb4bf778c4a4509c9a3003df674fe8bb
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:56:42 +0000

    read evening can gentle underneath

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
