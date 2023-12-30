98ac0f9e5db5698789f2b054a916eefde2d27eed
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:35:08 +0000

    store Cc turnback show opinion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
