6466af4dfe7d3213f3b9d860492a27677d10791f
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:59:29 +0000

    warning all disagree door preparation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
