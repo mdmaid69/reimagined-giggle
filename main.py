577ae81ebc8284c52b85d24f02644abe28372664
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:55:48 +0000

    leg ask plain mess effect

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
