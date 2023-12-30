93d93f3ee13e92bc4a76b1d826e13125698a7227
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:37:14 +0000

    fall run getoff chin discuss

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
