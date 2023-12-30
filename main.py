a04f3cd359761aa5bf719ebd4183e20fc098350e
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:07:00 +0000

    mistake basic persuade bury wing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
