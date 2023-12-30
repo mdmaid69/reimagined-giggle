f17f0761213e3268105659a47b6ff5ec2185fe1e
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:42:10 +0000

    discover police estimate use declare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import platform
+def get_os_info():
+        return platform.uname()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
