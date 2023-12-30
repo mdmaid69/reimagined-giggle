9197e9862f2906435dff7dfe42dffec55dae3c63
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:09:15 +0000

    sit do pronounce nature equal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_as_list(array):
+        return list(array)
