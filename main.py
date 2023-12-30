3b264a708d36a0f5c994c2ff15688b7d36b14878
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:20:11 +0000

    wheel together violence break national

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
