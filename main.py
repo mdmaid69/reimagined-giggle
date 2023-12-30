3cc3b5a0adde787c7cfb0db5ffd0df56656bef74
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:23:01 +0000

    handle mind mark drop foot

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
