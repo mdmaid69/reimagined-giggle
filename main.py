c5dd8573e12c92d62c4fd8d2ded1149b7c4ae39b
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:44:09 +0000

    left funny wrap stranger print

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-print(sum(range(10)))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
