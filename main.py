cf84cd0668d9875a167c98b3bd856f4ffe603204
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:17:19 +0000

    pause receive trap 568energy crazy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import math
-print(math.pi)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
