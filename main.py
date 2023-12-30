5eca6b89c22dd8a93b34b1f78e4f87f5f33b55af
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:18:33 +0000

    afford relationship mistake fair avoid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_typecode(array):
+        return array.typecode
