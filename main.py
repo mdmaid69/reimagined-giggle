59b01155cd6d473fa5cd002537c288318f46f784
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:24:52 +0000

    ticket worse keepaway however calm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
