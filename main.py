9281132cb52bb2cb7f03fc35e24e819591566880
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:37:22 +0000

    possession famous flow anxious set

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def find_unique_words(sentence):
+        return set(sentence.split())
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
