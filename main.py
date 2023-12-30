12ea242637ce8ae6e48654217d1a96436547ca18
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:40:21 +0000

    beautiful drop month repeat thorough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import numpy as np
-print(np.array([1, 2, 3]))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
