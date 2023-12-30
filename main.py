f518f6e27e676a829652535f68aa01c40c8d7264
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:12:11 +0000

    formal ahead mixture Dd sense

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def add_numbers(x, y):
-        return x + y
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
