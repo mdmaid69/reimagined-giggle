7913a57641fa679f0ec69d123c0e3271a6934709
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:48:24 +0000

    catch moon maximum exercise love

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
