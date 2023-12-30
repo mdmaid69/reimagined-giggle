d1ae00e2663a7d2b5447e6a6b8b34fa5a6ff48cf
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:06:51 +0000

    pencil discuss classroom correct unable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
