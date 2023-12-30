6a8a6ef4ac938c2431dbcf71b927e550f821caf8
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:56:51 +0000

    backwards 566end look bury tidy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
