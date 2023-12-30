faf048f0704cc2176e43699a6b898d7a13b8c5f8
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:49:23 +0000

    tall praise exactly human shoot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import json
+print(json.dumps({"name": "John", "age": 30}))
