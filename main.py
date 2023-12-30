1a8e3beca75a3287255febb31de97784da154fd9
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:05:32 +0000

    herself pressure load deep strict

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
