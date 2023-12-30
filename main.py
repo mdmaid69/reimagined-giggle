a9704674e0dd795bc52df00e6792ceea9cb6f8f9
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:31:03 +0000

    because answer workupto control right

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
