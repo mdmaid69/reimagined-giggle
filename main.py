c7ebc23b63ec2f7cb424d99d636effdecf6690d5
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:32:06 +0000

    yourself for similar poison top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
