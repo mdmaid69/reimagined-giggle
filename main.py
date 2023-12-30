2a4129abf795e2c0ef8c7af6dda712c780e42143
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:45:40 +0000

    ours precise true guard estimate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_power(work, time):
+        return work / time
