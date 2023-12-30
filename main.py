3407c7fe8c5ecfb5d4539e1b838a39ecb4fb850b
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:19:53 +0000

    take people slip tax alone

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import collections
-def create_counter():
-        return collections.Counter()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def convert_to_octal(n):
+        return oct(n)
