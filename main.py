b4afe458d9f929bbd3ff4423d06562242e11ee39
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:41:02 +0000

    confusing cross proper convenient you

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
