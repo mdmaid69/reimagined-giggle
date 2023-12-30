7a91ef942377be15256a6c2cf18b304297b6b740
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:33:46 +0000

    comedown try cook contrast life

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import time
-print(time.time())
+def get_time_since_epoch():
+        return time.time()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
