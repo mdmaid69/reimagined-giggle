4ac15d85f0c99c7a87352026b6bf2396b17a6490
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:50:27 +0000

    organize basically impressive board outside

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
