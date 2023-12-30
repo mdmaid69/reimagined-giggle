cdad0964c96aa7dbfd228bf85c36e00193048934
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:14:35 +0000

    secret protect settle scrape traffic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_current_directory():
+        return os.getcwd()
+import array
+def get_array_as_float(array):
+        return float(array[0])
