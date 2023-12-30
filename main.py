36997249deda0c93d96b8ca9cb415c92e4ff1327
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:07:25 +0000

    regular quiet game ground business

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-print(sum(range(10)))
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
