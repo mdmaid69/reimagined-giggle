6e9324378a0d7a69bd57a5196c602b94c95f8818
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:16:51 +0000

    calmly office score smell plan

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
