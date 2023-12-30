5a3734c5d940aceb3fc112eee5f2d2359e623595
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:15:24 +0000

    neck different temperature 179bite press

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import numpy as np
+print(np.array([1, 2, 3]))
