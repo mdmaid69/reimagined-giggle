05ce4267243f094a68de421fe55642bd3d1a043f
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:27:06 +0000

    slide thought damage gate to

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
 import array
-def get_array_length(array):
-        return len(array)
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
