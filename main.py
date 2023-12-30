b39b3d242105960720413f8de49f36050614cb9e
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:32:08 +0000

    clean lie hot secret deliberate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def cube_number(x):
-        return x**3
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+for i in range(10): print(i)
