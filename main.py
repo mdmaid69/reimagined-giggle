28d9e04a20d07bcb347fb85b0da8d38519cfcf16
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:29:24 +0000

    injury effect politics laugh keepfrom

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
