c247e9e07708a91dd3e27ea5ee3943ba102c4a32
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:34:29 +0000

    rubbish nervous advantage level 179bite

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import sys
-def print_python_version():
-        print(sys.version)
+import random
+print(random.randint(0, 100))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
