a68b25ecc8c60aaf9e800efa1a90a00198e85955
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:40:36 +0000

    rarely brush practical good deliberate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
