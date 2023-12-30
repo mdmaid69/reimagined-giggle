c878146bac997296fad16906438e4fa6544e22fb
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:43:03 +0000

    lean Dd slightly intend intend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_set(array):
+        return set(array)
+print("Hello, world!")
