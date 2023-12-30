62adff13b92922ee2e8c81299e5c1036f903244f
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:06:53 +0000

    provide runover opportunity hers sign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
