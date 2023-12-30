4e9ddc6247c8c12779ea389ff5465b9b4dc21d20
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:46:39 +0000

    shop regional each rest entrance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def convert_to_hex(n):
+        return hex(n)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
