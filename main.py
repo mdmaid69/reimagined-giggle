cc1116915d3cbdd89ffe3fd7b7e045a69f31b3f7
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:27:55 +0000

    male pattern increase under port

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  def convert_to_octal(n):
-        return oct(n)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
