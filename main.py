5ec57204b69bcd763d98494888105708b7d82a86
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:33:27 +0000

    cross defend extra occasional black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
