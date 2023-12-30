7be96cbed930029848b239e43bdbbefe4249017f
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:16:45 +0000

    town notice left used well

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def divide_numbers(x, y):
+        return x / y
