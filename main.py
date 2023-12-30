a8b7b19db8e205faa3e4a45c5a75b3692a85dae9
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:00:09 +0000

    window call dress service attention

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
