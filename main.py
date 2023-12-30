8ac6d4b47f1a1e9e1edb380acf1b889ab248e670
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:43:15 +0000

    hide stand protest anyone slow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
