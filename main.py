9e9795f94fef06b65bdcecdaf78ef85cf836f78f
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:31:27 +0000

    welcome distance will last plan

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
