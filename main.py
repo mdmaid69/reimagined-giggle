1da4f4c57d77da9bbd8215e57cf81bff4e34b405
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:42:47 +0000

    injury finish exit thankyou dangerous

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_area_rectangle(l, w):
+        return l * w
