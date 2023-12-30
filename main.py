0a2acfca8c8f64dc0a24edd6c85e1d2d8b76d3fd
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:57:05 +0000

    rise fill squeeze complete off

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
