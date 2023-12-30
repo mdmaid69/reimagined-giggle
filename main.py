55b3130b56053d771e4c8a90b2ea293b78b12f85
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:08:42 +0000

    trick limit friend store terrible

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import collections
+def create_stack():
+        return collections.deque()
