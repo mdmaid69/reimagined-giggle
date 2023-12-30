77720cab660f8f59ff56aed2c2453b1f4968afcc
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:07:36 +0000

    pencil but workout shop claim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def remove_duplicates(lst):
+        return list(set(lst))
