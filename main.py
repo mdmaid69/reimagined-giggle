cc36d5f9961d2d89570d84966ee9621d499768ae
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:12:43 +0000

    me holdup throw tip juice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
