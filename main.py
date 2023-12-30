c1404e205647c92668ebe51823c0344fd4915efd
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:25:35 +0000

    theirs hollow slightly skin trick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
