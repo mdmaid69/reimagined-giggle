c1331168348e625f90412b21a4f829d767be7726
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:34:29 +0000

    sensible his surprise afraid short

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
 import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
