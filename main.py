2ce09b5fd8d5c22ab36483f447cdf0ca84d11e1a
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:51:07 +0000

    present corner punishment plain across

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
