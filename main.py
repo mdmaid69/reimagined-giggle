e8421e2fa06f246e7b7f0e45f6c6926e8fde207a
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:40:44 +0000

    field one seriously wire unfair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
