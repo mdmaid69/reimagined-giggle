39b387e86ddbd440b26889fc4c9d48445f181c44
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:58:33 +0000

    believe company swing hurt characteristic

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
