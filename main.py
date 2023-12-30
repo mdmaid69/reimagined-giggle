3a0dd6b389f14899ef8d58f72a3c6e6725e227c4
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:59:31 +0000

    find beautiful relation where fine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def find_min(numbers):
+        return min(numbers)
