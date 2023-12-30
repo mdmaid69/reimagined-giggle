8cae53811fed73c9f89dfc077f958878cd07b962
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:42:02 +0000

    search moral trip stay sensible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_area(radius):
+        return 3.14 * radius * radius
