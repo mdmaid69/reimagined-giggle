76a043bcf7c220aa84a5554a25cd8f002a5b3bfd
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:01:46 +0000

    curious they attitude talk able

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def is_even(n):
-        return n % 2 == 0
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
