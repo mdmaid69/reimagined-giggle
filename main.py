7c610d927bd58729dc5e38547bb3ee8d92a294a6
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:47:17 +0000

    term band by brave disappointed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
