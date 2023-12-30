62637027fef169815a90046247508d7e955bcd38
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:16:47 +0000

    next limit objection crazy scream

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
