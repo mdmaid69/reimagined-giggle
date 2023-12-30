8e81a7409309b39c7b6b25a46e0cedebfaf2fec7
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:51:23 +0000

    uniform roof minor runinto worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
 list1 = [1, 2, 3]
 list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+print("Union:", set(list1) | set(list2))
+import array
+def get_array_as_str(array):
+        return str(array)
