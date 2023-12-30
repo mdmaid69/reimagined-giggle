81465b5a0bd01d73e292244138ce5fa9d7ef9f70
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:48:26 +0000

    everyone offence print I comeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
