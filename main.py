b649270c4aa1b65f84f1a94ad69b50ea600c2c76
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:25:37 +0000

    half sad national swim rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
