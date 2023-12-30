ad8fd5c2297a117fc5b4f5c181c136a3428a5750
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:12:55 +0000

    short other whenever inform speak

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def calculate_area_rectangle(l, w):
+        return l * w
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
