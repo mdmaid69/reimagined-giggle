26a74658f8701987c45c64362539010c994c1f19
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:35:56 +0000

    order description reach wide issue

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
