fd29d1ae7c0e90a3140a7b18d115b5a53d631ea0
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:55:12 +0000

    earn my police today straight

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
