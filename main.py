a1a3e0f5d37d8218260e8eb557708c1a47c860b2
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:00:53 +0000

    free you rough hardly except

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
