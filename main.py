93802fcb18336ab5cca254af7c03ec74e92f6a57
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:40:49 +0000

    land by fortunate just centre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import random
+def roll_die():
+        return random.randint(1, 6)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
