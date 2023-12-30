1dcaf01bd9a36069a078307172c54121568ff540
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:40:55 +0000

    flat bag public company introduce

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-print("Hello, world!")
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sys
+print(sys.version)
