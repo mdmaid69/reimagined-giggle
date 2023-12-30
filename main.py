6bad79bfda0ee362e9f8969dcb49479e1098ed39
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:27:15 +0000

    meaning market dear marry wrong

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
