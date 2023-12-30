c6a102713bb3fa9f67287c88f0677e0357986246
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:10:37 +0000

    most luck left spread reduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
