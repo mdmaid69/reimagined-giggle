130dadd164731e2adf5d352dbde21aa8c83ac462
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:40:02 +0000

    trial hate fact chief yesterday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
