7a2b567b012d167b7e4e9e2745a79398400983be
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:56:37 +0000

    fun mysterious Tt punish exist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
