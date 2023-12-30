895014e0ae3ad3ff65ead36c6a01dd278e038b79
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:11:47 +0000

    live hair lock risk hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
