0bbbbbe592bead7a29a198a17bf3d9b981432030
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:42:08 +0000

    like geton surprise knock leg

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def calculate_pressure(force, area):
-        return force / area
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
