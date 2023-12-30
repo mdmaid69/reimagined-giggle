e36c20d7c11497498260b153315309bb47975e50
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:27:01 +0000

    target detail convinced fair drop

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
