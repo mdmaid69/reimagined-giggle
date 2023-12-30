3e399801261e1ace866efd7307a6db42d4aab20d
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:08:52 +0000

    content share adjective solve tongue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
