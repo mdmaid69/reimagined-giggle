c5a07e675da923969fed946079c1423e0f67ca0a
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:14:33 +0000

    nervous top familiar major instant

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import numpy as np
+print(np.array([1, 2, 3]))
