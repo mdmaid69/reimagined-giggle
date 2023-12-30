7af5df2cfea3f708c4b1731de334c9e776d5d224
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:58:44 +0000

    environment thinkof plan complain comfortable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_length(array):
+        return len(array)
