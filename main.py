d1ff65e6a81afe22ea1cfe2b9e0a790237a4ea2a
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:51:31 +0000

    pride snow mistake bang boring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+from collections import Counter
+print(Counter("hello world"))
