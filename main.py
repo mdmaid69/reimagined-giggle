6ddceffa4c3f3c10739373613dc696f398154182
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:07:51 +0000

    sink fast yet majority moon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
