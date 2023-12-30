dcf10066fcbe9193cb5393f73fc4da891c8b9d4b
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:09:09 +0000

    flight towards cheat value thankyou

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
