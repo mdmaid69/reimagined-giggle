e589ed8e0eb71a48f779dbdfc0a97f16438dd619
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:39:55 +0000

    goback(to) Nn clear every contrast

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import re
+print(re.match("h.*o", "hello world"))
