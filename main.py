25acf27da1f40385b392627d4e8a6ea96684f3e1
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:20:58 +0000

    comeout judge pay this separate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import re
+print(re.match("h.*o", "hello world"))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
