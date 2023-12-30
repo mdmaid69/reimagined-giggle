c1969de0a3ebca356aaabd716c2b98367246d77d
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:30:27 +0000

    effort pile frequent makeup both

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
