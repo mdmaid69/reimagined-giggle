7f59523d0c91af48b07256ef06c1e6b8f4a7a511
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:31:53 +0000

    floor act custom mark summer

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
