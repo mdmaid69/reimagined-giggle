ac09a317ad094c7b2eba5720db739f6885368a66
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:29:47 +0000

    admit afternoon suddenly paper talkinto

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
