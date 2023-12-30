ccf9ea798cf5f02d77ee936afd73a5405e8e2aa3
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:37:38 +0000

    rescue into original guess price

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+print([x**2 for x in range(10)])
+def calculate_density(mass, volume):
+        return mass / volume
