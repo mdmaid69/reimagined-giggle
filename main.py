32c508d8cbd02132df07eadbead97dbd44f5c434
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:03:14 +0000

    free keepaway ear without squeeze

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def subtract_numbers(x, y):
-        return x - y
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_area_rectangle(l, w):
+        return l * w
