d862b997601bbf54784fcde74873c34f516f3257
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:50:35 +0000

    protest decision combination tough private

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def clear_array(array):
+        array *= 0
