5fa7757e7eee5f58cc2e55be9f6b96e0c07d881a
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:08:26 +0000

    fairly hard you high threaten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_current_directory():
+        return os.getcwd()
