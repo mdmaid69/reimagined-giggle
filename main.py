0f3a82ab9f1a0e259d13b898249445ff362a2cb8
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:52:43 +0000

    trade unhappy involve well aware

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
