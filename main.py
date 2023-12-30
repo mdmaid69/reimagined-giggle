3710c53993ee154f373ab7bc35def873e3cdeb2a
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:07:33 +0000

    turndown awake trust fence whom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
