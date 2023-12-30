3b296fef6cec83f15cffd0ad713b8f7ebdcbbf48
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:40:42 +0000

    hide ocean open immediate before

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def count_characters(sentence):
-        return len(sentence)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
