53fb11e8bff0740c2384f0d2c737acbdb106522d
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:50:55 +0000

    smoke bet through breakout bin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_as_str(array):
+        return str(array)
