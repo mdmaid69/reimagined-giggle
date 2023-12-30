9099a8324ec3373a3976c1b663bd58ef972dc377
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:17:22 +0000

    importance course tragedy world themselves

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_speed(distance, time):
+        return distance / time
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_set(array):
+        return set(array)
