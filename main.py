a9ee616fc88f012d487367b52f608c892a38b54e
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:34:04 +0000

    favourite thing scrape different impossible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
