678308b3e20fc96100dd564ca831301830b71f86
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:24:16 +0000

    escape show juice disturb him

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
