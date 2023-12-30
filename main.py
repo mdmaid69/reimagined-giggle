fe9b0cdeb21da075ae19d752bc3b65cfb2b9495c
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:26:57 +0000

    book trade water circle needle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
