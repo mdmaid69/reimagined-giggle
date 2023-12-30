14ea778ddb9695f2b9ab6c158bc017f8df6b00d1
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:56:17 +0000

    change oil Yy opponent blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
