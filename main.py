d1b17b51db0a495af0364fa64d22ce64be6079c5
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:04:00 +0000

    secret meeting plenty mass centre

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import array
+def get_array_as_float(array):
+        return float(array[0])
