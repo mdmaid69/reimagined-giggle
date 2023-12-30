6619d418661d751c430d763dfaedb4a0d3c2d3f9
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:27:23 +0000

    careful scream whenever too aware

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
