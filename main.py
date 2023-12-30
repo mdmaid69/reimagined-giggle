cf837464a6af9a7e3b777cf408bc105096e09470
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:00:32 +0000

    season emotion more fashion loud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
