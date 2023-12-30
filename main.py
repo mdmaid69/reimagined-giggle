eda167f678868efe45eef1d9242ceb255b92c182
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:46:59 +0000

    behave difficult word responsibility programme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
