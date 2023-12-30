ed005043664f36c0e53181086c59e23bc941cc31
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:23:50 +0000

    indeed entrance finally pause design

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def cube_number(x):
-        return x**3
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_density(mass, volume):
+        return mass / volume
