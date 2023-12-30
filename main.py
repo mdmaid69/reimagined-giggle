aad63c91319c57cf577f7a61388d19aa57238c88
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:54:32 +0000

    shop might public convincing string

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import sys
-  def get_python_version():
-        return sys.version
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
