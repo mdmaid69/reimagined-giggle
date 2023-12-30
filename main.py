470108677c19fd89014b4de1992575ef403e2e5e
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:32:04 +0000

    fill board communicate please education

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
