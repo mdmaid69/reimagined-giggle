70c6496aa7f61e627883e7deaf68285402af1abd
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:25:48 +0000

    dry place club respect mother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
