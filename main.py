df8f01ad19a95683e62e78697a62c4b5dafba65a
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:33:22 +0000

    approve formal thanks do middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
