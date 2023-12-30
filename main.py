d75f4d23edff30a91431ec6031e86d25467c2037
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:13:27 +0000

    excited surface relief study refer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+from collections import Counter
+print(Counter("hello world"))
