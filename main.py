6527fc04ae0df22b724429943943ad69bd31f762
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:42:43 +0000

    possible both part bread paper

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+print("Hello, world!")
