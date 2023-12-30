3844602e025ae51930c7be19d4a9aa109f0690bf
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:16:42 +0000

    question entrance present aware tongue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
