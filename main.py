b372143545745dfd6f52845923bfc4c64f39a428
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:47:06 +0000

    murder upstairs movement kind past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
