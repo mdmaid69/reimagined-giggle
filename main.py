a0eb8d7233f8679b40c35dd52c12bee6812fe33a
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:49:47 +0000

    swallow grade bottom whisper wild

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def multiply_numbers(x, y):
-        return x * y
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def sort_list(lst):
+        return sorted(lst)
