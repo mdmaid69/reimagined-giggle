8661b157ff53515f8e6b09b49e8f79aee53488b5
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:16:40 +0000

    match less content terrible serious

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
