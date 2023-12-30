c151c50f185280924c0dd733dc5c8414cbe2e89c
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:19:14 +0000

    attempt equal least completely search

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import collections
+def create_priority_queue():
+        return collections.deque()
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
