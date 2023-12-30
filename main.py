dc8490731034264cef54fd251c08737af31839a6
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:01:10 +0000

    Itself healthy last scale 565end

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import random
-print(random.randint(0, 100))
+def calculate_density(mass, volume):
+        return mass / volume
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
