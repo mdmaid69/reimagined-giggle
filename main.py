ec67c75c11ca523e2baebc3aa544bc0fe3df0499
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:35:13 +0000

    reject flavour combine block free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import collections
-def create_counter():
-        return collections.Counter()
-import platform
-def get_python_version():
-        return platform.python_version()
+def create_user_dict():
+        return collections.UserDict()
