884a0565a04488c423eebfd98cde7f34ccb40919
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:58:25 +0000

    they strong black brush downstairs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+import sys
+print(sys.version)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
