d4f188b0b011721895cfaac6aab326ff75869490
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:36:30 +0000

    clothes hope size furniture report

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def print_python_version():
+        return sys.version
