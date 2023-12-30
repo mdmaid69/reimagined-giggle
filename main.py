53a71cb11b2741953902a8525ae7766cac521a89
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:25:07 +0000

    excited make nice sick close

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
