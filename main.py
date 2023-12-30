1941f1459d2522eb40aeb74fc85f2f49c69a6379
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:59:29 +0000

    already corner left form sure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def subtract_numbers(x, y):
+        return x - y
