0951c2f2b5fe15f80a6938c7794af4b1aaad96b0
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:28:30 +0000

    whatever ground keepaway judge thankyou

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
