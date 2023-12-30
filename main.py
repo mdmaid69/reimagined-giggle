18883c521e367b4add2b9dbf08cf29f166f4cefb
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:42:05 +0000

    fast during pipe Ee walk

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def count_characters(sentence):
+        return len(sentence)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
