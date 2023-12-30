cbe0b1f683a6e4be4d2f502ddf696b2cbe0b6142
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:48:07 +0000

    wire OKorokay time wrap way

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
