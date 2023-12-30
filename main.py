5a7d31c04328c4c17e3d9c63fc00a25e8fd0d2a3
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:03:53 +0000

    shopping land second come either

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
+import sys
+print(sys.version)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
