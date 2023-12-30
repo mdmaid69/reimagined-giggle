2a4c81808c5eb215b0544ff0d2b1f632e5a936e6
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:15:23 +0000

    careless excellent steal reserve book

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
