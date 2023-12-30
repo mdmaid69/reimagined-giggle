9e13f03d65a9a089f681440288c24c3136dc58dc
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:43:59 +0000

    advise sport dish next somebody

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
