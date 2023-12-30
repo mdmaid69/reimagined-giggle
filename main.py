19a6c104236dd16866dcad1a098890ffaf7c0041
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:09:41 +0000

    bottom flat introduction that wish

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import time
-print(time.time())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
