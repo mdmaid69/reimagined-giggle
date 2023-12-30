f32f6210cef9970b5fb1da27c1c29f0572cf1d08
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:20:44 +0000

    present back think education physical

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+print("Sum:", sum(numbers))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
