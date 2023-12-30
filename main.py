ef435710577e54150a102d4dc7e6ff4dd803c7d2
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:22:37 +0000

    before this wipe slide whatever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
