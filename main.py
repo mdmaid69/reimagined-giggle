b4ee74f9a43542a2c3ac00b114a65aa87877654d
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:59:22 +0000

    tax why beneath easy deep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
