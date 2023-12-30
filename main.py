5c3445d858c349c18c5f69173d6231a1211f9c37
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:20:59 +0000

    detail find appeal ashamed deep

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+for i in range(10): print(i)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
