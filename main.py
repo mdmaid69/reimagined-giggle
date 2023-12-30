07fb8f44648e5938c108c1103126efab8c187fd1
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:34:46 +0000

    getup people wild sure classroom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_union(list1, list2):
+        return set(list1) | set(list2)
