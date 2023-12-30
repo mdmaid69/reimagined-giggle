18dbc5861ed2c09793885f51d977382cffaebc78
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:40:15 +0000

    could different where but book

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
