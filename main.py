80e3777996fcfa60e27e6ef4443caf65169ac808
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:56:46 +0000

    goup wave praise education later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-print(os.getcwd())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
