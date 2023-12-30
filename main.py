36e6b4f85756bd7035dccfc7ae58072168ed3560
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:29:18 +0000

    experiment runover memory content afternoon

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
