9d0058d82c3d3d120df5344cbe1242426511ed2d
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:25:10 +0000

    model worth commit father want

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def reverse_list(lst):
+        return lst[::-1]
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
