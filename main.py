09173602360d49aa20ee31b8257954bca48ee4e6
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:53:46 +0000

    future surprising succeed account hate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_float(array):
+        return float(array[0])
