a8f8c301b432fec06e887ad284fbd0a104a91fa4
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:23:53 +0000

    type comfort special authority equipment

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
