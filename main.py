6d913f85cf77df765df2aadb66729701d43a2c29
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:58:58 +0000

    give run scream scare certainly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def extend_array(array, iterable):
+        array.extend(iterable)
