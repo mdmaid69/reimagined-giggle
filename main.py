b012daa2118f253001cc2b082a98527d200f3188
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:10:54 +0000

    front book pot lend education

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
