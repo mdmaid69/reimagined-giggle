a442bd47fe1eca8a5f37d48b4411ede5a274b4a4
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:40:11 +0000

    compete problem boat stop Ss

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def square_number(x):
+        return x**2
