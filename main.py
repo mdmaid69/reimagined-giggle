4fbe71626814219fa8fe78e6ad43a0cd5d70c895
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:31:17 +0000

    spread of dish quarter move

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
