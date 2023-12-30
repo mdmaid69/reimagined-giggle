3647ba91c4bd85c6e9d3f722f50eee150bbc5843
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:22:43 +0000

    perhaps base blue concerning train

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
