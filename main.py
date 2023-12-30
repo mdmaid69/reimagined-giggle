37e62ff38854a35cf16b5533b32da4e7f36659fa
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:49:49 +0000

    lost happy far light approval

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
