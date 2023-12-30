cfcdfdc4fe5a6ceb068444ae19b88471b11f2502
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:43:44 +0000

    generous shout rule curious sky

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
