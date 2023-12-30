1c03ab836440847f215c5bdc58ebff7e75084cda
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:48:14 +0000

    service influence fasten radio whatever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
