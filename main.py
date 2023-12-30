d91986a6313bb0066d5d22dfde23cf780f572026
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:37:52 +0000

    unhappy abroad give nice point

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
