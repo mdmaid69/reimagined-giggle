cca45da0975239a234a817dbd11845e0ff326c9d
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:23:49 +0000

    tragedy have develop spill flat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_as_set(array):
-        return set(array)
+def clear_array(array):
+        array *= 0
