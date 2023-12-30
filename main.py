e25cb78df879bc46f3977d2c13aa57fdee301f71
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:44:06 +0000

    danger motion shoot light soak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
