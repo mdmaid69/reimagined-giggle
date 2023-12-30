1e2c0a22e0cb45e951331b935971f6dc6629cc8a
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:30:19 +0000

    make that team comein actually

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def cube_number(x):
-        return x**3
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def calculate_speed(distance, time):
+        return distance / time
