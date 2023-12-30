64cfd2040534c2700a4e531e76da9e702c9c5615
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:19:15 +0000

    there round keepoff flight rent

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  def convert_to_hex(n):
+        return hex(n)
