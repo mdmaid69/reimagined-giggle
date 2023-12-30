8656813bf74697fc19075fde837a847783d1f416
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:22:20 +0000

    memory detail mess value true

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_acceleration(speed, time):
+        return speed / time
