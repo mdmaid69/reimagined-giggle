bdf2bb69ec758116743d51f13c98ad48d85f708e
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:53:27 +0000

    bored direct oil criticism down

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+def calculate_density(mass, volume):
+        return mass / volume
