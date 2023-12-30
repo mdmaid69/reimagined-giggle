cbd45f59016ae4e6270217230bb8bcfcfb653bd4
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:44:03 +0000

    hold pretty use sing only

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_density(mass, volume):
-        return mass / volume
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
