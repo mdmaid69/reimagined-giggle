f430aedf48019e42312a6a39e2863809ac6e3df9
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:18:55 +0000

    immediately container best thin zero

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+print(math.pi)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
