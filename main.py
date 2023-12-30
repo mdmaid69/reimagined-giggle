756287729e0efca220a28452c24cc228fded0733
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:48:40 +0000

    backwards art funny centimetre subject

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
