ad3f9ad2ee3a02e89b92ac3b38ca36d2b7a403a6
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:07:59 +0000

    abroad patience design bar check

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
