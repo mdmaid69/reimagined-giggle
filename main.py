1bebd628ba12e492ac21f071b571b7625eb6540f
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:35:55 +0000

    ought arrival come properly quit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
