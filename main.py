578bc22dff60c9d32ef1472cf57debd4555188d5
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:28:23 +0000

    voice create blind artificial swing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
