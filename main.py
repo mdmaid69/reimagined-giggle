bf3fe424f12873abd4aa32d598ceb4d569348a3d
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:05:21 +0000

    character art raw watch takeaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import array
+def remove_from_array(array, item):
+        array.remove(item)
