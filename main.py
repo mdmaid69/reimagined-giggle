b1fa9408640b360af5c03028457fb5e448af701e
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:39:18 +0000

    fast about block mouth scrape

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def add_numbers(x, y):
-        return x + y
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
