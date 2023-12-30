191e3b070dc1e74a2f32ced00eb413ef21d22e40
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:49:44 +0000

    there Mm minute used about

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
