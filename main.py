b8eb909b2e83b18962af6c6ffdbe1f6be92697bc
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:05:03 +0000

    nonsense ashamed punish condition body

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def remove_duplicates(lst):
+  def remove_duplicates(lst):
         return list(set(lst))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
