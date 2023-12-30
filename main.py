5818bcd8ca369bf8fe5e68373e8088ed5f1e75df
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:10:18 +0000

    opportunity top scheme tape world

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_as_list(array):
+        return list(array)
