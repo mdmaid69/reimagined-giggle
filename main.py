e2e9044593424a6fc25083d0dc66117886977f2e
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:07:02 +0000

    reach condition holdup noisy pollution

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
