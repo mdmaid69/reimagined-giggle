8fd388c58ea30850f00a22f78347ef243ce261e3
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:55:14 +0000

    sensitive part pour hers story

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import tensorflow as tf
+print(tf.__version__)
