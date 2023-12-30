468d4dd06a7477ddb24d9b0c555fdeffa79f4caa
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:36:29 +0000

    gas clue comfort once file

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
