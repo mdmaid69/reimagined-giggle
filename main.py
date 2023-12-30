2e1ea32e7f607456e2d121e8bdc22f064f9e1e13
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:28:47 +0000

    least she reach stupid mother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_string_from_array(array):
+        return array.tobytes()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
