fe2657ea747c39dc02a9b5a4a386818c0c9070a1
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:30:38 +0000

    funny forever uniform just sting

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
