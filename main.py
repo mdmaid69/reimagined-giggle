abdac1cc2c5f879c0b42e1c6ec6a8c519645ddb1
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:34:34 +0000

    fast away software minute tooth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
