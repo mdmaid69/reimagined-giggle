b2a4140a662f42fe0ae7721cd1b5727d842bbc88
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:42:47 +0000

    unkind which bang try top

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_acceleration(speed, time):
-        return speed / time
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  def reverse_list(lst):
+        return lst[::-1]
