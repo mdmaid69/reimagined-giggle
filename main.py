de41b891b2f6f82d13747e3a51b8e3a0da283def
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:09:14 +0000

    shopping business active matter read

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
