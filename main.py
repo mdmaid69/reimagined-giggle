eceabea10c72ca56d925d89e7034fee884c1e5c8
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:09:48 +0000

    myself valuable bottom the right

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def remove_duplicates(lst):
+        return list(set(lst))
