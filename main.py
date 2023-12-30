20d7de512bcbdc6f1845d7bacaca458bbaad1dcb
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:58:15 +0000

    protection lend unhappy hang heat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_area_rectangle(l, w):
-        return l * w
+def find_max(numbers):
+        return max(numbers)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
