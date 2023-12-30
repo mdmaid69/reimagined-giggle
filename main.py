52b8954c1f512951c0b791bdb70456c1d665d42f
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:19:07 +0000

    sea smoke getaway dick taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
