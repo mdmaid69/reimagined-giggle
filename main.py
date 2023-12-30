b690a2b058bca0d29f2673b534782804c873ed48
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:56:10 +0000

    many but bus confidence bill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
