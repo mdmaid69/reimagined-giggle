ae5f945dd76d18fbee862ac07aef179fb0ec9394
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:47:40 +0000

    same cost star pronounce mine

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_pressure(force, area):
-        return force / area
+def cube_number(x):
+        return x**3
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
