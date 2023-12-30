20cc199e1082a75ebdaba125f89eb4e734434b61
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:37:56 +0000

    makeinto settle set laugh convincing

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def convert_to_binary(n):
+        return bin(n)
