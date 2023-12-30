01ab36bcf7857a2266fff25c8ffc4e28f3976ada
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:22:06 +0000

    contribute operation guard break replace

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import os
+def change_working_directory(path):
+        os.chdir(path)
