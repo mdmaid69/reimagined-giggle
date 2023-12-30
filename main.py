8252ac54a042439a4191f7fc38eda36b6737f09b
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:59:34 +0000

    unusual doubt hole goup steady

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def get_string_from_array(array):
+        return array.tobytes()
