ed739428058e56643d471c83483d68be3e71e168
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:49:54 +0000

    correct save police father respect

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-for i in range(10): print(i)
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
