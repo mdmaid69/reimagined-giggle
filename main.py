96be46467eef0173bf9953e03ea62484230bb665
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:35:08 +0000

    wall cut keepaway react shop

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
