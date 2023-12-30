2423db2d004ae9d1fa2b7fc49fbf39cdf0fa6ff4
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:27:26 +0000

    exam back tend connect fact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
