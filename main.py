96fcf5362612555b4c72b4c8cd258eb0f194dd4a
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:30:36 +0000

    loud union number hard contribute

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import os
+print(os.getcwd())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
