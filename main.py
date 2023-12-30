d57d91f6083cf7573cd8498454ea9602eeb89bfd
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:48:41 +0000

    address clue mess work else

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import platform
+def get_python_version():
+        return platform.python_version()
