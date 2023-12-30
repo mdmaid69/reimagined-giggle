a7068f03c09ea941e8404ff47020bd9d0705cef9
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:20:32 +0000

    twist marry male delight unkind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_bool(array):
+        return bool(array)
