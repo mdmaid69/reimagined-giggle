abbc56a13db7c1a7270bca68bd84c17cdda9f045
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:05:54 +0000

    shop hold lesson if forgive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
 def calculate_acceleration(speed, time):
         return speed / time
-import collections
-def create_user_dict():
-        return collections.UserDict()
