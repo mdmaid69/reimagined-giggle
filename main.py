50b37cb511d35123c8ab74852f904bb01c6c5d7c
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:29:27 +0000

    study alive begin definite start

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
