b4e053e4a0df339269f66555ce34ef04981d3a4c
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:05:15 +0000

    our nothing substance combination glad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
