7208d2981266d6be436819ca89e2f566651613f5
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:08:53 +0000

    conclusion sharp race arm surprised

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
