5d5bfba1e30bc1e32b60962dea127abd5405509e
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:13:32 +0000

    head nor much suckme fat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_priority_queue():
-        return collections.deque()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import json
+print(json.dumps({"name": "John", "age": 30}))
