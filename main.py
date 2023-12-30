f670a6278b318f7fd1ae36a339664a5e1f25ffeb
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:00:59 +0000

    rain formal doubt only hurt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
