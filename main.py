9952d2ef985f5b324f9dc49a5339e772ba074b36
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:47:29 +0000

    open like lonely treat already

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
