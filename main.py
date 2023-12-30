d2a580d58a691262f702b0a6394a15adba1c6572
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:22:53 +0000

    list welcome right sharp minimum

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
