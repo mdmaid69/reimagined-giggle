2f42b0e69e7f0b5049afd8f481ccca91ce622e2d
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:07:12 +0000

    command effect past that well

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
