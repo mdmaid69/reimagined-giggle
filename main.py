b410ea6a470b77d04ac3c27597108dc835365556
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:17:28 +0000

    inform hear near heart comeround

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-print(os.getcwd())
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
