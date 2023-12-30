dedff60be31378d173c2d5043dfd5e9047574bcc
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:04:02 +0000

    otherwise spread shrink score wake

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
