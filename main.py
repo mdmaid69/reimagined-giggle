cdee781f8f700a05595f4b6910443c638115c3b5
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:07:30 +0000

    surprised conversation drawing remark typical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
