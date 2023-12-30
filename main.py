037fd21108735fc56bd57801db7a03b70e2eb829
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:29:06 +0000

    offence ahead run trial upset

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
