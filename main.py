ba75110832c9b6b14dc0b06f5b712516b4f061b8
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:15:16 +0000

    shock neck suit protest various

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
