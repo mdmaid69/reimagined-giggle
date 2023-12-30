f4634677983a9e6f135b657de75cb063542ff976
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:50:50 +0000

    uponon importance stage deliver brief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
