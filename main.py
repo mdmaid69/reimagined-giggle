87ed29e6ef9e762b4cab9982f5399a15502dd699
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:11:47 +0000

    commit wish seriously gift cool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def subtract_numbers(x, y):
+        return x - y
