0c4dabe926f3f18cfbec227620d7c21ec387db0c
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:15:07 +0000

    suggestion tune truth goback(to) place

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+for i in range(5):
+        print(i)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
