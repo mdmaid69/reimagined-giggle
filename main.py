49b32cf0cf5c3b22b979c55c82e72bc1947176ac
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:07:16 +0000

    decision goup with wind discovery

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import sys
-def exit_program():
-        sys.exit()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
