770b8c91440f9ac8b12849d3cdaa930aa04a6f44
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:46:49 +0000

    snow sentence gooff cap bag

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def convert_to_binary(n):
+        return bin(n)
