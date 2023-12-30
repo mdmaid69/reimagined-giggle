623e911340a1e1b543905bfe3390a87edb389e22
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:11:55 +0000

    anywhere unable truth unfortunate feed

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def square_number(x):
+        return x**2
+def calculate_acceleration(speed, time):
+        return speed / time
