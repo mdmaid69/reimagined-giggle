cc648920e5de432e20ada9fd6fea66a673895f33
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:37:22 +0000

    past match press turnover big

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
