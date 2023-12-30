40edd40907b975819c15ded2e02b1d9e90f4f972
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:53:23 +0000

    round bury dear exact callout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def convert_to_binary(n):
-        return bin(n)
+  def calculate_area_rectangle(l, w):
+        return l * w
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
