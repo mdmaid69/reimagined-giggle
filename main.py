577ad6aac78fabc4259957ec14c56ac7f01ab24b
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:11:22 +0000

    where ilovedicks answer satisfactory waste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
