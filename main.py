170b77a9a6eadccd87e31e16cfaba0c886f81f68
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:08:56 +0000

    wake coat speech powerful rise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+print(math.pi)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
