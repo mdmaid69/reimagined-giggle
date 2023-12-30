6b45dcc58e4c634163e348f349d21c5945cd0187
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:02:14 +0000

    head hardly rough uniform live

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+for i in range(10): print(i)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
