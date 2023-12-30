468c16e93eddbc1aba86ca5b30ded301c1452376
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:24:01 +0000

    can dear yesterday last bite

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
