df6432ead9ae36f273efa5578385adb9bf7a7f68
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:15:44 +0000

    exam final embarrassing aim mean

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
