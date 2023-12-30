c16cd5e1dbd050a53b8b92cd4119d69f5e81c211
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:33:26 +0000

    land properly smoke upset rent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_list_from_array(array):
+        return array.tolist()
