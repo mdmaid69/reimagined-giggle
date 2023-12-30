5989c0ecef144f7b58e08ebcdd8e63ab4fc08448
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:17:03 +0000

    surprised profit may slightly near

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def count_characters(sentence):
-        return len(sentence)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
