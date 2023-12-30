a615c09f0c0f94bb547c5295c9cfff88087af0ad
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:24:02 +0000

    number judge afraid forgive early

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
