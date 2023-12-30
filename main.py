e7fdbdbd70ae6cc2c5f0079b9eb2f7a127d81cf3
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:57:25 +0000

    blame receive roof have authority

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_density(mass, volume):
+        return mass / volume
