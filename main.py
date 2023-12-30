c590b0c6af30ebb93340190ae04c35e0c198730a
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:33:01 +0000

    as bound minimum attach tie

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
