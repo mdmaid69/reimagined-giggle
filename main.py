a7bf326808b1153261a91154e082f4c059b81110
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:26:58 +0000

    independent sort characteristic mother handle

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 numbers = [1, 2, 3, 4, 5]
 print("Max:", max(numbers))
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
