3f8b58f1605ecf8df33956ca1476032fefaa593a
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:11:51 +0000

    hardly question foot remove respect

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
