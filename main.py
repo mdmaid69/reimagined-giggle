309556b8c9464870be35c8035c889f509deeeb54
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:58:26 +0000

    stamp lesson build throughout fast

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
