3f0c8ca4a770506eca12b260a8bfe88a1a95828f
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:03:11 +0000

    unit drop mean visit since

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
