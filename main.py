6d69ad89b1fae6e69e43a4c6a8ca5e3d78dc91e5
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:08:12 +0000

    round past breakdown taste affect

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_error_function(x):
+        return math.erf(x)
