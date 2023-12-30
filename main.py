a8f232eddfd1cf59e154b01cd56592f4306cb441
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:01:35 +0000

    season drink sound dick attach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
