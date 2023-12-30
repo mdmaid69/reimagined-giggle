79b2457069a4593981071a450d6771637b10ce33
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:36:01 +0000

    shoot mean thick home number

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import re
-print(re.match("h.*o", "hello world"))
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
