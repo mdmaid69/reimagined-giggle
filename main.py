8c29579b08aae05c99ce6f420a08d00548167d7b
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:00:35 +0000

    model load base us wrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
