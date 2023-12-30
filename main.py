c6ed6ab9212b7b283ebc1a7b8490648a169768d9
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:52:36 +0000

    path train month benefit strict

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
