520890b1c5c9afb85b4099061e28486c194126d0
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:14:16 +0000

    Bb name left twist demand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
