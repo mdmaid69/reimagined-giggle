75959ae31bb2cc1d09c98737d76b9e72e54ba731
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:44:20 +0000

    expensive admit away mainly fall

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
