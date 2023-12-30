a89199b80ea775e632862916d82d1b6ae2ecd804
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:45:27 +0000

    to once type or secret

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_area_rectangle(l, w):
+        return l * w
