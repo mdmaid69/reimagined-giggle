84c880e4e7bde58d60410919bc40497bdf8a168f
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:21:22 +0000

    born programme month cheque price

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
