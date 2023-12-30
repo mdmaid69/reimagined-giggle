96a8c7e8260ca6e92c09a0c9b5487e0a1bcfc71d
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:29:23 +0000

    change what bored pleasure move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
