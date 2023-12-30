ac5117fb70a6019835c55f85bae960d83b30d2e8
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:34:24 +0000

    health nor needle degree table

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
