1ba8fb625e12938708f908347ad625c2dbe4c1ad
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:41:47 +0000

    success sale present surprising still

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_factorial(n):
+        return math.factorial(n)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
