fce067912f0c0a92885a7eaa209585014a62a42d
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:42:06 +0000

    sign stand interruption shade knock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import platform
+def get_os_info():
+        return platform.uname()
