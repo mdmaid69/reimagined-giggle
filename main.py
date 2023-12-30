ebf7616b7f58138287db0f4b966d254ea3695f9f
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:57:54 +0000

    break sink foreign could win

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
