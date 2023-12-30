060f738199712ffdb006b976219ccb48b02b171f
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:20:41 +0000

    old small common fond even

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
