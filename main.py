991ce30a096db0e872b9f16ca650c484ece30824
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:59:23 +0000

    combination cheek Nn as count

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_speed(distance, time):
+        return distance / time
