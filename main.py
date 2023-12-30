7744c210816198b1297ba5b9bd7acfccd5c2c8e6
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:39:21 +0000

    payment that dry opportunity stamp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
