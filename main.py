78d5dc984816e6b211147d20908dd7d944683e9f
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:55:27 +0000

    laugh round communication distance scrap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import collections
-def create_priority_queue():
-        return collections.deque()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
