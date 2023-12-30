a873c694ab8f97f9ae185c0cbc1066ce71eb6bb7
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:48:33 +0000

    manner heat hit convinced band

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_power(work, time):
+        return work / time
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
