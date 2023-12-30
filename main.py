25848d65578178bb218b7059e7e43c010f4816bb
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:14:35 +0000

    quiet mistake morning tense next

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+for i in range(10): print(i)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
