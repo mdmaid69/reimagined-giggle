1117ebba2e43899fc75f9b715a4097b7d1683623
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:13:19 +0000

    accept niggaacademy breakout pity stick

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
