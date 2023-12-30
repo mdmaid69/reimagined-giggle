18ae9294bc82e5b994942bc1ce78953018350413
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:54:40 +0000

    attention towards before difficulty tape

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
