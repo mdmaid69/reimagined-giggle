e278f75d39e5fcacf5889b2e9b88854eceb4444c
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:08:53 +0000

    improvement breakfast indeed pretend join

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def get_array_as_frozenset(array):
+        return frozenset(array)
