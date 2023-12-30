040f84923c7918131e3685c72753b0879f482fb0
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:22:14 +0000

    air strict difference shut interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+from collections import Counter
+print(Counter("hello world"))
