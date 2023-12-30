d322d12018e958990b8dc9b2f47a3ebd6dc45438
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:52:49 +0000

    bite practice common contact heart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
