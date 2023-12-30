129b89b180dbdde7e154671a86df6b2af897756d
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:50:20 +0000

    half getout consist care traffic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_base_name(path):
+        return os.path.basename(path)
