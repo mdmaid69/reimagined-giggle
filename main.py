6d8fd35fc49b23af8342751cb239c9075d109d64
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:42:41 +0000

    achievement board prize persuade lean

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_area_rectangle(l, w):
+        return l * w
