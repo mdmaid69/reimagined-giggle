250236af99d37809b0fa4ff920c65d2032d5048d
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:22:47 +0000

    comeback favourite expression follow talkoutof

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
