39180cee5b801855622b9dc2228ea85e0628c4f1
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:54:47 +0000

    balance computer feature own persuade

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def is_odd(n):
+        return n % 2 != 0
+import os
+print(os.getcwd())
