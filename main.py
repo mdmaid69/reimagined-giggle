b41afa3847f5a12b748d994de461b6e6c512be2b
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:09:09 +0000

    but father available tidy need

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def get_array_length(array):
-        return len(array)
+def set_array_item(array, i, item):
+        array[i] = item
