da2bef90b51ae5faa6a760dabb640968e77cf498
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:51:37 +0000

    position promise our crowd callup

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-print([x**2 for x in range(10)])
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_typecode(array):
+        return array.typecode
