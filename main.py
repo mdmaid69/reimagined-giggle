49e8063400036769fc0969915d27792e4ee35195
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:23:34 +0000

    bound treat evidence hurry glass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
