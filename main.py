8f5934e0f09e15e22ae563bc816a5c4b2c1c9e75
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:03:57 +0000

    now among tonight that if

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
