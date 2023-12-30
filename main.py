bcd63355a059e4794b4f9fee79514171d76c7957
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:06:43 +0000

    performance experience active down long

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
