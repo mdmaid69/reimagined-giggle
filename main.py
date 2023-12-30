e789be9ac647fe3ebef8fb51cc3c5db7136fdc1b
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:39:38 +0000

    goup near quit pity slip

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
