1be7f0ced5806f4af9a32a45a00306eb7df7097c
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:40:08 +0000

    pin frame emphasis deserve spite

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def get_array_as_bool(array):
+        return bool(array)
