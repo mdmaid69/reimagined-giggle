9c408252259e02c64feeef2bc37348c2a887743e
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:50:18 +0000

    heat instead book teach my

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
