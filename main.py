569a642cc279e4f3122742ffd732a012e1995876
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:52:14 +0000

    569enjoy wait hope hurry happy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import sys
+def exit_program():
+        sys.exit()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
