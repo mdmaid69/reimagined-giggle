78e316399334017e0857dd4fd8a5543dbbca2d2b
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:20:29 +0000

    close clean broken snow target

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
