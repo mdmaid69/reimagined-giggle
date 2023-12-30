1f0d2603dba900b1722dec9ebe2bc296e7104bf4
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:14:29 +0000

    workon fashion practice family scale

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import platform
+def get_os_info():
+        return platform.uname()
