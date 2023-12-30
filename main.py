a2af31ac7655fea2e7b39f523691a49d7528f68f
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:39:40 +0000

    damage laugh deliver under attract

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
