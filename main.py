6cffd9deda9491c0b1b63f88f7e81b416e4ad8aa
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:13:18 +0000

    range instruction whistle arm grow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_power(work, time):
+        return work / time
