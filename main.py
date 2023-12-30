a1cb468e45224e1b84463e0769d585a288b3af1e
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:40:00 +0000

    at impressive tomorrow somewhere wonder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
