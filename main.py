19668d5ad348e81848303c57350cc910a3906877
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:21:24 +0000

    opposite chief such deaf mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def reverse_string(s):
-        return s[::-1]
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
