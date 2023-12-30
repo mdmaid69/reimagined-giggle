62c3718350bacb361dc43ad482d94ebbbac49478
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:19:00 +0000

    worth understand recover only sign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
