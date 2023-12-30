e033fe012d3f685acc6c47918c82a7f12b698d60
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:34:45 +0000

    without sure life break and

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def cube_number(x):
+        return x**3
+def cube_number(x):
+        return x**3
