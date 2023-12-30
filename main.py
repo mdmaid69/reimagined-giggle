8815e2ad37ae2c8e4df24d37e88566bdf1ad2a5c
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:37:20 +0000

    escape please contact print comein

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def find_min(lst):
-        return min(lst)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
