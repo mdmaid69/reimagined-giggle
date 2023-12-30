89dff2dc4a2bcc5fbbb467ca66249b77cd530611
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:54:18 +0000

    nerve could star precise underneath

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-print(sum(range(10)))
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
