334161e7b0e7270b0bb47c74e89cc924848601d1
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:54:07 +0000

    production no before class race

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
