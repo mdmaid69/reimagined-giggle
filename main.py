b838f62bb50bba16fb7243e9daaa8b7fd09fbe60
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:26:56 +0000

    relation callup hope lookaround buy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def find_max(numbers):
-        return max(numbers)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
