d0cf4e7fd1289aef72d65dc37d1ebb71cfb4fedf
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:43:34 +0000

    temper constantly mad round behind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
