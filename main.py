9a41e20a5fc79ae9d4e1d13f450d462998355468
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:57:52 +0000

    interview comfort satisfied supply shut

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
