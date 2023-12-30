a40a299ce7c37bd6a0a3d9747be4d6de7b8f83ba
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:23:48 +0000

    occasional question height its unlikely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
