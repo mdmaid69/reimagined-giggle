39d464531716ace79d4415ea99d411c5ba4d32f9
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:34:25 +0000

    complete lead bridge this stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
