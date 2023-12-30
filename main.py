bafb89bf8421e10e408f46cc2d49c8b222197f65
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:04:33 +0000

    through waste congratulate chief ticket

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
