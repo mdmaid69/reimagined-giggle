4cdc3db13e89b3ad1d8454784c230f9afd7ea2df
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:37:40 +0000

    brave police communicate promise hope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
