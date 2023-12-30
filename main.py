b957b83167bdd0dc6a0634bba75182d1bce4a496
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:04:56 +0000

    face nurse murder completely under

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
