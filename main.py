8f6f674c024980731f56946156c376a26a533d74
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:26:53 +0000

    move rescue visit book pass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import os
 def change_working_directory(path):
         os.chdir(path)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
