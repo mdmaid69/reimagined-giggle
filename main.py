c58afb84b56a52b24a4c78d90f8e7e86def841bd
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:49:34 +0000

    uncomfortable we adjective real impression

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import sys
-print(sys.version)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
