15fee59d721d10ef4636e7016f71e84146bd8772
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:47:31 +0000

    lock arrive shoot pattern appeal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+for i in range(5):
+        print(i)
