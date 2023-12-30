9d0a3abe78cc22ce32cd9d4f2e429169b088ad8e
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:39:57 +0000

    female once expression step build

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
