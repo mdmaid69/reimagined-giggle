a76966066a11c5edfbce7926e77fec2962892674
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:07:05 +0000

    sign welcome period tip perfect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_pressure(force, area):
+        return force / area
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
