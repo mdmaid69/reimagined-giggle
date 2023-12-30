6f22a334f51bf5e89b8a14f6834f4dc209bd3479
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:40:08 +0000

    blue giveback wake government original

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import array
-def get_array_length(array):
-        return len(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
