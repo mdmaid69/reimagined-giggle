1bbd015484214bfe6bd11c995adf628ad61b13ad
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:59:24 +0000

    kind break keen wait argue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_min(numbers):
+        return min(numbers)
