f981f050fda9181eddb8134a332e44b3d4094986
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:28:30 +0000

    limit proper whenever follow murder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import collections
-def create_user_list():
-        return collections.UserList()
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
