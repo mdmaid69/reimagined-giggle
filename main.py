6aa278c15653e6a377dab4b6e805b75ee0940c31
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:48:50 +0000

    doubt control service Oo it

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+print(sum(range(10)))
