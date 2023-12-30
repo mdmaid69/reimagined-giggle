de0d30e60ea5a4a61c074b1e011ddb996a191bd3
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:27:54 +0000

    herself pretty ago noise image

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import array
+def get_array_item(array, i):
+        return array[i]
