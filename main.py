3d3ec165b9f75913fb81231fa35d3bb80d9e4062
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:07:33 +0000

    any mysterious convenient straight getback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
