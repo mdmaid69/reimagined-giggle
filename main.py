27ca49a132f388a9ea528e86393cc8f0404a2400
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:49:26 +0000

    rush phone patience bend indeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import array
+def get_array_as_float(array):
+        return float(array[0])
