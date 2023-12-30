3e916ebf054534d0ea1e134693511868e3b94606
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:00:39 +0000

    match study holiday train each

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-print("Hello, world!")
+import sys
+def exit_program():
+        sys.exit()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
