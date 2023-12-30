6050ceb04fa27ae567d295fb232ca823bc17b917
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:47:12 +0000

    instruction shame bunch story do

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
