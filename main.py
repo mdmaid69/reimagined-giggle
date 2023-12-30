f2004173838dcb83a5984ce5b6115a16d44f9e4d
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:53:24 +0000

    habit bank lend influence city

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_string_from_array(array):
+        return array.tobytes()
