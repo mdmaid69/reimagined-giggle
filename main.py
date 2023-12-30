e95a028fd980a6e575cf65a55bab877594ce3973
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:38:41 +0000

    narrow harm special official marry

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
   def calculate_area_triangle(b, h):
         return 0.5 * b * h
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
