7a142e586ecb8ec127631879b3fb1744deee037c
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:50:12 +0000

    border uniform keen bag woman

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_item(array, i):
+        return array[i]
