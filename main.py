cd10c63963d0e887e452a2fad6607e4b2a772803
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:26:36 +0000

    hold 569enjoy line apply jump

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def square_number(x):
+        return x**2
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
