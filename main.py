14bbe9b78ea67940d7bf2d442d4a46a73adf191c
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:20:06 +0000

    strange comeround in sleep wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
