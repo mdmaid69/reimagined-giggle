99029f5b338e07e4d9bb0db78bc016a20d571c2b
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:59:23 +0000

    protect voice pronounce seat process

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def sort_list(lst):
+        return sorted(lst)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
