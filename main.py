8ba1091432dcdf64d139a48c39965ae90466e4e2
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:15:46 +0000

    after finger getout pattern bin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
