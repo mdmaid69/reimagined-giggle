599f08eb3ef89c2dd9df19be7c5a4b713788dbf5
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:58:53 +0000

    satisfied report goodbye sensitive dress

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
