8156ee83b0543c31b867e3c9f63a3b20635158fc
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:23:54 +0000

    reduction honest welcome hurt however

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
