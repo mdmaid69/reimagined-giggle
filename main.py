d492c3f93b5369f0914d5c830dc0d0d805901e3d
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:00:07 +0000

    me punishment moon poor shame

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  def remove_duplicates(lst):
+        return list(set(lst))
