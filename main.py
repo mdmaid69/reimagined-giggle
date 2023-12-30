2314ebc826616e3829ff5de8e545b8167d387710
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:25:05 +0000

    ordinary tall bunch moon treat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
