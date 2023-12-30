92ed3bfdd3bf5c936f39c1a4f4f43576096bcb0f
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:05:40 +0000

    they thanks cheat total action

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
