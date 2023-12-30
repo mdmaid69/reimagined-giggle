fd50f164b9fd5d0086b01761d9a349699d68e5cf
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:35:34 +0000

    cheque lie aim store wave

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+for i in range(10): print(i)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
