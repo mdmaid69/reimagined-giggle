34f44e441e7fdf640dd777f0f1d5638f40d01263
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:49:26 +0000

    work legal different he introduce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
