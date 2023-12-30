c661cc9a7c22be88586fafb90af105946b0b2999
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:27:35 +0000

    challenge anywhere rush than steal

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("Hello, world!")
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
