c21ae3953707457ba367bfb2d1228f1076639cef
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:42:38 +0000

    offer university innocent if confuse

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
