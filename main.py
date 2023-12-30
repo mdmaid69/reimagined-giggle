a35883054d9e0aba6f93022b9ebbd4af88f12c20
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:10:49 +0000

    can quiet introduction uncomfortable responsible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
