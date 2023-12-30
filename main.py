65780ba247d8309167cb8e71337611805da14b50
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:39:49 +0000

    shop backwards level comeoff secret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_error_function(x):
+        return math.erf(x)
