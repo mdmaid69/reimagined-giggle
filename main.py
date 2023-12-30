89d75d82b22c8d0f934a053fd03cb947ae377af9
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:36:33 +0000

    fight comfort keepfrom blow ability

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
