2590f55052625d73b1fafa09b418ac7b92a6f171
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:41:24 +0000

    public task discover bang getout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
