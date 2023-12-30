56f7556588c3c03fad9e0aebd91a3cd2379d213b
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:26:31 +0000

    think fact rule dry actual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
