ef2733521a0d6b9435ada7a94be7ec6a6179ebbb
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:56:46 +0000

    woman squash drop bin worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
