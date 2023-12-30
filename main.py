427dd58813948cde0c9fc2cc53025ef3c31a77af
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:38:27 +0000

    shelter soak rise 568energy sound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
