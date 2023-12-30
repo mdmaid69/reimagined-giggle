c59aeceff58255628ca0404fc369e81c9b91b857
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:35:51 +0000

    worried disturb right away organization

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
