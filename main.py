68bcdeac2cf1423c121a453378533ca76a965c76
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:28:09 +0000

    somebody definite sit computer environment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
