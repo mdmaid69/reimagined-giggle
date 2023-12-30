2f1854eb6f81ebf5eb67c7fc2fea3867836aea81
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:05:45 +0000

    cinema bar complete although anxious

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def count_characters(sentence):
-        return len(sentence)
+def multiply_numbers(x, y):
+        return x * y
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
