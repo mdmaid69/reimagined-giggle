950d5eae68ff6e8ea36d3f62347380271f454a91
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:41:07 +0000

    advantage century Ee direction guilty

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def find_min(lst):
+        return min(lst)
+print([x**2 for x in range(10)])
