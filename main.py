462f89c2a236fc5cfb41a41fe1dacafb4dc3cdef
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:44:52 +0000

    raw better recently artificial major

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
