0cc816b8debc39b53be266d83eb05db781866c12
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:27:34 +0000

    one bunch steam feel holiday

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def convert_to_hex(n):
-        return hex(n)
-def is_palindrome(s):
-        return s == s[::-1]
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
