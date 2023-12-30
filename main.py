0fcbffe070506c6fa22200a939b0b96b2f8d7bea
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:57:11 +0000

    anywayanyhow matter attention destroy value

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
