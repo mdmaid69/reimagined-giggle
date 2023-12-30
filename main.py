9de74f216fa369c883dd2189d1b92f9750d351cb
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:10:32 +0000

    rough adjective total comein mend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_power(work, time):
+        return work / time
+def calculate_density(mass, volume):
+        return mass / volume
