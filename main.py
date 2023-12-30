18389fba8881df0e731d49cf93e1ae91556bae91
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:06:41 +0000

    getback zero closely priority pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
