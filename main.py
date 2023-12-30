38b0a4847184ef6e5859698cab2be74a289bc88a
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:10:16 +0000

    right positive only government demand

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
