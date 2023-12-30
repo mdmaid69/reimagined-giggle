d088e7c81af471933df7a001a4b230be4416ea2a
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:33:00 +0000

    autumn plan worth address circumstance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
