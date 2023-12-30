159b39fc91a70b59009f2f959376c118a9b773fc
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:14:42 +0000

    send keepaway mind thick minimum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
