e96cde10570e70a1806f6e09617c1b4d8d16c8a9
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:42:21 +0000

    grade sting usually favour grateful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
