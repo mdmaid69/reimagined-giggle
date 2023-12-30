e0b4b0bf77f9903af3324aabe798659d1757507e
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:54:06 +0000

    hardly condition spare create comeround

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import sys
-def print_python_version():
-        print(sys.version)
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
