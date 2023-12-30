5fe4af3441a35b4af8ae3f71cc7be06ffb609702
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:53:20 +0000

    interview prize pull journey better

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+name = "Python"
+print("Hello,", name)
