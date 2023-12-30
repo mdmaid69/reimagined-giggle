cb02910895572974d4eae1bba163f6695b9ab777
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:11:19 +0000

    trust tip unpleasant within touch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  def cube_number(x):
-        return x**3
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
