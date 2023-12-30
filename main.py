2325f7a9097bc402cda11e2c36bfe7ecdf95575f
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:31:18 +0000

    big before new maintain meanwhile

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import numpy as np
+print(np.array([1, 2, 3]))
+def square_number(x):
+        return x**2
