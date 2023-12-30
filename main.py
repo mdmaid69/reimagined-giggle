7c9f1f4ee10aae368dea6a4e185f128caf36437b
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:13:41 +0000

    path inform ability disappointed health

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
