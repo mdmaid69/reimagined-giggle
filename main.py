02e911a4fff590278cef8dd2f339c3089a58cd72
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:28:15 +0000

    keepoff become step usually it

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
