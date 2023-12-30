3cdbb473f2f107a2981cd6d26742d6bf1d2e309a
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:15:11 +0000

    please dream only able themselves

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_blksize(file_name):
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_block_size(file_name):
         return os.stat(file_name).st_blksize
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
