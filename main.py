96b4cd2412e6f1798c396f7be775d5ce08c6a215
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:31:03 +0000

    avoid few spare explain trouble

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
