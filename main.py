ea99b3f59b93ad6f4e786840c3272bd29652f572
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:16:42 +0000

    comfortable help little pretend flight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
