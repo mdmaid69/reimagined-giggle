b2ee2bc44bfc3e8d5fe97554ed3c3a72bf34b4cf
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:56:14 +0000

    practical trap physical dare since

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
