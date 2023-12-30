cba3f1db1f22e50b71e1e0edd7c1e02e54ee21df
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:28:14 +0000

    accordingto hate wrong result clear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
