d35a953380091672839ccd6ab53e98d63b855b63
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:52:01 +0000

    interview wherever tap fear comeout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+x = 10
+y = 20
+print("Sum:", x + y)
