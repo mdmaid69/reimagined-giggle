456d0bb5ca9dc5697e147477e34f21fc9381192f
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:51:17 +0000

    load thanks open wide arrival

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def is_even(n):
+        return n % 2 == 0
