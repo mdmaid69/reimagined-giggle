0f7fd1ae5279a3e4b47702c7a62f694ca10b2fe9
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:55:41 +0000

    time patient medium attempt sweet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_unique_words(sentence):
-        return set(sentence.split())
+x = 10
+y = 20
+print("Sum:", x + y)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
