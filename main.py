6a3946a9cb08c2cf4a22abb099213384cb49b3aa
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:10:51 +0000

    search each plate happen meal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
