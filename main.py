7e83918ace9c6bdf32e168d14f00e9438c092c5e
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:05:54 +0000

    used Rr represent claim wipe

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
