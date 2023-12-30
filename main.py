ea8dfb4a0c335833e1f884533cc32401eeb67f54
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:28:12 +0000

    correct guard dangerous arrangement bang

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
