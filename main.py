18d9902e6d36a3ae910f62f41de2b01d84546882
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:46:41 +0000

    in soil breakdown practice meet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
