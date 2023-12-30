73820b98f4d9a8558d8d4525bccff341e956c218
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:34:46 +0000

    tap trip create instruction outside

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_length(array):
+        return len(array)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
