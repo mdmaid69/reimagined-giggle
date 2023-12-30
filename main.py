2126548f0ea955acd99fa97956f10ea73171bc5e
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:23:35 +0000

    guess kill smoke feature share

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def convert_to_binary(n):
+        return bin(n)
