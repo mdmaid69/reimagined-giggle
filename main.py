e1093f4fed9fed05864dbb2b3b875406074127b0
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:55:36 +0000

    used behind field cause squeeze

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-def add_numbers(a, b):
-        return a + b
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
