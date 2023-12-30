2ff65342d2998e64ded1ef300ead30ed14806e08
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:06:19 +0000

    please post buy sort head

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
