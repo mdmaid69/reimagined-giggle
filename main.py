496a492da097327116dd7b91c86d27cb28f60757
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:23:46 +0000

    flow coat process sign strike

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
