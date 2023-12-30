140ef0eb5f0ee33bfa14d616e6a727f7ea38cbfc
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:11:02 +0000

    discuss performance copy love either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
