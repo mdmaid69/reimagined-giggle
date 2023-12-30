e957ee88963f534f5e208f78171c8d571622076b
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:43:28 +0000

    proper getaway complaint getback scare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_as_memoryview(array):
+        return memoryview(array)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_length(array):
+        return len(array)
