d80af62703d50c7a31368f348a058a19e25d5c78
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:44:15 +0000

    area bye getback border fence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import time
+def get_current_time():
+        return time.ctime()
