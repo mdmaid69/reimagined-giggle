2552543ceac037b5832f8d314ed40e554ce29e7d
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:37:51 +0000

    pupil mind behaviour square active

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+for i in range(5):
+        print(i)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
