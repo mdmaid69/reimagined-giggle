50213dd3eacd0963b3c5a3c89cd70945205e81fc
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:03:09 +0000

    regional impatient frighten ever union

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
