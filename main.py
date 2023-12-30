c3fd4484e38ed67b5fd2c21432a067d760f49d1e
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:57:39 +0000

    mysterious load little wave forward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
