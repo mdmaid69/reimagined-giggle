e4f02722b715cb89b45ee1d42668e8d632ce86ae
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:26:04 +0000

    should disturb estimate demand certain

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def sort_list(lst):
-        return sorted(lst)
-import datetime
-print(datetime.datetime.now())
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def convert_to_hex(n):
+        return hex(n)
