8b3343ff463f65aaa25fa429b15da5392cb48d11
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:32:33 +0000

    habit cover middle company connect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def print_python_version():
+        return sys.version
