dafa414a24f6dac977704d08269d9f038aae0d71
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:47:43 +0000

    section slice Nn lost knock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def roll_die():
+        return random.randint(1, 6)
