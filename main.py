ce5c71609c536251ec6c6b81070abf7afd7e7282
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:36:41 +0000

    blind consider bunch hit dick

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_power(work, time):
+        return work / time
