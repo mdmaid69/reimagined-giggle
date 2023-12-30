1936de5b3c49c3f9661af4f85199c7b436cad173
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:37:51 +0000

    name reason common embarrassing runaway

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-print(math.pi)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
