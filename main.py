2c17a77a931e2a4f7c2f6794d9a2f303985c1bd3
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:07:09 +0000

    system relief talkback partner comein

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def add_numbers(x, y):
-        return x + y
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
