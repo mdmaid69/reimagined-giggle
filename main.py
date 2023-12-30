b0889eef5866bf1036d8b640ea4609c7797c5bfd
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:33:30 +0000

    they suffer post journey train

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
