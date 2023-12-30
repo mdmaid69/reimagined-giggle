e8595c147ff4279ea45caee831aaf63333c05e0a
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:40:34 +0000

    willing attitude glad common odd

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-print(sum(range(10)))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
