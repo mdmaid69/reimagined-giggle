570f02fe24c04b98d4bff2407ce7e449e61d6e48
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:39:30 +0000

    total lose priority comment kind

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import time
-print(time.time())
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
