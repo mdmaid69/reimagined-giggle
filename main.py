f6f8c56911c845f3a68ec386b5dd4052910086cc
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:07:28 +0000

    loss careful accept cut raise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
