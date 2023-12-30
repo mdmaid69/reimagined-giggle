f6608263b205dc4582cab960e4da382d7690529f
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:29:12 +0000

    near dress the twist block

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
