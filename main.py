cd6db0daf785f1a07811a6f66a16d373759a7dee
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:53:10 +0000

    waste fine responsibility hand style

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
