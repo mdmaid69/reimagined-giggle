f20f3b0f12574fad66fa022fe79cd972efbb16e0
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:43:55 +0000

    brief hungry battery interfere involve

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
