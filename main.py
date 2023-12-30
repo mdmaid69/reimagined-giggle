5bc6cab6567764b65ff9a8fa40dcce657b7fc38c
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:54:13 +0000

    music secret clean union foot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_density(mass, volume):
+        return mass / volume
 def calculate_payback_period(cash_flows):
         cumulative_cash_flow = 0
         for i, cf in enumerate(cash_flows):
