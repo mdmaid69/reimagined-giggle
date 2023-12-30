ab4834c06a182630a9d67decb210b5fa91bc1485
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:19:39 +0000

    dare need note head authority

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def add_numbers(x, y):
+        return x + y
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
