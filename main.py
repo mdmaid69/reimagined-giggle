7daa1bf4ab00fcf701289d2eccd03bfd68e7ec65
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:05:35 +0000

    soak neither callat bringback must

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
