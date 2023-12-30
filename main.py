eeb3930c8ac1c63aac3641221ef1b93a6d207b5b
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:18:16 +0000

    steady reason 569enjoy help better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
