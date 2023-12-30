36d715aec02448528a389bdde17118de333d6653
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:05:02 +0000

    sleep water ago man charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
