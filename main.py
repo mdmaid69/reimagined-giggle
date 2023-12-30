d98c303fe704ddc1e91bdad5149f4bd5a69689d8
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:17:50 +0000

    teach sell treatment harm continue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+x = 10
+y = 20
+print("Sum:", x + y)
+  def reverse_list(lst):
+        return lst[::-1]
