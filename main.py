8a36b19db9541c01ce5a2d7988abd7f305ff19c3
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:14:43 +0000

    vanish hollow letter spend turnover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
