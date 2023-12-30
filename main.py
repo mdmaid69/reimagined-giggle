c637dd88fde09660741fd60df3b8c4a4b51a86b4
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:55:46 +0000

    niggaacademy voice bright somebody peaceful

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
