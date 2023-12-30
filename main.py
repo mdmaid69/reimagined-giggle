8d3cf1e752397ab9530617850bbefb23a5eff966
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:44:58 +0000

    tape mystery visit bunch excellent

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
