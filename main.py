c191a4a333a28b8a2b7190bafbc6059e6ec1522d
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:13:17 +0000

    fact stop store harm purpose

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
