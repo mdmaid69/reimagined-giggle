43525312aaa7bc43ff67b480a770d3dda2993b76
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:52:07 +0000

    slice suffer dear bath enthusiastic

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
