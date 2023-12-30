12b579bd897f167b5caa976ef73b53e09f72c900
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:16:48 +0000

    process quick turnover ground unless

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
 import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
