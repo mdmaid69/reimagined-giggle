294c900d87d249a40edd2967ed4fdcd2de99c023
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:54:08 +0000

    division since defend importance march

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def append_to_array(array, item):
+        array.append(item)
