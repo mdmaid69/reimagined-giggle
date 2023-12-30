41fefde4ded6744ec2caa172946f3d59b9fc852f
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:52:05 +0000

    listen deal gas issue move

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
