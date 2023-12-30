6c4e61b4f5f6e7478d413801ff8d5969715f7b69
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:27:42 +0000

    interrupt hear communication worst method

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
