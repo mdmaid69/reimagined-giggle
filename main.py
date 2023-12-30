05b01a347fd7497d0fbcae34aa38eddf78348f96
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:56:46 +0000

    splash lay half central reject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def append_to_array(array, item):
+        array.append(item)
