756c16c0c9a76919a86f02169ade6deac8747c9e
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:26:04 +0000

    paper interfere any kind card

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
