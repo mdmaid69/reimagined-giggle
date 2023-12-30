d7665885ef15b668474e0bd41a095e6d295d537c
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:59:33 +0000

    takeout wealth style receive uncomfortable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_power(work, time):
+        return work / time
