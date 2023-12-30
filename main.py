067eee9915254c3eccfe5bdedabe9451976f1234
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:21:56 +0000

    Itself before stand admit fortunately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
 import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def get_array_as_set(array):
+        return set(array)
