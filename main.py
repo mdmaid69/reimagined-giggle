4930b779b8901ab6ad2511dbdb9682cd6f6320ce
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:58:14 +0000

    necessary performance workupto fortunately themselves

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_repr(array):
+        return repr(array)
