6955d428b425b0628958ce880fd60aaaf52c15fb
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:43:27 +0000

    surprise pretend murder tidy can

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def cube_number(x):
-        return x**3
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
