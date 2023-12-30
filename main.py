c907791eb7369f24ad94f2365836832f4cbc7d6b
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:11:23 +0000

    broken count decide suppose gas

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-def find_min(lst):
-        return min(lst)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
