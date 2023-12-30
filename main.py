485177c10d7fbbe5a5de12a595283731ada3b3c8
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:31:47 +0000

    below relationship holiday uncertain hair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
