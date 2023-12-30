aa81a4b06e770f40026c44aeb627cc05bfdb6a58
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:52:09 +0000

    entry Rr breakout shoulder getout

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import tensorflow as tf
-print(tf.__version__)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import sys
+def print_python_version():
+        return sys.version
