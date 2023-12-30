c3eb41a0bd3ee86cb45ef55bf115b87c67c1b5a7
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:20:22 +0000

    weak close he track successful

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_acceleration(speed, time):
+        return speed / time
