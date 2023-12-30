d62b9179aef02782f89aa01b75b0f0b8fdf467b6
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:39:35 +0000

    action already secret side begin

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
