44e46a7f127696faef643646083c197acfbc715e
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:26:24 +0000

    allow rain slight brown shop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
