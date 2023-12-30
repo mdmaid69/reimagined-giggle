2663decc596323c4eb724e5e997f79088d9fb141
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:28:03 +0000

    wire reliable about embarrassing fair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import random
-print(random.randint(0, 100))
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
