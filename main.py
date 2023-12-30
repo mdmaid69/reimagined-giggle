707ccdad3ab805b0c3cc1f32bb18d6af0ca432f6
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:19:39 +0000

    moment talk roll reasonable straight

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
