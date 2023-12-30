84669211542faeb197c7712b332efb2047f9736c
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:34:31 +0000

    weigh rain hard split quiet

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
