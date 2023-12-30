a495c6a75cdf6fe5a5cde506f2a428ed875e9510
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:34:51 +0000

    duty prize about appearance large

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_float(array):
+        return float(array[0])
