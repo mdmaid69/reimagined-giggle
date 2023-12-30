13366a64d5f9e2f4b8e8b81dfef5a850ba65d9e7
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:37:25 +0000

    have convenient dance line apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
