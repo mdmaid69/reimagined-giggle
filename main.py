206a6bf4623384e349508df753e9b11eb1dc5e10
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:07:17 +0000

    fair talk world consult Hh

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_power(work, time):
+        return work / time
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
