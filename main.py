4a613ee3f6d68792ba111b590a8e81dd618163d8
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:17:18 +0000

    deal scream university belief light

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+print(os.getcwd())
