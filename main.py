9802ad3e4041d979eb19e36953d54cd5f9479add
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:45:49 +0000

    Pp punish basis film survive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
