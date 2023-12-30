b0cd3848aa570b6e1caa4fd0a33b78ba9c08b1ad
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:03:19 +0000

    save tour lookup contrast separate

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
