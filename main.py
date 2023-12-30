66e197409593700f6c6b099c33392b1fda29cf61
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:45:50 +0000

    rise Mm far marry fierce

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_area_circle(r):
+        return 3.14 * r**2
