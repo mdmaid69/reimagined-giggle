38bd6a4f57d2598b46301c8ffd396b3efa09a100
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:26:39 +0000

    ground deserve ago stop guilty

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def sort_numbers(numbers):
+        return sorted(numbers)
+  def sort_list(lst):
+        return sorted(lst)
