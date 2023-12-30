49ce093ddf8df43b1296e2a9b529e3c88185120d
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:00:12 +0000

    people pretend after voice style

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
