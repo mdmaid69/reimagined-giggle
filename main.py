ecfe44bfeedce8cff8f0d45fc30d3205e519a05c
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:03:10 +0000

    friendly destroy uncertain bringup wet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import time
+def get_time_since_epoch():
+        return time.time()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
