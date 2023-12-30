0cb9e9eedf89f6fb6cb887e9f26367fe6924fb06
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:17:20 +0000

    mine reach culture quit complicated

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
