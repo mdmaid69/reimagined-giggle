2dc5c8e122303f0227ddcdc62d5848375e734bcd
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:31:46 +0000

    disagree benefit opposite bringdown recognize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import collections
+def create_user_list():
+        return collections.UserList()
+def find_min(lst):
+        return min(lst)
