c5c45f9c35718e6ee4c10c6db781a6641bdcacf7
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:43:00 +0000

    clean principle simple fairly machine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
