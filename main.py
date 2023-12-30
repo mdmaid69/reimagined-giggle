659e49d534e58e7fbc57cff1c4c9e05fc319a7a8
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:34:10 +0000

    satisfactory suckme minimum favourite freedom

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-import platform
-def get_python_version():
-        return platform.python_version()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_work(force, distance):
+        return force * distance
