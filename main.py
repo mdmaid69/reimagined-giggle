fe1b8e284a87704020b881cd9a6c8a9afbc6a445
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:45:29 +0000

    uniform successful talk area car

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
