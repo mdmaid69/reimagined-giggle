b00fef90bf4c4c8131b32428f6ff400eef3ccffa
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:13:01 +0000

    recover clearly relax completely team

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
