26aa91058d8427b909329b118ad2aa848d2df568
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:12:46 +0000

    takeback turn niggaacademy splash string

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
