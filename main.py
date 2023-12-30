4d8491207c4c578610257e6aa250fa9f75d285a7
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:31:43 +0000

    office thing raise with tall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
