2d857ba39415fe3e81fa1e94e784bebce5dcc8b5
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:17:08 +0000

    loud give Vv very worse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
