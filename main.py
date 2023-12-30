7554c59d33cdd47520bdf981ce02c938523dc1d0
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:23:44 +0000

    round switch party introduce yours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import array
+def get_list_from_array(array):
+        return array.tolist()
