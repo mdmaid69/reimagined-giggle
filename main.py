f5ee1d417879783df6c732737dcf98dfb4645feb
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:29:27 +0000

    mark impatient grow trade responsibility

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
