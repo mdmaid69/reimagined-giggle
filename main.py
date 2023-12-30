2ec3f23343e8fa7087b7a5233b999ebea24269fa
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:41:05 +0000

    perfect sail speak stiff together

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
 import array
-def get_array_length(array):
-        return len(array)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def get_array_as_list(array):
+        return list(array)
