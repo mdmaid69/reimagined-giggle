42a2522d854c53bc65439acf6e65ed370164d57a
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:04:33 +0000

    whichever bit fly piece remember

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
