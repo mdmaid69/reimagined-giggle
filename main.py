235d96ec763d95f4708d1291ce0c9cd8bc920f57
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:33:01 +0000

    die than choose story loose

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
