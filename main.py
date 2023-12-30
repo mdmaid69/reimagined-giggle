b8c325af2bba516497f012e4e747ad9ce4cf229e
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:19:18 +0000

    separate part radio slide soak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def find_max(numbers):
+        return max(numbers)
