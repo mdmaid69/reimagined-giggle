33217a644a04718d4e66178a412ba3ea4230f740
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:09:18 +0000

    rob programme sticky upwards waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
