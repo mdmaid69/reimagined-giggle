73d7f786571b28e207740896bb09234d8b2e20db
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:21:27 +0000

    home tidy check stand put

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
