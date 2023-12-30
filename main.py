4a9fdde4530182bfb3898a0f670e4a18b4e57481
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:08:29 +0000

    mark pleased chase reject this

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
