a4b1c79e06100951fac13e7dc91abc1f5b8d5cfa
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:06:09 +0000

    natural rent except command arrangement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
