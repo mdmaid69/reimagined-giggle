5262987b0168e826383a002851f43fc2a6271f76
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:20:24 +0000

    slide container protection divide near

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
