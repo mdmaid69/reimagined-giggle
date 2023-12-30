2c55419d71977c0e59e31cb83f44be7266e8fd12
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:17:16 +0000

    we claim breakfast human precise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def remove_directory(path):
+        os.rmdir(path)
