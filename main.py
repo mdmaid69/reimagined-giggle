76aebba73c8aa17e3242b134c65f59bbab857432
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:02:16 +0000

    defeat Gg compare rid lookaround

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def clear_array(array):
-        array *= 0
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def multiply_numbers(x, y):
+        return x * y
