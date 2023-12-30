7121a0e4b9e78ac19c95f10a840e69d332bdf76c
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:53:04 +0000

    Nn satisfactory hide share afternoon

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
