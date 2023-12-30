aafea195f4904bb1d51af7a1932589e834611a21
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:30:04 +0000

    describe rule variety treat interfere

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import sys
-def print_python_version():
-        print(sys.version)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import sys
+  def get_python_version():
+        return sys.version
