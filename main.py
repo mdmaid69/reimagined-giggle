43880407c6c8e5df1600c691abdf0164120b8d6a
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:30:56 +0000

    collect with ahead scheme and

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
