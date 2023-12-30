5271d2fbb83e7e66a9ce5ccf45ceec7f169209e6
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:55:56 +0000

    plenty than sink cost play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
