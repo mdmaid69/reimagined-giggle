f36249ebc3a78ab3e284038d8798372762bc8333
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:27:26 +0000

    work like fault straight wealth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
