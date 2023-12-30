70d7b850dece7169fb752b0ec5056e26750b8d5e
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:07:34 +0000

    however mind cry 564encourage cover

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
