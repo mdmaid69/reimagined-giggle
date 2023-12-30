f5e4581905eee9166353465f3a6e885e5bd74619
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:14:19 +0000

    receive surprised gentle knock about

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
