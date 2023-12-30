129c411130b2f38970431158848c055de95c1d1a
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:53:35 +0000

    use list vanish reach shake

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_speed(distance, time):
+        return distance / time
