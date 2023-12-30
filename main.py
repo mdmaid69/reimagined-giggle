5124cfddf0d6b40320e40ff200635d9aad1e8e6f
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:40:02 +0000

    quit broken damage perform bringup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+print([x**2 for x in range(10)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
