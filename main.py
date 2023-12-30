acf0643d944bd738fc6dd1b34a50236d82b0df83
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:29:14 +0000

    bus unlucky sick steady comfort

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
