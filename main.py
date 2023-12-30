3709cceddc8caa7d4f73fe186e1f52536a32386d
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:00:18 +0000

    concentrate moment comeround ago fact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
