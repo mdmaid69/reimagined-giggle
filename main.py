f9ce9fbdbb9fe8e8ce2d2f9bf5b841ed80182be3
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:58:19 +0000

    question tone chase tip use

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
