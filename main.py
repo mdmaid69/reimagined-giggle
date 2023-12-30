5c761f47c5e70aedff807ea0e04d88d20ca401e2
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:03:48 +0000

    best achievement exhibition polite chase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import platform
+def get_os_info():
+        return platform.uname()
