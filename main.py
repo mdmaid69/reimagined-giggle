707407c00afc89f147c452ba45a75b692b8422b7
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:37:57 +0000

    constantly doctor satisfied face Bb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
