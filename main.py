159f5c89c1e0fb7e541f4e22b1daf63182150e17
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:44:01 +0000

    fortunate centre light consult good

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
