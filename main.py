3a62d509baed422b7f069d6d2785d9ce99473afe
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:09:31 +0000

    anyone corner tie Ll importance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_pressure(force, area):
+        return force / area
