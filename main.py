aeaa5b17c98d3977d560950b02e668274d9fcca6
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:30:30 +0000

    term hurt cross paper comeup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def is_odd(n):
+        return n % 2 != 0
+i = 0
+while i < 5:
+        print(i)
+        i += 1
