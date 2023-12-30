02695a5dcf6f1f055c53792281f5d2ff219e6183
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:00:45 +0000

    fear smoke takeout wire back

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
