48c72011dc27cfee5ca9c0dd0e43ad62337aec35
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:17:29 +0000

    talkoutof us married me direction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_min(numbers):
+        return min(numbers)
