d4ab8965c63f1579a9b39633e0ed7b013c22c9d5
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:27:09 +0000

    make flat scare consist fierce

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+print([x**2 for x in range(10)])
+import os
+print(os.getcwd())
