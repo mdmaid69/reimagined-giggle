12d495759afd8b4b8545925edff4501669dfe491
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:03:33 +0000

    Gg pass test pour full

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
