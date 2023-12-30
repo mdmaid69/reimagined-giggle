306d6b099d3564532a4d4887dd133608056b0f09
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:36:04 +0000

    such date alive turnover hold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
