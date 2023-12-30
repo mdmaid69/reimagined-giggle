d6f33fa171766487c267f8a8c300d118eadb9ec1
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:32:31 +0000

    support include run several behaviour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
   import os
-  def get_file_uid(file_name):
+  def get_file_owner(file_name):
         return os.stat(file_name).st_uid
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
