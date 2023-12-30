bed6194b745922e57cdfa21f24313979b6497eb9
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:41:03 +0000

    skirt favourite month often drawing

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import array
+def reverse_array(array):
+        array.reverse()
+import collections
+def create_priority_queue():
+        return collections.deque()
