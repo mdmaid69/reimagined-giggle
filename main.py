fbcac26ea5827198dc8215c159b95e24eaf0fc10
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:03:03 +0000

    contribute react objection flat lonely

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
