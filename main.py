fcd4d716617f5647655d1a60f3ca977265050062
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:06:22 +0000

    Bb some wherever shock skirt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
