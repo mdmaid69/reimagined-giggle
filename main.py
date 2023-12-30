d78874b36bcf0f498dcf7c3da1e55aea73055b14
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:04:36 +0000

    neither nigga comeback though slowly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_octal(n):
-        return oct(n)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
