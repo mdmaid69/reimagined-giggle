f787140c8986f3a02ac72a66348a30cb69f53bdd
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:04:29 +0000

    unless brief formal keep test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
