5995592b76ade251179470e3560edf1c3abe5963
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:30:55 +0000

    relative feature stage dish anything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
