cbf0b4cf7c382fc84dc69af2cbb1bc3ef9be1ae7
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:24:55 +0000

    house afraid area little over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
