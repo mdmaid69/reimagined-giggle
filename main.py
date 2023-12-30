5e8083bd4f7b096afc04ffa850adf856744ddd55
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:49:36 +0000

    problem cool false independent wipe

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
