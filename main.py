bdf47ae61b934e270de88f1611dca22047d4f340
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:22:19 +0000

    wall flood direct regret else

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-for i in range(10): print(i)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
