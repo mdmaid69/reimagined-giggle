036f54905d731b08a10a1a0a1f5461dc697492d8
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:27:31 +0000

    embarrassing impossible thinkof new allow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
