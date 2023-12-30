98121e072b053460c205b0c3ed58f87534cacf81
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:23:17 +0000

    contribute popular honest explanation luck

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
-def convert_unicode_to_array(unicode, typecode):
+def append_to_array(array, item):
+        array.append(item)
+import array
+def convert_bytes_to_array(bytes, typecode):
         a = array.array(typecode)
-        a.fromunicode(unicode)
+        a.frombytes(bytes)
         return a
