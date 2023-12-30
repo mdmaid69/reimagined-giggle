2a51d82607fa26db5c013d0879103325469f235e
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:06:40 +0000

    steady example present thought sticky

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
