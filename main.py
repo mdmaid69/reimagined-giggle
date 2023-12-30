883e489ef9fe1a5acf8caed837833ed9d3e0fb01
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:51:39 +0000

    only fire class suddenly evidence

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_speed(distance, time):
+        return distance / time
