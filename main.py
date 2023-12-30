71eff05ff8512bb049b951f00acbb832f3b17775
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:15:13 +0000

    lost punishment pour 572enter software

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import sys
+def print_python_version():
+        return sys.version
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
