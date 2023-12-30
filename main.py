a5bc443bde37e385b8bc400ce181160261fe2adf
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:00:02 +0000

    comeover object follow responsibility surprised

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def get_array_slice(array, i, j):
+        return array[i:j]
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
