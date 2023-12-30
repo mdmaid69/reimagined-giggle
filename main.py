ced8100d42862119af139beed9aa4891c63ddb52
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:39:05 +0000

    bread turnon file in water

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def add_numbers(x, y):
+  def add_numbers(x, y):
         return x + y
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def find_max(numbers):
+        return max(numbers)
