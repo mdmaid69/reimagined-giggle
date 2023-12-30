85c6e384f0aad0c757d96ef5ec5e0657e7cdaf81
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:34:59 +0000

    congratulations workout Zz lesson tired

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
