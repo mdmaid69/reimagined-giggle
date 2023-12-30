6c1b0972cb48d622139db3dddf9de95f6f538fbe
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:59:33 +0000

    approve comeover tour contrast cut

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
