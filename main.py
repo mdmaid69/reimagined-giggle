bdd1cf8c650638a9c7def3016ee8df22226d859d
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:30:54 +0000

    step recommend travel letter workon

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import sys
-def exit_program():
-        sys.exit()
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
