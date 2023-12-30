106129ddea739e2e3f13a2ae783730ee308fea8f
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:41:55 +0000

    beside frame bin say post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
