5170f93cdb076058c4aa4d44a4ff49c40eeb33a7
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:51:05 +0000

    shoulder content influence poor hang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
