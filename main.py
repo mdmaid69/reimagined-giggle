2ccfcc8b9a2602a185d5fdf6b71f190ac9d3124c
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:28:20 +0000

    stamp best quick before comedown

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def sort_list(lst):
-        return sorted(lst)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
