37e6a9e9c01a872ba463dd837641d3df65dab6e5
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:40:13 +0000

    journey mess them lesson stamp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  def add_numbers(x, y):
+        return x + y
