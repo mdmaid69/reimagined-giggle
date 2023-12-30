27092ad12a41e334887c8893e813334a7cb4c7de
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:27:36 +0000

    study expensive move interesting blue

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import platform
+def get_os_info():
+        return platform.uname()
