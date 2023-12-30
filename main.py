ceb8a8e7f1daa06c301e5a1be3ddf4aac7a5234e
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:36:25 +0000

    float vote either material violence

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
