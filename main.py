d56f763f19bb9736132f8044b8cbd3d91830ee32
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:07:41 +0000

    hit angry refer error wipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
