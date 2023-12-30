f393005a54cfbedaa0c9280c4793b74f3442373a
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:13:10 +0000

    pay call its evidence boil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
