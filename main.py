2245ce2396143d4c5b3f145131b6af3b4e2cf18d
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:44:22 +0000

    smell on side glass hang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-def convert_to_binary(n):
-        return bin(n)
+def get_array_as_tuple(array):
+        return tuple(array)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
