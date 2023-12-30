e50590a52b6950cba5c2cec4d4a42ee3331f6c3e
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:16:19 +0000

    judge pollution excuse within name

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def sort_list(lst):
+        return sorted(lst)
