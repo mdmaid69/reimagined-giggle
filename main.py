86b73d8696cff82303040e6a63bf23c6fb15905f
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:17:12 +0000

    medium spin than mistake rush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
