49ec5830423e4fe5ebe3a2ff0a1686fd6b94b6ce
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:15:41 +0000

    fortunately pinch state mean pleasure

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
