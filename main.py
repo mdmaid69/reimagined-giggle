2a91bf5cb67fc6844f68d357a5630125b0e7fa66
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:50:44 +0000

    all none nose last really

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def is_even(n):
+        return n % 2 == 0
+import array
+def get_string_from_array(array):
+        return array.tobytes()
