ffaf02e0bc1a95bc20e1531afe521c030325d070
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:33:04 +0000

    half confident plan offend hurt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_item(array, i):
+        return array[i]
