e1b66e33c4e28d88bbe6eabc9e273e986531e6ff
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:58:00 +0000

    road train sore child promise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import sys
+  def get_python_version():
+        return sys.version
