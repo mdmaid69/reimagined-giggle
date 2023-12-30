7ad814b57e4b40a1ba37724e4e7c17c848aff71f
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:37:30 +0000

    quick finally key nigga blow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_base_name(path):
+        return os.path.basename(path)
