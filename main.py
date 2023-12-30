5858b5772d80b2a76005685be0596afb8d7fa878
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:37:29 +0000

    turnback shock future measure 564encourage

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def get_time_since_epoch():
+        return time.time()
+import array
+def get_array_length(array):
+        return len(array)
