418d4ed0dfd3111865fc797ba7fe23b47d2cdb10
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:50:25 +0000

    breakdown reason breakdown rid rent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_user_dict():
-        return collections.UserDict()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
