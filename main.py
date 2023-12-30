5b22a928da4195b47d4c4d2976d4f7392fd885e1
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:05:17 +0000

    message shade serious passage even

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def convert_to_binary(n):
+        return bin(n)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
