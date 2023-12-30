56c7259612369a24f6baf4e8c58ded3624aa4e92
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:23:09 +0000

    stop beneath begin none best

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import tensorflow as tf
-print(tf.__version__)
+def get_list_from_array(array):
+        return array.tolist()
