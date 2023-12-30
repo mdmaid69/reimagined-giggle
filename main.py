92b319de32ba93ca8b835dbec0478ae67ad9d71a
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:18:03 +0000

    result lose rather new eager

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+name = "Python"
+print("Hello,", name)
