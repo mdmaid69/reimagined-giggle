4126959185e85af59f99cfb4b27200368a354145
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:40:04 +0000

    false vision lookaround arrange answer

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
