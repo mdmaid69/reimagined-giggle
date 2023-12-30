cb6c1846e3811873db353acfbe85051c6c1837c5
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:43:37 +0000

    like black breakdown worth accident

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
