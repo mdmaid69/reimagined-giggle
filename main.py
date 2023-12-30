299e99bc6f1b3167438c8fd86214afc6188bb0ec
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:02:43 +0000

    thought often be wherever black

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
