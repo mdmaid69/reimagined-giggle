20b5f4de5b34a2e81a289e8312ccb8607b3119a7
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:55:33 +0000

    trousers closed short problem wish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def add_numbers(x, y):
-        return x + y
+import array
+def append_to_array(array, item):
+        array.append(item)
+import math
+def calculate_floor(x):
+        return math.floor(x)
