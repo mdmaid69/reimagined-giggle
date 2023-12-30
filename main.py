88a26a5faf5deb4f605cff73398e16de4dfc82ba
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:14:38 +0000

    leave calmly reply thank plastic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import heapq
 def get_smallest_elements(iterable, n):
         return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
