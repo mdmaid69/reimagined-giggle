9f043f8ed854d30cf9b8cceb8e0600fd326c69a2
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:17:10 +0000

    clean slip giveback turnup include

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
