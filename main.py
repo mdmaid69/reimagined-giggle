275356c1b17e18365bd2a625b0c1d0f9fb79109c
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:46:44 +0000

    afford square declare cheap train

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def convert_to_hex(n):
+        return hex(n)
