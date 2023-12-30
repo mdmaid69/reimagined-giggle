2ac563d9c1717c347b85ef806d85aeec8b8d012a
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:06:25 +0000

    necessary ready line high interfere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
