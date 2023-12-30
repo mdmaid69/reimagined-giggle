da21728e35846285f2c245cfb6018e086863a1c8
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:26:51 +0000

    loan ticket Cc yours Ss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
