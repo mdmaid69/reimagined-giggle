063d1e62e65a4c774f0f0ca23c78cf3ec84c8b74
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:15:56 +0000

    himself fight criticize cause thick

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def cube_number(x):
-        return x**3
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
