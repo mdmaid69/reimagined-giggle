26dad9cec0c825889542249e605dfcc16a428e14
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:36:55 +0000

    hard cinema group available programme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def is_even(n):
+        return n % 2 == 0
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
