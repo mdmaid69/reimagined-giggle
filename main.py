d0c7eec59fc53a92ba3dff495e460553c88b058c
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:17:15 +0000

    save rest pronunciation lose distant

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
