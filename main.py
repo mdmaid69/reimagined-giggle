2f5f173700c97f30a8bcf46393ff0aa831032a7e
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:51:06 +0000

    pot just lookback(on) dear unit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
