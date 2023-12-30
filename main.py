ab9bfb1757aa5477732870ae16f1a1775e7eb585
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:53:41 +0000

    pencil muscle Ff Nn miss

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def reverse_string(s):
-        return s[::-1]
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
