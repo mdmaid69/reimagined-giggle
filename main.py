ca2ad686fe93053f352e9cf8602e7d29aa86415b
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:35:24 +0000

    load female frighten onto naturally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
