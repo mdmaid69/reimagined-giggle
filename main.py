773d92a1a64ed94b39f1ba36de79e3e2c0c586c2
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:21:26 +0000

    pile figure interested activity bother

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def count_elements(lst):
-        return len(lst)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
