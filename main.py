89f5919d0a56b9d7c6b6a0a27d0a11b7711aadfe
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:54:36 +0000

    bored prefer figure discussion simple

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
