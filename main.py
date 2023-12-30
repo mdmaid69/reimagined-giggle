326c487c0136370b9e70d1b5ac705c1fd9380b9b
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:37:07 +0000

    would crash confidence rob breakin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import json
+def convert_to_json(data):
+        return json.dumps(data)
