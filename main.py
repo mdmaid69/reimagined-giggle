663dbc06da5224b139ba06bab2380698c8817100
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:39:53 +0000

    us boil extra error trouble

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
