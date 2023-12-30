c2dbf28858ced0f847f935c153af5176f128ec89
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:27:23 +0000

    work yours challenge promise whistle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_density(mass, volume):
+        return mass / volume
