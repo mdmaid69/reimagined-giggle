152f008680c710ab09159a6d9075a926bc990c12
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:14:42 +0000

    mend question over shoot spot

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def convert_to_octal(n):
+        return oct(n)
