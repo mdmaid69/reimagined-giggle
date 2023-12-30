19b0f785894a69d2ba248f5ddc1a9bcec550b72c
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:58:27 +0000

    reasonable string possession swallow runover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_power(work, time):
+        return work / time
