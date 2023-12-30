0975a88eb9db5ffc61c2f80f16522dbf6c0467ed
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:10:52 +0000

    thing lookafter spread pass noone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
