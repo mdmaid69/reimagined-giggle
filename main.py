81d40ab13013795fcbe5a542a89f1ac32fd950a8
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:24:05 +0000

    contact transparent container little flat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
