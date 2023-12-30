7ec3539cbce50a1033f7ac64741905e460ef393d
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:27:29 +0000

    speech row read produce used

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def split_path(path):
+        return os.path.split(path)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
