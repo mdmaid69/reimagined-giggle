24b004b5b9bbd60f91e13ad478cb59e18c1dfa15
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:23:35 +0000

    sometimes direction car clear your

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
