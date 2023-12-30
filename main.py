1bd0f171ccb4e69d0c2f4c248aad65139c592745
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:22:27 +0000

    nice means correction when play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
