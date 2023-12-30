9b6433e9b3e2d505ea9099c8ff7661b39ebe43d9
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:34:47 +0000

    exam prefer convince hate determined

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import sys
+print(sys.version)
