b4fd1b5ff646ba5ddfcf41f00e51a0a190b8af5f
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:09:23 +0000

    nowhere relief march concentrate brain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def get_array_as_float(array):
+        return float(array[0])
