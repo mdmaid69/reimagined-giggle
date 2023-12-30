f5cef10c5ee44b6e34e26715718c0a7a8b6a314c
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:39:37 +0000

    personal split comfort excellent possession

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
