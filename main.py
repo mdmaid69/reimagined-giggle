5503e672e39694d7d62a00d83c601b3da536ef50
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:50:13 +0000

    round from someone abroad sky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import collections
+def create_priority_queue():
+        return collections.deque()
