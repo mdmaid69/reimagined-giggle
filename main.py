b590e1fe56507f9e5700fc2ec8027a766d48f169
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:39:53 +0000

    show rid intend fly rest

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+print("Hello, world!")
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
