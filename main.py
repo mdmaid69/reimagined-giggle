0ef2643db8f7a8cc8e366ba45a4d330ee3f231ea
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:41:31 +0000

    die press illness getback sweep

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
