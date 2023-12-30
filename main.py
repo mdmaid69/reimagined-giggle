550cb8d1b0b15c10979a0ae3d84a8d014c24e996
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:31:59 +0000

    rest rob best reaction alone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
