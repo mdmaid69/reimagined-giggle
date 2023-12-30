d4787d54ea8e0d140ba4d3e3f7e92b9138e4fb59
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:11:11 +0000

    poison godown field similar stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def divide_numbers(x, y):
+        return x / y
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
