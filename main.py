56ffc17e2bf21e3ab32916accfb269474cc3c0b1
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:34:05 +0000

    address sail band centre little

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
