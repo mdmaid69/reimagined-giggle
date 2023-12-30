ce67217f1981ef3938e1cb9a20da00a2aedb3d59
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:15:29 +0000

    surprise discussion challenge continue university

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
