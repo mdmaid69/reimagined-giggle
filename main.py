fb4be669ba7da9b42b3d0fa9effcdb4a1ee6087f
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:58:03 +0000

    minor into smell dare record

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
