600eada4f7fc23adf6fc9baa730a42ba4eec10fe
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:54:18 +0000

    refer everyone note illness if

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def multiply_numbers(x, y):
-        return x * y
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import array
+def get_array_typecode(array):
+        return array.typecode
