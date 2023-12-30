30d9cab7a7154c5ae39bdfb475ede53aea7c743b
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:56:20 +0000

    sign mend concern collect this

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
