59a2a128afbc07f45fed0c67d0fbf32782bbb5c8
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:16:35 +0000

    rough Rr challenge target wash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def is_palindrome(s):
+        return s == s[::-1]
