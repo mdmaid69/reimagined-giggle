9ddc8d8bb72f4fc8d56074a5a62c5361a35c22de
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:12:46 +0000

    build mind home skill who

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_array_typecode(array):
+        return array.typecode
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
