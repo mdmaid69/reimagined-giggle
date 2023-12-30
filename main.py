a6be4cef5827c9317734b92da244e853220655ed
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:39:58 +0000

    job sign where excuse it

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
