fd494eb465bfcee86a69e31cd875d209c1192c7e
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:48:00 +0000

    tread pile agreement claim care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import sys
+def exit_program():
+        sys.exit()
+  def reverse_list(lst):
+        return lst[::-1]
