f0bee3ee159eda710883589fd191f5667336c883
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:46:29 +0000

    turndown objection interview unpleasant set

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
