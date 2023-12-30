89ebb092f3d755a760cf8f82a901889201857c16
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:07:02 +0000

    over drink watch face cool

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import array
+def convert_array_to_list(array):
+        return array.tolist()
