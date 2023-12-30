a31b9867ac148916166f666f561eee119d5d9eba
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:21:06 +0000

    law tap age blame Zz

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
