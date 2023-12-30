c4596a8982faaa7d9c6c6510270d139c2b0c095d
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:42:05 +0000

    everybody find last slip unit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def get_file_size(file_name):
+  def get_file_size_in_bytes(file_name):
         return os.stat(file_name).st_size
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
