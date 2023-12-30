a99f05aef931c568654c3c5ee80c6a0da4dca2b4
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:48:47 +0000

    supply scare sign character comefrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
