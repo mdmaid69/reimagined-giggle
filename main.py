3665b62b7f23afaf663ec82aa51759871d3f5ae4
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:18:47 +0000

    hold sail right unfortunate such

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
