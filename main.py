1354d1248456c3a0efef571934d57980965ef7f7
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:38:50 +0000

    this this report pleased tool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
