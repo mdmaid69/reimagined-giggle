8993bd723315624135bbd5a6e26a3194dda70f3b
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:29:39 +0000

    account account we concentrate next

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
