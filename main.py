41b0a590f6c57bb2277cf1800bde37e0b0bb1f20
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:39:13 +0000

    underneath shout old unkind chat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import os
+def get_current_working_directory():
+        return os.getcwd()
