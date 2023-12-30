235fa78223df17f4223c0101f6b5928e49c71c06
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:38:57 +0000

    sore safe one country genuine

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
