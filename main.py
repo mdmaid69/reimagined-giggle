670752b7cad13efec224411110c2436dcf5a6141
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:27:23 +0000

    actual of rather frightened comment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  def square_number(x):
-        return x**2
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
