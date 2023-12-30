a20514a5a583cc4588705d51ff7d9ae3f2d4570c
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:31:54 +0000

    generous treat admire bringback pencil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def subtract_numbers(x, y):
-        return x - y
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
