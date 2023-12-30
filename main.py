ec1ed8105ef29b3fbbd60b3ec90d095df4a9e668
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:10:27 +0000

    relax straight easy makeup sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import sys
+def exit_program():
+        sys.exit()
