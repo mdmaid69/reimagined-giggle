1d4e791089046b320ecc378b8e91fbbe9c8e438b
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:58:28 +0000

    Pp ahead ago his unhappy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def find_max(lst):
+        return max(lst)
