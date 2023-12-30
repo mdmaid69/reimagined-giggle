edf9613b1ef65626103821efb7f83abde4931454
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:59:11 +0000

    moment share base keepfrom desperate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
