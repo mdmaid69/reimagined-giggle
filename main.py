3b2edf81a223ed382434290f23380c22c00b3727
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:33:42 +0000

    each more read definite perhaps

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
