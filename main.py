a506f5204c10fc1f97c3c0c9bdcca7da093f2a73
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:45:53 +0000

    fire difficult ask wave artificial

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
