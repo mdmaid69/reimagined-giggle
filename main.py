cd58265de426f64be2035a0e2cb78a9166ed6230
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:48:48 +0000

    set sometimes sort tie plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import json
+def convert_to_json(data):
+        return json.dumps(data)
