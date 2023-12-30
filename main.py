34c9b564721fc16773100927443623fa14f649eb
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:02:07 +0000

    gentle nice fresh month fix

--- main.py
+++ main.py
@@ -3,5 +3,6 @@
         cursor = connection.cursor()
         cursor.execute(query)
         return cursor.fetchall()
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
