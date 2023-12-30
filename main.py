7ce48d9663717363a15af3004ade37e80f0a4f53
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:37:49 +0000

    smell steer fix style well

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+print(math.pi)
