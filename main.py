96ed01354f28dacc297bff9fb705fb387505b0c0
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:03:42 +0000

    hold tear sea class soil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
