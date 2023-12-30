27d5600d43e0b3c7464f11930b676e7afc7dae68
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:43:58 +0000

    physical like total earth include

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def get_array_as_set(array):
+        return set(array)
