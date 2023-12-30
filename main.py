35abd1e726dbd1269fc3cd6e6ef2317f2fd542d6
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:35:03 +0000

    pencil pull line fashion flight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
