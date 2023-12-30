d0d549502f318763d3323ee4f09f6ac215d34a66
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:01:12 +0000

    block bank process name pleased

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
