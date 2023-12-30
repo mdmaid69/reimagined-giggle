373e31092af8404b8adf7e3d6c922708d798f66c
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:53:29 +0000

    goaway uniform search because clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
