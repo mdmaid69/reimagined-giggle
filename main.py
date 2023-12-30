55d10fe1549cf13dc6ef8e0a41f609719687d554
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:46:42 +0000

    bush think passage children reserve

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
