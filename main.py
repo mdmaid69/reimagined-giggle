906f6f83a502e40ec106ad37d61dfb7f62c606fa
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:53:10 +0000

    fault education black below lookaround

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
