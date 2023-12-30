6c623e141ba8859d212e86d8350816cb7e31bae0
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:59:00 +0000

    smell scream argue legal then

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
