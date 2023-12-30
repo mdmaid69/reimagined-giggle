b3b629129b72519216f6bbc78ea4039db0b2c051
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:44:36 +0000

    cruelty very out reply number

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def subtract_numbers(x, y):
+        return x - y
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
