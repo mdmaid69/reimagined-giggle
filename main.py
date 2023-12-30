9160fb5e36adab23d89c91cc752c6531045fea84
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:35:46 +0000

    mainly achieve offer shrink phone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def find_min(numbers):
+        return min(numbers)
