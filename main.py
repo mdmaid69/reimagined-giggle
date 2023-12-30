1ccb9db0f12399b77507f69e951b62264835e1fb
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:12:13 +0000

    window this measure takeback protest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_power(work, time):
+        return work / time
