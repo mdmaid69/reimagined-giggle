c9744f7c279e4ca8c780db593f2ba62b7a179b72
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:19:16 +0000

    godown fault bet fast 570enjoyable

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
