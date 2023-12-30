46ec67965d64b44a55d15e0a8b69ee5fa0283ce9
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:04:34 +0000

    unpleasant bread record valuable takeback

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def convert_to_octal(n):
+        return oct(n)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
