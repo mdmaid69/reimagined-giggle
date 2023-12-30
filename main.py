b8ca9fdc5778df864f1db2092fb5c68271d0bcb4
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:03:17 +0000

    breath pin computer family stare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
