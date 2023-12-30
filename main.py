b9233dfe9720791fe66f50415f409c2235ad7ca8
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:59:04 +0000

    recommend turnback exam true operate

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
