71093be9f07945a34bcd9bdedf39a9b93129869a
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:51:16 +0000

    different back roll woman file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def find_max(lst):
-        return max(lst)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
