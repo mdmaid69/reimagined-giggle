ff731909272cad7d24c87bcace5d98b463a27d43
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:43:39 +0000

    one character mind every pure

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
