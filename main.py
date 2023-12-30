3192abee547034b286db9ac023ffda30bb62ad7d
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:08:05 +0000

    cheap dig one hold sometimes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
