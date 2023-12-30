38bbade43ed38fad002363f22327fc8818eb520b
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:08:31 +0000

    family popular crowded whole cheek

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import random
+print(random.randint(0, 100))
