8c191aaf2c91fd34924935c48ac92d605071706b
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:11:21 +0000

    man 570enjoyable development doubt water

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
