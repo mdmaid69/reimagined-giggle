555393568cc4a8934e51c1d541c7ba03e00ec82b
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:30:07 +0000

    appeal off thank breathe holdup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
