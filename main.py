7b19728751eb6a68d7c22ddaaaa970c036b190b0
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:02:56 +0000

    makeinto rarely wind turnback invite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
