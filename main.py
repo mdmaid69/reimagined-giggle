3ec8070d1148bf0496a50b720b6bd9c039371eab
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:02:54 +0000

    pair there argument interview freeze

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def convert_to_binary(n):
+        return bin(n)
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
