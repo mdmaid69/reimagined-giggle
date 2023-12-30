1bd94207530e58b25410ca854bc3fc0663f5c552
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:32:47 +0000

    slowly lead tidy Dd simple

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  def is_odd(n):
+        return n % 2 != 0
