0ccb5cb198c867f1da34be8bff3fb3fa09ac3ced
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:32:18 +0000

    565end protect translate grand fine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
