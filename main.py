a572ecac0e0370e26d060857d40b0810c0891251
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:26:13 +0000

    for squeeze name our accidentally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
