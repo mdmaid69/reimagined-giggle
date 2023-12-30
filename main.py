9814ebbf0043bdf6d63b2c091033bbc7e4e2a9ae
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:38:19 +0000

    programme fire appeal character so

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
