9bd5c67a9f0507c01e18d49b0cd5b98737fe3254
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:13:33 +0000

    burst grow stand car remark

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
