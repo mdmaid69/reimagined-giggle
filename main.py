dd9cdfaacfa0dd48c10680d9fa0097e2bd8c9eff
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:50:12 +0000

    container country complaint public history

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
