ba148f0c6663bf0ac8c6163459d6c1f342b9ce45
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:09:04 +0000

    drawing report everything lonely indeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def calculate_median(arr):
+        return np.median(arr)
