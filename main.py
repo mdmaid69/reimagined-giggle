5ae0b090548c60febe31c526326a141c83475eb9
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:47:01 +0000

    language company complicated strike deal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_pressure(force, area):
-        return force / area
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
