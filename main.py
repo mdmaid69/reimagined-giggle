a56d1f7a946e47fd7397ac16956d8cc455d20bfc
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:14:27 +0000

    word less nice machine breakin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import os
-def change_working_directory(path):
-        os.chdir(path)
+print(os.getcwd())
