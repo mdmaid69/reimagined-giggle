a6e255c08fa5245023e2d43ad774aab950c36fcd
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:26:35 +0000

    mark Hh big through wild

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_speed(distance, time):
+        return distance / time
