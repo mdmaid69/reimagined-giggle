462161c235d43af6d3ce8f616c96f0f50ad08772
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:48:00 +0000

    pipe set lucky mail oil

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
