d57b20a6fac1fc1f4fe96adea514392d4dcba2cf
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:01:15 +0000

    dead front confused quickly interview

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  def add_numbers(x, y):
-        return x + y
+def find_min(numbers):
+        return min(numbers)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
