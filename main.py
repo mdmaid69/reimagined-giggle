181cc8586629c2919c2f5acd65c81ea9bdb24743
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:52:02 +0000

    flavour city strange crowded central

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_current_directory():
+        return os.getcwd()
