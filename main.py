8a07058bf3e24cb802075b2facc5263d2aeddfe4
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:46:05 +0000

    except eat tragedy deliver respect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
