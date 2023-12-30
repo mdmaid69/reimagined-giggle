39635b8d6457eae97842c372f32834ab29dfa748
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:05:08 +0000

    himself wash tip mark callat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
