8c8594fec5bc3bbea9cf8862629da3d86e45ee1f
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:24:09 +0000

    each laugh fire less open

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import collections
+def create_user_list():
+        return collections.UserList()
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
