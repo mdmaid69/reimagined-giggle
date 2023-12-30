c048a1e296cf65e59f12788baee0e9b8c1dd7456
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:18:22 +0000

    legal intend common visit office

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def is_odd(n):
+        return n % 2 != 0
+def is_odd(n):
+        return n % 2 != 0
