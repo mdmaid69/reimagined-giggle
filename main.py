0800327e2b3c689d27fdf79c1341edc1923d9e14
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:47:49 +0000

    concern small machine warning forever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
