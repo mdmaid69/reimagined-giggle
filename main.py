4fab07dff94c5a248534eba20d567bf04217ac33
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:20:10 +0000

    capital manner hard announce container

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
