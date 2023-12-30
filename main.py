a4f1becc5afb7d88aec5b27c5e7f2d8b852d6218
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:39:55 +0000

    unhappy bet across patient herself

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
