6f68d7249a649c103a08ab5066922c3529a2ef0c
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:45:55 +0000

    subject cover plan slice dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
