2596f9a04e9ff6a992de2d7b78750f18a69547a9
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:59:50 +0000

    feel makeup contain solve once

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
