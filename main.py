48768cd31022f15abc285072702984a467593534
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:02:23 +0000

    reasonable direct second off wear

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
