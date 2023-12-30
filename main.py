8134c087d6a954d546a84186d5748669fda5e84f
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:13:44 +0000

    flat share off defend attractive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import sys
-  def get_python_version():
-        return sys.version
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
