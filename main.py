2edfa0267828231624db32e095e68bf3eabf11cb
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:54:43 +0000

    wear show lookback(on) belief test

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  def subtract_numbers(x, y):
-        return x - y
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
