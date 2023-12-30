75686fad92359d5bd430c0f1d8f9dbe6a876474b
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:13:31 +0000

    bringback accordingto rob press inquire

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def convert_to_octal(n):
-        return oct(n)
-  def subtract_numbers(x, y):
-        return x - y
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
