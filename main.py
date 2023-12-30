d249ebead41a5846e234631f313f1365668787f8
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:01:51 +0000

    also grand late abroad present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
