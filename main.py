4dc9884d64c665daf1dbd4c09c53b16a38ba2613
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:27:20 +0000

    getout frightened new beautiful first

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def get_array_itemsize(array):
+        return array.itemsize
