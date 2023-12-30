b2136ef0a246be24d476f8bd49aac6634acdc4ab
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:00:12 +0000

    entry indeed project region flat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+def find_max(lst):
+        return max(lst)
