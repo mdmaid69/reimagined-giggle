7786c70626977c5214adbec96f6e1de2b264d47d
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:56:11 +0000

    pen shout principle Hh history

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
