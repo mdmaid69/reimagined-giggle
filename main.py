36bb02212d053d8298100db3e9b0cb2b69fd97b7
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:03:12 +0000

    related steep set rate address

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import tensorflow as tf
+print(tf.__version__)
