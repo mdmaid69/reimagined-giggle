08f333447a5181b7c8977a5efd747c283eb07eb1
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:23:23 +0000

    shame shadow with belong bar

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import collections
+def create_stack():
+        return collections.deque()
