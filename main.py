7feb87316b2b637cfda4ff167cc3bf69ebc184a1
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:50:55 +0000

    fall save wander really base

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_square_root(x):
+        return math.sqrt(x)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
