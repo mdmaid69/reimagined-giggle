6ac9cc3fa8f579eb02fa0c118f531bd45eb6e656
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:13:18 +0000

    forward keepoff remove trade thankyou

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+print(os.getcwd())
