bdba1294eba718a8c88efb2fec1da0d3e97ccdbf
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:31:10 +0000

    careful heavy inform custom far

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import logging
-def log_message(message):
-        logging.info(message)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def remove_duplicates(lst):
+        return list(set(lst))
+for i in range(10): print(i)
