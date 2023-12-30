61136c2fe6575a09a6695890dbdc62eeee41c2f7
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:01:53 +0000

    unite typical than turnaway play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
