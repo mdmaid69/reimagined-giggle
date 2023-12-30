bd42142f754e6bdcefda8f8a649594239c17d467
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:14:29 +0000

    past slowly 566end key nurse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
