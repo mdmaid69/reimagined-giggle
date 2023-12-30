f0b4386be93f801a9d88dbe920be5955d50ee420
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:06:40 +0000

    will round remember design blow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
