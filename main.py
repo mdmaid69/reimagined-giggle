b7e06130cb98651362010a5f60a06daa7fad21df
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:55:39 +0000

    keepaway subject push no value

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def multiply_numbers(x, y):
-        return x * y
+def add_numbers(a, b):
+        return a + b
+import math
+def calculate_floor(x):
+        return math.floor(x)
