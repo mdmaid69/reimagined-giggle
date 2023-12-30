b9c77355c95f2ad5cbae98875842c5b754eb1bfa
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:25:46 +0000

    cross tap relationship depend wire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
