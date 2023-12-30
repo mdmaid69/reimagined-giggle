de7cdbbb153d5449f7925166295af9209c2b5a98
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:39:44 +0000

    race happen state interested dick

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-def count_characters(sentence):
-        return len(sentence)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
