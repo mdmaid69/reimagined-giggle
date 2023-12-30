86b72be022d750dc9babe70776e238a36bcf49f1
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:25:29 +0000

    love praise warn price interest

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
