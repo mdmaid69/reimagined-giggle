a724754d90fe887f8658f019590c867d4c5fb976
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:18:25 +0000

    back else opportunity hospital whether

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+print("Hello, world!")
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
