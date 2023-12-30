9dafdd9c68e9c227084ade4180bb4567f64bf86d
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:23:26 +0000

    change notice dream few dig

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_array_to_string(array):
+        return array.tostring()
