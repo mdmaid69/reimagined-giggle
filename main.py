acd65f4efa4370ab5f6aad842cdaf854c377a1c7
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:49:38 +0000

    authority apart down depend tight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-import array
-def get_array_as_set(array):
-        return set(array)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
