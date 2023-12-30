d314003b0de1a0f9a01b5b7583874440313b3cd6
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:37:25 +0000

    basic congratulate which plain opinion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
