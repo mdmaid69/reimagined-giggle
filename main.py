847a0d56b99694d40524cc6b97b916d7d7333ccb
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:04:33 +0000

    odd hit think unkind office

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
