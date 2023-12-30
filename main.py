273355b5cb47e3ad93396b7e9a2765dbd1255080
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:19:52 +0000

    566end error keepfrom trade late

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
