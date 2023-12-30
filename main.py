0e24e3f06a8acd56131c1e4600f03ecb914cf551
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:34:02 +0000

    argue involve compete goon march

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
