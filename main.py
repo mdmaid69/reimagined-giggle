7f27b1a07aea8286adb10bec00c846ed3873c928
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:39:32 +0000

    cost thirsty future open bringdown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
