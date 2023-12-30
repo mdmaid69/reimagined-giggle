61129786c0185043a27283a5c3335d72c9051675
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:39:25 +0000

    problem imagine warning admit object

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-print([x**2 for x in range(10)])
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
