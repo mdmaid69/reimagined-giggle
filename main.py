2858573e9ac1aa595050f776bba01c6c83572505
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:30:49 +0000

    list lunch exit birth quarter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_array_length(array):
-        return len(array)
+def convert_array_to_bytes(array):
+        return array.tobytes()
