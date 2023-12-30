2aebf7308f670e51e52100ed8f0f6d55fa72465e
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:10:31 +0000

    remove negative so glass sell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
