b93cc5b1c4d99063bc3e194fddada657757ac955
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:29:18 +0000

    Jj bet tonight balance less

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
