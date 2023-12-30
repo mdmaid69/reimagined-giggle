9b6fd72b8ee6b148a3257387a41824fd2bc1cbb7
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:58:51 +0000

    successful tight us acceptable hard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+name = "Python"
+print("Hello,", name)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
