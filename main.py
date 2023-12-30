44924a9623fea06b2775e8458e23cde16771f3ea
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:23:17 +0000

    lesson rest communication few on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
