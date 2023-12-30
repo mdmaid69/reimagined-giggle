7ec1362511254af36800336b350388936832776f
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:48:36 +0000

    breakin yes fire goup remember

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_queue():
-        return collections.deque()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
