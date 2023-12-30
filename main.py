ba8cea87a2c5d91318c0a81cd01f91f6fce55056
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:45:46 +0000

    steam band less save moral

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
