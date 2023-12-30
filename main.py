24b0dbcf9753db14e399a1b418e84b7a1f6f4301
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:57:10 +0000

    before mine goaway door embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
