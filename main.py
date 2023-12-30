952c3aa4f1613a998e129fd321d0ca40f010eac0
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:15:50 +0000

    related cheque good refer television

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
