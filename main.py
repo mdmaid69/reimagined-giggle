d699c6a30c1d8c530e23a9ab01534d5d12684cb0
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:18:34 +0000

    about noisy corner getoff hit

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
