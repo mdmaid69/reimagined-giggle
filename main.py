67cda89da319d209234fb9beeeca7886a32d62b0
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:14:05 +0000

    reduction even suggest settle store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
