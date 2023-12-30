bfd5d1586704d2e4b465cef9a67d60a534a253d6
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:00:28 +0000

    kill seem broken open fast

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_area_rectangle(l, w):
+        return l * w
