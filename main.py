46e443514a3d5bab04cbd10655e59774a9f7a0bc
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:57:24 +0000

    spring rough tonight few cough

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
