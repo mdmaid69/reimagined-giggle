d1d89748b4061677e30e463c6b9e79edf97a293e
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:36:43 +0000

    load nice ours rain invite

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def append_to_array(array, item):
+        array.append(item)
