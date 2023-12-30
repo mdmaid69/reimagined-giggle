bbd8f62eb12d23ae0d07244a0533bf33f1569663
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:57:37 +0000

    out remark talkinto hungry priority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def divide_numbers(x, y):
-        return x / y
+import os
+print(os.getcwd())
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
