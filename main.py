5e4a316992255362706ee33a63f425dfad3fd7e0
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:04:03 +0000

    table finish defeat slightly right

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import os
-print(os.getcwd())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
