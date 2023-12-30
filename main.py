eef2b4848a9b129dc159cce92c7ec2670573248a
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:07:40 +0000

    bear bang spare represent bear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
