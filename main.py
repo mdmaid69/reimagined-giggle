7171dcdf47e03d8483579d52d8b7aa64b1c9b4e4
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:37:51 +0000

    left miss lead fine upstairs

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def count_elements(lst):
+        return len(lst)
