926aae4bffd8798dcb96e5c00cca45d344ccd349
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:56:08 +0000

    push chance check plan burst

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def count_characters(sentence):
-        return len(sentence)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
