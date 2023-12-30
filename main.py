c94809a513e949ba975036487dbaf295525db7c6
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:12:13 +0000

    bother thinkof left big fault

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
