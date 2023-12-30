39d91e7ed26a022d415bc144f11e5002bee9d866
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:09:44 +0000

    increase sharp breakinto bet centre

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import time
-print(time.time())
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
