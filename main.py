51983585abdfa46deb925a3db2f4fe3cf3a48ade
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:36:35 +0000

    pretend possibly male project argument

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import json
+def convert_to_json(data):
+        return json.dumps(data)
