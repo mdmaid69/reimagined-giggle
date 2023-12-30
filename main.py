0a73f6c09f894fd1714f8ae40ecc35749bb8a80c
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:15:55 +0000

    exam broad language wide over

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def count_elements(lst):
+        return len(lst)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
