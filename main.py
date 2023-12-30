c6b0860e8315c9ca82d1b1909d2831642abaadfe
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:56:45 +0000

    only drink cost spread university

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import sys
+def print_python_version():
+        print(sys.version)
