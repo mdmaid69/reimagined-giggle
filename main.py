badcd07952ae26624690e17a28ab9d5304af548b
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:50:56 +0000

    forward replace everybody size bath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import os
+def get_current_working_directory():
+        return os.getcwd()
