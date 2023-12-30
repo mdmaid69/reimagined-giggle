9503951ddbd443c0f55540009a1b71ca93971d7a
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:38:41 +0000

    mystery division unit much size

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 text = "Hello, world!"
 print("Is palindrome:", text == text[::-1])
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
