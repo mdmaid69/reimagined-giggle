a54b53c2c7ea9877febe616ac2d31f1816033617
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:01:49 +0000

    slow Ll strange reasonable left

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
