126c2e1a7d19da21c8305d086ac59601cfdcaea8
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:10:44 +0000

    failure breakdown total mood one

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
