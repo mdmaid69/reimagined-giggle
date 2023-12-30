7a2f6ccc4005019a58a9bfa04fe90e4b4ef436f1
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:45:32 +0000

    start smell exactly repeat town

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
