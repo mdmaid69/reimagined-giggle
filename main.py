b58ea2b6cb0b90da5a097aea6824f7dc42b2cec9
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:32:05 +0000

    trick cap before corner same

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-print(sum(range(10)))
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
