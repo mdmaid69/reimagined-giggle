cffe4f417b061018733608beec34a282d0214377
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:29:27 +0000

    war appeal forward slight contents

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def set_array_item(array, i, item):
+        array[i] = item
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
