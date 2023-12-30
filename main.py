a1427298e1137e5d789673efa254dd751c521379
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:36:40 +0000

    container scrape city keepoff division

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
