4831befadbdf0c1bc7a8d2c3f61c36906ef55e5b
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:02:40 +0000

    promise strict appearance describe impossible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def square_number(x):
+        return x**2
