591bf32b75da8dada98b1eaa9e251a141f1b5028
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:59:19 +0000

    pull bother kind something hand

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def convert_to_binary(n):
+        return bin(n)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
