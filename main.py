26db03cb850a6c6e661f1d9b9cd995c87c738a5d
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:33:56 +0000

    support breath exactly relative chin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
