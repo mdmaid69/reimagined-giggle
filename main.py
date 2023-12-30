5bf863c22730fb5bb676d7333ecc4520b978131b
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:45:08 +0000

    praise peaceful only book Ss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
