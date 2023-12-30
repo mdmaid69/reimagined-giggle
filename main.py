93c24c4f57cb77f3c9bfe6e54cc826c41333f2a2
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:04:49 +0000

    minor rid always face body

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
