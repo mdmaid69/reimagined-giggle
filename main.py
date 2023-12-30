ec7fb37b792b06e22b8d4685254d8b9017ee78ce
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:00:26 +0000

    exist Yy typical insist occasion

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-name = "Python"
-print("Hello,", name)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
