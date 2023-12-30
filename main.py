7849423bef5cd130cefd03c38066c4f185363634
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:01:29 +0000

    indeed slice hungry official pay

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def is_odd(n):
+        return n % 2 != 0
+import sys
+def exit_program():
+        sys.exit()
