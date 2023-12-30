df1382c2e9a147fe085525816eadb01cd523f5da
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:20:08 +0000

    game settle turnon talk post

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
