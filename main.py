a53ddd791e10fcdf99edbdbd5258cb5551a008e9
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:47:02 +0000

    land settle shop goback(to) strong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
