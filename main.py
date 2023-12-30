0b25e1008ffa534d8c1059f6e282d2912d20d87f
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:16:07 +0000

    make patience whichever buy deaf

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
