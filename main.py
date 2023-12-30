94a69c8a2a2ee674b9a10c341ec7aaa276b28f10
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:39:21 +0000

    immediately compare possible means fierce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
