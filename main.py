b1ab6be76bd9d637c6ab1ad618fb81e2483d4548
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:03:30 +0000

    she mistake go ought climb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
