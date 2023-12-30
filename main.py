9fff17a309b9311b80f126f03e0d1e91a009c92f
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:38:14 +0000

    badly shop post left talk

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
