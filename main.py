2695e309fadbe3a1877a6cee83edf62abfb4c2ed
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:20:55 +0000

    vote exercise nowhere transparent make

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def print_python_version():
-        return sys.version
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def calculate_power(work, time):
+        return work / time
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
