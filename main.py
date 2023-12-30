c537c52d7649f1efb673277bdd247780767df7ac
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:42:26 +0000

    last shoulder keepoff collect may

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
