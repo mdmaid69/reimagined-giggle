474b4310083d8e7eb9d7c38a084c9bbde25dfe60
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:40:10 +0000

    feature might comment near mess

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
