32f37ecd5b43750a23df8b34d44f683d8a963aea
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:07:40 +0000

    under on school protest season

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
