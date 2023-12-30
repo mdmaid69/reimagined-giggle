7c624dc09b2f37782f494594a9a0ed0d0185974b
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:22:26 +0000

    against wish contrast future want

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
