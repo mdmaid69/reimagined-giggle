dcf9ada367b0e1df2d1edc65c7f3f2141a4749a0
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:52:49 +0000

    out variety deal language many

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
