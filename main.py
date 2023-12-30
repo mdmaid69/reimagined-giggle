9c553979b9508712bcb4baec642a5639f120d6fa
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:31:57 +0000

    become dry nurse thinkthrough travel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def delete_file(file_name):
+        os.remove(file_name)
