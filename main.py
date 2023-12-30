182d7b05a5b70a94fe037feb7744953bfd889621
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:29:37 +0000

    equal possession be fun embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def get_array_length(array):
-        return len(array)
-def calculate_pressure(force, area):
-        return force / area
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
