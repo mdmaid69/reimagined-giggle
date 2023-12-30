f0719c743c23793a906e01ae11bac7d0da475aaf
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:02:18 +0000

    best present wake either free

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+print([x**2 for x in range(10)])
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
