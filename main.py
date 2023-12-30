d007e99b5db238451fb2dd32d735d80cef70adbe
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:25:18 +0000

    flavour still temperature young rush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
