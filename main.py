d712ca964eebd206b889a77ac2d691730b622052
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:09:21 +0000

    myself note worth judge none

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def convert_to_octal(n):
+        return oct(n)
+import collections
+def create_priority_queue():
+        return collections.deque()
