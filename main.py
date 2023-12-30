aae489a7554e7e65e800c9fae4bd231a02f3472c
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:59:33 +0000

    lean press guess swim uncertain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
