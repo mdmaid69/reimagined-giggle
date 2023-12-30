0bbbe1e0e2b9fa0171613f28ea6081e483eaf098
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:33:09 +0000

    straight sensitive cry ride comeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
