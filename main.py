b9df6aee465ee450fe9e83df46f363856d0b9c32
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:37:12 +0000

    control book work permission respect

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
