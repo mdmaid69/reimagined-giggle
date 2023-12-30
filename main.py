457b2e16cd8ef8a24e8ee956b184beecfde08b8f
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:33:24 +0000

    sell principle free such belief

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-print(sum(range(10)))
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def append_to_array(array, item):
+        array.append(item)
