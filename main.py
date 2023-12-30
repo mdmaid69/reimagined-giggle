c6906f6146b1328ba1c4128529404592e7e19678
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:26:01 +0000

    guess challenge butter country position

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
