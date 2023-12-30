fe99074409d65468de7c71f8046db62d47d55eb2
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:34:56 +0000

    duty check learn none return

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
