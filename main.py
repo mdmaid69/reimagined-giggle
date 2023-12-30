53c313bd94ed78ad2d5c2e761129528e512ad120
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:57:22 +0000

    every variety flavour marry whisper

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def is_even(n):
+        return n % 2 == 0
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
