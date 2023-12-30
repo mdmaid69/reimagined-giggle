f5844a6403fcc387088f540acce34b4264be0bea
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:33:58 +0000

    spring beat physical in friend

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  def add_numbers(x, y):
-        return x + y
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
