82b334454eb9f0bf6056d593b61350cc5caa21d2
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:59:02 +0000

    takeout church age for second

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
