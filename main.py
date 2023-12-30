3acb7fbcbb1838cde4d85a9483f3096aa997d100
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:37:25 +0000

    turn goout method programme violence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_speed(distance, time):
-        return distance / time
+  def sort_list(lst):
+        return sorted(lst)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
