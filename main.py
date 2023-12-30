14734b3b0aa63f6b79a931d448b9febd1e98e475
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:28:55 +0000

    formal corner turnback even see

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import array
+def get_array_as_list(array):
+        return list(array)
