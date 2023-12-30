1c7094b9035314942bd09954186f5bd353d906d8
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:32:36 +0000

    tired relation light pin wander

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import tensorflow as tf
+print(tf.__version__)
