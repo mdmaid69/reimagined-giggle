8c774a8abd1535e57d56d75449addd4dee1191bb
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:48:35 +0000

    slightly bridge store generous reason

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import random
+print(random.randint(0, 100))
+def calculate_speed(distance, time):
+        return distance / time
