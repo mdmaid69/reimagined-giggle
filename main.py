9653c3fe00005d814a673577d25a6a2183b4beca
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:26:43 +0000

    estimate exam sudden send burn

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_force(mass, acceleration):
+        return mass * acceleration
