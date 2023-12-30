bcdb280ac2541e119436e89e98a84b269dc71a62
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:09:23 +0000

    beginning sentence gradually film hot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def is_odd(n):
-        return n % 2 != 0
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
