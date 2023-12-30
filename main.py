1e387a91db8bee5a315970ba0a8ec922c018f2c1
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:09:06 +0000

    star quit train display fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
