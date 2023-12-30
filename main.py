c3bf6036d217297e7d2440a381d08dd2b3edd0cf
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:20:49 +0000

    delight entrance win bill yet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_density(mass, volume):
+        return mass / volume
