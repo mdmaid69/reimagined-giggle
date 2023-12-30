d08785a45a6390127deaeff8e1510870fdb73f21
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:42:37 +0000

    shop Itself polite coffee tear

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+for i in range(5):
+        print(i)
