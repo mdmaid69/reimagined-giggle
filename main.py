7f7d47fa1f697453fea15c1c590d44f8ae517adc
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:29:10 +0000

    interested indeed hollow fault 566end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
