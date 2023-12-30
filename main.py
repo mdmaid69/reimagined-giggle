54b9d3164a42f55c437cd85acb455e3f2cb66843
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:36:24 +0000

    interruption worst round but getback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  def convert_to_binary(n):
+        return bin(n)
