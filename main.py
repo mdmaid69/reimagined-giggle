b2dc25b7eb464219966d6ee5085cf285aa702b29
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:41:19 +0000

    affect underneath show convinced stone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def add_numbers(x, y):
+        return x + y
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
