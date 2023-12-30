3ecefad8e1fff79247a4c020cc90fa216e5b3af9
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:57:43 +0000

    definite music increase secret conclusion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+name = "Python"
+print("Hello,", name)
