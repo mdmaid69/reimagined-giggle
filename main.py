0ad47b7cacb8eec9037d337629f33424893da145
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:43:07 +0000

    active bringforward out apply key

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-name = "Python"
-print("Hello,", name)
+import sys
+def print_python_version():
+        print(sys.version)
+  def add_numbers(x, y):
+        return x + y
