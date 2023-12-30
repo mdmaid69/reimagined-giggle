e3b4ea2d8214f82ed32980d0e30b64501106ec59
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:17:05 +0000

    cum Ll arrival sound hour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
