147dd348dfde5881f4ff0baa1311d747f385ab74
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:56:04 +0000

    bus state listenfor advantage name

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
