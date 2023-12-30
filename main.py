58a19fe123df637f8506e0ba8dbc61229ee8b8e4
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:44:34 +0000

    steal mend curious wash fine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+def count_words(sentence):
+        return len(sentence.split())
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
