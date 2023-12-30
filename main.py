c27e9dfacee4f60c557347dee01609afaf67cd7a
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:14:51 +0000

    not advanced mine side far

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import numpy as np
+print(np.array([1, 2, 3]))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
