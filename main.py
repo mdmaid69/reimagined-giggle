8f4a33b3421b8bcc5382f792e5ab98632201ffef
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:54:33 +0000

    holiday surprised object some community

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import time
+print(time.time())
