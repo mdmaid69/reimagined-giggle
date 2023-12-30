4ed2f45256ef2776d4d4327664054c04fc29d67e
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:10:10 +0000

    miss clean pressure body ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def remove_duplicates(lst):
+        return list(set(lst))
