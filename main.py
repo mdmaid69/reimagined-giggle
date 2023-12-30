c38ca3ea98db6000a5da39273c015e5f35a5c5cd
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:50:46 +0000

    bath people tall bringback police

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
