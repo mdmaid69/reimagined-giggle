d90d41531da1fc4ef811cc4311c712b7d8c3fac0
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:47:16 +0000

    contrast bend home slice blame

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
