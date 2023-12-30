deff8c0013cdcd5418bdc1463fe9571b333a3e1e
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:47:15 +0000

    plenty bottom snow depend convenient

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
