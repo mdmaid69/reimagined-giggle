599b99ba749abb63ce0b4e75c79c41bc13fe1e78
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:46:23 +0000

    accidental nigga building rule them

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
