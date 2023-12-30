1099e42c660538663bfe191db5f43c88ebb5a982
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:49:47 +0000

    sticky slice tool very in

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def subtract_numbers(x, y):
-        return x - y
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+name = "Python"
+print("Hello,", name)
