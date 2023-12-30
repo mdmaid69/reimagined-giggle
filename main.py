1563b577b66a633cb2a0ac890adc5645259b48a3
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:35:02 +0000

    destroy group song hospital right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
