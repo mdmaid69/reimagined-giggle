7a5a8b916200348274aece730c854964e6d22c2d
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:44:15 +0000

    rough history turnout order something

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
 import array
-def get_array_length(array):
-        return len(array)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def get_string_from_array(array):
+        return array.tobytes()
