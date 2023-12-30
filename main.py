8d629f2707b89b7f36642161d3f08cbaa4b341de
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:02:49 +0000

    total reduction record authority emphasize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
