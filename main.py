9e22028ca06834d8d61dc977c541a66688c6603b
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:22:35 +0000

    juice apply waste result father

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import math
+print(math.pi)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
