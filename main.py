89abdcdcf2931f9d07a38bea890822abab53a03e
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:28:53 +0000

    book opposite principle take understand

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
