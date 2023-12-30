0676ba0fdea094a9a8850f2f97f5ab385a2d9056
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:24:37 +0000

    patient keepout goodbye repeat material

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
