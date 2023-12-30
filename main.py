f0fe9ae2371b88d3fa189eac45463558443fe495
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:28:26 +0000

    warn excellent complain lay moral

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+print([x**2 for x in range(10)])
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
