74226c09e05ec6059048c5d9c158a130f52ab2f9
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:58:18 +0000

    summer favour compare confuse save

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def calculate_area_circle(r):
+        return 3.14 * r**2
