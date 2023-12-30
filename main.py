d9cc43bb961c3b16ba06f066838e7c56d389d340
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:52:34 +0000

    genuine calmly I job rely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_work(force, distance):
+        return force * distance
