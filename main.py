dee7611ae730407e58b35e3f1cf8ef7151704c72
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:18:15 +0000

    imagine communicate familiar juice happen

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
