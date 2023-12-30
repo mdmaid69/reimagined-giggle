f3705331ce4bb01b52f7a86d783bd277417ec16b
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:29:49 +0000

    distance surface couple in miss

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
