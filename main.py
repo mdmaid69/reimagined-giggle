c279f1ce55f8e893ca17b2b3d9058bd652df5556
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:40:54 +0000

    Ll away sea pay evening

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
