39ef893f967e98c4d295c8ace5c1b376159b5cce
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:31:19 +0000

    interrupt coat space season great

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
