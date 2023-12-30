cc78708fecae321eeaf74c6ce13dba5b4cf73064
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:49:34 +0000

    welcome wash feel consist lucky

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def calculate_volume(length, width, height):
+        return length * width * height
