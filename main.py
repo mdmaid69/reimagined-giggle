bd7fd723e794d01aae5fb1daf4e7d0e7b46cf895
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:28:56 +0000

    our catch foot please used

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
