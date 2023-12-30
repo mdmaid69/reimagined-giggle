d0cf8c6fc7e66a753f2cb1208d142090c033912b
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:19:37 +0000

    Qq collection drawing surprised lower

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  def convert_to_octal(n):
-        return oct(n)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
