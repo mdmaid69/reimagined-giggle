814fed5d334ed9e580b66c538c05fb88c3251dd6
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:01:16 +0000

    step press she herself sorry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
