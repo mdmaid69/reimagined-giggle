d8c3f2ab867529a752a29e642ff3390a3eb89777
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:13:44 +0000

    future zero maintain height collect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import time
+def get_current_time():
+        return time.time()
