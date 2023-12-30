46a6a9226a5e9342f2966c2c66dfefea19c11a4b
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:50:21 +0000

    equal remark computer heart cut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def find_max(numbers):
+        return max(numbers)
