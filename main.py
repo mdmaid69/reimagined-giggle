1d8f43972f67cfd45d80b97012beb80ad92fd52f
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:16:00 +0000

    actually purpose earth lay design

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
