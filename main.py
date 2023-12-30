170f057b023be2fa0efe36a485cc68635cc6fa81
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:40:26 +0000

    satisfied keepout mean genuine thinkover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
