0b3e1afb5aa9bd34d1b0ebbca9fc428f89b55726
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:32:53 +0000

    nurse back off mystery thing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
