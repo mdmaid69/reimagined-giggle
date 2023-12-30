b39b173e38d9fbaf6ccc8f9831c307b4e10e8053
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:33:28 +0000

    public not burst dick health

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
