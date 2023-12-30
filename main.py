121de45df392ee39e559aee4600ce7b99f3aa88b
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:52:43 +0000

    survive summer small reasonable university

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def divide_numbers(x, y):
+        return x / y
