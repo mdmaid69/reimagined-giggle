f59ffcd9536b0b38f0a13b391329cbd727a9e1be
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:22:12 +0000

    risk murder average which fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_string_from_array(array):
+        return array.tobytes()
