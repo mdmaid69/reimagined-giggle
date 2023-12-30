cacb29179dd2b8f58d8955c5038e7f957e63a530
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:14:38 +0000

    declaration measure on fat chase

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
