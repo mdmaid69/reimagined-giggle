665c74c7c030fbcc89e877bae98665b1e277d274
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:20:40 +0000

    secret within work available scream

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def find_max(lst):
+        return max(lst)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
