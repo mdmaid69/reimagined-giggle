ba228efdcff60d77cc8ab7bbf30d6b103684300f
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:54:14 +0000

    usually bus difficult deep able

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
