f9e4cd8027e9cb585926d9e85d541f61c848108d
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:59:25 +0000

    slice expression apologizeapologise crash takeout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
