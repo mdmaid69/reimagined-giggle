dd3913198a2710a9afa276cf5f17d222288995e0
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:29:12 +0000

    repeat ordinary back him above

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
