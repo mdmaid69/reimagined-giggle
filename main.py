ef8c90a6245d2d7ec4ee1a82b18cc5c75527637b
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:44:47 +0000

    admit subject store Cc mad

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def count_words(sentence):
+        return len(sentence.split())
