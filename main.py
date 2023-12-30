c2ff0f80bc8b184587360fc572b458ca3d2c2b82
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:22:01 +0000

    reduce copy dream path short

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import getpass
-def get_username():
-        return getpass.getuser()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
