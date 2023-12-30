6f8faa5617ae17ebf99bfece20589e75c86c4d65
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:06:44 +0000

    touch talkinto develop positive situation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
