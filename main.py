52ac9623512efd3b9db70224a129c3aaf6343084
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:05:35 +0000

    up impatient reply easy difference

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def convert_to_binary(n):
-        return bin(n)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_int(array):
+        return int(array[0])
