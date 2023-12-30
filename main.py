af8eb7efc3ff4f782693c5d27c5932558748107e
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:21:17 +0000

    brain rank remain shade characteristic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
