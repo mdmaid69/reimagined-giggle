458cb6477e7e6351e473bb7b0a095130618f8e29
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:56:17 +0000

    introduce important holdout detail related

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
