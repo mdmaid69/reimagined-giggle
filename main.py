1bb740158f33b14ab173e0dbc532622ad1cb7c46
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:03:51 +0000

    time important notice my under

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
