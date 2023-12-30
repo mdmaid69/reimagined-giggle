4db7db5e264a71aaf49362830ab07815a9b9f956
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:48:15 +0000

    calloff morning shut turnup drawing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
