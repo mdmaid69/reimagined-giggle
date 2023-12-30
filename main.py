62b52e115792d318285f958acbc194a3240610d3
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:23:43 +0000

    size fork glad fight that

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_int(array):
+        return int(array[0])
