4d7839147a4c4c0af86fa7c84160dccf0373cd02
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:56:06 +0000

    Yy progress manner after dull

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_density(mass, volume):
-        return mass / volume
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def count_characters(sentence):
+        return len(sentence)
