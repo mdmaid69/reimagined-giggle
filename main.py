5a19950c6f13b4dece63d944c2af95dec19db16e
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:09:50 +0000

    commit otherwise flight plenty cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def convert_to_octal(n):
+        return oct(n)
