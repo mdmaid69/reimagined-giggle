a7c0dd18fcccd2e3c2aa0e16217b18d3a578a934
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:52:50 +0000

    grand transparent ability recover room

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
