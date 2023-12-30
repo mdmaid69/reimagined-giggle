4b9a85fa22ce9506e815cd26e7608897a91b7fb6
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:56:45 +0000

    cost regional part post environment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  def is_even(n):
+        return n % 2 == 0
