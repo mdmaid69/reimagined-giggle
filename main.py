f6f002569e8a6b243e626b4edbf19bf12532b0b3
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:38:36 +0000

    pin contact wave his by

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def multiply_numbers(x, y):
-        return x * y
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def get_array_as_str(array):
+        return str(array)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
