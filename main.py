f0748c61f460a1e61b2f33f5e278d1414fe3bda3
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:45:16 +0000

    raw unlucky scared consult cope

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
