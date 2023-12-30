842d4ce41e772f24538d60c68441a0b18234926e
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:19:30 +0000

    sail thorough OKorokay challenge discovery

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import sys
+print(sys.version)
