6ee3e7668a461537d41c205c3e8d92045003f347
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:24:58 +0000

    level closed nonsense stage total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def reverse_list(lst):
+        return lst[::-1]
