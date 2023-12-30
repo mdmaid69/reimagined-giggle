7ace4b181c2d9cad5f0c268f7f365852e9bfb7cd
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:13:58 +0000

    state teach cover separate clear

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
