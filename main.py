9b72e63129dacd3972ae085cf1316bf201f1b7ba
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:11:55 +0000

    test part stranger bar often

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  def cube_number(x):
+        return x**3
