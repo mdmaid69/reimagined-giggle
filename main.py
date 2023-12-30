77fe217bc271162c4bca9b2422f803be21d805e4
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:31:36 +0000

    hard rather main there before

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import random
+def roll_die():
+        return random.randint(1, 6)
+def find_min(lst):
+        return min(lst)
