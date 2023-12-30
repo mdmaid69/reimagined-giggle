0afcc70283c851b1e948814a6e0225d0f584eac2
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:42:52 +0000

    strength alive mind trial fresh

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
