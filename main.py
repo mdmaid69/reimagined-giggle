393dd1338ed3b6fc100c14386625d1b33828b452
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:00:23 +0000

    smooth mark attractive season dance

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+print("Hello, world!")
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
