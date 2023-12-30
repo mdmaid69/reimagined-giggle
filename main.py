aec0997e4a80c8ca9072ad1b742293c9b6be2879
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:29:45 +0000

    string early achievement board furniture

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
