542c7e03978286672719695d3f57377c51440341
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:46:22 +0000

    move within conduct near dead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def square_number(x):
+        return x**2
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
