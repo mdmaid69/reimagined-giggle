80f1a1ecf82e1ad729eba6ac5e12d40efcef719e
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:57:10 +0000

    deserve drop failure last middle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_area_circle(r):
+        return 3.14 * r**2
