98a6cfabf47cf0e7fd4496c5e37077b8530d5c78
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:32:47 +0000

    watch score rob recently tune

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
