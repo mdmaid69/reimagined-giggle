e2d8580cb54f3347e78ecef8ac1534653e4e87e0
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:26:02 +0000

    scene nor colour expensive belief

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def calculate_pressure(force, area):
-        return force / area
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
