ef67d64e7afef3cf355d0c48a25ba1dcac1e27c4
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:30:04 +0000

    treat blame anxious sort frightened

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_length(array):
+        return len(array)
+def find_unique_words(sentence):
+        return set(sentence.split())
