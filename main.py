fba6ebd8b0d12e01cf4d7b7e104138849ac4ede7
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:02:00 +0000

    wind worry passenger keepaway boil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
