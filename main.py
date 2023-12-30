3f2e393908cea71fe37967688ac3d595a58f79ea
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:32:07 +0000

    all demonstrate pronounce fork block

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_float(array):
+        return float(array[0])
