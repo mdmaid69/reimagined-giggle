2cb6aaa5dc7cd1f1c7dbc9e916dc8a4b73ce12b6
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:47:48 +0000

    tired determined yours wide height

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
