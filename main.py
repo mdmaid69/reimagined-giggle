400f5fb658b237906b7b14e301ddd945929aa773
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:55:44 +0000

    tidy comment comfortable design Nn

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def count_elements(lst):
+        return len(lst)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
