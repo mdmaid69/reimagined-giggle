841335b3cadcad26cfdd16b39e3102c69db880ce
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:38:22 +0000

    once above what bank thin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def square_number(x):
-        return x**2
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
