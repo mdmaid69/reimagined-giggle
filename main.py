5125f66276dce8a151de08deb8edfe287824a029
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:32:11 +0000

    rely comeup dull friend challenge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
+import re
+def find_pattern(pattern, string):
         return re.findall(pattern, string)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def convert_to_octal(n):
+        return oct(n)
