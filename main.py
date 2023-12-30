af74762232abe80c998777bac43230fb021b1221
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:12:46 +0000

    find catch either estimate worried

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import sys
+  def get_python_version():
+        return sys.version
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
