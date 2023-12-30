8c093dc46f84cc3c37ffb963c17a56381ad1e859
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:27:37 +0000

    immediately hurt pick proper none

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_work(force, distance):
+        return force * distance
