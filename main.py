c744e0d2dd00bb82b828b41062567622f395b6df
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:20:33 +0000

    trust uponon practical neither guess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+x = 10
+y = 20
+print("Sum:", x + y)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
