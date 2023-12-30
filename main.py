23b7b6df665029f7befafca9a395e75157b4d7ff
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:08:10 +0000

    can talkback taste cry improve

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+print(os.getcwd())
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
