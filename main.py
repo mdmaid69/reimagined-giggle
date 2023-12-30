e991cc76c80e52ff073a47c8bc4744a34769ef05
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:04:57 +0000

    stick complicated happy scream amount

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import sys
+  def get_python_version():
+        return sys.version
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
