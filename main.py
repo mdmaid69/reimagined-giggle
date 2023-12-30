ca6da1c76f249f04f4e7fc56b9685908d9f8effd
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:40:20 +0000

    it through fork early interrupt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
