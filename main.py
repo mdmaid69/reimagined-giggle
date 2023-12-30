dae82a2b91ad69462b3b3e9b0b3cdf7c3c4a8c81
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:59:40 +0000

    return closely you small get

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_power(work, time):
+        return work / time
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
