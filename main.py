715d52b7aff310a59d25f7bdb667e2fa45c44e9e
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:20:47 +0000

    ago state mysterious include spoil

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_current_directory():
+        return os.getcwd()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
