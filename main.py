2e09a0f52f1505635d403553cbca4ad327720bcd
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:36:40 +0000

    sky cold certainly risk mine

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
