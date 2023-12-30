742ed6000e1711ef0e48fee1b09c3b53a9ba2b37
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:31:20 +0000

    police foot heavy there for

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
