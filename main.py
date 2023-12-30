0c881972228d1470d999813287611d78470afcf8
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:00:15 +0000

    undo guilty apologizeapologise afterwards only

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
