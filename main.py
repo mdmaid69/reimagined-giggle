86e82b6c8856c27297d1fd4125e7a9aaca3449d2
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:11:19 +0000

    many tidy explain suppose search

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_error_function(x):
+        return math.erf(x)
