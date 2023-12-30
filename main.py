74e3fc5d0611350ccd91a0bbb89912bf7695b836
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:46:04 +0000

    nor check allow rich city

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_acceleration(speed, time):
+        return speed / time
