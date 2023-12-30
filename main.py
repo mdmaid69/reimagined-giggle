5985a23548102b32e530b0f526ea22fa8fc5fed4
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:43:07 +0000

    opponent politics the Uu room

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sys
+def exit_program():
+        sys.exit()
