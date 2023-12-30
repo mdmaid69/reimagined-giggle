a98dd41c24e93e5989a8c5bef489d2548e282cc7
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:10:38 +0000

    interested minority pleasant afford punish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def convert_to_hex(n):
+        return hex(n)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
