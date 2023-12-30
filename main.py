ccf4c6d37cac08851a1b8d6c0e2bf34120fe45f0
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:18:46 +0000

    brave company stir strength command

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
