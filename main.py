e75ee61fbc1c9c7f49ca5867f73337b40fd44ee8
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:21:23 +0000

    wheel thing actual holdup dance

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
