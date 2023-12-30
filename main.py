387f84e95cc4ac65d5c5c7b0b772da4f2a7cf5e5
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:32:36 +0000

    by shock substance set smart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
