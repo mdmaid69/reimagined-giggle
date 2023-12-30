bfc831e7851de3e90ac74a18cb31e1bf68daab61
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:53:48 +0000

    perfect thought responsible score independent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import numpy as np
+print(np.array([1, 2, 3]))
