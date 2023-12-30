bc8c7d671ffd352c1a0a4a701a2f70cbb89f0728
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:08:15 +0000

    flat bet rain heavy waste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
