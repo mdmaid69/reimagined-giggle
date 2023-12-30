629b537a17c37202f6fd63ab8d7a913e3cbace6c
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:24:22 +0000

    move protest no performance hollow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
