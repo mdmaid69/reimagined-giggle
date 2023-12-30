a12050664e784b481369846be4420985c85ab40b
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:05:12 +0000

    scream exploration workout depend slow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_power(work, time):
-        return work / time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
