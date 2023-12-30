8e33ac7512a1cdef8e8fcc1213d126f1961981e4
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:16:14 +0000

    smart mind sail confident mistake

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
