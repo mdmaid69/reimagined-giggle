9dc0b8a70b1692d39af03b31833563038943acd6
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:14:39 +0000

    callback head sharp mass ought

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
