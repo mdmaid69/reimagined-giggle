9b954baaf938107223e2a6c221b23db269fa7749
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:47:11 +0000

    fall whatever shallow Bb compete

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_current_directory():
+        return os.getcwd()
