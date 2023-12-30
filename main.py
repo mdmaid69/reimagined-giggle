711f58ec3ebd2a04f66d0d3616ab2d993fa95f78
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:16:32 +0000

    workon almost aim public country

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import math
+print(math.pi)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
