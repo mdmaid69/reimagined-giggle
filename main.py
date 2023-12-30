ca73f9aa2bd71e3f49ff9685c20609c4caa62df6
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:26:24 +0000

    light fence stupid mention violence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def convert_to_hex(n):
+        return hex(n)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
