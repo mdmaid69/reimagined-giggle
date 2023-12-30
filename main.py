ef68f8c0ee01a86652a300c82cc4c4f8d4055f68
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:10:48 +0000

    trial row runinto learn crowd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
