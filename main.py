7bc24d35004f6cca43e8028e7addacfb7e9d1307
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:10:24 +0000

    sudden turnaway Tt until equipment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
