7a9631424cc38d601d3469d4c50935ef68dffb11
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:08:00 +0000

    all mad thing visit advice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def append_to_array(array, item):
-        array.append(item)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
