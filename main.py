7f20207898decd3ae6c4837080a8e80eb9dba1ee
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:54:07 +0000

    coat ordinary replace contact night

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
