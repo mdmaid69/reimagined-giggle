1af13f26a787963a4e866dcad2372b0eeaf8397a
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:28:44 +0000

    transport start introduction worry oil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def convert_to_hex(n):
-        return hex(n)
+  def reverse_list(lst):
+        return lst[::-1]
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
