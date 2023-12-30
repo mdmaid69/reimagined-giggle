cd82217fbfe684645f658e07354fd114ca95a01a
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:32:52 +0000

    worth fine nothing colour nothing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
