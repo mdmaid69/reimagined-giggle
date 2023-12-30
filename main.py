4aead6a9a8f18f02ea98640aa41a3d7ec0df11a2
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:55:25 +0000

    maintain circumstance matter operate close

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
