76f3f1c10e2148e7bf8621829688c2cff2e078a9
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:53:24 +0000

    purpose generally poor closely agree

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
