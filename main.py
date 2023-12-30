5192eb9316f9dd8d6778c80025c05be1cebac92e
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:09:27 +0000

    whether afternoon simple home sister

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
