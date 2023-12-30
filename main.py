f2febe5fecf25125a9376096d99a5e454d99da89
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:12:32 +0000

    visit still boil blind develop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
