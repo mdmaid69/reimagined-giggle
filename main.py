ea14c11c52847be6a944bed9660fc4ad987108a4
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:40:45 +0000

    ordinary swing love breakthrough row

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
