912d24bd40c2ccbea47144b33122089ae60a1b13
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:22:52 +0000

    thumb platform phone rush figure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
