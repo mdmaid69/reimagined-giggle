9eeb5ec4bcb3eaaa308352f49d729e427f3df678
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:21:39 +0000

    consult alive finger lookback(on) measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
