46ca85586fa5f771da4997ba6dbab41ebd002129
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:07:06 +0000

    declare complaint anything unkind your

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def subtract_numbers(x, y):
-        return x - y
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
