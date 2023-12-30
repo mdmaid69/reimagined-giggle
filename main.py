c0808d0905c1fbd869f39041bf51943792a547c3
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:32:38 +0000

    excited regard illness bottom adjective

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
