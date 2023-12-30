3e46fbb864fa2bbeb7cca6fe19ede2ac7da4b3e5
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:09:13 +0000

    drop guess walk congratulate spin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_power(work, time):
-        return work / time
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
