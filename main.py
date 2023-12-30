f75d08a1a80fe29305dc70fa0e75a8ca8de09a39
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:22:53 +0000

    lean achieve money bye permission

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def reverse_string(s):
+        return s[::-1]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
