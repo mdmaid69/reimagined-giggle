e90200925285cca82248a009d56a4c5b778048e5
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:46:31 +0000

    tour give organize callat branch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
