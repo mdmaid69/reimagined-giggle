97217f8b9f99c3c156c23f05d48553e41deb4f0f
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:41:31 +0000

    reject protection complaint Jj relation

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def get_bytes_from_array(array):
+        return array.tobytes()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
