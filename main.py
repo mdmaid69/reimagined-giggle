a3bed613ae70c8447de30e8a990497f3ac5b56c0
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:32:37 +0000

    tour otherwise world claim equipment

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
