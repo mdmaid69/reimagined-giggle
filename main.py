06c52ab62ce767ca2e0bf2637edc686afb706e5c
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:56:02 +0000

    walk advanced quantity black say

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def add_numbers(x, y):
+        return x + y
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
