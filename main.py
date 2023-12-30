cc794ba01ccdaa37aa68af7a0388350ae3b01999
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:03:34 +0000

    connected company exactly view appeal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
