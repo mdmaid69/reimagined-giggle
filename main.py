bb90f1d8088c1a7ba447cc33f520a29c9507f72d
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:01:09 +0000

    outside ill service store house

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import collections
+def create_user_string():
+        return collections.UserString()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
