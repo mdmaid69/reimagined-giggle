9bb4df87f60a041fc9db643d22d2fb22076dfb73
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:02:43 +0000

    profit pleasant claim fire than

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def find_max(lst):
+        return max(lst)
