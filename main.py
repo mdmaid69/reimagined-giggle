25f84ec07f328dc83d8767ed7f48f26986d18863
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:10:51 +0000

    backwards measure phone lose whether

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
