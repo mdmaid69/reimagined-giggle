7fbcc123c489e9d2269b678862c04fdb1588c556
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:52:09 +0000

    doubt shame autumn judge song

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def greet(name):
-        print(f"Hello, {name}!")
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
