c1111af4a483a16e24695da3bc8566bae9f8ca9e
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:44:09 +0000

    mind passage say strength curious

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+print(sum(range(10)))
