0718fe115c39e77fa4a03aaba4ec32ffd6f0b8eb
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:38:53 +0000

    opposite talkback increase estimate twist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def greet(name):
+        print(f"Hello, {name}!")
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
