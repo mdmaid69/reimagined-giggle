8145cd1c9d770e94c067ce0f5ed7dfa556c8a17a
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:47:00 +0000

    smoke spare pair clean scrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
