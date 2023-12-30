54658095878493a2b1d1bc81a4e7ed7138afa659
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:01:53 +0000

    group wise strict conduct wind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_as_list(array):
+        return list(array)
+print([x**2 for x in range(10)])
