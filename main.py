4c2b7f88ed7ba6b2a18c00ec5639d22fe5baa449
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:19:55 +0000

    plant interest exciting help score

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
