d40f9f6075b06b9a432796590828f18f7c099dd7
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:34:21 +0000

    almost plain morning achieve payment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
