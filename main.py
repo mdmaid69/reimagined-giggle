4d5b9201a31182c0d3bb257370c4731b6d6b92c6
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:14:25 +0000

    group slowly meaning advise by

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
