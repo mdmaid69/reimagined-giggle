bb2af730e395fb7a6294b23515fab55a9392d5b6
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:35:07 +0000

    warn 570enjoyable workout table 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
