2c52a6f370872a59af2e925761c87e34e61c454a
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:45:38 +0000

    well attempt shut radio splash

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def multiply_numbers(x, y):
-        return x * y
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
