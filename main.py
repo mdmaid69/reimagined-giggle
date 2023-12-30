6e7f6886d5292b5d91fde0bca24f87c82dfe4ae6
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:37:41 +0000

    all fairly ahead clearly commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_circle_area(radius):
+        return math.pi * radius**2
