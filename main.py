8f8c6821142a908890e1297dbdfc8c1a18bbb440
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:07:17 +0000

    extremely sure say class kick

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+print([x**2 for x in range(10)])
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
