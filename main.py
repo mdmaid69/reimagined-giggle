90d42fe90021219378134b99234203a87e1f7654
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:24:33 +0000

    callback band interruption power table

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+x = 10
+y = 20
+print("Sum:", x + y)
+import sys
+def exit_program():
+        sys.exit()
