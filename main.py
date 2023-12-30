8131154c557467e811ac23c8d95088310b91e570
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:16:53 +0000

    result join everybody since brother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
