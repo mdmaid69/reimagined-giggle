f4674109ce66bf2cea99548416bb33bec04cb60a
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:31:36 +0000

    lay body certainly person wild

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def get_array_as_list(array):
+        return list(array)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
