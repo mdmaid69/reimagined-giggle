a283ad988dcdc073c081ec4986b69235c4cc4434
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:41:33 +0000

    week level purpose arrive takeout

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
