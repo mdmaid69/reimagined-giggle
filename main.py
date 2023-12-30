4f5e65e7c191f4c3007da15318cea36686eebb46
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:12:43 +0000

    feature attention every runinto distant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def square_number(x):
-        return x**2
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import logging
+def log_message(message):
+        logging.info(message)
