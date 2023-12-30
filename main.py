f5ac272261bf74c681b6fd7a4e8dfaa181066d88
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:38:50 +0000

    phone confidence reasonable part reject

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
