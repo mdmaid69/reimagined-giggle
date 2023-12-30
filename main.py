5247e3571f19f8ce16437c8fa1c0a2a66a20d6fb
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:44:56 +0000

    problem normal report as friend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
