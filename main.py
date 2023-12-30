48acbc43c3c9b60f6ba66b67603d9b5c8ba284d3
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:59:10 +0000

    belief suppose understand appearance understand

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
