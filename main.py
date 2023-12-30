1d79494a9e9b1dd8232cca10f64a5114c3ee7288
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:06:11 +0000

    truth equipment corner fear closely

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import sys
+  def get_python_version():
+        return sys.version
