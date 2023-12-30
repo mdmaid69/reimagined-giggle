f623c43e37ec1d7b260d69f88002c6bd0a4a556b
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:52:44 +0000

    general sing past introduction pretty

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def cube_number(x):
+  def cube_number(x):
         return x**3
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
