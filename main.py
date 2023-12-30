85f4d3c90c464b7dbe1c7ca12307be08b68db36d
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:18:53 +0000

    worth separate ago fasten discovery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
