77ba6548902eaa028c414a68b08a6eb163afa017
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:56:03 +0000

    arrival minor train bear organization

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def is_odd(n):
+        return n % 2 != 0
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
