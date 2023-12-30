de73284bc828c69c2836a2ced413bb6b6fd75e85
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:08:35 +0000

    forget please seriously fork explanation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
