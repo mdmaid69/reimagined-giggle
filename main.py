6f039f7dc880483a59c088b1ccda2ae735f48cbc
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:41:27 +0000

    cruelty complain warm experiment increase

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
