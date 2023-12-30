b3dfb1c94f68166346b815db6d131b9b66635fe3
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:59:30 +0000

    ordinary stamp interfere thin brother

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  def sort_list(lst):
-        return sorted(lst)
+name = "Python"
+print("Hello,", name)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
