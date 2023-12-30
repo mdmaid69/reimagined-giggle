bda07f30cc8d6e1d98e4539002fb583b22b474b9
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:39:47 +0000

    contact it OKorokay objection week

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
