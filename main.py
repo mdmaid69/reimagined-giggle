6977adb66e51a879c00ae3ecceda18bf0e10bb2b
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:34:43 +0000

    belief characteristic clean guard raise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
