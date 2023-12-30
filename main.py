a7c719dfbcf5c8731ae5a746903dd9179bafa727
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:24:37 +0000

    sting week ready bury perhaps

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def sort_numbers(numbers):
+        return sorted(numbers)
