43bb2d89dda19d76df145398edc6674ba48f6cb4
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:35:26 +0000

    hurt getup address voice lock

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
