8ebf275e6af54a2276b5f8b9f32c812ac66535f7
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:47:31 +0000

    foot attitude pollution backwards willing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
