26d0bac4c4070282f72e931753fb0bd9dbb072e5
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:01:16 +0000

    full criticize effort means feeling

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
