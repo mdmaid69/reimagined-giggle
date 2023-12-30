6c08069c513f380922acb680a76402c011683069
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:24:28 +0000

    make takeover arm measure valuable

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import array
+def get_array_as_float(array):
+        return float(array[0])
