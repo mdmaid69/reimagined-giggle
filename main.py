b10765f089723901e60f35b3f8e0b2f8809e504b
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:34:57 +0000

    welcome now kill study after

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
