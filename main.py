a237618fdb447a00783c9be8ea5bce87b301197c
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:21:42 +0000

    needle cause weigh chance account

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
