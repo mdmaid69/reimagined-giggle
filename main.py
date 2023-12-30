7970214f97267bcb3f1c3dcd8957ab891f74e77f
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:29:12 +0000

    attach through child affect when

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def count_words(sentence):
+        return len(sentence.split())
