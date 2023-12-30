51333d283f8c461714a16b4f8d6d446c22e8c7b4
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:49:34 +0000

    mess scare replace noone read

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import collections
+def create_stack():
+        return collections.deque()
+import array
+def get_list_from_array(array):
+        return array.tolist()
