963f2f10b8ce5cd24006809d3e5b277514104b92
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:20:38 +0000

    Pp climb different calmly everything

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
