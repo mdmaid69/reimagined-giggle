c31573f47c996f596025db1f9298488ba9d2ed35
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:39:53 +0000

    exist repair instead fond responsibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import random
-def roll_die():
-        return random.randint(1, 6)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
