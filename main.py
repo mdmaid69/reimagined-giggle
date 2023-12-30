c1abfdf4795c540c55f066a966ec6c3bd04b58cc
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:06:05 +0000

    callat since night hope race

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
