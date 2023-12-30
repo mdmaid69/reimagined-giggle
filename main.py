3692933e0335d9be0bfb9175f7cf6750885560d4
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:14:24 +0000

    use leave meet day stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def remove_duplicates(lst):
+        return list(set(lst))
