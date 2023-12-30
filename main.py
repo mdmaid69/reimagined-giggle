e8ff60ac35efa05028b0118c3553cf5c891261b8
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:04:24 +0000

    interested limit giveaway fear drive

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
