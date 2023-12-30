6da557aee0940ac4171e889e799ac41a7a45278b
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:36:34 +0000

    considering excuse finish tongue travel

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_hex(n):
-        return hex(n)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
