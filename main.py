d1440b4317b6f78da75dfe97b7957b33a398a32f
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:52:57 +0000

    another deserve identify certainly concerned

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-for i in range(5):
-        print(i)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import json
+print(json.dumps({"name": "John", "age": 30}))
