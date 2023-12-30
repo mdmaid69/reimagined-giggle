7920d39b109ff7b1873146dd27bc7637e4e25436
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:58:31 +0000

    harm see pipe brush ask

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
