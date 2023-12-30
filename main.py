f328f42d5429dd881c7041d37229f0b548853f59
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:07:49 +0000

    compare order wander simple thinkthrough

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
