737ced40ad506a7a6aaa17b181beaa4517529570
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:09:02 +0000

    fortunately dangerous way fashion priority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_speed(distance, time):
-        return distance / time
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
