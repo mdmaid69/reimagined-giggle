c91e69a981220b95aa40f08a8d4283dfe0a2ccf4
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:59:11 +0000

    frequent to concern inside collect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
