70793f23233d54c60c762e76cb27fbee6fdf6f0c
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:30:35 +0000

    child meal game compare rest

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def is_even(n):
+        return n % 2 == 0
