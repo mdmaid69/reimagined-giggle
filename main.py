d54ce19de6bd9523f87151e6c8342d81259c39ae
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:07:04 +0000

    off some pronounce ocean suffer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def add_numbers(a, b):
-        return a + b
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def reverse_array(array):
+        array.reverse()
