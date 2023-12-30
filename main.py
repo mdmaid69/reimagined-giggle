76fe14993399666f55f653ebd423757a9851c156
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:16:01 +0000

    form tragedy division build like

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-for i in range(5):
-        print(i)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
