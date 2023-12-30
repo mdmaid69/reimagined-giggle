6857c58e16c3608cc867aa418530f4813037fff0
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:01:59 +0000

    subject shelter piece holdon activity

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import datetime
+print(datetime.datetime.now())
