e1c222bc087a9b0719ef7da80837e3b80a1c6fb0
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:19:59 +0000

    cough total trial consider risk

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
