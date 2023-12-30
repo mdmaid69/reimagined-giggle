3fcba725b362d10d1e47bb2d42e2fba1ccb92d3b
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:33:34 +0000

    break cold delicate ready them

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def find_max(lst):
-        return max(lst)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+print("Hello, world!")
