fdee05e6da949bc53cfd31d4becc5c5b75d84789
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:59:20 +0000

    comedown apart border guilty reaction

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
