8d43ec40432e72a30283b16ef23e25e2d027f581
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:04:28 +0000

    runinto fear action frighten attempt

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
