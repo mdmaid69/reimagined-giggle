aa916e55bf4cce697c7217fb040e1b4eb66b0478
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:58:34 +0000

    emphasis recovery anybody real line

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
