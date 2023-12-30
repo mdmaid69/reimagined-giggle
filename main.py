fe8833ca6b1473ecf686e6c4003b1dbc65210f64
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:26:39 +0000

    recently loss hold use offence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
