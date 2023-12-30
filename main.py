3145cb2f02ea6248b4d88c3e66f42aea902222d7
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:21:25 +0000

    communicate steep behind argument round

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
