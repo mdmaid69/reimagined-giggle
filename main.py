4e9f19281d292f2f042d055a493d1d457f9d84e1
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:55:04 +0000

    risk left fault cough dress

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def convert_to_octal(n):
+        return oct(n)
