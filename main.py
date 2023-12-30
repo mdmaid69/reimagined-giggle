b000bc886e577de8a91f5f139eafcbeb3abe3158
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:09:25 +0000

    anywhere offence type swimming face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
