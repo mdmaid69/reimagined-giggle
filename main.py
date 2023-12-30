e7480959afb074a86a021506a087ef260b43f4fc
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:43:51 +0000

    sleep direct turnoff cum decision

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
