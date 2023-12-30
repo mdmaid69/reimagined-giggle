78c151adf9243942ddec9ecb7a57b4b3901495a9
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:57:52 +0000

    no drawing basis appearance watch

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
