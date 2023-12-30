b60d7394b995fe1d05b0a4217bc6d170c1ff1d80
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:42:49 +0000

    powerful safety independent brother house

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def sort_list(lst):
+        return sorted(lst)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
