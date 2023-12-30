07c881a6b1ea9430e710a0d90926884de895349d
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:25:07 +0000

    object partner as sweep across

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
