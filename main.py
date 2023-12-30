7c0dec60c28359d9864037ed7b65bd6fc78c93df
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:34:25 +0000

    believe offer normal age holdon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
