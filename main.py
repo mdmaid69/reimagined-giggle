d6bbacf2831151e455347cd72ff8d5328aa28589
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:31:43 +0000

    sensible mind wash very cross

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def sort_list(lst):
+        return sorted(lst)
