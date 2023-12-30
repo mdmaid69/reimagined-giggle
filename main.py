e93a7f9028051d5756e31161d39350e9e7865024
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:14:52 +0000

    black as swing hour goout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import os
+def change_working_directory(path):
+        os.chdir(path)
