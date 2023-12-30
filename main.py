60cfc39a87a0d1776203e43ceb3baa4a28ea8d18
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:19:36 +0000

    experience this force breakin move

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
