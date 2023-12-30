56bf5b4071e0d14deb5623502ce20f5531381df1
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:32:16 +0000

    cut present draw translate chin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
