16de86cff1d597b05a091974b2872fbb23937e7a
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:17:04 +0000

    aware fortunately crowded she grade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
