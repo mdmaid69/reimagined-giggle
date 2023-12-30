c1b6c7de37bcfc2ba7681e211e868639fce49cd5
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:28:31 +0000

    cry repair workout settle party

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def get_array_as_list(array):
+        return list(array)
