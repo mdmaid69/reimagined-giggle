55572748ec67a1faf533e85f02d6ae1bd18eba19
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:26:24 +0000

    bad section laugh surprising thought

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+print([x**2 for x in range(10)])
