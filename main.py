a9d11004d95da918c39a498ef9a3928a2396e9f8
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:25:14 +0000

    brother fold activity project doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
