ba1525b2bf1d32a1e9d42b3df5c303f7e4f52f0e
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:30:46 +0000

    male acceptable empty actually forward

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import os
-print(os.getcwd())
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
