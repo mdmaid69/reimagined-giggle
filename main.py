f61a99757f2d28987695a3e0b7d181d525090086
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:58:49 +0000

    far reaction alone Itself ago

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
