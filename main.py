02623b73333049369dcf3ee3a120b00fb8575e32
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:30:56 +0000

    there cry OKorokay section female

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import getpass
+def get_username():
+        return getpass.getuser()
