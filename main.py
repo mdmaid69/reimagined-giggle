ca4191a1d7896e519e195712d11401f744637081
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:32:03 +0000

    put conversation about before public

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+print(sum(range(10)))
