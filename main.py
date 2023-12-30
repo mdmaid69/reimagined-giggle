f0df070d8857f208ca7e15b72e3750329e98966c
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:24:20 +0000

    comfort several release grade message

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
