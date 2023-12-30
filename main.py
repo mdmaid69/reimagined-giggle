465ccaf108534efe7451a7ff58b40a5a0514f03c
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:11:22 +0000

    goodbye vote 567enemy control challenge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
