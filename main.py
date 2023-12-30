3a685617bec8d6645c96de9943c4184b6e466cb4
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:56:37 +0000

    dream search first field design

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_volume(length, width, height):
-        return length * width * height
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
