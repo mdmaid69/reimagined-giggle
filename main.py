3e169ed98093eef4532021194f9657d13ff29581
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:49:00 +0000

    rarely present adjective sing pen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
