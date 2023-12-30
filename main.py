2e373c541427c560d867d769934c2de4415e450b
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:07:59 +0000

    perfect practice way large view

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
