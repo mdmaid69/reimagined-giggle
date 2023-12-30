5df5b7a66d2f7eb07ffe0a351d54fb680a54cc44
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:40:46 +0000

    decision swimming Ff zero fix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def delete_file(file_name):
+        os.remove(file_name)
