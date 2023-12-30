677f62c9c19dbe49172b5fb796e340d358fcb542
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:35:22 +0000

    prove really advise gate sensitive

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
