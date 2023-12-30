bd4f9320aee07454d307e8626532f2f2cbfb49a5
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:41:31 +0000

    scream thinkof increase earn consider

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
