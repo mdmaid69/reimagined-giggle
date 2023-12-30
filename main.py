a2e347d8e6886521d4c1f507240780d1bed26c71
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:21:03 +0000

    square broad actually by confused

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def subtract_numbers(x, y):
+        return x - y
