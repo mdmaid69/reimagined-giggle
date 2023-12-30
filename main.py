c42f787f72a5116ad6556a2d60b34db514d6ea6e
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:02:14 +0000

    forward plain limit contact loud

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
