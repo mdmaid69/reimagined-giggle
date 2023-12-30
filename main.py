a463b3502985e3633652acc64e867853d4594f4b
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:37:10 +0000

    laugh frame pretty noisy late

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
