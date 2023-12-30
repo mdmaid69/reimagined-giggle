a64ce3bffffef250e1ea1a8ddd137de3b650648d
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:24:45 +0000

    fortunately reliable further afterwards holdon

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+name = "Python"
+print("Hello,", name)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
