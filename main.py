f326f46a152d7b360cfb61cc2764742fdda598c2
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:35:07 +0000

    quarter proof wild safety post

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
