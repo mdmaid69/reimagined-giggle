f5a76b262f91a740d6f3298750ecdcd284e30588
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:08:16 +0000

    pronunciation so mix bush little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
