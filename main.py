2a81b6416fe1133a1238ad9f7c8c7444988379cc
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:15:28 +0000

    pocket freedom basis inquire where

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def subtract_numbers(x, y):
+        return x - y
