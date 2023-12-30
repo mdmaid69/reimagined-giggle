5e657f1be7dfb2d6c28eb49b5b7e42a1384e299c
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:44:53 +0000

    score beat good scheme rule

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
