68bc9e5593cc88111c35310617e0b8252c057b5c
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:23:21 +0000

    battery formal impatient beginning communicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
