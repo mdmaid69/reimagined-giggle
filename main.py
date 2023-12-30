231bb134bc104bfe0b29bb8145f2b06f46a29b40
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:32:08 +0000

    expensive marry equal dream probably

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
