1f1383b5721c77ab688f7b3caf5cf837f0b23951
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:23:05 +0000

    neither question false tune blue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_area(radius):
+        return 3.14 * radius * radius
