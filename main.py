ae40ba1d43fc48d4782a52790b92c2dc46b60975
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:51:44 +0000

    far organize wait angry middle

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_area_circle(r):
+        return 3.14 * r**2
