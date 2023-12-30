ea5d3b9d13d74a753398a50c867a2afcf3b293b0
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:11:00 +0000

    edge disappointed fresh plan clue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
