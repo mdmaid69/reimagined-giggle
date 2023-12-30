bcf65ce9c6fc15a34a26d674941f880da34f596d
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:20:53 +0000

    mystery weather piece present take

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def get_array_as_list(array):
+        return list(array)
