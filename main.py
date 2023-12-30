9fc35ba4cbeffce4e1515297df08c084b3f11e8a
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:57:23 +0000

    free spread acceptable disgusting bite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def is_even(n):
+        return n % 2 == 0
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
