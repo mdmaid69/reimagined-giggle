5ed384cccb382009163f3c3d47e3f5dc544292cd
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:42:39 +0000

    every glad lesson cook comeout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_power(work, time):
+        return work / time
