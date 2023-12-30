bf027262ac200e0c446dbf2d80884050bb18a58b
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:56:07 +0000

    risk mess thank pause refer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import random
-print(random.randint(0, 100))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
