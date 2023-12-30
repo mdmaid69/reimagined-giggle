b587e94e27fdae73cc23d926c4314b58d313b5a4
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:19:33 +0000

    getback study turn wipe certainly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
