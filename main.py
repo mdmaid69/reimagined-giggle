1fe4bb34c928b7db886dcbe5b21f56d44ab4a4f4
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:10:33 +0000

    reasonable round somebody become every

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
