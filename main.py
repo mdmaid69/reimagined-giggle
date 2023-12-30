917cd11c289645a9ea00e4ed761685c39a40ff2e
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:44:17 +0000

    cheek mouth careful couple action

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  def is_even(n):
+        return n % 2 == 0
