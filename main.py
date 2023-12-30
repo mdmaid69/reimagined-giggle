a5192c72d8fb1469a2babdceaddc8686557c79e0
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:21:45 +0000

    human climb knowledge lunch talkinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
