afeaf31545aa10ec6a234b105bbbaee428438763
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:47:43 +0000

    even break cause dark act

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import collections
+def create_counter():
+        return collections.Counter()
