a467199535b5f34bf9c684dc990fc408c40188ee
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:34:53 +0000

    peaceful space length remember pronounce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
