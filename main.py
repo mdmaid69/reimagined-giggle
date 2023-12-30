a3a54c0be4f0baef3a2b873fd0f40257992f9200
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:31:05 +0000

    difficulty declare upstairs help trust

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
