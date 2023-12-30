882ae26c838f7f1cf22ed3b98abf4bd94f65eb18
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:17:06 +0000

    pair flavour hold gradually from

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import sys
-print(sys.version)
-def calculate_density(mass, volume):
-        return mass / volume
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
