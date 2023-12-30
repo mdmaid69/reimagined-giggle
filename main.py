fd141af1cfe3a4a1f55120bb737c1439e7d68dbf
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:02:37 +0000

    interfere pump control mess actual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def square_number(x):
-        return x**2
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
