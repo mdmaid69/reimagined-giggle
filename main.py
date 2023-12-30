949a50c545108de9877700ddd8690116f1ad7d01
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:11:35 +0000

    rare wind develop unhappy promise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
