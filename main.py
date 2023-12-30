e2b5b687b3d8a5b9d4a51a8781667b83bcaf216b
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:17:44 +0000

    uponon whether sorry style recovery

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def subtract_numbers(x, y):
+        return x - y
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
