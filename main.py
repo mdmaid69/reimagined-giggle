e095ec08cc162bfc03408f255d96177c6dd3f5b4
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:50:01 +0000

    step manner hardly past blue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
