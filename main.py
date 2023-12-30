39d089bf1580fa947e4df7cb6505a19132f85759
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:55:57 +0000

    ill early wave Jj for

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
