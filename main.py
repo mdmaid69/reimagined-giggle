445950ad68322e88b9507a938357d77ec46b606f
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:27:44 +0000

    lose rank sign onto clever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import time
+print(time.time())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
