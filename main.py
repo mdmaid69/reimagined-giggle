29b78aec79970d6d6e33a44bb4ddab7d8c09bf3a
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:06:56 +0000

    combination formal relationship murder mine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_repr(array):
+        return repr(array)
