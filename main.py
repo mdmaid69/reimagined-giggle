bbc5abd36d364f9c0c0b503b1fd89f0aedfff65c
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:46:31 +0000

    pencil show accordingto beside otherwise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import numpy as np
-print(np.array([1, 2, 3]))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
