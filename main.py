72ee3b186808658d7f722f8c5010a0b50eb40cc6
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:43:09 +0000

    maximum accept already model lose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
