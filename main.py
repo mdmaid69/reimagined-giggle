9ebe2e8b65a686c949d3a63edbaf13ca581c0938
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:44:15 +0000

    work grow urgent slide interested

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
