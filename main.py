aff9c8bad7bd78c0e69e6fac2d4aad614b2d1ee0
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:53:08 +0000

    persuade party visit holiday lock

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
