a745436fb55122762836f140133eed6f29a8c2c0
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:03:30 +0000

    bar future sudden responsibility team

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def get_array_index(array, item):
+        return array.index(item)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
