312e26f9933864a809b0c1fc41675330454a091f
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:18:24 +0000

    will height sure do situation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
