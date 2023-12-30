e848dd972e786448286ea6a58176aa5ec132a655
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:35:55 +0000

    anything beginning computer away pretty

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
