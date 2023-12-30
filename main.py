617c179b67fad353286b208a392c49023d490ebf
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:19:06 +0000

    possibility guess moment return Kk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
