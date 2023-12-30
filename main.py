ba54c43e78b2933b4e3207e6ac84f84f1d7d9e78
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:29:47 +0000

    rob artificial handle order same

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
