d601794bec688745e668f9d16d8fbe6d220f3282
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:43:09 +0000

    twist or name delicate trade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
