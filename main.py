7d53b1c1a111f05b1f93120c061dab74ed290bd6
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:45:17 +0000

    sport relative live supply laugh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import re
-print(re.match("h.*o", "hello world"))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
