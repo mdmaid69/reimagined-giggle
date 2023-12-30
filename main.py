492210fda7e2e313bccc371a43613f93f9b43b22
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:11:05 +0000

    uniform shall comeback bill real

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import sys
+def exit_program():
+        sys.exit()
