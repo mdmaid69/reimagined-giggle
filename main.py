d93213ce800a3fb4b996b8b7f3417105a8858882
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:08:36 +0000

    for reduce dry afraid tip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
