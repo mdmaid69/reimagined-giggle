b184a446f700031f565230d49813d3b3494befca
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:04:45 +0000

    organization team disappear too her

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
