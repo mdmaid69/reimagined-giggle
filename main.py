d819d9cdf34b7cb525b99241a51573c378c5fb3f
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:15:45 +0000

    satisfactory and copy limit about

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def reverse_array(array):
+        array.reverse()
