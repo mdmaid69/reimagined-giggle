7c61f8908031c39ee3a2911e539d6e238b2d7984
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:09:12 +0000

    car feed of below gradually

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import platform
+def get_python_version():
+        return platform.python_version()
