6e8bb613cf8dea4ae882bfd440a665fa9806255d
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:25:48 +0000

    cry loud appear go conversation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
