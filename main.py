3e1e9b71adb4cd3081b90b766eb508850b3bb798
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:37:36 +0000

    claim she discussion label comefrom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def square_number(x):
-        return x**2
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
