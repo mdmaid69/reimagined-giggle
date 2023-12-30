24f7b1454691dc238b59f400e07bdd465c225bbf
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:05:13 +0000

    reasonable closely pretend tune Qq

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
