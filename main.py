a9683af9143003d5e56c456d484ebaa67b9a3ba2
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:36:09 +0000

    wear risk we whistle represent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
