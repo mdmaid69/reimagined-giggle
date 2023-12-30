341a61cba08b5a0d173c0e4e0f9e3e61581429af
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:14:15 +0000

    against work list kind suckme

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def convert_to_binary(n):
+        return bin(n)
