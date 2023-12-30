9d34adcecd0dbb4d015e6162109c76b63f1c3dff
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:02:38 +0000

    plenty sleep speak meeting sudden

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
