6026071684bb5ab58c2958c911af64c1e42fa98e
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:05:40 +0000

    rent greeting artificial law soon

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
