73cf820b53f264b5d24549a67fd75ce67516386f
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:05:51 +0000

    lunch since 569enjoy after blow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import math
+print(math.pi)
