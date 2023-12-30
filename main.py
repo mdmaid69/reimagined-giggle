3dcc3f73c976122580996ed2655e92d905b64b8d
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:59:00 +0000

    result nonsense sight transport talkback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
