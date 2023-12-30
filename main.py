1eb80c8f18d7ce2df39c137c80ab1187ef40279a
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:34:26 +0000

    morning mistake usually takeoff butter

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
