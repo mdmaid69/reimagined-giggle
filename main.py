3647e05dec368a276b16c7b977bfa0ca2ac754fa
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:47:13 +0000

    shallow announce thank declare ocean

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
