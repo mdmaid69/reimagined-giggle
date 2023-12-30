e806cdd6461eeb26f65ccbcd9ce66e942fd60550
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:20:30 +0000

    opinion cum whether accidentally attractive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
