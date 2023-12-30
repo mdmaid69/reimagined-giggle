b9a03aac9f539fb92e429c6b92619390470b220d
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:00:21 +0000

    remain listen she convincing Ll

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
