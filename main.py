8533b1454c196fee1bfa29f76b0e3dd5636e6dcf
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:08:13 +0000

    content half pen guilty account

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_acceleration(speed, time):
+        return speed / time
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
