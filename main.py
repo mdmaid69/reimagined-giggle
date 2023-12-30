abff9878e8ba966155d454ad583a1556d385baec
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:21:19 +0000

    trap get no phone tool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import sys
+def print_python_version():
+        print(sys.version)
+import array
+def get_array_as_str(array):
+        return str(array)
