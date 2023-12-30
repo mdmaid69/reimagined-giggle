66721993d548fe0e7d395cd6f6bcee86e562edd1
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:53:40 +0000

    innocent connect even message hope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
