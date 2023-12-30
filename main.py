59a84eaafc7e95765968160396229bc5856e6513
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:27:52 +0000

    surprising phone acceptable best expect

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import datetime
+def get_today_date():
+        return datetime.date.today()
