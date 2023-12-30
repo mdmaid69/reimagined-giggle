85de923579273708d29c7897aa8701e6b7895b65
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:11:29 +0000

    name concerning trouble also die

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
