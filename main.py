0d67c0de3ad5995715dcf4c2fb44e434457426a9
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:51:55 +0000

    alive married sharp careless future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
