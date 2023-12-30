1b6283cdfef5304a7fa716014f4b351bcae4083f
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:45:55 +0000

    relation adjective display offer connect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import random
+print(random.randint(0, 100))
