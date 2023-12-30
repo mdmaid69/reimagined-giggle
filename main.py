80a1e198acc95b487274725bcfaa753bc83b2de6
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:24:33 +0000

    job separate left tell uncertain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
