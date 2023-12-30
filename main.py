158f1dd8282da4ff5729af57a64458d3ea422501
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:34:36 +0000

    fond used help listen winter

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
