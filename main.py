27d197b9840b5a41cfaaa5987c59e96755612877
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:43:16 +0000

    pressure show shoulder as wire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_unique_words(sentence):
+        return set(sentence.split())
