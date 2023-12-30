55652723903d762879509832ef1b92ab97dcc6e4
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:25:29 +0000

    clothes none direct explanation especially

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+def find_min(lst):
+        return min(lst)
