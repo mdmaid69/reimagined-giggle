e30eab1347ac3c78ab9bb92e1f99efd7fcb735a3
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:42:10 +0000

    offence body bad excuse include

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import sys
-def print_python_version():
-        return sys.version
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
