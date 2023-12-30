86ba5fcab8bb60b8bd21b1905cb5ab14f842840a
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:37:21 +0000

    may speak software picture safety

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def get_array_buffer_info(array):
+        return array.buffer_info()
