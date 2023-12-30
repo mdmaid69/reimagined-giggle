c4e2d52d2fcd9ca6acf4d809a1f29822f6360859
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:01:41 +0000

    left church walk dark mine

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def cube_number(x):
+        return x**3
