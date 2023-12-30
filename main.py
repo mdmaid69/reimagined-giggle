86de78ee4f9b1286e87806f6c659405ddbc929b6
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:24:04 +0000

    definite attractive near with nothing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def reverse_list(lst):
-        return lst[::-1]
+  def get_current_directory():
+        return os.getcwd()
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
