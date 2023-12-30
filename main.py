da9edef775f10a924b0c87484180ff2f250cc806
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:43:34 +0000

    common storm holiday method bang

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_array_as_bool(array):
+        return bool(array)
