4d5154c4cfdd63027d930e86005d3ba57cfd5845
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:28:11 +0000

    foot every straight peace cause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
