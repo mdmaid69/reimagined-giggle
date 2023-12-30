e7f15807b876975b04f01787949b40a847a55a93
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:34:04 +0000

    relax forget actually price consist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
