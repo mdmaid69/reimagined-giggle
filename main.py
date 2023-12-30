1503767abc1115b0609d21a226d6cfdaccf86641
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:09:40 +0000

    far forgive mention your large

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def reverse_string(s):
-        return s[::-1]
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def subtract_numbers(x, y):
+        return x - y
