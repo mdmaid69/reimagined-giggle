0ca2a2ab7f9a770afc8d697cc098170e9c4be1a3
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:29:13 +0000

    return scream in split touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
