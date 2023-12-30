44bc975c621d897b2e6eb13fd883811099d9a939
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:56:15 +0000

    guide goout giveout boring goout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def get_array_as_str(array):
-        return str(array)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
