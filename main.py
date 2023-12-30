21e5cd9177df041b9d5236c82a7c02cd1e2cd6dc
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:01:37 +0000

    wet worse progress actually city

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
