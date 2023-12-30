294c0db1d8df4a5305df36fa199be39559441f60
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:02:44 +0000

    trust nearly emergency perhaps funny

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import math
+def calculate_floor(x):
+        return math.floor(x)
