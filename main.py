9fd4e41576a49434341fac9af923d7760d622e1e
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:05:26 +0000

    use means recover friend wash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_list(array):
-        return list(array)
+import collections
+def create_counter():
+        return collections.Counter()
+def reverse_list(lst):
+        return lst[::-1]
