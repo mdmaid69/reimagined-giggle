3a0cc0b1eca2bc12ee47672a74a8c8a5be43d771
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:02:47 +0000

    difficult count extra little common

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def square_number(x):
-        return x**2
+def add_numbers(a, b):
+        return a + b
+def sort_list(lst):
+        return sorted(lst)
