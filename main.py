1a7f99edfdf8431af36ad8ebaa2247d34545130d
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:58:04 +0000

    tight turnover shout turnaway ground

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
