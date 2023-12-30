836ff8fd99544f1c0f21a50daa22f42a06574aac
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:21:39 +0000

    vanish freedom arrival height getoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
