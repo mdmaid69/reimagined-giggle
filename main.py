157de4f7c5fae09dc6bde555015ad0817e067cd6
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:06:02 +0000

    persuade press carry properly majority

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_counter():
+        return collections.Counter()
