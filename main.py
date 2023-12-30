1184dafec99b5ccf126f2c2ed5fb191259c1c27f
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:55:09 +0000

    worry getaway apparently almost shall

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import collections
+def create_counter():
+        return collections.Counter()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
