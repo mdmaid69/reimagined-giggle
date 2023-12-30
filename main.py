4217339306b2dcd35877830d5f7b45861fb92196
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:12:54 +0000

    cause explain takeaway deliberate among

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def find_unique_words(sentence):
+        return set(sentence.split())
