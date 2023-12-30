bd4be8dc3e0e264c3c904dbac926ca2413c930b1
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:02:36 +0000

    work my window please purpose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
