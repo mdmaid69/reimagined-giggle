cf5a42d4ea50e2a132e3450a65ba8d281c9671a9
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:30:10 +0000

    wind kill protest glass Rr

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
 import os
 def get_file_modification_time(filename):
         return os.path.getmtime(filename)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
