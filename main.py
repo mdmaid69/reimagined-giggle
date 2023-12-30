b630e1fea416e28fc1ee9ef34c64f48f4ea105d8
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:05:08 +0000

    ilovedicks kind gooff already land

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
