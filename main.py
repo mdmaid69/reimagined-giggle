ec1c036de2a0988f246a28698000c515d945fb6e
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:30:18 +0000

    never something broad 566end regional

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import time
-def get_time_since_epoch():
-        return time.time()
+def get_array_as_bytes(array):
+        return bytes(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
