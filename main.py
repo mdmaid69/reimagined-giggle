21a58371223074a6a945a0305ddd738b9ca9170a
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:23:53 +0000

    down anyone active moment trial

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
