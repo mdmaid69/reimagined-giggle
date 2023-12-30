31817ebfdf3ce2484b862c78f4e26bb05902e708
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:21:45 +0000

    responsibility breathe practical crazy machine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import logging
+def log_message(message):
+        logging.info(message)
