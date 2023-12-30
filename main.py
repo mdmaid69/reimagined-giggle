aaeee1d99b131fa94618444a4de476083bc13ab3
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:33:46 +0000

    juice nor wrong Tt introduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
