4664d044abdcea031063bb7346d778e2ba016981
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:59:21 +0000

    listen young treat blame backwards

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  def convert_to_binary(n):
-        return bin(n)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
