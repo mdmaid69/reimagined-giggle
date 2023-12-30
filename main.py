6165123aafeea7d729777c27d13c114b0023c122
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:33:27 +0000

    walk business sure within sport

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def get_array_as_set(array):
+        return set(array)
+def calculate_density(mass, volume):
+        return mass / volume
