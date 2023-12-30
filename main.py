7d9b5c0d26c0f3684a3c4eb5aeb14467b9e7af86
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:29:52 +0000

    advantage tongue plate meaning example

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def is_even(n):
-        return n % 2 == 0
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import re
+print(re.match("h.*o", "hello world"))
