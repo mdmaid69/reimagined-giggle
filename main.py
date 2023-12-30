516a6f82c4215f38d09f259a3891627b37c9405a
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:33:54 +0000

    careless otherwise goback(to) glass lookup

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
