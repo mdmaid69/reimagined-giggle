12c12a3febba63bb92dbf0c3af944b4f38679a79
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:32:33 +0000

    uniform confused fresh high waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
