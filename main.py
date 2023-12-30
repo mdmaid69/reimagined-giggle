ff16c344ef92fddbd9c8386051ff81985ec94ee8
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:42:38 +0000

    curious turnon body here store

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_speed(distance, time):
+        return distance / time
