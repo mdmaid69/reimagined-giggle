6bf2a9553b657e1876dbd78d20d8d752a40fed11
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:18:07 +0000

    hurry answer lean suffer mystery

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def convert_to_hex(n):
-        return hex(n)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
