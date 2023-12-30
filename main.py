662c469e72ebe1dfc28593b21d35ef78340b7195
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:12:15 +0000

    instruction stay introduce price question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_list(array):
+        return list(array)
