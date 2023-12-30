04677d967f8924eb6d448d10e9e58e93cab48583
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:50:56 +0000

    runout turnon happen thank lot

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
