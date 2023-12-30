d3ca6b312aa9cd53b9ea5790d760ae6fe1e92847
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:38:47 +0000

    pair bill offer birthday rob

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import re
+print(re.match("h.*o", "hello world"))
