3e15b81f30d3d23578dbf65d00d98b453bafdb0b
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:17:02 +0000

    herself may swimming experiment responsible

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def convert_to_hex(n):
+        return hex(n)
