b038ea1bd97ecdad30b6d0b015e21175577381e9
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:11:40 +0000

    but Ee every birthday Pp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def sort_list(lst):
+        return sorted(lst)
