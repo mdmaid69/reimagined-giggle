fe8a0972b6e0f4c13cc573cba9a1e7cac11a6dc9
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:46:00 +0000

    niggaacademy unless friendly or wind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def convert_to_hex(n):
-        return hex(n)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
