654e207a2861ef6596be65797492d452fbf71df2
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:40:36 +0000

    my rid lose upset strange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
