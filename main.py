4fa5f6abf4c28ae2ac8d44e562fd5ac0c559f862
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:01:34 +0000

    square natural pronounce protect crime

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def subtract_numbers(x, y):
+        return x - y
