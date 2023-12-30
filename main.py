7968748eae143e6a4b9619db26762c945caf9531
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:26:40 +0000

    sight tooth score ignore flow

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  def calculate_area_rectangle(l, w):
+        return l * w
