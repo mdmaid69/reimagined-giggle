ad16b3eaeeb1caebbe08964aa785b5e14d1dd87e
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:47:51 +0000

    shelter often neck false fire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
