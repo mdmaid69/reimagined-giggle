c7689b64c1e42896edb47dcd0ac81c66787f60b1
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:23:30 +0000

    whether unfortunate thought tired dead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
