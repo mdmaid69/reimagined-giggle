c7e6b6db9287c77f2b9869b71c495a68983a4f07
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:21:04 +0000

    body we your once tidy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_exponential(x):
+        return math.exp(x)
