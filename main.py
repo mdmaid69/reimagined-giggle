1647da4e78e7842cfdf30ecd177d03cc2478527a
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:59:56 +0000

    now practice majority deserve part

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
