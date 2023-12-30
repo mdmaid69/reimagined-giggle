10fcf778144f313307e8f5e281fe8b73820a3022
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:41:27 +0000

    usual long mind habit purpose

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
