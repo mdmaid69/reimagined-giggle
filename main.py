516c6e35ccb889a2083460394460c17de6103889
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:03:54 +0000

    order want wrap meaning holdout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
