95ecd7c1c5e60a5357c8b75c94d8d4ef0ef347a4
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:21:01 +0000

    fix Rr could prevent century

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
