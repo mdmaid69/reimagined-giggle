a255af0d6d20a23f51682a0c23e6a1e0abde7cb9
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:35:09 +0000

    talk smell clearly walk die

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
