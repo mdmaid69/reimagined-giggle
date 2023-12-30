5699e965071bf6279df63d415fd8836418328be6
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:15:35 +0000

    concentrate hole basis do result

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def convert_to_octal(n):
+        return oct(n)
