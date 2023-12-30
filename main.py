1ceec845b6d9b22fc04a9ab523972f6a6bd5f527
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:48:21 +0000

    rise shape total fight outside

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import os
-print(os.getcwd())
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
