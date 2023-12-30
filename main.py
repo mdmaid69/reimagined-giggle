1cfbff40f962c982efb7ac9ba66bbe4b23e01af6
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:08:33 +0000

    read tape idea comment especially

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  def convert_to_binary(n):
-        return bin(n)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def sort_list(lst):
+        return sorted(lst)
