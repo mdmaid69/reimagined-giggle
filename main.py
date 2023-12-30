d0498d8bb1ec5937238994b0a0076f5615f4a6e7
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:57:32 +0000

    business hardly deal indeed after

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
