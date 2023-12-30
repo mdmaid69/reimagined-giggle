fe354c0dc8520279b6627d3822bc5679089a0d1f
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:13:55 +0000

    matter get present impossible comfort

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def get_array_length(array):
+        return len(array)
+import array
+def clear_array(array):
+        array *= 0
