f5f469b84afff5191a46c5de0018bfe4661bf89f
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:42:53 +0000

    manner unite absolutely joke treat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
