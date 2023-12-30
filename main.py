b1f74b36ce7f01b7a17f956eca5d37d5f79fb4b0
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:14:12 +0000

    system section freedom reasonable education

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def convert_to_hex(n):
+        return hex(n)
