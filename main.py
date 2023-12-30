08fa8c105d3a7890ac3d44ae612b4f52fb11a7f3
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:13:55 +0000

    brave design belong risk personal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def remove_directory(path):
+        os.rmdir(path)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
