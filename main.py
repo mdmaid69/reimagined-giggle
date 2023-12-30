f5de3e579583e669e2c5fa5a050a3dc9836fb1ee
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:18:14 +0000

    list lookafter brain university blame

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
