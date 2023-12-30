bd03f7d46c207a6c99698e21bb8525985672d036
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:14:40 +0000

    always file fire declare welcome

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_volume(length, width, height):
+        return length * width * height
