a228ae1d9ef8e7bcfe459b1103bb577f74db3540
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:37:25 +0000

    chase willing calm by near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
