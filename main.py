202db646d86de7d96efb3352d67b64e31784edbe
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:31:25 +0000

    date become opposite damage form

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_itemsize(array):
+        return array.itemsize
