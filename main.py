d7f366f9f955b3a6c910b1bb4e6aa679d9ddb691
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:13:05 +0000

    subject proof lead side account

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
