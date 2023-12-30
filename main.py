563eae5e36495b3661d5e4fcfcea95c6759f8731
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:30:22 +0000

    worth prefer duty anxious feed

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def clear_array(array):
-        array *= 0
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_area_rectangle(l, w):
+        return l * w
