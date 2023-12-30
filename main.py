8fd161a11e747112eb680c571688dc987d86033e
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:09:15 +0000

    weekend stretch temperature afford peace

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
