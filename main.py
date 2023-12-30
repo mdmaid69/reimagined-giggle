0540d4d03b9bd5a46d29304ee143fa53640d36a1
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:05:57 +0000

    memory open finish her convince

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_user_list():
+        return collections.UserList()
