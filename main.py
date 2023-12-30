93c321d86df007e994da7bd5accb3c7180f62ba2
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:49:03 +0000

    deliberately ago everyone trial collection

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
