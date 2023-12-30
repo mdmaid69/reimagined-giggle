5ed8bfe2f255aa1716cc2402e32719cade3d09a4
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:23:56 +0000

    squash soon favour bush join

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
