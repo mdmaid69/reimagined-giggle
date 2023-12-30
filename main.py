f391bd3b1f0c3291f5068fc942077afdd2218d7e
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:19:57 +0000

    race turninto reaction interrupt choose

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
