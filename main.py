93bab8f506851dc0f7b559cd9de093c12c0c3702
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:37:10 +0000

    free trust take exhibition confused

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import random
+print(random.randint(0, 100))
