80ded73f1615866b8efd9431870e57b832293e71
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:00:38 +0000

    everyone fail holdout after ahead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
