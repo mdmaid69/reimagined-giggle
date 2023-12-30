eab9d3b624953a4a86fb692fc1e1fa7915e5f7d2
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:54:02 +0000

    cross whether remember left worry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def convert_to_binary(n):
+        return bin(n)
