8e0d6ad4c142f57dc37eaadab89c82597f7564a7
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:34:55 +0000

    cap female goon ordinary innocent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def remove_from_array(array, item):
+        array.remove(item)
