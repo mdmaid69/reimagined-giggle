5e1d42442834951cea2500ed244b29974fd90cca
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:50:59 +0000

    cheek difficult getup order this

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
