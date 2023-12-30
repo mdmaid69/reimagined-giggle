badce1f63cba97d43ddec4ee066e8a4c9361808d
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:50:05 +0000

    check radio short please broken

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
