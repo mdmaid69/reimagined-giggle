5852562f991c682b6912aa15f917697f099a74f1
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:10:01 +0000

    polite uponon congratulations traffic special

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
