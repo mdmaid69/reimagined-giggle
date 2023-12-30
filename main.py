473e7a246719e516c9fa1dbcb330302280e68037
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:23:55 +0000

    empty present claim in dish

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-text = "Hello, world!"
-print("Characters:", len(text))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
