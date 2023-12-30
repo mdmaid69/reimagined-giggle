dfd0857e328d9a69c77e2cde9db755f0588b24b9
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:32:08 +0000

    figure turnback load rest development

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def square_number(x):
-        return x**2
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
