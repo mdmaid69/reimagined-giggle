e7492d16727a21f0a7629733cdeb71bcadce6452
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:45:06 +0000

    noone spot number flat duty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  def convert_to_octal(n):
-        return oct(n)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
