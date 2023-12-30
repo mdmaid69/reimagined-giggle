bb94a383030e36bf42c7341adc78b93cc98572a1
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:19:57 +0000

    appearance fault sail present concentrate

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import numpy as np
+print(np.array([1, 2, 3]))
