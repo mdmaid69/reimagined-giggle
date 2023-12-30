86441d67429e2ba23882f22f3d9c026547e9d9db
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:58:25 +0000

    lookaround guide twist name change

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import os
+print(os.getcwd())
