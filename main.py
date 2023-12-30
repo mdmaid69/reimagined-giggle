04af4413820c9ea4c186da39c48592863fc08b6f
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:53:56 +0000

    tool quarter so sell serve

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_max(lst):
+        return max(lst)
