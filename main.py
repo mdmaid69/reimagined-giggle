49fe16894d84ebd62bc91c5bdd1d5ae6b4b6ea91
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:30:01 +0000

    attract hardly comeout throw stranger

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
