2307697774fa8c894675fe19e6fe01abe2dba4c2
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:41:33 +0000

    favourite rush do during children

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  def cube_number(x):
+        return x**3
