cf2d7dba754deda1cfed062f9c36ea52ea00487a
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:28:51 +0000

    butter after rule reduce upwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
