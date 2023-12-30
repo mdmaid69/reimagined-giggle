c412c3230d90d5dd6dd6adb7ba787980bc3f8684
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:25:39 +0000

    turnon kind software rent lost

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import numpy as np
+print(np.array([1, 2, 3]))
+def find_max(numbers):
+        return max(numbers)
