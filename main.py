3cd04685a4ac527fb67fa00e62c862d12f08bebe
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:48:30 +0000

    skirt temper bye drive many

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def subtract_numbers(x, y):
+        return x - y
