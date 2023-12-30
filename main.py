782317a34b980a3eda5a966f62b5a1a8a1803ab6
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:25:32 +0000

    mention memory regional Qq likely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
