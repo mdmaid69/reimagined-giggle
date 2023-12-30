f87633fdb947fca8f781605c620e88b6a30f9afc
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:12:54 +0000

    address hide reliable defend nervous

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
