ec9557d5a221ca3af7cf0ef1260572c20509fcf9
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:24:44 +0000

    furniture circle wish fierce her

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
