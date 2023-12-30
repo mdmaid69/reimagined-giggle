fb09baf5d7b27977455afb2cf111e4119055848f
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:36:41 +0000

    switch treatment below relationship runover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import datetime
-print(datetime.datetime.now())
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  def cube_number(x):
+        return x**3
