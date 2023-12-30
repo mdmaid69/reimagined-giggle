6cac2ffab9c7e799ebbba140282a970855017966
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:23:04 +0000

    secret punish appeal swimming build

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def calculate_median(arr):
+        return np.median(arr)
