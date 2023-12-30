8769dafad64619133dcba67e16cb4dab3d6b4a59
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:39:35 +0000

    plant slowly comeround storm anywhere

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
