f569dee40ffa26653e472ed0a3d84cca3b14d654
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:30:06 +0000

    Qq less kind community involve

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import platform
-def get_os_info():
-        return platform.uname()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
