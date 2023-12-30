b13172e86efd3633bf9e8b89876e27f6d566a4d5
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:05:09 +0000

    hers seem talkback still wind

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
