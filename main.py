b3bb8a3957343cdcf883bb8402b2b08b2d993a08
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:38:52 +0000

    Hh load getout dish interview

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
