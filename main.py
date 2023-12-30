c7d427314e6ca2b60a8e10e2cfd6ea99e4ae2ddf
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:11:38 +0000

    guilty good cold yours treat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
