a3b9816b75d366053d3f34f0df482f0562fabf15
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:55:50 +0000

    sell confidence behave ask wonder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
