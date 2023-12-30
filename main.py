1919d8e9e632fd52cf7750014eeb774c69bf1d42
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:52:06 +0000

    warn most big switch appear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import datetime
+def get_today_date():
+        return datetime.date.today()
