46ac782a9f70c6b9a81628fdaf94de6b5aca5cd5
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:07:44 +0000

    godown refuse second speech any

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
