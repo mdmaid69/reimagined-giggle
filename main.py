038d09a422c586a099e08b8fe06152284ed95c3d
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:03:53 +0000

    operate mysterious muscle flow judge

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def reverse_string(s):
-        return s[::-1]
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
