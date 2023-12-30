76e5d8722dccd22b0604ed2b9633327b9ad66eed
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:27:53 +0000

    substance secret cum sore towards

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+print(os.getcwd())
