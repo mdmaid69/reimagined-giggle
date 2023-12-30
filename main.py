31c3fe2bf0d7d39dae3fc84e574020c5896a0986
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:28:24 +0000

    distance quit absolutely small collect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
