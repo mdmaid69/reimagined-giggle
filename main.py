04d64fb8916fd8b16ad4fcf58557c2a9bbf8902d
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:21:31 +0000

    miss relief forget introduction shrink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
