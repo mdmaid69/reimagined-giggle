1ce7a9f95fd1aed9dc221dd3534f12850e4a23b5
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:31:37 +0000

    569enjoy within arrange getback demand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
