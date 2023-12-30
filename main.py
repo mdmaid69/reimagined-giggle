bf4c8737d738cc33ff384a7cf0ad8baf0107e488
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:25:33 +0000

    hers touch refuse heavy runinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def reverse_array(array):
+        array.reverse()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
