9b47f3c4319c50fda09e57a0d04be2e569baa2ee
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:56:00 +0000

    undo still tight comein catch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
