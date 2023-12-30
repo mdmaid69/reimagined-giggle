6b6c989466021745e7d937af0633a2f8b7cce563
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:19:31 +0000

    test payment pollution excuse present

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
