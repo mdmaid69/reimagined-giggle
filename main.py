ee4f389929a3908703ee1feaf779ffc21fe0f3a9
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:03:10 +0000

    Bb minimum definite country although

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def add_numbers(a, b):
-        return a + b
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
