b7583229a8357761200f3f10aa235611a0c67574
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:59:40 +0000

    children cross interview broken attention

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def subtract_numbers(x, y):
+        return x - y
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
