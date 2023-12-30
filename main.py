5faf206a81271e8cfc5659e0cc9224f373e91dcf
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:31:46 +0000

    opposite exchange brown worried drive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  def square_number(x):
-        return x**2
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def cube_number(x):
+        return x**3
