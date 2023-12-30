de800423c82948e9ba07e59bf29b0d6d02d2bdb1
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:35:40 +0000

    especially refuse crash wash progress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
