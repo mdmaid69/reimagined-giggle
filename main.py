f7bc0b155509edd9b83a159cbc665e83e93801d7
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:37:47 +0000

    noise every dream tie address

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
