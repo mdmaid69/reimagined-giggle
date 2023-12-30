b66c2583ef62edab90933de3e5d904fe49b9b912
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:48:33 +0000

    tight behave since press train

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  def remove_duplicates(lst):
+        return list(set(lst))
