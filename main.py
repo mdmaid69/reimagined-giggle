8c0784c69aefb3f5ae970fbb84738f9b48a6b32a
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:50:38 +0000

    agreement fire book drawing Uu

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def reverse_string(s):
-        return s[::-1]
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_power(work, time):
+        return work / time
