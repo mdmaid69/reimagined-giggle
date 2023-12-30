5d8b2ac6664f9cc3ff1f46f6d057f7e93cec6fcc
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:45:22 +0000

    disapprove bit taste pleasant maybe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_floor(x):
+        return math.floor(x)
