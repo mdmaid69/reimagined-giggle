2820273e6daa35ae659ba07e2803d9eb095ea2b0
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:48:20 +0000

    face principle again report especially

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import os
+print(os.getcwd())
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
