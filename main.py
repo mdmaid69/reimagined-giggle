0866bbba74ffb8bdbe29c3c7873c06bae778d152
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:06:10 +0000

    false crazy sort furniture thumb

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+for i in range(10): print(i)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
