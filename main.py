29da43f0ffad0c7de39fcf157af8b92d78640369
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:02:24 +0000

    skill ordinary design attention such

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def split_path(path):
+        return os.path.split(path)
