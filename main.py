d0af9ee4ea7737b07e93b76a154f802da7867f32
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:14:00 +0000

    admire treat sticky guide uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def set_array_item(array, i, item):
+        array[i] = item
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
