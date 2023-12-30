9398c7e759900f20deeac4244d674c5c4f0eb9cb
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:01:36 +0000

    sign family thinkthrough raise division

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def add_numbers(a, b):
-        return a + b
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
