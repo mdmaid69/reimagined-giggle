cb9ea11453555e1786cabcdd25f72a3bf1b50d04
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:33:30 +0000

    polite print leg part class

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
