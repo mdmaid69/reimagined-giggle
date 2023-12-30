68c17dcc7c6e2991da4cfcb9eaae8f5b525cee94
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:39:13 +0000

    tense talkback pleased unlikely also

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_min(numbers):
-        return min(numbers)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
