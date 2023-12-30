8d037e5a65e3d54e0e245e6c5f68462617e17803
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:33:22 +0000

    peaceful injury noisy but period

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import numpy as np
+print(np.array([1, 2, 3]))
