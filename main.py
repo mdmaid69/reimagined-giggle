2fea2bb6eec7e1ac8f7a56431524977b44805a3c
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:11:07 +0000

    mainly difficult distant solve keepoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def reverse_string(s):
-        return s[::-1]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
