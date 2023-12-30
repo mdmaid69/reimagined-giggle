c4af319df2d8c9f83bdbb330edf3e8cb8aa6bc88
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:16:55 +0000

    home normal exit discuss out

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
