065bc7de764d2dbddc7e3a99ee3656557582f028
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:08:50 +0000

    scream human tape anywhere drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
