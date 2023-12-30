6fdb20b9a7051ba15c39d01e5d7cb810a3017edc
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:58:33 +0000

    settle hear dear turn determined

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def square_number(x):
+        return x**2
+import random
+print(random.randint(0, 100))
