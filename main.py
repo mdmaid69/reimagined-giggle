12da7f04f6edbd590b5c1c2949da420582f45811
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:28:07 +0000

    forward at summer attach various

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
