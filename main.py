22bbecc2d496b2d533f2604adbd4cba87fc9209b
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:57:39 +0000

    floor create roll please every

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+print(json.dumps({"name": "John", "age": 30}))
