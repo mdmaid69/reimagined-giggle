479394b00ae40cbd3f3cd397190c0e5f296051fc
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:31:09 +0000

    touch time moon brush breakdown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
