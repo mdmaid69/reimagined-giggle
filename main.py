be684c153193067ed6c4678798bf89a484f63011
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:35:44 +0000

    organization extra home excuse back

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import datetime
+def get_today_date():
+        return datetime.date.today()
