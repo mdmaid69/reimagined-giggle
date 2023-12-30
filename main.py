0b5e0f67675730aba4bb438124aa365700f65ae4
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:37:34 +0000

    married dear delight tune minimum

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-def get_current_time():
-        return time.ctime()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+print([x**2 for x in range(10)])
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
