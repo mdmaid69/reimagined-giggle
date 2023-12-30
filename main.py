45fa1d7c7fcd87e2f2e1f5444695599862f8a882
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:42:16 +0000

    walk spray goon own somebody

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import math
+print(math.pi)
