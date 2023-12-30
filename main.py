3095ba1e2499501e8e9d3b21c09e4dbab51e53cc
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:07:14 +0000

    glad free threaten school community

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def convert_to_octal(n):
-        return oct(n)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
