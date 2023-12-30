08d2e42871fc5c1c8d5fa1522ac703dbd5370a31
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:49:40 +0000

    plate limit partner rest clothes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
