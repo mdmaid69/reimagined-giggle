2fbef6ad2af298a85e1a045fb4279c9ff311a3c5
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:36:28 +0000

    confident holdon tip concentrate care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
