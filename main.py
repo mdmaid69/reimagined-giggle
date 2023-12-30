06c54b8828c14d63cabcb5c915810d169e552b35
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:37:13 +0000

    use telephone back unpleasant possibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
