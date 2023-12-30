7ae3231432b5c839bf8e74966b0d22371bcfcdce
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:34:43 +0000

    swim outside wander appeal long

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
