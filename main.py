58801d4072d4dc15604d79892a5d504fb5ac3c73
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:45:43 +0000

    language club first provide pleasure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
