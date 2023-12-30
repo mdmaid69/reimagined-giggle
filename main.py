5f8bbe3b5fc575567c306c73b82725cfb1078491
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:28:10 +0000

    copy comein standard single accidentally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_sine(x):
+        return math.sin(x)
