e47bd20145e72c45ebace437beb525a8e99dd865
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:42:22 +0000

    sensible separate past smart project

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def convert_to_hex(n):
-        return hex(n)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
