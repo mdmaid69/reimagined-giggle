551378e1d1a7de721399fb52d2d3cf7d7310cfcd
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:48:19 +0000

    content guess table arrangement nurse

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+text = "Hello, world!"
+print("Reversed:", text[::-1])
