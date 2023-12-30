aa7f86efde4a3a5fccd16eb9d640beec7cec13de
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:27:03 +0000

    cum error model sell but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
