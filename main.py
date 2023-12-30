91c9b7868a00f9ecde3b79519cf0fd51c8cbec4d
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:39:16 +0000

    keepout embarrassing direct comeback hour

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
