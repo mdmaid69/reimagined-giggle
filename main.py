5ed54d07eed27da69ed8cc9771f825e117dfc416
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:36:36 +0000

    Jj successful freedom after steam

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def subtract_numbers(x, y):
+        return x - y
