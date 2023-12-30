d92bcbb51f7e96e176062944fc92d37c9ab7f6d6
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:52:29 +0000

    land whoever either sit Ww

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_queue():
-        return collections.deque()
+def subtract_numbers(x, y):
+        return x - y
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
