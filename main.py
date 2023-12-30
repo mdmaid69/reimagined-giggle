5e7016e972d0505bd7af4ca1c9629d60252240fd
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:42:13 +0000

    frame understand special pile total

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import array
-def get_array_as_list(array):
-        return list(array)
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
