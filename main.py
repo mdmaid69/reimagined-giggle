2d626036a4acbfa97fc10eb9743092b1f91ab53f
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:34:05 +0000

    weigh receive under sound tidy

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-def convert_to_octal(n):
-        return oct(n)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
