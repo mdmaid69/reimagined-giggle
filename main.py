c27522fb83aa484ef592a66e85d78d684edbc2fb
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:41:49 +0000

    last trick female whistle variety

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
