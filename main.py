f8cb624f3bc5df774df9c2e09a5e0ce7b318062b
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:23:57 +0000

    power until suppose exist down

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
