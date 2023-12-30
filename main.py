8234027b72f69d49d844138a81124113ad446cdc
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:33:02 +0000

    step purpose company allow attach

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def square_number(x):
+        return x**2
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
