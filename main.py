5afdfdb1ca2010820af78c10942fd54429d40097
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:54:02 +0000

    identify rid maybe pass world

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
