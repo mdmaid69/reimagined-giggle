b6e4363b88c3a52c02cc30ebabc7d4d540388d58
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:07:44 +0000

    wash open answer permission punish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_list_from_array(array):
+        return array.tolist()
