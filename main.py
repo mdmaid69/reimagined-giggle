a30f228ff88906f632d4e1c69372d2734fcf3a84
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:49:25 +0000

    honest consider bet back guess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
