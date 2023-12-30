171ee4baacfdbf3d4a056374de37ee02a71ce9df
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:15:51 +0000

    uniform fetch fight secret lookaround

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+print([x**2 for x in range(10)])
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
