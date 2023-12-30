6371c9b89714af7dc841c4a362d5d18089ec2a13
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:23:59 +0000

    tidy high uncertain big young

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
