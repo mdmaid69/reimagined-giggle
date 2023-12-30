c846fbd61f6915d7e2e91344cad166af389959c4
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:02:10 +0000

    badly break petrol refuse bar

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def add_numbers(a, b):
-        return a + b
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
