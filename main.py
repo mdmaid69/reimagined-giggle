0c21ee7346b102572f933832341c60e0bf1ade7b
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:43:27 +0000

    runout lunch beat burst minority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
