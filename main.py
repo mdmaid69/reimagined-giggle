0f5f77e23c047344900a839581ca8baf305f5f9a
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:30:09 +0000

    government university letter getup deaf

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
