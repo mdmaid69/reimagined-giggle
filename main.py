08afff42020bb3fde79b90433c6da5d8c2fc51e5
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:06:37 +0000

    show impatient over truth absolutely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
