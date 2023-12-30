92093e17cbbbad8ac7741525ab5d4070535c20f1
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:40:23 +0000

    address approval influence least runout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def get_array_typecode(array):
+        return array.typecode
