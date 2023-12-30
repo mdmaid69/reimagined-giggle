22eed8d63c91214a9a735c4e2bbdf3c401c5f59f
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:25:05 +0000

    favourite blue lookup cheat while

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+for i in range(10): print(i)
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_as_bytearray(array):
+        return bytearray(array)
