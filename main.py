2552dc0174ca4030eebebf918cdc7d990e0ef1cd
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:50:00 +0000

    understand nature slice goaway start

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+for i in range(10): print(i)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
