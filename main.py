78dac60c46876330db947736e0a2521713198e17
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:47:58 +0000

    year better actual country period

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
