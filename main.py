b6f3f6a301a1926572e0a500f0ddfc6f77f18501
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:15:28 +0000

    sink police sick punish unite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import numpy as np
+print(np.array([1, 2, 3]))
