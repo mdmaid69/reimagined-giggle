a7340638cb51b1f90ea360c90083ab583cab3174
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:22:33 +0000

    uncomfortable strict loud trip smile

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
