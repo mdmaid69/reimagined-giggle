1bd2e63fb3a94f961d8cb6982bb40b7241eaef2d
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:02:29 +0000

    treatment careful impressive comeover straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
