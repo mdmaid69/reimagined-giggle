0febee8163473b7a064d9e5e61a6e7426a74ec1b
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:01:12 +0000

    trust alive nurse record feel

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+print([x**2 for x in range(10)])
