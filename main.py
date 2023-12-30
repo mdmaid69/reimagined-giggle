29ce5482117b67ae05fdf4ee0d49dc45810e372f
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:31:55 +0000

    home OKorokay suit best hate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
