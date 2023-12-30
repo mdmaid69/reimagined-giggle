1936fd9c6d3d0d45281875bffea64515deb9f245
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:29:42 +0000

    number crash create check distance

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_work(force, distance):
+        return force * distance
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
