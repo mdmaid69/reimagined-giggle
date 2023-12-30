aee397d364ef2d4895c30d742a5d76ef80d6c4f8
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:52:29 +0000

    price hole previous niggaacademy fortunately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
