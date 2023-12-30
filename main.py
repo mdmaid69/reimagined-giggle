c4fae37e83f8ff61bf1b7a74f4767bad846fe5b6
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:41:14 +0000

    firm ticket over tense damage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def find_min(numbers):
+        return min(numbers)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
