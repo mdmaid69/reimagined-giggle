2ca0d3168563a45a9ab9c163008f96513793a7be
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:55:51 +0000

    comefrom organization nose last wide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
