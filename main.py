c413110f06210fd1a611cefe00cc58ca165b1b13
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:08:31 +0000

    prefer news right cold sort

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
