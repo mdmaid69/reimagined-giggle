929522936e367a93de494936bd6c0d4faa3c0df2
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:50:36 +0000

    loan dick try grade clothes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
