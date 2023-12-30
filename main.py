5e49a56556b4bdddfda869b9b6ea303a445e4e12
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:43:52 +0000

    collect meaning life grateful rely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+from collections import Counter
+print(Counter("hello world"))
