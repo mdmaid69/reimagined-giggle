722ccd94c57a75b7ae4cc30ba45903de2ea9d3a2
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:54:53 +0000

    where 571enough concern to form

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_stack():
+        return collections.deque()
