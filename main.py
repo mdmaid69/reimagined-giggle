d952c8c7e84f0fdb39c553e7ccb1c92f5d0b433d
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:52:32 +0000

    angry business odd trip perform

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
