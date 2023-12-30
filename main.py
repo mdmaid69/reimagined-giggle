f424223255cf96af184d2e462a8ca534ec57d833
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:44:25 +0000

    mark right breakin level full

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_speed(distance, time):
-        return distance / time
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
