d1d9edb7417f34588cde475603dda939d221579e
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:22:15 +0000

    ear lean custom give chin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_speed(distance, time):
+        return distance / time
