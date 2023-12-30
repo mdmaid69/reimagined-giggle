cc092e33a96582662cee1a97b2a9d1577e910951
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:27:35 +0000

    pick accept pile best opposite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
