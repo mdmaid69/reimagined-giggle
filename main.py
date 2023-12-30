e304382ff76f5ad5ebd395e07dd2496cb4e89106
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:19:27 +0000

    inquire then choice modern improvement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
