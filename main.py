f412fc794b3f9f4e910e25c233bffacc423a7f3e
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:55:21 +0000

    interview takeback stiff connection spray

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  def sort_list(lst):
+        return sorted(lst)
