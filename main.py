3940fc88a5ee8661e92b85c00fc8d9526db892d1
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:48:38 +0000

    happy anyone trip usual tone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
