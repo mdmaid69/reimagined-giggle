b44928f13aad29e3ace090e20b661e59162c5cb3
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:01:47 +0000

    visit matter patient weigh deal

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
