0f5dcc40d8f3bed21784eac6edc5acd4d42d3ae1
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:40:23 +0000

    check fortunate range but fortunate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
