c5aa441dacf59e8603944746006bb81488d170b3
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:46:33 +0000

    start rent responsible project form

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
