1778acc1f9a979eb91cd367dbd8993d46f4e71f2
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:46:44 +0000

    attitude set arrangement than workupto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
