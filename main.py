ef39faade02b327f089e7d0388f8835ae55d7fb2
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:45:22 +0000

    sweep talkback protest risk workupto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
