5f9b2cf1cb4e50e86e52b5d32b97a1aa2503c914
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:24:55 +0000

    prize confuse mine cheap Qq

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def add_numbers(x, y):
+        return x + y
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
