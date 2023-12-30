25cde2741fd91e3f9d5e6e720c39b1faf2a9eed6
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:08:46 +0000

    add hope affect inside like

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
