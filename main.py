0c3413e86a6bfce36d1b00380d478aa6cee378a4
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:04:29 +0000

    accordingto thinkover bottom finally story

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
