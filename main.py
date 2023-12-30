d590c065ab52125407b4871d831749f2cbc8bef1
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:29:39 +0000

    this to rubbish minimum round

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
