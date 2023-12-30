3d61f86c4556ceeab6737f158d2bbfc79e007921
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:36:26 +0000

    cheap awake announce cinema answer

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   def convert_to_binary(n):
         return bin(n)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
