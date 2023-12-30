b73962c2ab0d415e51949de073240b666af28fbb
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:34:55 +0000

    fine work situation rob coffee

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
