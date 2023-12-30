c64be2d39ea4fd3215584227372dd3c81ea219b5
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:25:37 +0000

    oil sore contents from tight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_min(numbers):
+        return min(numbers)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
