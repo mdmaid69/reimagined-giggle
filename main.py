f6f1625adcff8d6d6c1bef1bda7f34310639c919
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:39:21 +0000

    just somebody properly bright outside

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_speed(distance, time):
+        return distance / time
+def reverse_string(s):
+        return s[::-1]
