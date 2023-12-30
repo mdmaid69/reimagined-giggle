1f79a9d250436eb0293fdaf368eabb2fbb9936c0
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:28:02 +0000

    equal nervous with certainly present

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import collections
-def create_priority_queue():
-        return collections.deque()
-  def reverse_list(lst):
-        return lst[::-1]
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
