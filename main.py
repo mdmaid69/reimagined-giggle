d8881ae79378f3e0312f1a93bf5727c5391f350e
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:28:21 +0000

    city cap realize lay priority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def find_max(lst):
+        return max(lst)
