837ba2bde6e999ef6da91a297a7da2d1e38b3f18
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:13:44 +0000

    autumn cap maybe visit previous

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
