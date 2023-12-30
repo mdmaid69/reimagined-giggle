dec3a0a6d23526ef2bf716ded514162633929185
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:02:20 +0000

    shape trick success software convince

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def extend_array(array, iterable):
+        array.extend(iterable)
+import datetime
+def get_current_date():
+        return datetime.date.today()
