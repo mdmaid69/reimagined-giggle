685a6a6fa22fd73cc67c85c004b46875f157ae06
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:44:31 +0000

    noise reason 567enemy Dd card

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import array
+def get_array_index(array, item):
+        return array.index(item)
