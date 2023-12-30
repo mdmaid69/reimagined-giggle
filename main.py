94c763b045c4512d5eab50ae340dba500b3709a5
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:50:32 +0000

    under disappointed tap hit unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
