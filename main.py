3a10e54d41002496053b8e61dac497a3ca78ca6f
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:54:38 +0000

    afterwards serious whoever allow professional

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import json
-def convert_to_json(data):
-        return json.dumps(data)
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
