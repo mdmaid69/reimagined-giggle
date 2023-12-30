bb548d19bc4efd1c5d40d744c2ecde8872a491fa
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:56:50 +0000

    along dream speak become possible

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
