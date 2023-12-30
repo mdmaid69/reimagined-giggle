0232a1c3f25f23663472dc3dc34face8e96ac200
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:32:21 +0000

    club book hope Ww somehow

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
