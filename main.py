6482b67f626c0294e38f480a842fc7f3cda0e45e
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:01:36 +0000

    disappear seriously scrap safety flat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
