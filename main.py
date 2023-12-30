ea7039709098daad06060df173d8af7376671bdc
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:18:07 +0000

    blue nearly gather splash since

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def multiply_numbers(x, y):
-        return x * y
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import sys
+def exit_program():
+        sys.exit()
