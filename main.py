f006bce4489dcca04709461546fb366f60784ec8
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:53:07 +0000

    before public Oo perform water

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+print(math.pi)
