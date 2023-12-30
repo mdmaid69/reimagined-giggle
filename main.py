651b5ee306460e492f056beffcca68721ed3139d
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:10:54 +0000

    stretch wet mind separate prepare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def divide_numbers(x, y):
+        return x / y
