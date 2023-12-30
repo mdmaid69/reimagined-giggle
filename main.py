dfc0498321669bcb8bb6a1baed2e4a1f95450178
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:54:50 +0000

    practise straight shut turnaway justice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
