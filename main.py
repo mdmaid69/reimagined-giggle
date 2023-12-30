004a53ef7e27be2a424f6094b55d611c4313e878
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:39:20 +0000

    unfortunate fun sea group wish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import sys
+def exit_program():
+        sys.exit()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
