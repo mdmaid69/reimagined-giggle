58f8dd260a1620dcd9bbc4e2f130a5b5031c708d
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:48:50 +0000

    list state fresh children bringback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
