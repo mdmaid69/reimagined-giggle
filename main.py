90ccbbe37fec1af8446d44fac8a8b73d9e243ec7
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:28:25 +0000

    summer study for point along

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
