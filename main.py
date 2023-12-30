4841d4a320845e73390742a87e46b9788b620571
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:41:59 +0000

    Ll wrap hope complain spare

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
