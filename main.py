f30c968d9a510e04b7fdd98774b34cc75d0986dd
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:06:48 +0000

    limit shout vanish patience can

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
