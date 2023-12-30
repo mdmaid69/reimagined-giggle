000b85b509629676fbda6b9368a0517168027b91
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:30:54 +0000

    sign wild cinema tidy official

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_power(work, time):
+        return work / time
