95023eae138a9979b50aa56e862b4717e3385781
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:26:01 +0000

    rate life nowhere test permission

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_work(force, distance):
-        return force * distance
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def convert_to_octal(n):
+        return oct(n)
