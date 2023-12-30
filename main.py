95dcb91d1e93aeebe247778631a49bc32d5f07cb
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:12:09 +0000

    platform when plastic curious lot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
