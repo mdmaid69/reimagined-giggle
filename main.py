d24e004f8f40e64b1201326e9e489ad8ecd9d6af
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:09:14 +0000

    standard walk murder cruelty would

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
