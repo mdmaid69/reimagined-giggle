cecb125aa789db282ee72c9a6ebdad3cb991217c
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:50:36 +0000

    during unpleasant authority tape visit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
