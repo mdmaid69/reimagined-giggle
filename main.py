da46b50f2f70c35afa589261a8c2043fc190da09
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:18:18 +0000

    her train rescue complaint lock

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def square_number(x):
+        return x**2
