8c34d00f47ce04ddc3be43dd94944b7b9901a175
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:43:49 +0000

    what feed whom fierce think

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
+import os
+print(os.getcwd())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
