8db4880557891f1be3f9c3ad3c714dd7ccd2db39
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:40:02 +0000

    sleep window thinkthrough generally fork

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
