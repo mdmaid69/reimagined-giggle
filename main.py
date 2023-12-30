f63c3fdab9c9b73ac9820fc64963872f3885969c
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:24:45 +0000

    afterwards regular rubbish deep bear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+for i in range(5):
+        print(i)
