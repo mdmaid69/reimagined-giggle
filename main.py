fa9f837989246704365c0cf378588410fae68059
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:31:26 +0000

    cheek up deliberate capital tape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
