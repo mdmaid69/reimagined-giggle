bc32b888f3b31fd964d1c200b4a7f01e1ecb0db7
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:57:44 +0000

    safety example block last either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
