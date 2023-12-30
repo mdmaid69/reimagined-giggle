5e901eca29fcc323eb0a0cb55ed40afab89af669
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:43:40 +0000

    instrument file leave comeback surprised

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
