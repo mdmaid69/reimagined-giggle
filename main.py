73e813f53f64692754d8abf16b55c3e648b7625f
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:21:01 +0000

    public exchange round as hang

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+print("Hello, world!")
