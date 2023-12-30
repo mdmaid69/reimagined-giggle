ba7eaac4369c7af5e6c3f793e2d92ac2271a2f39
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:21:54 +0000

    power gate shame phone seem

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_current_directory():
+        return os.getcwd()
