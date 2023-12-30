1e4ba5aaaf3a230ab3bfb9dc872dd5a5ca3bdc28
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:13:14 +0000

    throughout far cope mark authority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import re
+print(re.match("h.*o", "hello world"))
