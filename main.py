048fa18a535e0dc5771b5bcb9f4f7d7a614b7881
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:20:37 +0000

    objection about command worst tip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
