92d4558ef215666103208b311bd81596d6446030
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:21:06 +0000

    score use responsibility before collect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import array
+def clear_array(array):
+        array *= 0
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
