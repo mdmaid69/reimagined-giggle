fbf8b54a448e9ecc4a1aaaa37653c178373ea49f
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:00:07 +0000

    doctor most warning interfere thing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  def square_number(x):
+        return x**2
