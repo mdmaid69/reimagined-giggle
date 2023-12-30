0e6885343bc15bcb1628b89add6d40f3339040e9
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:26:11 +0000

    message punishment rarely mend flow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def reverse_list(lst):
-        return lst[::-1]
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
