a76639944ec0521101ce8176eab3067bf660d9e4
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:51:36 +0000

    practise pinch high bottom top

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_power(work, time):
-        return work / time
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def multiply_numbers(x, y):
+        return x * y
