3bc4884809ae869d247139f21a34fc6313cae59a
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:16:19 +0000

    needle giveback offend intend mark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
