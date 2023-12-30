2ebb197894b215cd829a10b2859ca55688a8e758
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:43:40 +0000

    remark describe hurt uniform tall

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
