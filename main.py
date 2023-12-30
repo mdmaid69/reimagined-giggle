f0e187b609e1ba17c4ed33520da7db94229a3bf3
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:06:09 +0000

    provide prepare the ability just

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
