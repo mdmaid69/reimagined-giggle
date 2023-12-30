90b9bb2ecc96dd7792b10b0f28fa3dc2a6e1be86
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:18:49 +0000

    trial mysterious record pretty collect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
