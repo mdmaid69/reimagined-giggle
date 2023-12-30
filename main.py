cc3d9222d7af344a648f2e0e7e6b74b0e8d50355
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:09:00 +0000

    card view below big arrange

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
