3abf4aac5e03c7469d8aa2140cefa24dd6cae4f0
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:59:27 +0000

    male sensitive Ee left style

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_base_name(path):
+        return os.path.basename(path)
