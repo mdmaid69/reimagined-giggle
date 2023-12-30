17f3b6bdc719552f20eab4e57bb10ed30d2e5555
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:52:42 +0000

    consist ilovedicks its crazy memory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
