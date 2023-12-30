9d81c32beede4cdbe989338539ebe6a4eb4bb4c6
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:24:12 +0000

    more 179bite feed completely tell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
