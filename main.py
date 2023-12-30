ca1c1ebf0979ca6425c40642ef89df16747a71e5
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:09:40 +0000

    minority among always each fairly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
