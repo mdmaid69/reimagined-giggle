41253dab53d886fefd337c95cabf5d8f1253ab5e
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:25:17 +0000

    correct twist immediately stamp tooth

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_acceleration(speed, time):
+        return speed / time
