e2b6fb62f1734872e62cdf8e3ae8898f230ddce0
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:50:15 +0000

    possible smoke hungry street drink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
