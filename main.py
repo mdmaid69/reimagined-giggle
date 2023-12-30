bdab9624ebd3863b93819dad4ea44af43cd77294
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:28:16 +0000

    whichever scare across die fork

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_as_repr(array):
+        return repr(array)
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_as_tuple(array):
+        return tuple(array)
