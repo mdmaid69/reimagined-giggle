c389f28a821a7e114c236b79fa2a98cee524992a
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:38:13 +0000

    port glad cap cause Qq

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
