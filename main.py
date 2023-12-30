ce49352d7d056245c687df0988551f887ddf5d3b
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:22:22 +0000

    description guilty dick way shut

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import numpy as np
+print(np.array([1, 2, 3]))
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
