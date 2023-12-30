368a9dfed352bead31cbb9182d50ba65a24c0498
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:52:45 +0000

    conclusion insist furniture act nerve

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def reverse_list(lst):
-        return lst[::-1]
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
