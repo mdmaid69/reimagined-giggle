2e8b648bc2f02af1dac7d3c29e3c09f88968029a
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:16:30 +0000

    seriously popular weekend start equal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import sys
-def exit_program():
-        sys.exit()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
