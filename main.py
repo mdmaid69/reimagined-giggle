dadab5afacc7a62726580ad3fa0535e4cb5a3ff9
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:03:19 +0000

    whoever open drawing appeal bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
