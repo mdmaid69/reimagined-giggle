ce565e0bf2c12b4042934eef0afbd6514401011f
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:37:54 +0000

    sweep turnup journey keepup Mm

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_speed(distance, time):
-        return distance / time
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
