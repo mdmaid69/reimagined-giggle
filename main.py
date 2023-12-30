0cf5e09934692f6975d18c940a25f3520cf9c657
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:22:00 +0000

    along average occasion comfortable fresh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
