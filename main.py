9e76ae71749da410e63bce33d7b819c5cc100a01
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:03:09 +0000

    separate dear condition art again

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
