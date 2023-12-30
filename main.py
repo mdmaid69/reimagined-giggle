c08a247e7aa90acc1875f3a949c951704ca5f80e
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:12:23 +0000

    wind wealth contrast yesterday space

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def insert_into_array(array, i, item):
+        array.insert(i, item)
