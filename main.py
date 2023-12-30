0e2f21e05bfba6ff75b7b3f07b8bb5f0d1ad4548
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:53:28 +0000

    contents tax mistake through strong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
