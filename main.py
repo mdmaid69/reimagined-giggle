4eb28267a4bcb8a9aec08d8ce3e19f24f346f78d
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:11:42 +0000

    beautiful getup compete each table

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
