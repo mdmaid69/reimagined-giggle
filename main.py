4e6e547cc37fff7849e5995c64745b4af9b4b590
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:35:50 +0000

    large rent old breakfast ready

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
