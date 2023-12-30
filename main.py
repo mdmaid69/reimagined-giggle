66c59db7fa1fd5ca74023b09363f524964f6bbaf
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:45:41 +0000

    aim wish error rely month

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
