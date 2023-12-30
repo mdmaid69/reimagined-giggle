d874ccf974b32469848294ebae4426bc46946580
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:51:18 +0000

    about comeup fight every interruption

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
