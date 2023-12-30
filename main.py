ae52a057475a201717d18b5f170af4aadfdd3f36
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:01:24 +0000

    rain clever supply interview shock

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
