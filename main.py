5ae4e448b222d798ca2b3b6f5081bd8175ccdec9
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:29:52 +0000

    lean joke variety reject let

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import collections
+def create_user_list():
+        return collections.UserList()
