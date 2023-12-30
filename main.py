cc006253991e023202f1abee6506fc00fd37a722
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:37:41 +0000

    where fat notice convinced store

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
