e34bfab785d51ef2ff9946bef3f871abf387bbbc
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:10:17 +0000

    last drink plastic imagine drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def get_array_typecode(array):
+        return array.typecode
