7fcc3e84685ba3c8b552d2473721ce14ccbb2c37
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:16:36 +0000

    OKorokay trip waste course contrast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
 n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
