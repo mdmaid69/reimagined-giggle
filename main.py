9061e5556148c119de1d9d0b79e42a84df4a987c
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:55:18 +0000

    term expression carry available borrow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def multiply_numbers(x, y):
+        return x * y
