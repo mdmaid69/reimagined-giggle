96afed6781156769d74b9e0413e162f8669996ea
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:17:25 +0000

    show neither reserve turnback count

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
