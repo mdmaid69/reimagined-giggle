846bcbd398746bcf16415107b8fa8398f8942a74
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:46:27 +0000

    his party manage prefer ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
