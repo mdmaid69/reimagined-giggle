cf1eb270d3b4eb75a949131561d3d4be3734699a
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:34:42 +0000

    meanwhile regard high greeting plastic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
