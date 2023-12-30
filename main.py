ba523874fb79601c233b4e128a3e8d6711cb7332
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:22:56 +0000

    fashion nonsense Cc play demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
