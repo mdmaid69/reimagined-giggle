92a6fd90003947562b0f65fce1ffb682b2eab7fc
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:14:45 +0000

    first lot stranger nearly cut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_stack():
+        return collections.deque()
