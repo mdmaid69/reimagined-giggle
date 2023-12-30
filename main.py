cab6fe66c893636f4788693951d65d0dc0051f3d
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:25:52 +0000

    fortunately relative round violent attempt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-def divide_numbers(x, y):
-        return x / y
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
