277da821753ceea3677c2abceeac83cbcaf0703a
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:36:28 +0000

    afternoon should mistake thirsty compare

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  def multiply_numbers(x, y):
-        return x * y
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
