902fd422ba53590c6bed98b14b4c94a989e2d47c
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:02:16 +0000

    rob unlucky quantity cope street

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
