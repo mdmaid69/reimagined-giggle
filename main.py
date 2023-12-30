a091bdd4845473dd6979390a30cd8be88961997e
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:59:47 +0000

    lookaround basically emphasize commit ability

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
