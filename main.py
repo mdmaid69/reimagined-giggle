d37a0ee93abfccdf0bb277a265b562b3f3b53e5d
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:04:43 +0000

    female brown dish you official

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
