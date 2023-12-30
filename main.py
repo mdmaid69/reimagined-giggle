32e4e899f77c163d748c8b7511c286d7ccd5caa5
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:53:18 +0000

    use waste later mother accidental

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+print("Hello, world!")
