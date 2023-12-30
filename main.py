d090058c60ddeba7bb95115f5ea998d547c41d54
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:14:31 +0000

    fetch mystery represent innocent seat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
