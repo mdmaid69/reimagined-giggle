03bdb60de36bd408da777decc2216bbd973e27b2
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:44:27 +0000

    slip abroad soak moon must

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
