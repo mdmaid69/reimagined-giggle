11f4aa2fd3a18cbf8bec11ea61397c13354456d8
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:10:21 +0000

    about laugh work travel pair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-text = "Hello, world!"
-print("Characters:", len(text))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import time
+def get_current_time():
+        return time.ctime()
