a31747023ee41ee84996c40a857fe53b49a2f872
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:27:40 +0000

    quickly anyone book Uu ago

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import datetime
+print(datetime.datetime.now())
