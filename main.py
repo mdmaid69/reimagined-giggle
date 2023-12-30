9f470130878b7137f54a25d1881191365e857504
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:06:40 +0000

    safety scare keen rank before

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
