5302358d33fc13d313652a61370a8a05431d63ef
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:37:24 +0000

    lock try kind crash field

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def add_numbers(a, b):
-        return a + b
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
