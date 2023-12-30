c2172902a213ccf638585d1ebad5359f25deba85
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:48:14 +0000

    destroy punishment learn really division

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
