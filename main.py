771e8274e629e47589da5af65be2313820b40b8a
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:34:01 +0000

    shout like compare serve shrink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
