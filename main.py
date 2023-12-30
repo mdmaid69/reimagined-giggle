c376dbadf8b433e9317663b159bff3a5c99e0bcd
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:08:54 +0000

    near statement roll accident law

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
