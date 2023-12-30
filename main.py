ce25047b2418dd90f79f889b32bc32100ef5d2fc
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:40:54 +0000

    send narrow embarrassing so offend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
