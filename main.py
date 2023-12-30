131a4e61f3aa427fde5e27147fd5740ebfea93b9
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:03:52 +0000

    runaway improvement concern far whistle

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  def calculate_area_circle(r):
+        return 3.14 * r**2
