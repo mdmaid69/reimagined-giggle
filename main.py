e85066e1e22f5f246654bd5dbf9105ad1d071675
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:31:25 +0000

    section print forget necessary report

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
