bd02f45d7e2399e5a30e7ed9a752caed4659b599
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:06:29 +0000

    real block case tired whether

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
