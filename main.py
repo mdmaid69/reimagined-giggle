0978189be5c00390d5c4873342bc111b7500052c
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:24:09 +0000

    quantity confuse offer straight mood

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import sys
-print(sys.version)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+x = 10
+y = 20
+print("Sum:", x + y)
