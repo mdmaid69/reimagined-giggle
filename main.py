4ab4b4e6f97415bb84214327655cb938f39b7d3a
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:32:32 +0000

    comein lunch quarter behind handle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def subtract_numbers(x, y):
+        return x - y
+  def convert_to_hex(n):
+        return hex(n)
