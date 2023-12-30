3d635ab8357a9fbdfe186243509bd8863d7b7585
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:25:03 +0000

    workupto note embarrassed confidence centre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def count_characters(sentence):
+        return len(sentence)
