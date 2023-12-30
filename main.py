43a9259c8cbe00bf3d1a6ea0a8812ea7f0352316
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:22:08 +0000

    label care about announce noone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
