871cacb49f626d17921c5d8126bc09a685f98759
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:37:14 +0000

    situation crime pool course press

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def convert_to_octal(n):
+        return oct(n)
+def count_words(sentence):
+        return len(sentence.split())
