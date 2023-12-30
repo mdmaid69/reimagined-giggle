0e9bb47931f9f35b95127abcf5ebd0c1168e6deb
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:02:12 +0000

    obtain suffer lucky roll minor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
