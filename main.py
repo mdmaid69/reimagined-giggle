14d1643da93ac61c0ebe68e7eeacf412d7b098f5
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:06:17 +0000

    getaway burn task quiet vision

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
