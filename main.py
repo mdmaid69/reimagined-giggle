0c4ae1ab4414dfe77afe5c7188b75d75e5678f9b
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:32:17 +0000

    age sleep tread fit plastic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
 import array
-def get_array_as_str(array):
-        return str(array)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
