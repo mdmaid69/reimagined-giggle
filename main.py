19737b7a9fe900667793edc01fca58d846eb98cb
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:48:53 +0000

    flight aware cry married afternoon

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
 import array
-def clear_array(array):
-        array *= 0
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_area(radius):
+        return 3.14 * radius * radius
