deffa06e371e2b4be09de580e43f81e5c385228e
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:31:05 +0000

    habit distant but age usual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
