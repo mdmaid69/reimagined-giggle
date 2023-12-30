51d01ca533ffbd507535e35bf79e81c996f77bc4
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:13:12 +0000

    turnaway demonstrate sell holdon estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
