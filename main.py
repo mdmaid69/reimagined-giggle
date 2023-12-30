36fdc8432d71f4576da3a0022cae6e89a64c82d4
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:21:42 +0000

    black bin definite term clear

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
