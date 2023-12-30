11fa266f93bcded9efd3635b98f0801c08838570
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:23:45 +0000

    equipment bad politics tight beside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
