6bda92b17c53e9242f46a65a8f3acb9089cccf07
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:53:58 +0000

    television entry kick complaint group

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
