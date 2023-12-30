2447dbe32e7e9ad8a054f78b27781fc88f57d692
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:05:24 +0000

    order light scare increase spill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
+import array
+def get_array_as_list(array):
+        return list(array)
