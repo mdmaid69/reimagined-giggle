b5cea90d003a3203e558ce780463d468c3dfdd78
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:19:08 +0000

    year minimum grateful commit law

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
