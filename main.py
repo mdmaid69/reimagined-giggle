1932141468a7f057267ddaaa14af6adb2c8f5438
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:52:52 +0000

    mine pause workout get start

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
