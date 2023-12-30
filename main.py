6c7b3cb0b199685d4343748324b5e8bfb9f9d2d9
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:16:29 +0000

    exhibition Pp disgusting look slow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
