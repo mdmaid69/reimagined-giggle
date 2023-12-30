70f40df482a973b237503b659f88fae539c80c23
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:48:59 +0000

    disapprove healthy dangerous drive tradition

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
