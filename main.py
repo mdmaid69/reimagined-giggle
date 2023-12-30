b25497b580aa8ca17e58913b12f50b6bb3acb921
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:01:44 +0000

    vote unite skin recent satisfied

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def is_even(n):
+        return n % 2 == 0
