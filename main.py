f15cadc63fe752aa68df824159fbd766d6bb0351
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:23:11 +0000

    Mm display see interesting weekend

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def print_python_version():
-        return sys.version
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
