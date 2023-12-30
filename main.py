0c52f72ef68f04b0a165e44d3f0e6b86176c25f5
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:36:59 +0000

    performance otherwise borrow bad protect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
