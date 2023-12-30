158c69d3fc680c24ce4f45d7a536b70489b18236
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:43:11 +0000

    young flat completely means pretty

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
