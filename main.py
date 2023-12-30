f921b94202704d744a9ccee2237a84b7850d312c
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:57:55 +0000

    mother pay argument survive clean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
