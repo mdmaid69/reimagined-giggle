f416acb7dd6a97a4e8e0b1ac911e04594987f488
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:02:40 +0000

    intend border reason believe that

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
