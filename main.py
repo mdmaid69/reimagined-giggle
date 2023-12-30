b517683c22d4f79c32b26a51c538af64e2daee8b
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:30:05 +0000

    shout please permission later cut

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
