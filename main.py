65526fa41cca2fe59572c6b207d95063537c648b
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:37:49 +0000

    string print difficult sound communicate

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
