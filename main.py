79b90135c18563b1f5911c3ff7bc71dad68921db
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:59:49 +0000

    laugh spring body dare miss

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+for i in range(5):
+        print(i)
