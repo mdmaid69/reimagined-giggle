c14c633a4fca1ae7f38fb3104add9e848495c8cb
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:52:25 +0000

    Vv wind loan song if

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
