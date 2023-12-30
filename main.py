d4cedcde11235f49cf6689e3df054b887c307831
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:56:55 +0000

    case hang operation aim relative

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
