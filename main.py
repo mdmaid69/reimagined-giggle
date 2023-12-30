f3acc7cea5ba66d22426a2402c1fb9d452b9715e
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:45:00 +0000

    bunch hold pour mess enthusiastic

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  def calculate_area_circle(r):
+        return 3.14 * r**2
