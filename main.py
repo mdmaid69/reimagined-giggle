62249ac57b9e8dcd858cffc57be2381172c8dc4e
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:58:40 +0000

    sign regular connection beyond mystery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
