9a4ca3824bb48591aad9484775f25af7aaffab9b
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:15:16 +0000

    bridge must plenty scared aware

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
