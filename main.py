1114d8d75cadffd99fdc63c2a1f9fb4c6a374172
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:16:08 +0000

    us live already easy dry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
