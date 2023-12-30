3f9e62f670a8cf6af5fcde104c5a31cebb897d6c
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:58:14 +0000

    talkback bringdown justice ground takeoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
