54ab7b4ccc57250b545e81f2e88757726dc26c2a
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:13:57 +0000

    greeting spin circumstance stay cut

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import re
-print(re.match("h.*o", "hello world"))
+import sys
+def print_python_version():
+        return sys.version
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
