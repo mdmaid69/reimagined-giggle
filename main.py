8cc763e54b1816f1a00f8e059616b16a3b52ea09
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:40:57 +0000

    minority search past control conduct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def convert_to_binary(n):
+        return bin(n)
