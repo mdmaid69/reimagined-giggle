c7ee9c8bcb0557235cb06db9876f44fc109a1f84
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:34:16 +0000

    scale other your regret shake

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+print([x**2 for x in range(10)])
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
