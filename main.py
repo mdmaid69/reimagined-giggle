0fadec23693c05d4e6a238b2e9e3150279da087c
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:49:52 +0000

    add hand extraordinary die false

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def cube_number(x):
-        return x**3
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def add_numbers(x, y):
+        return x + y
