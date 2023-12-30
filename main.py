cfd5e92331eb0cb0b088b33b4720b3c60b0bed83
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:17:09 +0000

    dare arrangement turn and maybe

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-for i in range(10): print(i)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def count_characters(sentence):
+        return len(sentence)
