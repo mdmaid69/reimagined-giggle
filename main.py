e4402a38984731b2ee8c7b401b11e784af33346b
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:47:50 +0000

    giveback purpose meet impatient estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
