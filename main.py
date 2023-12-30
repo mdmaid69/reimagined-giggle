86d37b67c4fe8d81c8a934c95cd9187d5d215fa0
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:05:42 +0000

    below risk cut Yy afford

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def is_even(n):
+        return n % 2 == 0
