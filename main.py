5c28bcca2493cae6f0817e256dcf9f0393141c97
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:04:13 +0000

    body club seem giveback slide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_repr(array):
+        return repr(array)
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
