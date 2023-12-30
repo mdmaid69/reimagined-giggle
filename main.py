db04f8aa8aac7b47638fa3049c68e4304903596d
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:35:14 +0000

    deaf combination anywayanyhow hear piece

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  def calculate_area_rectangle(l, w):
-        return l * w
+import collections
+def create_counter():
+        return collections.Counter()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
