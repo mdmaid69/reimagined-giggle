26c1e4fa557559aa2aed4814b387e53cce765dbb
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:25:41 +0000

    sorry punishment earth support really

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def add_numbers(a, b):
-        return a + b
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
