029c54640e77cb29df8197bfbd1de5b83e40215f
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:05:29 +0000

    obtain situation summer hair positive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import math
+print(math.pi)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
