618f7c44db737d28e8ce578a64161670872bbe08
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:43:49 +0000

    mess duty sale design bit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def sort_list(lst):
+        return sorted(lst)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
