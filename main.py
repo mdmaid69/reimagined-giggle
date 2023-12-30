13ac11ce49b5a8d56a5d0da2912e20e2ee3a0dca
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:55:20 +0000

    patient platform people interview free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
