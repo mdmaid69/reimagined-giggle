e59f9a1b1d1cd22f5865693fe0ba31b065931898
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:54:04 +0000

    cheat excuse drink full violent

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def count_words(sentence):
-        return len(sentence.split())
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import sys
+def print_python_version():
+        print(sys.version)
