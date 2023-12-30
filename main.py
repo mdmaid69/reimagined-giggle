e2b8bcc0fec37195881db8305406ce76dd34e5d1
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:50:14 +0000

    read pour worth please year

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_priority_queue():
+        return collections.deque()
