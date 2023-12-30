7d792b0f06b9f18bb4f5318433348c7a1ba0a19a
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:19:18 +0000

    roof sing read enthusiastic idea

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def exit_program():
-        sys.exit()
+def count_elements(lst):
+        return len(lst)
+  def count_elements(lst):
+        return len(lst)
