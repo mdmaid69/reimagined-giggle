5fa83fbdb4c46b07381fe093892c2658fec5abc9
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:14:56 +0000

    responsibility rank earn power wash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def remove_duplicates(lst):
+        return list(set(lst))
