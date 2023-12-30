29c455186a28f1db08ae02b24ac05922b57c35bd
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:18:33 +0000

    there hold impressive past plate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
