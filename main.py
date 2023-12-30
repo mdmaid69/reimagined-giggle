3acc3a5b32e9b1b54dfd533c7453cb7bb8485e43
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:49:27 +0000

    thinkthrough dangerous main through from

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def get_array_as_set(array):
-        return set(array)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
