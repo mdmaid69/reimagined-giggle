686c87e2ab3419bfa1f8ac1480fe413e8d90b382
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:30:33 +0000

    frightened climb cook smell claim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def is_even(n):
+        return n % 2 == 0
