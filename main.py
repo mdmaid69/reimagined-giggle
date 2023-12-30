8bf8eb3d29298a461d9ac38288be5662a8c6e742
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:42:41 +0000

    science beneath tell fact cheque

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def count_characters(sentence):
-        return len(sentence)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
