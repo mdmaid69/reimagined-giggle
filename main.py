fd5918442c1849470d6aa8d413ff9616a373c411
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:54:47 +0000

    fold produce ahead beneath minimum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
