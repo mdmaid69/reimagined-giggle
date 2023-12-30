5e84d0f5103ce308fe31762fe851af694d372865
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:04:41 +0000

    character breakup apologizeapologise brush convenient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def square_number(x):
+        return x**2
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
