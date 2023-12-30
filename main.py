71675d9eec0fdd3d851a0437841772f06a6f0238
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:39:50 +0000

    recommend spill mother win only

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_work(force, distance):
-        return force * distance
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def convert_to_binary(n):
+        return bin(n)
