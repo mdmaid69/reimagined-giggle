4e0e43e31a5e52df35a3435fff34b139134130d4
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:35:09 +0000

    appearance surprised moment small fortunate

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-def is_even(n):
-        return n % 2 == 0
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
