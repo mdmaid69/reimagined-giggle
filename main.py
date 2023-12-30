7bd6b0280344a867c4e60f9076673acb1dd88c4c
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:58:05 +0000

    goup rid greeting everything disturbing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def reverse_string(s):
+        return s[::-1]
