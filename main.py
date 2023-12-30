4c121c747bbb33a5515b247ea6fb171063785404
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:36:33 +0000

    getup communicate month address comeround

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
 n = 10
 print("Cube numbers:", [x**3 for x in range(n)])
+  def is_odd(n):
+        return n % 2 != 0
