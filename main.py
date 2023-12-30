fb85a1e9811d6c2ac5c65ef53797cb7b0136072f
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:17:05 +0000

    be discuss include wander fresh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_perpetuity(payment, rate):
+        return payment / rate
