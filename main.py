f35f6e4e3ac485bb38aa3e3ab67532b3058d8baa
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:16:20 +0000

    formal vanish move bite consult

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+print(os.getcwd())
