736258d8c8c59b644e7daeef910f534519f13d50
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:32:20 +0000

    bad discovery scene all prepare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-print(re.match("h.*o", "hello world"))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
