a516fedc58e212471e9636a0c29cf436f889334c
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:27:46 +0000

    bar ordinary listenfor mistake passage

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
