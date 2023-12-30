278473edfd1fe6c712508bdb135ad6668b53f55c
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:36:36 +0000

    word herself declaration success embarrassed

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_user_dict():
+        return collections.UserDict()
