70e592760d0445965d47202c2bae19086b9ba2c5
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:08:45 +0000

    terrible return rest still through

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
