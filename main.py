2619ca4ae26b0ebee859a6b0629431d521a1e81b
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:57:22 +0000

    wind detail eager tense pressure

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
