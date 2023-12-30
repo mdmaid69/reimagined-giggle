ef264f581119ff3a57d6087eaae7c48f15ad63e0
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:47:12 +0000

    delight connected satisfied thought can

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
