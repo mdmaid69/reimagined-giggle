aead256ec822c3fbc6008a27de95d699a3f3296f
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:39:16 +0000

    feed dead fast giveup say

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import time
+def get_current_time():
+        return time.time()
