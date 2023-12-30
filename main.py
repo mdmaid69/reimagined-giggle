3cdf2e32617504ccb5bcb5595db53c1fea93369a
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:13:42 +0000

    education try needle separate wind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import collections
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
