5689fa683088f14b5793d190d4c6e33ead36a714
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:04:12 +0000

    confuse excuse personal 566end time

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
