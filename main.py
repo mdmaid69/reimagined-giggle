5b1859d65190f8827b04634f4617f3504f18e753
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:26:58 +0000

    pool partner home fire excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
