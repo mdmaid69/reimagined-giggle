979037d7d84967519392081d40648e8cb81070b9
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:08:16 +0000

    to nor mouth available maximum

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
