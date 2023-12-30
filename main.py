436adc8cec5ccb6d0e037afa4a4737ffb7f64b31
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:35:23 +0000

    yours rush pay shape interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def count_words(sentence):
+        return len(sentence.split())
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_absolute_value(x):
+        return math.fabs(x)
