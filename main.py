271f94f50e38928ffba51b70df8d58d2b23561bc
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:58:53 +0000

    accidentally window frightened service trap

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-name = "Python"
-print("Hello,", name)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def convert_to_hex(n):
+        return hex(n)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
