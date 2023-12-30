7202b0466e4a4fe51918a64f0014e92bc63d7dbe
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:54:11 +0000

    mistake clue from careful further

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def find_max(lst):
+        return max(lst)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
