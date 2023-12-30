5b633719801239dadfed19d54e99acbd9ef672cd
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:49:00 +0000

    callup imagine basis anxious common

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+from collections import Counter
+print(Counter("hello world"))
