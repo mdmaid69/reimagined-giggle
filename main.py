7d7563cfcd4052ab4cf5f06542bdc717650b9184
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:38:40 +0000

    empty otherwise 564encourage movement wash

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def sort_numbers(numbers):
+        return sorted(numbers)
