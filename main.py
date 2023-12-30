0e686e2e68a4217a5f9fb4c33a31c463efa2ebed
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:04:19 +0000

    crash face side familiar else

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
