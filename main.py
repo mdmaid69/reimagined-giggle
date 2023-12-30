ec1d4720b8d7869579561cc07323b53fb926dc56
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:34:56 +0000

    laugh conduct voice secret window

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def clear_array(array):
-        array *= 0
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def get_array_as_bytes(array):
+        return bytes(array)
+import time
+def get_current_time():
+        return time.time()
