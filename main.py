8951218ec645b6facef1c7ca187b8aa2e2bc3ab6
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:59:04 +0000

    rule lookaround decide closed nigga

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
