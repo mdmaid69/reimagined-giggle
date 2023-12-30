92672678cb28b26ef794c900f5bdc751947bc336
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:38:52 +0000

    discovery Ww reject trick wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
