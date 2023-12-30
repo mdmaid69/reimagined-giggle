4ade7cca307591b94605ebff6a82c68b98fc7d5a
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:18:10 +0000

    this behaviour glass thinkthrough phone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_power(work, time):
-        return work / time
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
