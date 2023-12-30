05851aed2953433d991732efa4a076631d9a72f4
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:08:41 +0000

    load impression cause tool check

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import json
+print(json.dumps({"name": "John", "age": 30}))
