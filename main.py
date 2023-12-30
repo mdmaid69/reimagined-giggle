e25b72a1915aceecb173580b4600d8a5e897f529
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:52:34 +0000

    hurry pause then argue wish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
