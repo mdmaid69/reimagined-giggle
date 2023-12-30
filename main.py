747d9120001fad1e81132fb9ff096a7732168345
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:26:37 +0000

    enthusiastic float light ring shout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def is_even(n):
+        return n % 2 == 0
+  def is_even(n):
+        return n % 2 == 0
