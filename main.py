38d309a40753f6fab72eefe344d77a15ec0a638f
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:22:39 +0000

    surprised demand breakin so forward

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
