c77b8b284fe7874e34fc09a89d75afd5d26f1b82
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:34:33 +0000

    kind exhibition list show distance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
