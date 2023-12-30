ef24fad955ad965958e81d16de479e6ad8d0ec53
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:30:01 +0000

    everything takeover string home quarter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
