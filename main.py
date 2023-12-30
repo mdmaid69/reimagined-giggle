be87e4d81c2ea7caadcaf4b872d057210246fafb
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:55:36 +0000

    anything what talk production many

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
