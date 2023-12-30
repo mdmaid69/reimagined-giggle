37bfe0457e43461d240c19b1003eaa412fe80656
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:36:35 +0000

    politics should weekend cheat Ss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
