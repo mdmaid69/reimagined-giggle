2795c480f071636ec8ad8efce25562fe8f6beb58
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:07:31 +0000

    thankyou quantity murder someone slightly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_list_from_array(array):
+        return array.tolist()
