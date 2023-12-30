2209161010fc0e988f8400e6b001aefe064e1682
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:50:44 +0000

    announce interview fold 569enjoy wash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def get_array_index(array, item):
-        return array.index(item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
