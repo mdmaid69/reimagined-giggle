775019bf86d9612b7c3e84c4c6471d40c9bda795
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:29:20 +0000

    wear connect sense pause familiar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
