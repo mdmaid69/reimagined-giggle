0bb24d1f0e73f2e6a5558d0a84fd31e3c61fe418
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:51:28 +0000

    gate favour direct degree getup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
