d9057fd164a5c14fbffcd5b65dfffa435ca41213
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:04:25 +0000

    upwards noise quarter transparent dry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def is_even(n):
+        return n % 2 == 0
