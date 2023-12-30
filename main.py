db9332ec47ec40f3ffe663248d46a959ab279940
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:21:59 +0000

    estimate most address patient pipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import getpass
+def get_username():
+        return getpass.getuser()
