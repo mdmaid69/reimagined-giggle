6e6e8629634f4be141ba5a3d66460270173a6aa9
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:08:49 +0000

    benefit mass typical question error

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import sys
-  def get_python_version():
-        return sys.version
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
