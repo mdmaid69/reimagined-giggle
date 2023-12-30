dbd2408ee6baf32673bb9becc3f4f8367b3d3dd1
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:20:18 +0000

    Mm apparently fix speech definite

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
