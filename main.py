3825ebf8aae18c9a045d80061084116b47d7747f
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:38:42 +0000

    strike within material free offence

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import getpass
+def get_username():
+        return getpass.getuser()
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
