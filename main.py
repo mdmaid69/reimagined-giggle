746845f2241fea8e9c1d602ade6a5fb073402515
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:57:37 +0000

    church I spring threaten greeting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
