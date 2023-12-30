8e23a41b7f15ae08a95bede4656d19aebaecdf99
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:18:02 +0000

    fix string rich square though

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
