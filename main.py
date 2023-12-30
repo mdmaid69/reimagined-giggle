4102157f6b4590a8962dd8d0feb85e90688b49b4
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:03:19 +0000

    Yy order but repeat healthy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
