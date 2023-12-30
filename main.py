295b66e17616dac029696f13e3243ad716430551
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:41:25 +0000

    572enter train sorry precise catch

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import platform
-def get_os_info():
-        return platform.uname()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
