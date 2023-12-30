ee97f5d9c3da9d620b2c49a609937aabfaa3b555
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:54:38 +0000

    target contribute waste definite circle

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-name = "Python"
-print("Hello,", name)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
