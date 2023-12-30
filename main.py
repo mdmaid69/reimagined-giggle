5f892a868f1cc700759b75b86566d350feb18892
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:54:58 +0000

    motion spare disappointed forget flight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
