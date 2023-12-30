c9beae01b5d0880a88f5375d0b47d0f82335ddcd
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:27:46 +0000

    reach vote convinced dark flat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
