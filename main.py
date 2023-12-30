2d37d61a1a29444e7cf792d4cd457b922cb4bed7
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:44:00 +0000

    afternoon choose across strange murder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
 import array
-def get_array_as_set(array):
-        return set(array)
+def check_if_array_contains_item(array, item):
+        return item in array
+import sys
+def print_python_version():
+        print(sys.version)
