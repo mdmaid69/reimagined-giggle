71e22b4250fd3124849083d9b8f236fdf575f4ba
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:56:38 +0000

    allow region inquire lift television

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
