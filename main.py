085e6171e88597373d8b28012c14be701afa4728
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:32:53 +0000

    suddenly unit pretty Ll think

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import datetime
-print(datetime.datetime.now())
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
