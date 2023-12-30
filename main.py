70769e191e85c08e5459ccc43c5bed3ee2ea2b29
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:12:53 +0000

    government lose leg consist strict

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
