cec6bddef2b6201bdf9e40b37305ef01e4d0dbf1
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:06:35 +0000

    level aim attach visit angry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
