9f7d9d600acb88383822d48954a7760f58aabfb1
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:41:11 +0000

    alive spoil disappointed cover entrance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import numpy as np
+print(np.array([1, 2, 3]))
