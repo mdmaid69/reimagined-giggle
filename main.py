0a14b671cd6fb0460d6d892590c8706efe28bc0d
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:22:24 +0000

    limit programme into design production

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
