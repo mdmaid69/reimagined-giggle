8865fa2677ca973f2e1cdaabbbf0afcc3b2d6fcc
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:06:25 +0000

    tired friend separate destroy yesterday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def square_number(x):
-        return x**2
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
