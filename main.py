cf5565b7fde5e1b8f081348b550ebd30c79262ce
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:11:42 +0000

    jump shine underneath cough escape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
