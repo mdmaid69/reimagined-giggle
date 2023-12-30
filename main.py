75be46e43703bc1bd11b88b84b69b23a13a464b8
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:50:33 +0000

    should Ee crime best somehow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
