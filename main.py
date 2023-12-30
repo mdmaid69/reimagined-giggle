c1f9b40a5ce93d7846979403af5a93462a182176
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:11:19 +0000

    pretty stay port light again

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+def is_even(n):
+        return n % 2 == 0
