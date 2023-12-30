0acf4613b7a1c39570fa00fb7d594ef8dbeb4795
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:47:55 +0000

    let moment soon race space

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
