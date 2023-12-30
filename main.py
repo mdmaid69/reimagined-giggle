a1b35ec6240de2971dd0a3a7f8f10316beb3db4a
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:23:14 +0000

    charge steam university snow pretty

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import time
+def get_current_time():
+        return time.ctime()
