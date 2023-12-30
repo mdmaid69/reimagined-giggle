10b706291d2655ad314138c08ff18db996d90ba8
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:13:23 +0000

    finger height frighten try whistle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_work(force, distance):
+        return force * distance
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
