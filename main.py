3ea5addede59c9503251af48e4ee481ed02570e7
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:04:54 +0000

    film thanks stamp beat film

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
