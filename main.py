4b1e219a8fbb84f6a1832f4c283af00c51c6b4ef
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:20:47 +0000

    stone variety sea Uu decide

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def convert_to_hex(n):
-        return hex(n)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
