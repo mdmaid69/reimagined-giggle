3b6b7cb0aad2eb5e5c7064e233d7767768816aa3
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:39:02 +0000

    rid also yourself tonight make

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def convert_to_hex(n):
+        return hex(n)
