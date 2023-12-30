e9557dfda06ab7b86a637ac597bdc964351ca001
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:05:11 +0000

    comeout connect write available destroy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
