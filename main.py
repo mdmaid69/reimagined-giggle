b9e76d742db396afb069219005f55d6b3e023345
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:34:00 +0000

    disapprove our phone miss expensive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+print(math.pi)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
