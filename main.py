8e5521d2ab375bf6f4944d4da2f1d924414af185
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:42:26 +0000

    figure grade phone hand comeup

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
