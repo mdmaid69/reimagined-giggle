a72199e96ae9e867beb95fa1b5128abb839adb6e
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:43:45 +0000

    bringback nice matter speak use

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
