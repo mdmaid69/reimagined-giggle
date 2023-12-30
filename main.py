e37d1d7c6c3132206fda66946154ee53fdcc8d0d
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:33:16 +0000

    modern door win dress nice

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
