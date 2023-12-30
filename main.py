3150f03d9c8b9e78dfd0edd7df91362075576776
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:52:22 +0000

    back tap wherever muscle creature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+for i in range(5):
+        print(i)
+import os
+def get_current_working_directory():
+        return os.getcwd()
