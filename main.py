8769512fb22f62bbf2fa0087ca8bc15683e459b6
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:32:11 +0000

    bit unlucky sudden punishment hers

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import re
+print(re.match("h.*o", "hello world"))
