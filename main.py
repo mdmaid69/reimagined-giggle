d915f3af1bcd8788f83a03753bef45b99c3138b0
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:44:38 +0000

    intend basically profit serious loose

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
