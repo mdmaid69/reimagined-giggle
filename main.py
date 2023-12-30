167ff0fc9a3f4185eb51fc3bb63f363f9edb53d1
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:05:31 +0000

    claim married cost properly cover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
