d8346bb57932e17607da4ea48cf37415e2bb95f1
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:39:32 +0000

    mind criticize no plan harm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
