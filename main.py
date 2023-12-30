c2a7435c8bcec2f7053019e5624126fc4c5d87a1
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:19:13 +0000

    public shall anywayanyhow process flight

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
