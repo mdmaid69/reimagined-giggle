085ae0ef8df0d94aea7740313bb78f3d1cbc47e9
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:28:55 +0000

    die thinkof emphasize claim though

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_directory_name(path):
+        return os.path.dirname(path)
