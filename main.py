feceb5a654f403f427d4f98393533431ae05ff3e
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:10:52 +0000

    this finally appeal convince choice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
