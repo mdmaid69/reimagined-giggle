241df025ca0cf1d4957b775f661d6d54c99ce25e
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:51:47 +0000

    strong course Ss flat notice

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_power(work, time):
+        return work / time
