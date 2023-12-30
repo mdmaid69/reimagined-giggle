e9a04d2296da4a3d37a33c324405394157eff139
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:53:41 +0000

    police genuine capital hate I

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def convert_to_binary(n):
+        return bin(n)
