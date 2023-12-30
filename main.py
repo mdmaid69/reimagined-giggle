f0217c426fb5c9b31dab496c23f755bf0fa5f025
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:01:17 +0000

    ball leg place boring suggestion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
