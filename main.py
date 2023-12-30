cab238e71d765bc9c6c243f5d566f0faf8fc9e9e
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:29:56 +0000

    keepfrom scrap walk return admire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
