3c98852b886226aa5ad44b66b0fa3162db6ce21c
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:09:51 +0000

    need distant poor develop but

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
