1f0029d327897d300e3de8fbab13a34342dae2c6
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:52:52 +0000

    read summer peace pocket intend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
