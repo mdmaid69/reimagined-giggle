fc6e07f56f5bec643516688a3f2bac688a194415
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:18:46 +0000

    breakinto take company soak exam

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
