594777b2b45e0f910a4576018b8f96b3b6d1660f
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:13:19 +0000

    tomorrow yet successful prepare level

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_itemsize(array):
+        return array.itemsize
