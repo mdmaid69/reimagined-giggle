7652c5e3d652dbc7fc0b1800f5236290788b818d
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:05:15 +0000

    every adjective join so Ww

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import collections
+def create_priority_queue():
+        return collections.deque()
