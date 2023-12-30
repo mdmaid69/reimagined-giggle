acb5ed01a8295e3206705c4ea70d6a250b27030a
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:15:39 +0000

    rush Qq watch sale schedule

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import sys
+print(sys.version)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
