c0351acfd58fc209f364019caa95add5dceb0c8c
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:38:24 +0000

    Ll equal clear means ignore

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
