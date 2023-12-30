baccb3b9adf030e40959250b56e616cb86c615d8
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:38:42 +0000

    Qq poison contact single middle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def convert_to_json(data):
+        return json.dumps(data)
