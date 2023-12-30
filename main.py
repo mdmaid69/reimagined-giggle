3a70bb08d1e9d8ef3d7e94a8bba6a3edbe5b0127
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:25:09 +0000

    contrast drink machine disgusting expression

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
