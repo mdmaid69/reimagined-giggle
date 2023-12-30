6d2e81951aba36079556323f014d5f457b347b7b
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:08:23 +0000

    only mark actual drop test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
