184e2da8781e285f666066009b5ae4c08ef2e662
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:05:12 +0000

    exploration alone connect suddenly bottom

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
