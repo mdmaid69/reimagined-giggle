1cfb232c233a38d350163f6eb7c30b10a65fdb9a
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:17:55 +0000

    again rain team fast keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
