9236c5765425b105381c4ab6f70e7ddf0ea95351
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:47:26 +0000

    imagine building well traffic tall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
