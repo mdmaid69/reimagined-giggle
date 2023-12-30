5eba0e66c0aa2f2b3670dc43fc3ffcb5f55d5cb0
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:27:08 +0000

    Cc bringup organization turn major

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
