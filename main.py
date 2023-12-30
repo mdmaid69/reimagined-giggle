20d9376a8f65630540b2a17b8f51c1cdcb30edb2
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:59:09 +0000

    statement by bunch except while

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
