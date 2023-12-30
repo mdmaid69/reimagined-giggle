6974ccf926b6d3eafd8fdba70a4fe8f5d5aad14b
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:55:58 +0000

    seat quickly wave wrap frighten

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def subtract_numbers(x, y):
+        return x - y
