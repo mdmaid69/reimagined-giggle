480fbd1bd4a64b0ef322d04118e63f518c1542af
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:02:04 +0000

    combine tough connected deliver to

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  def reverse_list(lst):
+        return lst[::-1]
