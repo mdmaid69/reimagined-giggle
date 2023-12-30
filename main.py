7c05e26dcebd5e625cadda637cff260b3e4bd5ea
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:30:17 +0000

    her success anything letter force

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_list(array):
+        return list(array)
