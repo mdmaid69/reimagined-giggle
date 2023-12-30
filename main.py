e270d3edfe407df4d968c317a70cd561d9105bae
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:03:30 +0000

    joke repeat sleep very complain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_min(numbers):
+        return min(numbers)
