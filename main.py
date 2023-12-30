2e0a549d686139a81ad061bab6c563944e34e28b
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:55:02 +0000

    group sign score getup Cc

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def is_even(n):
-        return n % 2 == 0
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
