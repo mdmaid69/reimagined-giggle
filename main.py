fde3d1dd16f3b4e56efb532f6a9552339262f673
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:11:13 +0000

    ride warm wash hospital tight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
