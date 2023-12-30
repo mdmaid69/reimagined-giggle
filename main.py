e371c7172bd2ac87c1bdb4cab776c5180fe6c506
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:09:32 +0000

    copy cry left reliable full

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
