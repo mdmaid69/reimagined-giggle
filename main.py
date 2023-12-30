72de52cd2232467ab8f402d1420282f06bad8243
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:31:57 +0000

    usual mean moment apologizeapologise regret

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
