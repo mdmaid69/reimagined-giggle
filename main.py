98f8f22c0fa038e64d4c8dee80d15e069d729915
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:37:32 +0000

    rank small goon there pocket

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def reverse_string(s):
+        return s[::-1]
