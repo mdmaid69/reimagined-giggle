b0a905ccb255b7095596b21bf1dd8d0513abfe96
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:13:49 +0000

    round callback bin mistake among

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import collections
-def create_user_list():
-        return collections.UserList()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import sys
+def exit_program():
+        sys.exit()
