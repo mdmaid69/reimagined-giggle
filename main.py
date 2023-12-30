4e4c055f862cdbeb2ff1130c30d0a66463a88df8
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:26:12 +0000

    pay often spray under together

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
