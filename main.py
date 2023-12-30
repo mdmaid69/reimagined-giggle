c5dabb4d558c4ee7a58569602226861f70c01f80
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:48:47 +0000

    pretty ticket increase yet new

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import platform
+def get_os_info():
+        return platform.uname()
+def remove_duplicates(lst):
+        return list(set(lst))
