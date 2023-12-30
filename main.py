855a7a291afaa165a23615701b7d8aaed72d054f
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:36:46 +0000

    bend arrive makeinto stick level

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def remove_duplicates(lst):
+        return list(set(lst))
