195029028509bfd5f2d5c939369fee01570a3dd2
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:04:04 +0000

    hand business leg simple getaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
