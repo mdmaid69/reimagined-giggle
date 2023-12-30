0512f0820762cb9b2c0ea3eeab8b3ff41a8d3a27
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:23:29 +0000

    meal bottom always work willing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
