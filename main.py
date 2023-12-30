f4498c6f7d65e53dcf4961824b7fc9c489880a22
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:47:30 +0000

    more behave 573entertainment error judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
