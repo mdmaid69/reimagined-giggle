c1f65e6aa32caad3bbe514118125bfdb4404bcff
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:19:59 +0000

    broad relationship confused second right

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def reverse_list(lst):
+        return lst[::-1]
