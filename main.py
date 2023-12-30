61a725cb31e4559451e2d532e33e261c396bcee7
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:09:10 +0000

    dry Rr fly bin Vv

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def add_numbers(x, y):
-        return x + y
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def add_numbers(a, b):
+        return a + b
