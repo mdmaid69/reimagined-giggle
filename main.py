b5136c974cd9710187414b3ac11629a0c2395d02
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:07:05 +0000

    knock expect however nonsense agreement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
