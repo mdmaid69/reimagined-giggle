1529fdd570a607de1bfd2bad658861273411ec85
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:56:33 +0000

    defend crowded personal welcome persuade

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
