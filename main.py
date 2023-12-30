3838849b2c6f44e85a8f3d9f38f1013d1001cf61
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:38:01 +0000

    speed pot apparently bury apparently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def reverse_list(lst):
+        return lst[::-1]
