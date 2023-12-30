53bd5b0e880f3d50f56b85300ff9487abcafa2e1
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:59:50 +0000

    write begin lunch repair light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import tensorflow as tf
+print(tf.__version__)
