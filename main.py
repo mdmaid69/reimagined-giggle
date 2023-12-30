b392b17bef1acf811421f27299be1617410bbc72
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:24:42 +0000

    dream far breakthrough leave callup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
