03845b6da87a1cb9267fa1f1c79a13a28b3f63f5
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:29:12 +0000

    combine lookafter pour issue support

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def is_odd(n):
+        return n % 2 != 0
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
