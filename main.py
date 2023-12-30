ccd8c8dba86970260ead45dcda1ce9f211948b5a
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:53:03 +0000

    support respect temper tragedy beneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
