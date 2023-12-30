96559d84f1ee53c0dc80cc730d0ad5a93694b3e7
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:05:19 +0000

    pause minimum travel active us

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_string_from_array(array):
+        return array.tobytes()
