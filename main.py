24bf8b2f96e9374ce18ae043f1ee24c30618791b
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:58:37 +0000

    only disappointed right excuse throw

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def multiply_numbers(x, y):
+        return x * y
+def find_unique_words(sentence):
+        return set(sentence.split())
