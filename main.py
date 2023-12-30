b59279649008d52921938d82a8e73992b017ab49
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:30:52 +0000

    over bet somewhere telephone scale

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_max(numbers):
-        return max(numbers)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
