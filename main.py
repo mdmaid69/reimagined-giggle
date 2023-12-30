2c05088bc0e8052cf3b8bb88690c2aad4f56b0bc
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:17:23 +0000

    correct dress rent pressure lift

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
