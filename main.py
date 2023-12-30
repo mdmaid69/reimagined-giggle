d4a0d6d91bf34bb9f1246d3264d7c821f00adf63
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:48:38 +0000

    full abroad development view fortunately

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def convert_to_binary(n):
+        return bin(n)
+import re
+print(re.match("h.*o", "hello world"))
