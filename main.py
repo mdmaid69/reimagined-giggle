4a0816ff751d168948945df8eec9709bd4577742
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:52:39 +0000

    turn hear call murder compete

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
