edc84af16d2cc17434ff235196a63751be8a193a
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:26:42 +0000

    edge direct negative squeeze he

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
