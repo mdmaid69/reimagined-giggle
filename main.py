394159c704997d79fbd4a8fdc6fc115ed2bc7dd5
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:43:13 +0000

    pure variety quantity name knock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
 import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def check_if_array_contains_item(array, item):
+        return item in array
