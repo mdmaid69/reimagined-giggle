6a89ec3331d0c706099ae2338e82dba3d7d5e58f
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:16:12 +0000

    rubbish design necessary comedown now

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
