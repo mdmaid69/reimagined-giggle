9d92aac18b08818801755ab96dcb8c03c3593d52
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:36:57 +0000

    later Ee kind black crash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_counter():
+        return collections.Counter()
