27a4074117fdff743982f0beb553f9a3b2dc3d09
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:42:36 +0000

    ordinary matter standard burst influence

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-for i in range(10): print(i)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def convert_to_binary(n):
+        return bin(n)
