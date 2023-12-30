e5cdbfbadda352d47a841eba56deecd71a477527
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:21:57 +0000

    pin sky claim trouble surprising

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def convert_to_binary(n):
+        return bin(n)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
