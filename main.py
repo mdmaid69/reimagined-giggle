42eeb7c9b4ef4aa2ee6295cacf1448a6249ed932
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:27:33 +0000

    back single lead crowded start

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
