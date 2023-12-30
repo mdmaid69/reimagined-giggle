19221b53c45d1b49efa8286fc1d29a4c8fe92c42
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:26:29 +0000

    tax again grow young hurt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-def convert_to_hex(n):
-        return hex(n)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
