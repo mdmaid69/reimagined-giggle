43f1074b8a341c402bea2e352aa2aeee57a0c06a
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:17:52 +0000

    feeling far surprised raise fine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
