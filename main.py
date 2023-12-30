cc5d6e2da3deb84bfcaeafd2be25123e36cf25b3
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:23:11 +0000

    present mess run exact judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import time
+def get_time_since_epoch():
+        return time.time()
