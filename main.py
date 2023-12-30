5a6f44c2b265ece3616079a7520359ee3062f39c
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:01:01 +0000

    several occasional fold serve imagination

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
