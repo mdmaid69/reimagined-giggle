f2ecf2d699e30bf77e2bb532568ffc7ef553f0ad
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:46:34 +0000

    ear unpleasant shelter takeoff interesting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
