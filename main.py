cedc5f21d2b61ca68764722981a61528470691bc
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:18:59 +0000

    dinner for cool nerve cheque

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
