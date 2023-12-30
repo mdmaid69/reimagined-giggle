5a707f20b39d041d65b14901a3249441e4016345
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:11:40 +0000

    near develop ocean immediately bottom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import platform
+def get_os_info():
+        return platform.uname()
