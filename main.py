5efa57dc41d693b6b99d35d0742c1bf543b710d4
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:26:05 +0000

    properly prevent whom thing Ff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_current_directory():
+        return os.getcwd()
