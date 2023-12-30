da0c9450b97e6bf3a99719e638c2225c1e46bbc5
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:07:35 +0000

    pair operate own accident guide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
