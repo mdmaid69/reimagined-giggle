fe0ed0163a2e5857c90ccc4ac0f85971a8559d4d
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:21:20 +0000

    every beneath goback(to) succeed Ee

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-name = "Python"
-print("Hello,", name)
+print([x**2 for x in range(10)])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
