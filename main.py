7885499499630da51ed304f57c55e80b165cef60
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:08:14 +0000

    business admire straight book miss

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+print("Hello, world!")
