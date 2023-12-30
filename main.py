45aef919ad276e16a93895c182aaf900ade6fa26
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:06:54 +0000

    mind agreement combine invite plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+print(sum(range(10)))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
