24632ccf06652de7c8d8dbf59f3de31829e5557a
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:18:59 +0000

    dark bill same listen angry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
