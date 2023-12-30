78f1c4b0c952b4513ee0edbd7f6dda6ba685ae36
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:28:22 +0000

    except since bound punishment crowded

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
