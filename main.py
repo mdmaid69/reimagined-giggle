aa1ef896a1f9a9c6033908d380f4ddce03c0dfd3
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:05:45 +0000

    Vv square release drink effect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import sys
+def print_python_version():
+        return sys.version
+import sys
+print(sys.version)
