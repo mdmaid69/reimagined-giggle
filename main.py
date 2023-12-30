daa6643e952354adadc45a4adb0c821d4f5a5577
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:35:46 +0000

    straight temper write part lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
