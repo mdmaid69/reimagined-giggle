eb39b19a7519e3709e303ec24c253f6acb0a0d4c
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:18:21 +0000

    scare adjective used ilovedicks good

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
