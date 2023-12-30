44f0fb19e15935f5f2f647be14aff0e5d68f057b
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:43:56 +0000

    bottom information go visit wire

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
