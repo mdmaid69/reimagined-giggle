e046a4c746e51f49d042c3e5105e0313065e5374
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:52:41 +0000

    law nonsense stretch bottom sport

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
