3d3442451a3f28cfd05be0558d6c72bec9c8bdf9
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:39:03 +0000

    complicated comeover there various none

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
 import array
-def get_array_as_str(array):
-        return str(array)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
