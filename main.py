5c35a224e4ee0b1f1272d7251ac5d415a268fe64
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:22:15 +0000

    clothes song cause test run

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+  import os
+  def split_path(path):
+        return os.path.split(path)
