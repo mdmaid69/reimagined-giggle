3c833e5b664865fc2997d5809b4df15ee3c708f9
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:33:48 +0000

    raw let black weigh area

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
