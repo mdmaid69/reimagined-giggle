10edc3422a388a460d07c082664f920c4b1700b3
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:02:59 +0000

    comein seriously earn yours swimming

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
