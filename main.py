a43c042a30c9009084a35f2772f07a9603b95951
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:49:44 +0000

    proud anywayanyhow practical many load

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def reverse_list(lst):
+        return lst[::-1]
