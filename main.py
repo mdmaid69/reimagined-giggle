5cc1850a1928e82f4c1e88c1afde9f9cf594de4e
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:49:26 +0000

    tired fault rescue month exercise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import sys
+print(sys.version)
