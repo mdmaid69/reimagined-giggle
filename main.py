afc32999e3dbec2c1f1a325d8da9dd9db4bdbd9c
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:33:31 +0000

    left but land comeback vote

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  def calculate_area_circle(r):
+        return 3.14 * r**2
