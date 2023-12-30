ff8b2e68015e379dde721519987e21645054a157
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:54:22 +0000

    practice worse temper back cheque

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("Hello, world!")
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
