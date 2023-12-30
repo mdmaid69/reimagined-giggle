ac938ad37e81dd020f7cda89bf91f9364f19047c
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:11:19 +0000

    hand add convince final failure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def count_characters(sentence):
+        return len(sentence)
