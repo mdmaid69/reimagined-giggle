99330e890013bc9a332c999d727c692ab1661ef2
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:19:58 +0000

    shadow teach bringforward square money

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
