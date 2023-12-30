964144c8cf074ed82012cfa90a439500b42213ab
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:07:50 +0000

    rather appearance judge lonely exact

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
