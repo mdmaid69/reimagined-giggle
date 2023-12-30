415a887ae3b503b93555cb292507ff20e628f19a
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:31:32 +0000

    especially see style whichever want

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def remove_duplicates(lst):
-        return list(set(lst))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
