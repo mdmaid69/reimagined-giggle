cf0fabd17245141c68b5807a5a3c42a5c3dfe283
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:15:18 +0000

    description couple clearly responsible stamp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import collections
-def create_user_list():
-        return collections.UserList()
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import getpass
+def get_username():
+        return getpass.getuser()
