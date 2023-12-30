4c757729a03670bb137047540bf050418237ef08
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:22:28 +0000

    Yy communicate relax where fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
