4a2d00c04dd94d0353239cb4c8c868acec8c28a3
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:47:16 +0000

    holiday disturb with wind post

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
