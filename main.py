504171bbd2c919de82aa63b40772c50d555b71b3
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:05:55 +0000

    worth run winter group high

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
