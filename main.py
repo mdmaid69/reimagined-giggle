ef2462f907e2d75fb672f0fb6e0e1a011f66a8ba
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:00:51 +0000

    strange good meaning warm pupil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
