bab22867e55bd5fe86fc4aa16b734cb7ed200666
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:25:21 +0000

    use yours fierce Nn marry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_bytes_from_array(array):
+        return array.tobytes()
+name = "Python"
+print("Hello,", name)
