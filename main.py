6de77991838a4d26f193fddddc7a3aebf176bc51
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:53:54 +0000

    after road maximum find now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
