4115747c4ba8c6756f4993de86a7bbb76cc65bb6
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:56:07 +0000

    block common inquire twist note

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
