30f0a70842be5e95fcefb49810bbd47cebbac42a
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:18:11 +0000

    play greeting Nn identify their

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def subtract_numbers(x, y):
+        return x - y
