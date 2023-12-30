c0370ffa47d3df5490218091d6a6699d59f06cfc
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:51:01 +0000

    clear paper during ring make

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
 import os
 def get_file_size(filename):
         return os.path.getsize(filename)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
