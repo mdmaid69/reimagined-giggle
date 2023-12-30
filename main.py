50e24dad5840e255b6d413b754fc853b00fe942d
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:38:38 +0000

    family truth where will accept

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def split_path(path):
+        return os.path.split(path)
+def is_palindrome(s):
+        return s == s[::-1]
