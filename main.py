9a764326a4463b29d897aa8c949e5f592637b520
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:44:44 +0000

    past weather right odd switch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def greet(name):
+        print(f"Hello, {name}!")
