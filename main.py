8ee16224a3c9172ab5ac449e596a9cff2646901b
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:59:29 +0000

    drop team but slight owe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
