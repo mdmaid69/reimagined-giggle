43fe6ef3372478e692f9e2568787c49429cc85d3
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:46:32 +0000

    more lie train comment pity

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def convert_to_binary(n):
-        return bin(n)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
