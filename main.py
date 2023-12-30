806c38d7d9f6d76d50d378588492ab317fcfb0de
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:05:16 +0000

    sale match giveback behave sick

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  def is_even(n):
+        return n % 2 == 0
