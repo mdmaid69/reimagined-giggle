b91cd6a015aeea19b5e288c312a0b82668838f76
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:22:45 +0000

    make true talkback course emergency

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
