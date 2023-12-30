182f689780b601b42dd5dcfe818a91c141034c6e
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:40:18 +0000

    bringforward your upset smell bear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
