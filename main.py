fd065e9bec877c1fa4ecdd602c90b94348a2afb4
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:34:19 +0000

    middle smell that concerned factory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
