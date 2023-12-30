ca91c0bb1ecf97b95a236083830b21b50650b123
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:34:09 +0000

    private invitation modern on rank

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def is_odd(n):
-        return n % 2 != 0
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
