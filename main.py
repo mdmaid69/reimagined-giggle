94edc55b3491658fd2729bdbc84182c4b87ff99e
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:27:06 +0000

    opposite way whichever Ss beyond

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
