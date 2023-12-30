247d6b33c7334378fd28055cefb871e5969f4e7c
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:41:10 +0000

    clothes everything majority avoid mend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
