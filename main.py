587918d665a7b5890e8cc857405a4e4f3f8fcb31
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:36:09 +0000

    write mystery shrink comeup pronounce

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import numpy as np
+print(np.array([1, 2, 3]))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
