5f964e21197a755106c40231704ca62a952e5acb
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:47:54 +0000

    rather speak rob gentle attention

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
