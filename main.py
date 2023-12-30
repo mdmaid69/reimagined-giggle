1be131959763bdd305423bc5dccf6f8787e8a97d
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:58:14 +0000

    safe pupil hollow its police

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
