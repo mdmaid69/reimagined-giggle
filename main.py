6a575c11b0fe2f4bb5da29ab01cb091f491462cd
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:19:58 +0000

    ring describe talk advise cut

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
