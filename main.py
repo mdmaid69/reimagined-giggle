24ce3f65bacaa22516b2d202ce0d210d63738531
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:10:12 +0000

    computer cheat entry protest leg

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def reverse_string(s):
-        return s[::-1]
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+i = 0
+while i < 5:
+        print(i)
+        i += 1
