e7d8597ef115947c3e4850f53c5862814049e4a4
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:18:03 +0000

    food combination speak favour destroy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import math
+print(math.pi)
