23e27ae954dfed171ab738751abd785e3ee01efe
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:38:14 +0000

    private flow himself claim butter

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
