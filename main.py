61cae4e513cd25f597468f27c448948446d6e8d6
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:37:08 +0000

    spot return tend result scheme

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
