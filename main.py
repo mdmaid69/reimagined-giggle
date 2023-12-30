6367a25ee04b9ef986f4ec9dd141ac973ea3dc5c
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:17:16 +0000

    innocent somebody innocent turnover yet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import sys
-def print_python_version():
-        return sys.version
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
