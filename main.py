dbce4fd87a42b2f055242605084b5f9b04686239
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:13:39 +0000

    comment both straight criticism unit

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def calculate_pressure(force, area):
-        return force / area
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
