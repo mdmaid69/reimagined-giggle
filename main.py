c516e1a8db4837163c5e60a00a37d625719ba9e9
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:28:47 +0000

    autumn appeal disturb objection regard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_as_set(array):
+        return set(array)
