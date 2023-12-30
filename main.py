792ffa88ecb9cd35f6c544ebb918a3f0a36efc0b
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:00:16 +0000

    coat feed doubt ability talkinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def multiply_numbers(x, y):
+        return x * y
