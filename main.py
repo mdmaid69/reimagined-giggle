27e8ec5fa8f8fba20a5b17ecd6a905526fff999f
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:37:34 +0000

    cruel book couple bin please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
