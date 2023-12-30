35dfa9b64a53a43e44632ded0ef9286261114a58
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:59:33 +0000

    separate runover than except pretend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def get_array_as_set(array):
-        return set(array)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
