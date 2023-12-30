8711b1fae273faeb096cfc2cfeb25582dc947844
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:37:13 +0000

    protect into and wait use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
