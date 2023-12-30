835aa9fa04f3fed05f4f8819e815a5f15c44f1e3
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:45:28 +0000

    same flow just dress innocent

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
