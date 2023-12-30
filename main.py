2b62f7d03961041dd63cffaaee599a801aa6a366
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:30:44 +0000

    congratulate score damage print truth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
