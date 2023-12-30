8d0bb0e0685957e0e31e35bec45f23dff0cdcf4f
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:55:41 +0000

    then defend 179bite send reason

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
