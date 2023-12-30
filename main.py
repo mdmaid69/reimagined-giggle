3d2c7a4e4a577b52839846aa14433a738b24c6b1
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:43:55 +0000

    raise welcome spring nonsense no

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
