a15ee1db002654e05d4d89e721566f991276fc3f
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:02:36 +0000

    pattern calmly grateful physical post

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
