f7a395f0c84bb7e934774085d5b283d671876337
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:42:14 +0000

    instruction rare offence kind realize

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
