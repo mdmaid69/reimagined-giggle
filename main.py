377c533b617f9aebd2277691af0f77a9da5bd09a
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:10:47 +0000

    this mend bang discussion standard

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def reverse_string(s):
-        return s[::-1]
-def find_min(numbers):
-        return min(numbers)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
