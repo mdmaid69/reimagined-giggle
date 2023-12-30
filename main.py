3dca915393b9796a049c90055adf432262fffeb0
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:06:36 +0000

    play swallow bringup feature breakinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_float(array):
+        return float(array[0])
