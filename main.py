8e938be9f0e6c20f580aade84d80b9a32218f35a
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:00:48 +0000

    peace trick board fault theirs

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
