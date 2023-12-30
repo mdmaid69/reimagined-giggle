0214e6a3474d2476b04545335d13799c8e05ce3c
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:02:42 +0000

    order sleep drink whichever organize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
