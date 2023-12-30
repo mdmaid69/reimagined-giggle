c21ea6bab95c8aed4b899b2a8f39c64c0af1a535
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:23:06 +0000

    book suckme owe bringforward wrong

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
