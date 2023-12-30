8f2355085dc8ae70ff3718d1930f2d482a72bf3d
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:33:57 +0000

    steady reach our black he

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
