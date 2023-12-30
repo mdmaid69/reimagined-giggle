b01cf330a21b3aaaf1a7c722f3950aad9196803c
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:04:19 +0000

    confidence laugh shallow exciting strength

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-for i in range(10): print(i)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
