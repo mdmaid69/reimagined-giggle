6c9320b50e2fe489433adfc7e986f10f28ff9300
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:08:13 +0000

    connect drive favourite blow silent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
