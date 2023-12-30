250f67348fdc364bb88c32b58860612bcbeb32ee
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:59:43 +0000

    pretend gentle workon something lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
