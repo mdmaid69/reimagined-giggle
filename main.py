97d27c269697193c9c95a710e995f05c3474538b
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:12:42 +0000

    square bound runaway perhaps extremely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
