84be1a0e93f36fa420d6102c93ca3e54f9060203
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:16:10 +0000

    tread young estimate announce really

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
