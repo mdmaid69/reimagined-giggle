7957ab567d8f41e7a378f2e1c63c13dc82cdb01a
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:50:34 +0000

    naturally sail few exchange rather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def convert_to_binary(n):
+        return bin(n)
