18fcca858378dc3959c2953824319a8ed63d1f17
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:57:56 +0000

    error plenty command turnout forever

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
