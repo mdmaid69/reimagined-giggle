ed45f92624d2fcf85ae6591ef568cb4878fef2f5
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:43:50 +0000

    recognize spend cross start once

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
