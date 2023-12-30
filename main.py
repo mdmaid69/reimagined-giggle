e5c11b24b780403ad976b3cd777b993522a98850
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:22:16 +0000

    smart what discovery pressure shout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def reverse_array(array):
+        array.reverse()
