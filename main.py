5142f22b4ea1a2599abcfe2f092a1226a6b8718f
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:07:09 +0000

    claim label speed though miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import os
+def change_working_directory(path):
+        os.chdir(path)
