2e3605975c5a6008fba47bd0d5654bf36ee04e4f
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:48:52 +0000

    height heart dick flow where

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-print(sum(range(10)))
+  def remove_duplicates(lst):
+        return list(set(lst))
+def reverse_list(lst):
+        return lst[::-1]
