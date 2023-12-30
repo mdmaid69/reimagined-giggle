31108cb8b7a694acb7da9829eddca844f1fcb271
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:26:28 +0000

    little somewhere criticism season breakin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
