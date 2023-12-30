a2de46d05e2d27c59a31d427d4910338db9ec060
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:20:54 +0000

    turninto payment town Oo idea

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_area_circle(r):
+        return 3.14 * r**2
