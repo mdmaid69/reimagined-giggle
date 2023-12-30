d7aca48dd44ef86469d59a3921a3b681916d15e1
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:31:05 +0000

    promise when suit scream deliberately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
