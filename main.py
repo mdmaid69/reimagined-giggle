a2ee8d67bbf04617e528c94f7fab735e46676f88
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:49:51 +0000

    use question extraordinary our happy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def subtract_numbers(x, y):
+        return x - y
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
