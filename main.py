27a8ed809b9787c3fb733c6aad60ea6aac5c6459
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:33:08 +0000

    national juice Hh behaviour it

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_density(mass, volume):
+        return mass / volume
