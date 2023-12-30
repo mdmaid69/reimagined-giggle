6145f4bed2bb7f5cbd6f7535035d5c15f5892105
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:14:18 +0000

    consider bit sure temperature hit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
