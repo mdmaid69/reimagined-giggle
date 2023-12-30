5d58c21ec0fa9ba30d761ebb32e95eeb3edc414d
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:45:59 +0000

    relative way middle understand effect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
