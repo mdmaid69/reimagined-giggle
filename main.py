f9278af3b2815eaf186b6dd9b71d36cc88ce3934
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:46:34 +0000

    noisy any service complicated goaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
