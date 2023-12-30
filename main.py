534848c90c2d71a6f220aa51ed86811573c0f3bf
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:58:45 +0000

    error moon wing try earth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
