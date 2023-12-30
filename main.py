ae0d3c82827b57f58fd500d94e4726b32ab3a625
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:34:37 +0000

    total value late rid distance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
