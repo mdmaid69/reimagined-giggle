8bf90352d6587cf374ece58020c43b0427c862b4
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:57:38 +0000

    prevent strength fresh wash willing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def get_array_slice(array, i, j):
+        return array[i:j]
