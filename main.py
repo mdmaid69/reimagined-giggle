7978f4d0cd7317628b5695bac1d9ec96eecfa6bd
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:50:49 +0000

    pupil minute their exactly term

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import numpy as np
+print(np.array([1, 2, 3]))
