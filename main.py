db6d92435c48ba1f7ca63411e8fc26d9ecbddbac
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:59:04 +0000

    transport handle ask generous correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def is_palindrome(s):
+        return s == s[::-1]
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
