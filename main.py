e2aae6625eaebe6d08b8e149694a241cc9da7cd2
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:32:26 +0000

    in respect supply air beautiful

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
