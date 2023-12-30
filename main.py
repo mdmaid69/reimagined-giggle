c20f8f469cb4af7dd2cde771f0b36a2d0dfa1299
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:03:43 +0000

    reduction within wave switch glad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
