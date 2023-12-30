a38dcde45153925daf79db8e375e1d25250f5b23
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:55:51 +0000

    cover claim independent book dream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def is_odd(n):
-        return n % 2 != 0
+def get_array_as_str(array):
+        return str(array)
