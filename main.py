6524502808bb944b02b528541b2b2626e375beaa
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:14:08 +0000

    interrupt control drink cheek sleep

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
