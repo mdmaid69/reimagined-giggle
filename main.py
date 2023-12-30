1de01ed122c27415da41ce4afc699fa0d21bd721
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:31:26 +0000

    getup complete nervous female obtain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_max(lst):
+        return max(lst)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
