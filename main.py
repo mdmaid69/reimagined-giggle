68ee2d6d73b75aa441ddcfdd90ccea484fc8058c
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:49:38 +0000

    maybe Pp plant through fork

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_pressure(force, area):
-        return force / area
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
