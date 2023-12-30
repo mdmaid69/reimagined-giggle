5bfca797c90cb3e200e8598d99dde0f7152937b6
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:11:02 +0000

    what sink permission prove occasion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
