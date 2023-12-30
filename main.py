1e4259ab59d6b6841ca02005e42bc7de46fe18ea
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:50:46 +0000

    contact previous build measure arrival

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def cube_number(x):
-        return x**3
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
