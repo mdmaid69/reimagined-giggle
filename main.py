d40332eff4421c18454200d718c0e5bdfa8a95a2
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:23:31 +0000

    edge unlucky 179bite threaten list

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
