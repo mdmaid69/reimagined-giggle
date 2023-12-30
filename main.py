d411f3a43c0116c934659199fe2d33ec29a38abf
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:25:31 +0000

    fence out key accidental draw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def split_path(path):
+        return os.path.split(path)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
