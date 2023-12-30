cb6f7cbe35533ff7122050ce7b4f11a4c25494a5
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:49:28 +0000

    law switch instruction frame wash

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def count_characters(sentence):
+        return len(sentence)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
