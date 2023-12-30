3feef92385d2d5be8ac54c264a156a3af934f9d1
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:00:17 +0000

    light study pipe war moon

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
