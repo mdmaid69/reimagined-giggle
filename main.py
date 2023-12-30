a18400d0b77038ae806f2d736be5e28755444735
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:44:14 +0000

    stick voice home keen uponon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_current_working_directory():
+        return os.getcwd()
