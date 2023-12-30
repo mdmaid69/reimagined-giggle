d2055f76b65e042d6decd432bafdadbcafc63a11
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:15:06 +0000

    suffer natural class soon hungry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
