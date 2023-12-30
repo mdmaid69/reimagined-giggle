0b8d2f8bfbac6c4d484b761051a8a9ffed98410c
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:00:28 +0000

    why build power truth crowd

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
