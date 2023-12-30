a009e5493db5e8af204970b08874adc768a71df9
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:25:49 +0000

    yesterday crowded test turnup pressure

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import json
+print(json.dumps({"name": "John", "age": 30}))
