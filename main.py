ee35c2cf55dc47c6a0c56882d74b4bc78ec5a539
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:43:25 +0000

    whom past enthusiastic whom important

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
