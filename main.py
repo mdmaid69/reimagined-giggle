4d9fbb3759ab1475f7dc52dbd7b34685cdee1d38
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:53:27 +0000

    ever freedom discover work spin

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
