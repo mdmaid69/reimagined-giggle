123feb16ab91a058fbe15e569464736bbac5edcf
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:50:44 +0000

    leave face advise reduction runinto

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
