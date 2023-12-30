eecd3fe2805b0efe41532436eb392984d6c65543
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:49:40 +0000

    inform warm thorough heavy protest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import collections
+def create_user_list():
+        return collections.UserList()
