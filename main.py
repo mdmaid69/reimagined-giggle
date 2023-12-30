20147d5f3eda43d19fb4582e75aed4553d6cb13b
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:55:07 +0000

    floor unless reject free takeback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def reverse_list(lst):
+        return lst[::-1]
