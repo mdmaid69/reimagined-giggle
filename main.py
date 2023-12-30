d23b14c74aed846472b806022d82cc499150dfe4
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:21:42 +0000

    row keepfrom difficulty suggestion opposite

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def is_palindrome(s):
+        return s == s[::-1]
