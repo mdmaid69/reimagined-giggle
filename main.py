bd2042f911c2ca5a3a0a8b237595226c78b47ba6
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:43:50 +0000

    goon sad when country answer

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  def subtract_numbers(x, y):
+        return x - y
