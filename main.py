b6d1929af4ba67a9f7bd1c0632979852cb14e62d
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:59:25 +0000

    fashion but aim branch affect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
