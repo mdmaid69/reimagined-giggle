17f63600722cd784d4a07e7d31bcfda2592fce27
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:02:20 +0000

    smile plain contrast suckme water

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def split_path(path):
+        return os.path.split(path)
