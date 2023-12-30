f9ee72ba4df39ed88e68b7927845f1cca1ef5c28
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:15:22 +0000

    sky completely active operate polite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
