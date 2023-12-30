b7b8b3e3bd2ecc49c98213eb2e5f6796ff3af2f6
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:59:00 +0000

    edge getaway fond snow limit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
