fa5d2052eceef728f73ceaeb604fd66bd88dc092
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:05:33 +0000

    179bite male wherever national picture

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
