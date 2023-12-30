fe6220123c9fff2403d4076c32fd858daa9ac828
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:19:20 +0000

    television person trouble hurry roll

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def find_max(numbers):
-        return max(numbers)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
