e1cf0250aff3b99709dbca84e41f0705ff50c20c
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:49:37 +0000

    post journey someone ear connected

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sys
+def print_python_version():
+        print(sys.version)
