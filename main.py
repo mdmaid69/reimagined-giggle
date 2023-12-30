178ed88692d5fe1f4da76f3630c2587064981a10
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:46:14 +0000

    evening risk coat average although

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_index(array, item):
+        return array.index(item)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
