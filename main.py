f409437e2d5474b212d91f1792a734e5c149b954
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:42:35 +0000

    finish fetch sort treat rarely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
