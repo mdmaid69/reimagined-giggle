ca5d3d347ca247c8d0058f68191288d7a1509be4
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:41:45 +0000

    hair Ll the fill cinema

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
