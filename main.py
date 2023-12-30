c5e76711f3767b2695453af82b72692d69027d67
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:09:47 +0000

    building square shop his spoil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_int(array):
-        return int(array[0])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import collections
+def create_user_string():
+        return collections.UserString()
