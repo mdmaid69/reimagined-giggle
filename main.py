a72b20707363ebc1a79ee36d045ebfb175eb1f76
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:53:56 +0000

    meet everything safe difficulty country

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
