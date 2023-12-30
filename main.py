c5af5bcfa157b32739e022ce4d1ad99d8dfc7498
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:29:22 +0000

    wild flight 566end hit rise

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def get_array_as_tuple(array):
+        return tuple(array)
