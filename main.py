7b22f44c48d33ecd5a2d067a82955d64ef0e9bc6
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:17:29 +0000

    dangerous variety foot relation material

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
