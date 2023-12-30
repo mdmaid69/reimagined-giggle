e907984e23b04e6333a2367c20f1a82443813288
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:17:48 +0000

    personal cheek behaviour movement pin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
