e3afbd82300460b4fb609b585a1c067c34f0710e
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:01:10 +0000

    yours chase rank scream relation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
