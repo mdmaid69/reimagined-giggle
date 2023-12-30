7ae9b5ed908f137a16417ea67e5d683baaeb94fd
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:22:25 +0000

    neither actually yours combination secret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
