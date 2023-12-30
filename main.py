95c5817c2a478fc9cf451036cf38be37fbca6969
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:30:04 +0000

    lookback(on) regularly chief argument give

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def sort_list(lst):
-        return sorted(lst)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
