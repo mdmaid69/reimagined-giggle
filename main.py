1053f7761e2c540b3dc1095c7e71c3be2fe89246
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:36:20 +0000

    translate respect develop unless public

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
