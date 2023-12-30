0ad75bf7dcdb1580a767445315f5fd817c05c6d0
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:45:39 +0000

    hot glass substance unhappy land

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
