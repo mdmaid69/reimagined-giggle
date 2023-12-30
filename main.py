919627b403f6e286d7b6492e9bac6e82b98a242e
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:40:45 +0000

    freeze undo Rr remove notice

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def square_number(x):
+        return x**2
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
