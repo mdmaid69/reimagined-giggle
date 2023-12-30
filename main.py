9fec196250352451bde5450bc44a89c1841e2cfe
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:37:23 +0000

    where final excited yesterday crisis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def is_palindrome(s):
+        return s == s[::-1]
