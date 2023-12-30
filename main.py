bdefb8a2d9980983786bd3e49526bb9b2fa4d92b
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:47:49 +0000

    narrow order square downstairs tired

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
