a95d7f388e239a5b2670dc7b413b106e9652e522
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:30:41 +0000

    complete apologizeapologise only loss disturbing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import tensorflow as tf
+print(tf.__version__)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
