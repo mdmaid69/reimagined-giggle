6fb4c5385e4825ab95c85bbbc829f88f50551f26
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:10:55 +0000

    left clean keep hang possible

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def split_path(path):
+        return os.path.split(path)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
