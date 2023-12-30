856f939d710dc381648a998cf466d1279ea2db3b
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:30:38 +0000

    among walk gas listenfor backwards

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
 import datetime
-def get_today_date():
-        return datetime.date.today()
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
