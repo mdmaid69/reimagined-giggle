f51b07f8e0618a837800cd785b7db24bd0608240
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:21:56 +0000

    zero scrap grateful worse example

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
