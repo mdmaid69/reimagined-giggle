c6e73359b16e9e5cf9a6a1e7d050adc4ac6712ff
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:38:37 +0000

    happy seem view be quit

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-print(sum(range(10)))
+print([x**2 for x in range(10)])
+  def convert_to_octal(n):
+        return oct(n)
