1ebd1e56510eb61a415fca30a63a0a49feaaaf15
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:54:46 +0000

    herself want build listenfor this

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
