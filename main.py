53fd91cdb77a9dd25fccd306861a25382293737a
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:00:24 +0000

    unusual innocent somebody joke waste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def find_unique_words(sentence):
-        return set(sentence.split())
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_density(mass, volume):
+        return mass / volume
