9410ce1809b3de656a5694e5839cc866bdf070a6
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:19:06 +0000

    bath art careful scheme him

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  def multiply_numbers(x, y):
+        return x * y
