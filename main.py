3005fd8ab918fdf8697c1cfc5c3f4453acf3b5cd
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:19:24 +0000

    unfair task turnon sensitive Rr

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
