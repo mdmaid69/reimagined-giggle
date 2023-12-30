cd5cca5a6ebf3568cdc4268e2570bfc048f30a6a
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:29:42 +0000

    convinced boil scared mark leg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import heapq
 def get_smallest_elements(iterable, n):
         return heapq.nsmallest(n, iterable)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
