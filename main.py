86f5c979e3435fe8ac4a08bba1b6fb639a82506a
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:55:55 +0000

    interruption recently promise borrow change

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
