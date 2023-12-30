814cf6be82178598247925a7f944bcd01a71e128
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:45:45 +0000

    slowly project workupto tired quantity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import collections
+def create_priority_queue():
+        return collections.deque()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
