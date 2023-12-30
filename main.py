4e36797694c5fe4e8163971a17e3bc18e9e43eb8
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:50:06 +0000

    test translate front laugh sign

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
