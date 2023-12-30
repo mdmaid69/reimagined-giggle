7142ba9b0c64fb8691ce56f0d33a0cd115e98374
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:00:27 +0000

    again thirsty information tongue lookup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def reverse_list(lst):
+        return lst[::-1]
+  def subtract_numbers(x, y):
+        return x - y
