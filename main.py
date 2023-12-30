5941e5c85379c288a44dbf6151015a30af2dbb56
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:20:09 +0000

    total tip crowd where Itself

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def sort_list(lst):
+        return sorted(lst)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
