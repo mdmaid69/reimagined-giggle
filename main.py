f2b3d46e75e3b296cb3c97d24f14fe98962c6003
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:25:11 +0000

    possible sometimes add plenty wild

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import sys
+  def get_python_version():
+        return sys.version
