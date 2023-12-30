84484cca78b04c8b550ef7b592d3b2261c463cec
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:45:31 +0000

    break protest exchange thin each

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
