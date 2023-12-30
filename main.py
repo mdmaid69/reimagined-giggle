4b28b5bef3be88147019c04eaeddf50698f9e9b0
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:56:30 +0000

    share couple complete Dd provide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def divide_numbers(x, y):
+        return x / y
