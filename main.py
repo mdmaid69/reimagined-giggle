89ccefca2f22d7155f3ed1343906cca393dc5993
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:05:43 +0000

    month comment burn see ashamed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
