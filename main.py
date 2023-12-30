ad417c6f37925466987445e659cf19efaab7b2f5
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:58:27 +0000

    circumstance excellent yet immediate university

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  def convert_to_binary(n):
+        return bin(n)
