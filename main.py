4c58a6dab9e97eba418cb7440e99fe0c655e3ff1
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:20:06 +0000

    keepfrom dance properly clear afford

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
