04b9ea0b3f12d09878e2727cec9d47b66193e361
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:20:51 +0000

    whistle quit answer wear near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
