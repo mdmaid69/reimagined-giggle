ed5c7c3dd320541488e44300b83eab5f28158970
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:29:51 +0000

    shade mean lie with runout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_cosine(x):
+        return math.cos(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def delete_file(file_name):
+        os.remove(file_name)
