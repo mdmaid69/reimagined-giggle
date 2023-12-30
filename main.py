f73fe4acd70a77389d90fff6c789fbf977ebb0a4
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:39:03 +0000

    scale mother middle doctor land

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_as_bytearray(array):
+        return bytearray(array)
