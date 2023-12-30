cfb3b51e440204f285ec8ed67c9d71a495809315
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:26:32 +0000

    spot crazy skirt strong too

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def square_number(x):
+        return x**2
