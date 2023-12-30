5134dfa325e7f8bcab45f0481cd0cf0b2cd2101f
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:57:52 +0000

    road occur noise computer total

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import random
-print(random.randint(0, 100))
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
