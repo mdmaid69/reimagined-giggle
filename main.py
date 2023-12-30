b7f57584b95d79aae881cfe5a136892527a7e36d
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:49:38 +0000

    possibility unhappy independent feel natural

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def is_even(n):
-        return n % 2 == 0
+def square_number(x):
+        return x**2
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
