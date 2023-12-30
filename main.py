0e4ef200fff8065eb28d172c2eccccb3e845c49e
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:01:04 +0000

    straight soil before add present

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+for i in range(10): print(i)
