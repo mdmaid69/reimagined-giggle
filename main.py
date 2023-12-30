9bc9c06b1b0345dcb2f04a1c26f3cd5e73bd47e3
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:50:44 +0000

    anxious pain accept correct more

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def convert_to_binary(n):
-        return bin(n)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  def convert_to_hex(n):
+        return hex(n)
