49ece2e9cf6ffa125b538651e12997351d8fb45b
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:16:18 +0000

    regularly big usually nurse temperature

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def find_min(numbers):
-        return min(numbers)
+from collections import Counter
+print(Counter("hello world"))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
