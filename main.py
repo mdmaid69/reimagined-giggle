ddcbf0747cfd9ed1160d81b83e52f789f0351da9
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:23:07 +0000

    wire learn clearly 572enter bring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
