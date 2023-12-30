5cc2974c42cf1db701d1fba5e81c0cea21e9ecf6
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:02:02 +0000

    zero finger consult zero suckme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
