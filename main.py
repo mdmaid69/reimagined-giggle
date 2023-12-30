3c9dd64b3657d3102056d992940f8e28ab677225
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:10:29 +0000

    still Itself deliberately fire her

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def get_array_as_set(array):
-        return set(array)
-  def convert_to_binary(n):
-        return bin(n)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
