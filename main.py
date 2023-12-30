ba2a81874cc59b654f1cfdc9b47093291f18b49a
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:38:35 +0000

    equal boil lock face lucky

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-  def subtract_numbers(x, y):
-        return x - y
+import collections
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
