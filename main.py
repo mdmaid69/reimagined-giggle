10a3d490f7266e6110b19c41632894e5c51313d2
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:19:00 +0000

    Mm whichever plain record care

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
