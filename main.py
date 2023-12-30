a862618cdc9e474b6b5f95a53b780c3879c49b28
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:13:57 +0000

    tap slight sell reduction environment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
