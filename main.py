c2fd3096f577b131c0e66f23504e5ae4f7dab0cd
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:25:13 +0000

    shoot length shallow connect use

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
