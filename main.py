3bda677e3d63a2ffd1dd01a019afdde727f15cab
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:12:01 +0000

    best whichever drive window ago

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
