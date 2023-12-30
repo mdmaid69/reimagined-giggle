c78d7ff9d0a0acd2e0d4dcc0d0cb332276f48be2
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:24:26 +0000

    must store comment and know

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
