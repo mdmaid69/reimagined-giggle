1e363dfd641ff64527478a8596d21d197be36d01
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:10:07 +0000

    rub dance happy frightened tap

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
 import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
