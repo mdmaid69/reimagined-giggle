7eebd8958a26fbb494197b1395c11f87d32a3d7f
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:09:57 +0000

    unlucky float number depend uponon

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import logging
+def log_message(message):
+        logging.info(message)
