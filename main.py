c656111344806d9d99b6f6d861dac2da21214c09
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:28:07 +0000

    complaint injury quality nature rough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
