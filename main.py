094abdb076c717a2fe4542ffcefa34ff8e39f0bd
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:06:40 +0000

    complete permission record wide colour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_acceleration(speed, time):
+        return speed / time
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
