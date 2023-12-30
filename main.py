73605b9be61ce8fad2270be7440a3f167dfc7b36
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:15:08 +0000

    else plenty place most whistle

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
