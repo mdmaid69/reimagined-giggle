f77756fc125da9bf31a565a9c9012efe1326ebb9
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:43:02 +0000

    centimetre border concerned anywhere nerve

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
