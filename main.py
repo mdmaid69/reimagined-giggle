95a1d4f38475afb099244e19722ea254ef8dbfcf
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:43:01 +0000

    warning regard like ashamed interest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def add_numbers(a, b):
+        return a + b
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
