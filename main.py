7c5008496ff2d735d5e171c203008c43e959d8c1
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:55:45 +0000

    thanks pile ocean suit line

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def greet(name):
-        print(f"Hello, {name}!")
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
