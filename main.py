d5dd02e37c1087f4c91c5ea5ed85efdfc51b56df
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:30:07 +0000

    point supply takeback unkind stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import math
 def calculate_cube_root(x):
         return math.pow(x, 1/3)
