707436bfb47241bdb82849200552a7523f6381ed
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:59:31 +0000

    past hope loud finger push

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
