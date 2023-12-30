28bfee69cb5b07b36b3e06f608815f437e39b4ed
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:42:18 +0000

    tooth accidental Ll classroom now

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
