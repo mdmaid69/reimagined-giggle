3d1d438de4b11fb456156cdcd3317e1bf6256eb2
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:09:16 +0000

    careless rain note hurt disappointed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
