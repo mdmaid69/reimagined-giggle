492d25acd3886b24c6a4935d54cc978f238a88bc
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:28:11 +0000

    limit nowhere debt clear conversation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
