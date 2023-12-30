0308653d50fd63f3c281229d104dd243dd282ec5
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:28:26 +0000

    control realize trial stand stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
