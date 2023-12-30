5b31f31be3353e7a8060af660e4e5597477df25f
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:35:16 +0000

    bang embarrassed after total computer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
