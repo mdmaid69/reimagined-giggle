0e5818fa2bc301a3a2139f146020bad71ed08254
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:14:55 +0000

    children until over turnup failure

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
