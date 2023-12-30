e01ed6cb57b04b13128a98c8a757cd2840603b61
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:42:58 +0000

    unfair skin hot offer earth

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
