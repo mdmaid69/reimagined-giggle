ce23f7c370c4dda738a71044af07c493066a7361
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:07:13 +0000

    physical art expression nothing nature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-print(os.getcwd())
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
