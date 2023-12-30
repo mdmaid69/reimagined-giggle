741c3018d8504022743c54e8f190dfe065b5a8b8
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:09:09 +0000

    thing best welcome save lunch

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-for i in range(5):
-        print(i)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_floor(x):
+        return math.floor(x)
