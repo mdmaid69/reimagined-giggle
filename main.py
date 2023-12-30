bd3a3101f1c3887d98325988b25944a7fed0b405
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:15:00 +0000

    disturb involve build especially rid

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
