e31c0891b0274f945617b81b9947b9cc0106a193
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:06:16 +0000

    round Itself common discussion insist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
