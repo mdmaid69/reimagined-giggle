e7936bead5eae7b28792b941b48c459843948743
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:12:35 +0000

    walk cheek medium guilty evening

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
