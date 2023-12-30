1d4536f2bd35c4812e3083adc9c0742dfc034986
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:52:38 +0000

    rest process corner book murder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def clear_array(array):
+        array *= 0
