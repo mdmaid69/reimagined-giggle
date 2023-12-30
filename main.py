7f2b9d421dc83efcf2d9002e78109b15cb5c48c9
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:23:12 +0000

    reject length excuse middle Tt

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_cosine(x):
+        return math.cos(x)
