4823d5604a555396612c0ef3b0b1f56be62bb2ad
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:38:57 +0000

    scale tragedy type aim steal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
 import math
 def calculate_hyperbolic_tangent(x):
         return math.tanh(x)
