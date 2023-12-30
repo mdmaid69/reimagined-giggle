034cb4e2da8031b42f52015a21cab5d40807e9bb
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:37:43 +0000

    should please onto wherever dress

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def cube_number(x):
-        return x**3
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def get_array_as_float(array):
+        return float(array[0])
