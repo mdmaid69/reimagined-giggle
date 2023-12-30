421242dc0da8a6008680ef3bd8f88c4d90aab9c1
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:01:10 +0000

    convinced remember beneath dare unhappy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
