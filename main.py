097570d3e4c87c2f6e1133f1564922836007f0fa
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:26:09 +0000

    sorry reason start wear used

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import logging
+def log_message(message):
+        logging.info(message)
