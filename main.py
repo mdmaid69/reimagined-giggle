73770d0b651d82a433152da9aad6a9fb1c923085
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:48:41 +0000

    like change Nn invite plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
