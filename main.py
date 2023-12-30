efeb346bbd4f68fbc84cdf180c58d2a6468bccab
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:44:04 +0000

    cheat enthusiastic stamp eager achievement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def get_array_as_list(array):
+        return list(array)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
