923cd7e443995bad222586822bedebcfcac723f7
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:30:44 +0000

    lay wing shoulder cry so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
