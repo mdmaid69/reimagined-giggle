78b321ea834650556d453e26872e093b8f1bf7c7
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:19:22 +0000

    hot together urgent accidentally whom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
