3bffe9f6faf4a499759c42a7a960443aeca1288b
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:14:52 +0000

    design ourselves basis passenger test

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
