85733989e928c3b7d2f1c618f01f41b873c5c233
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:27:55 +0000

    wave sleep slip spread duty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
