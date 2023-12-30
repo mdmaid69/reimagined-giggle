3a1deb128af73475a7c8d1b2a940ce442277cfdc
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:27:13 +0000

    music question edge tip burst

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_item(array, i):
-        return array[i]
+for i in range(10): print(i)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
