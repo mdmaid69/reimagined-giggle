8e04a1ccd697961e7527ee00c7ba0a39e3edc877
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:20:06 +0000

    until buy complain shut production

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_acceleration(speed, time):
+        return speed / time
