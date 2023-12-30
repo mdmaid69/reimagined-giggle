1bfc8c43c0ed691ecbbf807e34ed77b5b5e6ee44
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:06:10 +0000

    plain unite agree clothes field

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
