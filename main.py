9ea269be66a816c58cf61b36c79c6cdedbd14729
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:57:43 +0000

    cinema temper natural speech keepfrom

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def get_array_typecode(array):
+        return array.typecode
+def calculate_force(mass, acceleration):
+        return mass * acceleration
