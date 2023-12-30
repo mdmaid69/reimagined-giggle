2389f489cb4a3d2c33122c27529053312a76460c
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:48:23 +0000

    nose care question pocket table

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def sort_list(lst):
-        return sorted(lst)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
