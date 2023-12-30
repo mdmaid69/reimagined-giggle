f94f26942f13f4680ca6830f853fea55d6f6665a
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:50:28 +0000

    freeze stand and stone platform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def find_max(numbers):
+        return max(numbers)
