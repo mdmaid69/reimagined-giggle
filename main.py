30daec447595f5d8c84006230fbfe21489009756
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:19:52 +0000

    example study aim development hope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
