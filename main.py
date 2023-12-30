1f926d2ce1d2e973aebfc7510252408ff2bb8640
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:59:14 +0000

    light demonstrate complete fasten second

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  def calculate_perimeter_triangle(a, b, c):
+def calculate_perimeter_triangle(a, b, c):
         return a + b + c
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
