c18728590a97080cf82f359c121e7e8e205c41e9
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:33:32 +0000

    cool substance hello mend delight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
