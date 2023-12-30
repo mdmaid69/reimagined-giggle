ad2ff62e0bbd5efaecc48f36f45ea83a46fce65f
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:10:07 +0000

    out hide receive nowhere describe

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
