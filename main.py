646ed7ea22a2572591926e8b736ceb8e3a61919a
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:48:18 +0000

    wise forward as reserve pipe

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_power(work, time):
+        return work / time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
