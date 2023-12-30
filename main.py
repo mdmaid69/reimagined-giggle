a3fd8592aaefdf2a167dce88917d428c2f57de5c
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:04:45 +0000

    dry organize possibility operate less

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
