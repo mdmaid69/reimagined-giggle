efbfbcbe35e81d5641933db7f542dce2d2f17860
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:15:42 +0000

    minority direct service cook proper

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
