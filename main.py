0c489c1dde7d593b49f597a148673a359ddd5ce0
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:06:44 +0000

    angry display dear ball last

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import collections
+def create_stack():
+        return collections.deque()
