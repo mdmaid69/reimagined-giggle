0bc12515f2e90061605eedd14f4d5399adb5acf5
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:45:30 +0000

    flat otherwise trick reply exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
