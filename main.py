ed32e0ac8cf2bdfc8943c5c4a00edc6feb3d2238
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:40:17 +0000

    character related keepfrom earth back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
