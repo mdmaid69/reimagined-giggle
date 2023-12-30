bd584a93c9c92de783f65fc2b2d6c31bed2f924d
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:53:04 +0000

    by hair slide medium cross

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
