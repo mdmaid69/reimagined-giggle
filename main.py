7d4b082bcc53be4f82fa578f7ff652474bc51c88
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:48:55 +0000

    home weather love less attract

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_pressure(force, area):
-        return force / area
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
