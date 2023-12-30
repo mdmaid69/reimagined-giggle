f9ea0659f354f67f42268fb645b25b0fc98c4b52
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:52:52 +0000

    admit completely term suffer how

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
