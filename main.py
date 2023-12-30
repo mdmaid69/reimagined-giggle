d73294bd001077ef96ab1815b9857413cc07d3b9
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:28:17 +0000

    habit pipe straight benefit poor

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
