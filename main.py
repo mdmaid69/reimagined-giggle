d54d43fabd7e15c0de2c7afc2c5e49720b64c3bc
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:06:27 +0000

    clean dull hollow rely attract

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
