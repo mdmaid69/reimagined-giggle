4362af8c71a68227ff43f5a562fe8aaf239e5644
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:07:15 +0000

    thorough whisper flight sort holdout

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
