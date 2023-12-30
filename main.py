a51dfe21a6cd3a015e7e89c553ce5a5a18431551
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:16:04 +0000

    Nn worst ashamed autumn long

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
