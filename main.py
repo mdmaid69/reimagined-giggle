6a7f1fc5d9aabe8df7cbf427247617a10f3a2553
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:58:33 +0000

    neat meal flight mind godown

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_int(array):
-        return int(array[0])
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def find_min(numbers):
+        return min(numbers)
