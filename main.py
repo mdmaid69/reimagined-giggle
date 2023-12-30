fc8a5fe1b8b4f77ec2c0e7139b1b2c4ecaf46b5a
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:49:25 +0000

    offer drink little environment phone

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  def is_odd(n):
+        return n % 2 != 0
