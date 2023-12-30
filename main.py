467c30eaa95aacaa5f30518f2ac72592fb963d2b
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:50:50 +0000

    record test way bar lucky

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
