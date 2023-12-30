3a47a3d37eb41cd0453a0c446387f622b599403e
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:18:25 +0000

    practical star capital way often

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import os
+def get_current_working_directory():
+        return os.getcwd()
