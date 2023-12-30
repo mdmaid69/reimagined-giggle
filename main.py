3f3de163ff2929f43f9cc996ad4b33d154075986
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:04:41 +0000

    comeup top mass receive stranger

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def count_words(sentence):
+        return len(sentence.split())
+import array
+def get_array_as_int(array):
+        return int(array[0])
