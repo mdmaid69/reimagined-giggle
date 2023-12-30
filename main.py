1b5ec568c3af8332a073b69e3e58da9d7972a348
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:35:04 +0000

    slow mine towards name morning

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
