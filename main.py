1814acab889fe5e8c071e670bd50df86650743cd
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:34:14 +0000

    it picture late generous hard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def sort_list(lst):
+        return sorted(lst)
