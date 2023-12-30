ecbfecf27369e6e2a0dbad7544291669d88a610e
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:15:15 +0000

    state public noisy exist grade

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
