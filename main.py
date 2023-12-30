27099e4b36727080e396a1b057cd2531374f7fc6
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:58:14 +0000

    criticize average possibility spin somewhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
