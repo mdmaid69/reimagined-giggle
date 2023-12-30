1f4085e8080d98a8b6f76abb1c22fb7261daff4b
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:58:28 +0000

    crisis plan dish zero example

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
