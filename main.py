d65678df85c7a9c803eeb946dfa4fbced0982f2d
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:21:56 +0000

    collect certain awake splash spoil

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
