d9514f698b04b8e3d3accdca8fd045023d9300a8
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:18:01 +0000

    reach murder pinch experience create

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
