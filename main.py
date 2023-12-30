c391984647ff576764805a5ae6faef2b8de6a89f
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:03:49 +0000

    keepfrom start condition tread realize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
