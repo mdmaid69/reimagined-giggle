05282b5bbf1847b1b707007af19d7ad0a95ab499
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:51:41 +0000

    frightened pot settle name repeat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import math
+print(math.pi)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
