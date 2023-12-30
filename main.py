6a176def72501185a1c3634f90f343ff6a258730
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:31:11 +0000

    worry goon yourself relationship issue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
