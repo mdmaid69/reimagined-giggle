fe140559c4a09d7ae7d305c14da944d180d1dbc4
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:51:20 +0000

    dream obtain physical crowd true

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+print(sum(range(10)))
+def calculate_density(mass, volume):
+        return mass / volume
