0a1ea27b48dcff25005d453d2fb9f1cb82d0369b
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:31:07 +0000

    getaway alone concentrate group left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
