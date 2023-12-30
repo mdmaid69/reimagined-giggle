5b3211ff6601ad806923cfd88ccf01df7e68ea25
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:52:29 +0000

    successful wave please university course

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_power(work, time):
+        return work / time
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
