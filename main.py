64bb5a2e84a03ac91a034167616063f05f396c6a
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:47:14 +0000

    way sail convenient minor chin

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def sort_list(lst):
+        return sorted(lst)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
