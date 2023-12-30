6105fcc9dc3562e7748ef4e93a1f0b280bfbefe4
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:24:25 +0000

    damage Ss cover announce limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
