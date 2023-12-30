1757739e33488366d07b13385903f735b34c7d20
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:47:02 +0000

    only usual Cc car corner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def count_words(sentence):
+        return len(sentence.split())
+  import sys
+  def get_python_version():
+        return sys.version
