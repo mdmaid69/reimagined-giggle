376a6f30d99da476da60096195ddaed579730bba
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:25:12 +0000

    what ignore when bunch me

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def calculate_work(force, distance):
+        return force * distance
