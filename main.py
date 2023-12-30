a63076e0c0599190cbfe2453acfc4504503022bf
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:35:50 +0000

    getout cross to safe rest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
