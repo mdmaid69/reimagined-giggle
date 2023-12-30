4951117e2e8fa15817ae32ba5064c4952651f6ad
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:12:37 +0000

    reaction view afterwards child town

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_work(force, distance):
+        return force * distance
