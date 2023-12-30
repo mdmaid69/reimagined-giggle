f88977f5d61a13e0adbe4639035031ca0f1a5fb2
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:57:20 +0000

    plastic bright geton debt cope

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_speed(distance, time):
+        return distance / time
