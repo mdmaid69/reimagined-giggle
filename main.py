fad0a5347bd82350c01028a0e7eaa0d96af7f7cb
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:04:31 +0000

    steer replace cinema promise translate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
