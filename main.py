58fd0777d20763418d1f48b0a1124337c749bfc4
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:27:55 +0000

    succeed give straight worst legal

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import os
-print(os.getcwd())
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def divide_numbers(x, y):
+        return x / y
