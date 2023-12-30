433827f8d62a5588a9304d126d02235027f96e88
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:39:22 +0000

    situation estimate shop radio turninto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
