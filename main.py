f21a4b2b46b123bd650eb224258324c67d3ad816
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:16:52 +0000

    goaway swallow spring refer I

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import random
-def roll_die():
-        return random.randint(1, 6)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
