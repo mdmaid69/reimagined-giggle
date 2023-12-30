a24ed92906ff88909733de1febc730584a79aac8
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:01:03 +0000

    hole offer persuade present regular

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import array
+def get_array_as_float(array):
+        return float(array[0])
+print([x**2 for x in range(10)])
