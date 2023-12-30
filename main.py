e59bd1513c60e4e3b919bc2887363d8d69578194
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:20:34 +0000

    wide statement contents good party

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
