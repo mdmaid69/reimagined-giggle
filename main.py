5931f820171ba50f730b68b38f71fe9910d03df9
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:09:22 +0000

    drive chin challenge their treatment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+name = "Python"
+print("Hello,", name)
