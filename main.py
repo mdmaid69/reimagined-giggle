fa57740bd7d393a930c4ce7a907d6d4d759cdc84
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:16:01 +0000

    light jump cool breathe drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
