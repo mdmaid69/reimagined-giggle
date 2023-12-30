8fa65821762f968b4629ecd7e9694dcad958abbf
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:53:35 +0000

    worth pretend furniture sink mess

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
