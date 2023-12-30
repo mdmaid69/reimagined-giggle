c8c4cfbd7bdacf33130ec64aea34f4e2b7164da7
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:58:22 +0000

    destroy idea weak bar confident

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_area_rectangle(l, w):
-        return l * w
+import datetime
+print(datetime.datetime.now())
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
