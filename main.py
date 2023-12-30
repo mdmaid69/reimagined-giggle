d9ec1e6028bf3afd3df72db5911f4e5c35976c36
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:23:56 +0000

    issue imagine thinkof turnon where

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
