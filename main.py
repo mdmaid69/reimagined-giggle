48a4f6d6198a9b06a6e51f39259c3af4201b40b3
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:36:30 +0000

    takeback honest anywayanyhow takeback range

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
