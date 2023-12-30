6da529e4ddb45bac2de200276a207a7b883e2469
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:55:32 +0000

    catch bath range mistake neat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
