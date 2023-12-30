57470b40b141d8bfdfa03502c044b3254efe52ac
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:33:40 +0000

    blue runaway physical floor half

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
