d6dc936263f24c0e73d0521ea5155b0a46d8236f
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:09:24 +0000

    bush add maintain plenty trust

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+for i in range(5):
+        print(i)
