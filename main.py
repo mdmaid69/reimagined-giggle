9404f45c70b16e452af8c66c70b0a16bc0df0666
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:33:26 +0000

    area swallow challenge board fat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
