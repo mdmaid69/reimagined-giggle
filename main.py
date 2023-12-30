db413a96a7552fb97c7fccf5c52fbcbc2888cd7d
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:42:12 +0000

    involve finger try petrol again

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import collections
-def create_priority_queue():
-        return collections.deque()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
