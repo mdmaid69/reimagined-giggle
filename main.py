d77b5450783066cdfcea5b8588a9803e57c994cf
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:44:53 +0000

    punish second care callback regional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  def cube_number(x):
+        return x**3
