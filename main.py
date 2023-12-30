5e42d11167852abd71d078eb8f2663bd2903cae1
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:54:40 +0000

    tongue under emotion brain medium

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def is_even(n):
+        return n % 2 == 0
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
