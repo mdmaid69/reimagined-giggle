924e6ffc31d2916a170a0294fe56ae65547c0c59
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:32:12 +0000

    human bang insist tie giveout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def sort_list(lst):
+        return sorted(lst)
+def find_max(numbers):
+        return max(numbers)
