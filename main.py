e7eba3190b53a531a9b305c6fb00606fb3a39998
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:22:27 +0000

    star possession talkback board goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
