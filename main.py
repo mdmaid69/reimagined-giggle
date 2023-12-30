dae87164d222d8ac08ce8a602d4092183e4dd279
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:28:45 +0000

    entry description compare error impatient

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_density(mass, volume):
-        return mass / volume
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def get_array_typecode(array):
+        return array.typecode
