0fe3599e27eaf79f085d5e8137bc693836f258e2
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:25:07 +0000

    rely share equipment failure preparation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_cosine(x):
+        return math.cos(x)
