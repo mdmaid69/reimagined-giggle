9efc1ec3b607af11aeab5d063543350bae0d73d0
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:55:58 +0000

    tread habit search breakinto fire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
