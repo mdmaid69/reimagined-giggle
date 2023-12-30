e97f7efc0d032c495a9282eaad4bbe6059405496
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:31:01 +0000

    cry profit thinkthrough respect ourselves

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import collections
-def create_priority_queue():
-        return collections.deque()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
