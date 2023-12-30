b5293fa953777dcb940a5fc2667a85d1ae5bb418
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:12:55 +0000

    along bottom soft list succeed

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import logging
+def log_message(message):
+        logging.info(message)
