dfd47bbfd76d6aa7d036d335e4a118e507fd787e
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:20:41 +0000

    score work gas interested spring

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
