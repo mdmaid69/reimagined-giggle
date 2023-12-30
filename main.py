d153245d4ca7a97d4b7dd7179dce20bd6417abd2
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:33:57 +0000

    meeting switch nose nearly approve

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def find_min(numbers):
+        return min(numbers)
