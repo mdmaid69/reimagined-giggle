4c6d84f195333165d4b31da924c46483f47bd346
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:51:24 +0000

    their warn song let total

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
