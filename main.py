faa88b5015c50b5cbb20f92c6c893dcde7b51599
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:21:52 +0000

    phone affect whether dish near

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_bool(array):
+        return bool(array)
