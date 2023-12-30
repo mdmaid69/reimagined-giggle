300af21be01e64909e0a9f1d1cb0ed054fafbf75
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:40:48 +0000

    exaggerate interested both partner compare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def delete_file(file_name):
+        os.remove(file_name)
+def square_number(x):
+        return x**2
