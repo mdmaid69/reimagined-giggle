cb0e5d05b225d739e6e81cfbc152e29c544a6bac
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:27:59 +0000

    foot deliberately call wind circle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def square_number(x):
+        return x**2
