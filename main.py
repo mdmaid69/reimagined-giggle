1b5e56dd7b5402a638369876fdb6ee562c94113a
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:07:51 +0000

    instant report continue pool achieve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
