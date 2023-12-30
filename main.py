4f8955d90656dd1023a768106f73275b89ec17ab
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:17:46 +0000

    plan talk spare mind pump

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
