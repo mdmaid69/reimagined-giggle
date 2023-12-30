651c11c913740b1feb3d4a3004634943e3585d89
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:55:15 +0000

    mean stupid error around unit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
