b9ae916a7ab00fe45bf568e0443eba86c53d0421
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:16:28 +0000

    far heat tough sound everyone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
