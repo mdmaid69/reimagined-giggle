0c5392943581e2a1e6c5c4a3dd3aeb37578700b3
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:17:34 +0000

    spend space later memory well

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def multiply_numbers(x, y):
-        return x * y
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
