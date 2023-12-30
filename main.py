d1d2424476ff6fe150e54bb4654006cb347c7261
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:46:12 +0000

    doctor march direct high affect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def convert_to_hex(n):
+        return hex(n)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
