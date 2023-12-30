5e16442beb7062c2dc30bd5734ff5eea43d38ad1
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:42:55 +0000

    season keepout fortunate summer prefer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
