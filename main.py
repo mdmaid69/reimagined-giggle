828622dd06beca8a070e038b037935e9fa265e3b
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:05:01 +0000

    acceptable fairly near deserve direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def remove_duplicates(lst):
+        return list(set(lst))
