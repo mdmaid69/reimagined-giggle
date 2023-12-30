fdad88ca027ee5d7116ea536eab735d470ff2dd6
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:28:19 +0000

    dull satisfactory bet tight remember

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
