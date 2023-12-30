6542c674cbd63ee29208a4b58635e7335d13ae8f
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:12:25 +0000

    belong hurt beneath benefit breathe

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
