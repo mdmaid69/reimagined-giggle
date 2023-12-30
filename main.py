a0f1aad7be6b23810c6df0474f13a724977e08dd
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:06:23 +0000

    live concern pull Cc error

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
