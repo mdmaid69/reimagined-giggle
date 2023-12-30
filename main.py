17e6dc082fa691a36cb361ce94bed93cfb784520
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:13:43 +0000

    keen trust black opposite successful

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
