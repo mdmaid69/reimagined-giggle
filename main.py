5272ab83396d989f3f68318b8e74a6c54fd543c5
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:32:21 +0000

    callout run no extraordinary cope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import numpy as np
+print(np.array([1, 2, 3]))
