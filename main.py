12e0cfb1ad7646c8dc13a38e9179881b89f4ad1c
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:15:06 +0000

    creature slice straight cross game

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
