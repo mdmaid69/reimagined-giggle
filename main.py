b098894c791894c0a714f0f3f560250205f55119
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:09:51 +0000

    refer herself declare however slight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
