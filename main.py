6528df307e8c64b77e7a6335e1b67af8d336b4e2
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:54:57 +0000

    mind characteristic otherwise organization cause

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def convert_to_octal(n):
-        return oct(n)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
