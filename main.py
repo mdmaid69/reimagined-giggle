7527a57fc2753430cffa96dac71e9f734e9492c9
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:32:46 +0000

    room acceptable set late post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
