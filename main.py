c54407f0092fbb615449d45e0c0f277b1af2ec3a
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:14:20 +0000

    fit accident water contain arm

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_volume(length, width, height):
+        return length * width * height
