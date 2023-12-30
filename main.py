4b8d59ca31a5adfb91540789847d8421e5d020c1
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:45:58 +0000

    feel load warning practical exit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def square_number(x):
+        return x**2
+def greet(name):
+        print(f"Hello, {name}!")
