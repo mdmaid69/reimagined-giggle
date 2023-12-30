2b3d453a981a184c4d81cad44117a918a810d32b
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:52:02 +0000

    confident unable project pronunciation really

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def square_number(x):
+        return x**2
