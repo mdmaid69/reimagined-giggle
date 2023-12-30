1471c0561494b10d2ec9594b68c71025b5a57013
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:20:57 +0000

    pain stand strong real blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
