d871a392cfc3d73032b8e237d14c2ca54b7b7217
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:41:16 +0000

    danger happy raise centimetre thanks

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
