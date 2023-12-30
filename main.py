4aed93dbacf5d1a6485270d172baed358d1de308
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:46:24 +0000

    say bit worth occur occur

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_power(work, time):
+        return work / time
+import re
+print(re.match("h.*o", "hello world"))
