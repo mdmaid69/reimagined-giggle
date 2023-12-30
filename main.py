4e23af0eca040e625d020d4acbcd1c38655131ff
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:33:38 +0000

    seriously treatment ourselves steal mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
