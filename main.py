70c615c52d24495a4e60d5c746615f022d33e187
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:17:04 +0000

    concern exhibition production hardly beside

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def add_numbers(x, y):
-        return x + y
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
