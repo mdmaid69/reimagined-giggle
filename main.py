22fef9bfcfa037acd593cc27b9225df46a52fb4a
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:06:00 +0000

    justice side somehow argument off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
