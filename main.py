16a75fbbca6750eb4e1ce9dab84bbb346444adc6
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:25:12 +0000

    another sentence past dish year

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
