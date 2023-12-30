d89b058533e513d96ecfcedd76be90891b5d65d3
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:03:21 +0000

    big completely slice view away

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
