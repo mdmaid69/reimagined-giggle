68f51e17c819b1841804a383e684f4be7dcd0e45
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:40:29 +0000

    Uu Hh against store big

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import os
+def get_current_working_directory():
+        return os.getcwd()
