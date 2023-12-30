8c73b17cfed16fbda8a9b7acc5f40ed337607e6f
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:58:33 +0000

    take hello feel real tradition

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
