93c37223cc78eb5389cace886d3974f3e691df94
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:04:25 +0000

    explanation description either loose fat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
