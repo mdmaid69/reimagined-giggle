a3f8dd80d1f7403026482b04d183585806402558
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:31:14 +0000

    noone smile roll pay over

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_area_rectangle(l, w):
-        return l * w
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
