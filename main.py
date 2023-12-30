bf737c86670b7a5f18167fe9f62e9fbaa15fb9ad
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:08:08 +0000

    across square shoulder base close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def add_numbers(x, y):
+        return x + y
