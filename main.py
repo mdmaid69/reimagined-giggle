bf6207b35651782694b494749a825dde3892912d
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:16:45 +0000

    over accommodation cut evening confidence

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_power(work, time):
+        return work / time
