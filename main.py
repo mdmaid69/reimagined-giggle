d71efeb8a8d13a54bacc1f2366ea2c8ce8a66d32
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:24:12 +0000

    turnback somebody deliberately unable set

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_speed(distance, time):
+        return distance / time
