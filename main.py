3fa4ed952b57963c382c5bcebae2e5721fa31bfb
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:14:26 +0000

    home either shop politics 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
