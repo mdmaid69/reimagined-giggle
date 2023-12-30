d207b6b2370cd03f1b866b542d53f35882a66851
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:37:33 +0000

    takeover care solve reject talk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import platform
+def get_python_version():
+        return platform.python_version()
