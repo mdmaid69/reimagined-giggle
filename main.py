33941c659345f4a0f5ba56a3ecdf38b61bd43af0
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:19:52 +0000

    Rr bringdown crash education hear

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import array
-def clear_array(array):
-        array *= 0
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
