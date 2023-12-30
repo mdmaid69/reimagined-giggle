81d4c47516c1e8fcc8bdebc86c5e969c59ff87cc
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:00:58 +0000

    drive table by method gap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def remove_duplicates(lst):
+        return list(set(lst))
