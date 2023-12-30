6c46d9135be640568f033aaf6d54501867a5a020
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:49:03 +0000

    stop wake fetch 569enjoy drive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
