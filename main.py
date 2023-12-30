9393472fc28edbcf4d28a82075b59451c7457a18
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:50:35 +0000

    especially lie bottom true average

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import os
+def get_current_working_directory():
+        return os.getcwd()
