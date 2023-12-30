09868d93d908a54531d5939189ca77f9a9948c7a
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:57:50 +0000

    weigh convenient music comfort wipe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
