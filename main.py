cc193da254a35060cc76ef7c8770c69f11b7f1ec
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:25:07 +0000

    error year steep plate this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def convert_to_hex(n):
+        return hex(n)
