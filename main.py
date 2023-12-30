19b4cfb2edd2381f43b4a001a32ef3465fb297aa
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:55:18 +0000

    dance dream wall shopping practice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_volume(length, width, height):
+        return length * width * height
