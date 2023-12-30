685cf9e672061dcb9cb233877310fb58ea29abf0
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:29:33 +0000

    pollution debt plenty time not

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sys
+print(sys.version)
