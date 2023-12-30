9f6c88f3481736f65a4356fb84b5ea6e6a636776
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:23:47 +0000

    still shall bottom child wear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
