e751d4efc783f6d1cfcb193194dae5587f6ca1cd
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:37:42 +0000

    furniture shape breakdown crowded door

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
