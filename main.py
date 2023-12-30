86d00b685b129667dbf7a97fc01f0066839d8836
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:23:48 +0000

    fold middle too considering fact

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
