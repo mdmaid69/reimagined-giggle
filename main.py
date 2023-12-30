fce2a71123182dc93ceb83cb091728011da052f7
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:23:01 +0000

    best impossible event must rain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
