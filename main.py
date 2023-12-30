1caad698ac158725a95a7406554cde8dae418a90
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:59:50 +0000

    advice mixture bite string smart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
