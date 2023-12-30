aeec7e24559c3eddc06aa47039a97f382a0a6a56
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:10:17 +0000

    regional song fierce file some

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
