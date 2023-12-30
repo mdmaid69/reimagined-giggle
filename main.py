6ad52b096bcda57a194b675759a8705cc7c36bbd
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:04:14 +0000

    judge afraid slow unhappy bag

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import tensorflow as tf
+print(tf.__version__)
