d7b4cbfb4f6e96df321b53150e47b6e85ae699c9
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:27:50 +0000

    party thirsty shop welcome at

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
