e82a06753ea62e67818e0a1f89b9a885fb1117d2
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:08:37 +0000

    know scene tone bank fortunate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
