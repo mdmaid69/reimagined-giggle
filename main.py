ba366d26007f6db6e5ca076711d082c9d998bcaf
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:55:04 +0000

    want actual disappointed health happy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
