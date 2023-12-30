029013d17f722f67f01ca8cc442b7fe077e6bb34
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:20:09 +0000

    rubbish before tough apart bear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
