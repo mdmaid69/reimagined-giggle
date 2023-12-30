38df2dfd98a3210db6cb58b545276031bc23537b
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:02:21 +0000

    recovery free middle club hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
