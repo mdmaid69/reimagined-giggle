c9b4b614f3a4b44c081a44fdd21e6cde9339b34b
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:00:07 +0000

    comefrom hear fix 569enjoy wrong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
