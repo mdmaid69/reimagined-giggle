f903f8785b3119cc631736dcc80d231ed5587f1d
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:34:36 +0000

    ocean translate borrow switch slowly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
