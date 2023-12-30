928b2eae3eb82f3dabe9fee38c664f710aabea18
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:57:41 +0000

    keepout apply tune black cheek

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def convert_to_hex(n):
+        return hex(n)
