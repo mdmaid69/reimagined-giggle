e2c81b2a266f93fc6f2a08e84bed2396d3c0fb1a
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:03:24 +0000

    state degree Gg trap find

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_array_to_list(array):
+def get_list_from_array(array):
         return array.tolist()
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
