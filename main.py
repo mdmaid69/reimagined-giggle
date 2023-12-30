124ef3018b1208e2d14a285bc41978b20965082c
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:56:12 +0000

    agreement final substance support touch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def print_python_version():
-        return sys.version
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
