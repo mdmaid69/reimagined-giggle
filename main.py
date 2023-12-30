431afe75974093a5d768c8ef596c88a255a10648
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:17:37 +0000

    although forever reply impressed star

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
