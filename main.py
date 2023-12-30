9f5ac1ae5f585024e85f0e2e219df0cf097a390a
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:33:56 +0000

    beautiful goback(to) pollution colour Ee

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def convert_to_octal(n):
+        return oct(n)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
