87af73be8fbbdf8026d8543e8378ff7e07e71565
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:17:01 +0000

    train centimetre breakup nurse temper

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
