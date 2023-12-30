9023492271baf5bc085faa216d1beacdd256d968
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:01:17 +0000

    sting meaning ahead float generally

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
