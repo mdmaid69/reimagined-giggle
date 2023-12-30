e61212331d4c9642f9b57c41d3b7ccbf2c87dd2a
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:12:17 +0000

    right turnon mouth play mention

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
