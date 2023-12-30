1fdfe5e1b587ba0b4ee6c7f2fa84ad62b8013667
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:42:42 +0000

    underneath comeoff goaway description correct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def is_palindrome(s):
-        return s == s[::-1]
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
