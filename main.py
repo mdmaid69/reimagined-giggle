b76bfdeaa83cc5ec80ac341191a23c3f0282c3b0
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:10:59 +0000

    need comeover description evidence close

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
