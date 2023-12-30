440d0c62dae26f79fa18a8e3c5ccbd86aec31bbc
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:09:35 +0000

    friend inquire just design definite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def get_array_as_list(array):
+        return list(array)
+import array
+def get_array_as_repr(array):
+        return repr(array)
