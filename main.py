021b4918e909945b3d1c4e6c8142615dbfb9fca7
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:38:10 +0000

    past crowded loan impatient system

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def count_elements(lst):
-        return len(lst)
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def reverse_array(array):
+        array.reverse()
