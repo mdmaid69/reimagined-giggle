279af7199dd1912f31e9bf803199ee3d45ef5be0
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:41:51 +0000

    about force cheque mouth brush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_work(force, distance):
-        return force * distance
+import array
+def clear_array(array):
+        array *= 0
+def find_max(numbers):
+        return max(numbers)
