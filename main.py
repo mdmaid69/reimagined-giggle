bc90102a5e4b3e37deb933669aae2928874529fc
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:54:08 +0000

    pick press think ring pull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
