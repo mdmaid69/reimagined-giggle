d16e136df9afb610a445eac922e3d330c62e7cd9
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:03:53 +0000

    in kind conduct second pinch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
