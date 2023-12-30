30fe529baf64cd0195ebd174ea4b4fcc0c9f0ac8
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:23:23 +0000

    ago respect it admire food

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def subtract_numbers(x, y):
+        return x - y
+import array
+def get_array_as_list(array):
+        return list(array)
