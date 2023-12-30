686ca47c7499508a81e2cef77acdfc264e88d5f2
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:47:00 +0000

    responsibility shade wave handle all

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
