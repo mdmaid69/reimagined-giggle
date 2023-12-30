70e79cae5dd864b3bb81d89bf146813768ab42ec
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:18:31 +0000

    sea fond fasten search forgive

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
