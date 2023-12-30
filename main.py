dec0ee3758526ccf67c626728cd24371f9f3aec2
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:30:15 +0000

    goback(to) embarrassing reserve wet pronounce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import sys
+def exit_program():
+        sys.exit()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
