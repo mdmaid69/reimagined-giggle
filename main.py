8cb07f732a423ea69d0ffa5e6e7b68c006563ad2
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:14:38 +0000

    probably bet where balance respect

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
