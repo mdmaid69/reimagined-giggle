efad2c66c8d1bd7f1aee0f03d03961ce7d7b5091
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:22:27 +0000

    keepoff judge smooth load step

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
