60251801b819bc6ad864cadeaafe6edd9c2f7a07
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:49:33 +0000

    luck cough mystery state profit

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
