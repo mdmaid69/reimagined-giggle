309fc6f3c2264d96b100507a137c6f2deaccc876
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:47:04 +0000

    get exact write cost slow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
