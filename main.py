74eee9bd974019de0ab3b66a6e819aed4bf6cc8e
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:59:05 +0000

    tomorrow profit repair often actually

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
