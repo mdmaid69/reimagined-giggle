2e8abf5ae9983c80fd50ac647b4d0e9b4bcdfd64
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:49:25 +0000

    rain noisy ask partner rescue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
