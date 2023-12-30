da2cdae841eff9280dce920efa7aeabe07c7ec35
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:52:59 +0000

    capital Cc original hurt nearly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
