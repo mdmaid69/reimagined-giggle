c93fd6b616e15e61068f855611ca659352934abd
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:40:38 +0000

    discuss order right then next

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_stack():
+        return collections.deque()
