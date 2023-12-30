9fce875997ad9ef0c722e0fdfe9839c4e26e95e8
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:47:47 +0000

    somewhere couple pure mistake factory

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def remove_directory(path):
+        os.rmdir(path)
