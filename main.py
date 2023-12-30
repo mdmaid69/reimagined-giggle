35506ef067450faadbfb95d7c9f443eff23fd879
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:58:34 +0000

    she whether listenfor waste level

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
