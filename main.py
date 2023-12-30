148a644216c4d32645a3f988a8ef06ece820fd36
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:51:09 +0000

    pain proof far angry about

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
