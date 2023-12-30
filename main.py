2603ebf18caf0e47106c241acbf682b8a504c220
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:36:44 +0000

    mind disturbing immediately right rough

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
