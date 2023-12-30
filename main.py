405942296429ae83f2eb79ef867c51cf5a38f6dd
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:05:12 +0000

    kill to rob world no

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def find_max(lst):
+        return max(lst)
