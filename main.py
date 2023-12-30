1b5cfe16e58e0dd168e539888397c24a168bfb4d
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:00:36 +0000

    prefer touch knock morning getout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
