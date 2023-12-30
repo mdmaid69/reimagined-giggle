4e42bcae21f5ab552c49f907ea34999c2321db64
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:12:14 +0000

    like against computer evidence break

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
