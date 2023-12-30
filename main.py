1e7a647dc0248bf3b6b2e6ef0600ca7afa446c19
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:07:09 +0000

    bill dry damage pair size

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def add_numbers(a, b):
-        return a + b
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def count_characters(sentence):
+        return len(sentence)
