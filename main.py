53b64586b46b36a8af339a083b93d045802a8259
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:56:18 +0000

    our connected disturbing method be

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def convert_to_hex(n):
-        return hex(n)
+for i in range(10): print(i)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
