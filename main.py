a5666f1b5c788f7cadc6866c7758db6791e9f75c
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:17:15 +0000

    play feed theirs size pretty

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
