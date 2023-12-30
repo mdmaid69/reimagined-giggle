3555b5ef72479729dcb64e1275aa2de631e7c041
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:45:03 +0000

    suddenly read lean later event

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
