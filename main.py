bf72e6f3dc03707ae593f05e87ec58bea689c878
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:32:26 +0000

    listen high lend quit invitation

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
