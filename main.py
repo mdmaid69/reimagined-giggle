623cbe517fcc9c18a9368a871bce4cb97efdbd63
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:32:53 +0000

    fast possible art shop official

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
