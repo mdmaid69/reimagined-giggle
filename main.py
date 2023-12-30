c333f7fa31cd80bc900b6d579e6b9d336b1c6299
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:03:53 +0000

    book sort phone lookback(on) intend

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+for i in range(5):
+        print(i)
+import json
+print(json.dumps({"name": "John", "age": 30}))
