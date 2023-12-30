e4f179fa7ba8be180ae36ae883427f691b60d66c
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:37:06 +0000

    divide form shout disappear blow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import array
+def get_array_as_repr(array):
+        return repr(array)
