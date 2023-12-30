fa2a1d8c68eec7d8841c4553be3205943cc43226
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:54:21 +0000

    gooff certain recommend create attention

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def count_words(sentence):
+        return len(sentence.split())
