9948f5178fa2cd9838912dbe34148513f08191e2
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:01:54 +0000

    copy touch react rent themselves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
