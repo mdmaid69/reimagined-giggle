081f876e4ac618bc8b336b27a7981d1576edbc44
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:50:18 +0000

    accommodation lift exercise giveup fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
