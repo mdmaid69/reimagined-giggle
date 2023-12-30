3b8c61ec54295c7e4b800539fdddc6828256b14a
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:39:56 +0000

    567enemy cry board regret takeaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
