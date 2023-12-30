5ee30df63c540fcf80e1e3f519a90fc60be62aba
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:44:56 +0000

    report Kk call obvious none

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def square_number(x):
-        return x**2
-import math
-print(math.pi)
+  def multiply_numbers(x, y):
+        return x * y
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
