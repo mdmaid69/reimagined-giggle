7bf8f2efcf5d9abe296c7f31d3f2e926d340bab7
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:14:45 +0000

    steal door artificial worry rubbish

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def square_number(x):
-        return x**2
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import sys
+def exit_program():
+        sys.exit()
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
