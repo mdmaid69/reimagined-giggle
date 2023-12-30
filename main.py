d206658e920bb9872788d99e704010ab47f93d44
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:24:13 +0000

    callback combination suit punishment unhappy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def greet(name):
+        print(f"Hello, {name}!")
