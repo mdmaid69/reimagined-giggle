dd64a73e18b10e15f0f165a2511ac2e575f8ea42
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:10:06 +0000

    wave regret together shame new

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
