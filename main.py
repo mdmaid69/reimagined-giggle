afd676082d5e08b74a06b22eb579f3316a0e3075
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:51:50 +0000

    active main question post form

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def calculate_area_rectangle(l, w):
-        return l * w
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
