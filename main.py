ecd53c5637fc5e670ca41d1b6e5d5350aaf728e3
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:24:03 +0000

    pride kind prize welcome fierce

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def convert_to_octal(n):
+        return oct(n)
