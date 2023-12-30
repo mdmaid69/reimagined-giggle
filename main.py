88257c7628cce03899367123198a20f716f57404
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:07:31 +0000

    round partner tune hurry depend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
