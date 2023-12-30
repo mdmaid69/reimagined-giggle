96666399ed04fbfae7c1e8916ce8062a60aa322a
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:24:17 +0000

    over exit bored smile mark

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from collections import Counter
-print(Counter("hello world"))
-def calculate_power(work, time):
-        return work / time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
