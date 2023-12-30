25af4d2ea20e0ed5524dbe99e00dcf7ac78a6d72
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:55:28 +0000

    experience nurse gentle but science

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_acceleration(speed, time):
+        return speed / time
+for i in range(5):
+        print(i)
