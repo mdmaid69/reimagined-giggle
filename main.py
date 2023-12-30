bc0071b1449d776e7ff9d15edf90ae848eee95a7
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:00:16 +0000

    make far frighten quarter ahead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def calculate_work(force, distance):
+        return force * distance
