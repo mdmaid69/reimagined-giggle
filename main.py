420b6be40928b4dfffd1d38befac7925abfac096
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:32:09 +0000

    breakout better unhappy identify share

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_speed(distance, time):
+        return distance / time
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
