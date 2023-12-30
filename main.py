07fce8869790d71e552f6719271295454545ce7f
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:08:58 +0000

    bite air successful turnaway comedown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
