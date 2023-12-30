d4404c8f0bf771c22857caad937cc393299804d9
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:53:02 +0000

    except down meeting show tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
