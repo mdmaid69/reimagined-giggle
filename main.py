8298ee82a01ab07a172d1492d4788b7110142840
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:43:58 +0000

    fire contact concern embarrassed when

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
