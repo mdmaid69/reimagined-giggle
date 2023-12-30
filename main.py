2f966e54482d7478b7c526149054d1ef062f6870
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:00:25 +0000

    think claim complete advice private

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
