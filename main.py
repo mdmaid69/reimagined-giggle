2d98b4505ab99fa5f9fc51d23c4b60d9b4bd24d2
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:05:27 +0000

    pupil through feature motion nose

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def count_elements(lst):
-        return len(lst)
-def add_numbers(x, y):
-        return x + y
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
