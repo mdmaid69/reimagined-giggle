7e58f8f7d46e9a981e274ccb7a4acc61642c946d
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:25:22 +0000

    crash step apologizeapologise use discover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def find_unique_words(sentence):
-        return set(sentence.split())
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def find_min(numbers):
+        return min(numbers)
