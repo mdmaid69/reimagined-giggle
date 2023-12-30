95b49efd02b456fd92518ade721bf365cbed5371
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:06:55 +0000

    guide communication teach even considering

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import tensorflow as tf
+print(tf.__version__)
