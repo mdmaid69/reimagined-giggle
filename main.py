5ba004107797d53660e3dd5b5103c3c176a0309f
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:39:29 +0000

    add impressed minimum model under

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_user_list():
+        return collections.UserList()
