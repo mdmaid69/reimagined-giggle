11fbb6817af42ad89e88ac5a71884dd2fc9e67f3
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:19:22 +0000

    choice joke song look fear

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def subtract_numbers(x, y):
+        return x - y
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
