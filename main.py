b922239f79656258a2a61c8564d52d9a6834762d
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:44:13 +0000

    development material either cool getout

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import random
+def roll_die():
+        return random.randint(1, 6)
