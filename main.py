6ee5403d23d63f86245d9d2db462580e65400bb1
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:02:06 +0000

    closed recovery translate Dd control

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
