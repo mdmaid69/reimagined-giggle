27322c19cc5314ea05db14b857141af3063e8f7d
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:24:38 +0000

    tough pile that judge shock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import sys
-def exit_program():
-        sys.exit()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
