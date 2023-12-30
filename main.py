bba99ed13d0be5c29d8bddb905eb332e146e336c
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:14:37 +0000

    track error general but later

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
