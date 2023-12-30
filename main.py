0d496f014f4018f42d66d1db244bbf6e83c15a08
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:46:21 +0000

    science quickly me organize report

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-print(random.randint(0, 100))
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
