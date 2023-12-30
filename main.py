3a254a0d87493498c5620c871fb3cc397351c4d1
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:12:32 +0000

    whichever hers keepout mistake shrink

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_user_string():
-        return collections.UserString()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def find_max(lst):
+        return max(lst)
