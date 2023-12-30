4d8ee82452b13f95916e0e825b959abdc4d5fdfb
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:52:02 +0000

    extremely wish influence platform avoid

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_pressure(force, area):
+        return force / area
