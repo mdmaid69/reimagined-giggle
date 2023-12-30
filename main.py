c4e778d193f9018fcb99356ac3af3f690b75758d
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:59:11 +0000

    union confuse gas especially agreement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
