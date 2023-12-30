c65d3163e82ed1652e654386e1445b30eaf56254
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:05:14 +0000

    pollution pleasure chief through his

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def find_unique_words(sentence):
+        return set(sentence.split())
