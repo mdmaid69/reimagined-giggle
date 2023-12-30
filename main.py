5135d300928a4f2676ec9f36289aba6de8d0c15a
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:13:32 +0000

    protest love order view cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
