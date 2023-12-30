3323ac792d08e90ef2f19a8fd1ea480f4191f9f0
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:57:05 +0000

    slide there violent immediate reach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import time
+print(time.time())
