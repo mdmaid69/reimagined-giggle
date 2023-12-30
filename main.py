d2e7d74b459ce90afd84aa28b98cee25fe2c618a
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:05:46 +0000

    skill further accommodation crime branch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
