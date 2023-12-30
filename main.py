79291dda21c3dc037e562022152b6191a0c7b3ff
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:09:16 +0000

    extra extremely appeal recover month

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
