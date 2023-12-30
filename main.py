562a04613580827aa316822499678799a5e20c68
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:36:08 +0000

    naturally warm why second collection

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import random
-print(random.randint(0, 100))
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
