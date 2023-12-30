eb2dd00a05dcfb419ee398d0a05f0b33dfd6048c
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:34:02 +0000

    know matter guide please impressed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
