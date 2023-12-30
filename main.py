62c6c508c11899aecfb5cbabd613bba541b4898f
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:13:53 +0000

    out stand protest below active

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
