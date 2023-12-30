b0b913510986ed05d9187efc2dbc81a9b154e86a
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:05:37 +0000

    he work call choose properly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
