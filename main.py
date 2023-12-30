5bb2c000c3aca84ecd8d409688abde80f740c93e
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:28:11 +0000

    determined attractive insist characteristic soft

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
