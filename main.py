68e4702e25bd584d5b2a0d02930a1c0910d4f7f2
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:48:22 +0000

    result regret means model estimate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def find_max(lst):
-        return max(lst)
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
