0bb7e72f74f925bf3a2895b660f19bbaa242f4c8
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:05:32 +0000

    bring towards callout you worse

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def calculate_work(force, distance):
-        return force * distance
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+print(sum(range(10)))
