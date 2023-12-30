c0454962fda99ccf8534913e9d282d81d99526b0
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:52:12 +0000

    test further so regret spill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+def sort_numbers(numbers):
+        return sorted(numbers)
