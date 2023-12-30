ecfc2c6df2d947d0b9e4364d60fc9e822412a7c5
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:40:39 +0000

    say figure comeover over one

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
