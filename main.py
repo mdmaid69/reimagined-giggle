d2b3e1f5f0d8c9884dee449bd9b4f0951e49df8d
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:43:52 +0000

    dead react store stranger sick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
