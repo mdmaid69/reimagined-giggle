bbeaeb455080bf87607275f66f45ea7573351174
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:52:17 +0000

    radio common pleased forget brain

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def count_words(sentence):
+        return len(sentence.split())
