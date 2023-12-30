aa9f63a6d98f9f86dedf43469ebbf5cba11e5ed4
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:23:04 +0000

    bringup material cross eat seat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def convert_to_binary(n):
+        return bin(n)
+def sort_list(lst):
+        return sorted(lst)
