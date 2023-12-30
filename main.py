6944f4ca3a3f828a2f85b6d54c802846c2f2f821
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:38:53 +0000

    tape discuss whenever impatient separate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
