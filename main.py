f0a00244d01d83a400b699aa8387c24e2650e5d5
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:53:49 +0000

    listen press news he connection

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
