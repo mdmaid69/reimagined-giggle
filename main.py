80a4a34a6ee429002e22ae1d473093472274f52f
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:28:01 +0000

    interested tour quiet please number

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+for i in range(10): print(i)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
