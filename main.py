be60f1c78cdc80b62cafdf551ee9db40c0b6fcd1
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:24:50 +0000

    admire shallow touch dull illness

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
