bfe6103f88f69eddaab3717aff45f4753fa0f5f9
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:47:24 +0000

    choose nature insist nonsense manner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
