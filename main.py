15716f8beaf12930488f92553606cb2ffcdb08ee
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:19:53 +0000

    thanks square give side concern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def count_characters(sentence):
-        return len(sentence)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
