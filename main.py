ae809411d7272bad0f9d933b97dd60197c8aefc9
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:59:26 +0000

    knock prefer dance telephone all

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_min(numbers):
-        return min(numbers)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
