d848419371154afe68c8f01f0bc58e64ca04de76
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:30:00 +0000

    clear neat mess help confident

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def subtract_numbers(x, y):
+        return x - y
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
