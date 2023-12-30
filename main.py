ce09fa21afd80d89321c9f33e9df8408db2201cf
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:39:28 +0000

    cap region raise object fierce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_pressure(force, area):
+        return force / area
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
