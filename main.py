fe8ec4e07b9b5e03db2b0068b1fcdbde7c740362
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:35:57 +0000

    talk attempt turnaway male cover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
