ff2805d3e9aa758637cd43613e5f278c60d8fffd
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:33:12 +0000

    factory by path disappointed score

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
