688b29356863d77ba15b9fbf01678037526b5cf9
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:36:38 +0000

    do bush convince charge occur

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
