9620b40d777b5169584b3b87424f59cf9a413dfc
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:45:47 +0000

    even proper clear rest scare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
