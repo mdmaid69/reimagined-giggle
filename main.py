adab2a60adec51de934ac7f5562505d0e3a47277
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:53:20 +0000

    just steam moon generally raw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
