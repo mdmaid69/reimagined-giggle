8f82a7250724ed05b196e176f9b90595837cda07
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:16:32 +0000

    authority join fear attempt our

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+from collections import Counter
+print(Counter("hello world"))
