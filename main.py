aec53af70c6952f2bbd1aa031c90911ae2820def
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:55:31 +0000

    turnover popular warning confusing attempt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
