f19d738f2ffe68e70ad843c9e65bb31d983a319d
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:08:31 +0000

    hand own inside born bite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+  def convert_to_binary(n):
+        return bin(n)
