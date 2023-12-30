c1b8d72b376c719a511b746ea18f799f06772736
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:39:55 +0000

    issue pause shout food so

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
