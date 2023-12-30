da7acf15519e64c8b1c3521edf2257c90200c3aa
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:55:42 +0000

    spot before discussion confident community

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
