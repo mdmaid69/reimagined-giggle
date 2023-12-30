c692819e3a840d5a0e39c69f398b3360b09bc21f
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:20:30 +0000

    window exchange profit lonely road

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def convert_to_hex(n):
+        return hex(n)
