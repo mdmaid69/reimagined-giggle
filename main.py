49bcc751fafeeeac4e6c6ba384071b7ea8a50e61
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:20:40 +0000

    lean murder bar smell man

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+def greet(name):
+        print(f"Hello, {name}!")
