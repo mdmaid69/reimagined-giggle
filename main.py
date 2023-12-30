74ce9b7b15c71b2d7d3726f6f7d2b003103de383
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:53:08 +0000

    debt anyone generous degree mainly

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
