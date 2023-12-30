4d878cef468fb18b9afcd4e73d10f38e8ab77ace
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:26:45 +0000

    upset confuse seem fit spoil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
