17a50bd414b6ba1213a0f42bd34e81c44bfabef4
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:31:51 +0000

    reason accommodation comfort sentence wide

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import numpy as np
+print(np.array([1, 2, 3]))
