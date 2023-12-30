98b1a0e9e69c4ffc7df1d96a1c87c1d3c524aad5
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:55:31 +0000

    instruction cover warm frighten because

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
