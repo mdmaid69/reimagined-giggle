f4b2f262dda4e2374372d2b912a966e414f3adfb
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:15:04 +0000

    see cause belief yes win

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def get_array_as_repr(array):
+        return repr(array)
