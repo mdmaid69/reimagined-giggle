623aba550b8604f035a234ae90dd1155b9eec760
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:35:52 +0000

    politics responsibility information noise knock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
