bf2a5cb9d06f947db4653a55d49d00a5ad327e0b
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:44:42 +0000

    spring before all reduce front

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+def calculate_work(force, distance):
+        return force * distance
 n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
