186c8597305be020775e56e8ebf855ec84a9dd51
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:27:53 +0000

    either ashamed once control train

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
