68d23df90ca86fba1f4fb92e6c27a8fbe5e1baf5
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:14:49 +0000

    pause attempt wonder police hers

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
