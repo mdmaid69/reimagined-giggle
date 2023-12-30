7000584fd755910b6cf16542ad93279735925908
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:46:21 +0000

    guess extra owe that teach

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
