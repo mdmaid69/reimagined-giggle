f12239c78a2ba6c61de5c6c1ed6ae2b85ec94b20
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:02:02 +0000

    exit public work offend university

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def sort_list(lst):
+        return sorted(lst)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
