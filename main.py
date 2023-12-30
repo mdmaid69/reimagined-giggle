397b6fb56afff823ee8d224255ad00cf9688a6d7
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:41:00 +0000

    mad sweet glass born confidence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+name = "Python"
+print("Hello,", name)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_index(array, item):
+        return array.index(item)
