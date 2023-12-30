1286927b565e3ee8080dfcd4900fcd826c5f018e
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:55:13 +0000

    accident doubt arrive peaceful complain

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
+def sort_list(lst):
+        return sorted(lst)
