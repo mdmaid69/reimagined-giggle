46fb2379d2ddc055e2c83286d1f801dd05880ba6
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:31:28 +0000

    dangerous free interview need cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
