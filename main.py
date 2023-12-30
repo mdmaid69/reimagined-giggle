9cbeae459700983b5265861c0b76492ad8558335
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:21:57 +0000

    protest flood smell immediate sad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
