02c35742e720a6b5210c159f4111523add4bfe82
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:34:36 +0000

    judge over straight yours behaviour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sys
+def exit_program():
+        sys.exit()
