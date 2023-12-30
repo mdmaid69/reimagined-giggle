8f8d35e5771e0879a5702289d54e1f201008e7e4
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:30:51 +0000

    moral talkoutof point hurt operation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
