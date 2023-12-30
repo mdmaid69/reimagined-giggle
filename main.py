fdd03522a95419af0b202343cc601f1185986844
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:32:45 +0000

    somebody laugh keen approve cruelty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
