3504f6a64bc5f922a87738175f0dadc82a8aa85e
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:27:17 +0000

    extremely praise cheap dare brave

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
