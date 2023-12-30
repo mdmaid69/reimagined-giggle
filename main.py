e7b0d41fb721816f4c981218f495730e8e3969df
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:19:09 +0000

    refer stage allow reasonable limit

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def find_min(numbers):
+        return min(numbers)
+import json
+print(json.dumps({"name": "John", "age": 30}))
