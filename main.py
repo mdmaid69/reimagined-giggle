a98e8ebdd700dd0dc92199799eac10919f6b610a
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:08:12 +0000

    yet classroom acceptable whistle die

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
