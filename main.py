70f65d9c738095d5ec9ca576c87c09b5ebe66c6c
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:12:05 +0000

    bang borrow tour promise rain

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import re
-print(re.match("h.*o", "hello world"))
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
