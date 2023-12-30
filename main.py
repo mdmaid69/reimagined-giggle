8a1902f5bf4d05f731eb35274e3e38452ee5435c
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:16:40 +0000

    arrangement convinced reaction account insist

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
