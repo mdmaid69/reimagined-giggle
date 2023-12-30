c9732555be3b2b28fa5c32d163a00ab3149f6399
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:56:17 +0000

    plenty after difficulty point might

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  def multiply_numbers(x, y):
-        return x * y
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
