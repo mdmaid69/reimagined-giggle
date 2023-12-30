61decc577814abb8a8187f645552b53ba567dc59
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:25:05 +0000

    certainly full pour goback(to) eye

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
