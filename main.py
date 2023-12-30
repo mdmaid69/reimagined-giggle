5d0a4b560a788cbf27a1b04d41ec5abf912efb8c
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:48:57 +0000

    arrange circumstance rain design production

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
