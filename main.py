377ba97f5c6a9d98fccb18f7c6bc05c5038bc085
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:25:55 +0000

    loan favour knowledge tidy neither

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def remove_from_array(array, item):
+        array.remove(item)
