aeb15bcef9d4ffc9391f51a378492b458bfefecd
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:30:33 +0000

    scare worst share spot go

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
