b8d9cb7c69d6532a70de8d1e9e01e1dda5a67a9e
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:52:14 +0000

    arrive affect clear joke central

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
