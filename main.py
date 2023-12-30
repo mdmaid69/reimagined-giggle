390376b6d6da6c1f14e39766ff119634110d0bf9
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:24:08 +0000

    polite confidence street tell chief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
