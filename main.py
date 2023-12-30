df8e0cc1194b03657f6d131d0d6a264ff5c7d5a0
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:02:58 +0000

    laugh basis brother on still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
