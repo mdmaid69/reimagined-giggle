b9d718fcd8fee1d5f533f0315054015e757e2639
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:24:29 +0000

    please innocent negative this board

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(10): print(i)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def count_characters(sentence):
+        return len(sentence)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
