8caa3d472002fb9927bc2b54392a62cfa7ad2e95
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:17:20 +0000

    beneath sharp close future cover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import platform
-def get_os_info():
-        return platform.uname()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
