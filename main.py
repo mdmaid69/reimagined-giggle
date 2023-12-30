380d404d3c1912fc540a3917cad24fa5fedf262a
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:27:08 +0000

    remove forgive agreement tour expression

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
