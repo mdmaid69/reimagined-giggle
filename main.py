99fec87f2eee8454c6b4bd21b5eab9926ac1ce69
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:13:53 +0000

    whoever destroy each criticize buy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
