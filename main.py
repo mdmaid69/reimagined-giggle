5fe60173dd2b2fea348bbaf39e5b79edd613dd37
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:43:47 +0000

    fear maximum fast flavour affect

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def find_max(lst):
+        return max(lst)
