ac83af97700c481b289fb55ac83ca7e74487bab4
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:09:25 +0000

    division life man mention race

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
