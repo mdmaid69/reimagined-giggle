6867311e4b70e4580cd5a6cc9caf8024aa74d662
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:30:01 +0000

    fear warning raise common characteristic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def sort_list(lst):
+        return sorted(lst)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
