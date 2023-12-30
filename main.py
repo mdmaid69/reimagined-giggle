d99b3f84e10e4eb4bcdf6424e93dcf4a801db619
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:43:03 +0000

    fit sick happen talkback reasonable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+def exit_program():
+        sys.exit()
