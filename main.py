fd9a444a3a256ce2b5c644eb714cfa4a238ea977
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:37:04 +0000

    trust apply 572enter scene male

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
