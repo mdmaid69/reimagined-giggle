c3624f39e84bd859590593a19a7c988a8b80bb18
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:43:15 +0000

    pronunciation declaration greeting thinkover polite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def is_even(n):
-        return n % 2 == 0
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
