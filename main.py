d92ae9344082e225b914998c51114663e023b39f
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:54:16 +0000

    occasional comeround include science check

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-text = "Hello, world!"
-print("Characters:", len(text))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def multiply_numbers(x, y):
+        return x * y
