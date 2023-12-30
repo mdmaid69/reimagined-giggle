8e940b47f0ffde2f31e2f1d0cde3c0074a1b555c
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:33:50 +0000

    rubbish unhappy free disappointed unlikely

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import numpy as np
+print(np.array([1, 2, 3]))
+  def convert_to_binary(n):
+        return bin(n)
