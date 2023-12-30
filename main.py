8cbd254b02c80fca25241b46babadc9215649047
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:00:41 +0000

    Gg sharp trade sell active

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def greet(name):
+        print(f"Hello, {name}!")
+print("Hello, world!")
