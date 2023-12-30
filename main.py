a513466d9e93c1486786f08a2ce52f67a4c7cd32
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:51:55 +0000

    entrance guess basis woman suckme

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
