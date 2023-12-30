09d61d7ee2af718b4918635bb333fb96876a8486
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:13:04 +0000

    but leave part machine goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
