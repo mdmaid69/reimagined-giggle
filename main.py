df571397e10bf880966211f7103b62d4c580592a
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:17:16 +0000

    early star flat ill worse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
