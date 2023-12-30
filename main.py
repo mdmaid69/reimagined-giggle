5f3aa9befa0b329fdcbd31923a1c2d73e8a1a551
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:14:12 +0000

    light even fall plenty answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
