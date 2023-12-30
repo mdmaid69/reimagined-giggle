29724ec620c8c1192c7cfa94574d0e0c2bd63b3d
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:54:11 +0000

    improve favour piece considering study

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import platform
+def get_os_info():
+        return platform.uname()
+def divide_numbers(x, y):
+        return x / y
