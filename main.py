02002b4dd89a5d38bd028a83cc52514097774cb8
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:35:52 +0000

    figure off short authority artificial

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_density(mass, volume):
+        return mass / volume
