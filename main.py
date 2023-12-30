657722978a3ba2c00158a7d8a190110987b22cc3
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:41:26 +0000

    combination behave plan what whom

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_square_root(x):
+        return math.sqrt(x)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
