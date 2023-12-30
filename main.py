db0fa25f4ed63245509918888a64b02530dcca7a
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:35:08 +0000

    too warn lend pool describe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
