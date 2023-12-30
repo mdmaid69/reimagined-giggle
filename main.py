7e016270ea09e58a431a0f6ab759dcd5a9f11549
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:11:56 +0000

    priority would fine apart custom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
