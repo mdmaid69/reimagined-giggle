d79be29359ce99ece489211fb845fb367304425a
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:06:25 +0000

    consider breakfast nerve this play

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
