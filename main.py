7a7f3e22ef7925b2c9b955632456e8130264a4f9
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:47:50 +0000

    bad trip sell manage off

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
