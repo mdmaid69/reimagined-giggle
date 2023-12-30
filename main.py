b8a51cf857a92fdc100537585910bda4acbcc6d4
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:40:44 +0000

    fill objection mean appear union

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def reverse_string(s):
-        return s[::-1]
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
