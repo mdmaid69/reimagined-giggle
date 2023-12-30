59fa5c46fbdaa85127d62a0dc35a72e5c4765647
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:25:32 +0000

    moral stiff since chat healthy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
