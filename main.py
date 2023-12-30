d4ec0437cc91dfc9070202938979b09cd96bbbbe
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:20:23 +0000

    turninto thank separate doubt calmly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_current_directory():
+        return os.getcwd()
+print(sum(range(10)))
