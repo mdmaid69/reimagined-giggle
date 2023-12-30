502de76ed8ecf609e0dd9eae0d2a32dbabceb75e
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:30:26 +0000

    tip week attempt telephone until

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
