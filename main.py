52c07c5b02d44821b232daefed72b79129c4ad1c
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:50:15 +0000

    sleep contain night dance bury

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
