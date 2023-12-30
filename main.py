a688b51f6501e7324470b35eebb9af826e8497bf
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:26:05 +0000

    increase fine refuse good worse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
