43c90278c5362a6e52d61e0f5d4407d3e5e8693d
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:24:33 +0000

    lean life guide before lie

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
