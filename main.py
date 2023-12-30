68017c0b7f59c55aa3a25db15a02ff1f03cb04d7
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:24:58 +0000

    need stage television boil talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
