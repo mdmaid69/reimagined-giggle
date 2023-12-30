dbd8b93f6ffc410a5da2ad31c8402eff5be75983
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:22:54 +0000

    pure lie ours Yy they

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  def multiply_numbers(x, y):
+        return x * y
