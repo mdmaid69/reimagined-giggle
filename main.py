8eebea42ed1cc89c7ada454654aa0ff49c3b4f4c
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:27:58 +0000

    turnaway colour Rr song group

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
