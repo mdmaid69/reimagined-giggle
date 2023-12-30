4d116741687a9ee519b430ec2452adb550cc76e4
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:38:41 +0000

    workout rather every section Ll

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def extend_array(array, iterable):
+        array.extend(iterable)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
