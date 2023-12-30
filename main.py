d30a2b75a5f38572615c1d738bf51435daf5dabc
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:33:20 +0000

    truth steep so anything argue

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
