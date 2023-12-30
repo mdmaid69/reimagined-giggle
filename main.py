4f2a358d16d283831de1ec1f63bdd6a500d86329
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:41:32 +0000

    length decide clean depend influence

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import collections
-def create_queue():
-        return collections.deque()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+print(sum(range(10)))
