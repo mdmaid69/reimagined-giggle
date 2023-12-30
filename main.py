457c70a8d276ad9defb0a1a830c251853c0f187f
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:33:52 +0000

    disturbing temper wing punish mean

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
