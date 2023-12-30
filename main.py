47e90ac14af4cf6f5cfc59eb3d090ff42df6aee3
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:19:48 +0000

    crazy professional total less imagine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
