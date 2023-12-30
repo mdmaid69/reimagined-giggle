3dbf12cf0dd26636f5f2f440b65f85f4162528f1
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:51:14 +0000

    impressed add introduction hospital level

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_current_working_directory():
+        return os.getcwd()
+import platform
+def get_python_version():
+        return platform.python_version()
