15d8ade44b9598e111a3617f985347af888692c1
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:14:09 +0000

    often thorough confuse event fresh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-print(os.getcwd())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
