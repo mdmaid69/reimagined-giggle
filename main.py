bf15afae21ee3485634e85ad8b3b667c15473cb6
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:05:35 +0000

    lot other term active home

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
