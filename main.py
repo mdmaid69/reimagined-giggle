668011f654ce60312e7052b97f77564016f9b5ad
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:26:46 +0000

    myself bag share word hardly

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def reverse_string(s):
+        return s[::-1]
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
