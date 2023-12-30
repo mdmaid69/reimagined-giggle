7f6f383b741cfc2eabca95ad275a23fc959489f1
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:31:33 +0000

    pity tool break danger refer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
