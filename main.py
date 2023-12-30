7a16950ae13a35a2718b461c41d8816a8c68f560
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:08:58 +0000

    face ask runaway shopping he

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def reverse_list(lst):
+        return lst[::-1]
