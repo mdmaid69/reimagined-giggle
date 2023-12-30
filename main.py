e8bae759a871809edb31a6c665a4cae28da19800
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:19:16 +0000

    nearly criticism somehow listen turnoff

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
