b6ff1cee6ab26bb97aeb0f1512eb9ecb98a3fc82
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:49:41 +0000

    because certainly relation window room

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_power(work, time):
-        return work / time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
