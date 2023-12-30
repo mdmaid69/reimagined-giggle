ccc111c80dcae96a762b4cb7e8a15a7a39233ed8
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:44:57 +0000

    before deliver favour divide achievement

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_acceleration(speed, time):
+        return speed / time
