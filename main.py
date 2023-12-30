1c00556eeffd8b541d251e151e7a694ced67af33
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:26:20 +0000

    habit strange cross left makeinto

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
