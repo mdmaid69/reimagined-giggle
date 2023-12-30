87d403f948c0a30e0072b4e5a3b42d6ba6f6c7dd
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:00:52 +0000

    deep honest goon success start

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
