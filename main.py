d81e0a0dfabd4d6c36fb9adbee079b234ccea3bf
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:37:19 +0000

    swimming loose free decide wipe

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def get_array_item_count(array, item):
+        return array.count(item)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
