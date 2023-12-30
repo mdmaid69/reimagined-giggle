0f5e07e74bd4727d88b430c10bc6e2eec924b7ea
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:39:02 +0000

    post discover across skin explain

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
