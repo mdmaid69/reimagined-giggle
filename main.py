30150f8002732275a64245fb8d7693ab727cfa7d
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:03:07 +0000

    contrast anywhere free different study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
