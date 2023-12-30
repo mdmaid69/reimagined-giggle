c6fa3b66d403e0fb52e684ec99e248a1e192e2e3
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:42:22 +0000

    throughout content exact turnon getout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
