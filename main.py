6a14070f8245bd8749d0a5d3a885dfefcb2b2b4b
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:40:08 +0000

    expression fast earth university juice

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
