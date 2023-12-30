20a2b3bdce5369979f3b216dbcc19920179480ec
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:26:40 +0000

    return challenge guess whole walk

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
