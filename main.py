a3edc70af3e5a2ff79e35f07c214262f7358fa89
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:12:25 +0000

    help sweep any respect powerful

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
