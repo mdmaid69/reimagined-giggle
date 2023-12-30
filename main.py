717e25f0100cf12d0210d7dc3fc0683ac5dbf5e4
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:06:06 +0000

    squash play connection set switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
