a5b2c8e43ad78c922ff95ba57e750f50c172b807
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:16:21 +0000

    out demand admit off escape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
