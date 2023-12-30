a02e0bed24fe6d6a01d56e98cfab0f8056f001e0
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:03:04 +0000

    time throw spring basis Ff

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def remove_duplicates(lst):
+        return list(set(lst))
