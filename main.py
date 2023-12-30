59d271f09908dfb8917c67ee464199b6629eb642
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:56:26 +0000

    home surface use weekend getaway

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
