26dbcc764a60f1019c218df761e0d64c6c2448a2
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:20:30 +0000

    purpose desperate damp Bb beside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
