4cf82b70eda06a9054b5f54ff950784978f1b0c1
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:30:19 +0000

    happy have no cry climb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
