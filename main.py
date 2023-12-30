0c53e7f4fc8fc5805a110345b2d43e1fca1deb3b
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:38:58 +0000

    calm wind muscle activity idea

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def change_working_directory(path):
+        os.chdir(path)
