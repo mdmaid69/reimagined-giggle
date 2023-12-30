87d37a6f485b94ed1eaa2035ec8f7be27b118bb6
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:20:52 +0000

    wash concerned mouth badly takeoff

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def add_numbers(x, y):
+        return x + y
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
