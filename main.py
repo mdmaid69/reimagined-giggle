12abc792efdeef86776080276ddaea81dcf09e7f
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:32:21 +0000

    stop best tell repeat related

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_density(mass, volume):
+        return mass / volume
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
