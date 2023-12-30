cc55aea8946d5d27c139668190b7a4252135acf3
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:20:54 +0000

    justice temperature carry command treat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import random
+def roll_die():
+        return random.randint(1, 6)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
