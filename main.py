ffbe54994bd9b1936d8e9099967bf51dbc110e48
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:08:35 +0000

    goodbye present bin rescue sticky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def sort_list(lst):
-        return sorted(lst)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
