13b561e4bf53dffdc0035c4a4e901c7b5e5ea945
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:20:36 +0000

    from statement pen follow noise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_int(array):
+        return int(array[0])
