5ef207da8fde6b320536527df1971d4345ac22ed
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:31:41 +0000

    snow wall excuse sell disturb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
