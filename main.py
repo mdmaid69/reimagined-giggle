9b98b3345a07b79aea36a74f864ecf538b181299
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:55:56 +0000

    murder accommodation check silent kick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import array
+def get_array_as_str(array):
+        return str(array)
