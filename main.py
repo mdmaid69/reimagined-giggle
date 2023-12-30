ab7ab8005228a621fcc3233a8a0e0da26eacea7f
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:28:00 +0000

    566end lose fall voice since

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
