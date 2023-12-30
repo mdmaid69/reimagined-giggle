33220e188a330da48a860401fc56e0c0b4d6983d
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:19:10 +0000

    father ever arrival instrument company

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_directory_name(path):
+        return os.path.dirname(path)
