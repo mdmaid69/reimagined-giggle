4b4ef51f60f385775c2802a32f509895b1036307
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:00:19 +0000

    shout boring risk wander fly

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
