286d67fab00047dae379a454c98260a1741db9c5
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:33:47 +0000

    drive giveaway cut sail shine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def square_number(x):
+        return x**2
