ac6d248e6a4bad802f26a475cc85f76cfb34f2f0
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:05:57 +0000

    rescue story uponon own another

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
