175861dd690c0daa7e76552f20501e00c14ccbaf
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:08:22 +0000

    mention skin learn shallow himself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
