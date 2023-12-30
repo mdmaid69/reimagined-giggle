2c908c393f5982a64848612d822a0ce55bda198c
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:24:17 +0000

    throughout soon crash complaint forget

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
