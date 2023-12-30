9a4583f4cdafc813b732171b7fbee746e6f34e11
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:20:39 +0000

    catch pass band decide rush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
