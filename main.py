5481a30b38121eca3250f6b5bd7eadb77f5c7116
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:36:59 +0000

    equal wind thought thin plenty

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
