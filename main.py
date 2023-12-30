1def2e90757549ee8a5de8162c29f03123505fd7
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:37:43 +0000

    activity reasonable shape before finally

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
