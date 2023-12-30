7e288c9765f74a7be51c500efd0e640db0977424
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:52:51 +0000

    Hh skin them drive keepout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_work(force, distance):
+        return force * distance
+for i in range(5):
+        print(i)
