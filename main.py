1a1b712148b442f9eac49056318c3a1dd1d5468a
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:21:06 +0000

    exist but unit under want

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def count_characters(sentence):
+        return len(sentence)
