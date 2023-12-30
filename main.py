61c129f46459a84458d39376b6bc6a6ff4eb0683
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:45:06 +0000

    transparent ever beyond ready bridge

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import sys
-  def get_python_version():
-        return sys.version
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
