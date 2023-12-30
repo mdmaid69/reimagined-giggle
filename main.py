9bdda191b0378e2ef8ae0582c47c678a72d7e0ea
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:03:36 +0000

    crash gap thinkover door in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
