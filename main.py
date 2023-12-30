f50949b14ac7106cddb989c75fd07dfc9423bf50
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:24:54 +0000

    judge display exchange crash connection

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
