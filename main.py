538a0c4ca777a26274c2993d921612abb2995824
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:59:41 +0000

    turnoff period tragedy love loose

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import random
+def roll_die():
+        return random.randint(1, 6)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
