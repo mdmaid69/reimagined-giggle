7ce0128e2324443db5571e5cd86e468ae342dc5f
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:51:40 +0000

    stamp rain occur lead hospital

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
