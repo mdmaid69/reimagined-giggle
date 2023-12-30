2a208782b67fc889aea553b0f4e640428fe5db68
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:43:56 +0000

    unlucky shoot dance fun 569enjoy

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_work(force, distance):
+        return force * distance
+print(sum(range(10)))
