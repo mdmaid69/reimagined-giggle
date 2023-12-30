3b077325e41d1d47f32bd8344c983547192f2a76
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:03:53 +0000

    collect risk range holdon name

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
