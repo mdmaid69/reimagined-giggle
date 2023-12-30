7f122f11e75c31d062891fce5b9520607c7fd88c
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:03:53 +0000

    turnup goon file relation simple

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def find_max(numbers):
+        return max(numbers)
