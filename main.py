31a58aa66b95fca27116c94a8d408f406e593262
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:45:52 +0000

    contrast bringback people many this

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
