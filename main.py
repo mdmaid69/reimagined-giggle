0c6a83aa88a710910b9f02bd1adce34f91889af2
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:41:55 +0000

    find read street burn embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
