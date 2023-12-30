fe84c76e265267c0fcf0efa953cb0a16d29b41f7
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:48:57 +0000

    voice fond refer special earth

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
