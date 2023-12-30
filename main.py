a831cdb5d0a7207f16f711714d1e135c301b5b05
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:12:20 +0000

    sight bath happy at slip

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
