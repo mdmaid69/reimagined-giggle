259c8f6765e737bdbe8f5d92206a89aef841b99f
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:47:45 +0000

    question dead know cross jump

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def sort_list(lst):
-        return sorted(lst)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
