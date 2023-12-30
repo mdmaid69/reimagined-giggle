ee57b13be0a7d8fbf658644b48a3096af874d08d
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:15:34 +0000

    try thin at workon nice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
