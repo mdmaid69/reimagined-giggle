0364a0e087961a3054d4894c90849e1a0f138fc6
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:50:50 +0000

    buy afford approval release attitude

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
