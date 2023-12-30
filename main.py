b90d86fdf052103cb09cccba8cda0c2c5eee1d9e
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:36:56 +0000

    bridge cruelty neither by swallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def convert_to_octal(n):
+        return oct(n)
