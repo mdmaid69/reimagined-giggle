640bd90588dfdfcd6642f547c193b25d383e48d5
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:47:08 +0000

    thinkover polite uniform post start

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-print([x**2 for x in range(10)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import platform
+def get_os_info():
+        return platform.uname()
