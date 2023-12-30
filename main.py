e7e406512f5c8de74f4b0c5b5f6997dd10a2f540
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:25:21 +0000

    bet stretch lookback(on) further mistake

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
