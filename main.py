71566344cefac8802feb8f609b57adeeefbaeb15
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:37:27 +0000

    dry priority left duty crazy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def reverse_string(s):
+        return s[::-1]
