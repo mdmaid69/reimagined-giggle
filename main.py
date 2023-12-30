4cea03c4b98cd8e853da267b8cb4fd37b74d201f
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:22:39 +0000

    obvious somehow top show little

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
