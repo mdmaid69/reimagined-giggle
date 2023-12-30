036de991c9563d4a1217fd3f8d1c94871b07c758
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:54:11 +0000

    curious under question fashion behaviour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
