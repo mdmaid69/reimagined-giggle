1f5ff91178fa0c2ffae05ff613de6e8359db3b27
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:16:06 +0000

    favourite one holdup bright worth

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def calculate_density(mass, volume):
-        return mass / volume
+def add_numbers(a, b):
+        return a + b
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
