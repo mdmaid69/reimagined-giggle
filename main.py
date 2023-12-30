6e4aeedf5b087b40c56127764a3390f24c436b19
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:04:02 +0000

    check file wide condition anywayanyhow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
