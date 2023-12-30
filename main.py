ec1c42770d7eaadd16e9768e5aeecfdbdcae41fb
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:32:39 +0000

    order quickly create 566end decide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
