6fa79e014ede5fd2a9cce03f7649e9d8e4aec12d
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:00:34 +0000

    suck nigga explain 573entertainment reply

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def is_even(n):
+        return n % 2 == 0
