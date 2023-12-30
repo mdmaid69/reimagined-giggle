a706a4f4f541918ff1ec5d4e2bd654d62db93fb9
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:17:59 +0000

    Rr eye happy fall plenty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
