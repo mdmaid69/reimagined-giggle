1f9e67eca7c18ea1cb31469a99746a73fd47a316
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:58:45 +0000

    argue middle protest patient lookafter

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_min(numbers):
+        return min(numbers)
+print(sum(range(10)))
