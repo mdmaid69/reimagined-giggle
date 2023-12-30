84695228e558fcd24b56863c0c2f5749755e0493
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:56:25 +0000

    tear debt sad occasion meanwhile

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
