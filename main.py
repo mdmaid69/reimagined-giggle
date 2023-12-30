8114f102ff683d7ef582e21225ff08f8d7ea4ef1
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:48:46 +0000

    bite distance though late government

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_average(lst):
+        return sum(lst) / len(lst)
