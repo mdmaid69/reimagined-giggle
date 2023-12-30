31053d9b67b9a193082a8fa0a7d9f813f3d47e3c
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:08:43 +0000

    voice comein comfort relief emotion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_repr(array):
+        return repr(array)
