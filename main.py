839c173cee52a3e6b933bf0fd5ea7ee87be7efca
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:15:03 +0000

    advise late search fire wealth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
