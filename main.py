f25e631dd5cfb2601ab5d6d7f599482e95451813
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:02:04 +0000

    favour please rich obtain speak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
