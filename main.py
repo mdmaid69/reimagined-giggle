6725d7d52e9741ced7997684e2b6b20b684b970a
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:29:37 +0000

    allow squeeze turn provide dark

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
