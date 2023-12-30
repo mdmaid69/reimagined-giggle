4199bdd0e8efdedcacbc7a1c27858fe442bc620b
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:45:04 +0000

    excuse situation they along brush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-print(math.pi)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
