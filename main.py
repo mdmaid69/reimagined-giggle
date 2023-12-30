f9a28c366a3a0d5b330b4a3ce158c562344aa974
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:18:42 +0000

    stare command loss jump for

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
