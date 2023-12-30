b22d8f08714e3286cde10cc2f844d194be1d7c37
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:15:52 +0000

    ability 568energy hit convince the

--- main.py
+++ main.py
@@ -2,5 +2,6 @@
   def plot_scatter_graph(x, y):
         plt.scatter(x, y)
         plt.show()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
