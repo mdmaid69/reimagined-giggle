5cc942998d401f2894ac578c51b8fbc61f0c04ed
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:26:18 +0000

    bound formal hide mind interest

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
