6311334b69caf87948471823f4ad483ac02f659a
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:52:50 +0000

    limit petrol greeting urgent more

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
