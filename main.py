45d12e61f6d66ae15306b9601b8eb55ef850aedb
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:59:13 +0000

    grand matter somewhere patient achievement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
