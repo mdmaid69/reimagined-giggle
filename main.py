9ae0f563ff6714e935182c49d9b357e7ed3991ce
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:33:34 +0000

    call home start Uu sound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
