c0598242cfe83df9b07757bc02f2231411a74b62
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:27:15 +0000

    572enter Pp live trap corner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
