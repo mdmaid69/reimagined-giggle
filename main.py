136aedff8020da5ac67310c7e402aabd9b0199fe
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:20:25 +0000

    its such drop chase phone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
