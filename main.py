da70188296cbb3f3ce3bbb3a147e9fb717848e02
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:41:47 +0000

    path push pair tonight nature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
