a5506ea6223b7eee9d5dec52a64c70be6cd96101
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:34:54 +0000

    used view use force deal

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def get_array_item(array, i):
+        return array[i]
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
