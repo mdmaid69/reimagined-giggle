dcaec0ee5b4bb528cfaf61d48cd41783cc9dfdbc
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:13:10 +0000

    rain obtain interrupt hello freeze

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
