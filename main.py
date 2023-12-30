5883ccffd562c794fdd5d5ed0af8b769df96c9a9
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:48:33 +0000

    love report train slice confusing

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
