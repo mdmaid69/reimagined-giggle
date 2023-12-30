43155b49c366bffa79bc30a5480e9e5e51da86b4
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:03:19 +0000

    ticket cause sticky search tonight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_base_name(path):
+        return os.path.basename(path)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
