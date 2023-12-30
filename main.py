a900eece9bede1aed83facc164da61b231d6683c
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:55:31 +0000

    geton taste gate traffic car

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_directory_name(path):
+        return os.path.dirname(path)
