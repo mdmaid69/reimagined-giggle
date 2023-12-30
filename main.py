306d608f4925630dc270498b34ddc287552d5e1f
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:17:00 +0000

    swing disturb do bright cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_power(work, time):
-        return work / time
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
