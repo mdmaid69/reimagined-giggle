48eedb36e8473bd4ad8d0d97e58117b099d236e2
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:53:43 +0000

    wire off impressive development world

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
