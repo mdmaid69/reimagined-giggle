2abd68ea67213ddc2ead8ff7159e1c53c0950f6b
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:07:11 +0000

    pump cope date fold beneath

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def is_even(n):
+        return n % 2 == 0
