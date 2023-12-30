41b1ce4ce451e9223c4e990518db0b68eef75c9e
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:11:57 +0000

    refer often clean trade now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def count_words(sentence):
+        return len(sentence.split())
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
