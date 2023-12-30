c7411adacd372adca7a2cc5b715285243afb4389
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:11:23 +0000

    interview unfortunate OKorokay favour too

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
