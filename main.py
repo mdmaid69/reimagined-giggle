896585b167dc5b429955a2a47d1aca8de29c51c5
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:48:40 +0000

    bet uponon answer short ground

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
