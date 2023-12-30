7118aa4e4a06ce932e47c39fb4ac66a9087a797b
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:58:34 +0000

    century little sting game peaceful

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
