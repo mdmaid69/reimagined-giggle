f7b80ceb6448a0509d852d6741cb1888c15f8584
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:45:43 +0000

    work chief sticky trick get

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def remove_duplicates(lst):
+        return list(set(lst))
+def find_unique_words(sentence):
+        return set(sentence.split())
