9005892f669af2068d8a727f15a6717e91881156
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:20:05 +0000

    Mm pause shoot read loss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
