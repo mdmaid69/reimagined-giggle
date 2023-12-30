527678e1c7978f99b26ed0384256a20c03aadff0
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:25:44 +0000

    smart same sort different satisfactory

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
