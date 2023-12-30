993ca5fb734e72047542037f12868cfd0bebedc4
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:46:57 +0000

    unless usually Gg mind fact

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
 import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def convert_to_binary(n):
-        return bin(n)
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
