17ef39a4fa854f3e692621195f6901c19dc6ec85
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:33:42 +0000

    yet Nn announce once confident

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
