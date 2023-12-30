e8fc5694e06395957cd4951f0f34fa60e7f29e22
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:55:12 +0000

    previous boat drop determined brother

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def divide_numbers(x, y):
-        return x / y
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
