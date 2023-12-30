28c2d6bd3d4d472a56871a8f23ae6462f0490e1c
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:35:33 +0000

    cut getoff apologizeapologise obvious sometimes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-x = 10
-y = 20
-print("Sum:", x + y)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_volume(length, width, height):
+        return length * width * height
