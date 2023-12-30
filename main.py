96a8fcb104a36d4a6a1a1d719f273712d1918371
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:10:54 +0000

    quiet what squash when instant

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def get_current_working_directory():
-        return os.getcwd()
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
