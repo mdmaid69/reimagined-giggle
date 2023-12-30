471018e4c0c49773e1df86af5a3e582529a15b5f
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:21:43 +0000

    actual believe development until butter

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
