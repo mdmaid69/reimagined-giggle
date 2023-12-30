838ed5b267e0c81c2f77e286b3686fefefbbf250
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:49:34 +0000

    protection hers obvious have in

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
