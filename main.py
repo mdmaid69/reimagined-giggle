2af858b14915df751521f8d715b596ff42ff7072
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:15:21 +0000

    sport community smoke persuade rescue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def square_number(x):
+        return x**2
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def convert_array_to_unicode(array):
+        return array.tounicode()
