6f924bdbbfeae8b29f9fd9e5026dd61ebbb8d6b2
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:46:40 +0000

    yours contrast refuse properly takeoff

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-def get_current_time():
-        return time.time()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
