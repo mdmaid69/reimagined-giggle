a0010105064c9b375d9781b8f36af071720c9061
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:52:55 +0000

    kind straight flat lay date

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
