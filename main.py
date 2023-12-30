06f3b399c0c2f2abe72e7247459c08942e1b3a14
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:42:35 +0000

    ashamed business each free ready

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-def get_time_since_epoch():
-        return time.time()
+def find_max(lst):
+        return max(lst)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
