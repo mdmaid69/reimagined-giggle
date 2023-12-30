9ac4686558daab1345f3bd2faf35684b724a25f8
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:05:36 +0000

    movement start time fire used

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def get_array_as_set(array):
-        return set(array)
+print(math.pi)
