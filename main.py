1d88ce47177a26f9ad222d7bfb1c9184e0c86ae9
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:40:02 +0000

    trick shade mix eat scheme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
