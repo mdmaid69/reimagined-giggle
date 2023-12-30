a56e6baeb98e70bc47442faa00edaf362ba2b783
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:17:43 +0000

    air close influence right reply

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
