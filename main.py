4c21d38f1a6e47fd690bd7ae67c21767946741e1
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:49:28 +0000

    welcome use authority tooth runout

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
