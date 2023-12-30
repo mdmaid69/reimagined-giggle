42e63ef9aeca957ec80aa49e7b23ed35190e36f5
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:20:33 +0000

    whole test road there than

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
