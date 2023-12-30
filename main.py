c651aaa2c38c1d5807c6e494806abedbb50475a9
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:22:27 +0000

    freeze please remove underneath abroad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
