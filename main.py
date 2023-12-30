1c03aceceb17cf667597ccdee7dcb107b1564dc7
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:51:53 +0000

    light book adjective win show

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
