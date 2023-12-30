213335519f3e6c9b71b435a6dfac112efcf64949
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:08:59 +0000

    doubt trick forgive circle comfort

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def convert_to_octal(n):
+        return oct(n)
