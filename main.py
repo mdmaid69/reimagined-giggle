31412e1a0328320608833505afd1922e6c781c9a
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:22:34 +0000

    accidental many nerve exit progress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
