18263b865c09a006046abe47fb150f6165434e92
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:32:43 +0000

    young benefit height square mark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
