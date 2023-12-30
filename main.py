44780fe1368d5876558edc9a03c5b79fd6b1eb9b
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:04:25 +0000

    aware press thumb the total

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
