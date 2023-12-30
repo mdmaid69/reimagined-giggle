7f2c7776346ff6ef23eb82f818a14b1ee05d869e
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:49:44 +0000

    move cross Ee walk afraid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-for i in range(5):
-        print(i)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
