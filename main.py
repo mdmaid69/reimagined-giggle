e289ba5f6606da32e4a18d161d888a30c7604820
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:16:57 +0000

    still talk unless right guess

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
