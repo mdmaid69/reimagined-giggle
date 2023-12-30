a44904d295b33f2dd3384c847207cb679bfbe96c
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:44:55 +0000

    Oo dress smoke my then

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  def cube_number(x):
-        return x**3
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
