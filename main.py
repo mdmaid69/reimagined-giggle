ddff1da1d4f6b7e32d09a135ac24747ff9f5f141
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:19:50 +0000

    noone chief fortunate convincing fast

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import os
-print(os.getcwd())
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
