b2669afab11ca15d0eb1905b9716a834af8f78fc
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:15:49 +0000

    himself law task desperate capital

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_array_as_str(array):
-        return str(array)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
