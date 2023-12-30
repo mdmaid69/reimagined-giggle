a0ce2fc1a47f344df71c5221b73fee196ce87de7
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:14:45 +0000

    star bend crash skirt borrow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+for i in range(10): print(i)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
