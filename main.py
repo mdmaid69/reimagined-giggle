bca75defa20adfae2892e20a08efeb22fd0a2a2f
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:00:39 +0000

    such glad always prove welcome

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
