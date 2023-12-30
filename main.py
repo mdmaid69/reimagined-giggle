b9edf2c6671eb51e006e8bcaf57bd5e5f7dc38d8
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:01:50 +0000

    total front twist embarrassed night

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
