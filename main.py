c8c79952a83bb8d32c593c0b23bf4c09ab4344fc
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:54:26 +0000

    hello bear afternoon into other

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
