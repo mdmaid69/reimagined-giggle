a102f519ddbab097c2246d23056faa1e032ce829
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:08:59 +0000

    curious bus thinkover promise married

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
