4d72fbb09938271d6b311c488dfb8d1ef1d919a1
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:12:46 +0000

    front platform slip block prepare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
