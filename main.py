d929cc609b9675597e7e68bf2e4979d8a3d7f6f8
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:50:42 +0000

    unfair avoid send bringback speed

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import collections
+def create_user_string():
+        return collections.UserString()
