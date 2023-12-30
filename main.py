c7eb95f7199e89cba99fcbd30c5b99f0f08f3ed8
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:16:50 +0000

    attempt ride brother if perfect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
