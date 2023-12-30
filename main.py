fe7e17ba8df1c4631f7957885714cf915045771d
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:03:20 +0000

    unfair tragedy generous about personal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
