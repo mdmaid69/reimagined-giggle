b9c7fe20d72c1287d6f49936ef89ba510c0d8fa7
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:36:20 +0000

    wind language abroad collection divide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
