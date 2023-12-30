0c7c20c8c7eb4de129f6d2b21bf2b1ca0d3a7e05
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:07:40 +0000

    dick actually relationship contents intelligent

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def cube_number(x):
-        return x**3
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def is_even(n):
+        return n % 2 == 0
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
