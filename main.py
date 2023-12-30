d3f26645ff2b2911d214c65243a1381bd65f0828
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:30:40 +0000

    fond hardly degree brief hope

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def count_elements(lst):
-        return len(lst)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
