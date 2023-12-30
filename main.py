9777f936f263251d516fd6b5be6696e09b796377
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:55:39 +0000

    gas may angry hospital dance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
