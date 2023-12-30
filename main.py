e32e9c8b8c534030feb0fd80b46c729555f8ff78
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:13:40 +0000

    street report accident far our

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
