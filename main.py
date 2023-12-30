535cc7dbd891fd4bedcb4e93c72f61f477b79d73
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:35:54 +0000

    world anywayanyhow confidence comeoff land

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import numpy as np
-print(np.array([1, 2, 3]))
+name = "Python"
+print("Hello,", name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
