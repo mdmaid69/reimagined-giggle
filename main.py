26c1c7e5e8c680f20d5a878e0fb1e61e607e047c
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:41:53 +0000

    worried even war before choice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
