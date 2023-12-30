885cb54d0de9b0de51456245e583281e42f14183
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:55:53 +0000

    better sad brush count reduction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-def find_min(lst):
-        return min(lst)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
