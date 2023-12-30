e6e6cc79a6d67b7cf2a304ad48b1ae42653c59fa
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:00:24 +0000

    speech model shape difficulty Nn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
