ccd3116c8fe6507f17eb34402303a9f152f6b9f6
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:13:00 +0000

    study blow free getoff nerve

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
