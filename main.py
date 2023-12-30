34871bf624f636b109bc94507a0ded022daeee9b
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:16:30 +0000

    weak criticism thin surprising size

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
