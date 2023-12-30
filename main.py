20271c3c628bfa3bb5723c223691203382e007a7
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:56:01 +0000

    needle tune cough should once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
