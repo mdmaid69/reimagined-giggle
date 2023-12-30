e45b5dc3d50ee6b590b22a39cb39c0d481938cf1
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:19:13 +0000

    simple unhappy survive punish come

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def square_number(x):
+        return x**2
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
