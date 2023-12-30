ef128edf53e7215f6ff0ab661c03f0d65bb681c2
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:43:31 +0000

    exactly coat carry rush fit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def add_numbers(x, y):
+        return x + y
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
