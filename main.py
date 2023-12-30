ae287dab26ab84f67dbf60dd522f7508b0a2d0bf
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:55:10 +0000

    insist ask name genuine appearance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
