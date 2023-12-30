06af1e820b8f78ccd6f7430c482490bb59ae1d33
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:22:45 +0000

    turnon leave tool frequent show

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def calculate_power(work, time):
+        return work / time
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
