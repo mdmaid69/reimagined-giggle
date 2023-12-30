77cf3b1cd13888c196d1f9ba411cce8659612cec
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:21:43 +0000

    giveup never run accident yet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def convert_array_to_list(array):
+        return array.tolist()
