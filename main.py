49252dbe39e7b9d49da16057bfdadf526cd69530
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:37:51 +0000

    worried ring lucky responsible present

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
