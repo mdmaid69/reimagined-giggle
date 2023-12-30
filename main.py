e9424f40e9126845c5368854ea8c9ea4b5ac18e7
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:41:20 +0000

    cry move near nigga century

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
