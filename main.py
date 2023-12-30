3dcf35c6b450a9cd7d5861d6bfd817b553320370
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:16:15 +0000

    water guess if platform attract

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
