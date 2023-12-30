ba5ec790bf25d81df4ed2d6567036df1b32bd780
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:09:49 +0000

    store bad keepaway tour blame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
