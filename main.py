85a2be2a730564f2af8c0e99c3a3c73ad3284c0d
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:38:42 +0000

    shallow connect only uncomfortable length

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def square_number(x):
-        return x**2
+def calculate_density(mass, volume):
+        return mass / volume
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
