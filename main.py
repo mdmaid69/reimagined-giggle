c057bc97c992994499190bd8d1fdbe4b108e7ccc
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:10:02 +0000

    fire condition exam record drop

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
