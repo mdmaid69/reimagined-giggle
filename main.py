d58b8764d378065955abe79c51d3d06ca19e2dbc
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:25:37 +0000

    cheek phone 568energy too official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def get_array_as_float(array):
+        return float(array[0])
