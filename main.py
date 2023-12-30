cfab806d64e5afef06cc7b2ee835e89240377224
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:54:21 +0000

    shock movement estimate innocent bringback

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def get_array_item(array, i):
+        return array[i]
