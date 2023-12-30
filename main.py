bf1cfafb113c6b4f51020d3283eb8e040a876dab
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:11:57 +0000

    criticism clear here turnout unkind

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
