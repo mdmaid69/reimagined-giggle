df8b855874c6ef0da3a11362b666a8d93a011bf6
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:23:19 +0000

    effect home patient welcome bang

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def add_numbers(a, b):
-        return a + b
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
