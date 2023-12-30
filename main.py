640a84410d4e13140b50d3941e5dd46f53daf098
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:26:24 +0000

    our opponent warning cover walk

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
