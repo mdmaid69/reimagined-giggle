0166cf2dec803f0251ae5f55c42a6382e721f195
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:17:09 +0000

    your rarely female bright curious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
