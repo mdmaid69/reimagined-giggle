5c379c7cfc2c801887474beb3289496ca37c403f
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:46:26 +0000

    himself argue giveaway kind cause

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def find_min(numbers):
-        return min(numbers)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
