07c0d2fdcd07e14bfcd7cb7d0ca603073ac0e3cd
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:01:31 +0000

    who out yours other row

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
