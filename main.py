0074c406d92b4406af579978e782647c91decea9
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:49:51 +0000

    callback hope pinch joke first

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_base_name(path):
+        return os.path.basename(path)
