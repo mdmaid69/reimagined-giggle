14904bcf0895926f1c5c16ebccb90852ccda2afe
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:23:12 +0000

    fire world expensive obvious repeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
