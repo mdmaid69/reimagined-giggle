cb836e0a5356cc7c0c810631eb2eb83cf6c4872b
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:40:22 +0000

    something passage club run identify

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
