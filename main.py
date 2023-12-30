fe662bb7356cbe2f867dcbe5939e44d64a604900
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:48:07 +0000

    share basis cap frightened anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
