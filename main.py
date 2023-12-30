8d86ede9743c995a853321f5834be824b842735a
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:27:54 +0000

    choose getback past underneath lookback(on)

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
