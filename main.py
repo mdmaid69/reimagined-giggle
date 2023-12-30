703ae3bd7cf4c24cf8f46d4f80438a855160611b
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:09:04 +0000

    fence intelligent control pronounce cruelty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
