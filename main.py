39f4676c06bfcbba64b77dc8254f9632ad872a2c
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:48:29 +0000

    total argument ago side star

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
