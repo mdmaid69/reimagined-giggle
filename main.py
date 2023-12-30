39955e397857d6eb317983c314bcea0ad7b29de1
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:07:58 +0000

    fact later surface favourite wise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def square_number(x):
-        return x**2
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def get_array_as_float(array):
+        return float(array[0])
