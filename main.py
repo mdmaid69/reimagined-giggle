b7b7ea46aac5da3ccde29ce4f0845866119a7640
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:42:47 +0000

    law laugh slight dream divide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def find_min(lst):
+        return min(lst)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
