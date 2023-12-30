1ac389c4e7c9af8a58c32c561daa0e7368bfb607
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:32:26 +0000

    cum tooth accidentally try become

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
 def calculate_mortgage(principal, rate, time):
         return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
