6282a4543748c2d5089830d50a15b8bbc0abf8e7
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:50:56 +0000

    arrangement argue tooth rest reduce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_number_of_links(file_name):
+  def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
