03b3424c5ff02aa862bdef28e094188adedce6c9
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:13:09 +0000

    wash where average best thumb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
