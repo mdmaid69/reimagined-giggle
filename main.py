a578d0cedd73546b795a48b633bb8cd2461925e8
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:54:35 +0000

    safe high careless cool lead

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def add_numbers(x, y):
-        return x + y
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
