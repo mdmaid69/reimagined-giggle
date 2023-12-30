62bdc9364c1e724bf98cf8b6290524b18819a616
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:56:40 +0000

    deliberate across play minimum case

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+print("Hello, world!")
