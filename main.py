f08696ff1d2b20832c13fdfa6036fb76fa4ea90c
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:51:44 +0000

    stick notice world dig sea

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import array
+def reverse_array(array):
+        array.reverse()
