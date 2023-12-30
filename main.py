f1b3228765677e1a07911edfaebec8b10a6bfe51
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:02:37 +0000

    cross region face callback medium

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-  def sort_list(lst):
-        return sorted(lst)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
