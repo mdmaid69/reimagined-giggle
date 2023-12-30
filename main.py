312c657a0aa85302df58527a26a3abb1b56ec54f
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:01:26 +0000

    runout decision lookback(on) used bad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
