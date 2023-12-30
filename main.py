f1d402a566a3a2c36f3a60936d2a39397a1c2014
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:55:07 +0000

    draw everywhere debt appeal him

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import math
-print(math.pi)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
