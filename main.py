32f691f8eb7c74789ffcabd0471accb136bb2ee3
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:38:59 +0000

    leave cut round Yy already

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
