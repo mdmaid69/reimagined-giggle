e4ee30948fd3b135991ea795ad55b8209c3d20d3
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:09:01 +0000

    badly deaf scare quickly model

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import sys
+def exit_program():
+        sys.exit()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
