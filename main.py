ac9c2f5646f4b8284578fe155c489cc6c307967c
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:58:46 +0000

    furniture protect must gas art

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
