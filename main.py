faee63e4f5d4ecf416edbc3c41d48868943b0cd1
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:10:06 +0000

    fill stay confident sign keepoff

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def convert_to_hex(n):
-        return hex(n)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
