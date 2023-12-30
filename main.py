8ea2970c013697a8e3f9531c983339a706263e80
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:54:42 +0000

    silent honest foreign correct mention

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_acceleration(speed, time):
-        return speed / time
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def set_array_item(array, i, item):
+        array[i] = item
