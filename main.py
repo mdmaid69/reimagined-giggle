6cad6960166d2da825e76ed170e17c354394a1bd
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:30:06 +0000

    answer live downstairs visit let

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
