d6c452cff98ba305d90072fbab20169174332a09
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:04:13 +0000

    spoil real find yes authority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def is_even(n):
+        return n % 2 == 0
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
