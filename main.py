c9a3a384e7518df041f4949dee9b7f9fe487dcee
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:58:01 +0000

    explanation care risk sister road

--- main.py
+++ main.py
@@ -2,6 +2,5 @@ import json
 def load_json(filename):
         with open(filename, "r") as f:
         return json.load(f)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import os
+print(os.getcwd())
