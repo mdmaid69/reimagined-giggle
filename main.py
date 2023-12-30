e52a570c0e152fa439aaaf842ac685a72e369214
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:01:53 +0000

    gradually clear here broad medium

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
