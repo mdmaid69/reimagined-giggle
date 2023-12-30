be1f0fdfb0dd5593dfd8cfa0a0dfe2d4ea501938
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:45:17 +0000

    could float burst bend correct

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
