4a3382cc9e62ef24018e692b859a1ce6128117d5
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:38:38 +0000

    also hollow slight type imagine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def delete_file(file_name):
+        os.remove(file_name)
