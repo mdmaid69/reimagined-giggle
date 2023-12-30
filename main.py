ca71956d360623eef62e4461cd807ef0d70f2a71
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:18:15 +0000

    shine fierce tooth yesterday rather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
