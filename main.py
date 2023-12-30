052133bb02f09ec8d053b7cdbf26fe637693ae6b
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:18:03 +0000

    necessary same plenty they basic

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def add_numbers(a, b):
-        return a + b
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
