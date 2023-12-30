1af24d9258afee474a93d0b8e9f11eac4cbfcdcb
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:55:23 +0000

    roof issue whisper border round

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
