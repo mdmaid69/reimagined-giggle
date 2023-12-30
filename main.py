4526f53dd08d19be8e1bd678302289121217b0ba
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:37:06 +0000

    exercise closely score dark party

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
