2289ea43c809106c89c422a3df16e644cd2741ba
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:14:12 +0000

    claim muscle separate major borrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
