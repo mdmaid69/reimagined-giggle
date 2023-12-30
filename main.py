0428533548deb23be3caff6e7dcd919dfc5dc6d5
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:30:25 +0000

    either broken top yes positive

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
