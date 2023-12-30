39df4e4f8c8c80adf6ad90e0a3ac8d45db98b986
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:53:58 +0000

    play kind heavy pause watch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
