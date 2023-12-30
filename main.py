0ea5505d895d86689f403e7ac4c9fd497584b4cf
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:38:03 +0000

    declare to argument actually lucky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
