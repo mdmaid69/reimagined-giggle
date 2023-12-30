f8dc841dc20ae550272efb4ca8cc9e736177f0a8
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:22:46 +0000

    beat comfortable cover reply charge

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+print(math.pi)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
