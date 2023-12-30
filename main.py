8782063c9688cce0ff7356ba316a5c7ccedb581f
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:58:59 +0000

    bear market somebody runinto progress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import itertools
 def flatten(iterable):
         return list(itertools.chain.from_iterable(iterable))
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
