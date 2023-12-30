8490bcd0c2f275764ce1e7e093c42e85b870b921
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:42:37 +0000

    this keep them low closely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import datetime
+print(datetime.datetime.now())
