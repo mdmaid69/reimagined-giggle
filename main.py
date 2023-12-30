8407d9be71948e4b7c59f020cb360d66e751b255
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:53:24 +0000

    inquire rest negative comeround sister

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
