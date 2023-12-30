382d0e59d8993be5a857b1cc6a8add18316c62ca
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:38:09 +0000

    final car each relationship floor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
   import os
-  def get_file_gid(file_name):
+  def get_file_group(file_name):
         return os.stat(file_name).st_gid
+def sort_numbers(numbers):
+        return sorted(numbers)
