4a08148741374973f4114ea1b4255f3f46a6d063
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:29:37 +0000

    limit about sail point pick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
