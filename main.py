ef7e929d9282ba6463dd2be7f225c510d0e2791d
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:57:26 +0000

    can worried memory murder means

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
