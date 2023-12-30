5898b55526941abfa05bdeacf6f2e30efdc3d408
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:43:59 +0000

    space either cruelty ill visit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def reverse_list(lst):
-        return lst[::-1]
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
