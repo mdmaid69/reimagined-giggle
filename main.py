ab064e51361a3364f6fc3f08f6b69c78678e82d9
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:16:49 +0000

    dull before curious rough check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
