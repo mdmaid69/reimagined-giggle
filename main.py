369cb158a754c1cf995ede22a0d91695e56aa802
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:23:54 +0000

    sound search actual everybody once

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
