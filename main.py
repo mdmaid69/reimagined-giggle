7e048ac2d6445dd7b8f1061d596aa26d14101751
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:14:09 +0000

    behind badly unkind win flavour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
