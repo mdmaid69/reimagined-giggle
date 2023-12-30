67f3d51d587f702f3d41ba3be883d9c32587ea8c
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:27:04 +0000

    sad shadow gooff this Mm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def get_array_length(array):
+        return len(array)
