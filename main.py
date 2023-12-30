daea217c9aac2c7c6b15d9198ce39a1837d022f4
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:08:33 +0000

    date foreign division would maintain

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
