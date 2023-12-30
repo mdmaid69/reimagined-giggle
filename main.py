8c92abb8778adf1f362044bac0d3c9cdf325bc34
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:37:51 +0000

    worst beyond real correction friendly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
