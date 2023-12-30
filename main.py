6017bbd123c073ce5991ff1466bd45fb1338bf32
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:18:31 +0000

    go angry outside develop father

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
