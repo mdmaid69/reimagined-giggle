79808bc7c8c0d709e25e3e94dd66771ac3548a40
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:07:48 +0000

    little onto hardly getout ours

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
