4db9279b6ec835121ad51bae98a7cc9b4ee5ce99
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:06:54 +0000

    stone crisis react mind bringup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
