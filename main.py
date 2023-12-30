4f77fca31ee92ee4ffc2925f4484dd937e86ac9f
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:46:47 +0000

    boring murder each something fortunate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def reverse_array(array):
-        array.reverse()
 n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
