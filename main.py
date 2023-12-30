8608bf05115b993c4eb03f731d13718a01e3e8b2
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:20:13 +0000

    describe draw rob condition shop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
