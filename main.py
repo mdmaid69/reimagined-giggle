f00756357e0224b7609c8516cba1e16498451d0b
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:52:22 +0000

    finish average major shame set

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_item(array, i):
-        return array[i]
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def subtract_numbers(x, y):
+        return x - y
