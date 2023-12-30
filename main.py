3a5b35abf30e7ddacf01d1e24b9c270190e0f168
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:21:54 +0000

    keepup hospital record Oo fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
