6e7d7bc8fad912d35f8cb2615ddcde93deb0f18c
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:31:04 +0000

    reason pocket colour dream brave

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def remove_duplicates(lst):
-        return list(set(lst))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+i = 0
+while i < 5:
+        print(i)
+        i += 1
