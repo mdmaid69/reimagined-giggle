c3e9ee6501887ac30a8d14201a57619667f092e2
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:07:56 +0000

    authority reliable bottom slow office

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
