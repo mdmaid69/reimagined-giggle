11af6b3a81ede3b49112d866593e080590dd8a91
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:19:13 +0000

    instead bag change now grow

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
