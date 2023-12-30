2d2450db23ffdea6a960c59e95f10a092592255a
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:23:45 +0000

    break fear soft rather pollution

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
