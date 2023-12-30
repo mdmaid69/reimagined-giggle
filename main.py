796f7290bbf90470e131584a8403025af44974e8
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:19:34 +0000

    callup stand flow bar object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import os
+def remove_directory(path):
+        os.rmdir(path)
