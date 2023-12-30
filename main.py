104e2969e7ad24cc2261ce5798e5bbb34e27f4ca
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:24:19 +0000

    getup respect birthday badly substance

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-text = "Hello, world!"
-print("Characters:", len(text))
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
