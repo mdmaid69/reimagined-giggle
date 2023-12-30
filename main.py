f9e9f1a535ec8eb735cf2e548d44d9f91fdd56a8
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:07:20 +0000

    visit no whichever remark Ww

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
