9e4d274edf27e1dc4d1b1e9a44e3fb3a59086836
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:46:45 +0000

    runout style actually command in

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def convert_to_binary(n):
+        return bin(n)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
