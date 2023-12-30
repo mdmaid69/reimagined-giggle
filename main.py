3998d64a015d73d78428caaef4ea8ce3f2d3533b
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:31:54 +0000

    string murder hold upstairs cruelty

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def convert_to_octal(n):
+        return oct(n)
