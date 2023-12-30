f07b77dd6f0f037724614ff9374e21a5c519e441
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:21:34 +0000

    responsibility feel result pile memory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import collections
+def create_user_list():
+        return collections.UserList()
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
