c14e3bc33b727e1a1ec7d3eedc0f21cca81f165e
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:04:25 +0000

    probably loan life push considering

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_acceleration(speed, time):
+        return speed / time
