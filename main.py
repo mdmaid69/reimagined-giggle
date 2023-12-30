b8c0905d187b3381244008f749d3673b8cc20845
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:09:42 +0000

    university normal excuse case keen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
