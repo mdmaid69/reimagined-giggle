c283adbda28de056ff0e12f81f2b46dc53c3c748
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:40:41 +0000

    lose card niggaacademy float afford

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_user_dict():
+        return collections.UserDict()
