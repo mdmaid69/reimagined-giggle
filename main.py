bf3b79075b4cca79ac67a206f52e4e61064e48db
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:56:11 +0000

    something peace health again culture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def sort_numbers(numbers):
+        return sorted(numbers)
