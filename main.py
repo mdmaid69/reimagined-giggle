2b3082ec492e5bf939a931679beb656866279ce6
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:54:51 +0000

    different centre attitude beautiful late

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import datetime
-print(datetime.datetime.now())
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
