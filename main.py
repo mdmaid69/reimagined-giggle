5a82aa85830ac6d59fb242cb80f4f21961ccbf9d
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:13:01 +0000

    original so month mad skill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
