efe7edaece7ec471dc3ac39720fedca2aa192355
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:21:08 +0000

    battery comeround fun music rain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
