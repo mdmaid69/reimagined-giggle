39176986f22c02b959e0f82aaf300dced2e15e89
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:10:24 +0000

    press accidentally 571enough impossible object

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_work(force, distance):
+        return force * distance
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
