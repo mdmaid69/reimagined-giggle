a4dab0ecf3624f6e3f9cfe4296d437c4839a7485
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:05:20 +0000

    field punishment stupid anxious sometimes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import math
+print(math.pi)
