3f869223ac79f394bc330bdb401b95b11c89dc5e
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:06:50 +0000

    secret kind goodbye criticize convince

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import collections
-def create_user_string():
-        return collections.UserString()
+def get_array_as_repr(array):
+        return repr(array)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
