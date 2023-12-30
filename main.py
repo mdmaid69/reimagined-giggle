1b616fd3678412e8dfa94af37bc9cd4893dd1e8a
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:58:22 +0000

    smell computer pile swimming avoid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
