31ccc806df36e1d6fa201ee7592ea7cd6bbca913
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:40:50 +0000

    her leg card approve bottom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
