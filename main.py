97331281563212397b996eac76dca9dc2ab04e8e
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:12:10 +0000

    impressive final ask although lost

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
