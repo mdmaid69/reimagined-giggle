eb13bc42ed551c958c1cc1e6e3c3888986c31f23
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:07:21 +0000

    stand influence object doubt turn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
