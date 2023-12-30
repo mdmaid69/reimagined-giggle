bf04519c075e5e6ebb6b1c25bf72b1d46e244424
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:05:25 +0000

    behaviour public agreement breakinto among

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+text = "Hello, world!"
+print("Characters:", len(text))
+def calculate_power(work, time):
+        return work / time
