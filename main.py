3de4cfa65443b7451c03b276173d28d7f5d1343c
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:22:56 +0000

    achievement settle Ll basis gentle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
