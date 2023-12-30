4c6f4903401cf54d61165916fdda47f49ed90708
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:19:51 +0000

    expression black feature spite sometimes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def count_characters(sentence):
+        return len(sentence)
