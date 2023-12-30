3aec4c17115949c1f3eea2c517184f09afcd2bb1
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:16:11 +0000

    argument protest low tooth about

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
