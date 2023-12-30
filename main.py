71e9da1300545b6a848cdc1b478791789a248db1
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:31:24 +0000

    everywhere discover crowded nervous develop

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
