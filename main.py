28b3c98da6000a418ee1debdf27fb2dc39a32e41
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:30:44 +0000

    when then ready scrape shallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_current_directory():
+        return os.getcwd()
