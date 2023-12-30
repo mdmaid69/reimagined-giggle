a73c3d63953f6936f8666b7a8b12c25dfaacc53b
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:35:47 +0000

    complete gate appeal admire black

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import numpy as np
-print(np.array([1, 2, 3]))
+def subtract_numbers(x, y):
+        return x - y
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
