8262a5b6a4ed9890beafd523ac81374ec6b8b18d
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:59:41 +0000

    sudden damp summer stop pause

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def convert_to_binary(n):
+        return bin(n)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
