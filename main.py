2c05d0a3fc5e8fd71c57190e13005896f9e660d7
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:11:27 +0000

    argument prize another meeting hang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
