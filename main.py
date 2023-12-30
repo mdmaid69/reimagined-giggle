9995465ab560fc4b657785a43a84df3019d27fe3
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:35:22 +0000

    attempt confusing mouth another neither

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
