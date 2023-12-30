d903a4854834b6ad9ea477b247b397ff86b1e9da
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:58:26 +0000

    splash vanish miss wonder must

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def find_max(lst):
-        return max(lst)
+def get_array_as_tuple(array):
+        return tuple(array)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
