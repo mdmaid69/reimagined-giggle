b2088308549af7b013784b564e23ffe8858cf937
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:13:13 +0000

    curious indeed trip hole permission

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
