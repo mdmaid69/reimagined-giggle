546231ecf090a6e465174b92b27d5f86ad0ee02e
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:08:48 +0000

    deliberate trial both sing find

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_as_str(array):
+        return str(array)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
