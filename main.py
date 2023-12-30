407165182415ca0c6932862fdf15950cd5942345
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:50:22 +0000

    worst but hurt perfect pretty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def find_max(lst):
+        return max(lst)
