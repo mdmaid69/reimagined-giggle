583b97f23ef279427de303297c5b93d163c13034
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:20:24 +0000

    now which main answer criticize

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+print(sum(range(10)))
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
