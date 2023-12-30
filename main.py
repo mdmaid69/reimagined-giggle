87ed578ed1d4378e372d66585cc9b9ceecd678e3
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:29:53 +0000

    begin organization instrument cook but

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
