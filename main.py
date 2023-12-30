18b8d31f8f16bafad8d63fa5712e3bfb18dac339
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:42:23 +0000

    later subject about best impatient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_uid(file_name):
+  def get_file_owner(file_name):
         return os.stat(file_name).st_uid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
