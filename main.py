2df9c5690996e7ec9da30a7130cb42b0f34aaac3
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:59:53 +0000

    anything lay back nose custom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
