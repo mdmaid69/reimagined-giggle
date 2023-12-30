e16d54a078bea63a9a86e4a67512d8f06d2ac741
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:31:55 +0000

    performance soak direction better independent

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_length(array):
+        return len(array)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
