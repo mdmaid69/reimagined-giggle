9d0764cccf18bb3e5079e46cf0ad822f13b82a26
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:16:12 +0000

    squeeze secret test position long

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-name = "Python"
-print("Hello,", name)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
