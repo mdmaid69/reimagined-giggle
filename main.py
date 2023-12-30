b89a0d5b614fa7d91182944609800f4e0340c564
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:54:03 +0000

    bother introduction drink stamp minimum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
