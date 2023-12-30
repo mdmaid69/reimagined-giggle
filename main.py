d57dc14171246bf19c50ee8c83fd21f6a10f4371
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:19:34 +0000

    bet Ee godown pupil dig

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
