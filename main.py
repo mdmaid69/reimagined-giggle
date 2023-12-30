fc3615c361db94db0aad82caac5239965858e0ee
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:26:04 +0000

    minimum cost 571enough comefrom constantly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
