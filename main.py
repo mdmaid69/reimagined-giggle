a27ebb291af804ad0481b3aaacba16003e55eacd
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:42:44 +0000

    occur Ee exaggerate entry upwards

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
