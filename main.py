41d5467823f3f3b354f2289928a37ebdd4b4cd70
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:36:11 +0000

    strike use would early violence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
