de09f50f5af62f42cf654526c05e35a270842db7
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:21:24 +0000

    owe Dd across bend approval

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import os
+print(os.getcwd())
