8921686b1870e0d72b6d95b4741a032a4d48997a
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:24:58 +0000

    combination see pride major mistake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
