36804a0bb8642e8891c20e13e0bbc333dd3cd935
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:44:38 +0000

    breakthrough sad uponon Rr experiment

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
