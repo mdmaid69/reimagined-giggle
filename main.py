f72a8e15b549076dc3cf6c2455a47ac3cc99db81
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:34:17 +0000

    affect fasten allow surprising room

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import collections
-def create_stack():
-        return collections.deque()
+def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
