75d7acd087aa2d186a5b7520274d21259108b7cd
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:52:23 +0000

    destroy kind burst cut weather

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
