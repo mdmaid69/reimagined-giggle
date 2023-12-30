5acf614410b41a9b1656cc1403cd320e3ba7f60b
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:28:03 +0000

    tear clue food turninto disagree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
