f38f575c8a698caf2af46758f7b40c9e7b713ed1
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:37:12 +0000

    interrupt once past answer pause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_current_working_directory():
+  def get_current_directory():
         return os.getcwd()
