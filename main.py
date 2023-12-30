d01aed03261464b203f3617f614dff6675a01ef8
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:13:15 +0000

    act practical sick for minimum

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
