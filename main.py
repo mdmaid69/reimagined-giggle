3adb17b9b37f40a2ddecde1711341ccd18535c78
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:52:49 +0000

    fork runinto agreement 565end nowhere

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import tensorflow as tf
-print(tf.__version__)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
