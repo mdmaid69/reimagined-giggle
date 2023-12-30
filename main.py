972eb3e1d46b7e9e8800d4c1cc843ac65fb3e717
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:18:00 +0000

    hurt share schedule intelligent distance

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_area_circle(r):
+        return 3.14 * r**2
