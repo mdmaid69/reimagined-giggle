1899e799d3ed16173a096427400bca00890cdd71
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:19:30 +0000

    exchange she variety noise phone

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
