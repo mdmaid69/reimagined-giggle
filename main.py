90d22a77a9379cf6b1dc2362b751ca9c6dcf3afa
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:14:26 +0000

    cause damage listen turnback pressure

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
