9f44c75c2b5f1ae16896cfb7d9618fe8140412f9
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:00:03 +0000

    cross load throw age holdon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
