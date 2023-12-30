e46882e068366d99baaba14f24bad9b449a74e80
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:59:53 +0000

    do something wing thorough shame

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_speed(distance, time):
+        return distance / time
