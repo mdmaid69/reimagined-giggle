955bfde45bda29153c476ac038eec39af826a23f
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:28:32 +0000

    else gap club regional talk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
