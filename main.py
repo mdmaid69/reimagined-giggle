9368d525eef8362b3589d9018ad65b43c65820cd
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:39:12 +0000

    desperate criticize little seem attempt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def count_elements(lst):
+        return len(lst)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
