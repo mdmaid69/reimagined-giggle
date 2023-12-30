b51a9ae699cb012d3cc87eccabfb418d035a29b9
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:13:41 +0000

    range snow right open trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
