10f0947e044f88ee9d86387f9ced83f0f4724b50
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:16:43 +0000

    sail fall hair quarter leg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
