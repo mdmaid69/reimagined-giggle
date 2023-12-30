21a32dfe1f6613822dbc7d465dd5c5719d847de7
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:20:09 +0000

    find stare convenient favourite otherwise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def sort_list(lst):
+        return sorted(lst)
