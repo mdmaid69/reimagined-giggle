95f7d2eb9748bce5015cdf136eb6137aba5b35dd
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:38:09 +0000

    dangerous might comeout safety her

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
