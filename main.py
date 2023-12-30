5923b0c5f8d2669b1af387e1e141b3f4d2eaff87
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:43:35 +0000

    steady sweep natural display try

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def get_array_as_repr(array):
+        return repr(array)
