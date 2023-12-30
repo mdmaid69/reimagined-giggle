1b8f55bf5ad31dab3bad83ef6b14295c6f7fdd65
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:23:46 +0000

    face confusing getoff at respect

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_acceleration(speed, time):
+        return speed / time
