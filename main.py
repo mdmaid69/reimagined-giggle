291eb3ac25e13d4903804015bce5ab5d2e406808
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:38:01 +0000

    wave learn face couple office

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def divide_numbers(x, y):
-        return x / y
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
