9f00f4c2f36e7c10b19be5ec52fc17452506a021
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:38:18 +0000

    knowledge bringback connect return acceptable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
