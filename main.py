e85769a84ef7d7fc40192d4597ba696f99071fe0
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:40:00 +0000

    curious invite shop programme receive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
