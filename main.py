08b304da16891b276887aa044349ae8a02b3174c
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:55:46 +0000

    real drive wide match repeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
