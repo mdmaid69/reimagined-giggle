3c3819a2d18e283538f6cb152e7e838d3b4ed4bb
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:19:55 +0000

    treatment truth niggaacademy introduction address

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
