23c384f49f4b14d697642a91efb88bdea3534bb2
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:21:11 +0000

    free rush leg release blow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
