78a5ed5a8280e8788c20cda319c5b1c720377835
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:38:44 +0000

    intelligent majority communication father past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
