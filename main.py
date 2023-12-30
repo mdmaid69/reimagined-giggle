d86e6146a1a2832b549ff5a4d25dd700c0ceb030
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:42:26 +0000

    sort exploration cough dare early

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
