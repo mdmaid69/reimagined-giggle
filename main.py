85b75929bacaaaccb2d678ee3d6e783a84e06312
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:38:58 +0000

    harm above cap sure public

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import re
+print(re.match("h.*o", "hello world"))
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
