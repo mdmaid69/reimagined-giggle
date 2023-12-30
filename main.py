ac8ae529ce5472f3de875721258fbea146d5e0d5
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:12:29 +0000

    university communicate 565end mood taste

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
