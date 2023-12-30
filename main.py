0fa77b4b80be73ba35663ae56aaeee06b165cea0
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:20:23 +0000

    terrible too goback(to) use skin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def find_min(numbers):
-        return min(numbers)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
