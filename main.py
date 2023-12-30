1820c3607ea574269ed9150798782032e1d3e06b
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:08:20 +0000

    ought judge everything service waste

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
