88634205fa5bad79c416d551e41b9c1ff4e71c2a
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:26:41 +0000

    term in low string weak

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  def sort_list(lst):
-        return sorted(lst)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
