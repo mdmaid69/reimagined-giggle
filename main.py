d293c37b8f11475e3b9464e22f1638b207cc9080
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:02:43 +0000

    572enter star track introduction muscle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
