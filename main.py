eabca4d6ab5aa030e852e994514bf98a2c493c4e
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:54:36 +0000

    past introduction head poor appearance

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def find_union(list1, list2):
+        return set(list1) | set(list2)
