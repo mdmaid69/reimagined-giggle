1b50d2fedb58fbba4a7c9a938f2031ab10de1ab6
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:41:12 +0000

    callback learn knock bringdown neat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
