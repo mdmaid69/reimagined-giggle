ac2d80e711f25af2096929a11c90dd425ba2897d
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:28:51 +0000

    responsibility clearly cheek legal persuade

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
