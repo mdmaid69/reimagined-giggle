6181f30a47fc960befcc20e964d5f3a46a8d4c02
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:28:12 +0000

    goodbye brave except scene exact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import sys
-def print_python_version():
-        print(sys.version)
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
