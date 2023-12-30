b4c31e672b6bf51eef007f0394ae4469773ef95f
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:01:38 +0000

    reasonable sink passage skill sentence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
