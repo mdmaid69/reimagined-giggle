4cfda3965ec0be618d12388686ef1439d4aa055b
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:23:43 +0000

    present foot respect importance money

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
