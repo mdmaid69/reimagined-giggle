1d9e155306e0f4d0eaa2d38e0ce1bc9f894c7e67
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:51:06 +0000

    disturbing yet physical lookback(on) Oo

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
