4e6db538875ff5d55cca503a00e88eba92462195
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:51:54 +0000

    Jj measure hour blind occasion

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
