e90623ab1fc8b0d392e01d1005459fb886616612
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:22:11 +0000

    used whoever involve heavy especially

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_volume(length, width, height):
+        return length * width * height
