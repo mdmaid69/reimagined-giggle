2634d048ad635633da2b278fbab510e791018b2a
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:31:02 +0000

    shop second and search wash

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import array
-def get_array_as_str(array):
-        return str(array)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
