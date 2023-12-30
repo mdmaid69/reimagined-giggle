9c97ddb5788190cc6a6a1a008b2422555a5e3e8e
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:44:11 +0000

    his risk long minimum scream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
