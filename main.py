5e6b01327514433e4fed6df6343425f3013e409d
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:23:22 +0000

    base slightly knock approval false

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
