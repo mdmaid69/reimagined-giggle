f5e7fca8eda37a3c292d10aad518f574b5a76b7f
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:36:17 +0000

    fit about work maintain fun

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+def cube_number(x):
+        return x**3
