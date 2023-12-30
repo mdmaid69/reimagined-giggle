48551ff48b64cea8e4308a9e061caa8981f0d1f6
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:54:26 +0000

    must thick wire direct brush

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
