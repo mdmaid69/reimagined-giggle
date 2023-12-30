dbb7e6675820b96f549386294790014bdf8ae33e
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:27:09 +0000

    full break finally convince comment

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
