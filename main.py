fff7e2151a6db07b3f0091c522d6586721fb50d1
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:24:47 +0000

    holdout distance distant judge interesting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
