bacadcf1cb16f598aa0f27bec637d58eb0766589
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:53:29 +0000

    giveout game breathe anxious clean

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import tensorflow as tf
-print(tf.__version__)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
