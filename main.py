951f3ea2098b64ba285e0e782e73c410238c90bb
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:07:27 +0000

    free print principle scrape mean

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def get_string_from_array(array):
+        return array.tobytes()
