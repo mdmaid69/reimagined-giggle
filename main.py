1a5d27a163994174beab0f08c6dbfce80e17ede4
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:04:06 +0000

    far handle 565end disturb people

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
