f2fc54ce613edddd533348233492044d2c1f59cb
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:52:57 +0000

    any memory yours programme any

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def sort_list(lst):
+        return sorted(lst)
