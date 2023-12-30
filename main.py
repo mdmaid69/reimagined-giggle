0d08c8e9c8501939369553a3e5664cbb55f381b8
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:51:40 +0000

    collection professional opposite punishment lucky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
