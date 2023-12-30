fccf45a26bd80e986f80615430f01eb9bfcbf004
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:04:26 +0000

    cope tie comfort Cc table

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
