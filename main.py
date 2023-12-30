cb857ed7a7fcda25171827783cc6eb5d7ba243d8
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:43:44 +0000

    succeed trap careful standard sell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
