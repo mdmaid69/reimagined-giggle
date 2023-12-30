5337ef4e5f50cb27afc1e14fb2832d90464236a8
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:29:56 +0000

    satisfactory stop person indeed active

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+print(json.dumps({"name": "John", "age": 30}))
