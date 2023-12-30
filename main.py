04a73a43d3fe623509b578ebdd65746f630c7008
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:24:42 +0000

    dick whistle book loud handle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
