158fe2f0ed59fb5a1ba0adcc836e7a68529e2335
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:11:30 +0000

    giveout happen on tone ticket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
