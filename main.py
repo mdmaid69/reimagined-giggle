f5a4af7bf34f0677772fd6695988b86a64caa348
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:16:53 +0000

    better especially somebody wrong priority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import random
+def roll_die():
+        return random.randint(1, 6)
