3bc3b9802462bf8391a69bc19f43e52a5fd3d321
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:32:16 +0000

    myself finally Qq bright thinkthrough

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
