c3ebcd06402804ea6ebc8e829c5cad64e8f28302
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:28:47 +0000

    life more in bottom she

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import sys
-def exit_program():
-        sys.exit()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
