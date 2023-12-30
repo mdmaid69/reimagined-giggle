a6aec4201fa855ca4c869a4d064de518c5fded98
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:13:31 +0000

    fact community week manage division

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import array
+def get_array_length(array):
+        return len(array)
+  def square_number(x):
+        return x**2
