a3ab1e15951579de3147188e789c84a1e6871705
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:39:31 +0000

    confused danger voice matter fast

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-def calculate_work(force, distance):
-        return force * distance
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
