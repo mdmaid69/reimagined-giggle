3e2a1867cd3927e4d6aa5578ee9641749b4d0d89
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:57:03 +0000

    understand score protection wind temper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
