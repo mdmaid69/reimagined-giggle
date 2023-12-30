d3791b2141ed54395387a0148d8f4b726b12206a
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:11:17 +0000

    judge purpose break war excited

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def append_to_array(array, item):
+        array.append(item)
