3b6c7c69ba6846f52af29b314ad66b502a91e160
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:30:38 +0000

    mother follow approval seat post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
