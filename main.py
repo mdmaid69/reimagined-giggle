f17f692157846c93dffc32f15d805aeb7d315d3f
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:10:23 +0000

    sentence now blame shape 565end

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
