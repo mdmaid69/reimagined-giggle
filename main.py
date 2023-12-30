82289831cc7dc8c89844bdebcb13e773b38d0312
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:35:39 +0000

    onto just borrow uncertain tidy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
