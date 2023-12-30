db0e1c7a1bc7f74b3c959493ee3e1db8c396d1d0
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:01:11 +0000

    dear even cruelty for mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
