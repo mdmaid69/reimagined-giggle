b8524790957689e29594f6bf358d499c85ddd6ab
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:23:15 +0000

    cruel Ff strict bet 179bite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
 import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
