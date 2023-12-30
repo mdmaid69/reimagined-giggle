9f9c3faffccc299e93bada9a08a908a89af5677f
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:19:56 +0000

    cap holiday pump concerning lift

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
