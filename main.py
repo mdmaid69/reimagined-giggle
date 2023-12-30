630d76b64cb3e2d6ba845242f35dd354667b28b0
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:36:45 +0000

    cheek wealth lend fire and

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def count_characters(sentence):
+        return len(sentence)
