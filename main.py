bcfdc433df1138d58fcebdba378665507b4f35be
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:58:37 +0000

    talk advanced real quit Zz

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
