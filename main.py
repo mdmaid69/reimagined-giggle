bb6e4cf34672a218189a832f866ae337235e9019
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:52:19 +0000

    project suggestion towards tip black

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
