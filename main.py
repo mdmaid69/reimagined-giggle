918f55b732d30d938dc7c43c5d260a7e92dc2926
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:32:42 +0000

    holdup load every second breakin

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
