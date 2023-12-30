3236165369d064712c56917fa58b8e65583bf2b6
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:16:37 +0000

    freeze up shock rely cry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
