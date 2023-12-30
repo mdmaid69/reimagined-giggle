8208c6c222ea8e090c02048525a3d010f9db8b72
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:33:19 +0000

    everywhere play rush full question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
