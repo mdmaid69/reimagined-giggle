1a263887af570c4946b70e984389f7cd021d5230
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:19:03 +0000

    possibility consider near wear proof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_acceleration(speed, time):
+        return speed / time
