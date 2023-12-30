0695e62177759d8fbf615e74d8fc2c0f82320b0f
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:14:55 +0000

    watch earth divide yes exit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
