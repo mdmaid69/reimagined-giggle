e8ff5d5f4a75a8255bc5459486e90c624f5d2005
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:28:08 +0000

    offer hole yours 573entertainment waste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def square_number(x):
-        return x**2
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
