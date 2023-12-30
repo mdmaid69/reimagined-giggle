9f19251252f943c13e97c7c2b05111a70019f3ff
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:13:31 +0000

    skin only firm upstairs bright

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def find_unique_words(sentence):
+        return set(sentence.split())
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
