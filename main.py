774af8e6814696a44a7e78dcf68475d24966a856
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:27:51 +0000

    learn congratulate definite pretty love

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
