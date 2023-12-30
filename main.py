fb922d306e05debe266ad4de8afec4e6411b3fc7
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:17:38 +0000

    message experience prepare holdon mistake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import array
-def reverse_array(array):
-        array.reverse()
+for i in range(5):
+        print(i)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
