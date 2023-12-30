9402770d85146f299577f7825eb1f8a38d1effc9
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:30:33 +0000

    hope involve middle modern occur

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import os
+print(os.getcwd())
