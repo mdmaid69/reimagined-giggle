936218f3f2fe838165b321f135012c0539dbaec3
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:06:54 +0000

    forget free question steal pretty

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
