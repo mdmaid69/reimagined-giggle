aeeeabdf2d8f9a648f12c41ed3d24c519d520fc2
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:45:08 +0000

    convince convinced it soft complete

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
