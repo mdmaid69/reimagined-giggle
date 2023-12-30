e0f3433371c234e66aa0cc258fc0068cad8c18c4
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:42:14 +0000

    passenger mind together through majority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  def sort_list(lst):
+        return sorted(lst)
