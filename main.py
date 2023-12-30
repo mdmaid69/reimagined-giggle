12da27a3b3af1ecc76b272eb89480828da83b924
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:08:50 +0000

    adjective pressure sport have cut

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def greet(name):
-        print(f"Hello, {name}!")
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
