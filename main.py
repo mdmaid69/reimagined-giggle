f45f436cf6c2205a15ec6cf02a85188bcb35d976
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:32:56 +0000

    song recently mess gather plenty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
