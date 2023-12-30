257b058d6f10c69c1d2360f50c281aa19f83648c
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:43:29 +0000

    harm improvement condition often basic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def divide_numbers(x, y):
-        return x / y
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
