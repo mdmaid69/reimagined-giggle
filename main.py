30b55a8ba52615593cd65d54f72572a038412606
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:43:42 +0000

    guess sure target guess smell

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
