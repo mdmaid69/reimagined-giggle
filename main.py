359fa525197e31ad703ffddf30b3c98567fd8499
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:11:21 +0000

    front by approve surface wake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import re
+print(re.match("h.*o", "hello world"))
