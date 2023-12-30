d674ac90757cf367d2df322ce1bae60b7e80d58d
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:48:03 +0000

    569enjoy read summer offend taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
