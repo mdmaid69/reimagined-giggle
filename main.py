ed1d87db402a4ac4014d2c13171d720ba9382df2
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:43:12 +0000

    throughout exchange report book step

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def is_even(n):
+        return n % 2 == 0
