27654fbce770e9a0f10ca4d81936fcafe83cdc07
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:06:15 +0000

    comfortable still mixture cough cross

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
