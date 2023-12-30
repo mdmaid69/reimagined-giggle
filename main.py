13527a00bc626362745a2392aac4815bf050825e
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:42:28 +0000

    last basically yet fasten grow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-def calculate_work(force, distance):
-        return force * distance
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
