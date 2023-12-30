06503285ee835e90020762a6c9027cbc9a0b8919
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:02:22 +0000

    breath pleased keepaway suit uniform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
