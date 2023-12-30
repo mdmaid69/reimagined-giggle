fc415ae187dbe393650f6b0743f12b17b5fd6157
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:39:26 +0000

    aim carry organize base sink

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def add_numbers(x, y):
-        return x + y
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
