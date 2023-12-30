e6962efd2a1a958260e949082a3086cb4eee5863
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:44:25 +0000

    contrast shape somehow distance create

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
