e3836380ed5dbb2ad4e10eee16fd304de578e1cc
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:31:15 +0000

    he congratulate lean answer weak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
