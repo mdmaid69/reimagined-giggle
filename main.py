5fd0128106371fecd1baaf7c5239be29d6b6c16b
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:47:19 +0000

    record left whole trust shut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
