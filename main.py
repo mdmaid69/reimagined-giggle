6da0b68a5cbd72455d72b6d0a39d725785998ecc
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:28:36 +0000

    hole butter his spring company

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
