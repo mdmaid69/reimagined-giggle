1cfa8483b90cc68182d54f2bcd8952ac4ad95996
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:28:27 +0000

    ability race fork do sink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
