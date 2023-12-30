cae2a0a2c9ccb8ba3ca12838e0b33f9bbed0a8b2
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:46:33 +0000

    original translate lookback(on) minor runaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
