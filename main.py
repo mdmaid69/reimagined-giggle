0e657c7f2197022cfede589c258e9376621725ce
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:15:30 +0000

    border choose schedule lift organize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
