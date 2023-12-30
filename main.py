51577b992651e46c2a946ca1b43ffc9a1c0f2674
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:21:05 +0000

    tip plain whisper comein scrap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def is_odd(n):
+        return n % 2 != 0
