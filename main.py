c642ee76603fb63d550a37a9a112e377560795e8
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:55:10 +0000

    Uu somehow surprising gap rob

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
