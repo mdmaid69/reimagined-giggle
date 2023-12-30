e5211ad5060cedba6aff8d2529630b7b42275257
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:02:41 +0000

    natural nigga block wish main

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+def remove_duplicates(lst):
+        return list(set(lst))
