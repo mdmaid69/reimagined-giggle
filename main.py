8d97c01b2334ba4972ced0b5a6d488e257a92d16
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:15:50 +0000

    coffee exchange rid colour godown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
