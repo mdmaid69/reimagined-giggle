9533725486c11cc6e432bbca4d2086dee3e82a69
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:50:31 +0000

    behind in wave bin will

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
