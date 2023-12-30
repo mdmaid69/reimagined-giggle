ae9553e17cf39e9d5caca56571f0a7bcc42186c1
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:37:32 +0000

    breathe accident burn bridge pride

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
