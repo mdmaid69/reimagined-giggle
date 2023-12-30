6593a984d5c32b5d95ca43b6f6230dc9b575837b
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:03:28 +0000

    grow event game door old

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def get_array_as_complex(array):
+        return complex(array[0])
