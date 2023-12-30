257e2d4b33c7872d123eda04cc0ccb929cc4cafa
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:31:31 +0000

    sad over brown sink home

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
