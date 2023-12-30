10c103d26432ff09f34371d6e1aa4138ffa0ca5a
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:51:57 +0000

    frighten past hardly principle have

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
