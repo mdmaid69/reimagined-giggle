5d53745ba441e15bcea58f8da1a9cd43930230ae
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:18:14 +0000

    precise impressed compare tax raw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
