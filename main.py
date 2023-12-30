d2b2cf0702079900c4731bbd103ce5c1173598cb
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:35:47 +0000

    grade slowly key task finger

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
