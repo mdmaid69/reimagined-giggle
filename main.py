0c6fd0c8d68eea0ff0da011696d66281c3fea76d
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:39:24 +0000

    turnoff plant heavy noone sell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
