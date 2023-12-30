066f8ca6c3e6a54e009fb37e0c93706a98a8f5a9
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:40:52 +0000

    makeup what report turnaway fast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
