afe52db49c6b5baedae9867acbad3745a42ca3fb
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:47:06 +0000

    tragedy charge difference television forgive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_as_repr(array):
+        return repr(array)
