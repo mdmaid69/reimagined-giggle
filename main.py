b048ee08da2e7acf9306c5ee731396680d66936e
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:08:19 +0000

    snow sale equipment little dig

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
