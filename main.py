6d7e71564cbf2825caa57564ace18d6f03b8d080
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:31:40 +0000

    inform holdup correct language stir

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
