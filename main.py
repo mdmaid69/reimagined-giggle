993eb5f54d7274ff92ca1efb5dff3ae25073edbb
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:44:37 +0000

    proper listenfor hers distant true

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def convert_to_hex(n):
-        return hex(n)
+  def reverse_list(lst):
+        return lst[::-1]
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
