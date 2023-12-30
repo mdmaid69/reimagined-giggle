5a8471eda28c090b0933f95dfb025f9aed7eb966
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:41:24 +0000

    date ill likely build brother

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+print([x**2 for x in range(10)])
