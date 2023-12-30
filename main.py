e352625d5859ce05a223ffa5d75c7b3341346549
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:41:57 +0000

    when put someone though statement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_density(mass, volume):
+        return mass / volume
