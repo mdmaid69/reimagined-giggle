54b5ef46de16769762114e0561c5d96ea2cf97c1
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:15:55 +0000

    smile such dead spring allow

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_work(force, distance):
+        return force * distance
