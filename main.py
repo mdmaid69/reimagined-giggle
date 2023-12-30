f27dda88f2fd671f5df12a7e52f72c8a4ecd5d85
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:24:38 +0000

    process goout still whisper through

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
