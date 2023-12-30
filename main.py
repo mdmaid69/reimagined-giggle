369cb87b4e9eeb5bc166bbb3f73219ce7f73e7c7
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:42:16 +0000

    sleep will rise colour language

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
