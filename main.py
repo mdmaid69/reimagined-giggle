b8408a7f29a82ae36c9f981aeaa160741bd922e0
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:19:17 +0000

    wipe this expression care goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
