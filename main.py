0e05b809bc3a363668d9da99727f29db043fe7f8
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:46:06 +0000

    block combination better mind dance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def remove_duplicates(lst):
+        return list(set(lst))
+import sys
+def exit_program():
+        sys.exit()
