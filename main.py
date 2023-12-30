48fed1ea95753b2e9227ae8c68be7416f0fed98d
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:52:26 +0000

    clear relative actual performance protect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
