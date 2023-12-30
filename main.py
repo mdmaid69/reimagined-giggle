33b8b043e13390a2d18fe0023a64e469f859579e
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:28:47 +0000

    base however sign distant bill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
