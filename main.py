731a31de4c280afcaae6c9ae9733a7fa5ee784f4
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:10:52 +0000

    doubt way sort have cause

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+print("Hello, world!")
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
