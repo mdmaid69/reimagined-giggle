89956421949053c86b2be02caf3d1eb7b084db58
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:15:29 +0000

    open touch few clear will

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
