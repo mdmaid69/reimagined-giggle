0ef63eafd6cf94a52e6e6ad6cccf7f0584d6b453
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:27:13 +0000

    alone peace bang comeback see

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
