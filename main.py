728772a1b6d95bbad02e6bf971bcc30b2ae3e2d9
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:20:37 +0000

    skin 569enjoy fix round lead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import platform
+def get_python_version():
+        return platform.python_version()
