7a468d596775b43c25a01e1172e0ba6b84529ab1
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:55:08 +0000

    hit goon kick steam concern

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import re
-print(re.match("h.*o", "hello world"))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import time
+def get_current_time():
+        return time.ctime()
