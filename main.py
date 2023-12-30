14643a38640163e36d1cdc921d30aba568553e46
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:43:52 +0000

    rest main Ii possibly catch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
