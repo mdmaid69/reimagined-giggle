a25676373c09dc8af90bc8e2bdf2c5cc72b1fd59
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:44:03 +0000

    copy so computer clear hear

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import os
+def remove_directory(path):
+        os.rmdir(path)
