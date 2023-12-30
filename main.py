7e499bdc393bcef2264d9d7048faa822de5097c7
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:44:44 +0000

    whole pile care slice who

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
