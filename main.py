5e9d562f2910370035d97cb517a54191e7b76486
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:40:09 +0000

    low fly habit your fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_max(numbers):
+        return max(numbers)
