9daaa9e5f7dabb306e1d424cc094aff471f00e24
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:52:02 +0000

    and lost group manage push

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  def is_odd(n):
+        return n % 2 != 0
