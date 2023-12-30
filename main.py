d454be4d3518a93646bbca65757b17c98e32d3bc
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:23:27 +0000

    record few survive trip difficult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
