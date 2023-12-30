ce30d5a28168a6a2d8f235963a733f481648d0e1
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:12:48 +0000

    bringup fair mention hurry sentence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  def square_number(x):
+        return x**2
