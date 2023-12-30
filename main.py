ca05b662b46b6a177d27fab5fdba46fb9aefa387
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:28:24 +0000

    contrast into drink attempt finish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
