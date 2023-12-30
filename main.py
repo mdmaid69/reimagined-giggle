34df7f744741324165248687a98c4921cd204748
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:33:26 +0000

    of danger wild combine book

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_acceleration(speed, time):
+        return speed / time
