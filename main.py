a8b5c53276c65cb718aaacdb84fcee77da2089f8
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:48:45 +0000

    word serve depend study 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
