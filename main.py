55db0d9b68c81f6d19472f2df2b17049c9a924a2
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:28:30 +0000

    scrape brush complicated thumb comfort

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
