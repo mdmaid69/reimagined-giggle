5003e67f1b9fe7cadc03b1bfd86722948b9000aa
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:50:58 +0000

    dangerous slight factory back bury

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
