8048dff30b430d354488a082833aeb0216e6d456
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:17:52 +0000

    fetch still disagree butter scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def remove_from_array(array, item):
+        array.remove(item)
