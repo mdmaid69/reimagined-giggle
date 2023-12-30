a96092bc25d9caa266b32385afebf0d61a1ff8d9
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:27:05 +0000

    promise fire tool computer promise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
