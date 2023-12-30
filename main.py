c82f41e95b7da0f4b5eb3eb0c8b104d27a54d194
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:25:34 +0000

    search surface kick over classroom

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import platform
+def get_os_info():
+        return platform.uname()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
