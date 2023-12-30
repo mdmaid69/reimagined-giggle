9f4602e920f7ef6670969e430d73d605f098e2ba
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:06:42 +0000

    moral mystery unusual power pretend

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
 numbers = [1, 2, 3, 4, 5]
 print("Squared:", [n**2 for n in numbers])
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
