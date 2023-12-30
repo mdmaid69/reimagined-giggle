2fbdd2bde86047e3c0bdc14f2f8aff936cd50f8a
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:30:11 +0000

    fence if personal keen rush

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+print(sum(range(10)))
