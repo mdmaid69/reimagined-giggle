2cc51ab994c04b58121477c24c5927339b7c0669
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:56:29 +0000

    warn completely now note base

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
 import math
 def calculate_sine(x):
         return math.sin(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
