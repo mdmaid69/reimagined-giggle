f75ec99128469e0f478fa937af895ecf9058b41d
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:42:29 +0000

    dry claim cap underneath slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
