c2197d2714535b49f12e9aa0f0acc7dab7274e09
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:22:31 +0000

    ask freeze fun dull medium

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+def square_number(x):
+        return x**2
