fd137af5845feb5f258620dbf11f96fd1da76158
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:15:17 +0000

    crowd record both lift goout

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  def cube_number(x):
+        return x**3
