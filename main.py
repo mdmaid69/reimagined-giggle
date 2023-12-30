6b9d7f8484c1f81179f24e6532defc32bd63760c
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:45:56 +0000

    what joke Gg realize evening

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_power(work, time):
+        return work / time
