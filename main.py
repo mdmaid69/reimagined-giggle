f3fa4211415c5e8f994dab6ce627c8c2699527b3
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:41:20 +0000

    rubbish piece cook Qq play

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
