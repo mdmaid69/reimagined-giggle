82b2d8ee8f0091a260da95644c167f02633b6d37
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:08:51 +0000

    mine die next once whatever

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
