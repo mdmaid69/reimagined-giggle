6c1ee4c29f9144f6b5de1a70f0003cc4e4965620
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:03:05 +0000

    instruction holdup firm brown miss

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
