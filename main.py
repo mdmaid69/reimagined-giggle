6b727ea5beb858dfcf0f86687d065b3594714fe8
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:39:42 +0000

    or occasion just benefit turnaway

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def find_max(lst):
+        return max(lst)
+  def sort_list(lst):
+        return sorted(lst)
