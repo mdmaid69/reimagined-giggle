cc23ed153cfb3bd3667556a20dcfd81e68e53371
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:48:06 +0000

    rush closed pair tear difference

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
