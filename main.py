7fb42d73802c1ef0bd83eceef9745ea56663cc5a
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:09:49 +0000

    tape team comeout answer wire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+for i in range(5):
+        print(i)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
