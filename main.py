9d66239684ac5bc9001471575da955f6557f6d2d
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:38:10 +0000

    burn difficult ear through cross

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
