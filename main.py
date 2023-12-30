241cc977714ef8f583a9794fc8dbe351624180a8
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:17:10 +0000

    love like takeoff demand accident

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_area_circle(r):
+        return 3.14 * r**2
