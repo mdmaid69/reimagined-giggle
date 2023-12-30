8365c4656a1ef1f6733baf835e35ec4cdb19edaa
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:01:31 +0000

    force story raw public 179bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
