a833f9c830e4c276616f7438c7e12a38d6161596
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:04:40 +0000

    pass 573entertainment alone gate quarter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
