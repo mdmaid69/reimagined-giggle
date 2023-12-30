fc8f966245cd97045c11ea8d8462be9130b539b6
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:01:57 +0000

    takeaway shadow when worse doctor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_counter():
+        return collections.Counter()
