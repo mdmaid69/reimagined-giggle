f2e0716e40f09ce90d429a786ea4e03c4ece2f48
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:05:07 +0000

    turnback uncomfortable taste love steady

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
