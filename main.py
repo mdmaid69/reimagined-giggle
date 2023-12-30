0163ce711e52ac4b39ab1ee357550c4af6a9dc43
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:22:05 +0000

    means arm pleased formal unit

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
