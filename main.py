96d81b6621063e4af50baa49d11c7866cf4c3739
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:39:59 +0000

    unusual welcome wash careless Qq

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def cube_number(x):
+        return x**3
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
