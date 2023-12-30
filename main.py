0430595d975eacf23fbd8e002879ef1ff9d2024b
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:02:27 +0000

    transparent Ii please damage within

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
