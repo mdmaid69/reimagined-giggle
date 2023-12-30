4f8753230f561f788027bb4b6d7b4d81327d2442
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:49:08 +0000

    circumstance getup polite she top

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_volume(length, width, height):
+        return length * width * height
