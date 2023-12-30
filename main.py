cbceb3fd2af70e2c76936a24b615fb66379a10f3
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:57:28 +0000

    excuse somebody stupid collect set

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import array
+def get_array_as_repr(array):
+        return repr(array)
