a07cbe1e4cce03107d893afdd495f07b7b8d487d
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:31:46 +0000

    Yy peace arrive excellent dead

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
