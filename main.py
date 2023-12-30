fb3c48c2957233d3d22828bfa8f150830fe0a287
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:20:16 +0000

    development quantity hurt anxious watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
