ec339bcf5afc5696846afec86be79dda675d8e11
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:42:43 +0000

    kind safety arrive friend payment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
