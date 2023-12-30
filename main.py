a4e4724fa208321c1e4bc2df7c90a5d718780cd7
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:13:11 +0000

    careless skill less market lock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import time
+def get_current_time():
+        return time.ctime()
+import time
+def get_current_time():
+        return time.ctime()
