1364487d08c11e6784e09fd4636e942ab805f18a
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:27:23 +0000

    lonely store pollution on build

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
