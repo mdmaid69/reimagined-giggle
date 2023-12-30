fff4050938893ff89bb7398c631f2e73f958bdda
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:48:09 +0000

    knowledge secret alone cause lookup

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
 text = "Hello, world!"
-print("Uppercase:", text.upper())
+print("Characters:", len(text))
