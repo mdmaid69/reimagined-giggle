910e1e52ca09a39629f0ba30de582e87aa0224e0
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:57:58 +0000

    Jj shall contrast crash nerve

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  def convert_to_binary(n):
+        return bin(n)
