c5987fe5434cb7c9c68ea4d695ccb006c1cfa623
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:09:38 +0000

    permission whoever everyone band same

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
