e3ed3370a9060b7122668625489ff45683c37aa0
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:12:14 +0000

    survive shock legal match sentence

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
