7988863d3c506436937a4e0d3655394d0f0c0b32
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:13:57 +0000

    interfere at spare tread calmly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
