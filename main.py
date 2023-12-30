10943c2db6a24a60bcb4443b953dc74583d21dd1
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:58:58 +0000

    pleasant noise dark abroad confuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-print(random.randint(0, 100))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
