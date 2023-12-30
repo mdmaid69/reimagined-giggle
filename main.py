90393e40da4da53fc9d46b7e5964e3d5daeb4444
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:49:24 +0000

    rubbish active pump scene appeal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
