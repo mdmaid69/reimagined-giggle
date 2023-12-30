670c7a186aa5512fbab0f0bbeea1b2298208e700
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:11:23 +0000

    sound decision week Pp check

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
