3f3a673fba695c65a54456dd7cb71cc44de20c7e
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:17:15 +0000

    centre female can sell glad

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def sort_numbers(numbers):
+        return sorted(numbers)
