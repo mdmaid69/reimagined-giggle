757c47dc951112efbde61aacf4180c1ab02ddbb2
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:33:24 +0000

    need any you speed achieve

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
