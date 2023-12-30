ea61601cbffff6911d6e238b38652f1e21326a7f
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:56:13 +0000

    shoot giveout look owe advice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import numpy as np
+print(np.array([1, 2, 3]))
