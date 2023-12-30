009a02039f0f449b2fd12953926a0d680d6c7831
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:29:09 +0000

    police label maximum past kind

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
