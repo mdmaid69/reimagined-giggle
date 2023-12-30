b8022eb3229d8236720f514f6be675fc2cbc9ec9
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:22:48 +0000

    reply however official soil cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
