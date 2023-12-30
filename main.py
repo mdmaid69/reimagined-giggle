1f7099f66c9cd91027c20a8e9ed1fd659c9b9850
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:31:20 +0000

    bunch possibly slice undo slight

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import math
+print(math.pi)
