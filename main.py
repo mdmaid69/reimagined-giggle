2220f6cc167d1e2daa472095e010fa34a0b13a5e
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:15:23 +0000

    sport agreement suckme movement programme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
