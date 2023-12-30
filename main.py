28b762dfe43eb1fd70410750e5dac89918a42606
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:06:51 +0000

    respect straight step below over

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def multiply_numbers(x, y):
+        return x * y
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
