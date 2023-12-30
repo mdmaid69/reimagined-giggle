fde98c17e74a107c853c4250c28025203a9a8c5b
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:52:25 +0000

    secret tap damp trip fix

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  def is_odd(n):
-        return n % 2 != 0
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
