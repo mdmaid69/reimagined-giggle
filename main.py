73359b70585330a50b7a0b5dc6c1bf2f9a140b6b
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:32:56 +0000

    friendly consider store wrap somewhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
