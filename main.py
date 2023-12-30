604c638ffa22c68caac7a0cba76f20281359e489
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:36:30 +0000

    over central or miss rely

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def convert_to_binary(n):
+        return bin(n)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
