00d67844fcc3751c4a6c83bb8f4ac1d393058807
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:43:43 +0000

    cut thought goup passenger you

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-def count_words(sentence):
-        return len(sentence.split())
+def is_palindrome(s):
+        return s == s[::-1]
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
