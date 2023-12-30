a1b64ba08e52963d9b50f67f7c028ea41f2a1e85
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:24:45 +0000

    equal actually matter opinion such

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
