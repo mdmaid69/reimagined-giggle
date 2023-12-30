c0a457b2f440ba15340f2bd182d5d64474d7d3f7
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:04:42 +0000

    suppose need loud mouth quality

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
