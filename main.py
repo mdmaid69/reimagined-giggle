461e4d3fd7e46ce0c937f04cb5bed8b8ec11ed48
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:43:17 +0000

    declare lose share even punishment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_average(lst):
+        return sum(lst) / len(lst)
