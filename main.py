25817a58cfd2f21acc302a466edd3867bb676861
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:12:31 +0000

    depend well motion acceptable cause

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
