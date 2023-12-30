dc4a4783706651bb0c3a9533a41e12d729a673f5
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:04:29 +0000

    watch later mysterious fond song

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
