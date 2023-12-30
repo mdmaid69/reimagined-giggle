e5a046a872e7d7b7399f1e2cfa1fd6c3ffd789e1
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:30:37 +0000

    news good affect sure community

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
