712d2a95991eb5c1e8923664d82db5b911232e2d
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:06:24 +0000

    problem debt unusual unfair young

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_volume(length, width, height):
-        return length * width * height
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
