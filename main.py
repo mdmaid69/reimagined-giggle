f51de93d6b587bc0c7e45592aca2e4a72a566d8a
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:56:57 +0000

    tip discussion commit emergency scrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
