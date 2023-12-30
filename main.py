57a07baeac157986ee0f9ce6c0413c9009558b60
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:16:08 +0000

    separate over problem thumb finally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
