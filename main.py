3869c0cd6e60104614d87aaf510bdf55fd8e049e
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:26:27 +0000

    car consider Dd think sting

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def reverse_list(lst):
+        return lst[::-1]
