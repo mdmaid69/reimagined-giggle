d01f8c0f1285bf4f1ebd88fcebdbe27dabab923a
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:23:26 +0000

    almost cook all fix sing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import collections
+def create_user_list():
+        return collections.UserList()
