00c307d863adb5b67d2828f6ed5070cf2b6fa7bd
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:35:44 +0000

    order brain exciting spin doctor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
