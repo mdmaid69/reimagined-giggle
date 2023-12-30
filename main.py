a43868d5ead860782bed6320f3287a4491c8413f
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:00:21 +0000

    important pile dig refuse except

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def calculate_area_rectangle(l, w):
-        return l * w
+def cube_number(x):
+        return x**3
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
