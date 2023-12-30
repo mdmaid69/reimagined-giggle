50bd942501e6a421d658a0e67841650d8beaabb8
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:32:41 +0000

    effort organization month abroad deep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def convert_to_hex(n):
+        return hex(n)
