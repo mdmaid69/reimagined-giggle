17851c62ab9631e6037331496b6f94dd82e551f8
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:56:15 +0000

    sometimes little interrupt safe offer

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
 import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def get_time_since_epoch():
+        return time.time()
