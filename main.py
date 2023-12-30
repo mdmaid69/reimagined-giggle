2504f78105ab1eea26e3faa12b7908d730ed9685
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:45:57 +0000

    even mine anything nigga school

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
