d3ed4f2ad3637375ce0285f4975d964c60e026be
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:06:33 +0000

    hope declare plant miss direction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_power(work, time):
+        return work / time
