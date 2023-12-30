fb122fe6133ed6dd656ae5b3b72d41277fd7f41f
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:11:19 +0000

    mean warning communication hang she

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def sort_list(lst):
+        return sorted(lst)
