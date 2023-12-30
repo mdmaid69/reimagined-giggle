bf436329d96c56455ba78136629fc3416447210b
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:10:57 +0000

    uncomfortable face scream independent perhaps

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
