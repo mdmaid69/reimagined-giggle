7b06cf2c56a09dc277668c2bbe91de52e50856da
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:43:42 +0000

    so takeback temperature feeling news

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-print(sum(range(10)))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
