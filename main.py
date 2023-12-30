7ed27549f1dc716e26a5967af04eb03c63cc1828
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:33:36 +0000

    other load total generally general

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def sort_list(lst):
+        return sorted(lst)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
