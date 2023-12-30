fb677b6f957cbb0af3e1b838bbe1f8043098b7c3
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:24:19 +0000

    plenty hard calm appeal deliberately

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
