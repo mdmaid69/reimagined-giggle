e38d0b4453e7687af28686cfed036a746ccc586a
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:31:23 +0000

    from fence snow giveup fetch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def reverse_string(s):
+        return s[::-1]
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
