282366fcf382629822a2852e91f719112e10aab2
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:37:12 +0000

    poor Pp single slice loss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
