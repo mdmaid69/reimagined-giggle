f243583b6659263414b21e5b0899e113eff25848
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:33:18 +0000

    late total watch contrast have

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tensorflow as tf
-print(tf.__version__)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
