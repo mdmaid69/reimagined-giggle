533750a89b2f72c920b75c5691565a2b2844a4dc
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:13:27 +0000

    blame like doubt supply explain

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
