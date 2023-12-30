8cc1f0c6547fe92358077b2659c96daeeb9a9a4b
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:12:24 +0000

    blow swing leg still full

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
