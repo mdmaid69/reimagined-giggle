e4e84856afdd66f0c8ad7e434d4b10a5c072958d
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:25:30 +0000

    thirsty strong flow cross comeoff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_current_directory():
+        return os.getcwd()
