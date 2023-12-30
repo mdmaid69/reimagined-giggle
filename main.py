ed3d029bafd8e6661bf073a88393ca464d526e61
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:45:17 +0000

    style common lock period soil

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import json
+print(json.dumps({"name": "John", "age": 30}))
