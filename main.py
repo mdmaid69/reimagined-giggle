cedb44c03dbb2dfe6a5a773cc786dce8dcca0355
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:25:40 +0000

    your plan upwards scare taste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_length(array):
-        return len(array)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_density(mass, volume):
+        return mass / volume
