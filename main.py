f937a114118b4363b1f3dd484860a8c72e0aeb95
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:39:51 +0000

    speak single touch Qq great

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
