b13ed779431f641434f89c132ab79ce2ba6ecd59
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:14:17 +0000

    city ought she sure storm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
