4095434a853b597ac9788f9e2adea3261a8df60d
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:33:45 +0000

    round best table telephone interview

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
