e9187f2b13d9aacd00deee43acf6e85865b7aa36
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:56:38 +0000

    tomorrow carry voice answer thinkover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
