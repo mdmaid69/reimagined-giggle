472420d20355bc8b5e583ea3168cac98fe470cb1
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:18:22 +0000

    since rush chat direct attractive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
 import matplotlib.pyplot as plt
 plt.plot([1, 2, 3], [4, 5, 6])
 plt.show()
