4d06d21125d78588dc74f2b964292db75fce75d1
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:13:29 +0000

    on taste custom hungry talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
