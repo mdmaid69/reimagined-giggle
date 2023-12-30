91b768b29cb38adc455dfe5c76b31facfd65b5ad
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:55:27 +0000

    club police love considering 572enter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
