8daadbd0a8ba7243c0e27028603b1f96f8d399b0
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:11:46 +0000

    string lookaround fine description confidence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import time
+print(time.time())
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
