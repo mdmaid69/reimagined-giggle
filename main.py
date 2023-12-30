5faaee64e03936c8429990bfd2120a0bed4c145c
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:21:12 +0000

    excuse live carry used high

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
