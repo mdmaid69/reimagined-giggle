34093ee53f185a33421802cb5a96224cdafda491
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:25:22 +0000

    smell common top under another

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import datetime
+def get_current_date():
+        return datetime.date.today()
