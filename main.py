22076ad44c331d4da22b9f0964265db39f2ddf82
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:03:06 +0000

    inside size standard peace standard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
