ad4c89411425571e96d9ce935a29f705d4317b17
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:33:35 +0000

    unable much school pity successful

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
