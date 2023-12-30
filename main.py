c1942936951395c935b0ddb3f5ca26adc486fafa
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:24:14 +0000

    dark needle distance hang arm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import sys
+print(sys.version)
