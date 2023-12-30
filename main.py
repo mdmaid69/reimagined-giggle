ddb89c3b82c0062ebb67ee68805f26c828546e00
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:16:58 +0000

    nerve heavy murder plan keep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
