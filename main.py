10e4f7e1b32fce69245cb3b1cafb9c3ab0c711a4
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:59:42 +0000

    shoulder definite world stage stir

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
