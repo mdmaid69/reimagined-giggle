d2fa1346beb4209eaead7ab79917c131178dc410
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:42:54 +0000

    complain Dd after niggaacademy nothing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
