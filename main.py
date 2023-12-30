2b17593a40e747c8f6d72bd7fa330b8f27a17ebd
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:27:28 +0000

    moment relax grand ball comfortable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cosine(x):
+        return math.cos(x)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
