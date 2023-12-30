0cd5251e47817f69b0cecffa0c015fac0e13c758
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:30:06 +0000

    listenfor afford expect separate wise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
