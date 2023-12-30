c00ddd3dcb0515e5a1c2136d9035ab1bc8723002
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:01:47 +0000

    float alive view hard bringback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def convert_to_octal(n):
+        return oct(n)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
