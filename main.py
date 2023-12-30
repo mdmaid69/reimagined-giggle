fdb9f68b55c2ae42d15db01691814ac45536f234
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:32:44 +0000

    post tour insist branch with

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
