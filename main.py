c43db04443c13889a51d50e753183191641b12a3
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:48:54 +0000

    sale style lay question score

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
