83d97c0f12f92c9ae38bb6910216bdb4fdcac6ea
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:21:17 +0000

    accidentally method whether bringforward listenfor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
