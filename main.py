d6cc296d51097a0d0129a1f6748adeff418ef569
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:05:41 +0000

    her light loan cope burst

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
