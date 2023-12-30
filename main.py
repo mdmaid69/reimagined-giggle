4be150a6857249cea3b31a890684f7ac9be68987
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:26:27 +0000

    detail whistle slowly us team

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
