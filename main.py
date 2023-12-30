06853d0ac616485cad85b7bcdcde31d3626af291
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:52:49 +0000

    squeeze expect doubt subject recover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_power(work, time):
-        return work / time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
