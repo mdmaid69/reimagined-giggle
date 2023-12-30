91de79c2ab5a24f63149fd88ec583c77b32f866d
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:32:40 +0000

    564encourage fine mine no eat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
