b82bc456894ab83acd980cf37d01c61b13497828
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:56:45 +0000

    smell behave rest fasten expect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import os
+def change_working_directory(path):
+        os.chdir(path)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
