e7f22afdbebf0660d32cf63b9b99f73dfcc02692
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:12:15 +0000

    from advantage sky stage oil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
