45879b733e1055ab894963fd10654f32fb527e7d
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:01:39 +0000

    possible far Jj sore different

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_acceleration(speed, time):
+        return speed / time
