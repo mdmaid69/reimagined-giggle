5c2956e04d5f30cfea735cb4d11ea2be9d69b626
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:37:41 +0000

    somehow obtain food wave morning

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
