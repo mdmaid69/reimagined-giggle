937618757deb8c7af62b0a64fd50240f4f564d2f
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:51:47 +0000

    indeed knock question supply desperate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
