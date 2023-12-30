20fea44a8d60b02b718b0fbb30387eda8e7ae402
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:54:27 +0000

    available turnover their concerned great

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-print("Hello, world!")
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
