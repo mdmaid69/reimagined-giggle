a0a42d20fd9ce9dbb528c14fd634133e1fd927ba
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:20:25 +0000

    walk might century flat trust

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
