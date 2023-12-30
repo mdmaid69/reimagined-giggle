e6a15bb7344b1c49d812010fed917e58248982a7
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:14:35 +0000

    bus angry step equal below

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
