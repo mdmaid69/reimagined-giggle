4b9d78131a5818a05ae0bae3fe4bd87bc31f66db
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:17:21 +0000

    near hold light cost regularly

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def clear_array(array):
+        array *= 0
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
