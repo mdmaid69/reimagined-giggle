bb25ae266d891c5f359a646f11b24dbdb64e8164
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:38:15 +0000

    the last black lot hard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_area_rectangle(l, w):
+        return l * w
