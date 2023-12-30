2a7b299435243bada1e9b684c33418066acbf7c8
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:03:08 +0000

    quarter delight fierce sail regularly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
