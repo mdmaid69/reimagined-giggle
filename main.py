2eecd6c181b83b63d73240363dd959e8e9888ab3
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:26:02 +0000

    hole play bottom shelter square

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
