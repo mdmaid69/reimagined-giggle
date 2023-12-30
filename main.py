fedea07221121c0e2dbb0e2cdc7b9a10a0b60f3a
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:36:57 +0000

    Hh ashamed evening let taste

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import os
-print(os.getcwd())
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
