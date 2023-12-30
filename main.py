ceff9b78a4fc65124f6422178db9c70e4a5c881c
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:38:45 +0000

    attitude discuss detail regularly fairly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import array
+def reverse_array(array):
+        array.reverse()
