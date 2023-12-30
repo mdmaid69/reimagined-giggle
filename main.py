bf1299bf7d8e404ff47aabb598708b8b506c143e
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:34:41 +0000

    distance wherever rely study comeover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_set(array):
+        return set(array)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
