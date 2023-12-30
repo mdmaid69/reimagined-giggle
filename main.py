1674a0f1f71004e8c9267b0ef3f129ffe9b37b68
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:45:44 +0000

    post quality stupid politics Uu

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_area_circle(r):
+        return 3.14 * r**2
