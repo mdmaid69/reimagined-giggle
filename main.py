92518d7b4e1b5c7b9e41a0b899260945a16c89fb
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:12:06 +0000

    result person strike pull forward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
