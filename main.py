286bf15f70f69b27bc8c61ed2387086f1dcd4d87
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:12:20 +0000

    fold luck lift direction mean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
