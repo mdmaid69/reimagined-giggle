5939512efb19d681a6a9e79e9fdb809afe622631
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:05:08 +0000

    become development themselves fairly will

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
