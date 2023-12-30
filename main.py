cc052f440adcae1f7f3fb262d667868e0ec5127d
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:00:35 +0000

    giveout increase confusing combine transparent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
