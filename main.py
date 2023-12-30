6153ba8879f781561f24713cbe24d4f275692415
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:41:15 +0000

    fork mixture bad truth stamp

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_pressure(force, area):
+        return force / area
+  def reverse_list(lst):
+        return lst[::-1]
