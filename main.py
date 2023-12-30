4484b21f46b7a5542adbdced519bf0f98df80fd1
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:09:24 +0000

    Qq open meanwhile familiar anxious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
