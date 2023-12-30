2ace5c4ec500f474c6300eee802dd762aba668b7
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:09:44 +0000

    false club intend fetch offer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def count_words(sentence):
-        return len(sentence.split())
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import getpass
+def get_username():
+        return getpass.getuser()
