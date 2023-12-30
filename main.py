1194bb05d79a023fea04ce944443091e63186002
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:41:49 +0000

    chief hungry hard related important

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_speed(distance, time):
-        return distance / time
+import sys
+def exit_program():
+        sys.exit()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
