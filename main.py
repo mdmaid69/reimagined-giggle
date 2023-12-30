f2e977f54d84ad71a719ed9cc94704c41e02c42e
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:18:42 +0000

    wild rescue top course long

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
