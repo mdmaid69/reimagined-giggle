856e2bcc4be2db11373f89b37eeada7e14a58220
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:44:29 +0000

    comment want Ss pause regional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
