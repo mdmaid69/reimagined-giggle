e16c8a0d156c49d47d8c2e131947f46184dd60cd
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:26:01 +0000

    talk rob way separate error

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
