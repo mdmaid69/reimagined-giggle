515b0852e709cc732f7dbc04ae171a8a8e3370e2
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:06:26 +0000

    lose official comment live clear

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
