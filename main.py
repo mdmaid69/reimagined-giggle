a08af0f5b70fc90b5df682721c2cb7fc818c6ac4
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:59:14 +0000

    repeat safety stir consult whichever

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
