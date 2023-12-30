bc41d33e71d26b420bf428d98962907ed8dc008a
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:54:56 +0000

    involve power star knock charge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def find_max(numbers):
-        return max(numbers)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
