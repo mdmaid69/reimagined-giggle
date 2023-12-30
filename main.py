6a4e35bd671c36efee1cb12706574603bd744559
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:24:39 +0000

    the accidentally train dead float

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import collections
+def create_user_dict():
+        return collections.UserDict()
