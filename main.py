4095fa77bfac63ddfc6304329e6caa1f3892a793
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:33:57 +0000

    friendly sick yet work that

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import datetime
-print(datetime.datetime.now())
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
