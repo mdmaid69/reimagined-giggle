a1ef7666abb081c403ccd70d7319c5e885f62778
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:30:33 +0000

    mean independent fly alone now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
