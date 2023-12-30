73267baad5d3f42b5343ed039d34fe67895ad6f1
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:43:05 +0000

    branch disgusting precise about cool

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sys
-def exit_program():
-        sys.exit()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+name = "Python"
+print("Hello,", name)
