751cfddf455d06de1f0d19edc972d5ed21b48e5f
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:20:23 +0000

    recommend rank back object strict

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
