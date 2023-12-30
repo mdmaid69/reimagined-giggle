74c51d2e1969048c53c6b2b3df628c700b819806
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:29:54 +0000

    petrol fly burst guide lookup

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_area_rectangle(l, w):
+        return l * w
+  def sort_list(lst):
+        return sorted(lst)
