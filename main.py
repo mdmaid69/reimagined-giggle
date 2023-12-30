3fc223002ff355c704301f170fdf9b1515b25609
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:16:45 +0000

    understand him kind sing detail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import os
+def get_current_working_directory():
+        return os.getcwd()
