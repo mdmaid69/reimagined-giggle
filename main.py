0a2284e612df15fa1880041efa68f9d843c1f91d
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:13:39 +0000

    but matter myself thought crowd

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
