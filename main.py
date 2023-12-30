740ea48a891fea8a742c777e2b14650c6fd93bb2
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:55:17 +0000

    collection situation wing underneath year

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_volume(length, width, height):
+        return length * width * height
