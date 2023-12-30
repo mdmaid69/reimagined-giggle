434e15b596f24a5fd543b6975f5aaffbc21067f0
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:04:37 +0000

    weak bother ability collect exercise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
