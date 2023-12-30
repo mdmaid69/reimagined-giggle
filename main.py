8ba00910a47919035cdf9cc8b6015cfb5927a99e
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:40:42 +0000

    scene treat pity nurse finally

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-print("Hello, world!")
+def divide_numbers(x, y):
+        return x / y
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
