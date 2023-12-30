e9ab1052ea673c3cc7a6a3f6f9798bae204a49fc
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:09:45 +0000

    transparent charge crowd 573entertainment course

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
