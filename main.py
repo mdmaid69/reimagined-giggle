3a7118227f0c26ebc4cbfbfe20228dce3e5e7615
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:15:15 +0000

    that one breathe reasonable paper

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
