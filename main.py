e3129c57b484ff94300ad3ebc1128fd5c8cf6d4e
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:55:27 +0000

    afford above involve official level

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def count_characters(sentence):
-        return len(sentence)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  def convert_to_hex(n):
+        return hex(n)
