1659e10cf9f291fe9acee9f7470f193e2b53a751
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:29:41 +0000

    pain serve keepaway measure draw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import time
+print(time.time())
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
