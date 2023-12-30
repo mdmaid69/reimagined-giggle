e3eb5bd9e4f79e1975c2e0fec4ffb1b1988fd446
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:34:48 +0000

    slight add pleasant life shame

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
