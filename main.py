94a38a14ba46e94806095d93c0d57e52697cd0a7
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:56:52 +0000

    formal contents fork breakout gather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
