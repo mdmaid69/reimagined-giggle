eec2501d1b1a248eae411d02a823f3e2045e66c2
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:28:43 +0000

    fat worth also down various

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_density(mass, volume):
+        return mass / volume
