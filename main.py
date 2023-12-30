a59b4528cf8e6755f524645258c9e7fa53304228
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:27:23 +0000

    whenever gradually might male murder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
