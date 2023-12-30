7ccf4e321d3ea61dfc3a7ac8ec7e192bbabbb37b
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:01:50 +0000

    calmly concerned path slip much

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
