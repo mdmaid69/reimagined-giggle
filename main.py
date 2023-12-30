5f4140344c2fc67cc51680b76cc7a7f2b506ccfd
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:50:18 +0000

    mark willing worry fall show

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def find_max(lst):
+        return max(lst)
+  def is_even(n):
+        return n % 2 == 0
