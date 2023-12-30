dff386c711a97e049ce222d6152b8598047f2994
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:33:08 +0000

    plan team firm cost communication

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
