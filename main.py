b66f52050e7e32d05a79c8c83245dc60baad640e
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:34:29 +0000

    ours with unit dangerous tell

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
