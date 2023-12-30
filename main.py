a69d64dab9ec7843abca3c7746cd21d5dd5e64c6
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:29:58 +0000

    thing home urgent congratulations forever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
