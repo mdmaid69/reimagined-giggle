eb55a64afaa2284c0b9400f25a93c20ae8224f48
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:14:42 +0000

    evidence word collection long late

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
