fc5edd3fbcdc31009daa52dd6c15e033da217edc
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:52:51 +0000

    burst scale pity unpleasant winter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
