fb250ecf980d64e2d9a5bb397c8fccf7588bf870
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:49:34 +0000

    late emphasize sense sport total

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
