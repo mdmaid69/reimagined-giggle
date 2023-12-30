b01fb8489c2f287aa0c9bf2256175d61f7f96f81
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:46:02 +0000

    exit split marry forget just

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
