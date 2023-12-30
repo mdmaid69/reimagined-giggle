9675242ec745c82e3c554d81f055e0cb0961d474
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:12:03 +0000

    safety temperature already Zz flow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
