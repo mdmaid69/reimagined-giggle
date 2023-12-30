aeacfc56206fca2fbe82d9bf34d570f2fcc39447
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:06:09 +0000

    wind split agreement sleep runover

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
