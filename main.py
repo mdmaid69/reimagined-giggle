e6ee1536ed5132e9b323fb7e04abc9bd077253f5
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:15:12 +0000

    trust lean fork second meanwhile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
