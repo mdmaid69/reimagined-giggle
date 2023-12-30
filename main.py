2c48d5a28fba2c9651b000c56fd9073d9fea9ddc
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:02:50 +0000

    discover work fail keepoff limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def convert_array_to_string(array):
+        return array.tostring()
