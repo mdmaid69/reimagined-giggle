a9787d94e816c01a44688620a6aea4e1c21e881e
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:07:00 +0000

    wall everyone plant surprise concentrate

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  def multiply_numbers(x, y):
-        return x * y
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
