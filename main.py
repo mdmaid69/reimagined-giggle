db7678656331e511fdbc1b1958ac895b4c1ef62a
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:07:13 +0000

    sting hole wise interest dead

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_floor(x):
+        return math.floor(x)
