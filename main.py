5f33209b6fb9e948a08626a5a9b8f004e4fd6fbf
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:12:56 +0000

    search character worth uncertain table

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
