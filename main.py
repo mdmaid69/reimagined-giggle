aa28166bc779b1bb7011a848df4b639537a7fa0a
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:08:22 +0000

    bound welcome uniform least apply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
