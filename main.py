63d5cf735a3548f0559e7e10e9372dfc06c26c88
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:53:05 +0000

    wave appearance land gate more

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
