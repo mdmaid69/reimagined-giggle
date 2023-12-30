a9442c7c9cc8944635b00c399eed711d6a8d27a8
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:12:36 +0000

    little yesterday situation later can

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  def convert_to_hex(n):
+        return hex(n)
