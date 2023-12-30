c8b0658befbffdf986e04dd552ad61778300c3a4
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:44:36 +0000

    distance couple last mood keen

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
