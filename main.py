d3a95889b748fb4acc318d1750f7d284100704d5
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:47:24 +0000

    division service win university report

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
