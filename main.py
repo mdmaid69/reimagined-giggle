14a547e93a01520428f23a7216f882fe0c535018
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:46:44 +0000

    form guilty operate freeze actually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
