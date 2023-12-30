b71f0dbc1bf51b28be7a00e97bcddc4b493abec6
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:05:20 +0000

    actually feed size 565end left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import collections
+def create_stack():
+        return collections.deque()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
