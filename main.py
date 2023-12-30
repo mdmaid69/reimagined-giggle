8b29186ff115f9fc38b27c52a4cd19a4537d2c9f
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:00:15 +0000

    chief danger type culture string

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_current_directory():
-        return os.getcwd()
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
