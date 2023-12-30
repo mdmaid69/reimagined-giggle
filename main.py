d672cdf0860554e3027c713037fb5b3394e76c95
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:37:25 +0000

    knock runaway branch dry pen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
