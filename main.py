66b4b274d68d3d187c35f3fdfa6b2edfd8e6b6df
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:04:10 +0000

    slide protest realize free hope

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import array
+def append_to_array(array, item):
+        array.append(item)
