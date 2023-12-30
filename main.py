b0eb0b99e141cc5e0f972af5d37ff312248cfc27
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:38:35 +0000

    accordingto eat probably opportunity smoke

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def find_max(numbers):
+        return max(numbers)
+  def square_number(x):
+        return x**2
