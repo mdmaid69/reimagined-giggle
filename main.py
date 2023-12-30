4ae510839961dc258fa3c75f32a804fbec81c6cb
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:36:48 +0000

    sensitive period concern callback insist

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
