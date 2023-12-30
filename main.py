c48bf784ead1a74d440f2679ac11e82aa9f5aa45
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:01:00 +0000

    comeround goout wear doubt challenge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def convert_array_to_list(array):
+        return array.tolist()
