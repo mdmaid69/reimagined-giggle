50346c745d7671cb7f65a1152fb2bea06bc43af1
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:55:58 +0000

    sleep wrong drop recently dig

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
