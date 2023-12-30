7f44ab66925770b07438ac0b20aeab0e79b9f391
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:12:37 +0000

    method protect call should true

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
