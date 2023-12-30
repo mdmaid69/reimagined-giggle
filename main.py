48db0c77c04758f59d593f1ce2c50fa4687cce83
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:48:05 +0000

    several dinner keep become sticky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
