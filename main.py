0516cada6cefc11776a105d7d471a2047de6212b
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:41:59 +0000

    official star bill mix over

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
