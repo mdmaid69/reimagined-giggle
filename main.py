59287c5436a037e5bd00f819881f789cff3746fd
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:29:03 +0000

    extra round scare sort extra

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def is_palindrome(s):
-        return s == s[::-1]
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
