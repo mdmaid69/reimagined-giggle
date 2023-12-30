42289d91bfcdc1b86c1b08e7de89f5e79491d770
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:36:40 +0000

    but talkback interruption conversation attract

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
