4f72cdbd0d170f1b5b3cc352a236d873847678fc
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:09:28 +0000

    nonsense move tomorrow breakdown cry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
