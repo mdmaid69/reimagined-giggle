97bc97c9ef919698f388f9b09c4da939bb513c2e
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:18:17 +0000

    pass on lonely can murder

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def convert_to_octal(n):
+        return oct(n)
