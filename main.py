74b8653988d84a01097af1aa11e107c6699711b7
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:25:27 +0000

    hit today over average lunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
