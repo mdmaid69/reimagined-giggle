e3db1d6ecaa76c743348f477e6c635a03b2f6c94
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:41:45 +0000

    runout squeeze correction foot every

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
