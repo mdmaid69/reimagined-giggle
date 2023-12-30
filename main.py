ce5c13378a149196e307826797b31fc2f39bbf6f
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:37:42 +0000

    declaration swim question sick listenfor

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
