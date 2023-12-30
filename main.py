91833a197cee72792b5483995cd27ca32c91438e
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:48:18 +0000

    happen stage welcome effect under

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
