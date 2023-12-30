96937298da7c7d1e6efb0499e6fab7824db392c9
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:19:30 +0000

    workout roof exam slowly blame

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
