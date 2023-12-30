a9c5508ee0cea11ea2c4fa56c390bc2cfd86fbeb
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:06:10 +0000

    takeout doubt earn exit characteristic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
