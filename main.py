5391602c996525912e7f218fde362ced12589d9f
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:17:32 +0000

    reasonable cum common responsibility 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-import logging
-def log_message(message):
-        logging.info(message)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
