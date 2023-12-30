d4a51918ebe12b2f09ede7944376d8164c557f5c
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:13:43 +0000

    nose small popular past official

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-for i in range(5):
-        print(i)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
