26971e8097b24db940f84948fdf528f4904235e2
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:55:24 +0000

    autumn official control loose avoid

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def square_number(x):
-        return x**2
-def calculate_power(work, time):
-        return work / time
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
