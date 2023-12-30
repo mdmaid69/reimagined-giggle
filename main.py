9d96ee784471bd7a9c8365d9cceb293bc3a7dac5
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:32:17 +0000

    stamp 179bite ability fat smoke

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def convert_to_binary(n):
+        return bin(n)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
