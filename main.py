df9e39cdcb90b1fc756e7a9e5aad1690038328a4
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:25:08 +0000

    care sensitive responsibility nigga shallow

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_min(numbers):
-        return min(numbers)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
