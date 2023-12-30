c502a9a9976945077200785ee55b463257cfcfcb
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:38:53 +0000

    it further limit affect charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import logging
-def log_message(message):
-        logging.info(message)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
