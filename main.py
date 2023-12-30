bf99c6072f5afca23f9e1185536075933624009f
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:45:28 +0000

    goon regional remember female crowd

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-import time
-def get_time_since_epoch():
-        return time.time()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
