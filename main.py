1a0ccf5df25d1e92b861ad84837bf14409ff6502
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:38:37 +0000

    shoot blind ask contrast spray

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_max(lst):
+        return max(lst)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
