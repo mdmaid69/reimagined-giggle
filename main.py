559e0b2512cd5d21e3b9037e4cf683f30ae3ce26
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:32:01 +0000

    door track hand approval rule

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
