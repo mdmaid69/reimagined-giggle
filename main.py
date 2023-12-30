594ef25166e4a173aa68c5072e961b52bb887e54
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:54:15 +0000

    inquire judge judge drive niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
