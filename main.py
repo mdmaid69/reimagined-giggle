779bff7a853ce1ad04430ea86462eeccc56e146a
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:04:33 +0000

    skirt pleasure owe union hello

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
