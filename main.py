510240b6c87daafed3fbb82300267de063fdbe99
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:37:18 +0000

    face politics lesson experience may

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
