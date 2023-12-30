58c59882c7afa9fc0612739e7dc00b8401ad0946
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:52:36 +0000

    against second flat disappointed separate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def reverse_list(lst):
+        return lst[::-1]
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
