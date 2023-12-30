b9f2f3d99da77791832127deb7f692c8ce6817f2
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:50:55 +0000

    brush start neat arrange regret

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
