17f21e9d1f6d2b0189592e48c1d786d76105c64a
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:11:04 +0000

    record train extremely family disapprove

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
