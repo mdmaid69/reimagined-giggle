4b376a23dfc6bb4d38166c07bd6ac7ca1340ff03
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:40:17 +0000

    shock discussion exercise risk base

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import sys
-def print_python_version():
-        print(sys.version)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
