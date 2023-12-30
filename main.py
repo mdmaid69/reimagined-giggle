56e08950c57bedad5bf192168e64e0cbd1624ae9
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:41:33 +0000

    effect wonder nice identify disapprove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
