38b6281b5b35cb876cf048ae905a4e459e48f666
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:19:41 +0000

    desperate guess other desperate demand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
