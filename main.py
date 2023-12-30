c84807972afc3b91f859fbd5b8e2c4157f235814
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:15:50 +0000

    within remember quantity suffer national

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
