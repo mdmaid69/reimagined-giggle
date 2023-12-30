1a10936063b4dfacd191fe94dc1290ba8dd61487
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:48:33 +0000

    unable low confusing bar protection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+for i in range(10): print(i)
