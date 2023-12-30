94ec020ef5347b1a5371e6f642720482cb9cfee8
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:51:02 +0000

    wind threaten lookback(on) regional nice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
