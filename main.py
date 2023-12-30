667b3d36fb5abb32f6e0392660918c66b668b246
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:27:50 +0000

    ignore talk guilty beginning cruel

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import array
-def get_array_as_float(array):
-        return float(array[0])
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
