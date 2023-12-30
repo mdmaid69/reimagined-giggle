eede81d5b82f71a8aa2033f6b8981bf29964a145
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:13:12 +0000

    least show tread willing pipe

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  def cube_number(x):
-        return x**3
+for i in range(5):
+        print(i)
+  def reverse_list(lst):
+        return lst[::-1]
