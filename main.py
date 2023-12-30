a46ae436eb4594ef5c975eae9ca4fdc6853b5a81
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:05:30 +0000

    hand what complicated instant black

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
