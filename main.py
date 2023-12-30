4278e877b00c562e92b1c30758bc9362fb5a2b63
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:39:47 +0000

    underneath star tomorrow holdout scream

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
