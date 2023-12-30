0213446f8a38dc2d34d54ee67cbb3178887cfa91
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:28:48 +0000

    very underneath allow no space

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
