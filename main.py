b5222538d47f62430fe33cab92db74ffe7483981
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:42:10 +0000

    lunch extra interview objection happy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+x = 10
+y = 20
+print("Sum:", x + y)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
