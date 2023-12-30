5dc0295f12f0892c460e88b1f68c633b1d3c4714
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:44:57 +0000

    shame guide clearly hurt minimum

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+print(sum(range(10)))
