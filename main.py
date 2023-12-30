bdd0ca12a009ca47d30e8fe78f1858c62c641870
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:59:42 +0000

    profit support risk funny completely

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_work(force, distance):
+        return force * distance
