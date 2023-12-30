52f5bcb29497047b2769afc8d58dd233475edbcb
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:46:58 +0000

    past burst sky light peaceful

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
