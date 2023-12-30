5f2b0ba08bde380c4d211f570402e4fb4276e80d
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:56:54 +0000

    check nothing play bottom destroy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
