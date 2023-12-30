19a4539b5e238b096f73bfa5de27b53f81508ff8
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:46:27 +0000

    listenfor peaceful willing divide alone

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def count_elements(lst):
+def count_elements(lst):
         return len(lst)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
