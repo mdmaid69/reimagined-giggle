9897a17c3e4aaafb849529ef438e31e58244b729
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:07:19 +0000

    foot frightened union damp like

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
