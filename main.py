3917f139e53a0f1ffa4b0ed6c6f4a7c7213a9a42
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:20:10 +0000

    interfere 573entertainment your contact firm

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
