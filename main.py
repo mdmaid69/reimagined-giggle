c598e06819c70f39c09912a52d3b9a7818c283f7
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:01:43 +0000

    mind equal adjective brush angry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import tensorflow as tf
+print(tf.__version__)
