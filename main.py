7361f9a8e7ea51b7b84e2a324dfcc5776923bc23
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:04:04 +0000

    force then however catch remark

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print("Hello, world!")
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
