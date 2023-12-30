6e9b693f9412563519d17bc4996e7fee7e9490e8
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:44:02 +0000

    study hers earth healthy store

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import numpy as np
+print(np.array([1, 2, 3]))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
