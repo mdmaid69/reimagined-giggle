9b0f73ebf40ac1f38159ba24afd616fe8c8127a2
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:24:18 +0000

    pay run minute soon sense

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
