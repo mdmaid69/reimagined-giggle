c0cc3e38c28e1d5c7627ab34926da2853486f58f
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:37:58 +0000

    spot find with cruelty hope

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
