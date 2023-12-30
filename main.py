9ce7c531b55ce0a22fd15fdd1f8ff7467be05da0
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:27:17 +0000

    sort extra temper substance hope

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def convert_to_octal(n):
+        return oct(n)
