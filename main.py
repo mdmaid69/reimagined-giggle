7dca99f3c46d81f765dc34de8c40e3fc91126aae
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:50:50 +0000

    invitation true one slice lesson

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
