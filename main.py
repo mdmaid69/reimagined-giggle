ef7865e6659d99b2b7498b8e6d260981dcf77db7
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:20:12 +0000

    rent before scrap either circumstance

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
