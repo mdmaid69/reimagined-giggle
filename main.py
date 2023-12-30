4db7009ea6afe7c4f80b6d349e9d43c8da3ecf2c
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:15:50 +0000

    doubt movement conclusion health interview

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
