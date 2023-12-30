70f298b2d133c2c0854df3698e58b1a0fd73a1ee
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:40:04 +0000

    against favourite price turn thankyou

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
