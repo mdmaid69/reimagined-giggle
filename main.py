a1c88b27b568c3dbfbda6315b490a7d1ac3227ed
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:11:47 +0000

    fierce pay class have swallow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_min(lst):
-        return min(lst)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
