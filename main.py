0a63ea704ff74ea78a8e11c041c40ad91f67ae44
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:38:34 +0000

    classroom calloff wash shape drawing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
