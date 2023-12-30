5e99de3f4d07e60b6f13db3b56373edbaa29e02f
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:51:16 +0000

    possible fence laugh open noisy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
