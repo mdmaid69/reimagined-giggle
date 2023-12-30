2368e1b8dcb658d93f6d54f6b364f53b2abec38c
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:56:20 +0000

    dinner promise takeoff uncomfortable strike

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import array
+def get_array_index(array, item):
+        return array.index(item)
