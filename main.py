ed35de2b75709339c9a319910e92a0b04cbcd98f
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:32:22 +0000

    short wall ago fold nearly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
