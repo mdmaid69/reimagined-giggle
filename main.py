dc2d1b4eb7069b5dfb36304b0471fa0b24714d16
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:24:22 +0000

    town act habit dear scream

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+print(math.pi)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
