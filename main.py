cbea8fd00652cf6cf3fe6364318531a2bea62f60
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:32:41 +0000

    to operate question bank legal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def find_max(lst):
+        return max(lst)
