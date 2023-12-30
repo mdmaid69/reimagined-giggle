083ffe111f96b59f8a92cc38f27d30935499fca4
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:58:50 +0000

    situation close contribute radio easy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
 import math
 def calculate_cartesian_to_polar_coordinates(x, y):
         return math.rect(x, y)
