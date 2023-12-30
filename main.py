bc08d2a7dfdb0461fc937a83465908d2e10463c6
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:40:52 +0000

    cum repair progress challenge judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import sys
-def exit_program():
-        sys.exit()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
