938fde6b919320d622d43d82cb526b14c9ac34d4
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:46:02 +0000

    truth depend impossible anything name

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
