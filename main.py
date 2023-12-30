fd86b67e1fd50dea5f59f267a47e4e7cba2f0611
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:07:54 +0000

    continue radio classroom takeback anybody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+name = "Python"
+print("Hello,", name)
