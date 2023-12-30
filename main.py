a7e4487faeccc5a63794285ad739cfa7529dcfe5
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:05:55 +0000

    contact write complicated sleep state

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
