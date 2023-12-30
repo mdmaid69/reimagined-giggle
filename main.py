d71dbcb9b4c68e9308ea56c37ea1f9b4abacd88f
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:37:26 +0000

    grow guess show arrival free

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
