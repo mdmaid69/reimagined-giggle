74691a5b5dffc224a96e21ff6814ec4140838dbe
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:37:05 +0000

    him oil fire desperate charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def multiply_numbers(x, y):
+        return x * y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
