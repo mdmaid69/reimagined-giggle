b7cc83e161fc7acff17c8dd9e25ba88375387c98
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:28:22 +0000

    slowly block regularly long cause

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  def square_number(x):
-        return x**2
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
