d6b9da0d69e56d1b92f210c11a2aff303eb9eaeb
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:24:39 +0000

    split noisy shout Rr score

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def subtract_numbers(x, y):
-        return x - y
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def get_array_item(array, i):
+        return array[i]
