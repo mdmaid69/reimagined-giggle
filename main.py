219405de10d3a498f61bebe323717764c9c34434
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:33:49 +0000

    business flavour definite up suit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import sys
+def print_python_version():
+        return sys.version
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
