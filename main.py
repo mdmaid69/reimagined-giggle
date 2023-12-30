5199e743d5ad7a17050c15257f5a600d26129ae5
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:03:10 +0000

    burn shallow gradually joke region

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
