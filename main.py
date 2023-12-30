418433ecd080cdf283d9a195ec3df1e80bc5a2a3
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:00:35 +0000

    finally delight afterwards wind opposite

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
