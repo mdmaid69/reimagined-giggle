afe7b2e3aac92afe47b01b5abaf5f5e35d077590
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:50:41 +0000

    compare wall disturbing organize sister

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
