4c19b916b93eebb7d96775a4f44484adec723130
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:51:50 +0000

    idea address favourite cross beyond

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def count_elements(lst):
+        return len(lst)
