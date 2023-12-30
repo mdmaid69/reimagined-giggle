b548edf4fe0f776ce1d88db3e9ea20b7524b6452
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:40:56 +0000

    join so flow cover there

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def find_max(numbers):
-        return max(numbers)
+def calculate_density(mass, volume):
+        return mass / volume
+def find_union(list1, list2):
+        return set(list1) | set(list2)
