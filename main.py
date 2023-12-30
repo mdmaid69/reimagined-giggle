777774f7c00d33e993a87c65003311bc462890a2
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:42:56 +0000

    through over morning illness moral

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
