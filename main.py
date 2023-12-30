2a594782e6b9fda530469cee35bbe3a58a9eada8
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:49:34 +0000

    fear that seriously giveout land

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_area_circle(r):
+        return 3.14 * r**2
