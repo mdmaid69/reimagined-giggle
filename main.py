2771631fc538f61761d85f4b63cfd42919a84ba5
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:40:22 +0000

    turndown vision party wipe frame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
