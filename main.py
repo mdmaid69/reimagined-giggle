74af8887f9918a7ae531c6efb14a83a1301fa65a
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:51:35 +0000

    over detail wherever hand noisy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def reverse_array(array):
-        array.reverse()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
