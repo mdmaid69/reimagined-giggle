9b5d98e65ca6ec19202a9c7a1ead1fee50352f38
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:21:08 +0000

    only safety add Hh create

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import json
-print(json.dumps({"name": "John", "age": 30}))
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import os
+print(os.getcwd())
