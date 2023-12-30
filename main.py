e3a9e8e5ef69d875903b3aa178d4be6211f8322a
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:47:48 +0000

    themselves instant birthday scheme travel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
