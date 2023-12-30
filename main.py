4ed6c92645e3e6517c4e8dedf404ebe21b0e34d5
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:27:17 +0000

    dry card trick shopping healthy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def cube_number(x):
+        return x**3
