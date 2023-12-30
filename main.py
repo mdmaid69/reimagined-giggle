75073a4ff56fbd4417d1a6850b1537a5413a4311
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:13:09 +0000

    serious generally represent board exploration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
 def get_array_from_list(list, typecode):
         return array.array(typecode, list)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
