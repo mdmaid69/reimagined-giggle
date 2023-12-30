65687557ceb2706288baf5510207ceb5d391e53b
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:51:04 +0000

    tired answer tip thorough runaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import sys
+print(sys.version)
