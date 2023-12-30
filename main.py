27b72e75e89ed935956617a9c6d4d54069935aec
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:18:32 +0000

    demonstrate characteristic intelligent comefrom meeting

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
