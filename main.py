b6d20ae38bb19e3cab01447522cf0fbef71ec3e9
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:15:31 +0000

    cold sing calmly approval course

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
