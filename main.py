099790b953cabb4c1dc9d158f0246eb99d20f2e1
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:51:02 +0000

    ahead winter direct national smile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
