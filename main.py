b94391fa6ba941f800349edee3f5a27ac891d242
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:13:45 +0000

    question cover laugh year aware

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def square_number(x):
+        return x**2
