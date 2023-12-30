907c3c21fb789fd4dc7bae559da2013e8d47ee57
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:15:59 +0000

    disappear guess still better safe

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def sort_list(lst):
-        return sorted(lst)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
