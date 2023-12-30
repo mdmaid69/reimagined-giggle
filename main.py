1d6ab1f4576eca8f942b91cf2011b5839eedf8ba
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:03:35 +0000

    organize wise steep unfortunate under

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
