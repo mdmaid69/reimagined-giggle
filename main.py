92a2287eeca4cf38a94d486395e9a3e819ff4df2
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:26:46 +0000

    surprised alone cheque satisfactory danger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
