febc7fd5bd79d16214215c778c37e7abf425169c
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:05:00 +0000

    blame extraordinary comeup somewhere cheque

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import time
+print(time.time())
