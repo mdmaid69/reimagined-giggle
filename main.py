14a6877e66ab68a04c1f77763ce354a859be8d1f
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:29:52 +0000

    precise before practical regard object

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-print([x**2 for x in range(10)])
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
