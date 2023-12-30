c12cc40d3bc6b1990589b1ad4c070506d3d4efea
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:43:52 +0000

    argue uncomfortable discussion line system

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_current_directory():
+        return os.getcwd()
+import math
+print(math.pi)
