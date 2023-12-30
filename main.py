ddae3f6029b863680b9938a0cb0c1f14aa02322e
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:31:38 +0000

    kind cut gradually generous certainly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(numbers):
-        return min(numbers)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
