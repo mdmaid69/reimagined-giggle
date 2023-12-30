02d9967fe581a5d711f50e5db2e280d8aff4e272
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:07:37 +0000

    Ll section thinkover us 571enough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
