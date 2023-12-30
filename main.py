9771484432ce7749052ba91044fc8cff10785d8d
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:10:11 +0000

    fit thumb convince how keen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
