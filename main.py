04bb50c1798e3e392a333628db89cd6f680a8a25
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:22:02 +0000

    criticism high occasional share black

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_inode(file_name):
+  def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+def count_elements(lst):
+        return len(lst)
