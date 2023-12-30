165619f1cc4668e66e3954393eae9a7d9a7143b0
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:52:48 +0000

    exercise borrow above fresh address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def split_path(path):
+        return os.path.split(path)
+import os
+def change_working_directory(path):
+        os.chdir(path)
