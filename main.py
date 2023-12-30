89c87dac43415bd7514fabbc3905c695e9a4aec0
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:52:34 +0000

    entrance be beautiful effort destroy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
