14f9bbeb3eaa7f92ce856c0e565f16399169bc15
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:11:42 +0000

    sleep related inside clear change

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def count_characters(sentence):
+        return len(sentence)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
