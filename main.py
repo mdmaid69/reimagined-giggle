38aec3b60bf012c8031b08e7425082359773b78a
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:43:39 +0000

    lookback(on) television impressed so system

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
