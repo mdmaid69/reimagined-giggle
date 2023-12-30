9773d2ae4fea61768827b8121c6359241d138c46
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:16:54 +0000

    public region term natural exam

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import sys
+def exit_program():
+        sys.exit()
