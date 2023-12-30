018114e8e4e807a7ed09789ba4844c1a58e5e071
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:56:17 +0000

    vision hate completely cope straight

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import csv
   def read_csv_file(file_name):
         with open(file_name, "r") as file:
           return list(csv.reader(file))
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
