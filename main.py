db6e9698415699aaa61f73579404f35c46a96ac8
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:01:10 +0000

    beautiful exaggerate odd giveup generous

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
