e259291d935f9158a2515bf2fad01112abbcdca2
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:50:40 +0000

    train guard seat combination appear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
