0f85bc34f3ead3bd9d731c7ce941bbc259f02802
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:51:50 +0000

    573entertainment usually cheque happen drop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_work(force, distance):
+        return force * distance
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
