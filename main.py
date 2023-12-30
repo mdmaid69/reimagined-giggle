395323e29324e8ba3c27c88e9de026b186a87243
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:47:04 +0000

    crazy drawing travel somewhere stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import random
+print(random.randint(0, 100))
