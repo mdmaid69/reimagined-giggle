a0bc0bb9b9bbb01d6f749be1acda85ac031f5c9c
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:59:48 +0000

    border before as low probably

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def is_odd(n):
+        return n % 2 != 0
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
