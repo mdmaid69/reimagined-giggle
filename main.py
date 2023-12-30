b5cd26d24f7ef4473459eea18c9aa2f7b2f62e4c
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:41:55 +0000

    interruption communicate both sale empty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_dev(file_name):
+  def get_file_device(file_name):
         return os.stat(file_name).st_dev
+def calculate_power(work, time):
+        return work / time
