50e681e1355759033c82604d9efb1fb0c4003138
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:27:02 +0000

    use direct so sleep over

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
