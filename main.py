0425ddfb1c87dad9d5715884b1d51b30180086e8
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:18:29 +0000

    recover fold pretty 572enter murder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
