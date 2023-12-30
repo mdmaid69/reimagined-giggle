7dab9f9e13a0ee2602963a677d32631aca7f9603
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:03:57 +0000

    back shop concentrate reason display

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_density(mass, volume):
+        return mass / volume
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
