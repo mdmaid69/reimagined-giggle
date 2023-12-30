8bd6cedb10601c9f5f8a181b5a537e0675909849
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:17:27 +0000

    secret dance condition maybe push

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
