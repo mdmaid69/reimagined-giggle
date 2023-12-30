5bfdff3a806b8df5754793f5ebd46ddacb6580f6
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:39:22 +0000

    authority black he thanks side

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import logging
+def log_message(message):
+        logging.info(message)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
