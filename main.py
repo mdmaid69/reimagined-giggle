87ef0266c575f558457231c85faf6bcac4f8792c
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:00:39 +0000

    lookup frequent party else giveup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def get_array_index(array, item):
+        return array.index(item)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
