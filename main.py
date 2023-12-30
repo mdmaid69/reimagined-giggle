86142921eec68a315e1d21429977cf0430c9653d
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:53:08 +0000

    famous knock solve reject would

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
