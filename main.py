21a31fc9bfba3ae3d77e2290003644a6d132b415
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:38:51 +0000

    good stretch same follow lunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
