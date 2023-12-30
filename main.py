0ae14744ec2f0e2336455d3b16641b830cf48bbb
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:20:29 +0000

    exchange hide hard clever commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
