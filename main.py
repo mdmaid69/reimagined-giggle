d449a74b274948d33a70bdb8a1751c413dea13f6
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:17:32 +0000

    delight getback send love serious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def get_array_as_repr(array):
+        return repr(array)
