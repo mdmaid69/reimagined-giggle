13632af4974094353c19eef3d6262900b5a25d38
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:24:11 +0000

    blame what shout official giveup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
