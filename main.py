63536ddc75b301f1c42d986c5334d7f8e22c808d
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:17:12 +0000

    again pleasure behaviour from takeoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
