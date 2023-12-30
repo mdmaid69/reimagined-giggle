7c08dc2eaedf7898a88c81702e01ecc798284011
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:05:05 +0000

    bad snow obtain firm language

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_length(array):
+        return len(array)
+def remove_duplicates(lst):
+        return list(set(lst))
