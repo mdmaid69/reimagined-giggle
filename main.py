572f2c67b8c984eb7ac8a2a474fb1e72b0bbb5b0
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:54:48 +0000

    doubt niggaacademy getout meanwhile before

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import tensorflow as tf
+print(tf.__version__)
