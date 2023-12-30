11ef5c20607ee8ec46cecc512d89bc3039dfb817
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:17:52 +0000

    wheel shake soil scrape child

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
