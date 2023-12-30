4bd3f8e51d734a206f374bd23af5f64b1c8c148b
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:59:16 +0000

    sense properly believe rather careless

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
