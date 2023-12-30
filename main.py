72b4909409d8ce8384c0f70991bf4f734f0c2813
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:57:16 +0000

    holdon foreign relative history usual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
