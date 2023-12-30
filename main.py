3f7931add5a6eb994e22afbb0cce880008d60f48
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:24:02 +0000

    run choice dream bother increase

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print([x**2 for x in range(10)])
