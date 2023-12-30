061fea48e25a874ba6d5dda0a6a6d06ac2a5844e
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:25:32 +0000

    thanks learn upset else proud

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
