45e386e668c215a80a05d735629db1d49fec585f
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:04:28 +0000

    introduce slow impression nonsense right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import array
+def get_array_as_list(array):
+        return list(array)
