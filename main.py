6bba0b6cae741bea44e8d309a16aa4ad13c3405f
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:34:49 +0000

    embarrassed handle accidentally well judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import time
+def get_current_time():
+        return time.time()
