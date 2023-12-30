04ce6d4c7d9db762a30fc085a2855d49d2059953
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:13:09 +0000

    wave outside worth happen healthy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_pressure(force, area):
-        return force / area
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_volume(length, width, height):
+        return length * width * height
