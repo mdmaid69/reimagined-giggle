e99f54208c3f82c36c20d77b76a74747a1a1c599
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:13:19 +0000

    spray danger fill outside rain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def square_number(x):
+        return x**2
