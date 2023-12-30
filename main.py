ca0efcabad3b0227718e5d5ca301d15fce2b4568
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:28:20 +0000

    hold tip 179bite pronunciation Rr

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
