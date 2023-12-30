2e63194690533f38c2cf2d4b4101c884239eb9a5
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:47:41 +0000

    size write mind pride convincing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
