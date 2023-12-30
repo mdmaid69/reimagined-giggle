22f812aebbc2470b3b3e5272f28ae14fd684f3a2
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:50:33 +0000

    back study scene visit put

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
