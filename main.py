49edccd5a3948f1a1a696f79e5c60b6f014b4c47
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:58:23 +0000

    comeup bother nature 564encourage sick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
