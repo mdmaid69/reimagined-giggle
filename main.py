93a2e7428ed8f4bac25b840b5dd9beb7fc4ac42c
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:09:27 +0000

    hurry pair shine respect settle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
