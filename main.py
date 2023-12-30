82118225931299b7866fbdeaebd59792c1119955
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:59:10 +0000

    behaviour under surprising makeup feature

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
