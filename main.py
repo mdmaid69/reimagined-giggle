094fcbb03a60a8f00386a58cac64bfaf5ca218de
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:23:02 +0000

    proud very present illness Ss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  def multiply_numbers(x, y):
-        return x * y
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
