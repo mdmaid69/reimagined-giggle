8e1546fb4858f2e439e8e590c0c259f35c0c0321
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:19:39 +0000

    start case money organization one

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+name = "Python"
+print("Hello,", name)
