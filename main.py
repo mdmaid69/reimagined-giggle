f84eb7f33a978011a17b0b3e009f0193825e627d
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:04:47 +0000

    private unfortunate new switch straight

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
