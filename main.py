4a8a6ef0aa125859415a56f10debe1a6be7d2b86
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:45:05 +0000

    quick recent traffic bar both

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
