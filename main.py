671cefe93c76c3eda6c69db37d0a8679dfaa3b89
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:47:29 +0000

    alone shape material temper back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
