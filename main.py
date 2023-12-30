573ec25bd1a9c02d7a2dfc993ea10bf5dbc8f6b4
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:17:04 +0000

    spend spite punish point lie

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_power(work, time):
-        return work / time
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
