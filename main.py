fcedab6da9108fd7c6acb20fe2d2a305de15a69b
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:55:31 +0000

    less slice steal trick wear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def add_numbers(x, y):
+        return x + y
