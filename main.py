c28954412f85d5f7f0cc6e980df4d89ece4bf5bc
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:01:52 +0000

    embarrassing everyone child interfere sail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
