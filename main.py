7240b0569781248b80022a93d5ec57e703a02b70
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:52:20 +0000

    571enough start time week trick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+x = 10
+y = 20
+print("Sum:", x + y)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
