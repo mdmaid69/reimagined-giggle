fde206aea7d8b5ea6ec65192400b94754dcd5fb5
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:37:15 +0000

    female short speech variety temper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
