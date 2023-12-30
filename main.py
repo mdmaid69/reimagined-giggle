ee342256ff92c4c5b0063b773328e1f329108427
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:22:02 +0000

    mixture authority problem gate Mm

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import collections
+def create_queue():
+        return collections.deque()
