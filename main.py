0f5d348c0bf1e78801090d01c657a30eb10be2c4
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:40:31 +0000

    sort afford quit talkback plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
