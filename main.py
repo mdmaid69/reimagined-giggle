e07a85e53d7ede73965f2ed8e9fcead5cee29aac
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:08:18 +0000

    never very content Yy still

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def count_elements(lst):
+        return len(lst)
