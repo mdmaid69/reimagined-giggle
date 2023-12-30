120b502abe9927bd099e03847a4acc715e77b993
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:05:22 +0000

    standard hang freeze lend transport

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
