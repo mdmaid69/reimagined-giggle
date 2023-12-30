929620adb699eea4ce41f85b1643f3cddfe36e91
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:06:33 +0000

    force on coffee secret Hh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
