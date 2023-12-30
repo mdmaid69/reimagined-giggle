a4598351342d651fcf3613e923d977b056fef667
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:17:31 +0000

    popular trousers poor believe cope

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
