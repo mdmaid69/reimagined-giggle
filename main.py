77895d41a737853fb99b54afcfe7328031826072
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:35:09 +0000

    over size fight late hear

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+text = "Hello, world!"
+print("Uppercase:", text.upper())
