abc719638626881c3c3d62a6ab94d2541f950af4
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:16:37 +0000

    knock school display play any

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
