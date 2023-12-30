43b93ad3f2d1a6a594fd7868ba49c80c0308c37f
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:30:48 +0000

    sign burn float load Qq

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import collections
-def create_user_list():
-        return collections.UserList()
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
