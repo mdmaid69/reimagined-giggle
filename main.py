006a2cfe08b90ce559a809e58f36351fa1a51cdc
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:45:53 +0000

    welcome open arrangement throw drop

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
