88b85e847f5be2359a9b63b01a9bac940fbd6602
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:55:48 +0000

    wise shrink improve before soon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
