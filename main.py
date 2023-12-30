c1486005bcec77d2b5420d62db68ad7206cf3b67
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:48:06 +0000

    activity bet bet state soil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
