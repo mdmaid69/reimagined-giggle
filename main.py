0b103f6c50c93315fb315d23b3f34fb204fd6cc5
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:24:42 +0000

    unit comeoff agree print preparation

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
