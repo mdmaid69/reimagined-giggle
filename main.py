4f825a051e4aade640912a2a8ad1a8c02cf5670b
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:06:07 +0000

    bush wish complaint live play

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
