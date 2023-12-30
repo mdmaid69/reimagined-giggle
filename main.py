060affe4f7f39b6513968443879e5c2680fc304b
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:07:41 +0000

    nowhere transport above conduct supply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def find_min(numbers):
+        return min(numbers)
