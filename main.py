90fa24011268c76622526739155b4d40250540b5
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:40:15 +0000

    mend estimate sit mistake favour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
