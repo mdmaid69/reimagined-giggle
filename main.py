41017d3fbe283648f4fd924502059d0912138147
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:41:04 +0000

    wave unusual temperature play offer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def convert_to_binary(n):
+        return bin(n)
+def find_max(lst):
+        return max(lst)
