48a1999e47bcf6986a0e897a720108c94c782b37
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:44:51 +0000

    left surprising relationship once OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
