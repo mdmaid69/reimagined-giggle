774186b65c88ee20d7e6d2ac2b730acf66555ad2
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:49:41 +0000

    fact far bar point dear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
