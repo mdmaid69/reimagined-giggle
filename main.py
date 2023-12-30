aff1f20595beca6e6345cbf3a090d8eee6e9f470
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:19:28 +0000

    more plenty interest decide level

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import time
-def get_current_time():
-        return time.ctime()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
