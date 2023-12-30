4aa3f0655b10c00336bb527c367908bee19d9f11
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:34:12 +0000

    decide situation argue extra plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
