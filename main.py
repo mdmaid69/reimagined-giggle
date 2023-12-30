e7c1b3c31a16e3bc1da5eb838821a1e70124fbe5
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:48:53 +0000

    us point unit last goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
