5609e045a19d55d772c0f7edc08f134e615cd6aa
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:45:37 +0000

    container dangerous angry area winter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
