e1e1e1cc8174843afa2f8c296002dbfb39a100ce
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:03:29 +0000

    getaway narrow protest unusual spare

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def is_even(n):
+        return n % 2 == 0
