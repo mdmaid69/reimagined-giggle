6ebeafda582c9d2214a4b20ccce1e482e659327a
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:19:20 +0000

    thought aim alive passage satisfactory

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
