96651f1157c5e2fc7472004aa05737986fd4dfda
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:07:58 +0000

    interview by spray replace pressure

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import array
+def convert_array_to_string(array):
+        return array.tostring()
