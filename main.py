9252473c78ec9567c49be54315f989fc03cfcf15
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:21:41 +0000

    flight sentence react exciting commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
