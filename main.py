c1f3f8d0ff0c933347cdb42d9ebe694dc0f1659a
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:48:59 +0000

    always peaceful otherwise join single

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+for i in range(5):
+        print(i)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
