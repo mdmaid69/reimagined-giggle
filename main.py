abd9ebb051efa117b872f3ff873487a7f7dbd38c
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:30:50 +0000

    prove copy guess catch relief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
