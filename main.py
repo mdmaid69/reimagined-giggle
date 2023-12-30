45bd01aae083d3ff15672c731f5be390ed7f104c
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:26:07 +0000

    set thinkover scare wonder bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def get_array_index(array, item):
+        return array.index(item)
