91ff94333184f1f455723b4e9aa666d3d9f344d2
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:25:24 +0000

    ticket public bite quit brown

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
