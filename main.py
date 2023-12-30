328466c4bebb488e3d137d9aa3249166208f2a6d
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:15:47 +0000

    little calmly wrong knock wait

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
