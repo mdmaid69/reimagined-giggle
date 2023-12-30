cc566fbe86add70b9125e2f5f25e380149da094b
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:45:43 +0000

    rule peace giveup mine actually

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
