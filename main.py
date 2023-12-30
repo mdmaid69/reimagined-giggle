be61d08e23e8ff921cb789cf5c905552c830d47e
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:02:46 +0000

    tidy this meet meal honest

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  def sort_list(lst):
+        return sorted(lst)
