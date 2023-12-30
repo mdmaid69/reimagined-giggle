a36be762535d7aabf22af91e9916761f44094093
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:49:25 +0000

    rule clue frequent disappear rule

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
