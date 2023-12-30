e640c5607c05c994e776c1c40d51863ca7f44687
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:58:00 +0000

    nor comeout equal laugh callout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_area_circle(r):
+        return 3.14 * r**2
