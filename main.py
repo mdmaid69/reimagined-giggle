288505aac6c2626bc8a2b553adb1a655b292a33a
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:45:22 +0000

    watch operate performance dig fat

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
