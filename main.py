83a7060a838a73fe13a27fb2506fbc58befc91ef
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:19:18 +0000

    sister crazy suckme pause organization

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def convert_to_octal(n):
+        return oct(n)
