8f782f4bc51b6f5c8e0c422ed2961269206fe4eb
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:15:02 +0000

    complaint turnout throughout demand in

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
