9dc85ff2393228a26fb75b29c28719b89ee386b6
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:05:33 +0000

    power maintain or certainly exchange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+name = "Python"
+print("Hello,", name)
