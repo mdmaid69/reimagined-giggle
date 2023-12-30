9eb9782771795bdb1bf0444a2d297de2ac8efac8
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:58:56 +0000

    bye radio factory now fond

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_density(mass, volume):
+        return mass / volume
