3d1b211641e833bb87d0b653dfd2c0e818295dee
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:25:48 +0000

    fun brother handle prevent frequent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
