accfb17fadfe6ce49ffb92555f7e2b58caafbb79
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:45:49 +0000

    fat uncertain runinto comeup private

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+for i in range(5):
+        print(i)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
