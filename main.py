9c11022b91f69116808b3c0cd72620d99b634fc5
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:15:12 +0000

    position should search woman zero

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+print(sum(range(10)))
