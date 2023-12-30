03de667c41e2997bfe63d1b47517e41e34359806
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:11:31 +0000

    comeoff average lie future Pp

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def find_max(numbers):
+        return max(numbers)
