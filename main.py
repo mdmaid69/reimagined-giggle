8c158553df7a92913cefb1aa43b772dfd84f2be7
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:13:56 +0000

    back soon about stamp manage

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import collections
-def create_user_string():
-        return collections.UserString()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
