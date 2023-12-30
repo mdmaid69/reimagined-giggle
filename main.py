3ba4e6070fb8a260c301d17f7d2d16cbd4ae4edc
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:02:57 +0000

    catch 179bite image see bread

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
