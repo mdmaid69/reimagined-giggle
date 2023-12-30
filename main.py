0fca2370dd281aeaed16facdc40635537b360d91
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:23:59 +0000

    basis face trip cross OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
