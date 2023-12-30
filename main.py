9bea53905433c5aafd9baeae508c6cd0cdd1301e
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:00:52 +0000

    compete pretend worst especially search

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
