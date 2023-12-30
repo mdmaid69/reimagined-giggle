aa7b63267846db9c54e4190c06fc8d421a2b9452
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:59:35 +0000

    getout disappear everyone machine underneath

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
