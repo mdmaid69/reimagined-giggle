bbf8ce1f682d38a4b61e07370c5af1d80e9c983e
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:34:28 +0000

    over brush announce drink score

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def reverse_list(lst):
+        return lst[::-1]
