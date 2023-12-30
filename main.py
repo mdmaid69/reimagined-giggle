5ab25f5f5e87961d53260723fdab0b6e6e7a1037
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:35:42 +0000

    cap upwards of along game

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Characters:", len(text))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
