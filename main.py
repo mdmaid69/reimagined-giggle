e5d6ac5a79caff143a0b66057253f634f0c0575b
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:52:18 +0000

    type thirsty disturbing strength guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
