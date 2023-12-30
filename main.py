9746f463d47a32aa38acce87793b5c37748ea67c
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:40:36 +0000

    callat tone sign speed disapprove

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def reverse_array(array):
+        array.reverse()
