b3cc31aa0a4cb388aec0ec98f4bd4796bdebb522
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:20:31 +0000

    creature term crazy relation talkinto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
