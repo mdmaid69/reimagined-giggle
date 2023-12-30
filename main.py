1006d4d210c4ba60667a72c6df8ed3386ce30647
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:54:04 +0000

    just backwards bother left prefer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
