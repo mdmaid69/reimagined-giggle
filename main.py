b92c3fcf433f919fb4009f1f53f69e73c98c06bd
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:50:55 +0000

    refuse experiment influence call fortunately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
