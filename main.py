4f001b15af7bad4cc5047136048a11a2c1b0602a
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:30:57 +0000

    add hot constantly brush nice

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
