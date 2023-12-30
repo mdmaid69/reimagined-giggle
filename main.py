a68c9b131374e3e3a717da92e378dda6bbaa6058
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:28:32 +0000

    cheap fairly proof great sound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
