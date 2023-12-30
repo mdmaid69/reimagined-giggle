b18173b07fc3a6eefda54dc971fd812a6d2fabef
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:51:51 +0000

    production holdup shopping ilovedicks by

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
