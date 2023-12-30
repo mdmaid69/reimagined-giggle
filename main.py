1d29c8aece0715b9abe06a40f8bd95f2ece63a3b
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:07:58 +0000

    shopping no child space bother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
