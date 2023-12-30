58f1454bfc99e72908f059ada47d76350952f18a
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:19:32 +0000

    joke foot shoot consider love

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_int(array):
+        return int(array[0])
