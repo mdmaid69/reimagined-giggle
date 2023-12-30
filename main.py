bd4706089e55381214006e2a66eff625b0676b1a
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:00:48 +0000

    lower nonsense wealth delicate thumb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
