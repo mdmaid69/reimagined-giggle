57b25f38378ba45675b4784852f680b4603ff719
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:49:28 +0000

    owe thin wind learn scale

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
