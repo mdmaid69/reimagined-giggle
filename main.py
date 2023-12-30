c326cf0d1e864402f5615b414be45215afb344a1
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:44:18 +0000

    pronounce amount later least today

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
