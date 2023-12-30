709e6fdb507e6908d24ddf60fe80cf368a18935c
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:18:28 +0000

    speech communicate poor cheat runaway

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
