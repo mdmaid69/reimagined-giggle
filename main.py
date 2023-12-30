bc900026747f56b46fc11f8ea63b87de7cdc52a1
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:13:12 +0000

    spend attitude slow correction able

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_force(mass, acceleration):
+        return mass * acceleration
