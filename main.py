fc7108f82a2451f52f36008770a5b36f4d4b8495
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:24:43 +0000

    forget shade our pay maybe

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+print(sum(range(10)))
