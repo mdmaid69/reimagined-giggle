ffac5744d45ad199d18a77506dc95be3e953a3ae
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:58:49 +0000

    level off plan practical question

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
