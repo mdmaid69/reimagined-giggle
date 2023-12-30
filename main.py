5209d8b6299e790738869aa4e0c30ea2ad1bad86
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:57:49 +0000

    minimum division interview market best

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import platform
+def get_os_info():
+        return platform.uname()
