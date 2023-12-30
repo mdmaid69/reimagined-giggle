a31a5c92ed10025a798e07c998ce7ff8c394cb35
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:40:33 +0000

    patience imagine central payment education

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+print(re.match("h.*o", "hello world"))
