f5449be1edd736582024f61f90d4f5f4f84cabb2
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:36:31 +0000

    tongue action mystery spring less

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def reverse_list(lst):
-        return lst[::-1]
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
