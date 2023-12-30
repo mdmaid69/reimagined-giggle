fa5fbf4a2c7cb0a8037603b0396ae4f9526bd05a
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:13:03 +0000

    bad yet listenfor properly perfect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
