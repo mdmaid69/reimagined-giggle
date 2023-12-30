059b9e7faeec68a69300db88cbb5d96c73cf0ba0
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:36:28 +0000

    organize whenever news high physical

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import getpass
+def get_username():
+        return getpass.getuser()
+def find_min(lst):
+        return min(lst)
