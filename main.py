088b048d2b97cb6f06cd3ff5d18f51be0e6209ae
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:43:45 +0000

    side measure common certainly error

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
