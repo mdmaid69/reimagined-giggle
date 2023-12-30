abec04ed4731878bc667793cb332cc0963256762
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:31:05 +0000

    cook glass damp worth they

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
