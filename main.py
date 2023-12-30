2e3fd62212cbc659f3116fa0b036d6b9f0157b20
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:05:42 +0000

    person fear ignore warm century

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_work(force, distance):
+        return force * distance
+import sys
+def exit_program():
+        sys.exit()
