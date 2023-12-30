2640c81eacc31a0f22d4f9cb3350369ca38035e7
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:03:42 +0000

    block worse sure yes disagree

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
