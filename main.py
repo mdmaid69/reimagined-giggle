3c9f8f440f74b53123f0e2b682439a1deaadc22c
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:37:19 +0000

    that to another admire see

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
