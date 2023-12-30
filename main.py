2062f4f233d020febb0165108cc82755e999e14b
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:44:16 +0000

    result none search since slide

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import sys
-def exit_program():
-        sys.exit()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
