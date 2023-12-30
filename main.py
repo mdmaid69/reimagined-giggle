a733ce58386162e81ae00ff1c40fa9889d1df33a
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:53:12 +0000

    patience independent style present damage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import random
-print(random.randint(0, 100))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
