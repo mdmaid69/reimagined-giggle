c67fcfb21ae036785fd058dbd3fc6e433d1958bc
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:59:34 +0000

    team fire boat illness pain

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_floor(x):
+        return math.floor(x)
