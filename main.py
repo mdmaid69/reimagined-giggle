bd3a9cc33233fd774ceafd6f27bb3037bb7a4a27
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:43:54 +0000

    want eager difficult range food

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_counter():
+        return collections.Counter()
