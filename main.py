fe0605b0a73e9e4bda570ee52ca938286cdd7427
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:37:20 +0000

    test unpleasant consult call generally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
