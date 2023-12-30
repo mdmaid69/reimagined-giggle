972353942fd80366eeb5d1423ac9c5d7287df56a
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:52:03 +0000

    sorry large minority yes suppose

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def is_even(n):
+        return n % 2 == 0
