903ca0483c82d4102878547a91524ac699a2eb0c
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:24:27 +0000

    scream deep copy design sound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
