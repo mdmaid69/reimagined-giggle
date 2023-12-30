c3bb07c3837483659651dd32806a9944a8b1dcbb
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:03:08 +0000

    impatient swing narrow pollution alive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
