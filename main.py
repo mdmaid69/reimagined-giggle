6fa8cdc9f5e2f6a833903aab9fd640c95f17f47b
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:17:10 +0000

    level underneath callback fond label

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def is_even(n):
+        return n % 2 == 0
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
