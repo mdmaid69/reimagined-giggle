1bdc61f3c7f40d9dae2a12453dd008789f29426c
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:35:21 +0000

    either should deaf arm accordingto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-name = "Python"
-print("Hello,", name)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
