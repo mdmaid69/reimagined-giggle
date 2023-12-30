f7d962981876b2f2c9b5cd7fc9f329a78b223c4e
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:25:57 +0000

    will sticky guilty own relax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def reverse_array(array):
+        array.reverse()
