ad710358414caefa225364052d96f335cfdb414c
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:47:25 +0000

    runinto face getup bet get

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
