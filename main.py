7176865a501a9646ba8eaa82f2268c1fd3db8129
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:56:09 +0000

    mass answer last upstairs match

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
