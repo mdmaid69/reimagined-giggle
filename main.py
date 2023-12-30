0baf444998808cd163f28d2da0532d476e26f82f
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:59:54 +0000

    relief approval keepfrom achieve mail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
 import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def get_array_as_bytearray(array):
+        return bytearray(array)
