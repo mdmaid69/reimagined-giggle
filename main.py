05442f12b61b7d667044b95b2e920c2af791d633
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:36:39 +0000

    slice close foreign advantage town

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import re
-print(re.match("h.*o", "hello world"))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
