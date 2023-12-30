fe691c825793229a80e925e973ff63b7e1aa3a0f
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:38:07 +0000

    whisper produce appearance since punish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
