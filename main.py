4f7d44e7e1bf63735419b340852e8f755d07f7bf
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:32:33 +0000

    present regard Ss open Cc

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import random
-print(random.randint(0, 100))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
