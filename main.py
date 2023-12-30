c9d992f7e72c4237cb4386d33c6925efd4048f88
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:20:28 +0000

    characteristic suggest choice free any

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
