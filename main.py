db690a0dd28a15c06dd60b574fb658b09763f423
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:27:45 +0000

    claim rid that load trade

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def is_odd(n):
-        return n % 2 != 0
-import collections
-def create_user_string():
-        return collections.UserString()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
