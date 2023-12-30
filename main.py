fc0fbf6aca24f601d522d9ce587cb38ae68798fc
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:15:33 +0000

    window turnaway choose breakfast cope

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
+for i in range(5):
+        print(i)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
