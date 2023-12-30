69752f9880adf91420bec5a9562eb80fedeeb1c1
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:36:17 +0000

    hurry callback feel though uncomfortable

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
