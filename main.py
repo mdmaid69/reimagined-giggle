594462a54c4dc14378b7cad2e8ad0285bce8fcb8
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:24:04 +0000

    dress effort innocent belief stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
