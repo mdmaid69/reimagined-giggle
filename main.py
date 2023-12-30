6c439a4958530ce8b9f0f70927b020b2f689fc1c
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:09:33 +0000

    advantage share what score worst

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
