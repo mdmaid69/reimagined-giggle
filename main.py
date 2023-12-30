db8b273b746c3cf4d15e56feaa593bc658cecab1
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:07:30 +0000

    stupid well considering company rich

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
 text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import collections
-def create_counter():
-        return collections.Counter()
+print("Reversed:", text[::-1])
