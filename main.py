5e2c9bcdb75d6125ee95b68f7f29ca5f4192acf2
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:37:49 +0000

    season interview instead themselves damage

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def add_numbers(a, b):
-        return a + b
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+print(sum(range(10)))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
