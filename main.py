d865a07373f10e530ffcec9a22081ee7a984da9e
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:38:35 +0000

    breakinto square love taste possession

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-print([x**2 for x in range(10)])
-  def add_numbers(x, y):
-        return x + y
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_speed(distance, time):
+        return distance / time
