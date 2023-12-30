54c4a6df33e98c886d4816d5e97a24967a113144
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:51:26 +0000

    play weak clever raw mind

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+print(json.dumps({"name": "John", "age": 30}))
