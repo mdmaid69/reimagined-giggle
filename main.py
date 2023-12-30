94d729238b1227bfa8f3f88ce1337b32ffbb2be7
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:46:01 +0000

    take wash temper jump muscle

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import collections
-def create_priority_queue():
-        return collections.deque()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
