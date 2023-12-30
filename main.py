ea7a480f14993b4795e24931f9db35b176a487e9
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:41:57 +0000

    temper pollution answer as invite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def find_max(lst):
-        return max(lst)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
