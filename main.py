96f018c1aa9f7fd1a3e62fb5e5eb67cf16336f67
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:30:49 +0000

    material compare sell worth rescue

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sys
-print(sys.version)
+def square_number(x):
+        return x**2
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
