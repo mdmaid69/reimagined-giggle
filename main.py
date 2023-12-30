aa2a063997128cac123c9395bcb5d554e7a01170
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:08:19 +0000

    previous most appear write workupto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
