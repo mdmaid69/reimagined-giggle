3e181bd23590f216e354b19d77529b8c3ff5d5b0
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:22:39 +0000

    house getoff train much true

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def clear_array(array):
-        array *= 0
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
