8c9f265ff92079f8b46eb8fb82a37fbeb039b109
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:00:30 +0000

    maybe Tt far school dull

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-name = "Python"
-print("Hello,", name)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import platform
+def get_os_info():
+        return platform.uname()
