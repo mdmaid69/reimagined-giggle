764310821ae991c4ca9bd5d9c655d7baaeef971d
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:58:10 +0000

    opinion force catch happy herself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
