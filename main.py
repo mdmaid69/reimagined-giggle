7e02b774893f93032fde1edb3c88aa11bb8db66e
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:41:07 +0000

    law happen pause fix live

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
