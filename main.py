81e48fcbb381c350e383f802a82623b4b0c109e5
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:38:06 +0000

    comment getout announce ought describe

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
