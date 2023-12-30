c710a1599a22e4d8f25bdb21983158f7750c4444
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:20:15 +0000

    another bang earth news question

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def find_max(numbers):
+        return max(numbers)
+def calculate_speed(distance, time):
+        return distance / time
