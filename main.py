c9c926b14c72e5036db07efd90de081fdfbb8b64
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:50:10 +0000

    bright tough importance balance Kk

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import os
-print(os.getcwd())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
