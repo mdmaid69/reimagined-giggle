d6ab05c13440adf76bcea254756bf7fbcfb54119
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:44:41 +0000

    ilovedicks cinema display professional loose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
