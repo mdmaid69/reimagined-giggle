14b14275fc2d7572b9ddda08835dc8562f38da8d
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:41:30 +0000

    last fun search previous telephone

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
