e555b18f147f9da0c840fafd20c7b38be0ae1169
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:39:40 +0000

    breakout up his loud scared

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
