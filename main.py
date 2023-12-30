51ca33519f39ddadb8b60db42fcc1fcdbc1c9fbd
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:59:18 +0000

    middle getoff punishment up include

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import tensorflow as tf
+print(tf.__version__)
