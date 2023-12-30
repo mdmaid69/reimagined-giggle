c80fcf5635ec0b4929fcb2a3700d417f3515bb73
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:21:59 +0000

    care abroad advanced reliable what

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
