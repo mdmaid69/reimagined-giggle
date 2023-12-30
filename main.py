439e6617397b573e292b3268d23d20d0884c89e8
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:48:09 +0000

    object rain bye opposite too

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
