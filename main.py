65067708a6dc69c1bdf533c3f1ff563ad6d3ae0a
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:48:22 +0000

    makeinto apparently guard smooth deliberately

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
