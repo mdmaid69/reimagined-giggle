18c14b2d686448e8c5ab4bce79ab52fe87dbc98e
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:09:19 +0000

    block perform notice surprising thankyou

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def count_characters(sentence):
+        return len(sentence)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
