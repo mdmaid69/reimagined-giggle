820c1ae7a5e593e9bbe6c6cda2be319112127b4f
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:12:27 +0000

    table deserve stop off tip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_length(array):
+        return len(array)
