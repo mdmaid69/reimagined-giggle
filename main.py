0b8b387951f87002f0df7e55d64ea5c55e74b967
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:45:31 +0000

    everything mention label commit heat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
