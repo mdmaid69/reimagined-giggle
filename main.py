e24bc373060998d04dc841e3e8ba27b3ad40b1aa
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:30:26 +0000

    show load near every nurse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def find_unique_words(sentence):
+        return set(sentence.split())
