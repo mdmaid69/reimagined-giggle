1d3ac217b88eb95a237cc84af87c715cf67f9258
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:10:25 +0000

    answer bush entrance grow spring

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+print(math.pi)
