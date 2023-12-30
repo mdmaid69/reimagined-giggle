199d84bd9c2c7f03181c1a17fa52642e21a47765
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:04:31 +0000

    squash Ee expensive exist spray

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
