c601e5e55b99f6de975e977d081d4de9d6f80044
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:20:58 +0000

    rather send shall impatient fair

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
