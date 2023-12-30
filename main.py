765f433c0e791145435fea558f3a65f8645ff621
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:44:25 +0000

    read beyond poor pollution brother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
