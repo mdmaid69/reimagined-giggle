41cabca22d35d8289711ed76f8e8c54eddc77ad4
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:16:28 +0000

    division love since average rain

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
