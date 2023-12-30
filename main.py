56a2ecb552eff4835e4b7482fb644dbe5c02cdaa
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:50:24 +0000

    admit interview discussion battery exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
