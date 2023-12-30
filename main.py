1f9c386cfe30d442acc3ff35ac88abf852791650
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:19:22 +0000

    among straight autumn night spoil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  def square_number(x):
-        return x**2
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
