dbfb9a32fbd9673f8476a876b03dd0255b4a6829
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:17:47 +0000

    last cold land confuse speech

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
