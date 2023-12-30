25e5bdb8d38b178d7b4cfc553fe8adbdee08aca3
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:27:24 +0000

    major thinkthrough circle meet fear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def convert_to_octal(n):
+        return oct(n)
