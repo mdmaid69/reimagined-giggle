4d090934b5aace00b2e9d0219f43b3a3c948c72b
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:48:17 +0000

    expensive alone bit market mother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
