e8567d3dccd3d692290672f1e0a4d8d8c6a08a21
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:34:22 +0000

    affect completely save lot force

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
