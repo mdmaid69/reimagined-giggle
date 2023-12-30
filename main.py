f6bc969d53bdf2ca36a0e03ebdfc28d5fc1fa102
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:00:19 +0000

    mind thinkof cause final light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
