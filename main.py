19ff2b256c9934b26280df03dbfbe47b27059fd2
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:39:57 +0000

    measure clever lose final shopping

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-print("Hello, world!")
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
