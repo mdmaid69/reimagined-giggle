78594a24ea4bf1ba1e151248e92423574c2a77c3
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:48:52 +0000

    corner soak unfortunate sister performance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
