65ab904b0fbbc5af180f06db92643a9d137a0598
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:01:55 +0000

    pupil contact table low year

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
