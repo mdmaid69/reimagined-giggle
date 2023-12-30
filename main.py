b66ca2655e4fcc5bd8418d163a0230e6182d1d50
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:41:28 +0000

    main best wrong smoke state

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
