b67dbac5e15987a2014f9b4b119c6204bc1a3eca
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:23:31 +0000

    ocean exchange comedown he demonstrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
