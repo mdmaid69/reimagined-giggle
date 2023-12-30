1f432e7f5dedb3f96fa4d7a9db489ec718f656d0
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:35:48 +0000

    bury precise evidence correction me

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def add_numbers(x, y):
+        return x + y
+def find_union(list1, list2):
+        return set(list1) | set(list2)
