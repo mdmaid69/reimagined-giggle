85bd9971405488736402f078d5fabfb8bae877f0
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:36:05 +0000

    test pride discovery available rain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
