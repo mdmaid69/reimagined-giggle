fc0efe2c2bb1710b44666780672d6efb097cfeab
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:37:11 +0000

    our behind shame confused narrow

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
