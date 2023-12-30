9a558c2a5a7882864eb907faf190555f57b98e96
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:13:15 +0000

    load afterwards giveup tool mood

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
