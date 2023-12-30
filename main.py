4c29034d7bb155a15c48c41c6a310683d28469e3
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:31:01 +0000

    floor brief workout join attention

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def multiply_numbers(x, y):
-        return x * y
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
