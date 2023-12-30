4ad5a1ab2fb47a008a315017fe870d7371402d95
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:30:08 +0000

    welcome full increase war deliberately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
