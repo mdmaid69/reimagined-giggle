5456670907a69afe65380a2ead9df72199e24080
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:16:25 +0000

    thanks Ww split phone half

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
+  def get_file_group(file_name):
         return os.stat(file_name).st_gid
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
