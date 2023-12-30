1851871f046c8dbb7112de2405473e6ecc3e8de5
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:49:40 +0000

    nowhere brother taste row education

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
