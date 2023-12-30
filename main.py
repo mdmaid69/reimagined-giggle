5d687717dfddc4100449fd4b0252502009d9ab11
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:14:50 +0000

    positive independent makeup cause fall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def divide_numbers(x, y):
+        return x / y
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
