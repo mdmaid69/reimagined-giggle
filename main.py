6588239f5c8c929cf69a8587ae1a374adbac0470
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:43:36 +0000

    succeed opponent price lookaround bring

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  def sort_list(lst):
+def sort_list(lst):
         return sorted(lst)
+import math
+def calculate_floor(x):
+        return math.floor(x)
