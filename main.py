20143b45f18f744312dce73a92fd8d701bbdce54
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:55:25 +0000

    arrangement succeed progress gradually design

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_density(mass, volume):
+        return mass / volume
