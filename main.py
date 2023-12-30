a31c6e3908d9cd9893208684d9a49785cdf5e721
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:14:48 +0000

    tidy minute shock shut slightly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
