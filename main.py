b9455600771f8efc45a3c596a57b05fbd3ba6513
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:49:34 +0000

    fail violence unkind coffee tell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-from collections import Counter
-print(Counter("hello world"))
+def get_string_from_array(array):
+        return array.tobytes()
