f0ad3dfa0f8aa3ea297d049c52a7f358d9576cca
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:34:47 +0000

    weigh steal shop prefer near

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
