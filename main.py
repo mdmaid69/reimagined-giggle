34a6d89de52ce9818a840dbd5061531c1ac6a4dc
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:08:45 +0000

    objection turninto ill sell keepoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_power(work, time):
-        return work / time
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_cosine(x):
+        return math.cos(x)
