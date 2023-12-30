f13b813a486d31b422e9a99d55b71008d27dddec
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:23:53 +0000

    amount smell arrival occur song

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def get_array_as_str(array):
-        return str(array)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
