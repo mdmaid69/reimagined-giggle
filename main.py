96ef766de642d42491d2f58a5419c2ee1932e3b0
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:16:49 +0000

    sharp detail this rid switch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
 import array
-def get_array_as_list(array):
-        return list(array)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
