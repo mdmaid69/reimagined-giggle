15f632603a599d7da884c70e2047df1b3748537c
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:09:00 +0000

    want entry determined pause miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def get_array_as_list(array):
-        return list(array)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
