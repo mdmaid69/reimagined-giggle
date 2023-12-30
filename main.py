c69a43fc78fef290a8c059ca271d22c6ebac9555
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:06:14 +0000

    tear fall defeat inform part

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
