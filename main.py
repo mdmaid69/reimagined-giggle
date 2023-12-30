59ded48bad5cf345c511b73b09d3f5701501b061
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:17:45 +0000

    will spring foot trap think

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import random
-print(random.randint(0, 100))
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
