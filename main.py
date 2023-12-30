2046b7dde4908dcd7f000db46d63f0158c80fed4
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:54:11 +0000

    length machine brush hard my

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
