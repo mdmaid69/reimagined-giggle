f5d40a52a8a1a98eeee2cc0d27a5c83b61f37b57
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:25:11 +0000

    public plant straight pin switch

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-print([x**2 for x in range(10)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
