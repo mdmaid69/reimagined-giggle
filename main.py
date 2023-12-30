cc735ea259b288c6f518314b94e7a85a33edd218
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:48:46 +0000

    commit bet would trial agreement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
