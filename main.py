5efebf8c2b776a1a3398001b8996d995cf9b846d
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:58:34 +0000

    hurry autumn account hardly consist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
