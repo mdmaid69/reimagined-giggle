4856128950c847090e810280fb1e02ca9a20b7d8
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:45:38 +0000

    will friend mood onto none

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
