7eea2cf8e29afb4ecfc192f9d9e8230845e943c1
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:49:35 +0000

    hurt true me store door

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
