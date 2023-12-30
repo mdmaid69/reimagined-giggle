79d79abad8632d599a34a2970f2c7286399e8f16
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:49:20 +0000

    culture classroom minimum actual pipe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
