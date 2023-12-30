120e003e86a536bea74e244b5455442184ae6e14
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:39:30 +0000

    permission exactly relax action confuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
