d7f0ac0b9db964cb972c580e6fe77d279bf95cce
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:24:18 +0000

    yours appeal entrance possession comfortable

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
