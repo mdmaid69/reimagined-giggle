da54d45cd4acb146567c323ef207d7fbc14953d9
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:34:17 +0000

    worth shall deaf temperature miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def split_path(path):
+        return os.path.split(path)
