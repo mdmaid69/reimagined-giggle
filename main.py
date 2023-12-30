b58ca4e72f6807ea08471ea6589155e764da2e3d
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:26:37 +0000

    turnout mixture object seat extra

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def count_elements(lst):
-        return len(lst)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
