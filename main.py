1e66a87c6ac2fe18c41632a4725d70cdebd6e499
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:39:57 +0000

    study scrap post spill you

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_sine(x):
+        return math.sin(x)
