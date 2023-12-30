370d7a9a66a19dd3ea738a7a46184272952c0af1
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:56:11 +0000

    influence interview age level remark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_directory_name(path):
+        return os.path.dirname(path)
