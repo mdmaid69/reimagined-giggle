6a8c1e4bbba5bd781e79ad1789e4001e081a934b
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:02:22 +0000

    bank tragedy deserve suckme exercise

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
