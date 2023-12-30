57fe1fd7c8a0989f65cef36628d35fc86769b660
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:26:43 +0000

    defeat mail unfair exchange want

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
