4b792d003af5aad7d9c3a7dcb9ebd66ce254a0ec
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:56:29 +0000

    male everything contact meal right

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+i = 0
+while i < 5:
+        print(i)
+        i += 1
