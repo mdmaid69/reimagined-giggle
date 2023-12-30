6935395058ff49a9dc0b65dc0426fd6775f00403
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:23:16 +0000

    bringforward phone stupid switch unite

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import time
+print(time.time())
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
