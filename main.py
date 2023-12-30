b63915cf7a80c1a71eae7eadf45495e96562bb6a
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:41:23 +0000

    represent bottom mark trap please

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
