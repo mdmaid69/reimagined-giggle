d042aafda58b0af404d53545e54d168999f87a7a
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:26:07 +0000

    why book material judge tell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def square_number(x):
+        return x**2
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
