158b2b746671c4a5c3c90e58c8fd6f64ffe1005d
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:31:54 +0000

    ours lot notice value culture

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+i = 0
+while i < 5:
+        print(i)
+        i += 1
