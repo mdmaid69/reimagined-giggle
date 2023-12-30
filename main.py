7912ea644bbcd80cc7d59cd591c9a3236a93fbe4
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:43:45 +0000

    switch though concerning partner try

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
