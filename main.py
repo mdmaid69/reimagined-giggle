f7d2d81f9f914f2d63f52c4a860e7dda6e0469aa
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:48:24 +0000

    world hit party base judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def count_elements(lst):
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def count_elements(lst):
         return len(lst)
