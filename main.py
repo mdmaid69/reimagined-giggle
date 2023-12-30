2f811963700b63fc70dca394967a98e7bb7e458c
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:02:39 +0000

    who tour question congratulations shall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
