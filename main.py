af98559bb352b3b6de2facd11911c1431d48388f
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:06:27 +0000

    believe account getoff harm how

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
