3776d937be6678290bc6faa312c3c3d4bea37d6c
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:00:22 +0000

    less related march talk communicate

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def add_numbers(x, y):
-        return x + y
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
