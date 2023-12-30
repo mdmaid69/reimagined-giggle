6f3c5da4a8d2ef5bf3853d75f0ac766237321cbe
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:29:35 +0000

    little evidence pause ask lay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
