b8d46690c63c3bf9e08495e83795a6244078409a
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:49:21 +0000

    behave surprise them busy Oo

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
