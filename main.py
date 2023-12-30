12474a97830e57f2227111c62977150ad12709b0
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:58:12 +0000

    sentence shout within business throw

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import sys
+def exit_program():
+        sys.exit()
