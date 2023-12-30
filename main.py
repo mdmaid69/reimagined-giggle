3673e46b353c042cef7ba801d50c3b11a6d476c2
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:32:01 +0000

    how reject waste people respect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
