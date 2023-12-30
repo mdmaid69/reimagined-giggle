36956beb9ab4d3bc79cc17e68706c46ccf2fc7f2
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:35:41 +0000

    neither defend receive possession worst

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_unique_words(sentence):
+        return set(sentence.split())
