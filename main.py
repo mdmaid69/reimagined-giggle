589c0fa2cf8f761937f486f4151301fefbad9b65
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:08:08 +0000

    window holdon food splash careful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
