1267315d1180031d57e71a2b6d178aff52a56dd4
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:53:09 +0000

    hit start near stamp share

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import collections
 def create_priority_queue():
         return collections.deque()
-  def convert_to_binary(n):
-        return bin(n)
