684a63671a8e08106db5944e9a06753d7582631a
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:13:19 +0000

    thing talkoutof subject hate few

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
