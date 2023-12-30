485ebf3e6fc32dd9ff1a04b12e6d8137be20a5da
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:35:09 +0000

    turnoff life joke exist such

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+i = 0
+while i < 5:
+        print(i)
+        i += 1
