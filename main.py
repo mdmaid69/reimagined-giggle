7ae26f34a7b3c71e0c94b0597eb5504de4b9d738
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:16:58 +0000

    comein set willing rent interest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def extend_array(array, iterable):
+        array.extend(iterable)
