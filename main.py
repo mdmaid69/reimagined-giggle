6dbc9fb3fa969c050474b08d7b62343554e5bf21
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:31:45 +0000

    whoever poor correct calmly takeover

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_work(force, distance):
+        return force * distance
+def calculate_speed(distance, time):
+        return distance / time
