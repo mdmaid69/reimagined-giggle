4f1b8f60be18f80d629a3bb329a324d585f56860
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:05:55 +0000

    fairly welcome payment war off

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def get_array_as_list(array):
+        return list(array)
