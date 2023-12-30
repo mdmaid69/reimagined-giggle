9ce05eb12359350eea5409228799e71c0670c9d9
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:51:48 +0000

    description cinema nonsense discovery ever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def calculate_area_rectangle(l, w):
+        return l * w
