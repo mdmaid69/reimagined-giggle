68f028d63a2ae0f8967840b448462689baa295c6
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:34:12 +0000

    harm such at tense complaint

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
