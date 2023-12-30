7c143b4db3f11603e08cef194acf106dd6c0ec74
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:18:00 +0000

    worried talkinto respect news never

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def sort_list(lst):
-        return sorted(lst)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
