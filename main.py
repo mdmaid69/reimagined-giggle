cc7307b2685624834352994fe5081f4783af7bd1
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:58:28 +0000

    right add bottom apply getback

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_work(force, distance):
-        return force * distance
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
