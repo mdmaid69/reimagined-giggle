370c0703c4f77e480f5953f31c3bd00d8a8d09c9
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:51:15 +0000

    plan radio very callout mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
