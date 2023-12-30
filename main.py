049dd0c39a058ffd10269e8f9cf49e4630e7bb78
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:45:57 +0000

    major this force after solve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
