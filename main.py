f87397893b128fc48b8261e6a64a861ac34e6669
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:41:58 +0000

    hot through fair mail before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
