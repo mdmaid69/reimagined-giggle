6a24e465a7c8a281988389a98952516681ef4b5e
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:19:15 +0000

    so prove week week actually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
