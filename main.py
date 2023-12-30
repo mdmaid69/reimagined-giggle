83906d1be413340b8eda517c53f3e777c7a0117c
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:19:47 +0000

    loss behind favourite without twist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  def square_number(x):
+        return x**2
