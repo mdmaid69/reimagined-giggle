ca0454b7d87202a269c3752d2042a11e116bdaff
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:04:51 +0000

    false weekend accidentally determined generous

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
