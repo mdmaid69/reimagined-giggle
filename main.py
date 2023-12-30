c98a624aae6c251ac768b2a49b3335dcf5054fe0
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:32:45 +0000

    birthday rule dinner open provide

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Uppercase:", text.upper())
