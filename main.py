1c66fb7d7da034e06fd6c1d9d50747ae95962009
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:45:00 +0000

    range whom church rarely excuse

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
