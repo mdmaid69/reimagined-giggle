bea7cc22942568f44c2d2228acabfec45a7acdd7
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:36:00 +0000

    score university top drop us

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
