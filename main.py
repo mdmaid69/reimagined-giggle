46e4d143002b68b70394f419062c3f93776ba8b6
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:24:16 +0000

    shut workon experience principle tour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def convert_to_octal(n):
+        return oct(n)
