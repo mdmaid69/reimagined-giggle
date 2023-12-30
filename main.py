4dd2f03c216798006667bb475f72d1916c4263fa
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:28:28 +0000

    allow pain wrong through anything

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import collections
+def create_user_list():
+        return collections.UserList()
