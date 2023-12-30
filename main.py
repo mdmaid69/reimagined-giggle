2ce1f1d694f581ff9892c7e1c903829db0fa63c5
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:13:53 +0000

    lead sell other depend contrast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_power(work, time):
+        return work / time
