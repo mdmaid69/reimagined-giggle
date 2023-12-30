9d22346f5cd7f9f46cc7bd9a466df1144ffd25fb
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:54:54 +0000

    influence past connect connect agree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
