5220c27033e44304c560f3ca3cd40e5954face7e
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:45:51 +0000

    mean apologizeapologise wave blow project

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+for i in range(5):
+        print(i)
