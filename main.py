4da50d1ebd7148c5ffab410ffee6d4193ff5f9e2
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:45:34 +0000

    turnaway time until Uu bored

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def cube_number(x):
+        return x**3
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
