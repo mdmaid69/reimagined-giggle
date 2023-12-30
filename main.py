0009420ebba793ae6be9039126f2a0ff6251f801
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:15:11 +0000

    abroad illness generous tradition along

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
