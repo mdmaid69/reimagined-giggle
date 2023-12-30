22a479ebdaddc51b5b04fc158ae43df4f4cf9aec
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:20:26 +0000

    memory programme room wash move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
