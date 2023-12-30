b36a97739331c6b8318fab51501b12c9878e2276
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:54:01 +0000

    slowly think skirt degree begin

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
