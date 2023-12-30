1f63e11747eb947905da82d897640a44af3b5ff3
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:10:36 +0000

    little pronunciation blow disappear mend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def multiply_numbers(x, y):
-        return x * y
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import array
+def get_array_as_set(array):
+        return set(array)
