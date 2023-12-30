1943bbd38cdb11d7bbae767141bb9a4b556a254a
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:31:13 +0000

    soft record fun shop control

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def sort_list(lst):
+        return sorted(lst)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
