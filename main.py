7a25b740c449389a3e6deb3dd32f37002e11e976
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:01:19 +0000

    cause near light high either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def is_even(n):
+        return n % 2 == 0
