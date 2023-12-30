b2b974dfd6a1c46ec460fe30cca3a417460f18b1
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:47:21 +0000

    chase private away tap find

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-print("Hello, world!")
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
