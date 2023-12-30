8dbc73b1a32321c074c6cfd50ebe30156b2f779d
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:43:42 +0000

    content absolutely comedown takeover waste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def count_words(sentence):
+        return len(sentence.split())
