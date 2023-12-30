87a172004fe85788a5b5bc86b8eeb462ade051ad
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:55:11 +0000

    deliberate home calloff name emphasis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import numpy as np
+print(np.array([1, 2, 3]))
