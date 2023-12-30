60b95e362849f8f5d844d9c70d0bc60974cc8b76
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:40:25 +0000

    traffic calm dry limit Yy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  def calculate_area_rectangle(l, w):
+        return l * w
