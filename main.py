8f7e6fc38aeac3d03d963743d3f04aa28e76377a
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:56:42 +0000

    memory distance autumn definite reserve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def count_words(sentence):
-        return len(sentence.split())
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
