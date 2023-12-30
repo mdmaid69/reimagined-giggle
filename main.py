3ee669fee6fa5e0614de4a69d86280f8fbf686ca
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:21:44 +0000

    score send reply floor building

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
