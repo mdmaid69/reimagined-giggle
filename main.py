4c2c4959f8db137ded5fb1cfc26089faf2a60c27
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:54:50 +0000

    narrow succeed handle begin 571enough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
