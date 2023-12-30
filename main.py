2d5aa59c6a5c9db44fb971ce15dff05504369bf7
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:02:44 +0000

    shake bus last doctor actual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
