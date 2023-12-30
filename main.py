f9d5972eda3ac4e907766c0f65e7aedda45c47f8
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:10:29 +0000

    fall crash by famous pile

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import time
+def get_current_time():
+        return time.ctime()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
