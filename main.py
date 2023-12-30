2d8899a1590a5781e93d1242062ec287b968e6f9
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:27:40 +0000

    official peace because mysterious naturally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def reverse_list(lst):
+        return lst[::-1]
