c4489fb838fbbb99b46258e3c0167d5d04f1a30b
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:00:32 +0000

    shake mail but ride practice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
