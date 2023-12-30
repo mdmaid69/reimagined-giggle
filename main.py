bd49220441b7c048c6144d138772680134504731
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:40:46 +0000

    dream open conduct anybody mouth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
