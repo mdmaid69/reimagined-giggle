e93eac3641554c9255381148432c3da2ea1770bf
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:40:56 +0000

    soft happy area cinema write

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
