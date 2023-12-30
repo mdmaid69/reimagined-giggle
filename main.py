a5cb5066c2cc9c9ffc175f0f62fc864f302f8fc2
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:28:05 +0000

    now shopping throughout heat replace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
