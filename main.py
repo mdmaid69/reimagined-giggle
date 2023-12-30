89f84d4d23c37dd3b23674224bf08d8f01c8d305
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:20:00 +0000

    work bringback successful escape correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_sign(x):
+        return math.copysign(1, x)
