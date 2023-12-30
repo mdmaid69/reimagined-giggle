e596ffe1e70344107ea0c94b357be7abb997bfe5
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:44:40 +0000

    storm give firm whisper shock

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import math
-print(math.pi)
-from collections import Counter
-print(Counter("hello world"))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
