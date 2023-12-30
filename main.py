28b0c88c6c052c788decdcb6b75bc6634d9e4161
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:19:52 +0000

    interview remove book form judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import collections
+def create_queue():
+        return collections.deque()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
