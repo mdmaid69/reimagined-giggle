bd8f9a3ab35c085be393c89ffbf54fa4df639074
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:38:27 +0000

    rub together profit design stupid

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
