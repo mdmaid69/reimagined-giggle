d2346a7b57648e86ebb47a79287fb16f63471c43
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:27:31 +0000

    extraordinary prevent affect waste state

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_sine(x):
+        return math.sin(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
