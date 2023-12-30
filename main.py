5d4fd92fa085b3a05f4e6bf3667d506b2208c5fc
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:22:08 +0000

    fix getoff major backwards company

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import collections
+def create_user_dict():
+        return collections.UserDict()
