81e120948965a057028069d2e47a11832083ddad
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:07:56 +0000

    brother bringup ahead guard start

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+for i in range(10): print(i)
