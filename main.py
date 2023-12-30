9812528c5eab3235ef7f01e714b942f7a7f3f026
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:41:46 +0000

    taste tense basis fail although

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def convert_to_hex(n):
+        return hex(n)
+import array
+def append_to_array(array, item):
+        array.append(item)
