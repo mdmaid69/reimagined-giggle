db1718d0b2f51dd45c64d5788248eb01b0c68fb9
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:08:21 +0000

    room unfair choice run special

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
