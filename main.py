882947b99f32a79cd11e70db3bd6f970b3a7d638
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:40:58 +0000

    radio book partner view naturally

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
