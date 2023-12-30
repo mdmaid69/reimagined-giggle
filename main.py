f3dcb36f1bc062dbcfe0abc2f1c0436589134371
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:26:55 +0000

    upstairs exit ocean connected there

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
