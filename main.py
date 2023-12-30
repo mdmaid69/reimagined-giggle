1f1e8ae0234133a3071ab85c30f03bbfe1b85e91
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:57:43 +0000

    match expression together part emphasize

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import datetime
+print(datetime.datetime.now())
+def add_numbers(x, y):
+        return x + y
