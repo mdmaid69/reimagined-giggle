07f07bd5ad5da21c366686996c2936ea995546fa
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:42:22 +0000

    angry consist kind upset nowhere

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
