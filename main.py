106ba09a57ff5d1ac6be6b5d1a6dae2fa6447af2
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:12:35 +0000

    family roll shape nor normal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
