895b9cd8e703e8d15786b53985f58a242569784d
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:09:15 +0000

    frightened refuse tool OKorokay find

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
