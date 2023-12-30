4095ae15ec9aa8297ad0fabd5d3af0e4ec21da2c
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:14:37 +0000

    turndown action tip attempt often

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
