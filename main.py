27287e1e40ba15c4c4aae2ebe1816259bf3f7961
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:56:59 +0000

    want often notice kind care

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
