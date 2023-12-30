6fbdd1fdb9dac733e34f608d44cadb586ef361f2
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:44:25 +0000

    tip need exchange march necessary

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_power(work, time):
-        return work / time
+def find_max(lst):
+        return max(lst)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
