6de06ba54b028595aefd58bbf1bb8bcc8501f058
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:20:55 +0000

    cinema against breakinto inside almost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_array_as_list(array):
-        return list(array)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
