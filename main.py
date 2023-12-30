78deb11099a1e6584071efbead23667087f75c8e
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:23:37 +0000

    long much thirsty sense dress

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
