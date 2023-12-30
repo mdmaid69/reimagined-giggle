947f283004bfdda1c877d0f3bf81f7831e24db9c
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:09:21 +0000

    apart command close fail success

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def find_max(numbers):
-        return max(numbers)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
