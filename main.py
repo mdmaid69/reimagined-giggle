168068c8947128e3ed843f0f006eebfa746b38eb
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:41:30 +0000

    people contact attractive raw trade

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import collections
+def create_queue():
+        return collections.deque()
