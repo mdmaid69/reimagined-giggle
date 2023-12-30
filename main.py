9181ea31acc644457aea8da2d327833e966fa3a5
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:44:56 +0000

    area cheek after one look

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import sys
+def exit_program():
+        sys.exit()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
