0a0e5ebb8c73ce314d0eeaa54e90ec5893c5e958
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:44:50 +0000

    pool worse remove possibly chief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
