6babb0d1d2c672f7ce2b593eabf47c0e3a93b555
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:48:05 +0000

    breakinto past prevent firm lie

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-x = 10
-y = 20
-print("Sum:", x + y)
+def calculate_volume(length, width, height):
+        return length * width * height
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
