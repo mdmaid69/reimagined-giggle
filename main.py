72c129a3f24892b4176e78a222490a6b0ad24e95
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:09:45 +0000

    mind snow waste sort position

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_itemsize(array):
+        return array.itemsize
