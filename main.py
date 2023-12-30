e0bf6661dbf2fb7523a71fdaaa91af1d30864d05
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:34:22 +0000

    afterwards mind recover friend appear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
