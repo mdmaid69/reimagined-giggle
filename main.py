9696f75cf8432d9fd38fb4fc96f9e41a9ab28ea8
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:14:24 +0000

    drop ours Nn behind tape

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
