e60f3f926489f22c136ecdc64c813cdb1556e27d
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:19:37 +0000

    duty secret school travel shop

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
