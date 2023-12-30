126c8078ce70cae27941624fa06b884d5c2d4adf
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:00:26 +0000

    home rely past add whichever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import array
+def get_array_as_float(array):
+        return float(array[0])
