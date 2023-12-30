69c3003574454ad6bca8708fa55db0b6c54c7a39
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:57:04 +0000

    impossible under last steady fear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def add_numbers(x, y):
+        return x + y
