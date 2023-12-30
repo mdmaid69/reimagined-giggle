e9b727654259f4a77d3d7355b5babef79e6a9233
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:49:11 +0000

    beneath example sorry side quarter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
