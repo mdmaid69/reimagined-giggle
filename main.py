07e2d943a401f478a782948cb39695c0dfe35678
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:55:34 +0000

    glass exam downstairs form cut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
