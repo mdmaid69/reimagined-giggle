88f233c48cdd9394ecad93185f7ffeeec77035bf
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:48:25 +0000

    scared dig mixture finger remove

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  def calculate_area_rectangle(l, w):
+        return l * w
