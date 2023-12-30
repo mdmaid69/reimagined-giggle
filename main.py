d581ab2ac805928bfc2f384706eef8eac95b0682
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:08:20 +0000

    mistake rest comfortable pleasure trade

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def convert_to_hex(n):
-        return hex(n)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
