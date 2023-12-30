c89ff5d4df4353933ecab4b8490dc50ac8527282
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:09:14 +0000

    smart study against drop exhibition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import sys
+def exit_program():
+        sys.exit()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
