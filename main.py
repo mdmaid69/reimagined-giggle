6b3667cd8494ed98fe788ee88e9e944a4347d730
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:44:26 +0000

    only sort proof object transparent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
