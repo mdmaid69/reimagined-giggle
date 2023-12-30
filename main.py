4200467449a743d1c1e7ede61483134b83a5385c
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:39:03 +0000

    please always reasonable gift pollution

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
