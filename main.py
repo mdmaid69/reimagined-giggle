ff2322b21091b60554cf87beb62585373db9c1b7
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:55:06 +0000

    we glass immediately piece visit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import sys
+print(sys.version)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
