5734b53b02023abe0e1786152ff541e6648c8702
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:28:05 +0000

    book job picture pour getup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def set_array_item(array, i, item):
+        array[i] = item
