8ed1cf2f8043c2fde16b75d600651120bba51d22
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:16:58 +0000

    giveback pocket breakup congratulate separate

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+x = 10
+y = 20
+print("Sum:", x + y)
