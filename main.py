ad3759aed3ab0ed9cd1e3bb82e7f25187f5b277a
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:39:53 +0000

    sport much slightly progress risk

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-print(random.randint(0, 100))
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
