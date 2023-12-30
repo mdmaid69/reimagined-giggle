9cf1446a99677c8b1fd52dab460b057b36f0176e
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:24:01 +0000

    collect introduce present target substance

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
