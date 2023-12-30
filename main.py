9eae87fa9bfe905fd2284079f6ba135a44686833
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:25:22 +0000

    influence before destroy study roof

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def count_characters(sentence):
-        return len(sentence)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
