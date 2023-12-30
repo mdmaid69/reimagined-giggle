3c274260d7dd06c6901d2c018a3e9476f0061be8
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:02:39 +0000

    though team slice Itself depend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_speed(distance, time):
+        return distance / time
