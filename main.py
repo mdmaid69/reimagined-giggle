6ab72913feea90bc1e57b4059fde99426c41750d
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:53:57 +0000

    keepout description slice term owe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
