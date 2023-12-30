3bcf7e185f89e47d23308a2888e9a8ce36e09163
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:07:12 +0000

    study protect organization to upset

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def square_number(x):
-        return x**2
+import array
+def get_array_item(array, i):
+        return array[i]
+import collections
+def create_priority_queue():
+        return collections.deque()
