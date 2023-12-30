719f000a4d37527553a03daceb8dbc5418c5bc39
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:19:45 +0000

    step completely slow official main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
