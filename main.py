4214e4da9c6b9ed7e79d63363dc758dcd14a1fe0
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:49:51 +0000

    reduce usually related mysterious trick

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
