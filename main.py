95ae89667dc28a62a77427955298a98c41ddeb42
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:48:42 +0000

    address eat post fall lead

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
