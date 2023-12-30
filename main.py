76aef72ddfa9f8f24fed34af8782c1e730cfaaf4
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:12:18 +0000

    police turnover thought sing argue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
