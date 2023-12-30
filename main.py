e146677b5cc64934d6e76c91ecdd722089b6fc0e
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:29:36 +0000

    statement error intend mistake nerve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
