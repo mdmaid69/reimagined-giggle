419f5117d41191b595bf3a8f55fcca5ef43e3627
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:10:02 +0000

    shine yes ourselves name pot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
