0252f2d0947aa236cecda8d95d3d93c268d69ffd
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:01:24 +0000

    impressed experience ring stiff secret

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_max(lst):
-        return max(lst)
-def add_numbers(a, b):
-        return a + b
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
