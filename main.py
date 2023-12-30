d3c2ec724f5c8a17d93300937226d2fab5979854
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:25:57 +0000

    climb concern stay row shape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
