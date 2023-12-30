cb64ceed042dc0a0bcd6fd7a7c89a98397c40480
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:14:37 +0000

    mark bin badly send proud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_itemsize(array):
+        return array.itemsize
