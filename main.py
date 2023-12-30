34d3be48404dd488d33efe662394ed2bde35ee7a
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:26:32 +0000

    towards obtain drive arrangement turn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
