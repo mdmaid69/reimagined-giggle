285258464fc36225a5733fc885e9d9c43fe008c4
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:32:50 +0000

    between ring likely change trade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def find_max(numbers):
+        return max(numbers)
