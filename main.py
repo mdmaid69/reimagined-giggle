867e9d11f44c187a64ebaa71c557a1cbd5298aff
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:47:25 +0000

    tune emotion call pity female

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_power(work, time):
+        return work / time
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
