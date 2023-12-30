f6ca78f6cb382e013c05932d7ca7b9caffb3ffe6
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:39:53 +0000

    perhaps compare hot vanish standard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
