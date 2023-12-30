1e355aa19e3b725402b7586a34726565a53e4455
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:03:29 +0000

    scared takeback speed once break

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
