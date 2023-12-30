ec1f8223670651ab5aa386d8e8edb10bf175bef5
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:35:33 +0000

    lot make recognize if yesterday

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def sort_list(lst):
-        return sorted(lst)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
