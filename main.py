e4c5482cd8027e6cbede02799ecf27325ec3f755
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:04:50 +0000

    dear quantity show last follow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def get_array_item(array, i):
-        return array[i]
+import tensorflow as tf
+print(tf.__version__)
+import time
+print(time.time())
