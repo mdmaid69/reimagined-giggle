76e5a759f98328f18050e10ce6bf4e6657aa169e
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:11:19 +0000

    strong damage getback private on

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
