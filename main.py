1b6925155d372c337b5e2ab654d7d96575455bd8
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:02:51 +0000

    stone system cheat prize keepout

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def remove_from_array(array, item):
+        array.remove(item)
