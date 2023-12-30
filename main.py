2582f95c78df146a4bc86899fbadda19366f2ab1
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:41:10 +0000

    wherever continue circumstance mainly Bb

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
