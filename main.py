512a7d0bfdcbe6993832c8d0efcaa8ecfebb429c
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:38:00 +0000

    love trap shout unpleasant tread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
