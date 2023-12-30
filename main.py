70dcaed4f1a7efcc0cc2aa5a2192ef156d5bc55d
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:30:33 +0000

    flavour violence compare check strike

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def get_list_from_array(array):
+        return array.tolist()
