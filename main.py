d60f1618be0b7bbbe22c54649a1427caf4648126
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:06:37 +0000

    storm influence active dress along

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
