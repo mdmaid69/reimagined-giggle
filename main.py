06fb30bbeb3074b5a4c8ceefca3cec57caf347ac
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:26:06 +0000

    contain medium difficulty single high

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def split_path(path):
+        return os.path.split(path)
