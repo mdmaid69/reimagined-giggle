b5ebc734d6ef30009c7fe69c5999fc2a453ca290
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:50:42 +0000

    close type mistake along later

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
