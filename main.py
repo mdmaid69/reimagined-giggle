85ae328f31352aa1d64ab099f7b78fee4d310d8f
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:17:38 +0000

    smooth attract not gas very

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def get_array_length(array):
+        return len(array)
