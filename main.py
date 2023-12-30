eda0d0974ee66fc70856fc3bc66a4f20da8a623a
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:14:54 +0000

    myself compare test independent narrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
