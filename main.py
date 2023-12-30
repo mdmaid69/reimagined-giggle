a9fc9ed57fa6e4a13bf7bb7c3b4528c9181f2e30
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:55:09 +0000

    meaning fond suit priority everyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def reverse_list(lst):
-        return lst[::-1]
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
