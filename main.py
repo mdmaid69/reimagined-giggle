126d30e59340ec465fcee1317ab97d1eaf6646e5
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:25:55 +0000

    breakout honest room hardly set

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
