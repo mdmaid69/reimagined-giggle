4e407cba1ea6c2b44b4ea83fd58b747e4f3521e3
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:25:23 +0000

    yet through pair runinto exit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
