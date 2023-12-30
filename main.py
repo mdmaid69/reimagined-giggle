978741a5cc7b33f8650716d9c6e41e8763620b25
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:17:23 +0000

    wind fix deliberate shop one

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
