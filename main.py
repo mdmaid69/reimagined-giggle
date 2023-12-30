81d291ecaa428bad9e75fcff7e1414ea9ef01776
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:44:06 +0000

    comeback control order charge tour

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def square_number(x):
+        return x**2
