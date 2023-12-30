bb473e179ea530027298af2d0df8276728d7bc20
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:56:25 +0000

    drop pick noise reject Hh

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
