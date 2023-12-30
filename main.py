eaab176839b0a7938c818fd74abd074930fa82ea
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:51:35 +0000

    night no tax permission appeal

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
