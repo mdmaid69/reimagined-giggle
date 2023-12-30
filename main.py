1bc46e12b1bb33d8187add21baefe97198693047
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:28:28 +0000

    centimetre fast where sight exchange

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
