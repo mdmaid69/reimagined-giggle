5f90013326b0bb6e599da77d7a0916aca06d0b49
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:06:35 +0000

    ago short life dangerous stick

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
