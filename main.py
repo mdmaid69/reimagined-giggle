35c960c4f4261826ea9e5f79d7a31142f9b4a6d9
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:36:39 +0000

    satisfactory open modern level sky

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import datetime
+print(datetime.datetime.now())
