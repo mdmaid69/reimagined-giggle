09f2886299efd80d149ed441ece0be77c319ab60
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:06:39 +0000

    basically height stretch team lesson

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
