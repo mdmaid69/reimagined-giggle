f997d66671f7036d36895300fa6718b1ce0c8092
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:58:49 +0000

    thanks ourselves advice house improvement

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+print(sum(range(10)))
