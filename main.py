0d17f57e0160b056e209777f0e4ac8fad2a28d1f
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:12:05 +0000

    favourite considering place guess practical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import platform
+def get_python_version():
+        return platform.python_version()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
