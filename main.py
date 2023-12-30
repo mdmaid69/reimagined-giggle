78b20347388aa4dac9962db9cc03b68495ba2ccd
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:22:36 +0000

    extra freedom separate surprise uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
