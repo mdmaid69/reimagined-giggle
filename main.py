4f4c5a970364495c1af500b1180c16bee7465464
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:57:26 +0000

    stand lot sweep beneath exciting

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-import math
-print(math.pi)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
