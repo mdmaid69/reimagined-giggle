2e6da4aaec90773bdd8d7ee5ffe3a7816a47524b
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:11:23 +0000

    shut out argue callat clear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
