3afb87f6beb49386ccef11fb8bfe23dcc3b2bbd1
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:17:06 +0000

    fire bridge century his basic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
