2297f81003cccf45d0da82d95ceca052803eb7e9
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:29:33 +0000

    key pick calm test destroy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
