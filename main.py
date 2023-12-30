4ed18548eba8190ba85148f23147ba2351c6031a
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:06:19 +0000

    replace own must runaway shout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
