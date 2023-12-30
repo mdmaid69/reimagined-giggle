408d219385250f07ca1cfde94e259df36a708133
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:41:02 +0000

    accommodation power record rise gate

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
