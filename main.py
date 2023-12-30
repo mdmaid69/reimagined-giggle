1597d2891e3df71b416e0064d2931e88447565dc
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:27:32 +0000

    noone undo scream squeeze sure

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-def calculate_work(force, distance):
-        return force * distance
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+print("Hello, world!")
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
