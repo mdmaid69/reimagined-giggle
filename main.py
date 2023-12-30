04d30dc72beaf01e883347d2ad892031e265a6e4
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:09:04 +0000

    recovery thing unit goup information

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
