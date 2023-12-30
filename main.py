e6ddfa7a86be39d8c3d5249e437cf8c60355d1de
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:29:41 +0000

    careful break entry mad arrange

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
