1e1fb74f0f37a3c6709a7f518bfc2c85f2b32ceb
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:13:18 +0000

    sing basic fall communicate exam

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def set_array_item(array, i, item):
+        array[i] = item
