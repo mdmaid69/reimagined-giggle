d5057cd76b51ea3c268e65693facc76e6e4d52a6
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:11:56 +0000

    note visit bread likely use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
