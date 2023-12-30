4c5171cba81ac6feb69937908b6ac54b386cc7dc
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:27:49 +0000

    difficult job over urgent burn

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import random
+def roll_die():
+        return random.randint(1, 6)
