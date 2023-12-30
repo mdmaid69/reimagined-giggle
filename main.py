ca27f8ec2de20355748594f70fa559841f64523b
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:25:13 +0000

    learn tonight already know date

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
