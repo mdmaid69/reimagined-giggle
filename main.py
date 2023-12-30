ef59a3e2cef16fbd6b541348b80a0da8bd1b5ca4
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:41:51 +0000

    trousers lesson plant get busy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
