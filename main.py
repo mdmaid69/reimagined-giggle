a27e2eb456345bcb74d2f62ff93733a9561f2365
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:38:53 +0000

    feeling keep age clean every

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
