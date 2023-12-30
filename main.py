f0e937490bd8d65562a7b7747e9a080b4385a65c
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:26:28 +0000

    support trip prevent long compare

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def split_path(path):
+        return os.path.split(path)
 import collections
-def create_queue():
-        return collections.deque()
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
