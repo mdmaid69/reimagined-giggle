009c6b18de3684301d5cd24b9894718a99f58a0a
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:54:17 +0000

    interruption keepfrom deep crisis female

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
