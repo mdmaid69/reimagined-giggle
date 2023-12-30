2604be1e3ee2f4b7d25ef4f5f9390c3314fec3a0
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:56:48 +0000

    spray march belief circle objection

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+print(sum(range(10)))
