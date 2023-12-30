4151666efe6f75cfe4e19257d003df0e96823762
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:40:05 +0000

    concentration sure otherwise conduct shadow

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
