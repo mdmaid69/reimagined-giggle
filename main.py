cadfa873a820ddb5656343e4c0497f2c68d25410
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:43:35 +0000

    nonsense murder battery explanation declare

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
