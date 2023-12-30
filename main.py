22c9b8e3deaf969d35101e47188af27bd44a30a8
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:57:01 +0000

    paper object advise mine sense

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import re
+print(re.match("h.*o", "hello world"))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
