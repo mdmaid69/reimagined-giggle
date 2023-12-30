ff8a3074e4fbf1caf678dd0dfbdc876e60a97285
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:27:59 +0000

    arrival moral partner send wonder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def calculate_work(force, distance):
+        return force * distance
