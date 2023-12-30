4367a4b225736014b16bd219993085747ca3351a
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:21:54 +0000

    kill seat direction afraid regular

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+for i in range(10): print(i)
