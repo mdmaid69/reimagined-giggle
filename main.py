19c0999e6f5bdaa7a5ba95bb19c21c4a4f8ceab6
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:07:35 +0000

    like sort powerful hot body

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
