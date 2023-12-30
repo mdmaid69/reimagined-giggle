673c80fae595abaad94e39e9d8a1b0e2ea06e957
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:05:36 +0000

    anywhere shade stone past pain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def find_min(numbers):
+        return min(numbers)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
