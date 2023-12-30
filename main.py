c70bcf3e3c3a02f81544852193f3a3322abb9f5a
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:38:26 +0000

    basically record dress regular autumn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
