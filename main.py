41cd999f35429b02804dae64b5d3044b533431b1
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:04:24 +0000

    vanish giveout back physical hear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def convert_to_binary(n):
+        return bin(n)
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+print(math.pi)
