39780c753c4a243b1565b5d71e0375fa044a488f
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:00:09 +0000

    discuss finish practise approval deliberately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
