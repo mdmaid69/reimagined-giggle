94abcd2663fdfe6ef9d38f3d2a3172df8db89ee9
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:09:09 +0000

    anxious intend boring centimetre thorough

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def square_number(x):
+        return x**2
+for i in range(10): print(i)
