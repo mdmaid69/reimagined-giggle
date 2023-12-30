1439bdb9fd29df4de013dab5bc9ca7a9bd7f3f9a
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:14:27 +0000

    immediately agreement pleasant ashamed contrast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_density(mass, volume):
+        return mass / volume
