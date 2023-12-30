02eb4d0f64359cd41195a7425aea08e426a72637
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:21:07 +0000

    this meal drawing mark building

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def reverse_list(lst):
+        return lst[::-1]
