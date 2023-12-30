bd4687ecb3f79cf7026059e62cba53c4bb0dc012
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:06:27 +0000

    exactly less shall wheel responsibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_factorial(n):
+        return math.factorial(n)
