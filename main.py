7c84b583fdb475b9a4ab8903f6f1f90e27a50c23
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:57:49 +0000

    more prove recognize right field

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
