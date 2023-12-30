17a31709825639f8e13bb027208ba77af6c9a455
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:05:04 +0000

    Tt insist create interview press

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def subtract_numbers(x, y):
+        return x - y
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
