87b319b24196826f3db072b0e4a8206ca584597f
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:14:14 +0000

    month die smell machine may

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import tensorflow as tf
+print(tf.__version__)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
