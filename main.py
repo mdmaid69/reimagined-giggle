343bfb4fb4ab4fbb1948100ec1b82595496304eb
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:02:54 +0000

    getaway born exact most goout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_counter():
-        return collections.Counter()
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
