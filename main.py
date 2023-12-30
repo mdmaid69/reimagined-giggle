3c4cc96c34be8922f82c7442404d2d799d7814bb
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:11:44 +0000

    activity Kk progress pretty night

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def get_string_from_array(array):
+        return array.tobytes()
