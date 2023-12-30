b391198c7400ebcf664932677d2c476786cf5ad8
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:24:04 +0000

    nature practical worth so wave

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-print([x**2 for x in range(10)])
+def sort_numbers(numbers):
+        return sorted(numbers)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
