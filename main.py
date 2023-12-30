2eb6cc7aecb8939ac866063fbc51100eed8ec519
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:20:07 +0000

    scrape situation stupid note then

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
 import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
