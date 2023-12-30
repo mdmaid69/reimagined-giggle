053cef62833be94bd9d36a11ed801f80e6275c85
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:26:33 +0000

    neat union part train tape

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def cube_number(x):
+        return x**3
+def convert_to_hex(n):
+        return hex(n)
