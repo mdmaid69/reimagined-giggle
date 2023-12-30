79a0e1724356be0b22ff3ce1189dfe1330558604
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:45:06 +0000

    classroom put choose crazy inside

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
