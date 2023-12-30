51c62cf1d21416bc6c660810782aafb884bd01bb
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:10:03 +0000

    block seem main stupid off

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_speed(distance, time):
+        return distance / time
+def is_palindrome(s):
+        return s == s[::-1]
