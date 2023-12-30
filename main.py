ce73e8fe434877a7b8d591ecc69c6f3d0508e386
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:30:03 +0000

    touch summer spite earn prove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import collections
+def create_user_string():
+        return collections.UserString()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
