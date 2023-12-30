35069ca56a6919df750a7d211be433d5417d2e1f
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:33:05 +0000

    accidentally whoever relationship emphasize freeze

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def convert_array_to_string(array):
+        return array.tostring()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
