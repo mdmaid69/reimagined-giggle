c014e2032a88be5a1a9bccf0fb7b87c0b5eb4bd6
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:04:13 +0000

    rest he report worth below

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-  def cube_number(x):
-        return x**3
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+print("Hello, world!")
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
