0cf93c887a1b275588e428e6ac07e4f410813af6
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:26:32 +0000

    almost through way hand transparent

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
