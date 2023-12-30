c9532df823a2769456895f6865fc5d6585f6c2b1
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:21:23 +0000

    Ee advantage city past good

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_string_from_array(array):
+        return array.tobytes()
