263476ec7b49d23c26fa7c3bb8eb3373b9d1cead
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:25:48 +0000

    closed characteristic sit complicated whoever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
