56d86ce1b2c0255054f1b4df8d1593d76dca55a6
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:39:27 +0000

    intend comment feeling uncertain suppose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
