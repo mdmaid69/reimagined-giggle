3d3c205c16c4eebcc41059b23370a23cfc88b380
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:46:39 +0000

    old table happen Ll shop

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
