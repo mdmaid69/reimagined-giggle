a950aff86f22ddbde14dce9406d228ef2398f714
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:26:20 +0000

    cruelty reach fit phone belong

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
