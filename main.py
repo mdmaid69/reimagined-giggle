5f7a567f0c065449001387e9ae4395ece9800cf4
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:16:45 +0000

    disturb protect worry willing muscle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
