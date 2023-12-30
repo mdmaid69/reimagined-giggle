f5bcacc942337ee8d27905a7cd8e9f28aa39f9cc
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:50:03 +0000

    invite attach broad force floor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_current_working_directory():
+        return os.getcwd()
