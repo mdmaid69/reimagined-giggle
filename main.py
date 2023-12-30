6790f83b883fb788f9787a12d608057ea7aa3511
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:40:16 +0000

    project instead across right cool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
