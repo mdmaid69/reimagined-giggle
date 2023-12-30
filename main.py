a94e0aa115d4cafa97213e28ca0624e1bc114db9
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:09:03 +0000

    wing unite soft answer start

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
