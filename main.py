90a5e6ff109a56fc30c11167e32898de03547b91
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:22:46 +0000

    reasonable mix object pretty promise

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def multiply_numbers(x, y):
+        return x * y
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
