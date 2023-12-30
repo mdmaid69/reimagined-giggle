e564834efcb276627443f881b4fb64993a1971c9
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:45:46 +0000

    recovery difficult design tragedy doctor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  def reverse_list(lst):
+        return lst[::-1]
