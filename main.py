5eb48b68d294b4feb56756eea57c886ff0a7744f
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:05:12 +0000

    bottom thing bill trial rid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
