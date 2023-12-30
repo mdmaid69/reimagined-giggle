5d7866b70faf8bbe03a72bc631bea5ee7d325161
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:11:17 +0000

    tape dream off skill especially

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import platform
+def get_os_info():
+        return platform.uname()
+print([x**2 for x in range(10)])
