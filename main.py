5a300fa940a486c4bc8679e2af0497f1ceb6d317
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:13:41 +0000

    admire scrap usually truth raise

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
