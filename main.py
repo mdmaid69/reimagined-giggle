82cb0887bbecefa9b28cadabab6c4886c89fa37f
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:23:12 +0000

    notice conversation arrange delicate sight

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
