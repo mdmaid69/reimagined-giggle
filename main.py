7d778e8d44e0e722371ca2819cf56291cf362985
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:44:18 +0000

    spite moon tape memory effort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
