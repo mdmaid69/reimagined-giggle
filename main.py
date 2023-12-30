9d4d3c0d7590517ba0c8407d062de6e09df8f476
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:37:37 +0000

    confidence congratulations letter crisis recovery

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
