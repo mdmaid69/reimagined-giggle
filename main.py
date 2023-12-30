359c4c3081ed8488868e4cb6729716dd89d82681
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:32:35 +0000

    entrance couple before dull his

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
