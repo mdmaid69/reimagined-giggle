962f02dd213ad2cee70789754c9df357b2325bdb
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:20:18 +0000

    muscle callout usually entrance attract

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
