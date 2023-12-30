40c70d46f082fdcadbc8cdf410760205759418a8
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:21:51 +0000

    fairly aware determined public else

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def cube_number(x):
-        return x**3
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
