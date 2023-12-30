90f7e23721cf58ac21a64ff0c125470102a4a973
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:35:37 +0000

    separate add usually raw no

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def count_elements(lst):
-        return len(lst)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
