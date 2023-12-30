407c318e90d51c03ba2572589de0c0c13f8437c0
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:51:34 +0000

    month sentence match load shut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_acceleration(speed, time):
+        return speed / time
