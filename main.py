dae9771ca7ee1942c4b001121eec6de9e9dcecb5
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:12:34 +0000

    opposite warn prepare match exciting

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-print("Hello, world!")
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
