2a9b98c9b92e659972c24282b4aad1e171610019
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:41:32 +0000

    lock wrong joke entrance hair

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
