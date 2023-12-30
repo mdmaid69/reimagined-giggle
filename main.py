54821a4da657b608797fe8b10d3750d972737fe0
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:08:06 +0000

    underneath section ashamed grand exciting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+for i in range(5):
+        print(i)
