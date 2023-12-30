230ca62a7c72d1ffb1754edcdb25f98806f5f20e
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:01:00 +0000

    spend snow fence explanation lead

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
