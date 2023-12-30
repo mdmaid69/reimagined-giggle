a9ca280861860d39db3d046b91a8f683aafe3e5c
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:38:22 +0000

    spare thin shoot release direct

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
