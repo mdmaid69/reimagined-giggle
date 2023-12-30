828f2e433615b7508f129f93755c3aa92bb20bd7
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:15:11 +0000

    shopping protection wide market Nn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
