ab6606d5f643f4e88185fd8ad12f47884d2c56e2
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:07:38 +0000

    getaway cut majority heart road

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
