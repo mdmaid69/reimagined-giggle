271aeab12487b913797d97ac141ca281fb2517fb
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:11:14 +0000

    hope live any tip neither

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def remove_from_array(array, item):
+        array.remove(item)
