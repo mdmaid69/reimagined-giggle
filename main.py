344aeed378d6c4f77ea5eafa1848a71eda83ea3a
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:36:32 +0000

    simple path plant reply mean

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
