2132103fdcdedf19b7688d32f24c8d70b68b5862
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:45:10 +0000

    attitude later boring so whistle

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import sys
+def print_python_version():
+        print(sys.version)
