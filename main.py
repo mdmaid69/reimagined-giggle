a14e4414362df9a6926423e6f01cd2f6fde48227
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:23:54 +0000

    basic flow back doubt passage

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
