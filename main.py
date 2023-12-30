fe7e23d171e7589925cf1be435cfb7f94aac1d37
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:33:18 +0000

    shout official pin declaration lead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
