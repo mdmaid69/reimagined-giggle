1d6ce879483b6b537cecb1ade9a32b5a08f7cde8
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:37:23 +0000

    basic set breakup open basically

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
