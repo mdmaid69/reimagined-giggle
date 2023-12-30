3b550f862ef69fe4b159196f07a9cc8f58267dbd
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:38:34 +0000

    depend copy produce image exact

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
