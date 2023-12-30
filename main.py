6ba728193fc321e187bdf30a65a3e6e03b406d52
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:26:46 +0000

    style opponent copy behaviour national

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
