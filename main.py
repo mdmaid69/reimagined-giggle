24b037347292b5501917bc01a7bea715bf28a02d
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:50:59 +0000

    traffic goup nose declaration else

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
