66bf25b3621f175d2d809fdcd9486eb86e35322b
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:14:23 +0000

    natural sea rarely left calm

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+for i in range(10): print(i)
