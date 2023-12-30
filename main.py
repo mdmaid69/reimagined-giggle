ac5649fbcacede2c89f41d1ba208fbca1b9902ff
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:35:54 +0000

    careful near typical so worse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
