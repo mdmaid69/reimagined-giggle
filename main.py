47f23faae1be8461e6b5b0ed764f528c1b7304e7
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:08:09 +0000

    talkoutof arrival turnaway loan worth

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
