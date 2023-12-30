1951ea62fa8f13086f25e3e8d84bf5a8e77dfdf2
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:51:00 +0000

    street write warning gap Zz

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
