93584e5938aa3779a88013e80dfdbe0ea2f63d92
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:33:35 +0000

    boil through degree meet Ff

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
