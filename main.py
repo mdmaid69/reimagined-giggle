d5c577462680ed0a448a3a0c8e41034f0d15ef0b
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:02:16 +0000

    expect flow balance no wet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
