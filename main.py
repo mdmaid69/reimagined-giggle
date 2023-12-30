0c39b9d8f4f3f8b518983690593211f9a1c27dc3
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:09:06 +0000

    draw fill plan squeeze give

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
