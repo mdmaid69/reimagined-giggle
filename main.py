9c63d7a0e0c649a8527a71c8117efaf3eeb38bd6
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:37:58 +0000

    shop answer pleased tape move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
