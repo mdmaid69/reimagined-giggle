c8a6b0f6a939388e70f8eafdc009d62429c9fd8f
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:27:40 +0000

    official after against hour silent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
