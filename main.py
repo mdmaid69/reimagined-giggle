ccaf12f9aae56fc2b023a329faf94a3288eaf9ee
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:48:27 +0000

    spend the so shoot better

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
