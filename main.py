47815487c96fc5f182c9a262bd642c53468b7cc8
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:44:40 +0000

    push frequent collect position raise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
