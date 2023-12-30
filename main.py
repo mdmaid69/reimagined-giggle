331375e4b77318cb501d0358f3320f2fa383fe2b
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:39:00 +0000

    accept during suggestion means reply

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
