385b8ceeb3a5a97ee0fbaf7383636a72cd408461
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:31:12 +0000

    play behind vote depend takeaway

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-for i in range(10): print(i)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
