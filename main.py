fcb5cbc5565ee2a361d5eb6521bcd5b7c1e920b3
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:07:40 +0000

    keen hang central serve search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_work(force, distance):
+        return force * distance
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
