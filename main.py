fedf7a6fe443406bcc053246f1524507a420b339
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:49:50 +0000

    popular interview temperature choice exactly

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def cube_number(x):
+        return x**3
