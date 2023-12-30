7291bcbdf58d9a1af0ca9293f56d2e062ce9b987
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:48:24 +0000

    why class waste along march

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
