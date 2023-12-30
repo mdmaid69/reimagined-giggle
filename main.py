c6ef1dab351dc4819620b35cfd98931ed5ba96e8
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:49:36 +0000

    prevent swim practice copy best

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-def divide_numbers(x, y):
-        return x / y
+def convert_to_binary(n):
+        return bin(n)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
