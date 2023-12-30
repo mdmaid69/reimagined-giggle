81419d3c5381041f71f5a575d75f40983bef7763
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:34:23 +0000

    exactly comefrom usually please blame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
