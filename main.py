470103560e130f1b23fbe172da7a7efec66e3501
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:33:06 +0000

    further future search sad blue

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
