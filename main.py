2ed5fcef9ff20f66993b0cd43873e9105b32e07d
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:25:43 +0000

    oil interview possibly leave pleasant

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
