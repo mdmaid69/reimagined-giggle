62adf84b2a7a105daf25b7d47ffe850b4b415da1
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:06:58 +0000

    cinema performance group neither bored

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def add_numbers(a, b):
-        return a + b
+  def is_odd(n):
+        return n % 2 != 0
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
