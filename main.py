05b9a738a8979fd0fb490cae91ec5f589a7e0305
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:38:40 +0000

    fall goaway nose lose track

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
