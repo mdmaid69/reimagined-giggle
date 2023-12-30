acade9eefe9996d0e1fce43a0263014f3571bb40
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:06:15 +0000

    pay think life partner song

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
