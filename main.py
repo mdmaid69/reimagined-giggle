61515a0d405fb35df34ac5bf34bfcebf9cba5817
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:42:33 +0000

    dress spread address communication wave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
