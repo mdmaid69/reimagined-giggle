2105caacbf7903b94fc7365c4180d30431258c10
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:48:23 +0000

    artificial group belief regularly dear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
