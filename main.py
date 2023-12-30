ac30701995be99143151a68e443ff737735c8b68
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:43:48 +0000

    avoid difficult create own stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
