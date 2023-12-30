d8450ecc76973f30ccbcc701953f061b038aa91a
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:34:46 +0000

    general cry fork fall wrap

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
