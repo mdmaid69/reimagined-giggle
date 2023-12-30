26bb663899630531fb516473738fb7ef7f7fb0ba
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:18:22 +0000

    material really takeaway new cost

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
