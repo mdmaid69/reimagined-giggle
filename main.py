36d7900bda74743a19dfad392a9da91ba7f9734b
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:32:30 +0000

    reach plate sure complete fond

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
-def count_words(sentence):
-        return len(sentence.split())
+import math
+print(math.pi)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
