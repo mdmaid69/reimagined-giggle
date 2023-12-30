a838ab0bbefe65c03ca16ac8c149d38b5f06a401
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:03:43 +0000

    breakfast effect abroad lock bringdown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
