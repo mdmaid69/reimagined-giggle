3f40cd40bf4c17f2fdc67462535da9e35ae54ad2
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:27:34 +0000

    dance underneath bus expression justice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
