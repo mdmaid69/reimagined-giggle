0d422c84bc48cb21528fe5506539f6f8767750ee
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:50:22 +0000

    brush sensitive slice mark hardly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
