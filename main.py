b69c780d652b97821c56145d62b895e55f8a4b63
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:30:08 +0000

    lesson argue hear worth yesterday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
