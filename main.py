004c40bd1885c071f8cd46f83ab072086ac31157
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:25:54 +0000

    laugh on smell great visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def get_array_typecode(array):
+        return array.typecode
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
