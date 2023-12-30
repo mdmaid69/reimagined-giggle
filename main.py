c3a601c6822817cf319cf68f1f75e8aa997e36cd
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:18:33 +0000

    tax part scare frightened tired

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
