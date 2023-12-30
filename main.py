6de9db776002f3fd96c7c41095df1558fcd39b52
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:18:40 +0000

    connected relax pump fine send

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
