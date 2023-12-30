faa5e5079935a6c193a16d9f994db4a1358e370d
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:36:40 +0000

    well deserve better wet contact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+text = "Hello, world!"
+print("Reversed:", text[::-1])
