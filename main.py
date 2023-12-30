e8ff34139e0661dafbca260c613a8457b95a5173
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:56:55 +0000

    advantage secret foot will trousers

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def sort_list(lst):
+        return sorted(lst)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
