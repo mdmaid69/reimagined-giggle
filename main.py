0c7cff5d0cc4055634a2b389356f30dae37db6eb
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:08:28 +0000

    crowd ours service majority attempt

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
