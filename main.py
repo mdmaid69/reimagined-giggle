898bdcbe68ac49ea2b4421279df80a1f9ab60a61
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:35:17 +0000

    care note right heavy drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
