4252a3074065f8f2eff8f58332139b55a6345339
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:21:14 +0000

    point idea rush basic well

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+print(math.pi)
