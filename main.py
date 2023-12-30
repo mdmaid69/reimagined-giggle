6feb99a90ca19c70ae9e8e271d11050699301f5e
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:45:54 +0000

    you section worry must middle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
