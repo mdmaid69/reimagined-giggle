b18ab2cd3e449d2243495df38735beaa2428be68
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:32:51 +0000

    holdon because decide blind bath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def get_array_as_bool(array):
+        return bool(array)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
