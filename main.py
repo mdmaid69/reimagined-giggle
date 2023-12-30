9f82b34879ab2cacf0ce985a07a66c217bcd79af
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:48:21 +0000

    health part uncomfortable already his

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def sort_list(lst):
+        return sorted(lst)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
