caab510f646909049baa566f55e2a043fe1ac86c
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:43:38 +0000

    married much stir truth soak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+name = "Python"
+print("Hello,", name)
