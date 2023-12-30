294a7dd0aa242665dde033c434a6d087eed74079
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:42:57 +0000

    disturb push slowly occasional politics

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
