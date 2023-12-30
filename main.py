6803871f0a31872b82b6ca39269c10f2f0f406ee
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:10:42 +0000

    earn bright problem it blind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
   import os
-  def get_file_device(file_name):
+  def get_file_dev(file_name):
         return os.stat(file_name).st_dev
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
