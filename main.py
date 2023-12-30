8b96ed95fbc1c23d21fb49f814fedfa2e400e8d4
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:20:03 +0000

    suck claim march cool plenty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def reverse_array(array):
+        array.reverse()
