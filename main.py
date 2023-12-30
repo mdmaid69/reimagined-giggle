aed2940e66c30abf9d6809524bd3e18e17f81737
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:56:22 +0000

    face power murder strict think

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import sys
+def exit_program():
+        sys.exit()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
