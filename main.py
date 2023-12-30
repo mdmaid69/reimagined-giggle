b612fc5db6a8efe831a9e7b7525386716a421f6c
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:08:30 +0000

    some injury combination expect 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
