4224ccd8ee2ddf5d8c10e56830fb3b56c014c702
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:06:50 +0000

    wrap control before knowledge statement

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
