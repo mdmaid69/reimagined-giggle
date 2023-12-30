2683c23e254d14f2351e18f8eb7be7fb28526138
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:59:11 +0000

    affect world opportunity below such

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def remove_duplicates(lst):
+        return list(set(lst))
