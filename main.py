610e2d5b50a1fd4f0492cfb2cb474c207c423aba
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:52:18 +0000

    interfere big wind or conclusion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_tangent(x):
+        return math.tan(x)
