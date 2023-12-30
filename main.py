0f64c1174f3fc12614a4ec3363bbbb6ba12d9d44
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:40:34 +0000

    confuse grade interesting weather next

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
