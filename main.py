2feb33e37aa3cce037de184c203c220e561f8ef1
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:06:13 +0000

    disagree back turn meal sing

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
