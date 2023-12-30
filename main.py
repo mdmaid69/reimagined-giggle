e1bd9b64b6c818a5b2b2fe97834a736a15a59347
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:22:43 +0000

    anybody radio storm sink handle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_max(numbers):
+        return max(numbers)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
