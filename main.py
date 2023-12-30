3cb4f3d26bab173c8a88e656b1d40e837f0fba4f
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:46:02 +0000

    hair factory middle so breakdown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def is_odd(n):
+        return n % 2 != 0
+import array
+def get_array_as_list(array):
+        return list(array)
