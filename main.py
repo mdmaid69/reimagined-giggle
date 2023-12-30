c83045545cf0b9a84ba89682b50e270d76d88dba
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:44:46 +0000

    slice welcome heart wind drop

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def set_array_item(array, i, item):
+        array[i] = item
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
