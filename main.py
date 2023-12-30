b3315205f8243e17edc950a388aee927b4723935
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:10:03 +0000

    gooff walk perhaps interest broad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
