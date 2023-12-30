5461cbd64ebe39dcc5565a9596fec15cdc27129f
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:59:14 +0000

    Ee breathe frighten shoot personal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
