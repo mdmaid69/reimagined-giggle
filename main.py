ce250cd923910bb3570aa9d3df812ca6982d53ec
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:30:19 +0000

    theirs supply greeting food arrive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+name = "Python"
+print("Hello,", name)
