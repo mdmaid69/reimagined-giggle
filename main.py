f486353dd5a6ae79369f945edbfd5e0f5680f7e1
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:26:22 +0000

    bend lock exchange strike straight

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
