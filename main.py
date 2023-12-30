adf2147959b926b343eb2a6919961a102389fd32
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:54:00 +0000

    dress possibly positive or few

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
