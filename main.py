ef2f952bef2dd48924ca17895f580b9fa217bb70
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:10:32 +0000

    dear search discover point getout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
