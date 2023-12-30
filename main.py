2a07eb0abb476f8de598e6143e37e0844fcd6457
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:36:34 +0000

    scare fold arrival sister lift

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def square_number(x):
+        return x**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
