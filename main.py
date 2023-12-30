e1575dff2ce327cdfc15ce760c22ef5d74fcba8c
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:52:58 +0000

    show before adjective wish congratulate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import array
+def get_array_length(array):
+        return len(array)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
