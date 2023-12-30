9e4ae35a5e60f5fcd11709d500cf40a3f2e9fc71
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:41:50 +0000

    black explanation by scrape over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+text = "Hello, world!"
+print("Characters:", len(text))
