ec69d18f0155361ebbdc200eb0e193a8c53bb563
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:51:46 +0000

    whistle back crisis worth see

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
