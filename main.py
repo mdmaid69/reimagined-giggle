dcbf979bd1a6840f29c9121f183d266f80da5462
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:20:08 +0000

    estimate raw right by tap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def count_characters(sentence):
-        return len(sentence)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
