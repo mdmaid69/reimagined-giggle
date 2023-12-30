d2da18b2414dae1988abfec41f9e2b47ee2b374f
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:10:57 +0000

    achieve rush what science train

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import time
+def get_current_time():
+        return time.time()
