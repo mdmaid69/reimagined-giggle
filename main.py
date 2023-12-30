1d7b27e24df0b6f519a6e09004c1d3940dd35efb
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:57:01 +0000

    probably touch tear wrap duty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import os
+def remove_directory(path):
+        os.rmdir(path)
