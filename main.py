ce91d2bf934dbe603f2457582989c2f25076b9a4
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:51:00 +0000

    excuse loose surprised dull replace

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
