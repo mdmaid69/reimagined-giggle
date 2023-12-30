62a2b16ce4587d44b554b9774abddfb6aa0d473c
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:51:00 +0000

    division capital lie way laugh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
