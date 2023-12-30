203ff2781d3b283f831318eb0187720643440380
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:52:06 +0000

    store shoulder lay mysterious bush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+def subtract_numbers(x, y):
+        return x - y
