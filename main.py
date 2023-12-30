5acf6593d0ee3800e57927bd35a002c30b67bc0e
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:43:04 +0000

    mood difficulty nor firm ask

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
