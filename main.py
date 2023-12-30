3d8b3439786bbff79060c7e57f0516c9672ce502
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:22:13 +0000

    brush advice total wherever disapprove

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def square_number(x):
+        return x**2
