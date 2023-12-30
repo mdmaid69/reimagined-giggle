496a30e3c4fd0cbd038127458b49d1f3de24b36e
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:02:31 +0000

    moment back mean protect future

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
