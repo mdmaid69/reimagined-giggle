3be34d871590b384afb790e5598d6cf3898eef83
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:49:51 +0000

    sink year day stand upset

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import numpy as np
+print(np.array([1, 2, 3]))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
