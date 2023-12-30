e01c965fad085407b1d0948ba70352f78753006a
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:35:43 +0000

    compete shopping danger experience space

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
