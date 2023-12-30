efef9094bb9f179c8486fd1209c73343ef5e5999
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:49:22 +0000

    mood mouth suggest wall open

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
