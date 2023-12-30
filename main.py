469ea3920a91913bc86195d91c90f9d40600b4c4
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:41:06 +0000

    interfere time number thankyou agreement

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
