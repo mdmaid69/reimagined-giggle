70f4460d6aca23d6e6bfb5f6bf900c9f8b4010ab
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:54:44 +0000

    true crowded doubt wear wall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
