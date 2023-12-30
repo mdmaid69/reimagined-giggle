4b0740d3acbdffd31a8886c1252275a95a0968e3
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:36:51 +0000

    damage care ours lead grateful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def reverse_array(array):
+        array.reverse()
