b3b4504e9124d5977c8ab5becf48a60a1fd4d346
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:47:23 +0000

    yes inside sense someone trust

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
