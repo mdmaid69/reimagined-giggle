57d671c3d3a02f78a696ac0b9b748864a4938f60
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:33:19 +0000

    row treat free criticize turndown

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sys
+def print_python_version():
+        print(sys.version)
