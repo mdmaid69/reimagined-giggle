753f53472a9ff217f984759acf9b914b47790a32
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:25:33 +0000

    cover agreement comeout survive that

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
