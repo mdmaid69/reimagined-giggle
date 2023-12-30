33254554ea4facf7e4aab263db6c322cc0fc4ad5
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:09:20 +0000

    react guess show consist complaint

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import tensorflow as tf
-print(tf.__version__)
+import time
+def get_current_time():
+        return time.time()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
