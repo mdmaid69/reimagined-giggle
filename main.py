36feee74f0dee0f6c9fe6f95bd76bb6f4e224047
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:39:56 +0000

    at smell workout sound private

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
