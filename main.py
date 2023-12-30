dbe1c1a78ea447262664b00f78f57e18c3b60cfa
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:37:30 +0000

    string discussion terrible period tooth

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-from collections import Counter
-print(Counter("hello world"))
+import array
+def reverse_array(array):
+        array.reverse()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
