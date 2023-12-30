e4cff13aed6db52c71418254da11cd394d033169
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:51:03 +0000

    violent odd Rr occasional arrive

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
