7402c1fb60e4bb782360fef67fae22bab51b4214
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:07:10 +0000

    badly container embarrassed case government

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
