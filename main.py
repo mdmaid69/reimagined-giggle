87e5321b0563867b25994b5e0bf00f2523b6af77
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:09:00 +0000

    necessary ours long attention roll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
