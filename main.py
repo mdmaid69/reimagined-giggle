675939a166a20797d8b8c73906cfd1b9cb778a50
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:17:24 +0000

    pity wing song lead reliable

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
