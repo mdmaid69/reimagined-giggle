cb0f786866b9aa74308fa3b91265050f3eb6c03d
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:24:31 +0000

    record dance agreement reaction activity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
