e186715acb9ead9eb9a8581e64037c1728f57d5c
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:42:06 +0000

    else genuine beat measure stupid

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import random
-def roll_die():
-        return random.randint(1, 6)
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
