9395d306de2f846bd3d79a0e2b36c92b3cd44b86
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:07:49 +0000

    should beyond connection pull ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
