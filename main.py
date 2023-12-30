6ee4c0be11ee8d91d061f9df95218ea74559f65c
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:06:37 +0000

    fault rule improvement round post

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def count_elements(lst):
-        return len(lst)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
