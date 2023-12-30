3cdcc5714dab925a801fc2ff69736c602f38afbf
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:06:15 +0000

    time good suddenly fast start

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
