f6ca6ab522908d11ea322921af5a6f379c42bbd6
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:49:44 +0000

    emotion bank guilty yet life

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
