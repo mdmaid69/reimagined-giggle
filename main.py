660199e15022a7b698ebb79d9b2dd2260e584998
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:14:26 +0000

    necessary police performance matter cinema

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import getpass
+def get_username():
+        return getpass.getuser()
