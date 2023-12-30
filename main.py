61b0dd752b44b2a07827e02d47d931395823bac6
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:05:39 +0000

    us mad improvement surprised fortunate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_sine(x):
+        return math.sin(x)
