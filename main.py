5953316cb1765210dc42b975993ec8df583c2b15
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:45:24 +0000

    person consult example stretch receive

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def calculate_acceleration(speed, time):
-        return speed / time
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
