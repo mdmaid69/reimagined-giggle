73734c16a51d1551607bc8a9ef9b70d669a259b7
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:00:40 +0000

    holdout how slice protest succeed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
