483d0d124255e6248f7e7247964b1f6198edcb43
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:16:58 +0000

    rather leave us put comeup

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
