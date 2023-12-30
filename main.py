178459e2071d8c5286ffae58efd3dc0188ab582b
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:14:47 +0000

    realize week respect eager mysterious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
