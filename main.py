766a92ed32a416733184a18162434ab7d6624ea6
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:54:57 +0000

    kind side hungry lesson pattern

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-print(sum(range(10)))
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
