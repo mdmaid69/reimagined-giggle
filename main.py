78c98ffd1d6b5fa515bffb3bc67e164089ad2ebb
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:27:50 +0000

    pleasure wash choose admit write

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
