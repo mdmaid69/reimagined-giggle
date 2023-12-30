fd505df45583faed77362f58e060a2c39fbee881
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:09:18 +0000

    piece acceptable point quarter plain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
