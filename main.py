77c0994a71029c164ba303270205e598fbaf7b32
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:29:45 +0000

    worried lean picture one possibly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
