30a872dd6ee11b7fa3c3b0fb3fbc7116ab371e2b
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:11:30 +0000

    square throw computer most past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
