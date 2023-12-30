4eb4b1266ead742548d59b8737cc35ff08cbb6e5
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:16:34 +0000

    disturb always awake supply except

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
