a1504c7123a5cbb23ab2aed317625d694df70ede
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:37:58 +0000

    interview mysterious wrong success open

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
