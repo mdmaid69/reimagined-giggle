a8fdc372701f6bf1caa272d2f6d18ff4f2d8aa01
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:00:44 +0000

    money seem suckme view pay

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
