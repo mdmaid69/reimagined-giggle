847bf34e2674707291983e532283167fec66b9fb
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:03:52 +0000

    mine small keepoff last deaf

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_area_circle(r):
+        return 3.14 * r**2
