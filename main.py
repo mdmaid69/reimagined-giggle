0d51ca41585b2ab9c8ec08cf30d4b4ad46482fe8
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:16:22 +0000

    divide change speech beside uniform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-import os
-print(os.getcwd())
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
