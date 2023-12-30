35973dc7fb91927c644e717f88d2da8d866db9aa
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:19:24 +0000

    slowly order centre suggestion time

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
