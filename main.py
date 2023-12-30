0016828cd1ae80751a886941f8a68d58aea9d790
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:27:18 +0000

    loose congratulations seriously personal perfect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
