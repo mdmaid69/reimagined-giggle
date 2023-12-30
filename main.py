84ff08e99e34fb92f49d728c47f9eb718512b2c7
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:43:39 +0000

    unite seat confusing drive thinkover

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def count_elements(lst):
+        return len(lst)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
