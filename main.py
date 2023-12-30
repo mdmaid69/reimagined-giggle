41f7f196049811c90c019ea520327e76d73f1034
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:39:03 +0000

    like accordingto Tt include person

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import logging
-def log_message(message):
-        logging.info(message)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
