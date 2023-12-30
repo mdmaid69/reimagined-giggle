53c88d56314ffd68ced0f7730a698797eb87dd29
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:18:47 +0000

    gooff sleep low minimum mean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_speed(distance, time):
+        return distance / time
