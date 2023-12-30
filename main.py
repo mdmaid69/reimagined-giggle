6f45437b10743616a0aa7094e26344fe874518ec
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:14:24 +0000

    time rest about agree ordinary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
