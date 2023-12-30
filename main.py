fc4278a416c8bef8e746e02e1077991f71149456
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:37:53 +0000

    pity interruption official tend justice

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
