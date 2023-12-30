9b246802f282ca8e7ca37b248ca242556643dfa5
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:05:46 +0000

    wild wire rate fortunately possible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
