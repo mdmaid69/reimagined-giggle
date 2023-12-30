677bcf759f670c4d92ac6cf530803d7a703c390f
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:52:38 +0000

    confidence open dear takeoff fence

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
