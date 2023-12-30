4edc29cd545d201e8e57eba3f2d262eaa7062c12
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:04:32 +0000

    point still whom wrong left

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_length(array):
-        return len(array)
+def calculate_power(work, time):
+        return work / time
+import random
+def roll_die():
+        return random.randint(1, 6)
