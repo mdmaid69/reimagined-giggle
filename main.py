c0e933806fbe7018b5ba5e814e90ad4b51a09106
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:26:20 +0000

    exchange remark lucky tool criticize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
