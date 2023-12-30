bb253c8f1e58eb4dd40189e87a803f7e542c462a
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:21:53 +0000

    telephone along invite sometimes begin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
