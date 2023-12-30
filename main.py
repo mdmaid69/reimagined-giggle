e6c42a43c3df334ba1ad25aa681a02bcf83de766
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:13:20 +0000

    drink use change pretty walk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import time
+def get_time_since_epoch():
+        return time.time()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
