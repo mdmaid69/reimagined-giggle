1534fe3bf4d123c29d70f0a423d563180a1da27d
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:28:18 +0000

    minimum advanced purpose everybody workon

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
