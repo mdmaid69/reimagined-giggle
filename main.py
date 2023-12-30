1b5d6b8d09ea7ba33ad14b4896b29701d251e49f
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:45:33 +0000

    makeup left decision spray gooff

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def find_max(numbers):
-        return max(numbers)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_pressure(force, area):
+        return force / area
