2a3ef27b730435873cf2749681640b01dc23e2e7
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:08:12 +0000

    attempt meaning underneath she sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_counter():
+        return collections.Counter()
