b65aa129f45c4f90c3666d6f64babaff6192dea2
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:29:01 +0000

    year after quickly both aware

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def cube_number(x):
-        return x**3
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
