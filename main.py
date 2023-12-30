c489f487d895010d81e7cf75f852a63a2fbb525c
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:20:56 +0000

    pride art position meal platform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
