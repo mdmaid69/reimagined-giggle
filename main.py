c16963922c9543448abb994494d9209395bc508d
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:19:54 +0000

    lookup scene earth repair mine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
