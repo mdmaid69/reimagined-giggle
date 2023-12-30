a2129000e7af4c878d0ed2f7834a4a15cb141eb2
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:04:39 +0000

    payment colour measure profit exact

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import tensorflow as tf
+print(tf.__version__)
 import sqlite3
 conn = sqlite3.connect(":memory:")
 c = conn.cursor()
 c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
