69ab8b81476ccb95cf0581c2d51c2d7fb2f96775
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:36:46 +0000

    spare drop of getup only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
