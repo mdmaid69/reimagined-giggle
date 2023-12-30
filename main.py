ef63d362f3a88c671d6ea557c400961dbb259a51
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:09:54 +0000

    demonstrate correct unlucky afternoon like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def convert_array_to_list(array):
+        return array.tolist()
