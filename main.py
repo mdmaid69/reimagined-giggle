1cd49cd4f4dd96d33cbbeb5f5c2a29a4a4dc65a2
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:27:07 +0000

    little such mention prepare write

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
