89b0247e891a5d1b86ee25bd23d9daeef968155f
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:12:19 +0000

    join estimate otherwise foreign little

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
