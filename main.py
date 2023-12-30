9b91b78830bda0d6be8e8d7aff1455d730116dc1
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:38:40 +0000

    invite confuse guess dear take

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import random
-def roll_die():
-        return random.randint(1, 6)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
