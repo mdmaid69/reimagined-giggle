0c630474aa786cc412af5db9a4309c72d5e08a58
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:53:59 +0000

    their juice tidy part rent

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
