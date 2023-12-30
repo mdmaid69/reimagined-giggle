7ebf5a22867cd55a2e0cf476c726a356b8e1f6d1
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:03:10 +0000

    crazy visit accident blame system

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def reverse_list(lst):
+        return lst[::-1]
