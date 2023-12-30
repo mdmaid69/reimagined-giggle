fdaba2c38c67de9197d5ce6de9430730bb3e6993
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:47:38 +0000

    education goout fair carry noise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
