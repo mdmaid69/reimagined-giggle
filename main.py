e025d6d0d88e56e7b29993e7c259a2b588c4d86e
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:06:28 +0000

    organize approve tax destroy crazy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
