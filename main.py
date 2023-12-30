a1dbe80a61cae19d7158e98c35cfadde0c6ceae9
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:27:28 +0000

    radio plate square experiment off

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
