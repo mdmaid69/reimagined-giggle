360b70e4c3c18202d740f7a1924f6c0b89718639
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:25:37 +0000

    let possibly science shame thirsty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def sort_list(lst):
+        return sorted(lst)
