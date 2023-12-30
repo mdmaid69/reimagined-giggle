ab71ed7915ef67b48c499ab8a7c86277935e32a2
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:32:06 +0000

    goout shape hair man town

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def square_number(x):
+        return x**2
