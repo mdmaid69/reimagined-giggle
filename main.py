d2538177e8dbd19cecc0763cadc4eb5f7051d62a
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:12:43 +0000

    reduction could ring cause beautiful

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import array
+def reverse_array(array):
+        array.reverse()
