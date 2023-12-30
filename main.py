0141341a100ca411940ee29ff9dfa563f8312046
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:19:24 +0000

    trap speed hit politics health

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
