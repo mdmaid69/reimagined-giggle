48c061dab466012a8f3e964f097fea9b1e972800
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:57:51 +0000

    urgent develop oil freedom squash

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
