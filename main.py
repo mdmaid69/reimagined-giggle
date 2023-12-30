a875712f5044197a683ccd7c24ac4fb51b6d65d5
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:52:28 +0000

    bread born probably official fit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_max(lst):
+        return max(lst)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_index(array, item):
+        return array.index(item)
