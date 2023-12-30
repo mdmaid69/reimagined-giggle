68a8add40fe5f330f23430cee30efa70af52f7a7
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:36:50 +0000

    study married transport character passenger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
