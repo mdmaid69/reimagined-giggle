94f86d7eb1492b8e619edcaecaf65ff86c6cfd78
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:37:39 +0000

    unkind divide then achieve smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
