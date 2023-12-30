7d7b941c56ce18d8cb3d99276e6bcb5e0ad108bd
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:12:40 +0000

    question challenge advantage evening many

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+print(json.dumps({"name": "John", "age": 30}))
