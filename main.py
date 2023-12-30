151627306cc4c04d315b2568360a328caee13323
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:30:14 +0000

    before original might view search

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def get_current_time():
-        return time.ctime()
-from collections import Counter
-print(Counter("hello world"))
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
