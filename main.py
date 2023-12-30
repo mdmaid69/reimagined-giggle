43693cbec4e2798fb34b7df08faaf3a19e14d17c
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:18:47 +0000

    moment benefit suggestion however hurt

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
