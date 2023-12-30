24f8905f0119eb7eaa2f0222204e4d64544246fe
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:32:18 +0000

    anywayanyhow flat neither strict closely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
