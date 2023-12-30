003134269133f03df04643795c26da087a081fff
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:29:05 +0000

    insist public pronounce hope tall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
