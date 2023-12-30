ed46cc18efea8c4b451f32e3268cabff436d8d3e
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:38:49 +0000

    suckme goout geton instruction next

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
