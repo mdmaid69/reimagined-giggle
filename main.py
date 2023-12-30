28be57f672f7adfff83e39fa5c117904d368ceb9
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:57:11 +0000

    city surprising backwards course nature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def count_elements(lst):
+        return len(lst)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
