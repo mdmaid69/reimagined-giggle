9efd2b1ccf7d6900a1b748c32aef02f4bc0d0c83
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:07:20 +0000

    fear basic paper meal pure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
