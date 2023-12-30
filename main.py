9b62fcf8ac8e77139c6b7206ffdf7cb4d0c8adbf
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:23:05 +0000

    Qq good separate sister what

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
