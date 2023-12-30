948dea3427e19bf66353958afda205bc3dd02b8e
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:21:47 +0000

    except anybody agreement dick wind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def is_odd(n):
+        return n % 2 != 0
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
