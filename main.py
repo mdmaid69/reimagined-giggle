575cbf99343be3a17b677f08528cf60726f9bdcc
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:45:29 +0000

    circumstance conclusion yourself precise left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_str(array):
+        return str(array)
+def square_number(x):
+        return x**2
