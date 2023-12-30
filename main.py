3794578c1dc84a929260b6d77b553759d0699db0
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:48:17 +0000

    community clean simple makeup difference

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
