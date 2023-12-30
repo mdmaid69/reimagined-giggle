47a0627958b81a82d505e5f2ee37b2698ce41379
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:50:43 +0000

    final press secret professional time

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
 import array
-def get_array_as_bool(array):
-        return bool(array)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
