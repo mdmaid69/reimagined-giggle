79bde666fa43b0a6e0251c780f9d97e87b5ff8da
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:23:14 +0000

    stage bad guide spite give

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
