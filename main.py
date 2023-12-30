59fbd7a90ba4b72a77c6a4f48f26e60124681e13
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:00:21 +0000

    style partner available influence carry

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
