52675daaccc165721f6e8ce463acbd892eb588a9
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:06:48 +0000

    performance angry criticize from recommend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def reverse_array(array):
+        array.reverse()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
