7e76a69d2700507fbf9fb165a66564995387a75b
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:11:10 +0000

    giveout hair imagination middle breakinto

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_work(force, distance):
+        return force * distance
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
