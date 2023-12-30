520e5272c7603b27fda0ccfd6e351a6b9abb5130
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:48:43 +0000

    thorough funny fold experience least

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
