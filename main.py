90b29439228149531aafd05789ceec74ad0a7619
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:17:25 +0000

    nigga Vv Ff arm appeal

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import sys
+def print_python_version():
+        return sys.version
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
