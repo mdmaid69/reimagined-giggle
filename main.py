651a72dabde7af2d5a55b9421257d996db8fe95a
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:45:44 +0000

    open trust poison scale each

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_area_circle(r):
+        return 3.14 * r**2
