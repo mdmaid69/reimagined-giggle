f6d8e4111a94c4eeaa958c9b4656d9dd6296464f
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:45:39 +0000

    bag turninto anyone say greeting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
