74ebd40837334a346c45eb855cc71bd27022d46e
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:27:26 +0000

    gradually thing mother autumn swimming

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
