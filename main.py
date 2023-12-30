e1fc8882adcf93a6176bcfa7bbb5df754dce265b
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:06:17 +0000

    press connect illness before stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
