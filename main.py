8de638c2952c678e6437348d4019bd5103bf2a44
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:44:50 +0000

    study side judge attempt cry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Characters:", len(text))
