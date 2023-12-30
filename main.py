fa17c6fda29913cb6b03c0813fdc018442d362ac
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:11:04 +0000

    pronounce cause than lookup able

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
