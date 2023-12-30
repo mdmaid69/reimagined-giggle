cf5da24f8285b6b7cfc632b1d8dfa4dd17225f41
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:44:09 +0000

    easy mix circle compete personal

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import datetime
+def get_current_date():
+        return datetime.date.today()
