94c0793914ac36cdaea92f210ce9faa43f271713
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:32:54 +0000

    earn disapprove lucky meal guess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def sort_list(lst):
+        return sorted(lst)
+import os
+def remove_directory(path):
+        os.rmdir(path)
