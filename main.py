e3a51fb6076e3be63b23e24b9e0cf98d1d54f05e
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:24:06 +0000

    smell doubt mad near wipe

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
