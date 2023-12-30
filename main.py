81d6c4753512bd80a445ff5870c2ae80dbaef545
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:04:40 +0000

    refuse container moon too set

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
