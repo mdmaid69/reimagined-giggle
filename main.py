9229e8c66323cb0a15a39a07241716c5aceffe5b
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:01:57 +0000

    everyone behind tight ear fire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def convert_array_to_list(array):
+        return array.tolist()
