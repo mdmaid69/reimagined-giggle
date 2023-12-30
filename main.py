6cbea14aa6f0edad2be9fe386936792fa6c29b0b
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:53:29 +0000

    uniform anywhere reply model scared

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def reverse_list(lst):
+        return lst[::-1]
