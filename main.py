86e15f0f3900812cb3990b478707af70af02109d
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:21:02 +0000

    shelter police expect Qq off

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
