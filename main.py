f9de2573f87de15aa5eaa79d886ff87a78c63d4d
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:54:29 +0000

    ring intelligent below dark slip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
