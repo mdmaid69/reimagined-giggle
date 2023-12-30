68e2207eb370455925c2449efb23e35be2228094
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:58:40 +0000

    off takeoff list somebody thankyou

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_slice(array, i, j):
+        return array[i:j]
+def count_elements(lst):
+        return len(lst)
