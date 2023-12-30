dbfe17892fbe4b2b92f164a7bc2dfbe4cdd458b2
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:50:14 +0000

    mention question second godown round

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
