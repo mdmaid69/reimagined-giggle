180546efc6d2e7ae4e5090801a078149e96fcb9c
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:26:36 +0000

    consider not interesting owe month

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
