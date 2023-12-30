b291e6449c0b7820c83143bdcbc8330f5b13a6ed
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:42:10 +0000

    steer underneath like mine upwards

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
