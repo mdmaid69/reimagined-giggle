770474a14dbae2bb07193d57426d3743a0766482
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:20:25 +0000

    house polite prepare message fond

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import time
+def get_current_time():
+        return time.ctime()
