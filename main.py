aa9a1520a25b3fdaef344bde4c031782f31099db
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:48:41 +0000

    respect wind female breakinto reserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def split_path(path):
+        return os.path.split(path)
