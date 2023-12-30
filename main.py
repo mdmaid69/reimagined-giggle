d4e14223c0d430bb409e466bf108b7e976efa739
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:10:14 +0000

    hang hide university formal dream

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
