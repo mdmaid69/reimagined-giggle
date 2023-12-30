657703db2b8cd9ae9f8b61082a09604613399bcd
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:19:27 +0000

    giveaway trick relief division abroad

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def square_number(x):
+        return x**2
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
