de077a7139b4a6677ef6a84c225ef117562cfae4
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:18:40 +0000

    environment evening breakin whom doubt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import array
+def get_array_as_repr(array):
+        return repr(array)
