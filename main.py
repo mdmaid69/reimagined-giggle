0db93cb9010981fd2b4de251d0bdfa721dcc8aa4
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:44:20 +0000

    march minimum raise like house

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def remove_duplicates(lst):
+        return list(set(lst))
