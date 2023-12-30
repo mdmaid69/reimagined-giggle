c9c2ded7e0eb8d42bcd4fe481910a64c9022f7b7
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:06:25 +0000

    land visit innocent influence safety

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+print("Powers of 2:", [2**x for x in range(n)])
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
