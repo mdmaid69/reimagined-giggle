aaae22e2f7271dd5228861bb1d75c375487f16e1
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:03:29 +0000

    by excuse defeat benefit shout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import random
-print(random.randint(0, 100))
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
