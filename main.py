55b5081a00cd1453a73fa8d1557ebd090310df0d
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:11:46 +0000

    grade train high connect reliable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
