01d91660ccf64c8c5827d7ad151fc18eec492e55
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:01:15 +0000

    actually container earth but unusual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
