3edcf0edd0b819a035984df26b374a35fc836295
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:41:41 +0000

    life intelligent classroom actual Vv

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  def is_odd(n):
+        return n % 2 != 0
