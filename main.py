c5301f27aee3b9bdb3d225c0bb1fb18ca038cf82
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:36:07 +0000

    change fit mail glass game

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import sys
+def exit_program():
+        sys.exit()
