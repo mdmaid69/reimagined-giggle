5354b1712b3111a0c308d6c356e1ef17476afd40
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:44:59 +0000

    him region smoke represent safety

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-print(sum(range(10)))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
