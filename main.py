7dfa1f9b3aa711ee5efdfb27715b756d9a78da5a
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:27:07 +0000

    survive occur certainly room callup

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import numpy as np
-print(np.array([1, 2, 3]))
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
