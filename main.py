944ac9906b7c92512990570deea45da65bef7090
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:54:44 +0000

    broad doctor teach sick main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def is_odd(n):
+        return n % 2 != 0
+import array
+def convert_array_to_string(array):
+        return array.tostring()
