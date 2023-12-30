b16472b852a65b659ee1e87d1083fe057ecd08c8
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:43:37 +0000

    board innocent serious solve shine

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def count_words(sentence):
+        return len(sentence.split())
