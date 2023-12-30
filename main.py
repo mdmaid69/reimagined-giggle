1c2fb5d9b5a828d1321e2bf59d8449ecd33198ab
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:28:54 +0000

    later there prize progress present

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
