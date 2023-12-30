0beeca0d509344f92ba28b10b610a5e3b30de8ce
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:40:23 +0000

    statement borrow steam environment love

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def get_array_itemsize(array):
+        return array.itemsize
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
