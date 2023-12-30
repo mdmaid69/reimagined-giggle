79753a5325f09e249a022d99d32e127463f0b741
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:38:24 +0000

    study soon Jj direct Uu

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
