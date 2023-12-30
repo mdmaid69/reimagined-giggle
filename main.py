06af2c23d4272320eed1d6bb73268b40e0d9d05a
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:52:13 +0000

    hit actual peaceful persuade into

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
