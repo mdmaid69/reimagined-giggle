df6c2c0bf4a780caca9b7cbe96fd486709094955
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:50:40 +0000

    in left pass silent government

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def multiply_numbers(x, y):
-        return x * y
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
