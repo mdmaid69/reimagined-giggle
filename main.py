050e880e9dce06dfe176d27f90c58adaf215e30d
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:36:14 +0000

    indeed home mass terrible steam

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
