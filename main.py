202e8871e6a08a9f00c7cd4445309911e78e5f29
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:16:10 +0000

    last precise cheap conduct lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
