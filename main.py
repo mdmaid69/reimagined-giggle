76e50baec31a342bf916e5a1ffb1bc9876a391fd
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:27:18 +0000

    process cough confident mad material

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import random
-print(random.randint(0, 100))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def is_odd(n):
+        return n % 2 != 0
