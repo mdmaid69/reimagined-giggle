a259e03c352868c3a97468c9d8998bf473e2b4a6
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:20:00 +0000

    invite protest power tired pretend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
