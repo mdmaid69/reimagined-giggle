21193397dbabdf1b5d02e1c4cd9c344cb789f582
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:37:56 +0000

    priority thinkthrough fly come window

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
