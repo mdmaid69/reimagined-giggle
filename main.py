48975c25e2976faf18e8aca28bcb7ac17b8b47b8
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:44:38 +0000

    him pinch flat limit vision

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
