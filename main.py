4cc53838b13751fc251ac8e3709cba6e755df808
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:14:52 +0000

    stand walk uponon yes duty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
