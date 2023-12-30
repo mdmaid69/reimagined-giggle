a02920036f0725323aa254772b85ffa9fcd4fc53
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:02:26 +0000

    their deliberately perform either press

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
