b315f8d43024cee9ad3f4955f3f24eabfb9bd96f
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:27:04 +0000

    bar 566end peaceful part fierce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_error_function(x):
+        return math.erf(x)
