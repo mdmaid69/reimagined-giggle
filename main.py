7d99edb6a245ec4259b8a7084d74a159291022a0
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:24:25 +0000

    geton difference goback(to) workon 571enough

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def subtract_numbers(x, y):
+        return x - y
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
