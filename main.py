65e419fb949f074be2e0f138b4d07cbcedee18d2
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:57:12 +0000

    meet temperature pen with violent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def get_array_as_list(array):
+        return list(array)
