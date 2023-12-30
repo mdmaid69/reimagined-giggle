e3434e433b8c712556649e6c4c479683d2f29266
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:39:39 +0000

    promise evening where fat rain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_power(work, time):
-        return work / time
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def subtract_numbers(x, y):
+        return x - y
