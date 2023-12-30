d5fac2dafa4e0150e447120a2f38ff26544f6faa
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:18:41 +0000

    rest break so level divide

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def is_odd(n):
+        return n % 2 != 0
