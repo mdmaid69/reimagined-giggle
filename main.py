bdcfd3cfa6e6272502eb9e7479bbac7cae371eb1
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:48:44 +0000

    lucky truth allow act who

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def square_number(x):
-        return x**2
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
