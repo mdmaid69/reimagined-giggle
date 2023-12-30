29f3524fdcbe21c7f24fe39531fa912181540a42
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:10:57 +0000

    explanation winter church if race

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_power(work, time):
+        return work / time
