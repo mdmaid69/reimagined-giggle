1d3b001b9b1181433c46e3f08106d6964f0c9f18
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:19:52 +0000

    everything cross influence nowhere difficult

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def remove_duplicates(lst):
+        return list(set(lst))
