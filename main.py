fd2895d85905dc902d76655361c039959c5c96f7
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:31:30 +0000

    crash shut present education introduce

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import getpass
+def get_username():
+        return getpass.getuser()
