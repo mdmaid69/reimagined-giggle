6d3ba9738adb535930ed5b4af7797a53d6d21bc4
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:44:29 +0000

    nurse school survive thinkthrough point

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def convert_to_hex(n):
+        return hex(n)
