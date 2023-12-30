0aef2201cdc4d8dd420b58291bf3a57565677980
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:33:39 +0000

    accidentally spot share protection slide

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import time
+def get_time_since_epoch():
+        return time.time()
