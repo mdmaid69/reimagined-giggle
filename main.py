c91388bb19173a7971907cb108a8db687b6587e2
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:38:08 +0000

    breakout command bother light national

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
