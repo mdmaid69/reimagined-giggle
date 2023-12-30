4301ab3a18bed637f5fa4af858bf2efa39a58918
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:38:14 +0000

    across outside nurse corner be

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
