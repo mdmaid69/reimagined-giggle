6bc5e81d5e6652489695da36d98a2a81828c6066
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:58:00 +0000

    argue chat where another criticism

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
