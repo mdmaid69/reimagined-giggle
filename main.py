7df9c02a1fb7f3c0286eafe4ea796eb33e70b978
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:46:11 +0000

    occasional home weak lunch hear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def clear_array(array):
+        array *= 0
