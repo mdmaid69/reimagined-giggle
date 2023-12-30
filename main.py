c7ecce71c69e39483598e80153be3415efd22066
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:31:38 +0000

    only half decision beginning that

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
