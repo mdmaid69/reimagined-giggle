fb22bf55f261f70536688f08a2ae56685ca08924
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:47:26 +0000

    combine sport none sweet flat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def square_number(x):
-        return x**2
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
