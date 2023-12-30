c3dd8bc8ee452307d0edd428471bd2d879fd0504
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:06:32 +0000

    squash close lock embarrassing quickly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
