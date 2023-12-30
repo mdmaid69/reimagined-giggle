b7cd0c360fbd4020ed9566d2ae7de498a64662c3
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:35:51 +0000

    broad form chin unit introduction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
