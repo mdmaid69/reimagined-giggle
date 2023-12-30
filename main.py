94aa23116bec4115a18cf80612bfba8afa993b36
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:01:45 +0000

    pretty tight freedom square swallow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
