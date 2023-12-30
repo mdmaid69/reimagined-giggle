4f52c46d46f988ed80d24963506a50727a649f11
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:57:13 +0000

    public turn dream apologizeapologise arrive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def count_elements(lst):
+        return len(lst)
