0c6831be28add4afe9a12c1389fce2e8b6b0154f
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:41:09 +0000

    project stretch congratulations there matter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
