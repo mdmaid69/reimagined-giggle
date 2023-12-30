6f218304cb1f11b2bf9157a8449237139d3cc52c
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:03:58 +0000

    silent listenfor comeoff goodbye disappointed

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
