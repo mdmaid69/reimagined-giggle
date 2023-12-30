e851667053cd969a2cb3d6598a8ad8297fe6b7ce
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:54:33 +0000

    practice slight contact bear healthy

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def add_numbers(x, y):
+        return x + y
