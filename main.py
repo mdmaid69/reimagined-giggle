1cff6220c74b855ff2dd85bc52632f27b1dad48b
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:11:16 +0000

    giveup lookup standard raw blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import sys
+def exit_program():
+        sys.exit()
+import sys
+def print_python_version():
+        return sys.version
