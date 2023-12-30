6786092b101b058159a45f957d85f33a10c55f47
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:34:50 +0000

    majority habit getoff makeinto Vv

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
