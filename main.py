be4453a87d090146698374ef2829cc3c1b24ed3c
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:36:04 +0000

    present workon Oo boat deliberately

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
