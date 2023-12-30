0c6c49f5b637ba5a54bcd5dd50e0284d3bf9d720
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:03:57 +0000

    dear prove 567enemy blue lock

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
