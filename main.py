b1c6da3cae2280f850cd5d7d9cb3c70c5c25d549
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:48:58 +0000

    climb science lead behaviour whistle

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
