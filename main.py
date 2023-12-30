34417e42f15c20a5a92adc2f15a285e54216a089
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:31:28 +0000

    blame turnback clearly talk child

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import platform
+def get_os_info():
+        return platform.uname()
+from collections import Counter
+print(Counter("hello world"))
