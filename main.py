93258a7effba374c092108ade1171328e581e1ae
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:02:22 +0000

    my clue wheel entry uniform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
