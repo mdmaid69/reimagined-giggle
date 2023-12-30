ff29146cf8ed1f0befa7392e2f11cab35aa500c5
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:47:40 +0000

    dress shrink future seat small

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
