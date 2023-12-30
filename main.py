f71553aef914bc31c93bdc8dd6c9d09acf7f9c7d
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:44:26 +0000

    thumb claim pass match pattern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_floor(x):
+        return math.floor(x)
