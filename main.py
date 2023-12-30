fbfe7c5c013d7e46d27d16cf829d02ecd3c65499
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:11:08 +0000

    year Gg on patient boil

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import getpass
+def get_username():
+        return getpass.getuser()
