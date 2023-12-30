26438a88c630086e979b8bf7072c1ef98304172b
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:51:48 +0000

    round try at sting dry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import tensorflow as tf
-print(tf.__version__)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
