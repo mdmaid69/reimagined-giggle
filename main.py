7d1880bcca463219cf213a113aa7c37a13c48238
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:03:49 +0000

    happy just permission enthusiastic beat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
