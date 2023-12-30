8cd4483fd7ed70fc85beb42b97fa7d3d0fbd0059
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:17:11 +0000

    even than wish hair soon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
