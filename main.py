b703913dcd99a8c94210c3c69af4870dc6ce986b
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:24:41 +0000

    lift answer guess look mouth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def get_array_as_str(array):
+        return str(array)
