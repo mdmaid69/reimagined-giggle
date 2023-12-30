a89e228c4e66f897f41816959f25bb7e66260c89
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:42:26 +0000

    course laugh better sure matter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
