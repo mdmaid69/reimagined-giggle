45ebe65a96f127c601ba3b4aca7a3fc79ba342b6
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:50:53 +0000

    getoff race hate callout post

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def sort_numbers(numbers):
+        return sorted(numbers)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
