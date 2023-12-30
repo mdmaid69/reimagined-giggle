8c6f6e6cc702f33309c4661d822c54ca3ca05667
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:57:40 +0000

    on block buy depend kick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
