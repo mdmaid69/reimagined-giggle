39378e63c7644c8c4399774f2e20beee2d054923
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:20:55 +0000

    computer cause watch ago week

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def get_array_as_list(array):
+        return list(array)
