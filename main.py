0610e4f75b02733b4f0ff6bb83e64736f8780d45
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:14:27 +0000

    cover completely bring strict read

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def reverse_string(s):
+        return s[::-1]
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
