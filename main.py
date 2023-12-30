5c39a6d67064c182f85a7ccbb818ba3f032eb9ca
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:14:54 +0000

    fire positive dress season smoke

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
