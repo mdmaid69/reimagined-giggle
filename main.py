cde27ff9d9a6bfd41f58b1ce583be3321e796d2c
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:19:45 +0000

    tear smell somebody surface actual

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
