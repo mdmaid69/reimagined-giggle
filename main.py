7893621a39227a1e651510f3fb687115671d3d14
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:53:03 +0000

    bite lunch meal interest doubt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
