55bf9f8837b35f10a1f9a7cfd25f110a5e3d68fa
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:36:37 +0000

    defeat know wrap strange appear

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def reverse_array(array):
+        array.reverse()
