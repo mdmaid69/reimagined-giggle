c9595b3b8505e013947da1ba898825890231ce32
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:25:17 +0000

    truth quick belief sorry mend

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
