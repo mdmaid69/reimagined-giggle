10ebaf2aa2efa161755bdcc588a4260596387b7f
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:19:32 +0000

    tap name influence after suddenly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import collections
-def create_queue():
-        return collections.deque()
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
