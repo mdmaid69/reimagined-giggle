2550634636d1202edf7e11e97eb3c7aa7f13040f
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:22:31 +0000

    deep eye square safe half

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_current_directory():
+        return os.getcwd()
