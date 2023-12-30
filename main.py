232dc7404fb3ddf014cff45bab937485e9f329b6
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:24:05 +0000

    borrow bunch movement scrap comment

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
