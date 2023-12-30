fd4b687ef03d00019bdf9b7dd6fb6c2258a0ac5a
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:02:28 +0000

    nature reason along declare care

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import random
+print(random.randint(0, 100))
+print(sum(range(10)))
