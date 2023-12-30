b209615270104f14aa0cda7f45bf3f9edcde180b
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:53:22 +0000

    back thirsty quarter language fault

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
