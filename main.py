7fd4168560f4e5935e66f2390bff38cbb2d9c8f4
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:14:29 +0000

    standard detail late else easy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
