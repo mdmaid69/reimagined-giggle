cecef2f38d21fb9845c02a871fd01748bed18b79
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:33:22 +0000

    mixture however mess weigh unit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
