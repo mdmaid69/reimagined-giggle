9b9cf8318230a28c7e4828bacdd193a222e8111a
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:01:03 +0000

    push bury violent success car

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+print([x**2 for x in range(10)])
