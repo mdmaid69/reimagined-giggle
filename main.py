7731099ecd5183f2f18cd4f8036cf2ef819da78b
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:38:36 +0000

    raw work squash what mend

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
