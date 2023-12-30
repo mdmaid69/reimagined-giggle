e9ba0f12ca3af3278d5a967f0d14a2f3af025ec4
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:16:49 +0000

    sink question chief brain radio

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
