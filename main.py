b1e7586c727baa179c90508be92b8cec006da5d0
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:06:06 +0000

    unless mark truth friend interruption

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
