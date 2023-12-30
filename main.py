2b9ec88b58cb52900d4694d745685decd6021f67
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:36:42 +0000

    order press neither tune think

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+import os
+print(os.getcwd())
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
