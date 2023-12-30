611d614c8b773230ea0515c908d0786d7125ee5e
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:02:31 +0000

    rain accommodation public school ability

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
