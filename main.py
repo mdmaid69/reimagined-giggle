1c24cab7caec7e8a66f079204e257a676abd8877
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:09:50 +0000

    exit example slow go eat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def remove_duplicates(lst):
-        return list(set(lst))
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  def count_elements(lst):
+        return len(lst)
