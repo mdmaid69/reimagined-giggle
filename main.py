13c56f6274c9e2bcd1b19211cecb45248a1d2930
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:38:13 +0000

    question then draw direction ahead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
