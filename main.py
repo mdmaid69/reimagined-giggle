f0b0e9bbe96a0f4a8e7c0e13cf13195f355f3907
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:08:23 +0000

    murder little drive find round

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def count_characters(sentence):
+        return len(sentence)
