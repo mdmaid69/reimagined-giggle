bca6e0375f60c92bed30d60c869e67bab1bbaaa6
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:54:07 +0000

    throw confident hot wake Jj

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
