a589bc4e3b4252a397b3dfb2a0a90c7ddfe645c7
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:27:03 +0000

    against 570enjoyable steep clue light

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
