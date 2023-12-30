a1cda5258c7091a382ddde7faec4e632010e30d6
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:24:32 +0000

    whatever boat healthy people street

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
