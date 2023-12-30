98f1573d3488ab5b84174064d8dea7874691d593
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:41:51 +0000

    meeting spring speak circle before

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-print(sum(range(10)))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
