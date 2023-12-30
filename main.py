b816436139cca463c82f99d5b750e3ccf7d3e613
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:49:43 +0000

    neck deaf short active print

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def count_words(sentence):
-        return len(sentence.split())
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
