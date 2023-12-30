88da377f1b6345f06439f327d2d5bae4d92d8218
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:25:35 +0000

    confuse scream translate whom gas

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
