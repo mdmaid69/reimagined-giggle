66cbba975d8a0efde4d1376da65063090a7a1f8b
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:59:36 +0000

    spring they to let head

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def add_numbers(x, y):
+        return x + y
