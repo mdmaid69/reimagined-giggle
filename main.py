a89772f82a176875a8bd121891e4a1ca160e9d71
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:37:08 +0000

    equipment smart eager pipe calmly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
