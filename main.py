edd27e00a0de05fd63c110e4c72339be44b13f6c
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:58:14 +0000

    noone above star imagine suppose

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import re
+print(re.match("h.*o", "hello world"))
