862f965b2b59cd89eacf7cc13804b484462f2896
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:57:53 +0000

    back ride less lucky but

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import os
-print(os.getcwd())
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
