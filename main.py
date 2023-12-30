7f5ddeac21d402292effb886275f806e4e0de45c
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:06:18 +0000

    task wild sore ocean present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
