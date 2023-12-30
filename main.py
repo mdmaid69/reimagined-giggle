89af4ef268c593c97f06bb2f880a25361f08de06
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:54:34 +0000

    environment tight detail grade mad

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
