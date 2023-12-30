e448b5e5e672c29dd4b3c5502fd825c4ca1c3ffb
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:39:58 +0000

    concerned serious position every weather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
 import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_as_memoryview(array):
+        return memoryview(array)
