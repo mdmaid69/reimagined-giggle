926b47b4148f8115e804df99e1773b2b6f662efc
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:38:23 +0000

    show pride brother my few

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
