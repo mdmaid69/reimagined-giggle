e899a4f4b626dadd78f56f5221dd75175e9ec14d
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:01:59 +0000

    standard word turninto people sister

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import time
-def get_time_since_epoch():
-        return time.time()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
