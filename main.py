65e2a5f9ab4a02e99c6ea48e03cc2c3fa24ca9cb
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:12:00 +0000

    seriously stamp shut whom same

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def square_number(x):
+        return x**2
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
