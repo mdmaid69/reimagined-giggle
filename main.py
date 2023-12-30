32f3afe14346028d31c3b98b9294c36b7a69b209
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:52:30 +0000

    prepare takeover appeal nor final

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
