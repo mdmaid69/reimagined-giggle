4ca0e37776f70479c3ecb004321cd3c2d750ee93
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:31:13 +0000

    search question 565end go father

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def multiply_numbers(x, y):
+        return x * y
+import array
+def set_array_item(array, i, item):
+        array[i] = item
