a74ef583c26741150e02b4fc2a92737ce7824111
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:22:13 +0000

    grade space opinion rich firm

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
