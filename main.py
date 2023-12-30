8249ee9f5abca79846aa9dab8b2cc3edabe812e3
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:30:50 +0000

    crash world single pen sink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  def cube_number(x):
+        return x**3
