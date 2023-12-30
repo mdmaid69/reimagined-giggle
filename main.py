9447df22fd3bddb69ed050e3126dfc07b05c61be
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:06:40 +0000

    dull advice complaint can eager

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
