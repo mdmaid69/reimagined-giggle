dca87bd4e87b25fa8da994678fb2566892a3a94d
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:58:37 +0000

    somewhere swallow drop Pp bright

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def cube_number(x):
+        return x**3
