99ab20b08d7c7af0b5db4872a390c2301622d985
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:26:03 +0000

    fit onto imagination fortunately turnout

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def convert_to_octal(n):
+  def convert_to_octal(n):
         return oct(n)
+  def is_even(n):
+        return n % 2 == 0
