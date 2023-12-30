94b413d358cca4a84861e137199bcde7782cba0f
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:08:38 +0000

    develop bang address advanced visit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
