aa48622196340498f84f2a8bde61cd7d28d886cd
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:33:14 +0000

    building journey speed dance basically

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
