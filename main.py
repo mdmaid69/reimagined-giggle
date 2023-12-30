c7fa316c8fb954e93e7cbe85f5c93f6c88250b4d
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:38:32 +0000

    blame standard constantly turnoff target

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def get_array_as_str(array):
+        return str(array)
