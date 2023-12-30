9416b20fb632c82cec2053b0948fe2e396e2e26f
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:56:50 +0000

    excited new treat use cold

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import array
+def get_array_as_set(array):
+        return set(array)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
