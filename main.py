2cc8ec77d9b2af69b221cfc504d8017d0b4965b9
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:48:05 +0000

    excuse amount breathe one emphasize

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
