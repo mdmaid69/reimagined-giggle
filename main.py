0840772c82a845a36c2e45d9e91355254fc3c6af
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:27:49 +0000

    discussion Rr clear practical pull

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
