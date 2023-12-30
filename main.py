d299616b912a902acb2cea9af12acedd84081fed
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:38:15 +0000

    capital drive truth naturally thinkover

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
