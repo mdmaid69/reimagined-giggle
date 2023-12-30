03e9626e195c1ec88167808ff55af1ed5b06b1ae
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:07:15 +0000

    soft enthusiastic somehow warning bunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
