2e960d091911b051bccb7189e3c99ca8111aebdb
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:40:55 +0000

    nigga broad smile water behave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
