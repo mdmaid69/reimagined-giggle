bde3d8390d2f7baa02dfd522c685bebbd7a59ec1
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:14:47 +0000

    him turnaway geton pretty meet

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  def calculate_area_rectangle(l, w):
-        return l * w
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
