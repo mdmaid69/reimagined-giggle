b4a5f10ff1049acda0ee2ed32321dd451f543bb3
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:08:03 +0000

    stir kind remark choose dead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
