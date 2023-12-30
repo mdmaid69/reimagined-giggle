52cac216cb669b3e7a8169894cb0946b8edbf119
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:28:10 +0000

    tear by band goout sure

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import sys
+def print_python_version():
+        print(sys.version)
