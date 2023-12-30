0228aa8784fd3df1c1f99bd0c8c7815dd7cc7554
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:36:32 +0000

    expensive play near trap week

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import sys
+  def get_python_version():
+        return sys.version
