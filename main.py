700df3aa548463d82d301911fbf15a9512428962
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:06:28 +0000

    bar difficulty blame recommend file

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+x = 10
+y = 20
+print("Sum:", x + y)
