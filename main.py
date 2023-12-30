612890a95b138ba4914b6b545a9cf441e63e2a03
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:49:08 +0000

    central Nn play young force

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
