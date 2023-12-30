8e0ace4712cc15d0f0c6b476b38f97c5bf21c0ed
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:03:53 +0000

    search afterwards priority today foreign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import datetime
+def get_current_date():
+        return datetime.date.today()
