b3c3054c60d3876e9f0ba73510e463785e63b0f1
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:20:16 +0000

    hollow contain face mine attach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
