50cd113d9fe7b666ff24cad40c65b4b1105fd507
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:45:59 +0000

    godown mistake whisper base range

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import platform
+def get_os_info():
+        return platform.uname()
