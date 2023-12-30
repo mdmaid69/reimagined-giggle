bad37f742404d32b8a87d860849be24b8ebdccc2
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:23:01 +0000

    demonstrate count break motion family

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
