e00d4507dfbbb5c470221c25898a87258898630e
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:48:09 +0000

    lean giveaway mark awake centre

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
