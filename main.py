2159623e095352c6cd1038068186cef2b8297dd7
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:17:55 +0000

    Oo region shape beside poor

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def find_max(lst):
-        return max(lst)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
