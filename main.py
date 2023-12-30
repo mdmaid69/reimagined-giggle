d95f049cd530e2a584e97e95c1dc14754e40c46e
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:18:33 +0000

    rest poor few right picture

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
