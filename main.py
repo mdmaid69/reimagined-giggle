fec677f7b98311f4d3cd89b7e4f82263e444e57a
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:13:01 +0000

    bottom takeaway freeze exchange wash

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def convert_to_binary(n):
-        return bin(n)
-import tensorflow as tf
-print(tf.__version__)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
