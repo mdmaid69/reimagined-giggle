aa5802f97b6a6f4bfcdb0956b309cb8a479040e4
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:25:07 +0000

    crowd I every fit disapprove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
