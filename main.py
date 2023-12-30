0daba357e3637d1730b19c56e225e7ac87fa4e8f
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:43:42 +0000

    least there within early force

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_bytes_from_array(array):
+        return array.tobytes()
