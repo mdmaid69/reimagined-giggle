edc4946fb1987493e28b45ffa44fc9fc32b97e86
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:30:22 +0000

    dress trade suddenly escape clear

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def is_palindrome(s):
+        return s == s[::-1]
