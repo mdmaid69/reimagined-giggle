ff7f0aa1ecdbebf7ddada414f32e81b2614068e9
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:18:33 +0000

    young slip evidence possibility touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
