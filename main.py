de588af93584a80e497da56e804b164a7af9aae2
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:20:22 +0000

    squeeze angry doubt must opposite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
