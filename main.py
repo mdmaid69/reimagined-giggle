8fd20ea09aace3108ad90c923eb1326cdbd33d3e
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:51:45 +0000

    relax hospital bother pleased adjective

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
