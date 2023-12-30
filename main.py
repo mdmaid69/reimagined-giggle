51264385b9dd7de66f20e4bda1d1caa8b2a2c274
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:07:24 +0000

    field reduction Tt Ll fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
