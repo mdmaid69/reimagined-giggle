0cb94a5ca0849fd78ded43723fb13eed2904e27b
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:19:15 +0000

    sign oil workupto yours wander

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
