099c905479de2e46e537dd1dcee03f33bd85ae86
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:32:49 +0000

    inform build holiday ride relief

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
