1763bb5c2c3c7da850e3e2e8b2ab57b0d81221e2
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:32:05 +0000

    common eye fly both judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
