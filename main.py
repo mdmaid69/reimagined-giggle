ffa3e11f39eb38c8ef93b083a321356930909b9a
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:36:26 +0000

    hang cry food lost lonely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import os
+def get_current_working_directory():
+        return os.getcwd()
