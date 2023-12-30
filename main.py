f31602779a65062cfe5bde11d78f6dfcfe6c9a93
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:04:08 +0000

    ever square clean below confused

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def convert_to_hex(n):
+        return hex(n)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
