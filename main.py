9eda5bfb3d43ec3a64e718bc960a958ffcfcea0e
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:27:32 +0000

    talk search keepoff waste attitude

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_hex(n):
+  def convert_to_hex(n):
         return hex(n)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def is_palindrome(s):
+        return s == s[::-1]
