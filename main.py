f4f0733a58494c21fe3d521585b342224ecfc3d5
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:59:40 +0000

    threaten lay shadow visit eager

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def convert_to_binary(n):
+        return bin(n)
