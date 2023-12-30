9f378c2f668dfa8735644c6bab2f55f28e647149
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:44:25 +0000

    lunch fit day treat space

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_power(work, time):
+        return work / time
