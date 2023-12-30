ed5bf1d185ae27ee65dceb12b475435a37cdfb5e
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:35:15 +0000

    this fairly over detail run

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import sys
-def exit_program():
-        sys.exit()
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
