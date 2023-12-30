0609fc8fa99a5cd05bced6c2c6f3cb329c482bd5
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:18:55 +0000

    substance whom somewhere succeed smoke

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
