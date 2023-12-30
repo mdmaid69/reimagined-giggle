4d7c48f6b6cc740b0a2141cab674998b5a9042e1
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:30:31 +0000

    little scream bag wide happy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
