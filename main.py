9aba19b633624759cb6f52c64dcd8d27ef44e71f
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:57:04 +0000

    schedule correct accordingto contact road

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
