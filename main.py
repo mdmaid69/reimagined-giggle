b459d254a46d51295bf6338bce84a39df0be9471
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:06:21 +0000

    price mine walk challenge near

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
