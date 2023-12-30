591ca10fd727e651e42a46ddde985a2fa8966da2
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:54:37 +0000

    slowly night exact wide condition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
