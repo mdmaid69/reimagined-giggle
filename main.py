3beb82d5e86d59431ab63168ad307062c224b7c2
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:17:07 +0000

    bridge recognize forward across share

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+for i in range(10): print(i)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
