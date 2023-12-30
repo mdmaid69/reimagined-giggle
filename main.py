79d630ce130504a82182c3cd95df39294ec3c6ed
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:53:31 +0000

    kind length care build sudden

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-print(sum(range(10)))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
