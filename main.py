6a2cc1c3ad356dda1546a6067082205fa8e83ca7
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:28:49 +0000

    breakinto turnover steep finish terrible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import collections
+def create_stack():
+        return collections.deque()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
