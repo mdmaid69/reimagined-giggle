61203869bd70e5ba73fe39c1b15fd289a67642b4
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:38:25 +0000

    pride avoid unusual any sleep

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
