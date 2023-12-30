580a7d1e936a2357cd471ff007c78f3f75610c18
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:00:23 +0000

    should hollow boil thinkover face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
