b3ea84a228f09b4454a3c0ffdbe123ada7fae5bd
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:45:24 +0000

    blue tone sale pen comeover

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def subtract_numbers(x, y):
-        return x - y
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import re
+print(re.match("h.*o", "hello world"))
