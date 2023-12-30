94ce3ec9a6c9f7a49aff123bd34d9b12e92912c4
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:09:30 +0000

    speech pain sometimes apart his

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
