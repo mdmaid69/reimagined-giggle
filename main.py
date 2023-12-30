96c3a5fe165e71838240c17fff8c620f05307460
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:35:13 +0000

    arrangement wave start trap failure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
