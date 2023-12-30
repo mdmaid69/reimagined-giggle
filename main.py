e0084e75e5a6377b62b2649be105246ec54fd9d9
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:52:45 +0000

    black profit burn suffer smooth

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_area_circle(r):
+        return 3.14 * r**2
