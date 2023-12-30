fc84237f210fb24dd3f96831c0edbdf254063677
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:42:05 +0000

    drive classroom remove name peace

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
