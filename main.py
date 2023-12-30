6b39c4bcf8512981514e20c80fe840e6a43f0b86
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:34:46 +0000

    wing perform Ff immediate giveout

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
