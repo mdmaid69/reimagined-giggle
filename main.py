e4028833f5fa81f2bc6a55715b5d5cae6abb17a6
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:35:11 +0000

    doctor calmly play firm hit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def convert_to_hex(n):
+        return hex(n)
+import array
+def get_array_typecode(array):
+        return array.typecode
