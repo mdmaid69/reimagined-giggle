c49f8e9e3870b568e044c020660d7684fa685c9a
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:35:28 +0000

    fail neat familiar Dd guess

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
