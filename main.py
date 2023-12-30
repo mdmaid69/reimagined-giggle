074e5169de73b89be0569e2ac82668b469f3fa74
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:44:31 +0000

    allow born sport me range

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
