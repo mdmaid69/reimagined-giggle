2b4affd923cdef6fd7be5715b9c17f2d324256b3
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:56:34 +0000

    similar opponent contribute road settle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import collections
+def create_priority_queue():
+        return collections.deque()
+def find_min(lst):
+        return min(lst)
