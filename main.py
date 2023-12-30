f048e64db38d4758eb2649c8be3bc0dd124d2833
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:06:30 +0000

    everything gas shoulder accidentally home

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-import collections
-def create_user_list():
-        return collections.UserList()
+def get_array_length(array):
+        return len(array)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
