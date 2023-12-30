1050afbfd125614c68daec98d781e8ecded45759
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:08:18 +0000

    confusing everything lookback(on) wrap collect

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
