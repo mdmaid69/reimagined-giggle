7e55589a9fec1af0eb4a8f37d0800b455a10b6d7
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:54:09 +0000

    under gate react Hh dear

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def find_min(numbers):
+        return min(numbers)
+import json
+print(json.dumps({"name": "John", "age": 30}))
