48a9d4ab3a4e9a9af8b1553d86899ec05f2b2096
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:11:04 +0000

    far game waste fear translate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
