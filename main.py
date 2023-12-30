90116e53aca8795dd10bf4859a14d13e44331d63
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:35:25 +0000

    charge record previous survive cruel

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+print([x**2 for x in range(10)])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
