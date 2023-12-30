ac8f8064d52fc7e015fcc1ec1818530f04e32fcc
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:06:20 +0000

    trust gas call insist custom

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-name = "Python"
-print("Hello,", name)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
