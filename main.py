b3fef481426f78314b399f9dc21152231bd031a8
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:25:13 +0000

    rise music pencil contrast believe

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
