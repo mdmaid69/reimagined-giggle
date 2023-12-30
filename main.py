3ce789e166ca038d1f3058d804ceacd13410d213
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:45:06 +0000

    agree unite giveup tense gift

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
