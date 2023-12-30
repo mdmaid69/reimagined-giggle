78e1e01d7ed4022043dc1fe97e8f9d9a8db5b7cf
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:22:58 +0000

    make during anxious then like

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def get_array_as_list(array):
+        return list(array)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
