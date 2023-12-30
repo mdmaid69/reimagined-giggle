a889af54b0dcaf00f41a6088ca99d9c8a9665c60
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:43:25 +0000

    value rain include confusing tend

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def print_python_version():
-        return sys.version
-import collections
-def create_queue():
-        return collections.deque()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
