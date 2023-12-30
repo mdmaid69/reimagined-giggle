dbd73efa81b721f163e3a32153ce9c90499e6402
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:05:16 +0000

    join prevent ignore goup trust

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
