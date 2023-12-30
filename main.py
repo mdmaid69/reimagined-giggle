786b4e292094ba2344a4e152e4616fb9618056c5
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:47:47 +0000

    pain scare port dull scream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
