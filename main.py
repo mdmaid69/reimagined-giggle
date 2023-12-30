62459ccb4e5254e93400be8c959a75a2b6cc9fe8
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:51:43 +0000

    snow cheque behave sit exploration

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
