25b0bc788543cf35ee0d4e2719214fadc1b0d314
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:46:53 +0000

    acceptable disgusting live prefer unhappy

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+for i in range(10): print(i)
