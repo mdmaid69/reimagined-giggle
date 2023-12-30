63ca01d6165d8c30326649240e169ee667c5dba5
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:41:20 +0000

    another building sleep getback alive

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  def remove_duplicates(lst):
+        return list(set(lst))
