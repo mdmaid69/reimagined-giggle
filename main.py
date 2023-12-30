3ab1a894b12e80ab57bd682d9bbdb60389e75233
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:06:42 +0000

    interruption second brief alone operate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
