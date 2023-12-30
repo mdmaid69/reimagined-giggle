bb9197f2f8be24cf55e614140a8307179f748d43
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:14:02 +0000

    announce trip argument boring rank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
