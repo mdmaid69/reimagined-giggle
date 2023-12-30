c0ea9935f111f8e73c75c39c416b24607dbb5ee2
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:06:16 +0000

    disapprove past Kk past accept

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
