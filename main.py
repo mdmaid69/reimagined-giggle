dd5a3f8fb166ac738d50a3b025bb56c786b82970
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:52:59 +0000

    best dark water sore real

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def get_array_as_int(array):
+        return int(array[0])
