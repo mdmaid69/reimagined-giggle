5ad63b23698060030b6b42680b1c83b0aa607a08
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:07:21 +0000

    takeaway relation tap mistake favourite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import random
 def generate_random_sample(population, k):
         return random.sample(population, k)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
