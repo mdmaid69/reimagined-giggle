3c208c38f5bb235503e7dfeaa9fb5790394b8cea
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:40:53 +0000

    holdout duty argue development drop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
