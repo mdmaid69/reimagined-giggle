b86c3abd15f92a8b33c9fe40ddd202f778c86e95
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:04:27 +0000

    hurry punishment appeal better exhibition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
