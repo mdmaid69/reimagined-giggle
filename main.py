9ef534bb8a9f5b914ce6c531a34df064fd8c7186
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:55:40 +0000

    gate hot arm odd gate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def multiply_numbers(x, y):
+        return x * y
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
