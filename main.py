0a96fce97f60c7e8a59e74632727a8d99de106af
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:24:36 +0000

    steal party which something pot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
