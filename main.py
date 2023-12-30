aedf9bcf62e0513ced48715015c55f6e77844f2e
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:33:37 +0000

    to tight reduce notice pot

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
