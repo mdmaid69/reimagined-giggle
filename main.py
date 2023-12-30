d8bd79846a104070bb58c8dcd1fcaed18637a333
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:23:04 +0000

    characteristic forgive accidentally interruption thorough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_item(array, i):
+        return array[i]
+  def square_number(x):
+        return x**2
