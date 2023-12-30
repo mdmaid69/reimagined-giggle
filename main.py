7785b0603fc612d80ee91c6a92d6e0d9cab411f3
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:40:15 +0000

    belong size sea move sing

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
