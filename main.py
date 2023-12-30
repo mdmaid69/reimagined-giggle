058d9f0decd12ca593773245e90a360185f00572
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:25:42 +0000

    ashamed train floor genuine physical

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
