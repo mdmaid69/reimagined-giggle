e030b067820ce1b459b21e0519b24533bab260ef
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:27:49 +0000

    crash getaway tape how say

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_volume(length, width, height):
+        return length * width * height
