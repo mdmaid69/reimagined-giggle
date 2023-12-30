e6dce040b1831f8c5958fcd28be659afad14f898
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:31:25 +0000

    science really excuse moon fat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
