40a80ebb468b6a7b7dfdc2024fe3ba50c0c2d9f4
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:22:32 +0000

    division height separate loud entry

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+for i in range(5):
+        print(i)
+def square_number(x):
+        return x**2
