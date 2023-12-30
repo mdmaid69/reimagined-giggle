48c1be42077f97a4994fe61e92b13637cbfb4946
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:29:54 +0000

    getoff appeal sale release Ee

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
