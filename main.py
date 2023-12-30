7135cdf9c66bfbd15cffa0c13e87675459abc2af
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:51:18 +0000

    post develop experience patience instruction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
