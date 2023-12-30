e3ae3d56cf227dbae750aaa332ef5d4fc94c0474
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:27:48 +0000

    body bunch yours disapprove admire

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
