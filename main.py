cad1895970724fe71663fc2cb92cdd2889650690
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:44:33 +0000

    read laugh gooff foot otherwise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
