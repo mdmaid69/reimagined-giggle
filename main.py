0c9ac58c8948f9ff364a7854e19495904355cfdc
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:22:49 +0000

    thinkover reason invitation cut back

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import os
+print(os.getcwd())
+def find_max(lst):
+        return max(lst)
