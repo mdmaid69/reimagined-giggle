1ab0b43fd327222714a8713a7fcebce3e9051524
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:37:55 +0000

    save react song harm test

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_perpetuity(payment, rate):
+        return payment / rate
