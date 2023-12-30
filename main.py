5ea105b1f6fc5641a2d7e6fedfa8d7439f023202
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:37:47 +0000

    573entertainment print bottom notice downstairs

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_max(lst):
-        return max(lst)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
