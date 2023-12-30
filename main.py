82f4cb4d062a1b41c133aff91ca33cb240e738d8
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:55:24 +0000

    friend sight material Rr breakinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
