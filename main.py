1e17c34412de4ad9a7eaeac68199241f02814dad
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:26:43 +0000

    we travel experience attempt cold

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def add_numbers(a, b):
+        return a + b
