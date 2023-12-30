7a22600014de29cd70c385c73da04ac73602b82f
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:42:24 +0000

    somewhere closely pattern about drawing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
