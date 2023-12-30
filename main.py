679a21b9dfa10d97cd4d47aa31ee2d40a06f7a9e
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:46:01 +0000

    obvious swim estimate bring shoot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def split_path(path):
+        return os.path.split(path)
