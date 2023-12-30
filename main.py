11ceeb5201bca9cb2ed8644a476789c08b427588
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:30:18 +0000

    human soon no yet stiff

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
