eb497a1758e3ae0352fdbb49f5ddaf43332ffb90
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:37:42 +0000

    duty listenfor laugh object regional

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
