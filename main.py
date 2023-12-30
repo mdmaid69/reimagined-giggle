6b93242609c0c56800718779a038122991c282a5
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:18:16 +0000

    forget everything funny bother together

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-print(math.pi)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
