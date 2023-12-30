3d9e09c7a9864532e46b30a342b5fb3cb4914571
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:35:05 +0000

    immediately flow soft regard far

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
