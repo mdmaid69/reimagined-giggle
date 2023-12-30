c76ea40bdf72178197b6c5975fce22fe0cfe3bd4
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:25:28 +0000

    pool dick after object concerned

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def convert_to_octal(n):
+        return oct(n)
