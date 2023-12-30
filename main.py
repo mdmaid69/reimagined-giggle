d8d235e5ab5933447cfd0e7df1efe9ccacdc6782
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:10:39 +0000

    runinto later present correct tune

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
