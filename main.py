def0fd64407e8c90855d9fa9c8599b3d86021364
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:51:49 +0000

    score disapprove comeback he to

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
