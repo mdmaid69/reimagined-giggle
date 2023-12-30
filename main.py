14a936f6f28e98b1cba141ce78962dfbfec3c368
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:33:48 +0000

    runover scrap impossible tread pen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
