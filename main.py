cdab04f6f464aa8049c6fc20a4c3a214eeba7da2
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:24:02 +0000

    flavour translate light punishment long

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+print(os.getcwd())
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
