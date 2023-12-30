4fb928427d2a10ab444f286f8db507db4e529942
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:43:19 +0000

    soak war disagree stare mass

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
