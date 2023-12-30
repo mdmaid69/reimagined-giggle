332b3424c7b7acbcaf6acbd4315bb88e2d76e035
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:34:40 +0000

    wash nearly clearly Ss respect

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
