2b648d17ca3aa781b174d725604ad5f921bdb42a
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:38:54 +0000

    myself bridge murder pile cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
