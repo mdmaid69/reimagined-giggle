e5ebd42f74c5b004ec4d8822e16336fde6669b84
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:54:00 +0000

    rich other together name difference

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_base_name(path):
+        return os.path.basename(path)
+  def calculate_area_rectangle(l, w):
+        return l * w
