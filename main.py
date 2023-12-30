1d9e2bd70628e0c39850576ed723e14e480b323e
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:28:21 +0000

    respect mess firm almost now

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
         plt.show()
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def reverse_string(s):
+        return s[::-1]
