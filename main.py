3a2de25af89beb3211e733cb56541e5164dbb01a
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:34:06 +0000

    correction be quantity pay wherever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def change_working_directory(path):
+        os.chdir(path)
