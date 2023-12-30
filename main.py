6ba42416965f778cd87f0621f3f7ea6059c3b543
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:27:13 +0000

    promise play comment thirsty pretend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def count_words(sentence):
+        return len(sentence.split())
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
