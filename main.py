12612533b819e8182aa9870234af64072be44841
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:12:12 +0000

    row exploration play whisper advantage

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
