54217032eaa893750dec72ac9ae84b07612a1982
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:46:54 +0000

    mind during storm probably handle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
