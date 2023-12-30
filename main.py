f717d52bb3e829e643b76361fa8121d5e2275f2f
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:20:52 +0000

    treat defend suggest about separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
