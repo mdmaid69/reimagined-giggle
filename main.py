5f1055fdc30dc5ca13bea040c7899fe87c9b9933
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:29:01 +0000

    promise off of create blame

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def square_number(x):
+        return x**2
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
