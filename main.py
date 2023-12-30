8ce46c7e89d0fde557da24cc78288bfbc3609298
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:15:14 +0000

    holdout interest wind radio tight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
