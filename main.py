9ffb341147a0d6ac6cfdeba9780ac25720185ce1
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:34:55 +0000

    object loud nerve situation church

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
