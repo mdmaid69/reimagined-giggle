3777e8d6d409ff07016bd488172955dc825eaeb2
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:29:56 +0000

    book visit substance depend survive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
