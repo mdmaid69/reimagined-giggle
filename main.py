85f62c69da0162d88123bab1bfed660dc0f81ac1
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:15:00 +0000

    complicated dinner production sleep fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
