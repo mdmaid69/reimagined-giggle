e9a36d96cea220cff3ad47b85170584662de7692
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:35:57 +0000

    exchange concerning now us smooth

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+from collections import Counter
+print(Counter("hello world"))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
