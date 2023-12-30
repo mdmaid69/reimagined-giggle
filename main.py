da8bc857e5728167ba8a2bf6b5b76d400c7df17e
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:35:32 +0000

    control rare permission injury woman

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import time
-def get_current_time():
-        return time.time()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
