5768de1292df7f26053150cca0a7bd6c02c1ef88
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:36:17 +0000

    important tell fence even grand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_typecode(array):
+        return array.typecode
