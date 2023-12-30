1be33b4e8108ecf62639de711da4700a49e27ae8
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:42:11 +0000

    relative rather circle holdout snow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def is_palindrome(s):
+        return s == s[::-1]
+import array
+def get_array_as_list(array):
+        return list(array)
