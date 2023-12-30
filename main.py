42d504832264ecb2de6bcc43c32172cb175c51df
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:08:31 +0000

    pool turndown Vv meal mention

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+name = "Python"
+print("Hello,", name)
