db3b5aa6a04e77bd17ad1bf2362d795d03a7985e
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:43:18 +0000

    protection generous murder exit strict

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def multiply_numbers(x, y):
+        return x * y
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
