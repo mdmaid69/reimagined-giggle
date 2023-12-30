d12b26ee3efdc73230d1166f6ac7af7ab020ad63
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:30:42 +0000

    train recovery idea public vision

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
