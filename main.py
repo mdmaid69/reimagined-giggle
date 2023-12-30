ffeaeff367638143b5a5bc56a2fb1fc0b824cd68
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:32:11 +0000

    behaviour practical date compare always

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
