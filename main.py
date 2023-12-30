fc6a463e4e96e8439385d93dc6b29be43ecf2bf7
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:20:29 +0000

    lie transport style story go

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def divide_numbers(x, y):
+        return x / y
