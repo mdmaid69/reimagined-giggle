ef50a36ca8cb61d4acff39e27b3439d6805146c7
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:08:50 +0000

    stage even reserve spot represent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def get_array_as_set(array):
+        return set(array)
+def convert_to_octal(n):
+        return oct(n)
