eec919395db78136eebca012dd20b60cb0f706d6
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:59:21 +0000

    store turnover tear 179bite frequent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
