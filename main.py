e51362188bec810f5d3e4a742322e0742606b65c
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:04:12 +0000

    flat blind shine collect borrow

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys
-print(sys.version)
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
