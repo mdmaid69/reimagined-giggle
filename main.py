6c884be42fd0234885cb0c817597ab3dc36fb479
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:23:11 +0000

    taste well frighten turnover solve

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
