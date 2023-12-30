3647b2228314a461a861bf99da3179b848b85dce
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:49:22 +0000

    building interruption creature point copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_block_size(file_name):
         return os.stat(file_name).st_blksize
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import sys
+def print_python_version():
+        return sys.version
