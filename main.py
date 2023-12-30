d5a8edb44fc57685b7f0671358e49b53fc0bc5d2
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:18:12 +0000

    confused fetch expensive city journey

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size_in_bytes(file_name):
+  def get_file_size(file_name):
         return os.stat(file_name).st_size
