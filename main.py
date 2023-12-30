347c2b56c849676595766e29af2f753ca39edfe5
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:34:53 +0000

    flight result slice later control

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
