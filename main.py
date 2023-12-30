5744e4d1c90941c9cebb29880f3aba7dfa872f9b
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:12:46 +0000

    swing plain late separate bringup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
