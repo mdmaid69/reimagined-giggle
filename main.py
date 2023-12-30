d39173749df0a37538a90f61edb1da705da90c8d
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:43:11 +0000

    future earn over have your

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  def reverse_list(lst):
-        return lst[::-1]
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
