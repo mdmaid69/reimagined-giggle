4f602f396f2185de6bb1ea4d882f04f919ab8bc1
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:18:39 +0000

    like until argue thinkthrough themselves

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_pressure(force, area):
+        return force / area
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
