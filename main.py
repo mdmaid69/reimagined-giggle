613f655e1b67ab3a22583a2bccfdbcff106c855c
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:07:31 +0000

    mention birth innocent read gooff

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
