7d2e87a3e8224466b3d7b77420188e5cd5249696
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:39:28 +0000

    please from zero crisis scream

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def get_array_as_set(array):
+        return set(array)
