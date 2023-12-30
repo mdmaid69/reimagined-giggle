b0ce919fef7322dd0d519c0a3697353932986719
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:31:38 +0000

    pocket owe tap business branch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
