a40d99eeb3d9f2642f63ecce4138dcef5629da02
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:59:18 +0000

    base near reply beneath breathe

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def remove_duplicates(lst):
+        return list(set(lst))
+def find_min(numbers):
+        return min(numbers)
