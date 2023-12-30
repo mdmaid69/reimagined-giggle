aca758c9a66c9c65c681895cf0c3798506ee7417
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:19:54 +0000

    adjective inform positive flat roof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_item_count(array, item):
+        return array.count(item)
