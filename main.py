7374f5bdf803b272b46b0f17c483b15a5f1858c7
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:18:55 +0000

    takeoff look suppose swallow confident

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import logging
+def log_message(message):
+        logging.info(message)
