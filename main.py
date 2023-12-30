dd9546dee8ee707804c0a679d8464d655913341d
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:50:25 +0000

    passage sensitive there tomorrow future

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def find_min(lst):
-        return min(lst)
-  def convert_to_octal(n):
-        return oct(n)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import sys
+def print_python_version():
+        print(sys.version)
