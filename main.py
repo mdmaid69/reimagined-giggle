c3a2197146af8407df90876417b12e02e863b462
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:35:41 +0000

    down sea protest practical more

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
