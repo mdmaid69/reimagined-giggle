431e75d6d7b87fde48e4c230b35e6da00c4f792f
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:21:22 +0000

    comment ignore spare press whichever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
