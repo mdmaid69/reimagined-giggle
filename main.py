51bdba4ec8f6525438bc7061d16b3d156d67cf11
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:43:35 +0000

    clean refuse type tape politics

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
