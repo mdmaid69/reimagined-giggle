0a49c330c1b771f8d6512493a120459932b5ddf2
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:56:23 +0000

    decision Rr bright talk view

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
