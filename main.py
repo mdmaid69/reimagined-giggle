1525f423854dc946b0a567368599305a4a0cb9e2
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:59:46 +0000

    study free public average foreign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+def find_min(lst):
+        return min(lst)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
