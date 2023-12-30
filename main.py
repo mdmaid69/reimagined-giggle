707576cd99da6d757695a091617a0f5653fdafde
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:23:06 +0000

    rain programme so finish 567enemy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def remove_duplicates(lst):
-        return list(set(lst))
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
