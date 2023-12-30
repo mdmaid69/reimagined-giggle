994af93bab5cb8f37d1ff25f5900bbdba6bc4ccf
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:09:16 +0000

    deep just depend class breakfast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
