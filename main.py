504100d73145cd9ef8dd103947196d63889fa76d
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:23:38 +0000

    run action spoil smile private

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import os
+print(os.getcwd())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
