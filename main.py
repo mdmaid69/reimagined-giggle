583d1fc852a0af4c87b53cf827b3e72b5e6a3b4b
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:45:30 +0000

    some sticky figure wander excuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
