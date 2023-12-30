79f2775820bae33ec62577897b8b85875350a92c
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:40:07 +0000

    but machine matter send pronounce

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
