9f29619c7fae075fdc58c96b243021947321a25f
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:28:07 +0000

    refer promise method among performance

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
