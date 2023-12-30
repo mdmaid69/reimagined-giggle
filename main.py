b7c279ce1c95ab4f0b13afe153b000991424be58
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:55:11 +0000

    over town bunch report bunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_float(array):
+        return float(array[0])
