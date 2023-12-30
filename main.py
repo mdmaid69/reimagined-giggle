5359e42191ba17ebd5ff0291e0dbfc2f04246ba9
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:56:37 +0000

    without into he fight Zz

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import datetime
+print(datetime.datetime.now())
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
