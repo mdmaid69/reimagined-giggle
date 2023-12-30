980e0ee8cf41aded90f705de2aae73989e5a87c6
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:06:46 +0000

    sail comment word bury neither

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
