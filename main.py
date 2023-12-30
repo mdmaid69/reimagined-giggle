b89476eaa24703564f112b3e299e02b3fad8ec8f
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:34:21 +0000

    arm start punish journey formal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_volume(length, width, height):
-        return length * width * height
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
