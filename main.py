870cf60041c4e1031c3918aefc236aa9ce384fba
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:05:02 +0000

    dull proper force serious winter

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
