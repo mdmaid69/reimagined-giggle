85b7ac016ade9f5771c2d0fd29c72fd4d8a073d6
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:54:16 +0000

    fat my both threaten check

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+print(sum(range(10)))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
