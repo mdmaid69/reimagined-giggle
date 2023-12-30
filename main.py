e4f4cf8daeceffc41116c80bbba0e9ee04b80623
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:24:43 +0000

    runaway it arrangement private spend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def convert_to_hex(n):
+        return hex(n)
