01a68ddf54ebdfa823484934adc2b27e872c2a60
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:39:09 +0000

    runaway juice give example brave

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def reverse_string(s):
+        return s[::-1]
