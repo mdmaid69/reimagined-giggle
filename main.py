c531eb11aa0b36c7b31209658b78994b3e81b843
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:57:42 +0000

    innocent argue brief our complete

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+name = "Python"
+print("Hello,", name)
