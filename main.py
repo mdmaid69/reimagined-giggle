32e7fa55afafb6b10046fa566902fac97b6fa5ac
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:30:11 +0000

    dig respect tax mouth shock

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import platform
-def get_python_version():
-        return platform.python_version()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def cube_number(x):
+        return x**3
