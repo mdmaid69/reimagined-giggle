46e5ddab6c17a40cbb7f38202668d310f1c60262
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:22:20 +0000

    believe ear uncomfortable rent tired

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def reverse_list(lst):
+        return lst[::-1]
