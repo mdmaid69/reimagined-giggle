e42e2227f9ab668e5c237a27cc79aec711ffd13c
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:44:54 +0000

    invite listen our recent circle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
