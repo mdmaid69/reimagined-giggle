7d2960044d558c8e2ccb749714e023b53b87d20a
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:07:42 +0000

    include quality congratulate powerful major

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
