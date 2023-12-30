cde3b3fd9d387a712bbc78170fcdef598d234343
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:50:37 +0000

    think pinch influence scrape improve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_typecode(array):
+        return array.typecode
