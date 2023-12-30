12b30b0a66985f21267dfe2f4048f99cfff133fc
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:09:26 +0000

    win appeal block lift dance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def add_numbers(x, y):
+        return x + y
