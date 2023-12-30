4686ad8092ce4c9b35c0df20dbb43aff66e74393
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:39:46 +0000

    rare bus heart debt afford

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
