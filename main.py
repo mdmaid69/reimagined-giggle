5bbfebb8c387ce2ef6c8e34894ac2770b0492235
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:27:29 +0000

    lookup disappear blue fly soak

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
