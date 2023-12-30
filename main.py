951a6d2515948f63804369e130e8d097f760736e
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:25:20 +0000

    argue drive fear Oo form

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
