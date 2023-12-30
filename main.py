2ddd6f24d5c25f00f4f7e02f578be6ad00e55449
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:05:27 +0000

    have sport hurt stamp muscle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
