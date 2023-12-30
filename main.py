b2143e52d5088bc288eaad635efc6d57f41d5caf
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:34:08 +0000

    single autumn key what maximum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
