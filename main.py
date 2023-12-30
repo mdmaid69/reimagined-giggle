7f402b9cd33122a567f9283180c6fed92b3b992b
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:13:02 +0000

    black clue sound handle cost

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
