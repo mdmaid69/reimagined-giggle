32c73c68989b0993234ded27f672d38bdd33a7c0
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:30:42 +0000

    aim delight before destroy spite

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def calculate_speed(distance, time):
+        return distance / time
