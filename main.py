5c1382b32840486158176842532bd4d9167d6f49
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:45:33 +0000

    perform should hollow area wipe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
