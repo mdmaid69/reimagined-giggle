c24bd29fd51b35ae047a201b430eda7d410406c4
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:02:32 +0000

    relax mention best well convenient

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
