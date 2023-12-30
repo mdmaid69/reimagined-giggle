5604dd39c52fac051d48e1dcdfe0f6096242b85d
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:14:28 +0000

    tax temperature instead somewhere answer

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def divide_numbers(x, y):
-        return x / y
+import array
+def get_array_as_int(array):
+        return int(array[0])
+i = 0
+while i < 5:
+        print(i)
+        i += 1
