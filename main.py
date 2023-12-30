33d88238af28239dfb700f91bab2a1b9b3e5a911
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:01:34 +0000

    ahead pinch substance fall corner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
