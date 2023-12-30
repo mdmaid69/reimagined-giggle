6d04be29cce332806a1ae7c0086601e0b422016d
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:20:15 +0000

    makeup ball myself hurt star

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
