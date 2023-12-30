37d7b023600bb9476ac88f842153cffb464567b1
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:13:21 +0000

    ring pure exchange pass embarrassed

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def square_number(x):
-        return x**2
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_max(lst):
+        return max(lst)
