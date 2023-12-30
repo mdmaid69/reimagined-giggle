110cb27ed1a5fec918cde21d0a8cd02b3998564a
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:27:51 +0000

    course confuse upset that label

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def get_array_item(array, i):
+        return array[i]
