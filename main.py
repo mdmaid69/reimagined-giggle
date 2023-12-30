c71e8cae9ce9f67a76011b15a2f1f95ba5cd2c41
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:11:36 +0000

    chin top depend solve pretend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def is_palindrome(s):
-        return s == s[::-1]
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
