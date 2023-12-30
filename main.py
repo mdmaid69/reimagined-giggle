926ded77b176ae3eae9a2936dbc308b7ef1492eb
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:15:47 +0000

    fat instruction debt taste market

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import math
+print(math.pi)
+def count_elements(lst):
+        return len(lst)
