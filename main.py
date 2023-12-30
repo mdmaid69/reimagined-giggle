954833a499837adbe74e3a73af22775279130694
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:55:30 +0000

    giveup gift apply pause tough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
