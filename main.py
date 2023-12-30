17c26f36fef55f8f775bb374ebf39829cbf6fb4e
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:48:16 +0000

    wear declare trust genuine fetch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
