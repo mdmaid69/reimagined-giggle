3348253c5f93674ddd2d4ae58435798cc9c4bdad
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:08:55 +0000

    centimetre bad split voice whenever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
