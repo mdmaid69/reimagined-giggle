87594eb3e39b1e8fc747522a1411063ddb7061e3
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:14:33 +0000

    challenge develop same contribute mainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_as_tuple(array):
+        return tuple(array)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
