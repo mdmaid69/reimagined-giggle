bf7286bb83f0a18ca214992a3f7692bba5912488
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:57:34 +0000

    fact keepfrom exaggerate even lookup

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
