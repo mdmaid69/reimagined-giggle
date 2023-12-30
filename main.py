2b5e296cbabeb4a1587bb2bb702034ba32b70a0c
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:15:05 +0000

    brain equal mix reach flat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_max(lst):
+        return max(lst)
