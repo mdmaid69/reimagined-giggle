54f1ba692de1a698a3bf2d80cc1b04a4be839750
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:27:49 +0000

    game backwards fear somewhere hurt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
