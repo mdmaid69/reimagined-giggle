57793e715df56bee0aabbf08853b76e15f4f5cde
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:54:05 +0000

    collect mean march label behind

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-def calculate_power(work, time):
-        return work / time
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
