d54aff42846f5df02d54231c97517ec018ab7574
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:18:52 +0000

    choose dead attract Cc take

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
