d7eadfacc0e885449b64c18820fcad4e1ad0288e
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:44:13 +0000

    seat ours power flight close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
