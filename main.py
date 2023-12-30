ca33f7f860934a67b310acd892264d3f3e632ea9
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:35:28 +0000

    rest anywhere excuse family behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
