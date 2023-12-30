32c92808325d33da969d258493a6b28483482234
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:28:28 +0000

    shop slight sink record pure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
