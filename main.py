94fa53fd666f7d5462b2e8ec194fdef8b5ad3f96
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:57:20 +0000

    summer trap the complain basic

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
