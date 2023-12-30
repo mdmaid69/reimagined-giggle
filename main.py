1b019a0532622418b75e8b179f087f1af6b70bcf
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:04:04 +0000

    suggest tend ground roll film

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def sort_list(lst):
+        return sorted(lst)
+import datetime
+def get_current_date():
+        return datetime.date.today()
