e24fb4fcace8a1f7c644010815fb053f7488d1f6
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:45:52 +0000

    speech band welcome simple heavy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
