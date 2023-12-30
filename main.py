5738731c7afdbea4b8c4b3fb0da079396bb1e6f8
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:41:51 +0000

    man sticky nothing address government

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
