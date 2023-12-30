f92078417b199b82e1ca11c8fb67054d95d0f99a
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:13:21 +0000

    region maybe nature match drawing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
