5fdcdc3bcd0862879fad70a59f9c7923e420fb08
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:26:29 +0000

    married beat his real part

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_area_circle(r):
+        return 3.14 * r**2
