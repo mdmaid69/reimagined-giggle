f0b74cf026f386b6465f0729c8d49ee7552a2117
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:32:42 +0000

    minimum branch peace innocent cost

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def is_even(n):
+        return n % 2 == 0
