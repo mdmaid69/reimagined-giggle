74d44ad92ce094c9d01cfab62b5bf9e1fa29b717
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:21:43 +0000

    hate consist comeout start listenfor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def count_characters(sentence):
+        return len(sentence)
+def find_max(lst):
+        return max(lst)
