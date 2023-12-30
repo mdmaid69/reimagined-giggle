79c33f439deefbae3c2d41a19ebdad933f34584d
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:01:39 +0000

    goaway prepare niggaacademy copy freeze

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  def reverse_list(lst):
-        return lst[::-1]
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import platform
+def get_python_version():
+        return platform.python_version()
