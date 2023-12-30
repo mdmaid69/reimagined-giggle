b25579e05af5c772a4a58b6e40e7379c2185e744
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:52:08 +0000

    crime but cheque air keepout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+for i in range(10): print(i)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
