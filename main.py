a4a31a3224d5fff8e8cdf63b4a9ab6c6ee7cf152
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:57:44 +0000

    environment sick rather shrink play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
