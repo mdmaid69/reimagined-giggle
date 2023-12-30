a89a321ca8d9ce17591c8358af12b70d218427aa
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:41:31 +0000

    remark taste square giveout match

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
