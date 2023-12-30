b62024812abfcf69ddddf7cdb019f6aa05ef21fe
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:25:46 +0000

    sink computer obtain us attract

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-def get_current_time():
-        return time.time()
-def calculate_density(mass, volume):
-        return mass / volume
+def find_max(lst):
+        return max(lst)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
