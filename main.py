4a59c7e8c481e9fbb096a06b88cf56e9070231d0
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:54:24 +0000

    bad card notice standard begin

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-print([x**2 for x in range(10)])
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
