60f77bbd196f0213d31242eba8849d0f4ce0e9e0
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:08:56 +0000

    embarrassed dry fold takeout hit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import logging
-def log_message(message):
-        logging.info(message)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
