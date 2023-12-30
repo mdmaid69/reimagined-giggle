ac173eb6ff65fc671452198d2c01ecf42ea2330a
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:15:00 +0000

    traffic board hello comeout snow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
