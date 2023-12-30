8d4bbd0d4e10b827725c76a65dff7d85e3998258
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:43:30 +0000

    fail afraid on factory load

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def get_array_length(array):
-        return len(array)
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
