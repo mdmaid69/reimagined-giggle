36091270c616d7a35be241a86c1c71c6913d660d
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:53:24 +0000

    Mm recommend exhibition wait kill

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  def sort_list(lst):
+        return sorted(lst)
