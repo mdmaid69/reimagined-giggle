363b666d2694a523c8df9e743c6491f807e553ab
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:08:35 +0000

    pull defeat her ashamed play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  def count_elements(lst):
+        return len(lst)
