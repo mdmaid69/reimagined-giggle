c8a1e708f2dbe25a6cb9ff2af3e40f51f61fc43d
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:07:35 +0000

    sing mark herself black report

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
