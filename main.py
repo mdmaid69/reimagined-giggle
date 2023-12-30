c77bf3ee07baf2a908c622cce2294e4dd532fd25
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:52:54 +0000

    Pp bound Ww share gradually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
