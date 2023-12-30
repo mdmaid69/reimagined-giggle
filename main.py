e7ed6ed79db19ba2838200f54f33acfc64327486
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:34:28 +0000

    start happen second clue expect

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def cube_number(x):
-        return x**3
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def convert_to_binary(n):
+        return bin(n)
+  def subtract_numbers(x, y):
+        return x - y
