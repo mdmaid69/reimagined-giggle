a01a87cff277e3528c3b7ac42edb260d561c4b72
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:12:41 +0000

    Ww service one offer able

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
