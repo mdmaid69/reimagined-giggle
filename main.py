19640c10180ddb4d3e3efe901c39a7812409f7d1
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:13:45 +0000

    bottom makeup godown past anywhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
