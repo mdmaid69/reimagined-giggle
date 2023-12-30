cb8d3fd75acd5b05cf9d28f613f9ba2e7fd73dc9
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:41:23 +0000

    classroom beside wire react takeout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import random
+print(random.randint(0, 100))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
