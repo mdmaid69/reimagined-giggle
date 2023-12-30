92d66d5e9363cd6e65af8eca2e83aec5fd8b94b6
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:14:09 +0000

    funny minimum thin dick slip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
