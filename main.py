70df0d79556885c64f1cc092256d70e61455ce48
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:56:53 +0000

    foot fail schedule wish challenge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
