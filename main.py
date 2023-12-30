307895e27800c9f4aeee2de9440edbb6265038c8
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:01:26 +0000

    drink see everybody takeout put

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
