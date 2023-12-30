544aad46fc895011f2c06090acebe4cb6490bd9f
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:41:27 +0000

    hand Cc 568energy cook march

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def calculate_pressure(force, area):
-        return force / area
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
