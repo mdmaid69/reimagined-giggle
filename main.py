08ffaa54a415c9fd9f5106ff530c573e23a2f864
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:53:18 +0000

    use bottom delicate habit mean

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
