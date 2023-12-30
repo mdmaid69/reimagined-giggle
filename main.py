42947356db5e83fc0040e21b56e6608055c71066
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:41:37 +0000

    marry thanks reason content clear

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-print(sum(range(10)))
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
