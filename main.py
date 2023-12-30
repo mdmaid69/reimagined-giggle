5a16d7cf0da1af1c6a77b482578e327f8f2476d6
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:09:05 +0000

    clear firm standard eat rough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
