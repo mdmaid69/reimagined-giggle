bb91cc9abd3274a1fabb8e0f95580e384bbcffe6
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:33:52 +0000

    swim mix both cool march

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
 import array
-def get_list_from_array(array):
-        return array.tolist()
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def get_array_buffer_info(array):
+        return array.buffer_info()
