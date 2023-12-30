356fcb11d6711de1a08857a6be700a5e17e1af37
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:59:08 +0000

    their shopping word rubbish basically

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
