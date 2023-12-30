03947e815d0af3b8083dfee9b71a1894804f3023
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:41:42 +0000

    handle concerning properly 567enemy complain

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
