e157033337d3dae9d26d622b5ee2e9365512d6e6
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:22:11 +0000

    exercise involve create somewhere name

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
