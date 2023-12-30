9ec1eb6dc0a013da6705db42d0272902c7223722
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:11:49 +0000

    pure note long date combine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_current_working_directory():
+        return os.getcwd()
