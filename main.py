f816619473b2b16834d2e879f73fbd15105cda71
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:26:38 +0000

    lucky mark profit float contact

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def find_unique_words(sentence):
-        return set(sentence.split())
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
