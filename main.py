4c1d0146fe324d6a135437025a8558148d5b20eb
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:55:22 +0000

    old national much mean serve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
