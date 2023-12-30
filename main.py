3c88d4becc69eb9237c7c429da08933a9abd2243
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:08:16 +0000

    sky idea answer show weak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import array
+def get_array_as_int(array):
+        return int(array[0])
+from collections import Counter
+print(Counter("hello world"))
