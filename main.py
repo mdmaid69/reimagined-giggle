6e8cb31a5b5e5d8f35e9f4e4b9ccde9bb9ec40d2
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:49:07 +0000

    lend month stick represent together

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
