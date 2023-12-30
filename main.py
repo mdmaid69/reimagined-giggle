08a79c1744db799d5c96af5700f4697bf03dd469
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:50:09 +0000

    568energy ticket every future limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
