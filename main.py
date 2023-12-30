e793f4ed796e1eab0085ec5293778293be31e5af
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:24:39 +0000

    pin pass tall 179bite pleasant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
