af52fc7a610624519036791e0858bb3f5d61bbbb
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:10:32 +0000

    free wherever deliberate reasonable Cc

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
