a468b12236ae6e7f83f38b2e0cf3ad5692b026c7
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:09:34 +0000

    join shut play practise once

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
