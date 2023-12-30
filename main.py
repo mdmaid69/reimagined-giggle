16e0d771f376532e3bece6cbf0d1364335574081
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:03:53 +0000

    bringdown feel deliberate actually city

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def get_array_item_count(array, item):
+        return array.count(item)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
