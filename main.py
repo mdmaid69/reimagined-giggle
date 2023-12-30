a1b9a655a8af4ab059b74891966c42dea0e5b33d
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:24:44 +0000

    thin bread behaviour sea another

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
