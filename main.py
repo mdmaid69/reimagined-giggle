0364bdde82df7b5998a4a282b3b7befc420479b7
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:32:42 +0000

    probably partner language public fairly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+for i in range(10): print(i)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
