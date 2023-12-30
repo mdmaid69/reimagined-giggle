2396093ae421c74884aee8cb216562d9cdeb4696
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:34:54 +0000

    load moment then temper breakup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-print(re.match("h.*o", "hello world"))
+  def delete_file(file_name):
+        os.remove(file_name)
