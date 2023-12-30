7c52e5d585b7b9069b7c65f5ca004d8c36d37291
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:17:34 +0000

    live drive basis home unhappy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
