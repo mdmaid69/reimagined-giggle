9c51e55a273c3269cefbf512c2958efd6884b791
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:44:07 +0000

    may pick criticism field thank

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import sys
-  def get_python_version():
-        return sys.version
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def convert_to_hex(n):
+        return hex(n)
