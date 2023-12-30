8e45503a0baac7deb6293d30231959cfc09c8872
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:35:20 +0000

    quit beginning bringup subject curious

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import array
+def get_array_as_str(array):
+        return str(array)
