ef98c49883b69298fbeb63c6615099d2bb2c2910
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:01:33 +0000

    post near foreign busy teach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def add_numbers(x, y):
+        return x + y
