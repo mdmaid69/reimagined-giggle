f09c2adce24a2d02a2f23986504b9ab45654a567
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:46:34 +0000

    share aware settle tonight tell

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  def sort_list(lst):
-        return sorted(lst)
+def sort_numbers(numbers):
+        return sorted(numbers)
+def remove_duplicates(lst):
+        return list(set(lst))
