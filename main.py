82323bcf6daf1c8c5e1d188dc10c6fd42e7f5c45
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:29:06 +0000

    regional own offer equal introduce

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
