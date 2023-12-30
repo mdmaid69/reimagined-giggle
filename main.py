b9adbe15f109ad341761db793eeaf07c8cccb5f7
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:56:30 +0000

    lucky process smile comeup flood

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
