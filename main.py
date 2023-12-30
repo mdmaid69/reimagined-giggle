e1d0ec81c7fe9f0abfb6d8746ea4b0fb4ffbc59d
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:17:14 +0000

    ahead plenty excited concern fat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
