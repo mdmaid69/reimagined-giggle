c130199f25aa6f946f2e204582df00705c4320ad
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:18:52 +0000

    accident whenever callup ahead mind

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+for i in range(10): print(i)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
