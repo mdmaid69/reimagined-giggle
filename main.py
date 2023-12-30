63080444faacbd4080912e7527671ccc4cd9a0cd
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:35:14 +0000

    tip dream swallow uncertain willing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
