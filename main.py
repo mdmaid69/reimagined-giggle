e951edbc02a3df9d9e472961c681f3f49d32bb76
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:25:02 +0000

    free style bringforward laugh brother

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
