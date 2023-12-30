219e145502db171eb0a0bb3ff77ce925cc69130b
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:10:46 +0000

    warn tie plenty exchange human

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
