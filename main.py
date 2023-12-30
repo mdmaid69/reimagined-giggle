2f2b653e3e5d2992d2590a56b138f3806833564b
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:04:23 +0000

    pick ear regularly church gentle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
