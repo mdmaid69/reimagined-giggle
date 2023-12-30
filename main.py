7e1fdd780ad2efc47f2739caefa5bd6334d2c3d4
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:07:02 +0000

    weak which bottom while suggestion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_float(array):
+        return float(array[0])
