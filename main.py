0ddb2bb82cc1e24eb10512a57e789c57009b98b1
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:15:27 +0000

    getout permission popular workupto thin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  def remove_duplicates(lst):
+        return list(set(lst))
