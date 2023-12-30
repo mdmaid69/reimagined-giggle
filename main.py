a60b34f4acbf912fdf4582d12322baf889448fe8
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:39:46 +0000

    right various bin lend front

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
