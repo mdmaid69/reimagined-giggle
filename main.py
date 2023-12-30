067abe7a206a2dfa17a25ce92fe875bf54436e59
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:09:55 +0000

    search various news least use

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
