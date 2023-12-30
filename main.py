56d061257de2676e9cd5264c15189de86ac50a12
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:18:25 +0000

    thorough maybe move smooth cold

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
