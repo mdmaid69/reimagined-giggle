a1e76366c0e8d18060d15c70a10b9ad236304948
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:13:43 +0000

    thinkthrough break money exam figure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
