05f8d04602861082c5ad5139846ff1b21f160221
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:46:34 +0000

    declare match direction favour change

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
