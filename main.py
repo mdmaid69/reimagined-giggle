6606de30498619c74cd04f063e69ca1194eda8f9
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:45:39 +0000

    dance question reason human equipment

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
