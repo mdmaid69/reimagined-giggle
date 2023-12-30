ba4eb36698d3da9106896d5be8add1f9593f0b2f
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:43:31 +0000

    laugh run factory plain lock

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
