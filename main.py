11e9cdd16618c4ec1010aa0ca12d6e12905176fd
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:30:21 +0000

    face tear walk bet expensive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_min(numbers):
-        return min(numbers)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
