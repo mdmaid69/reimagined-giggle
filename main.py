1ab0ef416e5ee34250f467528af22b67b882b7b7
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:07:40 +0000

    heart talk gate smart narrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
