e5ddf5287f84a10071c3efb9368589aa59ba2afb
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:40:19 +0000

    several hospital noise maybe combination

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import numpy as np
-print(np.array([1, 2, 3]))
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
