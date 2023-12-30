007e3c02e1e5b587960fb312c8f71dd7075e125f
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:45:58 +0000

    unlikely approve fold determined safe

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
