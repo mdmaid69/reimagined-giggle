495cc6659cb86354eaf6d781d6016998ba8baf06
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:21:47 +0000

    backwards music drawing disapprove damp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
