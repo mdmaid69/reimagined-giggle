154ffa09a18f5044e118f1c41107aba2b11d8939
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:33:53 +0000

    takeover answer task scrape rob

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
