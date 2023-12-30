b121ee42c9a71b455304425d234de281680ef92e
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:36:21 +0000

    stick evidence circumstance scream criticism

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def count_words(sentence):
-        return len(sentence.split())
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
