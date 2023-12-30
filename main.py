e03f93e5c49665afb4b6b111ab60b6bf62ae0551
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:35:45 +0000

    guess block cruel ours thinkof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
