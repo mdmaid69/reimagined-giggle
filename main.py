710fd074750c230c6bd50c60f880e8f913d10171
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:43:34 +0000

    other call profit year dinner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
