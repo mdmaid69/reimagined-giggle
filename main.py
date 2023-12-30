c6e7def023f59a5a5e4c2f74cc95719b09e7ce88
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:17:22 +0000

    concerned treat turnon lower fond

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import math
+print(math.pi)
