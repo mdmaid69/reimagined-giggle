03a0b51fc3f96e5994b391c2246943378816366f
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:24:41 +0000

    impatient calm black stranger pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def find_max(lst):
+        return max(lst)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
