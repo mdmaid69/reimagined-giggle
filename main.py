b224e04de721248bf01464a03b715e6cecb21c93
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:32:26 +0000

    display command loss study close

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
