bd46b0d681fdeef9256c3bc5c79efc2b84796be5
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:51:15 +0000

    hurry survive air miss record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
