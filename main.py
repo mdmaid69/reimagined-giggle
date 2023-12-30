1470b5c9684db9f85267ee16a9d3dcc560b5ac1e
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:46:40 +0000

    season point contact considering flavour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import sys
+def exit_program():
+        sys.exit()
