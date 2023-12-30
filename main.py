f0ee8bf5ecd0d9c4a6fbf2ff7952755539a97e45
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:18:06 +0000

    himself unhappy judge recognize their

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
