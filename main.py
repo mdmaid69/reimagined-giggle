bd066ddb92384bf57e5527ddb7047b378c292a62
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:41:17 +0000

    so everyone herself invite begin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+print(math.pi)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def clear_array(array):
+        array *= 0
