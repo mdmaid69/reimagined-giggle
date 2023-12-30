ae6c136c4515f6b7052be3bea4a00d052f3b2ad9
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:08:04 +0000

    recent class step early their

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import collections
+def create_queue():
+        return collections.deque()
