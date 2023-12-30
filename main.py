b4d5e88160c2d9f4da63607566c1fad299aba69d
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:05:47 +0000

    empty chat prepare region carry

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_work(force, distance):
+        return force * distance
