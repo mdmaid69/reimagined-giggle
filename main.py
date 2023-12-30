60f888c5e8f6cf2edd0f6831e2c3d9b9a9b24ba7
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:08:04 +0000

    climb dare them before better

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
