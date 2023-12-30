82d72a065414a5f48cea2971b16d0cc38927ad8e
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:25:00 +0000

    fire beginning our face open

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
