50103a56ba88c0cc1f5d028cb4fddc2c9dfade21
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:18:19 +0000

    story pair impatient through real

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
