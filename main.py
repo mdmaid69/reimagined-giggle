fd5fb263203b6220ff315a95aca974c23388a75f
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:10:23 +0000

    chin familiar 569enjoy stamp brown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 text = "Hello, world!"
 print("Characters:", len(text))
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
