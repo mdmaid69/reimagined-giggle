9a6e79933350a069cfd4db2464d97786455e9a3b
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:35:03 +0000

    hello press breath walk mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import platform
+def get_os_info():
+        return platform.uname()
