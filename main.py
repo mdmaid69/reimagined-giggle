2b70a84ad6fc4d5cc76e337f3e9cc336787abeba
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:57:19 +0000

    whoever week smart fast degree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def remove_duplicates(lst):
+        return list(set(lst))
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_as_repr(array):
+        return repr(array)
