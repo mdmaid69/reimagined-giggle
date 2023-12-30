7ba4248e30c42626cf3373e5080c0db161b11a95
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:37:47 +0000

    divide criticism better comeout rise

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def sort_list(lst):
+        return sorted(lst)
