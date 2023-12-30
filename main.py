abf5b39d9e331612a51b5f2f6deb2786e246fc18
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:29:02 +0000

    name sell reason light priority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import sys
+def exit_program():
+        sys.exit()
