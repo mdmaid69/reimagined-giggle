3d5cadea5b9c3a4a656f22b8c281ec60a70addbb
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:10:41 +0000

    thumb sail direction responsible live

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
