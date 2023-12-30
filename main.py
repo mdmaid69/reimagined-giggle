337369a5b7f509217c548eab5064b99dc2329fb5
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:55:11 +0000

    reply production confuse needle row

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import time
-def get_time_since_epoch():
-        return time.time()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
