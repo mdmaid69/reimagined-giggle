1537a55cabb3699ffeb2902860303aade6e5053d
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:13:55 +0000

    survive rarely purpose treat contain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import re
-print(re.match("h.*o", "hello world"))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
