0da985857b79c7fea3c52088e6ad4a2d41cabd4e
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:36:09 +0000

    reason period forgive earth 572enter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_density(mass, volume):
+        return mass / volume
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
