c5f5f6a8aa372d9fe417a9dff764515efff01cbd
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:46:36 +0000

    prevent space convenient single finally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import numpy as np
+print(np.array([1, 2, 3]))
