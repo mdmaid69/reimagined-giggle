af82d2712c99c7c15f932e10050b080703bbbc46
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:14:18 +0000

    Gg turnon necessary tape exchange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_sine(x):
+        return math.sin(x)
