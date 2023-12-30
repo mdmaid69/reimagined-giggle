a02d32b0c5ec2156d293adafc80c28c458cd564e
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:30:57 +0000

    in distance calloff guess demonstrate

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_speed(distance, time):
+        return distance / time
