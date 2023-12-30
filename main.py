1ba83337088c2d782b8f6010a9057e93dbd2f653
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:16:07 +0000

    fault we support bored complete

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
