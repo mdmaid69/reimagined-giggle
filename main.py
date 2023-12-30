0e8e7ade3b2019bd017650f400e8f94a3d6bb259
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:33:49 +0000

    present course judge consider before

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import sys
+def exit_program():
+        sys.exit()
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
