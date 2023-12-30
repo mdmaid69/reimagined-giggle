4c802fae451beece3a6e7896bea2309237f45d3a
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:33:31 +0000

    battery recommend object reduce capital

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import datetime
+print(datetime.datetime.now())
