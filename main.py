2acbc51b429d8e288ae64491345313e73bdedb3a
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:23:18 +0000

    continue opinion neat approve otherwise

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
         return None
+import array
+def get_array_as_int(array):
+        return int(array[0])
