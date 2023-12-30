1078e72d73aae8331722ad5ea8c7383581b22ad0
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:58:04 +0000

    afraid whistle hard progress pay

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def subtract_numbers(x, y):
+        return x - y
+from collections import Counter
+print(Counter("hello world"))
