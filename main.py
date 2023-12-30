a16af5858ef79e2991a39efe27daf0839969cb1c
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:24:14 +0000

    issue communicate properly accordingto deliberate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def convert_to_binary(n):
+        return bin(n)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
