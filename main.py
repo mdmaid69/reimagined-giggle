b4cfd249a805d96ccb3af59203e428cc40c6ed66
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:40:30 +0000

    rub normal usual violence ocean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
