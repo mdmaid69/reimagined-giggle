5d0a293c2d49ec4eb4e6e93306de4c1f24e18bf0
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:30:05 +0000

    trip boil rank cheap now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import sys
+def print_python_version():
+        return sys.version
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
