50981fec73c44fcc7d4508f588d61545c3336081
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:55:22 +0000

    trip talk land total border

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
