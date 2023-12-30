ab6b741f350940031b3921dee155e1d3e7528d3f
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:02:04 +0000

    should breakinto break be unlucky

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
