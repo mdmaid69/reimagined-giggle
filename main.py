e5f84e67e5c188705816cde5cb0c0bd4350e704c
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:07:00 +0000

    contribute intend tradition laugh luck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
