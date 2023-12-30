828cde1ed6e873d7c578eeda1fccc0f607a6c8be
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:20:10 +0000

    gate succeed mistake case report

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import logging
-def log_message(message):
-        logging.info(message)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_volume(length, width, height):
+        return length * width * height
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
