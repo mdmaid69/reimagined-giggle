ed0f08b4a37afb0e28d0a738efdcc6ccf04ae9f3
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:27:12 +0000

    instead climb war shall for

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
