06d9698292217936aa824f829339c66d3baf2d5f
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:48:30 +0000

    idea let fast patient state

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
