552fb85a9a2776363838d2a3e837cbc1b06f2f0d
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:19:39 +0000

    murder murder uncertain should after

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_work(force, distance):
+        return force * distance
