133ee33efec6626fbb4739351bc87ff9142d6e03
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:08:49 +0000

    question apart smell shall like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_sign(x):
+        return math.copysign(1, x)
