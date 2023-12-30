a44d74bc4274a2a12c96c71f9e321f8191bb56f8
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:49:02 +0000

    time drop list flow gather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
