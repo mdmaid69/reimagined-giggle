34ecccacefa0a5c4706ff6feea99a262c6c447e9
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:07:00 +0000

    permission rid person declaration honest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
