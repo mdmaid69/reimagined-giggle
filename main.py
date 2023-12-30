88543a1f61f75995ca98778140b96a8e4985250e
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:52:13 +0000

    also platform help somebody outside

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
