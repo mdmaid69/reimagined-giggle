9f58e6c08ad9447307a5d6f4f99442dea3c2d19f
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:25:03 +0000

    guess walk live breakinto offer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
+  def cube_number(x):
+        return x**3
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_bytes_from_array(array):
+        return array.tobytes()
