1f3f7c2ffe9483cf32240d8582a9240949811f88
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:21:26 +0000

    guard inside traffic trust suggest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
