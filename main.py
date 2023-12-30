a989056e2b3031a070a33e085cab4e7ea6a704ec
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:43:38 +0000

    sea fault mass have quick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-import collections
-def create_user_string():
-        return collections.UserString()
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def find_max(numbers):
+        return max(numbers)
