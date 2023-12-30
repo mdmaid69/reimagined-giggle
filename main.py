528997624fbe660335e6f26fa57210da168c79ee
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:11:38 +0000

    press bringup object follow pity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def delete_file(file_name):
+        os.remove(file_name)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
