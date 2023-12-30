666c868ca864de199de5f6fbdcc6ad7e81255a0f
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:34:50 +0000

    cruelty aim gentle reduction each

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
