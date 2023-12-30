b2c635cf4d03588531feeeb183b4f814ade30662
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:00:55 +0000

    history protection unfair kind consult

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
