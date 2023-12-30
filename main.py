c9bed80e94fc8d38c62c082d74e3c13a65fc86a6
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:46:39 +0000

    equal Ww against them nowhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
