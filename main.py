a9b87393f4b5032337f4a5638a4b52ce8422f4c5
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:05:32 +0000

    hope that Ii connection perfect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
