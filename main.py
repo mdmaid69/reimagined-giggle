409d7fb57ad24e0e1e4e4fe45933d38eab054d63
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:24:49 +0000

    little and model neither forget

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
