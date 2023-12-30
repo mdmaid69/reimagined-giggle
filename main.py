d2c7bf23d84728b6573d8cf2e75184e4315b17d7
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:37:01 +0000

    makeup exact stick neither thin

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def get_array_itemsize(array):
+        return array.itemsize
