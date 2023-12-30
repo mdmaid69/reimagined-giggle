f210a217b2f544d0f66c481f1c41e4bb20e2ba65
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:01:43 +0000

    workout extraordinary towards considering drop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
