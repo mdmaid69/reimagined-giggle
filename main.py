30a0d5a3337874e9fa24884ad369afd52119d9cd
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:20:54 +0000

    congratulations protest profit among half

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
