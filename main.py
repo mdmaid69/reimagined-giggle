1ef8bdc3afd144be767f5fb88fe2a048b204c4ad
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:03:05 +0000

    wind tight passage law unusual

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import os
+def change_working_directory(path):
+        os.chdir(path)
