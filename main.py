e24a0fd815e2b1227b9a42c0efac5f1810474298
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:44:23 +0000

    ourselves price refuse butter centimetre

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_power(work, time):
+        return work / time
+def calculate_power(work, time):
+        return work / time
