c32896da9244ae854ca1c3c11f0e7154c5a5a00b
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:08:22 +0000

    size deserve behind bar second

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import numpy as np
-print(np.array([1, 2, 3]))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
