a5493bd3e6fa805947623da0c6ef2af4442b2665
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:43:36 +0000

    relief read reply action scream

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
