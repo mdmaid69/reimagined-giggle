1d489e37af14c1cea7dddea1a0c56782b2a82fbb
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:44:01 +0000

    repeat favourite company finish generous

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
