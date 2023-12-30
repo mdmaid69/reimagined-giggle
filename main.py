d450c86e0edbbed61be97982ba91da8d89efa043
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:05:14 +0000

    great hang tap criticize race

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_area_rectangle(l, w):
+        return l * w
+print([x**2 for x in range(10)])
