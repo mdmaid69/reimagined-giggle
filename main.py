f55cc84c4347278fa89425404acef285c629c740
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:55:08 +0000

    takeaway reaction shop human violent

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
