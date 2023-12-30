78df0caf9e901a12f90105a75c2d1359760005bb
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:07:16 +0000

    prove ability throw dress positive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_acceleration(speed, time):
+        return speed / time
