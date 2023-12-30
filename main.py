92765361d84d7b068d5280b4bca2934884f083c9
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:00:15 +0000

    face tall type style distant

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
