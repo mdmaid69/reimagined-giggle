55ebd7e94562ef6214634a7c072e94fdef3d9a5d
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:58:36 +0000

    giveout name note mess 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
