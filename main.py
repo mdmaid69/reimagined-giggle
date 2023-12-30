3ab6dcd929529000501d4bd9f689dc1509018e72
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:15:45 +0000

    serious pair sight water chief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
