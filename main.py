84ce2cdf6f808d91a29daf69c2b53055fbf715f7
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:34:53 +0000

    that show answer all knowledge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_typecode(array):
+        return array.typecode
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
