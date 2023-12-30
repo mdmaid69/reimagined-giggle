90d03cae55dd4fca4d20f81e67e9fa47e6c11887
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:12:38 +0000

    value weekend read comment substance

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
