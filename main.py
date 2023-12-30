36da9841ef8237aba7b64bb09b55384a0295a303
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:34:27 +0000

    callout curious common handle create

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
