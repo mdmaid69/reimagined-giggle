0606f83872128b1f2ae5714dfd339a05b2776f9e
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:35:15 +0000

    spin out lot reduction thanks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def convert_to_octal(n):
+        return oct(n)
