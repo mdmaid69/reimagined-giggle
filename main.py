8a918a15c542a63eedd67bd5c4365f5a873a601b
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:46:26 +0000

    fun genuine plastic coffee mention

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
