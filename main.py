355b96ce4ae5824ddc039240ca409a6f23563984
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:19:41 +0000

    capital risk line once probably

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
