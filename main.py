3921b3104bda85cb2ba5048b8d23743a66307e52
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:28:13 +0000

    command talk cost apologizeapologise some

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
