ce0c688bfe2d3ebcad2eb3332aa4dfcef922ce86
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:12:27 +0000

    go opposite health fact any

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
