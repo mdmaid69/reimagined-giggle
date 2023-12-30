9c69874996fcef15c9440a8849c8699d14a72c2d
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:48:07 +0000

    worry laugh square offence affect

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
