7c7b0757192ede29426b487dfb86ba2a452f4c03
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:16:30 +0000

    OKorokay cinema news week goout

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
