53f11a149892c53881e7ec155149b4b50f20dbb2
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:36:08 +0000

    play its black above hollow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
