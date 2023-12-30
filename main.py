3167c2be5412f0fc7df697c5822d27479d7aca21
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:25:58 +0000

    emotion body bother occur like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
