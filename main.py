13c8b0f327a00cd974e4cf38ba2b255942ceee5e
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:29:29 +0000

    search custom fit regard close

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
