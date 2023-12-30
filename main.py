9dd8e6859b4dedd92fe35db967324aebca736feb
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:27:34 +0000

    spray trip content mother uponon

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def count_elements(lst):
+        return len(lst)
+import os
+print(os.getcwd())
