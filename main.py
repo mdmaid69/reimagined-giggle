614f21cedba22a9de4d2b8bab13e1c57c0f030d4
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:22:54 +0000

    unfair dress weigh inform cinema

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
