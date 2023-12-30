58b2f4718ab0dbb668db490ee74146a202fb26e2
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:50:30 +0000

    matter takeback refer appeal independent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def reverse_array(array):
-        array.reverse()
+  def convert_to_octal(n):
+        return oct(n)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
