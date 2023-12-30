58811628c1f2c1e81ab5c72bc618bb4ee44c19c2
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:30:51 +0000

    check wipe keepfrom bit value

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
