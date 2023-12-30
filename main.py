8a845a33995b6293de2390ae0e0aa56d87b5f85c
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:08:45 +0000

    start hers level occasional expect

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
