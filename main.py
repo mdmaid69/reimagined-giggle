c92c68c563beda50cb5d056af34b1998542ae4e1
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:43:09 +0000

    spite nigga him come past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
