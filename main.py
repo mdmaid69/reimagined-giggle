4c6ed8060ba06571c16ea57694bacb913f007334
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:10:08 +0000

    all divide ball all gather

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
