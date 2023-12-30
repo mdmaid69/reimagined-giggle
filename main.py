21bb6b2816376fda4c2178119e842435093b1b52
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:10:06 +0000

    keepout jump badly receive process

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def count_characters(sentence):
+        return len(sentence)
