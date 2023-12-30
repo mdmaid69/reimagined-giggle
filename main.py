a57828ddefca395b47f9662bc7b464b9bcdf11ae
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:40:13 +0000

    right ago exaggerate progress cost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def find_max(lst):
+        return max(lst)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
