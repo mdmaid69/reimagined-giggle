e486fcfc80b40d19d371170b7d5e50b1bbf068eb
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:44:40 +0000

    tight treat safe 566end bother

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
