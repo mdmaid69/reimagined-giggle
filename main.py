adcc6e1a11b29a1d4277cb2d87f4988515f62898
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:01:16 +0000

    bringup like improvement forgive comeoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
