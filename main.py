1407e07366ce510bab22a0934ee3641c5ab03452
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:28:28 +0000

    moon length church characteristic wall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
