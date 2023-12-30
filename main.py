e17db88dcd2daf2f982c95af4a0f4773bc30eada
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:16:34 +0000

    share benefit urgent deal live

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def calculate_power(work, time):
-        return work / time
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  def is_odd(n):
+        return n % 2 != 0
