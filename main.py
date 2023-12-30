40b0bbae97e74db67884274b2300dccdea01363e
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:44:47 +0000

    getaway understand but software reject

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
