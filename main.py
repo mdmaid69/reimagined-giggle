a7d11e9914c4e3eb748baec69b7f56f27318e5b8
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:51:19 +0000

    deal prepare through disagree rain

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_max(numbers):
-        return max(numbers)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+for i in range(10): print(i)
