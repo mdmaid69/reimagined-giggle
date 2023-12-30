bc48ecca811cb343c9d36380d1be8ee39d82d4b4
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:13:51 +0000

    flow cool workon eye fact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import sys
-def exit_program():
-        sys.exit()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
