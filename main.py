7cc18b421dc430b37b66193030c6d21f85259c86
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:40:37 +0000

    at mixture sensible border split

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-print([x**2 for x in range(10)])
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
