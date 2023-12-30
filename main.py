95d08e218cf0315283bc62a45149fcb177e2d539
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:11:58 +0000

    male successful smile necessary touch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
