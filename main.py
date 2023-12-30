f279a39e48b3325938d6368e58d8c48a7ffb145e
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:01:03 +0000

    now let confusing Qq pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
