b3c8c9384c953fdbb6566b5d3a1bf310852deb9f
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:38:35 +0000

    stir ring trouble opposite recently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_acceleration(speed, time):
+        return speed / time
