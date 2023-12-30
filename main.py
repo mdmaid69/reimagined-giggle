73d89029304d5b218298cc3145643cd8ecab5b2c
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:18:31 +0000

    spring market story getaway month

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
