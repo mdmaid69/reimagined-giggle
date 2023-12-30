4aad9387a3c78fe9f94c41a7b0c0fa8b0ec8f41a
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:35:19 +0000

    settle dark getback at like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
