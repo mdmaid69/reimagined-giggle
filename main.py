510b16cd24b02bd12b682f46797ce121f3f0157b
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:28:14 +0000

    interest official neither enthusiastic only

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
