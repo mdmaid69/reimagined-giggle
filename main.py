0d30011b8055af7ce83a472b1b99c0056adb000b
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:13:30 +0000

    invitation value square middle buy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def find_min(numbers):
+        return min(numbers)
