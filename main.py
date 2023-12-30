961efb345d191f672cad1cf99fc854e8572563b0
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:25:40 +0000

    popular correct freeze confidence lose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import os
+print(os.getcwd())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
