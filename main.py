43285cf203b7117aa452a3b32ed3f89c2193d3e1
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:17:14 +0000

    comeout declaration me thought return

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
