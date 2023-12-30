3f61e98aa0d40933d8d2e662baf21447ad8ba52d
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:50:34 +0000

    discuss head roll offend block

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
