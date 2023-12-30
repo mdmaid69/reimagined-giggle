c1aced6f384251b5535c4846387f818b36bea417
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:05:50 +0000

    class this spill Mm message

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def calculate_work(force, distance):
-        return force * distance
+from collections import Counter
+print(Counter("hello world"))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
