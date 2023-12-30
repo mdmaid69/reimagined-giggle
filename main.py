008d2066609dd7966fc46004dfe13650310deb08
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:18:05 +0000

    talkinto justice turnout similar moral

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def calculate_speed(distance, time):
+        return distance / time
