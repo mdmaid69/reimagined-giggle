51925c8b1569e0cfd6b98c18851c3a529a6f068a
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:11:21 +0000

    this method all trial bend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
