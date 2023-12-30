89da11205a4888edeb353ef239c55e06c0b12fab
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:19:19 +0000

    classroom line take voice must

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
