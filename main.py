8d2d54a215aea98f5c3516c5b8971717ebbbc5c9
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:08:47 +0000

    rain unable truth reasonable open

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
