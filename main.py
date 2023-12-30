08d9df70ec1d6e9b9fa696b6edc6fb4c93981909
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:57:15 +0000

    needle loan wild should early

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
