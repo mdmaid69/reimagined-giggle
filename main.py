ff336203da74f31a438c5fffc955302545dbd10c
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:14:01 +0000

    run sweep swimming but disgusting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def append_to_array(array, item):
+        array.append(item)
+def is_even(n):
+        return n % 2 == 0
