61f11cedb021e5faf374ec1c7680fd9f20bb6afb
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:26:07 +0000

    callback getoff moon appeal each

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
