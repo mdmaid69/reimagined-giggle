806816f43e93b720f94525e6038c436804374571
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:25:45 +0000

    progress stiff by open sing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
