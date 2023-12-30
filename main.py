636c9034ab393fa0cf7516473f11b76a730104c3
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:41:09 +0000

    keen average advice handle light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+for i in range(5):
+        print(i)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
