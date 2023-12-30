9a75038d8ca1f035dd6208f61f086680a87ab284
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:25:15 +0000

    fierce shadow large ticket mistake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
