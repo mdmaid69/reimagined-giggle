65baab33c6ffe81b15da30af59a4bbb88c356db4
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:55:41 +0000

    responsible glass gradually pair level

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
