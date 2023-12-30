4e04b9170ace89d70e2ac58513f133691bb0e92d
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:21:31 +0000

    geton luck whoever further grand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
