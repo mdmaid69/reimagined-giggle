ee7e1ef04deeeba9e083282e149a43d0cb2de65c
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:53:54 +0000

    perfect fill reduction long lower

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
