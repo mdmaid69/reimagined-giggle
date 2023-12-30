53e67db25e3b53a8482634149c0df1249751a96b
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:07:17 +0000

    afraid favour interest own deliver

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
