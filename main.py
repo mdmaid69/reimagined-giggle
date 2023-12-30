591f3958ab9a0674f77bdcfa2ab1f683cd727417
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:45:24 +0000

    takeoff complicated able of blue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
