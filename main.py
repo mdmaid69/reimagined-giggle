7ad74075af89e980b890aa348b7c62abc6e4252d
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:03:12 +0000

    brown somehow tense steep strange

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
