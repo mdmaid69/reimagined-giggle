6fe8756bc89a2dd0592eb3fc27c11397de34dd55
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:24:43 +0000

    behind exam club on custom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
