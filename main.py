0e5f8da4a692bfc65dbf9946b3fee9e0a76b636e
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:28:59 +0000

    behave scare appeal slip rest

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
