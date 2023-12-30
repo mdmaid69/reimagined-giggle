7c53f8376a9c5e58033cff067f2b2705255382a5
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:04:51 +0000

    information formal tight help send

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
