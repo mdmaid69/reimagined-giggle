a93d998dbe120fb152165bf8ac73706ab457bdc5
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:50:08 +0000

    suit advanced I travel excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
