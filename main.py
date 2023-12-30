65af3bb124ef7a1a6d65f2b68189f37bde828cbb
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:26:38 +0000

    mail exciting switch community name

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_power(work, time):
-        return work / time
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import json
+print(json.dumps({"name": "John", "age": 30}))
