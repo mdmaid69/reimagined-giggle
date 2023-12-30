2465bd45c54198992c5c9e8a1d04c75d82a19b6b
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:51:36 +0000

    journey Kk modern scare correction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
