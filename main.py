6839c323a014c756e292876f78ffa8ce81da0c1a
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:53:36 +0000

    classroom order could branch bush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def cube_number(x):
+        return x**3
