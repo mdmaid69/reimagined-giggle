ae802ebc8f2d2107fa330459bca74c486375a032
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:46:09 +0000

    right opinion brain slip help

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import collections
-def create_stack():
-        return collections.deque()
+for i in range(5):
+        print(i)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
