7950df234a3f0476253ab7702d17834b9ebacb92
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:43:23 +0000

    slide week giveout our Ee

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
