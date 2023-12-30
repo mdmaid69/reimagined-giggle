b3d8f1e17b9c096293d168226d489a92b5f8a180
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:26:48 +0000

    remark frighten comefrom first slice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
