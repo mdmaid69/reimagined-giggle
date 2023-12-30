09c341a4dc5d14c71f956d74c2a0c39c1c8a4c3d
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:19:00 +0000

    entrance about drop measure where

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
