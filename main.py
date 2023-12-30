6bb73aa2e2a91ed721f399de4b7e1c17dd7816b1
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:05:18 +0000

    less out meet once lean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_current_working_directory():
+        return os.getcwd()
