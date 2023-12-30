47222942faf965ceced711a39fb528ae2fb39866
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:48:28 +0000

    goaway spill price over serious

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_tangent(x):
+        return math.tan(x)
