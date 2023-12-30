09629b92108cd9fc32b543013464ca0309e6230c
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:29:22 +0000

    task show physical call trick

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def divide_numbers(x, y):
-        return x / y
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
