3dafe7e2b895fc387925d127ac422c5df3a2e236
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:46:07 +0000

    punishment her stare maintain programme

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import collections
+def create_stack():
+        return collections.deque()
