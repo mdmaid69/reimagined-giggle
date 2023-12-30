c1bb7b013caf1108bc2e5dd925327db5d6976943
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:27:30 +0000

    one weather crazy holdout around

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_work(force, distance):
+        return force * distance
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
