75439aedb4ec600a6bea7e7293b7abaa0e363f67
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:51:59 +0000

    meal broken search stop proof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
