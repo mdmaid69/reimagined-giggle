c08a0165d85c8b14f8ed01e44fa5e96f0121bb41
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:01:01 +0000

    honest giveback perform familiar feature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
