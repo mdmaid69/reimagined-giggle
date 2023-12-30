fc057843c7efc9298cf96908951c6fb3be129168
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:01:43 +0000

    ear run argument sit error

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
