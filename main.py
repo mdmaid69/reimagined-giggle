8b6eeb4a8c0fc011dc229e3818d0cb9812853036
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:39:46 +0000

    smooth jump unable jump well

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def add_numbers(x, y):
-        return x + y
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_typecode(array):
+        return array.typecode
