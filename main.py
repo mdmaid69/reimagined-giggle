1934802075078bc3d11a15fb38e442e26f6374d4
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:20:53 +0000

    sad nature sensitive comeout holdup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def count_elements(lst):
+        return len(lst)
