cd7eaa103dd1d8864f9a8ad8adcf46d4c85d1c7b
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:18:09 +0000

    so independent machine path the

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_array_as_set(array):
-        return set(array)
+def multiply_numbers(x, y):
+        return x * y
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
