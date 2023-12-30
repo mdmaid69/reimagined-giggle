7cff0cca9ef752fc99b91ccf6b2308039b73c593
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:00:22 +0000

    praise although region after free

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
