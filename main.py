590c7b77ca909931801b36f39d53bebd59422a6e
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:39:26 +0000

    rescue share cover belong perform

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-print("Hello, world!")
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
