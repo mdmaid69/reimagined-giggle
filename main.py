9e7b6fd995d462d21c2376bba2c5943265462608
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:30:17 +0000

    safe must evening empty expression

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
