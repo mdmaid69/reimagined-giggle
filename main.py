c974dd86d00517ca3503af260ae648411240e74c
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:35:28 +0000

    fire workout total better suggestion

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
