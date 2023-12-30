056a2a57d82810844f3875e84997db86280ccd52
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:51:49 +0000

    sign little it react temper

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
