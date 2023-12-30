530ea672459e4e4ac1cd948d60092415fa3aabc7
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:32:56 +0000

    bath size lose home uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
