dc162c521ec2359b544d6c23050efa5374e28c25
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:28:53 +0000

    order narrow fall damp bringdown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
