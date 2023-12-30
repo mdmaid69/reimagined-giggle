2a6088fac0ef7db0b3fcf7a0abea9f293c93ca6b
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:28:52 +0000

    friendly offend race control comein

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+def is_palindrome(s):
+        return s == s[::-1]
