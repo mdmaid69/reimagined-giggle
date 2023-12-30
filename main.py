511b52c2d76461934d34fd6f6dfac0af641a591b
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:15:51 +0000

    swim club long pronounce rob

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def find_max(lst):
+        return max(lst)
