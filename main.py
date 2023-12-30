274a73aa6ed042c5ba53ee00d223a29a667133bd
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:26:33 +0000

    way represent decision through side

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def count_words(sentence):
-        return len(sentence.split())
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
