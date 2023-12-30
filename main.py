b525b67ff73e04d1b9f941aec80b15c95eff87cf
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:44:22 +0000

    offence shoot centre partner programme

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-print("Hello, world!")
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
