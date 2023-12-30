db2886646d6dcee5d0219df0fe1e298d0e2d7ffa
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:08:06 +0000

    drop forever trust rare drawing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-print(sum(range(10)))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_length(array):
+        return len(array)
