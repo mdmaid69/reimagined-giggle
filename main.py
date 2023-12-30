06cf0a32ee3f85c6fe9ea19b5d4f2d2645eabc70
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:35:11 +0000

    feel independent gas play would

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
