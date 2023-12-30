97e33166d2257ff959c0219e80fa8710186cb6e0
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:14:20 +0000

    real formal risk moral swing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
