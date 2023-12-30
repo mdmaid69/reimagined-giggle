7b37c33e87be9598931eda5e6dcb941f9ee5a302
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:14:45 +0000

    thank perform support pause pipe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
