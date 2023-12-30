576511eb8cec76337c41e02e2a77df50704e91e4
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:24:51 +0000

    takeout effort shallow spray thinkover

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
