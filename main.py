5ce64a4bad0ae86825221986ab5795b885bc7a53
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:34:37 +0000

    Kk estimate become experiment pencil

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import collections
+def create_priority_queue():
+        return collections.deque()
   def cube_number(x):
         return x**3
