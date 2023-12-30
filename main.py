2fea139bea1c507d644004875592d9453ff41405
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:15:45 +0000

    true steal century Pp walk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
