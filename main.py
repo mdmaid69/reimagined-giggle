d0228c96b8c0a0bb55f644459bcdafe18e41be7e
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:41:50 +0000

    ill takeaway cheat separate off

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def add_numbers(x, y):
-        return x + y
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import os
+print(os.getcwd())
