687425f6c3c36b96a42542ef3ff2c25aca3589ed
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:14:03 +0000

    consult defend taste lunch slice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def convert_to_hex(n):
-        return hex(n)
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
