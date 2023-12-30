7fcba7fcee5e47b2eaf38e870572640f088c4b42
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:53:02 +0000

    close use vote attractive disturbing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
