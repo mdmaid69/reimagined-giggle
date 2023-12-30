2d7cce1a00ba61d89f27013717a8d80eb15a3786
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:43:29 +0000

    provide threaten takeout cheek nervous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
