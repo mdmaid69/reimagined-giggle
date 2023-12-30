c44cf2d942cc9e76c6dc89289f9e7f4034d4cbf8
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:01:40 +0000

    winter health pile pair for

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
