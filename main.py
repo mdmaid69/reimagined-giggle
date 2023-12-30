631d459dd6d438d80514cdfd4552eab49330965a
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:50:48 +0000

    proud doubt neat address yours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
