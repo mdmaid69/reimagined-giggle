eb24c4d832b68e981b8354b425ab182f1987e74d
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:50:16 +0000

    react apparently beside must interesting

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
