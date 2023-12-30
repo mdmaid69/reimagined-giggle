fae2d49dccd0229dd81d262b9ffff59545640362
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:32:24 +0000

    thinkof die bunch present wave

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
