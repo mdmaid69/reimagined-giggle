d1a9a5906313f090a21edce6bab31323968810f9
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:18:48 +0000

    but open school last behind

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
