b69d7203aba811b0ab53b216d094dc9cc20be54b
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:54:41 +0000

    tense breakfast quick row tidy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_area_rectangle(l, w):
+        return l * w
