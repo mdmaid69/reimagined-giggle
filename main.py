1524ccaffa1d18e99744e4c7c240db3208ebb379
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:31:09 +0000

    allow Oo steady alive yet

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import platform
-def get_python_version():
-        return platform.python_version()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
