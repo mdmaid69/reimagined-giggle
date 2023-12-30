03f32e5d4e03f3ab53a1edc097df1a54da07229b
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:00:54 +0000

    deliberately country call compete floor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
