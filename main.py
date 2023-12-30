e96f43804987967a23282b54667ec523d68e1942
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:13:13 +0000

    free big classroom charge loud

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
