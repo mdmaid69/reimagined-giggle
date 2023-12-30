b7e2cc7800819719f7ac8460ccad819b14fc41ac
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:57:22 +0000

    rather upwards period that what

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
