f9d8eb5b6056215c4158dfbc94e479f165959f0f
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:21:27 +0000

    workupto after describe cough term

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
