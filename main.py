51639bfb6fb697a6b76acb519d965792c80989f9
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:29:47 +0000

    neither need breakinto worse after

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
