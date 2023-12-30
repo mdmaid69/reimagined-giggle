20465d81fba201dee63af6d9448a8834facfdfde
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:16:13 +0000

    short connected really from hole

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+print("Hello, world!")
