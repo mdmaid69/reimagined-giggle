7862b749b44e23f7f02f70ee3833b7181b29083f
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:00:03 +0000

    pick lookafter success clean belief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
