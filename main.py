9f7b074d700c53fa080f12ade83a5ad47243581a
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:52:06 +0000

    lead create maximum ball emotion

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import tensorflow as tf
+print(tf.__version__)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
