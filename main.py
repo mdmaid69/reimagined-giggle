9e0970f65666761cf2de184e693b8b626bdcae15
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:54:10 +0000

    complete lead flavour estimate measure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def find_max(lst):
+        return max(lst)
