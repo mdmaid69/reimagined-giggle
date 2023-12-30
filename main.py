c4174dc8fa7b21550147ca3841c39f49239fee05
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:18:22 +0000

    constantly crash move famous else

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def get_list_from_array(array):
+        return array.tolist()
