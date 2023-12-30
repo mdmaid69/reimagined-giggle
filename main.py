d993a1da1214f9a99fa34f069ea15912ca3e1eb6
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:44:41 +0000

    plenty woman division major forward

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def is_even(n):
+        return n % 2 == 0
+def is_even(n):
+        return n % 2 == 0
