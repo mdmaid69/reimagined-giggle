871059c369df811dbb80d23118783cef8695ef46
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:27:49 +0000

    although empty sign Vv possible

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_work(force, distance):
+        return force * distance
+for i in range(5):
+        print(i)
