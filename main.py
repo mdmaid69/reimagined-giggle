0a61211e902051e8c4eff0610982c08ee0a1de76
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:18:07 +0000

    door normal extra off steal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  def is_odd(n):
+        return n % 2 != 0
