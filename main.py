91547cf6bc715b3821093ae2d8fdb6c06ddb22d4
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:55:27 +0000

    attention pretty win lot care

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  def is_even(n):
-        return n % 2 == 0
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_area_circle(r):
+        return 3.14 * r**2
