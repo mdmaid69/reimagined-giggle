b5b4ae8bc1c48353689bd79a5bd565add28bc3d4
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:08:21 +0000

    scream pity ocean wrong separate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
