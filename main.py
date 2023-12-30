e30026aae1538f2cd65cdec7ad9b0c8300108e28
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:55:11 +0000

    normal season project side breakup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
 n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_work(force, distance):
+        return force * distance
