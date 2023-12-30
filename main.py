b226834ce46d121b5ca0f19b1590bb89ff6b5c44
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:32:21 +0000

    cost disapprove anybody effort impatient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
