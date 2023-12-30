65657310aef118eee560a2d421ab8fb8b99b6dc9
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:43:20 +0000

    afford exhibition friend possession Itself

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def get_array_as_bytes(array):
+        return bytes(array)
+name = "Python"
+print("Hello,", name)
