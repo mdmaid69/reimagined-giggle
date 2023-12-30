0e44c79b5ec77cfe8fcaf986207bd42f31949c93
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:31:15 +0000

    taste regard interrupt friendly sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
