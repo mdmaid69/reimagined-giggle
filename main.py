719f9dfe5d101df595029bd3b3d2286d8718bba1
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:03:19 +0000

    no copy Cc recent follow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
