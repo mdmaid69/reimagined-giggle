4c01b82f3ee8cf4a3ad76247dafb16738fe084f0
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:13:28 +0000

    medium future recognize cost bus

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  def is_even(n):
+        return n % 2 == 0
