9eccf69dbb2d566a44d5cb9c13cf7296bd25e524
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:58:48 +0000

    declaration swallow normal would unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
