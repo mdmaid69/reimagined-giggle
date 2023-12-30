95b7cb9419d030fb7272e6934d6a92891cdef299
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:19:41 +0000

    our thirsty measure pick plate

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-  def add_numbers(x, y):
-        return x + y
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_itemsize(array):
+        return array.itemsize
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
