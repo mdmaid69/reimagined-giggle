b31f95c4e438f5c204f7f83b575866cacf440c8b
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:45:23 +0000

    teach pattern pronunciation succeed lend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def find_max(numbers):
+        return max(numbers)
