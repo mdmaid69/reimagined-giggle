b9c2aef49f76dafae45c91eab1bc29b663bcad82
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:16:17 +0000

    food hang fit worst will

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
