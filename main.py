8f930147aadd8290807847fe0e948f620e43f83a
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:55:03 +0000

    effort 572enter dark getout run

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_area_circle(r):
+        return 3.14 * r**2
