e509faf2174d00b6ea34404e808eaa15c63d58c3
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:42:39 +0000

    suck spring connect copy false

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def count_characters(sentence):
+        return len(sentence)
