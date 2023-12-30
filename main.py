a9d2fd8a3a8a9d604278ef773bd8c4051c65c4f1
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:43:32 +0000

    shadow have either pleased company

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
