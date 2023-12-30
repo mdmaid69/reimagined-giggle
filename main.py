631802f090145777b79456b043e5a2d4c22c82b6
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:52:33 +0000

    risk occasional themselves wake smile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import time
+def get_current_time():
+        return time.time()
