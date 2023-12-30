c6b61a0102769cac36afa5175e97670e53ff94d0
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:08:13 +0000

    dare ago surface believe behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
