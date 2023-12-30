218fe93d77d6524618706c99886d5a1c6564185e
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:16:56 +0000

    generally thinkof clothes discussion pump

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import datetime
-print(datetime.datetime.now())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
