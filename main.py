ded9bdcc0295bf1557329635b0b6f2ef65c98337
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:30:31 +0000

    Ee arm leg towards flat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
