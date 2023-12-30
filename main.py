14c6669a1a9ad0d9cf6cf188027f449a0f86feb7
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:49:48 +0000

    past with which shut believe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
