7543c319c23f7f47b433d2d22ae63d7aa362df5c
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:02:35 +0000

    company available along risk somehow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_speed(distance, time):
+        return distance / time
