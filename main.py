13a615c58a7c73a9d120333a8f711d10f5159af4
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:53:47 +0000

    polite protection release mother away

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
