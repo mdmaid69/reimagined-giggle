4f7c325c0e6484d407e0dc99bb282c2aef3ea89f
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:46:04 +0000

    centre pump home square freeze

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
