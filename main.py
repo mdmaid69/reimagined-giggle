ffa85daf24fe49337a5e889cb80a7ed989b0fa79
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:54:48 +0000

    knock listenfor gradually point design

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
