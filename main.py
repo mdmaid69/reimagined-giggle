c24c567ddfad016683d520a90932190dcff8742c
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:27:11 +0000

    takeover forget well quality health

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
