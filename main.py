6ba6897b2a8a2e09c9ddf49b24b3ae1a3b57aac5
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:42:03 +0000

    fence gentle please hello rate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
