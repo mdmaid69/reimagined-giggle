fb2aff1a5e267fb2ac7056a34628d8278c66d4f9
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:51:02 +0000

    bury walk cool afraid different

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
