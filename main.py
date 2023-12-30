073b80de4dbeaf4c56a7446ba06082afa308d1fe
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:03:22 +0000

    answer bringup stamp thank payment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
