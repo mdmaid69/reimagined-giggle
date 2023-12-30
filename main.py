bd20dbc74df4ba0a05e46505ec651e6e01d51f6d
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:44:11 +0000

    juice university thing crash motion

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def is_even(n):
+        return n % 2 == 0
