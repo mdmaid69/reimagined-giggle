a8b37ac827713385d406af75f207d3c4e8db6560
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:31:17 +0000

    beside vanish occasion thought escape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
