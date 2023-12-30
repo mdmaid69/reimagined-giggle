b27ab9134bc3640f4a7e9f451cbf1d7a0adbdd17
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:00:09 +0000

    cry proper suit separate prepare

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  def square_number(x):
-        return x**2
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
