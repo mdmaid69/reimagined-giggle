29292d76d468c5aa9200dea47708d090682139eb
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:09:27 +0000

    range drawing vanish payment stop

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import re
+print(re.match("h.*o", "hello world"))
