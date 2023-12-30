1f1bafdbb4ab86c8acbea078a4d3e06feaa5ae70
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:10:16 +0000

    runinto organization niggaacademy wild view

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
