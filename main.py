b9c156ee29051b18d029c1a4eba2455e874f6b01
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:28:28 +0000

    dark decide learn upstairs exit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
