0d63924f8b4934e4cdd7de5f99ed91ea40439755
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:56:09 +0000

    accidental bother passage nervous so

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import array
-def get_array_item(array, i):
-        return array[i]
+def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
