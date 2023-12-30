0b74bf07a40743b221355e765c2c8d59564517e9
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:21:08 +0000

    touch apart right chase Uu

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
