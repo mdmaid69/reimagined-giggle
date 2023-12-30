de07fd5a8d4f50db89fcda582050a404fee4cfaf
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:38:21 +0000

    in attitude uponon across do

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
