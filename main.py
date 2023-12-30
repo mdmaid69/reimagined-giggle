5446737c5ba8f52b6b44cca3bf6adadedcb9bd74
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:02:05 +0000

    complaint cope account say afraid

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
