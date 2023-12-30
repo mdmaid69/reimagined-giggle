983b454ec33f24cdcc535553ed3b6749cb474f90
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:55:57 +0000

    beyond giveout terrible each everywhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
