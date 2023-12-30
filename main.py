1189e8b0293f8dc713724ee6781c5deac3feb623
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:23:40 +0000

    top doubt whisper fair surface

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
