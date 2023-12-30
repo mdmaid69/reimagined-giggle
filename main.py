b7bdf3cc1a093791f19a6624373693a1098dcc1e
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:40:14 +0000

    566end nervous date dance word

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
