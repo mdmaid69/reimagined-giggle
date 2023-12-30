da28ef1d4b35ab0cf36fff6c77bcfef7193df36a
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:27:22 +0000

    offend build talk type all

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import sys
+def exit_program():
+        sys.exit()
