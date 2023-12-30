91756134a26760970bb73df0e32b0a957d82cf5b
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:05:36 +0000

    mess Ii constantly spread pity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
