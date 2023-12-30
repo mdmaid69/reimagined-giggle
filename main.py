5370cb83e16821518716c803e54c9774adbace18
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:47:14 +0000

    broad sign state shine underneath

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import sys
-print(sys.version)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
