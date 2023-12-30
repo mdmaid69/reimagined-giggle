0012e95c40712a026ab04bcc10534b07f41b197d
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:56:43 +0000

    fall bin sail confidence name

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def append_to_array(array, item):
+        array.append(item)
