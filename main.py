72b898c607f335d1dacae8abeadcb3c9e4deb3b5
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:35:13 +0000

    ring last workon capital ocean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
