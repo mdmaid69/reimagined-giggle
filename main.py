b789354c331e15daa721e01db5d3c1a2f10a0290
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:30:59 +0000

    prize behind trick motion where

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def square_number(x):
+        return x**2
