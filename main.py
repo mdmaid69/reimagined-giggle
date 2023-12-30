49b27d96bfabe5bc2e1782710bd48db326e0fee6
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:48:54 +0000

    race drawing dick this stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
