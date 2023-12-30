4ee018c460bc635f0106baa057b25afdfa2e4856
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:25:45 +0000

    perhaps never drawing somehow sick

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_pressure(force, area):
+        return force / area
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
