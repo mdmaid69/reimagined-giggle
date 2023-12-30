dbb5c5ce265e183032661f38e20f2fcec4fd1094
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:43:30 +0000

    quick force error swim rise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_as_repr(array):
+        return repr(array)
