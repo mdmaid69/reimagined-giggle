8ab844f8f3dc2fc4da4e689c233aeb5452b9426d
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:38:09 +0000

    clear neat ever wait nurse

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
