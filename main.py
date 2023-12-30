01b1f0bad093d396ec5a839e08461a896e0225b8
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:39:25 +0000

    ball pot average close possession

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 def calculate_force(mass, acceleration):
         return mass * acceleration
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
