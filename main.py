73dc0583517d73724f8afbb4fa64ebc3078775fc
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:50:39 +0000

    shut black rule increase emphasis

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import logging
+def log_message(message):
+        logging.info(message)
