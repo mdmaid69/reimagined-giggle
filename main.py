8d0f9233d77988b83c82c6cd69058f859f12bc9a
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:08:52 +0000

    present block valuable rough exit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
 import os
-print(os.getcwd())
+def get_file_size(filename):
+        return os.path.getsize(filename)
