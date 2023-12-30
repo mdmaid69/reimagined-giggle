87dc7a3051799d59a95c87039dd88aea6c9cd2bf
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:18:45 +0000

    shade this dream certainly finish

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_speed(distance, time):
-        return distance / time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
