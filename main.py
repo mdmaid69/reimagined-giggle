f4f30428cb8be12ffc99f50d40a2554fde22f049
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:13:09 +0000

    comment proof blame learn meaning

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
