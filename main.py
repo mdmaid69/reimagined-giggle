31356f37b8ec20719c78169eb0e60d3629cd8c81
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:06:21 +0000

    straight strange beginning kill tax

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  def square_number(x):
-        return x**2
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
