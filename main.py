ad1576f4c2f88aecc99e80a2a352d1ca5451a8d2
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:10:27 +0000

    like climb free winter visit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  def convert_to_binary(n):
+        return bin(n)
