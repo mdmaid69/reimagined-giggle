83ccd3aba60035f9ff045d267fc13ae7de11bdd1
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:48:28 +0000

    allow could corner earn Zz

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
