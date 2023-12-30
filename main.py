6a10e2b6a930ed921bf2bc6ef42621bb120c797c
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:49:47 +0000

    attempt traffic type mine already

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_pressure(force, area):
-        return force / area
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
