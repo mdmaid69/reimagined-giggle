b1f73bce27757dd0d14460cf490303ba6141f4d3
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:52:28 +0000

    negative pull takeback right properly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-print(time.time())
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
