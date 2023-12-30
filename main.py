1d2861e602b87b06e9be875422a4d6a57612efb3
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:31:36 +0000

    modern dull fair cruelty shop

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def greet(name):
+        print(f"Hello, {name}!")
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
