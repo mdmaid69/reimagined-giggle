e634bf1935e9f35a269f4b4dd43f9ecb9693bbc0
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:06:18 +0000

    activity recent food town science

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def convert_to_binary(n):
-        return bin(n)
-def convert_to_hex(n):
-        return hex(n)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
