0d034306d12d3dcbf73b66627b992fede1685084
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:40:29 +0000

    at unpleasant equal completely on

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def is_odd(n):
-        return n % 2 != 0
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
