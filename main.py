3cd5f778557672ff9e0857f94c8960858126dbf4
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:35:04 +0000

    protection business performance trust tense

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_power(work, time):
+        return work / time
