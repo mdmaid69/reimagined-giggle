8199e793d2b28a434061c00b9f2b987881a6e43a
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:46:28 +0000

    keen wave square stone report

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def remove_duplicates(lst):
+        return list(set(lst))
