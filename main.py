afdfcebdefb267ff55b9c12bec1fab930aa4efb2
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:07:11 +0000

    argue buy real risk value

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def sort_numbers(numbers):
+        return sorted(numbers)
