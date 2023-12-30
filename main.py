90d4dce45215f893baff729cd8002faee10f0cbf
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:18:48 +0000

    they keepout punishment wall Kk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import re
+print(re.match("h.*o", "hello world"))
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
