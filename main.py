17cbdba6c9d67f5f237f9810a5c326e717c1be4f
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:05:56 +0000

    until effect pot luck free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  def square_number(x):
+        return x**2
