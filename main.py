8a1b650edc124cf3aadf985604fd1742171cf711
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:30:15 +0000

    switch disagree shut figure say

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
