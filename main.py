d99a500d8c61ad45f8efe2bc82e1a6da40c948fc
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:14:33 +0000

    pump conclusion praise average earn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
