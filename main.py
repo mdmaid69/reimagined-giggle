a400310ff199d211256432e2a1f30bf2c3ca8a88
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:51:50 +0000

    along mystery hers wise choice

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
