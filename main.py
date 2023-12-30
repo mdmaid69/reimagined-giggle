59cadfc832d5f0dbe571c06d0bdb854a06040073
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:57:22 +0000

    570enjoyable influence injury pause talkback

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
