a2128fecb0301b6bc88a2dfcbc15eec5cef6d6b2
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:37:57 +0000

    cause report something getaway thanks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
