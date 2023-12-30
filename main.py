d10f8dc29c07547e1b991f32f0de434710a76de1
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:58:47 +0000

    test godown cruel standard branch

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
