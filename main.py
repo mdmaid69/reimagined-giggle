e86c9eeab9887229ada2862894530675c9a11912
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:39:17 +0000

    demonstrate accept occur storm write

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
