c1aa4e6693484244b47d78bd4c8d862e9208118e
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:15:13 +0000

    forget spray call dinner spoil

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
