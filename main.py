37bcaf2351660307b4b80b8a9300a08f41f7cfda
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:47:29 +0000

    frequent miss basis deliberate space

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+i = 0
+while i < 5:
+        print(i)
+        i += 1
