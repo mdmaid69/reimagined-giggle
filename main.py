0780736daaa2ec41a1fff6eae83e3a8658c2c658
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:55:13 +0000

    cook pot last aware reduction

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
