c4350be786c744ae94ae032ca7433639d4bf188f
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:37:13 +0000

    contact influence last treat fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
