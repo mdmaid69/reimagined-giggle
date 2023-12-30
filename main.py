83dcb978089901f3b776953c32774bba5705e554
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:13:09 +0000

    start face could lonely apparently

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
