68d69726d1f0fcd611269f77b1f435d436b2316c
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:52:12 +0000

    machine form float part lookaround

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def greet(name):
+        print(f"Hello, {name}!")
