7ad8277e946a28f469c69e15012abcd4a5c4e85a
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:25:34 +0000

    mood cinema disagree act extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
