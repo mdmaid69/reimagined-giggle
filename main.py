087a72991ad4fced7b7f36eb4c128bc28409b3fb
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:33:12 +0000

    place circumstance our note closed

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  def convert_to_binary(n):
-        return bin(n)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
