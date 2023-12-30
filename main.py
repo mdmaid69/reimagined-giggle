2e93ef572475e70a49eee9bc5942ca4a98102c88
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:15:00 +0000

    thirsty left thankyou finger please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import sys
+def exit_program():
+        sys.exit()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
