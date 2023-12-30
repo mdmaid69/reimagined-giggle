f2904f0de7118f9749dcbac44ed4b6dea0276df2
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:58:08 +0000

    estimate satisfactory manage priority plan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
