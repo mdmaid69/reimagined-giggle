e1d133a6e81dc648fc63efc15dede576aaa1a63e
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:58:09 +0000

    talkoutof about distant block task

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
