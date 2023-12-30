4cdada10100f6c355fd272c8da23dc3b5b243cac
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:39:39 +0000

    getaway equal design convenient powerful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
