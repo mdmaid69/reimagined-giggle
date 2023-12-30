e28bb4246b24c797cdc5a0f0debd3223cb5abd03
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:33:43 +0000

    dry get shopping find line

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_exponential(x):
+        return math.exp(x)
