5e983f03fc578ce2929c629a0da52bb708a90fbe
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:56:07 +0000

    free top confused insist broad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
