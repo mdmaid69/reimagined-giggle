ffe15c4291cfc45e00bead3c362144148ef2e739
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:42:25 +0000

    mixture family climb negative war

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
