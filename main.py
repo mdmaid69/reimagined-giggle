5f516aa1cf8035fa00332a378be58512161c83b2
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:19:14 +0000

    Ii organization wide except wish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
 import array
-def get_array_as_set(array):
-        return set(array)
+def set_array_item(array, i, item):
+        array[i] = item
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
