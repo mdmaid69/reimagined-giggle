8f85e1494bb071c5ade1b7b47fe188de6f0b6d73
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:42:18 +0000

    window our post nerve otherwise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def is_even(n):
+        return n % 2 == 0
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
