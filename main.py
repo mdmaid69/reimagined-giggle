8670d885a17e3078d31b8d68c258894852cf3069
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:42:04 +0000

    past exit he interview face

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-for i in range(10): print(i)
+def get_array_itemsize(array):
+        return array.itemsize
