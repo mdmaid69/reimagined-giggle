c25f7ad85eced66e98b71479c91b856a91937eae
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:06:20 +0000

    below stick Qq inquire soak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
