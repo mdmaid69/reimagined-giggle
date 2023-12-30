36950f2cd9d89601a9b8b6557f0eea18e70705a9
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:13:59 +0000

    smile tonight plan load shake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
