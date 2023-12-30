cafe75ee209678fe34a8bb714f4c065d0d52d9c6
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:02:22 +0000

    pump estimate grade poor play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
   def get_file_birthtime(file_name):
         return os.stat(file_name).st_birthtime
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
