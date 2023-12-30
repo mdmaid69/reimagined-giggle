be3eaa31ff292831c4a50ebd20f5003ecb0b911c
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:54:52 +0000

    modern keep demand accept declaration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
