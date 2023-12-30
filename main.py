a0bf3e3ff88e794b18f1ad6cce7ca8135a182d58
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:51:16 +0000

    journey skirt born related band

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
