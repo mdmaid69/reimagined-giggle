be7f69e71606a91f135f1ac8a9e946e1c5b6486b
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:55:32 +0000

    row turn hurt hold muscle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import collections
+def create_counter():
+        return collections.Counter()
