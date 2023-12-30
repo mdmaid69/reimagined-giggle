ac3943deaa6346b6144b8c7441351ce222ecfaae
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:26:56 +0000

    Ff speech blow chief certain

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def convert_array_to_list(array):
+        return array.tolist()
