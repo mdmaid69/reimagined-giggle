c3e323c2053b8f6e64b0d6eff5e1bbf1e38533a5
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:43:19 +0000

    depend not lost pronunciation even

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
