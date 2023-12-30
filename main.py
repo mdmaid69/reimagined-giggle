1cc66e7c2c38c8eb5cd13555cdf708aead8c1e3d
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:31:21 +0000

    in block exact wish again

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
