0f92d8177ec0005e8c0c83957190970768ceb0cc
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:54:31 +0000

    duty happen child although rich

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
