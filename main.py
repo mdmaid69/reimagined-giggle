1057efc1aba263a7a046cd549ca9997a25bd3f82
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:22:46 +0000

    treat address aware down medium

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def subtract_numbers(x, y):
+        return x - y
