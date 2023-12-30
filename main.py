9553227b0bc7b9f7d183be3e0efb669d24fc0f42
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:29:20 +0000

    picture shock uniform modern arrangement

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
