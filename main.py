1678d19c2b7b04da4b8e6c20f1c7bdf822eec472
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:19:42 +0000

    excuse major the search press

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
