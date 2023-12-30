4ef13d7879099d52e0d390bfaf595c9adb2aaa3b
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:34:50 +0000

    set both gift lead stare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
