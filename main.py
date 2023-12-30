710a2d6980ec7b07a933652c455e04436f21cec0
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:06:28 +0000

    autumn minor long turnout successful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_power(work, time):
+        return work / time
