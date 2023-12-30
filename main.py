6831651dc0db346c03e237232ef428fac3c3943f
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:00:32 +0000

    deaf bother stare bunch gooff

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-print(math.pi)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
