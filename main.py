1f79c2a5ed7872a24f0a35bfa92fc5f29c037d4f
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:43:46 +0000

    tour else length arm stupid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
