39dfea34f5d4776dad9eed07f37d0b93b96bd2ec
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:25:01 +0000

    comein relative people know upset

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
