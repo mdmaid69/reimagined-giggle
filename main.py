4dc13235216cc861e8f430f831a704da35158451
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:17:49 +0000

    major meeting extraordinary warning innocent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
