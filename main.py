ffc1d9654bffa08ec8a9e3dbad106d7af4605bcc
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:26:12 +0000

    progress breakup adjective generous object

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
