17c09511de4e7cdbbf9132c50f41e258171432ef
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:26:49 +0000

    speech process programme noisy brush

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_work(force, distance):
+        return force * distance
+print("Hello, world!")
