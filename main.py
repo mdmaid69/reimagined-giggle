09a46eed61de3826ea9e770ba0d781a0d7b83a89
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:45:59 +0000

    action rid answer else introduce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_density(mass, volume):
+        return mass / volume
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
