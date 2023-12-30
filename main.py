b76a8a22a81f2f895cde650995608a7179caf228
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:37:28 +0000

    dress declare search cheap produce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import collections
-def create_user_dict():
-        return collections.UserDict()
+def create_ordered_dict():
+        return collections.OrderedDict()
