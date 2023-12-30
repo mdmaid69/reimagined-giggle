941ba348b5bbdf3a5d5ab542f9a7e91fcf0966b4
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:04:04 +0000

    personal bringforward touch high flat

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
