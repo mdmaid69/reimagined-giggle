cd516ece4996fed9d6d10f609e8c2fb5b1a5821f
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:38:27 +0000

    possibility cook plate film lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
