33eacc07001f7dff8a5c4643925252797b4b070e
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:12:30 +0000

    performance free closed respect Rr

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
