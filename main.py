bb153acf88b4b927eec0d3e8892474c648ec3d3f
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:49:20 +0000

    full plastic crowded date twist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_work(force, distance):
-        return force * distance
+def get_array_slice(array, i, j):
+        return array[i:j]
