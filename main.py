1e3011cd3e86ee0911991614960446d092a8dbc5
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:56:57 +0000

    before small term car doctor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
