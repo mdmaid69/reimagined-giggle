06977466b39e57d61dedca3b07cc6438ca9e758f
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:09:05 +0000

    act pretty basis home environment

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
