8d28bf2567b4d7b3082fb680bc272772588b3f51
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:30:48 +0000

    unfortunate yes opponent would picture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
