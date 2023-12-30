6823fa1629a4d58e48f223083d0dc94974feb182
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:55:37 +0000

    leg this address frequent cover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
