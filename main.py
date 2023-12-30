63f60c4fc791ac960feda0238ebfcf3521c50d50
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:00:47 +0000

    influence dear threaten health best

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def remove_duplicates(lst):
+        return list(set(lst))
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_string_from_array(array):
+        return array.tobytes()
