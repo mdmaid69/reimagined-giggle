6eedaa6ed0c2a295c096a061a265e53677b5c699
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:21:32 +0000

    consider vote interested proud theirs

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-for i in range(10): print(i)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
