aaa15150e6ec3e5be9ac9417c0e69087c715e85f
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:06:44 +0000

    previous past file church ours

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def greet(name):
+        print(f"Hello, {name}!")
