6aac90bfc43971d5c02127a3ffe4503d7fda0c26
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:52:32 +0000

    private black that workupto complaint

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
