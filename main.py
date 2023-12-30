b53f2965323353e97f3206539f2e328bfd918bd9
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:27:56 +0000

    generally alive trade sure rush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
