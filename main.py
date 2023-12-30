30b94d0b29f8c4eac7691b61beda2e30181ee14d
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:11:01 +0000

    famous brief present maintain conduct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def reverse_list(lst):
-        return lst[::-1]
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
