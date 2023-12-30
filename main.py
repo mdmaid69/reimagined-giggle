53779b98e84db419cd0c7f1071f5947bc230af42
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:50:29 +0000

    model imagine beneath left thanks

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_user_list():
+        return collections.UserList()
