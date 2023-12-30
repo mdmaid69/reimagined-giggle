3480c52a4e6e92d8e563354a422760f479051698
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:39:30 +0000

    march achievement left wave practical

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
