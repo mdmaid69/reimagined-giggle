57906fa5cc52370af5526cac95a62aa214b1e02e
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:18:05 +0000

    university very worse tone slide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def calculate_work(force, distance):
+        return force * distance
