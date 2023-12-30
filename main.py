d613c46f9f23c194134ba72caaa7a31669fd7bab
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:34:47 +0000

    uncertain into relation runout air

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def cube_number(x):
-        return x**3
-def cube_number(x):
-        return x**3
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def add_numbers(x, y):
+        return x + y
