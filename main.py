16372b75eeb2d60925c3de51c3b76c770c2b4780
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:46:49 +0000

    scrape Yy anywayanyhow getup but

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
