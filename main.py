1e788b6ccb66e2cc1d8f44aec2dc368af74ff2f9
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:57:43 +0000

    exchange heart arrange extremely replace

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
