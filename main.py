001e4aa9e6efbfe9a409b1f1b325562a367e9f77
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:38:53 +0000

    blame upwards everywhere Zz class

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_volume(length, width, height):
+        return length * width * height
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
