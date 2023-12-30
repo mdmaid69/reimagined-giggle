cb5c7d59bfa876c44794c2f10f58b979638c3766
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:31:50 +0000

    raise unite foreign call boat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
