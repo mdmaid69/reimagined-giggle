e9f4b4334ce45e6b0c27986940bad71830309a82
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:58:05 +0000

    part almost course ask forget

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
