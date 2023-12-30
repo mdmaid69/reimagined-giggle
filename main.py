af724a21b76a87ca1e330007aa05be8482651b17
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:53:04 +0000

    carry long regret attach late

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
