104f953490c68f6ba5275964d880726bf349ebe8
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:36:12 +0000

    under apart Ee doubt formal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
