d475a14cc6a31b4b49429f229933cbffde3af369
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:57:45 +0000

    apart test last claim exchange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import collections
+def create_user_list():
+        return collections.UserList()
