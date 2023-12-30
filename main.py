723cf16de0cc632d0e6efd069fb6e4f58a67c38f
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:30:25 +0000

    funny too accident independent hand

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
