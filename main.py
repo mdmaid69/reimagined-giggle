7f85ae93deefc58e2b00489798517dd0d067ef55
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:47:39 +0000

    business paper label personal discover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_union(list1, list2):
+        return set(list1) | set(list2)
