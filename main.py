3ab4050159f39007bd70252930ea9b66895f6b6e
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:41:21 +0000

    pool head equipment gap like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
