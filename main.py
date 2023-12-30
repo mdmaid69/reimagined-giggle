a3df80fd0b0d88c1118a51c618a8e6ee4fd361ba
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:26:44 +0000

    tip because learn wrong characteristic

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import tensorflow as tf
-print(tf.__version__)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
