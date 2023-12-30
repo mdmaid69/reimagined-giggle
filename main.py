e476ae6fb596983bd9bf957cdbbd1f74acfa660e
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:19:44 +0000

    group case back through type

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
