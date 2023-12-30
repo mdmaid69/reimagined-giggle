a1e23c381f02f5ef886d375f6f7786e4220089a6
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:31:35 +0000

    past gooff exaggerate beautiful bite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
 print(sys.version)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
