c3a27df3164763bb30e61673f63b44fd3211b88b
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:56:59 +0000

    game dress blow fight complete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import numpy as np
+print(np.array([1, 2, 3]))
