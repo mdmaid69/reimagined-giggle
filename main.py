75fc651b48485f024c693e6b140ba3605faafb08
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:46:08 +0000

    however area health drink regularly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import json
+print(json.dumps({"name": "John", "age": 30}))
