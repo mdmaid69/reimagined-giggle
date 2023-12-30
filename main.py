bcb17cc135338ec23e8d138b6faca3bbbab3fa71
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:51:47 +0000

    everywhere shrink cool contrast break

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
-print(os.getcwd())
-def calculate_density(mass, volume):
-        return mass / volume
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def square_number(x):
+        return x**2
