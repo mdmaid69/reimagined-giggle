f26f46b03f4e3737c926aa39b188a069bf99c8e6
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:30:23 +0000

    production harm ear surprising advise

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
