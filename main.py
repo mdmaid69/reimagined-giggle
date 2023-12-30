c2fbaaf608699d8cd7cf8c5857c6ac2e901cff9d
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:17:21 +0000

    from successful trap long direct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_typecode(array):
+        return array.typecode
