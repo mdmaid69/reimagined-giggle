436bb089f3545c7f4b664b5932fc39f24687d1f2
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:17:44 +0000

    connect exciting Pp art can

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
