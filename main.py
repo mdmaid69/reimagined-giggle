1ebdcdd7d00b8ae7d6c828323b419d69ff04c6ee
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:40:23 +0000

    frame point crash stranger wander

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
