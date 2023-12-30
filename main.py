9000c1431532a87f4fc0b9ab7e038836d82e2e3c
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:51:33 +0000

    woman side private tape ahead

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
