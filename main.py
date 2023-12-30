5d6c19562c9b01cca128a3316698f1571a518ddb
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:25:50 +0000

    fly warm forever brain limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
