e6d1729f34a650564a12d8a61073d69e9053e145
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:22:46 +0000

    kind form Hh fact trust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
