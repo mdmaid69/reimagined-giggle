8f2aea3bc77190c13c437213f236fbf20eb785c8
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:43:20 +0000

    care myself something crime plain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def calculate_area_rectangle(l, w):
+        return l * w
