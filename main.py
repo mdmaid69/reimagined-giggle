adfcb0b49de7f050ab9c0495947804e920d86494
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:38:47 +0000

    lie comment room shake fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import sys
+def exit_program():
+        sys.exit()
