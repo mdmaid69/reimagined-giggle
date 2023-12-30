0796ac23f1d2fe02e22a259be00bfc458d04d471
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:28:28 +0000

    pencil strange build broad run

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_as_list(array):
+        return list(array)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
