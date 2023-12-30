2bb9aa57694b138b6b22f8fc9ce3c930b84dbc4b
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:34:12 +0000

    on reduce comeup create before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
