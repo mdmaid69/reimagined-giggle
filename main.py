78224a016782649518759eff3a908b9933d60865
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:56:11 +0000

    early lookup rub last still

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
