635bcd1f12211be77a81569f65d9a7448570d23a
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:28:34 +0000

    rent narrow miss health cope

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  def subtract_numbers(x, y):
-        return x - y
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def count_words(sentence):
+        return len(sentence.split())
