d3a90d4476880aeacd8cb377bf1d3d96072662ab
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:16:23 +0000

    defeat contents satisfied muscle laugh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def remove_duplicates(lst):
+        return list(set(lst))
