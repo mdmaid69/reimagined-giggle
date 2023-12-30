09828ea92365b2314de63acbbde7eed2349c1b50
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:26:44 +0000

    praise artificial pull comeout right

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
