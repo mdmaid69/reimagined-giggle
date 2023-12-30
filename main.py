b9a5aff9d9734789baa29dce0ffab3a1502fc0f2
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:22:09 +0000

    refer different slide accommodation reply

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import re
-print(re.match("h.*o", "hello world"))
+def square_number(x):
+        return x**2
+import math
+def calculate_error_function(x):
+        return math.erf(x)
