48fb417d7bc13e4984d677c2e0760ec8581a6239
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:38:33 +0000

    and matter ring desperate sign

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
