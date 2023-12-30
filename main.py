560b608c3f09188bfe984af4df1b3f4f8e077847
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:08:29 +0000

    improvement grade number geton dead

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
