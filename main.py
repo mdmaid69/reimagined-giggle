55ec29b5924a90f1fba05d7638169f23035c4b48
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:17:30 +0000

    best runaway present before mixture

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
