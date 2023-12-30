000a279bc5abc4ac3de1b24c045f5094ff01c635
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:08:27 +0000

    too slide describe protest trap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
