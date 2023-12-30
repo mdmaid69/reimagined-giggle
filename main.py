0ede81c0bcef5236b7834a868923bd4299b7c169
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:29:10 +0000

    die study hurry related disappointed

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
