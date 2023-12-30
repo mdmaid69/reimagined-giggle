f9c1f1568eff146fe14b18a8ef4320613fcaa213
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:06:53 +0000

    slow scrap accidentally supply proof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_density(mass, volume):
+        return mass / volume
