72b710c60f092da6a7c60f5ba3d76d1d57e8096f
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:16:24 +0000

    scrap herself emotion work string

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import platform
+def get_os_info():
+        return platform.uname()
