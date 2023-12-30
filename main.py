5e29e9ba8ea792043ae547066560a6561978280f
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:53:28 +0000

    letter arm crime suit over

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-from collections import Counter
-print(Counter("hello world"))
+import sys
+def exit_program():
+        sys.exit()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
