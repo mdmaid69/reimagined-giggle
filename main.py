9fcb989bf72129d3db3aef4a1869ff29ddca8b7e
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:20:08 +0000

    machine date as family underneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import logging
+def log_message(message):
+        logging.info(message)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
