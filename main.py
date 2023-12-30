b8fbbcb2a8f5c6eb3b2bb799c2db4655064ccf6e
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:29:00 +0000

    taste capital scream spill wing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
