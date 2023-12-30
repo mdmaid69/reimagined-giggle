fa3bca5d92e8328a6490325cc57a1f81ef6a53b5
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:52:30 +0000

    pen here bye complain face

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
