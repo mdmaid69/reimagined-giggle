84285c6a3ac712eba298cab4ae3d3304c228258d
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:01:55 +0000

    describe pretend frightened argument immediately

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
