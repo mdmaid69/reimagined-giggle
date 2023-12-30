5a3bcdb17d5f8da5c29ade212bb9f092ca43f535
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:19:57 +0000

    actual lunch store objection walk

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
