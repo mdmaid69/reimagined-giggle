36aec1ea8794b1316680bfa805c1e06002541a9d
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:22:28 +0000

    Gg book meet bend serious

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def count_characters(sentence):
+        return len(sentence)
+import platform
+def get_python_version():
+        return platform.python_version()
