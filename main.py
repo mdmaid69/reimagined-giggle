86a96d4eb9a5b9e8e9b06151fee97b33946e118f
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:07:47 +0000

    age takeout intend rub disappear

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(lst):
-        return max(lst)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def is_odd(n):
+        return n % 2 != 0
+def count_words(sentence):
+        return len(sentence.split())
