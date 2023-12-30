3f8b6cdb388f8858c4eedcd848db9a3f10a8a501
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:08:41 +0000

    comeround though bring thin big

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
