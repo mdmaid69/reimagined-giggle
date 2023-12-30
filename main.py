37f4edb1915ba6b51565966e757752b84aac36c7
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:29:21 +0000

    letter would intelligent front home

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  def convert_to_octal(n):
+        return oct(n)
