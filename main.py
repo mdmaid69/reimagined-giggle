a2e07aa8740482b774a6dea17f7a0fa421b1d85e
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:21:25 +0000

    beneath pinch visit invite contain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
