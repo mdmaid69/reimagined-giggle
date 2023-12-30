830eeef98e27443cbf2a716d2bf9251ab11780da
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:46:12 +0000

    something visit mean branch stupid

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
