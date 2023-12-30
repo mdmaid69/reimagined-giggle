cdae8ec65ba4249f2abb98e3050de3c39e6a099b
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:15:20 +0000

    now thirsty couple exciting such

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import os
+def change_working_directory(path):
+        os.chdir(path)
