0f50f4d33310ced96c8a303be5de65db8a6be2b8
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:44:24 +0000

    left inquire worth probably several

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
