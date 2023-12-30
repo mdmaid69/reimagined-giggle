216eaec6789761244aad51fbd881eac97ef0df24
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:46:16 +0000

    beyond petrol choice total culture

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import time
-print(time.time())
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
