bcc36d993dcd35affe4ffea1eff69b6680bb8d25
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:17:26 +0000

    majority smile meal closed callat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def count_characters(sentence):
-        return len(sentence)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def count_words(sentence):
+        return len(sentence.split())
