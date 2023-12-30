8c15c42a1222c725ffa4e0ac8bd5e714ab04119d
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:29:56 +0000

    offend like noone steady slice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_directory_name(path):
+        return os.path.dirname(path)
