f752200fd1723f2951c6e2072a34cf9e521e1cb2
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:31:14 +0000

    argue firm slightly fold card

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
   import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
