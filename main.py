a8f91bbcb82617a183550eef69e2633b4cf28c95
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:13:16 +0000

    bend somewhere breakfast sure joke

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def find_max(lst):
+        return max(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
