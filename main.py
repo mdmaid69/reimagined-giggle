1179c18ff3bca196578bf840f1bf16d702f742cc
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:33:54 +0000

    damp let lock injury several

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
