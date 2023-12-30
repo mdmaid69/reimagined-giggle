015a7609e0931695327d1531c247c95778c796a6
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:35:54 +0000

    wide safe owe nowhere appeal

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
