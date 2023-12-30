a49495dcb5fc215a32fef6936ee661b1f42028c9
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:57:14 +0000

    back ashamed song gather rent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
