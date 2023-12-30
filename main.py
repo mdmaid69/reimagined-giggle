dd203502e2c44833d44506c40253846bb5045a5e
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:22:56 +0000

    instruction lost question danger attitude

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
