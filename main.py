b99972a4560587a57ac7c57b548b3a48a1bc43a7
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:58:37 +0000

    zero base conduct discuss past

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
