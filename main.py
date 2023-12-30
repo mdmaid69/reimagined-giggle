459eea0dacaf6946c2b0109db11731e84630e2e7
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:49:30 +0000

    complete Kk card coat second

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
