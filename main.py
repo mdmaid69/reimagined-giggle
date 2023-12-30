9464e012de5cbd1578c00e19c18cfe953a9315c8
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:50:54 +0000

    cut discover stay impossible past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_as_list(array):
+        return list(array)
+def add_numbers(x, y):
+        return x + y
