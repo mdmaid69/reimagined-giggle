69d661e695a9d1a0ab273a890c742293a7eea47a
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:04:12 +0000

    exciting near ride quality everywhere

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def remove_duplicates(lst):
+        return list(set(lst))
