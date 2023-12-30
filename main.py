c3d774f6bbe7ae707dc5ddf11f29b6d45bf5efff
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:44:51 +0000

    vanish crash different blind hold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
