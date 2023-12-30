f664e9582b2f2455428cbfe4551c4b0ad71408af
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:39:48 +0000

    total shame measure report hello

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
