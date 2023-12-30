f5a69ca100503a0b90499f97c79ce3d1759d94c7
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:27:38 +0000

    getaway develop government appearance tip

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
