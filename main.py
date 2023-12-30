31caaf117b8a70858adbcf2c92134f3500393301
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:12:35 +0000

    getout punishment blow after measure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
