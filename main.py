627a1ee8e79086ee35df785ec69cf61bed800a5b
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:11:00 +0000

    wet model scare skin thanks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
