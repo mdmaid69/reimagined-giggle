af48b8ff523964b5793753db23ff40b8e43276b5
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:35:27 +0000

    please corner anything advice convenient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
