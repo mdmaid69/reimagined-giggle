d53aa1074f49696db1e4b9c79d723fcf42a89651
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:30:21 +0000

    not fix escape means heat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_area_rectangle(l, w):
+        return l * w
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
