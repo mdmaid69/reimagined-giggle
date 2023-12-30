c85ca872dbb75a0c305d826a1b0e61dc76f28987
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:05:30 +0000

    most spill check fierce calm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_work(force, distance):
+        return force * distance
