98eb72f0b6eceb5211024a281ee60fdecb0fad17
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:37:38 +0000

    dance example crash cap manner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import os
+def remove_directory(path):
+        os.rmdir(path)
