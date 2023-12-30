9c7181b612e25dbbeced1d3ec7d353f2d9e8fcbc
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:56:36 +0000

    awake safe ashamed injury excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
