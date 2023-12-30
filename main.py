da143c2c9723716a2daa6628648e2206dfa4b4a4
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:13:20 +0000

    exercise march difficulty much warning

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
