012b8bc74dc8fce6b1ae032a897bf793a7619a65
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:19:07 +0000

    care bit recover cheap goback(to)

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import sys
+def print_python_version():
+        return sys.version
