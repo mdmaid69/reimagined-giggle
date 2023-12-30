ebdbaa57dabdd281a7fd09b909d3ce8c1ac46ea9
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:26:56 +0000

    area artificial high knock difficulty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_as_str(array):
+        return str(array)
