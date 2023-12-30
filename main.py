9a92b6c64aae6fe82d2942ed5f782c10b5e95031
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:00:41 +0000

    unpleasant round division project trial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
