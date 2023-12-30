adade96f5419d516f5482b456a19e093818cd041
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:34:29 +0000

    air clearly breakfast enthusiastic watch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
