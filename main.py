df29f1af3b390a0ce9cc01baaf222e1e6aba51e0
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:45:33 +0000

    snow door interfere condition stamp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import numpy as np
-print(np.array([1, 2, 3]))
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
