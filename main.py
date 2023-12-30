4bbd307cd538131a66bf426fe295a8651e1aa95a
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:57:18 +0000

    press bite nor hollow danger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
