0b4a2a49b76891aa168ae5e702f18d2894bef41f
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:46:58 +0000

    comment lost although chin soft

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_current_directory():
+        return os.getcwd()
