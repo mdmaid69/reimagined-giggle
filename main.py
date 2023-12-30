4407ba6a87cb14769d2904b2ef56876565c8382d
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:17:44 +0000

    pronounce lesson laugh know pure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
