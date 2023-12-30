6e9e97878232915c78ef87e1bb102dec55b14493
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:08:31 +0000

    valuable machine guilty proof about

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
