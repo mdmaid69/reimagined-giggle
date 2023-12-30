2035d0fff3d6fd75103e68f3ee7ed9f463e7f05b
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:17:19 +0000

    rid drawing provide answer spill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def divide_numbers(x, y):
+        return x / y
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
