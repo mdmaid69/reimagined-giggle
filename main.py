37a10925d5245b232ee9cf2316cf18cbc5204933
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:26:02 +0000

    close object birthday exhibition trick

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
