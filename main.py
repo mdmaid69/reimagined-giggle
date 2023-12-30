727d421becbb61282f5777168ed9fae0c043ad07
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:41:01 +0000

    unfair top takeback disagree safety

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
