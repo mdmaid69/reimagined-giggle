dea0c4158bd6c6976d6a18665b70d8ab4a77f5c4
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:09:33 +0000

    design if command quality cruel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
