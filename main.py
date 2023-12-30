e6a3896614dd3d6451691dbbdafaefef0f78f5bb
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:21:14 +0000

    weather but period rest accidentally

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
