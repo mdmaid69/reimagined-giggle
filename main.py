2f622f78db249f83a315c9a9f1edefd1892d289c
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:51:30 +0000

    medium explanation luck shall suit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
