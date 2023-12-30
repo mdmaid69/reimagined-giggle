9aa40bfe383cd7bfa2b53c150d46f234a691c742
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:21:31 +0000

    check payment lend then contain

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
