fff2e662881fccbb5caabaa01ab8208e026a0c82
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:13:43 +0000

    announce scared mood immediate noise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
