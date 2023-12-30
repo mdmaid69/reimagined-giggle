2a8db379bb17262e9067f7f1df0c78867353dc7f
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:38:03 +0000

    leave sentence by mind in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+x = 10
+y = 20
+print("Sum:", x + y)
+import random
+print(random.randint(0, 100))
