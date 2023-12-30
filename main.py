a7265c3574c8842f4683470759eba5427d14c6ab
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:17:25 +0000

    brush environment top achievement develop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
