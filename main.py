fdeacee626b07688358b571699ea432087fc859b
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:45:41 +0000

    dead stamp of side exhibition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import numpy as np
+print(np.array([1, 2, 3]))
