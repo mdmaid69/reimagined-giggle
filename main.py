ae63bbb37c0c9de1d59885d8055c02c41dad090f
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:01:18 +0000

    marry general among proud tense

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def reverse_list(lst):
+        return lst[::-1]
