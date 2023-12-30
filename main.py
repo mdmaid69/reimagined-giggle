15cdd3509dc929c6506dde0a739a60936ce5761f
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:58:17 +0000

    down shock factory path file

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
