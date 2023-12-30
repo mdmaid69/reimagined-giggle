0e814072370211ef79d721a4ae1b030af0f25f61
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:30:26 +0000

    cause ought occasional police something

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
