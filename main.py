7546083e8ad4f0c0acc9a9432a74b5b9090ba956
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:59:28 +0000

    comefrom would bottom later another

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
