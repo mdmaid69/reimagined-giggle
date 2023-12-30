87a8a7cc7ca6c76b3b2037bb3aac3f956743f64a
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:39:33 +0000

    otherwise pour emphasis wish breakout

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import collections
+def create_queue():
+        return collections.deque()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
