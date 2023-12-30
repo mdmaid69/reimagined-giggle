fa237652b956ee6585bb8f091e722f072c937202
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:19:19 +0000

    probably food acceptable on use

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+i = 0
+while i < 5:
+        print(i)
+        i += 1
