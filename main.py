f1bf433b5d9a95d04a93270eb284893efcfdf11e
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:54:19 +0000

    scheme pinch instead situation side

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
