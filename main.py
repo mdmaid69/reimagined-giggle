bec79d88a8013513861fc4f1d043d9fabb8b32bc
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:15:21 +0000

    little bush owe music physical

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
