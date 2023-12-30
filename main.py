4c4819d762fb467651c5aba3b75ca1821d0113c0
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:31:57 +0000

    we happen cry fat create

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
