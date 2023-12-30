4cfc702c4155bc879d150fde9535c447898b903b
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:10:44 +0000

    left wing risk dance see

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
