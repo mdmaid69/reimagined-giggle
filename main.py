95e614b8f0f618478c80aac4d51d1eb1ff353295
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:28:53 +0000

    gap gradually approve month your

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def is_even(n):
+        return n % 2 == 0
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
