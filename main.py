f1f88ec0017c8f4768180efb9ec03d71b0a13469
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:18:08 +0000

    wherever through case simple urgent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
