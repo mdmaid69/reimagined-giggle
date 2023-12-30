9d79749f87e4a69abcbd1d7298c6bca646e64b6a
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:37:11 +0000

    vanish high Itself properly my

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
