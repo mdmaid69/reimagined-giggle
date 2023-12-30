06d9f32c89fed25c23c88d41b12e52fc16373347
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:50:17 +0000

    game equal hide book front

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
