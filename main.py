ad895d9359c8131ccaec8ae0f714693af8c65d74
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:03:02 +0000

    exploration demonstrate yes dinner accept

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
