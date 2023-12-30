f50634e054579b67772794f4cb8e916e42b749f1
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:11:31 +0000

    also giveaway appear who rise

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_power(work, time):
+        return work / time
