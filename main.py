d60b3e6bdbb0728686a570578ef32c5331b9512c
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:04:10 +0000

    amount buy receive move listen

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def convert_to_binary(n):
-        return bin(n)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
