31a47db31ed5024b64f3640aab4ec51bd06ad018
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:18:44 +0000

    purpose close regular new after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import time
 def get_current_time():
         return time.time()
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
