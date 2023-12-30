1f8b8b93f18212c7d7e2ee2e9375efb7a52ec5b9
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:57:21 +0000

    peaceful human mean regional experiment

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-for i in range(5):
-        print(i)
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
