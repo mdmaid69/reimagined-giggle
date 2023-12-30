764e76aa49d22794298d26a679c8b9e12108232c
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:16:58 +0000

    lesson environment few check really

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import numpy as np
-print(np.array([1, 2, 3]))
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
