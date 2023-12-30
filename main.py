85e47b99bb5dd520e32aacada7a15c8bb1e09bca
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:32:08 +0000

    promise talkback forget full shallow

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import sys
-def print_python_version():
-        print(sys.version)
+import random
+print(random.randint(0, 100))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
