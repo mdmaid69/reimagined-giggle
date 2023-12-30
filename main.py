711590a0ea5c6545815c61e4e51c8ab114a3c1b6
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:10:05 +0000

    state suggest half least add

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def calculate_density(mass, volume):
+        return mass / volume
