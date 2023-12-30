20098109a81b4fbc06aa192e65649daf62cd7cac
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:13:12 +0000

    comein in quickly sentence bang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
