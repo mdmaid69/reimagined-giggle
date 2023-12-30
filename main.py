291e4e7e0c2939870f73918e8e5f984b3259538f
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:03:08 +0000

    avoid impressed ear believe repeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
