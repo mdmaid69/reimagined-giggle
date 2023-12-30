57252199fe87300abb3a79defe9b761d1a267524
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:59:52 +0000

    run wealth brother fierce typical

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
