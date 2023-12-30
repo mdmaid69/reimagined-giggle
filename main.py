cfb7928f2c695005506e4d5292310e08ae96bc68
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:49:35 +0000

    great need dress unit consider

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
