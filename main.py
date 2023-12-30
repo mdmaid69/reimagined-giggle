c84ce06b560deb8ac116f4f46134791ee1af7a15
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:40:11 +0000

    than turndown computer decision urgent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
