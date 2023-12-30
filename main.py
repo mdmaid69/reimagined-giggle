ca2e8e991aa32f318207457c434c891b7f03f587
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:04:02 +0000

    aim this book opinion smell

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+name = "Python"
+print("Hello,", name)
