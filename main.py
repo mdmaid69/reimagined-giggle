1fd3fe382908cdfb202fd06e0094a842b4f9ce46
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:24:57 +0000

    grand quit charge work sound

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def convert_to_binary(n):
+        return bin(n)
+  def remove_duplicates(lst):
+        return list(set(lst))
