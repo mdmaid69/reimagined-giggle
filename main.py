a4710bfde5235d82f406b4d487ebe03d1e6fdaf3
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:23:50 +0000

    fly divide search right loud

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
