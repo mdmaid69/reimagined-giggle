5f037469eff6a08bb9f9202bc2a804f00ba52a14
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:13:11 +0000

    comein company advantage lookafter mine

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def add_numbers(x, y):
-        return x + y
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_power(work, time):
+        return work / time
