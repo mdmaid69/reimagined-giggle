7eba64561e8f2702ae98ee7c03ba0ebae3aac9b4
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:33:02 +0000

    crime translate party expression lean

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
