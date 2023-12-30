35c22e838f9b93613e84f61070923fec5fc7bff5
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:04:42 +0000

    fight lay wind grow might

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import math
+def calculate_sine(x):
+        return math.sin(x)
