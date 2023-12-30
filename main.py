f790a789086b93ab2390269d98aa4d6b1738b3a4
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:09:56 +0000

    hollow person basis judge slowly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
