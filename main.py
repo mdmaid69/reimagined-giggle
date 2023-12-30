c95b95c6889f1ead948fe330e931fed440331a23
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:28:03 +0000

    light month affect traffic circle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
