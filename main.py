8869127a949a066a428c31f6f68ee9e44cb7fb2b
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:28:13 +0000

    method mad store sensible pleasant

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
