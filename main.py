2c88484f3740d87fc415629d357fec7ca2c11904
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:18:59 +0000

    accept sick attempt clever very

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
