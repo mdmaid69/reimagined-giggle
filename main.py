cefa206276f5749a99fec0a28a92207f892ccafd
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:51:11 +0000

    snow theirs float 566end difference

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
