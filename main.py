560c60ca30704a494d3fe07a232abcb7b6f58915
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:44:16 +0000

    no independent convince evidence card

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
