573da3367cea733dd9edf74a29aec61ae398634e
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:11:31 +0000

    bet remark miss sure raw

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
