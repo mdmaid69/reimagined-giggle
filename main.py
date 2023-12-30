94be5c56ffd6f51c7cc49d46c33a953c6050af90
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:02:53 +0000

    control sport why hand somehow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
