90bc35f1ec251b88a08f19423a41d18af2b02797
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:18:33 +0000

    mean treat teach quit sight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def set_array_item(array, i, item):
+        array[i] = item
