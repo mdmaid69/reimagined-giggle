c05e85e444348c8932ace0e1583d1ec0cc2ac399
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:40:25 +0000

    comeover brief protect bear if

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
