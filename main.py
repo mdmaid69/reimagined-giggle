296770f45be6f140d747e90d3824d2f349c2c52b
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:03:31 +0000

    reasonable later rise hurry wise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
