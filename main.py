1cce1968bcfa253a306e7f91ae62888d0152c1b1
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:07:38 +0000

    improvement nurse old form answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  def is_odd(n):
+        return n % 2 != 0
