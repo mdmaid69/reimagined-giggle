1c521ae80a2c381bc0e0c833bdd61424ccdb1898
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:08:47 +0000

    realize cost true various relative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
