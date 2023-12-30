2bbbdcea4988fa54fb94feeee2a82246b69425fa
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:58:39 +0000

    university right point promise sure

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def count_characters(sentence):
+        return len(sentence)
