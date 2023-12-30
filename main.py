3ac8c0e1a522f5b23a3276c7a3cec5f5419427c3
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:52:58 +0000

    mistake talkback weather cause creature

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import tensorflow as tf
-print(tf.__version__)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
