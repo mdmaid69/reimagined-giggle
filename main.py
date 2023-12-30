8c6b7f4131211e50b4d08519a42464d76060dab3
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:19:16 +0000

    job inside hello stay house

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
