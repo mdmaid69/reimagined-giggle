9f3851d3e25bbd18e2fde2ffd8fa08866edd97d9
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:34:33 +0000

    act reduction after dick still

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import array
+def get_array_item(array, i):
+        return array[i]
+def calculate_density(mass, volume):
+        return mass / volume
