90b0cf5166613e82b909ab53166d28416c3498da
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:56:34 +0000

    obtain squeeze surprised fit throughout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
