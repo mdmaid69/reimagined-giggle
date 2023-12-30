b2e575f8221876e5e4317740f0dba4bdeba714af
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:29:54 +0000

    keep fork age disgusting borrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import collections
-def create_user_list():
-        return collections.UserList()
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
