96ed98b68708b36b2f80985b761d0a497ed0c8b3
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:33:23 +0000

    song amount movement pain respect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
