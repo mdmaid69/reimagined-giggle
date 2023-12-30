98d5d6c051bf4992b9055688dff70d183cd62844
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:49:09 +0000

    hurt steal outside model which

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def convert_to_hex(n):
+        return hex(n)
