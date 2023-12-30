4fd564c725295db1fdaa94ac6bce7e658b3f7981
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:25:38 +0000

    reasonable against out difficulty mix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
