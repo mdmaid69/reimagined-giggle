eea08dbe963fe142cb5b92ad09abbc662aeaad1a
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:49:28 +0000

    someone waste please hit real

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import platform
-def get_os_info():
-        return platform.uname()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
