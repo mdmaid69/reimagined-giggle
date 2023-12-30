5729069e787d4e0d4723cdc42413ba7a1ef40cb9
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:21:33 +0000

    smooth objection whatever reason afterwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
