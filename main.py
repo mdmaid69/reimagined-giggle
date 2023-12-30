cd3a90de332ba6360af29ee9f87f130330f6669d
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:00:35 +0000

    last he how strength no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
