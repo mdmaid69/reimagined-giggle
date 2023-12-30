ec620027c5b65f02dded47984e8cbfc4627ab3e7
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:41:40 +0000

    bag convince description either connection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def is_even(n):
-        return n % 2 == 0
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
