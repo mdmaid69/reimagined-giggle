be69719006664632b2524e6764ad22a08cf3badd
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:35:40 +0000

    collection expression pot along whenever

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import sys
+def exit_program():
+        sys.exit()
