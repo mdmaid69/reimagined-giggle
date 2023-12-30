78cff1cbe8348f9dea0467b500ef148ee9555f6c
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:58:28 +0000

    hard available brother door term

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
