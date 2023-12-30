5f98ef9ebd931ee05400a5c6e2ca50febe3fee9b
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:46:36 +0000

    since track ought good luck

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def read_from_json(json_string):
+        return json.loads(json_string)
