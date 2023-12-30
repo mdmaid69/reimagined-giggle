62c8550df548749085a458a4921c2ac19a768d80
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:56:40 +0000

    identify trip peaceful lean plan

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
