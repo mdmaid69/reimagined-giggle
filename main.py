0f72c5cafdd339b0495d82018662812db2ade7be
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:43:10 +0000

    loud bang practice row runover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
