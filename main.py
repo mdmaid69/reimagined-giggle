2ec8a639b050628d6f321597cf73871e71fe46b8
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:59:43 +0000

    year offend feed reduce comeoff

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def reverse_string(s):
-        return s[::-1]
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
