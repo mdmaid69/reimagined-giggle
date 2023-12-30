ad021081e3032a2abaca15e2b425c634f9397dce
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:23:49 +0000

    lie mystery attitude attention although

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
