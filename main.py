b4888f15f1048850dbd51807e5f2c4cb8f2d4736
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:24:48 +0000

    sensible error sting fit flood

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import sys
+def print_python_version():
+        return sys.version
