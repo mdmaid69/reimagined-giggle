7072b0d31ed207d1fa56f0defcfc0afdaf64895b
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:37:56 +0000

    equipment properly pleased means control

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
