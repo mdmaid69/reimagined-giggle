b586f7b350a889e3ee91f3f6d48c48abe682d860
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:07:20 +0000

    side left race out pretend

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def extend_array(array, iterable):
+        array.extend(iterable)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
