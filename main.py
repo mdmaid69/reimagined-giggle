76dbc2618a4fb68a306430da289e21e3c3a4fcf8
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:28:09 +0000

    satisfactory it partner plan pain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
