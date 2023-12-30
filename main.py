32b01ec01f7b8b1b6f3b3f6a027e6ac8b8b237b9
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:22:00 +0000

    unite for talkback direction unhappy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
