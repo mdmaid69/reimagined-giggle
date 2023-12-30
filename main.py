ad5da26afe98fb28561abfffbbf4960acc1d2782
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:35:24 +0000

    tell drive smooth ordinary only

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
