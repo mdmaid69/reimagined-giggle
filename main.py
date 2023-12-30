b1b180b275b987df77379c3ca6ee772168b0d9d2
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:49:18 +0000

    software Cc road label immediately

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
