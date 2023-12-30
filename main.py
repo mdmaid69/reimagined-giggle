861d2eb3df44cababcef44048998b4bc9a85d2e6
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:25:31 +0000

    score change sense stir in

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
