714366a4f040c461284f21e1fd668d3f13a6cdd4
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:04:02 +0000

    escape once measure why score

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import numpy as np
+print(np.array([1, 2, 3]))
