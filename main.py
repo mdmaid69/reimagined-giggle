3810a488b9e0265042cf5a3adc967a927d1d4c8e
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:37:58 +0000

    thick public turnout turnoff transport

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
