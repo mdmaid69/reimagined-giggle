ad6f2ad9db7688deb63baadf3ab6f94b4e1fe503
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:17:54 +0000

    especially contribute near manner serve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
