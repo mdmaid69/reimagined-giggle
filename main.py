e135d770ad570ca573d9e3a008368dbce0186bee
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:20:35 +0000

    difficult back regular ability easy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
