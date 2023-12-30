6d8f3ddd8d7c04f90d39bf480d2e624ef8832094
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:39:03 +0000

    scream noisy row trust himself

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_list_from_array(array):
+        return array.tolist()
