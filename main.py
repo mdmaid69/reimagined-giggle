2a8e6d3aedcf217f15d71813d2ae0481032a0102
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:35:13 +0000

    steam genuine pass confident again

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
