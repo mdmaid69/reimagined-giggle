ec5cc4c31d602e0880ca26943c554e95d3ee0d59
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:23:15 +0000

    recognize empty divide correct cheque

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
