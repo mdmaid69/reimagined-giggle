6057417f5c16d9bdd297d875593858844e5ae230
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:26:59 +0000

    move explanation wide case permission

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  def square_number(x):
-        return x**2
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
