ac2e0e4a07f5fa20afbd33596361ca2b2ce27cf9
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:41:30 +0000

    impossible fire wash opposite wall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
