03a2716d1b6a54015fcde879fc25b7b6571f4d20
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:14:31 +0000

    hurry mad show future careful

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
