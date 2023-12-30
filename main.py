0fb980ea220e3fae4a6eec65e5615b001c16a1f6
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:33:43 +0000

    strong criticism train translate apply

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def count_elements(lst):
+        return len(lst)
