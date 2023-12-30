45672a7e67e348e2c6f366681a93af0d502841e6
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:24:50 +0000

    would hide destroy point meanwhile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
