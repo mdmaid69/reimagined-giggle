1b5fd0db2da465c8a48851253cb143dc9ff51234
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:04:04 +0000

    rare comeoff contact pure guard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-name = "Python"
-print("Hello,", name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
