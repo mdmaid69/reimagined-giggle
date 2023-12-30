c456d44528d1d8463e8d0aead315e3e3a1d2c604
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:50:16 +0000

    confusing your play base discuss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
