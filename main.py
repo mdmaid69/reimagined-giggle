5165a04f91007c1a56d74d7669d77b875d28e487
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:01:39 +0000

    confused warn while lesson thorough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
