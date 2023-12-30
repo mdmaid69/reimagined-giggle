6e8e4b1f2beac2ca2b4e06cb022ce10046e3016a
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:50:01 +0000

    measure constantly many wander opposite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
