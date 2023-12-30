411e9d4069568e12e2d9e3c23d5a1134fe7cda1a
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:32:10 +0000

    general seem long move callat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def square_number(x):
-        return x**2
+def calculate_volume(length, width, height):
+        return length * width * height
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
