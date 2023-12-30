47dfb426b662f0227d677e5150183d3c26aa965e
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:48:13 +0000

    government record there promise trouble

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
