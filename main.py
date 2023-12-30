f9566f28bef43937a8987f234355bd068c5e7c95
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:06:24 +0000

    phone home familiar reason design

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_power(work, time):
+        return work / time
