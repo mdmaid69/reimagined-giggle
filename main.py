2fd727c7dcae9b35134281c69268b51af7e9d077
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:47:05 +0000

    doubt moral waste difference borrow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
