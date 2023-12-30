a56d1274a863bbb617fc669225ec07a02051e372
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:13:16 +0000

    bringback Rr length score suggest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
+  def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-import re
-print(re.match("h.*o", "hello world"))
+def reverse_string(s):
+        return s[::-1]
