c410f0d20ff00fe48feea7615d4643a397563bba
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:57:41 +0000

    keepout boring care cheap powerful

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
