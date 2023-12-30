cbcdb0b0491d3862d84aa3324f0585adb8bc36b9
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:45:59 +0000

    any choice stamp mix wise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
