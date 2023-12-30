ee2767c4771b902925993bfb90a8f6381917d404
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:35:49 +0000

    better fire deal season treat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
