d3c9e21b77840cc7334fb0591c24f0f3e7610968
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:59:18 +0000

    breakout brave relax exist answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
