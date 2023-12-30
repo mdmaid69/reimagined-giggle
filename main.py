4c40afdc7dc9e41795dceab8e4c8448e14c11f4f
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:29:54 +0000

    corner 568energy whether bang silent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
