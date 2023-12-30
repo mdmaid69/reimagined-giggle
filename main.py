b11f72dab2ba33eddadac566a608e897903fe6bb
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:33:10 +0000

    show rely debt run unit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  def convert_to_binary(n):
+        return bin(n)
