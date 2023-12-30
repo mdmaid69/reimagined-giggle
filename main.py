460d9895246a8594b130b7792aa015c106dd29bb
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:58:21 +0000

    dear why reply alone best

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def subtract_numbers(x, y):
+        return x - y
