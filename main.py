a427583925354a29d750c139e38fa93f67ce6347
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:03:21 +0000

    preparation allow world concerned cause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
