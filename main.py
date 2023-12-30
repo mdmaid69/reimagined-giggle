5711607c353f69fb5acc4263fe7a2a982e49005c
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:02:29 +0000

    relief contrast bright lesson learn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def remove_duplicates(lst):
+        return list(set(lst))
