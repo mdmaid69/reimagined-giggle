66e24ca2df3e858df74d78270ca25f28998f2568
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:53:07 +0000

    best ourselves complete butter month

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def multiply_numbers(x, y):
-        return x * y
+def calculate_power(work, time):
+        return work / time
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
