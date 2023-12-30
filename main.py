a989875295f5080c2b333686997b21d084d10cab
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:31:23 +0000

    feeling limit just sensitive uniform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def pop_from_array(array, i=-1):
+        return array.pop(i)
