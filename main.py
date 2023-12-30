c6565cdc18de2520d24bc0cb3a8ad2204f07d7eb
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:06:58 +0000

    future take board hole tip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
