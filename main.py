8d4c5b82c09e703115017710da6773fc889d4b83
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:48:26 +0000

    pupil principle achieve so kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mode(file_name):
+  def get_file_permissions(file_name):
         return os.stat(file_name).st_mode
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
