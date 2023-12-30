9bfd1f6a252a5b3fd7f5a0ed5a890e0c0e53d84f
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:48:28 +0000

    drink meeting clear otherwise object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
