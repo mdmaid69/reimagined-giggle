59f6dc5aa2f94d38135c67861cf72d47c3675302
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:04:15 +0000

    Mm hurt term together relax

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
