8089d920e012154bdcdfec1bff852bcc6baea4d8
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:35:13 +0000

    present about disapprove to pride

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import sys
+def exit_program():
+        sys.exit()
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
