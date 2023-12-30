4748180c04a182142c9f3928b77b0119a7eb20d1
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:23:48 +0000

    affect juice tired listen confused

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
