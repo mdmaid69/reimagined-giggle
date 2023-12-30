aabb6119b5507d156cabee13fed4da6a4440d42a
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:01:24 +0000

    reject doctor consider weak dress

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  def is_odd(n):
-        return n % 2 != 0
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import collections
+def create_priority_queue():
+        return collections.deque()
