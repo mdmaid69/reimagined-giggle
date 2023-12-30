bdaff15dc4a348e68eb4e68ed6c72b2c9534fa59
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:30:27 +0000

    try feel invite 571enough where

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def count_words(sentence):
+        return len(sentence.split())
+def is_even(n):
+        return n % 2 == 0
