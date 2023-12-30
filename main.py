76416bfe24be28377ecc8dbfc267f08b70a1205d
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:32:03 +0000

    fit precise opinion classroom keepfrom

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
