44896c9406dfa9389fbe2fa078ae7a052d4272f5
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:45:02 +0000

    noisy also colour thinkthrough drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
