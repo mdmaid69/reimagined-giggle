8d469d6bd9182d74220622fabc5ad8e34e64fc1b
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:08:15 +0000

    able wrong low plate love

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def reverse_array(array):
-        array.reverse()
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
