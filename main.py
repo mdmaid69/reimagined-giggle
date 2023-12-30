a0aa84c7265333ea16a8a8dc8cbb47255fad016f
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:15:34 +0000

    protect also pronunciation yesterday song

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
