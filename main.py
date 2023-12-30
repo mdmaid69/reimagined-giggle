5975b2f27c3f01e7446a11385b8b0ad518a16e73
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:57:20 +0000

    mood language wake muscle performance

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_error_function(x):
+        return math.erf(x)
