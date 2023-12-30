4d42f6a392d8a997b3f8a7507ab8db15bc607217
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:12:07 +0000

    basis war afterwards circle know

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
