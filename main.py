21b243b922f12f5c8c74f430318a1564a92ef54e
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:09:10 +0000

    scrap anywhere black frighten burst

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def remove_duplicates(lst):
+        return list(set(lst))
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_length(array):
+        return len(array)
