b9c3c1a412accc0d83183e3a948525e2d65e9a44
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:02:21 +0000

    game goout print young condition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def is_odd(n):
+        return n % 2 != 0
+x = 10
+y = 20
+print("Sum:", x + y)
