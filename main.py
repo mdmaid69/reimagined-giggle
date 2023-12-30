b227fe1f7a9fe11a394098ec36ced67743d11085
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:16:18 +0000

    rent cheap suckme correct goodbye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def convert_array_to_list(array):
+        return array.tolist()
