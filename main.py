e081de320697cbe451ab26c0bf1ecc1d198b5458
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:02:10 +0000

    choice gas rescue obtain certain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def delete_file(file_name):
+        os.remove(file_name)
