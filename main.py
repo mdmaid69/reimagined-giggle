39304505c47556955de7b6331e4eeb7b70ddc7d4
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:09:40 +0000

    value steep apply this any

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
