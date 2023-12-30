c5773dc589a4d1367a8f4b80778a3c6b2f4ee819
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:28:16 +0000

    little consist pay left breakin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
