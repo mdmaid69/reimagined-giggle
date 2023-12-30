3e0bc2b31fa0f539ef6198174ba7a171b3cbed06
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:54:50 +0000

    entry shake backwards bank criticism

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
