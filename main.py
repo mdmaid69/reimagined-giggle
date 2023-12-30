cf5266cfd5bf7f329e4662f2d5ba6dc21bad7598
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:53:22 +0000

    brown stretch callback remember grade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def divide_numbers(x, y):
-        return x / y
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
