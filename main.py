07dece097a656ff62d3be53a46599ed3b554dac3
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:55:34 +0000

    off free mine about year

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def is_odd(n):
+        return n % 2 != 0
