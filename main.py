ba7f678cdd2c48826e513c8d464476b52ef2ecfe
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:08:27 +0000

    favour might regard safety money

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def count_characters(sentence):
+        return len(sentence)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
