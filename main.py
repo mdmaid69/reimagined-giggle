d1efef8d485ab18fb62b5ac2d39f1e8750ca8024
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:06:45 +0000

    seat artificial sit attention sleep

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
