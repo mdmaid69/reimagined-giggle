ef2085082d1a3be55320fd44fe7838fe0bc3301a
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:53:50 +0000

    mistake experiment peaceful move speak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def find_max(numbers):
+        return max(numbers)
