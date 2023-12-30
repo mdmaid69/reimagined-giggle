5312268cf1fb0acd0cf9351cb440db3f6d705876
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:31:00 +0000

    final regard afterwards thin vanish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_density(mass, volume):
+        return mass / volume
