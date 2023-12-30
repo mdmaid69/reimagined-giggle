e72be18b22bfaf46e09214ef4d9358f37ce309c1
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:06:08 +0000

    far fast unit absolutely crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
