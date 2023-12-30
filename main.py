32778bd584c2f1f00f59ed67d6de9965ac972f75
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:17:59 +0000

    above splash intend stop turnback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
