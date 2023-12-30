143d44aaba056e2ed5d2693b10b95a774090d5c7
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:03:02 +0000

    warn genuine whom breakfast valuable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
