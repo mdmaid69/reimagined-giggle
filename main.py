0a0615b475281c9dd717e861f830ebd180704d94
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:50:24 +0000

    through once return well water

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+def exit_program():
+        sys.exit()
