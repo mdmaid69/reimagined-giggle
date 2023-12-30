b9b176439f1276386e900e2b2a9c491ff27f70d9
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:44:36 +0000

    already glad interfere themselves rob

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
