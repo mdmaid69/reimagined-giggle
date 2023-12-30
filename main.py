9627782d0ae4dec80bd5208f445be7f398b911b9
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:12:38 +0000

    damp approval realize divide serious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
