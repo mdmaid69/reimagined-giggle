44e42e4ffbd6f5158864bf358a3779bb9dc7cb26
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:40:30 +0000

    pull traffic his holdout answer

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
