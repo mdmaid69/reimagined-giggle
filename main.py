0d558be4b17505db4d4da638665331a036259883
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:55:56 +0000

    keepfrom phone excuse waste touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def get_array_as_bool(array):
+        return bool(array)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
