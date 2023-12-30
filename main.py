d260cedb52489731fb816e77ebad6ff9f0671c44
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:16:44 +0000

    band left demand over fast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def clear_array(array):
-        array *= 0
-  def convert_to_binary(n):
-        return bin(n)
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
