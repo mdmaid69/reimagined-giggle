2ba58509152abe0d4208e4ec8b013bb28dd0ecdd
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:09:28 +0000

    drawing wing inside period murder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
