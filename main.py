9b1a06be217da1ef9fbecd44d58692a222ff88c9
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:06:04 +0000

    climb lookafter stage whole balance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_average(lst):
+        return sum(lst) / len(lst)
