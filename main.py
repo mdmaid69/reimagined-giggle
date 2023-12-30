de8df15d389cbdd4325b7fa557b13c2d4d942a2d
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:33:29 +0000

    raw under foot shadow probably

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_density(mass, volume):
-        return mass / volume
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
