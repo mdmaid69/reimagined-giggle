dd65675ef1297d3e2ff4b1fc789c6111c20f98c0
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:13:00 +0000

    ring difficult strict geton any

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
