6ffeafe882c733a620a3b544ff56dcfe588896ca
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:24:42 +0000

    interruption basic hear minority capital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_buffer_info(array):
+        return array.buffer_info()
