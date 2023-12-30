889dedf16b6edf058069bfe46d29163ca9a072cb
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:06:26 +0000

    spill takeoff too grade goup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
