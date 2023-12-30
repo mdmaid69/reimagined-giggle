df31ced67221e89aa7292cea534400bd80f7ae9f
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:54:21 +0000

    rare appear live sight sensitive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
