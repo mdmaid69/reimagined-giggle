44990242faf7a4f5d0a96ab7529404537204c44e
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:35:39 +0000

    564encourage happy patient giveaway effect

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
