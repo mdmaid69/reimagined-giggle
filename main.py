1ee50cda89f4955c0cae94e8f2ea9f7b35f6df13
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:54:56 +0000

    well kill whichever bringback spray

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
