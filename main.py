c6e0a16a184445310b4a2425aed44b2501038602
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:24:54 +0000

    emergency sting accept friend shock

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
