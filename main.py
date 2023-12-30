6546ef8dd37aa7180b5c91c6699516eb85c143ec
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:23:32 +0000

    easy tip room cheek each

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
