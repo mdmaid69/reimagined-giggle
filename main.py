82413a560cf4cc7bd0dbe1f4a9039613c261091f
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:26:54 +0000

    whistle collect soon wire well

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def find_max(numbers):
+        return max(numbers)
