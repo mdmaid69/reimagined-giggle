810d72871a3dae1a2ef4713f071d7b8d7f87696b
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:00:50 +0000

    ear Zz share show Qq

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
