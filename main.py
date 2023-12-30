0b8684175149aa0b255442db559fc04d43928a66
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:15:33 +0000

    once embarrassing floor left tie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
