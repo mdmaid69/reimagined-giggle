987fe43e812e01a25f6b68acbba2cb27d0cf0bd3
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:39:01 +0000

    evening further ignore smell instead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def get_array_itemsize(array):
+        return array.itemsize
