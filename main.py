457363cec536d00180358b4d05471656692c20b2
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:45:10 +0000

    heavy fasten your difficult feel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def clear_array(array):
+        array *= 0
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
