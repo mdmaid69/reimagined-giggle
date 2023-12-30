3442daa7b90fe0040428ab9868155c014c4169ba
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:09:05 +0000

    his reasonable side hello lost

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
