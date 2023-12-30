30206cd9a5cb049b111731219c46b275d3b0974e
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:08:46 +0000

    otherwise look knowledge move estimate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import os
+print(os.getcwd())
