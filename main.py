dd7065e138be20f8bb8a4a522d58c00a0e315bc3
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:46:08 +0000

    fine damage behind objection certain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
