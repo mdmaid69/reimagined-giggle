85b50b70f5e3308b9af9dfeeed15aa9acc4fac85
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:19:50 +0000

    beat introduction bet air flight

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-print("Hello, world!")
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import array
+def clear_array(array):
+        array *= 0
