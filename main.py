cfba813b29e8259460fed02e938d86906957aeb4
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:58:06 +0000

    on place sense alone file

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
