9d46d6bd980207f603c8d13805c9e547d503cc3c
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:28:51 +0000

    apparently future dish next act

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
