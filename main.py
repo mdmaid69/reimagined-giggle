e5a5373642b3034f13bd84d88166f140864ab430
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:33:37 +0000

    smile name stranger kind left

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
