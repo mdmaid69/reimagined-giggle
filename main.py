05d01fbbadf9b756f3091b476da4d3e7c6ccdceb
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:48:26 +0000

    advice platform it one estimate

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
