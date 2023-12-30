c0f32269ec94379b0211964b2d87fc3f787a8126
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:47:41 +0000

    turndown callback recommend period laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
