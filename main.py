f3e7423541ce30effa1a6058d3bf9e001397c583
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:51:20 +0000

    arm strict suffer afraid mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
