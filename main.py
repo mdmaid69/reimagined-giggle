3d2ac2b9525b15c13d8f69fd1c157f23165fbcba
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:59:46 +0000

    lookafter calm recently commit comeout

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
