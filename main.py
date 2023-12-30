2baa4b88d1d038caf24bb49738dd1c43508b5bb9
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:49:01 +0000

    height class force independent talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_speed(distance, time):
+        return distance / time
