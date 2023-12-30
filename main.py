f4705965a0b00449822a1dff819a8902da8f3b8b
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:16:17 +0000

    public accordingto yet ever clean

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-x = 10
-y = 20
-print("Sum:", x + y)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
