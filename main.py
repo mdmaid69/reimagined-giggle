95929eebd8e48caa2b0cb7b12fe0df0e0b4269c9
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:06:24 +0000

    desperate rescue much come murder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_work(force, distance):
+        return force * distance
