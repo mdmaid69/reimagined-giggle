23c0369dac5fc88c39644faf82df26dccf2758e0
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:00:12 +0000

    dark pretend style original black

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
