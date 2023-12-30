41a79456ded95ba7d0616491943f6ffbf78362a3
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:22:24 +0000

    independent pass however front comment

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
