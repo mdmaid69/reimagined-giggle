645df8b1b2de54f58ef843df4ef5832090805fdb
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:09:06 +0000

    protect prevent comment alone fork

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-def find_min(numbers):
-        return min(numbers)
+  def count_elements(lst):
+        return len(lst)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
