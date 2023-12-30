31af47187e37be25184f404b5594bec5bdacea52
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:17:26 +0000

    sound hot clever flood support

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def subtract_numbers(x, y):
-        return x - y
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def sort_list(lst):
+        return sorted(lst)
