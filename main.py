aec3de29c83f26cd5e266d0acb4ebbddb05eef2d
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:14:24 +0000

    target juice discussion description rub

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
