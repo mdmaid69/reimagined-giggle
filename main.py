c64e2d8b02dcddae2bca47af04671546040b7e17
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:02:09 +0000

    partner busy round name Zz

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def get_array_as_str(array):
+        return str(array)
