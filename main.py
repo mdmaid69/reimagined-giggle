55ce9dcc9f3378a9030d58cb53582227fc38ff9a
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:06:39 +0000

    duty behind indeed dear taste

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def is_even(n):
+        return n % 2 == 0
