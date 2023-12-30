89a194f24a64cd209713a5b071fad230aa464175
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:09:44 +0000

    unkind fond test fierce today

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
