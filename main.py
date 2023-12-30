8a5c8c5a411f60265406ea85b845db21c5ba88dc
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:33:20 +0000

    so begin whom punishment weak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  def is_odd(n):
+        return n % 2 != 0
