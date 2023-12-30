52ab55c4ebf9cd765f0b0d986baf162798019987
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:22:38 +0000

    dish takeaway weekend task transport

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
