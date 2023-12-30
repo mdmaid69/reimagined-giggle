80c7008ceab89ec245b270ad13790421067ace71
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:21:11 +0000

    basis winter hurry base height

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
