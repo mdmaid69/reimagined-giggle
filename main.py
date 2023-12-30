674aba6d3108d932c2014e1d1c4ca22e9d7fa13f
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:29:39 +0000

    size thanks strike fasten subject

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
