450f119ff3bdf451d8fca2c651749e70da66433e
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:54:36 +0000

    getup build some dull rare

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import array
+def get_array_as_bool(array):
+        return bool(array)
