e656cf5f532884364d54d0df7e1ee06d843e6723
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:08:34 +0000

    open physical gate stop unlucky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def subtract_numbers(x, y):
+        return x - y
