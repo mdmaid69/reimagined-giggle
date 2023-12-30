229fc87b566ac07de85bc1c9943f930e9e7b572b
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:05:15 +0000

    when development until tired quickly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
