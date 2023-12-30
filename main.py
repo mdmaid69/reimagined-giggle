ebdc0bba49cf0256b873d91b150a9f7cacd3efc5
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:48:46 +0000

    adjective die reach lie right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import getpass
+def get_username():
+        return getpass.getuser()
