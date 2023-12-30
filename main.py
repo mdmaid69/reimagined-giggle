0336a85d7e3ebd366671f99d781d2f477a1bdb4b
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:46:02 +0000

    either cruel dark approve carry

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
