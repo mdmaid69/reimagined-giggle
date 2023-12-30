b48c89fc0a23df64c58088dff540055846f05c28
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:30:11 +0000

    brief 571enough proof bit along

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import os
+print(os.getcwd())
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
