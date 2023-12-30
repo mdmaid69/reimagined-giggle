2f986b5d1516c72eaf338936e0b48fa2cea27f2b
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:19:02 +0000

    perform try survive dig land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
