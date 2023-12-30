517a98a62666548de1cf9ef520a35e9a5b956f36
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:31:34 +0000

    getup noone hole tomorrow natural

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
