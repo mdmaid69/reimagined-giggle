2b74b6bd9288cb5774bf14cd4d0eb9d363ea2875
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:17:42 +0000

    goodbye obvious my recovery announce

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_uid(file_name):
+  def get_file_owner(file_name):
         return os.stat(file_name).st_uid
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
