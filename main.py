97f673ab3e741e99c857511eab56ec50a7942345
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:03:21 +0000

    level decide late habit build

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def remove_duplicates(lst):
+        return list(set(lst))
