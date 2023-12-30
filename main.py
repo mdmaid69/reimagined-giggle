39de74b4826405ee5b4281236413cb315956a4fd
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:21:02 +0000

    keepout interview comfortable address nice

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def remove_duplicates(lst):
+        return list(set(lst))
