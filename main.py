4219ede33196c767d119752b773dcaff9148a529
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:39:13 +0000

    phone proper themselves platform operate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def delete_file(file_name):
+        os.remove(file_name)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
