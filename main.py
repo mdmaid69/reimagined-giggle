4b987ba20f50b2c4226789510674bc7a150fcf57
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:54:39 +0000

    pocket fact load expensive advanced

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import array
-def reverse_array(array):
-        array.reverse()
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
