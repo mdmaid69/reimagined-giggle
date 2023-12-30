b7c3a3a1be81c13ff9c3b1fc6497b706ee55d770
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:01:12 +0000

    division course protest willing part

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
