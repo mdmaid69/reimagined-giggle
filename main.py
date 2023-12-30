32e6b4936a5538e9595b3349455d5bbd7dd8943b
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:39:18 +0000

    closed clean who common commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import time
-def get_current_time():
-        return time.time()
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
