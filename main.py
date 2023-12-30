9cc8598bd6dda87d364447cbc3852ff41bd266c7
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:26:15 +0000

    task same plate group bend

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def multiply_numbers(x, y):
+        return x * y
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
