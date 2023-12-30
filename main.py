895ca5bf09340f88d93587bcc8151c3d70707087
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:11:33 +0000

    question several polite once ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
