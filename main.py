c91da6a6b8b35aad122d1af05bd74525086af904
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:24:20 +0000

    direct person us quiet charge

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import random
+print(random.randint(0, 100))
