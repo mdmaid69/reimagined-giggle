c6e4c090913fc13b3a58475c9a30d76a14d216a0
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:13:14 +0000

    getout before back runover bit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def count_words(sentence):
+        return len(sentence.split())
