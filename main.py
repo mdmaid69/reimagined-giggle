d0d8f5452c761f2b7e81a3130afed2286bd68546
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:24:46 +0000

    press flow mouth short impatient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
