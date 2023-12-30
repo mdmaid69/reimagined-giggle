a3730cd8b6106dffe173514d666bd19cdad27e75
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:15:06 +0000

    return early Nn brown set

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
