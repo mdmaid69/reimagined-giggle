f3d56f2893249bdd7eb730696235b2c85aab4659
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:31:00 +0000

    lucky mine table expression thirsty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_pressure(force, area):
+        return force / area
