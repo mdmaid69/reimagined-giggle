5ab8067915a588575c6986403fd084e1a0f92050
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:23:49 +0000

    upstairs us hate hurt discussion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
