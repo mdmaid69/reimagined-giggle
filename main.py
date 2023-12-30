3fb475cb446cf7ca7d02ddf44bf45c82c676824e
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:09:57 +0000

    outside use emphasis challenge positive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
