3fcfcee9896c39cdf180bb510a26451c1db1cc37
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:32:16 +0000

    catch turninto behaviour feel behave

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def find_unique_words(sentence):
-        return set(sentence.split())
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def find_min(lst):
+        return min(lst)
