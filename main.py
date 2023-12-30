ea8d48367cf85eafd864afcc05814b74ded10ec2
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:11:12 +0000

    complete want impression interest bear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-for i in range(10): print(i)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
