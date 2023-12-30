5e2d932e1fcc7b0b4f7c2d7f9874dd98233e1f9c
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:06:03 +0000

    turnaway learn long movement comeback

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+for i in range(10): print(i)
