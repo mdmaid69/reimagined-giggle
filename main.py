bdf72784d2ad928e0612f134ebc18ea8df35524f
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:57:19 +0000

    away excited university connected claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
