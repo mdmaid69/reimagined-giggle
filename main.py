f1eca8959ecc8d141bf56998cf5d023a1cb72d3d
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:26:27 +0000

    damage convinced along closed measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
