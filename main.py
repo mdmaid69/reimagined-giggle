f38576698373efa6ffbcdaacac5cc51625aed1e2
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:18:35 +0000

    573entertainment typical Ff float taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_min(numbers):
+        return min(numbers)
