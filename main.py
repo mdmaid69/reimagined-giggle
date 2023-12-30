8c366cad24178b3d8485dc23bf9ef0ef8349b49d
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:33:05 +0000

    lend my interrupt interest pollution

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
