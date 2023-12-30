85cad7e577b27bba75b85ea9f66f7c378aa3e673
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:54:31 +0000

    strike protest would characteristic without

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
