bc769021852b834843ef47373576aef78d7e48a5
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:07:04 +0000

    history quick yet responsible only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
