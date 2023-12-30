3ccd8c51181d83bb37b8518b8f99bfd826a8fb1e
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:22:41 +0000

    it hope ready accordingto run

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
