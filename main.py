325e69d78a3736b213ac9212b07b8673a457231b
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:18:23 +0000

    sick part experiment constantly runaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
