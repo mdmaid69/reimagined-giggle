567170b5dad9d30ae441b7c231c753d9ce1f0cde
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:56:28 +0000

    relief bother stick fire hang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
