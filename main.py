ede6bd0ec376c45b50eb1604d3fef2dadb68fdcb
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:02:13 +0000

    uncertain giveback professional party any

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
