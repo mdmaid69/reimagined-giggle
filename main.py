fab0b90bafea7644355fa48c3d2c257b1443124f
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:20:41 +0000

    own completely new hot occasion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
