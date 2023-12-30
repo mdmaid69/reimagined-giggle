7e1d42673c04ec14b1fedf1dbc94ab50871caf5f
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:06:49 +0000

    position what forget secret hollow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
