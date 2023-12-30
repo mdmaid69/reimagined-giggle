4dfaaa2fdb95c93334907d459c310f853a311ebc
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:16:26 +0000

    suffer neither process sorry disgusting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
 import math
 def calculate_sine(x):
         return math.sin(x)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
