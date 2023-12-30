2387f1e3b5cdac111488554eb5c350d749f75962
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:33:04 +0000

    birth paper rarely over Uu

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
