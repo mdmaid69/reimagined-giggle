28c22aedde0ddc359e89a448ef5fd8f46830d12a
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:30:15 +0000

    beside effect practical benefit label

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
