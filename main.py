a8c824a09eec6cc8fb21f54e02c9db19f1e174cf
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:28:28 +0000

    tense wake ring against freedom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  def remove_duplicates(lst):
+        return list(set(lst))
