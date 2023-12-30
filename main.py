815ed08a5970d3d633c6105e56af377cf34ca220
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:14:39 +0000

    news passenger whom offence shrink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
