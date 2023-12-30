e3361388ddd0ef7d8ae650051e38f2f967939dea
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:36:56 +0000

    heart come estimate frequent sing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
