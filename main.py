a2a9e439a9521327a646d54a5601ba07e4af033f
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:18:59 +0000

    pronunciation turnon rich calloff dangerous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
