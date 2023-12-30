20fd7192bae8a70b0617fd310076fd600487a3e3
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:16:30 +0000

    theirs call test movement not

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def count_elements(lst):
+        return len(lst)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
