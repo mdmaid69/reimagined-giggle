48cff5577c4e25dee21155d5ea0d62a23f99383c
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:40:20 +0000

    beside distant measure without taste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
