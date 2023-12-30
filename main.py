21994e1727da075d98fa7bd779945242a3030bfb
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:31:52 +0000

    branch shade generally hit cheap

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
