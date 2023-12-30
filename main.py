4d63601c6fcbb04282015e1a7a2ef1080c3eabec
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:02:46 +0000

    concentrate prize splash nature side

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_volume(length, width, height):
+        return length * width * height
