38fae0611302ebbf1599e84a36395ebbfcc5e4d3
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:41:03 +0000

    accident exaggerate bill beat classroom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
