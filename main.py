144e42c5ba298f7a4269bf61bc4d6ee352140b18
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:56:23 +0000

    before computer once regret opinion

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
