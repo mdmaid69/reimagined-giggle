0551a04c7181948e67d9e6901b56400e0920d915
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:06:54 +0000

    he talkinto tragedy state admit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def multiply_numbers(x, y):
-        return x * y
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def calculate_area_circle(r):
+        return 3.14 * r**2
