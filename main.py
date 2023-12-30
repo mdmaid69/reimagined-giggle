35f07d71fa5aa549f54ba23a59d26ddb25b53d45
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:29:47 +0000

    trip communicate Mm because history

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
