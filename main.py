8947475c85cd7fb2f497dc23f0ed445908c4c4f3
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:30:21 +0000

    physical arrangement Pp empty big

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def is_odd(n):
+        return n % 2 != 0
