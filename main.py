96b037ad254bfd6731c936ed58ce0fa8d18be8d2
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:10:22 +0000

    hurry indeed likely clear talkoutof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import os
+def remove_directory(path):
+        os.rmdir(path)
