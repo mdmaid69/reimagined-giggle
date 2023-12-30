fe068f35abb500216a8ac8bc901439b52aebcab0
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:03:26 +0000

    risk comein tie fond undo

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
