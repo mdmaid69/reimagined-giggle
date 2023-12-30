afe8cbba4118e3ea1d24807793c37bf4897c1ee1
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:42:25 +0000

    can flat how bend watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-print(math.pi)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
