b1993898dacec034ac7f348398f228858caf4c34
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:48:00 +0000

    near gap object tall mysterious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
