a9888e0b770e81183d238e433d59b7a799c9737d
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:27:38 +0000

    immediately question profit loud clue

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
