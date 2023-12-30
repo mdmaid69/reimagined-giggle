352c3bb4177244cc3123b0ea7258fd635340d35f
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:24:05 +0000

    not dry emphasis maximum common

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def convert_to_octal(n):
-        return oct(n)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
