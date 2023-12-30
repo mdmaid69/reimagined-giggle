06e5ca2dfb4ecb758a68ac7ec963e9c833c7c4c8
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:40:34 +0000

    behave fail total central after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import collections
+def create_counter():
+        return collections.Counter()
