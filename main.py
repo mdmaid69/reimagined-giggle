97640e8431ec3c3763a26477abf8d76ec2fad1ef
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:40:01 +0000

    foreign impatient say afterwards begin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
