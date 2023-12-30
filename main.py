cbafd2a4b7938564c598ea1562d0d1f7512bf270
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:34:01 +0000

    glass precise might rest pretend

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-for i in range(10): print(i)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def find_max(numbers):
+        return max(numbers)
