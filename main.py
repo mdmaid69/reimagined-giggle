d9cf8e2ecb54186d83ee029f44b1941c68227a71
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:30:35 +0000

    breathe total strike noisy concentrate

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
+  def plot_graph(x, y):
+        plt.plot(x, y)
         plt.show()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
