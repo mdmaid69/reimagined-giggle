f92f4195eeedf85357845de9e361e4af25d93d18
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:39:08 +0000

    urgent pause attitude content admit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
