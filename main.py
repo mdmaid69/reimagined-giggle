236d47261ec39ec43e28eb313fc519de34b98002
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:08:42 +0000

    pay afternoon meaning about such

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def square_number(x):
+        return x**2
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
