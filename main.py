932210d0373251c0615bde9faf2b33524f1cec69
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:18:10 +0000

    drink forever rain thinkof confused

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
