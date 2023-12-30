7ea979077ff24f606d154f9896e16851138b2264
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:19:12 +0000

    slow takeover guess certain make

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
