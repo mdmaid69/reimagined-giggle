ec1d5648b29c7e210849cc4313a851e08b126e4c
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:37:52 +0000

    key sound see computer suggest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
