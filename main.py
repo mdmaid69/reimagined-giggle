0428791d98ed1cac2cd39aff8d0d53f3abca4424
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:50:19 +0000

    usually church plenty would though

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
