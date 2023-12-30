16e7b3fe0c675a9d627b9980c25c7c3a4a5b7350
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:35:15 +0000

    disturbing dry suggest head immediate

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
