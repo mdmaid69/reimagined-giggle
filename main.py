e231790eecfcb7b1e07443f707e16f7f1dd9ddfa
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:37:28 +0000

    responsibility brush wipe rank reduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
