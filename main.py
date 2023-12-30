47dab0322b2c581945b87cc2c566d85f1f1a9288
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:17:40 +0000

    blow thin attach cause in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
