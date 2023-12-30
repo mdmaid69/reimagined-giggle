471fca11960c594aa5448b2a7fa922eb65b5be93
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:46:58 +0000

    just drawing mysterious pass to

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  def square_number(x):
-        return x**2
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
