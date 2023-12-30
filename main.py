0e4fe16c1fc3b9eacfd12f93fd386dc23faac476
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:07:08 +0000

    nor forward exploration wise dig

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import datetime
+def get_today_date():
+        return datetime.date.today()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
