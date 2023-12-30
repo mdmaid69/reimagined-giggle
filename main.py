a91c3c8324ec748361f5e4c68fb837256b2b0733
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:32:39 +0000

    accidental bet crazy stamp food

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
