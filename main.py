fcea07100deb4faffc4aa38a10ef433cd44b3e86
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:29:07 +0000

    general community pleasant risk whether

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_bool(array):
+        return bool(array)
