bb188b8a0add15d388b512c5a0c4b6bdfa3c29c5
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:33:09 +0000

    judge aim vote suck empty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import math
+print(math.pi)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
