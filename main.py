bd0b34ea505157fe7b1b9b9c75c9d60029aa1b09
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:40:52 +0000

    slice feature past change crazy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_as_str(array):
-        return str(array)
+text = "Hello, world!"
+print("Characters:", len(text))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
