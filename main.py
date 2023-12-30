2d3c250db256750cb77a475aa2abddf556781291
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:20:10 +0000

    space creature interrupt wear glad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
