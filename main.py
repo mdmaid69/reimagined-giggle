e252987bc6d9dd09fd1a94a5acca9ed6a3cca7fe
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:14:43 +0000

    arrangement tired however connection town

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_pressure(force, area):
-        return force / area
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def square_number(x):
+        return x**2
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
