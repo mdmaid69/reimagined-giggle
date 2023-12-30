4907af76305ff7dd9aa35b066c16367344596187
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:16:17 +0000

    field time point all slight

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def get_array_as_list(array):
+        return list(array)
