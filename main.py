ca505af2e1fec10bca23a0885a327a1a2ce8a974
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:42:47 +0000

    day leave failure plenty boring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
