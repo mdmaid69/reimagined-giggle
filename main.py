60dc24e5249180b7ed924ce165798c5e44c1858b
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:37:42 +0000

    class involve raw fly unkind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def convert_to_hex(n):
+        return hex(n)
