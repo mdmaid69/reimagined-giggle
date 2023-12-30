927f04e6036976722fc822edcb3db7ca76c03c25
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:07:57 +0000

    sport long spread Nn Uu

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import sys
+def print_python_version():
+        print(sys.version)
