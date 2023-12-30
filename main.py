5a01722d3255a196a8db8f4abbb32d0207821303
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:41:47 +0000

    themselves better comeoff turn eat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
