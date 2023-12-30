0684e25e1dc4b7df8f4a18d1153567c5a9745cab
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:18:08 +0000

    sea country grateful priority shock

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_power(work, time):
+        return work / time
+def calculate_power(work, time):
+        return work / time
