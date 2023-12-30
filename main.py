daa0b71e4e944d30487e6c6ea71686715aaf0829
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:18:03 +0000

    whistle transparent building bet protection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def remove_duplicates(lst):
+        return list(set(lst))
