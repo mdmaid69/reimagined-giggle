4828b978bf11f02f8133479f8af88f172f24fe87
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:36:18 +0000

    contact sting urgent happen education

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
