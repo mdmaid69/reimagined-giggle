251898acadb8cbe64709cc673d48baa44ca0f5a9
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:49:28 +0000

    brain market leg and ourselves

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
