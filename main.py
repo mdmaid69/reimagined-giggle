6784f1249663c38f09c83e943e6046fb68eaf4da
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:10:48 +0000

    once name cook right respect

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
