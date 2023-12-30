a4181e0866497a658fc42af0ba7aee1dd23d22bf
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:11:37 +0000

    edge well feed give school

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_acceleration(speed, time):
+        return speed / time
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
