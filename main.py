a6f0d27a6cbb145fafe76094f1f0c581af661e3e
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:32:22 +0000

    silent lift inquire hollow draw

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def remove_duplicates(lst):
-        return list(set(lst))
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
