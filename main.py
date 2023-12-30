9b306738c5dd34928dd5f4b96ada75f534eca2aa
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:25:52 +0000

    below list general defend image

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  def sort_list(lst):
+        return sorted(lst)
