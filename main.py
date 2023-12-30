6b34061a00afb956d2441814e6e34b5453afa7c9
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:39:05 +0000

    set switch project excuse music

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
