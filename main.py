f13cba9df674eef2e36b05e0368961de8acee8d1
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:24:35 +0000

    cut wild downstairs beside reliable

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def sort_list(lst):
+        return sorted(lst)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
