39632b71ab5fe1bd32556d4637665e1ede3e66b7
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:22:43 +0000

    early tap within niggaacademy powerful

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def square_number(x):
+        return x**2
