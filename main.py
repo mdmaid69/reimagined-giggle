9a4b0b1bcb8ed0b8833a883c640be6cdcfa878c8
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:55:55 +0000

    unkind Yy repair bite release

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_current_working_directory():
+        return os.getcwd()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
