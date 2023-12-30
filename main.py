cf3810532a28f1b06d351d800e68bb622499e511
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:45:45 +0000

    getout noise science sign moment

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
