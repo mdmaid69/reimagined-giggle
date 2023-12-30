2f2bed9e1c80ffc611584485f2b987f439432a2d
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:47:27 +0000

    everywhere pretty importance mention murder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
 import array
-def clear_array(array):
-        array *= 0
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def get_array_typecode(array):
+        return array.typecode
