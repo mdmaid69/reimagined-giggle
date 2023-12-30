975703d00873f3d56a84d386a5baf5e116f474b1
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:59:56 +0000

    below shame score list second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
