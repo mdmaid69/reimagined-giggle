d4c65793936ad24641610d341b54d82a5d4fc21b
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:43:27 +0000

    nerve cheque defeat ashamed shoulder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
