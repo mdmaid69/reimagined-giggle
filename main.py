595252f35d1b3f6841a0014b3b5d57196f5dbc79
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:03:09 +0000

    right point sleep satisfactory disagree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
