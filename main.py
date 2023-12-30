e6021fb3a2fda0f56974efb6070398cfea3f8a52
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:09:41 +0000

    cross turn pot taste goodbye

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
