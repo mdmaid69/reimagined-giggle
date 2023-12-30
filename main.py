c536d2517f0c548afd793fb18ae8dd63cc9270c6
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:43:19 +0000

    trial vanish shoulder occur kill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
