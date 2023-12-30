ff4cd81ebc7246bc29ac7518980ac85b32ed00a9
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:40:23 +0000

    into each about treat mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
