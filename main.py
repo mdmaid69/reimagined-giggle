cd5d57e64575dab933211329b58207a16d067cb5
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:56:54 +0000

    except problem spring giveout content

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
