cb0bcfaac91ab536e4e4ec85a79f9109a58f7162
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:19:23 +0000

    stage collection recognize broad book

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
