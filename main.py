beb897a9aa23b89570222e89e5d40e62e8288c00
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:22:11 +0000

    too ever either Gg school

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
