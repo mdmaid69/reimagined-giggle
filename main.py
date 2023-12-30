f7ac7511f1f92717cfac5e1c647b0203bf773049
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:05:02 +0000

    easy famous to healthy practice

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import os
+def get_current_working_directory():
+        return os.getcwd()
