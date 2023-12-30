a006c3f52e483b457900e3c91c6f4a6acd7e0f46
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:18:56 +0000

    fun cold impatient approval thank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
