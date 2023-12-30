acfecaac1887d15d8b1cbc236613efab7a254090
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:15:29 +0000

    favourite next subject stupid proud

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
