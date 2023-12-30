e4fe169d4836838496e4a42729ec1036f27e5431
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:22:45 +0000

    second advantage beginning straight to

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def square_number(x):
-        return x**2
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import os
+def remove_directory(path):
+        os.rmdir(path)
