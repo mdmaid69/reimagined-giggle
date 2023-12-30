2b8d7b7d5e1b287de154d80d9f635917e76573f6
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:44:45 +0000

    black command national either edge

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
