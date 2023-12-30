57f4c995d03fe3ea2f225f6dad8567ab0d40efa6
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:23:19 +0000

    owe give live shallow task

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def subtract_numbers(x, y):
-        return x - y
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
