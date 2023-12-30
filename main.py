fe21e672d8eb135fe8f36fc153fcb19468021d05
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:05:22 +0000

    concentration accidentally dangerous sad card

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_length(array):
+        return len(array)
