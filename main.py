94b89abee6bfeee87854001915eaf4865c7e17ed
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:55:23 +0000

    break warm except shame speak

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import getpass
-def get_username():
-        return getpass.getuser()
+print(sum(range(10)))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
