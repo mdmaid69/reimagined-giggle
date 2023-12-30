8a20ce4930effface08899bd83a85f656f8301c7
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:25:38 +0000

    string shout prize pen free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_speed(distance, time):
+        return distance / time
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
