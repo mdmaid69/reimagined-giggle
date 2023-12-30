53045b44f764ced4ec1642010719e8f5ae9ea933
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:10:43 +0000

    future near cross call preparation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+print(sum(range(10)))
