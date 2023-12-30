b86215b8d7cfd789debd65e3de6077dd7d3ddc6b
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:01:15 +0000

    fit doubt level face large

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def convert_to_hex(n):
-        return hex(n)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
