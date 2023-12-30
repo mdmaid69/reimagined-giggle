31d03c8d48b04faf56e146b5d3c56d54376b7beb
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:13:36 +0000

    reject gather lookback(on) why comment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import collections
+def create_counter():
+        return collections.Counter()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
