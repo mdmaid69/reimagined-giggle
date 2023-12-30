6cce04597cf0b9b507f63ea471a79644617700e0
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:59:53 +0000

    period throw nearly reject bored

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_work(force, distance):
-        return force * distance
-import getpass
-def get_username():
-        return getpass.getuser()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
