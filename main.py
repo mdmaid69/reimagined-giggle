0afa0e88e22ed85a0914b81f00dfaf6b7e58d168
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:25:25 +0000

    age repair sail need proof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
