49ef1938e552613e8c3494706bb995c751f72b59
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:28:19 +0000

    generous operation bad still wish

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+text = "Hello, world!"
+print("Words:", len(text.split()))
