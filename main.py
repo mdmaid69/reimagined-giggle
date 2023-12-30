3b72d8789ac88ed25c3349f95b25c30e1e3bf648
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:33:06 +0000

    book already means usually steer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
