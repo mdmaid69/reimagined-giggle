d652c869b1b957d92d7464145af6df70bcd11c1c
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:09:34 +0000

    storm post lay use float

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_work(force, distance):
+        return force * distance
