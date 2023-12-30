7fb736f1ff96951a7b81229ebee4a235dfaf4bbb
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:38:22 +0000

    furniture bet make callout glass

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import platform
+def get_os_info():
+        return platform.uname()
+import array
+def append_to_array(array, item):
+        array.append(item)
