5ec8dab096501236b1222c59b0014763485bb20b
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:30:56 +0000

    show afraid period load skirt

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
