330eb8036872274787481cb7366898256c459d58
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:04:55 +0000

    criticism destroy birthday discuss afraid

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_str(array):
-        return str(array)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
