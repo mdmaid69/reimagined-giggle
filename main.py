b6dde0bac70858eb3800683a74f4d30da944eeb9
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:10:45 +0000

    lead know view stop fall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def find_min(numbers):
+        return min(numbers)
