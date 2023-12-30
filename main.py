3e4d50f44751c0136b5a7ce21c18763f92b0b071
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:48:22 +0000

    happen open display reason turninto

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_string_from_array(array):
+        return array.tobytes()
