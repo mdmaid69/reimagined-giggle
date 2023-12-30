5e4a266332cb97e157ab66f9d92ef41ac982b340
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:32:19 +0000

    right 571enough purpose fine yes

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
