1d662b4dfaaf4183ccb51efd13f3324169b9c358
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:19:38 +0000

    effect marry along sudden surface

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import platform
-def get_os_info():
-        return platform.uname()
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
