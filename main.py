ec607ba720f42bedcaa3c871b3327775d916eb37
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:06:12 +0000

    content judge substance careful punish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  def subtract_numbers(x, y):
+        return x - y
