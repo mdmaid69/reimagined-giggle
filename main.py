709282c16383d87241f46927cbfa1e7d04f5f29c
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:22:33 +0000

    special workout thanks able my

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def square_number(x):
-        return x**2
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
