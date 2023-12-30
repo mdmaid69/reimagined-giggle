933f51b7610acd136a5becd68f75dfa9465be65c
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:45:17 +0000

    push hit basic gooff receive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def find_max(lst):
+        return max(lst)
+text = "Hello, world!"
+print("Words:", len(text.split()))
