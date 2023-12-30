ea9d8addbdb581589e41141182ce32bcda96f703
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:38:50 +0000

    cold congratulate getaway method noone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+for i in range(10): print(i)
