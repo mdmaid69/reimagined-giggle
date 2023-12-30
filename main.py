b1941f0078d09f4a5bcdef96a899aa6fd20670d5
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:32:33 +0000

    whistle guess basic repair comeout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def count_characters(sentence):
+        return len(sentence)
