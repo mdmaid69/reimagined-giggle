60bafe14b5ef2de90f47bef1e852cddfcd2be635
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:12:27 +0000

    goback(to) pencil history Hh 568energy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import sys
+  def get_python_version():
+        return sys.version
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
