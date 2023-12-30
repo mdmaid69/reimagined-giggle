c75d4124b19ae9dffb9600efb6454a6204e62254
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:45:49 +0000

    scared takeback previous will male

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_base_name(path):
+        return os.path.basename(path)
