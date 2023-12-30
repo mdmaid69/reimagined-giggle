e50c7237773fc6ae999b8058ead9b15a58f7fd14
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:59:07 +0000

    protest exist anywhere remark float

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
