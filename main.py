2b838b5ce4240bde5a5dd5d073e740dede3be2ee
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:56:18 +0000

    bar offer software sure contrast

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
