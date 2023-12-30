e92c01d7078f3e4f7475f6fffa084f9c5eefb86d
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:20:45 +0000

    poison present clean every warn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_speed(distance, time):
+        return distance / time
