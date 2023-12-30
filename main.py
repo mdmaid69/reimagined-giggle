0c7d642c212c8a1ab245d11d3e4b391a2e1a2682
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:34:19 +0000

    reject change meaning several sleep

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-for i in range(5):
-        print(i)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
