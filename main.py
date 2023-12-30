3e7ff2de4c82718dfc1edae06178ffa189bc03d6
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:16:56 +0000

    then piece form fold size

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
