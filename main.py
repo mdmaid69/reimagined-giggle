40f929f35be54357a3b8bdd7e8d66f2abfb22a25
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:13:33 +0000

    flow music still cross pleasure

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def convert_to_octal(n):
+        return oct(n)
