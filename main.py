08c88327b177c9e9eefa9ddf88af00f6b76404fd
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:52:54 +0000

    distant keepaway whistle comeup trousers

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-x = 10
-y = 20
-print("Sum:", x + y)
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
