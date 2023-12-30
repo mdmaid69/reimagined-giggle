920618cfde35cf3cbe546bc495e622607e2239b1
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:46:20 +0000

    unkind worried unite answer success

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
