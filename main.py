11c749e82f033158d4a710ecb6564f32b539f1ed
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:20:22 +0000

    thinkover between cope pure accidentally

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def append_to_array(array, item):
-        array.append(item)
+def set_array_item(array, i, item):
+        array[i] = item
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
