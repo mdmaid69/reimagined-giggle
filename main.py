8fe7adac28ad505268301d6def76362c5073b0a0
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:25:11 +0000

    loud organization couple opponent important

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
