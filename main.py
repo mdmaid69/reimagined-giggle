58ba8f116eb4471459b6435d7cedd49bb10b6a34
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:12:24 +0000

    move smell phone train sink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
