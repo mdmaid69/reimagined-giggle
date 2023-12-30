f0a7bd2005b82ac233e74d9aa1a7ef8330b19cc2
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:32:28 +0000

    office copy father cook someone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
