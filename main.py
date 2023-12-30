83c2b9bc97a9b8bd07d2881e7d6f4b2059c258a9
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:13:46 +0000

    front healthy pour clearly recovery

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
