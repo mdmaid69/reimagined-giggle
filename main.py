a5421aaf682dc52ce8e1ecc2c9104d2336b99c4d
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:09:04 +0000

    chat store clearly simple we

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
