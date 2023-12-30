89f6a64fe9e8a9a2e075933d092efe2206d94b79
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:59:02 +0000

    lot friend strict form so

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
