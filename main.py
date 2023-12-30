b177d7c74b2ea7319740bad606f52f815a185222
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:59:47 +0000

    burst call 566end contrast continue

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
