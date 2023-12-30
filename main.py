da68ba2e0a4b641a8048b0b779fe11677cc1f7a5
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:21:49 +0000

    lean service schedule common thinkover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
