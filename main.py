3dd53b461a9f8ee59f0d3b1f74c957e6d370b509
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:05:52 +0000

    win shop interview ordinary Ee

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
