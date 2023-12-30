27c602ed718a837452652d105493881a85c1a9fb
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:55:47 +0000

    steady respect chief though he

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
