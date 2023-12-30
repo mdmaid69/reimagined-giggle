2d51dfc42e609250b875a43a468e94eb8b1784b0
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:56:36 +0000

    towards confident clearly mouth move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def get_array_length(array):
+        return len(array)
