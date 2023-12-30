f1fead2e24df43e328caf8e9d54cf4e7b5f8e58c
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:07:16 +0000

    tidy such notice track body

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-print(sum(range(10)))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def calculate_area_rectangle(l, w):
+        return l * w
