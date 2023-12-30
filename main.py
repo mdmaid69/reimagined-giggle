6465b4fc2262aa00affcf863ef1f4a7541b5146e
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:35:11 +0000

    left order accidentally suggest passage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sys
+print(sys.version)
