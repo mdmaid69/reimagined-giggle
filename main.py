fd76148b847fc4831bb650d15cb362e6fc17b40e
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:34:24 +0000

    probably broad comeover indeed new

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_current_working_directory():
+        return os.getcwd()
