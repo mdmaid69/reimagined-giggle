ef91e91d6b622e47a6df9df4b89bdb1144d1775d
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:56:43 +0000

    style Zz grow previous smell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_interest(principal, rate, time):
         return principal * (1 + rate)**time
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
