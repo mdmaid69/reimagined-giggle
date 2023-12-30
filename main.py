056a4314e4789b594a5e6f0f059e87415795015f
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:51:53 +0000

    ability movement split entrance bath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
