3db0469a137b7f5617d4375ecc38e792f93001ad
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:50:57 +0000

    expensive fierce can interested table

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import collections
-def create_user_string():
-        return collections.UserString()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
