ac74837edade9abe75aa8a4b934a14267f9e7c65
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:42:15 +0000

    wave lean nigga gap ago

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
