7b5734291e11c2ff60b76e267f8e104864e1321c
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:02:30 +0000

    grand please somewhere yours tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
