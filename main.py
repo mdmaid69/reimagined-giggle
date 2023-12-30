be65f250c39991f26fd47752ba13321c05b9d8df
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:01:59 +0000

    small charge dry variety giveup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
