c86def907644e9658c3be1b70f318cb9bc3e4085
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:14:03 +0000

    address exist comeup quantity importance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
