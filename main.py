6a26d611c6557a62d542bd932075a13e372dd4e4
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:24:15 +0000

    cry advice flow star discover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
