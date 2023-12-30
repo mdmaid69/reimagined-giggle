e8f6d505a52ec5f3820c38d919af62f2ce38c464
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:20:34 +0000

    event down scrape recent drive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
