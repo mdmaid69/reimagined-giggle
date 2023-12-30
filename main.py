ccc0f708b6e54bec785615a2509b45ee575d1fd4
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:14:48 +0000

    reserve include crowd giveback you

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
