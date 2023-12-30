baf502ac3f31e756fee40d418dafdf6b9729ac79
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:14:04 +0000

    possible sometimes take condition calm

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def calculate_pressure(force, area):
+        return force / area
