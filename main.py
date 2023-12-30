19ecaf9e6b3322612885dd19e1eb443e0d779174
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:10:04 +0000

    closed satisfactory Nn love murder

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
