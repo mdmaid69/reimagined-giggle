ba6b8412a431576c23116ac3ad99af1c442c5742
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:15:11 +0000

    worse some Jj beneath independent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+text = "Hello, world!"
+print("Words:", len(text.split()))
