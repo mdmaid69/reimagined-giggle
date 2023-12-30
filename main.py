ba9840a49a229b0f125a52578188e8daf939f7da
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:37:22 +0000

    nearly still gas plant Hh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
