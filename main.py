0e9d1412c7c4f8c7ac49de56b8a3b4e620b64abe
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:01:32 +0000

    Qq face model negative wild

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
 import array
 def get_array_item_count(array, item):
         return array.count(item)
