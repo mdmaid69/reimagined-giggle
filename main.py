746016150a1d39f43cc29ef8dba0f7c650af65cb
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:08:58 +0000

    sink juice thinkover loud balance

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
