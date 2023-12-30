2e52029179c7622b7fa7cb40deb179b84da4bff5
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:48:46 +0000

    burn aim tragedy regional child

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  def convert_to_binary(n):
+        return bin(n)
