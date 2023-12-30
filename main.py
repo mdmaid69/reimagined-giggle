8c3c0dc25b3587d55fffce5a4cd74bc5bfac9671
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:53:37 +0000

    note congratulate action embarrassing by

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
