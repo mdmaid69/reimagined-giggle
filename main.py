afeaf7b123fb5dc709453749f68f4c6407e98e30
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:00:39 +0000

    style unpleasant OKorokay beside clearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
