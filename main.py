6c3042fabbd82efbbe8bb24bb5af018121989498
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:51:01 +0000

    excuse vanish risk trust judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import sys
-  def get_python_version():
-        return sys.version
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
