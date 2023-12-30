4b2d6f7a17e3acff2d8d9384a808d155f099dc7d
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:22:34 +0000

    wave thanks count waste rare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-def square_number(x):
-        return x**2
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
