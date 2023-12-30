efbda945e0492eb2d029ab20f0797f7be027b1bd
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:17:09 +0000

    company practical approve check know

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
