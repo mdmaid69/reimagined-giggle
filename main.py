a3a600ed87148809ab82a8770af63ab4150ce548
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:57:24 +0000

    if more again quickly sky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+print(math.pi)
