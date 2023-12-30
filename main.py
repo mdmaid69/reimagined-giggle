40f2e7d635bca1f197cc811b3592c3b30ff470a7
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:18:16 +0000

    good shine trousers freedom period

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
