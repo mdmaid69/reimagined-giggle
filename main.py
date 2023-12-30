28b2dab4231fb53fd3e0df24b4d7c52857a43194
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:33:22 +0000

    thumb dance shout workon home

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
