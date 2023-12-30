7fd57c39fb96163369a33d4b05e61b2f7e78adc0
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:30:22 +0000

    Qq create react his definite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 n = 10
 print("Square numbers:", [x**2 for x in range(n)])
