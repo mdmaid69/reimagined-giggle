00545f6ab4d4f6e4240faefb32110eb14cbdd387
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:49:42 +0000

    among stand increase pen he

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
