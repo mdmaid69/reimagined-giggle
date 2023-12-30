146f7e35c9e66c70f3cc3f7c9ca9a44dece2ace1
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:20:07 +0000

    bottom sensitive report cope nurse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
