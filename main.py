9178265206c0c6dbb643405402a8f6711bd13dd6
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:44:21 +0000

    I describe butter little guilty

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Reversed:", text[::-1])
