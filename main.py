e20d0c9efdd05af24e3f50b165b18dea8b6b4f54
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:44:24 +0000

    temperature love book crisis wide

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
