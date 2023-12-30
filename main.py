e8946edb3847783f15391dc9272df7fd10b0a76c
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:27:29 +0000

    wall direct film contrast raw

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def get_array_as_bool(array):
+        return bool(array)
+import numpy as np
+print(np.array([1, 2, 3]))
