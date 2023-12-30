1f5c3ff74605d17d0fabc63f159088ba36d445b5
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:02:23 +0000

    properly regular example marry brain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_absolute_value(x):
+        return math.fabs(x)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
