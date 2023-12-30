6a481ddeb396f8195488d32cf7099d2356f321df
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:27:49 +0000

    building mess now uponon gather

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
