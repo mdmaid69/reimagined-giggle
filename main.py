a7cd316af36000caaaf98c1876a3f5d18988b8dd
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:29:29 +0000

    compare shade situation image unlikely

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
