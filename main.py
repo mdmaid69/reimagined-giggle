c9ae79a2db2fb8f8ed168c7d8975d76be9a8a45e
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:31:57 +0000

    tap land once no suit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
