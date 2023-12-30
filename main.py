e4e32089eb15733804758cb44eb09683f26432cb
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:55:21 +0000

    winter to apart arrangement positive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
