2b13f9240c4384918d87d6df50c25552351119a6
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:22:22 +0000

    various ball radio involve mystery

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
