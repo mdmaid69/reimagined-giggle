966be7652af4720c29883fd3f0e2e47c411bdabf
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:18:58 +0000

    oil tired nowhere runout crash

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_power(work, time):
+        return work / time
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
