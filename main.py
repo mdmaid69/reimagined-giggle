92e465e1152f3cdf6abac75b211eb8facee1abb4
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:02:28 +0000

    home low brief Vv with

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
