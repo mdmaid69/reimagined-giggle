60e010c95b7e95d7c66e3f50a3336a303bc84e25
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:34:35 +0000

    foreign Hh freedom listenfor warm

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
