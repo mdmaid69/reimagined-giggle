76a80ed1d45bf384368355323226bac1cfe42c84
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:22:52 +0000

    beat Nn beautiful priority sticky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import time
-def get_current_time():
-        return time.time()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
