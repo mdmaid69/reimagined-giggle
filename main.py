e822363564ad7f9227ab58a8252b4a4c46d3f385
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:29:11 +0000

    everyone rub translate you ride

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
