d39577b3fde01eed7e73a2be67398c5d5b2731b9
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:04:53 +0000

    survive kind label ear behind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_str(array):
+        return str(array)
