a016eb33dac90f721c3bdbda27a202cfc743802a
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:25:51 +0000

    correct demand interruption beyond sometimes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
