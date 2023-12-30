9fed98965ebbf8c672c092ec6eb36569226f0087
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:48:47 +0000

    lucky command meet Tt make

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
