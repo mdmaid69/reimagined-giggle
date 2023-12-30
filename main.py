88c5288bc04f5a9f88b375205945b34738d9583f
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:21:49 +0000

    corner declare consider frightened border

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def square_number(x):
-        return x**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def create_user_list():
+        return collections.UserList()
