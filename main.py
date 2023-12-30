39d4131981c470f822d86e7c73f770f95ed6cca3
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:21:41 +0000

    few chance dress less popular

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
