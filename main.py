f235590160cef29a6713ce5fff0c7009586e4062
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:35:54 +0000

    nervous finally nor well blow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def calculate_density(mass, volume):
-        return mass / volume
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
