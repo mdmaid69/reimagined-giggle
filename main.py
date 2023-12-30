fc3779c83fedfb3ce28be27e5ca25eb13a1f224d
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:54:01 +0000

    organization takeaway come actual knowledge

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
