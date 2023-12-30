dfccd270be4c25452a679ad489a126d6a74c18d8
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:25:09 +0000

    improvement chance lookaround brush voice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sys
-print(sys.version)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
