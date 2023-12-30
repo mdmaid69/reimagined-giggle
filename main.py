25ca8b40db283f917d2914be580b4b82e42d6d36
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:20:00 +0000

    himself else excited best declaration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
