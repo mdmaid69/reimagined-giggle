d65aecd37fdb499d20e92e570ab24822a3606a4b
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:50:12 +0000

    accident through science contents block

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def square_number(x):
-        return x**2
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
