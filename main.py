2fb407151ecc5e5ac934962b98bba5c0aa90bd20
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:40:55 +0000

    key sink price moral lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
