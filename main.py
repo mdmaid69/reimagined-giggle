47b41d8ccbe918e160f7f8683964ec06f6e9feb6
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:07:39 +0000

    warn lie mess Ee sorry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_max(numbers):
+        return max(numbers)
