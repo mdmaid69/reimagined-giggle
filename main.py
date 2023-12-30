89b1b7c9bf8db6a081b72b0dd21a6bf6684586c4
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:46:06 +0000

    wipe anything soon achievement need

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
