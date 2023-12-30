17f30cd5499b9ce4aa7647a139b7ea834675a1ad
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:49:18 +0000

    tell breakout physical female towards

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_factorial(n):
+        return math.factorial(n)
