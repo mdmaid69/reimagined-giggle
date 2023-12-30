15b59a05a1b8c465d9fc3eec987ef4f8c7314e47
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:41:49 +0000

    old content even square do

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
