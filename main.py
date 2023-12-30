2804aa6f6cde063aa57adecff3e4fa19e7f53e90
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:35:01 +0000

    statement divide intend fasten birthday

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
