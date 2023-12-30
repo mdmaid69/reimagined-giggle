b7b63285e32e62bed4f71a0961947b8a91daff72
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:28:01 +0000

    distant escape perform clean protest

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import tensorflow as tf
+print(tf.__version__)
