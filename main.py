82d27b0cf91fb361c3acb38e568ca714d86c2809
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:11:29 +0000

    anybody recovery game could sit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
