caeba979419cdb93e3c154654f23916906b610de
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:42:30 +0000

    workupto watch often heavy trap

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def convert_to_octal(n):
+        return oct(n)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
