bc3f8ba79b87fc853c4671838872a09921f01fe3
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:23:51 +0000

    represent life dinner course instrument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
