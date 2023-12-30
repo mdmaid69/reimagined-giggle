a466a0187eb3e2e9014c0725efb886b01faf5def
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:03:50 +0000

    surface instant tight transparent colour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
