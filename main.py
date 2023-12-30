d2240acc20984c896c49674ffe8a4fbc3051ff9e
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:46:04 +0000

    involve whistle holdup passage impression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def find_max(numbers):
+        return max(numbers)
