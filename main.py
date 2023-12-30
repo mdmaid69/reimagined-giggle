7f7cf504f8c02b32cd67a496cfed1e74c7ee99a9
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:48:40 +0000

    practical walk finally Ee half

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
