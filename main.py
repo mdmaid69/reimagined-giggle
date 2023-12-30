4229e1e3636f3ca1599a5c93f736b7b4622cd466
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:15:47 +0000

    conclusion alive confuse within flow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def get_array_as_repr(array):
+        return repr(array)
