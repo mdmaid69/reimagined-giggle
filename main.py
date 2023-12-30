92dae8a4794fbad353b4aca966eaa770c46d0741
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:51:11 +0000

    spring schedule holdout train lesson

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(lst):
-        return max(lst)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def is_palindrome(s):
+        return s == s[::-1]
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
