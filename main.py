6b57d13e8df6b32e1d963d07fed93e0a599800ab
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:46:29 +0000

    computer race takeoff realize black

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
