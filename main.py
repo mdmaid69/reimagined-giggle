bef5fbf563d392e7a6fdd5eb6b2a6a810854d524
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:00:32 +0000

    sweet control giveout area advanced

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def convert_to_octal(n):
-        return oct(n)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
