a326e1c7b53d91b7a7427d23a5c80aa6d45d84d6
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:32:46 +0000

    minimum building wish pair further

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
