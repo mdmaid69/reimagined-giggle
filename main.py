ebbbbb1f66f175e32f3ca64d40cc92bcee5f9233
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:04:18 +0000

    strike know immediately since stretch

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
