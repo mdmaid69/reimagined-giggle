a11e6234873d57ac55a12a3fb290d5e8ef545a9c
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:59:35 +0000

    strange perfect lost sell hour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
