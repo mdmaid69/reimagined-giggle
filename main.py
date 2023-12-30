6035c7a9dc1da1f1209274d70373d0461c653179
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:47:34 +0000

    secret away share Ll lot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def split_path(path):
+        return os.path.split(path)
