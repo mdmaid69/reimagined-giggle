26414e316181842ae8d35c91fb5fcef3ef178ba0
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:55:23 +0000

    keepfrom name eye worried finally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def split_path(path):
+        return os.path.split(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
