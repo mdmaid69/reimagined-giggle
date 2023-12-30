7cd357e0863c2b4ac71a83a3a00d954e9f63e54c
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:25:38 +0000

    start once point cause whichever

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def square_number(x):
+        return x**2
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
