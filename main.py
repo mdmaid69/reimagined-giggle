c9107f77ef42abba727a5c4b78edf7acd497c9e0
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:59:45 +0000

    remove ring business touch shame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
