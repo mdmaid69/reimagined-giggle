afe48633b09b7658663582d2713484dadbd5a5fb
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:38:21 +0000

    building purpose card heavy problem

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-for i in range(10): print(i)
-def multiply_numbers(x, y):
-        return x * y
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_power(work, time):
+        return work / time
