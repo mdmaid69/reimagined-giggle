2a017f28b93748532247efe5ed963a1f8ea06aa0
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:36:38 +0000

    appeal congratulations final succeed throw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
