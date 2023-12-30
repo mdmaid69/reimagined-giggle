121270d3a69d35939be04fd198b9a551515946fd
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:12:45 +0000

    borrow way cry rely 566end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_floor(x):
+        return math.floor(x)
