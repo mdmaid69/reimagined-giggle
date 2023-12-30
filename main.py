d8f85569c809baa69426fd835a16934b053ff3aa
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:45:13 +0000

    roof feeling exciting shoot clean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
