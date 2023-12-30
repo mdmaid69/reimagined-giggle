2ecbc56b05e30e8262cbd8503351a46acbfefb20
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:36:55 +0000

    yourself chief safety central tread

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_min(numbers):
-        return min(numbers)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
