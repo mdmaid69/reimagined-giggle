f38c79c55c84fac4cf250d39ea8d1d39f19bf4a0
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:22:03 +0000

    news brush ball fall smart

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
