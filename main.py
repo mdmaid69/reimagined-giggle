9610098ab33d622b09086931d766be04aacca65d
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:10:48 +0000

    runout case sensitive hurt design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import numpy as np
+print(np.array([1, 2, 3]))
