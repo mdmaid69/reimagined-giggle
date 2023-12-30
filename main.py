12689565816b13c2f90f61b730e3bc98c07450e2
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:23:42 +0000

    sensitive fill Ii tense finish

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def is_odd(n):
+        return n % 2 != 0
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
