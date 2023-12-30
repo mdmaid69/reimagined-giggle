29f7815954072f2cc80357004dff983f7b7a0f40
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:03:18 +0000

    apart raw pretty after I

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import logging
+def log_message(message):
+        logging.info(message)
