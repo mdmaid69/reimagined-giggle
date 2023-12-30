ea9b70c141b5994b9ddd039917486bdaf4334091
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:38:43 +0000

    dance attitude profit available the

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
