5964353d1a6956eddf463a9472ea83e9e19f4e9b
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:41:15 +0000

    dark stage brain excellent grand

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import math
+print(math.pi)
+def divide_numbers(x, y):
+        return x / y
