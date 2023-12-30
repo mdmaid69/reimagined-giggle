ca83b6a55bcaf88908fb518e15881d3a40c09c66
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:04:08 +0000

    note joke exploration lower relationship

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
