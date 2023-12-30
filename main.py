73fdf9da5312e5c20e9b8ebd7bcf386c1ec1d3d1
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:50:54 +0000

    run check report do war

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import array
+def append_to_array(array, item):
+        array.append(item)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
