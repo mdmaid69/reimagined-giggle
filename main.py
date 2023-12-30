47eeeba806d33cb0fb57ba1b49ec412433c6478b
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:51:23 +0000

    judge Ii accident priority spray

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
