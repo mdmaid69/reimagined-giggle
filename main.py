d620ab2fb6730e3a87c184b9e6de78f2d14a249b
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:15:24 +0000

    buy bored goout swing turnout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
