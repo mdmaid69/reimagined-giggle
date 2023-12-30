d667b9b7326f3a1bef1f02eb83f2e76c10ebbe98
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:41:35 +0000

    communication recent feature warn hollow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def square_number(x):
-        return x**2
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
