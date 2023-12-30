2d1dca0c79aa237adfb03255fb795f2b781c4e00
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:34:07 +0000

    beyond giveup free mention bridge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
