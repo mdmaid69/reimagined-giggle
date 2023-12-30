c7e6bf4a0819fa37c12d3612fe4f8b9c7326ffc6
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:40:42 +0000

    impression with principle process song

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
