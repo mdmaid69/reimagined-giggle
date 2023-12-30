95f6642b67e2f55e975885d7e00cf7b28f9f4695
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:45:42 +0000

    bother contact scared surprising unable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
