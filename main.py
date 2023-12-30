87baa57e27a30ce332f62704bd66156176f2a885
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:04:50 +0000

    show beginning relative case Kk

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import random
+print(random.randint(0, 100))
