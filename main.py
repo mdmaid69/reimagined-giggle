a68d7e10d762fc2676d79d7da42e87b8518d4090
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:51:45 +0000

    between bang wise neat cold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def get_array_as_complex(array):
+        return complex(array[0])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
