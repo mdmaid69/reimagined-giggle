412206887c628fbaee8826a62e65f2c7af8dea03
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:23:59 +0000

    share bear prevent relation fasten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
