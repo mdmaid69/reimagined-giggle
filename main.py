27846bbaabefacbb64c500936f8d1ebd9eaa5f89
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:17:50 +0000

    someone design escape music comefrom

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
