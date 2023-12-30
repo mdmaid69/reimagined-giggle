69259c272e8f459e31b43d80b649fdb712b699c0
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:42:31 +0000

    really shut butter demand world

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def remove_duplicates(lst):
+        return list(set(lst))
