014def08f824886507dee2792c0831d7ea100273
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:05:03 +0000

    couple wake trip original discovery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
