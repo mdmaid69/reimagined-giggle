fc8a3581098b24e14fff9d5719a5e0a6586bae5d
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:14:51 +0000

    hard suckme look prize anywhere

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
