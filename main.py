4445b9e445d156b6b7d639d84751c52384e3aa75
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:40:34 +0000

    sink chat list wrong official

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-import math
-print(math.pi)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
