5395d0aed0d8adea8b3d31ad427090fd996062ba
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:34:52 +0000

    aim trick arrival understand escape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import re
+print(re.match("h.*o", "hello world"))
