478653a5b4441944a3d55bd9aa59653f36682810
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:00:13 +0000

    sister stand influence reduction eye

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_list_from_array(array):
+        return array.tolist()
