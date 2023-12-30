8e3483cdb82fd4046276d1d8634930f61d5404b3
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:38:35 +0000

    undo suggestion name difficulty develop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def count_elements(lst):
-        return len(lst)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
