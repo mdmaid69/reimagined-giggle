21dcbd21c6081ccb60255ee68b37d5ef0dcfbc67
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:04:11 +0000

    giveout will behind turnout lean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_speed(distance, time):
+        return distance / time
