6b5060662b31f061fb4de6b9f2b0d169a5c54d66
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:42:03 +0000

    hospital backwards advise forever book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def clear_array(array):
+        array *= 0
+def cube_number(x):
+        return x**3
