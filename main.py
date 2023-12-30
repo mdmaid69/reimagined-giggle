7e5b1c39372aa85c005213dea422b39af2ae9997
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:06:21 +0000

    allow giveout advanced fight awake

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_area_circle(r):
+        return 3.14 * r**2
