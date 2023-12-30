105302c0605731cfa655d7a3d5f987a5825f24cf
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:39:15 +0000

    better gift pile minority wash

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
