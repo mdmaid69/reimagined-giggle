540c04bcdc58a762700dc6c73e0b570e29c177b1
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:53:42 +0000

    react throw cheque back cough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_exponential(x):
+        return math.exp(x)
