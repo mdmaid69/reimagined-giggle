7a3e66559f51efc1ffbf42af4d8fd6700c87bc00
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:12:57 +0000

    callup mean early much be

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
