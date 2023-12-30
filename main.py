c117739536a1316cd5be46116dca435a5a6a1fcc
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:26:09 +0000

    police burn dry pronunciation example

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def reverse_list(lst):
+        return lst[::-1]
