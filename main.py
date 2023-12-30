f998e1d9216cce709c1b6176b8e2cba982db9a61
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:03:17 +0000

    effort bringup stir shut fit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def exit_program():
+        sys.exit()
