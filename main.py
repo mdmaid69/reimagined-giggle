1050d8be5182abc8a7071c983cdf4c923be6447a
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:49:04 +0000

    instrument offer unlikely apart squeeze

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
