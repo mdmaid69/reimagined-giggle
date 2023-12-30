f444323088302bfdd7fdf338d78d143df94454eb
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:25:57 +0000

    other book turn acceptable he

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
