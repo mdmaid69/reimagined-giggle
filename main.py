98ef04fe15cdf2057c2d37baa9b932aa7bb17d69
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:16:55 +0000

    pronunciation news care dare present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
