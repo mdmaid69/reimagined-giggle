16a448f590a152de1267926d1201d0979cbc62ee
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:16:07 +0000

    collection pool spring over previous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
