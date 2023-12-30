325bda5bc92efccb90eb1a283645f2e62d22f1cf
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:29:58 +0000

    lie similar sail neither reason

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
