c1b11c5194929476562a3f2b808ad5ddf350a39b
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:56:06 +0000

    plenty birthday honest study opinion

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_item(array, i):
+        return array[i]
