78a075c112e03c2a2d0faaa53d5259f43d7263c0
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:34:35 +0000

    benefit smell bet everywhere cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
