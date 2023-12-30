c33d2740d2b5ac25e57db3efa832b2c80c9954f8
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:24:19 +0000

    over marry dig another fight

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+from collections import Counter
+print(Counter("hello world"))
