076f62302b74256ea7ddd2b81d628249097afcd9
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:17:35 +0000

    little sting cut payment mention

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import time
+print(time.time())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
