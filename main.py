cee6c335efc5371743f94aeff4b645d821cdd080
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:13:10 +0000

    getback mess afterwards education whistle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_float(array):
+        return float(array[0])
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
