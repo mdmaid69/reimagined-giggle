2abbceb6620722c421f8d881ec69aa4482729dec
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:54:23 +0000

    pipe show pour put callback

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
