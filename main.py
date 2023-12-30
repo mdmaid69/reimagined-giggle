aadf2dd2946349f077f960f871bab29dbd90e363
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:22:10 +0000

    public view stranger birth nerve

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import sys
+print(sys.version)
