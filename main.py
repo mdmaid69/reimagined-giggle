d1854b5d41c141fb1333641f726f3cc7a7656ba6
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:13:34 +0000

    mystery exam moment pipe study

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
