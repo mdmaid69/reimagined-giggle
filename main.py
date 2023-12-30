2324c92f2ea51fe31f5e2f6f2a6f16d2df87e4f4
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:46:51 +0000

    getout fresh object round mother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import os
+def get_current_working_directory():
+        return os.getcwd()
