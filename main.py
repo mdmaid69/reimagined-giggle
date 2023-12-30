b68a333115338b9dbf552162519c9731a6251a5f
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:44:38 +0000

    tie guess bang play eye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-for i in range(5):
-        print(i)
+import getpass
+def get_username():
+        return getpass.getuser()
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
