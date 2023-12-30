248a868b7737676094ba55718c25188c8280a0e0
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:29:36 +0000

    small mess distance pure hand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def reverse_array(array):
-        array.reverse()
+import sys
+def print_python_version():
+        return sys.version
+def reverse_list(lst):
+        return lst[::-1]
