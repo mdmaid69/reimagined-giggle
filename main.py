043bb967c65ab7a3d26a72098e0484a6c8007d73
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:44:21 +0000

    phone comment contents turnaway off

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import tensorflow as tf
+print(tf.__version__)
+import random
+print(random.randint(0, 100))
