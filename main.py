3b640c8faf7c4b4e491957756232543df4e12c13
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:31:44 +0000

    sense opposite Yy waste between

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def is_even(n):
+        return n % 2 == 0
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
