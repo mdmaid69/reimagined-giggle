58632dd7996f3d41c084d3117dd40a3915650f39
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:20:20 +0000

    responsible criticize busy rate strength

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+print(sum(range(10)))
