1955a0f4ee53824e393ecdb23ef756cf5512f2a4
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:12:57 +0000

    split drawing sure fit inside

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
