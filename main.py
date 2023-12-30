93659a0e36604c6784c2c862a3c82f95a16aea90
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:45:23 +0000

    gap but mind piece interrupt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
