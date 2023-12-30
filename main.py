f8d31171582071906aed68e876e3bd005a16a00f
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:20:18 +0000

    least calm start become disturbing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
