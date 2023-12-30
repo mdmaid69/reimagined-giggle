acd1aab877102a2bc9d3b0ad030ac096a7ee4d10
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:45:00 +0000

    glad steep belief tired feed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
