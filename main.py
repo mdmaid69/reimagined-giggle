715aaeb7d85cd0b5fcab84a2c8dcf184d6b070e2
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:21:38 +0000

    far recover interrupt might she

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
