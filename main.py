8c837eb197ba30888ca6d5e5a39433655fc4962d
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:41:56 +0000

    cap needle damp later talkinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
