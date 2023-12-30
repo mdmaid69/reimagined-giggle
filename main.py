6e025eab482b45bbc202a6fbec209b6404cbecee
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:27:05 +0000

    low judge should soak show

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+from collections import Counter
+print(Counter("hello world"))
