1e1e3dbce956f340f58cda2ce4e47d5b3ad5cb24
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:38:48 +0000

    skin block ring description string

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import collections
-def create_user_string():
-        return collections.UserString()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
