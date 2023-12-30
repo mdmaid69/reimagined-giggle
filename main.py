8ead32fd460cfb20fed37726554c9029ac930287
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:53:00 +0000

    Ss preparation first keepfrom but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
