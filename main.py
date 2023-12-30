60dff1537c9e03028591672327b34c96e24a6f26
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:43:33 +0000

    free production interfere people dress

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
