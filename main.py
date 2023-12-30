cbb285477b382a2fc13c11510d1c358cc47c36ef
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:21:22 +0000

    bin stamp hollow beautiful figure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import time
-def get_current_time():
-        return time.time()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
