3f624314599d9f2a4b148c8ea1f59217e73145a8
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:51:13 +0000

    delicate backwards first now forgive

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
