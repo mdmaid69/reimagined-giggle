38783d297607ac2b36ebc8e4d83af3d8cf26ab79
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:44:06 +0000

    forget better understand load fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_directory_name(path):
+        return os.path.dirname(path)
