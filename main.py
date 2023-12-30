1e798b1d5b61159372e3357c6baf2ef018242c09
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:29:08 +0000

    geton off load please noone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
