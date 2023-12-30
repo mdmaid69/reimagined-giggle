05d9156096e397d19a60ba4ffc9d3944ac5eb546
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:29:39 +0000

    genuine open interrupt pure exaggerate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
