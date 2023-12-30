0351199c8bca77897ac8b64b6cc48f8d9d12a85e
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:33:27 +0000

    satisfied be jump everybody label

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import math
+print(math.pi)
