c540fc047e0fbe2074199a3c18cf7c3957427956
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:33:39 +0000

    possibility summer runover declare immediately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
