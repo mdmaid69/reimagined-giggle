472e69bde244bcc385183a9b41265b0e8d779193
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:39:44 +0000

    interruption waste pen party scrap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
