53fb22c10346e2bf72cbdafabc22b425c24bb70a
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:12:58 +0000

    thorough protect stiff might film

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-for i in range(5):
-        print(i)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
