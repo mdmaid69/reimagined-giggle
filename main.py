88366b795863b1e24e5031af43e942bc8b366253
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:57:12 +0000

    disturbing breakin hurry sport dare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
