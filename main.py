4a5ab47ae54e76e4f145b1926bd110b8dfbb72ac
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:31:36 +0000

    train empty innocent straight suppose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
