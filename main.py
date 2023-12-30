3a0bb9e7adb3a9aa46ea3404778dc03be520dff3
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:22:22 +0000

    quit through advantage than window

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
