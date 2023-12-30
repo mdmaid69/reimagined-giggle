d850e1609c2adb04d2e9995656564969e3a7113c
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:42:52 +0000

    relative flow ill score regular

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
