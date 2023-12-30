96db765c796f8b64d6e7ce54144a53b8b31737f8
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:25:38 +0000

    face pronunciation have set commit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
