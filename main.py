28dc75d1cfef319c8c1e555d2f705102a4d95865
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:44:27 +0000

    healthy family dry sudden sky

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
