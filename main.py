80e4322638790075a0abe28b769d01cced198f48
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:56:24 +0000

    print load reach push central

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
