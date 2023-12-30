00f71428f93b14a329fa89d1b9c124b3c048216c
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:04:25 +0000

    getup politics office calm hour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
