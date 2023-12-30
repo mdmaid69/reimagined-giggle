77a153866dc2ebdc3a895a88cc92fbb62c7cf938
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:48:57 +0000

    Cc seem say watch over

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
