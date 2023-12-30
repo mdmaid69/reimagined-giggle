7fd4cf6f997aedf281e7e9dd50999808ee46e6ac
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:37:07 +0000

    forgive more obtain catch tend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
