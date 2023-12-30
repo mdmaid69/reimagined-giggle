1e5b521222eadcb16a44cdfc335b62165b7005a6
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:24:56 +0000

    skirt pinch hello treatment own

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
 def sort_list(lst):
         return sorted(lst)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
