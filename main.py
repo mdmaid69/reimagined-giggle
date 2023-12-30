c8cd2c6c8803273ae58d9dafd78891f869d3a333
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:25:23 +0000

    land match unhappy whistle promise

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
