3a212c6e31cee6df7b0347461a4b8574392f4d91
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:30:23 +0000

    sell pretty measure could rest

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def greet(name):
+        print(f"Hello, {name}!")
+import array
+def convert_array_to_string(array):
+        return array.tostring()
