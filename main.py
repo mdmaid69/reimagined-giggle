49f986b7c32f59e4962205f2c0fbe06a4627ef4b
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:46:40 +0000

    comfort weak car point eye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
