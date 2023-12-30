fa5c9b70f3613dee58c1d632aae333c60b6a9df0
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:54:01 +0000

    instead niggaacademy behind behave create

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
