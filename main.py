c48e03e04a6ab822b05fed5e7add8b1a164c70fb
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:12:02 +0000

    measure build drive hold close

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
   import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
         plt.show()
+  def convert_to_octal(n):
+        return oct(n)
