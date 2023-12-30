1ec89a7c2049664aaf8cd8041bbe6b50fd25154e
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:10:19 +0000

    fast hole provide tip pride

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
