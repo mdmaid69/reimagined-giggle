2203f1790c22e7bcff72363b7ddffe63d6c10cf5
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:32:24 +0000

    radio introduction way choice sure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_base_name(path):
+        return os.path.basename(path)
