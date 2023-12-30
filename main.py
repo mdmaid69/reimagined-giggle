331f6faf56d5bc436bbc2a44c667bb5364c8e27e
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:59:47 +0000

    be possession form weigh stretch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+for i in range(5):
+        print(i)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
