98dacaf657a68dbf82ed8b00220465319c650c43
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:22:10 +0000

    duty practise ground professional Ll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import shutil
 def delete_directory(path):
         shutil.rmtree(path)
