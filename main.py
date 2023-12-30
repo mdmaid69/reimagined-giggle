562748dbc2672478050a7a2c523f28e30eac7960
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:48:53 +0000

    hide light sail through mood

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
