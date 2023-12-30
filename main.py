6c01253dc62a6b2b4edaf215b4042cd1afee2aa4
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:08:06 +0000

    experiment mean shut live fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import tensorflow as tf
-print(tf.__version__)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
