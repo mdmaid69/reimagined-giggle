d71dc83ef59f716ea5aa2a82e2a0b3b9bcd80b73
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:51:43 +0000

    sight appear grand breakfast independent

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
