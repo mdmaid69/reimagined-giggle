0093a47e76a3f12ec01dd7fe2202b1e76822dab9
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:43:03 +0000

    left mistake clearly really bury

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def reverse_list(lst):
+        return lst[::-1]
