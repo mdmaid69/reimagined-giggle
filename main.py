ed6a92b1fee75773d0c51662e9e538a3771c43f3
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:23:34 +0000

    dick under plan between pay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
