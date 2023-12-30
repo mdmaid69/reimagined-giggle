c562cf82743dcce5597a3abd339aaf3ddc5ad414
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:16:56 +0000

    because holiday comeback repeat capital

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
