3c1a70b17d983d2318270d2e846df2c850c2e89e
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:44:12 +0000

    regularly vanish development term company

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
