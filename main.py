faa061d5aabdaa3f70d28855b8f49c83ada527db
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:54:22 +0000

    reliable shock relief another sorry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
