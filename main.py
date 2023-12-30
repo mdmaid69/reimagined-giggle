3e574325d0a8b785e9d8aa95d0a8f81c011572d6
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:39:57 +0000

    exploration among tonight offence comein

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
