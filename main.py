19205b0f624ef46e65c606dbd8225f4b0f14f1dc
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:12:03 +0000

    immediate fill fierce brush picture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
