af977a8ce7079985f133b7bad7f63e223e87a353
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:55:21 +0000

    perhaps command badly disapprove mainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
