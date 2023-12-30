e1e5db5c0f23e3d30bdaf7d2d5985471f350e7ed
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:05:26 +0000

    concern clean 569enjoy legal correct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_stack():
+        return collections.deque()
