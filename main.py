7f6074541da6f56329bd052026eafbb97841f92b
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:09:02 +0000

    unlucky crowded ear realize nowhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
