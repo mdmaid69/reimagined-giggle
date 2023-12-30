2fe6ae555f99016aac91a7d2e7ea0585beca316d
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:32:36 +0000

    see full release comment march

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def get_array_as_float(array):
+        return float(array[0])
