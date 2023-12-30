1eb004fe63d9b0d67c6426a140eb5fc15c6af3ab
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:51:36 +0000

    mess reserve minority club nearly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
