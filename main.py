7676ac1d7cb4ed168b8acc8ee3e528c7eccbf485
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:30:16 +0000

    thought fork sing possibly pleased

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import sys
-print(sys.version)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
