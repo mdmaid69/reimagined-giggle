bec135b678790321e75b0ae797308d020ddea0ac
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:16:58 +0000

    development goon importance telephone consider

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_exponential(x):
+        return math.exp(x)
