84a170f2d19d9f23b4b09accfcb4c155b88b39ef
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:26:22 +0000

    sort Cc sticky hurry fine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-from collections import Counter
-print(Counter("hello world"))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
