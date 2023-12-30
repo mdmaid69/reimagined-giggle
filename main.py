bc47a5b7b1dc9a7824fdef8ab76b92e44fe89962
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:52:59 +0000

    half birth mass naturally evening

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def square_number(x):
+        return x**2
+import sys
+print(sys.version)
