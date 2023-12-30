5ca552b9130d53dd2e8b61b4fe83edb537d794ba
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:57:16 +0000

    reduction off healthy join children

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def is_even(n):
-        return n % 2 == 0
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
