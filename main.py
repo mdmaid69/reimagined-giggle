9daa6fc5396228494dee73ad09a9fbc60ee5042c
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:30:50 +0000

    patience forget truth ride level

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
