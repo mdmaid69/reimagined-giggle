f77e28c6ac46004b963aae9bc508987fddf88a5b
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:13:24 +0000

    though treat beside friendly actually

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
