7b10ae81a3c0875449c0cddd403093fce06beda3
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:14:58 +0000

    reserve correct bush comment house

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import os
+def get_current_working_directory():
+        return os.getcwd()
