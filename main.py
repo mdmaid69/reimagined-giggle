d15132362bfdce0a58c5b18c4f8d5b096e449c03
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:08:56 +0000

    holdup wealth bet little duty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
