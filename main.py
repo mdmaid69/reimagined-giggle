3439baaf4b1fecf5fb7b6299824aede1e4fde966
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:58:12 +0000

    report entrance think inside performance

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import array
-def get_array_as_float(array):
-        return float(array[0])
+print([x**2 for x in range(10)])
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
