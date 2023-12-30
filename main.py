fbf03d3c70a3c2d76fd530b0a81ce26824bcf8ef
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:23:34 +0000

    goodbye platform meal concerned drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
