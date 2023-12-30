2df39f693415661b454d2fd30910d0a6f5892edf
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:30:41 +0000

    dance tragedy cause sharp behaviour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
