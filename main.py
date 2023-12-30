942f97d13f48d76926fa1e3c8c9512755d664a92
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:15:11 +0000

    disturbing criticize boring party pump

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import array
+def get_array_typecode(array):
+        return array.typecode
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
