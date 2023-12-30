aee148faf091d88fa7dfae324042cd33ba6c1ba7
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:01:41 +0000

    start spread straight fortunate look

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-print([x**2 for x in range(10)])
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
