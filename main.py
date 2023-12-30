8d4fb93cc5b03898cf39f057395317d080bdd44a
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:49:09 +0000

    personal pot clothes shall operate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
