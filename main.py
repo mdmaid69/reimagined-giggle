980ab6515653e6a9bb12f8bfb1e150aaf9988726
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:49:10 +0000

    recovery fast direct sleep taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import numpy as np
+print(np.array([1, 2, 3]))
+import sys
+def print_python_version():
+        return sys.version
