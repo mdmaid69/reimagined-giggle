0243fa5c1ad415abd15c3fe516528d7f7fb075dd
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:05:58 +0000

    hit fairly door lock possession

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
