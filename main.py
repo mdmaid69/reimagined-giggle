a2156b47274bf9946aa2e97e1b8fe759a1e94da3
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:28:18 +0000

    runover present term pain community

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def count_elements(lst):
+        return len(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
