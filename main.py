f2b5c7ed0351cd8f5cb5097d5659b367b26ef681
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:42:51 +0000

    date defend mean holiday count

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
