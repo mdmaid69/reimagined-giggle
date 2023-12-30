80c3ffe61c15f13a0796e760eda4f2582c0bb632
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:42:19 +0000

    guess although reduction explanation minute

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_current_working_directory():
+        return os.getcwd()
