de92b0ac03f76cf8a1614654748293d51df393de
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:41:35 +0000

    thirsty morning recent completely set

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
