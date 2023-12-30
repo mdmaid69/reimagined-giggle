79b426f62d71aeb8fe12fafc9acd7afe7de7b04a
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:18:21 +0000

    attach teach print hold language

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
