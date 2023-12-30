f0fb5dfc3adb2547d590399850317b8232c73feb
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:15:19 +0000

    doctor cum talkinto prefer interesting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
