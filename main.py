fb394aeed42757831a883c3eda24fded14e443d7
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:22:20 +0000

    welcome act lead even by

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
