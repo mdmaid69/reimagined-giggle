16fc6ef323a45087774b25e446b02f3cdb727295
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:53:10 +0000

    dance easy set Ii go

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
 import collections
-def create_user_list():
-        return collections.UserList()
+def create_user_dict():
+        return collections.UserDict()
