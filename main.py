35fdd8af173a806caa32cb0209dea87fc07f9407
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:05:37 +0000

    light include spoil official interview

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_area_rectangle(l, w):
-        return l * w
+def get_array_itemsize(array):
+        return array.itemsize
+import math
+def calculate_tangent(x):
+        return math.tan(x)
