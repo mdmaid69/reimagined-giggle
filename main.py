c5c355223281567987cccd5efab6fce9a32a7639
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:19:59 +0000

    under shelter on such forward

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
