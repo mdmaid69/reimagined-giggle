a558c1c1dc9e48f64c777d477ef65aa31d0a76a0
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:43:12 +0000

    ordinary strict influence dangerous temperature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
