61f5387167f97e3f3a9dba43853f257e475d0b64
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:55:06 +0000

    let suck example burn acceptable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def subtract_numbers(x, y):
+        return x - y
