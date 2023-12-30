f6a5eb6edbf37843a9e007cd3027d5f1d02f8e07
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:26:13 +0000

    edge ticket mind dig sweep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def get_array_length(array):
+        return len(array)
