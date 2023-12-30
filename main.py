c0f43ee5a057034d58c64fda4f2c1ff83ebbaeeb
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:52:32 +0000

    tour something lie like soft

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
