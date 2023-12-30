5895524b5a84c97405411ddf14629b717c9dbb67
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:58:10 +0000

    press allow joke pour damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
