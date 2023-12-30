21868361310aab62895bf9b265c2bdb564218796
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:55:31 +0000

    confuse calmly union deal contrast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_work(force, distance):
+        return force * distance
