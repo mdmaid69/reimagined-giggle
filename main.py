cfa3c61cc6bfddbb78614ae1deb481a75a36e5a9
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:28:59 +0000

    copy steal strange explanation combine

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import datetime
-print(datetime.datetime.now())
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import numpy as np
+print(np.array([1, 2, 3]))
