a2028ec2709fe41185b699e88b264d1a78e73b44
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:45:26 +0000

    about book clothes almost flow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def convert_to_octal(n):
-        return oct(n)
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
