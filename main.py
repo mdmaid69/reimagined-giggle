1855328df9402aa36678db7cc7e0f76408ce6d5d
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:46:30 +0000

    nature remember corner pollution excellent

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def remove_duplicates(lst):
-        return list(set(lst))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
