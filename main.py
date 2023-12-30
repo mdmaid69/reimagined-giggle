ca5609c90358f30857d719a3fbd39d8a46949adc
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:10:27 +0000

    rank dinner failure sure immediately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import getpass
-def get_username():
-        return getpass.getuser()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
