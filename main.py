bfbcac13efb1d43cf2bec40bf58ecb806ca8cb03
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:53:58 +0000

    someone fight sleep open handle

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
