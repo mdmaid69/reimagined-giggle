cbfc3bfd9c8736f879b13d48b21869a1cd0e813e
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:01:25 +0000

    compete hospital need exchange comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def print_python_version():
+        return sys.version
