b2134d67a8ec0dba36a2714248be8cc0f93b77b2
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:51:46 +0000

    please pleasure name man fat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def remove_from_array(array, item):
+        array.remove(item)
