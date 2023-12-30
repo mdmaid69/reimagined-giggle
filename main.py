369eb6802e835fbfbf9ca35e28ba2f922e1db8de
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:00:17 +0000

    address feel force hurt oil

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
