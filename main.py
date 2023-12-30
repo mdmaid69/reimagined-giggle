bb0cf4e74c019405dc198bfb389a4b6c412322a2
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:13:47 +0000

    send still climb right bin

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def square_number(x):
-        return x**2
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
