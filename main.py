3c3323b98d7599d7a6a1474a1eee740be901a1e6
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:03:02 +0000

    contrast because sorry kill wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import array
+def clear_array(array):
+        array *= 0
