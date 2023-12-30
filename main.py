c8a6cfea48b1c31ceb90a3afad7bc5ea7109210b
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:06:18 +0000

    clean account lower soil zero

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
