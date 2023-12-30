0bc2513b9b68268aafd50b2a7167f02cf6346f9a
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:23:41 +0000

    field label getout mean personal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
