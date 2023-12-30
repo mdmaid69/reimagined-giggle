c2f1953573cb8794944cb5765189a7c0a15ed0ad
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:14:26 +0000

    free lock easy 565end cope

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import math
+print(math.pi)
+def add_numbers(x, y):
+        return x + y
