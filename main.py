18b9b6d883d1eb757e523836ebc6fd87e7e576e4
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:28:43 +0000

    form bet calm voice calmly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
