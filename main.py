cc016518d5ef255d9270119386ddcc57187e9995
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:29:47 +0000

    ourselves married protest wire edge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def get_array_typecode(array):
+        return array.typecode
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
