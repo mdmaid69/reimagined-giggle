74e3913744b14923f37e3655e84c1c540212044f
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:09:58 +0000

    region perhaps go suddenly difference

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
