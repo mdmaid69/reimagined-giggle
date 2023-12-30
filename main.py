1092eae8068d07b099c20e6db4363bba744c041d
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:03:03 +0000

    takeout report print history them

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
