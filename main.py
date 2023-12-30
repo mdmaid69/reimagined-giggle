e28743a77fe1825646157750fedb4d28bb9aa356
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:20:59 +0000

    produce under agree comeover star

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
