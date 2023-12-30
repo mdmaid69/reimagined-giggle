38cce6716613e6137ff077d0c209acf682c4e932
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:50:14 +0000

    manage race petrol moment general

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import tensorflow as tf
-print(tf.__version__)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
