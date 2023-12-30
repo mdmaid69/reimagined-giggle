2aff20313b7777e6bb9e56a52161a7c3034a1930
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:36:47 +0000

    yours comein judge herself attach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def count_characters(sentence):
-        return len(sentence)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
