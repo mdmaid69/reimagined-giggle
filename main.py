9983554cf2baae2e03ae7e59e5cb1a763671de81
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:48:12 +0000

    principle split makeinto rest level

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
