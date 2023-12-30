2d77a90c7ee268e8447bbccc620de145de2eb096
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:09:02 +0000

    sound event design plain character

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_device(file_name):
+  def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
