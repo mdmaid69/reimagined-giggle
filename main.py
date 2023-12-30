9fec1d66583d56e871d1b7cc560bb872e8a2df49
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:33:49 +0000

    plenty burst idea reply maintain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
