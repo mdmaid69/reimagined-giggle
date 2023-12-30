65d70f58421a15c9265ea4eac53a7ca191af1541
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:45:33 +0000

    will sleep wish extraordinary relation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
