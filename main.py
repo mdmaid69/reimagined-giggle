fbb59928d97705c49f70ef7c6d7f496cd9d490a2
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:11:10 +0000

    opportunity guilty event prize length

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import time
+print(time.time())
