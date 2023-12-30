33f8e4e6b6e307d4f078dfd2a129d99575108165
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:17:46 +0000

    shock niggaacademy frightened feed brain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def is_odd(n):
+        return n % 2 != 0
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
