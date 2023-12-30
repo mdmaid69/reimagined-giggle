bcbe4b50c23b2bf872364c4c47ea0f7d0e66c561
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:28:28 +0000

    sad various concern offer indeed

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_error_function(x):
+        return math.erf(x)
