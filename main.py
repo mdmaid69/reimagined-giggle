89472fdb9c9de73b9ea392e8ab20200cda6de5af
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:34:07 +0000

    twist unit strict tie name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
