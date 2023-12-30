04580ba79f77601fedb0c4efe2d79901b97403ce
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:44:29 +0000

    OKorokay on involve contents getup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def is_even(n):
+        return n % 2 == 0
