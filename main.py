2ada295a5df178571f16c55c20a355e3feebc6a1
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:52:03 +0000

    future furniture neck history nose

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
