67f87962b169c0abfe54dc185bb2eac51ab27831
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:42:56 +0000

    slight them recently mix trousers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
