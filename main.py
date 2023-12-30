3e44f558515586e150d562dfbec3afc803b50ac8
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:09:07 +0000

    used regional walk realize common

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import math
+def calculate_error_function(x):
+        return math.erf(x)
