6328781ee1abc4b700ced8ad5bab1454920376e3
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:31:38 +0000

    bang scream perhaps standard reason

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
