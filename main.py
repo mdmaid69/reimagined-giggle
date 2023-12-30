a9f7d8fe8913a19ee9afba96a0bc54bf2e018977
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:47:58 +0000

    consult exaggerate happy rush doubt

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import array
+def get_array_index(array, item):
+        return array.index(item)
