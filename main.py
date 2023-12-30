93cfba5c5da8204c82e3c5e2738f3388ef4738fa
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:30:53 +0000

    artificial treatment race moral separate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
