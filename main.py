0be55f4571985fd1972e56710c099889529cf59e
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:46:48 +0000

    load 567enemy safe bridge entry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import math
+def calculate_tangent(x):
+        return math.tan(x)
