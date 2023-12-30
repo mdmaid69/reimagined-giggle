a2054ea64888f45ff70526ad725bcc0b0f50d21d
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:27:05 +0000

    swallow match innocent geton clothes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def remove_duplicates(lst):
+        return list(set(lst))
