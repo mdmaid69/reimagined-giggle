17128489920cc529cadd1a67d2f3980ea4323b62
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:17:39 +0000

    or hurt wake storm difference

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
