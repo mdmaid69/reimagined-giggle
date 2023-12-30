112bcf10b992e0c8485f720e2f49a3a247a20592
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:13:03 +0000

    nurse loss separate workupto centre

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def multiply_numbers(x, y):
+        return x * y
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
