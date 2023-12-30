bc4e94ffbd32a8eb74f04da620a9c88c9d7857bc
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:27:14 +0000

    deep high jump model communicate

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 def fibonacci(n):
         a, b = 0, 1
         while a < n:
         print(a, end=" ")
         a, b = b, a+b
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
