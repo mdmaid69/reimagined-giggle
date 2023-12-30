f430a86e8dfe50707569276b4b2bbfa7db22742e
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:58:53 +0000

    attempt keepoff church swimming return

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def count_characters(sentence):
-        return len(sentence)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def convert_to_octal(n):
+        return oct(n)
