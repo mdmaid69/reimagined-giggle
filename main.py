c3b5b2b86fbb7d982bffb5cee7c22e2dd09a3836
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:54:38 +0000

    interruption rob noise recover chat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_area_circle(r):
+        return 3.14 * r**2
