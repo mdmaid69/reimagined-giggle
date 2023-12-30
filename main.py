27a2a2ba9fe8330d30d16d93d0fde122753322ee
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:32:27 +0000

    nigga mine month might convincing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def find_min(lst):
+        return min(lst)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
