d75a34d4a34472d079d929921423e37c496c2727
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:16:58 +0000

    later shock will violent home

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_power(work, time):
+        return work / time
