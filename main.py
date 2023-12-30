9452f18b09ca1f0a0d09513b4f3ee05ecb7d2ae6
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:07:11 +0000

    loan across wall busy label

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
