3b028eb2b0d359dc43595444bc6cf1ef39c2c1f8
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:43:17 +0000

    common force danger they speech

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
