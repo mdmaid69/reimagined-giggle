ae81cdd5611a6b65837efdfdfd48e206f5b59046
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:39:32 +0000

    us characteristic handle affect value

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def calculate_acceleration(speed, time):
+        return speed / time
