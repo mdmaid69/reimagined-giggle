7ac8ec7eabdaedc1f381c6b89f8f87af5accff23
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:52:26 +0000

    hospital gift beyond hold stay

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
