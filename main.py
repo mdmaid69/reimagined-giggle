80810011a9e1d264cc0be0d7b5beccc1b7640e43
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:06:35 +0000

    through material praise tall popular

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_power(work, time):
-        return work / time
+def count_words(sentence):
+        return len(sentence.split())
+print(sum(range(10)))
