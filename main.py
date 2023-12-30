30efb087d309b35a12c6ebd8a21bbc5729ca2b1f
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:27:16 +0000

    and print surface strike ball

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import random
+print(random.randint(0, 100))
