401521cda31a9a5ea089b845b92b67b0f98a1fe1
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:29:13 +0000

    path train read manner giveout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
