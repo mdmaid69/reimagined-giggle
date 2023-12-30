9629393d236c43ff627839c52219d79f2b522024
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:16:25 +0000

    clean find gentle only question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def reverse_array(array):
+        array.reverse()
