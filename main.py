ea8ab296fb9386240037b51fcf82d7eb65e97ef1
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:45:51 +0000

    mysterious mix brave give as

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
