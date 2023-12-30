707406c7ee1653bc76adf2009f7256e71684850a
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:17:23 +0000

    comeover past what juice pleasant

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import collections
+def create_user_string():
+        return collections.UserString()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
