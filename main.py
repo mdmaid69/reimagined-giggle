26a37277aac8553846cc5ea82cdae5b5cb442253
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:32:59 +0000

    friendly path black or suppose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
