bb56a58f038d2a817fad4a97ae9bd82f292ac356
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:12:12 +0000

    freeze drink around pay except

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_area_rectangle(l, w):
+        return l * w
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
