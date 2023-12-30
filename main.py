53290cd232da72345a1e020a7b9b6788aff11a3a
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:17:44 +0000

    raw language central wet before

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
