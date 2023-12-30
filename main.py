f6532901b2cea77af719d9d47073d76917d71179
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:27:10 +0000

    mind authority fix cost 567enemy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
