314cfddbe62508e47918726af2755d7b8bdad3fc
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:52:12 +0000

    rich guide search surprise steam

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
