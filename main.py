7eff0a6ff0543ac9130f60db5446245b5926436a
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:17:25 +0000

    throw protection belief chin buy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
