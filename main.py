7f5405042ca77cb1102f5c54984380ace2cfa04b
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:33:15 +0000

    back excuse without deliver include

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def is_odd(n):
+        return n % 2 != 0
