c7370bbf500036cce9a6cdb757d30c023468aca2
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:26:31 +0000

    test very unhappy past form

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-print([x**2 for x in range(10)])
+text = "Hello, world!"
+print("Characters:", len(text))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
