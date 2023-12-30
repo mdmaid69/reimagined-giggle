d0d3b0267fda1a9840b6e690300ce29b878b041a
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:33:07 +0000

    attempt runinto practise contents too

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_pressure(force, area):
+        return force / area
+def square_number(x):
+        return x**2
