d26542aceee26890432efa5701144fb2e68ebaef
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:20:40 +0000

    stir fear heat shall disappear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
