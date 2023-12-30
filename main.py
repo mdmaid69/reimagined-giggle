a803a663fd751fde66d72bc30ce2aa993395aa80
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:50:54 +0000

    evidence deep so laugh just

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
