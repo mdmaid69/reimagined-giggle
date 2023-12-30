628fdd80528dcc153ed4b516013322e0bf50a318
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:49:27 +0000

    none dull during responsibility period

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
