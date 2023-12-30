a9bfe4bc549e7d553a84308552ab9d5549c97e65
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:48:10 +0000

    study scared scale breakup announce

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import sys
+print(sys.version)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
