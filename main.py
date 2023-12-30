fc57cac908838aab277154a778af2d99d96bbb52
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:15:19 +0000

    quick excuse play certain load

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
