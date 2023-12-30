d0c99333e8512468f727e320eb2a26196666b469
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:41:51 +0000

    whether wild peaceful month bar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def clear_array(array):
+        array *= 0
