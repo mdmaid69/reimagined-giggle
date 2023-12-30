3d582d48c4f274bd98597a01076427992e3bc585
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:36:01 +0000

    frightened best present rent doctor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def convert_to_binary(n):
+        return bin(n)
