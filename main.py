8b5808e73fe877cde4f6bae2d24e2bd16bd60f23
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:37:45 +0000

    season thorough mother sick future

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def find_max(lst):
+        return max(lst)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
