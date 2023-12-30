115fb584faeeff40e33bf29a4e10aec69f5e4d78
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:56:44 +0000

    ignore tool bottom happy Gg

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def calculate_power(work, time):
-        return work / time
+import math
+print(math.pi)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
