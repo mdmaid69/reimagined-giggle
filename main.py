2a198130358ba9152bce10f66cac60b9859250a7
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:43:11 +0000

    defeat confusing careless fair hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
