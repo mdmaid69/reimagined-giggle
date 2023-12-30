fe2ac7596b96861be15707b9b323157a2136ac88
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:38:40 +0000

    complaint exploration ocean we dig

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_unique_words(sentence):
+        return set(sentence.split())
