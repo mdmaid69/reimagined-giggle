3095315fee24d50f88c3a1291f2e1c8121a995b5
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:46:51 +0000

    home custom interesting tired nature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
