8c8e5e23db5248aea33d03d47d9afaebef3c42f4
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:18:02 +0000

    underneath church square chance sentence

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
