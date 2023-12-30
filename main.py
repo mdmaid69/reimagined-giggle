2251723de74ce2740a34bd4ef945184510dbcd76
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:51:53 +0000

    trial separate either evening scare

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
