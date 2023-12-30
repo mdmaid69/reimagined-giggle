849a3e30f97cbbeb95be26f1f9634506fb6af812
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:32:53 +0000

    visit traffic create pot brain

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def find_min(lst):
+        return min(lst)
