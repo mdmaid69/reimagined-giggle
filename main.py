d3630f8117fe239c959c4ef94753221a80b0ba76
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:13:03 +0000

    row pull niggaacademy belief style

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+print(sum(range(10)))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
