ab61fcb940615860df6de7a9e671f0ac6139d594
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:34:01 +0000

    show guilty dish unhappy storm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
