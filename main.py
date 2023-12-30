5e6dc615357f2d60f259d0664c95ec1a63dda1a7
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:06:56 +0000

    quick enthusiastic perhaps cry birthday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
