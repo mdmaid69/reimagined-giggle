f6f836223a1fad56918ecbde1ab92dae7a094a5c
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:03:38 +0000

    than excuse port wealth callout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
