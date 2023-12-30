7b0c65529f2159a76e10332add30959a29839890
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:14:00 +0000

    possibly able wise create but

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  def fibonacci(n):
         return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def convert_to_octal(n):
-        return oct(n)
