22f27efb26305652f762aaecb62f96f7b0e7b254
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:06:32 +0000

    confusing confuse debt clean production

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
