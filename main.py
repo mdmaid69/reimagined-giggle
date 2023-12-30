434610f55dee1871d1742c554f0e174e92e67ed4
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:27:44 +0000

    anyone further conversation repeat time

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
