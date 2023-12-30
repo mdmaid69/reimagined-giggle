929dce7516ec3330586ee9e441ca0623397808cf
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:22:14 +0000

    time apply information onto anywayanyhow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_directory_name(path):
+        return os.path.dirname(path)
