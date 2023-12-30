699d0a41d0faeed8ab1a666adb64f7d821194920
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:05:30 +0000

    rich hardly real try conduct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
 n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
