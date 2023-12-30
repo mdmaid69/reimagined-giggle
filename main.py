7b47bd9bae440532c8055efe05dc12b5f7e6135b
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:53:42 +0000

    cross steal suggestion shut subject

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
