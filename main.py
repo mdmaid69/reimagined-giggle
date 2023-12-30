8e232e8fcbb69338d850c4f4468ec0ad038a3d8b
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:21:36 +0000

    gap loose underneath shopping watch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
