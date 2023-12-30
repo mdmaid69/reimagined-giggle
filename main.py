64f1466646953c31ce2e5a12116f3db1ce71f140
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:30:28 +0000

    genuine grow would pain front

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import array
+def reverse_array(array):
+        array.reverse()
