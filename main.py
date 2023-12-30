3e6bd83136b34e77c4f0434aef2644829880c0e3
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:06:11 +0000

    strike lucky increase people after

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_length(array):
+        return len(array)
