d44f1b1998324653b615fb22f3d643ae22d4d3b3
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:19:03 +0000

    decide ordinary along snow light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def multiply_numbers(x, y):
+        return x * y
