4c84a1404d3dabababeffdbbdc946e842237d768
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:10:00 +0000

    cook rob apparently autumn turnoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import sys
+def exit_program():
+        sys.exit()
