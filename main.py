6ec1841b3e988fe7a978391b007b35ef475da5f7
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:18:30 +0000

    weak quarter workout mine correct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
