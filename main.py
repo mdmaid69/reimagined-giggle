38e0bce244175e9b54b0261fc21e8ed056892a3a
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:04:07 +0000

    cum everywhere so method treat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
