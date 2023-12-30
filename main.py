389314f22f87e2136a0f971dc8a2c42539405750
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:55:06 +0000

    nigga community allow hand adjective

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import collections
+def create_priority_queue():
+        return collections.deque()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
