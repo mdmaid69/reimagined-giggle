f0dfc49fcaf191f6077de32b821ff4d5d38a80cb
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:29:10 +0000

    until makeup demand partner circumstance

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
