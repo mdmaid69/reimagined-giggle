e201145192f92e59417567dc39e76cd1a90375c4
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:19:48 +0000

    demonstrate fun breath generous peaceful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
