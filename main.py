2f1243b3e89286146839e246631ab0b8d623016f
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:02:09 +0000

    upset inquire spin course obvious

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def reverse_string(s):
+        return s[::-1]
+  def reverse_list(lst):
+        return lst[::-1]
