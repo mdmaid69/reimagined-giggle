48876899cba0ab54f2dcae1c5212c6ab6a90dfb3
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:27:31 +0000

    hope train fence survive right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
