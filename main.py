da4b3e47fece6cfd6452d2635f8274e63f34a0ec
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:08:21 +0000

    convinced shop vanish female brother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def reverse_list(lst):
+        return lst[::-1]
