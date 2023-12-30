ebf5a1bd94a2d7faca9ff16b187de88e23e86458
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:29:01 +0000

    creature needle so appearance dance

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def cube_number(x):
-        return x**3
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
