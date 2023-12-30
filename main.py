e7461b1764ac6206c6f59279c9c576d9b37e6b66
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:48:17 +0000

    criticism to kick Vv development

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import sys
+print(sys.version)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
