ed61680341423dd63a7041e5f6dbf183836e50d0
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:15:11 +0000

    fit market as error 564encourage

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
