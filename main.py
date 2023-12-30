8daf43e47c619502de6a5beddac421ec5a6ab543
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:35:49 +0000

    Ee until perfect sweet criticize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def square_number(x):
+        return x**2
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
