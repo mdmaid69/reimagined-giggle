d6f7f98dc72b900d5a26396f44dd5bf6968eebac
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:31:51 +0000

    software laugh hair wheel trip

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import sys
-print(sys.version)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
