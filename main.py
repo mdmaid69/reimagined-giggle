d637a617e447616a0a0889763e0ee6edae129150
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:57:13 +0000

    willing pure welcome cause at

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
