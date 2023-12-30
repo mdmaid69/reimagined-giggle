26d9884f41bc5472c94d06052db2b8a9b1cfb886
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:21:12 +0000

    push whether offer outside handle

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_power(work, time):
-        return work / time
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
