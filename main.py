f3c64cbd179117b2c01a0cc89023bb2d824f46b9
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:17:44 +0000

    thirsty male false out dream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
