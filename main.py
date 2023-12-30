67e2d7269da95b4881d5544fcebcffced6e3f06c
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:33:41 +0000

    stop corner advanced lead mail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_array_as_set(array):
-        return set(array)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import sys
+def print_python_version():
+        print(sys.version)
