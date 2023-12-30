de03b507ea797b60ce9cef6a5cdae185fd5a8a29
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:30:58 +0000

    once everyone whoever nowhere also

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
