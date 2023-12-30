b553894aa9902bad15b305894276cd49f608b649
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:06:40 +0000

    intend connection hurry attach ready

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
