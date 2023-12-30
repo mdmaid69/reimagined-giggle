512beb6761e9735f2615bf1305d7149f58067d6f
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:50:23 +0000

    frame woman refuse suggestion why

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_user_dict():
+        return collections.UserDict()
