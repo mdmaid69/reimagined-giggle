40eae710a39b8066aa6ae26a70e23f2da9f43e6d
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:05:25 +0000

    couple intelligent memory rain exist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
