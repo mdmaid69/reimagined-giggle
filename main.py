30d1490173d502d87e2a4e7def7fcf024193b0a4
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:38:04 +0000

    join soon laugh pick firm

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def sort_list(lst):
+        return sorted(lst)
