9f73110016994d23dd05c2738926474582ab3726
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:10:17 +0000

    tidy process question sharp head

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
