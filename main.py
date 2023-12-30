0915bdd7f1d6c833e32c5304d34ca02653bda1b1
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:01:39 +0000

    positive may out strong snow

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
