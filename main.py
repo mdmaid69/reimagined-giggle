17cdb5f69fe40c7cbe1e517d017ac7368cd14d0d
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:35:46 +0000

    frighten sharp advise check range

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
