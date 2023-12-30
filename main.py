69c488fd0f85b6ee0b38f5c25a7a5fab92491cd0
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:08:37 +0000

    consider deep difficult runinto load

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
