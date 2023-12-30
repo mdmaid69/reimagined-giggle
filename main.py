9739b4a59961e47f0fddabe34592c05886773a5d
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:22:03 +0000

    rest surprised rise tour whom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 text = "Hello, world!"
-print("Uppercase:", text.upper())
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+print("Reversed:", text[::-1])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
