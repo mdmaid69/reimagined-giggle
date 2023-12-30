ae3bb02641cd7c365369ab95903ae9dfda738bf4
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:15:15 +0000

    bottom not takeaway head supply

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print(sum(range(10)))
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
