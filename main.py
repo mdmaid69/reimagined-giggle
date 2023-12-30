765278b411daeb2791844f2849ee93f885acb66d
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:28:45 +0000

    run market argue cheat kind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
