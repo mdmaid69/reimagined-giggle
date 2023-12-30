2c2ad6fbfe5ffce1ac1c28a1976661d523296db2
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:32:20 +0000

    active frighten skirt action least

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
