3110c7f368a3f732af7386fed6b6c39af9758eb1
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:20:41 +0000

    natural rest keepfrom intend invitation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
