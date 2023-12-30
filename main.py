d35ebe9a95f8ffa402908443110ef07b0ff36915
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:17:51 +0000

    pride zero mouth slide relative

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
