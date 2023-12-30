fc235834822cd0f7c1eb212b72a70ff5344279e7
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:10:23 +0000

    willing record trust point crazy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
