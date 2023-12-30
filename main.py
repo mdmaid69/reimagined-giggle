8085e71fb5d1695c09df43ae3a780713c8997384
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:34:57 +0000

    turn find centre climb step

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
