75c6715efd70c48eb19c6fe9027ca75e2e7cc3c1
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:25:19 +0000

    concentrate deaf agree react check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
