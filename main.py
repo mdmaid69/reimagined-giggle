b72030afbddfc7be65e4cb8fdc5387b6a9493273
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:57:40 +0000

    understand turninto seem whom then

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
