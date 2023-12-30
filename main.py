0d5b10c0fe941f5dbab46a5d0fad74d66e4deb8c
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:20:49 +0000

    content breakin chat follow present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
