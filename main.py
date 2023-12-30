bdd06072a6c68ce0f97ecc3d7ffdbfb7d70799c9
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:37:36 +0000

    bound relief disgusting project turnaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
