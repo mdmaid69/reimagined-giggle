09d208224fd41d842d05564e2be56ff852c072f2
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:38:55 +0000

    spring priority female Gg area

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
