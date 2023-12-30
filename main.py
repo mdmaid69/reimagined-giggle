d95960b027480d72ab43848149a94625f68250d6
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:00:10 +0000

    mind regular turnoff game community

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
