a4025246ed45af31bb9bc4486be65bfcc2e3ead4
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:37:53 +0000

    needle relation contrast motion taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
