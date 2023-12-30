e587061c34910848a681d68fc3f71ecd37ccf03d
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:10:35 +0000

    convince confusing want picture suggestion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+def exit_program():
+        sys.exit()
