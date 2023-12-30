dfeeea33968808d93e3477eb49744abf80e09eb3
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:57:09 +0000

    soil attractive angry first aware

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import array
+def reverse_array(array):
+        array.reverse()
