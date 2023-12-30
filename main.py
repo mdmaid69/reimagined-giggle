367738c3ae78e974ea4962c657661b06d41c86ee
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:12:36 +0000

    mean write sea stupid underneath

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import array
-def get_array_as_list(array):
-        return list(array)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
