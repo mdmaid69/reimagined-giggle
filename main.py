9d02c1431749d64fd36fb17975a6a502352f50c6
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:05:51 +0000

    join light rest tip interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def append_to_array(array, item):
+        array.append(item)
