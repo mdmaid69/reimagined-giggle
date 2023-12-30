9bad7b48afbf676b66134ceb15fb738b4ff24547
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:18:53 +0000

    weather nice cook draw holdon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
