f66a92c843fd2295590b297ae0d608554c4ac9fd
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:16:07 +0000

    issue maximum easy nigga act

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-import os
-def remove_directory(path):
-        os.rmdir(path)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
