a999759e47071f4ef2f114a3999ac90f24242c16
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:06:48 +0000

    combination test means anywhere maybe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
+  def get_file_permissions(file_name):
         return os.stat(file_name).st_mode
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
