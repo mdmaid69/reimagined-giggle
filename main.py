0136cb9a6443734b7597a22b9557ab75223a8f99
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:19:22 +0000

    invite demand lunch many ago

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
