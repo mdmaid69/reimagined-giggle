dc9994c71660ff68f767e7e8e72e8f94a5777ad3
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:43:42 +0000

    trap bus comeover peace only

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import time
-def get_current_time():
-        return time.time()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
