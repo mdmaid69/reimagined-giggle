c06021108561987048a722ce618bfd8b22a82143
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:40:18 +0000

    law please demand comein appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
