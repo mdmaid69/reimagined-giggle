12baa21f127a04cd0bcbbb69f6bf5e5bb64e781a
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:10:44 +0000

    correction rough door shall scene

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_area_circle(r):
+        return 3.14 * r**2
