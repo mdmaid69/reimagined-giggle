f15c9af23324466451ec043a39e6c46691a85abf
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:28:55 +0000

    truth recently weather expression relationship

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
