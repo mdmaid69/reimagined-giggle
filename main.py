ad7ca013d31ad302917541651275e844e1fa7051
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:41:22 +0000

    big love across ring sure

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def find_max(lst):
+        return max(lst)
+import datetime
+def get_today_date():
+        return datetime.date.today()
