044b2e8ae984e7a5670935a103b816146ded701c
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:05:13 +0000

    learn real check goback(to) business

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
