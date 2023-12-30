6a4912723467071f00b54ae6659832b4fa94304b
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:43:21 +0000

    feeling chin tidy anxious off

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+text = "Hello, world!"
+print("Reversed:", text[::-1])
