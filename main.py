e6b7574eb7a1bff04709b537d770e6602b2216ed
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:01:01 +0000

    tense crisis careless attitude spend

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import threading
+def greet(name):
+        print(f"Hello, {name}!")
+import unittest
 
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
