8f2e33b8c6d11ad996e859bd3c442783ea3f3263
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:04:28 +0000

    accidental task frightened when emphasis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
