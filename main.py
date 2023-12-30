f6844e5a9ffcade40a9c95ac9be88680578059e5
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:13:36 +0000

    correct neat comeoff discover shoulder

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def calculate_speed(distance, time):
-        return distance / time
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
