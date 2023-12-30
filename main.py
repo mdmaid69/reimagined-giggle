c37bfe249c800bcaad789183219c82c1122365f6
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:12:41 +0000

    beside touch birth nearly present

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def convert_to_octal(n):
+        return oct(n)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
