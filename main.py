7eb55880519ad6543040083c7026a88c0a2214e0
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:56:13 +0000

    kind hide return reserve turnup

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import time
-print(time.time())
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
