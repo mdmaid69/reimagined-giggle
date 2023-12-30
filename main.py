d3849c8ef70e3fa440a4b4d0a386512f53ca85ab
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:01:30 +0000

    middle his thinkover characteristic minute

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
