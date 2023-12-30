9e9af671a14c688febf79e55fb5cb278dcfa9091
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:08:12 +0000

    getoff majority another mistake something

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def add_numbers(x, y):
+        return x + y
+import json
+print(json.dumps({"name": "John", "age": 30}))
