e908d98abc58de34227fbcdbf2c208276c2b137c
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:51:44 +0000

    curious fact defeat fair almost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
