6b052447f5cdecad8ff0123c52e16d7196c13992
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:26:38 +0000

    however field butter limit under

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
