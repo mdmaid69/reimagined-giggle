3d07f5785ad5888f8140b9cd765afc8bb721f9a4
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:47:07 +0000

    seem image command Ff copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
