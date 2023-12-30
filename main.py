aeb353336480d32adc925d6bf1e7af95c4082b46
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:37:12 +0000

    communication immediately possible sleep easy

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
