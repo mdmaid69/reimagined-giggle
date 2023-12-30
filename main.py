933aeb3ad88f59422e29b751e9f31671e1e484ae
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:36:15 +0000

    unless minor period cruel why

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import collections
-def create_priority_queue():
-        return collections.deque()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
