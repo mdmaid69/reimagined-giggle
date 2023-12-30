fdee922356542f41721995f58e12f9e06d0896fe
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:29:52 +0000

    fun act rare relative activity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_power(work, time):
+        return work / time
