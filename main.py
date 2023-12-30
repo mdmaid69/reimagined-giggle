6fa8e2f47eba47c8c90f7dfa137f6ca7f6bdf80d
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:35:58 +0000

    protest fix right base exercise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
