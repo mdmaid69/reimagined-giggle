f5ed18a96b11480dee2ed5b35a2c2b7400ac4d16
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:07:20 +0000

    satisfied ready offer evening the

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def find_max(lst):
+        return max(lst)
