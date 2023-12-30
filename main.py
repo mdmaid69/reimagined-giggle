caef01b8d0d11ab0d11d726d9e5e09089c9f5346
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:51:50 +0000

    true busy 567enemy step appeal

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import sys
-def print_python_version():
-        print(sys.version)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
