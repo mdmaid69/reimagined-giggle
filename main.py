2eff0fc02a9ba4e89e78f9436bb8b3c8934613b7
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:57:02 +0000

    neat even this into equal

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
