0d0547a0bea3d658b66b823a0a6d18519522e1db
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:02:11 +0000

    medium whoever answer bury defeat

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
