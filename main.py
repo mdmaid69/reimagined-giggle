8af094e3571884f3b425886b600557e2beacbf6a
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:02:39 +0000

    picture movement runaway sore notice

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_volume(length, width, height):
-        return length * width * height
+  def reverse_list(lst):
+        return lst[::-1]
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
