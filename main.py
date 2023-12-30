06b02fb12db554e7e6c35da82991abfc3af4c42b
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:52:03 +0000

    turndown shut safety effect will

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  def square_number(x):
+        return x**2
