9350c5c8da9a2486373be7fd4a92ac9ae4d4972f
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:45:39 +0000

    breakdown permission question thank base

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 def count_characters(sentence):
         return len(sentence)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
