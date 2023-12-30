3628be32d22f82366b5a20b6bbe6f7af727124cb
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:59:35 +0000

    gentle organize behind bag will

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_power(work, time):
-        return work / time
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import logging
+def log_message(message):
+        logging.info(message)
