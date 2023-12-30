dc20e632f800954c81c36d9b08a0b7067348bfe3
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:09:21 +0000

    me point gentle previous common

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def convert_to_octal(n):
+        return oct(n)
+import tensorflow as tf
+print(tf.__version__)
