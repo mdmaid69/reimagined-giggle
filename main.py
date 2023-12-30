d2f9cf91facb49cd8e1755ec956c1a22b1041091
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:19:13 +0000

    important hungry why acceptable disagree

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-print([x**2 for x in range(10)])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
