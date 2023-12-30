46977797acff23f0ff4c270b97e351e7fc120a2a
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:10:22 +0000

    custom favour listenfor ready storm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_work(force, distance):
+        return force * distance
