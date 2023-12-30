058732ae92cfdfe83e23b522ad65d0489883e1bb
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:15:43 +0000

    offend this he comeout game

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import json
+def convert_to_json(data):
+        return json.dumps(data)
