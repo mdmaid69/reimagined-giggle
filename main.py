3d7b1f10e3457ccb0d245efb5ffd16a11e70c2a4
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:58:08 +0000

    smell cold dick slice steep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+x = 10
+y = 20
+print("Sum:", x + y)
