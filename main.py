3bfff43a89caa0611c145277eef3822eb6017ec5
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:40:37 +0000

    change wet target satisfactory little

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
