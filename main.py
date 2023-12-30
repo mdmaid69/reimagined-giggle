44342cf135f113a6f2016f0f583747e8117812a0
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:28:37 +0000

    introduce low think respect gift

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
