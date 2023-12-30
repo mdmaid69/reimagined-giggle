76338c4b8e2b4262ad829edb0afa9a8b866f9e2e
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:50:44 +0000

    claim active comeoff gather wash

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def read_csv_file(filename):
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+def load_json(filename):
         with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_as_list(array):
-        return list(array)
+        return json.load(f)
