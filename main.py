fed7d88b3e2abec24f5d5b10c3db404c630dd6f7
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:05:21 +0000

    afraid as notice shrink sight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
