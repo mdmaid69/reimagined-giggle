b4108fd92df14dc543b8dd82ba971238ed14cd07
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:29:43 +0000

    age Qq appear against pour

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_set(array):
-        return set(array)
+def cube_number(x):
+        return x**3
+import numpy as np
+print(np.array([1, 2, 3]))
