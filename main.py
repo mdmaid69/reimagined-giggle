2b6bf33c2c445908898df67ee22c47a68739e955
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:00:56 +0000

    question no operation wrong giveout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def subtract_numbers(x, y):
-        return x - y
+import getpass
+def get_username():
+        return getpass.getuser()
+import os
+def get_current_working_directory():
+        return os.getcwd()
