ead243562029fdc6465bd6d8ada7fbac70e23bb1
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:46:08 +0000

    justice week contrast intend free

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
