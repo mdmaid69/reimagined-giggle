546aeb7d80272d89eec9075c560549a69bab2d3a
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:06:08 +0000

    room art profit child slip

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import time
+def get_current_time():
+        return time.time()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
