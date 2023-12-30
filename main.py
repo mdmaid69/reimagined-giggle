452cb9a35a5e3f8c00d7691212d842ddf97870a1
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:41:14 +0000

    question fairly attempt system admire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
