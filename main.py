6c8e00bba13269cdcea4779ae39ce5ce8a06723c
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:14:02 +0000

    force classroom big but cruel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
