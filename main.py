534da710ca1aa9b59a8ff8b6630fdf1095e22b1a
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:27:07 +0000

    owe normal exciting substance imagine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
