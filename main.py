f31e44cfddf91e018aae80284224e3e0beb6f503
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:52:17 +0000

    habit broad under complete extremely

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
