020b0da88679fc8619d6befd6de0d0f820b94781
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:07:38 +0000

    crisis better where stage slide

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
