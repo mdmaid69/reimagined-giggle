09355b9f4f4b157377102420a95f62022d1ddecb
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:24:42 +0000

    tax safety operation range me

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_work(force, distance):
+        return force * distance
