f25438bb4c9aba553cd08b85be865a80554f872b
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:29:52 +0000

    hand present morning well occasion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
