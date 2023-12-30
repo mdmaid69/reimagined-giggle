a0cbe5f583dc6271586221d2761424d8f7dea310
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:34:11 +0000

    contrast understand such plenty ill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_current_directory():
+        return os.getcwd()
