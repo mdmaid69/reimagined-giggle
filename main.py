b21497f6adf7ab8613f9429724d071d1417a68e7
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:57:47 +0000

    major opinion able sting recover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
