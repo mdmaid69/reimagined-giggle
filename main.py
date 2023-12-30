10e80abbc24b3fa167432e96fec64f52ab4cbb78
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:43:46 +0000

    pen strong connection shake class

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import platform
+def get_python_version():
+        return platform.python_version()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
