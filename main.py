c076b2927b5c1dfcc56c9a34a2ec06dff6934ebd
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:21:14 +0000

    slow keen trip important clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_min(numbers):
+        return min(numbers)
