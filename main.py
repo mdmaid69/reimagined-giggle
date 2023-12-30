8ecce0ffc56942eec36a5e5c94d95474bd70081e
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:01:11 +0000

    fix though meal gate regular

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
