4d74dedfa8ca1c881c9de2c7cd43c571e5fd2810
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:23:40 +0000

    drive understand ocean sound shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
