c807b4443a64286f10eb6d635919c470fa20bfbb
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:39:30 +0000

    sign shame danger priority bringback

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import platform
+def get_os_info():
+        return platform.uname()
