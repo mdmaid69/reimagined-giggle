86e39d0554a08a36b807789560762f5896f816a0
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:03:57 +0000

    chat 568energy share effect inquire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
