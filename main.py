5fe6dbf59f05d3142e96946a14db00020ebe35ef
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:00:07 +0000

    compare hand subject slightly difficult

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
