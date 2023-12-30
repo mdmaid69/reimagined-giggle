6d670140df76d1215d800f220e9f188b57c88128
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:12:42 +0000

    closed some grade bunch crisis

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
