0dfb6397965dff6f03b2b563a0078242470e82a4
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:22:22 +0000

    back hold weekend welcome you

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_pressure(force, area):
+        return force / area
