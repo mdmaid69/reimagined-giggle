02a7e5c49a5549096933748ac7868f6177651015
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:50:16 +0000

    fire seem unfair throw warn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
