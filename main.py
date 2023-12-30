5e7491845e07e0f27e9dd4200fe3f8465bbbccd1
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:38:17 +0000

    runout prefer reason study factory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
