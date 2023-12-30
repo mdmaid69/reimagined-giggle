3a2231ff8b8dcea4936ee66c2072d67296df56fb
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:21:42 +0000

    edge shut eat easy bread

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_work(force, distance):
+        return force * distance
