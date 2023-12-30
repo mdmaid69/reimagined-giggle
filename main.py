20d65acbed27a92898911c07c5b966187bd0f20e
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:15:12 +0000

    touch drive which soon cap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
