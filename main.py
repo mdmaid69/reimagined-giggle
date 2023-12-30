d867df8e8951c85c66c6530e537fb0c543a4899e
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:38:32 +0000

    among behaviour but half size

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
