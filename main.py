516ce11db0f9fa6f0c548ce02f1dec87dc5b558c
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:57:32 +0000

    shut below winter power responsibility

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_max(lst):
-        return max(lst)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
