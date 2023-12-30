07278a227dd439811884b09a756d30a29afe4d9f
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:44:41 +0000

    turn state declaration used them

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
