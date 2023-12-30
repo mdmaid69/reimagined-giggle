c5d7b531d498d2b0fc1594e4637e9d7ad55ae94f
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:48:59 +0000

    close interrupt position shade direct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def is_odd(n):
-        return n % 2 != 0
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def calculate_work(force, distance):
+        return force * distance
