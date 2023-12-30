e32c0d691c997a9f1253efcf2da00d307c901453
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:24:45 +0000

    profit waste runinto dangerous kind

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-  def subtract_numbers(x, y):
-        return x - y
+def count_words(sentence):
+        return len(sentence.split())
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
