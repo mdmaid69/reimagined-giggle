dc80cac2e905a8bd1f790e22ff3539319f2510e5
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:08:23 +0000

    settle down phone dig practise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
