2a20b859932d4f1b91e44b81aab604988b845443
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:59:23 +0000

    cook rub little difficulty funny

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def delete_file(file_name):
+        os.remove(file_name)
