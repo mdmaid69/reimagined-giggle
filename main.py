2581f10e9c755a44c8fdd66bfd96c6782c14691c
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:47:09 +0000

    forgive below dry point apparently

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
