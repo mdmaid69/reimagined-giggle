7a8e8060046aa0d505af8e128a64953b78f1777e
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:29:34 +0000

    appeal rest sort earn effort

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import array
-def get_array_item(array, i):
-        return array[i]
+def reverse_array(array):
+        array.reverse()
