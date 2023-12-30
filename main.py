1eb3661939b82e1248e446e67c049c7c72769189
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:16:26 +0000

    dance young takeover game accommodation

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
