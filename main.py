3ce5ab18bd9a6561e511e623f51d8435c77d419a
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:28:35 +0000

    answer emergency fix like constantly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
