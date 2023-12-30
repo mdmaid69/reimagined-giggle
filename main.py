ddf98d43761f8b679faf5fbe63971ce85ba883dc
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:28:10 +0000

    satisfied bag paper repeat bit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
