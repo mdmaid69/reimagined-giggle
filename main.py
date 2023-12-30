99c627702dc5c29faee25accd5ab9a004442ab22
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:18:42 +0000

    goout impossible separate importance now

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
