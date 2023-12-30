0f805b21cc44bd8a33d6afe66592e91a452229dc
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:19:26 +0000

    identify square line rest mood

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
