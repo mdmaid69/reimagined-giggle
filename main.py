7bb1430b5d924243031fe9e9b1ea5cf8755d4765
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:41:43 +0000

    search short weigh fix 568energy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import array
+def get_array_item(array, i):
+        return array[i]
