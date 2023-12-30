cf87586537a3c3a3035519b66725e8a76366600d
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:22:11 +0000

    lend punishment sea tape mean

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import os
-print(os.getcwd())
+  def get_base_name(path):
+        return os.path.basename(path)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
