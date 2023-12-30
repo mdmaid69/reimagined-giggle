167b75063904ccbf2aa0f0d5d8a3e10c2ce7df06
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:00:02 +0000

    day yourself battery force gather

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import collections
-def create_priority_queue():
-        return collections.deque()
+def convert_to_hex(n):
+        return hex(n)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
