e9aaaa25740f645c2d2a1cfcf52b3728b35c49ae
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:55:29 +0000

    demand heat side convenient lay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
 import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def create_user_list():
+        return collections.UserList()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
