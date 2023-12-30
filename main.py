147d28732121f8189cf3ce8cf24ac9f05c0b3916
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:07:30 +0000

    degree workupto habit behaviour treat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
