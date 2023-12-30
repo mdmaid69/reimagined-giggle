f0abb6dba5ee1fa4b99b25fdc97a654b5063e95c
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:40:01 +0000

    information matter grow niggaacademy total

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_user_dict():
+        return collections.UserDict()
