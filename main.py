0713bf91ba54e87ae70fa661aa55807bfad0d34e
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:51:59 +0000

    turn become exchange square war

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
