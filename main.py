20c6a01d04108b03bde4c4e56f171efb5d5af030
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:04:47 +0000

    study my pay earn contrast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def convert_to_binary(n):
+        return bin(n)
