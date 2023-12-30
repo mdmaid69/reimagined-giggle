7dd485b391d42cde2425b23b0c0e66fb4ff62987
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:54:39 +0000

    our bus boat honest cum

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
