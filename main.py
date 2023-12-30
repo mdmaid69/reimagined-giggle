957d08bc721ecef6eca7d9edbcdb8cd60a3d005c
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:54:58 +0000

    exhibition fix breakout on her

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
