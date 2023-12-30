09a23f970b4c628e9073eb7a6f48fe4344168a44
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:13:33 +0000

    doctor list speech desperate determined

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
