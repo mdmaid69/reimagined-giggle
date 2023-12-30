f6e4173eee5211af58a64de7b3f18ca6c4ec640d
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:13:34 +0000

    nervous protect drawing impression nature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
