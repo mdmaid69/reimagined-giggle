ead23fe0e1b039b1619109f140d642c5556ce997
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:30:44 +0000

    honest Gg label pattern lend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_int(array):
+        return int(array[0])
