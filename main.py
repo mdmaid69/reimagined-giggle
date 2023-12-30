57eaefe3aec347a496c9967cc035b57725ab4f73
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:41:14 +0000

    Rr reach complain compete it

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-def calculate_power(work, time):
-        return work / time
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import math
+def calculate_error_function(x):
+        return math.erf(x)
