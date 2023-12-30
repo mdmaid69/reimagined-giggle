9a14ad625d751e7c3cb8c76212ad43c2c6fcc599
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:25:42 +0000

    nowhere sometimes down accidentally compare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import array
+def get_array_as_int(array):
+        return int(array[0])
