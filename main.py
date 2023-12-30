bf9fe5acd877caa39eb6299373468a73210eca8e
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:30:36 +0000

    hospital gradually exchange defeat under

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def sort_numbers(numbers):
+        return sorted(numbers)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
