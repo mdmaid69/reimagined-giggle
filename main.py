ba68236c6e7af2193a3b63e7a7baebcea890de77
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:16:17 +0000

    bag minimum birthday ring deliberate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def cube_number(x):
+        return x**3
