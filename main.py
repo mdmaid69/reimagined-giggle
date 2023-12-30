36898270bc8aad0a3bc030cf69b9d29635ae6570
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:02:55 +0000

    wonder couple forget equal deliberately

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
