6db90f03d1842d2165d5e06ebb4efb8dec737017
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:09:52 +0000

    573entertainment imagine raw right increase

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+i = 0
+while i < 5:
+        print(i)
+        i += 1
