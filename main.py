8a72dddd2e3664675c84bc5e45348d9adb13b1c9
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:03:09 +0000

    process late estimate supply thorough

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def square_number(x):
+        return x**2
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
