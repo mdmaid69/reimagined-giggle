2ae570f9fc02b1b905940955c8fe6208b44e8ad3
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:20:53 +0000

    whole arrange furniture modern before

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
