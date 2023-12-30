b8b9a45d3f41dcab6016475ba1444b0a0daf3ba4
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:02:07 +0000

    who maybe underneath late best

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
