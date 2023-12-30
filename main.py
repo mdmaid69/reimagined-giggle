62dfa0b69f27d59f787be32230c7104b9e1f8b79
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:08:07 +0000

    pure sight partner ability health

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
