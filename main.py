ef994abc1c6fcbd18a394c7bd55d7411092a0efc
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:26:29 +0000

    runinto into listen train smooth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
