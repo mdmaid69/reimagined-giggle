499cae81931bb3d6429ffbbe8875dac6d7262d03
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:01:05 +0000

    knowledge loan everybody usual regularly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
