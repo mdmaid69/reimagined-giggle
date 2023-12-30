b65abffbb7ace671430232a89c272fd66dfa1ab5
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:04:36 +0000

    invitation admire shine nature tragedy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
