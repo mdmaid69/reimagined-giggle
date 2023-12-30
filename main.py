28f634ffe6d800a5dddf653476bbdc267a204f78
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:48:01 +0000

    rare quit float sort arrive

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import os
-print(os.getcwd())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
