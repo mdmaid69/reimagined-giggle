bc4d5dd2ebe27d0e01294aabe471ab5442d1f4f4
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:13:10 +0000

    want its practical stamp herself

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-for i in range(5):
-        print(i)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
