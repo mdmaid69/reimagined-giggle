e5c3f4306ce0353db5d73ecbb29cb36b065a11f5
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:35:48 +0000

    difficult steep answer disappear bear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_power(work, time):
-        return work / time
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
