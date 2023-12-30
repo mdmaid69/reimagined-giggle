8d49fc01f3ec3d40b7ed3f9b0d5f690df6e56482
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:06:28 +0000

    trust subject join may hit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
