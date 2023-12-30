26e3bcaa8da9c03e601eb51c7a2adee6ac477ca7
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:21:18 +0000

    drink stage involve tip turninto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
