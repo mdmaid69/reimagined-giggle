a4dded014aa1859f9ef4aa86922bbd3fc9688113
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:00:42 +0000

    this excuse any woman moment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
