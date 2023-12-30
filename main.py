1e2cb8445f332c53cc6887cdf39e3712c33fee89
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:15:04 +0000

    flow book cinema protest fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
