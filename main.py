e06b219e71d1d4f9149cd2ba36560cfc05c286e4
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:53:21 +0000

    or police marry thinkthrough date

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
