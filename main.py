3f8a7ea6cb7af9d21a93190deced057c9b7e4b3a
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:23:16 +0000

    position statement mark shame treat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
