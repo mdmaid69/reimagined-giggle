6c93fb46ff7e862f5ea0002bf543299d7ecba82f
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:06:39 +0000

    swing stay needle wall shelter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def is_palindrome(s):
         return s == s[::-1]
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
