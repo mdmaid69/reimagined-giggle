ef6fdf00d2c897283c2e03dbbac8c5403d065f62
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:51:14 +0000

    whistle child first length border

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
