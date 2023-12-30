c6690109d5eb87fad55385dfeb8eeeb27e7ac2f1
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:49:38 +0000

    wander post thing impossible underneath

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
