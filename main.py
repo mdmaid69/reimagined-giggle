7c80e730aa4a39d57e83ba87ca1528d0bd466190
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:50:00 +0000

    task order separate live slight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
