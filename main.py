56d035fcab4fcf90c8d45755b4a8da69f1df7004
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:43:29 +0000

    disturbing unlikely workupto sweep clear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
