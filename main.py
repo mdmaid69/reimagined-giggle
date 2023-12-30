f3fdafb3f5a3bd09cb7349ecb353495a0edbd302
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:24:36 +0000

    inquire content blame sign die

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def delete_file(file_name):
+        os.remove(file_name)
