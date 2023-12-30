4f8da94a1d3c59be80bf34a5a1e46d25e4063242
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:15:02 +0000

    tape glass claim operate about

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def sort_numbers(numbers):
-        return sorted(numbers)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def count_words(sentence):
+        return len(sentence.split())
