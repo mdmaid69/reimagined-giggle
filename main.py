98806e0a6bc938a4e51d13ae1bb0998a868d2b69
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:38:51 +0000

    wonder develop temper breakin grade

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
