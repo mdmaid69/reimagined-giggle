56f9567782acb0f9e29201a0365cd7511a97f2b8
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:27:28 +0000

    church neck find action new

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
