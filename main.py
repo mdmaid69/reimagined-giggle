8094289c39dc8954e4c041e092eb9088b5bafc45
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:08:38 +0000

    water border roll wide touch

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
