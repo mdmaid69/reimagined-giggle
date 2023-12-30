c935479de800ec4a121af31c660b8a59a8d1ada7
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:52:51 +0000

    computer word movement minimum hers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
