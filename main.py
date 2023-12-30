e73e6296f114fdea563a29c85a07bccd299d6971
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:29:45 +0000

    advice dig owe strong beyond

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
