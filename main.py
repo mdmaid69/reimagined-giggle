296eda0056548bfe690ed99fda0255b16d9255b4
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:29:32 +0000

    storm slip game Vv total

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import tensorflow as tf
-print(tf.__version__)
-def convert_to_octal(n):
-        return oct(n)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import array
+def get_array_item(array, i):
+        return array[i]
