6ff6484ae197721048989283d5b91ca11c06c0a3
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:48:19 +0000

    behind worry bet size show

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
