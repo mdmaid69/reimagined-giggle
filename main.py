3bda3bf38cc2e7f8c67304ae373e57b3d8c53e52
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:46:27 +0000

    sister dick party base foreign

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def add_numbers(a, b):
-        return a + b
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
