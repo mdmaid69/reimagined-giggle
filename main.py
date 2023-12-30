1ad4fd14a63039a2780f0d804209eb90cf8ce85d
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:52:34 +0000

    score ago callback fierce season

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def greet(name):
-        print(f"Hello, {name}!")
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
