fd1dd9b4526803e8fdfd13a816b09ab1f183ccbe
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:27:46 +0000

    house between round dance convinced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
