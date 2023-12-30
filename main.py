3be921c49a4747086bf186300d53c7bcd887e1a6
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:03:00 +0000

    blame tragedy bright rest work

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import re
+print(re.match("h.*o", "hello world"))
+  def add_numbers(x, y):
+        return x + y
