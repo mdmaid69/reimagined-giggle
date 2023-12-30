9efc6cb4acb39413d9808238d8a6d2c9f96fac99
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:24:26 +0000

    back cross conduct allow fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
