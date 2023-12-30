385d136908a756232e2965207ee472ab954571dc
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:04:45 +0000

    average burn lunch murder fence

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
