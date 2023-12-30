f771363b9d130632e7e00e40070851b55c004d03
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:49:26 +0000

    study lean ring outside face

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def multiply_numbers(x, y):
+        return x * y
