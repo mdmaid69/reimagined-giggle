8c4357280a1dbc36cd360973b9f436c85539d525
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:15:51 +0000

    exchange win Itself line cum

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_work(force, distance):
+        return force * distance
