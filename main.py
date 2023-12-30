97f5d8170897899ce13e2b2e9877c1e2169e6dc6
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:51:00 +0000

    post unless trial earn bottom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import array
+def get_array_typecode(array):
+        return array.typecode
