ace9e39da3ae560cde51739a7f775daeaf6c1030
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:36:34 +0000

    thumb change half thumb partner

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
