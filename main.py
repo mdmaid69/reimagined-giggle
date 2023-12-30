ac612da165c5b47d58ff26b11e7fc5d6fdca9885
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:24:41 +0000

    spread embarrassed split from underneath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import time
-def get_current_time():
-        return time.ctime()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
