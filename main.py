9427a7cf0fdaec2a76e24fee865fc1e60ba905d5
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:50:00 +0000

    must careful inform replace giveup

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
