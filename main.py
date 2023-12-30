e646a25b68923f3c86de63288cd7079482ef7e8e
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:51:16 +0000

    range mend measure seem calloff

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
