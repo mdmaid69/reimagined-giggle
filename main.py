16379c5f448e7880ae066fb2a192d8bdc756d126
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:31:56 +0000

    promise hollow terrible ashamed guard

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
