67c45337e3794b6dbdb1327e32a804fbe7762d6e
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:47:42 +0000

    yours grateful shadow debt strict

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+print("Hello, world!")
