8a5574947ec47985fe8605b7a2dfd8629034bd16
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:43:26 +0000

    head hers confident meal lock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
