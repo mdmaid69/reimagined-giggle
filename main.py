dc088f797219078821db1793d5c94f9f335b1dc2
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:14:57 +0000

    takeback grateful extra little so

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import random
+print(random.randint(0, 100))
