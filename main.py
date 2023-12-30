8aa1b28504944bad4bcb0347370276824665970c
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:19:08 +0000

    quarter chin force disapprove goodbye

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
