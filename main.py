b1817870ac32b3995706e80e4c0980bb329bd694
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:52:19 +0000

    star content heat shop software

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
