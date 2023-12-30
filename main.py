11acb66152bdfc89fe425b20584130b259d9404c
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:43:40 +0000

    along earn compete anywhere forward

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
