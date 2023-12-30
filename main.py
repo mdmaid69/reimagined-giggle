d28e7a683bb4a6707a3ee46e59bc074aaf1b4cac
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:37:01 +0000

    tone me tie stage between

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
