1645f500c046e6c0b6d43f1910387f16dc038077
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:46:28 +0000

    smart towards pin moment trap

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
