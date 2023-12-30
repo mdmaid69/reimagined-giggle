2e6cc35f9fd5ffad6bbac77b92739f17f401c21f
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:52:53 +0000

    weekend confusing exchange our deaf

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
