2fdea797ee1cebba99f8d785071f6c08795cfabb
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:26:26 +0000

    patience catch cry power behind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
