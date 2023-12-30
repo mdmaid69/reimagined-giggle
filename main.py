60c2ef1caeb585669bcfd650defd960e1969c02b
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:18:19 +0000

    during extremely refuse rule innocent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
