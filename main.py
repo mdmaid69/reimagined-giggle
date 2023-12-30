4927ec1d1ebae851d0359334be1d8bf99f7538d1
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:05:10 +0000

    remove outside degree large opinion

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
