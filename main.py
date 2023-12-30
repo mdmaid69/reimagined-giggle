54cea743e47f64ad372b3efde164d9d7cb10cf1b
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:03:35 +0000

    live impatient young principle comeup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
