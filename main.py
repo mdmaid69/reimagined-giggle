d1185d06eefce2f91023b31f0f6beab209bc3d3a
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:16:03 +0000

    contact show whenever risk average

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
