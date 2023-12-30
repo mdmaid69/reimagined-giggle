6a4aec8bccd8cd8f9db5afc3b390eb03c4d9803b
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:47:43 +0000

    answer learn perfect clear answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
