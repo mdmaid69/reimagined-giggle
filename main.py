22071a5ad42f8031c039efbc445449d684b343ee
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:57:48 +0000

    official impressive example maybe study

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def find_min(lst):
+        return min(lst)
+def convert_to_hex(n):
+        return hex(n)
