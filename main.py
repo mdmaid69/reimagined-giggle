e8bf8c9b33b97160f90a0942fd696710d5256da3
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:20:05 +0000

    maybe spare beyond fight pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import os
+print(os.getcwd())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
