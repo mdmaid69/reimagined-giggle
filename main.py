c9ce544d135a3f36fd36de1624bf496ee3294d67
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:16:29 +0000

    shock hello plain shut nerve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
