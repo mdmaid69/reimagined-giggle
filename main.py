4526fad19e42f084d623afba623922123c161b42
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:35:29 +0000

    bus goout sleep contact measure

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
