795fb9fc1d4bb269c041531cc7764743dfacaa4e
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:41:15 +0000

    feeling know university quit fortunately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
