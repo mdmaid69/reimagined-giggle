8dbe977fa36c65fff159f78cd8da7a9ad90fc14c
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:50:02 +0000

    yes responsible bus cruelty blame

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-print(math.pi)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
