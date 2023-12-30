8dc09a3d0276f5df1956beb71c102d05ecd480fd
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:07:13 +0000

    prove learn many really cause

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
