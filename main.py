f99a0fbc47781cf670409f3ef04c6c27e5b10195
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:01:24 +0000

    turndown tonight man something 573entertainment

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import random
-print(random.randint(0, 100))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
