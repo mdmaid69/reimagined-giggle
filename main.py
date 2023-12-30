4181bc2e47fadafe13852b0f9f95699fe22f697d
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:08:36 +0000

    whichever afraid back wave remove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
