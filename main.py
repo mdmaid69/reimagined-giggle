0c10a12febcd721204a4c8f9ee621ea0566084d5
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:42:37 +0000

    miss moon everything runover education

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
