6fd79a437f3ccb6ed3336862f35106de96eb60d8
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:43:24 +0000

    charge broad let likely ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
