fa0d453a7b3ff4561b9ee052c2170fa0e305ba1e
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:03:00 +0000

    equal smile fairly message Tt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
