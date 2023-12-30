119b471cae64d4d6bce14408124c20ae602f40eb
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:39:35 +0000

    reply train combine repeat attempt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def subtract_numbers(x, y):
+        return x - y
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
