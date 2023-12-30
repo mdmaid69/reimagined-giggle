f3088c93716de68b4246f811b0ac7e7d4932fb61
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:48:42 +0000

    anxious greeting left vanish suckme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
