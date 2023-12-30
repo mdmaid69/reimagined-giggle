3c715a01a6648bfde360cf864787e5edf6d2df77
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:10:50 +0000

    piece condition precise whatever ear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def find_max(lst):
+        return max(lst)
