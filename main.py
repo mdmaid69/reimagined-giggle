fcdfeadb6be6e617cc5844c68aa1b7f17c48fa1e
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:50:05 +0000

    classroom burst big point glass

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
