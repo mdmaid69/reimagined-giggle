252fda9e35081b298209d6b746d38f95fc96c695
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:58:56 +0000

    suit dear maybe roof plenty

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-def greet(name):
-        print(f"Hello, {name}!")
-print([x**2 for x in range(10)])
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
