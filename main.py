8dcd1aa8bbff9e91129ece97f29bada8d2211f54
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:12:21 +0000

    her before circle 565end estimate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_factorial(n):
+        return math.factorial(n)
+def divide_numbers(x, y):
+        return x / y
