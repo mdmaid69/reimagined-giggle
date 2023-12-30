42e3ad928670b63caeda7f3168ca886ab47456da
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:48:27 +0000

    again rank boat limit one

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-import array
-def get_array_length(array):
-        return len(array)
