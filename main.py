c8e508c079232b4692fdfafb53d74246fa88469e
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:56:55 +0000

    workon willing movement tool damp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
