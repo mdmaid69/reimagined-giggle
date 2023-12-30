7008a07ea9d8071f3a9bf5961b47203ba9ad3c91
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:14:30 +0000

    post consult spite various surface

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
