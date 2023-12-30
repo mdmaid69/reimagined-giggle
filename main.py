1f1a0ee354fe005b12ed47bbfe3d4f250109e144
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:06:56 +0000

    opposite remember swing include callback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import os
-print(os.getcwd())
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
