2604346a67c7715a9967fe6f114807000ac09a97
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:10:54 +0000

    generous none able street on

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_max(numbers):
-        return max(numbers)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
