48b51b5b515934a1a9c9e792198851a1fd4ac4e8
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:00:11 +0000

    curious window bye scrape leave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
