ea780ac32654b9fbeaff93eb896c9ef20683e9a4
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:52:39 +0000

    all this expensive exactly never

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
