15fa5423143575085a9e1d18c1925fece5893baf
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:21:17 +0000

    should fork man release suckme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
