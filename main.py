3477fda51a237f1001da0e029c8ea566a946aae4
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:11:39 +0000

    owe detail taste through fortunate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
