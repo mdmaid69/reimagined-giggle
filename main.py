58177108bf966bb2a0d980bdff071708dd7d37b5
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:04:21 +0000

    treatment discussion busy connect clearly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
