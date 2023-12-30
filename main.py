feee553743cb9e66a56fc4e682e3096bfd4b2e3d
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:16:08 +0000

    water operate load compete shop

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def get_array_length(array):
+        return len(array)
