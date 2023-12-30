ffbe894f72c0a64dd817b47b7a1df189d80e9876
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:53:16 +0000

    intend should emphasize add equipment

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
