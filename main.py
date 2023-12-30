dc65f185deaea84451d80868aab4b3649c7ac781
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:31:07 +0000

    everybody accept waste phone reject

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
