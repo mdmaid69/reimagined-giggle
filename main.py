3467bc6ddfcaa5fd009ee08ed31aba7c3cf2d05d
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:42:19 +0000

    match underneath quarter hard direction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
