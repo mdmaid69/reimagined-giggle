824789e5b9e58aaed7fdc05083065af655b01cf2
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:11:55 +0000

    substance bother pump party together

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
