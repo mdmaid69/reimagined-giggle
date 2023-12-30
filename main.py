48ff168c26a17648913d1f2d68bac692ad2cb1e1
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:35:02 +0000

    man may country invitation turndown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def square_number(x):
+        return x**2
