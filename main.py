e19ca8b4c2b6fb3113157c55ec49b017f5f8acd5
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:45:55 +0000

    apply soft related search track

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
