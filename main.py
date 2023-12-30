2d1cb3e7059f1f2cd42ee5077d13eaccec071e2d
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:51:40 +0000

    consist drop next recent improve

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
