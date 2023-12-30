35c819e22fa65037d1dbc6874a904b59ac25ee45
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:57:04 +0000

    slide spray spend pretend section

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
