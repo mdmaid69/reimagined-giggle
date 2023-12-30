24b81903e1f6b5806f72ef2293e325a8113fcfa6
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:39:39 +0000

    heart recent punish convinced downstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
