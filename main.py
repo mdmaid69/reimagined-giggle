ce0d2c72bba52e726119b37daecb7324588bf7e8
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:42:10 +0000

    damp rate method swimming mention

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import collections
+def create_user_list():
+        return collections.UserList()
+def convert_to_hex(n):
+        return hex(n)
