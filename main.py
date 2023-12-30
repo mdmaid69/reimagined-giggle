d05c60e5220e6c8aad6a182da5ed192c269adc25
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:53:42 +0000

    complete connection drawing avoid full

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import random
-print(random.randint(0, 100))
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
