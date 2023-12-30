eceffa1b300a184b1adc25255931c0de520613d4
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:51:51 +0000

    unite successful ourselves bread mystery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_as_repr(array):
+        return repr(array)
