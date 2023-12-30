ec496e473773bdee0e8c756a02f36fb2cbb987ed
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:07:31 +0000

    good bar sky table classroom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
