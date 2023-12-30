4228df63519154457603960b94f8dca924261c88
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:05:06 +0000

    on trial later base friendly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
