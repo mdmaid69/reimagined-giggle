a295df9de97e2c07f442188bf3f586b8c4986a12
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:20:50 +0000

    piece relax declare empty introduction

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
