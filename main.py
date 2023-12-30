b2018a9d4d9c02a439c42596ee4efed3eee40570
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:21:54 +0000

    thin hit disturbing flow depend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def add_numbers(a, b):
-        return a + b
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
