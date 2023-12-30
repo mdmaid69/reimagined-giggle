6a3021b0956ed2d95e4ad47e99fca0c43fb7503b
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:02:34 +0000

    take high admit swallow thirsty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
