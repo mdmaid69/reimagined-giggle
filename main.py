32b9e618de3cdcbd9a2f01c1b2cf91ce4e574830
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:42:06 +0000

    correct address fear help ball

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
