16d7abd30413a46c27ba22898fb31bf07df05ed9
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:05:59 +0000

    since once steer on progress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def count_words(sentence):
+        return len(sentence.split())
