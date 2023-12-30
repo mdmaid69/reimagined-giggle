9d8b5fff154f0df2f53237c62c8a45c554b0b9ca
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:49:43 +0000

    Kk small keen sometimes work

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import json
+print(json.dumps({"name": "John", "age": 30}))
