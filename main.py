66bb27d9ee093a9c299b40a4c15fb9ebb6fff7c0
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:02:08 +0000

    accommodation shallow far switch tongue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import json
+def convert_to_json(data):
+        return json.dumps(data)
