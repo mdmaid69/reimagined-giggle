6b759e506927ddc14941d3eb7395904d4fbf2a30
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:22:50 +0000

    back support debt sweet rubbish

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
