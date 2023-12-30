42b19509e2fb9eff27e106fa76809594e3ef1eae
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:18:47 +0000

    yesterday open do Hh stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
