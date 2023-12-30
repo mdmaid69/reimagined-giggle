6f84937384fd0659489e5fa905730bc1df34fdff
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:25:41 +0000

    lookafter answer guilty scrap spot

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
