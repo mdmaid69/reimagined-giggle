d6fef5206002cc0595927e17f30c2a653e7501e3
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:20:05 +0000

    congratulate health charge powerful steer

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
