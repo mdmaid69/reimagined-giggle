bca3ac74108a435deec3d784644f86617665dd62
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:48:11 +0000

    ready innocent light approve ground

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
