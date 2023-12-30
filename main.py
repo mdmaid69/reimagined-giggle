a4ee0cd430c5aba1b69ac10173ca1e561c14a9ce
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:18:29 +0000

    few mine shout achievement below

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
