82b1f8b9f6893f14ec9c44638a40f981a2384dd8
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:55:32 +0000

    worst trade goon steer entrance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
