f7af801f2f038e64ac785bf6249f462e701ebb90
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:03:26 +0000

    black obtain dream anywayanyhow him

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
