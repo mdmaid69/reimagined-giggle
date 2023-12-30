f1c8588ee5fe94d9bac3f71e42149bc74d3e072f
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:42:50 +0000

    post content company equal score

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
