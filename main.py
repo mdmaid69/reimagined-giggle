3060f2b1cf23e48ad13a9d3480c269524551eada
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:47:51 +0000

    sensible mind score stiff absolutely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
