381efcdfe20222aeb94f278d6b5a8f3057acd833
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:29:48 +0000

    soon turn lead list foreign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
 import array
-def get_array_typecode(array):
-        return array.typecode
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def get_array_as_list(array):
+        return list(array)
