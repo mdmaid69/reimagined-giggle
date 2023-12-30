ab0e496f9ee206be5bdf92fb03b7ac21871738da
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:37:36 +0000

    attitude that one delicate environment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+text = "Hello, world!"
+print("Characters:", len(text))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
