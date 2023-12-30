22e036663eb0a49ae10fd69f5832e07894b3d153
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:08:07 +0000

    hole either moon not paper

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def convert_to_octal(n):
+        return oct(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
