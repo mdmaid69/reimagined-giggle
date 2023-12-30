be40ea5292e6da50acca46d64d8a2fc508c3dcd6
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:33:35 +0000

    plan wild introduce tough meet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
