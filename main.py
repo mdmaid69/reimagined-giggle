0abec2d67167bc8581e022964accb1e8b7cb2651
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:11:32 +0000

    excuse shop nature cause runinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-print("Hello, world!")
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def find_unique_words(sentence):
+        return set(sentence.split())
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
