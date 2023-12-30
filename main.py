e2f6f468382dd93aabc1065efe3c7ff7591c0adc
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:36:05 +0000

    short warning pride too Mm

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
