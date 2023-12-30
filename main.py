b1d3f0f430562ea408712d1668953fb118012774
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:36:41 +0000

    calmly before repair defend desperate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
