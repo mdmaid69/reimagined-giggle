89f196f30a5516e7a0bdc9698201e2c6e5f25dd5
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:19:46 +0000

    up begin steady shop getoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
