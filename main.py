32b9644b254171061f37902fc2f76d9208cb9632
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:34:53 +0000

    minimum find begin winter comfortable

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
