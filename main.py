effd506daeef669fe547ef14079095c705882db0
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:26:39 +0000

    uncertain too wave connection still

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def is_even(n):
-        return n % 2 == 0
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
