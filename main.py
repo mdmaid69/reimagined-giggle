dd0dd1e73c1528200894fc32d8763a66ec193e60
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:10:34 +0000

    block choose tooth house powerful

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
