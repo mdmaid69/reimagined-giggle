285ae7541657997132b954d40e7c635a414c6244
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:59:41 +0000

    noone belong now hot worst

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
