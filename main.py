49a609352f2811f06e7b391c5760b2c3d0a3b173
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:16:31 +0000

    calmly mine stir string peace

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_max(numbers):
+        return max(numbers)
