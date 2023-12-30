bcc8dab6b9122add049c5ce4a432dd464606e451
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:43:05 +0000

    organize deliberate any twist emphasize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
