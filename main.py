c88baefbdedf342afb320c0556c5acde2900b7f3
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:46:09 +0000

    keepaway pump place block public

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
