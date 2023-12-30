09a52785670f07bc142589e58c5fb335a31eea74
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:32:02 +0000

    until private argument more hungry

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
