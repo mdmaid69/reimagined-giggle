cac4cd6d28de37bb017268005af45c80485e6104
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:10:19 +0000

    film loan whether perform storm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def subtract_numbers(x, y):
+        return x - y
