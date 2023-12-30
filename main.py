2a62027a65e54f4b621e59be5e8f99ca85672bd7
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:55:51 +0000

    Gg fortunate light same turninto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import math
+print(math.pi)
