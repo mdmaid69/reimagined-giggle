c6a788f9fe13514a72da4eb77a641ce6a8fc158e
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:59:29 +0000

    shop message ability yourself pipe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import re
+print(re.match("h.*o", "hello world"))
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
