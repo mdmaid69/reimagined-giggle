68771c5512b979988e51801f80fe484fa78576a9
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:56:02 +0000

    sentence ready true reliable afraid

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
