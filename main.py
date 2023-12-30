0c7ac75c5db572c2b0abb14a0cf2d38e7e5833ed
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:40:46 +0000

    collect succeed pool all note

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
