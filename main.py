bfafcf17dcd178a228e16ad3f098e40d4ea045dd
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:01:21 +0000

    late straight market target while

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def reverse_list(lst):
+        return lst[::-1]
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
