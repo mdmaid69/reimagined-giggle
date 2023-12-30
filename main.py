1395a1c45b511e425a2ef4aff569bf2e862c8b53
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:17:54 +0000

    machine live health march become

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
