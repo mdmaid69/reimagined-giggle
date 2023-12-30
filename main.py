f0eb11c4186830a4e01473659783ea258eb78eb5
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:41:42 +0000

    might terrible nowhere copy recover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_stack():
+        return collections.deque()
