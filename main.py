826fc26725143653c4f1dc761468d1e7cd86bf25
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:17:56 +0000

    ought path pick year load

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def subtract_numbers(x, y):
+        return x - y
+def add_numbers(x, y):
+        return x + y
