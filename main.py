abe3fb0b4b4e0de5a36fe11990cc3e1c92e12572
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:18:35 +0000

    produce cry through image judge

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  def count_elements(lst):
+        return len(lst)
