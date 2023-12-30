d707a896a5b171b0d5023cff4e1b026c7758d0cc
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:20:04 +0000

    goon along exhibition order interfere

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
