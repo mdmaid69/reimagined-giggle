28795e4eed18d38dfceb2c169ecc89df23c1fea9
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:32:12 +0000

    inside stretch official soon through

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
