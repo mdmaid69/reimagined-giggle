1aef8ab9961afa3a93938da9387c12f2ec47d7b7
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:05:45 +0000

    rank brush home school situation

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_max(lst):
-        return max(lst)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def convert_to_binary(n):
+        return bin(n)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
