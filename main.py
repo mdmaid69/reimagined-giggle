c8fcd15dab8d844d4c3f6165e6c799900031d044
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:16:03 +0000

    include guilty number opposite escape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def add_numbers(x, y):
-        return x + y
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
