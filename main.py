a16e7836240a8679e5b2c2e9eb35b52bd2afb77c
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:08:52 +0000

    operation load course holdup breakin

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
