9de31198634c186a083823643e9ac63fb8c5c505
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:38:29 +0000

    odd correct it phone broad

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
