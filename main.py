dd3fd2fa23d5887301819151543a7916bea28d91
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:40:39 +0000

    less style activity offer lonely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
