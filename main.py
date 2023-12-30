f4dcde4a2518ea387163f5abd2446503f629d086
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:35:03 +0000

    justice own teach Dd personal

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print(sum(range(10)))
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import time
-print(time.time())
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
