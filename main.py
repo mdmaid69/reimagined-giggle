b52b0ed21371b878fa8797bb78708cdd94a3fa89
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:22:32 +0000

    recent better home trick find

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_min(lst):
+        return min(lst)
