6dfa43874b795c29b42c60559476c9c3605a5893
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:58:12 +0000

    increase slip apparently door arrival

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import re
+print(re.match("h.*o", "hello world"))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
