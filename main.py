af6325fc7211dfab5c0e832620a907a1a3ed8699
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:53:31 +0000

    climb gooff sing dear match

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def is_odd(n):
+        return n % 2 != 0
