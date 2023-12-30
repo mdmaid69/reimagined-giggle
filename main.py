1ef8f9e5449029c4aebe0c6c420c9d2746f24c9a
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:43:40 +0000

    club shame really it doubt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
