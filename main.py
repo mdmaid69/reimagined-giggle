19ec089c65dcc1339e32ba94427da3ee8ff2dc89
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:58:03 +0000

    pause keepout freedom wire fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
