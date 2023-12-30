e2aaabf960ae1637bcd4e81861af9a49b9bfca14
mdmaid69 <robot@alexsyw.me>
2023-12-30 13:55:12 +0000

    under live goon suppose lucky

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def reverse_list(lst):
+        return lst[::-1]
+import sys
+print(sys.version)
