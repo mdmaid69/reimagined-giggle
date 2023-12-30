6bb8043451aa8d6c90a48235759b6ab55c2f641b
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:04:32 +0000

    ourselves emphasize makeinto popular behind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
