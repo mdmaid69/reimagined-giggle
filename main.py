4eeae0129f4e4390206aa3a123fc5a502ad28e0d
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:02:16 +0000

    against the fit load sure

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def is_odd(n):
-        return n % 2 != 0
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
