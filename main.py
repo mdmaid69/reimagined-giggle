d2e89245751a91ab7717f77296af0e0d7f152823
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:55:25 +0000

    basis perhaps keepfrom worried buy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  def is_even(n):
+        return n % 2 == 0
