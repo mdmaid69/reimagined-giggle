ca60d72f367e18b31a1ff40f2a721eaeea2294b9
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:04:31 +0000

    hour instant describe power snow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
