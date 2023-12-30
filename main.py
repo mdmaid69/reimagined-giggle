57d439f5dc3af04b6510d32ee248ea0154d30919
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:09:47 +0000

    no weigh male keep language

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
