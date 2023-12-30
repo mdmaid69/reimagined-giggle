ad65392de51ed3359b294ecb005ea4a8cbae8cbd
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:56:40 +0000

    speed damp terrible manner weigh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
