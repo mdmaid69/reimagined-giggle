c2fc9bccff46c9cffcabf2eb232060263978f5c5
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:25:11 +0000

    correct argue lock wear suggestion

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
