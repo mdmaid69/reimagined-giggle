5d637030fb5c1cca5843f5db843100739ace4291
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:04:40 +0000

    save condition freedom give angry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import re
+print(re.match("h.*o", "hello world"))
