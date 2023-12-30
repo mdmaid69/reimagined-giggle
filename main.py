84ac1db9fadf3e644f48a0b8eade16a82cc08ab6
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:25:34 +0000

    naturally hand just burst stick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
