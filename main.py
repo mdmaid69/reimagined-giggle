6427c4fad68042694dc1092c7be1b613a4ca0fa2
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:55:19 +0000

    past allow apparently freeze favour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
