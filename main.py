3a555e4ef343e1448a6babf54c682ac2ad603992
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:00:30 +0000

    Ww trade problem noisy frightened

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import getpass
+def get_username():
+        return getpass.getuser()
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
