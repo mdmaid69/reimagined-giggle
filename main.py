d273a8ffc6f28844074b06b48f81e04d3d376986
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:20:50 +0000

    then Bb goback(to) communicate limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def get_string_from_array(array):
+        return array.tobytes()
