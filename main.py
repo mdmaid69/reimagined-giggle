a9fb9798e92d833efc707da510bce4bd4bc5eadc
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:58:37 +0000

    remark since influence calloff imagination

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
