316c43da01db772f6057933f1203dc69cd3d7b3d
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:50:19 +0000

    wash gift so bar its

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_item(array, i):
+        return array[i]
+  def subtract_numbers(x, y):
+        return x - y
