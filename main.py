6bf1606fae0c2fa322ff2c3b488e3953d8b48fb3
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:09:03 +0000

    house project measure temper similar

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import random
+print(random.randint(0, 100))
+def remove_duplicates(lst):
+        return list(set(lst))
