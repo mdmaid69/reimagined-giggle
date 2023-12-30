9dcc07d59fff3e61022b9bc42b3a2e7ab5d39cdb
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:46:02 +0000

    he wipe drawing involve mind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def divide_numbers(x, y):
+        return x / y
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
