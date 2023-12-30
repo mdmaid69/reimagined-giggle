ddbc40733693455027ff1757ed7c9cb17878463f
mdmaid69 <robot@alexsyw.me>
2023-12-30 11:46:37 +0000

    contents phone storm whom winter

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def is_even(n):
+        return n % 2 == 0
