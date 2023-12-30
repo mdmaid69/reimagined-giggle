4d17d1174fb76ecc958e34578410198035977cce
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:23:04 +0000

    wrong thought fair Cc anxious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_cosine(x):
+        return math.cos(x)
