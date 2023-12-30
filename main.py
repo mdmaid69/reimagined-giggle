c145be2faa8c7cb49a371fecb6da4e39a1a75941
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:26:50 +0000

    even waste fix below explain

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_user_string():
-        return collections.UserString()
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def calculate_density(mass, volume):
+        return mass / volume
