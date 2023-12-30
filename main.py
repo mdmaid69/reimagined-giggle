abed4b655b9fe4aaaae927cb33ab960ca550702b
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:23:00 +0000

    people bank smoke themselves next

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
