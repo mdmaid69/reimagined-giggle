cbc504e1c67f4532eea6c32cdde4c911dc9e78b1
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:40:19 +0000

    address far mysterious condition possession

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import numpy as np
+print(np.array([1, 2, 3]))
