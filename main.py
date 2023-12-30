b0c483a08411ed6a0bcc15a704d9f05cac54d36a
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:32:52 +0000

    tour rough ordinary usual black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
 import collections
-def create_priority_queue():
-        return collections.deque()
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
