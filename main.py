5565e35a949a660db02bda4a1376a98308d5f922
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:15:45 +0000

    eager pot convenient easy considering

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
