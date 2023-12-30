2671a492999135aae70874b40d37c4dc185709fc
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:44:00 +0000

    frequent wrong call brush interfere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
