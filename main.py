b77c4c17b041bf1b98f43e997456c8a85a2658b9
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:01:19 +0000

    catch increase learn show report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
