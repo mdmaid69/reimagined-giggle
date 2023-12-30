5f696d626edd0540c8fa5947e13d85c8ab133124
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:15:53 +0000

    criticize there remark trust promise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_priority_queue():
-        return collections.deque()
+def sort_numbers(numbers):
+        return sorted(numbers)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
