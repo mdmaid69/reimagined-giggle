5066d2c97470d8f09ffae112b649cdd99051e47b
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:42:35 +0000

    start niggaacademy union bringforward Cc

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
