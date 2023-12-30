5c65933da567bddeb98d1217b8a36ddb0998a901
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:14:53 +0000

    tax favourite declaration bend funny

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
