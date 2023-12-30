3e58488554472c9be1100704e39206169b977cd9
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:20:37 +0000

    environment content cost holdout organize

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
