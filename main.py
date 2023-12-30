71e72b30d82e5de155ebd8d46d2e0d3043217eab
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:31:07 +0000

    body file piece cruel let

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_str(array):
+        return str(array)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
