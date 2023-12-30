0a60b0288be9378ed5412e9bc7dbfbfa310c2e1b
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:10:59 +0000

    wash unit raise nose objection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
