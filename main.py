e4e7496cf512c1a37683770e64e654dbe474a54a
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:46:02 +0000

    open save fine sea bread

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
