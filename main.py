56e355286fed691dd751d5b345afe5b51b639fb3
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:46:15 +0000

    move transparent now comefrom claim

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
