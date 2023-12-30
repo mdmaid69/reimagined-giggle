94ee6b314c0b10dccca79004e20712b3589f063a
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:30:50 +0000

    scheme workupto with guilty sick

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+print("Hello, world!")
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
