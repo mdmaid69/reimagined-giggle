05fae6245413af65c4082fff36364dcf8b14a359
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:08:33 +0000

    impression smell operate greeting alive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-name = "Python"
-print("Hello,", name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
