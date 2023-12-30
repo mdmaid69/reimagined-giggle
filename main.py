1d68ee93e0c456c7eb4d4d17268a28c280a0f048
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:41:57 +0000

    person peace permission under except

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import collections
+def create_priority_queue():
+        return collections.deque()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
