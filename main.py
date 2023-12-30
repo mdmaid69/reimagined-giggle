8204f50bf9937e7382fe915bbf16bb51329cb541
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:24:16 +0000

    back somehow OKorokay sticky truth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
