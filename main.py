eb546c61e8996f535628f308ffe11f141c430c82
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:59:53 +0000

    appeal period slice post doctor

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import array
-def append_to_array(array, item):
-        array.append(item)
+def subtract_numbers(x, y):
+        return x - y
+  def convert_to_binary(n):
+        return bin(n)
