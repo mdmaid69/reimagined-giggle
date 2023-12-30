7b2af4d4c838c3e08c4cd03f5b7ec4bf14ab295c
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:56:26 +0000

    raise geton speak forget cope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
