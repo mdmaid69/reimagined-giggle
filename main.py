909c9b7ad4a0ebfbb001fd818a74f25ac19a8d30
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:40:47 +0000

    choose definite survive below as

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
