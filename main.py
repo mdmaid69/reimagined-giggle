ee90cc9c3922d0d3b54a1c0746fbc287e6340603
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:47:25 +0000

    computer include me loan naturally

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_volume(length, width, height):
+        return length * width * height
