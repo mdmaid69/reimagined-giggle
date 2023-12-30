4125a7db8b4519e701801c23932877583218d6b7
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:27:42 +0000

    gas he evening point 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
