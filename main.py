e31511a4954f6a6944a903e21725dfd6b2427174
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:16:21 +0000

    physical fix side stamp tongue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def reverse_list(lst):
+        return lst[::-1]
