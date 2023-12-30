17e121c654fa60dbec4aeffc874de0b2a7bf6174
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:07:09 +0000

    favourite shrink relation opposite exactly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def cube_number(x):
-        return x**3
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def iterate_over_array(array):
+        for item in array:
+        print(item)
