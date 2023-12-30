33fdeafa88bc4a707329910289d695ee0a77d53e
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:19:54 +0000

    tongue reduce occasional left respect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import datetime
+def get_current_date():
+        return datetime.date.today()
