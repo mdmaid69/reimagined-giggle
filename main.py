ab3a0c8919f4daf96edf9ddbb1bdfd6aa72a855d
mdmaid69 <robot@alexsyw.me>
2023-12-30 19:40:53 +0000

    precise this loud alone weak

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def cube_number(x):
+        return x**3
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
