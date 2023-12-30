5ae573e1a143f3d490c4f041136a6fea9248d269
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:36:49 +0000

    tomorrow him guide class constantly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def reverse_list(lst):
+        return lst[::-1]
