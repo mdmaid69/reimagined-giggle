e4c8db5439421da0472887e46c234f48a4b297ce
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:45:58 +0000

    right story drawing children up

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-def calculate_power(work, time):
-        return work / time
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
