938c976f69da9135f8a798a9033112e549f8045b
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:09:54 +0000

    tongue back ring fast police

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
