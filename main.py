89deff91566e9ce3554d86819e618b1eb1a28eb9
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:02:17 +0000

    shopping runout true term jump

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
