c484d6e0ebc3e1add97234b24c25632ca8568901
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:01:05 +0000

    work exaggerate neither sister flood

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-print([x**2 for x in range(10)])
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
