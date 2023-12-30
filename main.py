a030ebb9d0ab8679ebe2465aedeaedc0290320fc
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:37:46 +0000

    argument bush test fall children

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
