e61d403cdfd75742c0e82a1488d0092ca72db447
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:49:59 +0000

    geton tragedy divide politics nowhere

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
