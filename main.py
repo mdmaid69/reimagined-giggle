963d2b427b9cb7d170fe09a068b36ce03aa0d840
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:21:02 +0000

    invite road imagine roll lie

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
