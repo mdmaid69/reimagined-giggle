6df6d90ecc705028897a8a6ecc009b91e4d37f27
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:54:25 +0000

    edge murder lookaround listenfor people

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+x = 10
+y = 20
+print("Sum:", x + y)
