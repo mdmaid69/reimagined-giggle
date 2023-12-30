0a788d7ec676a4aecc0845d2b20c6d946510f4ce
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:44:20 +0000

    like attitude may task ordinary

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  def is_even(n):
-        return n % 2 == 0
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+print("Hello, world!")
