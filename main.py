47b58386ae5780889f69b0b022b33afb4e5e9b0e
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:05:58 +0000

    comeout consist region disapprove funny

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
