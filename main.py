4ff21f040a584eccc4580578e236f7bf4c097dea
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:50:29 +0000

    reasonable autumn trousers mix cover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
