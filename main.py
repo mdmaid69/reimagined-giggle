b8ad00dbfdcd0aa9657012be6f748a78531f7371
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:19:24 +0000

    pin business 570enjoyable it brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
