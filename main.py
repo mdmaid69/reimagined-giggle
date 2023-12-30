04df3ab8e4242126fe66f4ece21a19c0a6013230
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:14:09 +0000

    health comeround nerve upstairs Pp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
