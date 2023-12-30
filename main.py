3b132829b711eb7304fcbc2087129390aa1fa966
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:21:20 +0000

    Zz unfortunate rest software dinner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+for i in range(5):
+        print(i)
