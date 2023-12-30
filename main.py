9ce871f744cc0b9386381ac2e3528ccf6779570a
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:02:04 +0000

    where watch finish finger every

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
