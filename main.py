982ba904956b148c394571ddd1b0e065b5fe61ea
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:46:02 +0000

    quantity keepfrom already well occur

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
