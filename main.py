7ebbb891fc34231448d1a196dea9f26a32473d6e
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:22:22 +0000

    delicate nature answer muscle remove

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def reverse_list(lst):
-        return lst[::-1]
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
