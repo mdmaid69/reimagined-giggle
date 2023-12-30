908320d4710f982290c4f90c6bb37f3b859f433b
mdmaid69 <robot@alexsyw.me>
2023-12-30 14:13:59 +0000

    suit correction wait inform pause

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def convert_to_octal(n):
-        return oct(n)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
