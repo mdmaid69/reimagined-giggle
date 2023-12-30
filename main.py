8e1d643928579db5d7c787c0f8cc8af0f3964c57
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:03:01 +0000

    convince exam quantity measure wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
