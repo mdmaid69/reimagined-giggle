eb79780a147e1c18b2806da3195db10ad40fdbab
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:49:21 +0000

    like suckme wheel form do

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_speed(distance, time):
-        return distance / time
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
