0e38718d02dab3f0567f3852be41cd8a66b9c362
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:01:32 +0000

    environment buy earn organize squeeze

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
