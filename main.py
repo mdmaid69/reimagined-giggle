07113fe060451fecf4bbf22d2d665d4e55c6f38f
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:20:53 +0000

    spite port like education century

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
