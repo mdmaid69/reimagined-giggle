bb26144302c57023ac489488469e8d82b70fc857
mdmaid69 <robot@alexsyw.me>
2023-12-30 03:14:25 +0000

    ahead total what poor delight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def cube_number(x):
-        return x**3
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def sort_numbers(numbers):
+        return sorted(numbers)
