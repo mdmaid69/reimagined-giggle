f70087070cf397a10b0ceda050906d3431bc6a0b
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:41:44 +0000

    talk though feature arrive brief

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
