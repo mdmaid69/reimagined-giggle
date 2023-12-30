5cfab0dbac784e407e5f949f1bca3250cf2a7ef0
mdmaid69 <robot@alexsyw.me>
2023-12-30 05:56:02 +0000

    ocean second apart least difficult

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
