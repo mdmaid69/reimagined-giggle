99a0a4c5d5935828ad2aaed853ce2aebd41de955
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:18:12 +0000

    call read admit bridge wild

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import os
-print(os.getcwd())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
