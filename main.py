ab299b13b727e148dd4789c6e6faeebade5eb96c
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:41:16 +0000

    moment collect yours born tragedy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
