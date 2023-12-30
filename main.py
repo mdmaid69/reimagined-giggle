e87ed1f7711627a8254291ddd300e9db3b57a338
mdmaid69 <robot@alexsyw.me>
2023-12-30 02:20:51 +0000

    bring bunch year country aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
