22aaddca7c3e33779ce7bc337af59341983ce151
mdmaid69 <robot@alexsyw.me>
2023-12-30 21:14:29 +0000

    choice release fond whether short

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
