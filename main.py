40eba12d9cca542e005eb64bf34e252fc656d956
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:31:44 +0000

    behind talkinto combination inquire traffic

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def get_current_time():
-        return time.ctime()
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
