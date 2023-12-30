0faeaf20bdbead59eed95a841c20ac3a96a745ef
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:57:27 +0000

    clever permission record land accidental

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+print(sum(range(10)))
