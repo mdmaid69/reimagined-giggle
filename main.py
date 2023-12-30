c7861873c8f23b6e5ace86a2ef949c4d7af9da57
mdmaid69 <robot@alexsyw.me>
2023-12-30 07:34:26 +0000

    blow to whoever claim inquire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
