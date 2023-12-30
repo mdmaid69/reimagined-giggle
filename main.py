de6d354a8424f253a13518374c45ee0647f70232
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:16:41 +0000

    happy feed mark strange book

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import math
+print(math.pi)
+import math
+print(math.pi)
