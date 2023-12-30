f4a5378cce1fd4522f0db4fe36eb2d6728892747
mdmaid69 <robot@alexsyw.me>
2023-12-30 00:25:53 +0000

    plain mixture cover ashamed good

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_unique_words(sentence):
+        return set(sentence.split())
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
