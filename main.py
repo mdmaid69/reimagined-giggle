118414b9d7f816fa9a6108d32a48ab1d1c1c95d2
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:02:41 +0000

    storm tape judge proof keepup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
