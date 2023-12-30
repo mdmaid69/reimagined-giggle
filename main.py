046d188e114cafde87fdd19028c9aed22fd56178
mdmaid69 <robot@alexsyw.me>
2023-12-30 10:26:30 +0000

    suffer post square share middle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
