5ddc091cac08bcde49a0d81df384c3a684b59528
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:29:57 +0000

    miss instead hole shop spring

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def sort_list(lst):
+        return sorted(lst)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
