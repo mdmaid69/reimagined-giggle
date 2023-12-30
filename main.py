18c7a8bc57677ef2f59ab6410cc29bd64446d03b
mdmaid69 <robot@alexsyw.me>
2023-12-30 17:35:38 +0000

    waste past mind like peaceful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
