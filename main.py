b7a0a789d6997b78d3ed22e81f23a9491ab5ebf1
mdmaid69 <robot@alexsyw.me>
2023-12-30 23:23:10 +0000

    slip keen dress negative escape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-print("Hello, world!")
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
