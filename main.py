d18ff419874c5022c716594abd5e4b054a613cff
mdmaid69 <robot@alexsyw.me>
2023-12-30 08:05:48 +0000

    occasion tough smell university makeup

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_item(array, i):
+        return array[i]
+def add_numbers(a, b):
+        return a + b
