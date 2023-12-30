921cc5983dbfaf4e318a83dd9052bbd56a3fb314
mdmaid69 <robot@alexsyw.me>
2023-12-30 06:59:07 +0000

    advanced try middle laugh lot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_bool(array):
+        return bool(array)
