b4c17a8026752d319daab3d14ec981b47c875f63
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:41:35 +0000

    involve more zero defend survive

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+print(sum(range(10)))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
