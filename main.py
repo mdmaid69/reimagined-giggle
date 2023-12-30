77d8e3938ba017cee753addbddfebb82e6c79570
mdmaid69 <robot@alexsyw.me>
2023-12-30 12:05:12 +0000

    only platform dream top concern

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  def square_number(x):
-        return x**2
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
