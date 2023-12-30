cce699613f49a95a1e87cd35c10da47e3a5c42bc
mdmaid69 <robot@alexsyw.me>
2023-12-30 16:56:36 +0000

    read quickly lean search undo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import time
-print(time.time())
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
