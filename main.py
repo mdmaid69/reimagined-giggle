5dec146a076fed0920858ecf70e4d59bd5e2424f
mdmaid69 <robot@alexsyw.me>
2023-12-30 01:10:43 +0000

    Hh know our discover suggestion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def reverse_array(array):
+        array.reverse()
