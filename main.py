3ff9af94489e00f9fe18bb3d80f3ca2f5b7e028b
mdmaid69 <robot@alexsyw.me>
2023-12-30 20:05:53 +0000

    door spite lose honest warn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
