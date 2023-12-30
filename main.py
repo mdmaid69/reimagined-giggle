51685633585fc7685142683a9f691146fe0075c6
mdmaid69 <robot@alexsyw.me>
2023-12-30 18:47:58 +0000

    back window about across flat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
