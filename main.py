8b747473af212c1cd78218ac8156aad3e6aeb85b
mdmaid69 <robot@alexsyw.me>
2023-12-30 22:18:06 +0000

    surface lonely formal have breakdown

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
