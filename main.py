61114bd4864f79e889c184e56bd28a47e13b7f5b
mdmaid69 <robot@alexsyw.me>
2023-12-30 15:52:55 +0000

    admire police too convincing print

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
