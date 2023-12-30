64d178392bd56b56a101c7f503e38272a84ab2a8
mdmaid69 <robot@alexsyw.me>
2023-12-30 04:58:08 +0000

    strange cry strange hardly disagree

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  def convert_to_hex(n):
+        return hex(n)
