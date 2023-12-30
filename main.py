93f3a53ace00b69cb9a14d3773ffa4adf7824ef9
mdmaid69 <robot@alexsyw.me>
2023-12-30 09:49:04 +0000

    better Cc without nigga quit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
